6e3882e121e8b7404a0a1a1b5bdfe2fb681555db
Yoshinori Okuji <yo@nexedi.com>
2004-01-22 09:05:05 +0000

    Make the user a variable and change the directory explicitly.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@237 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/run_test_example.sh
+++ product/ERP5Type/tests/run_test_example.sh
@@ -1,3 +1,8 @@
-export INSTANCE_HOME=/home/jp/zope
+export INSTANCE_HOME=/home/$USER/zope
 export SOFTWARE_HOME=/usr/lib/zope/lib/python/
+
+dir="`dirname $0`"
+if test -n "$dir"; then
+  cd $dir
+fi
 python runalltests.py
