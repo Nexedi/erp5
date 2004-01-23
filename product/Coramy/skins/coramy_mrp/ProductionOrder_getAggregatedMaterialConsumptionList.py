6ef154f38132c8ba1d833694458bae9692c7a5b1
Jean-Paul Smets <jp@nexedi.com>
2004-01-23 15:59:40 +0000

    improved by JPS
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@290 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/Coramy/skins/coramy_mrp/ProductionOrder_getAggregatedMaterialConsumptionList.py
+++ product/Coramy/skins/coramy_mrp/ProductionOrder_getAggregatedMaterialConsumptionList.py
@@ -10,6 +10,8 @@
 movement_list = context.getOrderRelatedMovementList()
 movement_uid_list = map(lambda o:o.getUid(), movement_list)
 if len(movement_uid_list) > 0 :
-  return context.ProductionOrder_zGetAggregatedMaterialConsumptionList(order_related_movement_uid_list = movement_uid_list, query=kw.get('query', None))
+  return context.ProductionOrder_zGetAggregatedMaterialConsumptionList(order_related_movement_uid_list = movement_uid_list, 
+                                                                       query=kw.get('query', None), 
+                                                                       at_date=context.getStartDate().Date())
 else :
   return []
