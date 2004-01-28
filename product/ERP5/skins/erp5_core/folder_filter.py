fbae6898a3a06294a5b6129243e417fa8312f88d
Jean-Paul Smets <jp@nexedi.com>
2004-01-28 11:57:07 +0000

    first implementation of multi page checkd uids
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@363 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/folder_filter.py
+++ product/ERP5/skins/erp5_core/folder_filter.py
@@ -1,8 +1,23 @@
-##parameters=selection_name, uids
+##parameters=selection_name, uids=[], listbox_uid=[]
 
 
 request = context.REQUEST
-context.portal_selections.setSelectionToIds(selection_name, uids, REQUEST=request)
+
+# Set selection to currently checked items, taking into consideration changes in uids
+selection_uids = context.portal_selections.getSelectionCheckedUidsFor(selection_name, REQUEST=request)
+filtered_uid_dict = {}
+listbox_uid = map(lambda x:int(x), listbox_uid)
+uids = map (lambda x:int(x), uids)
+for uid in uids:
+   filtered_uid_dict[uid] = 1
+for uid in selection_uids:
+  if uid in listbox_uid:
+    if uid in uids:
+      filtered_uid_dict[uid] = 1
+  else:
+    filtered_uid_dict[uid] = 1
+context.portal_selections.checkAll(selection_name, uids, REQUEST=None)
+context.portal_selections.setSelectionToIds(selection_name, filtered_uid_dict.keys(), REQUEST=request)
 url = context.portal_selections.getSelectionListUrlFor(selection_name, REQUEST=request)
 
 request.RESPONSE.redirect(url)
