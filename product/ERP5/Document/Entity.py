453acd2f87a8ba6cf484c0ea6b309e2cfb1178fc
Jean-Paul Smets <jp@nexedi.com>
2004-02-08 17:21:06 +0000

    fixed Solanes typo getDefaultRegion removed (implemented by acquisisiotn system)
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@409 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/Entity.py
+++ product/ERP5/Document/Entity.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
 # Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
-#                    Jean-Paul Smets-Solane <jp@nexedi.com>
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
@@ -56,16 +56,6 @@ class Entity:
         """
         try:
           return self.getDefaultAddressValue().asText()
-        except:
-          return ''
-
-    security.declareProtected(Permissions.View, 'getDefaultRegion')
-    def getDefaultRegion(self):
-        """
-          Returns the default address as a text string
-        """
-        try:
-          return self.getDefaultAddressValue().getDefaultRegion()
         except:
           return ''
 
