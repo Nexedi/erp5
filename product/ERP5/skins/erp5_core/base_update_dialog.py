e2b92217fe487ce8affa28694ef0fdd8bd432232
Sebastien Robin <seb@nexedi.com>
2004-03-29 22:03:14 +0000

    use base_dialog instead of base_view_dialog
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@631 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/base_update_dialog.py
+++ product/ERP5/skins/erp5_core/base_update_dialog.py
@@ -3,6 +3,5 @@
 # This script is only used in order to add a third button on
 # dialog pages wich is only used for udpates
 
-
 dialog_method = update_method
-return context.base_view_dialog(form_id,cancel_url,dialog_method,selection_name,dialog_id)
+return context.base_dialog(form_id,cancel_url,dialog_method,selection_name,dialog_id)
