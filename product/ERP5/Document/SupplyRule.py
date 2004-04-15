55619bf0f35ed48fe0a2b32507934b5c7223730a
Jean-Paul Smets <jp@nexedi.com>
2004-04-15 16:22:49 +0000

    supply rule allows to setup supply systelm
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@676 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/SupplyRule.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Globals import InitializeClass, PersistentMapping
+from AccessControl import ClassSecurityInfo
+
+from Products.CMFCore.WorkflowCore import WorkflowAction
+
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+from Products.ERP5Type.XMLMatrix import XMLMatrix
+from Products.ERP5.Document.MappedValue import MappedValue 
+
+class SupplyRule(MappedValue, XMLMatrix):
+    """
+      Supply rule contains mapped values (if required) for each possible
+      variation of a resource. Structure should be similar to
+      transformed resource (domain / category membership)
+    """
+
+    meta_type = 'ERP5 Supply Rule'
+    portal_type = 'Supply Rule'
+    add_permission = Permissions.AddPortalContent
+    isPortalContent = 1
+    isRADContent = 1
+
+    # Declarative security
+    security = ClassSecurityInfo()
+    security.declareObjectProtected(Permissions.View)
+
+    # Declarative properties
+    property_sheets = ( PropertySheet.Base
+                      , PropertySheet.XMLObject
+                      , PropertySheet.CategoryCore
+                      )
+
+    security.declarePrivate( '_edit' )
+    def _edit(self, **kw):
+      """
+      Solve here some constraints
+      """
+      ERP5MappedValue._edit(self, **kw)
+      self.setPredicateValueList(self.getCategoryMembershipList(('taille','coloris'), base=1))
+
+    security.declareProtected( Permissions.ModifyPortalContent, 'edit' )
+    edit = WorkflowAction( _edit )
