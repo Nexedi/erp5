22c26d1978e1545bcd8107bb770d238a1b43e258
Sebastien Robin <seb@nexedi.com>
2004-04-21 15:14:37 +0000

    check if there is listbox_key
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@712 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Base_updateDialogForm.py
+++ product/ERP5/skins/erp5_core/Base_updateDialogForm.py
@@ -3,17 +3,22 @@
 from string import zfill
 request = context.REQUEST
 
+from string import zfill
+
 for k in kw.keys():
   v = kw[k]
-  listbox = {}
-  if k == 'listbox_lines':
-    i = 1
-    for line in v:
-      #key = '_%s' % zfill(i,3)
-      key = '_%s' % line['listbox_key']
-      listbox[key] = line
-      i+=1
-    request.set('listbox',listbox)
+  if k == 'listbox':
+    listbox = {}
+    if v is not None:
+      i = 1
+      for line in v:
+        if line.has_key('listbox_key'):
+          key = '%s' % line['listbox_key']
+        else:
+          key = str(zfill(i,3))
+        listbox[key] = line
+        i+=1
+      request.set('listbox',listbox)
   else:
     request.set('my_%s' % k, v)
 
