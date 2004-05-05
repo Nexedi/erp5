efd1eac6a53c1dccadff0439c953ec2f8edc4be1
Sebastien Robin <seb@nexedi.com>
2004-05-05 12:44:34 +0000

    added simulation tool
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@840 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/ERP5Site_printObjectPath.py
+++ product/ERP5/skins/erp5_core/ERP5Site_printObjectPath.py
@@ -8,6 +8,9 @@ for o in site.objectValues():
       object_ids = o.objectIds()
       for id in object_ids:
         object_list += [path + (id,)]
+path = site.portal_simulation.getPhysicalPath()
+for id in site.portal_simulation.objectIds():
+  object_list += [path + (id,)]
 object_list.sort()
 for path in object_list:
   print '/'.join(path)
