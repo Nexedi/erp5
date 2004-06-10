dd5d38c7bdf1c474977b976cdf994770cfa0d1fa
Sebastien Robin <seb@nexedi.com>
2004-06-10 16:45:32 +0000

    add object workspace and section
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@996 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5CPS/Extensions/Site_resetDocids.py
+++ product/ERP5CPS/Extensions/Site_resetDocids.py
@@ -1,5 +1,55 @@
-def main(self):
-  self.sections.setDocid(1100194998)
-  self.workspaces.setDocid(356775224)
-  return "ok"
+from Products.ERP5SyncML.Conduit.ERP5Conduit import ERP5Conduit
 
+def main(self):
+  xml_workspace = """<erp5><object id="1320758020__0001" portal_type="Workspace">
+    <layout_and_schema type="object">None</layout_and_schema>
+    <Coverage type="string"></Coverage>
+    <CreationDate type="date">2004-06-01 12:57:21</CreationDate>
+    <Creator type="string">seb</Creator>
+    <Description type="string"></Description>
+    <EffectiveDate type="date">None</EffectiveDate>
+    <ExpirationDate type="date">None</ExpirationDate>
+    <Format type="string">text/html</Format>
+    <Language type="string">en</Language>
+    <ModificationDate type="date">2004-06-01 12:57:21</ModificationDate>
+    <Relation type="string"></Relation>
+    <Rights type="string"></Rights>
+    <Source type="string"></Source>
+    <Title type="string">Root of Workspaces</Title>
+    <allow_discussion type="int">0</allow_discussion>
+    <hidden_folder type="int">0</hidden_folder>
+    <preview type="object">None</preview>
+    <local_role id="seb" type="tokens">permission:Modify_portal_content</local_role>
+  </object>
+  </erp5>"""
+
+  xml_section = """<erp5><object id="628256376__0001" portal_type="Section">
+    <layout_and_schema type="object">None</layout_and_schema>
+    <Coverage type="string"></Coverage>
+    <CreationDate type="date">2004-06-01 12:57:21</CreationDate>
+    <Creator type="string">seb</Creator>
+    <Description type="string"></Description>
+    <EffectiveDate type="date">None</EffectiveDate>
+    <ExpirationDate type="date">None</ExpirationDate>
+    <Format type="string">text/html</Format>
+    <Language type="string">en</Language>
+    <ModificationDate type="date">2004-06-01 12:57:21</ModificationDate>
+    <Relation type="string"></Relation>
+    <Rights type="string"></Rights>
+    <Source type="string"></Source>
+    <Title type="string">Root of Sections</Title>
+    <allow_discussion type="int">0</allow_discussion>
+    <hidden_folder type="int">0</hidden_folder>
+    <preview type="object">None</preview>
+  </object>
+  </erp5>"""
+
+  portal_repository = self.portal_repository
+
+  conduit = ERP5Conduit()
+  conduit.addNone(xml=xml_workspace,object=portal_repository)
+  conduit.addNone(xml=xml_section,object=portal_repository)
+  
+  self.workspaces.setDocid(1320758020)
+  self.sections.setDocid(628256376)
+  return "ok"
