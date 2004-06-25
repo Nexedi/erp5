e4dffb905b38fe07052021736852374502882226
Yoshinori Okuji <yo@nexedi.com>
2004-06-25 12:43:12 +0000

    first submission
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1093 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/PaySheetTransaction.py
@@ -0,0 +1,64 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+
+from Products.ERP5Type.XMLObject import XMLObject
+
+class PaySheetTransaction(XMLObject):
+    """
+    A paysheet will store data about the salary of an employee
+    """
+
+    meta_type = 'ERP5 Pay Sheet Transaction'
+    portal_type = 'Pay Sheet Transaction'
+    add_permission = Permissions.AddPortalContent
+    isPortalContent = 1
+    isRADContent = 1
+
+    # Declarative security
+    security = ClassSecurityInfo()
+    security.declareObjectProtected(Permissions.View)
+
+    # Default Properties
+    property_sheets = ( PropertySheet.Base
+                      , PropertySheet.SimpleItem
+                      , PropertySheet.CategoryCore
+                      , PropertySheet.Task
+                      , PropertySheet.Arrow
+                      , PropertySheet.Delivery
+                      , PropertySheet.PaySheetTransaction
+                      , PropertySheet.Movement
+                      , PropertySheet.Amount     
+                      , PropertySheet.XMLObject                      
+                      )
+
+    # Declarative Interface
+    __implements__ = ( )
+
---
+++ product/ERP5/Document/PaySheetTransactionLine.py
@@ -0,0 +1,66 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solane <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Globals import InitializeClass, PersistentMapping
+from AccessControl import ClassSecurityInfo
+
+from Products.CMFCore.WorkflowCore import WorkflowAction
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+from Products.ERP5.Document.AccountingTransactionLine import AccountingTransactionLine
+
+class PaySheetTransactionLine(AccountingTransactionLine):
+  """
+  Pay Sheet Transaction Lines allow to move some quantity of money from a source to a destination like
+  an Accounting Transaction Line with more informative content
+
+  A paysheet transaction line contain details of the paysheet calculation for accounting(ex: insurances, pension, helps...).
+
+  """
+
+  meta_type = 'ERP5 Pay Sheet Transaction Line'
+  portal_type = 'Pay Sheet Transaction Line'
+  add_permission = Permissions.AddERP5Content
+  isPortalContent = 1
+  isRADContent = 1
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.View)
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.SimpleItem
+                    , PropertySheet.CategoryCore
+                    , PropertySheet.Amount
+                    , PropertySheet.Task
+                    , PropertySheet.Arrow
+                    )
+
+  # Declarative interfaces
+  __implements__ = ( )
+
