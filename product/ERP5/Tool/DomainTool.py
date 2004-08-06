71cf9eaa24a0add829e102a5a8c1acabd842d9af
Jean-Paul Smets <jp@nexedi.com>
2004-08-06 13:11:47 +0000

    initial upload
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1319 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Tool/DomainTool.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Products.CMFCore.utils import UniqueObject
+
+from Acquisition import Implicit
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass, DTMLFile
+from Products.ERP5Type import Permissions
+from Products.ERP5Type import _dtmldir
+from Products.ERP5Type.Tool.BaseTool import BaseTool
+from Products.ERP5Type.Document.Folder import Folder
+
+class DomainTool(BaseTool):
+    """
+         A tool to define ranges and subranges (predicates)
+    """
+    id = 'portal_domains'
+    meta_type = 'ERP5 Domain Tool'    
+    portal_type     = 'Domain Tool'
+    allowed_types   = ( 'ERP5 Domain',)
+
+    # Declarative Security
+    security = ClassSecurityInfo()
+
+    #
+    #   ZMI methods
+    #
+    #manage_options =  Folder.manage_options                     
+
+    security.declareProtected( Permissions.ManagePortal, 'manage_overview' )
+    manage_overview = DTMLFile( 'explainDomainTool', _dtmldir )
+
+InitializeClass(DomainTool)
