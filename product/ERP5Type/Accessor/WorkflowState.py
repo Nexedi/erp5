84249e895eeb94c979391f2f6a50dfddce0aa45e
Jean-Paul Smets <jp@nexedi.com>
2004-08-10 10:20:01 +0000

    initial import
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1336 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/Accessor/WorkflowState.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2002-2003 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Accessor import Accessor as Method
+from Products.CMFCore.utils import getToolByName
+
+# Creation of default constructor
+class func_code: pass
+
+class Getter(Method):
+    """
+      Gets an attribute value. A default value can be
+      provided if needed
+    """
+    _need__name__=1
+
+    # Generic Definition of Method Object
+    # This is required to call the method form the Web
+    func_code = func_code()
+    func_code.co_varnames = ('self',)
+    func_code.co_argcount = 1
+    func_defaults = ()
+
+    def __init__(self, id, key):
+      self._id = id
+      self.__name__ = id
+      self._key = key
+
+    def __call__(self, instance):
+      portal_workflow = getToolByName(instance, 'portal_workflow')
+      wf = portal_workflow.getWorkflowById(self._key)
+      return wf._getWorkflowStateOf(instance, id_only=1)
