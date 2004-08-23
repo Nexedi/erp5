2d4ab71e9606dbcfe81a55b0577407468390885d
Jean-Paul Smets <jp@nexedi.com>
2004-08-23 08:08:07 +0000

    *** empty log message ***
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1414 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/Constraint/__init__.py
@@ -0,0 +1,5 @@
+from Constraint import Constraint
+from PropertyTypeValidity import PropertyTypeValidity
+from CategoryMembershipArity import CategoryMembershipArity
+from CategoryRelatedMembershipArity import CategoryRelatedMembershipArity
+from AttributeEquality import AttributeEquality
