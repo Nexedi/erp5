5ec96a3b535155a4b1a2b5a196a071f01fc9ec2a
Sebastien Robin <seb@nexedi.com>
2004-09-01 16:04:36 +0000

    first submission
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1483 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/skins/erp5_core/BusinessTemplate_install.py
@@ -0,0 +1,17 @@
+## Script (Python) "BusinessTemplate_build"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=form_id=''
+##title=Build a business template
+##
+REQUEST=context.REQUEST
+
+context.install()
+
+ret_url = context.absolute_url() + '/' + form_id
+qs = '?portal_status_message=Installed.'
+
+return REQUEST.RESPONSE.redirect( ret_url + qs )
