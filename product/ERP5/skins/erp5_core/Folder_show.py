c03c4787d9f5651641240ecd8c48044c12a2345c
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 12:58:39 +0000

    Renamed from folder_show.py to Folder_show.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1559 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Folder_show.py
+++ product/ERP5/skins/erp5_core/Folder_show.py
@@ -1,6 +1,12 @@
+## Script (Python) "Folder_show"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=selection_name
-
-
+##title=
+##
 request = context.REQUEST
 context.portal_selections.setSelectionToAll(selection_name, REQUEST=request)
 url = context.portal_selections.getSelectionListUrlFor(selection_name, REQUEST=request)
