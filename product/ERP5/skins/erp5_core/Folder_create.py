27398dba1014724e1d2cc6abd02cea23ec755354
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 12:57:28 +0000

    Renamed from folder_new.py to Folder_create.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1554 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Folder_create.py
+++ product/ERP5/skins/erp5_core/Folder_create.py
@@ -1,3 +1,12 @@
+## Script (Python) "Folder_create"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
 REQUEST=context.REQUEST
 content_types = context.allowedContentTypes()
 
