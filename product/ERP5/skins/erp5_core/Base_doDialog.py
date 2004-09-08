bffb05f53e532aceecba6d867af06556d81830c5
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 12:53:56 +0000

    Renamed from doDialog.py to Base_doDialog.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1540 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Base_doDialog.py
+++ product/ERP5/skins/erp5_core/Base_doDialog.py
@@ -1,9 +1,16 @@
+## Script (Python) "Base_doDialog"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=dialog_select, form_id, dialog_category, selection_name, cancel_url
-
+##title=
+##
 import string
 
-doAction = dialog_select.split()
-doAction0 = doAction[0]
+Base_doAction = dialog_select.split()
+doAction0 = Base_doAction[0]
 request = context.REQUEST
 
 return request.RESPONSE.redirect(doAction0 + '?selection_name=%s&dialog_category=%s&cancel_url=%s&form_id=%s' %
