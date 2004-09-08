9f1fa5f521b56c86d4af349af838126680bbdcbd
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 12:51:15 +0000

    Renamed from base_list_dialog.py to Base_callListDialogMethod.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1529 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Base_callListDialogMethod.py
+++ product/ERP5/skins/erp5_core/Base_callListDialogMethod.py
@@ -1,5 +1,12 @@
+## Script (Python) "Base_callListDialogMethod"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=form_id,cancel_url,dialog_method,selection_name,dialog_id
-
+##title=
+##
 # Updates attributes of an Zope document
 # which is in a class inheriting from ERP5 Base
 
@@ -10,26 +17,26 @@ from string import join
 request=context.REQUEST
 
 #Exceptions for UI and Sort
-if dialog_method == 'base_list_ui':
-  return context.base_list_ui(form_id=form_id,
+if dialog_method == 'Base_configureUI':
+  return context.Base_configureUI(form_id=form_id,
                               selection_name=selection_name,
                               field_columns=getattr(request,'field_columns'),
                               stat_columns=getattr(request,'stat_columns'))
-if dialog_method == 'base_sort_on':
-  return context.base_sort_on(form_id=form_id,
+if dialog_method == 'Base_configureSortOn':
+  return context.Base_configureSortOn(form_id=form_id,
                               selection_name=selection_name,
                               field_sort_on=getattr(request,'field_sort_on'),
                               field_sort_order=getattr(request,'field_sort_order'))
-if dialog_method == 'update_relation':
-  return context.update_relation(form_id=form_id,
+if dialog_method == 'Base_updateRelation':
+  return context.Base_updateRelation(form_id=form_id,
                               field_id=request.get('field_id'),
                               selection_name=selection_name,
                               selection_index=request.get('selection_index'),
                               object_uid=request.get('object_uid'),
                               uids=request.get('uids'),
                               listbox_uid=request.get('listbox_uid'))
-if dialog_method == 'base_create_relation':
-  return context.base_create_relation(form_id=form_id,
+if dialog_method == 'Base_createRelation':
+  return context.Base_createRelation(form_id=form_id,
                               selection_name=selection_name,
                               selection_index=request.get('selection_index'),
                               base_category=request.get('base_category'),
@@ -39,8 +46,8 @@ if dialog_method == 'base_create_relation':
                               dialog_id=dialog_id,
                               portal_type=request.get('portal_type'),
                               return_url=request.get('cancel_url'))
-if dialog_method == 'folder_delete':
-  return context.folder_delete(form_id=form_id,
+if dialog_method == 'Folder_delete':
+  return context.Folder_delete(form_id=form_id,
                               field_id=request.get('field_id'),
                               selection_name=selection_name,
                               selection_index=request.get('selection_index'),
@@ -48,7 +55,6 @@ if dialog_method == 'folder_delete':
                               uids=request.get('listbox_uid'),
                               md5_object_uid_list=request.get('md5_object_uid_list'),
                               cancel_url=request.get('cancel_url'))
-
 
 try:
   # Validate the form
