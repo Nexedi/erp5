62babc659bdd6221c3cd4797c01f6e72fffd4937
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 13:04:24 +0000

    Renamed from Account_getTotalSourceCredit.py to AccountModule_getTotalSourceCredit.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1577 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/skins/erp5_accounting/AccountModule_getTotalSourceCredit.py
@@ -0,0 +1,37 @@
+## Script (Python) "AccountModule_getTotalSourceCredit"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=brain=None, selection=None, **kw
+##title=
+##
+params = selection.getParams()
+
+url = params.get('accounting_transaction_line_currency')
+if url:
+  currency = context.restrictedTraverse(url)
+else:
+  currency = None
+
+kwd = {}
+
+if params.get('from_date'):
+  kwd['from_date'] = params['from_date']
+if params.get('to_date'):
+  kwd['to_date'] = params['to_date']
+if currency is not None:
+  kwd['resource_uid'] = [currency.getUid()]
+kwd['simulation_state'] = params.get('transaction_simulation_state', ('delivered', 'stopped'))
+kwd['section_category'] = params.get('transaction_section_category', 'group/nexedi')
+
+total = 0.0
+try:
+  inventory = context.Resource_zGetInventory(node_uid=context.getUid(), omit_output=1, omit_simulation=1,
+                                             **kwd)
+  total = inventory[0].inventory or 0.0
+except:
+  pass
+
+return '%.02f' % total
