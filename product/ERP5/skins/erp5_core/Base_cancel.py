71f706bf556b764629d8bf6983a2bf1aab42d8e3
Yoshinori Okuji <yo@nexedi.com>
2004-09-08 12:45:10 +0000

    Renamed from base_cancel.py to Base_cancel.py.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1520 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/skins/erp5_core/Base_cancel.py
+++ product/ERP5/skins/erp5_core/Base_cancel.py
@@ -1,5 +1,12 @@
+## Script (Python) "Base_cancel"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=form_id,cancel_url
-
+##title=
+##
 # Updates attributes of an Zope document
 # which is in a class inheriting from ERP5 Base
 
