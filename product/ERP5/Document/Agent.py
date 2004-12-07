b4811e4c76e37534a221fcad4f8f034b7888727b
Sebastien Robin <seb@nexedi.com>
2004-12-07 08:39:43 +0000

    first submission
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1952 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/Agent.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Sébastien Robin <seb@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+from Products.ERP5.Base import Base
+
+class Agent(Base):
+  """
+    A Path defines a planning element. A path is the combination of
+
+    - a MappedValue (Paths can hold an undetermined number of properties
+      associated to extra conditions)
+
+    - an Arrow (a path is associated to a source and destination)
+
+    - a Path (which defines the resource involved)
+
+    - a FlowCapacity (which defines the capacities on the Path)
+  """
+  meta_type = 'ERP5 Agent'
+  portal_type = 'Agent'
+  add_permission = Permissions.AddPortalContent
+  isPortalContent = 1
+  isRADContent = 1
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.View)
+
+  # Declarative interfaces
+  __implements__ = ( Interface.Predicate )
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.XMLObject
+                    , PropertySheet.CategoryCore
+                    , PropertySheet.Agent    # The path defines the efficiency & resource
+                    )
+
+
