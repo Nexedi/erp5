a7988a1d5f45dab2a67b4c631bbe162f146ffc31
Alexandre Boeglin <alex@nexedi.com>
2004-12-13 10:30:15 +0000

    A AccountingRuleCell object allows to add TransactionLines into a Matrix.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@1988 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/AccountingRuleCell.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+#from Globals import InitializeClass, PersistentMapping
+#from Products.CMFCore.utils import getToolByName
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+
+from Products.ERP5.Document.Predicate import Predicate
+from Products.ERP5.Document.AccountingTransaction import AccountingTransaction
+
+class AccountingRuleCell(Predicate, AccountingTransaction):
+    """
+      A AccountingRuleCell object allows to add
+      TransactionLines into a Matrix
+    """
+
+    # Default Properties
+    property_sheets = ( PropertySheet.Base
+                      , PropertySheet.XMLObject
+                      , PropertySheet.CategoryCore
+                      , PropertySheet.DublinCore
+                      , PropertySheet.Task
+                      , PropertySheet.Arrow
+                      , PropertySheet.Movement
+                      , PropertySheet.Delivery
+                      , PropertySheet.Amount
+                      , PropertySheet.Reference
+                      , PropertySheet.PaymentCondition
+                      )
+
+    # CMF Type Definition
+    meta_type = 'ERP5 Accounting Rule Cell'
+    portal_type = 'Accounting Rule Cell'
+    add_permission = Permissions.AddPortalContent
+    isPortalContent = 1
+    isRADContent = 1
+    isDelivery = 1
+
+    # Declarative security
+    security = ClassSecurityInfo()
+    security.declareObjectProtected(Permissions.View)
+
