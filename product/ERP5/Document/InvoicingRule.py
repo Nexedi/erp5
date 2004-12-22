638382fd21f705c8790315c2851e109c9534eae4
Sebastien Robin <seb@nexedi.com>
2004-12-22 16:27:20 +0000

    first submission
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@2097 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/InvoicingRule.py
@@ -0,0 +1,133 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Sebastien Robin <seb@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+from Acquisition import aq_base, aq_parent, aq_inner, aq_acquire
+from Products.CMFCore.utils import getToolByName
+
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+from Products.ERP5.Document.Rule import Rule
+
+from zLOG import LOG
+
+class InvoicingRule(Rule):
+    """
+      Transformation Sourcing Rule object make sure
+      items required in a Transformation are sourced
+    """
+
+    # CMF Type Definition
+    meta_type = 'ERP5 Sourcing Rule'
+    portal_type = 'Sourcing Rule'
+    add_permission = Permissions.AddPortalContent
+    isPortalContent = 1
+    isRADContent = 1
+
+    # Declarative security
+    security = ClassSecurityInfo()
+    security.declareObjectProtected(Permissions.View)
+
+    # Default Properties
+    property_sheets = ( PropertySheet.Base
+                      , PropertySheet.XMLObject
+                      , PropertySheet.CategoryCore
+                      , PropertySheet.DublinCore
+                      )
+
+    security.declareProtected(Permissions.AccessContentsInformation, 'test')
+    def test(self, movement):
+      """
+        Tests if the rule (still) applies
+      """
+      parent = movement.getParent()
+      if parent.getPortalType()=='Applied Rule' and parent.getSpecialiseId()=='default_order_rule':
+        return 1
+      return 0
+
+    security.declareProtected(Permissions.ModifyPortalContent, 'expand')
+    def expand(self, applied_rule, **kw):
+      """
+        Expands the current movement downward.
+
+        -> new status -> expanded
+
+        An applied rule can be expanded only if its parent movement
+        is expanded.
+      """
+      delivery_line_type = 'Simulation Movement'
+
+      # Source that movement from the next node / stock
+      my_context_movement = applied_rule.getParent()
+      LOG('InvoicingRule.expand, my_context_movement.getPhysicalPath()',0,my_context_movement.getPhysicalPath())
+      LOG('InvoicingRule.expand, my_context_movement.getSource()',0,my_context_movement.getSource())
+      LOG('InvoicingRule.expand, my_context_movement.getTargetSource()',0,my_context_movement.getTargetSource())
+      LOG('InvoicingRule.expand, my_context_movement.showDict()',0,my_context_movement.showDict())
+      LOG('InvoicingRule.expand, my_context_movement.getTargetSource',0,my_context_movement.getTargetSource)
+      if my_context_movement.getSource() is not None:
+        # We should only expand movements if they have a source
+        # otherwise, it creates infinite recursion
+        # This happens for example whenever the source of a movement is acquired
+        # from an order which is deleted afterwards
+        # LOG('Sourcing', 0, str(my_context_movement.getDefaultResource()))
+        new_id = 'invoice_line'
+        transformation_source = getattr(aq_base(applied_rule), new_id, None)
+        if transformation_source is None:
+          transformation_source = applied_rule.newContent(
+                type_name = delivery_line_type,
+                id = new_id
+              )
+
+        #resource = my_context_movement.getResource()
+        #if resource.find('operation/') >= 0:
+          # This is an operation - produce it
+        transformation_source._edit(
+                target_quantity = my_context_movement.getTargetQuantity(),
+                target_efficiency = my_context_movement.getTargetEfficiency(),
+                resource = resource,
+                target_start_date = my_context_movement.getTargetStartDate(),
+                target_stop_date = my_context_movement.getTargetStartDate(),
+                target_source_list = (),
+                target_source_section_list = (),
+                quantity_unit = my_context_movement.getQuantityUnit(),
+                target_destination = my_context_movement.getTargetSource(),
+                target_destination_section = my_context_movement.getTargetSourceSection(),
+                deliverable = 0   # We do not need to source explicitely operations
+            )
+        #  transformation_source.setVariationCategoryList(
+        #            my_context_movement.getVariationCategoryList())
+
+      # Create one submovement which sources the transformation
+      Rule.expand(self, applied_rule, **kw)
+
+
+    def isDeliverable(self, m):
+      resource = m.getResource()
+      if m.getResource() is None:
+        return 0
+      else:
+        return 1
