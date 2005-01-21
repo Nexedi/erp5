6fb9c1815d67dba810d19391935df2574af2c291
Sebastien Robin <seb@nexedi.com>
2005-01-21 15:31:28 +0000

    put it back for compatibility reasons
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@2263 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Tool/Category.py
@@ -0,0 +1,8 @@
+from Products.ERP5.Document.Category import Category as ERP5Category
+from Products.ERP5.Document.BaseCategory import BaseCategory as ERP5BaseCategory
+
+class Category(ERP5Category):
+  pass
+
+class BaseCategory(ERP5BaseCategory):
+  pass
