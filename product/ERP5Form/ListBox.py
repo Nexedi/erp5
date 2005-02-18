a6497a954f664f7f0b767889d2d5c11e8c6eca0c
Sebastien Robin <seb@nexedi.com>
2005-02-18 10:27:51 +0000

    do not display an error message if the category of the domain tree does not exist, so the user can change of base category
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@2501 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Form/ListBox.py
+++ product/ERP5Form/ListBox.py
@@ -110,7 +110,7 @@ def makeTreeBody(form, root_dict, domain_path, depth, total_depth, unfolded_list
         domain_path = domain_path[1:]
       else:
         domain_path = ()
-    is_empty_level = (root.objectCount() == 0) and (len(domain_path) != 0)
+    is_empty_level = root is not None and (root.objectCount() == 0) and (len(domain_path) != 0)
     if is_empty_level: base_category = domain_path[0]
 
   tree_body = ''
