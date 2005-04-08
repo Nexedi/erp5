2762b5e77bffa270599e185973baf151a794008d
Romain Courteaud <romain@nexedi.com>
2005-04-08 07:43:47 +0000

    First submission.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@2851 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Tool/OrderTool.py
@@ -0,0 +1,85 @@
+##############################################################################
+#
+# Copyright (c) 2005 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Romain Courteaud <romain@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Products.CMFCore.utils import UniqueObject
+
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass, DTMLFile
+from Products.ERP5Type.Document.Folder import Folder
+from Products.ERP5Type import Permissions
+
+from Products.ERP5 import _dtmldir
+
+from zLOG import LOG
+
+class OrderTool(UniqueObject, Folder):
+    """
+    The OrderTool implements portal object
+    expand policies.
+
+    Status : not ready
+
+    """
+    id = 'portal_orders'
+    meta_type = 'ERP5 Order Tool'
+    portal_type = 'Order Tool'
+    allowed_types = ()
+
+    # Declarative Security
+    security = ClassSecurityInfo()
+
+    #
+    #   ZMI methods
+    #
+    manage_options = ( ( { 'label'      : 'Overview'
+                         , 'action'     : 'manage_overview'
+                         }
+                        ,
+                        )
+                     + Folder.manage_options
+                     )
+
+    security.declareProtected( Permissions.ManagePortal, 'manage_overview' )
+    manage_overview = DTMLFile( 'explainOrderTool', _dtmldir )
+
+    # Filter content (ZMI))
+    def __init__(self):
+        return Folder.__init__(self, OrderTool.id)
+
+    # Filter content (ZMI))
+    def filtered_meta_types(self, user=None):
+        # Filters the list of available meta types.
+        all = OrderTool.inheritedAttribute('filtered_meta_types')(self)
+        meta_types = []
+        for meta_type in self.all_meta_types():
+            if meta_type['name'] in self.allowed_types:
+                meta_types.append(meta_type)
+        return meta_types
+
+
+InitializeClass(OrderTool)
