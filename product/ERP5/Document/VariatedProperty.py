4ebfb7ba91559847f2549b553f969b8be5067992
Yoshinori Okuji <yo@nexedi.com>
2005-05-24 14:47:12 +0000

    Remove obsolete things.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@3074 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/VariatedProperty.py
+++ product/ERP5/Document/VariatedProperty.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
-# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
-#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+# Copyright (c) 2005 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Yoshinori Okuji <yo@nexedi.com>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
@@ -60,28 +60,12 @@ class VariatedProperty(XMLObject, XMLMatrix, Variated):
     # Declarative properties
     property_sheets = ( PropertySheet.Base
                       , PropertySheet.SimpleItem
-                      , PropertySheet.VariatedProperty
                       , PropertySheet.VariationRange
                       , PropertySheet.MappedValue
                       )
 
     # Declarative interfaces
     __implements__ = ( Interface.Variated, )
-
-    # Local property sheet
-    _properties = (
-      { 'id'          : 'variation_base_category',
-        'storage_id'  : 'variation_base_category_list',
-        'description' : "",
-        'type'        : 'tokens',
-        'acquisition_portal_type'   : Expression('python: portal.getPortalResourceTypeList()'),
-        'acquisition_copy_value'    : 0,
-        'acquisition_mask_value'    : 0,
-        'acquisition_sync_value'    : 0,
-        'acquisition_accessor_id'   : 'getVariationBaseCategoryList',
-        'acquisition_depends'       : None,
-        'mode'        : 'w' },
-    )
 
     # Factory Type Information
     factory_type_information = \
@@ -127,41 +111,9 @@ A VariatedProperty."""
 
 
     security.declareProtected( Permissions.ModifyPortalContent, 'newCellContent' )
-    def newCellContent(self, id,**kw):
+    def newCellContent(self, id, **kw):
       """
           This method can be overriden
       """
-      self.invokeFactory(type_name="Set Mapped Value",id=id)
+      self.invokeFactory(type_name="Set Mapped Value", id=id)
       return self.get(id)
-
-    security.declarePrivate('_checkConsistency')
-    def _checkConsistency(self, fixit=0):
-      """
-        Check the constitency of transformation elements
-      """
-      error_list = XMLMatrix._checkConsistency(self, fixit=fixit)
-
-      # First quantity
-      # We build an attribute equality and look at all cells
-      constraint = Constraint.AttributeEquality(
-        domain_base_category_list = ('coloris', 'taille',),
-        predicate_operator = 'SUPERSET_OF',
-        mapped_value_property_list = ['code_ean13'] )
-      for k in self.getCellKeys(base_id = 'cell'):
-        LOG("VariatedProperty: ",0,"_check: k: %s" % str(k))
-        kw={}
-        kw['base_id'] = 'cell'
-        c = self.getCell(*k, **kw)
-        if c is not None:
-          LOG("VariatedProperty: ",0,"_check: c: %s" % str(c))
-          predicate_value = []
-          for p in k:
-            if p is not None: predicate_value += [p]
-          constraint.edit(predicate_value_list = predicate_value)
-          if fixit:
-            error_list += constraint.fixConsistency(c)
-          else:
-            error_list += constraint.checkConsistency(c)
-
-      return error_list
-
