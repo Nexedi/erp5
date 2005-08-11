08f99fc89749526aac2b5f9be938a4986a4f3a65
Kevin Deldycke <kevin@nexedi.com>
2005-08-11 16:47:45 +0000

    Salary level is now a category
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@3570 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/PropertySheet/Assignment.py
+++ product/ERP5/PropertySheet/Assignment.py
@@ -1,7 +1,8 @@
 ##############################################################################
 #
-# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
-#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+# Copyright (c) 2003-2005 Nexedi SARL and Contributors. All Rights Reserved.
+#                         Jean-Paul Smets-Solanes <jp@nexedi.com>
+#                         Kevin Deldycke <kevin_AT_nexedi_DOT_com>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
@@ -28,24 +29,29 @@
 
 from Products.ERP5.PropertySheet.Path import Path
 
+
 class Assignment(Path):
   """
-        Properties for an Assignment.
+    These properties are used by Career and Assignment
   """
 
   _properties = (
-      {   'id'          : 'salary_coefficient',
-          'description' : 'A coefficient related to the salary classification of the person.',
-          'type'        : 'int',
-          'mode'        : 'w' },
-      {   'id'          : 'salary_level',
-          'description' : 'A level to classify the salary of the person.',
-          'type'        : 'int',
-          'mode'        : 'w' },
-      {   'id'          : 'collective_agreement_title',
-          'description' : 'A title that identify the collective agreement of this person in the case of employee/employer relation.',
-          'type'        : 'string',
-          'mode'        : 'w' },
+    { 'id'          : 'salary_coefficient'
+    , 'description' : 'A coefficient related to the salary classification of the person'
+    , 'type'        : 'int'
+    , 'mode'        : 'w'
+    },
+    { 'id'          : 'collective_agreement_title'
+    , 'description' : 'A title that identify the collective agreement of this person in the case of employee/employer relation'
+    , 'type'        : 'string'
+    , 'mode'        : 'w'
+    },
   )
 
-  _categories = ('activity', 'function', 'grade', 'site', 'role', 'skill', 'group',)
+  _categories = ( # Assignment categories
+                  'group', 'site', 'activity'
+                  # Career categories
+                , 'grade', 'role', 'skill', 'subordination', 'salary_level'
+                  # Dual categories
+                , 'function'
+                )
