12390d0d997e49d26113f864807b60d096621a27
Sebastien Robin <seb@nexedi.com>
2005-10-04 15:54:59 +0000

    removed deprecated methods about target
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@3954 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/TargetSolver/TargetSolver.py
+++ product/ERP5/TargetSolver/TargetSolver.py
@@ -53,22 +53,6 @@ class TargetSolver:
     self.__dict__.update(kw)
     self.previous_target = {}
 
-  def savePreviousTarget(self, movement):
-    from Products.ERP5.Tool.SimulationTool import Target
-    # Saves ONCE ONLY the previous target of a given movement
-    uid = movement.getUid()
-    if not self.previous_target.has_key(uid):
-      self.previous_target[uid] = Target(
-             target_quantity = movement.getTargetQuantity(),
-             target_start_date = movement.getTargetStartDate(),
-             target_stop_date = movement.getTargetStopDate())
-
-  def getPreviousTarget(self, movement):
-    # Returns the previous target for a given movement
-    # This is useful for the first movement solved for example
-    self.savePreviousTarget(movement)
-    return self.previous_target[movement.getUid()]
-
   def solve(self, simulation_movement):
     """
       Solve a simulation movement
