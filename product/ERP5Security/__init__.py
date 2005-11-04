b69416c172f6d497748cf7ecee09b7ff35f244ce
Kevin Deldycke <kevin@nexedi.com>
2005-11-04 11:36:09 +0000

    Use GPL instead of ZPL.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@4229 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Security/__init__.py
+++ product/ERP5Security/__init__.py
@@ -1,15 +1,28 @@
 ##############################################################################
 #
-# Copyright (c) 2001 Zope Corporation and Contributors. All Rights
-# Reserved.
+# Copyright (c) 2005 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Klaus Wölfel <k.woelfel_AT_gmx_DOT_de>
 #
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this
-# distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
 """ ERP5Security product initialization.
@@ -32,7 +45,7 @@ def initialize(context):
     context.registerClass( ERP5UserManager.ERP5UserManager
                          , permission=ManageUsers
                          , constructors=(
-                            ERP5UserManager.manage_addERP5UserManagerForm, 
+                            ERP5UserManager.manage_addERP5UserManagerForm,
                             ERP5UserManager.addERP5UserManager, )
                          , visibility=None
                          , icon='www/portal.gif'
@@ -41,7 +54,7 @@ def initialize(context):
     context.registerClass( ERP5GroupManager.ERP5GroupManager
                          , permission=ManageGroups
                          , constructors=(
-                            ERP5GroupManager.manage_addERP5GroupManagerForm, 
+                            ERP5GroupManager.manage_addERP5GroupManagerForm,
                             ERP5GroupManager.addERP5GroupManager, )
                          , visibility=None
                          , icon='www/portal.gif'
