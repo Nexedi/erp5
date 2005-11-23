0e9ff96fb50521d20a0277cdf385df44cbbef4fa
Romain Courteaud <romain@nexedi.com>
2005-11-23 17:19:55 +0000

    Initial import. Added this test to be sure that Purchase Order is OK.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@4387 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/tests/testPurchaseOrder.py
@@ -0,0 +1,72 @@
+##############################################################################
+#
+# Copyright (c) 2004, 2005 Nexedi SARL and Contributors. All Rights Reserved.
+#          Romain Courteaud <romain@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+##############################################################################
+
+#
+# Skeleton ZopeTestCase
+#
+
+from random import randint
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+# Needed in order to have a log file inside the current folder
+os.environ['EVENT_LOG_FILE'] = os.path.join(os.getcwd(), 'zLOG.log')
+os.environ['EVENT_LOG_SEVERITY'] = '-300'
+
+from zLOG import LOG
+
+from testOrder import TestOrder
+
+class TestPurchaseOrder(TestOrder):
+  """
+    Test business template erp5_trade 
+  """
+  run_all_test = 1
+  order_portal_type = 'Purchase Order'
+  order_line_portal_type = 'Purchase Order Line'
+  cell_portal_type = 'Delivery Cell'
+  packing_list_portal_type = 'Purchase Packing List'
+  packing_list_line_portal_type = 'Purchase Packing List Line'
+  packing_list_cell_portal_type = 'Purchase Packing List Cell'
+  delivery_builder_id = 'purchase_packing_list_builder'
+  order_workflow_id='order_workflow'
+
+  def getTitle(self):
+    return "Purchase Order"
+
+if __name__ == '__main__':
+    framework()
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(TestPurchaseOrder))
+        return suite
+
