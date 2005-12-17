7923795b400f0b76a3bdd386d1e53c7850b818ad
Yoshinori Okuji <yo@nexedi.com>
2005-12-17 08:35:04 +0000

    Import ERROR and INFO.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@4699 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/patches/BTreeFolder2.py
+++ product/ERP5Type/patches/BTreeFolder2.py
@@ -22,7 +22,7 @@ from BTrees.OIBTree import OIBTree, union
 from BTrees.Length import Length
 from OFS.ObjectManager import BadRequestException, BeforeDeleteException
 from Products.ZCatalog.Lazy import LazyMap
-from zLOG import LOG, WARNING
+from zLOG import LOG, WARNING, ERROR, INFO
 
 class ERP5BTreeFolder2Base(BTreeFolder2Base):
   """
