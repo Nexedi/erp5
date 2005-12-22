28f0486ad72904a552775a3edbecfbd643778513
Yoshinori Okuji <yo@nexedi.com>
2005-12-22 15:14:55 +0000

    Allow a restricted environment to call quote via portal_url.quote.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@4865 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -41,6 +41,7 @@ from Products.ERP5Type.patches import FSZSQLMethod
 from Products.ERP5Type.patches import ActionInformation
 from Products.ERP5Type.patches import ActionProviderBase
 from Products.ERP5Type.patches import CookieCrumbler
+from Products.ERP5Type.patches import URLTool
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
