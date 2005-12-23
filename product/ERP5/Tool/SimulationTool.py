c79828d3fb98406bb0718005eeaac6a4a916337a
Sebastien Robin <seb@nexedi.com>
2005-12-23 14:06:27 +0000

    added is_accountable parameter to getInventoryList
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@4875 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Tool/SimulationTool.py
+++ product/ERP5/Tool/SimulationTool.py
@@ -189,7 +189,7 @@ class SimulationTool (BaseTool):
         simulation_state=None, transit_simulation_state = None, omit_transit=0,
         input_simulation_state = None, output_simulation_state=None,
         variation_text=None, sub_variation_text=None,
-        variation_category=None,
+        variation_category=None,is_accountable=None,
         **kw) :
       """
       generates keywork and calls buildSqlQuery
@@ -220,6 +220,9 @@ class SimulationTool (BaseTool):
       resource_uid_list = self._generatePropertyUidList(resource)
       if len(resource_uid_list) :
         new_kw[table + '.resource_uid'] = resource_uid_list
+
+      if is_accountable is not None:
+        new_kw[table + '.is_accountable'] = is_accountable
 
       node_uid_list = self._generatePropertyUidList(node)
       if len(node_uid_list) :
@@ -403,6 +406,8 @@ class SimulationTool (BaseTool):
       omit_input
 
       omit_output
+
+      is_accountable - 0 or 1. Select only movement from deliveries, not orders
 
       selection_domain, selection_report - see ListBox
 
