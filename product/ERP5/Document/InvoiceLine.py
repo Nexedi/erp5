6ccdb59cd41415e237253573a1af18da57fcc6eb
Jérome Perrin <jerome@nexedi.com>
2006-01-31 18:57:38 +0000

    Remove FTI. wrap to 80 characters
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@5480 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/InvoiceLine.py
+++ product/ERP5/Document/InvoiceLine.py
@@ -72,63 +72,10 @@ class InvoiceLine(DeliveryLine):
                       , PropertySheet.EcoTax
                       , PropertySheet.CopyrightTax
                       )
-
-    # Factory Type Information
-    factory_type_information = \
-      {    'id'             : portal_type
-         , 'meta_type'      : meta_type
-         , 'description'    : """\
-Une ligne tarifaire."""
-         , 'icon'           : 'order_line_icon.gif'
-         , 'product'        : 'ERP5'
-         , 'factory'        : 'addInvoiceLine'
-         , 'immediate_view' : 'invoice_line_view'
-         , 'allow_discussion'     : 1
-         , 'allowed_content_types': ('Invoice Cell',
-                                      )
-         , 'filter_content_types' : 1
-         , 'global_allow'   : 1
-         , 'actions'        :
-        ( { 'id'            : 'view'
-          , 'name'          : 'View'
-          , 'category'      : 'object_view'
-          , 'action'        : 'invoice_line_view'
-          , 'permissions'   : (
-              Permissions.View, )
-          }
-        , { 'id'            : 'list'
-          , 'name'          : 'Object Contents'
-          , 'category'      : 'object_action'
-          , 'action'        : 'folder_contents'
-          , 'permissions'   : (
-              Permissions.View, )
-          }
-        , { 'id'            : 'print'
-          , 'name'          : 'Print'
-          , 'category'      : 'object_print'
-          , 'action'        : 'order_line_print'
-          , 'permissions'   : (
-              Permissions.View, )
-          }
-        , { 'id'            : 'metadata'
-          , 'name'          : 'Metadata'
-          , 'category'      : 'object_view'
-          , 'action'        : 'metadata_edit'
-          , 'permissions'   : (
-              Permissions.View, )
-          }
-        , { 'id'            : 'translate'
-          , 'name'          : 'Translate'
-          , 'category'      : 'object_action'
-          , 'action'        : 'translation_template_view'
-          , 'permissions'   : (
-              Permissions.TranslateContent, )
-          }
-        )
-      }
-
+    
     # Cell Related
-    security.declareProtected( Permissions.ModifyPortalContent, 'newCellContent' )
+    security.declareProtected( Permissions.ModifyPortalContent,
+                               'newCellContent' )
     def newCellContent(self, id,**kw):
       """
           This method can be overriden
@@ -136,7 +83,8 @@ Une ligne tarifaire."""
       self.invokeFactory(type_name="Invoice Cell",id=id)
       return self.get(id)
 
-    security.declareProtected(Permissions.AccessContentsInformation, 'isAccountable')
+    security.declareProtected( Permissions.AccessContentsInformation,
+                               'isAccountable' )
     def isAccountable(self):
       """
         Returns 1 if this needs to be accounted
@@ -146,7 +94,8 @@ Une ligne tarifaire."""
       # Never accountable
       return 0
 
-    security.declareProtected(Permissions.View, 'isDivergent')
+    security.declareProtected( Permissions.AccessContentsInformation,
+                              'isDivergent' )
     def isDivergent(self):
       """
         Returns 1 if the target is not met according to the current information
@@ -158,7 +107,8 @@ Une ligne tarifaire."""
       # Never divergent
       return 0
 
-    security.declareProtected(Permissions.AccessContentsInformation, 'getGroupCriterion')
+    security.declareProtected( Permissions.AccessContentsInformation,
+                               'getGroupCriterion' )
     def getGroupCriterion(self):
       """
         Return the criterion for grouping. This should be overriden by each class.
