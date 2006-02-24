0f6bf2f6a36e23ac79c6e1e30b8571a2ff216817
Alexandre Boeglin <alex@nexedi.com>
2006-02-24 21:40:12 +0000

    Supplies are not Movement but Path, renamed the group and methods.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@5840 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ERP5Type.py
+++ product/ERP5Type/ERP5Type.py
@@ -141,7 +141,7 @@ class ERP5TypeInformation( FactoryTypeInformation, RoleProviderBase, Translation
       'container', 'container_line', 'delivery', 'delivery_movement',
       'discount', 'invoice', 'invoice_movement', 'item',
       'order', 'order_movement', 'node', 'payment_node', 'payment_condition',
-      'resource', 'supply', 'supply_movement', 'transformation', 'variation',
+      'resource', 'supply', 'supply_path', 'transformation', 'variation',
       'sub_variation'
     )
     group_list = ()
