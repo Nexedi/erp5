f9539d5e4624b795fb605d57d9f7c65aa5957829
Guillaume Michon <guillaume@nexedi.com>
2006-03-09 15:16:16 +0000

    Added ImmobilisationValidityError, ImmobilisationCalculationError, and WorkflowException
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@6058 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/Errors.py
+++ product/ERP5Type/Errors.py
@@ -5,6 +5,7 @@
 # from Products.ERP5.Errors import DeferredCatalogError
 
 from Products.PythonScripts.Utility import allow_class
+from Products.CMFCore.WorkflowCore import WorkflowException
 
 class DeferredCatalogError(Exception):
 
@@ -13,4 +14,10 @@ class DeferredCatalogError(Exception):
         self.error_key = error_key
         self.field_id = context.getRelativeUrl()
 
+class ImmobilisationValidityError(Exception):pass
+class ImmobilisationCalculationError(Exception):pass
+
 allow_class(DeferredCatalogError)
+allow_class(ImmobilisationValidityError)
+allow_class(ImmobilisationCalculationError)
+allow_class(WorkflowException)
