418c09db292c946f9b81eba58489a02d60113fe8
Romain Courteaud <romain@nexedi.com>
2006-05-17 08:59:56 +0000

    Add groups: ticket and event.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@7304 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ERP5Type.py
+++ product/ERP5Type/ERP5Type.py
@@ -142,7 +142,7 @@ class ERP5TypeInformation( FactoryTypeInformation, RoleProviderBase, Translation
       'discount', 'invoice', 'invoice_movement', 'item',
       'order', 'order_movement', 'node', 'payment_node', 'payment_condition',
       'resource', 'supply', 'supply_path', 'transformation', 'variation',
-      'sub_variation', 'web_document'
+      'sub_variation', 'web_document', 'event', 'ticket'
     )
     group_list = ()
 
