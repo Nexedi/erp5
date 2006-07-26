97d7ff21e693d1fe872e2d6cdf221a41d907ebcb
Jérome Perrin <jerome@nexedi.com>
2006-07-26 09:35:27 +0000

    acquisition_object_id should not be acquired, we only check if the context object have a subobject with id acquisition_object_id
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@8792 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/CMFCategory/CategoryTool.py
+++ product/CMFCategory/CategoryTool.py
@@ -879,7 +879,7 @@ class CategoryTool( UniqueObject, Folder, Base ):
           return result
         # First we look at local ids
         for object_id in base_category_value.getAcquisitionObjectIdList():
-          my_acquisition_object = getattr(context,object_id,None)
+          my_acquisition_object = context._getOb(object_id, None)
           if my_acquisition_object is not None:
             #my_acquisition_object_path = my_acquisition_object.getPhysicalPath()
             #if my_acquisition_object_path in acquired_object_dict:
