Nexedi/erp5

commit 5c8f4d3b7c5618ae6a9d100b36d45e8176952dca

get test suite using the default test_suite function, unless some class names are specified on the command line.
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@8478 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5Type/tests/runUnitTest.py
@@ -59,7 +59,7 @@ def runUnitTestList(test_list) :
   os.environ['EVENT_LOG_FILE'] = os.path.join(tests_home, 'zLOG.log')
   os.environ['EVENT_LOG_SEVERITY'] = '-300'
 
-  execfile(os.path.join(tests_framework_home, 'framework.py')) 
+  execfile(os.path.join(tests_framework_home, 'framework.py'))
 
   import unittest
   from Testing import ZopeTestCase
@@ -98,7 +98,7 @@ def runUnitTestList(test_list) :
   sys.path.extend((real_tests_home, tests_home))
 
   # Make sure that locally overridden python modules are used
-  sys.path.insert(0, os.path.join(real_instance_home, 'lib%spython' % os.sep))
+  sys.path.insert(0, os.path.join(real_instance_home, 'lib', 'python'))
 
   # XXX Allowing to load modules from here is a wrong idea. use the above path
   # instead.
@@ -106,25 +106,32 @@ def runUnitTestList(test_list) :
   # this allows to bypass psyco by creating a dummy psyco module
   # it is then possible to run the debugger by "import pdb; pdb.set_trace()"
   sys.path.insert(0, tests_framework_home)
-
+ 
+  filtered_tests_class_names = 0
   for test in test_list:
     if ':' in test:
       test_module = test.split(':')[0]
       if test_module.endswith('.py'):
         test_module = test_module[:-3]
       test_class_list = test.split(':')[1:]
+      filtered_tests_class_names = 1
     else:
       if test.endswith('.py'):
         test = test[:-3]
       test_module = test
       test_class_list = None
     m = __import__(test_module)
-    for attr_name in dir(m) :
-      attr = getattr(m, attr_name)
-      if (type(attr) == type(type)) and (hasattr(attr, '__module__')) and \
-          (attr.__module__ == test_module) :
-        if test_class_list is None or attr.__name__ in test_class_list:
-          suite.addTest(unittest.makeSuite(attr))
+    if not filtered_tests_class_names and hasattr(m, 'test_suite'):
+      suite = m.test_suite()
+    else:
+      # dynamically create the test suite using class names passed on the
+      # command line.
+      for attr_name in dir(m):
+        attr = getattr(m, attr_name)
+        if (type(attr) == type(type)) and (hasattr(attr, '__module__')) and \
+            (attr.__module__ == test_module) :
+          if test_class_list is None or attr.__name__ in test_class_list:
+            suite.addTest(unittest.makeSuite(attr))
 
   return TestRunner().run(suite)
 

commit 2811be92a72fcf079bb17186f20ddf073b961453

initial getopt support (-v is ignored) incrementally add tests to the suite so that we can run tests from multiple class again ( was broken by r8478 )
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@8925 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5Type/tests/runUnitTest.py
@@ -2,12 +2,26 @@
 #
 # Runs the tests passed on the command line
 #
-import os, sys
+import os
+import sys
+import getopt
+
+__doc__ = """%(program)s: unit test runner for the ERP5 Project
+
+usage: %(program)s [options] [UnitTest1[:TestClass1[:TestClass2]] [UnitTest2]]
+
+Options:
+-v/--verbose -- produce verbose output
+
+"""
+
 
 def getUnitTestFile() :
   return os.path.abspath(__file__)
 
-def initializeInstanceHome(tests_framework_home, real_instance_home, instance_home):
+def initializeInstanceHome(tests_framework_home,
+                           real_instance_home,
+                           instance_home):
   if not os.path.exists(instance_home):
     os.mkdir(instance_home)
   for d in ('Constraint', 'Document', 'PropertySheet', 'tests', 'var'):
@@ -56,8 +70,8 @@ def runUnitTestList(test_list) :
   os.environ['SOFTWARE_HOME'] = software_home
   os.environ['COPY_OF_INSTANCE_HOME'] = instance_home
   os.environ['COPY_OF_SOFTWARE_HOME'] = software_home
-  os.environ['EVENT_LOG_FILE'] = os.path.join(tests_home, 'zLOG.log')
-  os.environ['EVENT_LOG_SEVERITY'] = '-300'
+  os.environ.setdefault('EVENT_LOG_FILE', os.path.join(tests_home, 'zLOG.log'))
+  os.environ.setdefault('EVENT_LOG_SEVERITY', '-300')
 
   execfile(os.path.join(tests_framework_home, 'framework.py'))
 
@@ -122,7 +136,7 @@ def runUnitTestList(test_list) :
       test_class_list = None
     m = __import__(test_module)
     if not filtered_tests_class_names and hasattr(m, 'test_suite'):
-      suite = m.test_suite()
+      suite.addTest(m.test_suite())
     else:
       # dynamically create the test suite using class names passed on the
       # command line.
@@ -135,12 +149,35 @@ def runUnitTestList(test_list) :
 
   return TestRunner().run(suite)
 
-if __name__ == '__main__' :
-  test_list = sys.argv[1:]
-  if len(test_list) == 0 :
+def usage(stream, msg=None):
+  if msg:
+    print >>stream, msg
+    print >>stream
+  program = os.path.basename(sys.argv[0])
+  print >>stream, __doc__ % {"program": program}
+
+def main():
+  try:
+    opts, args = getopt.getopt(sys.argv[1:],
+        "hv", ["help", "verbose", ] )
+  except getopt.GetoptError, msg:
+    usage(sys.stderr, msg)
+    sys.exit(2)
+  
+  for opt, arg in opts:
+    if opt in ("-v", "--verbose"):
+      os.environ['VERBOSE'] = "1"
+    if opt in ("-h", "--help"):
+      usage(sys.stdout)
+      sys.exit()
+
+  test_list = args
+  if not test_list:
     print "No test to run, exiting immediately."
-    print "Usage : %s UnitTest1[:TestClass1[:TestClass2]...] UnitTest2 ..." % sys.argv[0]
     sys.exit(1)
+  
   result = runUnitTestList(test_list=test_list)
-  if len(result.failures) or len(result.errors) :
-    sys.exit(1)
+  sys.exit(len(result.failures) + len(result.errors))
+
+if __name__ == '__main__':
+  main()