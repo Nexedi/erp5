58a66f1a8e27355cf92c3d315c3071e3ad2ee664
Aurélien Calonne <aurel@nexedi.com>
2006-08-08 11:57:42 +0000

    fix bt5 list
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9089 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Banking/tests/testERP5BankingCheckbookReception.py
+++ product/ERP5Banking/tests/testERP5BankingCheckbookReception.py
@@ -78,7 +78,8 @@ class TestERP5BankingCheckbookReception(TestERP5BankingMixin, ERP5TypeTestCase):
       need to be installed to run the test on.
     """
     return ('erp5_base',
-            'baobab_unit_test',
+            'erp5_trade',
+            'erp5_accounting',
             'erp5_banking_core',
             'erp5_banking_inventory',
             'erp5_banking_check'
