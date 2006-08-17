02a0e9e5df7aa78276a89270497ec28b1eb104a0
Bartek Górny <bartek@gorny.edu.pl>
2006-08-17 15:17:52 +0000

    Parsing search string for extra tricks
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9246 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ bt5/erp5_dms/ExtensionTemplateItem/searchUtils.py
@@ -0,0 +1,35 @@
+
+import re
+
+# parsing defined here
+r=re.compile('(\w+:"[^"]+"|\w+:[\w\-.]+)')
+filetyper=lambda s:('original_filename','%%.%s' % s)
+filestripper=lambda s: ('original_filename',s.replace('"',''))
+paramsmap=dict(file=filestripper,type='portal_type',reference='reference',filetype=filetyper)
+
+def analyze(params):
+    params['SearchableText']=''
+    def cutter(s):
+        ss=s.split(':')
+        if len(ss)==1:
+            params['SearchableText']+=ss[0]
+        if len(ss)==2:
+            try:
+                ps=paramsmap.get(ss[0])(ss[1])
+                params[ps[0]]=ps[1]
+            except TypeError:
+                if paramsmap.has_key(ss[0]):
+                    params[paramsmap.get(ss[0])]=ss[1]
+    return cutter
+
+def parseSearchString(searchstring):
+    params={}
+    l=r.split(searchstring)
+    print l
+    map(analyze(params),l)
+    return params
+
+if __name__=='__main__':
+    #searchstring='byle cisnie zego file:"ble ble.doc" filetype:doc type:Text poza tym reference:abc-def'
+    searchstring='byle "cisnie zego" file:"ble ble.doc" type:Text poza tym reference:abc-def'
+    print parseSearchString(searchstring)
