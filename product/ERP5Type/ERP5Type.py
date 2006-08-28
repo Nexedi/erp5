5fd2a423a184d3f62c746641236abef40da71aef
Bartek Górny <bartek@gorny.edu.pl>
2006-08-28 15:58:17 +0000

    added a group for dms documents
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9518 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ERP5Type.py
+++ product/ERP5Type/ERP5Type.py
@@ -147,7 +147,7 @@ class ERP5TypeInformation( FactoryTypeInformation,
       'discount', 'invoice', 'invoice_movement', 'item',
       'order', 'order_movement', 'node', 'payment_node', 'payment_condition',
       'resource', 'supply', 'supply_path', 'transformation', 'variation',
-      'sub_variation', 'web_document', 'event', 'ticket'
+      'sub_variation', 'web_document', 'event', 'ticket', 'dms_document'
     )
     group_list = ()
 
@@ -632,3 +632,5 @@ Products.CMFCore.TypesTool.typeClasses.append(
                            'permission':'Manage portal'}, )
 Products.CMFCore.TypesTool.TypesTool.manage_addERP5TIForm = manage_addERP5TIForm
 
+
+# vim: filetype=python syntax=python shiftwidth=2 
