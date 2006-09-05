2614b60ebc8bf5e22f72405600e6f5df5c96a8d9
Bartek Górny <bartek@gorny.edu.pl>
2006-09-05 15:27:31 +0000

    Extended search functions (requires SQLCatalog patch)
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9667 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_dms/ExtensionTemplateItem/searchUtils.py
+++ bt5/erp5_dms/ExtensionTemplateItem/searchUtils.py
@@ -2,13 +2,17 @@
 import re
 
 # parsing defined here
-r=re.compile('(\w+:"[^"]+"|\w+:[\w\-.]+)')
+simulation_states=('released','public','submitted')
+r=re.compile('(\w+:"[^"]+"|\w+:[\w/\-.]+)')
 filetyper=lambda s:('source_reference','%%.%s' % s)
 filestripper=lambda s: ('source_reference',s.replace('"',''))
-paramsmap=dict(file=filestripper,type='portal_type',reference='reference',filetype=filetyper)
+addarchived=lambda s: ('simulation_state',simulation_states+('archived',))
+paramsmap=dict(file=filestripper,type='portal_type',reference='reference',filetype=filetyper,archived=addarchived,\
+        language='language',version='version')
 
 def analyze(params):
     params['SearchableText']=''
+    params['simulation_state']=simulation_states
     def cutter(s):
         ss=s.split(':')
         if len(ss)==1:
@@ -20,6 +24,8 @@ def analyze(params):
             except TypeError:
                 if paramsmap.has_key(ss[0]):
                     params[paramsmap.get(ss[0])]=ss[1]
+                else:
+                    params[ss[0]]=ss[1]
     return cutter
 
 def parseSearchString(searchstring):
@@ -27,9 +33,10 @@ def parseSearchString(searchstring):
     l=r.split(searchstring)
     print l
     map(analyze(params),l)
+    params['SearchableText']=params['SearchableText'].strip()
     return params
 
 if __name__=='__main__':
     #searchstring='byle cisnie zego file:"ble ble.doc" filetype:doc type:Text poza tym reference:abc-def'
-    searchstring='byle "cisnie zego" file:"ble ble.doc" type:Text poza tym reference:abc-def'
+    searchstring='byle "cisnie zego" file:"ble ble.doc" type:Text poza tym reference:abc-def dupa:kwas/zbita'
     print parseSearchString(searchstring)
