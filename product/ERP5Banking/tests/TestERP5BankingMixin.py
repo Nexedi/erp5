3ed849fabca0c23451694f8690bfb5af6853c6f8
Aurélien Calonne <aurel@nexedi.com>
2006-09-06 12:32:38 +0000

    use the right method to set the property
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9698 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Banking/tests/TestERP5BankingMixin.py
+++ product/ERP5Banking/tests/TestERP5BankingMixin.py
@@ -565,7 +565,7 @@ class TestERP5BankingMixin:
     # the erp5 site
     self.portal = self.getPortal()
     # the default currency for the site
-    self.portal.setProperty('reference_currency_id', 'EUR', type='string')
+    self.portal.manage_addProperty('reference_currency_id', 'EUR', type='string')
     # the person module
     self.person_module = self.getPersonModule()
     # the organisation module
