ccddd11b46e5b35fcfbb3e663a0ce398df9eaa1c
Bartek Górny <bartek@gorny.edu.pl>
2006-09-12 19:30:42 +0000

    external data source aux properties
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@9852 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5OOo/PropertySheet/ExternalDocument.py
@@ -0,0 +1,11 @@
+
+class ExternalDocument:
+  """
+  """
+  _properties = (
+        {   'id'          : 'status_message',
+            'description' : 'message about status',
+            'type'        : 'string',
+            'mode'        : 'r' },
+        )
+
