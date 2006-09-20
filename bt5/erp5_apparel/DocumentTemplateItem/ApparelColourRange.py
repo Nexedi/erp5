96726d69573f0b5299a97f07c4e575e18265b45e
Sebastien Robin <seb@nexedi.com>
2006-09-20 22:01:47 +0000

    fixed the import of resource
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@10217 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_apparel/DocumentTemplateItem/ApparelColourRange.py
+++ bt5/erp5_apparel/DocumentTemplateItem/ApparelColourRange.py
@@ -30,7 +30,7 @@
 
 from AccessControl import ClassSecurityInfo
 
-from Products.ERP5Type.Document.Resource import Resource
+from Products.ERP5.Document.Resource import Resource
 from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
 
 class ApparelColourRange(Resource):
