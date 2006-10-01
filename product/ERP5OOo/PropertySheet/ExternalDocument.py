d392b1df8ce4b53a6a242097e8dce897c09fd52b
Bartek Górny <bartek@gorny.edu.pl>
2006-10-01 19:27:52 +0000

    external web page, supports recursive spidering
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@10447 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5OOo/PropertySheet/ExternalDocument.py
+++ product/ERP5OOo/PropertySheet/ExternalDocument.py
@@ -7,5 +7,14 @@ class ExternalDocument:
             'description' : 'message about status',
             'type'        : 'string',
             'mode'        : 'w' },
+        {   'id'          : 'option_recursively',
+            'description' : 'do we want recursive spidering (meaningless in some classes)',
+            'type'        : 'int',
+            'mode'        : 'w'},
+        {   'id'          : 'recursion_depth',
+            'description' : 'how deep should recursive spidering be (0 - no recursion) (meaningless in some classes)',
+            'type'        : 'int',
+            'default'     : 5,
+            'mode'        : 'w'},
         )
 
