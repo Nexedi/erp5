9715a9e38c53a88a0b8303352444fbf807b7a151
Aurélien Calonne <aurel@nexedi.com>
2006-10-23 12:10:05 +0000

    use newContent with temp_object to generate the domain tree
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@10881 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/Domain.py
+++ product/ERP5/Document/Domain.py
@@ -105,8 +105,7 @@ class Domain(Predicate, MetaNode, MetaResource):
     We need a way to know how deep we are in the domain generation
     to prevent infinite recursion XXX not implemented
     """
-    from Products.ERP5Type.Document import newTempDomainGenerator
-    klass = tmp_domain_generator = newTempDomainGenerator(self, 'new_generator')
+    klass = tmp_domain_generator = self.newContent(portal_type='Domain', temp_object=1)
     script = self.getDomainGeneratorMethodId('')
     return tmp_domain_generator.getDomainGeneratorList(depth=depth, klass=klass, script=script, parent=self)
 
