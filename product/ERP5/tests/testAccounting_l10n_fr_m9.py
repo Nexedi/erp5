520444f70c1440aedd2727dcf5d75fcf098e1cc7
Jérome Perrin <jerome@nexedi.com>
2006-10-24 15:52:01 +0000

    More guessing of the account_type based on gap
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@10916 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testAccounting_l10n_fr_m9.py
+++ product/ERP5/tests/testAccounting_l10n_fr_m9.py
@@ -224,7 +224,33 @@ class TestAccounting_l10n_M9(ERP5TypeTestCase):
                                account_type='equity')
     self.assertEquals(1, len(account.checkConsistency(fixit=1)))
     self.assertEquals('liability/payable', account.getAccountType())
+  
+  # Members of gap/fr/m9/4/47 are very specific
+  # for now, we do not fix 476 or 477
+  def test_AccountTypeConstaintFixFor4718(self):
+    account = self._getAccount('4718',
+                               gap='fr/m9/4/47/471/4718', )
+    self.assertEquals(1, len(account.checkConsistency(fixit=1)))
+    self.assertEquals('asset/receivable', account.getAccountType())
+  
+  def test_AccountTypeConstaintFixFor4721(self):
+    account = self._getAccount('4721',
+                               gap='fr/m9/4/47/472/4721', )
+    self.assertEquals(1, len(account.checkConsistency(fixit=1)))
+    self.assertEquals('liability/payable', account.getAccountType())
 
+  def test_AccountTypeConstaintFixFor4731(self):
+    account = self._getAccount('4731',
+                               gap='fr/m9/4/47/473/4731', )
+    self.assertEquals(1, len(account.checkConsistency(fixit=1)))
+    self.assertEquals('asset/receivable', account.getAccountType())
+
+  def test_AccountTypeConstaintFixFor4735(self):
+    account = self._getAccount('4735',
+                               gap='fr/m9/4/47/473/4735', )
+    self.assertEquals(1, len(account.checkConsistency(fixit=1)))
+    self.assertEquals('liability/payable', account.getAccountType())
+    
 if __name__ == '__main__':
   framework()
 else:
