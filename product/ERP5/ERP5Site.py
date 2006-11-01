9690dc60f7ec9c8e127c45ee9413c4440fa884e9
Ivan Tyagov <ivan@nexedi.com>
2006-11-01 07:24:07 +0000

    Added creation of tool portal_caches during ERP5 Site creation. This way we're consistent with erp5_core template which will create default Cache Factory(erp5_user_interface) under this tool.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11027 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/ERP5Site.py
+++ product/ERP5/ERP5Site.py
@@ -1005,6 +1005,8 @@ class ERP5Generator(PortalGenerator):
       addTool('ERP5 Order Tool', None)
     if not p.hasObject('portal_tests'):
       addTool('ERP5 Test Tool', None)
+    if not p.hasObject('portal_caches'):
+      addTool('ERP5 Cache Tool', None)
 
     try:
       addTool = p.manage_addProduct['ERP5Subversion'].manage_addTool
