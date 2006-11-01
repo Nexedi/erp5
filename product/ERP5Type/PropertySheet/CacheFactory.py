99b45621795a55cb4a2079f1c0d084c43e61bcda
Ivan Tyagov <ivan@nexedi.com>
2006-11-01 10:04:07 +0000

    Initial import of PropertySheets for Cache configuration objects
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11031 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/PropertySheet/CacheFactory.py
@@ -0,0 +1,13 @@
+class CacheFactory:
+  """
+  CacheFactory. 
+  """
+
+  _properties = (
+        {   'id'          : 'cache_duration',
+            'description' : 'Cache duration',
+            'type'        : 'int',
+            'default'     : 360,
+            'mode'        : 'w' ,
+        },
+    )
