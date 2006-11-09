cbcc6be825a3c9f9b9d1f5691ffc67d7da739f84
Bartek Górny <bartek@gorny.edu.pl>
2006-11-09 21:26:43 +0000

    utilities to convert and generate files outside OOoDocument objects (from raw data)
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11237 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_dms/ExtensionTemplateItem/documentUtils.py
+++ bt5/erp5_dms/ExtensionTemplateItem/documentUtils.py
@@ -1,4 +1,6 @@
 import zipfile, cStringIO, re
+import xmlrpclib, base64
+from Products.CMFCore.utils import getToolByName
 
 rx_atr='([\w]+)###([\w/]+)'
 rx_atr=re.compile(rx_atr)
@@ -22,5 +24,28 @@ def getAttrFromContent(data):
 def getDoctypeFromContent(data):
   atrs=getAttrFromContent(data)
   return atrs.get('doctype')
+
+def convertToOdf(name,data):
+  sp=mkProxy()
+  kw=sp.run_convert(name,data)
+  odf=base64.decodestring(kw['data'])
+  return odf
+
+def mkProxy(self):
+  pref=getToolByName(self,'portal_preferences')
+  adr=pref.getPreferredDmsOoodocServerAddress()
+  nr=pref.getPreferredDmsOoodocServerPortNumber()
+  if adr is None or nr is None:
+    raise Exception('you should set conversion server coordinates in preferences')
+  sp=xmlrpclib.ServerProxy('http://%s:%d' % (adr,nr),allow_none=True)
+  return sp
+
+def generateFile(self,name,data,format):
+  sp=mkProxy(self)
+  kw=sp.run_generate(name,data,None,format)
+  res=base64.decodestring(kw['data'])
+  return res
+
+
 
 # vim: syntax=python shiftwidth=2 
