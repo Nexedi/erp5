8a4a59e88cf71c5d811aace35dc789627e6b2778
Sebastien Robin <seb@nexedi.com>
2006-11-21 14:37:23 +0000

    open the counter date a bit later to make sure security is working
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11403 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Banking/tests/testERP5BankingInternalMoneyDeposit.py
+++ product/ERP5Banking/tests/testERP5BankingInternalMoneyDeposit.py
@@ -134,11 +134,6 @@ class TestERP5BankingInternalMoneyDeposit(TestERP5BankingMixin, ERP5TypeTestCase
                              line_list=line_list)
 
 
-    # open counter date and counter
-    self.openCounterDate(site=self.paris)
-    self.openCounter(site=self.bi_counter_vault)
-
-
     # now we need to create a user as Manager to do the test
     # in order to have an assigment defined which is used to do transition
     # Create an Organisation that will be used for users assignment
@@ -153,6 +148,10 @@ class TestERP5BankingInternalMoneyDeposit(TestERP5BankingMixin, ERP5TypeTestCase
     self.createERP5Users(user_dict)
     self.logout()
     self.login('super_user')
+    # open counter date and counter
+    self.openCounterDate(site=self.paris)
+    self.openCounter(site=self.bi_counter_vault)
+
 
 
 
