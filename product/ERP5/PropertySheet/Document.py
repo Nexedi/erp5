fc70e6c83a229c594474e87987b7dbf29041a089
Jean-Paul Smets <jp@nexedi.com>
2006-12-31 15:57:01 +0000

    Added taxonomy for document management.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11805 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/PropertySheet/Document.py
+++ product/ERP5/PropertySheet/Document.py
@@ -1,46 +1,56 @@
 ##############################################################################
 #
-# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
-#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+## Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
 #
-# WARNING: This program as such is intended to be used by professional
+## WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
-#
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-##############################################################################
+# # consequences resulting from its eventual inadequacies and bugs
+# # End users who are looking for a ready-to-use solution with commercial
+# # garantees and support are strongly adviced to contract a Free Software
+# # Service Company
+# #
+# # This program is Free Software; you can redistribute it and/or
+# # modify it under the terms of the GNU General Public License
+# # as published by the Free Software Foundation; either version 2
+# # of the License, or (at your option) any later version.
+# #
+# # This program is distributed in the hope that it will be useful,
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# # GNU General Public License for more details.
+# #
+# # You should have received a copy of the GNU General Public License
+# # along with this program; if not, write to the Free Software
+# # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# #
+# ##############################################################################
 
 class Document:
-    """
-        Properties for a simple Document
-    """
+  """
+    This propert sheet defines the default taxonomy for Document management in ERP5.
+  """
 
-    _properties = (
-        {   'id'          : 'text_content',
-            'description' : 'The content of the document considered as a text string',
-            'type'        : 'text',
-            'mode'        : 'w'
-            },
-        {   'id'          : 'text_format',
-            'description' : 'The format of the text content of this document',
-            'type'        : 'string',
-            'mode'        : 'w'
-            }
-        )
+  _properties = (
+  )
+
+  _categories = ('similar', 'predecessor', 'successor', 'contributor', 'classification',
+                 # Source project is set by default since project is equivalent to workspace
+                 'source_project',
+                 # Source is defined in DMS documentation
+                 'source',
+                 # XXX-JPS where are these defined in documentation. Why ?
+                 'destination',
+                 # XXX-JPS all the following properties should be configured on portal type
+                 # and removed from here
+                 'publication_section', 'function', 'group', 'site')
+
+  _constraints = (
+      {
+      'id'          :   'unique_coordinates',       # XXX-JPS plural naming - rename to unique_coordinate
+      'description' :   'coordinate triplet must be complete and unique',
+      'type'        :   'DocumentCoordinatesConstraint'}, # XXX-JPS plural - rename to DocumentCoordinateConstraint
+    )
+ 
+
+# vim: shiftwidth=2
 
