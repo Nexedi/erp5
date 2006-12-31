52b3c3b27022bcf4e99888042d2c95a8f43e583c
Jean-Paul Smets <jp@nexedi.com>
2006-12-31 16:05:07 +0000

    Patch to support webdav on folderish files.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11812 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -42,6 +42,7 @@ from Products.ERP5Type.patches import CookieCrumbler
 from Products.ERP5Type.patches import Localizer
 from Products.ERP5Type.patches import CMFMailIn
 from Products.ERP5Type.patches import CMFCoreUtils
+from Products.ERP5Type.patches import PropertySheets
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
