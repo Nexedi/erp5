3491680068c65755fb4d45d723d1d2fee661953f
Jean-Paul Smets <jp@nexedi.com>
2007-01-03 01:49:31 +0000

    Moved to Core.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@11851 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/Document/__init__.py
+++ product/ERP5Type/Document/__init__.py
@@ -1,20 +1,20 @@
 # ERP5Type has a specific __init__ file to circumvent circular references
 
-# Hide internal implementation
-from Globals import InitializeClass
-import Folder as ERP5Folder
-# Default constructor for Folder
-# Can be overriden by adding a method addFolder in class Folder
-def addFolder(folder, id, REQUEST=None, **kw):
-  o = ERP5Folder.Folder(id)
-  folder._setObject(id, o)
-  if kw is not None: o.__of__(folder)._edit(force_update=1, **kw)
-  # contentCreate already calls reindex 3 times ...
-  # o.reindexObject()
-  if REQUEST is not None:
-      REQUEST['RESPONSE'].redirect( 'manage_main' )
+## Hide internal implementation
+#from Globals import InitializeClass
+#import Products.ERP5Type.Core.Folder as ERP5Folder
+## Default constructor for Folder
+## Can be overriden by adding a method addFolder in class Folder
+#def addFolder(folder, id, REQUEST=None, **kw):
+  #o = ERP5Folder.Folder(id)
+  #folder._setObject(id, o)
+  #if kw is not None: o.__of__(folder)._edit(force_update=1, **kw)
+  ## contentCreate already calls reindex 3 times ...
+  ## o.reindexObject()
+  #if REQUEST is not None:
+      #REQUEST['RESPONSE'].redirect( 'manage_main' )
 
-InitializeClass(ERP5Folder.Folder)
+#InitializeClass(ERP5Folder.Folder)
 
 from Products.ERP5Type.Base import TempBase
 from Products.PythonScripts.Utility import allow_class
