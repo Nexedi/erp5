7c517167fc267d882d3bb39230bfded35bbdc884
Nicolas Delaby <nicolas@nexedi.com>
2007-01-12 10:55:29 +0000

    Del wrong uncomment line
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@12044 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/ImmobilisationMovement.py
+++ product/ERP5/Document/ImmobilisationMovement.py
@@ -30,7 +30,6 @@ from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
 
 from Products.ERP5Type import Base, Permissions, PropertySheet, Constraint, Interface
-from Products.ERP5.Core import MetaNode, MetaResource
 from Products.CMFCore.WorkflowCore import WorkflowMethod
 
 from Products.ERP5Type.XMLObject import XMLObject
