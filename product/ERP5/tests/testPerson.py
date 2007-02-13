e92982ea40292a14ae069b4cea40a5ea5be6bc4f
Ivan Tyagov <ivan@nexedi.com>
2007-02-13 15:20:32 +0000

    Comment fixed.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@12668 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testPerson.py
+++ product/ERP5/tests/testPerson.py
@@ -90,8 +90,8 @@ class TestPerson(ERP5TypeTestCase):
       person_copy = person_module.manage_copyObjects(ids=(person.getId(),))
       person_copy_id = person_module.manage_pasteObjects(person_copy)[0]['new_id']
       person_copy_obj = person_module[person_copy_id]
-      ## because we copy/paste Person object in the same ERP5 
-      ## instance its reference must be resetted
+      ## because we setup Person object from business template 
+      ## its reference must NOT be resetted
       self.assertEquals(person_copy_obj.getReference(), person.getReference())
 
 if __name__ == '__main__':
