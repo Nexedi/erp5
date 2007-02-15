f123a62cb6b2498169369efddedecf5bf6c56236
Jérome Perrin <jerome@nexedi.com>
2007-02-15 12:26:36 +0000

    secuity declaration was missing for getSearchableText
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@12722 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/Coordinate.py
+++ product/ERP5/Document/Coordinate.py
@@ -120,12 +120,15 @@ class Coordinate(Base):
         return self()
 
     security.declareProtected( Permissions.AccessContentsInformation,
-                               'SearchableText' )
+                               'getSearchableText' )
     def getSearchableText(self):
         """
             text for indexing
         """
         return "%s %s %s" % (self.title, self.description, self.asText())
+
+    security.declareProtected( Permissions.AccessContentsInformation,
+                               'SearchableText' )
     SearchableText = getSearchableText
 
     security.declareProtected( Permissions.AccessContentsInformation,
