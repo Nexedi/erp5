2da4f7041904de67619268263ccd28422e5eaf2a
Jean-Paul Smets <jp@nexedi.com>
2007-02-23 09:27:39 +0000

    More comments
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@12984 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/__init__.py
+++ product/ERP5Type/__init__.py
@@ -97,6 +97,7 @@ from AccessControl.SecurityInfo import allow_module
 
 allow_module('Products.ERP5Type.Cache')
 allow_module('Products.ERP5Type.Utils') # XXX this looks dangerous
+                                        # selected methods only should be allowed eg. cartesianProduct
 allow_module('Products.ERP5Type.Message')
 allow_module('Products.ERP5Type.Log')
 
