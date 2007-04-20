d71636f5272e7d064459f9e9d1c948838b6e7c41
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2007-04-20 09:26:53 +0000

    install erp5_pdm_ui_test before erp5_web_ui_test because erp5_web_ui_test changes the authentication.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@14135 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/prepareFunctionalTest.py
+++ product/ERP5Type/tests/prepareFunctionalTest.py
@@ -54,8 +54,8 @@ class TestZelenium(ERP5TypeTestCase):
         return ('erp5_base', 'erp5_ui_test', 'erp5_forge',
                 'erp5_trade', 'erp5_pdm', 'erp5_pdf_style',
                 'erp5_accounting', 'erp5_accounting_ui_test',
-                'erp5_web', 'erp5_web_ui_test',
                 'erp5_pdm_ui_test',
+                'erp5_web', 'erp5_web_ui_test',
                 # 'erp5_accounting_l10n_fr', 'erp5_payroll',
                 # 'erp5_payroll_ui_test',
                 )
