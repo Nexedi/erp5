b4140896cec14f9727b84155422e23dbadb9816f
Romain Courteaud <romain@nexedi.com>
2007-05-25 15:41:27 +0000

    Check that Packing List has no issue with float number. The idea was contributed by Lukasz Nowak.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@14620 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/tests/testPackingListRoundingPrecision.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2007 Nexedi SARL and Contributors. All Rights Reserved.
+#          Courteaud Romain <romain@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+##############################################################################
+    
+from testPackingList import TestPackingList
+
+class TestPackingListRoundingPrecision(TestPackingList):
+  """
+  Test that Order/Packing List haven't rounding issues with float values.
+  The idea was contributed by Lukasz Nowak
+  """
+  default_quantity = 59.333333333333336
+
+  def getTitle(self):
+    return "Packing List Rounding Precision"
+
+if __name__ == '__main__':
+  framework()
+else:
+  import unittest
+  def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestPackingListRoundingPrecision))
+    return suite
