99a8e383083c31f774023cad407d33d8b7b32ac1
Vincent Pelletier <vincent@nexedi.com>
2007-08-07 12:54:35 +0000

    Remove implicit join with catalog. You need to update Resource_zGetInventoryList and Resource_zGetMovementHistoryList from erp5_core.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@15527 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ZSQLCatalog/SQLCatalog.py
+++ product/ZSQLCatalog/SQLCatalog.py
@@ -2071,7 +2071,7 @@ class Catalog( Folder,
               new_key = '%s.%s' % (query_table, key)
             elif key == 'uid':
               # uid is always ambiguous so we can only change it here
-              new_key = 'catalog.uid'
+              new_key = '%s.uid' % (query_table, )
           else:
             new_key = key
           if new_key is not None:
@@ -2087,7 +2087,7 @@ class Catalog( Folder,
     select_expression = ''
     group_by_expression = ''
 
-    from_table_dict = {'catalog' : 'catalog'} # Always include catalog table
+    from_table_dict = {}
     if len(kw):
       if kw.has_key('select_expression'):
         select_expression_list.append(kw['select_expression'])
