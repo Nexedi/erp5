9473cfdd648e9a7ac82694ce9786d81f2c145f43
Jérome Perrin <jerome@nexedi.com>
2007-09-10 09:26:39 +0000

    add comments to ease understanding of the assertRaise(ConversionError here
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@16208 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5OOo/tests/testOOoBatchMode.py
+++ product/ERP5OOo/tests/testOOoBatchMode.py
@@ -101,6 +101,8 @@ class TestOoodResponse(ERP5TypeTestCase):
     request = self.portal.REQUEST
     request.RESPONSE.setHeader('content-type', 'text/html')
     ERP5Site_viewNothingAsOdt = self.getPortal().ERP5Site_viewNothingAsOdt
+    # This assumes that a conversion error is raised because oood coordinates
+    # are not defined in preferences.
     self.assertRaises(ConversionError, ERP5Site_viewNothingAsOdt,
                       batch_mode=0, format='pdf')
     self.assertEqual('text/html',
@@ -110,6 +112,8 @@ class TestOoodResponse(ERP5TypeTestCase):
     request = self.portal.REQUEST
     request.RESPONSE.setHeader('content-type', 'text/html')
     ERP5Site_viewNothingAsOdt = self.getPortal().ERP5Site_viewNothingAsOdt
+    # This assumes that a conversion error is raised because oood coordinates
+    # are not defined in preferences.
     self.assertRaises(ConversionError, ERP5Site_viewNothingAsOdt,
                          batch_mode=1, format='pdf')
     self.assertEqual('text/html', request.RESPONSE.getHeader('content-type').split(';')[0])
