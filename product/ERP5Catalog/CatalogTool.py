bdcf2ae88ad60f1b7b83cfc6d0cbbf148a5717eb
Vincent Pelletier <vincent@nexedi.com>
2007-09-10 14:16:07 +0000

    Test only when required.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@16226 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Catalog/CatalogTool.py
+++ product/ERP5Catalog/CatalogTool.py
@@ -715,10 +715,10 @@ class CatalogTool (UniqueObject, ZCatalog, CMFCoreCatalogTool, ActiveObject):
         """
           Remove from catalog.
         """
-        if path is None and uid is None and object is None:
-          raise TypeError, 'One of uid, path and object parameters must not be None'
         if path is None and uid is None:
           path = self.__url(object)
+        if object is None:
+          raise TypeError, 'One of uid, path and object parameters must not be None'
         self.uncatalog_object(path=path,uid=uid, sql_catalog_id=sql_catalog_id)
 
     security.declarePrivate('beforeUnindexObject')
