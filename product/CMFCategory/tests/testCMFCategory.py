e95bb73b9b93e2651195e3c206e2fc3ca59b09bf
Jérome Perrin <jerome@nexedi.com>
2007-09-18 13:17:14 +0000

    fix undefined names
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@16453 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/CMFCategory/tests/testCMFCategory.py
+++ product/CMFCategory/tests/testCMFCategory.py
@@ -26,13 +26,7 @@
 #
 ##############################################################################
 
-import os, sys
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py'))
-
-# Needed in order to have a log file inside the current folder
-os.environ['EVENT_LOG_FILE'] = os.path.join(os.getcwd(), 'zLOG.log')
-os.environ['EVENT_LOG_SEVERITY'] = '-300'
+import unittest
 
 from Testing import ZopeTestCase
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
@@ -906,12 +900,8 @@ class TestCMFCategory(ERP5TypeTestCase):
                       bc.getCategoryChildTitleItemList(base='bar'))
 
 
-if __name__ == '__main__':
-    framework()
-else:
-    import unittest
-    def test_suite():
-        suite = unittest.TestSuite()
-        suite.addTest(unittest.makeSuite(TestCMFCategory))
-        return suite
+def test_suite():
+  suite = unittest.TestSuite()
+  suite.addTest(unittest.makeSuite(TestCMFCategory))
+  return suite
 
