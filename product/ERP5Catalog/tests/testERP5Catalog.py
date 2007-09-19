632b906d40b5cb0bcc8964191bd72c7863771874
Jérome Perrin <jerome@nexedi.com>
2007-09-19 12:22:16 +0000

    remove unused code
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@16472 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Catalog/tests/testERP5Catalog.py
+++ product/ERP5Catalog/tests/testERP5Catalog.py
@@ -1787,15 +1787,11 @@ class TestERP5Catalog(ERP5TypeTestCase, LogInterceptor):
       message = 'Float Format'
       ZopeTestCase._print('\n%s ' % message)
       LOG('Testing... ',0,message)
-    org_a = self._makeOrganisation(title='org_a')
-    org_b = self._makeOrganisation(title='org_b')
-    sql_connection = self.getSQLConnection()
-    # Add a method in order to directly put values we want into
-    # the catalog.
-    catalog_kw = {'uid':{'query':'2 567.54',
-                               'format':'1 234.12',
-                               'type':'float'}}
-    sql_src = self.getCatalogTool()(src__=1,**catalog_kw)
+
+    catalog_kw = {'uid': {'query': '2 567.54',
+                          'format': '1 234.12',
+                          'type': 'float'}}
+    sql_src = self.getCatalogTool()(src__=1, **catalog_kw)
     self.failUnless("TRUNCATE(catalog.uid,2) = '2567.54'" in sql_src)
 
   def test_SearchOnOwner(self):
