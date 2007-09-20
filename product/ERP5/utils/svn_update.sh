a300098d23c1c9250af65ee3436ec9c5b9a01d19
Jérome Perrin <jerome@nexedi.com>
2007-09-20 15:45:53 +0000

    The official URL for business template repository is http://www.erp5.org/dists/snapshot/bt5/
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@16517 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/utils/svn_update.sh
+++ product/ERP5/utils/svn_update.sh
@@ -29,7 +29,8 @@ for p in $PRODUCTS
 # Get latests Business Templates
 echo ""
 echo "----- Updating Business Templates -----"
-wget -nv -N --no-host-directories -r --cut-dirs=2 --level=2 --relative --no-parent --accept=bt5,bt5list http://torrent.erp5.org/dists/snapshot/bt5/
+wget -nv -N --no-host-directories -r --cut-dirs=2 --level=2 --relative \
+  --no-parent --accept=bt5,bt5list http://www.erp5.org/dists/snapshot/bt5/
 rm -f robots.txt
 
 # Restore good right
