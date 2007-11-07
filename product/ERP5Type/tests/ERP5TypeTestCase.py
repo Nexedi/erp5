f1f3c24a5971f47dcf325e07d60f73e05ee6bb67
Yoshinori Okuji <yo@nexedi.com>
2007-11-07 03:07:03 +0000

    Use the original _compile against FSPythonScript.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@17430 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/ERP5TypeTestCase.py
+++ product/ERP5Type/tests/ERP5TypeTestCase.py
@@ -763,11 +763,12 @@ def optimize():
       PythonScript_compile(self)
     return PythonScript_exec(self, *args)
   PythonScript._exec = _exec
+  # Filesystem Python Script inherits from PythonScript, but
+  # the behavior is a bit inconsistent, and hard to override
+  # correctly. So just get back the original _compile. Note
+  # that _exec is overridden, so no need to recover.
   from Products.CMFCore.FSPythonScript import FSPythonScript
-  FSPythonScript_write = FSPythonScript._write
-  def _write(self, text, compile):
-    return FSPythonScript_write(self, text, 0)
-  FSPythonScript._write = _write
+  FSPythonScript._compile = PythonScript_compile
 
 
 optimize()
