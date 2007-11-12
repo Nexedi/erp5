1311b0d1832839e46f20eac8e9d0276a098dba92
Yoshinori Okuji <yo@nexedi.com>
2007-11-12 17:58:27 +0000

    Flush cache after creating a script.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@17542 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testGeographicalAddress.py
+++ product/ERP5/tests/testGeographicalAddress.py
@@ -170,6 +170,9 @@ return '%s\\n%s %s' % \\
        (context.getStreetAddress(),
         context.getZipCode(), context.getCity())
 """)
+    # Flush the cache because _getTypeBasedMethod caches the result,
+    # so that script will not be used.
+    self.getPortal().portal_caches.clearCache(cache_factory_list=('erp5_content_long',))
   
   def stepCheckAddressAsTextScript(self, sequence=None,
                                    sequence_list=None, **kw):
