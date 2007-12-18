7d63f617557fd59f51db82c04b85cccf91767ad5
Jérome Perrin <jerome@nexedi.com>
2007-12-18 17:11:27 +0000

    install erp5_web before erp5_dms, it's a requirement
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@18399 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/prepareFunctionalTest.py
+++ product/ERP5Type/tests/prepareFunctionalTest.py
@@ -48,9 +48,10 @@ class TestZelenium(ERP5TypeTestCase):
                 'erp5_trade', 'erp5_pdm', 'erp5_pdf_style',
                 'erp5_accounting', 'erp5_invoicing', 'erp5_accounting_ui_test',
                 'erp5_pdm_ui_test',
-                'erp5_dms', 'erp5_dms_mysql_innodb_catalog', 'erp5_dms_ui_test',
-                # erp5_web must run at the last.
-                'erp5_web', 'erp5_web_ui_test',
+                'erp5_web', 'erp5_dms', 'erp5_dms_mysql_innodb_catalog',
+                'erp5_dms_ui_test',
+                # erp5_web_ui_test must run at the last.
+                'erp5_web_ui_test',
                 # 'erp5_accounting_l10n_fr', 'erp5_payroll',
                 # 'erp5_payroll_ui_test',
                 )
