10fbabe968c4708d001f1a496f435b8b7d120e8c
Nicolas Delaby <nicolas@nexedi.com>
2008-02-07 18:19:41 +0000

    If one of user Role has View permission, authorised all roles
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19148 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Catalog/CatalogTool.py
+++ product/ERP5Catalog/CatalogTool.py
@@ -144,25 +144,27 @@ class IndexableObjectWrapper(CMFCoreIndexableObjectWrapper):
             new_dict[key] = new_list
         localroles = new_dict
         for user, roles in localroles.items():
+          # Added for ERP5 project by JP Smets
+          # The reason why we do not want to keep Owner is because we are
+          # trying to reduce the number of security definitions
+          # However, this is a bad idea if we start to use Owner role
+          # as a kind of bamed Assignee and if we need it for worklists. Therefore
+          # we may sometimes catalog the owner user ID whenever the Owner
+          # has view permission (see getAllowedRolesAndUsers bellow
+          # as well as getViewPermissionOwner method in Base)
+          view_role_list = [role for role in roles if allowed.has_key(role) and role != 'Owner']
           for role in roles:
             if allowed.has_key(role):
               if withnuxgroups:
                 allowed[user] = 1
               else:
                 allowed['user:' + user] = 1
-              # Added for ERP5 project by JP Smets
-              # The reason why we do not want to keep Owner is because we are
-              # trying to reduce the number of security definitions
-              # However, this is a bad idea if we start to use Owner role
-              # as a kind of bamed Assignee and if we need it for worklists. Therefore
-              # we may sometimes catalog the owner user ID whenever the Owner
-              # has view permission (see getAllowedRolesAndUsers bellow
-              # as well as getViewPermissionOwner method in Base)
-              if role != 'Owner':
-                if withnuxgroups:
-                  allowed[user + ':' + role] = 1
-                else:
-                  allowed['user:' + user + ':' + role] = 1
+            if view_role_list:
+              #One of Roles has view Permission.
+              if withnuxgroups:
+                allowed[user + ':' + role] = 1
+              else:
+                allowed['user:' + user + ':' + role] = 1
         if allowed.has_key('Owner'):
           del allowed['Owner']
         return list(allowed.keys())
