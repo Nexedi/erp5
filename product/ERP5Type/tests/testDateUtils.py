d98ce9ff50eabe8f0eb3f07370303ffef52aa321
Nicolas Delaby <nicolas@nexedi.com>
2008-02-08 09:35:21 +0000

    Use Explicit timezone GMT+7
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19156 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/testDateUtils.py
+++ product/ERP5Type/tests/testDateUtils.py
@@ -55,71 +55,74 @@ from DateTime import DateTime
 from Products.ERP5Type.DateUtils import addToDate
 
 class TestDateUtils(unittest.TestCase):
-  """Tests field value caching system
   """
+  Tests DateUtils
+  """
+
+  timezone = 'GMT+7'
 
   def getTitle(self):
     return "Date Utils"
 
   def test_integer_add_to_date(self):
-    date = DateTime('2000/01/01 UTC')
-    self.assertEqual(DateTime('2000/01/01 00:01:30 UTC').ISO(),
-                              addToDate(date, second=90).ISO())
-    self.assertEqual(DateTime('2000/01/01 01:30:00 UTC').ISO(),
-                              addToDate(date, minute=90).ISO())
-    self.assertEqual(DateTime('2000/01/04 18:00:00 UTC').ISO(),
-                              addToDate(date, hour=90).ISO())
-    self.assertEqual(DateTime('2000/03/31 UTC').ISO(),
-                              addToDate(date, day=90).ISO())
-    self.assertEqual(DateTime('2007/07/01 UTC').ISO(),
-                              addToDate(date, month=90).ISO())
-    self.assertEqual(DateTime('2090/01/01 UTC').ISO(),
-                              addToDate(date, year=90).ISO())
+    date = DateTime('2000/01/01 %s' % self.timezone)
+    self.assertEqual(DateTime('2000/01/01 00:01:30 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, second=90).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/01 01:30:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, minute=90).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/04 18:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, hour=90).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/03/31 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, day=90).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2007/07/01 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, month=90).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2090/01/01 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, year=90).toZone('UTC').ISO())
 
   def test_negative_add_to_date(self):
-    date = DateTime('2000/01/01 UTC')
-    self.assertEqual(DateTime('1999/12/31 23:59:59 UTC').ISO(),
-                              addToDate(date, second=-1).ISO())
-    self.assertEqual(DateTime('1999/12/31 23:59:00 UTC').ISO(),
-                              addToDate(date, minute=-1).ISO())
-    self.assertEqual(DateTime('1999/12/31 23:00:00 UTC').ISO(),
-                              addToDate(date, hour=-1).ISO())
-    self.assertEqual(DateTime('1999/12/31 00:00:00 UTC').ISO(),
-                              addToDate(date, day=-1).ISO())
-    self.assertEqual(DateTime('1999/12/01 00:00:00 UTC').ISO(),
-                              addToDate(date, month=-1).ISO())
-    self.assertEqual(DateTime('1999/01/01').ISO(),
-                              addToDate(date, year=-1).ISO())
+    date = DateTime('2000/01/01 %s' % self.timezone)
+    self.assertEqual(DateTime('1999/12/31 23:59:59 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, second=-1).toZone('UTC').ISO())
+    self.assertEqual(DateTime('1999/12/31 23:59:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, minute=-1).toZone('UTC').ISO())
+    self.assertEqual(DateTime('1999/12/31 23:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, hour=-1).toZone('UTC').ISO())
+    self.assertEqual(DateTime('1999/12/31 00:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, day=-1).toZone('UTC').ISO())
+    self.assertEqual(DateTime('1999/12/01 00:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, month=-1).toZone('UTC').ISO())
+    self.assertEqual(DateTime('1999/01/01 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, year=-1).toZone('UTC').ISO())
 
   def test_float_add_to_date(self):
-    date = DateTime('2000/01/01 UTC')
-    self.assertEqual(DateTime('2000/01/01').ISO(),
-                              addToDate(date, second=0.5).ISO())
-    self.assertEqual(DateTime('2000/01/01 00:00:30 UTC').ISO(),
-                              addToDate(date, minute=0.5).ISO())
-    self.assertEqual(DateTime('2000/01/01 00:30:00 UTC').ISO(),
-                              addToDate(date, hour=0.5).ISO())
-    self.assertEqual(DateTime('2000/01/01 12:00:00 UTC').ISO(),
-                              addToDate(date, day=0.5).ISO())
-    self.assertEqual(DateTime('2000/01/16 12:00:00 UTC').ISO(),
-                              addToDate(date, month=0.5).ISO())
-    self.assertEqual(DateTime('2000/07/01').ISO(),
-                              addToDate(date, year=0.5).ISO())
+    date = DateTime('2000/01/01 %s' % self.timezone)
+    self.assertEqual(DateTime('2000/01/01 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, second=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/01 00:00:30 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, minute=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/01 00:30:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, hour=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/01 12:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, day=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/01/16 12:00:00 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, month=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2000/07/01 %s' % self.timezone).toZone('UTC').ISO(),
+                              addToDate(date, year=0.5).toZone('UTC').ISO())
 
   def test_complex_float_add_to_date(self):
-    complex_date = DateTime('2004/03/16 01:23:54 UTC')
-    self.assertEqual(DateTime('2004/03/16 01:23:54 UTC').ISO(),
-                              addToDate(complex_date, second=0.5).ISO())
-    self.assertEqual(DateTime('2004/03/16 01:24:24 UTC').ISO(),
-                              addToDate(complex_date, minute=0.5).ISO())
-    self.assertEqual(DateTime('2004/03/16 01:53:54 UTC').ISO(),
-                              addToDate(complex_date, hour=0.5).ISO())
-    self.assertEqual(DateTime('2004/03/16 13:23:54 UTC').ISO(),
-                              addToDate(complex_date, day=0.5).ISO())
-    self.assertEqual(DateTime('2004/03/31 13:23:54 UTC').ISO(),
-                              addToDate(complex_date, month=0.5).ISO())
-    self.assertEqual(DateTime('2004/09/16 01:23:54 UTC').ISO(),
-                              addToDate(complex_date, year=0.5).ISO())
+    complex_date = DateTime('2004/03/16 01:23:54 %s' % self.timezone)
+    self.assertEqual(DateTime('2004/03/16 01:23:54 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, second=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2004/03/16 01:24:24 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, minute=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2004/03/16 01:53:54 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, hour=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2004/03/16 13:23:54 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, day=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2004/03/31 13:23:54 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, month=0.5).toZone('UTC').ISO())
+    self.assertEqual(DateTime('2004/09/16 01:23:54 %s' % self.timezone).toZone('UTC').ISO(),
+                   addToDate(complex_date, year=0.5).toZone('UTC').ISO())
 
 def test_suite():
   suite = unittest.TestSuite()
