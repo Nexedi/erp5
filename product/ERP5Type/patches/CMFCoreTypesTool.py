5918aad03134a43370874b60c9bace107984092d
Vincent Pelletier <vincent@nexedi.com>
2008-02-11 16:44:15 +0000

    Add missing parenthesis.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19249 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/patches/CMFCoreTypesTool.py
+++ product/ERP5Type/patches/CMFCoreTypesTool.py
@@ -4,7 +4,7 @@ from zLOG import LOG, INFO
 if getattr(TypeInformation, 'manage_main', None) is not None:
   LOG('ERP5Type/patches/CMFCoreTypesTool.py', INFO, 'This patch is no longer needed. Skipping.')
 elif None in (getattr(TypeInformation, 'manage_propertiesForm__roles__', None),
-              getattr(TypeInformation, 'manage_propertiesForm', None):
+              getattr(TypeInformation, 'manage_propertiesForm', None)):
   LOG('ERP5Type/patches/CMFCoreTypesTool.py', INFO, 'manage_propertiesForm not found. Skipping.')
 else:
   TypeInformation.manage_main__roles__ = TypeInformation.manage_propertiesForm__roles__
