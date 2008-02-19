96f7ac4b150366ba25f220dac7bb4654bb02c065
Yusei Tahara <yusei@nexedi.com>
2008-02-19 19:13:06 +0000

    Add a script which collect message from entire system and export pot file.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19422 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_forge/ExtensionTemplateItem/Glossary.py
+++ bt5/erp5_forge/ExtensionTemplateItem/Glossary.py
@@ -8,3 +8,22 @@ def getPropertySheetAttributeList(name):
     # we want to get only normal property.
     result.append(i['id'])
   return result
+
+
+def getActionTitleListFromAllActionProvider(portal):
+  result = {}
+  provider_list = []
+  for provider_id in portal.portal_actions.listActionProviders():
+    if provider_id in ('portal_types', 'portal_workflow'):
+      continue
+    provider = getattr(portal, provider_id, None)
+    if provider is None:
+      continue
+    provider_list.append(provider)
+
+  for typeinfo in portal.portal_types.objectValues():
+    provider_list.append(typeinfo)
+
+  for action in provider.listActions():
+      result[action.title] = None
+  return result.keys()
