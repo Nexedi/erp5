b3b83622d51b6200c697dd85a7c39a25c88a66d8
Yusei Tahara <yusei@nexedi.com>
2008-02-20 14:31:44 +0000

    Support message id extraction from page template.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19436 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_forge/ExtensionTemplateItem/Glossary.py
+++ bt5/erp5_forge/ExtensionTemplateItem/Glossary.py
@@ -27,3 +27,60 @@ def getActionTitleListFromAllActionProvider(portal):
   for action in provider.listActions():
       result[action.title] = None
   return result.keys()
+
+
+from StringIO import StringIO
+from TAL.HTMLTALParser import HTMLTALParser
+def findStaticTranslationText(page_template):
+  def iterate(node, target_name, function):
+    if type(node) is list:
+      for i in node:
+        iterate(i, target_name, function)
+    elif type(node) is tuple and node:
+      if node[0]==target_name:
+        function(node)
+      else:
+        for i in node[1:]:
+          iterate(i, target_name, function)
+
+  text_dict = {}
+  def addText(node):
+    if len(node)!=2:
+      node = (node[0], node[1:])
+    program = [node]
+    macros = {}
+    engine = MyDummyEngine(macros)
+    output = StringIO()
+    interpreter = MyDummyTALInterpreter(program, macros, engine, output)
+    interpreter()
+    if interpreter._i18n_message_id_dict is not None:
+      text_dict.update(interpreter._i18n_message_id_dict)
+
+  parser = HTMLTALParser()
+  parser.parseString(page_template._text)
+  iterate(parser.gen.program, 'insertTranslation', addText)
+  return text_dict.keys()
+
+#
+# Utility class for findStaticTranslationText
+#
+from TAL.TALInterpreter import TALInterpreter
+from TAL.DummyEngine import DummyEngine
+
+class MyDummyEngine(DummyEngine):
+  
+  def evaluate(self, expression):
+    return None
+
+class MyDummyTALInterpreter(TALInterpreter):
+
+  _i18n_message_id_dict = None
+  _currentTag = None
+
+  def translate(self, msgid, default, i18ndict, obj):
+    try:
+      self._i18n_message_id_dict[msgid] = None
+    except TypeError:
+      self._i18n_message_id_dict = {msgid:None}
+
+    return TALInterpreter.translate(self, msgid, default, i18ndict, obj)
