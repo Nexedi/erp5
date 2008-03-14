76765f4cfed3b4b23ade882f13101fba064226a7
Romain Courteaud <romain@nexedi.com>
2008-03-14 14:36:58 +0000

    Revert r19228, which break planningbox editor.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@19911 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Form/PlanningBox.py
+++ product/ERP5Form/PlanningBox.py
@@ -1867,9 +1867,6 @@ class BasicGroup:
             #else:
             #  url = object.getUrl()
             url = object.getUrl()
-            if url is not None:
-              # View is considered the default action from one object
-              url += "/view"
 
             # XXX should define height of block here
             #height = None
@@ -1961,9 +1958,6 @@ class BasicGroup:
 
           # get object url, not group url
           url = self.object.getObject().getUrl()
-          if url is not None:
-            # View is considered the default action from one object
-            url += "/view"
 
           # creating new activity instance
           activity=BasicActivity(title=info['info_center'], name=name,
