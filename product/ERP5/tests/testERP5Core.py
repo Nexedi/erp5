25a1b136d07f4b7c0fc297ddaefa1853a556cbce
Jérome Perrin <jerome@nexedi.com>
2008-03-20 08:34:41 +0000

    getPortalModuleTypeList must be called on the portal
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@20061 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testERP5Core.py
+++ product/ERP5/tests/testERP5Core.py
@@ -104,7 +104,8 @@ class TestERP5Core(ERP5TypeTestCase, ZopeTestCase.Functional):
                       self.portal._getOb(module_id).getTitle())
     self.assertNotEqual(types_tool.getTypeInfo(module_portal_type), None)
     self.assertNotEqual(types_tool.getTypeInfo(object_portal_type), None)
-    self.assertTrue(module_portal_type in self.getPortalModuleTypeList())
+    self.assertTrue(module_portal_type
+                      in self.portal.getPortalModuleTypeList())
     
     skin_folder = skins_tool._getOb(portal_skins_folder, None)
     self.assertNotEqual(skin_folder, None)
