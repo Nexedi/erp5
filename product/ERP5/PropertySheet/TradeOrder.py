ae14c211abea942f2476c233e13374465dd2896c
Alexandre Boeglin <alex@nexedi.com>
2008-04-09 15:00:48 +0000

    Add a new PropertySheet containing constraints that can be enforced on Trade Orders. Currently not enabled by default, as removing constraints is not really convenient.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@20391 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/PropertySheet/TradeOrder.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2008 Nexedi SA and Contributors. All Rights Reserved.
+#          Alexandre Boeglin  <alex@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+
+class TradeOrder:
+  """
+  Constraints that can be enforced on Trade Orders
+  """
+
+  _properties = (
+  )
+
+  _categories = (
+  )
+
+  _constraints = (
+    { 'id'            : 'category_existence',
+      'description'   : 'Source, Destination and Sections must be defined',
+      'type'          : 'CategoryExistence',
+      'portal_type'   : ('Person', 'Organisation'),
+      'source'        : 1,
+      'destination'        : 1,
+      'source_section': 1,
+      'destination_section': 1,
+    },
+    { 'id'            : 'total_quantity',
+      'description'   : 'Total Quantity must not be 0',
+      'type'          : 'TALESConstraint',
+      'expression'    : 'python: object.getTotalQuantity() > 0',
+      'message_expression_false': 'Total Quantity must not be 0',
+    },
+    { 'id'            : 'start_date',
+      'description'   : 'Start Date must be defined',
+      'type'          : 'PropertyExistence',
+      'start_date'    : 1,
+    },
+    { 'id'            : 'date_coherency',
+      'description'   : 'Stop Date must be after Start Date',
+      'type'          : 'TALESConstraint',
+      'expression'    : 'python: object.getStopDate() >= object.getStartDate()',
+      'message_expression_false': 'Stop Date must be after Start Date',
+    },
+  )
