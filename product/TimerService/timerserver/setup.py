ff4be137e34775ec64bfff7ff37aea1341fb2e11
Jérome Perrin <jerome@nexedi.com>
2008-05-22 09:34:14 +0000

    Reorganize timerserver python package to stop hardcoding "./lib/python%s.%s/site-packages/%s'%(sys.version_info[0]"  and be setuptools compatible.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@21079 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/TimerService/timerserver/setup.py
+++ product/TimerService/timerserver/setup.py
@@ -1,23 +1,35 @@
+
+modname = 'timerserver'
+version = open('version.txt').read().strip()
+numversion = version.split('.') 
+
+license = 'GPL'
+copyright = '''Nikolay Kim (c) 2004'''
+
+author = "Nikolay Kim"
+author_email = "fafhrd@legco.biz"
+
+short_desc = "Timer Server for Zope"
+long_desc = short_desc 
+
+web = ""
+ftp = ""
+mailing_list = ""
 #!/usr/bin/env python
 import sys
-from distutils import util
-from distutils.core import setup, Extension
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
 
-from __pkginfo__ import modname, version, license, short_desc, long_desc,\
-     web, author, author_email
-
-if __name__ == '__main__' :
-    dist = setup(name = modname,
-                 version = version,
-                 license =license,
-                 description = short_desc,
-                 long_description = long_desc,
-                 author = author,
-                 author_email = author_email,
-                 url = web,
-                 package_dir = {modname: '.'},
-                 packages = [modname,],
-		 data_files = [
-		   ('./lib/python%s.%s/site-packages/%s'%(sys.version_info[0], sys.version_info[1], modname), 
-		   ['component.xml'])]
-                 )
+setup(name='timerserver',
+      version='2.0',
+      license='GPL',
+      description='Timer Server for Zope',
+      long_description='',
+      author='Nikolay Kim',
+      author_email='fafhrd@legco.biz',
+      packages=['timerserver'],
+      zip_safe=False,
+      package_data={'timerserver': ['component.xml']},
+    )
