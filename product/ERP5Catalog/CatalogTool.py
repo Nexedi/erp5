a8f843075431db1f9d8566adbea967341efa5691
Aurélien Calonne <aurel@nexedi.com>
2008-06-02 09:49:43 +0000

    revert previous commit, seems not to work in some cases
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@21262 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Catalog/CatalogTool.py
+++ product/ERP5Catalog/CatalogTool.py
@@ -43,7 +43,6 @@ from Products.CMFActivity.ActiveObject import ActiveObject
 from Products.ERP5Type.TransactionalVariable import getTransactionalVariable
 
 from AccessControl.PermissionRole import rolesForPermissionOn
-from ExtensionClass import Base
 
 from Products.PageTemplates.Expressions import SecureModuleImporter
 from Products.CMFCore.Expression import Expression
@@ -94,7 +93,7 @@ def getSecurityProduct(acl_users):
     return SECURITY_USING_NUX_USER_GROUPS
 
 
-class IndexableObjectWrapper(CMFCoreIndexableObjectWrapper, Base):
+class IndexableObjectWrapper(CMFCoreIndexableObjectWrapper):
 
     def __setattr__(self, name, value):
       # We need to update the uid during the cataloging process
@@ -746,8 +745,8 @@ class CatalogTool (UniqueObject, ZCatalog, CMFCoreCatalogTool, ActiveObject):
         if predicate_property_dict is not None:
           vars['predicate_property_dict'] = predicate_property_dict
         vars['security_uid'] = security_uid
-        return w.__of__(object.aq_parent)
-        #return w
+
+        return w
 
     security.declarePrivate('reindexObject')
     def reindexObject(self, object, idxs=None, sql_catalog_id=None,**kw):
