Nexedi/erp5

commit dfb5f28b64de1213af4a6deac22eea9350c80e2b

Finish r21301 (always convert in float).
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@21303 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/Measure.py
@@ -132,7 +132,7 @@ class Measure(XMLMatrix):
     metric_type = self.getMetricType()
     if quantity_unit is not None and metric_type and \
         quantity_unit.getParentId() == metric_type.split('/', 1)[0]:
-      return quantity_unit.getProperty('quantity')
+      return float(quantity_unit.getProperty('quantity'))
 
   security.declareProtected(AccessContentsInformation, 'getConvertedQuantity')
   def getConvertedQuantity(self, variation_list=()):
@@ -190,7 +190,7 @@ class Measure(XMLMatrix):
       if quantity is not None:
         quantity *= quantity_unit
         if (not default or quantity ==
-            resource.getQuantityUnitValue().getProperty('quantity')):
+            float(resource.getQuantityUnitValue().getProperty('quantity'))):
           return (uid, resource_uid, '^', metric_type_uid, quantity),
       return ()
 

product/ERP5/Document/Resource.py
@@ -887,7 +887,7 @@ class Resource(XMLMatrix, Variated):
         # At this point, we know there is no default measure and we must add
         # a row for the management unit, with the resource's uid as uid, and
         # a generic metric_type.
-        quantity = quantity_unit_value.getProperty('quantity')
+        quantity = float(quantity_unit_value.getProperty('quantity'))
         metric_type_uid = self.getPortalObject().portal_categories \
                               .getCategoryUid(metric_type, 'metric_type')
         if quantity and metric_type_uid:

product/ERP5/Tool/SimulationTool.py
@@ -1222,8 +1222,8 @@ class SimulationTool(BaseTool):
         table_alias_list=(("measure", "measure"),))
 
       if isinstance(quantity_unit, str):
-        quantity_unit = getCategory(quantity_unit,
-                                   'quantity_unit').getProperty('quantity')
+        quantity_unit = float(getCategory(quantity_unit, 'quantity_unit')
+                              .getProperty('quantity'))
 
       method = getattr(self,'get%sInventoryList' % simulation_period)
       return method(quantity_unit=quantity_unit, **kw)

commit 48e45be50bf9fe2bad9511ede2793ac46d49fb18

Fix testInventoryAPI.TestUnitConversion (broken by r21303).
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@21315 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/Measure.py
@@ -32,6 +32,13 @@ from Products.ERP5Type.Permissions import AccessContentsInformation
 from Products.ERP5Type.XMLMatrix import XMLMatrix
 from Products.ERP5.Variated import Variated
 
+
+def getQuantity(quantity_unit_value):
+  quantity = quantity_unit_value.getProperty('quantity')
+  if quantity is not None:
+    return float(quantity)
+
+
 class Measure(XMLMatrix):
   """
     A Measure
@@ -132,7 +139,7 @@ class Measure(XMLMatrix):
     metric_type = self.getMetricType()
     if quantity_unit is not None and metric_type and \
         quantity_unit.getParentId() == metric_type.split('/', 1)[0]:
-      return float(quantity_unit.getProperty('quantity'))
+      return getQuantity(quantity_unit)
 
   security.declareProtected(AccessContentsInformation, 'getConvertedQuantity')
   def getConvertedQuantity(self, variation_list=()):
@@ -190,7 +197,7 @@ class Measure(XMLMatrix):
       if quantity is not None:
         quantity *= quantity_unit
         if (not default or quantity ==
-            float(resource.getQuantityUnitValue().getProperty('quantity'))):
+            getQuantity(resource.getQuantityUnitValue())):
           return (uid, resource_uid, '^', metric_type_uid, quantity),
       return ()
 

product/ERP5/Document/Resource.py
@@ -887,11 +887,11 @@ class Resource(XMLMatrix, Variated):
         # At this point, we know there is no default measure and we must add
         # a row for the management unit, with the resource's uid as uid, and
         # a generic metric_type.
-        quantity = float(quantity_unit_value.getProperty('quantity'))
+        quantity = quantity_unit_value.getProperty('quantity')
         metric_type_uid = self.getPortalObject().portal_categories \
                               .getCategoryUid(metric_type, 'metric_type')
         if quantity and metric_type_uid:
           uid = self.getUid()
-          insert_list += (uid, uid, '^', metric_type_uid, quantity),
+          insert_list += (uid, uid, '^', metric_type_uid, float(quantity)),
 
       return insert_list