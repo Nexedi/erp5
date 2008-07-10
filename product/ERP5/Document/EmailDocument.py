b589d1fda5cb0aec3b9425ace656ed798db1fec6
Nicolas Delaby <nicolas@nexedi.com>
2008-07-10 08:06:36 +0000

    Handle declared wrong enconding
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@22413 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/EmailDocument.py
+++ product/ERP5/Document/EmailDocument.py
@@ -279,7 +279,10 @@ class EmailDocument(File, TextDocument):
       if part.get_content_type() == 'text/plain' and not text_result and not part.is_multipart():
         part_encoding = part.get_content_charset()
         if part_encoding not in (None, 'utf-8',):
-          text_result = part.get_payload(decode=1).decode(part_encoding).encode('utf-8')
+          try:
+            text_result = part.get_payload(decode=1).decode(part_encoding).encode('utf-8')
+          except UnicodeDecodeError:
+            text_result = part.get_payload(decode=1)
         else:
           text_result = part.get_payload(decode=1)
       elif part.get_content_type() == 'text/html' and not html_result and not part.is_multipart():
