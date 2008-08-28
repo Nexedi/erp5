23685d16238513520f5b88fae2e818ea2f4c183d
Fabien Morin <fabien@nexedi.com>
2008-08-28 16:29:49 +0000

    enable worklist displaying on another workflow
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@23258 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_egov/ExtensionTemplateItem/ERP5EGov_Extensions.py
+++ bt5/erp5_egov/ExtensionTemplateItem/ERP5EGov_Extensions.py
@@ -39,7 +39,7 @@ def getPoralTypeListForWorkflow(self, workflow):
 
   return portal_type_list
 
-def getPortalTypeWorklistDictForWorkflow(self, workflow):
+def getPortalTypeWorklistDictForWorkflow(self, workflow_list):
   """
     return a dict containing portal_type and all informations about work_list
     of workflow :
@@ -56,43 +56,50 @@ def getPortalTypeWorklistDictForWorkflow(self, workflow):
               .....
     }
   """
-  portal_type_list = self.getPoralTypeListForWorkflow(self, workflow=workflow)
-  portal_workflow = self.getPortalObject().portal_workflow
-  workflow = getattr(portal_workflow, workflow, None)
-
   portal_type_worklist_dict = {}
+  portal_workflow = self.getPortalObject().portal_workflow
 
-  if workflow is not None:
-    worklist_dict = workflow.worklists._mapping
-    for portal_type in portal_type_list:
-      for worklist_id, worklist in worklist_dict.items():
-        state = None
-        if worklist.var_matches.has_key('validation_state'):
-          state=worklist.var_matches['validation_state']
-        local_role_list=worklist.guard.roles
-        if state is None:
-          continue
+  if not isinstance(workflow_list, list):
+    workflow = [workflow_list]
 
-        result = self.getPortalObject().portal_catalog(portal_type=portal_type,
-                                        local_roles=local_role_list,
-                                        validation_state=state)
-        if not len(result):
-          continue
+  for workflow in workflow_list:
 
-        if not portal_type_worklist_dict.has_key(portal_type):
-          portal_type_worklist_dict[portal_type] = {}
+    portal_type_list = self.getPoralTypeListForWorkflow(self, 
+        workflow=workflow)
+    workflow = getattr(portal_workflow, workflow, None)
 
-        portal_type_worklist_dict[portal_type][worklist_id] = {}
-        result_dict = portal_type_worklist_dict[portal_type][worklist_id]
-        result_dict['id']=worklist_id
-        result_dict['count']=len(result)
-        result_dict['description']=worklist.description
-        result_dict['validation_state']=state
-        result_dict['formated_name']=worklist.actbox_name
-        result_dict['formated_url']=worklist.actbox_url
-        result_dict['formated_url']='Base_viewSearchResultList?validation_state=%s&local_roles=%s&reset=1&portal_type=%s'\
-          % (state[0], local_role_list[0], portal_type)
-        result_dict['category']=worklist.actbox_category
-        result_dict.update(worklist.guard.__dict__)
+    if workflow is not None:
+      worklist_dict = workflow.worklists._mapping
+      for portal_type in portal_type_list:
+        for worklist_id, worklist in worklist_dict.items():
+          state = None
+          if worklist.var_matches.has_key('validation_state'):
+            state=worklist.var_matches['validation_state']
+          local_role_list=worklist.guard.roles
+          if state is None:
+            continue
+
+          result = self.getPortalObject().portal_catalog(\
+                                          portal_type=portal_type,
+                                          local_roles=local_role_list,
+                                          validation_state=state)
+          if not len(result):
+            continue
+
+          if not portal_type_worklist_dict.has_key(portal_type):
+            portal_type_worklist_dict[portal_type] = {}
+
+          portal_type_worklist_dict[portal_type][worklist_id] = {}
+          result_dict = portal_type_worklist_dict[portal_type][worklist_id]
+          result_dict['id']=worklist_id
+          result_dict['count']=len(result)
+          result_dict['description']=worklist.description
+          result_dict['validation_state']=state
+          result_dict['formated_name']=worklist.actbox_name
+          result_dict['formated_url']=worklist.actbox_url
+          result_dict['formated_url']='Base_viewSearchResultList?validation_state=%s&local_roles=%s&reset=1&portal_type=%s'\
+            % (state[0], local_role_list[0], portal_type)
+          result_dict['category']=worklist.actbox_category
+          result_dict.update(worklist.guard.__dict__)
   return portal_type_worklist_dict
 
