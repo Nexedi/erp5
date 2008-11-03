f7398b3c526621826dc4d2345a93e1585b3d4c74
Aurélien Calonne <aurel@nexedi.com>
2008-11-03 14:31:02 +0000

    for unit test do not set not working days
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@24459 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Banking/tests/TestERP5BankingMixin.py
+++ product/ERP5Banking/tests/TestERP5BankingMixin.py
@@ -745,6 +745,12 @@ class TestERP5BankingMixin:
       self.portal.manage_addProperty('reference_currency_id', 'EUR', type='string')
     else:
       self.portal._updateProperty('reference_currency_id', "EUR")
+    # not working days
+    if not self.portal.hasProperty('not_working_days'):
+      self.portal.manage_addProperty('not_working_days', '', type='string')
+    else:
+      self.portal._updateProperty('not_working_days', "")
+      
     setattr(self.portal,'functionnal_test_mode',1)
     # the person module
     self.person_module = self.getPersonModule()
