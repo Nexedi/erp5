554d8f5e901da9d45c226a729a1de71d883709d3
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2009-02-01 10:47:17 +0000

    compare content-type without charset, because zope-2.9 adds charset here.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@25406 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testBase.py
+++ product/ERP5/tests/testBase.py
@@ -992,7 +992,8 @@ class TestBase(ERP5TypeTestCase, ZopeTestCase.Functional):
     # if it didn't raise Unauthorized, Ok
     response = self.publish('%s/Base_download' % f.getPath())
     self.assertEquals(file(__file__).read(), response.body)
-    self.assertEquals('text/plain', response.headers['content-type'])
+    self.assertEquals('text/plain',
+                      response.headers['content-type'].split(';')[0])
     self.assertEquals('attachment; filename="%s"' % os.path.basename(__file__),
                       response.headers['content-disposition'])
 
