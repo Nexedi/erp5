0895ee7be2387a6ad3fc3b9c51fe187955e541e3
Ivan Tyagov <ivan@nexedi.com>
2009-02-25 09:08:41 +0000

    Moved to ERP5Type.Utils
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@25689 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Wizard/Tool/WizardTool.py
+++ product/ERP5Wizard/Tool/WizardTool.py
@@ -38,7 +38,8 @@ from zLOG import LOG, INFO, WARNING, ERROR, DEBUG
 from cStringIO import StringIO
 from UserDict import UserDict
 import xmlrpclib, socket, sys, traceback, urllib, urllib2, base64, cgi
-from AccessControl.SecurityManagement import newSecurityManager, getSecurityManager, setSecurityManager
+from AccessControl.SecurityManagement import setSecurityManager
+from Products.ERP5Type.Utils import _setSuperSecurityManager
 import zLOG, cookielib
 from urlparse import urlparse, urlunparse
 from base64 import encodestring, decodestring
@@ -83,19 +84,6 @@ def _getAcCookieFromServer(url, opener, cookiejar, username, password, header_di
   request = urllib2.Request(url, data, header_dict)
   f = opener.open(request)
   return f
-
-def _setSuperSecurityManager(self, user_name=None):
-  """ Change to super user account or passed user_name.
-      Return original Security Manager
-  """
-  original_security_manager = getSecurityManager()
-  if user_name is not None:
-    user_folder = self.getPortalObject().acl_users
-    user = user_folder.getUserById(user_name).__of__(user_folder)
-  else:
-    user = self.getWrappedOwner()
-  newSecurityManager(self.REQUEST, user)
-  return original_security_manager
 
 class GeneratorCall(UserDict):
   """ Class use to generate/interpret XML-RPC call for the wizard. """
