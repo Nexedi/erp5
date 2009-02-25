dceb769cef402ccf99144c88ff0b87c18cbcea0c
Ivan Tyagov <ivan@nexedi.com>
2009-02-25 08:21:17 +0000

    Moved from ERP5Wizard (soon to be refactored as a tool - portal_webservice).
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@25681 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/ConnectionPlugin/XMLRPCConnection.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2006 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Ivan Tyagov <ivan@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import xmlrpclib
+from urlparse import urlparse
+
+class XMLRPCConnection:
+  """
+    Holds an XML-RPC connection to a remote XML-RPC server.
+  """
+
+  def __init__(self, url, user_name = None, password = None):
+    self.url = url
+    self._user_name = user_name
+    self._password = password
+
+  def connect(self):
+    """Get a handle to a remote connection."""
+    url = self.url
+    if self._user_name is not None and self._password is not None:
+      # add HTTP Basic Authentication
+      schema = urlparse(url)
+      url = '%s://%s:%s@%s%s' %(schema[0], self._user_name, self._password,
+                                schema[1], schema[2])
+    return xmlrpclib.ServerProxy(url, allow_none=1)
---
+++ product/ERP5Type/WebServiceMixIn.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2006 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Ivan Tyagov <ivan@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions
+from Products.ERP5Type.ConnectionPlugin.XMLRPCConnection import XMLRPCConnection
+from Products.ERP5Type.ConnectionPlugin.SOAPConnection import SOAPConnection
+
+class WebServiceMixIn:
+  """
+  Do all kinds of web services in all kinds of protocols.
+  """
+
+  security = ClassSecurityInfo()
+
+  security.declareProtected('connect', Permissions.ManagePortal)
+  def connect(self, url, user_name=None, password=None, transport=None):
+    """
+    Connect to remote instances
+    of any kind of web service (not only ERP5) with many
+    different kinds of authentication.
+    authentication_method: 'xml-rpc' or 'soap'
+    """
+    # XXX: implement connection caching per zope thread
+    if transport == 'xml-rpc':
+      connection_handler = XMLRPCConnection(url, user_name, password)
+    elif transport == 'soap':
+      connection_handler = SOAPConnection(url, user_name, password)
+
+    connection_handler = connection_handler.connect()
+    return connection_handler
