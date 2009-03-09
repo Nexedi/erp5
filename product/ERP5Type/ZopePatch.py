ee937e2f9ff50345531b2cdaf826805071150158
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2009-03-09 09:37:56 +0000

    allow access to AcceptLanguage for itools 0.16.2 or later.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@25914 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -38,6 +38,7 @@ from Products.ERP5Type.patches import FSZSQLMethod
 from Products.ERP5Type.patches import ActionInformation
 from Products.ERP5Type.patches import ActionProviderBase
 from Products.ERP5Type.patches import CookieCrumbler
+from Products.ERP5Type.patches import i18n
 from Products.ERP5Type.patches import Localizer
 from Products.ERP5Type.patches import CMFMailIn
 from Products.ERP5Type.patches import CMFCoreUtils
---
+++ product/ERP5Type/patches/i18n.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
+#
+##############################################################################
+
+# allow access to AcceptLanguage for itools 0.16.2 or later
+from itools.i18n.accept import AcceptLanguage
+if getattr(AcceptLanguage, '__allow_access_to_unprotected_subobjects__',
+           None) is None:
+  AcceptLanguage.__allow_access_to_unprotected_subobjects__ = 1
+del AcceptLanguage
