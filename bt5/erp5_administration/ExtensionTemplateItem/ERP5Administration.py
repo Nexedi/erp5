a78cf6742ea2c9cd5d7fcbb3292c0cbfbd104570
Łukasz Nowak <luke@nexedi.com>
2009-04-07 13:23:57 +0000

    - add scripts and alarm to check and fix _folder_handler
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@26335 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_administration/ExtensionTemplateItem/ERP5Administration.py
+++ bt5/erp5_administration/ExtensionTemplateItem/ERP5Administration.py
@@ -1,3 +1,4 @@
+from zLOG import LOG, INFO
 def dumpWorkflowChain(self):
   # This method outputs the workflow chain in the format that you can
   # easily get diff like the following:
@@ -27,3 +28,34 @@ def dumpWorkflowChain(self):
     for chain in i['chain']:
       output.append('%s,%s' % (i['id'], chain))
   return '\n'.join(output)
+
+def checkFolderHandler(self, fixit=0, **kw):
+  error_list = []
+  try:
+    is_btree = self.isBTree()
+    is_hbtree = self.isHBTree()
+  except AttributeError:
+    return error_list
+  message = '%s' % self.absolute_url_path()
+  problem = False
+  if not is_btree and not is_hbtree:
+    problem = True
+    message = '%s is NOT BTree NOR HBTree' % message
+    if fixit:
+      try:
+        result = self._fixFolderHandler()
+      except AttributeError:
+        result = False
+      if result:
+        message = '%s fixed' % message
+      else:
+        message = '%s CANNOT FIX' % message
+  if is_btree and is_hbtree:
+    problem = True
+    message = '%s is BTree and HBTree' % message
+    if fixit:
+      message = '%s CANNOT FIX' % message
+  if problem:
+    error_list.append(message)
+    LOG('checkFolderHandler', INFO, message)
+  return error_list
