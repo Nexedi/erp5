153b27d94f77ddb01f684255e2ed49663b798179
Jean-Paul Smets <jp@nexedi.com>
2009-04-15 13:28:07 +0000

    Initial revision of interfaces which we be used for Open Order and SImulation in general
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@26440 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/SubscriptionItem.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SA and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Globals import InitializeClass, PersistentMapping
+from AccessControl import ClassSecurityInfo
+
+from Products.ERP5Type import Permissions, PropertySheet, Constraint, Interface
+from Products.ERP5.Document.Item import Item
+
+class SubscriptionItem(Item):
+  """
+    A SubscriptionItem is an Item which can be expanded
+    whenever it related to a valid Open Order
+  """
+  meta_type = 'ERP5 Subscription Item'
+  portal_type = 'Subscription Item'
+  isPortalContent = 1
+  isRADContent = 1
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  # Declarative interfaces
+  __implements__ = (Interface.Expandable, Interface.OpenOrderExpander)
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.XMLObject
+                    , PropertySheet.CategoryCore
+                    , PropertySheet.DublinCore
+                    , PropertySheet.Price
+                    , PropertySheet.Item
+                    , PropertySheet.Amount
+                    , PropertySheet.Reference
+                    )
+
+  # Expandable Interface Implementation
+  def expand(self, *args, **kw): 
+    """
+      Lookup start / stop properties in related Open Order
+      or Path and expand.
+    """
+
+  security.declarePrivate('expandOpenOrderRule')
+  def expandOpenOrderRule(self, applied_rule_id=None, force=0, **kw):
+    """
+      Provides the default implementation of expand for Open Orders
+    """
+
