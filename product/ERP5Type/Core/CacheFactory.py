be9c3562c6cc4a961f19b77356be32c66f5b97c9
Nicolas Delaby <nicolas@nexedi.com>
2009-05-18 15:35:59 +0000

    remove blank spaces
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27031 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/Core/CacheFactory.py
+++ product/ERP5Type/Core/CacheFactory.py
@@ -39,18 +39,18 @@ class CacheFactory(XMLObject):
   """
   CacheFactory is a collection of cache plugins. CacheFactory is an object which lives in ZODB.
   """
-    
+
   meta_type = 'ERP5 Cache Factory'
   portal_type = 'Cache Factory'
   isPortalContent = 1 
   isRADContent = 1
-  
+
   allowed_types = ('ERP5 Ram Cache', 
                    'ERP5 Distributed Ram Cache', 
                    'ERP5 SQL Cache',
                    'ERP5 Zodb Cache',
                   )
-    
+
   security = ClassSecurityInfo()
   security.declareProtected(Permissions.ManagePortal,
                             'manage_editProperties',
@@ -63,26 +63,26 @@ class CacheFactory(XMLObject):
                     , PropertySheet.Folder
                     , CacheFactory  
                     )
-    
-       
+
+
   def getCachePluginList(self):
     """ get ordered list of installed cache plugins in ZODB """
     cache_plugins = self.objectValues(self.allowed_types)
     cache_plugins = map(None, cache_plugins)
     cache_plugins.sort(key=lambda x: x.getIntIndex(0))
     return  cache_plugins
-    
+
   security.declareProtected(Permissions.AccessContentsInformation, 'getRamCacheFactory')
   def getRamCacheFactory(self):
     """ Return RAM based cache factory """
     erp5_site_id = self.getPortalObject().getId()
     return CachingMethod.factories[erp5_site_id][self.cache_scope]
-    
+
   security.declareProtected(Permissions.AccessContentsInformation, 'getRamCacheFactoryPluginList')
   def getRamCacheFactoryPluginList(self):
     """ Return RAM based list of cache plugins for this factory """
     return self.getRamCacheFactory().getCachePluginList()
-  
+
   def clearCache(self):
     """ clear cache for this cache factory """
     for cp in self.getRamCacheFactory().getCachePluginList():
