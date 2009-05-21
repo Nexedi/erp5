645bc2119d9d31fe0e31c2c1d2e60eddcc31e553
Jean-Paul Smets <jp@nexedi.com>
2009-05-21 14:20:13 +0000

    Added support for the documentatoin of portal type role definitions in the context of a business template.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27085 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/DocumentationHelper/BusinessTemplateRoleDocumentationHelper.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2007-2008 Nexedi SA and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
+from Products.ERP5Type import Permissions
+from PortalTypeDocumentationHelper import PortalTypeDocumentationHelper
+
+class BusinessTemplateRoleDocumentationHelper(PortalTypeDocumentationHelper):
+  """
+    Provides access to all documentation information
+    of a portal type relate to role definition in the 
+    context of a business template.
+  """
+
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  _section_list = (
+    dict(
+      id='role',
+      title='Role Definitions',
+      class_name='PortalTypeRoleDocumentationHelper',
+    ),
+  )
+
+InitializeClass(PortalTypeDocumentationHelper)
