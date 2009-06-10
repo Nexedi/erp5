f77132c3d844a3f044cd535a28d830b02bf5fcd0
Nicolas Delaby <nicolas@nexedi.com>
2009-06-10 14:10:50 +0000

    Change property server by relation to link to Memcached Plugin object
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27498 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/PropertySheet/DistributedRamCache.py
+++ product/ERP5Type/PropertySheet/DistributedRamCache.py
@@ -31,11 +31,4 @@ class DistributedRamCache:
   """
   """
 
-  _properties = (
-        {'id'          : 'server',
-         'description' : 'Memcached server address( you can specify multiple servers by separating them with ;)',
-         'type'        : 'string',
-         'default'     : '127.0.0.1:11211',
-        },
-        )
-
+  _categories = ( 'specialise', )
