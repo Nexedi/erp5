5b8495675d9e6760e47621c840fa061510e6b693
Nicolas Delaby <nicolas@nexedi.com>
2009-06-15 15:29:52 +0000

    add missing fake method
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27584 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/Tool/MemcachedTool.py
+++ product/ERP5Type/Tool/MemcachedTool.py
@@ -341,5 +341,6 @@ else:
         ' server administrator to enable it by installing python-memcached.'
 
     manage_beforeDelete = failingMethod
+    manage_afterAdd = failingMethod
     memcached_tool_configure = failingMethod
     getMemcachedDict = failingMethod
