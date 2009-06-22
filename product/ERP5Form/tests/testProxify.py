0c7f215d11ee239a34417ff88fbf259835a52945
Yusei Tahara <yusei@nexedi.com>
2009-06-22 03:16:01 +0000

    Remove unnecessary function.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27707 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Form/tests/testProxify.py
+++ product/ERP5Form/tests/testProxify.py
@@ -204,37 +204,3 @@ def test_suite():
   suite = unittest.TestSuite()
   suite.addTest(unittest.makeSuite(TestProxify))
   return suite
-
-
-def get_field_data(field):
-  from Products.Formulator.MethodField import Method
-  value_dict = {}
-  tales_dict = {}
-
-  if field.meta_type=='ProxyField':
-    template_field = field.getRecursiveTemplateField()
-    for ui_field_id in template_field.form.fields.keys():
-      value = field.get_recursive_orig_value(ui_field_id)
-      if isinstance(value, Method):
-        value = value.method_name
-      tales = field.get_recursive_tales(ui_field_id)
-      if tales:
-        tales_text = tales._text
-      else:
-        tales_text = ''
-      value_dict[ui_field_id] = value
-      tales_dict[ui_field_id] = tales_text
-  else:
-    for ui_field_id in field.form.fields.keys():
-      value = field.get_orig_value(ui_field_id)
-      if isinstance(value, Method):
-        value = value.method_name
-      tales = field.get_tales(ui_field_id)
-      if tales:
-        tales_text = tales._text
-      else:
-        tales_text = ''
-      value_dict[ui_field_id] = value
-      tales_dict[ui_field_id] = tales_text
-
-  return value_dict, tales_dict
