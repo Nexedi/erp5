eb80a5051733fae9d6ff4e9319e98eee9b302992
Fabien Morin <fabien@nexedi.com>
2009-07-02 09:46:57 +0000

    add TradeModelLineConstraint
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@27866 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/PropertySheet/TradeModelLineConstraint.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SA and Contributors. All Rights Reserved.
+#                    Fabien Morin <fabien@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+class TradeModelLineConstraint:
+  """
+   Trade Model Line Constraints
+  """
+  _constraints = (
+    { 'id'            : 'reference_existence',
+      'description'   : 'Property reference must be defined',
+      'type'          : 'PropertyExistence',
+      'reference'    : None,
+      "message_property_not_set" : 'Reference must be defined in lines',
+      "message_no_such_property" : 'Reference must be defined in lines'
+    },
+    { 'id'            : 'resource',
+      'description'   : 'Resource must be defined',
+      'type'          : 'CategoryMembershipArity',
+      'min_arity'     : '1',
+      'max_arity'     : '1',
+      'portal_type'   : ( ),
+      'base_category' : ('resource',),
+      'message_arity_not_in_range' : 'Service must be defined in lines',
+    },
+  )
