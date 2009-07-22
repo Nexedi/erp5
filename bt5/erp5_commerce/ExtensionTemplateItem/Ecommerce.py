f8a1d35102105de6d96d74f92ca60f17dc1b33c1
Vincent Desmares <vincentd@nexedi.com>
2009-07-22 15:53:15 +0000

    vincentd * Include Paypal payment into erp5_commerce
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28150 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_commerce/ExtensionTemplateItem/Ecommerce.py
+++ bt5/erp5_commerce/ExtensionTemplateItem/Ecommerce.py
@@ -1,5 +1,22 @@
 from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 
+import urllib
+import mechanize
+
 def getProductPrice(product):
-    getPrice = UnrestrictedMethod(product.getPrice)
-    return getPrice()
+  getPrice = UnrestrictedMethod(product.getPrice)
+  return getPrice()
+
+def submitPaypalNVPRequest(parameter_dict, nvp_url):
+  request = mechanize.Request(nvp_url)
+  params = urllib.urlencode(parameter_dict)
+  try:
+    response = mechanize.urlopen(request, data=params)
+  except:
+    return None
+  parameter_list = response.read().split('&')
+  response_parameter_dict = {}
+  for parameter in parameter_list:
+    tmp = parameter.split('=')
+    response_parameter_dict[tmp[0]] = tmp[1]
+  return response_parameter_dict
