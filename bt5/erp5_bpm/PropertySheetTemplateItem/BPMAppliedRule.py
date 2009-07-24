184f04e134eede7a69d0b487c1239e8f15739b61
Łukasz Nowak <luke@nexedi.com>
2009-07-24 09:57:54 +0000

    - property sheet with matching_property rule configuration
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28172 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ bt5/erp5_bpm/PropertySheetTemplateItem/BPMAppliedRule.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# Copyright (c) 2002 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+class BPMAppliedRule:
+  """
+    BPM extended Applied Rule properties
+  """
+
+  _properties = (
+        {  'id'          : 'matching_property',
+           'description' : 'List of properties used in to compare previsions '
+                           'with Simulation Movements',
+           'type'        : 'lines',
+           'default'     : [],
+           'multivalued' : 1,
+           'mode'        : 'w' },
+  )
