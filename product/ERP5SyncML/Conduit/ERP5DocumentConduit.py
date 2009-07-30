bfc2fe00af1a9520cc2345442e9ae97b0fa59b43
Danièle Vanbaelinghem <daniele@nexedi.com>
2009-07-30 12:23:44 +0000

    Create a specific conduit for the synchronisation of documents
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28222 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5SyncML/Conduit/ERP5DocumentConduit.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SA and Contributors. All Rights Reserved.
+#          Danièle Vanbaelinghem <daniele@gmail.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+##############################################################################
+
+from Products.ERP5SyncML.Conduit.ERP5Conduit import ERP5Conduit
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions
+from Products.ERP5SyncML.SyncCode import SyncCode
+
+from zLOG import LOG
+
+class ERP5DocumentConduit(ERP5Conduit):
+  """
+  ERP5DocumentConduit provides two methods who permit to have the GID
+  The Gid is composed by the title : "Reference-Version-Language"
+  this class is made for unit test
+  """
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  def getGidFromObject(self, object):
+    """
+    return the Gid generate with the reference, object, language of the object
+    """
+    return "%s-%s-%s" %\
+      (object.getReference(), object.getVersion(), object.getLanguage())
+
+#  def getGidFromXML(self, xml):
+#    """
+#    return the Gid composed of FirstName and LastName generate with a peace of
+#    xml
+#    """
+#    #to be defined
