Nexedi/erp5

commit abb36c89c98c47aee1ea41d83b1531a7e8749dfe

no need to call hasConversion() before getConversion() because it does mostly the same thing and waste of time. use try ... except KeyError instead.
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28375 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/PDFDocument.py
@@ -99,15 +99,19 @@ class PDFDocument(Image, ConversionCacheMixin):
     Implementation of conversion for PDF files
     """
     if format == 'html':
-      if not self.hasConversion(format=format):
+      try:
+        return self.getConversion(format=format)
+      except KeyError:
         data = self._convertToHTML()
         self.setConversion(data, mime='text/html', format=format)
-      return self.getConversion(format=format)
+        return (mime, aq_base(data))
     elif format in ('txt', 'text'):
-      if not self.hasConversion(format='txt'):
+      try:
+        return self.getConversion(format='txt')
+      except KeyError:
         data = self._convertToText()
         self.setConversion(data, mime='text/plain', format='txt')
-      return self.getConversion(format='txt')
+        return (mime, aq_base(data))
     else:
       return Image.convert(self, format, **kw)
 

commit 933ca5f8c11b9c78b44fcb63023e9a2127f52287

fix a bug introduced in r28375.
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28381 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/PDFDocument.py
@@ -102,16 +102,18 @@ class PDFDocument(Image, ConversionCacheMixin):
       try:
         return self.getConversion(format=format)
       except KeyError:
+        mime = 'text/html'
         data = self._convertToHTML()
-        self.setConversion(data, mime='text/html', format=format)
-        return (mime, aq_base(data))
+        self.setConversion(data, mime=mime, format=format)
+        return (mime, data)
     elif format in ('txt', 'text'):
       try:
         return self.getConversion(format='txt')
       except KeyError:
+        mime = 'text/plain'
         data = self._convertToText()
-        self.setConversion(data, mime='text/plain', format='txt')
-        return (mime, aq_base(data))
+        self.setConversion(data, mime=mime, format='txt')
+        return (mime, data)
     else:
       return Image.convert(self, format, **kw)
 