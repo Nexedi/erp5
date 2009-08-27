1d1947e4808767e97fd745624728a50a0f4707b3
Julien Muchembled <jm@nexedi.com>
2009-08-27 11:41:21 +0000

    New interfaces for roles and actions
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28655 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/interfaces/__init__.py
+++ product/ERP5Type/interfaces/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from action_provider import IAction, IActionProvider
 from cache_plugin import ICachePlugin
 from category_access_provider import ICategoryAccessProvider
 from value_access_provider import IValueAccessProvider
 from constraint import IConstraint
+from role_provider import ILocalRoleAssignor, ILocalRoleGenerator
