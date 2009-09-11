fa0ac0a3d2cf1f65948da9e3b8eed51e4d74259d
Vincent Pelletier <vincent@nexedi.com>
2009-09-11 14:06:33 +0000

    Make indentation style consistent.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@28962 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/CMFActivity/ActiveProcess.py
+++ product/CMFActivity/ActiveProcess.py
@@ -41,120 +41,120 @@ from zLOG import LOG
 manage_addActiveProcessForm=DTMLFile('dtml/ActiveProcess_add', globals())
 
 def addActiveProcess(self, id, title='', REQUEST=None, activate_kw=None, **kw):
-    """Add a new Active Process.
-    """
-    o = ActiveProcess(id)
-    if activate_kw is not None:
-      o.__of__(self).setDefaultActivateParameters(**activate_kw)
-    o.uid = self.portal_catalog.newUid()
-    self._setObject(id, o)
-    o = self._getOb(id)
-    if kw: o._edit(force_update=1, **kw)
-    if REQUEST is not None:
-        REQUEST['RESPONSE'].redirect( 'manage_main' )
-    return o
+  """Add a new Active Process.
+  """
+  o = ActiveProcess(id)
+  if activate_kw is not None:
+    o.__of__(self).setDefaultActivateParameters(**activate_kw)
+  o.uid = self.portal_catalog.newUid()
+  self._setObject(id, o)
+  o = self._getOb(id)
+  if kw: o._edit(force_update=1, **kw)
+  if REQUEST is not None:
+    REQUEST['RESPONSE'].redirect( 'manage_main' )
+  return o
 
 class ActiveProcess(Base):
+  """
+      ActiveProcess is used to centralise interaction between multiple ActiveObject
+      RENAME: ActiveResult
+  """
+
+  meta_type='CMF Active Process'
+  portal_type='Active Process'
+  isPortalContent = 0 
+  isRADContent = 1
+  icon = None
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareProtected(CMFCorePermissions.ManagePortal,
+                            'manage_editProperties',
+                            'manage_changeProperties',
+                            'manage_propertiesForm',
+                              )
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.SimpleItem
+                    , PropertySheet.Folder
+                    , PropertySheet.ActiveProcess )
+
+  # Declarative constructors
+  constructors =   (manage_addActiveProcessForm, addActiveProcess)
+
+  def _generateRandomId(self):
     """
-        ActiveProcess is used to centralise interaction between multiple ActiveObject
-        RENAME: ActiveResult
+    Generate a random int depending on the size of the result list
     """
+    random_id = randint(1, 10000 * (self.result_len.value + 1)) 
+    return random_id
 
-    meta_type='CMF Active Process'
-    portal_type='Active Process'
-    isPortalContent = 0 
-    isRADContent = 1
-    icon = None
-
-    # Declarative security
-    security = ClassSecurityInfo()
-    security.declareProtected(CMFCorePermissions.ManagePortal,
-                              'manage_editProperties',
-                              'manage_changeProperties',
-                              'manage_propertiesForm',
-                                )
-
-    # Declarative properties
-    property_sheets = ( PropertySheet.Base
-                      , PropertySheet.SimpleItem
-                      , PropertySheet.Folder
-                      , PropertySheet.ActiveProcess )
-
-    # Declarative constructors
-    constructors =   (manage_addActiveProcessForm, addActiveProcess)
-
-    def _generateRandomId(self):
-      """
-      Generate a random int depending on the size of the result list
-      """
-      random_id = randint(1, 10000 * (self.result_len.value + 1)) 
-      return random_id
-
-    security.declareProtected(CMFCorePermissions.ManagePortal, 'postResult')
-    def postResult(self, result):
-      if getattr(self, 'result_list', None) is None:
-        self.result_list = IOBTree()
-        self.result_len = Length()
+  security.declareProtected(CMFCorePermissions.ManagePortal, 'postResult')
+  def postResult(self, result):
+    if getattr(self, 'result_list', None) is None:
+      self.result_list = IOBTree()
+      self.result_len = Length()
+    random_id = self._generateRandomId()
+    _marker = []
+    # use a random id in order to store result in a way with
+    # fewer conflict errors
+    while self.result_list.get(random_id, _marker) is not _marker:
       random_id = self._generateRandomId()
-      _marker = []
-      # use a random id in order to store result in a way with
-      # fewer conflict errors
-      while self.result_list.get(random_id, _marker) is not _marker:
-        random_id = self._generateRandomId()
-      self.result_list[random_id] = result
-      self.result_len.change(1)
+    self.result_list[random_id] = result
+    self.result_len.change(1)
 
-    security.declareProtected(CMFCorePermissions.ManagePortal, 'getResultList')
-    def getResultList(self, **kw):
-      """
-        Returns the list of results
-      """
-      if getattr(self, 'result_list', None) is None:
-        self.result_list = IOBTree()
-        self.result_len = Length()
-      # Improve this to include sort order XXX
-      return self.result_list.values()
+  security.declareProtected(CMFCorePermissions.ManagePortal, 'getResultList')
+  def getResultList(self, **kw):
+    """
+      Returns the list of results
+    """
+    if getattr(self, 'result_list', None) is None:
+      self.result_list = IOBTree()
+      self.result_len = Length()
+    # Improve this to include sort order XXX
+    return self.result_list.values()
 
-    security.declareProtected(CMFCorePermissions.ManagePortal, 'activateResult')
-    def activateResult(self, result):
-      if result not in (None, 0, '', (), []):
-        self.postResult(result) # Until we get SQLQueue
+  security.declareProtected(CMFCorePermissions.ManagePortal, 'activateResult')
+  def activateResult(self, result):
+    if result not in (None, 0, '', (), []):
+      self.postResult(result) # Until we get SQLQueue
 
-    security.declareProtected( CMFCorePermissions.View, 'hasActivity' )
-    def hasActivity(self, **kw):
-      """
-        Tells if there is still some activities not finished attached to this process
-      """
-      activity_tool = getattr(self, 'portal_activities', None)
-      if activity_tool is None: return 0 # Do nothing if no portal_activities
-      return activity_tool.hasActivity(None, active_process_uid = self.getUid(), **kw)
+  security.declareProtected( CMFCorePermissions.View, 'hasActivity' )
+  def hasActivity(self, **kw):
+    """
+      Tells if there is still some activities not finished attached to this process
+    """
+    activity_tool = getattr(self, 'portal_activities', None)
+    if activity_tool is None: return 0 # Do nothing if no portal_activities
+    return activity_tool.hasActivity(None, active_process_uid = self.getUid(), **kw)
 
-    security.declareProtected( CMFCorePermissions.View, 'hasErrorActivity' )
-    def hasErrorActivity(self, **kw):
-      """
-        Tells if some attached activities are in a error 
-      """
-      return self.hasActivity(processing_node = INVOKE_ERROR_STATE)
+  security.declareProtected( CMFCorePermissions.View, 'hasErrorActivity' )
+  def hasErrorActivity(self, **kw):
+    """
+      Tells if some attached activities are in a error 
+    """
+    return self.hasActivity(processing_node = INVOKE_ERROR_STATE)
 
-    security.declareProtected( CMFCorePermissions.View, 'hasInvalidActivity' )
-    def hasInvalidActivity(self, **kw):
-      """
-        Tells if an object if active
-      """
-      return self.hasActivity(processing_node = VALIDATE_ERROR_STATE)
+  security.declareProtected( CMFCorePermissions.View, 'hasInvalidActivity' )
+  def hasInvalidActivity(self, **kw):
+    """
+      Tells if an object if active
+    """
+    return self.hasActivity(processing_node = VALIDATE_ERROR_STATE)
 
-    def getCreationDate(self):
-      """
-        Define a Creation Date for an active process
-        thanks to the start date
-      """
-      return self.getStartDate()
+  def getCreationDate(self):
+    """
+      Define a Creation Date for an active process
+      thanks to the start date
+    """
+    return self.getStartDate()
 
-    def flush(self):
-      # flush  activities related to this process
-      activity_tool = getattr(self, 'portal_activities', None)
-      if activity_tool is None: return # Do nothing if no portal_activities
-      return activity_tool.flush(None, active_process = self, invoke = 0) # FLush
+  def flush(self):
+    # flush  activities related to this process
+    activity_tool = getattr(self, 'portal_activities', None)
+    if activity_tool is None: return # Do nothing if no portal_activities
+    return activity_tool.flush(None, active_process = self, invoke = 0) # FLush
 
 
 InitializeClass( ActiveProcess )
