d230d761471346770dc672caf03c3a4edcb6a786
Fabien Morin <fabien@nexedi.com>
2009-09-28 14:29:23 +0000

    - use compostion in the same way than on Apparel Fabrics : with category selection and matrixbox to define values, it's much better than a text field. - add composition to categories of ApparelModel property sheet to be able to store composition ont Apparel Model objects - add Mapped Value to allow and hidden content types to be able to store composition values
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@29230 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_apparel/PropertySheetTemplateItem/ApparelModel.py
+++ bt5/erp5_apparel/PropertySheetTemplateItem/ApparelModel.py
@@ -67,10 +67,6 @@ class ApparelModel:
       'acquisition_accessor_id'   : 'getTitle',
       'acquisition_depends'       : None,
       'mode'        : 'w' },
-    { 'id'          : 'composition', 
-      'description' : 'Apparel Model composition',
-      'type'        : 'text',
-      'mode'        : 'w' },
     { 'id'          : 'margin_ratio', ### XXX
       'description' : 'Margin coefficient',
       'type'        : 'float',
@@ -143,7 +139,7 @@ class ApparelModel:
         'mode'        : 'w' },
   )
 
-  _categories = ( 'transformation_state', 'pricing', 'origin', 'brand', 'tariff_nomenclature' )
+  _categories = ( 'composition', 'transformation_state', 'pricing', 'origin', 'brand', 'tariff_nomenclature' )
   #_categories = ( 'transformation_state', 'apparel_pricing', 'apparel_creation_type', 'brand', 'tariff_nomenclature' )
                   #     XXXXXXX              XXXX                XXXX   apparel_model_creation_type                        (As in Brussels Tariff Nomenclature)               
 
