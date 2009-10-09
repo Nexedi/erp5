083c1a8194ac17b90e3c1b179fd1c77d9c8183fc
Łukasz Nowak <luke@nexedi.com>
2009-10-09 09:51:05 +0000

    - check that deleted Person objects are not providing valid logins anymore
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@29510 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Security/tests/testERP5Security.py
+++ product/ERP5Security/tests/testERP5Security.py
@@ -327,6 +327,15 @@ class TestUserManagement(ERP5TypeTestCase):
                          assignment_stop_date=date-1)
     self._assertUserDoesNotExists('the_user', 'secret')
 
+  def test_DeletedPersonIsNotUser(self):
+    p = self._makePerson(reference='the_user', password='secret')
+    self._assertUserExists('the_user', 'secret')
+
+    p.delete()
+    transaction.commit()
+
+    self._assertUserDoesNotExists('the_user', 'secret')
+
 class TestLocalRoleManagement(ERP5TypeTestCase):
   """Tests Local Role Management with ERP5Security.
 
