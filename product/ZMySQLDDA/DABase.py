39d09001b7ac09b1f15269eb5bbafb5b590a2e39
Jérome Perrin <jerome@nexedi.com>
2009-11-10 13:42:09 +0000

    ImageFile is not needed
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@30472 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ZMySQLDDA/DABase.py
+++ product/ZMySQLDDA/DABase.py
@@ -89,7 +89,6 @@ __version__='$Revision: 1.5 $'[11:-2]
 
 import Shared.DC.ZRDB.Connection, sys
 from App.special_dtml import HTMLFile
-from ImageFile import ImageFile
 from ExtensionClass import Base
 import Acquisition
 
