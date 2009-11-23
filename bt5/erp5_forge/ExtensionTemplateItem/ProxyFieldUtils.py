b865ec5551472ccca709c5c548e20cf5253ea3e4
Sebastien Robin <seb@nexedi.com>
2009-11-23 08:36:30 +0000

    2009-11-23 Seb * Add missing extension
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@30803 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ bt5/erp5_forge/ExtensionTemplateItem/ProxyFieldUtils.py
@@ -0,0 +1,38 @@
+### External Method
+
+from Products.Formulator.MethodField import Method
+
+#
+# This function is useful to check if two fields is equal or not.
+#
+def get_field_data(field):
+  value_dict = {}
+  tales_dict = {}
+
+  if field.meta_type=='ProxyField':
+    template_field = field.getRecursiveTemplateField()
+    for ui_field_id in template_field.form.fields.keys():
+      value = field.get_recursive_orig_value(ui_field_id)
+      if isinstance(value, Method):
+        value = value.method_name
+      tales = field.get_recursive_tales(ui_field_id)
+      if tales:
+        tales_text = tales._text
+      else:
+        tales_text = ''
+      value_dict[ui_field_id] = value
+      tales_dict[ui_field_id] = tales_text
+  else:
+    for ui_field_id in field.form.fields.keys():
+      value = field.get_orig_value(ui_field_id)
+      if isinstance(value, Method):
+        value = value.method_name
+      tales = field.get_tales(ui_field_id)
+      if tales:
+        tales_text = tales._text
+      else:
+        tales_text = ''
+      value_dict[ui_field_id] = value
+      tales_dict[ui_field_id] = tales_text
+
+  return value_dict, tales_dict
