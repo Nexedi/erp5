279a8d455aed6a5053495a14ce0396cc9396cc36
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2009-12-08 16:17:34 +0000

    add NewOrderRule, that is experimental for new simulation implementation, and will replace OrderRule.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@31165 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/NewOrderRule.py
@@ -0,0 +1,135 @@
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SARL and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+##############################################################################
+"""
+XXX This file is experimental for new simulation implementation, and
+will replace OrderRule.
+"""
+
+import zope.interface
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions, PropertySheet, interfaces
+from Products.ERP5.Document.Predicate import Predicate
+from Products.ERP5.mixin.rule import RuleMixin
+
+class NewOrderRule(RuleMixin, Predicate):
+  """
+  Order Rule object make sure an Order in the simulation
+  is consistent with the real order
+
+  WARNING: what to do with movement split ?
+  """
+  # CMF Type Definition
+  meta_type = 'ERP5 New Order Rule'
+  portal_type = 'New Order Rule'
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  # Declarative interfaces
+  zope.interface.implements(interfaces.IRule,
+                            interfaces.IDivergenceController,
+                            interfaces.IMovementCollectionUpdater,)
+
+  # Default Properties
+  property_sheets = (
+    PropertySheet.Base,
+    PropertySheet.XMLObject,
+    PropertySheet.CategoryCore,
+    PropertySheet.DublinCore,
+    PropertySheet.Task,
+    PropertySheet.Predicate,
+    PropertySheet.Reference,
+    PropertySheet.Version,
+    PropertySheet.Rule
+    )
+
+  # Portal Type of created children
+  movement_type = 'Simulation Movement'
+
+  # XXX this method is missing in interface.
+  def isOrderable(self, movement):
+    return 1
+
+  # XXX this method is missing in interface.
+  def isDeliverable(self, movement):
+    if movement.getSimulationState() in movement.getPortalDraftOrderStateList():
+      return 0
+    return 1
+
+  # XXX this method should be defined in the mixin or the base class.
+  security.declareProtected(Permissions.View, 'getDivergenceList')
+  def getDivergenceList(self, movement):
+    """
+    Returns a list of divergences of the movements provided
+    in delivery_or_movement.
+
+    movement -- a movement, a delivery, a simulation movement,
+                or a list thereof
+    """
+    result_list = []
+    for divergence_tester in self._getDivergenceTesterList():
+      result = divergence_tester.explain(movement)
+      if isinstance(result, (list, tuple)): # for compatibility
+        result_list.extend(result)
+      elif result is not None:
+        result_list.append(result)
+    return result_list
+
+  def _getMovementGenerator(self):
+    """
+    Return the movement generator to use in the expand process
+    """
+    return OrderRuleMovementGenerator()
+
+  def _getMovementGeneratorContext(self, context):
+    """
+    Return the movement generator context to use for expand
+    """
+    return context
+
+  def _getMovementGeneratorMovementList(self):
+    """
+    Return the movement lists to provide to the movement generator
+    """
+    return []
+
+  def _isProfitAndLossMovement(self, movement):
+    # For a kind of trade rule, a profit and loss movement lacks source
+    # or destination.
+    return (movement.getSource() is None or movement.getDestination() is None)
+
+class OrderRuleMovementGenerator(object):
+  def getAggregatedMovementList(self, context, movement_list=None,
+                                rounding=False):
+    """Input movement list comes from order"""
+    order = context.getDefaultCausalityValue()
+    if order is not None:
+      return [x.asContext(order=x.getRelativeUrl()) for x in order.getMovementList(
+        portal_type=order.getPortalOrderMovementTypeList())]
+    else:
+      return []
