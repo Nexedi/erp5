e20d949a9b974f650ee34276c58ac427d245bc06
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2009-12-16 15:35:46 +0000

    fix a typo of the method name.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@31340 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Tool/SolverTool.py
+++ product/ERP5/Tool/SolverTool.py
@@ -148,7 +148,7 @@ class SolverTool(BaseTool):
     # Create an empty solver process
     new_solver = self.newContent(portal_type='Solver Process')
     # And build decisions
-    new_solver.updateSolverDecision(delivery_or_movement=delivery_or_movement)
+    new_solver.buildSolverDecisionList(delivery_or_movement=delivery_or_movement)
     return new_solver
 
   def getSolverProcessValueList(self, delivery_or_movement=None, validation_state=None):
