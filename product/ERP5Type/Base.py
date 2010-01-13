7fb5f2710966159dc84efdbf9adec3f66c38e460
Leonardo Rochael Almeida <leonardo@nexedi.com>
2010-01-13 16:19:33 +0000

    Make sure public methods of TempBase are truly public on Zope 2.12
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@31732 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/Base.py
+++ product/ERP5Type/Base.py
@@ -3813,6 +3813,12 @@ class TempBase(Base):
 
   security.declarePublic('edit')
 
+# Persistence.Persistent is one of the superclasses of TempBase, and on Zope2.8
+# it's __class_init__ method is InitializeClass. This is not the case on
+# Zope2.12 which requires us to call InitializeClass manually, otherwise
+# allow_class(TempBase) in ERP5Type/Document/__init__.py will trample our
+# ClassSecurityInfo with one that doesn't declare our public methods
+InitializeClass(TempBase)
 
 def newTempDocumentationHelper(folder, id, REQUEST=None, **kw):
   o = TempDocumentationHelper(id)
