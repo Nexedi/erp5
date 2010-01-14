16546633a7a670a85d2078b33d09f72ee6a51b21
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2010-01-14 12:57:24 +0000

    add a new target solver class.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@31747 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5/Document/UnifySolver.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2009 Nexedi SA and Contributors. All Rights Reserved.
+#                    Jean-Paul Smets-Solanes <jp@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import zope.interface
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions, PropertySheet, interfaces
+from Products.ERP5Type.XMLObject import XMLObject
+from Products.ERP5.mixin.solver import SolverMixin
+from Products.ERP5.mixin.configurable import ConfigurableMixin
+
+class UnifySolver(SolverMixin, ConfigurableMixin, XMLObject):
+  """
+  """
+  meta_type = 'ERP5 Unify Solver'
+  portal_type = 'Unify Solver'
+  add_permission = Permissions.AddPortalContent
+  isIndexable = 0 # We do not want to fill the catalog with objects on which we need no reporting
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  # Default Properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.XMLObject
+                    , PropertySheet.CategoryCore
+                    , PropertySheet.DublinCore
+                    , PropertySheet.TargetSolver
+                    )
+
+  # Declarative interfaces
+  zope.interface.implements(interfaces.ISolver,)
+
+  # ISolver Implementation
+  def solve(self):
+    """
+    Adopt new property to simulation movements, with keeping the
+    original one recorded.
+    """
+    solved_property = self._getPortalTypeValue().getTestedProperty()
+    for movement in self.getDeliveryValueList():
+      import pdb; pdb.set_trace()
+      configuration_dict = self.getConfigurationPropertyDict()
+      new_value = configuration_dict.get('value')
+      movement.setProperty(solved_property, new_value)
+      simulation_movement_list = movement.getDeliveryRelatedValueList()
+      # if movement here is a delivery, we need to find simulation
+      # movements by its movements.
+      if len(simulation_movement_list) == 0:
+        simulation_movement_list = sum(
+          [x.getDeliveryRelatedValueList() \
+           for x in self.getDeliveryValue().getMovementList()], [])
+      for simulation_movement in simulation_movement_list:
+        if not simulation_movement.isPropertyRecorded(solved_property):
+          simulation_movement.recordProperty(solved_property)
+        simulation_movement.setProperty(solved_property, new_value)
+        simulation_movement.expand()
+    # Finish solving
+    self.succeed()
