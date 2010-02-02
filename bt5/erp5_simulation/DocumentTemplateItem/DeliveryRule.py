4182a9e17eba8a82b70644f5fb02f3dbfd19b17b
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2010-02-02 14:33:26 +0000

    use rule's updating testers for creating first level simulation movements.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@32185 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_simulation/DocumentTemplateItem/DeliveryRule.py
+++ bt5/erp5_simulation/DocumentTemplateItem/DeliveryRule.py
@@ -101,9 +101,11 @@ class DeliveryRuleMovementGenerator(MovementGeneratorMixin):
     Input movement list comes from delivery
     """
     ret = []
+    rule = context.getSpecialiseValue()
     for input_movement, business_path in self \
             ._getInputMovementAndPathTupleList(context):
-      kw = self._getPropertyAndCategoryList(input_movement, business_path)
+      kw = self._getPropertyAndCategoryList(input_movement, business_path,
+                                            rule)
       simulation_movement = context.newContent(
         portal_type=RuleMixin.movement_type,
         temp_object=True,
