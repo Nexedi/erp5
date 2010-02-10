e950352ff080b9c20a68af42ae02f9d1395c3b07
Nicolas Dumazet <nicolas.dumazet@nexedi.com>
2010-02-10 10:32:04 +0000

    Make sure that a /var directory is created after installation, or mysqld wont start.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@32406 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- buildout/mysql-tritonn-5.0/src/mysql_hooks.py
+++ buildout/mysql-tritonn-5.0/src/mysql_hooks.py
@@ -1,3 +1,4 @@
+import errno
 import os
 
 # the comand below assumes there is only one sub-directory under the
@@ -12,5 +13,12 @@ autoconf
 touch sql/sql_yacc.yy
 """.strip()
 
-def hook(options, buildout):
+def pre_configure_hook(options, buildout):
     os.system(CMDS % options['compile-directory'])
+
+def post_make_hook(options, buildout):
+    try:
+    	os.mkdir("%s/var" % options['location'])
+    except OSError, e:
+	if e.errno != errno.EEXIST:
+	    raise
