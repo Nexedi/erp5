3aa8b37cebbbe6b5003f5ae351d3079f540b3b47
Łukasz Nowak <luke@nexedi.com>
2010-02-12 15:25:41 +0000

    - initialise development of standalone Zope instance recipe
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@32505 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ buildout/recipes/erp5.recipe.standaloneinstance/src/erp5/recipe/standaloneinstance/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+import zc.buildout
+import erp5.recipe.zope2instance
+
+class Recipe(erp5.recipe.zope2instance.Recipe):
+  def __init__(self, buildout, name, options):
+    self.egg = zc.recipe.egg.Egg(buildout, options['recipe'], options)
+    self.buildout, self.options, self.name = buildout, options, name
+    self.zope2_location = options.get('zope2-location', '')
+
+    standalone_location = options.get('location')
+    if not standalone_location:
+      raise zc.buildout.UserError('Location have to be specified')
+    options['bin-directory'] = os.path.join(standalone_location, 'bin')
+    options['scripts'] = '' # suppress script generation.
+
+    # Relative path support for the generated scripts
+    relative_paths = options.get(
+        'relative-paths',
+        buildout['buildout'].get('relative-paths', 'false')
+        )
+    if relative_paths == 'true':
+        options['buildout-directory'] = buildout['buildout']['directory']
+        self._relative_paths = options['buildout-directory']
+    else:
+        self._relative_paths = ''
+        assert relative_paths == 'false'
+
+  def install(self):
+    erp5.recipe.zope2instance.Recipe.install(self)
+    # we return nothing, as this is totally standalone installation
+    return []
+
+  def build_zope_conf(self):
+    # preparation for further fixing (chroot everything inside instance, etc)
+    erp5.recipe.zope2instance.Recipe.build_zope_conf(self)
