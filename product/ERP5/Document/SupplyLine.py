Nexedi/erp5

commit 3310292d4368f2aeb8a83eab3e274be906a7b942

Supply Line and Supply Cell are not movements, they are paths. Also clean up a few imports in the process
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@31762 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/SupplyLine.py
@@ -28,16 +28,14 @@
 ##############################################################################
 
 from AccessControl import ClassSecurityInfo
-from Products.ERP5Type import Permissions, PropertySheet, Constraint, interfaces
+from Products.ERP5Type import Permissions, PropertySheet
 from Products.ERP5Type.Accessor.Constant import PropertyGetter as ConstantGetter
 from Products.ERP5Type.XMLMatrix import XMLMatrix
-from Products.ERP5.Document.DeliveryLine import DeliveryLine
 from Products.ERP5.Document.Path import Path
 from Products.ERP5Type.Utils import convertToUpperCase
 
-from zLOG import LOG
 
-class SupplyLine(DeliveryLine, Path):
+class SupplyLine(Path, XMLMatrix):
     """A Supply Line is a path to define price 
     """
 
@@ -73,7 +71,6 @@ class SupplyLine(DeliveryLine, Path):
     security.declareProtected(Permissions.AccessContentsInformation,
                               'getPrice')
     def getPrice(self):
-      # If price not defined, look up price
       if getattr(self, 'price', None) is None:
         self.price = 0.0
       # Return the price

product/ERP5/Document/SupplyCell.py
@@ -26,21 +26,13 @@
 #
 ##############################################################################
 
-from Products.ERP5Type.Globals import InitializeClass, PersistentMapping
 from AccessControl import ClassSecurityInfo
 
-from Products.ERP5Type import Permissions, PropertySheet, Constraint, interfaces
-
-from Products.ERP5.Document.DeliveryCell import DeliveryCell
+from Products.ERP5Type import Permissions, PropertySheet
 from Products.ERP5.Document.Path import Path
-from Products.ERP5.Document.Predicate import Predicate
 
-class SupplyCell(Predicate, DeliveryCell, Path):
-    """
-      A DeliveryCell allows to define specific quantities
-      for each variation of a resource in a delivery line.
-
-      (PATH)
+class SupplyCell(Path):
+    """A Supply Cell is used for different variations in a supply line.
     """
 
     meta_type = 'ERP5 Supply Cell'
@@ -67,3 +59,9 @@ class SupplyCell(Predicate, DeliveryCell, Path):
                       , PropertySheet.Reference
                       )
 
+    security.declareProtected( Permissions.AccessContentsInformation,
+                               'hasCellContent' )
+    def hasCellContent(self, base_id='movement'):
+      """A cell cannot have cell content itself.
+      """
+      return 0

commit 02e80d87000421706432e62f79fb764d134fe82a

Fix problem introduced in r31762 : Supply Line need Reference property sheet and it has been removed by removing DeliveryLine from SupplyLine inheritance. Tests has been added in r32616 and r32618
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@32668 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/SupplyLine.py
@@ -63,6 +63,7 @@ class SupplyLine(Path, XMLMatrix):
                       , PropertySheet.FlowCapacity
                       , PropertySheet.Predicate
                       , PropertySheet.Comment
+                      , PropertySheet.Reference
                       )
 
     #############################################