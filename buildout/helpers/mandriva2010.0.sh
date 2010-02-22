e97e5fdbf7ec59c54bfd514b0837a3bf03a258da
Nicolas Dumazet <nicolas.dumazet@nexedi.com>
2010-02-22 11:30:53 +0000

    extend a bit the helper with options so we can reuse it in RPM generation
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@32934 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- buildout/helpers/mandriva2010.0.sh
+++ buildout/helpers/mandriva2010.0.sh
@@ -1,27 +1,48 @@
 #!/bin/sh
 
-PACKAGE_LIST="""
-  bison
-  cpio
-  flex
-  gcc
-  gcc-c++
-  libbzip2-devel
-  libgdbm-devel
-  libglib2.0-devel
-  libjpeg-devel
-  libncurses-devel
-  libopenssl-devel
-  libtermcap-devel
-  libxml2-devel
-  libxslt-devel
-  make
-  patch
-  rpm
-  subversion
-  subversion-devel
-  subversion-tools
-  x11-server-xvfb
-  zlib1-devel
+USAGE="""\
+Helper for buildout-base installation of ERP5 on Mandriva 2010.
+ Usage:
+   mandriva2010.sh [-h|-l]
+
+   With no options, attempts to install the dependencies required
+   to run the buildout.
+
+ Options:
+   -h  shows this message
+   -l  lists the required dependencies\
 """
-urpmi $PACKAGE_LIST
+PACKAGE_LIST="""\
+bison
+cpio
+flex
+gcc
+gcc-c++
+libbzip2-devel
+libgdbm-devel
+libglib2.0-devel
+libjpeg-devel
+libncurses-devel
+libopenssl-devel
+libtermcap-devel
+libxml2-devel
+libxslt-devel
+make
+patch
+rpm
+subversion
+subversion-devel
+subversion-tools
+x11-server-xvfb
+zlib1-devel\
+"""
+if [ x"$1" == x ]; then
+  urpmi $PACKAGE_LIST
+elif [ "$1" = "-l" ]; then
+  echo "$PACKAGE_LIST"
+elif [ "$1" = "-h" ]; then
+  echo "$USAGE"
+else
+  echo "Unknown argument."
+  echo "$USAGE"
+fi
