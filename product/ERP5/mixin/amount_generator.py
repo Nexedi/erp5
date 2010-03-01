4407076fbaf616e2546d468a3b0665e69c7c647c
Jean-Paul Smets <jp@nexedi.com>
2010-03-01 15:01:10 +0000

    First working version for transformations (using new transformation code which is not yet committed).
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@33187 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/mixin/amount_generator.py
+++ product/ERP5/mixin/amount_generator.py
@@ -27,6 +27,7 @@
 ##############################################################################
 
 import zope.interface
+from zLOG import LOG
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type import Permissions, interfaces
 from Products.ERP5.Document.Amount import Amount
@@ -34,8 +35,15 @@ from Products.ERP5.Document.Amount import Amount
 class AmountGeneratorMixin:
   """
   This class provides a generic implementation of IAmountGenerator.
+  It is used by Transformation, Trade Model, Paysheet, etc. It is 
+  designed to support about any transformation process based
+  on IMappedValue interface. The key idea is that the Amount Generator
+  Lines and Cell provide either directly or through acquisition the 
+  methods 'getMappedValuePropertyList' and 'getMappedValueBaseCategoryList'
+  to gather the properties and categories to copy from the model
+  to the generated amounts.
 
-  NOTE: this is an early prototype of implementation
+  NOTE: this is an first prototype of implementation
   """
 
   # Declarative security
@@ -47,27 +55,48 @@ class AmountGeneratorMixin:
 
   def _getGlobalPropertyDict(self, context, amount_list=None, rounding=False):
     """
-    This method can be overridden to define global
-    properties involved in trade model line calculation
+    This method must be overridden to define global
+    properties involved in trade model line or transformation calculation
+
+    TODO:
+      default implementation could use type based method 
     """
     raise NotImplementedError
+    # Example of return value
     return {
-      'delivery': 1,
-      'employee': 100,
+      'delivery': 1,     # Sets the base_amount 'delivery' to 1
+                         # so that it is possible to create models based
+                         # on the number of deliveries (instead of quantity)
+      'employee': 100,   # Sets the base_amount 'employee' to 100
+                         # so that it is possible to create models based
+                         # on the number of employee (instead of quantity)
     }
 
   def _getAmountPropertyDict(self, amount, amount_list=None, rounding=False):
     """
-    This method can be overridden to define local
-    properties involved in trade model line calculation
+    This method must be overridden to define per amount local
+    properties involved in trade model line or transformation calculation
+
+    TODO:
+      default implementation could use type based method 
     """
     raise NotImplementedError
+    # Example of return value
     return dict(
         price=amount.getPrice(),
+          # Sets the base_amount 'price' to the price
+          # This base_amount often uses another name though
         quantity=amount.getQuantity(),
+          # Sets the base_amount 'quantity' to the quantity
+          # This base_amount often uses another name though
         unit=(amount.getQuantityUnit() == 'unit') * amount.getQuantity(),
+          # Sets the base_amount 'unit' to the number of units
+          # so that it is possible to create models based
+          # on the number of units
         ton=(amount.getQuantityUnit() == 'ton') * amount.getQuantity(),
-        # more base applications could be set here
+          # Sets the base_amount 'ton' to the weight in tons
+          # so that it is possible to create models based
+          # on the weight in tons
       )
 
   security.declareProtected(Permissions.AccessContentsInformation, 'getGeneratedAmountList')
@@ -82,6 +111,10 @@ class AmountGeneratorMixin:
     - is rounding really well supported (ie. before and after aggregation)
       very likely not - proxying before or after must be decided
     """
+    # It is the only place we can import this
+    from Products.ERP5Type.Document import newTempAmount
+    portal = self.getPortalObject()
+
     # Initialize base_amount global properties (which can be modified
     # during the calculation process)
     base_amount = self._getGlobalPropertyDict(context, amount_list=amount_list, rounding=rounding)
@@ -104,13 +137,13 @@ class AmountGeneratorMixin:
 
     # Each amount in amount_list creates a new amount to take into account
     # We thus need to start with a loop on amount_list
-    for amount in amount_list:
+    for delivery_amount in amount_list:
       # Initialize base_amount with per amount properties
-      amount_propert_dict = self._getAmountPropertyDict(amount, amount_list=amount_list, rounding=rounding)
+      amount_propert_dict = self._getAmountPropertyDict(delivery_amount, amount_list=amount_list, rounding=rounding)
       base_amount.update(amount_propert_dict)
 
       # Initialize base_amount with total_price for each amount applications
-      for application in amount.getBaseApplicationList(): # Acquired from Resource
+      for application in delivery_amount.getBaseApplicationList(): # Acquired from Resource
         base_amount[application] = amount.getTotalPrice()
     
       # Browse recursively the trade model and accumulate
@@ -119,7 +152,7 @@ class AmountGeneratorMixin:
         amount_generator_line_list = amount_generator_line.contentValues(portal_type=self.getPortalAmountGeneratorLineTypeList())
         # Recursively feed base_amount
         if len(amount_generator_line_list):
-          amount_generator_line_list = amount_generator_line_list.sort(key=lambda x: x.getIntIndex())
+          amount_generator_line_list.sort(key=lambda x: x.getIntIndex())
           for amount_generator_line in amount_generator_line_list:
             accumulateAmountList(amount_generator_line)
           return 
@@ -133,21 +166,27 @@ class AmountGeneratorMixin:
         resource_amount_aggregate = {} # aggregates final line information
         value_amount_aggregate = {} # aggregates intermediate line information
         for amount_generator_cell in amount_generator_cell_list:
-          if amount_generator_cell.test(amount): # XXX-JPS getTargetLevel not supported
+          if amount_generator_cell.test(delivery_amount): # XXX-JPS getTargetLevel not supported
             # Case 1: the cell defines a final amount of resource 
-            if amount_generator_cell.getResource():
+            if amount_generator_cell.getResource() and\
+               getattr(amount_generator_cell, 'getBaseApplication', None) is not None:
               # We must aggregate per resource, variation
               key = (amount_generator_cell.getResource(), amount_generator_cell.getVariationText()) # Variation UID, Hash ?
               resource_amount_aggregate.setdefault(key, {})
               # Then collect the mapped properties (resource, quantity, net_converted_quantity, base_contribution_list, base_application, etc.)
               for property_key in amount_generator_cell.getMappedValuePropertyList():
-                # Handling of property lists ? XXX?
+                # XXX-JPS Make sure handling of list properties can be handled
                 resource_amount_aggregate[key][property_key] = amount_generator_cell.getProperty(property_key)
               resource_amount_aggregate[key]['category_list'] = amount_generator_cell.getCategoryMembershipList(
                  amount_generator_cell.getMappedValueBaseCategoryList())
+              resource_amount_aggregate[key]['resource'] = amount_generator_cell.getResource()
+              # For final amounts, base_application and id MUST be defined
+              resource_amount_aggregate[key]['base_application'] = amount_generator_cell.getBaseApplication() # Required
+              resource_amount_aggregate[key]['id'] = amount_generator_cell.getRelativeUrl().replace('/', '_')
             # Case 2: the cell defines a temporary calculation line
-            else:
+            elif getattr(amount_generator_cell, 'getBaseContributionList', None) is not None:
               # We must aggregate per base_application
+              # Therefore, base_application MUST be defined
               key = amount_generator_cell.getBaseApplication()
               value_amount_aggregate.setdefault(key, {})
               # Then collect the mapped properties
@@ -155,6 +194,8 @@ class AmountGeneratorMixin:
                 value_amount_aggregate[key][property_key] = amount_generator_cell.getProperty(property_key)
               value_amount_aggregate[key]['category_list'] = amount_generator_cell.getCategoryMembershipList(
                  amount_generator_cell.getMappedValueBaseCategoryList())
+              # For intermediate calculations, base_contribution_list MUST be defined
+              value_amount_aggregate[key]['base_contribution_list'] = amount_generator_cell.getBaseContributionList() # Required
         if resource_amount_aggregate:
           for key, property_dict in resource_amount_aggregate.items():
             resource, variation_text = key
@@ -164,6 +205,7 @@ class AmountGeneratorMixin:
             else:
               category_list = None
             base_application = property_dict['base_application']
+            del property_dict['base_application']
             # property_dict should include
             #   resource - VAT service or a Component in MRP
             #   quantity - quantity in component in MRP, (what else XXX)
@@ -177,12 +219,16 @@ class AmountGeneratorMixin:
             # values converted to the default management unit
             # If not quantity is provided, we consider that the value is 1.0 (XXX is it OK ?)
             property_dict['quantity'] = base_amount[amount_generator_line.getBaseApplication()] * \
-                (property_dict.get('net_converted_quantity', property_dict.get('quantity')), 1.0)
+                (property_dict.get('net_converted_quantity', property_dict.get('quantity', 1.0)))
+            # This sounds wrong if cell has getBaseApplication()
+ 
             # We should not keep net_converted_quantity
             if property_dict.get('net_converted_quantity', None) is not None:
               del property_dict['net_converted_quantity']
             # Create an Amount object
-            amount = Amount() # XXX-JPS we could use a movement for safety
+            id = property_dict['id']
+            del property_dict['id']
+            amount = newTempAmount(portal, id) # XXX-JPS Could we use a movement for safety ?
             if category_list: amount._setCategoryList(category_list)
             amount._edit(**property_dict)
             if rounding:
@@ -221,7 +267,11 @@ class AmountGeneratorMixin:
     Implementation of a generic transformation algorith which is
     applicable to payroll, tax generation and BOMs. Return the
     list of amounts with aggregation.
+  
+    TODO:
+    - make working sample code
     """
     raise NotImplementedError
+    # Example of return code
     result = self.getGeneratedAmountList(context, movement_list=movement_list, rounding=rounding)
     return SomeMovementGroup(result)
