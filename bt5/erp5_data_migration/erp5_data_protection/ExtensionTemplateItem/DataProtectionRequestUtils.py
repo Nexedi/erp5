507c82edd49712280c18d5cad0031667a21e9627
Nicolas Delaby <nicolas@nexedi.com>
2010-03-01 12:44:53 +0000

    Initial import of new business template which aim to create Data Protection Request from any document (Global Action). These requests are managed by data protection manager who can delete compromised data on any document (Script proxyfied by role Manager). Once the data protection manager reviewed the Data Protection Request, he can delete some properties on related document or delete all comments in workflow history. Each clean up operation is stored on document as comment in edit_workflow, in order to track data regression.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@33175 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ bt5/erp5_data_migration/erp5_data_protection/ExtensionTemplateItem/DataProtectionRequestUtils.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2008-2010 Nexedi SA and Contributors. All Rights Reserved.
+#                    Nicolas Delaby <nicolas@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Products.ERP5Type.Utils import createExpressionContext
+from Products.ERP5Form.FormBox import FormBox
+from urlparse import urlparse
+
+def Base_updatePropertyMapListWithFieldLabel(self, property_map_list):
+  """Try to get the title of field which edit the given inside property_map_list
+  - This script loopup portal_type, then list actions of type view
+  - Fetch the form by parsing url
+  - list fields, and get the field label
+  - update the dictionary and return the list of updated dictionaries.
+  """
+  def getLabelFromForm(form, property_id):
+    for field in form.get_fields():
+      if isinstance(field, FormBox):
+        # Special use case for FormBox
+        delegated_form_id = field.get_value('formbox_target_id')
+        form_box = getattr(field, delegated_form_id)
+        label = getLabelFromForm(form_box, property_id)
+        if label:
+          return label
+      if 'my_%s' % property_id == field.id:
+        label = field.get_value('title')
+        return label
+  portal = self.getPortalObject()
+  result_list = []
+  portal_type = self.getPortalType()
+  portal_type_object = portal.portal_types[portal_type]
+  action_view_url_list = [action.getAction() for action in \
+                                portal_type_object.getActionInformationList() \
+                                if 'view' in action.getActionType()]
+  action_url_list = [action_view_url(createExpressionContext(self))\
+                                   for action_view_url in action_view_url_list]
+  form_id_list = [urlparse(action_url)[2].replace('%s/' % self.getPath(), '') \
+                                             for action_url in action_url_list]
+  for property_map in property_map_list:
+    label = None
+    for form_id in form_id_list:
+      form = getattr(portal, form_id)
+      if form.meta_type != 'ERP5 Form':
+        continue
+      label = getLabelFromForm(form, property_map['id'])
+      if label:
+        property_map['label'] = label
+        break
+    result_list.append(property_map)
+  return tuple(result_list)
+
+def Base_purgeWorkflowHistoryCommentList(self):
+  """Delete all comment in wokflow history stored on document.
+  """
+  for workflow_id in self.workflow_history:
+     [history.update({'comment': None}) for history in self.workflow_history[workflow_id]]
