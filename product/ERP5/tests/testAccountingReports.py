Nexedi/erp5

commit 007d51db1b004e5bb3e7b0ea91e6fdee10cd93ad

make it possible to extend account_type/expense and account_type/income
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@34121 20353a03-c40f-0410-a6d1-a30d3c3de9de

bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountModule_getAccountListForTrialBalance.xml
@@ -166,7 +166,7 @@ for node in getInventoryList(\n
 \n
 ### profit & loss accounts {{{\n
 for node in getInventoryList(\n
-                node_category_strict_membership=profit_and_loss_account_type,\n
+                node_category=profit_and_loss_account_type,\n
                 from_date=max(period_start_date, from_date),\n
                 group_by_node=1,\n
                 omit_output=1,\n
@@ -182,7 +182,7 @@ for node in getInventoryList(\n
   total_debit += round(total_price, precision)\n
 \n
 for node in getInventoryList(\n
-                node_category_strict_membership=profit_and_loss_account_type,\n
+                node_category=profit_and_loss_account_type,\n
                 from_date=max(period_start_date, from_date),\n
                 group_by_node=1,\n
                 omit_input=1,\n
@@ -387,7 +387,7 @@ for node in getInventoryList(\n
 \n
 ### profit & loss accounts {{{\n
 for node in getInventoryList(\n
-                node_category_strict_membership=profit_and_loss_account_type,\n
+                node_category=profit_and_loss_account_type,\n
                 omit_output=1,\n
                 from_date=min(period_start_date,\n
                               initial_balance_date),\n
@@ -403,7 +403,7 @@ for node in getInventoryList(\n
                     \'initial_debit_balance\', 0) + total_price\n
 \n
 for node in getInventoryList(\n
-                node_category_strict_membership=profit_and_loss_account_type,\n
+                node_category=profit_and_loss_account_type,\n
                 omit_input=1,\n
                 from_date=min(period_start_date,\n
                               initial_balance_date),\n

bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountModule_getGeneralLedgerReportSectionList.xml
@@ -258,7 +258,7 @@ for inventory in portal.portal_simulation.getInventoryList(\n
 \n
 # profit & loss -> same, but from date limited to the current period\n
 for inventory in portal.portal_simulation.getInventoryList(\n
-                            node_category_strict_membership=profit_and_loss_account_type,\n
+                            node_category=profit_and_loss_account_type,\n
                             portal_type=portal.getPortalAccountingMovementTypeList(),\n
                             from_date=max(from_date, period_start_date),\n
                             at_date=at_date,\n

bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingPeriod_createBalanceTransaction.xml
@@ -113,7 +113,8 @@ for section_uid in \\\n
         \'account_type/asset/receivable\',\n
         \'account_type/liability/payable\'):\n
       group_by_mirror_section_node_category_list.append(node_category_url)\n
-    elif node_category_url in (\'account_type/income\', \'account_type/expense\'):\n
+    elif node_category.isMemberOf(\'account_type/income\') or \\\n
+         node_category.isMemberOf(\'account_type/expense\'):\n
       profit_and_loss_node_category_list.append(node_category_url)\n
     else:\n
       group_by_node_node_category_list.append(node_category_url)\n

bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/Node_getAccountingTransactionList.xml
@@ -92,15 +92,18 @@ if not \'parent_portal_type\' in params:\n
 new_result  = []\n
 net_balance = 0.0\n
 \n
-node_account_type = None\n
+# accounts from PL have a balance calculated differently\n
+is_pl_account = False\n
 if params.get(\'node_uid\'):\n
   if context.getUid() == params[\'node_uid\']:\n
-    node_account_type = context.getAccountTypeId()\n
+    is_pl_account = context.isMemberOf(\'account_type/expense\')\\\n
+                 or context.isMemberOf(\'account_type/income\')\n
   else:\n
     node = portal.portal_catalog.getObject(params[\'node_uid\'])\n
-    node_account_type = node.getAccountTypeId()\n
+    is_pl_account = node.isMemberOf(\'account_type/expense\')\\\n
+                 or node.isMemberOf(\'account_type/income\')\n
 \n
-if from_date or node_account_type in (\'expense\', \'income\'):\n
+if from_date or is_pl_account:\n
   period_start_date = None\n
   # Create a new parameter list to get the previous balance\n
   get_inventory_kw = params.copy()\n
@@ -109,7 +112,7 @@ if from_date or node_account_type in (\'expense\', \'income\'):\n
   get_inventory_kw.pop(\'at_date\', None)\n
 \n
   if params.has_key(\'period_start_date\'):\n
-    if node_account_type in (\'expense\', \'income\'):\n
+    if is_pl_account:\n
       # if we have on an expense / income account, only take into account\n
       # movements from the current period.\n
       period_start_date = params[\'period_start_date\']\n
@@ -126,13 +129,12 @@ if from_date or node_account_type in (\'expense\', \'income\'):\n
         from_date = period_start_date\n
 \n
   # Get previous debit and credit\n
-  if from_date == period_start_date and \\\n
-                      node_account_type in (\'expense\', \'income\'):\n
+  if from_date == period_start_date and is_pl_account:\n
     previous_total_debit = previous_total_credit = 0\n
   else:\n
     getInventoryAssetPrice = portal.portal_simulation.getInventoryAssetPrice\n
     # first to the balance at the period start date\n
-    if node_account_type in (\'expense\', \'income\'):\n
+    if is_pl_account:\n
       period_openning_balance = 0\n
     else:\n
       period_openning_balance = getInventoryAssetPrice(\n
@@ -268,7 +270,8 @@ return portal.portal_simulation.getMovementHistoryList(\n
                             <string>_write_</string>
                             <string>new_result</string>
                             <string>net_balance</string>
-                            <string>node_account_type</string>
+                            <string>False</string>
+                            <string>is_pl_account</string>
                             <string>node</string>
                             <string>period_start_date</string>
                             <string>get_inventory_kw</string>

bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/Node_statAccountingBalance.xml
@@ -93,7 +93,8 @@ if params.get(\'period_start_date\', 0) and params.get(\'node_uid\'):\n
     node = context\n
   else:\n
     node = portal.portal_catalog.getObject(params[\'node_uid\'])\n
-  if node.getAccountTypeId() in (\'expense\', \'income\'):\n
+  if node.isMemberOf(\'account_type/expense\') or\\\n
+        node.isMemberOf(\'account_type/income\'):\n
     # For expense or income accounts, we only take into account transactions\n
     # from the beginning of the period.\n
     params[\'from_date\'] = params[\'period_start_date\']\n

bt5/erp5_accounting/bt/revision
@@ -1,1 +1,1 @@
-1148
+1150

commit dfd972b0c0e6291cdbcd0b4a7959f81a8213599f

tests for GL filtered by gap category (it is broken since r34121)
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@34335 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/tests/testAccountingReports.py
@@ -2899,6 +2899,66 @@ class TestAccountingReports(AccountingTestCase, ERP5ReportTestCase):
     self.assertEquals(1, len(data_line_list))
     self.checkLineProperties(data_line_list[0], debit=2900, credit=2900)
  
+
+  def testGeneralLedgerGAPFilter(self):
+    # General Ledger filtered by GAP category
+    # we will use the same data set as account statement
+    self.createAccountStatementDataSet(use_two_bank_accounts=0)
+
+    # set request variables and render
+    request_form = self.portal.REQUEST.form
+    request_form['from_date'] = DateTime(2006, 1, 1)
+    request_form['at_date'] = DateTime(2006, 12, 31)
+    request_form['gap'] = 'my_country/my_accounting_standards/4/40'
+    request_form['section_category'] = 'group/demo_group'
+    request_form['section_category_strict'] = False
+    request_form['simulation_state'] = ['delivered']
+
+    report_section_list = self.getReportSectionList(
+                                    self.portal.accounting_module,
+                                    'AccountModule_viewGeneralLedgerReport')
+    self.assertEquals(2, len(report_section_list))
+
+    self.assertEquals('40 - Payable (Client 1)',
+                      report_section_list[0].getTitle())
+    line_list = self.getListBoxLineList(report_section_list[0])
+    data_line_list = [l for l in line_list if l.isDataLine()]
+    
+    self.assertEquals(2, len(data_line_list))
+    self.checkLineProperties(data_line_list[0],
+          Movement_getSpecificReference='1',
+          Movement_getExplanationTitle='Transaction 1',
+          date=DateTime(2006, 2, 1),
+          Movement_getExplanationTranslatedPortalType='Accounting Transaction',
+          Movement_getExplanationReference='AT 1',
+          Movement_getMirrorSectionTitle='Client 1',
+          debit=0, credit=100, running_total_price=-100, )
+    
+    self.checkLineProperties(data_line_list[1],
+          Movement_getSpecificReference='2',
+          Movement_getExplanationTitle='Transaction 2',
+          date=DateTime(2006, 2, 1, 0, 1),
+          Movement_getExplanationTranslatedPortalType='Accounting Transaction',
+          Movement_getExplanationReference='AT 2',
+          Movement_getMirrorSectionTitle='Client 1',
+          debit=200, credit=0, running_total_price=100, )
+    
+    self.failUnless(line_list[-1].isStatLine())
+    self.checkLineProperties(line_list[-1],
+          Movement_getSpecificReference=None,
+          Movement_getExplanationTitle=None,
+          date=None,
+          Movement_getExplanationTranslatedPortalType=None,
+          Movement_getExplanationReference=None,
+          Movement_getMirrorSectionTitle=None,
+          debit=200, credit=100, )
+    
+    self.assertEquals('Total', report_section_list[1].getTitle())
+    line_list = self.getListBoxLineList(report_section_list[1])
+    data_line_list = [l for l in line_list if l.isDataLine()]
+    self.assertEquals(1, len(data_line_list))
+    self.checkLineProperties(data_line_list[0], debit=200, credit=100)
+ 
  
   def testProfitAndLoss(self):
     # Simple test of profit and loss