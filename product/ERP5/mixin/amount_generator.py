caadbf432c5a83c790513c414d4cf372e6bcc131
Romain Courteaud <romain@nexedi.com>
2010-04-27 09:27:40 +0000

    Transformation needs to get properties defined on the transformation line
    git-svn-id: https://svn.erp5.org/repos/public/erp5/sandbox/amount_generator@34806 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/mixin/amount_generator.py
+++ product/ERP5/mixin/amount_generator.py
@@ -170,12 +170,12 @@ class AmountGeneratorMixin:
       # Try to collect cells and aggregate their mapped properties
       # using resource + variation as aggregation key or base_application
       # for intermediate lines
-      amount_generator_cell_list = self.contentValues(
+      amount_generator_cell_list = [self] + self.contentValues(
         portal_type=amount_generator_cell_type_list)
       resource_amount_aggregate = {} # aggregates final line information
       value_amount_aggregate = {} # aggregates intermediate line information
 
-      for amount_generator_cell in amount_generator_cell_list or (self,):
+      for amount_generator_cell in amount_generator_cell_list:
         if not amount_generator_cell.test(delivery_amount):
           continue
         base_application_list = amount_generator_cell.getBaseApplicationList()
