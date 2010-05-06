9505b2a39f91424cbcb65eb4969101c1e44f36d0
Lucas Carvalho <lucas@nexedi.com>
2010-05-06 19:04:19 +0000

    Added helper for openSUSE 11.2.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@35068 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ buildout/helpers/opensuse.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+USAGE="""\
+Helper for buildout-base installation of ERP5 on OpenSUSE 11.2
+ Usage:
+   opensuse.sh [-h|-l]
+
+   With no options, attempts to install the dependencies required
+   to run the buildout.
+
+ Options:
+   -h  shows this message
+   -l  lists the required dependencies\
+"""
+PACKAGE_LIST="""\
+automake
+bison
+cpio
+flex
+gcc
+gcc-c++
+groff
+libzip-devel
+gdbm-devel
+glib-devel
+libjpeg-devel
+libldapcpp-devel
+ncurses-devel
+libneon-devel
+libopenssl-devel
+readline-devel
+libgsasl-devel
+termcap
+libxml2-devel
+libxslt-devel
+make
+patch
+python-devel
+python-setuptools
+rpm
+subversion
+subversion-devel
+subversion-tools
+xorg-x11-server
+zip
+zlib-devel\
+"""
+if [ x"$1" == x ]; then
+  zypper install $PACKAGE_LIST
+elif [ "$1" = "-l" ]; then
+  echo "$PACKAGE_LIST"
+elif [ "$1" = "-h" ]; then
+  echo "$USAGE"
+else
+  echo "Unknown argument."
+  echo "$USAGE"
+fi
