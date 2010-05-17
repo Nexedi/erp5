a249cf393f2b169e0433d2158884d00f3b5c60c8
Tatuya Kamada <tatuya@nexedi.com>
2010-05-17 06:06:03 +0000

    Revert the r35339 commit, since the commit testFormPrintoutAsODT.test_08_OOoConversion started to fail.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@35370 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5OOo/Document/OOoDocument.py
+++ product/ERP5OOo/Document/OOoDocument.py
@@ -309,7 +309,7 @@ class OOoDocument(PermanentURLMixIn, BaseConvertableAndFileMixin, File,
     """
     #XXX if document is empty, stop to try to convert.
     #XXX but I don't know what is a appropriate mime-type.(Yusei)
-    if not self.hasData():
+    if not self.get_size() == 0:
       return 'text/plain', ''
     # if no conversion asked (format empty)
     # return raw data
