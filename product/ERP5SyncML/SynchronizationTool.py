d4c8f3d467806123eb9ea5223566f3c80ccc2c32
Nicolas Delaby <nicolas@nexedi.com>
2010-05-31 15:09:44 +0000

    Use ERP5 API to access acquired parent of object
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@35799 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5SyncML/SynchronizationTool.py
+++ product/ERP5SyncML/SynchronizationTool.py
@@ -545,7 +545,7 @@ class SynchronizationTool( SubscriptionSynchronization,
       if copy_path is not None:
         #LOG('p_sync.applyPublisherValue, conflict_list empty on : ', TRACE, signature)
         # Delete the copy of the object if the there is one
-        directory = object.aq_parent
+        directory = object.getParentValue()
         copy_id = copy_path[-1]
         #LOG('p_sync.applyPublisherValue, copy_id: ', TRACE, copy_id)
         if hasattr(directory.aq_base, 'hasObject'):
