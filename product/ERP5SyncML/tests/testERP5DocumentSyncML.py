620af1ebe75fedc2150dc39c0239c43fa860198a
Nicolas Delaby <nicolas@nexedi.com>
2010-05-31 17:07:16 +0000

    Compare modified objects
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@35832 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5SyncML/tests/testERP5DocumentSyncML.py
+++ product/ERP5SyncML/tests/testERP5DocumentSyncML.py
@@ -686,9 +686,10 @@ class TestERP5DocumentSyncML(TestERP5DocumentSyncMLMixin, ERP5TypeTestCase):
     self.tic()
     self.synchronize(self.sub_id1)
     self.checkSynchronizationStateIsSynchronized()
-    document_s = document_server._getOb(self.id1)
-    self.assertEqual(document_s.getDescription(), self.description2)
+    document_c = document_client1._getOb(self.id1)
+    self.assertEqual(document_c.getDescription(), self.description2)
     self.assertEqual(document_s.getShortTitle(), self.short_title1)
+    self.assertEqual(document_s.getBaseData(), document_c.getBaseData())
     self.assertXMLViewIsEqual(self.sub_id1, document_s, document_c)
 
   def test_04_DeleteObject(self, quiet=0):
