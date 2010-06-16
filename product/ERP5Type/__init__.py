cd29190147307f08ebffafa8eb3aa13d532f68e1
Jérome Perrin <jerome@nexedi.com>
2010-06-16 13:50:45 +0000

    allow PropertyTypeValidity to be used in restricted code
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@36397 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/__init__.py
+++ product/ERP5Type/__init__.py
@@ -145,6 +145,7 @@ allow_module('Products.ERP5Type.Error')
 allow_module('Products.ERP5Type.JSONEncoder')
 allow_module('Products.ERP5Type.Log')
 ModuleSecurityInfo('Products.ERP5Type.JSON').declarePublic('dumps', 'loads')
+ModuleSecurityInfo('Products.ERP5Type.Constraint').declarePublic('PropertyTypeValidity')
 ModuleSecurityInfo('pprint').declarePublic('pformat', 'pprint')
 
 if sys.version_info[0:2] == (2, 4):
