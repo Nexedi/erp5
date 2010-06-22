90e74f7fe9a7a9ace202a93ac3c7f600ac508d84
Mohamadou Mbengue <mmbengue@gmail.com>
2010-06-22 15:06:58 +0000

    Create role categories to test security
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@36518 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testERP5eGov.py
+++ product/ERP5/tests/testERP5eGov.py
@@ -115,7 +115,9 @@ class TestEgov(ERP5TypeTestCase):
     """Returns a list of categories that should be created."""
     return ('group/client','group/client/dgid/di', 'group/client/dgid/bf',
             'function/impots/taxes_indirectes', 'function/impots/section/chef',
-            'role/entreprise')
+            'role/citoyen', 'role/citoyen/national', 'role/citoyen/etranger', 
+            'role/entreprise', 'role/entreprise/agence', 'role/entreprise/siege', 'role/entreprise/succursale', 
+            'function/entreprise/mandataire')
 
   def afterSetUp(self):
     uf = self.getPortal().acl_users
