c78ca8dff7cd7371c4021c7072ac41dea0119051
Lucas Carvalho <lucas@nexedi.com>
2010-06-23 19:11:00 +0000

    Added expectedFailure as Rafael requested to me.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@36559 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testBusinessTemplate.py
+++ product/ERP5/tests/testBusinessTemplate.py
@@ -6752,6 +6752,8 @@ class TestBusinessTemplate(ERP5TypeTestCase, LogInterceptor):
     self.assertEquals(self.portal.exported_path,
         self.portal.portal_catalog.getResultValue(uid=uid))
 
+  # XXX (lucas): Rafael requested to add expectedFailure
+  @expectedFailure
   def test_build_and_export_bt5_into_same_transaction(self):
     """
       Copy, build and export a business template into the same transaction.
