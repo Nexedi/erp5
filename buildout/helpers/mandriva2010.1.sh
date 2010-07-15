5ebdd8cf916cb50ff2b9ddd306e508d06b0d5303
Łukasz Nowak <luke@nexedi.com>
2010-07-15 07:31:09 +0000

    - add helpers for Mandriva 2010.1  - add there libgc1, as needed by w3m
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@37115 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ buildout/helpers/mandriva2010.1.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+USAGE="""\
+Helper for buildout-base installation of ERP5 on Mandriva 2010.
+ Usage:
+   mandriva2010.sh [-h|-l]
+
+   With no options, attempts to install the dependencies required
+   to run the buildout.
+
+ Options:
+   -h  shows this message
+   -l  lists the required dependencies\
+"""
+PACKAGE_LIST="""\
+automake
+bison
+cpio
+flex
+gcc
+gcc-c++
+gettext
+groff
+libSDL-devel
+libSDL_gfx-devel
+libSDL_image-devel
+libboost-devel
+libbzip2-devel
+libgc-devel
+libgc1
+libgdbm-devel
+libglib2-devel
+libgomp-devel
+libjpeg-devel
+libldap-devel
+libncurses-devel
+libneon0.27-devel
+libopenssl-devel
+libpng-devel
+libreadline-devel
+librsync-devel
+libsasl2-devel
+libtermcap-devel
+libtool
+libungif-devel
+libxml2-devel
+libxslt-devel
+make
+patch
+python-devel
+python-setuptools
+rpm
+scons
+subversion
+subversion-devel
+subversion-tools
+tesseract-devel
+x11-server-xvfb
+zip
+zlib1-devel\
+"""
+if [ x"$1" == x ]; then
+  urpmi $PACKAGE_LIST
+elif [ "$1" = "-l" ]; then
+  echo "$PACKAGE_LIST"
+elif [ "$1" = "-h" ]; then
+  echo "$USAGE"
+else
+  echo "Unknown argument."
+  echo "$USAGE"
+fi
