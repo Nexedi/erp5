f057931e040cc258457e610bafd00207b1a7b3a0
Łukasz Nowak <luke@nexedi.com>
2010-07-15 15:34:53 +0000

    - typo
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@37147 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Tool/TemplateTool.py
+++ product/ERP5/Tool/TemplateTool.py
@@ -580,7 +580,7 @@ class TemplateTool (BaseTool):
       bt5_path_list.append(os.path.join(site_configuration.instancehome, 'bt5'))
       # add locally saved Business Templates, not perfect, but helps some
       # people doing strict TTW development
-      bt5_paht_list.append(site_configuration.clienthome)
+      bt5_path_list.append(site_configuration.clienthome)
       test_cmd_args += ['--bt5_path', ','.join(bt5_path_list)]
       test_cmd_args += test_list
       # prepare message - intentionally without any additional formatting, as
