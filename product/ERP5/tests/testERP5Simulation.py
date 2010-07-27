995551aa12f1f0fd3d6d8a4687c31021409d372d
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2010-07-27 20:21:33 +0000

    revert a part of r36960. test_05d_SimulationChangeResourceOnOneSimulationMovementForMergedLine still will not pass.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@37301 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testERP5Simulation.py
+++ product/ERP5/tests/testERP5Simulation.py
@@ -549,6 +549,10 @@ class TestERP5SimulationPackingList(TestERP5SimulationMixin, TestPackingList):
   pass
 
 for failing_method in [
+  # This test does not work as it is because of the different behaviour of
+  # Adopt Solver.
+  'test_05d_SimulationChangeResourceOnOneSimulationMovementForMergedLine',
+
   # Those tests currently fail because they are making assertions on an applied
   # rule which with the new simulation structure is not the same as in the
   # original test packing list
