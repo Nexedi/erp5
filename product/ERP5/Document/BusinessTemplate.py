Nexedi/erp5

commit befb65f2c47fa59c4a0a144b3ce0323c4635ce01

Use classes instead of meta types to determine attribute cleanup conditions. It does not necessarily requires us to load the object.
git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@38601 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/BusinessTemplate.py
@@ -585,20 +585,20 @@ class BaseTemplateItem(Implicit, Persistent):
     """
     Remove unneeded properties for export
     """
-    meta_type = getattr(aq_base(obj), 'meta_type', None)
-    if meta_type == 'Script (Python)':
-      meta_type = 'ERP5 Python Script'
+    klass = obj.__class__
+    classname = klass.__name__
 
     attr_set = set(('_dav_writelocks', '_filepath', '_owner', 'uid',
                     'workflow_history', '__ac_local_roles__'))
     if export:
-      attr_set.update({
-        'ERP5 Python Script': (#'func_code', 'func_defaults', '_code',
-                               '_lazy_compilation', 'Python_magic'),
-        #'Z SQL Method': ('_arg', 'template',),
-      }.get(meta_type, ()))
-
-      if interfaces.IIdGenerator.providedBy(obj):
+      # PythonScript covers both Zope Python scripts
+      # and ERP5 Python Scripts
+      if classname == 'PythonScript':
+        attr_set.update((#'func_code', 'func_defaults', '_code',
+                         '_lazy_compilation', 'Python_magic'))
+      #elif classname == 'SQL' and klass.__module__== 'Products.ZSQLMethods':
+      #  attr_set.update(('_arg', 'template'))
+      elif interfaces.IIdGenerator.providedBy(obj):
         for dict_name in ('last_max_id_dict', 'last_id_dict'):
           if getattr(obj, dict_name, None) is not None:
             delattr(obj, dict_name)
@@ -607,10 +607,10 @@ class BaseTemplateItem(Implicit, Persistent):
       if attr in attr_set or attr.startswith('_cache_cookie_'):
         delattr(obj, attr)
 
-    if meta_type == 'ERP5 PDF Form':
+    if classname == 'PDFForm':
       if not obj.getProperty('business_template_include_content', 1):
         obj.deletePdfContent()
-    elif meta_type == 'ERP5 Python Script':
+    elif classname == 'PythonScript':
       if export:
         # XXX forward compatibility: set to None instead of deleting '_code'
         #     so that old BT code can import recent BT

commit 2e3a7aed0dff33fb6baf4cb31d22ecb5864bbbf0

Do garbage collection to remove references from non-persistent objects
This commit is similar to r32811 and should fix regressions caused by r38601
for good.

git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@38635 20353a03-c40f-0410-a6d1-a30d3c3de9de

product/ERP5/Document/BusinessTemplate.py
@@ -3374,6 +3374,7 @@ class DocumentTemplateItem(BaseTemplateItem):
             # Then we need to flush from all caches, not only the one from this
             # connection
             self.getPortalObject()._p_jar.db().cacheMinimize()
+            gc.collect()
     else:
       BaseTemplateItem.install(self, context, trashbin, **kw)
       for id in self._archive.keys():