5a9a395354a8f8e4516c47f0d908a4020372a249
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2010-11-09 12:26:13 +0000

    simplify mysql-tritonn-hooks.py. * no need to invoke 'cd' because you are already there. * no need to specify version for aclocal and automake, because their location should be specified by PATH environment variable. * convert TAB to white spaces.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@40089 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- buildout/hooks/mysql-tritonn-hooks.py
+++ buildout/hooks/mysql-tritonn-hooks.py
@@ -4,21 +4,20 @@ import os
 # the comand below assumes there is only one sub-directory under the
 # 'compile-directory', which is why the cd .../* would work.
 CMDS = """
-cd %s/*
 libtoolize -c -f
-which aclocal-1.9 >/dev/null 2>/dev/null && aclocal-1.9 || aclocal
+aclocal
 autoheader
-which automake-1.9 >/dev/null 2>/dev/null && automake-1.9 -c -a -i || automake -c -a -i
+automake -c -a -i
 autoconf
 touch sql/sql_yacc.yy
 """.strip()
 
 def pre_configure_hook(options, buildout):
-    os.system(CMDS % options['compile-directory'])
+    os.system(CMDS)
 
 def post_make_hook(options, buildout):
     try:
-    	os.mkdir("%s/var" % options['location'])
+        os.mkdir("%s/var" % options['location'])
     except OSError, e:
-	if e.errno != errno.EEXIST:
-	    raise
+        if e.errno != errno.EEXIST:
+            raise
