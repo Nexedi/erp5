23a78d9edbb7e892837ac9b464be855c34b981ab
Łukasz Nowak <luke@nexedi.com>
2010-11-22 12:53:17 +0000

    - Predicate was moved from ERP5 to ERP5Type
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@40460 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/vifib_open_trade/DocumentTemplateItem/SubscriptionItemRootSimulationRule.py
+++ bt5/vifib_open_trade/DocumentTemplateItem/SubscriptionItemRootSimulationRule.py
@@ -33,7 +33,7 @@ will replace DeliveryRule.
 import zope.interface
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type import Permissions, PropertySheet, interfaces
-from Products.ERP5.Document.Predicate import Predicate
+from Products.ERP5Type.Document.Predicate import Predicate
 from Products.ERP5.mixin.rule import RuleMixin
 from Products.ERP5.mixin.movement_collection_updater import \
      MovementCollectionUpdaterMixin
