d390555ca9dd1ac6a73ec36a71522c0a4b07cd3d
Julien Muchembled <jm@nexedi.com>
2010-12-22 13:11:09 +0000

    Fix testLegacyBPMCore
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@41658 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/erp5_simulation_legacy/DocumentTemplateItem/SimulationLegacyPatches.py
+++ bt5/erp5_simulation_legacy/DocumentTemplateItem/SimulationLegacyPatches.py
@@ -35,7 +35,53 @@ def patch():
 
   composition._LEGACY_SIMULATION = True
 
+  ## Movement
+
+  from Products.ERP5.Document.Movement import Movement
+
+  def isFrozen(self):
+    """
+    Returns the frozen status of this movement.
+    a movement in stopped, delivered or cancelled states is automatically frozen.
+    If frozen is locally set to '0', we must check for a parent set to '1', in
+    which case, we want the children to be frozen as well.
+
+    BPM evaluation allows to set frozen state list per Business Path.
+    """
+    business_path = self.getCausalityValue(portal_type='Business Path')
+    if business_path is None:
+      # XXX Hardcoded
+      # Maybe, we should use getPortalCurrentInventoryStateList
+      # and another portal method for cancelled (and deleted)
+      #     LOG("Movement, isFrozen", DEBUG, "Hardcoded state list")
+      if self.getSimulationState() in ('stopped', 'delivered', 'cancelled'):
+        return 1
+    else:
+      # conditional BPM enabled frozen state check
+      # BPM dynamic configuration
+      if self.getSimulationState() in business_path.getFrozenStateList():
+        return True
+
+    # manually frozen
+    frozen = self._baseIsFrozen()
+    if frozen == 0:
+      self._baseSetFrozen(None)
+    return frozen or False
+
+  Movement.isFrozen = isFrozen
+
   ## SimulationMovement
+
+  from Products.ERP5.Document.SimulationMovement import SimulationMovement
+  del SimulationMovement.isFrozen
+
+  def isCompleted(self):
+    """Zope publisher docstring. Documentation in ISimulationMovement"""
+    # only available in BPM, so fail totally in case of working without BPM
+    return self.getSimulationState() in self.getCausalityValue(
+        portal_type='Business Path').getCompletedStateList()
+
+  SimulationMovement.isCompleted = isCompleted
 
   def asComposedDocument(self, *args, **kw):
     # XXX: What delivery should be used to find amount generator lines ?
@@ -57,7 +103,22 @@ def patch():
         return self.getOrderValue().asComposedDocument(*args, **kw)
       self = grand_parent
 
-  from Products.ERP5.Document.SimulationMovement import SimulationMovement
   SimulationMovement.asComposedDocument = asComposedDocument
+
+  def isBuildable(self):
+    """Simulation Movement buildable logic"""
+    if self.getDeliveryValue() is not None:
+      # already delivered
+      return False
+
+    # might be buildable - business path dependent
+    business_path = self.getCausalityValue(portal_type='Business Path')
+    explanation_value = self.getExplanationValue()
+    if business_path is None or explanation_value is None:
+      return True
+
+    return len(business_path.filterBuildableMovementList([self])) == 1
+
+  SimulationMovement.isBuildable = isBuildable
 
 patch()
