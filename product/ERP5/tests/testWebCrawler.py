4e0ca62cf4c4d8d028b8ed1130caab8631bf6584
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2011-01-16 21:32:17 +0000

    now normalised URL should be like this, i.e. '%20' instead of ' ', with the recent urlnorm library, that is installed by the recent buildout.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@42361 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testWebCrawler.py
+++ product/ERP5/tests/testWebCrawler.py
@@ -215,7 +215,7 @@ class TestWebCrawler(ERP5TypeTestCase):
     web_page.edit(text_content=text_content)
     self.assertEquals(web_page.getContentBaseURL(), "http://www.example.com")
     self.assertEquals(web_page.getContentNormalisedURLList(),
-                    ["http://www.example.com/I don't care I put what/ I want/",
+                    ["http://www.example.com/I%20don't%20care%20I%20put%20what/%20I%20want/",
                      'http://www.example.com/section',
                      'http://www.example.com/section2',])
     # relative links without base tag
