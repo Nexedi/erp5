fb7304ea048940e136f13d5e09a341652167a416
Lucas Carvalho <lucas@nexedi.com>
2011-01-18 23:15:43 +0000

    Added missing business template.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@42433 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/prepareFunctionalTest.py
+++ product/ERP5Type/tests/prepareFunctionalTest.py
@@ -75,8 +75,8 @@ class TestZelenium(ERP5TypeTestCase):
                 'erp5_credential',		
                 'erp5_km', 'erp5_km_ui_test',
                 'erp5_l10n_fr',		
-                'erp5_crm', 'erp5_configurator', 'erp5_workflow',
-                'erp5_configurator_standard_ui_test',
+                'erp5_crm', 'erp5_workflow', 'erp5_configurator',
+                'erp5_configurator_standard', 'erp5_configurator_standard_ui_test',
                 # erp5_web_ui_test must run at the last, because it logs out
                 # manager user and continue other tests as a user created in
                 # that test.
