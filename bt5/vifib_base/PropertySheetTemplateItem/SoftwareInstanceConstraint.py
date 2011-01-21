90fb81cb75d0b06287bf74b1367f96a0f0dc8365
Romain Courteaud <romain@nexedi.com>
2011-01-21 10:21:01 +0000

    More properties are required on Software Instance
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@42560 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- bt5/vifib_base/PropertySheetTemplateItem/SoftwareInstanceConstraint.py
+++ bt5/vifib_base/PropertySheetTemplateItem/SoftwareInstanceConstraint.py
@@ -36,12 +36,40 @@ class SoftwareInstanceConstraint:
       "message_property_not_set" : 'XML must be set',
       "message_no_such_property" : 'XML must be set'
     },
+    { 'id'            : 'reference_property_existence',
+      'description'   : 'Property reference must be defined',
+      'type'          : 'PropertyExistence',
+      'reference'     : None,
+      "message_property_not_set" : 'Reference must be set',
+      "message_no_such_property" : 'Reference must be set'
+    },
     { 'id'            : 'property_existence',
       'description'   : 'Property source reference must be defined',
       'type'          : 'PropertyExistence',
       'source_reference'     : None,
-      "message_property_not_set" : 'Reference must be set',
-      "message_no_such_property" : 'Reference must be set'
+      "message_property_not_set" : 'Source Reference must be set',
+      "message_no_such_property" : 'Source Reference must be set'
+    },
+    { 'id'            : 'destination_reference_property_existence',
+      'description'   : 'Property destination reference must be defined',
+      'type'          : 'PropertyExistence',
+      'destination_reference'     : None,
+      "message_property_not_set" : 'Destination Reference must be set',
+      "message_no_such_property" : 'Destination Reference must be set'
+    },
+    { 'id'            : 'ssl_key',
+      'description'   : 'Property SSL Key must be defined',
+      'type'          : 'PropertyExistence',
+      'ssl_key'     : None,
+      "message_property_not_set" : 'SSL Key must be set',
+      "message_no_such_property" : 'SSL Key must be set'
+    },
+    { 'id'            : 'ssl_certificate',
+      'description'   : 'Property SSL Certificate must be defined',
+      'type'          : 'PropertyExistence',
+      'ssl_certificate'     : None,
+      "message_property_not_set" : 'SSL Certificate must be set',
+      "message_no_such_property" : 'SSL Certificate must be set'
     },
     { 'id'            : 'text_content_validation',
       'description'   : 'Property text content must be valid against Softwa'\
