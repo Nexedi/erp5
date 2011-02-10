a73c8dabc1d3db5b71d524789a40eb713f7dc991
Romain Courteaud <romain@nexedi.com>
2011-02-10 14:19:12 +0000

    Add CategoryAcquiredMembershipArityConstraint class
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@43271 20353a03-c40f-0410-a6d1-a30d3c3de9de

---
+++ product/ERP5Type/Core/CategoryAcquiredMembershipArityConstraint.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2002, 2005 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Sebastien Robin <seb@nexedi.com>
+#                    Romain Courteaud <romain@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Products.ERP5Type.Core.CategoryMembershipArityConstraint \
+          import CategoryMembershipArityConstraint
+
+class CategoryAcquiredMembershipArityConstraint(CategoryMembershipArityConstraint):
+  """
+    This method checks if an object respects the arity.
+
+    For example we can check if every Order has at least one source.
+    Configuration example:
+    { 'id'            : 'source',
+      'description'   : '',
+      'type'          : 'CategoryAcquiredMembershipArity',
+      'min_arity'     : '1',
+      'max_arity'     : '1',
+      'portal_type'   : ('Organisation', ),
+      'base_category' : ('source',)
+      'condition'     : 'python: object.getPortalType() == 'Foo',
+    },
+  """
+
+  def _calculateArity(self, obj):
+    base_category = self.constraint_definition['base_category']
+    portal_type = self.constraint_definition['portal_type']
+    import pdb; pdb.set_trace()
+    return len(obj.getAcquiredCategoryMembershipList(base_category,
+                                              portal_type=portal_type))
+
