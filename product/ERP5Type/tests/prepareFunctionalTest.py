043b2ecd4e3846991c6f0bb4de3711b8bb46f3c7
Ivan Tyagov <ivan@nexedi.com>
2011-02-18 14:03:08 +0000

    Add needed bt5s.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@43477 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/tests/prepareFunctionalTest.py
+++ product/ERP5Type/tests/prepareFunctionalTest.py
@@ -57,7 +57,8 @@ class TestZelenium(ERP5TypeTestCase):
         """
           Return the list of business templates.
         """
-        return ('erp5_base', 'erp5_ui_test_core', 'erp5_ui_test', 'erp5_forge',
+        return ('erp5_core_proxy_field_legacy', 'erp5_full_text_myisam_catalog',
+                'erp5_base', 'erp5_ui_test_core', 'erp5_ui_test', 'erp5_forge',
                 'erp5_dhtml_style', 'erp5_dhtml_ui_test',
                 'erp5_xhtml_jquery_style', 'erp5_jquery', 'erp5_jquery_ui',
                 'erp5_knowledge_pad',
