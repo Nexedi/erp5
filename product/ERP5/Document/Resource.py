c27b5e0ddecbd07a98a75db2c21ccdcc8999d17d
Aurélien Calonne <aurel@nexedi.com>
2011-03-09 09:51:54 +0000

    do not use the catalog to retrieve individual variations because in some cases we might miss them
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@44068 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/Resource.py
+++ product/ERP5/Document/Resource.py
@@ -113,7 +113,7 @@ class Resource(XMLObject, XMLMatrix, VariatedMixin):
         elif isinstance(base_category_list, str):
           base_category_list = (base_category_list,)
 
-        individual_variation_list = self.searchFolder(
+        individual_variation_list = self.contentValues(
             portal_type=self.getPortalVariationTypeList(),
             sort_on=[('title','ascending')])
         individual_variation_list = [x.getObject() for x in
@@ -185,7 +185,7 @@ class Resource(XMLObject, XMLMatrix, VariatedMixin):
                             display_id=display_id, base=base, **kw)
 
       if not omit_individual_variation:
-        individual_variation_list = self.searchFolder(
+        individual_variation_list = self.contentValues(
             portal_type=self.getPortalVariationTypeList())
         individual_variation_list = [x.getObject() for x in
             individual_variation_list]
