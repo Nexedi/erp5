1decef021bacf6e447649680dfa8f1711e8935ed
Jérome Perrin <jerome@nexedi.com>
2011-03-15 09:07:28 +0000

    fix typos
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@44281 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/mixin/equivalence_tester.py
+++ product/ERP5/mixin/equivalence_tester.py
@@ -119,8 +119,8 @@ class EquivalenceTesterMixin:
   def compare(self, prevision_movement, decision_movement):
     """
     Returns True if prevision_movement and delivery_movement
-    match. Returns False else. The method is asymmetric and
-    the order of parameter matters. For example, a sourcing
+    match. Returns False otherwise. The method is asymmetric and
+    the order of parameters matters. For example, a sourcing
     rule may use a tester which makes sure that movements are
     delivered no sooner than 2 weeks before production but
     no later than the production date.
@@ -202,8 +202,8 @@ class EquivalenceTesterMixin:
 
   def getUpdatablePropertyDict(self, prevision_movement, decision_movement):
     """
-    Returns a list of properties to update on decision_movement
-    prevision_movement so that next call to compare returns True.
+    Returns a mapping of properties to update on decision_movement so that next
+    call to compare against prevision_movement returns True.
 
     prevision_movement -- a simulation movement (prevision)
 
