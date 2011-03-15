de1a961e3532081278bf4f4c2f05449bc52106d9
Rafael Monnerat <rafael@nexedi.com>
2011-03-15 19:32:50 +0000

    Enable ERP5 Site creation and convert options to lists.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@44322 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- slapos/recipe/erp5/src/slapos/recipe/erp5/__init__.py
+++ slapos/recipe/erp5/src/slapos/recipe/erp5/__init__.py
@@ -33,8 +33,6 @@ import sys
 import zc.buildout
 import zc.recipe.egg
 
-CREATE_ERP5_SITE = 0
-
 # Taken from Zope2 egg
 def write_inituser(fn, user, password):
   fp = open(fn, "w")
@@ -72,8 +70,7 @@ class Recipe(BaseSlapRecipe):
         apache_login=self.installLoginApache(ip=self.getGlobalIPv6Address(),
           port=13000, backend=zope_access, key=ca_conf['login_key'],
           certificate=ca_conf['login_certificate']))
-    if CREATE_ERP5_SITE:
-      self.installERP5Site(user, password, zope_access,
+    self.installERP5Site(user, password, zope_access,
             mysql_conf, conversion_server_conf, memcached_conf, self.site_id)
     self.installTestRunner(ca_conf, mysql_conf, conversion_server_conf)
     self.linkBinary()
@@ -346,8 +343,9 @@ class Recipe(BaseSlapRecipe):
     mysql_connection_string = "%(mysql_database)s@%(ip)s:%(tcp_port)s %(mysql_user)s %(mysql_password)s" % mysql_conf
 
     # XXX URL list vs. repository + list of bt5 names?
-    bt5_url_list = self.options.get("bt5_url_list", '')
-    bt5_repository_list = self.options.get("bt5_repository_list", '')
+    bt5_url_list = self.options.get("bt5_url_list", '').split()
+    bt5_repository_list = self.options.get("bt5_repository_list", '').split()
+
     self.path_list.extend(zc.buildout.easy_install.scripts([('erp5_update',
             __name__ + '.erp5', 'updateERP5')], self.ws,
                   sys.executable, self.wrapper_directory,
