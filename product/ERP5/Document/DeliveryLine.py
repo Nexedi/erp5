fab281dbef0fc12dec1494da46898b23da4e85a3
Jérome Perrin <jerome@nexedi.com>
2011-03-16 13:41:30 +0000

    revive DeliveryLine.isDivergent, it exists here because it also checks that delivery cells are divergent. Update the docstrings to make it explicit
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@44355 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/Document/DeliveryLine.py
+++ product/ERP5/Document/DeliveryLine.py
@@ -367,10 +367,17 @@ class DeliveryLine(Movement, XMLObject, XMLMatrix, ImmobilisationMovement):
 
 # divergence support with solving
 
+    security.declareProtected(Permissions.AccessContentsInformation, 'isDivergent')
+    def isDivergent(self):
+      """Returns true if the delivery line is divergent, or if any contained
+      cell is divergent.
+      """
+      return bool(self.getDivergenceList())
+
     security.declareProtected(Permissions.AccessContentsInformation, 'getDivergenceList')
     def getDivergenceList(self):
-      """
-      Return a list of messages that contains the divergences
+      """Returns a list of messages that contains the divergences for that line
+      and the cells it may contain.
       """
       divergence_list = []
       if self.hasCellContent():
