e28dbecbc6d6828fa946892ede6e98801ee4faa3
Sebastien Robin <seb@nexedi.com>
2011-03-17 10:28:37 +0000

    remove two useless scripts
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@44379 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5/tests/testBPMCore.py
+++ product/ERP5/tests/testBPMCore.py
@@ -141,10 +141,6 @@ class TestBPMMixin(ERP5TypeTestCase):
   def createBusinessLink(self, business_process=None, **kw):
     if business_process is None:
       business_process = self.createBusinessProcess()
-    kw['destination_method_id'] = kw.pop('destination_method_id',
-        'BusinessLink_getDefaultDestinationList')
-    kw['source_method_id'] = kw.pop('source_method_id',
-        'BusinessLink_getDefaultSourceList')
     business_link = business_process.newContent(
       portal_type=self.business_link_portal_type, **kw)
     return business_link
