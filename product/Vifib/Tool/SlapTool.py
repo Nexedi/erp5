c3cfe4c0563c219d3be0550e321ad0d9105db538
Łukasz Nowak <luke@nexedi.com>
2011-04-04 11:54:17 +0000

    - revert 45036, as the newest packing list with given state shall work
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@45041 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/Vifib/Tool/SlapTool.py
+++ product/Vifib/Tool/SlapTool.py
@@ -415,6 +415,7 @@ class SlapTool(BaseTool):
 
       elif movement.getResource() == \
               portal_preferences.getPreferredInstanceCleanupResource():
+
         if movement.getSimulationState() in ('confirmed', 'started', 'stopped'):
           slap_partition._need_modification = 1
 
@@ -720,14 +721,25 @@ class SlapTool(BaseTool):
       service = portal.restrictedTraverse(service_relative_url)
       service_uid_list.append(service.getUid())
 
+    # Get associated software release
+    state_list = []
+    state_list.extend(portal.getPortalCurrentInventoryStateList())
+    state_list.extend(portal.getPortalReservedInventoryStateList())
+    state_list.extend(portal.getPortalTransitInventoryStateList())
+
     # Use getTrackingList
-    return portal.portal_catalog.getResultValue(
+    catalog_result = portal.portal_catalog(
       portal_type='Sale Packing List Line',
+      simulation_state=state_list,
       aggregate_relative_url=computer_partition_document.getRelativeUrl(),
       default_resource_uid=service_uid_list,
       sort_on=(('movement.start_date', 'DESC'),),
       limit=1,
     )
+    if len(catalog_result):
+      return catalog_result[0].getObject()
+    else:
+      return None
 
   def _reportComputerUsage(self, computer, usage):
     """Stores usage report of a computer."""
