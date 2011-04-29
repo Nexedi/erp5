3efacd9a0cabed2ad1038c9f0e51720e11cc9bec
Vincent Pelletier <vincent@nexedi.com>
2011-04-29 14:07:18 +0000

    Make a monkeypatch out of LP Bug #706946.
    Uses slightly modified version of Kazuhiko's patch, as attached on comment #1.
    See https://bugs.launchpad.net/zope2/+bug/706946
    
    
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk@45756 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -71,6 +71,7 @@ from Products.ERP5Type.patches import ZODBConnection
 # dropped support for older versions.
 from Products.ERP5Type.patches import TransactionAddBeforeCommitHook
 from Products.ERP5Type.patches import ZopePageTemplate
+from Products.ERP5Type.patches import ZopePageTemplateUtils
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
