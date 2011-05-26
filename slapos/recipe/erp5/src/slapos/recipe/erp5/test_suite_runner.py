1bc7ccd1eae846866db0529cbc5fd5a7a373dafe
Nicolas Delaby <nicolas@nexedi.com>
2011-05-26 18:48:57 +0200

    Deal with Shebang size limitation

--- slapos/recipe/erp5/src/slapos/recipe/erp5/test_suite_runner.py
+++ slapos/recipe/erp5/src/slapos/recipe/erp5/test_suite_runner.py
@@ -8,4 +8,16 @@ def runTestSuite(args):
   env['PATH'] = ':'.join([d['prepend_path']] + os.environ['PATH'].split(':'))
   env['INSTANCE_HOME'] = d['instance_home']
   env['REAL_INSTANCE_HOME'] = d['instance_home']
-  os.execve(d['call_list'][0], d['call_list'] + sys.argv[1:], env)
+  # Deal with Shebang size limitation
+  executable_filepath = d['call_list'][0]
+  file_object = open(executable_filepath, 'r')
+  line = file_object.readline()
+  file_object.close()
+  argument_list = []
+  if line[:2] == '#!':
+    executable_filepath = line[2:].strip()
+    argument_list.append(executable_filepath)
+  argument_list.extend(d['call_list'])
+  argument_list.extend(sys.argv[1:])
+  argument_list.append(env)
+  os.execle(executable_filepath, *argument_list)
