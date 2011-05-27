0526e075307b75605532bd62ce1076c00385a2c4
Łukasz Nowak <luke@nexedi.com>
2011-05-27 15:05:27 +0200

    SlapOS related code moved out.
    SlapOS code has been moved to: http://git.erp5.org/gitweb/slapos.git

--- slapos/hook/pdftk-hooks.py
+++
@@ -1,4 +0,0 @@
-import os
-def pre_make_hook(options, buildout):
-  # change to pdftk directory, where built process shall be done
-  os.chdir('pdftk')
--- slapos/recipe/erp5.recipe.testnode/README.txt
+++
@@ -1,1 +0,0 @@
-The erp5.recipe.tesnode aims to install generic erp5 testnode.
