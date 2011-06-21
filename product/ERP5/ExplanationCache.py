1a837cd68a70754bf4588b948b37d49cd4ff027e
Leonardo Rochael Almeida <leonardo@nexedi.com>
2011-06-21 17:16:12 +0200

    Look up trade_phase on the simulation movement itself
    Previously Simulaiton Movements did not carry trade_phase, but they do now, and looking up trade_phase on the Trade Model Path forces one TMP per trade_phase

--- product/ERP5/ExplanationCache.py
+++ product/ERP5/ExplanationCache.py
@@ -178,16 +178,14 @@ class ExplanationCache:
     def getParentSimulationMovementValueList(obj, movement_list, trade_phase):
       parent = obj.getParentValue()
       while parent.getPortalType() == "Simulation Movement":
-        if parent.getCausalityValue(portal_type="Trade Model Path"
-            ).isMemberOf(trade_phase, strict_membership=1):
+        if parent.isMemberOf(trade_phase, strict_membership=1):
           movement_list.append(parent)
         parent = parent.getParentValue().getParentValue()
 
     def getChildSimulationMovementValueList(obj, movement_list, trade_phase):
       for child in obj.objectValues():
         if (child.getPortalType() == "Simulation Movement" and
-            child.getCausalityValue(portal_type="Trade Model Path"
-              ).isMemberOf(trade_phase, strict_membership=1)):
+            child.isMemberOf(trade_phase, strict_membership=1)):
           movement_list.append(child)
         getChildSimulationMovementValueList(child, movement_list, trade_phase)
 
