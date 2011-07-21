db01e2463630160def42e9d1f65fac8299841893
Łukasz Nowak <luke@nexedi.com>
2011-07-21 16:18:10 +0200

    Initial distribution creation.

---
+++ README.erp5.util.txt
@@ -0,0 +1,7 @@
+erp5.util
+=========
+
+Package containing various ERP5 related utilities.
+
+Modules documentation
+=====================
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import glob
+import os
+
+version = '0.1-dev'
+name = 'erp5.util'
+long_description = open("README.erp5.util.txt").read() + "\n"
+
+for f in sorted(glob.glob(os.path.join('erp5', 'util', 'README.*.txt'))):
+  long_description += '\n' + open(f).read() + '\n'
+
+long_description += open("CHANGES.erp5.util.txt").read() + "\n"
+
+# silence setuptools, create README.txt
+if not os.path.exists('README.txt'):
+  os.symlink('README.erp5.util.txt', 'README.txt')
+
+setup(name=name,
+      version=version,
+      description="ERP5 related utilities.",
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: POSIX",
+          "Programming Language :: Python",
+          "Topic :: Utilities",
+        ],
+      url='http://www.erp5.org',
+      author='The ERP5 Development Team',
+      author_email='erp5-dev@erp5.org',
+      keywords='erp5 utilities',
+      license='GPLv3',
+      namespace_packages=['erp5', 'erp5.util'],
+      install_requires=[
+        'setuptools', # namespaces
+      ],
+      zip_safe=True,
+      packages=find_packages(),
+      include_package_data=True,
+      entry_points={
+        'console_scripts': [
+          'testnode = erp5.util.testnode:main',
+        ],
+      }
+    )
+
+# cleanup garbage
+if os.path.islink('README.txt'):
+  if os.readlink('README.txt') == 'README.erp5.util.txt':
+    os.unlink('README.txt')
