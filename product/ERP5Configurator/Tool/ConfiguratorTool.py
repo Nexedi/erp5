61b78ab14429711971ddeb16543787f1ae6f2029
Rafael Monnerat <rafael@nexedi.com>
2011-07-21 01:41:56 -0300

    Tinnies code style changes.

--- product/ERP5Configurator/Tool/ConfiguratorTool.py
+++ product/ERP5Configurator/Tool/ConfiguratorTool.py
@@ -216,12 +216,11 @@ class ConfiguratorTool(BaseTool):
       return self.view()
     response = self._next(business_configuration=bc, kw=kw)
     ## Parse server response
-    command = response["command"]
-    if command == "show":
+    if response["command"] == "show":
       return self.ConfiguratorTool_dialogForm(previous=response['previous'],
                                         form_html=response["data"],
-                                        next = response['next'])
-    elif command == "install":
+                                        next=response['next'])
+    elif response["command"] == "install":
       return self.startInstallation(bc, REQUEST=REQUEST)
 
   def _next(self, business_configuration, kw):
@@ -354,7 +353,7 @@ class ConfiguratorTool(BaseTool):
 
     if html is None:
       ## we have no more forms proceed to build
-      response.update(command = "install", data = None)
+      response.update(command="install", data=None)
     else:
       ## we have more forms
       next_state = self.restrictedTraverse(business_configuration.getNextTransition()\
@@ -371,12 +370,12 @@ class ConfiguratorTool(BaseTool):
         he can no longer continue. """
     if reason == 'no_available_transitions':
       form_html = self.BusinessConfiguration_viewStopForm()
-      response.update(command = "show", next = None, \
-                      previous = None, data = form_html)
+      response.update(command="show", next=None, \
+                      previous=None, data=form_html)
     elif reason == 'authentification_failure':
       form_html = self.BusinessConfiguration_viewUnauthenticatedForm()
-      response.update(command = "show", data = form_html,
-                      next = None, previous = None,)
+      response.update(command="show", data=form_html,
+                      next=None, previous=None,)
 
     return response
 
@@ -403,7 +402,7 @@ class ConfiguratorTool(BaseTool):
     ## is client is not allowed access ?
     if business_configuration is None:
       form_html = self.BusinessConfiguration_viewUnauthenticatedForm()
-      return self.ConfiguratorTool_dialogForm(form_html = form_html)
+      return self.ConfiguratorTool_dialogForm(form_html=form_html)
     ## client can not go further his business configuration is already built
     if business_configuration.isEndConfigurationState():
       form_html = self.BusinessConfiguration_viewStopForm()
@@ -417,9 +416,9 @@ class ConfiguratorTool(BaseTool):
         business_configuration.getNextTransition().getDestination())
 
     response['data'] = self.Base_mainConfiguratorTemplate(
-        form_html = form_html,
-        current_state = next_state,
-        business_configuration = business_configuration)
+        form_html=form_html,
+        current_state=next_state,
+        business_configuration=business_configuration)
     return response
 
   security.declarePublic(Permissions.AccessContentsInformation,
@@ -466,7 +465,7 @@ class ConfiguratorTool(BaseTool):
     REQUEST.set('active_process_id', active_process.getId())
     request_restore_dict = {'__ac_key': REQUEST.get('__ac_key',
                                                        None), }
-    self.activate(active_process=active_process, tag = 'initialERP5Setup'
+    self.activate(active_process=active_process, tag='initialERP5Setup'
         ).initialERP5Setup(business_configuration.getRelativeUrl(), request_restore_dict)
     return self.ConfiguratorTool_viewInstallationStatus(REQUEST)
 
