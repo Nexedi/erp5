aad600d027b62b68c4a0908b993e009cdbc102d5
Łukasz Nowak <luke@nexedi.com>
2011-07-22 16:17:25 +0200

    Back to the future (fix dates in copyrights).

--- erp5/util/testnode/__init__.py
+++ erp5/util/testnode/__init__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2010 Nexedi SA and Contributors. All Rights Reserved.
+# Copyright (c) 2011 Nexedi SA and Contributors. All Rights Reserved.
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsibility of assessing all potential
