08990c2dcb061aadca3784c5a0ab1a571e207757
Lucas Carvalho <lucas@nexedi.com>
2011-07-26 17:26:26 +0200

    Typo, fixing wrong import.

--- product/ERP5/__init__.py
+++ product/ERP5/__init__.py
@@ -51,7 +51,7 @@ from Tool import CategoryTool, SimulationTool, RuleTool, IdTool, TemplateTool,\
                  GadgetTool, ContributionRegistryTool, IntrospectionTool,\
                  AcknowledgementTool, SolverTool, SolverProcessTool,\
                  ConversionTool, RoundingTool, UrlRegistryTool,\
-                 CertificateAuthorityTool.CertificateAuthorityTool
+                 CertificateAuthorityTool
 import ERP5Site
 from Document import PythonScript
 object_classes = ( ERP5Site.ERP5Site,
