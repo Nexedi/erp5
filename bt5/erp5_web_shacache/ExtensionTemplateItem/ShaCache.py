1004c3ec1e864484e06c260f60ec9c41107c0365
Lucas Carvalho <lucas@nexedi.com>
2011-07-28 12:08:29 +0200

    Performance improvement: download files does not kill Zope.
    Shacache instance is now able to provide big file without getting killed
    by the OS, due a memory leak.
    
    Desactivating the previous object is an approach to send the data without
    overloading the RAM Memory.

--- bt5/erp5_web_shacache/ExtensionTemplateItem/ShaCache.py
+++ bt5/erp5_web_shacache/ExtensionTemplateItem/ShaCache.py
@@ -107,3 +107,43 @@ def WebSection_putFactory(self, name, typ, body):
             .WebSite_publishDocumentByActivity()
 
   return document
+
+
+def File_viewAsWeb(self):
+  """
+    Make possible to send the file data to the client without consume the
+    RAM memory.
+  """
+  RESPONSE = self.REQUEST.RESPONSE
+  RESPONSE.setHeader('Content-Type', self.getContentType())
+  RESPONSE.setHeader('Content-Length', self.getSize())
+  RESPONSE.setHeader('Accept-Ranges', 'bytes')
+
+  #  If the file is not a Pdata, we should return the data directly.
+  data=self.data
+  if isinstance(data, str):
+    RESPONSE.setBase(None)
+    return data
+
+  # Once the object is PData type,
+  # we must iterate and send chunk by chunk.
+  while data is not None:
+    # Break!! If the channel is closed.
+    # if the client side stops the connection brutally
+    # the server side should stop.
+    if RESPONSE.stdout._channel.closed:
+      break
+    # Send data to the client.
+    RESPONSE.write(data.data)
+
+    # Load next object.
+    next_data = data.next
+
+    # Desactivate the object, make sure that this object content
+    # is not loaded in RAM memory.
+    data._p_deactivate()
+
+    # Set the new data.
+    data=next_data
+
+  return ''
