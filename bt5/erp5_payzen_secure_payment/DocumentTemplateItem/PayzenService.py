b1f80057f4a1aa5ec340a2b51b0241c98ee4c8a4
Łukasz Nowak <luke@nexedi.com>
2011-10-06 17:32:02 +0200

    Revert "Allow to manipulate freely sent dict."
    This reverts commit e1356f85fa2842556aca3a34dc957a0fcda4b4cc.

--- bt5/erp5_payzen_secure_payment/DocumentTemplateItem/PayzenService.py
+++ bt5/erp5_payzen_secure_payment/DocumentTemplateItem/PayzenService.py
@@ -47,36 +47,35 @@ class PayzenService(XMLObject):
     return self._getTypeBasedMethod("abortPayment")(**kw)
 
   # proposed methods
-  def getFormString(self, document, content_dict=None):
+  def getFormString(self, document, **kw):
     """Returns unterminated form for current document
 
     The responsiblity of the caller is to finish the form."""
     self.Base_checkConsistency()
-    if content_dict is None:
-      content_dict = dict()
-    content_dict['vads_action_mode'] = self.getPayzenVadsActionMode()
-    content_dict['vads_amount'] = int(document.getTotalPrice() * 100)
+    content_kw = dict()
+    content_kw['vads_action_mode'] = self.getPayzenVadsActionMode()
+    content_kw['vads_amount'] = int(document.getTotalPrice() * 100)
     integration_tool = self.restrictedTraverse(self.getIntegrationSite())
-    content_dict['vads_currency'] = integration_tool.getMappingFromCategory(
+    content_kw['vads_currency'] = integration_tool.getMappingFromCategory(
       'resource/currency_module/%s' % document.getPriceCurrencyReference()
       ).split('/')[-1]
-    content_dict['vads_ctx_mode'] = self.getPayzenVadsCtxMode()
-    content_dict['vads_page_action'] = self.getPayzenVadsPageAction()
-    content_dict['vads_payment_config'] = document\
+    content_kw['vads_ctx_mode'] = self.getPayzenVadsCtxMode()
+    content_kw['vads_page_action'] = self.getPayzenVadsPageAction()
+    content_kw['vads_payment_config'] = document\
       .Base_getPayzenServicePaymentConfig()
-    content_dict['vads_site_id'] = self.getServiceUsername()
+    content_kw['vads_site_id'] = self.getServiceUsername()
     # date as YYYYMMDDHHMMSS
-    content_dict['vads_trans_date'] = document.getStartDate().strftime(
+    content_kw['vads_trans_date'] = document.getStartDate().strftime(
       '%Y%m%d%H%M%S')
-    content_dict['vads_trans_id'] = document.Base_getPayzenTransId()
-    content_dict['vads_version'] = self.getPayzenVadsVersion()
+    content_kw['vads_trans_id'] = document.Base_getPayzenTransId()
+    content_kw['vads_version'] = self.getPayzenVadsVersion()
     # all data are completed, now it is time to create signature
-    sorted_keys = content_dict.keys()
+    sorted_keys = content_kw.keys()
     sorted_keys.sort()
     signature = ''
     form = '<FORM METHOD="POST" ACTION="%s">\n' % self.getLinkUrlString()
     for k in sorted_keys:
-      v = str(content_dict[k])
+      v = str(content_kw[k])
       signature += v + '+'
       form += '<INPUT TYPE="HIDDEN" NAME="%s" VALUE="%s">\n' % (k, v)
     signature += self.getServicePassword()
