Nexedi/erp5

commit 77d94dd5ba999f510c27c0cf3edb9eb99aa5c757

Speed up RuleTool.searchRuleList by quickly filtering out rules

product/ERP5/Tool/RuleTool.py
@@ -30,8 +30,6 @@ from Products.ERP5Type.Tool.BaseTool import BaseTool
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type.Globals import InitializeClass, DTMLFile
 from Products.ERP5Type import Permissions
-from Products.CMFCore.utils import getToolByName
-
 from Products.ERP5 import _dtmldir
 
 class RuleTool(BaseTool):
@@ -96,13 +94,31 @@ class RuleTool(BaseTool):
       - the rule must be of a known portal type
       - Predicate criterions can be used (like start_date_range_min)
       """
-      domain_tool = getToolByName(self.getPortalObject(), "portal_domains")
+      portal = self.getPortalObject()
+      # Most rules are only configured through their test_method_id,
+      # so filter out them quickly before invoking slow searchPredicateList.
+      rule_uid_list = []
+      for rule in portal.portal_catalog.unrestrictedSearchResults(
+          portal_type=portal.getPortalRuleTypeList(),
+          validation_state="validated", **kw): #XXX "validated" is hardcoded
+        rule = rule.getObject()
+        try:
+          for test_method_id in rule.getTestMethodIdList():
+            if test_method_id == 'Rule_testFalse' or \
+               not getattr(movement, test_method_id)(rule):
+              break
+          else:
+            rule_uid_list.append(rule.getUid())
+        except Exception:
+          # Maybe the script is old (= it takes no argument). Or we should not
+          # have called it (= rule would have been excluded before, depending
+          # on other criterions). Or there may be a bug.
+          # We don't know why it failed so let searchPredicateList do the work.
+          rule_uid_list.append(rule.getUid())
 
-      rule_list = domain_tool.searchPredicateList(context=movement,
-          tested_base_category_list=tested_base_category_list,
-          portal_type=self.getPortalRuleTypeList(),
-          validation_state="validated", **kw) #XXX "validated" is hardcoded
+      return rule_uid_list and portal.portal_domains.searchPredicateList(
+          context=movement, uid=rule_uid_list,
+          tested_base_category_list=tested_base_category_list)
 
-      return rule_list
 
 InitializeClass(RuleTool)

commit c13e7a787572eeea7f1fb41fee1ef1130573866c

Fix order of rules returned by RuleTool.searchRuleList
This fixes a regression introduced in 77d94dd5ba999f510c27c0cf3edb9eb99aa5c757
("Speed up RuleTool.searchRuleList by quickly filtering out rules"),
causing TestRule.test_09_expandTwoRules to fail.

product/ERP5/Tool/RuleTool.py
@@ -100,7 +100,7 @@ class RuleTool(BaseTool):
       rule_uid_list = []
       for rule in portal.portal_catalog.unrestrictedSearchResults(
           portal_type=portal.getPortalRuleTypeList(),
-          validation_state="validated", **kw): #XXX "validated" is hardcoded
+          validation_state="validated"): #XXX "validated" is hardcoded
         rule = rule.getObject()
         try:
           for test_method_id in rule.getTestMethodIdList():
@@ -118,7 +118,7 @@ class RuleTool(BaseTool):
 
       return rule_uid_list and portal.portal_domains._searchPredicateList(
           context=movement, uid=rule_uid_list,
-          tested_base_category_list=tested_base_category_list)
+          tested_base_category_list=tested_base_category_list, **kw)
 
 
 InitializeClass(RuleTool)