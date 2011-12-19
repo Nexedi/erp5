36d46997f20566561e7a3932074ef3db9b80ab2e
Ivan Tyagov <ivan@nexedi.com>
2011-12-19 16:53:16 +0200

    Add needed bt5.

--- product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
+++ product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
@@ -55,6 +55,7 @@ class TestZeleniumStandaloneUserTutorial(ERP5TypeFunctionalTestCase):
             'erp5_web', 'erp5_dms', 'erp5_credential',
             'erp5_rss_style', 'erp5_discussion',
             'erp5_l10n_fr', 'erp5_crm',
+            'erp5_accounting_ui_test',
             'erp5_user_tutorial_ui_test',
            )
 
