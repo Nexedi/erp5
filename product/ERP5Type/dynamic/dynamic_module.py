9dd11e5e83589ac2f31d5179ce90816b0f9caabf
Julien Muchembled <jm@nexedi.com>
2012-01-20 10:26:22 +0100

    Fix race condition causing PicklingError when there are several Zope threads

--- product/ERP5Type/dynamic/dynamic_module.py
+++ product/ERP5Type/dynamic/dynamic_module.py
@@ -1,5 +1,6 @@
 from types import ModuleType
 import sys
+import threading
 
 class DynamicModule(ModuleType):
   """This module may generate new objects at runtime."""
@@ -11,23 +12,27 @@ class DynamicModule(ModuleType):
   def __init__(self, name, factory, doc=None):
     super(DynamicModule, self).__init__(name, doc=doc)
     self._factory = factory
+    self._lock = threading.Lock()
 
   def __getattr__(self, name):
     if name[:2] == '__':
       raise AttributeError('%r module has no attribute %r'
                            % (self.__name__, name))
-    obj = self._factory(name)
-    # _factory can return an instance, a constant, or a class
-    if isinstance(obj, type):
-      # if it's a class we want to set __module__
-      obj.__module__ = self.__name__
-    elif isinstance(obj, ModuleType):
-      # if it's a module we want to set the name according to the
-      # module it's being added to
-      obj.__name__ = "%s.%s" % (self.__name__, name)
-
-    setattr(self, name, obj)
-    return obj
+    with self._lock:
+      try:
+        return super(DynamicModule, self).__getattribute__(name)
+      except AttributeError:
+        obj = self._factory(name)
+        # _factory can return an instance, a constant, or a class
+        if isinstance(obj, type):
+          # if it's a class we want to set __module__
+          obj.__module__ = self.__name__
+        elif isinstance(obj, ModuleType):
+          # if it's a module we want to set the name according to the
+          # module it's being added to
+          obj.__name__ = "%s.%s" % (self.__name__, name)
+        setattr(self, name, obj)
+        return obj
 
 def registerDynamicModule(name, factory):
   d = DynamicModule(name, factory)
