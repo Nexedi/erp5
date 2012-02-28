a53a143336d27ca637b88bb432d3d98fe25a2ac8
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2012-02-28 19:44:33 +0900

    Fix typo.

--- product/ERP5Type/tests/SecurityTestCase.py
+++ product/ERP5Type/tests/SecurityTestCase.py
@@ -184,10 +184,10 @@ class SecurityTestCase(ERP5TypeTestCase):
     try:
       self._loginAsUser(username)
       user = getSecurityManager().getUser()
-      valid_transistion_list =[ai['id'] for ai in
-                            self.workflow_tool.listActions(object=document) if
-                            ai['category'] == 'workflow']
-      if transition in valid_transistion_list:
+      valid_transition_list =[ai['id'] for ai in
+                              self.workflow_tool.listActions(object=document) if
+                              ai['category'] == 'workflow']
+      if transition in valid_transition_list:
         self.fail('User %s can pass %s transition on %s %s. Roles: [%s]' % (
                   username, transition, document.getPortalTypeName(), document,
                   ", ".join(user.getRolesInContext(document))))
@@ -201,10 +201,10 @@ class SecurityTestCase(ERP5TypeTestCase):
     try:
       self._loginAsUser(username)
       user = getSecurityManager().getUser()
-      valid_transistion_list =[ai['id'] for ai in
-                            self.workflow_tool.listActions(object=document) if
-                            ai['category'] == 'workflow']
-      if transition not in valid_transistion_list:
+      valid_transition_list =[ai['id'] for ai in
+                              self.workflow_tool.listActions(object=document) if
+                              ai['category'] == 'workflow']
+      if transition not in valid_transition_list:
         # Build a comprehensive error message
         workflow_states_description = []
         workflow_transitions_description = []
