c7722d50a516a3d066bbae9437be29f1bc53aea9
Rafael Monnerat <rafael@nexedi.com>
2012-04-04 00:47:32 -0300

    Fix Typo mistake on BT5 name

--- product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
+++ product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
@@ -51,7 +51,7 @@ class TestZeleniumStandaloneUserTutorial(ERP5TypeFunctionalTestCase):
             'erp5_accounting', 'erp5_invoicing',
             'erp5_simplified_invoicing', 'erp5_project',
             'erp5_simulation',
-            'erp5_configurator_standard_trade_solver',
+            'erp5_configurator_standard_solver',
             'erp5_configurator_standard_trade_template',
             'erp5_configurator_standard_accounting_template',
             'erp5_configurator_standard_invoicing_template',
