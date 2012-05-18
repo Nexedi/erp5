97dc0a2e83377d73f4bcf6683a5da96e48e2a836
Łukasz Nowak <luke@nexedi.com>
2012-05-18 14:15:35 +0200

    Simplify.
    Use volatile cache to store tokens and one key per site.

--- product/ERP5Security/ERP5BearerExtractionPlugin.py
+++ product/ERP5Security/ERP5BearerExtractionPlugin.py
@@ -35,24 +35,18 @@ from Products.PluggableAuthService.utils import classImplements
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 from Products.ERP5Security.ERP5UserManager import SUPER_USER
 from Products.PluggableAuthService.PluggableAuthService import DumbHTTPExtractor
-from Products.PluggableAuthService.permissions import ManageUsers
 from AccessControl.SecurityManagement import getSecurityManager,\
     setSecurityManager, newSecurityManager
-from DateTime import DateTime
-from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
-from zLOG import LOG, ERROR
 
 #Form for new plugin in ZMI
 manage_addERP5BearerExtractionPluginForm = PageTemplateFile(
   'www/ERP5Security_addERP5BearerExtractionPlugin', globals(),
   __name__='manage_addERP5BearerExtractionPluginForm')
 
-def addERP5BearerExtractionPlugin(dispatcher, id, token_portal_type,
-  token_validation_method, title=None, REQUEST=None):
+def addERP5BearerExtractionPlugin(dispatcher, id, title=None, REQUEST=None):
   """ Add a ERP5BearerExtractionPlugin to a Pluggable Auth Service. """
 
-  plugin = ERP5BearerExtractionPlugin(id, token_portal_type,
-    token_validation_method, title)
+  plugin = ERP5BearerExtractionPlugin(id, title)
   dispatcher._setObject(plugin.getId(), plugin)
 
   if REQUEST is not None:
@@ -69,8 +63,6 @@ class ERP5BearerExtractionPlugin(BasePlugin):
 
   meta_type = "ERP5 Bearer Extraction Plugin"
   security = ClassSecurityInfo()
-  token_portal_type = ''
-  token_validation_method = ''
 
   manage_options = (({'label': 'Edit',
                       'action': 'manage_editERP5BearerExtractionPluginForm',},
@@ -78,25 +70,10 @@ class ERP5BearerExtractionPlugin(BasePlugin):
                     + BasePlugin.manage_options[:]
                     )
 
-  _properties = (({'id':'token_portal_type',
-                   'type':'string',
-                   'mode':'w',
-                   'label':'Portal Type with tokens'
-                   },
-                  {'id':'token_validation_method',
-                   'type':'string',
-                   'mode':'w',
-                   'label':'Method to validate found token'
-                   },                  )
-                 + BasePlugin._properties[:]
-                 )
-
-  def __init__(self, id, token_portal_type, token_validation_method, title=None):
+  def __init__(self, id, title=None):
     #Register value
     self._setId(id)
     self.title = title
-    self.token_portal_type = token_portal_type
-    self.token_validation_method = token_validation_method
 
   ####################################
   #ILoginPasswordHostExtractionPlugin#
@@ -124,31 +101,14 @@ class ERP5BearerExtractionPlugin(BasePlugin):
       #   Not implemented as considered as unsecure.
       pass
 
-    if token is not None and self.token_portal_type \
-        and self.token_validation_method:
+    if token is not None:
       sm = getSecurityManager()
       if sm.getUser().getId() != SUPER_USER:
         newSecurityManager(self, self.getUser(SUPER_USER))
       try:
-        token_document = self.portal_catalog.getResultValue(
-          portal_type=self.token_portal_type,
-          reference=token,
-          query=SimpleQuery(
-            comparison_operator='>=', expiration_date=DateTime()
-          ),
-          validation_state='validated'
-        )
-        if token_document is not None:
-          result = False
-          try:
-            result = getattr(token_document,
-            self.token_validation_method)()
-          except Exception:
-            LOG('BearerExtractionPlugin', ERROR, 'Problem while calling token '
-              'validation method %r on %r:' % (self.token_validation_method,
-              token_document.getPath()), error=True)
-          if result is True:
-            creds['external_login'] = token_document.getDestinationReference()
+        reference = self.Base_extractBearerTokenInformation(token)
+        if reference is not None:
+          creds['external_login'] = reference
       finally:
         setSecurityManager(sm)
       if 'external_login' in  creds:
@@ -166,32 +126,6 @@ class ERP5BearerExtractionPlugin(BasePlugin):
       'www/ERP5Security_editERP5BearerExtractionPlugin',
       globals(),
       __name__='manage_editERP5BearerExtractionPluginForm')
-
-  security.declareProtected(ManageUsers,
-    'manage_editERP5BearerExtractionPlugin')
-  def manage_editERP5BearerExtractionPlugin(self, token_portal_type,
-      token_validation_method, RESPONSE=None):
-    """Edit the object"""
-    error_message = ''
-
-    if token_portal_type == '' or token_portal_type is None or \
-      token_validation_method == '' or token_validation_method is None:
-      error_message += 'Token Portal Type is missing '
-    else:
-      self.token_portal_type = token_portal_type
-      self.token_validation_method = token_validation_method
-
-    #Redirect
-    if RESPONSE is not None:
-      if error_message != '':
-        self.REQUEST.form['manage_tabs_message'] = error_message
-        return self.manage_editERP5BearerExtractionPluginForm(RESPONSE)
-      else:
-        message = "Updated"
-        RESPONSE.redirect('%s/manage_editERP5BearerExtractionPluginForm'
-                          '?manage_tabs_message=%s'
-                          % (self.absolute_url(), message)
-                          )
 
 #List implementation of class
 classImplements( ERP5BearerExtractionPlugin,
