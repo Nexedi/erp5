e2d2dd3e21ba8977e357cecdeb29a114dec5cb2d
Leonardo Rochael Almeida <leonardo@nexedi.com>
2012-06-21 17:16:51 +0200

    Force default-zpublisher-encoding to utf-8

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -71,6 +71,7 @@ from Products.ERP5Type.patches import ExternalMethod
 from Products.ERP5Type.patches import User
 from Products.ERP5Type.patches import zopecontenttype
 from Products.ERP5Type.patches import OFSImage
+from Products.ERP5Type.patches import default_zpublisher_encoding
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
