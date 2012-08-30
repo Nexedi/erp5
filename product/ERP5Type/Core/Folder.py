695e978267a8a5f011f0e3662bcb379d002ba62f
Vincent Pelletier <vincent@nexedi.com>
2012-08-30 13:36:18 +0200

    Initialise underlying folder if not initialised on _setObject call.
    Fixes Zope 2.13 .

--- product/ERP5Type/Core/Folder.py
+++ product/ERP5Type/Core/Folder.py
@@ -974,8 +974,12 @@ class Folder(CopyContainer, CMFBTreeFolder, CMFHBTreeFolder, Base, FolderMixIn):
 
   def _setObject(self, *args, **kw):
     if self._folder_handler == HBTREE_HANDLER:
+      if self._htree is None:
+        HBTreeFolder2Base.__init__(self, self.id)
       return CMFHBTreeFolder._setObject(self, *args, **kw)
     else:
+      if self._tree is None:
+        BTreeFolder2Base.__init__(self, self.id)
       return CMFBTreeFolder._setObject(self, *args, **kw)
 
   def get(self, id, default=None):
