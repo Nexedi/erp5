edc48778d08148adfe6449f38cd32bfa17fae058
Ivan Tyagov <ivan@nexedi.com>
2012-10-11 14:35:49 +0200

    Allow pre conversion to happen on any ERP5 Web Page's content  which include relative images (through <img> tags).

---
+++ bt5/erp5_dms_conversion_catalog/ExtensionTemplateItem/DocumentConversion.py
@@ -0,0 +1,11 @@
+import re
+
+def Base_extractImageUrlList(self, text_content=None):
+  """
+    Extract list of image URLS used in a Text document (i.e. Web Page)
+  """
+  if text_content is None:
+    text_content = self.getTextContent()
+  if text_content is not None:
+    return re.findall('src=[\"\'](.[^\"\']+)[\"\']', text_content, re.I) 
+  return []
