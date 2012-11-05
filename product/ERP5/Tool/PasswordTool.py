Nexedi/erp5

commit ecb09a0da25b33b786f0c0fcba7882c71ff7fd67

preserve notification_message's category and language into a generated event document.

product/ERP5/Tool/PasswordTool.py
@@ -198,9 +198,14 @@ class PasswordTool(BaseTool):
       else:
         message = notification_message.asText(substitution_method_parameter_dict=message_dict)
 
+    event_keyword_argument_dict={
+      'resource':notification_message.getSpecialise(),
+      'language':notification_message.getLanguage(),
+    }
     self.getPortalObject().portal_notifications.sendMessage(sender=sender, recipient=[user,],
                                                             subject=subject, message=message,
-                                                            store_as_event=store_as_event)
+                                                            store_as_event=store_as_event,
+                                                            event_keyword_argument_dict=event_keyword_argument_dict)
     if REQUEST is not None:
       msg = translateString("An email has been sent to you.")
       parameter = urlencode(dict(portal_status_message=msg))

commit 441688843b14df42597ff4bb3cbcd69b90b0c586

preserve notification_message's category and language only if notification_message is found. this commit fixes a regression in ecb09a0da25b33b786f0c0fcba7882c71ff7fd67.

product/ERP5/Tool/PasswordTool.py
@@ -191,17 +191,18 @@ class PasswordTool(BaseTool):
                 "Thank you",
                 mapping=message_dict)
       message = message.translate()
+      event_keyword_argument_dict={}
     else:
       subject = notification_message.getTitle()
       if notification_message.getContentType() == "text/html":
         message = notification_message.asEntireHTML(substitution_method_parameter_dict=message_dict)
       else:
         message = notification_message.asText(substitution_method_parameter_dict=message_dict)
+      event_keyword_argument_dict={
+        'resource':notification_message.getSpecialise(),
+        'language':notification_message.getLanguage(),
+      }
 
-    event_keyword_argument_dict={
-      'resource':notification_message.getSpecialise(),
-      'language':notification_message.getLanguage(),
-    }
     self.getPortalObject().portal_notifications.sendMessage(sender=sender, recipient=[user,],
                                                             subject=subject, message=message,
                                                             store_as_event=store_as_event,