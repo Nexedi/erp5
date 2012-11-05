9e91eaa703083a32971b0a2d0675bfc0c58de546
Sebastien Robin <seb@nexedi.com>
2012-11-05 12:08:47 +0100

    erp5.util: increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.4.13'
+version = '0.4.14'
 name = 'erp5.util'
 long_description = open("README.erp5.util.txt").read() + "\n"
 
