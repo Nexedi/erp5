f523bb61520235892cc11e58811f96d0672fe7ef
Jérome Perrin <jerome@nexedi.com>
2012-12-20 13:18:51 +0100

    Configure a minimal social title category

--- bt5/erp5_configurator_standard/TestTemplateItem/testStandardConfigurationWorkflow.py
+++ bt5/erp5_configurator_standard/TestTemplateItem/testStandardConfigurationWorkflow.py
@@ -83,6 +83,7 @@ class StandardConfigurationMixin(TestLiveConfiguratorWorkflowMixin):
       stepCopyAndPasteOrganisation
       stepEntityWorkflow
       stepViewCreatedPersons
+      stepCheckSocialTitleCategory
       stepViewCreatedOrganisations
       stepViewCreatedAssignemnts
       stepAddAccoutingPeriod
@@ -224,6 +225,12 @@ class StandardConfigurationMixin(TestLiveConfiguratorWorkflowMixin):
       assignment_list = person.contentValues(portal_type='Assignment')
       self.assertEquals(len(assignment_list), 1)
       self.assertEquals('my_group', assignment_list[0].getGroup())
+
+  def stepCheckSocialTitleCategory(self, sequence=None,sequence_list=None, **kw):
+    """Check that the social title category is configured.
+    """
+    self.assertNotEquals(0, 
+       len(self.portal.portal_categories.social_title.contentValues()))
 
   def stepCheckValidOrganisationList(self, sequence=None, sequence_list=None, **kw):
     """
