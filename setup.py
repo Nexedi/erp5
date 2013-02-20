8cb19830bd8e977db48153d97d37555c1c5f18de
Sebastien Robin <seb@nexedi.com>
2013-02-20 12:07:19 +0100

    release erp5.util 0.4.30

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.4.29'
+version = '0.4.30'
 name = 'erp5.util'
 long_description = open("README.erp5.util.txt").read() + "\n"
 
