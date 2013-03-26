5dee36160179690302896ad58de44f2a935ac996
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2013-03-26 09:16:33 +0100

    if ignore_layout is specified, do not change skin at all.
    otherwise the result is unpredictable.

--- product/ERP5/Document/WebSite.py
+++ product/ERP5/Document/WebSite.py
@@ -110,10 +110,11 @@ class WebSiteTraversalHook(Persistent):
     self._v_request = request
     request.physicalPathToVirtualPath = self._physicalPathToVirtualPath
 
-    # If a skin selection is defined in this web site, change the skin now.
-    skin_selection_name = container.getSkinSelectionName()
-    if skin_selection_name and request.get('portal_skin', None) is None:
-      container.getPortalObject().changeSkin(skin_selection_name)
+    if not request.get('ignore_layout', None):
+      # If a skin selection is defined in this web site, change the skin now.
+      skin_selection_name = container.getSkinSelectionName()
+      if skin_selection_name and request.get('portal_skin', None) is None:
+        container.getPortalObject().changeSkin(skin_selection_name)
 
     # Set default language if any
     default_language = container.getDefaultAvailableLanguage()
