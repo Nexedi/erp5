970d1d191b8659ff173828fe1987b380e1e0c77d
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2013-04-29 13:59:27 +0200

    enable the site preference when setting up the site.
    otherwise installation fails if a business template contains OOo document in PathTemplateItem.

--- product/ERP5Type/tests/ERP5TypeTestCase.py
+++ product/ERP5Type/tests/ERP5TypeTestCase.py
@@ -888,6 +888,8 @@ class ERP5TypeCommandLineTestCase(ERP5TypeTestCaseMixin):
                         self.getDefaultSitePreferenceId()]
       preference._setPreferredOoodocServerAddress(conversion_dict['hostname'])
       preference._setPreferredOoodocServerPortNumber(conversion_dict['port'])
+      if self.portal.portal_workflow.isTransitionPossible(preference, 'enable'):
+        preference.enable()
 
     def _updateMemcachedConfiguration(self):
       """Update default memcached plugin configuration
