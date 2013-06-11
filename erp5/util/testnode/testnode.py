400e65ac7228287dfe575721791bd02d617305b4
Benjamin Blanc <benjamin.blanc@tiolive.com>
2013-06-11 14:39:13 +0200

    Test

--- erp5/util/testnode/testnode.py
+++ erp5/util/testnode/testnode.py
@@ -334,7 +334,7 @@ branch = %(branch)s
           portal = taskdistribution.TaskDistributionTool(portal_url, logger=DummyLogger(log))
           test_suite_portal = taskdistribution.TaskDistributor(portal_url, logger=DummyLogger(log))
           print portal_url
-          print test_suite_porta.getTitle()
+          print test_suite_portal.getTitle()
           print test_suite_portal.getTestType()
           
           
