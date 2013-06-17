dfa35f3026ef595ef7eb0ca67cafcbee49aeb5ed
Benjamin Blanc <benjamin.blanc@tiolive.com>
2013-06-17 09:37:32 +0200

    Commit before renaming some variables

--- erp5/util/testnode/testnode.py
+++ erp5/util/testnode/testnode.py
@@ -299,12 +299,6 @@ branch = %(branch)s
     test_node_slapos.edit(working_directory=self.config['slapos_directory'])
     ## /BLOCK OK
 
-    time.sleep(5)
-    print config['link_to_testsuite_directory']
-    print config['link_to_testsuite_directory']
-    print config['link_to_testsuite_directory']
-    print config['link_to_testsuite_directory']
-
     try:
       while True:
         try:
