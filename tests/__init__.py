e44ceb5ceddca5ccb77259cce8aa157ccb17d765
Benjamin Blanc <benjamin.blanc@tiolive.com>
2013-09-10 09:45:38 +0200

    tests: ERP5_scalability: change users number

--- tests/__init__.py
+++ tests/__init__.py
@@ -137,7 +137,7 @@ class ERP5_scalability(_ERP5):
     return 'erp5/util/benchmark/examples/scalabilityUsers'
 
   def getUserNumber(self, test_number):
-    return [45, 120, 160, 200, 230][test_number]
+    return [45, 135, 170, 220, 250][test_number]
 
   # Test duration in seconds
   def getTestDuration(self, test_number):
