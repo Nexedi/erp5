6ded20b9822898cea784335398f7f9e17fa54222
Sebastien Robin <seb@nexedi.com>
2013-09-20 11:04:32 +0200

    erp5-util-benchmark: cleanup useless spaces

--- erp5/util/benchmark/examples/createSaleOrder.py
+++ erp5/util/benchmark/examples/createSaleOrder.py
@@ -17,7 +17,7 @@ MAX_PRODUCT = 5
 def addOrderLine(browser, my_title, result) :
   """
   Add an order line to the sale order
-  
+
   @param browser: Browser
   @type browser: Browser
   @param my_title: The sale order title
@@ -26,12 +26,12 @@ def addOrderLine(browser, my_title, result) :
   # Create a new Sale Order Line
   browser.mainForm.submitSelectAction(label="Add Sale Order Line")
   assert browser.getTransitionMessage() == 'Object created.'
-  
+
   # Fill the quantity randomly
   browser.mainForm.getControl(name='field_my_quantity').value = str(random.randint(1, 20))
   result('SetRelationProduct', browser.mainForm.submitSave(
       sleep=(TMIN_SLEEP, TMAX_SLEEP)))
-  
+
   ## Choose the product randomly
   fillRelatedObjects(browser, result,
       "portal_selections/viewSearchRelatedDocumentDialog0:method", 1,
@@ -54,20 +54,20 @@ def createSaleOrder(result, browser):
   """
   # Open ERP5 homepage and log in
   result('Open', browser.open())
-  
+
   # Log in unless already logged in by a previous test suite
   result('Login', browser.mainForm.submitLogin(
                             sleep=(TMIN_SLEEP, TMAX_SLEEP)))
 
   # Go to sale Order module
-  result('GotoModule', 
+  result('GotoModule',
       browser.mainForm.submitSelectModule(value='/sale_order_module',
                                        sleep=(TMIN_SLEEP, TMAX_SLEEP)))
-    
+
   # Create a new sale order and record the time elapsed in seconds
-  result('Create', 
+  result('Create',
     browser.mainForm.submitNew(sleep=(TMIN_SLEEP, TMAX_SLEEP)))
-    
+
   # Check whether it has been successfully created
   assert browser.getTransitionMessage() == 'Object created.'
   my_order_sale_url = browser.url.split("?")[0]
@@ -75,15 +75,15 @@ def createSaleOrder(result, browser):
   # Fill the title
   my_title = PREFIX_TITLE + generateString(6)
   browser.mainForm.getControl(name='field_my_title').value = my_title
-  
+
   # Set some random informations
   my_str = generateString(random.randint(1,100))
   browser.mainForm.getControl(name='field_my_comment').value = my_str
   browser.mainForm.getControl(name='field_my_description').value = my_str
-   
+
   # Select some options randomly
   selectRandomOption(browser, "field_my_order")
-  
+
   # Set dates
   date = datetime.datetime.now()
   browser.mainForm.getControl(name='subfield_field_my_start_date_day').value = str(date.day)
@@ -92,7 +92,7 @@ def createSaleOrder(result, browser):
 
 
   # Submit the changes, record the time elapsed in seconds
-  result('Save', 
+  result('Save',
     browser.mainForm.submitSave(sleep=(TMIN_SLEEP, TMAX_SLEEP)))
 
   # Check whether the changes have been successfully updated
@@ -104,24 +104,24 @@ def createSaleOrder(result, browser):
   # Click on the specified menu
   result('GoToSaleTradeConditionRelations', browser.mainForm.getControl(
       name="portal_selections/viewSearchRelatedDocumentDialog2:method").click())
-  assert browser.getTransitionMessage() == 'Please select one object.'  
+  assert browser.getTransitionMessage() == 'Please select one object.'
   line_number = browser.getListboxPosition(SALE_TRADE_CONDITION_NAME, column_number=2)
   # Check the box corresponding to line_number
   browser.mainForm.getListboxControl(line_number=line_number, column_number=1).click()
-  result('SubmitSaleTradeConditionRelation', 
+  result('SubmitSaleTradeConditionRelation',
       browser.mainForm.submit(name='Base_callDialogMethod:method',
                                         sleep=(TMIN_SLEEP, TMAX_SLEEP)))
-    
+
   # Add Sale order lines
   max_ite = random.randint(1,MAX_PRODUCT)
   for i in range (0, max_ite):
     browser.open(sale_order_url+"/view")
     addOrderLine(browser, my_title, result)
- 
+
   browser.open(my_order_sale_url)
 
   # Validate the Sale Order
-  browser.mainForm.submitSelectWorkflow(value='plan_action')                                      
-  result('Validate', 
+  browser.mainForm.submitSelectWorkflow(value='plan_action')
+  result('Validate',
       browser.mainForm.submitDialogConfirm(sleep=(TMIN_SLEEP, TMAX_SLEEP)))
   assert browser.getTransitionMessage() == 'Status changed.'
