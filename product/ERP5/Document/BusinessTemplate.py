feded98f7ace5c000df11b4392bbf42f4b138660
Jérome Perrin <jerome@nexedi.com>
2013-10-24 11:37:47 +0200

    BusinessTemplate: do not fail when regenerating roles of a path that no longer exist

--- product/ERP5/Document/BusinessTemplate.py
+++ product/ERP5/Document/BusinessTemplate.py
@@ -1489,18 +1489,19 @@ class PathTemplateItem(ObjectTemplateItem):
   def install(self, context, *args, **kw):
     super(PathTemplateItem, self).install(context, *args, **kw)
 
+    # Regenerate local roles for all paths in this business template
     p = context.getPortalObject()
     portal_type_role_list_len_dict = {}
     update_dict = defaultdict(list)
     for path in self._objects:
-      obj = p.unrestrictedTraverse(path)
+      obj = p.unrestrictedTraverse(path, None)
 
       try:
         portal_type = aq_base(obj).getPortalType()
       except Exception, e:
         LOG("BusinessTemplate", WARNING,
-            "Could not update Local Roles as Portal Type for '%s' is not "
-            "available (%s)" % (obj, e))
+            "Could not update Local Roles as Portal Type for '%s' (obj: %s)"
+            " is not available" % (path, obj), error=True)
 
         continue
 
