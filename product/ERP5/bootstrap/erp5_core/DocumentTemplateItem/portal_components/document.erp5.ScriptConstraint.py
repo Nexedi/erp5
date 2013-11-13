91ef6772590f6333cb5fc1297d74b838645d09b2
Gabriel Monnerat <gabriel@tiolive.com>
2013-11-13 16:46:36 +0100

    Add Script Constraint document, view and actions
    Also, Added constraint property sheets to Script Constraint portal type

---
+++ product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.ScriptConstraint.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2013 Nexedi SA and Contributors. All Rights Reserved.
+#                                Gabriel Monnerat <gabriel@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from Products.ERP5Type.mixin.constraint import ConstraintMixin
+
+class ScriptConstraint(ConstraintMixin):
+  """
+    This constraint call one script that should has all logic to check and
+    fix consistency
+  """
+  meta_type = 'ERP5 Script Constraint'
+  portal_type = 'Script Constraint'
+
+  def _createConsistencyMessage(self, object_relative_url, message):
+    # XXX If I put in the right place I have TypeError: 'NoneType' object is not callable
+    from Products.ERP5Type.ConsistencyMessage import ConsistencyMessage
+    return ConsistencyMessage(self,
+      object_relative_url=object_relative_url,
+      message=message)
+
+  def _checkConsistency(self, obj, fixit=0, **kw):
+    """
+      Call script to [check|fix]Consistency
+    """
+    script_id = self.getScriptId()
+    if script_id is None:
+      raise RuntimeError("Script id not defined")
+    method = getattr(obj, script_id, None)
+    if method is None:
+      raise RuntimeError('Script (%s) not found %s' % (script_id, self))
+    object_relative_url = obj.getRelativeUrl()
+    createConsistencyMessage = self._createConsistencyMessage
+    return [createConsistencyMessage(object_relative_url, message) \
+      for message in method(fixit=fixit, **kw)]
