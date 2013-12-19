655deb57677bd73adf40df505ef050f688b6d0a2
Aurélien Calonne <aurel@nexedi.com>
2013-12-19 11:16:52 +0100

    test page are now part of a bt5, so test them locally

--- product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
+++ product/ERP5Type/tests/testFunctionalStandaloneUserTutorial.py
@@ -27,45 +27,28 @@
 #
 ##############################################################################
 
-import os
 import unittest
 
 from Products.ERP5Type.tests.ERP5TypeFunctionalTestCase import \
         ERP5TypeFunctionalTestCase
 
-BASE_REMOTE_SELENIUM_TEST_URL_LIST = [
-     "http://www.erp5.com/user-Howto.Create.Persons/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.Organisations/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Link.Persons.and.Organisations/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.Campaigns/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.Outgoing.Events/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Post.Outgoing.Events/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Manage.Support.Request/TestPage_viewSeleniumTest",
-
-     # Part 2 - PDM, Trade and simulation related tests
-     "http://www.erp5.com/user-Howto.Create.and.Manage.Products/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.Trade.Conditions/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.Sale.Orders/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Manage.Packing.Lists/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Manage.Invoices/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-Howto.Create.A.Payment/TestPage_viewSeleniumTest",
-
-     # Extra Tests, Additional Tests not yet related to any previous tutorial
-     "http://www.erp5.com/user-HowTo.Use.FullText.Search-TESTONLY/TestPage_viewSeleniumTest",
-     "http://www.erp5.com/user-HowTo.Change.Language-TESTONLY/TestPage_viewSeleniumTest"
-]
-
 class TestZeleniumStandaloneUserTutorial(ERP5TypeFunctionalTestCase):
 
   run_only = "user_tutorial_zuite"
-  remote_code_url_list = BASE_REMOTE_SELENIUM_TEST_URL_LIST
+
+  def afterSetUp(self):
+    url_list = []
+    for x in self.portal.test_page_module.objectValues():
+      url_list.append(x.absolute_url())
+    self.remote_code_url_list = url_list
+    ERP5TypeFunctionalTestCase.afterSetUp(self)
 
   def getBusinessTemplateList(self):
     """
       Return the list of business templates.
     """
     return ('erp5_core_proxy_field_legacy', 'erp5_full_text_myisam_catalog',
-            'erp5_base', 'erp5_ui_test_core', 'erp5_ui_test', 'erp5_forge',
+            'erp5_base', 'erp5_ui_test_core', 'erp5_forge',
             'erp5_dhtml_style',
             'erp5_jquery', 'erp5_jquery_ui',
             'erp5_knowledge_pad',
@@ -84,6 +67,8 @@ class TestZeleniumStandaloneUserTutorial(ERP5TypeFunctionalTestCase):
             'erp5_l10n_fr', 'erp5_crm',
             'erp5_accounting_ui_test',
             'erp5_user_tutorial_ui_test',
+            'erp5_run_my_doc',
+            'erp5_test_page_test_data',
            )
 
 def test_suite():
