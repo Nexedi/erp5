521cc67ed3129a3b968f5c1e7b407205f252189c
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2014-03-10 16:32:43 +0900

    WebDAV: Subfolder of CMF Folder were created at the root of the site.
    As MKCOL_handler() is not implemented on OFS.Folder.Folder class, the one from
    ERP5Site was acquired when calling webdav.NullResource.NullResource.MKCOL().

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -79,6 +79,7 @@ from Products.ERP5Type.patches import AceEditorZMI
 from Products.ERP5Type.patches import CachingPolicyManager
 from Products.ERP5Type.patches import AcceleratedHTTPCacheManager
 from Products.ERP5Type.patches import ExceptionFormatter
+from Products.ERP5Type.patches import WebDAV
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
