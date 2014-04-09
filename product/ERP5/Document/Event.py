5c99171908fe67afc78e3b64e815de6bfe651e46
Gabriel Monnerat <gabriel@tiolive.com>
2014-04-09 08:35:21 -0700

    erp5_core: Keep erp5 core minimal.
    As Event_send is doing nothing, we don't need it. Then, return None is enough when type based method is not found.

--- product/ERP5/Document/Event.py
+++ product/ERP5/Document/Event.py
@@ -162,8 +162,8 @@ class Event(Movement, EmailDocument, AcknowledgeableMixin):
     """
     send_script = self._getTypeBasedMethod('send')
     if send_script is None:
-      raise NotImplementedError("`send` type based method not found. "
-                                "Please update erp5_base and erp5_crm")
+      return
+
     return send_script(
         from_url, to_url, reply_url, subject, body, attachment_format, attachment_list,
         download, **kw
