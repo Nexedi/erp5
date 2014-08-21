129f97e3339b0fb9b3ac01749178a686ce9e97b3
Vincent Pelletier <vincent@nexedi.com>
2014-08-21 15:03:51 +0200

    ZSQLCatalog.SearchKey: Simplify auto-loading magic.

--- product/ZSQLCatalog/SearchKey/__init__.py
+++ product/ZSQLCatalog/SearchKey/__init__.py
@@ -1,13 +1,7 @@
 # Automaticaly import all SearchKeys
 import os
-module_path = os.path.dirname(os.path.abspath(__file__))
 global_dict = globals()
 __relative_file__ = os.path.basename(__file__)
-for filename in os.listdir(module_path):
+for filename in os.listdir(os.path.dirname(os.path.abspath(__file__))):
   if filename.endswith('.py') and filename != __relative_file__:
-    modulename = filename[:-3]
-    try:
-      module = __import__(modulename, global_dict, None, [])
-    except ImportError:
-      continue
-
+    __import__(filename[:-3], global_dict, None, [])
