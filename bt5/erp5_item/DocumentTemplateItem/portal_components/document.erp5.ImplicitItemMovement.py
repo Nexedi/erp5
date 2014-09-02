fa24a5da3d9869e8f1bebe3b042b92e84777f950
Gabriel Monnerat <gabriel@tiolive.com>
2014-09-02 11:42:28 +0200

    Initial release of Implicit Item Movement, simple movements that can be used to have a movement from nowhere to the node where this item starts to be tracked

---
+++ bt5/erp5_item/DocumentTemplateItem/portal_components/document.erp5.ImplicitItemMovement.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2002-2014 Nexedi SA and Contributors. All Rights Reserved.
+#
+##############################################################################
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions
+from Products.ERP5.Document.DeliveryLine import DeliveryLine
+
+class ImplicitItemMovement(DeliveryLine):
+
+  meta_type = 'ERP5 Implicit Item Movement'
+  portal_type = 'Implicit Item Movement'
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  security.declareProtected(Permissions.AccessContentsInformation,
+                            'isAccountable')
+  def isAccountable(self):
+    """
+      Returns 1 if this needs to be accounted
+      Only account movements which are not associated to a delivery
+      Whenever delivery is there, delivery has priority
+    """
+    return not self.hasCellContent()
+
+  security.declareProtected(Permissions.AccessContentsInformation,
+                            'getRootDeliveryValue')
+  def getRootDeliveryValue(self):
+    """
+    Returns the root delivery responsible of this line
+    """
+    return self
+
+# Workaround buggy implementation of getImmobilisationState
+ImplicitItemMovement.getImmobilisationState = None
