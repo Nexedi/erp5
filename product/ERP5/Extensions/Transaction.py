14322ab63ce8261f5fb752c808c35cd978a97738
Julien Muchembled <jm@nexedi.com>
2014-10-03 17:36:44 +0200

    Drop Transaction extension & allow use of transaction.doom from Restricted Python

--- product/ERP5/Extensions/Transaction.py
+++
@@ -1,9 +0,0 @@
-import transaction
-
-def abort_transaction():
-    # FIXME: aborting a transaction means it could be commited later on. The
-    # transaction should be doom()ed instead, but transaction.doom() is not
-    # available on Zope 2.8. We should provide our own doom() implementation
-    # which raises an exception on pre-commit-hook, which does exist
-    # in Zope 2.8
-    transaction.abort()
