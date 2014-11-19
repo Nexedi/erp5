647142e56174db8a8e64d7d47421f10585a5ee7c
Klaus Wölfel <klaus@nexedi.com>
2014-11-19 19:28:30 +0100

    erp5_trade: Temporarly Revert "stop copying (deprecated) Payment Condition documents from Trade Condition to Order, and from Order to Packing List."
    This reverts commit eb5fdbe3613b320b0ae92d18458f831620e0ac15.
    
    Conflicts:
    	bt5/erp5_trade/bt/revision
    	product/ERP5/tests/testTradeCondition.py

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/PackingList_copyOrderProperties.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/PackingList_copyOrderProperties.py
@@ -6,3 +6,10 @@ if packing_list.getSimulationState() == 'draft':
     comment = related_order.getComment(),
     title = related_order.getTitle()
   )
+
+# copy order's payment conditions
+payment_condition_copy_id_list = related_order.contentIds(filter={'portal_type':'Payment Condition'})
+if len(payment_condition_copy_id_list) > 0:
+  clipboard = related_order.manage_copyObjects(
+      ids=payment_condition_copy_id_list)
+  packing_list.manage_pasteObjects(clipboard)
