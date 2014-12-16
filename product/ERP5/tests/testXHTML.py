8652422c551ee6462f38b70965ed7bf63f007ddd
Jérome Perrin <jerome@nexedi.com>
2014-12-16 12:04:52 +0900

    testXHTML: do not jslint erp5_ace_editor

--- product/ERP5/tests/testXHTML.py
+++ product/ERP5/tests/testXHTML.py
@@ -169,7 +169,8 @@ class TestXHTMLMixin(ERP5TypeTestCase):
     for script_path, script in skins_tool.ZopeFind(
               skins_tool, obj_metatypes=['File','DTML Method','DTML Document'], search_sub=1):
       is_required_check_path = True
-      ignore_bts = ['erp5_jquery','erp5_fckeditor','erp5_xinha_editor', 'erp5_jquery_ui']
+      ignore_bts = ['erp5_jquery','erp5_fckeditor','erp5_xinha_editor',
+                    'erp5_jquery_ui', 'erp5_ace_editor']
       if script_path.endswith('.js'):
         for ignore_bt_name in ignore_bts:
           if script_path.startswith(ignore_bt_name):
