dcf1eb855299d84f0d39fd9fe4cec816f1c68a45
Klaus Wölfel <klaus@nexedi.com>
2015-01-27 03:30:38 +0000

    erp5_trade: Order Report improvements
    - Optionally group by function
    - Give higher priority to Order Report compared to Workflow Report
    - Optionally filter by Quantity Unit and enable Stat Columns for Quantity
    
    TODO: Quantity also for Thirs Party if filtered by quantity Unit

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/OrderModule_getOrderReportSectionList.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/OrderModule_getOrderReportSectionList.py
@@ -1,8 +1,6 @@
 from Products.ERP5Form.Report import ReportSection
 from DateTime import DateTime
-
 params, stat_columns, selection_columns = context.OrderModule_getOrderReportParameterDict()
-
 context.REQUEST.set('stat_columns', stat_columns)
 result=[]
 result.append(ReportSection(
@@ -11,6 +9,4 @@ result.append(ReportSection(
               listbox_display_mode='FlatListMode',
               selection_params=params,
               form_id='OrderModule_viewOrderStatList'))
-
-
 return result
