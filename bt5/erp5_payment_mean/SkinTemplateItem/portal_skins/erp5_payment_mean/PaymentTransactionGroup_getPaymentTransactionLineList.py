ccb1c81101404f2b472fe72e6331cd8f48655447
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2015-03-23 23:33:17 +0100

    respect passed kw in PaymentTransactionGroup_getPaymentTransactionLineList.

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineList.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineList.py
@@ -1,14 +1,14 @@
 portal = context.getPortalObject()
 
-search_kw = dict(
-  parent_portal_type='Payment Transaction',
-  section_uid=context.getSourceSectionUid(),
-  default_aggregate_uid=context.getUid(),
-)
+kw.update({
+  'parent_portal_type':'Payment Transaction',
+  'section_uid':context.getSourceSectionUid(),
+  'default_aggregate_uid':context.getUid(),
+})
 
 if context.getSourcePayment():
   precision = context.getQuantityPrecisionFromResource(
     context.getSourcePaymentValue().getPriceCurrency())
   portal.REQUEST.set('precision', precision)
 
-return portal.portal_simulation.getMovementHistoryList(**search_kw)
+return portal.portal_simulation.getMovementHistoryList(**kw)
--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_statPaymentTransactionLineList.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_statPaymentTransactionLineList.py
@@ -1,10 +1,11 @@
 from Products.PythonScripts.standard import Object
 portal = context.getPortalObject()
 
-search_kw = dict(
-  parent_portal_type='Payment Transaction',
-  section_uid=context.getSourceSectionUid(),
-  default_aggregate_uid=context.getUid(),
-)
+kw.update({
+  'parent_portal_type':'Payment Transaction',
+  'section_uid':context.getSourceSectionUid(),
+  'default_aggregate_uid':context.getUid(),
+  'node_category':'account_type/asset/cash/bank',
+})
 
-return Object(total_quantity=portal.portal_simulation.getInventory(**search_kw)),
+return Object(total_quantity=portal.portal_simulation.getInventory(**kw)),
