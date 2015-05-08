2345cdeb4b86c6b2cf460c55633e785eb25546da
Jérome Perrin <jerome@nexedi.com>
2015-05-08 08:34:38 +0200

    remove an obsolete way to run test directly from business template

--- product/ERP5Type/tests/runUnitTest.py
+++ product/ERP5Type/tests/runUnitTest.py
@@ -154,18 +154,6 @@ When no unit test is specified, only activities are processed.
 sys.modules['Products.ERP5Type.tests.runUnitTest'] = sys.modules[__name__]
 
 
-def getUnitTestFile():
-  """returns the absolute path of this script.
-  This is used by template tool to run unit tests."""
-  # ERP5_TEST_RUNNER is set when the instance was created by a buildout
-  # It should point to a wrapper to this script setting up the necessary paths
-  test_runner = os.environ.get('ERP5_TEST_RUNNER')
-  if test_runner is not None and os.path.exists(test_runner):
-    return test_runner
-  else:
-    return os.path.abspath(__file__)
-
-
 def initializeInstanceHome(tests_framework_home,
                            real_instance_home,
                            instance_home):
