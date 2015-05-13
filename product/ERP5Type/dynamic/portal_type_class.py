e2ec37e6e993a708fc7056a1d138940aa1a20fe2
Wenjie Zheng <wenjie.zheng@tiolive.com>
2015-05-13 13:01:31 +0000

    portal_type_class.py: revert unused modifications added during development.

--- product/ERP5Type/dynamic/portal_type_class.py
+++ product/ERP5Type/dynamic/portal_type_class.py
@@ -395,8 +395,8 @@ def synchronizeDynamicModules(context, force=False):
     # stored in WorkflowMethod objects: our generation of workflow
     # methods adds/registers/wraps existing methods, but does not
     # remove old chains. Do it now.
-
     resetRegisteredWorkflowMethod()
+
     # Some method generations are based on portal methods, and portal
     # methods cache results. So it is safer to invalidate the cache.
     cache_tool = getattr(portal, 'portal_caches', None)
