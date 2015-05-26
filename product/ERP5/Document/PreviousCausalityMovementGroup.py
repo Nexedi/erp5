fdd48a155ef90e586586a506a67de627ce7631ea
Sebastien Robin <seb@nexedi.com>
2015-05-26 06:50:31 +0000

    simulation: allow to define wished portal type in PreviousCausalityMovementGroup

--- product/ERP5/Document/PreviousCausalityMovementGroup.py
+++ product/ERP5/Document/PreviousCausalityMovementGroup.py
@@ -27,21 +27,29 @@
 
 from Products.ERP5.Document.FirstCausalityMovementGroup import \
      FirstCausalityMovementGroup
-from zLOG import LOG
 
 class PreviousCausalityMovementGroup(FirstCausalityMovementGroup):
   """
-  Group by previous causality. For movements going to Sale Invoices,
-  the previous causality is SPL
+  Group by previous causality of given type.
+
+  This movement group will try to find a causality of given portal
+  type by looking upper in the simulation tree. Merge will happen only
+  if same causality of given type is the same.
+
+  Sometimes, we can not find expected causality with given type because
+  a movement has been added by hand into an intermediate delivery. In
+  such case, we will look at causalities defined on deliveries
+
+  For example, you might decide to merge in same invoice only
+  movement from a particular Packing List, or only movement coming
+  from a particular Order.
   """
   meta_type = 'ERP5 Previous Causality Movement Group'
   portal_type = 'Previous Causality Movement Group'
 
-  causality_portal_type = 'Sale Packing List'
-
   def test(self, movement, property_dict, **kw):
     """Compare explanation to now if it is possible to update delivery"""
-    explanation = property_dict.get('_explanation','')
+    explanation = property_dict.get('_previous_explanation','')
     if movement == movement.getDeliveryValue():
       # We are at delivery level, check if the explanation is part of the causality
       delivery = movement
@@ -51,7 +59,49 @@ class PreviousCausalityMovementGroup(FirstCausalityMovementGroup):
         return False, {}
     raise NotImplementedError("What should we do ?")
 
+  def testExplanation(self, explanation_value):
+    """
+    Check if explanation is valid
+
+    We might later have other ways to test explanation, for example by a script
+    """
+    result = False
+    causality_portal_type = self.getCausalityPortalType()
+    if causality_portal_type is None or \
+        explanation_value.getPortalType() == causality_portal_type:
+      result = True
+    return result
+
+  def _searchUpperInTree(self, movement):
+    explanation_value = None
+    if movement.getParentValue() != movement.getRootAppliedRule() :
+      # get the explanation of parent movement if we have not been
+      # created by the root applied rule.
+      movement = movement.getParentValue().getParentValue()
+      intermediate_explanation_value = movement.getExplanationValue()
+      if self.testExplanation(intermediate_explanation_value):
+        explanation_value = intermediate_explanation_value
+      if explanation_value is None:
+        explanation_value = self._searchUpperInTree(movement)
+    return explanation_value
+
+  def _searchThroughDelivery(self, delivery):
+    explanation_value = None
+    if self.testExplanation(delivery):
+      explanation_value = delivery
+    else:
+      for causality_value in delivery.getCausalityValueList():
+        explanation_value = self._searchThroughDelivery(causality_value)
+        if explanation_value:
+          break
+    return explanation_value
+
   def _getPropertyDict(self, movement, **kw):
-    property_dict = super(PreviousCausalityMovementGroup, self).\
-        _getPropertyDict(movement, **kw)
+    previous_explanation = self._searchUpperInTree(movement)
+    if previous_explanation is None:
+      movement = movement.getRootAppliedRule()
+      previous_explanation = self._searchThroughDelivery(movement.getExplanationValue())
+    property_dict = {}
+    if previous_explanation is not None:
+      property_dict["_previous_explanation"] = previous_explanation.getRelativeUrl()
     return property_dict
