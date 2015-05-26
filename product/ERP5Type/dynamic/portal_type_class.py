16e9e4a4bc26b049f64fa364ee5a378d18d68ced
Wenjie Zheng <wenjie.zheng@tiolive.com>
2015-05-26 16:12:37 +0000

    portal_type_class.py: add ERP5WorkflowTool to the bootstrap list.

--- product/ERP5Type/dynamic/portal_type_class.py
+++ product/ERP5Type/dynamic/portal_type_class.py
@@ -327,8 +327,9 @@ def synchronizeDynamicModules(context, force=False):
       from Products.ERP5Type.Tool.PropertySheetTool import PropertySheetTool
       from Products.ERP5Type.Tool.TypesTool import TypesTool
       from Products.ERP5Type.Tool.ComponentTool import ComponentTool
+      from Products.ERP5.Tool.ERP5WorkflowTool import ERP5WorkflowTool
       try:
-        for tool_class in TypesTool, PropertySheetTool, ComponentTool:
+        for tool_class in TypesTool, PropertySheetTool, ComponentTool, ERP5WorkflowTool:
           # if the instance has no property sheet tool, or incomplete
           # property sheets, we need to import some data to bootstrap
           # (only likely to happen on the first run ever)
