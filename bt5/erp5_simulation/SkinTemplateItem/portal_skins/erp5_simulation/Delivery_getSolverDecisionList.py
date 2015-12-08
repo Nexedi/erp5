8358c2ed5d0e68c2545663f4f36841da026777d6
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2015-12-08 19:29:12 +0900

    erp5_simulation: Display Divergence dialog even if there is nothing to solve (same as legacy Simulation Dialog).
    This fixes the following Site Error when there is no Divergence to solve:
      AttributeError: 'NoneType' object has no attribute 'SolverProcess_getSolverDecisionList'
    
    This may happen if user selects 'Solve Divergence' Action and solve
    Divergences, then immediately goes back and selects again this action
    whereas there is actually no Divergence to solve.

--- bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_getSolverDecisionList.py
+++ bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_getSolverDecisionList.py
@@ -1,3 +1,6 @@
 solver_process = context.Delivery_getSolverProcess()
+if solver_process is None:
+  return []
+
 # XXX should omit 'solved' decision?
 return solver_process.SolverProcess_getSolverDecisionList()
