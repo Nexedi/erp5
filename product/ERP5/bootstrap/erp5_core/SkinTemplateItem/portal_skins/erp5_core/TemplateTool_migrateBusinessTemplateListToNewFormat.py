16524df172f202aa3f9f87ecaa61a5000bbc6156
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2016-03-09 15:42:42 +0000

    remove TemplateTool_viewMigrateBusinessTemplateListToNewFormatDialog  from erp5_core

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/TemplateTool_migrateBusinessTemplateListToNewFormat.py
+++
@@ -1,6 +0,0 @@
-if not repository_list:
-  return context.Base_redirect(keep_items={'portal_status_message': 'No '+
-       'repository was defined'})
-context.portal_templates.importAndReExportBusinessTemplateListFromPath(repository_list)
-return context.Base_redirect(keep_items={'portal_status_message': 'Migration '+
-       'started, please check your portal activities'})
