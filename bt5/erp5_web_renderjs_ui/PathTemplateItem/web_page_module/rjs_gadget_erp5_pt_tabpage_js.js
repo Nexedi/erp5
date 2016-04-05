87fafe89c50bb73ba49215e71abc71b925e154ab
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2016-04-05 12:48:25 +0000

    erp5_web_renderjs_ui: Tab gadget display jump and is used in form_view

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_tabpage_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_tabpage_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, RSVP, Handlebars */
+/*global window, rJS, RSVP, Handlebars, URI */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS, RSVP, Handlebars) {
+(function (window, rJS, RSVP, Handlebars, URI) {
   "use strict";
 
   /////////////////////////////////////////////////////////////////
@@ -44,22 +44,33 @@
     .declareMethod("render", function (options) {
       var view_list,
         tab_list = [],
+        jump_action_list = [],
         gadget = this,
-        erp5_document;
+        erp5_document,
+        jump_list;
 
       return gadget.jio_getAttachment(options.jio_key, "links")
         .push(function (result) {
           var i,
             promise_list = [];
-          erp5_document = result,
-          view_list = erp5_document._links.view;
+          erp5_document = result;
+          view_list = erp5_document._links.view || [];
+          jump_list = erp5_document._links.action_object_jump || [];
 
           // All ERP5 document should at least have one view.
           // So, no need normally to test undefined
           if (view_list.constructor !== Array) {
             view_list = [view_list];
           }
+          if (jump_list.constructor !== Array) {
+            jump_list = [jump_list];
+          }
 
+          promise_list.push(gadget.getUrlFor({command: 'change', options: {
+            view: "view",
+            page: undefined,
+            editable: undefined
+          }}));
           for (i = 0; i < view_list.length; i += 1) {
             promise_list.push(gadget.getUrlFor({command: 'change', options: {
               view: view_list[i].href,
@@ -67,37 +78,51 @@
               page: undefined
             }}));
           }
-          promise_list.push(gadget.getUrlFor({command: 'change', options: {
-            view: "view",
-            page: undefined,
-            editable: undefined
-          }}));
+          for (i = 0; i < jump_list.length; i += 1) {
+            promise_list.push(gadget.getUrlFor({command: 'push_history', options: {
+              extended_search: new URI(jump_list[i].href).query(true).query,
+              page: 'search'
+            }}));
+          }
           return RSVP.all(promise_list);
         })
         .push(function (all_result) {
-          var i;
-
-          for (i = 0; i < all_result.length - 1; i += 1) {
+          var i, j;
+          j = 1;
+          for (i = 0; i < view_list.length; i += 1) {
             tab_list.push({
               title: view_list[i].title,
               i18n: view_list[i].title,
-              link: all_result[i]
+              link: all_result[j]
             });
+            j += 1;
           }
-
+          for (i = 0; i < jump_list.length; i += 1) {
+            jump_action_list.push({
+              title: jump_list[i].title,
+              link: all_result[j],
+              i18n: jump_list[i].title
+            });
+            j += 1;
+          }
           return gadget.translateHtml(table_template({
             definition_title: "Views",
             definition_i18n: "Views",
             definition_icon: "eye",
             documentlist: [{
               title: view_list[0].title,
-              link: all_result[all_result.length - 1]
+              link: all_result[0]
             }]
           }) + table_template({
             definition_title: "Editables",
             definition_i18n: "Editables",
             definition_icon: "edit",
             documentlist: tab_list
+          }) + table_template({
+            definition_title: "Jumps",
+            documentlist: jump_action_list,
+            definition_icon: "plane",
+            definition_i18n: "Jumps"
           }));
         })
         .push(function (my_translated_html) {
@@ -119,4 +144,4 @@
         });
     });
 
-}(window, rJS, RSVP, Handlebars));
+}(window, rJS, RSVP, Handlebars, URI));
