cec142640d557b516295b8317230b5fb20feb32d
Vincent Pelletier <vincent@nexedi.com>
2016-04-15 08:48:44 +0200

    erp5_core: Reduce reliance on zpt_* skin folders.

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_auto_logout/logout.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_auto_logout/logout.py
@@ -1,5 +1,7 @@
 portal = context.getPortalObject()
 portal.portal_sessions.manage_delObjects(portal.Base_getAutoLogoutSessionKey())
-# XXX: I would like to use skinSuper, but this may not be defined on context (it's defined on ERP5Type.Base, and context may be ERP5.ERP5Site).
-# As a result, hardcode expected logout location (which is marginally better than duplicating it).
-return portal.portal_skins.zpt_control.logout()
+REQUEST = portal.REQUEST
+if REQUEST.has_key('portal_skin'):
+  portal.portal_skins.clearSkinCookie()
+REQUEST.RESPONSE.expireCookie('__ac', path='/')
+return REQUEST.RESPONSE.redirect(REQUEST.URL1 + '/logged_out')
