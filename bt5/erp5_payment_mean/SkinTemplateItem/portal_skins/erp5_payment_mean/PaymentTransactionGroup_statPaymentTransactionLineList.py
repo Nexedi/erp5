392161f245e3a68875357643af1bfa052d7a8c29
Vincent Pelletier <vincent@nexedi.com>
2016-04-22 13:12:36 +0200

    PaymentTransactionGroup_statPaymentTransactionLineList: Do not use node_category.
    node_category adds a join with catalog which tends to be preferred by query
    optimiser over the agrgegate relation (because there are fewer accounts than
    there are aggregared documents), which makes displaying a payment group
    proportional to the number of payments on the accoutns instead of
    proportional to current group's related aggregate list.
    Also, drop parent_portal_type condition, as payment lines aggregated to a
    group should be children of a Payment Transaction (and it saves one more
    join - although much less costly than the node_category one).
    Also, make aggregate relation condition strict (there should be no group
    sub-objects from which we would want to get related aggregates).

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_statPaymentTransactionLineList.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_statPaymentTransactionLineList.py
@@ -2,10 +2,12 @@ from Products.PythonScripts.standard import Object
 portal = context.getPortalObject()
 
 kw.update({
-  'parent_portal_type':'Payment Transaction',
-  'section_uid':context.getSourceSectionUid(),
-  'default_aggregate_uid':context.getUid(),
-  'node_category':'account_type/asset/cash/bank',
+  'section_uid': context.getSourceSectionUid(),
+  'strict_aggregate_uid': context.getUid(),
+  'node_uid': [x.uid for x in portal.portal_catalog(
+    portal_type='Account',
+    strict_account_type_uid=portal.portal_categories.account_type.asset.cash.bank.getUid(),
+  )],
 })
 
 return Object(total_quantity=portal.portal_simulation.getInventory(**kw)),
