057254ed28ce1e99783c7ecf37dd4c42f0848239
Tristan Cavelier <tristan.cavelier@nexedi.com>
2016-04-22 15:10:46 +0000

    erp5_core: predicate lists only non expired categories

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Predicate_getMembershipCriterionCategoryList.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Predicate_getMembershipCriterionCategoryList.py
@@ -9,15 +9,18 @@ for item in multimembership_base_list :
 category_list = []
 
 ctool = context.portal_categories
+def getCategoryChildCompactLogicalPathItemList(base_category, **kw):
+  return [i for i in base_category.getCategoryChildCompactLogicalPathItemList(base=1) if i[1] == "" or getattr(ctool.restrictedTraverse(i[1]), "getValidationState", str)() in ("", "embedded", "published", "protected")]  # [:]
+
 for item in mixed_list:
   base_category = ctool[item]
-  item_list = base_category.getCategoryChildCompactLogicalPathItemList(base=1)[:]
+  item_list = getCategoryChildCompactLogicalPathItemList(base_category, base=1)
   if item_list == [['', '']]:
     for fallback_category_id in base_category.getFallbackBaseCategoryList():
       fallback_category = ctool.restrictedTraverse(fallback_category_id, None)
       if fallback_category is not None and fallback_category.objectIds():
         item_list.extend([('%s/%s' % (fallback_category_id, x[0]), '%s/%s' % (item, x[1])) \
-          for x in fallback_category.getCategoryChildCompactLogicalPathItemList(base=1) if x[0]])
+          for x in getCategoryChildCompactLogicalPathItemList(fallback_category, base=1) if x[0]])
         break
 
   category_list.extend(item_list)
