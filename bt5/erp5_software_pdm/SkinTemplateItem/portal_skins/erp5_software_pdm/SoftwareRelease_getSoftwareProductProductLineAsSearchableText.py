4ab86abcba469c3453bc4f1cc7c4e2526b024030
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2016-05-04 14:25:41 +0000

    erp5_software_pdm: Add advance tools for software publication
    erp5_software_pdm: Fix FullText indexation of Software Publication Line and Software Release

--- bt5/erp5_software_pdm/SkinTemplateItem/portal_skins/erp5_software_pdm/SoftwareRelease_getSoftwareProductProductLineAsSearchableText.py
+++ bt5/erp5_software_pdm/SkinTemplateItem/portal_skins/erp5_software_pdm/SoftwareRelease_getSoftwareProductProductLineAsSearchableText.py
@@ -2,5 +2,5 @@ r = ''
 
 software_product = context.getFollowUpValue(portal_type='Software Product')
 if software_product:
-    r = software_product.getProductLineTitle() + ' ' + software_product.getDescription() + software_product.getTitle()
+  r = software_product.getProductLineTitle('') + ' ' + software_product.getDescription('') + software_product.getTitle('')
 return r
