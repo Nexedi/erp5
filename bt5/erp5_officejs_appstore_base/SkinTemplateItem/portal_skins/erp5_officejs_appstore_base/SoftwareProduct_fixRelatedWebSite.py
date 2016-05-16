f5982e71009e438905d39d0473a426b64a8dcb79
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2016-05-16 15:16:09 +0000

    erp5_officejs_appstore_base: Update Consistency scripts related to web site. Add Export Tool

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_fixRelatedWebSite.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_fixRelatedWebSite.py
@@ -9,27 +9,59 @@ if not web_site:
   web_site.edit(
     title=context.getTitle(),
     short_title=context.getTitle(),
+    id=context.getReference().lower(),
   )
   context.setFollowUpValue(web_site)
 
+# This is dangerous
+if not web_site.getId() == context.getReference().lower():
+  web_site.setId(context.getReference().lower())
+
+
+# Update version on development document
+# XX Do we need version???
+document_list = portal.portal_catalog(
+  portal_type= portal.getPortalDocumentTypeList(),
+  validation_state="draft",
+  strict_follow_up_uid=context.getUid(),
+)
+
+# XX Doesn't seem to be unique enough
+version = context.getReference()[:6] + "-dev"
+for document_brain in document_list:
+  document = document_brain.getObject()
+  document.setVersion(version)
+
+
 development_section = web_site["development"]
-if not development_section.getAggregate():
-  aggregate_list = portal.portal_catalog(
-    portal_type="Web Page",
-    strict_follow_up_uid=context.getUid(),
-    strict_publication_section_uid=portal.portal_categories.publication_section.application.landing_page.getUid(),
-    validation_state="draft",
-    #XXX Hackish
-    sort_on=[("modification_date", "descending")],
-    limit=1,
-    select_list=["relative_url"],
-  )
-  if aggregate_list:
-    development_section.setAggregate(aggregate_list[0].relative_url)
+# Update development section to use version and follow_up
+development_section.setCriterionPropertyList([
+  "version",
+  "validation_state",
+])
+development_section.setCriterion('version', version)
+development_section.setCriterion('validation_state', 'draft')
+development_section.setMembershipCriterionBaseCategoryList(['follow_up'])
+development_section.setMembershipCriterionCategoryList(['follow_up/' + context.getRelativeUrl()])
+
+
+# Update default page for development version.
+landing_publication_uid = portal.portal_categories.publication_section.application.landing_page.getUid()
+aggregate_list = portal.portal_catalog(
+  portal_type="Web Page",
+  strict_follow_up_uid=context.getUid(),
+  strict_publication_section_uid=landing_publication_uid,
+  validation_state="draft",
+  #XXX Hackish
+  sort_on=[("modification_date", "descending")],
+  limit=1,
+  select_list=["relative_url"],
+)
+if aggregate_list:
+  development_section.setAggregate(aggregate_list[0].relative_url)
 
 development_section.setTitle(context.getTitle() + " Development")
 development_section.setShortTitle(context.getShortTitle())
 development_section.setDescription(context.getDescription())
 
 return web_site
-# XXX Should fix default page for development version.
