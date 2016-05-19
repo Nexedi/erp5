37a88765f2740d1635ca29a18beeace48e0c4820
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2016-05-19 13:45:12 +0000

    erp5_officejs_appstore_base: Use dedicated application redirect manifest

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareRelease_publishRelatedWebDocument.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareRelease_publishRelatedWebDocument.py
@@ -1,13 +1,12 @@
 portal = context.getPortalObject()
-
 software_release = context
 
+# Publish Web Document
 web_document_list = portal.portal_catalog(
   portal_type=portal.getPortalDocumentTypeList(),
   strict_follow_up_uid=software_release.getUid(),
   validation_state="submitted",
 )
-
 
 today = DateTime().earliestTime()
 for web_document in web_document_list:
@@ -15,11 +14,9 @@ for web_document in web_document_list:
   web_document.publish()
 
 software_product = context.getFollowUpValue(portal_type="Software Product")
-
-
 web_site = software_product.SoftwareProduct_getRelatedWebSite()
 
-
+# Publish Web Site, Websection and update version
 today = DateTime().earliestTime()
 if web_site.getValidationState() != 'published':
   web_site.setEffectiveDate(today)
@@ -33,6 +30,28 @@ web_site.edit(
   configuration_latest_version=software_release.getVersion(),
 )
 
+# Update appcache
+web_manifest_reference = web_site.getId() + ".appcache"
+web_manifest = portal.portal_catalog.getResultValue(
+  portal_type="Web Manifest",
+  reference=web_manifest_reference,
+  )
+
+if not web_manifest:
+  web_manifest = portal.web_page_module.newContent(
+    portal_type="Web Manifest",
+    reference=web_manifest_reference,
+    title=web_site.getTitle() + " Manifest",
+    )
+  web_manifest.publishAlive()
+
+web_manifest.setData("""CACHE MANIFEST
+# generated on %s
+CACHE:
+NETWORK:
+*""" % (DateTime().rfc822()))
+
+
 # Is latest Websection what we want?
 if "latest" not in web_site.objectIds():
   latest_web_section = version_web_section.Base_createCloneDocument(batch_mode=True)
@@ -44,8 +63,3 @@ else:
   latest_web_section.setCriterion('version', software_release.getVersion())
   latest_web_section.setTitle(version_web_section.getTitle())
   latest_web_section.setAggregate(version_web_section.getAggregate())
-
-# Archive former versions
-#for web_section in web_site.objectValues(portal_type="Web Section"):
-#  if web_section.getId() not in ('latest', 'development', 'hateoas', version_web_section.getId()):
-#    web_section.setCriterion('validation_state', 'archived')
--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_web_versioning/WebSection_renderOfficeJSApplicationPage.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_web_versioning/WebSection_renderOfficeJSApplicationPage.py
@@ -9,4 +9,5 @@ web_section = REQUEST.get("current_web_section")
 
 return default_web_page.WebPage_viewAsWeb(mapping_dict={
   "latest_version": web_section.getLayoutProperty("configuration_latest_version", default="development"),
+  "application_appcache": web_section.getId() + ".appcache",
 })
