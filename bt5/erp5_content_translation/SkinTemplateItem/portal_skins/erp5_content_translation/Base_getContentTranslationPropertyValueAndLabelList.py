073f6cf1fac34f2616a60f88902d7dcf15224b0e
Sebastien Robin <seb@nexedi.com>
2016-06-03 09:17:01 +0000

    erp5_content_translation: display text to translate as textarea, otherwise it is not usable
    It would be surely better to have matrixbox having possibility to
    define which widget we use to display matrix variation labels.

--- bt5/erp5_content_translation/SkinTemplateItem/portal_skins/erp5_content_translation/Base_getContentTranslationPropertyValueAndLabelList.py
+++ bt5/erp5_content_translation/SkinTemplateItem/portal_skins/erp5_content_translation/Base_getContentTranslationPropertyValueAndLabelList.py
@@ -2,5 +2,7 @@ result = []
 for property_name in context.getTypeInfo().getContentTranslationDomainPropertyNameList():
   original_text = context.getProperty(property_name)
   if original_text:
+    if pretty:
+      original_text = "<textarea readonly>" + original_text + "</textarea>"
     result.append((property_name, original_text))
 return result
