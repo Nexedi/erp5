9668f92915db6a5e48b884bce892c2dc7032dab8
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2016-06-14 07:45:56 +0000

    erp5_base: removes dangerous test condition in Delivery_init (fix for 56ef22ac6f65)

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_init.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_init.py
@@ -5,5 +5,4 @@ elif kw.get('ledger', None) is not None:
 else:
   portal_type_id = context.getPortalType()
   portal_type = context.getPortalObject().portal_types.get(portal_type_id, None)
-  if portal_type:
-    context.setLedger(portal_type.getDefaultLedger(None))
+  context.setLedger(portal_type.getDefaultLedger(None))
