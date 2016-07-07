477365ae824dd47d57e35bb1c491356b8ede8b55
Yusei Tahara <yusei@nexedi.com>
2016-07-07 09:24:33 +0000

    erp5_ui_test_core: Don't use Base_doLanguage. Base_doLanguage opens another selenium test runner in the bottom frame of the original selenium test runner by redirect and another selenium test runner does the same thing recursively.

---
+++ bt5/erp5_ui_test_core/SkinTemplateItem/portal_skins/erp5_ui_test_core/Base_setLanguageWithoutRedirect.py
@@ -0,0 +1,7 @@
+context.Base_doLanguage(select_language)
+# Don't redirect. Base_doLanguage tries to redirect to the same page
+# and then selenium main page is loaded again in the bottom frame of
+# the original selenium main page and one more selenium test starts
+# running in another frame. This happens recursively.
+context.REQUEST.RESPONSE.setStatus(200)
+return 'done'
