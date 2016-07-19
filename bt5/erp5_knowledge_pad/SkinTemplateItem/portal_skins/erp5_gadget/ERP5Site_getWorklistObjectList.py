737a1971de1f17faff04a6d1b3e308e32cd069d0
Jérome Perrin <jerome@nexedi.com>
2016-07-19 03:01:08 +0000

    knowledge_pad/worklist gadget: support displaying a worklist that does not respect the (%(count)s) convention

--- bt5/erp5_knowledge_pad/SkinTemplateItem/portal_skins/erp5_gadget/ERP5Site_getWorklistObjectList.py
+++ bt5/erp5_knowledge_pad/SkinTemplateItem/portal_skins/erp5_gadget/ERP5Site_getWorklistObjectList.py
@@ -4,10 +4,11 @@ return_list = []
 i = 1
 portal = context.getPortalObject()
 for worklist in context.portal_workflow.listActionInfos():
-  # XXX worklist translation process is a bit tricky. We translate only the first part of "XXX to Validate (count)"
   title = worklist['title']
-  title, count = title.split(' (', 1)
-  title = "%s (%s" % ( translateString(title), count )
+  if ' (' in title:
+    # Worklist translation process is a bit tricky. We translate only the first part of "X to Validate (count)"
+    title, count = title.split(' (', 1)
+    title = "%s (%s" % ( translateString(title), count )
   o = newTempDocument(portal, str(i))
   o.edit(
     count=worklist['count'],
