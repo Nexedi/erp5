617ac1f29935746303270e5bb40f356deb3710b5
Ayush Tiwari <ayush.tiwari@nexedi.com>
2016-07-21 15:20:25 +0000

    erp5_catalog: Add ERP5 SQLMethod objects
    Significant changes/additions :
    	- Add dtml file for adding ERP5 SQL Method
    	- Update ERP5.__init__ with the new ERP5 SQL Method document

---
+++ product/ERP5/Document/SQLMethod.py
@@ -0,0 +1,131 @@
+##############################################################################
+#
+# Copyright (c) 2010 Nexedi SARL and Contributors. All Rights Reserved.
+#                    Sebastien Robin <seb@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+
+from Products.ERP5Type.Globals import DTMLFile
+from Products.ERP5Type import Permissions, PropertySheet
+from App.special_dtml import HTMLFile
+from Products.ERP5Type.XMLObject import XMLObject
+from Products.PythonScripts.PythonScript import \
+  PythonScript as ZopePythonScript
+from Products.ZSQLMethods.SQL import SQL as ZSQL
+
+# Migration function
+def migrateSQLMethodToERP5Object(self, id, title=''):
+  pass
+
+manage_addSQLMethodForm = DTMLFile('../dtml/addSQLMethod', globals())
+
+# New ZSQLMethod addition function
+def manage_addSQLMethod(self, id, title='',
+                connection_id = '',
+                arguments = '',
+                template = '',
+                REQUEST=None,
+                *args,
+                **kw):
+  """
+  Add ERP5 SQL Method to the folder
+  """
+  id = str(id)
+  title = str(title)
+
+  # Create SQLMethod object container
+  c = SQLMethod(id, title, connection_id, arguments, template, self)
+
+  self._setObject(id, c)
+  c = self._getOb(id)
+  if REQUEST is not None:
+    REQUEST['RESPONSE'].redirect( 'manage_main' )
+  return c
+
+class SQLMethod(XMLObject, ZSQL):
+  """SQLMethod for ERP5.
+  """
+
+  meta_type = 'ERP5 SQL Method'
+  portal_type = 'SQL Method'
+  icon = None
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  # Add constructor methods
+  constructors = (manage_addSQLMethodForm, manage_addSQLMethod)
+
+  # Override manage and manage_main with ZSQL manage and manage_main respectively
+  manage = manage_main = ZSQL.manage
+
+  # View content list, Force /view, Standard option in SQLMethod
+  manage_options = ({'icon':'', 'label':'View','action':'view'},) \
+                    + ZSQL.manage_options
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.Base
+                    , PropertySheet.XMLObject
+                    , PropertySheet.CategoryCore
+                    , PropertySheet.DublinCore
+                    , PropertySheet.SQLMethod
+                    )
+
+  # Inheritence conflicts solved
+  __call__    = ZSQL.__call__
+  __getitem__ = ZSQL.__getitem__
+  PUT         = ZSQL.PUT
+  index_html  = ZSQL.index_html
+  manage_FTPget = ZSQL.manage_FTPget
+  dav__init     = ZSQL.dav__init
+
+  def __init__(self, id, title='',
+               connection_id = '',
+               arguments = '',
+               template = ''
+               ):
+    """
+    Assign attributes to this class and override ZSQL init method to have
+    consistency with manage_edit(as ZSQL init also creates manage_edit)
+    """
+    # Add the properties as the attributes for the SQL Method objects
+    # Useful while migrating data from ZSQLMethods to ERP5 SQLMethod objects 
+    self.id = id
+    # Do we really need to initialize XMLObject class ?
+    #XMLObject.__init__(self, *args, **kw)
+
+    # Initializing ZSQL class object is important as this will call manage_edit
+    # which will change database method properties
+    ZSQL.__init__(self, id, title, connection_id, arguments, template)
+
+  def _setArgument(self, value):
+    """
+    We need to override this so as to generate _arg attribute for SQL Method.
+    """
+    self._baseSetArgument(value)
+    self.manage_edit(self.title, self.connection_id, self.arguments_src,
+                    self.src)
