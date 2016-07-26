5edc4afa3709818b1b004786a9bd6443557c15d8
Jérome Perrin <jerome@nexedi.com>
2016-07-26 10:11:52 +0200

    upgrader: refactor dependencies between each steps
    This should fix some race conditions which made post-upgrade steps not
    executed sometimes.
    
    * each steps use an defaultActivateParameterDict so that activity they
      may create inherits the same tag, so that next step also runs after
      this). This makes it possible for example to use post-upgrade step on
      documents newly installed in upgrade step.
    * use tag / after tag between upgrader steps instead of checking if
      previous alarm had errors. We also now allow force running only one
      step regardless of the previous step completion state, there was no
      strong reason to prevernt this.
    * do not use two different active sense and fix method for alarms, just
      use an active sense method understanding `fixit` parameter

--- bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Alarm_runUpgrader.py
+++ bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Alarm_runUpgrader.py
@@ -1,5 +1,5 @@
 """
-  Run Upgrader
+  Run Upgrade step
 
   IMPORTANT: Don't use the constraint_type upgrader to data migration or big amount of objects,
   because this step is suppose to run all constraints in the same transaction. 
@@ -9,15 +9,6 @@
 REINDEX_SPLIT_COUNT = 100
 portal = context.getPortalObject()
 portal_alarms = portal.portal_alarms
-active_process = context.newActiveProcess()
-
-# We should not run upgrader if pre upgrade was not solved or never executed 
-alarm = getattr(portal_alarms, 'upgrader_check_pre_upgrade')
-if not(force) and alarm.sense() in (None, True):
-  active_process.postActiveResult(summary=context.getTitle(),
-      severity=1,
-      detail=["Is required solve Pre Upgrade first. You need run active sense once at least on this alarm"])
-  return
 
 _, type_per_constraint_type = context.Base_getConstraintTypeListPerPortalType()
 portal_type_list = type_per_constraint_type.get('upgrader', [])
@@ -26,21 +17,27 @@ tool_portal_type = 'Template Tool'
 if tool_portal_type in portal_type_list:
   portal_type_list.remove(tool_portal_type)
 
-method_kw = {'fixit': True,
-  'filter': {"constraint_type": 'upgrader'},
-  'active_process': active_process.getRelativeUrl()}
+activate_kw = params or {}
 
-portal.portal_templates.Base_postCheckConsistencyResult(**method_kw)
-for portal_type in portal_type_list:
-  if portal.portal_catalog.countResults(
-      portal_type=portal_type_list)[0][0] > REINDEX_SPLIT_COUNT:
-    portal.portal_catalog.searchAndActivate('Base_postCheckConsistencyResult',
-      activate_kw=activate_kw,
-      portal_type=portal_type,
-      method_kw=method_kw)
-  else:
-    for result in portal.portal_catalog(portal_type=portal_type):
-      result.Base_postCheckConsistencyResult(**method_kw)
+with context.defaultActivateParameterDict(activate_kw, placeless=True):
+  active_process = context.newActiveProcess()
+
+  method_kw = {'fixit': fixit,
+    'filter': {"constraint_type": 'upgrader'},
+    'active_process': active_process.getRelativeUrl(),
+    'activate_kw': activate_kw,
+  }
+
+  portal.portal_templates.Base_postCheckConsistencyResult(**method_kw)
+  for portal_type in portal_type_list:
+    if portal.portal_catalog.countResults(
+        portal_type=portal_type_list)[0][0] > REINDEX_SPLIT_COUNT:
+      portal.portal_catalog.searchAndActivate('Base_postCheckConsistencyResult',
+        activate_kw=activate_kw,
+        portal_type=portal_type,
+        method_kw=method_kw)
+    else:
+      for result in portal.portal_catalog(portal_type=portal_type):
+        result.Base_postCheckConsistencyResult(**method_kw)
 
 context.setEnabled(False)
-return
