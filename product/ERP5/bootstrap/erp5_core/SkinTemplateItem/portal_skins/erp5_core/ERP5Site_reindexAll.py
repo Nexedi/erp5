217ef709bcb86910e87989995289923feac04acf
Ayush Tiwari <ayush.tiwari@nexedi.com>
2016-07-29 14:28:21 +0000

    erp5_catalog: Reindex erp5 sql methods in ERP5Site_reindexAll

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_reindexAll.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_reindexAll.py
@@ -141,6 +141,13 @@ for path, obj in portal.portal_skins.ZopeFind(portal.portal_skins, obj_metatypes
                priority=object_priority,
                after_tag=folder_after_tag).immediateReindexObject(sql_catalog_id=sql_catalog_id)
 
+# Then we index ERP5 SQL Methods
+print "#### Indexing ERP5 SQL Methods ####"
+for path, obj in portal.portal_skins.ZopeFind(portal.portal_skins, obj_metatypes=('ERP5 SQL Method',), search_sub=1):
+  obj.activate(tag=folder_tag,
+               priority=object_priority,
+               after_tag=folder_after_tag).immediateReindexObject(sql_catalog_id=sql_catalog_id)
+
 # Then we index everything except inventories
 for folder in portal.objectValues(("ERP5 Folder",)):
   if folder.getId().find('inventory') < 0:
