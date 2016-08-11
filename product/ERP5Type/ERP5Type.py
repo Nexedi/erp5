174c9bd3db2ac324baf5ec126b8144690ad215e5
Klaus Woelfel <klaus@nexedi.com>
2016-08-11 10:36:36 +0000

    Add type groups for wendelin

--- product/ERP5Type/ERP5Type.py
+++ product/ERP5Type/ERP5Type.py
@@ -306,6 +306,10 @@ class ERP5TypeInformation(XMLObject,
       'personal_item',
       # Base
       'entity',
+      # Wendelin
+      'device',
+      'device_configuration',
+      'data_descriptor',
       # LEGACY - needs a warning - XXX-JPS
       'tax_movement',
     )
