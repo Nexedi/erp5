f16c48825065c7f6818048ff551d9e890778316e
Gabriel Monnerat <gabriel@tiolive.com>
2016-08-19 21:26:06 +0000

    erp5_oauth_google_login: Implementation of login in ERP5 with Google Account
    Google Login follow the same implementation of ERP5 Login(subobject of Person) and with an action in preferences, the user can add Google Login to his person.

---
+++ bt5/erp5_oauth_google_login/SkinTemplateItem/portal_skins/erp5_oauth_google_login/Base_createOauth2User.py
@@ -0,0 +1,60 @@
+from DateTime import DateTime
+
+if context.REQUEST.get('Base_createOauth2User') is not None:
+  return
+
+context.REQUEST.set('Base_createOauth2User', 1)
+portal = context.getPortalObject()
+
+if portal.portal_activities.countMessageWithTag(tag) > 0:
+  return
+
+person = portal.Base_getUserValueByUserId(reference)
+if person is not None:
+  context.log("Person already exists please " + \
+              "consider this object: %s" % person.getRelativeUrl())
+  return
+
+activate_kw = {'tag': tag}
+# In future we can move this script to another, because this script is generic enough
+# to support Facebook login
+assert login_portal_type in ("Google Login",), "Impossible to select a portal type"
+
+if erp5_username in ("Anonymous User", None):
+  person = portal.person_module.newContent(portal_type='Person',
+                                           user_id=reference,
+                                           first_name=first_name,
+                                           last_name=last_name,
+                                           default_email_coordinate_text=email,
+                                           activate_kw=activate_kw)
+  # Support erp5_credential
+  getDuration = getattr(portal.portal_preferences,
+                        "getPreferredCredentialAssignmentDuration",
+                        None)
+  assignment_duration = getDuration and getDuration() or 365
+  today = DateTime()
+  delay = today + assignment_duration
+
+  # Support erp5_credential
+  getAssignmentCategoryList = getattr(portal.portal_preferences,
+                                      "getPreferredSubscriptionAssignmentCategoryList",
+                                      None)
+
+  category_list = getAssignmentCategoryList and getAssignmentCategoryList() or []
+  assignment = person.newContent(
+    portal_type='Assignment',
+    category_list=category_list,
+    start_date=today,
+    stop_date=delay,
+    activate_kw=activate_kw)
+  assignment.open(activate_kw=activate_kw)
+  person.setDefaultCareerRoleList(assignment.getRoleList())
+else:
+  person = context.Base_getUserValueByUserId(erp5_username)
+
+login = person.newContent(portal_type=login_portal_type,
+                          reference=reference)
+login.validate(activate_kw=activate_kw)
+
+if person.getValidationState() != "validated":
+  person.validate(activate_kw=activate_kw)
