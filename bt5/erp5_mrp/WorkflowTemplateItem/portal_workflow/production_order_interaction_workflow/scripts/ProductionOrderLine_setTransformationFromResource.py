a6851930755f4a04291805638e3a4ed5df49a359
Sebastien Robin <seb@nexedi.com>
2016-09-06 07:43:22 +0000

    mrp: a production order has a specialise link for business template, so we have to look for transformations

--- bt5/erp5_mrp/WorkflowTemplateItem/portal_workflow/production_order_interaction_workflow/scripts/ProductionOrderLine_setTransformationFromResource.py
+++ bt5/erp5_mrp/WorkflowTemplateItem/portal_workflow/production_order_interaction_workflow/scripts/ProductionOrderLine_setTransformationFromResource.py
@@ -1,6 +1,9 @@
 production_order_line = state_change['object']
+portal = production_order_line.getPortalObject()
 
-if not production_order_line.hasSpecialise():
+transformation = production_order_line.getSpecialiseValue(
+  portal_type=portal.getPortalTransformationTypeList())
+if transformation is None:
   portal = production_order_line.getPortalObject()
   resource_uid = production_order_line.getResourceUid()
   if resource_uid:
@@ -8,6 +11,6 @@ if not production_order_line.hasSpecialise():
       portal_type=portal.getPortalTransformationTypeList(),
       validation_state="!=invalidated",
       default_resource_uid=resource_uid)
-    if len(transformation_list) == 1:
+    if len(transformation_list) >= 1:
       transformation = transformation_list[0].getRelativeUrl()
       production_order_line.setSpecialise(transformation)
