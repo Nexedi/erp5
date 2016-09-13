529f76044dd08881844aa7884edae2ea8edae7f0
Xiaowu Zhang <xiaowu.zhang@tiolive.com>
2016-09-13 17:35:28 +0000

    erp5_web_renderjs_ui_test: add test for translation

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Base_createUITestLanguages.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Base_createUITestLanguages.py
@@ -1,7 +1,7 @@
 portal = context.getPortalObject()
 Localizer = portal.Localizer
 
-for language in ('wo', 'xh'):
+for language in language_list:
   Localizer.manage_addLanguage(language)
   for catalog in Localizer.objectValues():
     catalog.manage_addLanguage(language)
