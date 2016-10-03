ac84278bcf548106240c90682f7795c9af06208f
Gabriel Monnerat <gabriel@tiolive.com>
2016-10-03 17:22:51 +0000

    erp5_authentication_policy: Migrate scripts to new format

--- bt5/erp5_authentication_policy/WorkflowTemplateItem/portal_workflow/password_interaction_workflow/scripts/afterChangePassword.py
+++ bt5/erp5_authentication_policy/WorkflowTemplateItem/portal_workflow/password_interaction_workflow/scripts/afterChangePassword.py
@@ -1,18 +1,17 @@
-from DateTime import DateTime
-portal = context.getPortalObject()
-person = state_change['object']
+login = state_change['object']
+portal = login.getPortalObject()
 
 # check preferences and save only if set
 number_of_last_password_to_check = portal.portal_preferences.getPreferredNumberOfLastPasswordToCheck()
 
 if number_of_last_password_to_check is not None and number_of_last_password_to_check:
   # save password and modification date
-  current_password = person.getPassword()
+  current_password = login.getPassword()
   if current_password is not None:
-    password_event = portal.system_event_module.newContent(portal_type = 'Password Event',
-                                                           source_value = person,
-                                                           destination_value = person,
-                                                           password = current_password)
+    password_event = portal.system_event_module.newContent(portal_type='Password Event',
+                                                           source_value=login,
+                                                           destination_value=login,
+                                                           password=current_password)
     password_event.confirm()
     # Person_isPasswordExpired cache the wrong result if document is not in catalog.
     # As the document is created in the same transaction, it is possible to force reindexation
