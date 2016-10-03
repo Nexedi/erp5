17995f8e263f92aca16d82f9b44ecaa3cf141d63
Romain Courteaud <romain@nexedi.com>
2016-10-03 13:17:11 +0000

    [erp5_web_renderjs_ui] Autoresize textarea and support keyboard shortcut to save

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_textareafield_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_textareafield_js.js
@@ -1,63 +1,52 @@
-/*global window, rJS, document, RSVP, loopEventListener */
+/*global window, rJS */
 /*jslint indent: 2, maxerr: 3 */
-(function (window, rJS, document, RSVP, loopEventListener) {
+(function (window, rJS) {
   "use strict";
 
   rJS(window)
-    .ready(function (gadget) {
-      gadget.property_dict = {
-        textarea_deferred: RSVP.defer()
-      };
-      return gadget.getElement()
-        .push(function (element) {
-          gadget.property_dict.element = element;
-        });
+    .setState({
+      tag: 'pre'
     })
     .declareMethod('render', function (options) {
-      var new_element,
-        field_json = options.field_json || {},
-        value = field_json.value || field_json.default || "";
+      var field_json = options.field_json || {},
+        state_dict = {
+          value: field_json.value || field_json.default || "",
+          editable: field_json.editable,
+          name: field_json.key,
+          title: field_json.title
+        };
+      state_dict.text_content = state_dict.value;
+      return this.changeState(state_dict);
+    })
 
-      this.property_dict.editable = (field_json.editable === 1);
-      this.property_dict.name = field_json.key;
-      if (this.property_dict.editable) {
-        new_element = document.createElement('textarea');
-
-        new_element.value = value;
-        new_element.setAttribute('name', this.property_dict.name);
-        new_element.setAttribute('title', field_json.title);
-        if (field_json.required === 1) {
-          new_element.setAttribute('required', 'required');
-        }
-        /*
-        if (field_json.editable !== 1) {
-          textarea.setAttribute('readonly', 'readonly');
-          textarea.className += "ui-state-readonly";
-          // textarea.setAttribute('disabled', 'disabled');
-        }
-        */
-        this.property_dict.textarea_deferred.resolve(new_element);
+    .declareMethod('updateDOM', function () {
+      var element = this.element,
+        gadget = this,
+        url;
+      if (gadget.state.editable) {
+        url = 'gadget_html5_textarea.html';
       } else {
-        new_element = document.createElement('pre');
-        new_element.setAttribute("class",  "ui-content-non-editable");
-        new_element.textContent = value;
+        url = 'gadget_html5_element.html';
       }
-      this.property_dict.element.appendChild(new_element);
+      return this.declareGadget(url, {scope: 'sub'})
+        .push(function (input) {
+          // Clear first to DOM, append after to reduce flickering/manip
+          while (element.firstChild) {
+            element.removeChild(element.firstChild);
+          }
+          element.appendChild(input.element);
+          return input.render(gadget.state);
+        });
     })
 
     .declareMethod('getContent', function () {
-      var field,
-        value,
-        result = {};
-      if (this.property_dict.editable) {
-        field = this.property_dict.element.querySelector('textarea');
-        value = field.value;
-      } else {
-        field = this.property_dict.element.querySelector('pre');
-        value = field.textContent;
+      if (this.state.editable) {
+        return this.getDeclaredGadget('sub')
+          .push(function (gadget) {
+            return gadget.getContent();
+          });
       }
-      result[this.property_dict.name] = value;
-      return result;
+      return {};
     });
 
-}(window, rJS, document, RSVP, loopEventListener));
+}(window, rJS));
