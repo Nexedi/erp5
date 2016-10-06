894788243443b219b8cd0a16ff67957f7ce8647d
Ayush Tiwari <ayush.tiwari@nexedi.com>
2016-10-06 17:49:27 +0200

    ERP5 Base: Refactor addition of new objects for erp5 objects
    It's better to create new objects using same script rather than
    keeping 2 scripts that try to do the same thing: Folder_create
    (script used to add in case we create new object using the icon next to actions bar)
    did not support the getRedirectParameter API. We now do all the work
    related to adding new object in Folder_create itself.

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Folder_create.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Folder_create.py
@@ -1,12 +1,18 @@
 Base_translateString = context.getPortalObject().Base_translateString
-allowed_type_list = context.getVisibleAllowedContentTypeList()
 
-if not allowed_type_list:
-  return context.ERP5Site_redirect(context.absolute_url(), keep_items={'portal_status_message': Base_translateString("You are not allowed to add new content in this context.")})
+if not type_name:
+  allowed_type_list = context.getVisibleAllowedContentTypeList()
+  if not allowed_type_list:
+    return context.Base_redirect(keep_items={'portal_status_message':
+      Base_translateString("You are not allowed to add new content in this context.")})
+  # newContent will add the first allowed type when we do not specify portal_type=
+  type_name = allowed_type_list[0]
 
-# newContent will add the first allowed type when we do not specify portal_type=
-new_object = context.newContent(portal_type=allowed_type_list[0])
-
-return context.ERP5Site_redirect(new_object.absolute_url(),
-                                 keep_items={'portal_status_message': Base_translateString("Object created."),
-                                             'editable_mode': 1})
+if keep_items is None:
+  keep_items =  {}
+new_content = context.newContent(portal_type=type_name)
+keep_items['portal_status_message'] = Base_translateString("Object created.")
+keep_items['editable_mode'] = 1
+kw = new_content.getRedirectParameterDictAfterAdd(context, **kw)
+redirect_url = kw.pop('redirect_url', None)
+return new_content.Base_redirect(redirect_url, keep_items=keep_items, **kw)
