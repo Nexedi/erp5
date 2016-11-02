8efdd9f56a0d3815d1b03d185fa11253c1fb4de6
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2016-11-02 11:26:40 +0100

    erp5_base: fix conditino of create_user_action.
    This transition is now available when Person has no validated Login document.
    Person's reference is now set only when it is not yet set.

--- bt5/erp5_base/WorkflowTemplateItem/portal_workflow/user_account_workflow/scripts/UserAccountWorkflow_createUser.py
+++ bt5/erp5_base/WorkflowTemplateItem/portal_workflow/user_account_workflow/scripts/UserAccountWorkflow_createUser.py
@@ -1,7 +1,8 @@
 kwargs = state_change['kwargs']
 person = state_change['object']
 
-person.edit(reference=kwargs['reference'])
+if not person.hasReference():
+  person.edit(reference=kwargs['reference'])
 person.newContent(
   portal_type='ERP5 Login',
   password=kwargs['password'],
