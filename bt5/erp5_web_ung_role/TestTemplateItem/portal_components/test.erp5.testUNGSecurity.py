6517ae9e9829e3d22032fa79c08cb612b44c6ebe
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2016-11-10 00:15:40 +0100

    fixup! test: migrate to ERP5 Login authentication.

--- bt5/erp5_web_ung_role/TestTemplateItem/portal_components/test.erp5.testUNGSecurity.py
+++ bt5/erp5_web_ung_role/TestTemplateItem/portal_components/test.erp5.testUNGSecurity.py
@@ -51,6 +51,11 @@ class TestUNGSecurity(ERP5TypeTestCase):
     assignment = person.newContent(portal_type='Assignment')
     assignment.setFunction("function/ung_user")
     assignment.open()
+    login = person.newContent(
+      portal_type='ERP5 Login',
+      reference='ung_user',
+    )
+    login.validate()
     self.tic()
 
   def testERP5Site_createNewWebDocumentAsAnonymous(self):
@@ -82,6 +87,11 @@ class TestUNGSecurity(ERP5TypeTestCase):
     assignment = person.newContent(portal_type='Assignment')
     assignment.setFunction("function/ung_user")
     assignment.open()
+    login = person.newContent(
+      portal_type='ERP5 Login',
+      reference='ung_user2',
+    )
+    login.validate()
     self.tic()
     self.login("ung_user")
     self.changeSkin("UNGDoc")
@@ -173,6 +183,11 @@ class TestUNGSecurity(ERP5TypeTestCase):
     assignment = person.newContent(portal_type='Assignment')
     assignment.setFunction("function/ung_user")
     assignment.open()
+    login = person.newContent(
+      portal_type='ERP5 Login',
+      reference='ung_user2',
+    )
+    login.validate()
     self.tic()
     self.login("ung_user")
     self.changeSkin("UNGDoc")
