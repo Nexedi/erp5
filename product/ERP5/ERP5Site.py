b59b52d4f7c6b99788c1cdc2dd0d09c7e353981a
Ayush Tiwari <ayush.tiwari@nexedi.com>
2016-11-15 10:52:59 +0000

    erp5_catalog: Solve name conflict between isIndexable property for ERP5Site and ERP5 Catalog
    Earlier, In ERP5Site, we use property 'isIndexable' to have control over indexing and unindexing.
    This was usefull specially in case of bootstrap. To have this control, we had a function
    `isIndexable` in SQLCatalog which we used to switch for enabling-disabling indexation. (769f9fb21febbd)
    
    Now, we do have ERP5 Catalog, which itself is an ERP5 Object, hence it has an accessor `isIndexable`
    of itself which we plan to disable for now(as we don't want circular dependency while indexing
    catalog in itself). But as we did have a function `isIndexable` in SQLCatalog which had control
    over the indexing, we needed to rename this property for ERP5Site.
    
    Hence, now we use property 'isIndexingRequired' for ERP5Site and use to have overall control
    over indexing process.
    
    Please note, we do not intend to change the same property name for any other object. Its just
    for ERP5Site.

--- product/ERP5/ERP5Site.py
+++ product/ERP5/ERP5Site.py
@@ -235,7 +235,7 @@ class ERP5Site(FolderMixIn, CMFSite, CacheCookieMixin):
   last_id = 0
   icon = 'portal.gif'
   # Default value, prevents error during upgrade
-  isIndexable = ConstantGetter('isIndexable', value=True)
+  isIndexingRequired = ConstantGetter('isIndexingRequired', value=True)
 
   _properties = (
       { 'id':'title',
@@ -1959,7 +1959,7 @@ class ERP5Generator(PortalGenerator):
     portal = self.klass(id=id)
     # Make sure reindex will not be called until business templates
     # will be installed
-    setattr(portal, 'isIndexable', ConstantGetter('isIndexable', value=False))
+    setattr(portal, 'isIndexingRequired', ConstantGetter('isIndexingRequired', value=False))
 
     # This is only used to refine log level.
     # Has no functional use, and should never have any:
@@ -2251,7 +2251,7 @@ class ERP5Generator(PortalGenerator):
     # When no SQL connection was define on the site,
     # we don't want to make it crash
     if p.erp5_sql_connection_type is not None:
-      setattr(p, 'isIndexable', ConstantGetter('isIndexable', value=True))
+      setattr(p, 'isIndexingRequired', ConstantGetter('isIndexingRequired', value=True))
       # Clear portal ids sql table, like this we do not take
       # ids for a previously created web site
       p.portal_ids.clearGenerator(all=True)
