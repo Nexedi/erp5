da31b0edb762ef4e78c4b0b729ff59728f955aff
Vincent Pelletier <vincent@nexedi.com>
2016-12-08 09:31:03 +0100

    erp5_web_ung_theme: migrate to ERP5 Login authentication.

--- bt5/erp5_web_ung_theme/SkinTemplateItem/portal_skins/erp5_web_ung_theme/ERPSite_createUNGUser.py
+++ bt5/erp5_web_ung_theme/SkinTemplateItem/portal_skins/erp5_web_ung_theme/ERPSite_createUNGUser.py
@@ -11,14 +11,17 @@ person = portal.person_module.newContent(portal_type="Person")
 person.edit(first_name=form.get("firstname"),
             last_name=form.get("lastname"),
             email_text=form.get("email"),
-            password=form.get("password"),
-            reference=login,
+            reference=login, # BBB
 )
 
 assignment = person.newContent(portal_type='Assignment')
 assignment.setFunction("ung_user")
 assignment.open()
 
+login = person.newContent(portal_type='ERP5 Login',
+                          password=form.get("password"),
+                          reference=login)
+login.validate()
 person.validate()
 
 return json.dumps(True)
