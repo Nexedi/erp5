130a8d4cd56f7f4d1a919fa7d214aee0f8f8e7ea
Jérome Perrin <jerome@nexedi.com>
2017-01-05 09:58:10 +0000

    crm: fix Ticket_getArrowItemList returning same persons multiple times
    Fixes [#20161227-D1FA4](https://nexedi.erp5.net/bug_module/20161227-D1FA4)
    
    Also modernize code by using set() and not using
    ERP5Site_getAuthenticatedMemberPersonValue

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_getArrowItemList.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_getArrowItemList.py
@@ -1,23 +1,18 @@
 portal = context.getPortalObject()
 arrow = portal.portal_property_sheets.get("Arrow")
 
-result = [('', ''),]
-result_append = result.append
+result = set([('', ''),])
 
 # add current user
-user = portal.ERP5Site_getAuthenticatedMemberPersonValue()
+user = portal.portal_membership.getAuthenticatedMember().getUserValue()
 if user is not None:
-  result_append((user.getTitle(), user.getRelativeUrl()))
+  result.add((user.getTitle(), user.getRelativeUrl()))
 
 for property_value in arrow.contentValues():
   value_list = context.getProperty("%s_value_list" %property_value.getReference(), None)
   if value_list is not None:
     for value in value_list:
-      if value and value.getPortalType() in portal.getPortalNodeTypeList():
-        value = (value.getTitle(), value.getRelativeUrl())
-        if value not in value_list:
-          result_append(value)
+      if value is not None and value.getPortalType() in portal.getPortalNodeTypeList():
+        result.add((value.getTitle(), value.getRelativeUrl()))
 
-result.sort(key=lambda x: x[0])
-
-return result
+return sorted(result)
