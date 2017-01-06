a38331828ab5f2c91eb6b1c258561e6e5fa78033
Vincent Pelletier <vincent@nexedi.com>
2017-01-06 17:24:10 +0900

    erp5_base: Do not rely on the absence of surprising user ids.

--- bt5/erp5_base/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeOwnerTitleSearchQuery.py
+++ bt5/erp5_base/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeOwnerTitleSearchQuery.py
@@ -1,8 +1,11 @@
 from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
-return SimpleQuery(owner=[
+owner = [
   x.user_id for x in
   context.getPortalObject().portal_catalog(
     select_list=['user_id'],
     title=value,
   )
-] or value or -1)
+] or value
+if owner:
+  return SimpleQuery(owner=owner)
+return SimpleQuery(uid=-1)
