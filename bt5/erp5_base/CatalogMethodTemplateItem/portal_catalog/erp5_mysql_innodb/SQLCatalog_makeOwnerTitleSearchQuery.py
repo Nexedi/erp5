73b4299a40e7a5ad936d591b955ffed7aa4915c1
Vincent Pelletier <vincent@nexedi.com>
2017-01-06 17:17:34 +0900

    erp5_base: Fix owner_title scriptable key.
    This was broken since the use of user_id on Person.
    Also, add a test.

--- bt5/erp5_base/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeOwnerTitleSearchQuery.py
+++ bt5/erp5_base/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeOwnerTitleSearchQuery.py
@@ -1,11 +1,8 @@
 from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
-
-portal = context.getPortalObject()
-
-reference_list = [x.reference for x in
-                  portal.portal_catalog(SimpleQuery(reference=None,
-                                                    comparison_operator="is not"),
-                                        select_list=['reference'],
-                                        portal_type="Person", title=value)]
-
-return SimpleQuery(owner=reference_list or value or -1)
+return SimpleQuery(owner=[
+  x.user_id for x in
+  context.getPortalObject().portal_catalog(
+    select_list=['user_id'],
+    title=value,
+  )
+] or value or -1)
