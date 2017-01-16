368c536d61547a24de4821720332dae94f32205a
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2017-01-16 14:17:06 +0100

    erp5_payment_mean: add 'ledger' domain tree to PaymentTransactionGroup_view

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineList.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineList.py
@@ -1,14 +1,11 @@
 portal = context.getPortalObject()
 
-kw.update({
-  'parent_portal_type':'Payment Transaction',
-  'section_uid':context.getSourceSectionUid(),
-  'default_aggregate_uid':context.getUid(),
-})
+kw['section_uid'] = context.getSourceSectionUid()
+kw['default_aggregate_uid'] = context.getUid()
+kw['parent_portal_type'] = ('Payment Transaction', 'Accounting Transaction')
 
 if context.getSourcePayment():
   precision = context.getQuantityPrecisionFromResource(
     context.getSourcePaymentValue().getPriceCurrency())
   portal.REQUEST.set('precision', precision)
-
 return portal.portal_simulation.getMovementHistoryList(**kw)
