d93e6f7487931d6838479848f48cf9d3f7567ff9
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2017-01-16 14:17:06 +0100

    erp5_payment_mean: add 'ledger' domain tree to PaymentTransactionGroup_view

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineCountAndStat.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getPaymentTransactionLineCountAndStat.py
@@ -4,6 +4,7 @@ cache_key = context.getId() + '_' + script.id
 try:
   return cache[cache_key]
 except KeyError:
+  params = {'selection_domain': context.portal_selections.getSelectionDomainDictFor(selection_name)} if selection_name else {}
   row, = portal.portal_simulation.getInventoryList(
     select_dict={'count': 'COUNT(*)'},
     ignore_group_by=1,
@@ -13,6 +14,7 @@ except KeyError:
       portal_type='Account',
       strict_account_type_uid=portal.portal_categories.account_type.asset.cash.bank.getUid(),
     )],
+    **params
   )
   cache[cache_key] = result = (row.count, row.total_quantity)
   return result
