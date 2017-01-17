3fa7c03e5905fb9a61f81fc36930316ace77969f
Ayush Tiwari <ayush.tiwari@nexedi.com>
2017-01-17 14:57:12 +0000

    bt5_prototype: Change erp5_mysql_innodb_catalog from bt5 to bp5

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z0_uncatalog_transformation.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM transformation
-WHERE <dtml-sqltest uid op=eq type=int>
--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_search_security.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  DISTINCT uid
-FROM 
-  roles_and_users
-WHERE 
-  allowedRolesAndUsers 
-  IN (<dtml-in security_roles_list><dtml-var sequence-item><dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
