6481a3ac97669cd39fce3505964ae4f6adc61d00
Ayush Tiwari <ayush.tiwari@nexedi.com>
2017-01-19 10:04:13 +0000

    bt5_prototype: Remove properties and objects installed by other bt5

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/PathTemplatePackageItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferFullTextIndex.py
+++
@@ -1,2 +0,0 @@
-# This script is called to defer fulltext indexing in a lower priority.
-context.activate(activity='SQLQueue', priority=4, group_method_id=None).SQLCatalog_deferFullTextIndexActivity(path_list=list(getPath))
