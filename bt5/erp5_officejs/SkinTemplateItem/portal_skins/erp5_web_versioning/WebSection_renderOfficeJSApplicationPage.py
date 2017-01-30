adbe033896fcfaf25016ce4c7426e8c100aa86b3
Vincent Bechu <vincent.bechu@nexedi.com>
2017-01-30 13:24:28 +0000

    erp5_officejs: add gadget_bootloader to officejs.

---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_versioning/WebSection_renderOfficeJSApplicationPage.py
@@ -0,0 +1,18 @@
+if REQUEST is None:
+  REQUEST = context.REQUEST
+if response is None:
+  response = REQUEST.RESPONSE
+
+default_web_page = context
+web_section = REQUEST.get("current_web_section")
+#raise ValueError(web_section.getLayoutProperty("configuration_latest_version", default="development"))
+
+return default_web_page.WebPage_viewAsWeb(mapping_dict={
+  "latest_version": web_section.getLayoutProperty("configuration_latest_version", default="development"),
+  "latest_document_version": web_section.getLayoutProperty("configuration_latest_document_version", default=context.getWebSiteValue().getId() + "-dev"),
+  "application_appcache": web_section.getId() + ".appcache",
+  "redirect_url": web_section.getLayoutProperty("configuration_redirect_url", default=""),
+  "cache_file": web_section.getLayoutProperty("configuration_cache_file", default=""),
+  "application_name": web_section.getTitle(),
+  "sub_gadget_installer": web_section.getLayoutProperty("configuration_sub_gadget_installer", default="")
+})
