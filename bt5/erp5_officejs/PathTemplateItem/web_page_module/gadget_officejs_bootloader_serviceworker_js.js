18449e47b83b0d31c96f4d856a8c4388fde924dd
Vincent Bechu <vincent.bechu@nexedi.com>
2017-01-30 13:24:28 +0000

    erp5_officejs: add gadget_bootloader to officejs.

---
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_bootloader_serviceworker_js.js
@@ -0,0 +1,148 @@
+/*jslint indent: 2*/
+/*global self, fetch, Request, Response */
+var global = self, window = self;
+
+(function (self, fetch, Request, Response) {
+  "use strict";
+
+  self.DOMParser = {};
+  self.sessionStorage = {};
+  self.localStorage = {};
+  self.openDatabase = {};
+  self.DOMError = {};
+  self.postMessage = function () {return; };
+
+  self.importScripts('rsvp.js', 'jiodev.js');
+
+  self.storage = {};
+
+  function createStorage(database) {
+    return self.jIO.createJIO({
+      type: "uuid",
+      sub_storage: {
+        type: "indexeddb",
+        database: database
+      }
+    });
+  }
+
+  self.setting_storage = createStorage("serviceWorker_settings");
+
+  self.addEventListener("message", function (event) {
+
+    event.waitUntil(new self.RSVP.Queue()
+      .push(function () {
+        var data = JSON.parse(event.data);
+
+        if (data.action === "install" &&
+            data.url_list !== undefined) {
+
+          self.storage = createStorage(self.registration.scope);
+          return new self.RSVP.Queue()
+            .push(function () {
+              var promise_list = [];
+              data.url_list.map(function (url) {
+                promise_list.push(
+                  new self.RSVP.Queue()
+                    .push(function () {
+                      return self.storage.get(url);
+                    })
+                    .push(undefined, function () {
+                      return new self.RSVP.Queue()
+                        .push(function () {
+                          return fetch(new Request(url));
+                        })
+                        .push(function (response) {
+                          if (response.status === 200) {
+                            return self.RSVP.all([
+                              self.storage.put(
+                                url,
+                                {"content_type": "blob"}
+                              ),
+                              response.blob()
+                            ]);
+                          }
+                          throw new Error(response.statusText);
+                        })
+                        .push(function (result) {
+                          return self.storage.putAttachment(
+                            url,
+                            "body",
+                            result[1]
+                          );
+                        })
+                        .push(function () {
+                          self.console.log("Saved: ", url);
+                        })
+                        .push(undefined, function (error) {
+                          event.ports[0].postMessage(error);
+                        });
+                    })
+                );
+              });
+              return self.RSVP.all(promise_list);
+            })
+            .push(function () {
+              event.ports[0].postMessage("success");
+            });
+        }
+      }));
+  });
+
+  self.addEventListener('install', function (event) {
+    event.waitUntil(self.skipWaiting());
+  });
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(self.clients.claim());
+  });
+
+  self.addEventListener("fetch", function (event) {
+    var relative_url = event.request.url.replace(self.registration.scope, "")
+      .replace(self.version_url, "");
+
+    event.respondWith(
+      new self.RSVP.Queue()
+        .push(function () {
+          if (relative_url === "") {
+            return self.setting_storage.get(self.registration.scope)
+              .push(function (doc) {
+                relative_url = doc.landing_page || "/";
+              });
+          }
+        })
+        .push(function () {
+          if (self.storage.get === undefined) {
+            self.storage = createStorage(self.registration.scope);
+          }
+          return self.storage.get(relative_url);
+        })
+        .push(function (doc) {
+          if (doc.content_type !== "blob") {
+            return new Response(doc.text_content, {
+              'headers': {
+                'content-type': doc.content_type
+              }
+            });
+          }
+          return self.storage.getAttachment(relative_url, "body")
+            .push(function (blob) {
+              return new Response(blob, {
+                'headers': {
+                  'content-type': blob.type
+                }
+              });
+            });
+        })
+        .push(undefined, function (error) {
+          self.console.log(
+            "Relative_Url: ",
+            relative_url,
+            "\nCause: ",
+            error.message
+          );
+          return fetch(event.request);
+        })
+    );
+  });
+
+}(self, fetch, Request, Response));
