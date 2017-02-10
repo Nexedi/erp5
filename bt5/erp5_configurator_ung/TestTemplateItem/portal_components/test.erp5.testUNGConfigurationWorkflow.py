aea163cda2616a3508c68c30836276b5f3c86d3c
Wenjie Zheng <wenjie.zheng@tiolive.com>
2017-02-10 15:59:25 +0100

    ERP5Workflow: changes related to the configurators

--- bt5/erp5_configurator_ung/TestTemplateItem/portal_components/test.erp5.testUNGConfigurationWorkflow.py
+++ bt5/erp5_configurator_ung/TestTemplateItem/portal_components/test.erp5.testUNGConfigurationWorkflow.py
@@ -183,26 +183,26 @@ class TestUNGConfiguratorWorkflowMixin(TestLiveConfiguratorWorkflowMixin):
     sequence.edit(next_dict=next_dict)
 
   def stepCheckMultipleUserAccountThreeBrazil(self, sequence=None, sequence_list=None, **kw):
-     """ Check if the users were created correctly """
-     business_configuration = sequence.get("business_configuration")
-     person_config_save = business_configuration["5"]
-     person_config_item = person_config_save["1"]
-     self.assertEqual(person_config_item.getReference(), "person_creator")
-     person_config_item = person_config_save["2"]
-     self.assertEqual(person_config_item.getReference(), "person_assignee")
-     person_config_item = person_config_save["3"]
-     self.assertEqual(person_config_item.getReference(), "person_assignor")
+    """ Check if the users were created correctly """
+    business_configuration = sequence.get("business_configuration")
+    person_config_save = business_configuration["5"]
+    person_config_item = person_config_save["1"]
+    self.assertEqual(person_config_item.getReference(), "person_creator")
+    person_config_item = person_config_save["2"]
+    self.assertEqual(person_config_item.getReference(), "person_assignee")
+    person_config_item = person_config_save["3"]
+    self.assertEqual(person_config_item.getReference(), "person_assignor")
 
   def stepCheckMultipleUserAccountThreeFrance(self, sequence=None, sequence_list=None, **kw):
-     """ Check if the users were created correctly """
-     business_configuration = sequence.get("business_configuration")
-     person_config_save = business_configuration["5"]
-     person_config_item = person_config_save["1"]
-     self.assertEqual(person_config_item.getReference(), "french_creator")
-     person_config_item = person_config_save["2"]
-     self.assertEqual(person_config_item.getReference(), "french_assignee")
-     person_config_item = person_config_save["3"]
-     self.assertEqual(person_config_item.getReference(), "french_assignor")
+    """ Check if the users were created correctly """
+    business_configuration = sequence.get("business_configuration")
+    person_config_save = business_configuration["5"]
+    person_config_item = person_config_save["1"]
+    self.assertEqual(person_config_item.getReference(), "french_creator")
+    person_config_item = person_config_save["2"]
+    self.assertEqual(person_config_item.getReference(), "french_assignee")
+    person_config_item = person_config_save["3"]
+    self.assertEqual(person_config_item.getReference(), "french_assignor")
 
   def stepCheckConfigureInstallationForm(self, sequence=None, sequence_list=None, **kw):
     """ Check the installation form """
