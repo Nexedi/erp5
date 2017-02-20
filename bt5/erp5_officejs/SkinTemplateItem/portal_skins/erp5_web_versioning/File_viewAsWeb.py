1e4d96e43db112295fa5c11ba5ae29c0c7421d7d
Vincent Bechu <vincent.bechu@nexedi.com>
2017-02-20 14:17:05 +0000

    erp5_officejs: remove officejs portal_skin depencies

---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_versioning/File_viewAsWeb.py
@@ -0,0 +1,18 @@
+if REQUEST is None:
+  REQUEST = context.REQUEST
+if response is None:
+  response = REQUEST.RESPONSE
+file = context
+file_content = file.getData()
+
+if REQUEST.getHeader('If-Modified-Since', '') == file.getModificationDate().rfc822():
+  response.setStatus(304)
+  return ""
+
+#file_content_type = file.getContentType()
+#if file_content_type is None:
+#  file_content_type = 'application/octet-stream'
+
+response.setHeader('Content-Type', (file.getContentType() or 'application/octet-stream'))
+
+return file_content
---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_versioning/WebPage_viewAsWeb.py
@@ -0,0 +1,52 @@
+if REQUEST is None:
+  REQUEST = context.REQUEST
+if response is None:
+  response = REQUEST.RESPONSE
+
+web_page = context
+web_section = REQUEST.get("current_web_section")
+
+if REQUEST.getHeader('If-Modified-Since', '') == web_page.getModificationDate().rfc822():
+  response.setStatus(304)
+  return ""
+
+portal_type = web_page.getPortalType()
+if portal_type in ("Web Page", "Web Script", "Web Manifest", "Web Style"):
+  web_content = web_page.getTextContent()
+else:
+  if portal_type == "Web Section":
+    web_page = web_page.getAggregateValue()
+  response.setHeader('Content-Type', web_page.getContentType())
+  web_content = web_page.getData()
+
+# set headers depending on type of script
+if (portal_type == "Web Script"):
+  response.setHeader('Content-Type', 'application/javascript')
+
+elif (portal_type == "Web Style"):
+  response.setHeader('Content-Type', 'text/css')
+
+elif (portal_type == "Web Manifest"):
+  response.setHeader('Content-Type', 'text/cache-manifest')
+
+elif (portal_type == "Web Page"):
+  if (mapping_dict is not None):
+    web_content = web_page.TextDocument_substituteTextContent(web_content, mapping_dict=mapping_dict)
+
+  content_security_policy = "default-src 'self' data: blob: *"
+  x_frame_options = "SAMEORIGIN"
+  if (web_section):
+    content_security_policy = web_section.getLayoutProperty("configuration_content_security_policy", default=content_security_policy)
+    x_frame_options = web_section.getLayoutProperty("configuration_x_frame_options", default=x_frame_options)
+
+  # Do not allow to put inside an iframe
+  if not x_frame_options == "ALLOW-FROM-ALL":
+    response.setHeader("X-Frame-Options", x_frame_options)
+  response.setHeader("X-Content-Type-Options", "nosniff")
+
+  # Only fetch code (html, js, css, image) and data from this ERP5, to prevent any data leak as the web site do not control the gadget's code
+  response.setHeader("Content-Security-Policy", content_security_policy)
+
+  response.setHeader('Content-Type', web_page.getContentType('text/html'))
+
+return web_content
