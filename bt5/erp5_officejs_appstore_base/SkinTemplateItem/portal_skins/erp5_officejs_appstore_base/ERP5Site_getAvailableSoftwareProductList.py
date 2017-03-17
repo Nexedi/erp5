8c82ba1b4e2c240006db3cefdfcdd88e60fd091d
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-03-17 17:22:36 +0000

    erp5_officejs_appstore_base: Improve upload process and move to zip
    erp5_officejs_appstore_base: Fix Improve Publication process.
    
    * Only one submit alarm is run at a time
    * Move creation dialog to Software Publication for consistency
    * Person is a contributor for the web document of its application
    
    erp5_officejs_appstore_base: Update to use Static Web Section, remove RJS versionning
    
    erp5_officejs_appstore_base: Various improvment for ERP5JS compatibility
    
    erp5_officejs_appstore_base: Add Action to attach Software Product to Software Publication
    
    erp5_officejs_appstore_base: Update to use version in reference
    
    erp5_officejs_appstore_base: Update upload dialog to have all in one place + use product line category

---
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/ERP5Site_getAvailableSoftwareProductList.py
@@ -0,0 +1,6 @@
+return [('', '')] + [(x.getTitle(), x.relative_url) for x in context.getPortalObject().portal_catalog(
+  portal_type="Software Product",
+  # Is validation state necessary here?
+  # validation_state="validated",
+  select_list=("relative_url")
+  )]
