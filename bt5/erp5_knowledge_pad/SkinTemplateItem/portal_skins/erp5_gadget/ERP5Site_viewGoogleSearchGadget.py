90df3380324228a4cccbb0ca30d2f670c89015b7
Jérome Perrin <jerome@nexedi.com>
2017-03-24 08:18:51 +0000

    knowledge_pad: remove google gadgets
    Google stopped providing this service
    
    testBasicGadgetSystem and testGadgetEditPreferences from
    erp5_knowledge_pad_ui_test have been updated to rely other on other
    gadgets to test gadget framework on the frontpage.

--- bt5/erp5_knowledge_pad/SkinTemplateItem/portal_skins/erp5_gadget/ERP5Site_viewGoogleSearchGadget.py
+++
@@ -1,8 +0,0 @@
-preferences = box.KnowledgeBox_getDefaultPreferencesDict()
-h = str(preferences.get('preferred_height'))
-w = str(preferences.get('preferred_width'))
-t = str(preferences.get('preferred_title'))
-
-s = """<script type="text/javascript" src="http://www.gmodules.com/ig/ifr?url=http://hosting.gmodules.com/ig/gadgets/file/107548476258517015585/google.xml&amp;synd=open&amp;w=""" + w + """&amp;h=""" + h + """&amp;title=""" + t + """&amp;border=%23ffffff%7C3px%2C1px+solid+%23999999&amp;output=js"></script>"""
-
-return s
