d1c27dae2bb30c172edb12a6a037c5f6682d8147
Jérome Perrin <jerome@nexedi.com>
2017-03-30 04:48:24 +0000

    system_event: document how HTTP Exchange works

--- bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/HTTPExchange_setFollowUpAndInquiry.py
+++ bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/HTTPExchange_setFollowUpAndInquiry.py
@@ -1,4 +1,22 @@
-assert context.getFollowUp() is None, context.getFollowUp()
+"""Call HTTPExchange_getFollowUpFor* to find the document to use as follow up
+of this HTTP Exchange and call inquiry type-based method on this document.
+
+To use HTTP Exchange framework:
+ * you must have created categories in portal_categories/http_exchange_resource
+ * your "entry point" script must create an HTTP Exchange using an HTTP Exchange
+   Resource as resource category and confirm this HTTP Exchange.
+   In this entry point script, you should also set source and destination categories
+   if it helps traceability.
+   In this entry point script, you should set request and/or response properties.
+ * you must create HTTPExchange_getFollowUpFor${HTTP Exchange Resource Codification}
+   configuration script that will be called on the context of the HTTP Exchange and
+   will be responsible for returning the document on which inquiry will be done.
+   This script can create a new document and return it to use as follow up if applicable.
+ * you must create a `inquiry` type based method that will be called on this follow up
+   document, recieving the HTTP Exchange as argument.
+
+"""
+assert context.hasFollowUp(), context.getFollowUp()
 
 http_exchange_resource = context.getPortalObject().portal_categories.http_exchange_resource
 resource = context.getResourceValue()
