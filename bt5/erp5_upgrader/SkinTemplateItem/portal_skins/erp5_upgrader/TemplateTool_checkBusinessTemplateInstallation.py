6b08dd43aaf00c4df67ef11edad0b74c6d0101c0
Jérome Perrin <jerome@nexedi.com>
2017-03-31 07:39:19 +0000

    upgrader: upgrade catalog schema right after installing BTs
    If updating business templates brings new tables that are required for
    catalog / alarm system to work, then we may not be able to execute the
    post upgrade alarm, so TemplateToolTableConsistencyConstraint won't be
    able to update the schema.

--- bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/TemplateTool_checkBusinessTemplateInstallation.py
+++ bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/TemplateTool_checkBusinessTemplateInstallation.py
@@ -1,7 +1,19 @@
 bt5_list, keep_bt5_id_set = context.Base_getUpgradeBusinessTemplateList()
 
 template_tool = context.getPortalObject().portal_templates
-return template_tool.upgradeSite(bt5_list,
+
+# First, upgrade business templates
+message_list = template_tool.upgradeSite(bt5_list,
   delete_orphaned=True,
   keep_bt5_id_set=keep_bt5_id_set,
   dry_run=(not fixit))
+
+if fixit:
+  # if We have installed some business templates, we must also update
+  # the catalog tables structures in the same transaction, because these tables
+  # might be required for alarm framework to work and we may not be able to
+  # execute the post upgrade alarm.
+  message_list.extend(
+    template_tool.TemplateTool_checkTableConsistency(fixit=fixit))
+
+return message_list
