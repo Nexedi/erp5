e47b776443f3f505457bf8ee7f0ae909d8fd925f
Klaus Wölfel <klaus@nexedi.com>
2017-04-04 19:44:29 +0000

    enable data analysis as causality for other data analysis and adding input and output parameter as dict with additional information

--- wendelin/bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataAnalysis_executeDataOperation.py
+++ wendelin/bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataAnalysis_executeDataOperation.py
@@ -1,3 +1,4 @@
+portal = context.getPortalObject()
 operation = None
 use = None
 parameter_dict = {}
@@ -14,21 +15,30 @@ for analysis_line in context.objectValues(portal_type="Data Analysis Line"):
     operation_analysis_line = analysis_line
     operation = analysis_line.getResourceValue()
   else:
-    reference = analysis_line.getReference()
-    aggregate = analysis_line.getAggregateDataSinkValue() or \
-                analysis_line.getAggregateDataArrayValue() or \
-                analysis_line.getAggregateDataDescriptorValue()
-    parameter_dict[reference] = aggregate
-    progress_indicator = analysis_line.getAggregateProgressIndicatorValue()
-    if progress_indicator is not None:
-      parameter_dict["%s_progress_indicator" %reference] = progress_indicator
+    parameter = {}
+    for portal_type in ["Data Array", "Progress Indicator"] + \
+                        list(portal.getPortalDataSinkTypeList()) + \
+                        list(portal.getPortalDataDescriptorTypeList()):
+      value = analysis_line.getAggregateValue(portal_type=portal_type)
+      if value is not None:
+        parameter[portal_type] = value
+
     for base_category in analysis_line.getVariationRangeBaseCategoryList():
-      parameter_dict["%s_%s" %(reference, base_category)] = \
-          analysis_line.getVariationCategoryItemList(
-              base_category_list=(base_category,))[0][0]
+      parameter[base_category] = analysis_line.getVariationCategoryItemList(
+                                   base_category_list=(base_category,))[0][0]
+    reference = analysis_line.getReference()
+    # several lines with same reference wil turn the parameter into a list
+    if reference in parameter_dict:
+      if not isinstance(parameter_dict[reference], list):
+        parameter_dict[reference] = [parameter_dict[reference]]
+      parameter_dict[reference].append(parameter)
+    else:
+      parameter_dict[reference] = parameter
 script_id = operation.getScriptId()
-getattr(operation_analysis_line, script_id)(**parameter_dict)
+out = getattr(operation_analysis_line, script_id)(**parameter_dict)
 
 # only stop batch ingestions
 if use == "big_data/ingestion/batch":
   context.stop()
+
+return out
