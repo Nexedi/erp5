c8c19219365a7ee85bf1d44e6ff26559fde3ba10
Vincent Bechu <vincent.bechu@nexedi.com>
2017-04-06 14:23:14 +0000

    erp5_officejs: add web site officejs_export prototype

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_jio_appcachestorage_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_jio_appcachestorage_js.js
@@ -5,13 +5,15 @@
 
   function AppCacheStorage(spec) {
     this._manifest = spec.manifest;
-    this._url = window.location.origin + window.location.pathname +
+    this._origin_url = spec.origin_url !== undefined ? spec.origin_url :
+      (window.location.origin + window.location.pathname +
       (window.location.pathname.endsWith('/') ? '' : '/') +
-      spec.version + (spec.version.endsWith('/') ? '' : '/');
-    this._url_list = ["/"];
+      ((spec.version !== undefined) ?
+      (spec.version + (spec.version.endsWith('/') ? '' : '/')) : ""));
+    this._relative_url_list = [this._origin_url, spec.manifest];
   }
 
-  AppCacheStorage.prototype.get = function (url) {
+  AppCacheStorage.prototype.get = function (id) {
     return {};
   };
 
@@ -21,74 +23,64 @@
 
   AppCacheStorage.prototype.getAttachment = function (doc_id, attachment_id) {
     var storage = this, url = attachment_id;
-    if (this._url === doc_id) {
-      return new RSVP.Queue()
-      .push(function () {
-        return jIO.util.ajax({
-          type: "GET",
-          url: (url.startsWith("http") || url.startsWith("//")) ?
-            url : storage._url + url,
-          dataType: "blob"
-        });
-      })
-      .push(function (result) {
-        return result.target.response;
-      })
-      .push(undefined, function (error) {
-        if (error.target.status === 404) {
-          throw new jIO.util.jIOError("Can't find attachment: " + url, 404);
-        }
-        throw error;
+    return new RSVP.Queue()
+    .push(function () {
+      return jIO.util.ajax({
+        type: "GET",
+        url: (url.startsWith("http") || url.startsWith("//")) ?
+          url : storage._origin_url + url,
+        dataType: "blob"
       });
-    }
-    throw new jIO.util.jIOError("Can not find " + doc_id, 404);
+    })
+    .push(function (result) {
+      return result.target.response;
+    });
   };
 
   AppCacheStorage.prototype.allAttachments = function (url) {
-    var result = {}, i, len = this._url_list.length;
+    var result = {}, i, len = this._relative_url_list.length;
     for (i = 0; i < len; i += 1) {
-      result[this._url_list[i]] = {};
+      result[this._relative_url_list[i]] = {};
     }
     return result;
   };
 
   AppCacheStorage.prototype.buildQuery = function (options) {
-    return [{id: this._url, doc: {}, value: {}}];
+    return [{id: "/", doc: {}, value: {}}];
   };
 
   AppCacheStorage.prototype.repair = function () {
     var storage = this;
-    console.log("start getting cache");
     return new RSVP.Queue()
       .push(function () {
         return jIO.util.ajax({
           type: "GET",
-          url: storage._url + storage._manifest
+          url: storage._origin_url + storage._manifest
         });
       })
       .push(function (response) {
         var text = response.target.responseText,
-          url_list = text.split('\r\n'),
+          relative_url_list = text.split('\r\n'),
           i,
           take = false;
-        if (url_list.length === 1) {
-          url_list = text.split('\n');
+        if (relative_url_list.length === 1) {
+          relative_url_list = text.split('\n');
         }
-        if (url_list.length === 1) {
-          url_list = text.split('\r');
+        if (relative_url_list.length === 1) {
+          relative_url_list = text.split('\r');
         }
-        for (i = 0; i < url_list.length; i += 1) {
-          if (url_list[i].indexOf("NETWORK:") >= 0) {
+        for (i = 0; i < relative_url_list.length; i += 1) {
+          if (relative_url_list[i].indexOf("NETWORK:") >= 0) {
             take = false;
           }
           if (take &&
-              url_list[i] !== "" &&
-              url_list[i].charAt(0) !== '#' &&
-              url_list[i].charAt(0) !== ' ') {
-            url_list[i].replace("\r", "");
-            storage._url_list.push(url_list[i]);
+              relative_url_list[i] !== "" &&
+              relative_url_list[i].charAt(0) !== '#' &&
+              relative_url_list[i].charAt(0) !== ' ') {
+            relative_url_list[i].replace("\r", "");
+            storage._relative_url_list.push(relative_url_list[i]);
           }
-          if (url_list[i].indexOf("CACHE:") >= 0) {
+          if (relative_url_list[i].indexOf("CACHE:") >= 0) {
             take = true;
           }
         }
