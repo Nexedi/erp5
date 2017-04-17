cbd40e626ea121bcf8b00b632f11ddbe302133d7
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2017-04-17 11:15:25 +0200

    erp5_payroll_l10_fr: no need to re-generate DSN just for changing sending mode
    Sending mode is set by user when printing the DSN report

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/DSNReport_printAsFile.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/DSNReport_printAsFile.py
@@ -1,6 +1,13 @@
-portal = context.getPortalObject()
-
 data = context.getTextContent().decode('utf-8').encode('iso-8859-1')
+
+# Update sending mode "on the fly"
+dsn_line_list = data.split('\n')
+for line_number, dsn_line in enumerate(dsn_line_list):
+  if dsn_line.split(',', 1)[0] == 'S10.G00.00.005':
+    dsn_line_list[line_number] = 'S10.G00.00.005,\'%s\'' % sending_mode
+
+data = '\n'.join(dsn_line_list)
+
 filename = context.getTitle()
 
 RESPONSE.setHeader("Content-Type", "text/plain; charset=iso-8859-1")
