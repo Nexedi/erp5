dcbb8ba650c5f15caf7fcc00cb436a790c33002c
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2017-05-03 14:46:06 +0200

    erp5_mysql_innodb_catalog: add safety LIMIT in z_getitem_by_path and z_getitem_by_uid to avoid fetching all rows in case of empty argument.

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_getitem_by_path.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_getitem_by_path.sql
@@ -1,10 +1,10 @@
 SELECT <dtml-if uid_only>uid<dtml-else>uid,path</dtml-if> from catalog 
 WHERE 
-  1 = 1
-<dtml-if path>
-  AND <dtml-sqltest path op=eq type="string">
-</dtml-if>
 <dtml-if path_list>
-  AND path IN (<dtml-in path_list><dtml-sqlvar sequence-item type="string">
-               <dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
+  path IN (<dtml-in path_list><dtml-sqlvar sequence-item type="string">
+           <dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
+  LIMIT <dtml-sqlvar expr="len(path_list)" type="int">
+<dtml-else>
+  <dtml-sqltest path op=eq type="string">
+  LIMIT 1
 </dtml-if>
--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_getitem_by_uid.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_getitem_by_uid.sql
@@ -1,10 +1,10 @@
 SELECT <dtml-if path_only>path<dtml-else>uid,path</dtml-if> from catalog 
 WHERE 
-  1 = 1
-<dtml-if expr="uid is not None">
-  AND <dtml-sqltest uid op=eq type="int">
-</dtml-if>
 <dtml-if uid_list>
-  AND uid IN (<dtml-in uid_list><dtml-sqlvar sequence-item type="int">
-               <dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
+  uid IN (<dtml-in uid_list><dtml-sqlvar sequence-item type="int">
+          <dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
+  LIMIT <dtml-sqlvar expr="len(uid_list)" type="int">
+<dtml-else>
+  <dtml-sqltest uid op=eq type="int">
+  LIMIT 1
 </dtml-if>
