535dd563250012581e53f0e06167a65847a8d918
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2017-05-03 08:41:46 +0200

    erp5_upgrader: no need to call activeSense in activities in Alarm_runFullUpgrader

--- bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Alarm_runFullUpgrader.py
+++ bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Alarm_runFullUpgrader.py
@@ -15,27 +15,39 @@ post_upgrade_tag = '%s-postupgrade' % tag
 active_process = context.newActiveProcess()
 
 
-portal_alarms.upgrader_check_pre_upgrade.activate(
-  activity='SQLQueue',
-  tag=pre_upgrade_tag,
-).activeSense(fixit=fixit, params={'tag': pre_upgrade_tag})
+portal_alarms.upgrader_check_pre_upgrade.activeSense(
+  fixit=fixit,
+  activate_kw={
+    'activity': 'SQLQueue',
+    'tag': pre_upgrade_tag,
+  },
+  params={'tag': pre_upgrade_tag},
+)
 
-portal_alarms.upgrader_check_upgrader.activate(
-  activity='SQLQueue',
-  tag=upgrade_tag,
-  after_tag=pre_upgrade_tag,
- ).activeSense(fixit=fixit, params={'tag': upgrade_tag})
+portal_alarms.upgrader_check_upgrader.activeSense(
+  fixit=fixit,
+  activate_kw={
+    'activity': 'SQLQueue',
+    'tag': upgrade_tag,
+    'after_tag': pre_upgrade_tag,
+  },
+  params={'tag': upgrade_tag},
+)
 
 if fixit:
-  portal_alarms.upgrader_check_post_upgrade.activate(
-    activity='SQLQueue',
-    tag=post_upgrade_tag,
-    after_tag=upgrade_tag,
-  ).activeSense(fixit=fixit, params={'tag': post_upgrade_tag})
+  portal_alarms.upgrader_check_post_upgrade.activeSense(
+    fixit=fixit,
+    activate_kw={
+      'activity': 'SQLQueue',
+      'tag': post_upgrade_tag,
+      'after_tag': upgrade_tag,
+    },
+    params={'tag': post_upgrade_tag},
+  )
 
 
 # start another activity to collect the results from each upgrader step
-context.activate(after_tag=post_upgrade_tag).Alarm_postFullUpgradeNeed(
+context.activate(after_tag=(upgrade_tag, post_upgrade_tag)).Alarm_postFullUpgradeNeed(
   active_process=active_process.getRelativeUrl())
 
 # Nothing else to do, so we can disable.
