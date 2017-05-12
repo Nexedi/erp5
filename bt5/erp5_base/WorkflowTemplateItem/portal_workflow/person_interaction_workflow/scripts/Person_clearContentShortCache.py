d8727062ee84ddbc4812744a5a1dee31f7d6dbfe
Jérome Perrin <jerome@nexedi.com>
2017-05-12 03:11:35 +0000

    base: drop person_interaction_workflow
    We do not need to pre-create user preferences, especially that creating
    them too early creates one preferences for each person created in person
    module and does not allow Person_createUserPreference to create
    preference based on person's assignments are they are not created yet.
    
    We do not need to clear cache when deleting user logins anymore. This
    problem was fixed differently.
    
    Adjust tests that was relying on cache being cleared when persons ar
    deleted:
    
    testERP5Web.TestERP5Web.test_15_Check_LastModified_Header was never
    isolated from test_14.
    
    test_14 was filling Base_getWebDocumentDrivenModificationDate cache and
    this cache got clear when the persons where being deleted during
    tearDown. When removing this interaction clearing cache, we revealed
    this weakness. Choosen solution was to clear cache before checking
    response headers, to make sure we don't get something filled by a
    previous test.

--- bt5/erp5_base/WorkflowTemplateItem/portal_workflow/person_interaction_workflow/scripts/Person_clearContentShortCache.py
+++
@@ -1,6 +0,0 @@
-# XXX-Luke: Seb pointed out that this is very bad idea to clear cache.
-document = state_change['object']
-if document.Person_getUserId() is not None:
-  cache_tool = document.getPortalObject().portal_caches
-  cache_tool.clearCache(cache_factory_list=('erp5_content_short', ),
-                        before_commit=True)
