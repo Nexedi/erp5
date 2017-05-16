10681c93e04c7bd93de0c0c2437cf7235839c051
Ayush Tiwari <ayush.tiwari@nexedi.com>
2017-05-16 08:36:38 +0000

    bt5_config: Move erp5_xhtml_style to BM5 format

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/ERP5Site_prepareAction.py
+++
@@ -1,8 +0,0 @@
-# Script to call in action scripts before executig the actual action.
-from Products.ERP5Type.Log import log
-log("ERP5Site_prepare is deprecated, "
-    "use Base_updateListboxSelection instead")
-
-# Update checked uids
-if None not in (selection_name, uids, listbox_uid):
-  context.getPortalObject().portal_selections.updateSelectionCheckedUidList(selection_name, uids=uids, listbox_uid=listbox_uid, REQUEST=context.REQUEST)
