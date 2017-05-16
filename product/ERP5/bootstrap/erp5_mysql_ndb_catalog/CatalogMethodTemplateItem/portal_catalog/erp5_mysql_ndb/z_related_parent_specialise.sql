f8e636d8cc29a369a163b9d088d572d24024c8fa
Ayush Tiwari <ayush.tiwari@nexedi.com>
2017-05-16 09:09:47 +0000

    bt5_config: Migrate erp5_mysql_ndb_catalog to BM5 format

--- product/ERP5/bootstrap/erp5_mysql_ndb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_ndb/z_related_parent_specialise.sql
+++
@@ -1,4 +0,0 @@
-<dtml-var table_1>.uid = <dtml-var table_0>.category_uid
-AND <dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.specialise.getUid()">
-AND <dtml-var table_0>.uid = catalog.parent_uid
-
