91619838a60539df5028c3c0afecce5733fd18ba
Łukasz Nowak <luke@nexedi.com>
2017-05-18 15:08:51 +0200

    erp5_trade: Allow to edit quantity for Items
    It is unsure if quantity should not be editable in case of Items
    aggregated on the movement.
    
    Additionally some designs with Items require setting quantity on
    movements.

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_isQuantityEditable.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_isQuantityEditable.py
@@ -6,8 +6,6 @@ cannot edit this line which is just a container, but no actual movement.
 * If this line has variation category list, then it means it's a line that
 will contain cell, so it's already not possible to set quantity, user have
 to create cells and set quantities on cells.
-
-* If items are used, quantity is set by the item quantity.
 """
 
 if not context.isMovement():
@@ -16,4 +14,4 @@ if not context.isMovement():
 if context.getVariationCategoryList(omit_optional_variation=1) and not 'Cell' in context.getPortalType():
   return False
 
-return not (context.getResource() and context.getResourceValue().getAggregatedPortalTypeList())
+return True
