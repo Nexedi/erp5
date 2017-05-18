1f01f38205d4287ed89d5e18784147b3c9743516
Łukasz Nowak <luke@nexedi.com>
2017-05-18 16:48:39 +0200

    erp5_item: Factorise quantity update

--- bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_selectItemList.py
+++ bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_selectItemList.py
@@ -92,23 +92,7 @@ for item_uid in uids:
   movement_to_update.setAggregateValueSet(
       movement_to_update.getAggregateValueList() + [item])
 
-if context.isMovement():
-  movement_list = context,
-else:
-  movement_list = context.getCellValueList(base_id='movement')
-for movement in movement_list:
-  quantity = 0
-  item_list = movement.getAggregateValueList()
-  for item in item_list:
-    if item.getQuantityUnit() != movement.getQuantityUnit():
-      if len(item_list) > 1:
-        raise NotImplementedError(
-          'Quantity unit from the movement differs from quantity'
-          ' unit on the item')
-      else:
-        movement.setQuantityUnit(item.getQuantityUnit())
-    quantity += item.getQuantity()
-  movement.setQuantity(quantity)
+context.Movement_updateQuantityFromAggregateList()
 
 return context.Base_redirect(form_id, keep_items=dict(
        portal_status_message=translateString('Items aggregated')))
