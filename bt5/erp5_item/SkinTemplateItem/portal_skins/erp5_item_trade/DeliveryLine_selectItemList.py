6b074d5a43a7e248d6bdcefdc0a2e5cf72dc3753
Łukasz Nowak <luke@nexedi.com>
2017-05-18 15:11:52 +0200

    erp5_item: Update quantity unconditionally
    Since Movement_isQuantityEditable does not say anything about Item related
    movements, there is no reason to use any condition while updating
    quantities.

--- bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_selectItemList.py
+++ bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_selectItemList.py
@@ -92,25 +92,23 @@ for item_uid in uids:
   movement_to_update.setAggregateValueSet(
       movement_to_update.getAggregateValueList() + [item])
 
-update_quantity = not context.Movement_isQuantityEditable()
-if update_quantity:
-  if context.isMovement():
-    movement_list = context,
-  else:
-    movement_list = context.getCellValueList(base_id='movement')
-  for movement in movement_list:
-    quantity = 0
-    item_list = movement.getAggregateValueList()
-    for item in item_list:
-      if item.getQuantityUnit() != movement.getQuantityUnit():
-        if len(item_list) > 1:
-          raise NotImplementedError(
-            'Quantity unit from the movement differs from quantity'
-            ' unit on the item')
-        else:
-          movement.setQuantityUnit(item.getQuantityUnit())
-      quantity += item.getQuantity()
-    movement.setQuantity(quantity)
-  
+if context.isMovement():
+  movement_list = context,
+else:
+  movement_list = context.getCellValueList(base_id='movement')
+for movement in movement_list:
+  quantity = 0
+  item_list = movement.getAggregateValueList()
+  for item in item_list:
+    if item.getQuantityUnit() != movement.getQuantityUnit():
+      if len(item_list) > 1:
+        raise NotImplementedError(
+          'Quantity unit from the movement differs from quantity'
+          ' unit on the item')
+      else:
+        movement.setQuantityUnit(item.getQuantityUnit())
+    quantity += item.getQuantity()
+  movement.setQuantity(quantity)
+
 return context.Base_redirect(form_id, keep_items=dict(
        portal_status_message=translateString('Items aggregated')))
