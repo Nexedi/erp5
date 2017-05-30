eff300b55521adf8ce81379e9ac649c089c534b5
Romain Courteaud <romain@nexedi.com>
2017-05-30 15:05:07 +0000

    [erp5_officejs_appstore_base] configuration_latest_version is set when acception the Software Publication

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareRelease_publishRelatedWebDocument.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareRelease_publishRelatedWebDocument.py
@@ -5,26 +5,5 @@ software_product = context.getFollowUpValue(portal_type="Software Product")
 web_site = software_product.SoftwareProduct_getRelatedWebSite()
 
 web_site.edit(
-  configuration_latest_version=software_release.getReference(),
+  configuration_latest_version=software_release.getReference()
 )
-
-# Update appcache
-web_manifest_reference = web_site.getId() + ".appcache"
-web_manifest = portal.portal_catalog.getResultValue(
-  portal_type="Web Manifest",
-  reference=web_manifest_reference,
-  )
-
-if not web_manifest:
-  web_manifest = portal.web_page_module.newContent(
-    portal_type="Web Manifest",
-    reference=web_manifest_reference,
-    title=web_site.getTitle() + " Manifest",
-    )
-  web_manifest.publishAlive()
-
-web_manifest.setData("""CACHE MANIFEST
-# generated on %s
-CACHE:
-NETWORK:
-*""" % (DateTime().rfc822()))
