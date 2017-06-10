4ccbb2b2ce34405c724da78aec71e71c40ce5cf2
Vincent Pelletier <vincent@nexedi.com>
2017-06-10 23:08:02 +0900

    erp5_immobilisation: Drop trivial related key declaration.
    aggregate_uid can be auto-generated with one less join, and chosen
    ZSQLMethod name gets in the way of auto-generated related key
    (default_aggregate_uid and the like).

--- bt5/erp5_immobilisation/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_aggregate.sql
+++
@@ -1,3 +0,0 @@
-<dtml-var table_1>.uid = <dtml-var table_0>.category_uid
-AND <dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.aggregate.getUid()">
-AND <dtml-var table_0>.uid = catalog.uid
