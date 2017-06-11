2e7ab4b77a30e356d62d32bdbcf6b444b13a3a77
Vincent Pelletier <vincent@nexedi.com>
2017-06-11 09:05:25 +0900

    ERP5Catalog: Stop looking for shorter base categories when found
    Catalog does not need "resource_uid" when it requested for
    "resource_use_uid", so don't spend time computing it.

--- product/ERP5Catalog/CatalogTool.py
+++ product/ERP5Catalog/CatalogTool.py
@@ -1016,6 +1016,7 @@ class CatalogTool (UniqueObject, ZCatalog, CMFCoreCatalogTool, ActiveObject):
                 expected_base_cat_id +
                 (RELATED_DYNAMIC_METHOD_NAME if related else '')
               )
+              break
 
       return related_key_list
 
