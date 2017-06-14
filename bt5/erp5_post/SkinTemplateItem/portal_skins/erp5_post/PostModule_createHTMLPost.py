6eab4290c3d069d362d2ddb050c095a0f96ffbe9
Boxiang Sun <boxiang.sun@nexedi.com>
2017-06-14 14:18:09 +0000

    erp5_post: Add HTML Post and Post Module.
    Post is a comtainer which contains raw data without any metadata.
    A post can be seen as any item send or received by an instant message application.
    HTML Post is one type of post, which contains richtext in HTML and UTF-8.
    Eugene Shen wrote a post system before, which inpired us. This bt5 is a
    new implementation which under Tristan Cavelier's guide.

---
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
@@ -0,0 +1,43 @@
+portal = context.getPortalObject()
+now = DateTime()
+
+
+# create an HTML Post
+post_module = portal.post_module
+post_1 = post_module.newContent(portal_type='HTML Post')
+
+# get the related object
+object_list = portal.portal_catalog(relative_url=follow_up) # with id keyword, this function will return a sequence data type which contains one element.
+follow_up_object = None
+if object_list:
+  follow_up_object = object_list[0].getObject()
+else:
+  raise LookupError("Target follow up object not found")
+
+follow_up_object.edit(
+  modification_date = now
+)
+
+post_1.edit(
+  start_date=now,
+  follow_up_value=follow_up_object,
+  predecessor_value = predecessor if predecessor else None,
+  text_content=data,
+)
+
+# handle attachments
+if file != "undefined":
+  document_kw = {'batch_mode': True,
+                  'redirect_to_document': False,
+                  'file': file}
+  document = context.Base_contribute(**document_kw)
+  # set relation between post and document
+  post_1.setSuccessorValueList([document])
+  # depending on security model this should be changed accordingly
+  document.publish()
+
+# We need to reindex the object on server. So the page will get the post which
+# just submmitted.
+post_1.immediateReindexObject()
+
+return
---
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_getAscendingRelatedPostListAsJson.py
@@ -0,0 +1,46 @@
+from json import dumps
+
+portal = context.getPortalObject()
+
+comment_list = []
+if not follow_up:
+  return dumps(comment_list)
+
+# get the follow up object
+object_list = portal.portal_catalog(relative_url=follow_up) # with id keyword, this function will return a sequence data type which contains one element.
+if object_list:
+  follow_up_object = object_list[0].getObject()
+else:
+  raise NotImplementedError(follow_up)
+  
+# get the all HTML Posts which related to this follow up object
+post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=follow_up_object.getUid()) # with id keyword, this function will return a sequence data type which contains one element.
+post_list = sorted(post_list, key=lambda x:x.getStartDate(), reverse=False)
+
+preferred_date_order = portal.portal_preferences.getPreferredDateOrder()
+
+def format_date(date):
+  # XXX modification date & creation date are still in server timezone.
+  #   See merge request !17
+  #
+  # if default_time_zone:
+  #   date = date.toZone(default_time_zone)
+  if preferred_date_order == 'dmy':
+    return "%s/%s/%s&nbsp;&nbsp;&nbsp;%s" % (date.dd(), date.mm(), date.year(), date.TimeMinutes())
+  if preferred_date_order == 'mdy':
+    return "%s/%s/%s&nbsp;&nbsp;&nbsp;%s" % (date.mm(), date.dd(), date.year(), date.TimeMinutes())
+  # ymd
+  return "%s/%s/%s&nbsp;&nbsp;&nbsp;%s" % (date.year(), date.mm(), date.dd(), date.TimeMinutes())
+
+for post in post_list:
+  owner = post.Base_getOwnerTitle()
+  time_stamp = format_date(post.getStartDate())
+  content = post.getTextContent()
+  successor_list = post.getSuccessorValueList()
+  successor_name = successor_link = None
+  if successor_list:
+    successor_link, successor_name = successor_list[0].getRelativeUrl(), successor_list[0].getFilename()
+    
+  comment_list.append((owner, time_stamp, content, successor_link, successor_name))
+  
+return dumps(comment_list)
