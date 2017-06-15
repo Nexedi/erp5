ac308c9eea0c63b7919905ce65de52d0c6b09b08
Jérome Perrin <jerome@nexedi.com>
2017-06-15 06:18:37 +0000

    mysql_innodb_catalog: use INSERT after DELETE, not REPLACE
    If rows have been deleted before, it's more natural to simply INSERT and
    no need for REPLACE

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_non_movement_category_list.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_non_movement_category_list.sql
@@ -14,7 +14,7 @@ WHERE
 <dtml-call expr="category_list.append((uid[loop_item], uid_item[0], uid_item[1], uid_item[2]))">
 </dtml-in></dtml-if></dtml-let></dtml-if></dtml-in>  
 <dtml-if expr="category_list">
-REPLACE INTO category VALUES
+INSERT INTO category VALUES
 <dtml-in prefix="loop" expr="category_list">
 (<dtml-sqlvar expr="loop_item[0]" type="int">, <dtml-sqlvar expr="loop_item[1]" type="int">, <dtml-sqlvar expr="loop_item[2]" type="int">, <dtml-sqlvar expr="loop_item[3]" type="int">)<dtml-if sequence-end><dtml-else>,</dtml-if>
 </dtml-in>
