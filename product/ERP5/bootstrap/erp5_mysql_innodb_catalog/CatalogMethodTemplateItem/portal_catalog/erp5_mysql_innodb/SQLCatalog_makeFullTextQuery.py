c2dbe1db5af9a38fb61782ff1779503593929bcd
Jérome Perrin <jerome@nexedi.com>
2017-06-20 05:44:18 +0200

    full text: do not overwrite SQLCatalog_makeFullTextQuery
    Each full text business templates was overwritting
    portal_catalog/erp5_mysql_innodb/SQLCatalog_makeFullTextQuery , which
    does not work well with the way upgrading business template works.
    
    Instead of overwriting, erp5_mysql_innodb's SQLCatalog_makeFullTextQuery
    will call implementation scripts installed by a full text business
    template.

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeFullTextQuery.py
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeFullTextQuery.py
@@ -1,8 +1,18 @@
-"""
-  Default full text searchable key implementation.
+"""Entry point for full text query generation.
+
+Delegates the query generation to full text business template, or
+fallback to searching title or reference.
 """
 from Products.ZSQLCatalog.SQLCatalog import ComplexQuery
 from Products.ZSQLCatalog.SQLCatalog import Query
+
+for script_name in (
+  'SQLCatalog_makeSphinxSEFullTextQuery',
+  'SQLCatalog_makeMroongaFullTextQuery',
+  'SQLCatalog_makeMyISAMFullTextQuery'):
+  script_object = getattr(container, script_name, None)
+  if script_object is not None:
+    return script_object(value)
 
 query = ComplexQuery(Query(title=value),
                      Query(reference=value),
