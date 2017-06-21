57c4d81278aa774c9b89cf422b2bf91c074fbd4e
Jérome Perrin <jerome@nexedi.com>
2017-06-21 02:25:27 +0000

    administration: repair ERP5Site_checkPythonScriptsWithPyflakes
    Name is unchanged, but it will use
    Products.ERP5Type.Utils.checkPythonSourceCode which uses pylint.

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_checkPythonScriptsWithPyflakes.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_checkPythonScriptsWithPyflakes.py
@@ -1,45 +1,28 @@
-"""Runs pyflakes on all python scripts.
+"""Runs pylint/pyflakes on all python scripts.
 
 TODO / BUGS:
-* there is an offset in the line numbers in the reports
 * script containing only a comment cannot be parsed
-* integrate this directly in python script ZMI
 * wouldn't it be better to use this on a business template to check scripts
 from the business template ?
 """
+import json
 
-pyflakes = context.ERP5Site_runPyflakes
-
-def indent(text):
-  return ''.join(("  " + line) for line in text.splitlines(True))
-
-def make_body(script):
-  """rewrite a python script as if it was a python function with all
-  bound names in the signature.
-  """
-  bound_names = script.getBindingAssignments().getAssignedNamesInOrder()
+def checkPythonSourceCode(script_instance):
   # printed is from  RestrictedPython.RestrictionMutator the rest comes from
   # RestrictedPython.Utilities.utility_builtins
-  extra_builtins= ['printed', 'same_type', 'string', 'sequence', 'random',
+  extra_builtins = ['printed', 'same_type', 'string', 'sequence', 'random',
     'DateTime', 'whrandom', 'reorder', 'sets', 'test', 'math']
-  
-  params = script.params()
-  
-  signature_parts = bound_names + extra_builtins
-  if params:
-    signature_parts += [params]
-  signature = ", ".join(signature_parts)
-  
-  function_name = script.getId().replace(".", "__dot__").replace(" ", "__space__")
-  
-  body = "def %s(%s):\n%s" % (function_name, signature, indent(script.body()) or "  pass")
-  return body
-
+  return json.loads(context.ERP5Site_checkPythonSourceCodeAsJSON(
+      {'bound_names': extra_builtins + script_instance.getBindingAssignments().getAssignedNamesInOrder(),
+       'params': script_instance.params(),
+       'code': unicode(script_instance.read(), 'utf8')
+      }))['annotations']
 
 for script_container in (context.portal_skins, context.portal_workflow):
-  for script_path, script in context.ZopeFind(script_container, obj_metatypes=['Script (Python)'], search_sub=1):
-    err = pyflakes(make_body(script), '%s/%s' % (script_container.getId(), script_path))
-    if err:
-      print err,
+  for script_path, script_instance in context.ZopeFind(script_container, obj_metatypes=['Script (Python)'], search_sub=1):
+    for annotation in checkPythonSourceCode(script_instance):
+      annotation["script_path"] = "%s/%s" % (script_container.getId(), script_path)
+      print "{script_path}:{row}:{column}:{text}".format(**annotation)
+
 
 return printed
