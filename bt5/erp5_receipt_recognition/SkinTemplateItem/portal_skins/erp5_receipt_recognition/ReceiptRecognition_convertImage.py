3034150011c83201ea42afd6be75e29e19516b66
Francois Le Corre <francois.lecorre@nexedi.com>
2017-06-29 13:26:27 +0000

    erp5_receipt_recognition Update bt5 following merge request advices

--- bt5/erp5_receipt_recognition/SkinTemplateItem/portal_skins/erp5_receipt_recognition/ReceiptRecognition_convertImage.py
+++ bt5/erp5_receipt_recognition/SkinTemplateItem/portal_skins/erp5_receipt_recognition/ReceiptRecognition_convertImage.py
@@ -2,17 +2,17 @@ image = context.getFollowUpValue()
 if image is not None:
   try:
     total = container.ReceiptRecognition_getReceiptValue(image.getData())
-    msg = "Total found"
+    message = "Total found"
     context.edit(
       total = total,
     )
   except ValueError as e:
-    msg = "Could not find value, please submit it manually"
+    message = "Could not find value, please submit it manually"
 else:
-  msg = "Cannot find the image"
+  message = "Cannot find the image"
 
 if batch_mode:
   return
 
 context.Base_redirect(
-  'view', keep_items = dict(portal_status_message=msg, my_source="test"))
+  'view', keep_items = dict(portal_status_message=message, my_source="test"))
