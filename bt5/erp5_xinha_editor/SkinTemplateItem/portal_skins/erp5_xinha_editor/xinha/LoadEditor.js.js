16c64b692ecca57e7144750d9cbef703521d8480
Tristan Cavelier <tristan.cavelier@nexedi.com>
2017-07-25 12:34:54 +0000

    erp5_xinha_editor: allow override xinha css
    author: Sven Franck

--- bt5/erp5_xinha_editor/SkinTemplateItem/portal_skins/erp5_xinha_editor/xinha/LoadEditor.js.js
+++ bt5/erp5_xinha_editor/SkinTemplateItem/portal_skins/erp5_xinha_editor/xinha/LoadEditor.js.js
@@ -9,6 +9,7 @@ function xinha_init()
 {
   if(!Xinha.loadPlugins(xinha_plugins, xinha_init)) return;
   var xinha_config = new Xinha.Config();
+  xinha_config.pageStyleSheets = ["xinha_custom.css"];
   xinha_editors = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);
   Xinha.startEditors(xinha_editors);
 }
