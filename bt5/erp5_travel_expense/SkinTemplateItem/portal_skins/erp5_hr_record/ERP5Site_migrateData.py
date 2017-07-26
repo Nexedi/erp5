3ec3371432f2b116dd99ac30c9e311de7e5af962
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-07-26 10:24:44 +0000

    erp5_travel_expense: Update migration data for Travel expense

--- bt5/erp5_travel_expense/SkinTemplateItem/portal_skins/erp5_hr_record/ERP5Site_migrateData.py
+++ bt5/erp5_travel_expense/SkinTemplateItem/portal_skins/erp5_hr_record/ERP5Site_migrateData.py
@@ -1,3 +1,30 @@
+portal = context.getPortalObject()
+
+# Update Photos
+for record in portal.expense_record_module.objectValues(portal_type="Expense Record"):
+  ticket = record.getFollowUpValue()
+  photo_data = record.getPhotoData()
+  if photo_data:
+    if "," in photo_data and ticket:
+      if ticket.getReference():
+        photo_data = photo_data.split(",")[1]
+        image = portal.portal_contributions.newContent(
+          data = photo_data.decode('base64'),
+          reference=ticket.getReference()+ "-justificatif",
+          title = ticket.getReference() + " Justificatif",
+          description = ticket.getDescription(),
+          filename="tmp.png",
+          follow_up=ticket.getRelativeUrl(),
+          publication_section=portal.ERP5Site_getPreferredExpenseDocumentPublicationSectionValue().getRelativeUrl(),
+        )
+        image.share()
+      else:
+        print ticket.getRelativeUrl()
+
+print DateTime()
+#return printed
+
+
 for i in context.portal_catalog(portal_type='Expense Validation Request'):
   sourceReference = i.getSourceReference()
   if sourceReference:
