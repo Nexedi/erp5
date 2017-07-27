3a8b3565e2d09f27a264c73a06fca2336060be8f
Romain Courteaud <romain@nexedi.com>
2017-07-27 10:41:05 +0000

    [erp5_http_proxy] Prototype of an usable HTTP proxy.
    It can be used with an external method by providing the URL to proxy as a query parameter:
    ERP5Site_getHTTPResource?url=URL
    
    The query is synchronous (no portal_activities), so, the Zope thread will be blocked until the external request is other.
    In order to prevent consuming all threads, there is a really aggressive timeout of 1 second only.
    This is enough for a RSS reader.
    If the timeout is to short, do not increase it, but change the proxy design to use portal_activities instead.
    
    For now, only authenticated user can use it (to prevent becoming an open proxy).
    Only GET query are allowed (until a use case required any other HTTP method).

---
+++ bt5/erp5_http_proxy/ExtensionTemplateItem/portal_components/extension.erp5.HTTPProxy.py
@@ -0,0 +1,61 @@
+import requests
+
+# Extremely aggressive and hardcoded value
+TIMEOUT = 1
+
+def request(self, url, REQUEST):
+  RESPONSE = REQUEST.RESPONSE
+
+  portal = self.getPortalObject()
+  if (portal.portal_membership.isAnonymousUser()):
+    RESPONSE.setStatus(403)
+    return ""
+  elif REQUEST.other['method'] != "GET":
+    RESPONSE.setStatus(405)
+    return ""
+
+  proxy_query_header = {}
+  for k in ["Content-Type", "Accept", "Accept-Language", "Range",
+            "If-Modified-Since", "If-None-Match"]:
+    v = REQUEST.getHeader(k, None)
+    if v is not None:
+      proxy_query_header[k] = v
+
+  try:
+    proxy_response = requests.request(
+      REQUEST.other['method'],
+      url,
+      # Propage the HTTP body (for POST)
+      data=REQUEST.get('BODY'),
+      # Propagate to headers to use HTTP cache as much as possible
+      headers=proxy_query_header,
+      # Do not block ERP5 if queried server is too slow
+      timeout=TIMEOUT
+    )
+  except requests.exceptions.SSLError:
+    # Invalid SSL Certificate
+    status_code = 526
+  except requests.exceptions.ConnectionError:
+    status_code = 523
+  except requests.exceptions.Timeout:
+    status_code = 524
+  except requests.exceptions.TooManyRedirects:
+    status_code = 520
+  else:
+    status_code = proxy_response.status_code
+    if status_code == 500:
+      status_code = 520
+
+    for k, v in proxy_response.headers.items():
+      k = k.title()
+      if k in ["Content-Disposition", "Content-Type", "Date", "Last-Modified",
+               "Vary", "Cache-Control", "Etag", "Accept-Ranges",
+               "Content-Range"]:
+        RESPONSE.setHeader(k, v)
+      """
+      elif k == "Location":
+        # In case of redirect, allow to directly fetch from proxy
+      """
+
+  RESPONSE.setStatus(status_code)
+  return proxy_response.content
