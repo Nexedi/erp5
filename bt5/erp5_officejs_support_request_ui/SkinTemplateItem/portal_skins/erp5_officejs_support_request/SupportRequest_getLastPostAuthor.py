ce238b315e56e7301dda9ba565cb7e20ed5ee7f0
Boxiang Sun <boxiang.sun@nexedi.com>
2017-08-18 17:29:57 +0200

    erp5_officejs_support_request_ui: Choose the correct post author.
    The comment user field in the listbox which list Support Requests should show
    the latest person who left comment.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostAuthor.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostAuthor.py
@@ -7,11 +7,9 @@ support_request_object = support_request_list[0].getObject()
 
 # get the all HTML Posts which related to this Support Request
 
-post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=support_request_object.getUid()) # with id keyword, this function will return a sequence data type which contains one element.
-
-post_list = sorted(post_list, key=lambda x:x.getStartDate(), reverse=False)
-
-if post_list:
+post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=support_request_object.getUid(), sort_on=(('modification_date', 'descending'),), limit=1, validation_state="published") # with id keyword, this function will return a sequence data type which contains one element.
+  
+if len(post_list):
   return post_list[0].Base_getOwnerTitle()
 else:
   return None
