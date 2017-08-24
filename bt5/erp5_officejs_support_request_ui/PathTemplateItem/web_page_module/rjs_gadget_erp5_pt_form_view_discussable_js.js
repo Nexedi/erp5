7a613c1d73b49342d4a6b0308a0942dd8a3d248b
Tristan Cavelier <tristan.cavelier@nexedi.com>
2017-08-24 17:51:21 +0200

    erp5_officejs_support_request_ui: fix ckeditor gadget url

--- bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_form_view_discussable_js.js
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_form_view_discussable_js.js
@@ -185,7 +185,7 @@
     })
     .declareService(function () {
       var gadget = this;
-      return gadget.declareGadget('officejs_ckeditor_gadget/development/',
+      return gadget.declareGadget("https://texteditor.app.officejs.com/d8f869616f/officejs_ckeditor_gadget/app/",
         {
           element: gadget.element.querySelector('.editor'),
           sandbox: 'iframe',
