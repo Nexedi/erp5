28b3cc36f81147575c84011704a749cd9b7eb9e6
Vincent Pelletier <vincent@nexedi.com>
2017-08-25 16:47:11 +0900

    erp5_free_subscription: Fix related key section order.
    Sections must be in table order: table_0 and table_1 in first section, then
    table_3, then table_4 and query_table must be in last query section.

--- bt5/erp5_free_subscription/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_destination_free_subscription_resource.sql
+++ bt5/erp5_free_subscription/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_destination_free_subscription_resource.sql
@@ -4,15 +4,8 @@ AND <dtml-var table_1>.portal_type = 'Free Subscription'
 AND <dtml-var table_1>.validation_state = 'validated'
 
 <dtml-var RELATED_QUERY_SEPARATOR>
-<dtml-var table_0>.category_uid = <dtml-var query_table>.uid
-
-<dtml-var RELATED_QUERY_SEPARATOR>
 <dtml-var table_0>.uid = <dtml-var table_2>.uid
 AND <dtml-var table_2>.base_category_uid = <dtml-var "portal_categories.resource.getUid()">
-
-<dtml-var RELATED_QUERY_SEPARATOR>
-<dtml-var table_2>.category_uid = <dtml-var table_4>.uid
-AND <dtml-var table_4>.portal_type = 'Service'
 
 <dtml-var RELATED_QUERY_SEPARATOR>
 <dtml-var table_3>.uid = <dtml-var table_1>.uid
@@ -20,3 +13,10 @@ AND <dtml-var table_4>.portal_type = 'Service'
   AND ( <dtml-var table_3>.effective_date is NULL OR <dtml-var table_3>.effective_date <= <dtml-sqlvar now type="datetime"> ) 
   AND ( <dtml-var table_3>.expiration_date is NULL OR <dtml-var table_3>.expiration_date >= <dtml-sqlvar now type="datetime"> )
 </dtml-let>
+
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_2>.category_uid = <dtml-var table_4>.uid
+AND <dtml-var table_4>.portal_type = 'Service'
+
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_0>.category_uid = <dtml-var query_table>.uid
