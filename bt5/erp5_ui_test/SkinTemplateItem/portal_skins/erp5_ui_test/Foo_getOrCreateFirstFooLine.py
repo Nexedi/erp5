6d8e519c66d28486e5e045abc7a425eedafd8c54
Tomas Peterka <tomas.peterka@nexedi.com>
2017-08-29 12:08:40 +0200

    [erp5_renderjs_ui_test] Add tests for FormBox Field

---
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_getOrCreateFirstFooLine.py
@@ -0,0 +1,11 @@
+"""Get-or-Create Foo Line inside context (Foo object).
+
+This script is used from Foo_viewFormBoxFooLine from a FormBox and thus 
+can receive 'field' and 'REQUEST' kwargs.
+"""
+
+foo_line_list = context.contentValues(portal_type='Foo Line')
+if foo_line_list:
+  return foo_line_list[0]
+
+return context.newContent(portal_type='Foo Line')
