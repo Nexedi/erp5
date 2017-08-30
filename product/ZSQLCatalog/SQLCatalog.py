6845e45dfb316530b302d6ebda524a6511a45685
Vincent Pelletier <vincent@nexedi.com>
2017-08-30 15:53:42 +0900

    ZSQLCatalog: Stop exposing SQLQuery.

--- product/ZSQLCatalog/SQLCatalog.py
+++ product/ZSQLCatalog/SQLCatalog.py
@@ -2594,6 +2594,5 @@ def getComparisonOperatorInstance(operator):
   return operator_dict[operator]
 
 from Query.EntireQuery import EntireQuery
-from Query.SQLQuery import SQLQuery
 
 verifyClass(ISearchKeyCatalog, Catalog)
