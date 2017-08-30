bdc6c47ec6b117da9ac2679058353008583e9303
Vincent Pelletier <vincent@nexedi.com>
2017-08-30 15:53:42 +0900

    XXX CHERRY-PICK ZSQLCatalog: Stop exposing SQLQuery.

--- product/ZSQLCatalog/__init__.py
+++ product/ZSQLCatalog/__init__.py
@@ -38,7 +38,7 @@ def initialize(context):
 from AccessControl import ModuleSecurityInfo, ClassSecurityInfo
 ModuleSecurityInfo('Products.ZSQLCatalog.SQLCatalog').declarePublic(
             'ComplexQuery', 'Query', 'NegatedQuery', 'AndQuery', 'OrQuery',
-            'BaseQuery', 'SQLQuery')
+            'BaseQuery')
 
 from Query import Query, SimpleQuery
 from SearchKey import SearchKey
