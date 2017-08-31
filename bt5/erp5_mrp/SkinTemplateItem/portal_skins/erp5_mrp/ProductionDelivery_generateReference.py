4a2449728971471a3aff5171c74c86374297b7c5
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-08-31 18:57:12 +0200

    Manufacturing/MRP: Moving Production Report to Manufacturing Execution

--- bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/ProductionDelivery_generateReference.py
+++ bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/ProductionDelivery_generateReference.py
@@ -9,7 +9,7 @@ prefix = ''.join([x for x in object_type if x in uppercase_list])
 # dirty hack
 # Production Order upper case letters are same as Purchase Order upper case letters
 # So I (Luke) replaced first letter with R - pRoduction, that way we have
-# RO for Production Order, RPL for Production Packing List, RR for Production Report
+# RO for Production Order, RPL for Production Packing List, RR for Manufacturing Execution
 prefix = 'R' + prefix[1:]
 
 id_group = '-'.join((parent_type, object_type))
