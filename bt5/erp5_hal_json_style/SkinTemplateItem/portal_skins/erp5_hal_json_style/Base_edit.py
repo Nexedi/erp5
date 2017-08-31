91a5c400fd5b3375b8e96831824c2610f05d5b9a
Tomas Peterka <tomas.peterka@nexedi.com>
2017-08-31 10:22:46 +0200

    [json_style] Refactor Base_edit, Add MatrixBox and FloatField.input_style to getHateoas

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_edit.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_edit.py
@@ -51,7 +51,8 @@ except FormValidationError as validation_errors:
       value = request.get(field_id)
       if callable(value):
         value(request)
-  if silent_mode: return context.ERP5Document_getHateoas(form=form, REQUEST=request, mode='form'), 'form'
+  if silent_mode:
+    return context.ERP5Document_getHateoas(form=form, REQUEST=request, mode='form'), 'form'
   request.RESPONSE.setStatus(400)
   return context.ERP5Document_getHateoas(form=form, REQUEST=request, mode='form')
 
@@ -92,103 +93,109 @@ def editListBox(listbox_field, listbox):
 def editMatrixBox(matrixbox_field, matrixbox):
   """ Function called to edit a Matrix box
   """
-  if matrixbox is not None:
-    cell_base_id = matrixbox_field.get_value('cell_base_id')
-    portal_type = matrixbox_field.get_value('cell_portal_type')
-    getter_method = matrixbox_field.get_value('getter_method')
-    if getter_method not in (None, ''):
-      matrix_context = getattr(context,getter_method)()
+  if matrixbox is None:
+    return
+
+  cell_base_id = matrixbox_field.get_value('cell_base_id')
+  portal_type = matrixbox_field.get_value('cell_portal_type')
+  getter_method = matrixbox_field.get_value('getter_method')
+  if getter_method not in (None, ''):
+    matrix_context = getattr(context,getter_method)()
+  else:
+    matrix_context = context
+
+  if matrix_context is None:
+    return
+
+  kd = {}
+  kd['portal_type'] = portal_type
+  kd['base_id'] = cell_base_id
+  gv = {}
+  if matrixbox_field.has_value('global_attributes'):
+    hidden_attributes = [x[0] for x in matrixbox_field.get_value('global_attributes')]
+    for k in hidden_attributes:
+      gv[k] = getattr(request, k, None)
+  if matrixbox_field.get_value('update_cell_range'):
+    as_cell_range_script_id = matrixbox_field.get_value(
+            'as_cell_range_script_id')
+    lines = []
+    columns = []
+    tabs = []
+    extra_dimension_list_list = []
+    if as_cell_range_script_id:
+      cell_range = getattr(matrix_context,
+          as_cell_range_script_id)(matrixbox=True, base_id=cell_base_id)
+      if len(cell_range) == 1:
+        lines, = cell_range
+      elif len(cell_range) == 2:
+        lines, columns = cell_range
+      elif len(cell_range) == 3:
+        lines, columns, tabs = cell_range
+      elif len(cell_range) > 3:
+        lines = cell_range[0]
+        columns = cell_range[1]
+        tabs = cell_range[2]
+        extra_dimension_list_list = cell_range[3:]
     else:
-      matrix_context = context
-    if matrix_context is not None:
-      kd = {}
-      kd['portal_type'] = portal_type
-      kd['base_id'] = cell_base_id
-      gv = {}
-      if matrixbox_field.has_value('global_attributes'):
-        hidden_attributes = [x[0] for x in matrixbox_field.get_value('global_attributes')]
-        for k in hidden_attributes:
-          gv[k] = getattr(request, k, None)
-      if matrixbox_field.get_value('update_cell_range'):
-        as_cell_range_script_id = matrixbox_field.get_value(
-                'as_cell_range_script_id')
-        lines = []
-        columns = []
-        tabs = []
-        extra_dimension_list_list = []
-        if as_cell_range_script_id:
-          cell_range = getattr(matrix_context,
-              as_cell_range_script_id)(matrixbox=True, base_id=cell_base_id)
-          if len(cell_range) == 1:
-            lines, = cell_range
-          elif len(cell_range) == 2:
-            lines, columns = cell_range
-          elif len(cell_range) == 3:
-            lines, columns, tabs = cell_range
-          elif len(cell_range) > 3:
-            lines = cell_range[0]
-            columns = cell_range[1]
-            tabs = cell_range[2]
-            extra_dimension_list_list = cell_range[3:]
-        else:
-          lines = matrixbox_field.get_value('lines')
-          columns = matrixbox_field.get_value('columns')
-          tabs = matrixbox_field.get_value('tabs')
+      lines = matrixbox_field.get_value('lines')
+      columns = matrixbox_field.get_value('columns')
+      tabs = matrixbox_field.get_value('tabs')
 
-        column_ids = map(lambda x: x[0], columns)
-        line_ids = map(lambda x: x[0], lines)
-        tab_ids = map(lambda x: x[0], tabs)
-        extra_dimension_category_list_list = [[category for category, label in dimension_list] for dimension_list in extra_dimension_list_list]
+    column_ids = map(lambda x: x[0], columns)
+    line_ids = map(lambda x: x[0], lines)
+    tab_ids = map(lambda x: x[0], tabs)
+    extra_dimension_category_list_list = [[category for category, label in dimension_list] for dimension_list in extra_dimension_list_list]
 
-        # There are 3 cases
-        # Case 1: we do 1 dimensional matrix
-        # Case 2: we do 2 dimensional matrix
-        # Case 3: we do 2 dimensional matrix + tabs
-        # Case 4: we do 2 dimensional matrix + tabs + extra
-        cell_range = matrix_context.getCellRange(base_id = cell_base_id)
-        if (len(column_ids) == 0) or (column_ids[0] is None):
-          matrixbox_cell_range = [line_ids]
-          if cell_range != matrixbox_cell_range:
-            matrix_context.setCellRange(line_ids, base_id=cell_base_id)
+    # There are 3 cases
+    # Case 1: we do 1 dimensional matrix
+    # Case 2: we do 2 dimensional matrix
+    # Case 3: we do 2 dimensional matrix + tabs
+    # Case 4: we do 2 dimensional matrix + tabs + extra
+    cell_range = matrix_context.getCellRange(base_id = cell_base_id)
+    if (len(column_ids) == 0) or (column_ids[0] is None):
+      matrixbox_cell_range = [line_ids]
+      if cell_range != matrixbox_cell_range:
+        matrix_context.setCellRange(line_ids, base_id=cell_base_id)
 
-        elif (len(tab_ids) == 0) or (tab_ids[0] is None):
-          matrixbox_cell_range = [line_ids, column_ids]
-          if cell_range != matrixbox_cell_range:
-            matrix_context.setCellRange(line_ids, column_ids, base_id=cell_base_id)
+    elif (len(tab_ids) == 0) or (tab_ids[0] is None):
+      matrixbox_cell_range = [line_ids, column_ids]
+      if cell_range != matrixbox_cell_range:
+        matrix_context.setCellRange(line_ids, column_ids, base_id=cell_base_id)
 
-        else:
-          matrixbox_cell_range = [line_ids, column_ids, tab_ids]
-          if extra_dimension_category_list_list:
-            matrixbox_cell_range = matrixbox_cell_range + extra_dimension_category_list_list
-          if cell_range != matrixbox_cell_range:
-            matrix_context.setCellRange(base_id=cell_base_id, *matrixbox_cell_range)
+    else:
+      matrixbox_cell_range = [line_ids, column_ids, tab_ids]
+      if extra_dimension_category_list_list:
+        matrixbox_cell_range = matrixbox_cell_range + extra_dimension_category_list_list
+      if cell_range != matrixbox_cell_range:
+        matrix_context.setCellRange(base_id=cell_base_id, *matrixbox_cell_range)
 
-      for k,v in matrixbox.items():
-        # Only update cells which still exist
-        if matrix_context.hasInRange(*k, **kd):
-          c = matrix_context.newCell(*k, **kd)
-          if c is not None:
-            c.edit(edit_order=edit_order, **gv)  # First update globals which include the def. of property_list
-            if v.has_key('variated_property'):
-              # For Variated Properties
-              value = v['variated_property']
-              del v['variated_property']
-              if gv.has_key('mapped_value_property_list'):
-                # Change the property which is defined by the
-                # first element of mapped_value_property_list
-                # XXX May require some changes with Sets
-                key = gv['mapped_value_property_list'][0]
-                v[key] = value
-            # Form: '' -> ERP5: None
-            cleaned_v = v.copy()
-            for key, value in cleaned_v.items():
-              if value == '':
-                cleaned_v[key] = None
-            c.edit(edit_order=edit_order, **cleaned_v) # and update the cell specific values
-          else:
-            return "Could not create cell %s" % str(k)
-        else:
-          return "Cell %s does not exist" % str(k)
+  for cell_index_tuple, cell_value_dict in matrixbox.items():
+    # Only update cells which still exist
+    if not matrix_context.hasInRange(*cell_index_tuple, **kd):
+      return "Cell %s does not exist" % str(cell_index_tuple)
+
+    cell = matrix_context.newCell(*cell_index_tuple, **kd)
+    if cell is None:
+      return "Could not create cell %s" % str(cell_index_tuple)
+
+    cell.edit(edit_order=edit_order, **gv)  # First update globals which include the def. of property_list
+    if cell_value_dict.has_key('variated_property'):
+      # For Variated Properties
+      value = cell_value_dict['variated_property']
+      del cell_value_dict['variated_property']
+      if gv.has_key('mapped_value_property_list'):
+        # Change the property which is defined by the
+        # first element of mapped_value_property_list
+        # XXX Kato: What? Why?
+        # XXX May require some changes with Sets
+        key = gv['mapped_value_property_list'][0]
+        cell_value_dict[key] = value
+    # Form: '' -> ERP5: None
+    cleaned_v = cell_value_dict.copy()
+    for key, value in cleaned_v.items():
+      if value == '':
+        cleaned_v[key] = None
+    cell.edit(edit_order=edit_order, **cleaned_v) # and update the cell specific values
 
 
 edit_kwargs = {}  # keyword arguments for `edit` function on context
@@ -220,7 +227,8 @@ try:
       editMatrixBox(field, request.get(field.id))
 
   # Return parsed values 
-  if silent_mode: return (edit_kwargs, encapsulated_editor_list), 'edit'
+  if silent_mode:
+    return (edit_kwargs, encapsulated_editor_list), 'edit'
 
   # Maybe we should build a list of objects we need
   # Update basic attributes
