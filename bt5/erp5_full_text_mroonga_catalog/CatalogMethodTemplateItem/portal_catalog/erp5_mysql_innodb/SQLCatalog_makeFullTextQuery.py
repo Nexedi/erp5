f635bd0e5f0579b962aa3e8b2cd591d1765f1586
Ayush <ayush.tiwari@nexedi.com>
2017-09-07 15:10:43 +0200

    bt5_config

--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeFullTextQuery.py
+++
@@ -1,7 +0,0 @@
-"""
-  MySQL's full text searchable key implementation.
-"""
-from Products.ZSQLCatalog.SQLCatalog import Query
-
-query = Query(**{'full_text.SearchableText': value})
-return query
--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z0_drop_content_translation.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS content_translation
