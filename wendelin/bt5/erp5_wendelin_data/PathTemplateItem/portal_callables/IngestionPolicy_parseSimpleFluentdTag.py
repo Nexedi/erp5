768237376cfe554ce41ac15c54804c77dc155fc5
Ivan Tyagov <ivan@nexedi.com>
2017-09-07 12:27:07 +0000

    WIP: Work on new Wendelin model added

---
+++ wendelin/bt5/erp5_wendelin_data/PathTemplateItem/portal_callables/IngestionPolicy_parseSimpleFluentdTag.py
@@ -0,0 +1,4 @@
+project_reference = reference.split('.')[0]
+return {'resource_reference' : reference,
+        'specialise_reference': project_reference,
+        'reference': project_reference   }
