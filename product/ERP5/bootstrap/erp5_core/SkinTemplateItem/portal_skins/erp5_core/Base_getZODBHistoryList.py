b3513b16d58530a1833c17d07965537bc1b9e7a3
Jérome Perrin <jerome@nexedi.com>
2017-09-13 06:33:55 +0000

    core: respect "View History" permission in ZODB History
    This action should only be possible if user have View History
    permission.
    
    erp5_data_protection relies on removing the "View History" permission to
    make sure users cannot see the properties before protection in the
    history tab. This was supported by Base_viewHistory, but not by ZODB
    History
    
    (cherry picked from commit d2c08463f5ff4e9a32c9bf659490ab3bcf44ea09)

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getZODBHistoryList.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getZODBHistoryList.py
@@ -1,6 +1,11 @@
+from AccessControl import getSecurityManager
+from zExceptions import Unauthorized
 from Products.ERP5Type.Document import newTempBase
 portal = context.getPortalObject()
 result = []
+
+if not getSecurityManager().getUser().has_permission('View History', context):
+  raise Unauthorized()
 
 def beautifyChange(change_dict):
   return ["%s:%s" % (k,change_dict[k]) for k in sorted(change_dict.keys())]
