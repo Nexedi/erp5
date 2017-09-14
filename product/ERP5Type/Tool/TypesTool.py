3d6ee94590f62c50e0331ccaa9b5236ee12b5437
Ayush <ayush.tiwari@nexedi.com>
2017-09-14 17:13:04 +0200

    bt5_config: Use bootstrap_bm to bootstrap TypesTool as we already have migrated all bootstrap BM

--- product/ERP5Type/Tool/TypesTool.py
+++ product/ERP5Type/Tool/TypesTool.py
@@ -126,10 +126,10 @@ class TypesTool(TypeProvider):
     from Products.ERP5.ERP5Site import ERP5Generator
     ERP5Generator.bootstrap_allow_type(self, 'Catalog Tool')
     # Bootstrap Business Manager as it'll be needed while installation
-    ERP5Generator.bootstrap(self, 'erp5_business_package', 'PortalTypeTemplateItem', (
-      'Business Manager',
-      'Business Item',
-      'Business Property Item'
+    ERP5Generator.bootstrap_bm(self, 'erp5_business_package', (
+      'portal_types/Business Manager',
+      'portal_types/Business Item',
+      'portal_types/Business Property Item'
     ))
     ERP5Generator.bootstrap_bm(self, 'erp5_core', (
       'portal_types/Business Template',
