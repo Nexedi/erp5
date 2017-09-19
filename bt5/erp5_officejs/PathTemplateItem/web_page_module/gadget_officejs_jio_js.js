812ed576093eae013d4f9ac98689db6147f2b632
Vincent Bechu <vincent.bechu@nexedi.com>
2017-09-19 14:40:09 +0000

    [erp5_officejs] Add redirection when storage can't be create in ojs jio gadget

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_jio_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_jio_js.js
@@ -66,11 +66,15 @@
     .declareAcquiredMethod('getUrlFor', 'getUrlFor')
 
     .declareMethod('createJio', function (jio_options) {
-      var gadget = this;
+      var gadget = this, redirect = false;
       if (jio_options === undefined) {
         return;
       }
-      this.state_parameter_dict.jio_storage = jIO.createJIO(jio_options);
+      try {
+        this.state_parameter_dict.jio_storage = jIO.createJIO(jio_options);
+      } catch (error) {
+        this.state_parameter_dict.jio_storage = undefined;
+      }
       return this.getSetting("jio_storage_name")
         .push(function (jio_storage_name) {
           gadget.state_parameter_dict.jio_storage_name = jio_storage_name;
