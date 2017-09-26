8fd4109355251c79905669b5a3a45f42e4d8aceb
Boxiang Sun <boxiang.sun@nexedi.com>
2017-09-26 23:41:57 +0000

    erp5_officejs_support_request_ui: Remove redundant query in these two python script.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportRequestLink.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportRequestLink.py
@@ -1,10 +1,3 @@
-portal = context.getPortalObject()
-
-# get the related Support Request, this should not be None
-support_request_list = portal.portal_catalog(portal_type="Support Request", id=context.getId()) # with id keyword, this function will return a sequence data type which contains one element.
-
-support_request_object = support_request_list[0].getObject()
-
 support_request_link = context.absolute_url()
 hash_position = support_request_link.index('support_request_module')
 
