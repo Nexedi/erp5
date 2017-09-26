8504b2f84cdf75ceb5793a7de87165c85a24babb
Tomas Peterka <tomas.peterka@nexedi.com>
2017-09-26 11:35:35 +0200

    [renderjs_ui + json_style] Deletion of an object returns meaningful response

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_deleteObject.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_deleteObject.py
@@ -1,12 +1,14 @@
+"""Delete object - this script is called as the last frontier thus its result
+gets printed into response body.
+
+The code is a modified version of Folder_delete. We split into two files not to
+further complicate the mentioned script.
+"""
+
 from ZODB.POSException import ConflictError
 
-"""
-some duplicated code with Folder_delete instead of modify Folder_delete
-intended to make code easy understanding 
-"""
-
 portal = context.getPortalObject()
-
+translate = portal.Base_translateString
 
 if context.isDeletable(check_relation=True):
   parent = context.getParentValue()
@@ -16,10 +18,16 @@ if context.isDeletable(check_relation=True):
       portal.portal_workflow.doActionFor(context, 'delete_action')
     else:
       parent.manage_delObjects(ids= [context.getId()])
-    return
+    # redirect back to the container since the context was deleted
+    return container.Base_redirect(
+      keep_items={
+        "portal_status_message": translate("Document deleted")
+      })
+
   except ConflictError:
     raise
   except Exception:
+    # XXX Catch-them-all expression is never a good idea
     pass
 
 request = portal.REQUEST
