5ba6d609e9e7fda057ab4ed151f7bc9552a2a67e
Boxiang Sun <boxiang.sun@nexedi.com>
2017-09-26 23:41:57 +0000

    erp5_officejs_support_request_ui: Remove redundant query in these two python script.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostContent.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostContent.py
@@ -1,13 +1,7 @@
 portal = context.getPortalObject()
 
-# get the related Support Request, this should not be None
-support_request_list = portal.portal_catalog(portal_type="Support Request", id=context.getId()) # with id keyword, this function will return a sequence data type which contains one element.
-
-support_request_object = support_request_list[0].getObject()
-
 # get the all HTML Posts which related to this Support Request
-
-post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=support_request_object.getUid(), sort_on=(('modification_date', 'descending'),), limit=1, validation_state="published") # with id keyword, this function will return a sequence data type which contains one element.
+post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=context.getUid(), sort_on=(('modification_date', 'descending'),), limit=1, validation_state="published") # with id keyword, this function will return a sequence data type which contains one element.
   
 if len(post_list):
   return post_list[0].getTextContent()
