848c28eb5663cfe9639f221b8e2918f17b029d15
Boxiang Sun <boxiang.sun@nexedi.com>
2017-09-27 00:14:52 +0000

    erp5_officejs_support_request_ui: Drop the SR which more than 30 days.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportRequestStatisticsAsJson.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportRequestStatisticsAsJson.py
@@ -2,11 +2,6 @@ from json import dumps
 
 portal = context.getPortalObject()
 
-support_request_list = portal.portal_catalog(portal_type="Support Request")
-
-count_by_state = {}
-count_by_date = {"le2": {}, "2to7": {}, "7to30": {}, "gt30": {}}
-
 # Get the split date
 now_date = DateTime()
 date_2 = now_date - 2
@@ -17,6 +12,13 @@ date_2_midnight = DateTime(str(date_2.year()) + "-" + str(date_2.month()) + "-" 
 date_7_midnight = DateTime(str(date_7.year()) + "-" + str(date_7.month()) + "-" + str(date_7.day()))
 date_30_midnight = DateTime(str(date_30.year()) + "-" + str(date_30.month()) + "-" + str(date_30.day()))
 
+support_request_list = portal.portal_catalog(
+  portal_type="Support Request",
+  modification_date={'query':date_30_midnight,'range':'nlt'}
+)
+
+count_by_state = {}
+count_by_date = {"le2": {}, "2to7": {}, "7to30": {}}
 
 for sr in support_request_list:
   sr_date = sr.getModificationDate()
@@ -33,13 +35,9 @@ for sr in support_request_list:
     count_by_date["le2"][sr_state] = count_by_date["le2"][sr_state] + 1
   elif sr_date >= date_7_midnight:
     count_by_date["2to7"][sr_state] = count_by_date["2to7"][sr_state] + 1
-  elif sr_date >= date_30_midnight:
-    count_by_date["7to30"][sr_state] = count_by_date["7to30"][sr_state] + 1
   else:
-    count_by_date["gt30"][sr_state] = count_by_date["gt30"][sr_state] + 1
-  
-  if sr_date < date_30_midnight:
-    continue
+    count_by_date["7to30"][sr_state] = count_by_date["7to30"][sr_state] + 1
+
   count_by_state[sr_state] = count_by_state[sr_state] + 1
 
 result = {}
