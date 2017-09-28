898d9f629e4809d13f69398183579f330e9cec5c
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2017-09-28 08:13:21 +0000

    erp5_crm: always display workflow status in the ticket listbox
    All Tickets portal types don't use the same workflow variable (validation_state/simulation_state). By retrieving first the correct workflow variable, we can then deduce the correct getter to access to each object workflow's state

---
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_getWorkflowStateTranslatedTitle.py
@@ -0,0 +1,9 @@
+from Products.ERP5Type.Utils import UpperCase
+
+portal = context.getPortalObject()
+workflow_tool = portal.portal_workflow
+
+for workflow in workflow_tool.getWorkflowsFor(context):
+  # Exclude interaction workflows and edit_workflow
+  if workflow.state_var != 'state':
+    return getattr(context, 'getTranslated%sTitle' % UpperCase(workflow.state_var))()
