cc59cd4e70d09f49a2854995166d7fdc0012fb8d
Boxiang Sun <boxiang.sun@nexedi.com>
2017-10-04 14:26:45 +0000

    erp5_officejs: Automatically update the ckeditor from 4.5.3 to 4.7.3
    This commit was generated by a Python script. It checks the files in a ckeditor zip file.
    If encounter a new file, create and publish it in erp5. If encounter an existed file, just
    update the file content.
    
    The files listed in below are not ported to erp5:
    
    ckeditor/skins/moono-lisa/readme.md
    ckeditor/plugins/specialchar/dialogs/lang/_translationstatus.txt
    ckeditor/plugins/scayt/LICENSE.md
    ckeditor/plugins/scayt/CHANGELOG.md
    ckeditor/plugins/scayt/README.md
    ckeditor/plugins/wsc/LICENSE.md
    ckeditor/plugins/wsc/README.md
    ckeditor/plugins/a11yhelp/dialogs/lang/_translationstatus.txt
    ckeditor/samples/toolbarconfigurator/lib/codemirror/LICENSE
    ckeditor/samples/toolbarconfigurator/font/LICENSE.txt
    ckeditor/samples/toolbarconfigurator/font/fontello.woff
    ckeditor/samples/toolbarconfigurator/font/config.json
    ckeditor/samples/toolbarconfigurator/font/fontello.ttf
    ckeditor/samples/toolbarconfigurator/font/fontello.eot
    ckeditor/samples/old/htmlwriter/assets/outputforflash/outputforflash.fla
    ckeditor/samples/old/htmlwriter/assets/outputforflash/outputforflash.swf
    ckeditor/samples/old/assets/posteddata.php
    ckeditor/samples/old/sample_posteddata.php
    ckeditor/LICENSE.md
    ckeditor/README.md
    ckeditor/CHANGES.md

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/ckeditor_build-config_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/ckeditor_build-config_js.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/e6b8a045f8f984a69463975ca3e6524a
+ * (2) http://ckeditor.com/builder/e36bab0e1b0cd2508382db742151f0ca
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/e6b8a045f8f984a69463975ca3e6524a
+ * (3) http://ckeditor.com/builder/download/e36bab0e1b0cd2508382db742151f0ca
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,28 +25,29 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'moono',
+	skin: 'moono-lisa',
 	preset: 'full',
 	ignore: [
-		'.bender',
-		'bender.js',
-		'bender-err.log',
-		'bender-out.log',
-		'dev',
 		'.DS_Store',
+		'.bender',
 		'.editorconfig',
 		'.gitattributes',
 		'.gitignore',
-		'gruntfile.js',
 		'.idea',
 		'.jscsrc',
 		'.jshintignore',
 		'.jshintrc',
-		'less',
 		'.mailmap',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
 		'node_modules',
 		'package.json',
-		'README.md',
 		'tests'
 	],
 	plugins : {
@@ -59,6 +60,7 @@ var CKBUILDER_CONFIG = {
 		'colorbutton' : 1,
 		'colordialog' : 1,
 		'contextmenu' : 1,
+		'copyformatting' : 1,
 		'dialogadvtab' : 1,
 		'div' : 1,
 		'elementspath' : 1,
@@ -103,6 +105,7 @@ var CKBUILDER_CONFIG = {
 		'stylescombo' : 1,
 		'tab' : 1,
 		'table' : 1,
+		'tableselection' : 1,
 		'tabletools' : 1,
 		'templates' : 1,
 		'toolbar' : 1,
@@ -113,6 +116,7 @@ var CKBUILDER_CONFIG = {
 	languages : {
 		'af' : 1,
 		'ar' : 1,
+		'az' : 1,
 		'bg' : 1,
 		'bn' : 1,
 		'bs' : 1,
@@ -121,6 +125,7 @@ var CKBUILDER_CONFIG = {
 		'cy' : 1,
 		'da' : 1,
 		'de' : 1,
+		'de-ch' : 1,
 		'el' : 1,
 		'en' : 1,
 		'en-au' : 1,
@@ -128,6 +133,7 @@ var CKBUILDER_CONFIG = {
 		'en-gb' : 1,
 		'eo' : 1,
 		'es' : 1,
+		'es-mx' : 1,
 		'et' : 1,
 		'eu' : 1,
 		'fa' : 1,
@@ -157,6 +163,7 @@ var CKBUILDER_CONFIG = {
 		'nb' : 1,
 		'nl' : 1,
 		'no' : 1,
+		'oc' : 1,
 		'pl' : 1,
 		'pt' : 1,
 		'pt-br' : 1,
