7523174b276b67c73ba7640448ebca6836bb89c2
Tristan Cavelier <tristan.cavelier@nexedi.com>
2017-10-13 14:33:50 +0000

    erp5_post: do not call edit in addition to newContent

--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
@@ -2,7 +2,6 @@ portal = context.getPortalObject()
 
 # create an HTML Post
 post_module = portal.post_module
-post = post_module.newContent(portal_type='HTML Post')
 
 # get the related object
 object_list = portal.portal_catalog(relative_url=follow_up) # with id keyword, this function will return a sequence data type which contains one element.
@@ -21,7 +20,10 @@ post_edit_kw = {
 if predecessor is not None:
   predecessor_value, = portal.portal_catalog(relative_url=predecessor)
   post_edit_kw["predecessor_value"] = predecessor_value.getObject()
-post.edit(**post_edit_kw)
+post = post_module.newContent(
+  portal_type='HTML Post',
+  **post_edit_kw
+)
 
 post.publish()
 # We need to reindex the object on server. So the page will get the post which
