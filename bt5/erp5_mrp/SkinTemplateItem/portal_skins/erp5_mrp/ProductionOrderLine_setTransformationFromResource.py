8159b324d6456c0622b9ca53fb2006944f6d2d4d
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-10-18 13:43:17 +0000

    erp5_mrp: Update Transformation on Order Line if resource changed

--- bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/ProductionOrderLine_setTransformationFromResource.py
+++ bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/ProductionOrderLine_setTransformationFromResource.py
@@ -3,7 +3,8 @@ portal = production_order_line.getPortalObject()
 
 transformation = production_order_line.getSpecialiseValue(
   portal_type=portal.getPortalTransformationTypeList())
-if transformation is None:
+if transformation is None \
+    or (transformation.getResource() != production_order_line.getResource()):
   resource_uid = production_order_line.getResourceUid()
   if resource_uid:
     transformation_list = portal.portal_catalog(
@@ -13,3 +14,5 @@ if transformation is None:
     if len(transformation_list) == 1:
       transformation = transformation_list[0].getRelativeUrl()
       production_order_line.setSpecialise(transformation)
+      return
+  production_order_line.setSpecialise(None)
