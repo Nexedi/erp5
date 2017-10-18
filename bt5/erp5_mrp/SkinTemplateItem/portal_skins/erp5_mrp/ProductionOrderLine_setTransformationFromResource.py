4d6fea67d352e28670d04218564b15d8838bd2a0
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-10-18 13:34:58 +0000

    erp5_mrp: move ProductionOrderLine_setTransformationFromResource to skin folder
    * For the purpose of easy customisation

---
+++ bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/ProductionOrderLine_setTransformationFromResource.py
@@ -0,0 +1,15 @@
+production_order_line = context
+portal = production_order_line.getPortalObject()
+
+transformation = production_order_line.getSpecialiseValue(
+  portal_type=portal.getPortalTransformationTypeList())
+if transformation is None:
+  resource_uid = production_order_line.getResourceUid()
+  if resource_uid:
+    transformation_list = portal.portal_catalog(
+      portal_type=portal.getPortalTransformationTypeList(),
+      validation_state="!=invalidated",
+      default_resource_uid=resource_uid)
+    if len(transformation_list) == 1:
+      transformation = transformation_list[0].getRelativeUrl()
+      production_order_line.setSpecialise(transformation)
--- bt5/erp5_mrp/WorkflowTemplateItem/portal_workflow/production_order_interaction_workflow/scripts/ProductionOrderLine_setTransformationFromResource.py
+++ bt5/erp5_mrp/WorkflowTemplateItem/portal_workflow/production_order_interaction_workflow/scripts/ProductionOrderLine_setTransformationFromResource.py
@@ -1,16 +1,1 @@
-production_order_line = state_change['object']
-portal = production_order_line.getPortalObject()
-
-transformation = production_order_line.getSpecialiseValue(
-  portal_type=portal.getPortalTransformationTypeList())
-if transformation is None:
-  portal = production_order_line.getPortalObject()
-  resource_uid = production_order_line.getResourceUid()
-  if resource_uid:
-    transformation_list = portal.portal_catalog(
-      portal_type=portal.getPortalTransformationTypeList(),
-      validation_state="!=invalidated",
-      default_resource_uid=resource_uid)
-    if len(transformation_list) == 1:
-      transformation = transformation_list[0].getRelativeUrl()
-      production_order_line.setSpecialise(transformation)
+state_change['object'].ProductionOrderLine_setTransformationFromResource()
