278addde2e54a8d964182d669b523d1903cc206b
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2017-10-25 15:51:11 +0000

    erp5_mrp: add Production Simulation Rule
    erp5_mrp: Fix naming to Simulation Movement test Production Simulation Rule
    
    erp5_mrp: Have ProductionSimulationRule works without items
    
    erp5_mrp: Transformation Simulation rules is adapted to use Manufacturing Order

--- bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/SimulationMovement_testProductionSimulationRule.py
+++ bt5/erp5_mrp/SkinTemplateItem/portal_skins/erp5_mrp/SimulationMovement_testProductionSimulationRule.py
@@ -6,8 +6,6 @@ if parent.getPortalType() == 'Applied Rule' and movement.getSpecialiseValue(port
   if grand_parent.getPortalType() == "Simulation Movement" and \
       grand_parent.getDeliveryValue() and \
       grand_parent.getDeliveryValue().getPortalType() == "Production Order Line":
-    resource_value = context.getResourceValue()
-    if resource_value:
-      result = (len(resource_value.getAggregatedPortalTypeList()) != 0) and \
-                 ("production" in resource_value.getUseList())
+    resource_value = grand_parent.getDeliveryValue().getResourceValue()
+    result = resource_value is not None
 return result
