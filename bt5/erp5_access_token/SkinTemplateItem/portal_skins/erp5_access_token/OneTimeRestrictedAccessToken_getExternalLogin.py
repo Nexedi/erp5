a7b54da5bce2d9be56a70d3ca7dfbf5e4b229965
Rafael Monnerat <rafael@nexedi.com>
2017-11-03 16:39:41 +0000

    erp5_access_token: Fix *_getExternalLogin API
    Since the change for use of User ID this code should return reference and portal type of the ERP5 Login object.

--- bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/OneTimeRestrictedAccessToken_getExternalLogin.py
+++ bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/OneTimeRestrictedAccessToken_getExternalLogin.py
@@ -2,7 +2,7 @@ from zExceptions import Unauthorized
 if REQUEST is not None:
   raise Unauthorized
 
-result = None
+result = None, None
 access_token_document = context
 request = context.REQUEST
 
@@ -14,7 +14,10 @@ if access_token_document.getValidationState() == 'validated':
 
     agent_document = access_token_document.getAgentValue()
     if agent_document is not None:
-      result = agent_document.Person_getUserId()
+      portal = agent_document.getPortalObject()
+      for erp5_login in agent_document.objectValues(portal.getPortalLoginTypeList()):
+        if erp5_login.getValidationState() == "validated":
+          result = erp5_login.getReference(), erp5_login.getPortalType()
       comment = "Token usage accepted"
 
   access_token_document.invalidate(comment=comment)
