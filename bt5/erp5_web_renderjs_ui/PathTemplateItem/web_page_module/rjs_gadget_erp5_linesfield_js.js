f4e03d67a43a66d026deaf04a4e0d12754d733f2
Tomas Peterka <tomas.peterka@nexedi.com>
2017-11-09 17:00:12 +0100

    [renderjs_ui] LinesField works in non-editable mode
    /#/bug_module/20171103-82F24A
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/489

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_linesfield_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_linesfield_js.js
@@ -8,79 +8,53 @@
     return lines;
   }
 
-  function listToBR(lines) {
-    if (Array.isArray(lines)) {return lines.join("<br/>\n"); }
-    return lines;
-  }
-
   rJS(window)
-
-    .declareMethod('render', function (options) {
-      return this.changeState(options.field_json);
+    .setState({
+      gadget_rendered: false
     })
 
-    .onStateChange(function (modification_dict) {
+    .declareMethod('render', function (options) {
       var gadget = this,
-        url,
-        value;
+        new_state = {
+          "default": listToNewlines(options.field_json.default),
+          "editable": options.field_json.editable,
+          "required": options.field_json.required,
+          "hidden": options.field_json.hidden,
+          "title": options.field_json.title,
+          "key": options.field_json.key
+        };
 
-      if (modification_dict.hasOwnProperty('editable')) {
-        if (gadget.state.editable) {
-          url = 'gadget_html5_textarea.html';
-          value = listToNewlines(gadget.state.value || gadget.state.default || []);
-        } else {
-          url = 'gadget_html5_element.html';
-          value = listToBR(gadget.state.value || gadget.state.default || []);
-        }
-        return this.declareGadget(url, {scope: 'sub'})
-          .push(function (sub_gadget) {
-            // Clear first to DOM, append after to reduce flickering/manip
-            while (gadget.element.firstChild) {
-              gadget.element.removeChild(gadget.element.firstChild);
-            }
-            gadget.element.appendChild(sub_gadget.element);
-            // Use full-blown render when the widget is new
-            return sub_gadget.render({
-              value: value,
-              name: gadget.state.key,
-              editable: gadget.state.editable,
-              required: gadget.state.required,
-              title: gadget.state.title,
-              hidden: gadget.state.hidden
-            });
+      if (this.state.gadget_rendered === false) {
+        return gadget.declareGadget("gadget_erp5_field_textarea.html", {scope: 'sub'})
+          .push(function (subgadget) {
+            gadget.element.appendChild(subgadget.element);
+            new_state.gadget_rendered = true;
+            return gadget.changeState(new_state);
           });
       }
+      return this.changeState(new_state);
+    })
 
-      return gadget.getDeclaredGadget('sub')
-        .push(function (input) {
-          if (modification_dict.hasOwnProperty("value")) {
-            if (gadget.state.editable) {
-              modification_dict.value = listToNewlines(modification_dict.value);
-            } else {
-              modification_dict.value = listToBR(modification_dict.value);
-            }
-          }
-          // when we only receive changes we can simply pass (minimaly modified) modification dictionary
-          return input.render(modification_dict);
+    .onStateChange(function () {
+      var gadget = this;
+      return this.getDeclaredGadget('sub')
+        .push(function (subgadget) {
+          return subgadget.render({field_json: gadget.state});
         });
     })
 
     .declareMethod('getContent', function () {
-      var gadget = this;
-      return gadget.getDeclaredGadget('sub')
+      return this.getDeclaredGadget('sub')
         .push(function (sub_gadget) {
           return sub_gadget.getContent();
         });
     })
 
     .declareMethod('checkValidity', function () {
-      if (this.state.editable) {
-        return this.getDeclaredGadget('sub')
-          .push(function (gadget) {
-            return gadget.checkValidity();
-          });
-      }
-      return true;
+      return this.getDeclaredGadget('sub')
+        .push(function (subgadget) {
+          return subgadget.checkValidity();
+        });
     });
 
 }(window, rJS));
