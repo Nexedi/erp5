3d735cbeced23bce7cf8b770c3528f0af3a51ea3
Jérome Perrin <jerome@nexedi.com>
2017-11-16 02:55:28 +0000

    bank_reconciliation: extend related key to support internal transaction line

--- bt5/erp5_bank_reconciliation/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_aggregate_bank_reconciliation.sql
+++ bt5/erp5_bank_reconciliation/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_aggregate_bank_reconciliation.sql
@@ -7,3 +7,14 @@ AND <dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.aggregat
 </dtml-if>
 <dtml-var RELATED_QUERY_SEPARATOR>
 <dtml-var table_0>.uid = <dtml-var query_table>.uid
+
+-- A line can be reconcilied for source_payment or destination_payment
+-- so we also add a condition that the related bank reconciliation is
+-- for "this side". Another approach, more efficient, is to customize `stock`
+-- table to add `aggregate_bank_reconciliation_date` and `aggregate_bank_reconciliation_uid`
+-- columns. See https://lab.nexedi.com/nexedi/erp5/merge_requests/495
+AND stock.payment_uid in (
+  select category.category_uid
+  from category
+  where <dtml-var table_1>.uid = category.uid
+  and category.base_category_uid = <dtml-var "portal_categories.source_payment.getUid()"> )
