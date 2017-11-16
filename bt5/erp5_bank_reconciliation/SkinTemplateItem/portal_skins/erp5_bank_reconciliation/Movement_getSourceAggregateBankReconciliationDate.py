337451e87611f5e05646fd906e2ab3281ef87aa9
Jérome Perrin <jerome@nexedi.com>
2017-11-16 03:01:57 +0000

    bank_reconciliation: add scripts for indexing bank_reconciliation in stock table
    These scripts are not used by default, because it's not convenient to
    extend stock table, but sites with large amount of payment transactions
    are advised to customize stock table (by patching `z_create_stock` and
    `z_catalog_stock_list` to add:
    
     * `stock.aggregate_bank_reconciliation_date`, indexing
      `Movement_getSourceAggregateBankReconciliationDate` for source side
      and `Movement_getDestinationAggregateBankReconciliationDate` for
      destination side.
    * `stock.aggregate_bank_reconciliation_uid`, indexing
      `Movement_getSourceAggregateBankReconciliationUid` for source side
      and `Movement_getDestinationAggregateBankReconciliationUid` for
      destination side.
    * configure `aggregate_bank_reconciliation_date` and
      `stock.aggregate_bank_reconciliation_date` as datetime keys.
    
    These two columns `aggregate_bank_reconciliation_date` and
    `aggregate_bank_reconciliation_uid` should have priority over related keys
    of the same name (It may be needed to remove the related keys as part of
    the customisation)

---
+++ bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/Movement_getSourceAggregateBankReconciliationDate.py
@@ -0,0 +1,3 @@
+for bank_reconciliation in context.getAggregateValueList(portal_type='Bank Reconciliation'):
+  if bank_reconciliation.getSourcePayment() == context.getSourcePayment():
+    return bank_reconciliation.getStopDate()
