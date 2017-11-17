df3a4d27fc3b2cc692d039b49173f9f906e29259
Alain Takoudjou <alain.takoudjou@nexedi.com>
2017-11-17 16:48:08 +0000

    [erp5_web_monitoring] Monitoring dispatch now be used to access items in monitor.
    If the search result is only one item, redirect to the item view page, else redirect to the list of that items and let user choose the good one.

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, Handlebars */
+/*global window, rJS, RSVP, Handlebars */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS, Handlebars) {
+(function (window, rJS, RSVP, Handlebars) {
   "use strict";
 
   var gadget_klass = rJS(window),
@@ -8,6 +8,60 @@
                          .getElementById("dispatch-template")
                          .innerHTML,
     dispatch_template = Handlebars.compile(source);
+
+  function searchItem(gadget, search_query) {
+    var redirect_options = {},
+      select_list = ["portal_type"];
+    if (search_query === undefined || search_query === "") {
+      return new RSVP.Queue()
+        .push(function () {
+          return;
+        });
+    }
+    return new RSVP.Queue()
+      .push(function () {
+        return gadget.getSetting("listbox_lines_limit", 20);
+      })
+      .push(function (lines_limit) {
+        return gadget.jio_allDocs({
+          query: search_query,
+          select_list: select_list,
+          limit: [0, lines_limit]
+        });
+      })
+      .push(function (result) {
+        if (result === undefined || result.data.total_rows === 0) {
+          // no result from the query
+          return;
+        }
+        if (result.data.total_rows === 1) {
+          // one item found, redirect to it
+          redirect_options = {
+            "jio_key": result.data.rows[0].id
+          };
+          if (result.data.rows[0].value.portal_type === "opml") {
+            redirect_options.page = "ojsm_hosting_subscription_view";
+            redirect_options.opml_key = result.data.rows[0].id;
+          }
+          return gadget.redirect({"command": "index", options: redirect_options});
+        }
+        redirect_options = {
+          extended_search: gadget.state.query
+        };
+        if (gadget.state.portal_type === undefined) {
+          // take the first one
+          gadget.state.portal_type = result.data.rows[0].value.portal_type;
+        }
+        if (gadget.state.portal_type === "opml") {
+          redirect_options.page = "ojsm_hosting_subscription_list";
+        } else if (gadget.state.portal_type === "Software Instance") {
+          redirect_options.page = "ojsm_software_instance_list";
+        } else if (gadget.state.portal_type === "promise") {
+          redirect_options.page = "ojsm_status_list";
+        }
+        return gadget.redirect({"command": "display", options: redirect_options});
+      });
+  }
 
   gadget_klass
 
@@ -17,21 +71,113 @@
     .declareAcquiredMethod("translateHtml", "translateHtml")
     .declareAcquiredMethod("getUrlFor", "getUrlFor")
     .declareAcquiredMethod("updateHeader", "updateHeader")
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+    .declareAcquiredMethod("redirect", "redirect")
+    .declareAcquiredMethod("getSetting", "getSetting")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
     /////////////////////////////////////////////////////////////////
+    .declareMethod("triggerSubmit", function () {
+      var argument_list = arguments;
+      return this.getDeclaredGadget('form_view')
+        .push(function (gadget) {
+          return gadget.triggerSubmit.apply(gadget, argument_list);
+        });
+    })
     .declareMethod("render", function (options) {
+      var gadget = this,
+        regex = /portal_type\s*\:\s*[\'\"]([\w+\s+]+)[\'\"]/i,
+        regex_list = /\(?([\w\d\_]+)\s*\:\s*\(([\"\w\_\d\-\.\s*\,]+)\)\)?/g,
+        query_list = [],
+        i,
+        tmp,
+        page_title = "Monitoring Search",
+        original_query,
+        portal_type,
+        pt_result,
+        result;
+
+      result = regex_list.exec(options.query);
+      if (result !== null) {
+        tmp = result[2].split(',');
+        for (i = 0; i < tmp.length; i += 1) {
+          query_list.push(result[1] + ': ' + tmp[i].trim());
+        }
+        options.query = options.query
+          .replace(regex_list, '(' + query_list.join(' OR ') + ')');
+      }
+      original_query = JSON.parse(JSON.stringify(options.query || ""));
+
+      pt_result = regex.exec(options.query);
+      if (pt_result !== null) {
+        page_title = "Searching " + pt_result[1] + "(s)";
+        if (pt_result[1] === "Hosting Subscription") {
+          portal_type = "opml";
+          options.query = options.query.replace(pt_result[1], 'opml');
+        } else {
+          portal_type = pt_result[1];
+        }
+      } else {
+        portal_type = "promise";
+      }
+
+      return gadget.changeState({
+        original_query: original_query,
+        query: options.query,
+        page_title: page_title,
+        portal_type: portal_type
+      });
+    })
+    .onStateChange(function () {
       var gadget = this;
 
-      gadget.element.innerHTML = dispatch_template({item: options.query || undefined});
-      return gadget.getUrlFor({command: 'display', options: {page: 'ojsm_status_list'}})
-        .push(function (url) {
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.getUrlFor({command: 'display', options: {page: 'ojsm_status_list'}});
+        })
+        .push(function (back_url) {
           return gadget.updateHeader({
-            back_url: url,
-            page_title: "Jump to Object"
+            page_title: gadget.state.page_title,
+            back_url: back_url
           });
+        })
+        .push(function () {
+          return gadget.getDeclaredGadget('erp5_searchfield');
+        })
+        .push(function (searchfield) {
+          return searchfield.render({
+            extended_search: gadget.state.original_query,
+            focus: true
+          });
+        })
+        .push(function () {
+          return searchItem(gadget, gadget.state.query);
+        })
+        .push(function (search_result) {
+          if (search_result === undefined && gadget.state.query) {
+            gadget.element.querySelector('.search-result')
+              .innerHTML = dispatch_template({});
+          }
         });
-    });
+    })
+    .onEvent('submit', function () {
+      var gadget = this;
 
-}(window, rJS, Handlebars));
+      return gadget.getDeclaredGadget("erp5_searchfield")
+        .push(function (search_gadget) {
+          return search_gadget.getContent();
+        })
+        .push(function (data) {
+          var options = {
+            page: "ojsm_dispatch"
+          };
+          if (data.search) {
+            options.query = data.search;
+            return gadget.redirect({command: 'change', options: options});
+          }
+        });
+
+    }, false, true);
+
+}(window, rJS, RSVP, Handlebars));
