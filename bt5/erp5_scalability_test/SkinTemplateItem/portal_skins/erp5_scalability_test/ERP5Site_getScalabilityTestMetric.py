e3492d2fe584428a93755601cd215f21c4dc5026
Roque Porchetto <roqueporchetto@gmail.com>
2017-11-30 13:36:18 +0100

    erp5_scalability_test: new script for metrics - ERP5Site_getScalabilityTestMetric script gets metrics of last created objects - Bootstrap script now creates the users for test

---
+++ bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_getScalabilityTestMetric.py
@@ -0,0 +1,14 @@
+import json
+from DateTime import DateTime
+now = DateTime()
+now_minus_6 = now - 1.0/24/60*6
+now_minus_1 = now - 1.0/24/60*1
+catalog_kw = {'creation_date': {'query': (now_minus_6, now_minus_1), 'range': 'minmax'}, 'validation_state': 'validated'}
+portal = context.getPortalObject()
+portal_catalog = portal.portal_catalog
+person_count = len(portal_catalog(portal_type="Person", **catalog_kw))
+sale_order_count = len(portal_catalog(portal_type="Sale Order", **catalog_kw))
+person_per_hour = 60*person_count/5
+sale_order_per_hour = 60*sale_order_count/5
+output_string = """Person: %r doc/hour SaleOrder: %r doc/hour""" % (person_per_hour, sale_order_per_hour)
+return json.dumps({"person_per_hour" : person_per_hour, "sale_order_per_hour": sale_order_per_hour})
