e7e8ea0d8073ad1ee937f9b4f80a74907701ae5c
Tristan Cavelier <tristan.cavelier@nexedi.com>
2017-12-09 12:56:49 -0500

    erp5_post: allow to set follow_up_value

--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
@@ -1,24 +1,13 @@
 portal = context.getPortalObject()
-
-# create an HTML Post
 post_module = portal.post_module
 
-# get the related object
-follow_up_object, = portal.portal_catalog(relative_url=follow_up, limit=2)
-follow_up_object = follow_up_object.getObject()
-
 now = DateTime()
-post_edit_kw = {
-  "start_date": now,
-  "follow_up_value": follow_up_object,
-  "text_content": data,
-}
-if predecessor is not None:
-  predecessor_value, = portal.portal_catalog(relative_url=predecessor, limit=2)
-  post_edit_kw["predecessor_value"] = predecessor_value.getObject()
+assert "text_content" in post_edit_kw or "data" in post_edit_kw
 post = post_module.newContent(
   portal_type='HTML Post',
+  start_date=now,
   **post_edit_kw
 )
+assert post.getFollowUp()
 
 return post
--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
@@ -1,5 +1,9 @@
-return context.PostModule_createHTMLPost(
-  follow_up=follow_up,
-  predecessor=predecessor,
-  data="<p>" + data.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("  ", " &nbsp;").replace("\n", "<br/>") + "</p>",
-)
+def textToP(s):
+  return "<p>" + s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("  ", " &nbsp;").replace("\n", "<br/>") + "</p>"
+
+for key in ("text_content", "data"):
+  __traceback_info__ = key
+  if key in post_edit_kw:
+    post_edit_kw[key] = textToP(post_edit_kw[key])
+
+return context.PostModule_createHTMLPost(**post_edit_kw)
