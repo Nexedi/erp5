6a66502d3ed33b44cbc707e18947e597f20db4cd
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2017-12-15 08:56:34 +0000

    erp5_core: use preferred category child item list method in Predicate tab.

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Predicate_getMembershipCriterionCategoryList.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Predicate_getMembershipCriterionCategoryList.py
@@ -8,16 +8,17 @@ for item in multimembership_base_list :
 
 category_list = []
 
+display_method_id = context.getPortalObject().portal_preferences.getPreference('preferred_category_child_item_list_method_id', 'getCategoryChildCompactLogicalPathItemList')
 ctool = context.portal_categories
 for item in mixed_list:
   base_category = ctool[item]
-  item_list = base_category.getCategoryChildCompactLogicalPathItemList(base=1)[:]
+  item_list = getattr(base_category, display_method_id)(base=1)[:]
   if item_list == [['', '']]:
     for fallback_category_id in base_category.getFallbackBaseCategoryList():
       fallback_category = ctool.restrictedTraverse(fallback_category_id, None)
       if fallback_category is not None and fallback_category.objectIds():
         item_list.extend([('%s/%s' % (fallback_category_id, x[0]), '%s/%s' % (item, x[1])) \
-          for x in fallback_category.getCategoryChildCompactLogicalPathItemList(base=1) if x[0]])
+          for x in getattr(fallback_category, display_method_id)(base=1) if x[0]])
         break
 
   category_list.extend(item_list)
