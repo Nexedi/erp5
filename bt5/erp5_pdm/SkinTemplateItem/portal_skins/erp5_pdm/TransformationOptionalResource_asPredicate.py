eecfbbe12a7207568d037e8828dfa1c338ce2c7b
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2017-12-15 08:33:34 +0000

    erp5_pdm: respect existing predicate in TransformationOptionalResource_asPredicate.

--- bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/TransformationOptionalResource_asPredicate.py
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/TransformationOptionalResource_asPredicate.py
@@ -1,9 +1,11 @@
-option_category = context.getOptionList()
-option_base_category = []
-if len(option_category) > 0:
-  option_base_category = [option_category[0].split('/')[0]]
+membership_criterion_category_list = context.getMembershipCriterionCategoryList()
+membership_criterion_base_category_list = context.getMembershipCriterionBaseCategoryList()
+
+for option in context.getOptionList():
+  membership_criterion_category_list.append(option)
+  membership_criterion_base_category_list.append(option.split('/')[0])
 
 new_context = context.asContext(
-    membership_criterion_category=option_category,
-    membership_criterion_base_category=option_base_category)
+    membership_criterion_category=tuple(set(membership_criterion_category_list)),
+    membership_criterion_base_category=tuple(set(membership_criterion_base_category_list)))
 return new_context
