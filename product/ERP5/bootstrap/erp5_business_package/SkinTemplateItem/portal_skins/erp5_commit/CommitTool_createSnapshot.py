a10f5685ed25e5bc49e5e125687e938edf43a46f
Ayush <ayush.tiwari@nexedi.com>
2017-12-19 16:57:15 +0100

    [erp5_business_package]: Update view, add similar category

---
+++ product/ERP5/bootstrap/erp5_business_package/SkinTemplateItem/portal_skins/erp5_commit/CommitTool_createSnapshot.py
@@ -0,0 +1,15 @@
+# Check if the conext is Commit Tool
+if context.getPortalType() != 'Commit Tool':
+  return 'context is not commit tool'
+
+# Check if the last created sub-object in Commit Tool is a commit and is not
+# raise an Error
+last_obj = max(context.objectValues(), key=(lambda x: x.getCreationDate()))
+
+if last_obj.getPortalType() != 'Business Commit':
+  return 'No commits added after last snapshot. Add a commit and try again'
+
+# Create a new snapshot based on last commit
+snapshot = last_obj.createEquivalentSnapshot()
+
+return context.Base_redirect('view')
