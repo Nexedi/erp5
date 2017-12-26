44ef3b0c935a1682c18697861a5cc26d716fa829
Yusei Tahara <yusei@nexedi.com>
2017-12-26 15:19:18 +0900

    [erp5_base] Fix Delivery_getLedgerItemList to make it work with non-Delivery Type portal type documents.

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_getLedgerItemList.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_getLedgerItemList.py
@@ -6,6 +6,10 @@ This will return only ledgers allowed in the delivery type definition.
 portal = context.getPortalObject()
 
 ledger_item_list = context.Base_getPreferredCategoryChildItemList(portal.portal_categories.ledger)
-allowed_ledger_list = portal.portal_types[context.getPortalType()].getLedgerList()
 
-return [('', '')] + [item for item in ledger_item_list if item[1] in allowed_ledger_list]
+type_info = portal.portal_types[context.getPortalType()]
+if getattr(type_info, 'getLedgerList', None) is not None:
+  allowed_ledger_list = type_info.getLedgerList()
+  return [('', '')] + [item for item in ledger_item_list if item[1] in allowed_ledger_list]
+else:
+  return ledger_item_list
