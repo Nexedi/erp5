3d33c837402d974e81565f113dd666d96d53756a
Ayush Tiwari <ayush.tiwari@nexedi.com>
2017-12-28 17:32:37 +0100

    [erp5_business_package]: Cleanup workflow for Commit Tool

--- product/ERP5/bootstrap/erp5_business_package/WorkflowTemplateItem/portal_workflow/business_manager_interaction_workflow/scripts/BusinessManager_updateBuildingState.py
+++
@@ -1,6 +0,0 @@
-modified = state_change['object']
-
-manager = modified.getParentValue()
-if manager.getPortalType() == 'Business Manager':
-  # Explicilty update the building_state of Business Manager
-  manager.changeBuildingStatetoModified()
