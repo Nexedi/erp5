5e49eb818bf73352a775ac15fc6a082ee436e9f6
Tristan Cavelier <tristan.cavelier@nexedi.com>
2017-12-29 10:47:28 +0000

    erp5_core: Base_reindexObjectSecurity use getattr instead of __getitem__

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_reindexObjectSecurity.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_reindexObjectSecurity.py
@@ -4,7 +4,7 @@
 # with lots of content could mean hours of non-usable overloaded system.
 type_tool = context.getPortalObject().portal_types 
 for portal_type_name in context.getTypeInfo().getTypeAllowedContentTypeList():
-  portal_type = type_tool[portal_type_name]
+  portal_type = getattr(type_tool, portal_type_name)
   if portal_type.getTypeAcquireLocalRole():
     reindex = context.recursiveReindexObject
     break
