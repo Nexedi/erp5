d90f7c8fb70d60d615b8ad75cd4f18394d90d6bd
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2018-01-08 13:21:11 +0000

    erp5_payroll_l10n_fr: individual contribution for retirement's base is 0.
    Only quantity should be set

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_getSocialContributionDict.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_getSocialContributionDict.py
@@ -106,17 +106,18 @@ def makeTaxableBaseComponentBlock(movement, category):
 
 def makeIndividualContributionBlock(movement, category):
   base = quantity = 0.0
-  if category in ('018', '063', '064'):
+  if category in ('063', '064'):
     quantity = (getattr(movement, 'employer_total_price', 0) + getattr(movement, 'employee_total_price', 0)) * -1
     # If "reduction generale but CTP is 801P then it should be positive, as we have to give money back
-    if category == '018' and 'base_amount/payroll/l10n/fr/ctp/801P' not in movement.getBaseContributionList():
-      assert quantity <= 0., "Quantity in %s should be negative" % movement.absolute_url()
-    elif category == '018':
-      assert quantity >= 0., "Quantity in %s should be positive" % movement.absolute_url()
+  elif category == '018':
     base = movement.base
+    quantity = (getattr(movement, 'employer_total_price', 0) + getattr(movement, 'employee_total_price', 0)) * -1
+    if 'base_amount/payroll/l10n/fr/ctp/801P' not in movement.getBaseContributionList():
+      assert quantity <= 0., "Quantity in %s should be negative" % movement.absolute_url()
+    else:
+      assert quantity >= 0., "Quantity in %s should be positive" % movement.absolute_url()
   elif category in ('059',):
     quantity = (getattr(movement, 'employer_total_price', 0) + getattr(movement, 'employee_total_price', 0)) * -1
-    base = 0.
   else:
     base = movement.base
   return {
