899f29ac85229231ee0e9080a742cf18fac6e248
Tristan Cavelier <tristan.cavelier@nexedi.com>
2018-01-10 10:48:19 +0000

    erp5_base: restore Create User action condition

--- bt5/erp5_base/WorkflowTemplateItem/portal_workflow/user_account_workflow/scripts/UserAccountWorkflow_verifyUserCreationInput.py
+++ bt5/erp5_base/WorkflowTemplateItem/portal_workflow/user_account_workflow/scripts/UserAccountWorkflow_verifyUserCreationInput.py
@@ -1,6 +1,9 @@
 from Products.DCWorkflow.DCWorkflow import ValidationFailed
 from Products.ERP5Type.Message import translateString
 
+if len(object.searchFolder(portal_type="ERP5 Login", validation_state="validated", limit=1)):
+  raise ValidationFailed(translateString("Account already created."))
+
 kwargs = state_change['kwargs']
 password = kwargs['password']
 password_confirm = kwargs['password_confirm']
