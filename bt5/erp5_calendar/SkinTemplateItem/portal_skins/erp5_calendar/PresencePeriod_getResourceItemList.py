e6bedee72379423b41ae92ca581575be01a8bbd0
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2018-01-11 09:15:32 +0000

    erp5_calendar: Presence Request Period to use service as resource
    Allign with Leave Request Period

--- bt5/erp5_calendar/SkinTemplateItem/portal_skins/erp5_calendar/PresencePeriod_getResourceItemList.py
+++ bt5/erp5_calendar/SkinTemplateItem/portal_skins/erp5_calendar/PresencePeriod_getResourceItemList.py
@@ -1,17 +1,1 @@
-portal = context.getPortalObject()
-
-preferred_presence_calendar_period_type = portal.portal_preferences\
-        .getPreferredPresenceCalendarPeriodType()
-
-method_id = portal.portal_preferences.getPreference(
-     'preferred_category_child_item_list_method_id', 'getCategoryChildCompactLogicalPathItemList')
-
-category = portal.portal_categories.calendar_period_type
-
-if preferred_presence_calendar_period_type:
-  category = category.restrictedTraverse(preferred_presence_calendar_period_type, category)
-
-return getattr(category, method_id)(local_sort_id=('int_index', 'translated_title'),
-                                    checked_permission='View',
-                                    is_self_excluded=0,
-                                    base=1)
+return context.Ticket_getResourceItemList()
