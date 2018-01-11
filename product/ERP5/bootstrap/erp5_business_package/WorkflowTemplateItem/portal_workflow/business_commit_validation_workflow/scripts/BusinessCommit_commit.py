6360af92277bb22aa4a03a841c433de05d607943
Ayush Tiwari <ayush.tiwari@nexedi.com>
2018-01-11 16:23:54 +0100

    [erp5_business_package]: Update workflows and add jump action on Business Item objects

--- product/ERP5/bootstrap/erp5_business_package/WorkflowTemplateItem/portal_workflow/business_commit_validation_workflow/scripts/BusinessCommit_commit.py
+++ product/ERP5/bootstrap/erp5_business_package/WorkflowTemplateItem/portal_workflow/business_commit_validation_workflow/scripts/BusinessCommit_commit.py
@@ -6,13 +6,16 @@ from Products.DCWorkflow.DCWorkflow import ValidationFailed
 from Products.ERP5Type.Message import translateString
 
 business_commit = state_change['object']
-listbox = state_change.kwargs.get('listbox')
 
 business_commit.Base_checkConsistency()
-
 business_item_list = business_commit.objectValues()
+
+commit_path_list = business_commit.getItemPathList()
+# Raise error in case the commit have 2 items which has same path
+if len(commit_path_list) != len(set(commit_path_list)):
+  raise ValidationFailed(translateString('Multiple items have same path !'))
 
 # Raise error in case there is no Business Item or Business Property Item added
 # in the Business Commit
 if not business_item_list:
-  raise ValidationFailed(translateString('Please add item in commit before committing'))
+  raise ValidationFailed(translateString('Please add item(s) in commit before committing'))
