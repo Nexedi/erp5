d0472bc2ea9f6ebc2b31cf7ca27ee8a5e6dd1480
Vincent Pelletier <vincent@nexedi.com>
2018-01-19 18:15:09 +0900

    CMFActivity: Stop deleting duplicates during SQLDict.distribute
    Duplicate message detection is not good enough: different messages with
    the same unicity value may bear different serialization_tags. This code
    does not takes this into account, which can lead to deleting such tagged
    message and validate an untagged one, which breaks serialization_tag
    contract of preventing any further activity validation until execution
    of all such-tagged validated activities is successful.
    Also, it is not validation's node job to deduplicate: it can happen during
    message execution without slowing down this crucial (performance-wise)
    activity node.
    As a result, distribute methods of SQLDict and SQLQueue can be factorised.

--- product/CMFActivity/Activity/SQLBase.py
+++ product/CMFActivity/Activity/SQLBase.py
@@ -40,6 +40,10 @@ from Products.CMFActivity.ActivityRuntimeEnvironment import (
 from Queue import Queue, VALIDATION_ERROR_DELAY, VALID, INVALID_PATH
 from Products.CMFActivity.Errors import ActivityFlushError
 
+# Stop validating more messages when this limit is reached
+MAX_VALIDATED_LIMIT = 1000
+# Read this many messages to validate.
+READ_MESSAGE_LIMIT = 1000
 # TODO: Limit by size in bytes instead of number of rows.
 MAX_MESSAGE_LIST_SIZE = 100
 INVOKE_ERROR_STATE = -2
@@ -314,6 +318,62 @@ class SQLBase(Queue):
   def _log(self, severity, summary):
     LOG(self.__class__.__name__, severity, summary,
         error=severity>INFO and sys.exc_info() or None)
+
+  def distribute(self, activity_tool, node_count):
+    assignMessage = getattr(activity_tool, 'SQLBase_assignMessage', None)
+    if assignMessage is None:
+      return
+    offset = 0
+    now_date = self.getNow(activity_tool)
+    validated_count = 0
+    while 1:
+      result = self._getMessageList(
+        activity_tool,
+        processing_node=-1,
+        to_date=now_date,
+        offset=offset,
+        count=READ_MESSAGE_LIMIT,
+      )
+      if not result:
+        return
+      transaction.commit()
+
+      validation_text_dict = {'none': 1}
+      message_dict = {}
+      for line in result:
+        message = Message.load(line.message, uid=line.uid, line=line)
+        if not hasattr(message, 'order_validation_text'): # BBB
+          message.order_validation_text = self.getOrderValidationText(message)
+        self.getExecutableMessageList(activity_tool, message, message_dict,
+                                      validation_text_dict, now_date=now_date)
+      if message_dict:
+        distributable_uid_set = set()
+        serialization_tag_dict = {}
+        for message in message_dict.itervalues():
+          serialization_tag = message.activity_kw.get('serialization_tag')
+          if serialization_tag is None:
+            distributable_uid_set.add(message.uid)
+          else:
+            serialization_tag_dict.setdefault(serialization_tag,
+                                              []).append(message)
+        for message_list in serialization_tag_dict.itervalues():
+          # Sort list of messages to validate the message with highest score
+          message_list.sort(key=sort_message_key)
+          distributable_uid_set.add(message_list[0].uid)
+          group_method_id = message_list[0].line.group_method_id
+          if group_method_id == '\0':
+            continue
+          for message in message_list[1:]:
+            if group_method_id == message.line.group_method_id:
+              distributable_uid_set.add(message.uid)
+        distributable_count = len(distributable_uid_set)
+        if distributable_count:
+          assignMessage(table=self.sql_table,
+            processing_node=0, uid=tuple(distributable_uid_set))
+          validated_count += distributable_count
+          if validated_count >= MAX_VALIDATED_LIMIT:
+            return
+      offset += READ_MESSAGE_LIMIT
 
   def getReservedMessageList(self, activity_tool, date, processing_node,
                              limit=None, group_method_id=None):
--- product/CMFActivity/Activity/SQLQueue.py
+++ product/CMFActivity/Activity/SQLQueue.py
@@ -26,17 +26,7 @@
 #
 ##############################################################################
 
-from Products.CMFActivity.ActivityTool import Message
-from ZODB.POSException import ConflictError
-from SQLBase import SQLBase, sort_message_key
-from zExceptions import ExceptionFormatter
-
-import transaction
-
-# Stop validating more messages when this limit is reached
-MAX_VALIDATED_LIMIT = 1000
-# Read this many messages to validate.
-READ_MESSAGE_LIMIT = 1000
+from SQLBase import SQLBase
 
 class SQLQueue(SQLBase):
   """
@@ -46,54 +36,3 @@ class SQLQueue(SQLBase):
   """
   sql_table = 'message_queue'
   uid_group = 'portal_activity_queue'
-
-  def distribute(self, activity_tool, node_count):
-    offset = 0
-    assignMessage = getattr(activity_tool, 'SQLBase_assignMessage', None)
-    if assignMessage is not None:
-      now_date = self.getNow(activity_tool)
-      validated_count = 0
-      while 1:
-        result = self._getMessageList(activity_tool, processing_node=-1,
-                                      to_date=now_date, processing=0,
-                                      offset=offset, count=READ_MESSAGE_LIMIT)
-        if not result:
-          return
-        transaction.commit()
-
-        validation_text_dict = {'none': 1}
-        message_dict = {}
-        for line in result:
-          message = Message.load(line.message, uid=line.uid, line=line)
-          if not hasattr(message, 'order_validation_text'): # BBB
-            message.order_validation_text = self.getOrderValidationText(message)
-          self.getExecutableMessageList(activity_tool, message, message_dict,
-                                        validation_text_dict, now_date=now_date)
-        if message_dict:
-          distributable_uid_set = set()
-          serialization_tag_dict = {}
-          for message in message_dict.itervalues():
-            serialization_tag = message.activity_kw.get('serialization_tag')
-            if serialization_tag is None:
-              distributable_uid_set.add(message.uid)
-            else:
-              serialization_tag_dict.setdefault(serialization_tag,
-                                                []).append(message)
-          for message_list in serialization_tag_dict.itervalues():
-            # Sort list of messages to validate the message with highest score
-            message_list.sort(key=sort_message_key)
-            distributable_uid_set.add(message_list[0].uid)
-            group_method_id = message_list[0].line.group_method_id
-            if group_method_id == '\0':
-              continue
-            for message in message_list[1:]:
-              if group_method_id == message.line.group_method_id:
-                distributable_uid_set.add(message.uid)
-          distributable_count = len(distributable_uid_set)
-          if distributable_count:
-            assignMessage(table=self.sql_table,
-              processing_node=0, uid=tuple(distributable_uid_set))
-            validated_count += distributable_count
-            if validated_count >= MAX_VALIDATED_LIMIT:
-              return
-        offset += READ_MESSAGE_LIMIT
