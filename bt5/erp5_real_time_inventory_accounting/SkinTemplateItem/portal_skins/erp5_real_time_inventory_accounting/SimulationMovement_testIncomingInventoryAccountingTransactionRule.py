79ba26edf05018f44768b2bf96d0d7ef6b10aa37
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2018-01-24 18:59:43 +0900

    Add erp5_real_time_inventory_accounting and its test bt5s.
    This implements Outgoing/Incoming Accounting Rules and Builders.

---
+++ bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/SimulationMovement_testIncomingInventoryAccountingTransactionRule.py
@@ -0,0 +1,13 @@
+parent = context.getParentValue()
+if parent.getPortalType() != 'Applied Rule':
+  return False
+
+if context.getUse() != 'trade/purchase':
+  return False
+
+parent_rule = parent.getSpecialiseValue()
+if parent_rule.getPortalType() not in ('Delivery Root Simulation Rule',
+                                       'Delivery Simulation Rule'):
+  return False
+
+return True
---
+++ bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/SimulationMovement_testInvoiceTransactionSimulationRule.py
@@ -0,0 +1,21 @@
+movement = context
+
+parent = movement.getParentValue()
+if parent.getPortalType() != 'Applied Rule':
+  return False
+
+parent_rule = parent.getSpecialiseValue()
+if parent_rule.getPortalType() not in ('Inventory Asset Price Accounting Transaction Rule',
+                                       'Invoice Root Simulation Rule',
+                                       'Invoice Simulation Rule',
+                                       'Trade Model Simulation Rule',
+                                       'Pay Sheet Root Simulation Rule'):
+  return False
+
+delivery_movement = movement.getDeliveryValue()
+if delivery_movement is not None and (
+   delivery_movement.getPortalType() not in movement.getPortalInvoiceMovementTypeList()
+    and delivery_movement.getPortalType() not in movement.getPortalTaxMovementTypeList()):
+  return False
+
+return True
