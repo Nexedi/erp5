0ca9f53e07edddb730e4c61789c1e7353853c817
Vincent Pelletier <vincent@nexedi.com>
2018-01-26 18:51:59 +0900

    all: Get rid of Folder_reindex{All,Objectlist,TreeObjectList}
    These are now superseded by recursiveReindexObject.

--- bt5/erp5_demo_smb/SkinTemplateItem/portal_skins/erp5_demo_smb/Alarm_installBusinessTemplateList.py
+++ bt5/erp5_demo_smb/SkinTemplateItem/portal_skins/erp5_demo_smb/Alarm_installBusinessTemplateList.py
@@ -5,7 +5,7 @@ portal.portal_templates.updateRepositoryBusinessTemplateList(
                               ['http://www.erp5.org/dists/snapshot/bt5/'])
 business_template_list = context.Base_getDemoSMBBusinessTemplateList()
 
-kw = dict(tag="start", after_method_id = ["immediateReindexObject", "Folder_reindexAll"])
+kw = {'tag': "start", 'after_method_id': ["immediateReindexObject"]}
 for business_template_id in business_template_list:
   portal.portal_templates.activate(**kw).installBusinessTemplatesFromRepositories((business_template_id,))
   kw["after_tag"] = kw["tag"]
