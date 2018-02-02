4985f1aef6414b4f8f52989b4aa2db97a46992b1
Vincent Pelletier <vincent@nexedi.com>
2018-02-02 15:07:29 +0900

    ERP5.mixin.document: Remove unused import.

--- product/ERP5/mixin/document.py
+++ product/ERP5/mixin/document.py
@@ -26,7 +26,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-from Products.CMFCore.utils import getToolByName
 from AccessControl import ClassSecurityInfo, Unauthorized
 from Products.ERP5Type.Globals import InitializeClass
 from Products.ERP5Type import Permissions
