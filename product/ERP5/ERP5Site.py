ec610aeea6dd900e5084f1100cfbe8fc546642b0
Vincent Pelletier <vincent@nexedi.com>
2018-02-07 14:27:56 +0900

    ERP5Site: Do reindex the whole site after creation.
    Also, drop isIndexable instance property, unmasking class property, which
    already has the value we need.
    Also, factorise catalog clearing by reusing ERP5Site_reindexAll's.

--- product/ERP5/ERP5Site.py
+++ product/ERP5/ERP5Site.py
@@ -2197,13 +2197,14 @@ class ERP5Generator(PortalGenerator):
   def setupIndex(self, p, **kw):
     # Make sure all tools and folders have been indexed
     if kw.get('reindex', 1):
-      setattr(p, 'isIndexable', ConstantGetter('isIndexable', value=True))
+      delattr(p, 'isIndexable')
       # Clear portal ids sql table, like this we do not take
       # ids for a previously created web site
       p.portal_ids.clearGenerator(all=True)
-      # Then clear the catalog and reindex it
-      p.portal_catalog.manage_catalogClear()
-      # Calling ERP5Site_reindexAll is useless.
+      # Calling ERP5Site_reindexAll is important, as some needed indexation
+      # activities may have been skipped (not spawned) while portal was tagged
+      # as non-indexable.
+      p.ERP5Site_reindexAll(clear_catalog=True)
 
   def setupUserFolder(self, p):
       # Use Pluggable Auth Service instead of the standard acl_users.
