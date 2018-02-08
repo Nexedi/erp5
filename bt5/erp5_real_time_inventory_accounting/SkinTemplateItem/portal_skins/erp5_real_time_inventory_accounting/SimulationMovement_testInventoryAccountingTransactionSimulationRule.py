78815680839264fbe051e122057f2d8d9eb0785e
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2018-02-08 16:41:28 +0900

    erp5_real_time_inventory_accounting: Merge Rules into a single one.
    This is enough for now to handle both Outgoing/Incoming/Transit use cases.

--- bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/SimulationMovement_testInventoryAccountingTransactionSimulationRule.py
+++ bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/SimulationMovement_testInventoryAccountingTransactionSimulationRule.py
@@ -2,9 +2,6 @@ parent = context.getParentValue()
 if parent.getPortalType() != 'Applied Rule':
   return False
 
-if context.getUse() != 'trade/sale':
-  return False
-
 parent_rule = parent.getSpecialiseValue()
 if parent_rule.getPortalType() not in ('Delivery Root Simulation Rule',
                                        'Delivery Simulation Rule'):
