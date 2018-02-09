ba9a141847e46ecddb5cfd726b18d4c8ab3c5643
Vincent Pelletier <vincent@nexedi.com>
2018-02-09 11:43:50 +0900

    Revert "erp5_officejs_support_request, erp5_post: Pass follow-up by value"
    This reverts commit c49cf8d83501bcd22253d7e8a69f5bc0e93c51ef.
    Callee is directly accessed from other places, so changing this API is
    wrong.
    This it not immediately visible in tests, but this makes
    erp5_officejs_support_request_ui_test:testFunctionalSupportRequest
    testCheckComment test randomly fail (apparently because the javascript
    error message may appear after or before the test final step is
    satisfied - which means this tests is bugged, but that's a separate
    issue).

--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
@@ -1,5 +1,5 @@
 return context.PostModule_createHTMLPost(
-  follow_up_value=follow_up_value,
+  follow_up=follow_up,
   predecessor=predecessor,
   data="<p>" + data.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("  ", " &nbsp;").replace("\n", "<br/>") + "</p>",
 )
