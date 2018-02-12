6daac17e097bb4eae149599e75114e7620808edd
Romain Courteaud <romain@nexedi.com>
2018-02-12 10:07:21 +0000

    [erp5_web_renderjs_ui] Module list: manually filter/sort the modules

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_frontpage_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_frontpage_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS */
-/*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS) {
+/*global window, rJS, jIO, RSVP */
+/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
+(function (window, rJS, jIO, RSVP) {
   "use strict";
 
   rJS(window)
@@ -10,6 +10,7 @@
     .declareAcquiredMethod("updateHeader", "updateHeader")
     .declareAcquiredMethod("getUrlParameter", "getUrlParameter")
     .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -19,19 +20,67 @@
     })
     .allowPublicAcquisition('getUrlFor', function (argument_list) {
       if (argument_list[0].command === 'index') {
-        return this.getUrlFor({command: 'display_stored_state', options: {jio_key: argument_list[0].options.jio_key}});
+        return this.getUrlFor({
+          command: 'display_stored_state',
+          options: {jio_key: argument_list[0].options.jio_key}
+        });
       }
       return this.getUrlFor.apply(this, argument_list);
     })
-    .allowPublicAcquisition('getUrlParameter', function (argument_list) {
-      return this.getUrlParameter(argument_list)
-        .push(function (result) {
-          if ((result === undefined) && (argument_list[0] === 'field_listbox_sort_list:json')) {
-            return [['title', 'ascending']];
+
+    .allowPublicAcquisition('jio_allDocs', function (argument_list) {
+      // ERP5 does not support filtering/sorting on translated properties
+      // Fetch the list of all modules from ERP5
+      // and filter/sort it manually after
+      var allDocs_options = argument_list[0],
+        select_list = ['translated_title',
+                       'business_application_translated_title', 'uid', 'id',
+                       'title', '__id'],
+        new_allDocs_options = {
+          limit: allDocs_options.limit,
+          query:
+            '(parent_uid:"0" AND meta_type:"ERP5 Folder" AND id:"%_module")',
+          select_list: select_list,
+          sort_on: []
+        },
+        context = this;
+      return new RSVP.Queue()
+        .push(function () {
+          return RSVP.all([
+            context.jio_allDocs(new_allDocs_options),
+            context.getUrlParameter('extended_search')
+          ]);
+        })
+        .push(function (result_list) {
+          var data_rows = [],
+            i,
+            len = result_list[0].data.total_rows,
+            query = result_list[1] || "";
+          for (i = 0; i < len; i += 1) {
+            // queries do not accept null value
+            result_list[0].data.rows[i].value
+                                       .business_application_translated_title =
+              result_list[0].data.rows[i].value
+                            .business_application_translated_title || '';
+            result_list[0].data.rows[i].value.id =
+              result_list[0].data.rows[i].id;
+            data_rows.push(result_list[0].data.rows[i].value);
           }
-          return result;
+          return jIO.QueryFactory.create(query)
+            .exec(data_rows, {query: query, select_list: select_list,
+                              sort_on: allDocs_options.sort_on});
+        })
+        .push(function (document_list) {
+          var len = document_list.length,
+            i,
+            result = [];
+          for (i = 0; i < len; i += 1) {
+            result.push({id: document_list[i].id, value: document_list[i]});
+          }
+          return {data: {rows: result, total_rows: len}};
         });
     })
+
     .declareMethod("triggerSubmit", function () {
       var argument_list = arguments;
       return this.getDeclaredGadget('form_list')
@@ -53,7 +102,9 @@
         })
         .push(function (form_gadget) {
           var column_list = [
-            ['translated_title', 'Title']];
+            ['translated_title', 'Title'],
+            ['business_application_translated_title', 'Domain']
+          ];
           return form_gadget.render({
             erp5_document: {"_embedded": {"_view": {
               "listbox": {
@@ -65,10 +116,13 @@
                 "key": "field_listbox",
                 "lines": 1000,
                 "list_method": "portal_catalog",
-                "query": "urn:jio:allDocs?query=parent_uid%3A%220%22%20AND%20meta_type%3A%22ERP5%20Folder%22%20AND%20id%3A%22%25_module%22",
+                "query": "urn:jio:allDocs?query=parent_uid%3A%220%22%20AND" +
+                         "%20meta_type%3A%22ERP5%20Folder%22%20AND" +
+                         "%20id%3A%22%25_module%22",
                 "portal_type": [],
                 "search_column_list": column_list,
                 "sort_column_list": column_list,
+                "sort": [["translated_title", "ASC"]],
                 "title": "Modules",
                 "type": "ListBox"
               }
@@ -88,4 +142,4 @@
           });
         });
     });
-}(window, rJS));
+}(window, rJS, jIO, RSVP));
