19eb7e1ce98a88cce5fdddc763cdda63c5faf2c7
Rafael Monnerat <rafael@nexedi.com>
2018-02-20 13:11:16 +0000

    erp5_web_renderjs_ui: Remove hashes from google/facebook when logout.

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSite_logout.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSite_logout.py
@@ -6,6 +6,8 @@ REQUEST = context.REQUEST
 if REQUEST.has_key('portal_skin'):
   context.portal_skins.clearSkinCookie()
 REQUEST.RESPONSE.expireCookie('__ac', path='/')
+REQUEST.RESPONSE.expireCookie('__ac_google_hash', path='/')
+REQUEST.RESPONSE.expireCookie('__ac_facebook_hash', path='/')
 REQUEST.RESPONSE.setHeader('Location', came_from or context.getPermanentURL(context))
 REQUEST.RESPONSE.setStatus(303)
 # REQUEST.RESPONSE.redirect(came_from or context.getPermanentURL(context));
