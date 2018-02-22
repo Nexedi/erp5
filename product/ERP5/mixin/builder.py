9f329a70b89805c6b864ce455df646b98bd5a825
Julien Muchembled <jm@nexedi.com>
2018-02-22 22:26:12 +0100

    Fix workflow history explosion with possibly useless reindex when building in existing delivery
    Reindexing a delivery is always recursive so in scenario where lines are added
    by many different builders and the values in property_dict are always the same,
    the useless reindexing was slower and slower. In the worst case, each builder
    adding 1 line, the number of reindexing activities increased quadratically.
    
    See also commit c020b93b10d517eee552d6eb537464a56659dc90.

--- product/ERP5/mixin/builder.py
+++ product/ERP5/mixin/builder.py
@@ -484,7 +484,7 @@ class BuilderMixin(XMLObject, Amount, Predicate):
       self._setUpdated(delivery, 'delivery')
       if property_dict:
         property_dict.setdefault('edit_order', ('stop_date', 'start_date'))
-        delivery.edit(**property_dict)
+        delivery._edit(reindex_object=1, **property_dict)
 
       # Then, create delivery line
       for grouped_node in movement_group_node.getGroupList():
