66405222b97d8f787d9905c89d144001e3a1fde6
Tomas Peterka <tomas.peterka@nexedi.com>
2018-02-23 14:48:01 +0100

    [hal_json_style] Set correct HTTP STATUS for invalid form while rendering

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_renderForm.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_renderForm.py
@@ -1,3 +1,10 @@
-# request.RESPONSE.setStatus(200)
 form = getattr(context, form_id)
+
+request = REQUEST or context.REQUEST
+
+# HAL JSON uses HTTP codes to communicate what the payload represents
+if request.get('field_errors', None):
+  # HTTP400 means invalid form
+  request.RESPONSE.setStatus(400)
+
 return context.ERP5Document_getHateoas(form=form, mode='form')
