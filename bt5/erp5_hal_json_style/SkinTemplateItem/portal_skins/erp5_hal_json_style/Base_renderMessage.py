7cf484713a7e38bd9a6199a5851dd55e66463922
Tomas Peterka <tomas.peterka@nexedi.com>
2018-02-27 16:54:32 +0100

    [hal_json] Replace Base_returnFailureWithMessage with more generic Base_renderMessage

---
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_renderMessage.py
@@ -0,0 +1,30 @@
+"""
+Return JSON with message to be displayed and set according HTTP STATUS for message severity.
+
+:param message: {str}
+:param level: {str | int} use ERP5Type.Log levels or simply strings like "info", "warning", or "error"
+"""
+import json
+from Products.ERP5Type.Log import WARNING, ERROR
+
+if isinstance(level, (str, unicode)):
+  if level.lower() == "error":
+    response_code = 500
+  elif level.lower().startswith("warn"):
+    response_code = 403
+  else:
+    response_code = 200
+else:
+  if level == ERROR:
+    response_code = 500
+  elif level == WARNING:
+    response_code = 403
+  else:
+    response_code = 200
+
+
+response = request.RESPONSE if request is not None else context.REQUEST.RESPONSE
+# Set the response code and header info in the response
+response.setStatus(response_code)
+response.setHeader("Content-type", "application/json; charset=utf-8")
+return json.dumps({"portal_status_message": str(message)})
