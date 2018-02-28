2f1f295370a1f62a42119d783eb28e2e641bb26d
Roque Porchetto <roqueporchetto@gmail.com>
2018-02-28 09:18:49 +0000

    scalability: check site availability script

---
+++ bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_isReady.py
@@ -0,0 +1,4 @@
+number_of_activities = len(context.portal_activities.getMessageList())
+if number_of_activities == 1: # ignore alarms activities
+  number_of_activities = 0
+return str(number_of_activities)
