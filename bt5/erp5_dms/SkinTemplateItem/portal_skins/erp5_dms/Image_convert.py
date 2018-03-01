6da6766618c943b8283811f175bd6ffb46fd2280
Tomas Peterka <tomas.peterka@nexedi.com>
2018-03-01 15:23:59 +0100

    [erp5_dms] Improve export/import code for Image so it works with JIO

--- bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/Image_convert.py
+++ bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/Image_convert.py
@@ -1,18 +1,15 @@
 """
-Processes requests for conversion of and Image to a desired format.
+Convert Image (context) to a desired format and return as a binary `filename` file.
 Requires format and desired filename (adds extension if missing).
 Sets headers and uses native Zope method to produce output.
 """
 
 request = context.REQUEST
-fname = request.get('filename')
-format = request.get('format')
 
-if fname is None or format is None:
-  return
-if not fname.endswith('.' + format):
-  fname += '.' + format
+if not filename.endswith('.' + format):
+  filename += '.' + format
 
-request.RESPONSE.setHeader('Content-type', 'application/' + format)
-request.RESPONSE.setHeader('Content-disposition', 'attachment; filename="%s"' % fname)
-return context.index_html(request, request.RESPONSE, **kw)
+request.RESPONSE.setHeader('Content-Type', 'application/' + format)
+request.RESPONSE.setHeader('Content-Disposition', 'attachment; filename="%s"' % filename)
+
+return context.index_html(request, request.RESPONSE)  # quality, resolution, frame, image_size, crop will remain default values
