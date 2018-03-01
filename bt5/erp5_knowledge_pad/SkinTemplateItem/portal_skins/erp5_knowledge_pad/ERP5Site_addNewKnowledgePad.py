5195675ad0d0aec2f82c2269cc36cd2413d4fc3a
Vincent Pelletier <vincent@nexedi.com>
2018-03-01 16:12:46 +0900

    all: Avoid most direct calls to {recursiveI,i}mmediateReindexObject
    These methods must not be called synchronously:
    - they can break catalog by not being careful enough about other
      reindexations which may happen in parallel. See the serialization_tag
      mechanism for more.
    - indexation gets executed in the security context of the user causing the
      call, which may lead to an indexation result different from what happens
      when indexation happens with an all-accesses user.
    
    Also, simplify a few scripts while doing so.

--- bt5/erp5_knowledge_pad/SkinTemplateItem/portal_skins/erp5_knowledge_pad/ERP5Site_addNewKnowledgePad.py
+++ bt5/erp5_knowledge_pad/SkinTemplateItem/portal_skins/erp5_knowledge_pad/ERP5Site_addNewKnowledgePad.py
@@ -1,16 +1,16 @@
-pad = context.knowledge_pad_module.newContent(portal_type='Knowledge Pad',
-                                              title = pad_title)
-# for web mode
-if mode in ('web_front', 'web_section',):
-  # in Web Mode we can have a temporary Web Site objects created based on current language
-  real_context = context.Base_getRealContext()
-  pad.setPublicationSectionValue(real_context)
+from Products.ERP5Type.ImmediateReindexContextManager import ImmediateReindexContextManager
+with ImmediateReindexContextManager() as immediate_reindex_context_manager:
+  pad = context.knowledge_pad_module.newContent(portal_type='Knowledge Pad',
+                                                immediate_reindex=immediate_reindex_context_manager,
+                                                title = pad_title)
+  # for web mode
+  if mode in ('web_front', 'web_section',):
+    # in Web Mode we can have a temporary Web Site objects created based on current language
+    real_context = context.Base_getRealContext()
+    pad.setPublicationSectionValue(real_context)
 
-# set it as active
-context.ERP5Site_toggleActiveKnowledgePad(pad, mode=mode, redirect=False)
-
-# See ERP5Site_createDefaultKnowledgePadListForUser
-pad.immediateReindexObject()
+  # set it as active
+  context.ERP5Site_toggleActiveKnowledgePad(pad, mode=mode, redirect=False)
 
 if redirect_url:
   return context.REQUEST.RESPONSE.redirect(redirect_url)
--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPostFromText.py
@@ -2,4 +2,5 @@ return context.PostModule_createHTMLPost(
   follow_up=follow_up,
   predecessor=predecessor,
   data="<p>" + data.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("  ", " &nbsp;").replace("\n", "<br/>") + "</p>",
+  immediate_reindex_context_manager=immediate_reindex_context_manager,
 )
