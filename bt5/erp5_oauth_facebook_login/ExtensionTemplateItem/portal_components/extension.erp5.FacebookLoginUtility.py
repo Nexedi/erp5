849a2d1ecbb8cde550636fbda46ed13f3154dd81
Rafael Monnerat <rafael@nexedi.com>
2018-03-01 19:26:40 +0000

    erp5_oauth_facebook_login: Use unrestrictedSearchResults to avoid security_uid (Speed up)
    When a script with manager proxy role is called from anonymous context, it include a HUGE list of security_uids, use unrestrictedSearchResults skips the unecessary usage of security_uids on catalog.

--- bt5/erp5_oauth_facebook_login/ExtensionTemplateItem/portal_components/extension.erp5.FacebookLoginUtility.py
+++ bt5/erp5_oauth_facebook_login/ExtensionTemplateItem/portal_components/extension.erp5.FacebookLoginUtility.py
@@ -2,6 +2,7 @@ import facebook
 from ZTUtils import make_query
 
 from Products.ERP5Security.ERP5ExternalOauth2ExtractionPlugin import getFacebookUserEntry
+from zExceptions import Unauthorized
 
 def _getFacebookClientIdAndSecretKey(portal, reference="default"):
   """Returns facebook client id and secret key.
@@ -36,6 +37,22 @@ def getAccessTokenFromCode(self, code, redirect_uri):
   return facebook.GraphAPI(version="2.7").get_access_token_from_code(
     code=code, redirect_uri=redirect_uri,
     app_id=client_id, app_secret=secret_key)
+
+def unrestrictedSearchFacebookConnector(self):
+  return self.getPortalObject().portal_catalog.unrestrictedSearchResults(
+            portal_type="Facebook Connector",
+            reference="default",
+            validation_state="validated",
+            limit=2)
+
+def unrestrictedSearchFacebookLogin(self, login, REQUEST=None):
+  if REQUEST is not None:
+    raise Unauthorized
+
+  return self.getPortalObject().portal_catalog.unrestrictedSearchResults(
+    portal_type="Facebook Login",
+    reference=login,
+    validation_state="validated", limit=1)
 
 def getUserEntry(token):
   return getFacebookUserEntry(token)
