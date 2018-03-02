0b766e90af0b510526220b9b59af781a96921785
Vincent Bechu <vincent.bechu@nexedi.com>
2018-03-02 16:12:29 +0000

    [erp5_web_renderjs_ui] Update jio with cloudoo storage

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
@@ -7540,6 +7540,7 @@ return new Parser;
    * @param  {String} [param.dataType=""] The data type to retrieve
    * @param  {String} param.url The url
    * @param  {Any} [param.data] The data to send
+   * @param  {Number} param.timeout The request timeout value
    * @param  {Function} [param.beforeSend] A function called just before the
    *    send request. The first parameter of this function is the XHR object.
    * @return {Promise} The promise
@@ -7571,6 +7572,12 @@ return new Parser;
             xhr[k] = param.xhrFields[k];
           }
         }
+      }
+      if (param.timeout !== undefined && param.timeout !== 0) {
+        xhr.timeout = param.timeout;
+        xhr.ontimeout = function () {
+          return reject(new jIO.util.jIOError("Gateway Timeout", 504));
+        };
       }
       if (typeof param.beforeSend === 'function') {
         param.beforeSend(xhr);
@@ -7631,6 +7638,9 @@ return new Parser;
       result_list;
     if (obj === undefined) {
       return undefined;
+    }
+    if (obj === null) {
+      return 'null';
     }
     if (obj.constructor === Object) {
       key_list = Object.keys(obj).sort();
@@ -10528,6 +10538,8 @@ return new Parser;
     } else {
       this._catch_error = false;
     }
+    // If timeout not set, use 0 for no timeout value
+    this._timeout = spec.timeout || 0;
   }
 
   HttpStorage.prototype.get = function (id) {
@@ -10536,7 +10548,8 @@ return new Parser;
       .push(function () {
         return jIO.util.ajax({
           type: 'HEAD',
-          url: id
+          url: id,
+          timeout: context._timeout
         });
       })
       .push(undefined, function (error) {
@@ -10586,7 +10599,8 @@ return new Parser;
         return jIO.util.ajax({
           type: 'GET',
           url: id,
-          dataType: "blob"
+          dataType: "blob",
+          timeout: context._timeout
         });
       })
       .push(undefined, function (error) {
@@ -14319,4 +14333,332 @@ return new Parser;
 
   jIO.addStorage('facebook', FBStorage);
 
-}(jIO, RSVP, UriTemplate));
+}(jIO, RSVP, UriTemplate));;/*jslint nomen: true*/
+/*global jIO, RSVP, Blob, Uint8Array, DOMParser, XMLSerializer*/
+(function (jIO, Uint8Array, Blob, RSVP, DOMParser, XMLSerializer) {
+  "use strict";
+
+  var content_type_dict = {
+    "application/x-asc-text": "docy",
+    "application/x-asc-presentation": "",
+    "application/x-asc-spreadsheet": ""
+  },
+    parser = new DOMParser(),
+    serializer = new XMLSerializer();
+
+  function newXml() {
+    return parser.parseFromString(
+      '<?xml version="1.0" encoding="UTF-8"?><methodCall>' +
+        '<methodName>convertFile</methodName><params>' +
+        '<param><value><string></string></value></param>' +
+        '<param><value><string></string></value></param>' +
+        '<param><value><string></string></value></param></params></methodCall>',
+      'text/xml'
+    );
+  }
+
+  function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = window.atob(b64Data),
+      byteArrays = [],
+      slice,
+      byteArray,
+      byteNumbers = [],
+      offset,
+      i;
+
+    for (offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      slice = byteCharacters.slice(offset, offset + sliceSize);
+      for (i = 0; i < slice.length; i += 1) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, {type: contentType});
+  }
+
+  function getInfoDocId(id, attachment_id) {
+    return 'cloudoo/' + id + '/' + attachment_id;
+  }
+
+  function convert(storage, blob, from, to) {
+    return new RSVP.Queue()
+      .push(function () {
+        return jIO.util.readBlobAsDataURL(blob);
+      })
+      .push(function (result) {
+        // WIP: use something cleaner
+        var file = result.target.result.split('base64,')[1],
+          xml = newXml(),
+          string_list = xml.getElementsByTagName('string');
+        string_list[0].textContent = file;
+        string_list[1].textContent = from;
+        string_list[2].textContent = to;
+        return jIO.util.ajax({
+          type: 'POST',
+          url: storage._url,
+          data: serializer.serializeToString(xml)
+        });
+      })
+      .push(function (result) {
+        var data_list = parser.parseFromString(
+          result.target.responseText,
+          "application/xml"
+        ).getElementsByTagName('string');
+        if (data_list.length) {
+          return b64toBlob(data_list[0].textContent, to);
+        }
+        throw new jIO.util.jIOError('conversion failed', 404);
+      });
+  }
+
+  function getOrCreateInfoDoc(storage, id, attachment_id) {
+    return storage._sub_storage.get(getInfoDocId(id, attachment_id))
+      .push(undefined, function (error) {
+        if (error instanceof jIO.util.jIOError && error.status_code === 404) {
+          return storage.get(id)
+            .push(function (doc) {
+              var format = doc.content_type || undefined;
+              if (content_type_dict.hasOwnProperty(format)) {
+                format = content_type_dict[format];
+              }
+              return {
+                portal_type: "Conversion Info",
+                convert_dict: {},
+                format: format,
+                attachment_id: attachment_id,
+                doc_id: id
+              };
+            });
+        }
+        throw error;
+      });
+  }
+
+  function convertFromBaseFormat(storage, id, attachment_id, format) {
+    var info_doc;
+    return getOrCreateInfoDoc(storage, id, attachment_id)
+      .push(function (doc) {
+        info_doc = doc;
+        return storage._sub_storage.getAttachment(id, attachment_id)
+          .push(function (blob) {
+            return convert(storage, blob, info_doc.format, format);
+          })
+          .push(function (blob) {
+            return storage._sub_storage.putAttachment(
+              id,
+              attachment_id + '?' + format,
+              blob
+            )
+              .push(function () {
+                info_doc.convert_dict[format] = true;
+                return storage._sub_storage.put(
+                  getInfoDocId(id, attachment_id),
+                  info_doc
+                );
+              })
+              .push(function () {
+                return blob;
+              });
+          }, undefined, function (error) {
+            info_doc.convert_dict[format] = false;
+            return storage._sub_storage.put(
+              getInfoDocId(id, attachment_id),
+              info_doc
+            )
+              .push(function () {
+                throw error;
+              });
+          });
+      });
+  }
+
+  function convertToBaseFormat(storage, id, attachment_id, format, blob) {
+    var info_doc;
+    return getOrCreateInfoDoc(storage, id, attachment_id)
+      .push(function (doc) {
+        info_doc = doc;
+        return convert(storage, blob, format, info_doc.format);
+      })
+      .push(function (blob) {
+        return storage._sub_storage.putAttachment(id, attachment_id, blob);
+      })
+      .push(function () {
+        info_doc.convert_dict[format] = true;
+        return storage._sub_storage.put(
+          getInfoDocId(id, attachment_id),
+          info_doc
+        );
+      });
+  }
+
+  function removeConvertedAttachment(storage, id, attachment_id) {
+    var doc_info;
+    return getOrCreateInfoDoc(storage, id, attachment_id)
+      .push(function (doc) {
+        var format, promise_list = [];
+        doc_info = doc;
+        for (format in doc.convert_list) {
+          if (doc.convert_list.hasOwnProperty(format)) {
+            if (doc.convert_list[format]) {
+              promise_list.push(
+                storage._sub_storage.removeAttachment(
+                  id,
+                  attachment_id + '?' + format
+                )
+              );
+            }
+          }
+        }
+        return RSVP.all(promise_list);
+      })
+      .push(function () {
+        doc_info.convert_dict = {};
+        return doc_info;
+      });
+  }
+
+  /**
+   * The jIO CloudooStorage extension
+   *
+   * Convert attachment : att_id?format
+   * cloudoo_info : 
+   * {
+   * portal_type: "Conversion Info"
+   * base_format: format de base ( content_type du document )
+   * to_convert_list: [liste des format a convertir pendant la synchro]
+   * format_avaible_list : [liste des formats deja disponible]
+   * }
+   * 
+   * @class CloudooStorage
+   * @constructor
+   */
+  function CloudooStorage(spec) {
+    this._url = spec.url;
+    this._sub_storage = jIO.createJIO(spec.sub_storage);
+  }
+
+  CloudooStorage.prototype.get = function () {
+    return this._sub_storage.get.apply(this._sub_storage, arguments);
+  };
+  CloudooStorage.prototype.post = function () {
+    return this._sub_storage.post.apply(this._sub_storage, arguments);
+  };
+  CloudooStorage.prototype.put = function () {
+    return this._sub_storage.put.apply(this._sub_storage, arguments);
+  };
+  CloudooStorage.prototype.remove = function () {
+    return this._sub_storage.remove.apply(this._sub_storage, arguments);
+  };
+  CloudooStorage.prototype.getAttachment = function (id, attachment_id) {
+    var storage = this;
+    return this._sub_storage.getAttachment.apply(this._sub_storage, arguments)
+      .push(undefined, function (error) {
+        var att_id_list = attachment_id.split('?');
+        if (error instanceof jIO.util.jIOError &&
+            error.status_code === 404 &&
+            att_id_list.length > 1) {
+          return convertFromBaseFormat(
+            storage,
+            id,
+            att_id_list[0],
+            att_id_list[1]
+          );
+        }
+        throw error;
+      });
+  };
+  CloudooStorage.prototype.putAttachment = function (id, attachment_id, blob) {
+    var storage = this;
+    return this._sub_storage.putAttachment.apply(this._sub_storage, arguments)
+      .push(function (result) {
+        var att_id_list = attachment_id.split('?');
+        if (att_id_list.length === 1) {
+          return removeConvertedAttachment(storage, id, attachment_id)
+            .push(function (doc_info) {
+              return storage._sub_storage.put(
+                getInfoDocId(id, attachment_id),
+                doc_info
+              );
+            })
+            .push(function () {
+              return result;
+            });
+        }
+        if (att_id_list.length === 2) {
+          return convertToBaseFormat(
+            storage,
+            id,
+            att_id_list[0],
+            att_id_list[1],
+            blob
+          );
+        }
+      });
+  };
+  CloudooStorage.prototype.removeAttachment = function (id, attachment_id) {
+    var storage = this;
+    return this._sub_storage.removeAttachment.apply(
+      this._sub_storage,
+      arguments
+    )
+      .push(function () {
+        var att_id_list = attachment_id.split('?');
+        if (att_id_list.length === 1) {
+          return removeConvertedAttachment(storage, id, attachment_id)
+            .push(function () {
+              return storage._sub_storage.remove(
+                getInfoDocId(id, attachment_id)
+              );
+            });
+        }
+      });
+  };
+  CloudooStorage.prototype.allAttachments = function () {
+    return this._sub_storage.allAttachments.apply(this._sub_storage, arguments);
+  };
+  CloudooStorage.prototype.repair = function () {
+    var storage = this;
+    return this._sub_storage.repair.apply(this._sub_storage, arguments)
+      .push(function () {
+        return storage._sub_storage.allDocs({
+          query: 'portal_type: "Conversion Info"',
+          select_list: ['convert_dict', 'doc_id', 'attachment_id']
+        });
+      })
+      .push(function (result) {
+        var i, promise_list = [], format, value;
+        for (i = 0; i < result.data.total_rows; i += 1) {
+          value = result.data.rows[i].value;
+          for (format in value.convert_dict) {
+            if (value.convert_dict.hasOwnProperty(format) &&
+                value.convert_dict[format] === false) {
+              promise_list.push(convertFromBaseFormat(
+                storage,
+                value.doc_id,
+                value.attachment_id,
+                format
+              ));
+            }
+          }
+        }
+        return RSVP.all(promise_list);
+      });
+  };
+  CloudooStorage.prototype.hasCapacity = function (name) {
+    return this._sub_storage.hasCapacity(name);
+  };
+  CloudooStorage.prototype.buildQuery = function () {
+    return this._sub_storage.buildQuery.apply(this._sub_storage,
+                                              arguments);
+  };
+
+  jIO.addStorage('cloudoo', CloudooStorage);
+
+}(jIO, Uint8Array, Blob, RSVP, DOMParser, XMLSerializer));
