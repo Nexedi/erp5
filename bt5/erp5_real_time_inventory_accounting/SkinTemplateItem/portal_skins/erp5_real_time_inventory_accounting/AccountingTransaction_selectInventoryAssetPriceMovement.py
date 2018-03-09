6a7d73f933bc0507ee9911b2f59459c74a3ccce2
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2018-03-09 16:40:59 +0900

    erp5_real_time_inventory_accounting: Split the BP into dedicated ones for Sale, Purchase and Internal.
    Checking 'delivery' portal_type for PL type will not work on real
    implementations as the PL is not built during the first expand and
    expand_related=0 after and this is how it should be done anyway.

--- bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/AccountingTransaction_selectInventoryAssetPriceMovement.py
+++ bt5/erp5_real_time_inventory_accounting/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting/AccountingTransaction_selectInventoryAssetPriceMovement.py
@@ -1,12 +1,9 @@
-kw['parent_specialise_reference'] = ['default_inventory_accounting_transaction_rule']
-kw['grand_grand_parent_specialise_reference'] = ['default_delivering_rule', 'default_delivery_rule']
-kw['explanation_portal_type'] = ['Sale Packing List', 'Purchase Packing List', 'Internal Packing List', 'Production Packing List']
 kw['portal_type'] = 'Simulation Movement'
 kw['delivery_uid'] = None
 kw['left_join_list'] = ['delivery_uid']
 kw['select_dict'] = dict(delivery_uid=None)
 kw['group_by'] = ('uid',)
-kw['simulation_state'] = ['planned', 'auto_planned']
+kw['grand_parent_simulation_state'] = 'started', 'stopped', 'delivered'
 
 result = context.portal_catalog(**kw)
 context.log("%r => %r" % (kw, [brain.getObject() for brain in result]))
---
+++ bt5/erp5_real_time_inventory_accounting_test/SkinTemplateItem/portal_skins/erp5_real_time_inventory_accounting_test/TradeModelPath_getInventoryAccountingSaleTransitLedgerOutCreditSource.py
@@ -0,0 +1,4 @@
+if movement.getSource() == 'organisation_module/hoge':
+  return ['source/account_module/stock_car_transit']
+
+return []
