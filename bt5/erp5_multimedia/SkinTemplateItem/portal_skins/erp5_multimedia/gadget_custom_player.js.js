cfeab7e8fa8fb80b8c150dda2ae59441b2d2363c
Mukul <mukul.kumar@nexedi.com>
2018-03-13 11:13:54 +0100

    [erp5_officejs][erp5_multimedia]: Adds officejs music player app.

---
+++ bt5/erp5_multimedia/SkinTemplateItem/portal_skins/erp5_multimedia/gadget_custom_player.js.js
@@ -0,0 +1,126 @@
+/*global window, rJS, RSVP, jIO, MediaSource,
+  loopEventListener
+*/
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS) {
+  "use strict";
+
+  rJS(window)
+    .setState({ play: false, mute: false })
+
+    //////////////////////////////////////////////
+    // Acquire Method
+    //////////////////////////////////////////////
+    .declareAcquiredMethod("jio_get", "jio_get")
+
+    /////////////////////////////////////////////
+    // Declare Method
+    ////////////////////////////////////////////
+    .allowPublicAcquisition('updateCurrentTime', function (time) {
+      this.element.querySelector('.current_time').textContent = new Date(time * 1000).toISOString().substr(11, 8);
+      this.element.querySelector('progress').value = time;
+    })
+
+    .allowPublicAcquisition('updateTotalTime', function (time) {
+      this.element.querySelector('.total_time').textContent = new Date(time * 1000).toISOString().substr(11, 8);
+      this.element.querySelector('progress').max = time;
+    })
+
+    .allowPublicAcquisition('onEnd', function () {
+      return this.changeState({ play: false, mute: false });
+    })
+
+    .declareMethod('togglePlayPause', function () {
+      var gadget = this,
+        play_button = gadget.element.querySelector('.play-btn');
+
+      if (gadget.state.play) {
+        play_button.classList.add('ui-icon-pause');
+        play_button.classList.remove('ui-icon-play');
+      } else {
+        play_button.classList.add('ui-icon-play');
+        play_button.classList.remove('ui-icon-pause');
+      }
+      return gadget.getDeclaredGadget('controller')
+        .push(function (controller) {
+          return controller.handlePlayPause(gadget.state.play);
+        });
+    })
+
+    .declareMethod('toggleSound', function () {
+      var gadget = this,
+        volume_button = gadget.element.querySelector('.vol-btn');
+
+      if (gadget.state.mute) {
+        volume_button.classList.remove('ui-icon-volume-up');
+        volume_button.classList.add('ui-icon-volume-off');
+      } else {
+        volume_button.classList.remove('ui-icon-volume-off');
+        volume_button.classList.add('ui-icon-volume-up');
+      }
+      return gadget.getDeclaredGadget('controller')
+        .push(function (controller) {
+          return controller.handleSound(gadget.state.mute);
+        });
+    })
+
+    .declareMethod('render', function (params) {
+      var gadget = this;
+      return gadget.jio_get(params.value)
+        .push(function (doc) {
+          var name_array = doc.title.split('.'),
+            type = name_array[name_array.length - 1];
+          if (type === 'mp3' && MediaSource.isTypeSupported('audio/mpeg')) {
+            return gadget.declareGadget('gadget_custom_player_controller.html', {
+              element: gadget.element.querySelector('.controller'),
+              scope: 'controller'
+            });
+          }
+          return gadget.declareGadget('gadget_custom_player_controller_fallback.html', {
+            element: gadget.element.querySelector('.controller'),
+            scope: 'controller'
+          });
+        })
+        .push(function (controller) {
+          return controller.render({
+            id: params.value,
+            name: params.name
+          });
+        });
+    })
+
+    .onStateChange(function (modification_dict) {
+      if (modification_dict.hasOwnProperty('play')) {
+        return this.togglePlayPause(modification_dict.play);
+      }
+      if (modification_dict.hasOwnProperty('mute')) {
+        return this.toggleSound(modification_dict.mute);
+      }
+    })
+
+    .declareService(function () {
+      var gadget = this;
+      return loopEventListener(
+        gadget.element.querySelector('.play-btn'),
+        'click',
+        false,
+        function () {
+          return gadget.changeState({ play: !gadget.state.play });
+        },
+        true
+      );
+    })
+
+    .declareService(function () {
+      var gadget = this;
+      return loopEventListener(
+        gadget.element.querySelector('.vol-btn'),
+        'click',
+        false,
+        function () {
+          return gadget.changeState({ mute: !gadget.state.mute });
+        },
+        true
+      );
+    });
+}(window, rJS));
---
+++ bt5/erp5_multimedia/SkinTemplateItem/portal_skins/erp5_multimedia/gadget_custom_player_controller_fallback.js.js
@@ -0,0 +1,133 @@
+/*global window, rJS, RSVP, URL, Blob,
+  loopEventListener, document*/
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS, RSVP, URL, loopEventListener) {
+  "use strict";
+
+  rJS(window)
+    .setState({ currentTime: 0 })
+    //////////////////////////////////////////////
+    // Acquire methods
+    /////////////////////////////////////////////
+    .declareAcquiredMethod('jio_get', 'jio_get')
+    .declareAcquiredMethod('getSetting', 'getSetting')
+    .declareAcquiredMethod('jio_getAttachment', 'jio_getAttachment')
+    .declareAcquiredMethod('togglePlayPause', 'togglePlayPause')
+    .declareAcquiredMethod('toggleSound', 'toggleSound')
+    .declareAcquiredMethod('updateCurrentTime', 'updateCurrentTime')
+    .declareAcquiredMethod('updateTotalTime', 'updateTotalTime')
+    .declareAcquiredMethod('notifySubmitted', 'notifySubmitted')
+    .declareAcquiredMethod('onEnd', 'onEnd')
+
+    //////////////////////////////////////////////
+    // Declare methods
+    /////////////////////////////////////////////
+    .declareMethod('getAudioChunk', function () {
+      var gadget = this;
+      // Call `getAttachment` method of jIO to fetch a chunk of data from IDB storage.
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.jio_getAttachment(gadget.params.id, gadget.params.name, {
+            start: 0
+          });
+        })
+        .push(undefined, function (error) {
+          if ((error.constructor.name === 'jIOError' && error.status_code === 404) ||
+              (error.target && error.target.result === undefined)) {
+            return gadget.notifySubmitted({message: error.message || error.target.error, status: 'fail'});
+          }
+          throw error;
+        });
+    })
+
+    .declareMethod('handlePlayPause', function (play) {
+      var audio = this.element.querySelector('audio');
+      if (play) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    })
+
+    .declareMethod('handleSound', function (mute) {
+      this.element.querySelector('audio').muted = mute;
+    })
+
+    .declareMethod('render', function (params) {
+      var gadget = this,
+        queue = new RSVP.Queue();
+      gadget.params = params;
+
+      if (gadget.params.id) {
+        if (!gadget.params.name) {
+          queue
+            .push(function () {
+              return gadget.getSetting('hateoas_url');
+            });
+        }
+        queue
+          .push(function (hateoas_url) {
+            if (!gadget.params.name) {
+              gadget.params.name = hateoas_url + gadget.params.id;
+            }
+            return gadget.getAudioChunk()
+              .push(function (blob) {
+                if (!(blob instanceof Blob)) {
+                  blob = new Blob();
+                }
+                gadget.element.querySelector('audio').src = URL.createObjectURL(blob);
+                gadget.element.querySelector('audio').onloadeddata = function () {
+                  gadget.updateTotalTime(gadget.element.querySelector('audio').duration);
+                }.bind(gadget);
+              });
+          });
+      }
+      return queue;
+    })
+
+    .onStateChange(function (modification_dict) {
+      if (modification_dict.hasOwnProperty('currentTime')) {
+        return this.updateCurrentTime(modification_dict.currentTime);
+      }
+    })
+
+    .declareService(function () {
+      var gadget = this,
+        audio = gadget.element.querySelector('audio');
+      return loopEventListener(
+        audio,
+        'timeupdate',
+        false,
+        function () {
+          return gadget.changeState({ currentTime: audio.currentTime });
+        },
+        true
+      );
+    })
+
+    .declareService(function () {
+      var gadget = this,
+        audio = gadget.element.querySelector('audio');
+      return RSVP.Queue()
+        .push(function () {
+          return loopEventListener(
+            audio,
+            'ended',
+            false,
+            function () {
+              audio.currentTime = 0;
+              return gadget.onEnd();
+            },
+            true
+          );
+        })
+        .push(undefined, function (error) {
+          if (error instanceof RSVP.CancellationError) {
+            // Pause when gadget go out of scope { CancellationError }.
+            audio.pause(); 
+          } else {
+            throw error;
+          }
+        });
+    });
+}(window, rJS, RSVP, URL, loopEventListener));
