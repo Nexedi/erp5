73951db63466f69c233ccd442db4a885208e0237
Tomas Peterka <tomas.peterka@nexedi.com>
2018-03-22 10:12:21 +0100

    [erp5_core] Mark generic actions as JIO compatible

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/BusinessTemplate_getPortalTypesProperties.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/BusinessTemplate_getPortalTypesProperties.py
@@ -3,5 +3,6 @@ context.getPortalTypesProperties()
 REQUEST = context.REQUEST
 
 if REQUEST is not None:
-  ret_url = context.absolute_url() + '/' + REQUEST.get('form_id', 'view')
-  REQUEST.RESPONSE.redirect("%s?portal_status_message=Portal+Types+Properties+Updated"% ret_url)
+  return context.Base_redirect(
+    REQUEST.get("form_id", "view"),
+    keep_items={'portal_status_message': 'Portal Types Properties Updated'})
