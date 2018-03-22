fba2f0213c6aa8d6aedc4d4e0ea0ba9e9e18b953
Tomas Peterka <tomas.peterka@nexedi.com>
2018-03-22 10:12:21 +0100

    [erp5_core] Mark generic actions as JIO compatible
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/640

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/BusinessTemplate_fillPortalTypesFields.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/BusinessTemplate_fillPortalTypesFields.py
@@ -3,5 +3,6 @@ context.guessPortalTypes()
 REQUEST = context.REQUEST
 
 if REQUEST is not None:
-  ret_url = context.absolute_url() + '/' + REQUEST.get('form_id', 'view')
-  REQUEST.RESPONSE.redirect("%s?portal_status_message=Portal+Type+Data+Updated"% ret_url)
+  return context.Base_redirect(
+    REQUEST.get("form_id", "view"),
+    keep_items={'portal_status_message': 'Portal Types Data Updated'})
