65d4019bec53050d6dcd02c5ecba364048324941
Jérome Perrin <jerome@nexedi.com>
2018-04-04 06:03:23 +0000

    testFunctionalAnonymousSelection: add missing dependency
    erp5_l10n_fa is needed to test the RTL direction.
    
    same as b6eec549a591851e76440bd93ee394323a8c3220

--- product/ERP5Type/tests/testFunctionalAnonymousSelection.py
+++ product/ERP5Type/tests/testFunctionalAnonymousSelection.py
@@ -43,6 +43,7 @@ class TestAnonymousSelection(TestZeleniumCore):
         """
         return ('erp5_core_proxy_field_legacy', 'erp5_full_text_mroonga_catalog',
                 'erp5_base', 'erp5_ui_test_core', 'erp5_ui_test', 'erp5_forge',
+                'erp5_l10n_fa',
                 )
 
 def test_suite():
