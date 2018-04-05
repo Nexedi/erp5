Nexedi/erp5

commit a5a2f1cdad867a0f4588d8cd35644c556c653aa6

[erp5] FormBoxEditor keeps correct context as its attribute

product/ERP5Form/FormBox.py
@@ -42,6 +42,7 @@ from Products.PythonScripts.standard import url_quote_plus
 from Products.Formulator.Errors import FormValidationError, ValidationError
 
 import string
+import copy
 
 class FormBoxWidget(Widget.Widget):
   """
@@ -123,9 +124,15 @@ class FormBoxEditor:
   """
   A class holding all values required to update the object
   """
-  def __init__(self, field_id, result):
+  def __init__(self, field_id, result, context=None):
+    """Initialize with all necessary information for editing.
+
+    Keep a reference to the correct context and don't expect the caller to provide it
+    during the edit phase because they don't have access to the widget anymore.
+    """
     self.field_id = field_id
-    self.result = result
+    self.attr_dict, self.editor_list = result
+    self.context = context
 
   def view(self):
     return self.__dict__
@@ -134,8 +141,12 @@ class FormBoxEditor:
     pass
 
   def edit(self, context):
-    context.edit(**self.result[0])
-    for encapsulated_editor in self.result[1]:
+    """Edit inside correct context."""
+    if self.context is not None:
+        context = self.context
+
+    context.edit(**self.attr_dict)
+    for encapsulated_editor in self.editor_list:
       encapsulated_editor.edit(context)
 
   def as_dict(self):
@@ -144,8 +155,8 @@ class FormBoxEditor:
     XXX This API is probably not stable and may change, as some editors are used to
     edit multiple objects.
     """
-    result_dict = self.result[0]
-    for encapsulated_editor in self.result[1]:
+    result_dict = copy.copy(self.attr_dict)
+    for encapsulated_editor in self.editor_list:
       if hasattr(encapsulated_editor, 'as_dict'):
         result_dict.update(
             encapsulated_editor.as_dict())
@@ -181,7 +192,7 @@ class FormBoxValidator(Validator.Validator):
     # XXX Hardcode script name
     result, result_type = here.Base_edit(formbox_target_id, silent_mode=1, key_prefix=key)
     if result_type == 'edit':
-      return FormBoxEditor(field.id, result)
+      return FormBoxEditor(field.id, result, context=here)
     elif result_type == 'form':
       formbox_field_errors = REQUEST.get('field_errors', [])
       current_field_errors.extend(formbox_field_errors)

commit ab388d7fdf8e7547f06fe2ac01d8cdbc8a308f48

ERP5Form: when editing a FormBox, do not compute the context twice (and wrongly)
This first reverts the following 2 commits:
  566c0c5f88e2565c6379030a0faecdd20a484b54
  3a08c7580b97d9433129cd34889cb694a711a38f

to at least fix the issue that the context method takes 2 arguments that aren't
available from FormBoxEditor.edit() without slowing things even more.

About deferred style, widget editor objects can be ignored for the moment. If
we ever need to restore fully functional objects, the regression in commit
  a5a2f1cdad867a0f4588d8cd35644c556c653aa6
could be solved with the following patch:

--- a/product/ERP5Form/FormBox.py
+++ b/product/ERP5Form/FormBox.py
@@ -131,22 +131,32 @@ def render(self, field, key, value, REQUEST, render_prefix=None):
 class FormBoxEditor:
   """An editor returned from FormBox validation able to `edit` document."""
 
+  path = None
+
   def __init__(self, result, context):
     """Initialize with all necessary information for editing.
 
     Keep a reference to the correct context and don't expect the caller to provide it
     during the edit phase because they don't have access to the widget anymore.
     """
     self.attr_dict, self.editor_list = result
+    self.context = context
     self.edit = lambda _: self._edit(context)
 
   def __getstate__(self):
-    pass
+    return (self.attr_dict, self.editor_list,
+            self.path or self.context.getPhysicalPath())
+
+  def __setstate__(self, state):
+    self.attr_dict, self.editor_list, self.path = state
 
   def __call__(self, REQUEST):
     # Called by Base_edit in case of FormValidationError
     pass
 
+  def edit(self, context):
+    return self._edit(context.unrestrictedTraverse(self.path))
+
   def _edit(self, context):
     """Edit inside correct context."""
     context.edit(**self.attr_dict)

This commit also removes the unused view() method on editors.

/reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/622

product/ERP5Form/FormBox.py
@@ -131,34 +131,33 @@ class FormBoxWidget(Widget.Widget):
 class FormBoxEditor:
   """An editor returned from FormBox validation able to `edit` document."""
 
-  def __init__(self, result, context_method_id=None):
-    """Initialize with all necessary information for editing a document.
+  def __init__(self, result, context):
+    """Initialize with all necessary information for editing.
 
-    :result: tuple of attributes, editors intended as parameters for edit function
-    :context_method_id: editor needs to operate on the correct context (Document)
-                        but it cannot hold reference because then weird failures
-                        appear; thus we keep name of the context-obtaining method
+    Keep a reference to the correct context and don't expect the caller to provide it
+    during the edit phase because they don't have access to the widget anymore.
     """
     self.attr_dict, self.editor_list = result
-    self.context_method_id = context_method_id
+    self.edit = lambda _: self._edit(context)
 
-  def view(self):
-    return self.__dict__
-
-  def __call__(self, REQUEST):
+  def __getstate__(self):
+    # With deferred style, to render reports of form in activities (and never
+    # to edit), we could be pickled but it's always for nothing for the moment.
     pass
 
-  def edit(self, context):
-    if self.context_method_id:
-      context = getattr(context, self.context_method_id)()
+  def __call__(self, REQUEST):
+    # Called by Base_edit in case of FormValidationError
+    pass
 
+  def _edit(self, context):
+    """Edit inside correct context."""
     context.edit(**self.attr_dict)
     for encapsulated_editor in self.editor_list:
       encapsulated_editor.edit(context)
 
   def as_dict(self):
     """
-    This method is used to return parameter dict.
+    This method is used by Base_callDialogMethod.
     XXX This API is probably not stable and may change, as some editors are used to
     edit multiple objects.
     """
@@ -186,7 +185,6 @@ class FormBoxValidator(Validator.Validator):
   def validate(self, field, key, REQUEST):
     # TODO: Handle 'cell' for validation inside listboxes,
     #       like it is done for rendering.
-    context_method_id = field.get_value('context_method_id')
     formbox_target_id = field.get_value('formbox_target_id')
 
     # Get current error fields
@@ -196,7 +194,7 @@ class FormBoxValidator(Validator.Validator):
       # XXX Hardcode script name
       result, result_type = here.Base_edit(formbox_target_id, silent_mode=1, key_prefix=key)
       if result_type == 'edit':
-        return FormBoxEditor(result, context_method_id)
+        return FormBoxEditor(result, here)
       elif result_type == 'form':
         formbox_field_errors = REQUEST.get('field_errors', [])
         current_field_errors.extend(formbox_field_errors)

product/ERP5Form/MultiRelationField.py
@@ -465,9 +465,6 @@ class MultiRelationEditor:
         # Make sure no default value appears
         REQUEST.set(self.field_id, None) # XXX Dirty
 
-    def view(self):
-      return self.__dict__
-
     def edit(self, o):
       if self.relation_editor_list is None:
         return