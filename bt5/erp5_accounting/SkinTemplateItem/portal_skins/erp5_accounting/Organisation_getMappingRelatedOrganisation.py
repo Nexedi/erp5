8fbd61f5831728d10c40176545df47989d2b86b7
Jérome Perrin <jerome@nexedi.com>
2018-04-11 08:43:55 +0200

    accounting: rework Organisation_getMappingRelatedOrganisation ( WIP )
    This script was returning organisations in random order !
    
    When organisation does not have accounting periods, but another organisation at the same level of the same group has, the result was undefined.
    
    This fixes by considering all organisation from the same group.

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/Organisation_getMappingRelatedOrganisation.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/Organisation_getMappingRelatedOrganisation.py
@@ -1,8 +1,19 @@
-"""Returns the main organisation for that group.
-"""
+"""Returns the "main organisation" for that group, from accounting point of view.
 
-if len(context.contentValues(filter=
-    dict(portal_type='Accounting Period'))) or context.getMapping():
+Other organisation from this group are considered subsidiaries and have same
+accounting periods as the main organisation. This is also used for invoice reconciliation.
+
+XXX: the name of this script should include something "Accounting"
+TODO: unify with Base_getAccountingPeriodStartDateForSectionCategory
+"""
+portal = context.getPortalObject()
+
+def isMainOrganisation(organisation):
+  return bool(organisation.contentValues(
+    portal_type='Accounting Period',
+    checked_permission='Access contents information'))
+
+if isMainOrganisation(context):
   return context
 
 def getOrganisationForSectionCategory(section):
@@ -24,9 +35,17 @@ group = context.getGroupValue()
 if group is None:
   return context
 
-group_chain = []
 while group.getPortalType() != 'Base Category':
-  group_chain.append(group)
+  section_uid_list = portal.Base_getSectionUidListForSectionCategory(
+      section_category=group.getRelativeUrl(),
+      strict_membership=True)
+  if -1 in section_uid_list:
+    section_uid_list.remove(-1) # XXX explain this
+  if section_uid_list:
+    for brain in portal.portal_catalog(uid=section_uid_list):
+      section = brain.getObject()
+      if isMainOrganisation(section):
+        return section
   group = group.getParentValue()
 
 for group in group_chain:
