1ce386491a3cf8fba37065e5b9b06b9014c5951a
Ayush <ayush.tiwari@nexedi.com>
2018-04-20 17:54:41 +0200

    [erp5_diff]: Use iframe to display diffs. Also add revision list to History tab

--- product/ERP5/bootstrap/erp5_diff/SkinTemplateItem/portal_skins/erp5_diff/Base_getZODBDiff.py
+++ product/ERP5/bootstrap/erp5_diff/SkinTemplateItem/portal_skins/erp5_diff/Base_getZODBDiff.py
@@ -1,3 +1,8 @@
+"""
+Script that returns the diff of last 2 historical states of the context. This
+should be used as a 'list method' as it returns a temp base object which have
+its properties as : `path`, `t1` and `t2`
+"""
 from Products.ERP5Type.Document import newTempBase
 
 portal =  context.getPortalObject()
@@ -5,11 +10,10 @@ portal_diff = portal.portal_diff
 
 # Get the last 2 history of the context
 history = context.Base_getZODBChangeHistoryList(context, size=2)
-#history = [l['changes'] for l in history]
 
-if len(history) >= 1:
+if len(history) > 1:
   # Using this last 2 history dicts, create a Diff
-  diff = portal_diff.diffPortalObject(history[0], history[1]).asBeautifiedJSONDiff()
+  diff = portal_diff.diffPortalObject(history[1], history[0]).asBeautifiedJSONDiff()
   # Return a list of TempBase objects which can be displayed in a listbox
   tempbase_list = []
   uid = 0
@@ -20,5 +24,5 @@ if len(history) >= 1:
     temp_obj.setUid(int(uid))
     uid = uid + 1
     tempbase_list.append(temp_obj)
-  
+
   return tempbase_list
