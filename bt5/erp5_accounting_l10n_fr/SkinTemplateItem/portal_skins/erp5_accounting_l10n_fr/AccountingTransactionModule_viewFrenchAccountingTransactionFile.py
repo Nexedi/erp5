6ef72d5c449697b5cb4f76badf61ae2d44270224
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2018-04-25 11:50:21 +0900

    erp5_accounting_l10n_fr: FEC Report: Allow to group by ledger or ledger+portal_type in addition as per portal_type (the default).
    This required refactoring the source files as the implementation was
    portal_type-centric. This adds a dedicated Python Script with ProxyRole=Manager
    to create Active Processes and thus avoid executing code as Manager as much as
    possible.

--- bt5/erp5_accounting_l10n_fr/SkinTemplateItem/portal_skins/erp5_accounting_l10n_fr/AccountingTransactionModule_viewFrenchAccountingTransactionFile.py
+++ bt5/erp5_accounting_l10n_fr/SkinTemplateItem/portal_skins/erp5_accounting_l10n_fr/AccountingTransactionModule_viewFrenchAccountingTransactionFile.py
@@ -1,6 +1,10 @@
 from Products.ERP5Type.Message import translateString
 portal = context.getPortalObject()
 
+if group_by in ('ledger', 'portal_type_ledger') and ledger is None:
+  return context.Base_redirect('view', keep_items=dict(
+              portal_status_message=translateString("At least one Ledger must be selected")))
+
 person_value = portal.portal_membership.getAuthenticatedMember().getUserValue()
 if person_value is None:
   portal.changeSkin(None)
@@ -15,11 +19,11 @@ if portal.portal_activities.countMessageWithTag(tag) or \
   return context.Base_redirect(form_id, keep_items=dict(
               portal_status_message=translateString("Report already in progress.")))
 
-  
 context.activate().AccountingTransactionModule_viewFrenchAccountingTransactionFileActive(
   section_category,
   section_category_strict,
   at_date,
+  group_by,
   simulation_state,
   ledger,
   user_name=person_value.Person_getUserId(),
