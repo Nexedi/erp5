a603cbfc56d13dadcec5651595cdcd1bf4cc5b9a
Valentin Benozillo <valentin.benozillo@nexedi.com>
2018-04-30 14:05:03 +0000

    [erp5_web_renderjs_ui][erp5_officejs] Add Progressive APP feature for Officejs application

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_officejs_ui/WebSection_renderOfficeJSApplicationPage.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_officejs_ui/WebSection_renderOfficeJSApplicationPage.py
@@ -6,10 +6,18 @@ if response is None:
 default_web_page = context
 web_section = REQUEST.get("current_web_section")
 
-return default_web_page.WebPage_viewAsWeb(mapping_dict={
+mapping_dict={
   "latest_version": web_section.getLayoutProperty("configuration_latest_version", default="development"),
   "latest_document_version": web_section.getLayoutProperty("configuration_latest_document_version", default=context.getWebSiteValue().getId() + "-dev"),
   "redirect_url": web_section.getLayoutProperty("configuration_redirect_url", default=""),
   "cache_file": web_section.getLayoutProperty("configuration_cache_file", default=""),
   "application_name": web_section.getTitle(),
-})
+}
+
+configuration_webapp_manifest_url = web_section.getLayoutProperty("configuration_webapp_manifest_url", default=None)
+if configuration_webapp_manifest_url is None:
+  mapping_dict["webapp_manifest_full_link_tag"] = ''
+else:
+  mapping_dict["webapp_manifest_full_link_tag"] = '<link rel="manifest" href="' + configuration_webapp_manifest_url + '">'
+
+return default_web_page.WebPage_viewAsWeb(mapping_dict=mapping_dict)
