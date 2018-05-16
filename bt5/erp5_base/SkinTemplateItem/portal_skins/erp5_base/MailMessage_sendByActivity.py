d97233bdcd784900f76f5e01d0cb7e7ea3eab6d0
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2018-05-16 10:25:08 +0200

    erp5_base: MailMessage_sendByActivy can be retried
    As since the introduction of Internet Message Post it is more likely that
    conflicts happen. Also, this operation is now safe as sending email in
    Entity_sendEmail is made transactionally (as the interaction with MailHost
    is done through the creation of an activity)

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/MailMessage_sendByActivity.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/MailMessage_sendByActivity.py
@@ -1,11 +1,7 @@
-# We do not want to retry those activities, as sending email is not transactional safe
-activate_kw = kw.pop('activate_kw', {})
-activate_kw['max_retry'] = 0
-activate_kw['conflict_retry'] = False
 
 context.getPortalObject().portal_catalog.searchAndActivate(
   method_id="Entity_sendEmail",
   destination_related_uid=context.getUid(),
   method_kw=method_kw,
-  activate_kw=activate_kw,
+  activate_kw=kw.pop('activate_kw', {}),
   **kw)
