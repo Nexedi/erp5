ea9fe262ef9f469732c540c418f6b76748a11b65
Ivan Tyagov <ivan@nexedi.com>
2018-05-17 11:43:03 +0200

    Add Data Bucket Stream to consistency checks.

--- wendelin/bt5/erp5_neo_administration/ExtensionTemplateItem/portal_components/extension.erp5.NEOAdministration.py
+++ wendelin/bt5/erp5_neo_administration/ExtensionTemplateItem/portal_components/extension.erp5.NEOAdministration.py
@@ -6,6 +6,48 @@ from ZODB import DB
 from neo.client.Storage import Storage
 import hashlib
 
+def DataBucketStream_getChecksumListFromNEONodeListForKey(self, \
+                            node_list, \
+                            ca_file, \
+                            cert_file, \
+                            key_file, \
+                            key, \
+                            threshold):
+  """
+  Directly connect to NEO backends and check checksums of this Data Bucket Stream for this key.
+  """
+  checksum_list = []
+
+  # get directly checksum as we have access to data stream over self
+  data = self.getBucket(key)
+  data = data[:threshold]
+  checksum = hashlib.sha256(data).hexdigest()
+  checksum_list.append(checksum)
+
+  for node in node_list:
+    kw = {'master_nodes': node[0],
+          'name':         node[1],
+          'ca':           ca_file,
+          'cert':         cert_file,
+          'key':          key_file}
+
+    # make a direct connection
+    stor = Storage(**kw)
+    db = DB(stor)
+    conn = db.open()
+    root = conn.root()
+    data_stream_id = self.getId()
+    data_stream = root['Application'].erp5.data_stream_module[data_stream_id]
+    data = data_stream.getBucket(key)
+    data = data[:threshold]
+    conn.close()
+    db.close()
+
+    checksum = hashlib.sha256(data).hexdigest()
+    checksum_list.append(checksum)
+
+  return checksum_list
+
 def DataStream_getChecksumListFromNEONodeListForStartStopOffset(self, \
                             node_list, \
                             ca_file, \
