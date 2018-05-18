c5ee45a80fcf2e2d2d8bbbd959db0e41d4d001e8
Jérome Perrin <jerome@nexedi.com>
2018-05-18 10:57:33 +0200

    erp5_monaco_editor: initial submission
    https://microsoft.github.io/monaco-editor/ as an ERP5 source code editor
    
    It was built using https://lab.nexedi.com/jerome/monaco-editor-erp5/ and
    uploaded to ERP5 following this procedure:
    
    First enable webdav by adding in zope conf:
    ```
    <webdav-source-server>
      address 127.0.0.1:12200
      force-connection-close off
    </webdav-source-server>
    ```
    
    This was built and uploaded to ERP5 with:
    ```bash
    git clone https://lab.nexedi.com/jerome/monaco-editor-erp5.git/
    cd monaco-editor-erp5
    npm
    npm run build
    lftp http://login:pwd@127.0.0.1:12200/erp5/portal_skins/erp5_monaco_editor/monaco-editor/ -e 'mput dist/*'
    
    ```
    
    TODO:
     * [ ] Feature parity with erp5_code_mirror:
       * [ ] diff viewer
       * [ ] full page editor
       * [ ] fullscreen editor ?
       * [ ] do not reload page on save ?
       * [ ] pylint errors + warnings ? ( or language server ? )
       * [ ] SQL editor
       * [ ] no hacks in erp5.css
       * [ ] renderjs gadget version
     * [ ] New features:
       * [ ] python language server ? https://github.com/palantir/python-language-server
       * [ ] [prettier](https://github.com/prettier/prettier) integration, see https://github.com/prettier/prettier-vscode

---
+++ bt5/erp5_monaco_editor/SkinTemplateItem/portal_skins/erp5_monaco_editor/monaco-editor/0.bundle.js.js
@@ -0,0 +1,14 @@
+(self["webpackJsonp"] = self["webpackJsonp"] || []).push([[0],{
+
+/***/ "./node_modules/monaco-editor/esm/vs/editor/common/services sync recursive":
+/*!***********************************************************************!*\
+  !*** ./node_modules/monaco-editor/esm/vs/editor/common/services sync ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function webpackEmptyContext(req) {\n\tvar e = new Error('Cannot find module \"' + req + '\".');\n\te.code = 'MODULE_NOT_FOUND';\n\tthrow e;\n}\nwebpackEmptyContext.keys = function() { return []; };\nwebpackEmptyContext.resolve = webpackEmptyContext;\nmodule.exports = webpackEmptyContext;\nwebpackEmptyContext.id = \"./node_modules/monaco-editor/esm/vs/editor/common/services sync recursive\";\n\n//# sourceURL=webpack:///./node_modules/monaco-editor/esm/vs/editor/common/services_sync?");
+
+/***/ })
+
+}]);
