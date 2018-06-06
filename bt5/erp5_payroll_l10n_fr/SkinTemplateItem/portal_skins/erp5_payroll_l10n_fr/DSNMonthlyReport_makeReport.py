38f1e0e07a256ab8f186f216548e6d2fc0b6bf28
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2018-06-06 09:20:32 +0000

    erp5_payroll_10n_fr: declare taxes only for the DSN report of January

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/DSNMonthlyReport_makeReport.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/DSNMonthlyReport_makeReport.py
@@ -201,7 +201,7 @@ elif len(paysheet_list):
                                     target=aggregated_social_contribution_dict[ctp_code]))
 
 # Annual Taxes
-if organisation == establishment:
+if organisation == establishment and declared_month == 12:
   tax_list = organisation.Organisation_getAnnualTaxDictList(context)
   for tax in tax_list:
     dsn_file.append(getDSNBlockDict(block_id='S21.G00.44', target=establishment, **tax))
