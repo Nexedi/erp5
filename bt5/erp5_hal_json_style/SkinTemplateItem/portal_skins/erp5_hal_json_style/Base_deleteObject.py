6c8accf3cb8454826de7e72841b198e78494fa4e
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2018-06-13 16:39:34 +0000

    erp5_hal_json_style: Fix condition in Base_deleteObject to be similar to what
    is used in Folder_delete, in order to decide if we would delete the object from ZODB
    or do workflow transition.
    
    Previous code was not harmful, since isDeletable check above would block
    the case of existing workflow with 'delete_action' not possible in current state.
    Yet, it was confusing.
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/696

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_deleteObject.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_deleteObject.py
@@ -13,11 +13,18 @@ translate = portal.Base_translateString
 if context.isDeletable(check_relation=True):
   parent = context.getParentValue()
   try:
-    if parent.portal_type != 'Preference' and \
-       portal.portal_workflow.isTransitionPossible(context, 'delete'):
-      portal.portal_workflow.doActionFor(context, 'delete_action')
+    history_dict = context.Base_getWorkflowHistory()
+    history_dict.pop('edit_workflow', None)
+    if history_dict == {} or context.aq_parent.portal_type == 'Preference':
+      # Objects that have no workflow history and
+      # templates inside preference will be unconditionnaly physically deleted
+      parent.manage_delObjects(ids=[context.getId()])
     else:
-      parent.manage_delObjects(ids= [context.getId()])
+      # If a workflow manage a history,
+      # object should not be removed, but only put in state deleted
+      # No need to check if the action is possible,
+      # isDeletable would return False in other case
+      portal.portal_workflow.doActionFor(context, 'delete_action')
     # redirect back to the container since the context was deleted
     return parent.Base_redirect(
       keep_items={
