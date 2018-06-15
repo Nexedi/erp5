e1708acd9642bffed1d4ca9f9e0217fe1eb06f1e
Romain Courteaud <romain@nexedi.com>
2018-06-15 15:52:23 +0000

    [erp5_gadget_interface_validator] No need to copy renderJS code

--- bt5/erp5_gadget_interface_validator/PathTemplateItem/web_page_module/gadget_interface_js.js
+++ bt5/erp5_gadget_interface_validator/PathTemplateItem/web_page_module/gadget_interface_js.js
@@ -1,51 +1,6 @@
 /*jslint nomen: true, indent: 2, maxerr: 30, maxlen: 80 */
 /*global DOMParser, document, rJS, renderJS, RSVP, window,
          jIO*/
-/*
- * DOMParser HTML extension
- * 2012-09-04
- *
- * By Eli Grey, http://eligrey.com
- * Public domain.
- * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
- */
-/*! @source https://gist.github.com/1129031 */
-(function (DOMParser, document) {
-  "use strict";
-  var DOMParser_proto = DOMParser.prototype,
-    real_parseFromString = DOMParser_proto.parseFromString;
-
-  // Firefox/Opera/IE throw errors on unsupported types
-  try {
-    // WebKit returns null on unsupported types
-    if ((new DOMParser()).parseFromString("", "text/html")) {
-      // text/html parsing is natively supported
-      return;
-    }
-  } catch (ignore) {}
-
-  DOMParser_proto.parseFromString = function (markup, type) {
-    var result, doc, doc_elt, first_elt;
-    if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
-      doc = document.implementation.createHTMLDocument("");
-      doc_elt = doc.documentElement;
-
-      doc_elt.innerHTML = markup;
-      first_elt = doc_elt.firstElementChild;
-
-      if (doc_elt.childElementCount === 1
-          && first_elt.localName.toLowerCase() === "html") {
-        doc.replaceChild(first_elt, doc_elt);
-      }
-
-      result = doc;
-    } else {
-      result = real_parseFromString.apply(this, arguments);
-    }
-    return result;
-  };
-}(DOMParser, document));
-
 (function (window, rJS, RSVP, DOMParser, renderJS, jIO) {
   "use strict";
 
