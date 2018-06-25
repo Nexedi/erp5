8b93517d517dc0ee55c08645ca9c4c8aebfb05a8
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-06-25 10:17:20 +0200

    runTestSuite: add conversion_server_retry_count parameter
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/705

--- product/ERP5Type/tests/runTestSuite.py
+++ product/ERP5Type/tests/runTestSuite.py
@@ -54,6 +54,7 @@ def main():
   parser.add_argument('--db_list', help='A list of sql connection strings')
   # parameters that needs to be passed to runUnitTest
   parser.add_argument('--conversion_server_url', default=None)
+  parser.add_argument('--conversion_server_retry_count', default=None)
   parser.add_argument('--conversion_server_hostname', default=None)
   parser.add_argument('--conversion_server_port', default=None)
   parser.add_argument('--volatile_memcached_server_hostname', default=None)
