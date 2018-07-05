b773592326517e72b6adfa2d38c54dd4540579a7
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2018-07-05 16:01:23 +0000

    erp5_base: do not clone logins when cloning persons

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Person_afterClone.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Person_afterClone.py
@@ -1,9 +1,16 @@
 """Hook called when a person object is closed.
 
-We want to reset reference, which is the user login in ERP5Security.
+We want to reset reference, which is the user login in the old ERP5Security.
+We don't want neither to clone the Logins of the user.
 One exception is when a person object is installed from business template.
 """
 context.setUserId(None)
+context.manage_delObjects(ids=[
+  document.getId() for document in context.objectValues(
+    portal_type=context.getPortalObject().getPortalLoginTypeList()
+  )
+])
+
 context.Person_initUserId()
 if not context.REQUEST.get('is_business_template_installation', 0):
   context.setReference(None)
