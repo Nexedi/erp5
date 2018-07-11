cf1fc0968ea9ce19f29fe163b9fed61bf268565a
Vincent Pelletier <vincent@nexedi.com>
2018-07-11 16:34:06 +0900

    erp5_mysql_innodb_catalog: Insert NULL when getUserId returns None.
    And not the string "None". I thought ZSQLMethods were supposed to complain
    when receiving None for non-optional fields...

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_user_list.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_user_list.sql
@@ -2,7 +2,7 @@ REPLACE INTO `user` (`uid`, `user_id`) VALUES
 <dtml-in prefix="loop" expr="_.range(_.len(uid))">
 (
 <dtml-sqlvar expr="uid[loop_item]" type="int">,
-<dtml-sqlvar expr="getUserId[loop_item]" type="string">
+<dtml-sqlvar expr="getUserId[loop_item]" type="string" optional>
 )
 <dtml-if sequence-end><dtml-else>,</dtml-if>
 </dtml-in>
