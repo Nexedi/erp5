41feeea3b6b82cbc3cb028e60af6b034ca2fb43c
Jérome Perrin <jerome@nexedi.com>
2018-07-17 10:30:49 +0200

    test: re-enable some functional configurator tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -83,10 +83,7 @@ class ERP5(_ERP5):
       if test_case.startswith('testLive') or test_case.startswith('testVifib') \
          or test_case.find('Performance') > 0 \
          or test_case in ('testERP5LdapCatalog', # XXX (Ivan), until LDAP server is available this test will alway fail
-                          # tests reading selenium tables from erp5.com
                           # not maintained
-                          'testFunctionalConfigurator',
-                          'testFunctionalConfiguratorConsulting',
                           'testERP5eGov',
                           'testAccounting_l10n_fr_m9',
                           # Not a test
