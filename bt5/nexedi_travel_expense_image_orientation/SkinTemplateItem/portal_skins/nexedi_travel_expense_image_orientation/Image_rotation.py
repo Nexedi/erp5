34bd30fd5f29e321d2eb13d510db3384ab6034bc
valentin <valentin.benozillo@nexedi.com>
2018-07-23 09:59:02 +0000

    [nexedi_travel_expense_image_orientation] Add the "metadata" orientation to the images

--- bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
+++ bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
@@ -7,4 +7,8 @@ image_oriented = context.Base_rotateImage(image=image, rotation=rotation)
 
 context.setData(image_oriented.getvalue())
 
+actual_orientation = context.getProperty("orientation", 0)
+new_orientation = (actual_orientation + rotation) % 360
+context.edit(orientation=new_orientation)
+
 return ""
