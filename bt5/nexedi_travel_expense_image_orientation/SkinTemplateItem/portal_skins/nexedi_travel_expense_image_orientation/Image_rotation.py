c8146dce0bb82d45b3de329cac0127ea67ef36ea
valentin <valentin.benozillo@nexedi.com>
2018-07-23 10:13:14 +0000

    [nexedi_travel_expense_image_orientation] Fixup : Comments from Tristant

--- bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
+++ bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
@@ -3,12 +3,12 @@ import StringIO
 data = context.getData()
 image = StringIO.StringIO(data)
 
-image_oriented = context.Base_rotateImage(image=image, rotation=rotation)
+image_oriented = context.Base_rotateImage(image=image, rotation=degree)
 
 context.setData(image_oriented.getvalue())
 
-actual_orientation = context.getProperty("orientation", 0)
-new_orientation = (actual_orientation + rotation) % 360
+old_orientation = context.getProperty("orientation", 0)
+new_orientation = (old_orientation + degree) % 360
 context.edit(orientation=new_orientation)
 
 return ""
