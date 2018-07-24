df675988cfce351f93abf5b06d9e2bfcc206ed32
valentin <valentin.benozillo@nexedi.com>
2018-07-24 08:25:38 +0000

    [nexedi_travel_expense_image_orientation] Change image property name to rotation

--- bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
+++ bt5/nexedi_travel_expense_image_orientation/SkinTemplateItem/portal_skins/nexedi_travel_expense_image_orientation/Image_rotation.py
@@ -7,8 +7,8 @@ image_oriented = context.Base_rotateImage(image=image, rotation=degree)
 
 context.setData(image_oriented.getvalue())
 
-old_orientation = context.getProperty("orientation", 0)
+old_orientation = context.getProperty("rotation", 0)
 new_orientation = (old_orientation + degree) % 360
-context.edit(orientation=new_orientation)
+context.edit(rotation=new_orientation)
 
 return ""
