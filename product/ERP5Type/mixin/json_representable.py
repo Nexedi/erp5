fcaa4499075d032b642121d000831a6c72f1acde
Ayush <ayush.tiwari@nexedi.com>
2018-08-01 09:37:19 +0200

    Products/ERP5Type: Do not break projects not having xmltodict installed yet

--- product/ERP5Type/mixin/json_representable.py
+++ product/ERP5Type/mixin/json_representable.py
@@ -28,7 +28,13 @@
 ##############################################################################
 
 import json
-import xmltodict
+import warnings
+try:
+  import xmltodict
+except ImportError:
+  xmltodict = None
+  warnings.warn("Please install xmltodict, it is needed by json_representable mixin",
+                 DeprecationWarning)
 import zope.interface
 from OFS import XMLExportImport
 from StringIO import StringIO
