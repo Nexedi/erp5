1f7ff6041da7b11efb4b1cedb1dca510b32c3b8b
Ayush <ayush.tiwari@nexedi.com>
2018-08-07 17:53:49 +0200

    Diff Tool: Redirect to the context in case there is selection of more or less than 2 objects in a module for diff
    Also, update portal action regarding the module selection and add tests

---
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_diff/Base_viewDiffDialogForModuleSelection.py
@@ -0,0 +1,25 @@
+"""
+This script redirects to Diff Tool Dialog. It is used for the case where we
+select 2 objects from a Module and try to get the diff between them.
+
+In case there is selection of more or less than 2 objects, it simply redirects
+back to the module with a portal message.
+"""
+request = context.REQUEST
+portal = context.getPortalObject()
+
+list_selection_name = request.get('list_selection_name', None)
+# In case the list_selection_name is there, it can be the case of selection
+# from the module
+if list_selection_name is not None:
+  selected_obj_list = portal.portal_selections.getSelectionCheckedValueList(selection_name=list_selection_name)
+  # Check if the number of selected object is 2 only. otherwise redirect back to
+  # context with a portal message.
+  if len(selected_obj_list) != 2:
+    message = context.Base_translateString("This action can only compare exactly 2 objects.")
+    return context.Base_redirect('view', keep_items={'portal_status_message': message})
+  else:
+    # Keep the `list_selection_name` in keep_items as we will be needing it
+    # while displaying the paths of the selected objects.
+    return context.Base_redirect('ERP5Site_viewDiffTwoObjectDialog',
+                                  keep_items={'list_selection_name': list_selection_name})
