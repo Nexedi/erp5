0c2ffad5adc164a078067cac5456dc36e67ebd84
Romain Courteaud <romain@nexedi.com>
2018-08-14 10:26:16 +0000

    [erp5_core/base/dms] Allow to download/convert documents from ERP5JS UI.
    * erp5_core: allow to use Base_download from ERP5JS dialog
    * erp5_base/dms* move Document_viewDownloadDialog into erp5_base
    * erp5_base: add a dialog to download Image.
    * erp5_web_renderjs_ui_test: test Dms download actions
    * erp5_dms: allow to download original/converted documents from ERP5JS UI

---
+++ bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/OOoDocument_convert.py
@@ -0,0 +1,15 @@
+"""
+Convert OOoDocument (context) to a desired format and return as a binary `filename` file.
+Requires format and desired filename (adds extension if missing).
+Sets headers and uses native Zope method to produce output.
+"""
+
+request = context.REQUEST
+
+if not filename.endswith('.' + format):
+  filename += '.' + format
+
+request.RESPONSE.setHeader('Content-Type', 'application/' + format)
+request.RESPONSE.setHeader('Content-Disposition', 'attachment; filename="%s"' % filename)
+
+return context.index_html(request, request.RESPONSE)
