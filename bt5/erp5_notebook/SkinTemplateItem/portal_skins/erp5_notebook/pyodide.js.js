1ceea992db0718018ff8be9602093f2f59fdced5
Richard Szczerba <richard.szczerba@nexedi.com>
2018-08-23 11:07:09 +0000

    [erp5_notebook][erp5_officejs][erp5_officejs_ui_test]: update iodide to run pyodide

--- bt5/erp5_notebook/SkinTemplateItem/portal_skins/erp5_notebook/pyodide.js.js
+++ bt5/erp5_notebook/SkinTemplateItem/portal_skins/erp5_notebook/pyodide.js.js
@@ -1,31 +1,201 @@
+/**
+ * The main bootstrap script for loading pyodide.
+ */
+
 var languagePluginLoader = new Promise((resolve, reject) => {
-    let baseURL = "";
-    let wasmURL = `${baseURL}pyodide.asm.wasm`;
-    let wasmXHR = new XMLHttpRequest();
-    wasmXHR.open('GET', wasmURL, true);
-    wasmXHR.responseType = 'arraybuffer';
-    wasmXHR.onload = function() {
-        let Module = {};
+  // This is filled in by the Makefile to be either a local file or the
+  // deployed location. TODO: This should be done in a less hacky
+  // way.
+  const baseURL = '';
 
-        if (wasmXHR.status === 200 || wasmXHR.status === 0) {
-            Module.wasmBinary = wasmXHR.response;
+  ////////////////////////////////////////////////////////////
+  // Package loading
+  var packages = undefined;
+  let loadedPackages = new Set();
+
+  let loadPackage = (names) => {
+    // DFS to find all dependencies of the requested packages
+    let packages = window.pyodide.packages.dependencies;
+    let queue = new Array(names);
+    let toLoad = new Set();
+    while (queue.length) {
+      const package = queue.pop();
+      if (!loadedPackages.has(package)) {
+        toLoad.add(package);
+        if (packages.hasOwnProperty(package)) {
+          packages[package].forEach((subpackage) => {
+            if (!loadedPackages.has(subpackage) && !toLoad.has(subpackage)) {
+              queue.push(subpackage);
+            }
+          });
         } else {
-            console.warn(
-                `Couldn't download the pyodide.asm.wasm binary.  Response was ${wasmXHR.status}`);
-            reject();
+          console.log(`Unknown package '${package}'`);
         }
+      }
+    }
 
-        Module.baseURL = baseURL;
-        Module.postRun = () => {
-            resolve();
+    let promise = new Promise((resolve, reject) => {
+      if (toLoad.size === 0) {
+        resolve('No new packages to load');
+      }
+
+      pyodide.monitorRunDependencies = (n) => {
+        if (n === 0) {
+          toLoad.forEach((package) => loadedPackages.add(package));
+          delete pyodide.monitorRunDependencies;
+          const packageList = Array.from(toLoad.keys()).join(', ');
+          resolve(`Loaded ${packageList}`);
         }
+      };
+
+      toLoad.forEach((package) => {
         let script = document.createElement('script');
-        script.src = `pyodide.asm.js`;
-        script.onload = () => {
-            window.pyodide = pyodide(Module);
-        };
+        script.src = `${baseURL}${package}.js`;
+        script.onerror = (e) => { reject(e); };
         document.body.appendChild(script);
+      });
+
+      // We have to invalidate Python's import caches, or it won't
+      // see the new files. This is done here so it happens in parallel
+      // with the fetching over the network.
+      window.pyodide.runPython('import importlib as _importlib\n' +
+                               '_importlib.invalidate_caches()\n');
+    });
+
+    if (window.iodide !== undefined) {
+      window.iodide.evalQueue.await([ promise ]);
+    }
+
+    return promise;
+  };
+
+  function fixRecursionLimit(pyodide) {
+    // The Javascript/Wasm call stack may be too small to handle the default
+    // Python call stack limit of 1000 frames. This is generally the case on
+    // Chrom(ium), but not on Firefox. Here, we determine the Javascript call
+    // stack depth available, and then divide by 50 (determined heuristically)
+    // to set the maximum Python call stack depth.
+
+    let depth = 0;
+    function recurse() {
+      depth += 1;
+      recurse();
+    }
+    try {
+      recurse();
+    } catch (err) {
+      ;
+    }
+
+    let recursionLimit = depth / 50;
+    if (recursionLimit > 1000) {
+      recursionLimit = 1000;
+    }
+    pyodide.runPython(
+        `import sys; sys.setrecursionlimit(int(${recursionLimit}))`);
+  };
+
+  ////////////////////////////////////////////////////////////
+  // Loading Pyodide
+  let wasmURL = `${baseURL}pyodide.asm.wasm`;
+  let Module = {};
+  window.Module = Module;
+
+  Module.noImageDecoding = true;
+  Module.noAudioDecoding = true;
+  let isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+  if (isFirefox) {
+    console.log("Skipping wasm decoding");
+    Module.noWasmDecoding = true;
+  }
+
+  let wasm_promise = WebAssembly.compileStreaming(fetch(wasmURL));
+  Module.instantiateWasm = (info, receiveInstance) => {
+    wasm_promise.then(module => WebAssembly.instantiate(module, info))
+        .then(instance => receiveInstance(instance));
+    return {};
+  };
+
+  Module.filePackagePrefixURL = baseURL;
+  var postRunPromise = new Promise((resolve, reject) => {
+    Module.postRun = () => {
+      delete window.Module;
+      fetch(`${baseURL}packages.json`)
+          .then((response) => response.json())
+          .then((json) => {
+            window.pyodide.packages = json;
+            fixRecursionLimit(pyodide);
+            resolve();
+          });
     };
-    wasmXHR.send(null);
+  });
+
+  var dataLoadPromise = new Promise((resolve, reject) => {
+    Module.monitorRunDependencies =
+        (n) => {
+          if (n === 0) {
+            delete Module.monitorRunDependencies;
+            resolve();
+          }
+        }
+  });
+
+  Promise.all([ postRunPromise, dataLoadPromise ]).then(() => resolve());
+
+  let data_script = document.createElement('script');
+  data_script.src = `${baseURL}pyodide.asm.data.js`;
+  data_script.onload = (event) => {
+    let script = document.createElement('script');
+    script.src = `${baseURL}pyodide.asm.js`;
+    script.onload = () => {
+      window.pyodide = pyodide(Module);
+      window.pyodide.loadPackage = loadPackage;
+    };
+    document.head.appendChild(script);
+  };
+
+  document.head.appendChild(data_script);
+
+  ////////////////////////////////////////////////////////////
+  // Iodide-specific functionality, that doesn't make sense
+  // if not using with Iodide.
+  if (window.iodide !== undefined) {
+    // Load the custom CSS for Pyodide
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = `${baseURL}renderedhtml.css`;
+    document.getElementsByTagName('head')[0].appendChild(link);
+
+    // Add a custom output handler for Python objects
+    window.iodide.addOutputHandler({
+      shouldHandle : (val) => {
+        return (typeof val === 'function' && pyodide.PyProxy.isPyProxy(val));
+      },
+
+      render : (val) => {
+        let div = document.createElement('div');
+        div.className = 'rendered_html';
+        var element;
+        if (val._repr_html_ !== undefined) {
+          let result = val._repr_html_();
+          if (typeof result === 'string') {
+            div.appendChild(new DOMParser()
+                                .parseFromString(result, 'text/html')
+                                .body.firstChild);
+            element = div;
+          } else {
+            element = result;
+          }
+        } else {
+          let pre = document.createElement('pre');
+          pre.textContent = val.toString();
+          div.appendChild(pre);
+          element = div;
+        }
+        return element;
+      }
+    });
+  }
 });
 languagePluginLoader
