e0f9fc0f9cb314ae0cdf4cef28d631fe11004e45
Jérome Perrin <jerome@nexedi.com>
2018-08-28 09:24:04 +0200

    runTestSuite: support multiple zserver addresses and frontends URLs

--- product/ERP5Type/tests/runTestSuite.py
+++ product/ERP5Type/tests/runTestSuite.py
@@ -10,8 +10,14 @@ def _parsingErrorHandler(data, _):
   print >> sys.stderr, 'Error parsing data:', repr(data)
 taskdistribution.patchRPCParser(_parsingErrorHandler)
 
-def makeSuite(node_quantity=None, test_suite=None, revision=None,
-              db_list=None, **kwargs):
+def makeSuite(
+    node_quantity=None,
+    test_suite=None,
+    revision=None,
+    db_list=None,
+    zserver_address_list=None,
+    zserver_frontend_url_list=None,
+    **kwargs):
   # BBB tests (plural form) is only checked for backward compatibility
   for k in sys.modules.keys():
     if k in ('tests', 'test',) or k.startswith('tests.') or k.startswith('test.'):
@@ -32,6 +38,8 @@ def makeSuite(node_quantity=None, test_suite=None, revision=None,
   suite = suite_class(revision=revision,
                       max_instance_count=node_quantity,
                       mysql_db_list=db_list.split(','),
+                      zserver_address_list=zserver_address_list.split(','),
+                      zserver_frontend_url_list=zserver_frontend_url_list.split(','),
                       **kwargs)
   return suite
 
@@ -51,7 +59,11 @@ def main():
   parser.add_argument('--master_url',
                       help='The Url of Master controling many suites',
                       default=None)
-  parser.add_argument('--db_list', help='A list of sql connection strings')
+  parser.add_argument('--db_list', help='A list of comma separated sql connection strings')
+  parser.add_argument('--zserver_address_list', help='A list of comma seperated host:port for Zserver')
+  parser.add_argument(
+      '--zserver_frontend_url_list',
+      help='A list of comma seperated frontend URLs, one for each of zserver_address_list, in the same order')
   # parameters that needs to be passed to runUnitTest
   parser.add_argument('--conversion_server_url', default=None)
   parser.add_argument('--conversion_server_retry_count', default=None)
@@ -71,10 +83,21 @@ def main():
   master = taskdistribution.TaskDistributor(args.master_url)
   test_suite_title = args.test_suite_title or args.test_suite
   revision = args.revision
+
+  if len(args.zserver_address_list.split(",")) < args.node_quantity:
+    print >> sys.stderr, 'Not enough zserver address/frontends for node quantity %s (%r)' % (
+        args.node_quantity, args.zserver_address_list)
+    sys.exit(1)
+
+  # sanity check
+  assert len(args.zserver_address_list.split(",")) == len(args.zserver_frontend_url_list.split(","))
+
   suite = makeSuite(test_suite=args.test_suite,
                     node_quantity=args.node_quantity,
                     revision=revision,
                     db_list=args.db_list,
+                    zserver_address_list=args.zserver_address_list,
+                    zserver_frontend_url_list=args.zserver_frontend_url_list,
                     bt5_path=args.bt5_path,
                     firefox_bin=args.firefox_bin,
                     xvfb_bin=args.xvfb_bin)
