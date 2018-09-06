824046f4f07c55c27ddc5b3ff8cb0e0ce0b2930a
Ayush <ayush.tiwari@nexedi.com>
2018-09-06 18:14:28 +0200

    [erp5_core] Add titles, fix tests and add links for current value

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getCurrentValueUrl.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getCurrentValueUrl.py
@@ -2,6 +2,7 @@
 This URL script returns the URL for current value for the
 properties in the Historical Comparison Diff.
 """
+from Products.ERP5Type.Log import log
 if brain is None:
   brain = context
 
@@ -9,12 +10,17 @@ if brain is None:
 # parameter to the next view
 path = brain.getProperty('path')
 property_name = path.split('/')[2]
-
+log(context.aq_parent)
+log(brain)
 if url_dict:
-  return {'command': 'raw',
+  return {'command': 'push_history',
           'options': {
-            'url': 'google.com'
-            }
+            'jio_key': context.getRelativeUrl(),
+            },
+          'view_kw': {
+            'jio_key': context.getRelativeUrl(),
+            'view': 'Base_viewCurrentValueForLargeText'
+          }
     }
 
 return 'Base_viewCurrentValueForLargeText?property_name=%s' % property_name
