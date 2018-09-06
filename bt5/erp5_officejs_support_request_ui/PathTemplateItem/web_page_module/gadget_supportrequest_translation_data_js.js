88c7b9dbc724bd209cf4ddb436789cdd4465068b
Jérome Perrin <jerome@nexedi.com>
2018-09-06 12:43:24 +0200

    officejs_support_request_ui: Add translation gadget with french translations

---
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_translation_data_js.js
@@ -0,0 +1,65 @@
+/*globals window*/
+/*jslint indent: 2, nomen: true, maxlen: 100*/
+(function (window) {
+  "use strict";
+
+  window.translation_data = {
+    en: {
+    },
+    fr: {
+      // XXX Change this to "Support Requests Dashboard" ?
+      "Customer Support Dashboard": "Tableau de bord des demandes d'assistance",
+      // XXX change this to "Submit new Support Request"
+      "Submit New Support Request": "Soumettre une nouvelle demande d'assistance",
+      "Generate RSS": "Générer un RSS",
+      "RSS Link": "Lien vers le RSS",
+      "Support Requests WorkList": "Worklists des demandes d'assistances",
+
+      "Support Request Pipe": "Demande d'assistance en cours",
+      "Last Month Activity": "Activité du mois",
+      // English ?
+      "Restore Filter": "Réinitaliser le filtre",
+      // Count ?
+      "Number": "Nombre",
+      "Day": "Jour",
+
+      "Home": "Accueil",
+      "Support Request": "Demande d'assistance",
+      "Support Requests": "Demandes d'assistance",
+      "Preferences": "Préférences",
+      "Logout": "Se déconnecter",
+
+      /* workflow states */
+      "Opened": "Ouvert",
+      "Submitted": "Soumis",
+      "Suspended": "Suspendu",
+      "Closed": "Terminé",
+
+      "Post Comment": "Poster un commentaire",
+      "Comments": "Commentaires",
+
+
+      // Listbox / UI standard
+      "Select": "Sélectionner",
+      "Configure": "Configurer",
+      "Sort": "Trier",
+      "Include": "Inclure",
+      "Exclude": "Exclure",
+      "Records": "Enregistrements",
+
+      "Views": "Vues",
+      "Export": "Exporter",
+      "Previous": "Précédent",
+      "Next": "Suivant",
+      "Editable": "Éditable",
+
+      "Confirm": "Confirmer",
+      "Language": "Langue",
+      "Clone": "Dupliquer",
+      "Delete": "Supprimer",
+      "Save": "Sauvegarder",
+      "User": "Utilisateur"
+    }
+  };
+
+}(window));
