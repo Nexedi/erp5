dba23b54c905c7f5e04107fcefc0c546b3ba6a26
Jérome Perrin <jerome@nexedi.com>
2018-09-07 04:15:48 +0200

    officejs_support_request_ui: Another approach for worklists
    This reapplies parts of 9a84ef83f9aa46cf93cad43aec440dab65cebddc , but a
    different server side approach:
     * use portal_workflow API to count worklists
     * introspect workflow to build queries
     * use translated state in the query, trying to be a bit more user
       friendly. We could do for portal_type as well.
    
    This workarounds #20180907-24A654C a problem with queries generated by
    gadget worklist does not seem to support complex queries with multiple
    values ( local_roles: ['Assignor', 'Assignee'] ). This code generates
    queries:
    
    ```
    ( ( local_roles:  "Assignee" OR local_roles:  "Assignor" ) AND portal_type:  "Support Request" AND translated_simulation_state_title:  "Opened" )
    ```
    
    where the previous version generated queries like:
    
    ```
    ( portal_type:  "Support Request" AND simulation_state:  "validated" AND local_roles: (  "Assignee" OR  "Assignor" ) )
    ```
    
    which did not match anything. Maybe because local_roles is handled
    differently in catalog

---
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequestModule_getWorklistAsJson.py
@@ -0,0 +1,21 @@
+# return worklists from ticket workflow in JSON format
+from Products.ERP5Type.Message import translateString
+import json
+portal = context.getPortalObject()
+
+
+worklist_query_dict = portal.ERP5Site_getTicketWorkflowWorklistInfoDict()
+
+# Query portal actions to get the worklist count and
+# extend this information with the query from our helper script.
+worklist_action_list = [
+  {
+    'action_name': unicode(translateString(action['name'].rsplit(' (', 1)[0])), # Action name include the count, but we display it separatly.
+    'action_count': action['count'],
+    'query': worklist_query_dict[action['worklist_id']],
+  }
+  for action in portal.portal_actions.listFilteredActionsFor(context)['global']
+  if action['category'] == 'global' and action.get('workflow_id') == 'ticket_workflow'
+]
+
+return json.dumps(worklist_action_list)
