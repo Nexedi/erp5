204bed7831c7901cb077d861694124d2c775cb2c
Jérome Perrin <jerome@nexedi.com>
2018-09-07 05:04:08 +0200

    officejs_support_request_ui_test: test for worklists

---
+++ bt5/erp5_officejs_support_request_ui_test/SkinTemplateItem/portal_skins/erp5_officejs_support_request_test/ERP5Site_createSupportRequestUITestDataSet.py
@@ -0,0 +1,14 @@
+"""Create some data for support request UI test.
+
+Logged in user needs to be Assignee / Assignor on the support requests
+included in business template.
+"""
+portal = context.getPortalObject()
+
+for support_request in portal.support_request_module.contentValues():
+  if support_request.getId().startswith('erp5_officejs_support_request_ui_test'):
+    support_request.manage_addLocalRoles(
+        portal.portal_membership.getAuthenticatedMember().getId(),
+        ['Assignee', 'Assignor'])
+
+return "Done."
