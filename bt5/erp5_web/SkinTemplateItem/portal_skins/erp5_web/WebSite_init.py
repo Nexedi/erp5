0881ff56e85cb808f48f2ca1143114aabd63cca9
Jérome Perrin <jerome@nexedi.com>
2018-09-11 11:44:29 +0200

    fixup! web: no need initialize web site languages

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web/WebSite_init.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web/WebSite_init.py
@@ -6,4 +6,6 @@
 context.setContainerLayout('erp5_web_layout')
 context.setContentLayout('erp5_web_content_layout')
 context.setLayoutConfigurationFormId('WebSection_viewDefaultThemeConfiguration')
+# At least one available language is required
+context.setAvailableLanguageSet([context.Localizer.get_selected_language()])
 context.setStaticLanguageSelection(1)
