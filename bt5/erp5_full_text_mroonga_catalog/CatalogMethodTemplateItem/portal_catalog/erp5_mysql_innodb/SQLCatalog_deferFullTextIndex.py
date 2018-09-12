da7e0f0b41c4363d355537560f1257aa9c791faa
Vincent Pelletier <vincent@nexedi.com>
2018-09-12 18:28:20 +0900

    SQLCatalog_deferFullTextIndex{,Activity}: Do not use getObject .
    getObject is generally a shortcut for "return self", and on brains for
    traversing to corresponding document.
    Except... Not everywhere. portal_catalog for example defines a completely
    unrelated method which takes a UID as argument and does a query retrieving
    one document. Which is basically totally redundant with its __call__
    method. Still, of course, it is used in several places.
    Also, ERP5Form.Report defines yet another getObject method, taking an
    acquisition context as argument and traversing to some other path.
    
    So do not use getObject (at least until the code recovers some sanity and
    consistency), but getPath + custom activity API. Oh well.

--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferFullTextIndex.py
+++ bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferFullTextIndex.py
@@ -1,7 +1,9 @@
 # This script is called to defer fulltext indexing in a lower priority.
 GROUP_METHOD_ID = context.getPath() + '/SQLCatalog_deferFullTextIndexActivity'
-for document_value, root_document_path in zip(getObject, getRootDocumentPath):
-  document_value.activate(
+activateObject = context.getPortalObject().portal_activities.activateObject
+for document, root_document_path in zip(getPath, getRootDocumentPath):
+  activateObject(
+    document,
     activity='SQLQueue',
     priority=4,
     group_method_id=GROUP_METHOD_ID,
