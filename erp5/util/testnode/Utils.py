086285fabf17a219c7582c6d00ec4de4f8e0703a
Jérome Perrin <jerome@nexedi.com>
2018-09-19 02:56:40 +0200

    testnode: properly support deletion of chmod'ed files
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/748

--- erp5/util/testnode/Utils.py
+++ erp5/util/testnode/Utils.py
@@ -1,30 +1,36 @@
 import os
 import stat
 import shutil
+import errno
 
 def rmtree(path):
   """Delete a path recursively.
 
   Like shutil.rmtree, but supporting the case that some files or folder
   might have been marked read only.  """
-  def chmod_retry(func, path, _):
-    """Make sure the file is writeable / the directory is executable.
+  def chmod_retry(func, failed_path, exc_info):
+    """Make sure the directories are executable and writable.
     """
-    if not os.path.exists(path):
-      # because we are calling again rmtree on listdir errors, this path might
-      # have been already deleted by the recursive call to rmtree.
-      return
+    e = exc_info[1]
+    if isinstance(e, OSError):
+      if e.errno == errno.ENOENT:
+        # because we are calling again rmtree on listdir errors, this path might
+        # have been already deleted by the recursive call to rmtree.
+        return
+      if e.errno == errno.EACCES:
+        if func is os.listdir:
+          os.chmod(failed_path, 0o700)
+          # corner case to handle errors in listing directories.
+          # https://bugs.python.org/issue8523
+          return shutil.rmtree(failed_path, onerror=chmod_retry)
+        # If parent directory is not writable, we still cannot delete the file.
+        # But make sure not to change the parent of the folder we are deleting.
+        if failed_path != path:
+          os.chmod(os.path.dirname(failed_path), 0o700)
+          return func(failed_path)
+    raise
 
-    os.chmod(path, 0o777)
-    if func is os.listdir:
-      # corner case to handle errors in listing directories.
-      # https://bugs.python.org/issue8523
-      # This might raises MaxRecursionError when the directory cannot be listed
-      # for other reasons than "user does not have read permssion"
-      return shutil.rmtree(path, onerror=chmod_retry)
-    func(path)
   shutil.rmtree(path, onerror=chmod_retry)
-
 
 def createFolder(folder, clean=False):
   if os.path.exists(folder):
