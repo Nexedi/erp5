09352b5ecffb6a599121003e914853dab0b934ce
Jérome Perrin <jerome@nexedi.com>
2018-09-19 02:56:40 +0200

    testnode: properly support deletion of chmod'ed files
    The implementation from 0341ea0dc7a926ecbcc53f7e8a32a032716f9cd3 did not
    cover the case where directory was readble but not writeable.

--- erp5/util/testnode/Utils.py
+++ erp5/util/testnode/Utils.py
@@ -7,24 +7,31 @@ def rmtree(path):
 
   Like shutil.rmtree, but supporting the case that some files or folder
   might have been marked read only.  """
-  def chmod_retry(func, path, _):
+  def chmod_retry(func, failed_path, _):
     """Make sure the file is writeable / the directory is executable.
     """
-    if not os.path.exists(path):
+    if not os.path.exists(failed_path):
       # because we are calling again rmtree on listdir errors, this path might
       # have been already deleted by the recursive call to rmtree.
       return
 
-    os.chmod(path, 0o777)
+    os.chmod(failed_path, 0o777)
     if func is os.listdir:
       # corner case to handle errors in listing directories.
       # https://bugs.python.org/issue8523
       # This might raises MaxRecursionError when the directory cannot be listed
-      # for other reasons than "user does not have read permssion"
-      return shutil.rmtree(path, onerror=chmod_retry)
-    func(path)
-  shutil.rmtree(path, onerror=chmod_retry)
+      # for other reasons than "user does not have read permission"
+      return shutil.rmtree(failed_path, onerror=chmod_retry)
+    try:
+      func(failed_path)
+    except OSError:
+      # if parent directory is not writable, we still cannot delete the file.
+      if failed_path != path: # we don't want to change the parent of the folder we are deleting
+        parent_folder = os.path.normpath(os.path.join(failed_path, '..'))
+        os.chmod(parent_folder, 0o777)
+      func(failed_path)
 
+  shutil.rmtree(path, onerror=chmod_retry)
 
 def createFolder(folder, clean=False):
   if os.path.exists(folder):
