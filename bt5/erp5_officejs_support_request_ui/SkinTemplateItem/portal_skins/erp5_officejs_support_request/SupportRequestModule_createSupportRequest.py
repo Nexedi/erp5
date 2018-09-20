396b595965b1738917b70a24bc62901af50c480c
Jérome Perrin <jerome@nexedi.com>
2018-09-20 05:19:21 +0200

    officejs_support_request: create only one support request
    Also:
     * don't immediate reindex, this is a coding crime
     * don't search catalog by ID, use direct module access

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequestModule_createSupportRequest.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequestModule_createSupportRequest.py
@@ -4,40 +4,21 @@ logged_in_user_value = portal.portal_membership.getAuthenticatedMember().getUser
 
 now = DateTime()
 
-# default_field_your_resource,field_your_title, description, field_your_project, default_field_your_project, form_id, field_your_resource, dialog_id, resource, title, file, project, dialog_method, field_my_description, field_your_file=None
-project_list = portal.portal_catalog(portal_type="Project", id=project) # with id keyword, this function will return a sequence data type which contains one element.
-project_object = project_list[0].getObject()
+project_object = portal.project_module[project]
 
-with ImmediateReindexContextManager() as immediate_reindex_context_manager:
-  support_request = portal.support_request_module.newContent(
-    immediate_reindex=immediate_reindex_context_manager,
-    portal_type='Support Request',
-    title=title,
-    resource="service_module/" + resource,
-  )
+support_request = portal.support_request_module.newContent(
+  portal_type='Support Request',
+  title=title,
+  resource="service_module/" + resource,
+  destination_decision_value=logged_in_user_value,
+  source_decision_value = project_object.getSourceDecisionValue(),
+  source_section_value = project_object.getSourceSectionValue(),
+  source_project_value = project_object,
+  destination_value = project_object.getDestinationValue(),
+  start_date=now,
+)
 
-#    - Reference = automatically generated - already implemented
-#    - Requester = current user person
-#    - Start date = now
-#    - State = submitted
-#    - Billing Supplier = Project related Billing Supplier
-#    - Location = Project related Location
-#    - Supervisor = Project related Supervisor
-
-  support_request = portal.support_request_module.newContent(
-    portal_type='Support Request',
-    title=title,
-    resource="service_module/" + resource,
-    destination_decision_value=logged_in_user_value,
-    source_decision_value = project_object.getSourceDecisionValue(),
-    source_section_value = project_object.getSourceSectionValue(),
-    source_project_value = project_object,
-    destination_value = project_object.getDestinationValue(),
-    start_date=now,
-  )
-
-
-  support_request.submit()
+support_request.submit()
 
 if description is not None or file is not None:
   portal.post_module.PostModule_createHTMLPostForSupportRequest(
