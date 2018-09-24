a0733cd47143b9f554891a20f39886aa6f7c4122
Ayush <ayush.tiwari@nexedi.com>
2018-09-24 11:47:51 +0200

    [erp5_core] Revert view for Historical Comparison.
    Partial revert from 2f121c199baa9537e803e

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getHistoricalComparisonDifferenceList.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getHistoricalComparisonDifferenceList.py
@@ -1,42 +1,65 @@
-from Products.ERP5Type.Document import newTempBase
+from Products.PythonScripts.standard import Object
 from ZODB.POSException import ConflictError
 from zExceptions import Unauthorized
 Base_translateString = context.Base_translateString
 
-portal =  context.getPortalObject()
-portal_diff = portal.portal_diff
 try:
   context.HistoricalRevisions[serial]
 except (ConflictError, Unauthorized):
   raise
-except Exception:
-  return [newTempBase(portal, Base_translateString('Historical revisions are'
+except Exception: # POSKeyError
+  return [Object(property_name=Base_translateString('Historical revisions are'
                       ' not available, maybe the database has been packed'))]
 
 if next_serial == '0.0.0.0':
-  # In case the next serial is 0.0.0.0, we should always be considering the
-  # new object as the current context
   new_getProperty = context.getProperty
-  new = context
 else:
   new = context.HistoricalRevisions[next_serial]
   new_getProperty = new.getProperty
 old = context.HistoricalRevisions[serial]
 result = []
 
-# XXX: Instead of creating a separate property list here, we can use DiffTool
-# to directly find out the beautified diff and send it to the listbox
+binary_data_explanation = Base_translateString("Binary data can't be displayed")
+base_error_message = Base_translateString('(value retrieval failed)')
 
-diff = portal_diff.diffPortalObject(old, new).asBeautifiedJSONDiff()
+for prop_dict in context.getPropertyMap():
+  prop = prop_dict['id']
+  error = False
+  try:
+    current_value = context.getProperty(prop)
+  except TypeError:
+    error = True
+    current_value = base_error_message
+  try:
+    old_value = old.getProperty(prop)
+  except TypeError:
+    error = True
+    old_value = base_error_message
+  try:
+    new_value = new_getProperty(prop)
+  except TypeError:
+    error = True
+    new_value = base_error_message
+  if new_value != old_value or error:
+    # check if values are unicode convertible (binary are not)
+    if isinstance(new_value, (str, unicode)):
+      try:
+        unicode(str(new_value), 'utf-8')
+      except UnicodeDecodeError:
+        new_value = binary_data_explanation
+    if isinstance(old_value, (str, unicode)):
+      try:
+        unicode(str(old_value), 'utf-8')
+      except UnicodeDecodeError:
+        old_value = binary_data_explanation
+    if isinstance(current_value, (str, unicode)):
+      try:
+        unicode(str(current_value), 'utf-8')
+      except UnicodeDecodeError:
+        current_value = binary_data_explanation
 
-tempbase_list = []
-uid = 900
-for x in diff:
-  temp_obj = newTempBase(portal,
-                        x['path'],
-                        **x)
-  temp_obj.setUid('new_%s' % uid)
-  uid = uid + 1
-  tempbase_list.append(temp_obj)
-
-return tempbase_list
+    result.append( Object( property_name=prop,
+                           new_value=new_value,
+                           old_value=old_value,
+                           current_value=current_value))
+return result
