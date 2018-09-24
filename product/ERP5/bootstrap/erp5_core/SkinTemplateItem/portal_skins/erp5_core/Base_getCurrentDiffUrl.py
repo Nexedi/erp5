9a82a8ef74d4d0530fb6724a8e6c8b981cd9f3e8
Ayush <ayush.tiwari@nexedi.com>
2018-09-24 13:44:22 +0200

    [erp5_core] Use DiffTool to display historical revision diff

---
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getCurrentDiffUrl.py
@@ -0,0 +1,48 @@
+"""
+This URL script is used for Historical comparison list
+to show the diff between the new value and current value.
+
+In case there new value is the current value, there is no
+need to show the diff link.
+"""
+request = context.REQUEST
+
+# In this case, the next serial should be the current value, i.e, 0.0.0.0
+serial = getattr(brain, 'next_serial', '0.0.0.0')
+next_serial = '0.0.0.0'
+time = getattr(brain, 'time', '')
+action = getattr(brain, 'action', '')
+actor = getattr(brain, 'actor', '')
+
+# There is no need to compare revisions in case its the
+# first version.
+if serial != next_serial:
+  if url_dict:
+    return {'command': 'index',
+            'options': {
+              # XXX: This shouldn't be the referred way to get
+              # relative URL, but if we directly use context here
+              # to get the relative URL, it will return us the
+              # URL of tempbase object of list, which is not
+              # what we desire, thus taking the approach to
+              # get it via REQUEST
+              'jio_key': request.get('relative_url'),
+            },
+            'view_kw': {
+              'view': 'view_historical_diff',
+              'jio_key': request.get('relative_url'),
+              'extra_param_json': {
+                'first_serial': serial,
+                'second_serial': next_serial,
+                'time': time,
+                'action': action,
+                'actor': actor,
+              }
+            }
+           }
+
+  return 'Base_viewHistoricalComparisonDiff?first_serial=%s&amp;second_serial=%s&amp;time=%s&amp;action=%s&amp;actor=%s'\
+      % ( serial, next_serial, time, action, actor )
+
+elif url_dict:
+  return {}
