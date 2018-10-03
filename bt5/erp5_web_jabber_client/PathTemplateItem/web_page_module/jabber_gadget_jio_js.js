ad602133a0ff199d786d056a0938e378ac7bb9d9
Vincent Bechu <vincent.bechu@nexedi.com>
2018-10-03 15:42:36 +0000

    [erp5_web_jabber_client] Use promise instead synchron code

--- bt5/erp5_web_jabber_client/PathTemplateItem/web_page_module/jabber_gadget_jio_js.js
+++ bt5/erp5_web_jabber_client/PathTemplateItem/web_page_module/jabber_gadget_jio_js.js
@@ -193,6 +193,16 @@
       });
   }
 
+  function createNotification(title, body) {
+    if (window.Notification !== undefined) {
+      return window.Notification.requestPermission()
+        .then(function (result) {
+          if (result === 'granted') {
+            return new window.Notification(title, {body: body});
+          }
+        });
+    }
+  }
   rJS(window)
     .ready(function (g) {
       g.props = {};
@@ -265,24 +275,12 @@
 
     .allowPublicAcquisition("notifyXMPPMessageTextReceived",
                             function (argument_list) {
-
         if (!document.hasFocus()) {
           // Only notify when page has no focused.
           // It simplifies a lot notification status
           document.querySelector("link[rel='shortcut icon']").setAttribute("href", "gadget_jabberclient_notification_warning.ico");
 
-	  if ("Notification" in window) {
-            if (Notification.permission === "granted") {
-              var notification = new Notification(argument_list[0], {body: argument_list[2]});
-            }
-            else if (Notification.permission !== "denied") {
-              Notification.requestPermission(function (permission) {
-                if (permission === "granted") {
-                  var notification = new Notification(argument_list[0], {body: argument_list[2]});
-                }
-              });
-            }
-          }
+          createNotification(argument_list[0], argument_list[0][2]);
         }
 
         var gadget = this;
