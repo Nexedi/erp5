b5153d8b7c732f565edbc3427dc03c262d62af7c
Jérome Perrin <jerome@nexedi.com>
2018-10-09 08:00:01 +0200

    post: review Post API
    * add a `source_reference` field to store message-id
    * store the raw content "as is" - there is not preview for this reason (
      because it might be unsafe html or whatever)
    * remove proxy roles on scripts. User must have permission.
    * minor simplifications

--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/PostModule_createHTMLPost.py
@@ -1,4 +1,7 @@
+# XXX do we need two scripts ??
+
 portal = context.getPortalObject()
+traverse = context.getPortalObject().restrictedTraverse
 
 # create an HTML Post
 post_module = portal.post_module
@@ -6,14 +9,16 @@ post_module = portal.post_module
 now = DateTime()
 post_edit_kw = {
   "start_date": now,
-  "follow_up_value": context.getPortalObject().restrictedTraverse(follow_up),
+  "follow_up_value": traverse(follow_up),
   "text_content": data,
+  "source_reference": source_reference,
+  "title": title,
 }
-if predecessor not in (None, ""):
-  predecessor_value, = portal.portal_catalog(relative_url=predecessor, limit=2)
-  post_edit_kw["predecessor_value"] = predecessor_value.getObject()
+
+if predecessor:
+  post_edit_kw["predecessor"] = traverse(predecessor)
+
 post = post_module.newContent(
-  immediate_reindex=immediate_reindex_context_manager,
   portal_type='HTML Post',
   **post_edit_kw
 )
