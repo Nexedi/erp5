74fc68a7cc73cc5c1e00264797fb5e7f0b1f55f3
Jérome Perrin <jerome@nexedi.com>
2018-10-09 08:03:07 +0200

    support_request_ui: Ingest post into Web Message
    Also:
     * post are now HTML and not plain text
     * all event types are returned by
    SupportRequest_getCommentPostListAsJson (so that the app can also
    display support request used in "traditional" erp5 interface)
     * instead of immediate reindex, we store the "just posted" data in
    portal_session.
     * events are created using resource defined as a web site "layout"
    property (that's not yet editable)
     * posts do not receive a message-id yet (because ui part is not yet
    updated for that), so generate a message-id server side (by just using
    id)

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/PostModule_createHTMLPostForSupportRequest.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/PostModule_createHTMLPostForSupportRequest.py
@@ -1,26 +1,60 @@
-from Products.ERP5Type.ImmediateReindexContextManager import ImmediateReindexContextManager
-follow_up_value = context.getPortalObject().restrictedTraverse(follow_up)
+from Products.ERP5Type.Message import translateString
+portal = context.getPortalObject()
+follow_up_value = portal.restrictedTraverse(follow_up)
 assert follow_up_value.getPortalType() == "Support Request"
-follow_up_value.edit()  # update modification date
 
-with ImmediateReindexContextManager() as immediate_reindex_context_manager:
-  post = context.PostModule_createHTMLPostFromText(
-    follow_up=follow_up,
-    data=data,
-    immediate_reindex_context_manager=immediate_reindex_context_manager,
-  )
+# update modification date
+portal.portal_workflow.doActionFor(
+  follow_up_value,
+  'edit_action',
+  comment=translateString('New message posted.'))
 
-  if file not in ("undefined", None):  # XXX "undefined" ? should also be fixed in javascript side
-    document_kw = {'batch_mode': True,
-                   'redirect_to_document': False,
-                   'file': file}
-    document = context.Base_contribute(**document_kw)
-    # set relation between post and document
-    # XXX successor is used as a way to put a relation between the attachment and the post,
-    #     the actual way should be to use a proper container like an Event that will have
-    #     one or several posts and one or several attachments.
-    post.setSuccessorValueList([document])
-    # XXX depending on security model this should be changed accordingly
-    document.publish()
+post = context.PostModule_createHTMLPostFromText(
+  follow_up=follow_up,
+  data=data,
+  source_reference=source_reference,
+)
 
-  post.publish()
+if file not in ("undefined", None):  # XXX "undefined" ? should also be fixed in javascript side
+  document_kw = {'batch_mode': True,
+                  'redirect_to_document': False,
+                  'file': file}
+  # XXX this Base_contribute might update in place another document with same reference
+  # and leave the post with a "dead link" for successor value.
+  document = context.Base_contribute(**document_kw)
+  # set relation between post and document
+  # XXX successor is used as a way to put a relation between the attachment and the post,
+  #     the actual way should be to use a proper container like an Event that will have
+  #     one or several posts and one or several attachments.
+  post.setSuccessorValueList([document])
+  # XXX depending on security model this should be changed accordingly
+  document.publish() # XXX isn't it share a better default ?
+
+
+# XXX the UI of support request app should be responsible for generating a unique
+# "message id" for each posted message.
+if not post.getSourceReference():
+  post.setSourceReference(post.getId())
+
+if not web_site_relative_url:
+  web_site_relative_url = context.getWebSiteValue().getRelativeUrl()
+
+post.publish() # XXX
+post.activate().Post_ingestMailMessageForSupportRequest(# XXX This API is not agreed
+    web_site_relative_url=web_site_relative_url
+)
+
+# to be able to display the just posted data in SupportRequest_getCommentPostListAsJson,
+# we store it in a session variable.
+successor_list = post.getSuccessorValueList()
+successor_name = successor_link = None
+if successor_list:
+  successor_link, successor_name = successor_list[0].getRelativeUrl(), successor_list[0].getFilename()
+portal.portal_sessions[
+    '%s.latest_comment' % follow_up_value.getRelativeUrl()]['comment_post_list'] = (
+    post.Base_getOwnerTitle(),
+    post.getStartDate().rfc822(),
+    post.asStrippedHTML(),
+    successor_link,
+    successor_name,
+    post.getSourceReference(),
