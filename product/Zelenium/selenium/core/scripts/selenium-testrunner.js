5de34070fe5027da238ad21199be4914435fe18c
Jérome Perrin <jerome@nexedi.com>
2018-10-16 05:18:52 +0200

    Zelenium: do not include (HTML) link in case of expected failure
    89115b8861 is great, but with "testFunctionalAnonymousSelection" the
    result HTML is more than 40Mo.

--- product/Zelenium/selenium/core/scripts/selenium-testrunner.js
+++ product/Zelenium/selenium/core/scripts/selenium-testrunner.js
@@ -502,15 +502,18 @@ objectExtend(HtmlTestCaseRow.prototype, {
 
         // Store the test HTML as a dataURL to ease debugging from posted test results
         // This code should be synchronous (no Blob asDataURL method for example)
-        var aElement = this.trElement.ownerDocument.createElement("a");
-        aElement.textContent = ' (HTML)';
-        function b64EncodeUnicode(str) {
-          return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-            return String.fromCharCode('0x' + p1);
-          }));
+        // We don't do this with "expected failures" tests, not to create too big result pages
+        if (testFrame.currentTestCase.headerRow.trElement.textContent.indexOf("expected failure") === -1) {
+          var aElement = this.trElement.ownerDocument.createElement("a");
+          aElement.textContent = ' (HTML)';
+          function b64EncodeUnicode(str) {
+            return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+              return String.fromCharCode('0x' + p1);
+            }));
+          }
+          aElement.href = 'data:text/html;charset=utf-8;base64,' + b64EncodeUnicode(sel$('selenium_myiframe').contentWindow.document.body.innerHTML);
+          this.trElement.cells[2].appendChild(aElement);
         }
-        aElement.href = 'data:text/html;charset=utf-8;base64,' + b64EncodeUnicode(sel$('selenium_myiframe').contentWindow.document.body.innerHTML);
-        this.trElement.cells[2].appendChild(aElement);
     },
 
     setMessage: function(message) {
