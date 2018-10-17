405ee70eb14a2cdd580e850512957efcec0f0ea2
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2018-10-17 17:56:48 +0200

    SelectionTool/Listbox: Fix in calculating the start of last page in selection
    as the comment (which is removed now) said:
    
    ```
            XXX: This method is broken, since "total_size" field is not
            present in the listbox rendering any longer. It should be
            removed.
    ```
    
    Also:
    - make sure that Listbox always knows how many lines are to be displayed.
    - add testPagerWithCustomListMethod in listbox_zuite of erp5_ui_test,
    which checks that listbox jumping to previous/next/first/last works correctly
    for a list method that returns only the elements that the listbox is to render.
    
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/776

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/ListBoxZuite_resetReportSelections.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/ListBoxZuite_resetReportSelections.py
@@ -1,4 +1,4 @@
-"""Reset report sleections"""
+"""Reset report selections"""
 
 manage_deleteSelection = context.getPortalObject().portal_selections.manage_deleteSelection
 for index in xrange(3):
@@ -6,4 +6,8 @@ for index in xrange(3):
     manage_deleteSelection('x%s_foo_dummy_listbox_selection' % (index, ))
   except KeyError:
     pass
+try:
+  manage_deleteSelection('foo_dummy_returning_only_what_is_shown_listbox_selection')
+except KeyError:
+  pass
 return "Done."
