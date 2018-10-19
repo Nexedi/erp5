267947048fb16b4e7bcb1ffff6ca1621c889d52d
Romain Courteaud <romain@nexedi.com>
2018-10-19 07:27:32 +0000

    [erp5_hal_json_style] Do not convert None to a string

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -1282,7 +1282,8 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
   if relative_url:
     try:
       traversed_document = site_root.restrictedTraverse(str(relative_url))
-      view = str(view)
+      if (view is not None):
+        view = str(view)
       is_site_root = False
     except:
       raise NotImplementedError(relative_url)
