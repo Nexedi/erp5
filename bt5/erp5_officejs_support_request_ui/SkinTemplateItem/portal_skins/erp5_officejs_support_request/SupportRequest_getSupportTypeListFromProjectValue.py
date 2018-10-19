d94f82ce494a0d6928028cf32ec47862d2c23307
Jérome Perrin <jerome@nexedi.com>
2018-10-19 17:06:31 +0900

    officejs_support_request_ui: Take into account preferred SR use
    Only services whose use is included in the preferred SR use are
    allowed when openning a support request.
    
    We still filter the services according to supply lines applicable for
    the selected project, but allow all SR services when project does not
    define supplies.
    
    Another user visible change is that test we no longer select a random
    service, it must be a user decision (eventhough one possible
    sophistication could be to select the service when there's only one).
    Update test accordingly.
    
    Also use relative URL for service, not just the service ID (this is API
    breaking change, but the javascript part should not have this cached)

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportTypeListFromProjectValue.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getSupportTypeListFromProjectValue.py
@@ -1,6 +1,15 @@
+"""Returns the services that can be used on support request for this project.
+"""
 # /!\ proxy role !
+
 portal = context.getPortalObject()
-result = []
+item_list = portal.Ticket_getResourceItemList(
+    portal_type='Support Request',
+    include_context=False,
+)
+
+allowed_resource_relative_url_set = set([])
+# if this project has supplies, only allow projects from the supplies.
 if project_value is not None:
   sale_supply_list = portal.portal_catalog(portal_type="Sale Supply", destination_project_uid=project_value.getUid())
   for sale_supply in sale_supply_list:
@@ -8,6 +17,9 @@ if project_value is not None:
     for supply_line in sale_supply_line_list:
       service = supply_line.getResourceValue()
       if service is not None:
-        result.append((service.getTitle(), service.getId()))
+        allowed_resource_relative_url_set.add(service.getRelativeUrl())
+if allowed_resource_relative_url_set:
+  return [('', '')] + [item for item in item_list if item[1] in allowed_resource_relative_url_set]
 
-return result
+# otherwise return all support request services
+return item_list
