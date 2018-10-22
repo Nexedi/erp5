8582305ba2294247c3ae50494274f344efcf46e2
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-10-22 10:05:46 +0000

    erp5_corporate_identity: make inline anchors  work

--- bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_createTableOfContent.py
+++ bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_createTableOfContent.py
@@ -15,14 +15,13 @@ blank = ""
 header_current = 1
 header_initial = None
 table_of_content = blank
-
+index = 0
 for header in re.findall("<h[1-6].*?</h[1-6]>", doc_content or blank):
   header_level = header[2]
   header_initial = header_initial or header_level
   header_reference = re.findall(">(.*)<", header)[0]
   header_lowercase = header_reference.lower()
-  header_reference_prefix = header_lowercase.replace(" ", "-")
-
+  header_reference_prefix = header_lowercase.replace(" ", "-") + str(index)
   if header_level == header_current:
     table_of_content += '</li>'
 
@@ -37,15 +36,16 @@ for header in re.findall("<h[1-6].*?</h[1-6]>", doc_content or blank):
     table_of_content += '</li></ol>' * iterations
     header_current = header_level
 
+  index += 1
   # add anchor in content
   snippet = ''.join(['>', header_reference])
   named_snippet = ''.join([
     '>',
     '<a name="', html_quote(header_reference_prefix), '_anchor"></a>',
     header_reference,
-    '<a class="custom-para" href=', doc_reference, '#', header_reference_prefix, '><span style="font-size:.75em;line-height:1em;padding-left:.5em;">&para;</span></a>'
+    '<a class="custom-para" href=', '#', header_reference_prefix, '_anchor', '><span style="font-size:.75em;line-height:1em;padding-left:.5em;">&para;</span></a>'
   ])
-  doc_content = doc_content.replace(snippet, named_snippet)
+  doc_content = doc_content.replace(header, header.replace(snippet, named_snippet), 1)
 
   # create table of content entry
   table_of_content += ''.join([
