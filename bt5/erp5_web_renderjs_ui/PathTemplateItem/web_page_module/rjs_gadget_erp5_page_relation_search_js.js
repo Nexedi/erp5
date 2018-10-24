e6c306c8a5278c44f7b18e924f2dea4392e4c7c5
Romain Courteaud <romain@nexedi.com>
2018-10-24 11:47:43 +0200

    [erp5_hal_json_style/erp5_web_renderjs_ui] Stop rendering relation field listboxes
    Speed up JSON calculation by dropping not needed info for rendering.
    
    Render a listfield to select the relation field listboxes on the 'relation_form' page template.

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_relation_search_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_relation_search_js.js
@@ -1,31 +1,9 @@
-/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
-/*global window, rJS, RSVP, Handlebars*/
-(function (window, rJS, RSVP, Handlebars) {
+/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80, unparam: true */
+/*global window, rJS, RSVP*/
+(function (window, rJS, RSVP) {
   "use strict";
 
-  /////////////////////////////////////////////////////////////////
-  // Handlebars
-  /////////////////////////////////////////////////////////////////
-  // Precompile the templates while loading the first gadget instance
-  var gadget_klass = rJS(window),
-    search_source = gadget_klass.__template_element
-                         .getElementById("search-template")
-                         .innerHTML,
-    search_template = Handlebars.compile(search_source);
-
-  Handlebars.registerHelper('equal', function (left_value,
-    right_value, options) {
-    if (arguments.length < 3) {
-      throw new Error("Handlebars Helper equal needs 2 parameters");
-    }
-    if (left_value !== right_value) {
-      return options.inverse(this);
-    }
-    return options.fn(this);
-  });
-
-
-  gadget_klass
+  rJS(window)
     /////////////////////////////////////////////////////////////////
     // Acquired methods
     /////////////////////////////////////////////////////////////////
@@ -33,7 +11,6 @@
     .declareAcquiredMethod("getUrlFor", "getUrlFor")
     .declareAcquiredMethod("redirect", "redirect")
     .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
-    .declareAcquiredMethod("translateHtml", "translateHtml")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -50,38 +27,34 @@
         });
     })
     .declareMethod("render", function (options) {
-      var gadget = this,
-        select_template = options.select_template || "";
+      var gadget = this;
       return new RSVP.Queue()
         .push(function () {
           return RSVP.all([
             gadget.jio_getAttachment(options.url, options.view),
-            gadget.getDeclaredGadget('form_list')
+            gadget.getDeclaredGadget('form_list'),
+            gadget.getDeclaredGadget('field_list')
           ]);
         })
         .push(function (result_list) {
-          var field = result_list[0]._embedded._view[
-            options.back_field.slice("field_".length)
-          ],
-            listbox = field.listbox,
-            listbox_key_list = Object.keys(field.listbox);
+          var listbox = result_list[0]._embedded._view.listbox,
+            proxy_form_id = result_list[0]._embedded._view.proxy_form_id_list;
+          listbox.command = "history_previous";
+          listbox.line_icon = true;
+          listbox.editable = 0;
 
-          if (listbox_key_list.length > 1) {
-            if (select_template === "") {
-              select_template = listbox_key_list[0];
-            }
-          } else {
-            select_template = listbox_key_list[0];
-          }
-          listbox[select_template].command = "history_previous";
-          listbox[select_template].line_icon = true;
+          proxy_form_id.editable = 1;
+          proxy_form_id.hidden = (proxy_form_id.items.length < 2);
 
           return RSVP.all([
-            gadget.changeState({options: JSON.stringify(listbox_key_list),
-                                select_template: select_template}),
+            result_list[2].render({
+              field_json: proxy_form_id,
+              field_type: proxy_form_id.type,
+              label: true
+            }),
             result_list[1].render({
               erp5_document: {"_embedded": {"_view": {
-                "listbox": listbox[select_template]
+                "listbox": listbox
               }},
                 "title": result_list[0].title,
                 "_links": result_list[0]._links
@@ -97,21 +70,7 @@
         });
 
     })
-    .onStateChange(function () {
-      var gadget = this,
-        option_list = JSON.parse(gadget.state.options);
-      if (option_list.length <= 1) {
-        gadget.element.querySelector(".left").innerHTML = '';
-        return;
-      }
-      return gadget.translateHtml(search_template({
-        options: option_list,
-        select_template: gadget.state.select_template
-      }))
-        .push(function (html) {
-          gadget.element.querySelector(".left").innerHTML = html;
-        });
-    })
+
     .declareMethod("triggerSubmit", function () {
       var argument_list = arguments;
       return this.getDeclaredGadget('form_list')
@@ -119,19 +78,23 @@
           return gadget.triggerSubmit.apply(gadget, argument_list);
         });
     })
-    .onEvent('change', function (evt) {
-      var target = evt.target,
-        value;
-      if (target.nodeName === 'SELECT') {
-        value = target.options[target.selectedIndex].value;
-        this.state.select_template = value;
-        return this.redirect({
-          command: 'change',
-          options: {
-            select_template: value
-          }
-        });
+
+    .allowPublicAcquisition('notifyChange', function (ignore, scope) {
+      var gadget = this;
+      if (scope === 'field_list') {
+        return gadget.getDeclaredGadget(scope)
+          .push(function (result) {
+            return result.getContent();
+          })
+          .push(function (result) {
+            return gadget.redirect({
+              command: 'change',
+              options: {
+                view: result.proxy_form_id_list
+              }
+            });
+          });
       }
     });
 
-}(window, rJS, RSVP, Handlebars));
+}(window, rJS, RSVP));
