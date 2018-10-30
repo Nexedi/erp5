4a67b651e59dc3e48e03cfa73317638889100d15
Alain Takoudjou <alain.takoudjou@nexedi.com>
2018-10-30 11:25:57 +0000

    [erp5_web_monitoring] do not loop on asking to import setting from slapos master

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_synchronize_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_synchronize_js.js
@@ -12,7 +12,7 @@
     .declareAcquiredMethod("getSetting", "getSetting")
     .declareAcquiredMethod("setSetting", "setSetting")
 
-    .declareMethod("render", function () {
+    .declareMethod("render", function (options) {
       var gadget = this;
       return this.updateHeader({
         page_title: "Monitoring Synchronization"
@@ -26,6 +26,16 @@
         })
         .push(function () {
           var redirect_options = {"page": "ojsm_status_list"};
+          if (options.reset === "1") {
+            // reset redirections
+            return gadget.setSetting("sync_redirect_options", undefined)
+              .push(function () {
+                return gadget.redirect({
+                  "command": "display",
+                  "options": redirect_options
+                });
+              });
+          }
           return gadget.getSetting('sync_redirect_options')
             .push(function (redirect_dict) {
               if (redirect_options) {
