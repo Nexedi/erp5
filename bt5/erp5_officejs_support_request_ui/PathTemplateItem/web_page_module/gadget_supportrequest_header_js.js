3516fe2c47fe391f74154988a0a90c593c2546fc
Jérome Perrin <jerome@nexedi.com>
2018-10-30 04:35:54 +0100

    officejs_support_request_ui: use a custom header instead of a custom page
    erp5_hal_json does not seem to allow using gadgets by page name,
    anymore, so instead of using a custom "page template" gadget (
    disclaimer: I have not really understood this fully at this point).
    
    Instead of using a custom page gadget, just use a header gadget that
    "extends" the default gadget but removes the add_url

---
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_header_js.js
@@ -0,0 +1,44 @@
+/*global window, rJS */
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS) {
+  'use strict';
+  /**
+   * A gadget which embeds default header, but remove add button, because in the
+   * case of support request app, adding is done through fast input.
+   */
+  var gadgetKlass = rJS(window)
+    .ready(function () {
+      var gadget = this;
+      return this.getDeclaredGadget('header').push(function (headerGadget) {
+        gadget.changeState({
+          headerGadget: headerGadget
+        });
+      });
+    })
+    .declareMethod('render', function (options) {
+      delete options.add_url;
+      return this.getDeclaredGadget('header').push(function (headerGadget) {
+        return headerGadget.render.bind(headerGadget)(options);
+      });
+    });
+
+  /**
+   * Delegate a method to embedded header gadget
+   * @param {string}methodName method name
+   */
+  function delegateMethod(methodName) {
+    gadgetKlass.declareMethod(methodName, function () {
+      var methodArguments = arguments;
+      return this.getDeclaredGadget('header').push(function (headerGadget) {
+        return headerGadget[methodName].bind(headerGadget)(methodArguments);
+      });
+    });
+  }
+  delegateMethod('notifyLoaded');
+  delegateMethod('notifyLoading');
+  delegateMethod('notifySubmitted');
+  delegateMethod('notifySubmitting');
+  delegateMethod('notifyError');
+  delegateMethod('notifyChange');
+  delegateMethod('setButtonTitle');
+}(window, rJS));
