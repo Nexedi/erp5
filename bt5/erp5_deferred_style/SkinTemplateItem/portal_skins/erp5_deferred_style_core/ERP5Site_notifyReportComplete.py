eb9345484b2aec8982506e18e6767a3bde829ae7
Aurélien Calonne <aurel@nexedi.com>
2018-11-13 10:35:37 +0100

    allow to send a containing links to reports instead of the report itself

--- bt5/erp5_deferred_style/SkinTemplateItem/portal_skins/erp5_deferred_style_core/ERP5Site_notifyReportComplete.py
+++ bt5/erp5_deferred_style/SkinTemplateItem/portal_skins/erp5_deferred_style_core/ERP5Site_notifyReportComplete.py
@@ -1,11 +1,53 @@
 portal = context.getPortalObject()
+prefs = portal.portal_preferences
+report_item_list = []
+from Products.PythonScripts.standard import newline_to_br, html_quote
+message_text_format = "text/plain"
+
+if prefs.getPreferredDeferredReportStoredAsDocument():
+  for attachment in attachment_list:
+    document = portal.portal_contributions.newContent(
+          data=attachment['content'],
+          publication_section=prefs.getPreferredDeferredReportPublicationSection(),
+          classification=prefs.getPreferredDeferredReportClassification(),
+          filename=attachment['name'],
+          title=attachment['name'],)
+    document.share()
+    report_item_list.append(
+      (attachment.get('title', attachment['name']),
+        document.getRelativeUrl()))
+
+  url_base = portal.ERP5Site_getAbsoluteUrl()
+  report_url_text = '<br/>'.join([
+    '''<a href="%s/%s">%s</a>''' % (url_base , report_url, report_name ) for (report_name, report_url) in report_item_list ])
+  message = '%s<br/>%s' % ( newline_to_br(html_quote(message)), report_url_text )
+  message_text_format = "text/html"
+  attachment_list = []
+  notification_message_reference = prefs.getPreferredDeferredReportNotificationMessageReference()
+  if notification_message_reference:
+    notification_message = portal.portal_notifications.getDocumentValue(reference=notification_message_reference)
+    if notification_message is None:
+      raise ValueError('Notification message not found by %r' % prefs.getPreferredDeferredReportNotificationMessageReference())
+    notification_mapping_dict={
+        'report_link_list': report_url_text,
+      }
+    if notification_message.getContentType() == "text/html":
+      message = notification_message.asEntireHTML(
+        safe_substitute=False,
+        substitution_method_parameter_dict={'mapping_dict':notification_mapping_dict})
+    else:
+      message_text_format = "text/plain"
+      message = notification_message.asText(
+        safe_substitute=False,
+        substitution_method_parameter_dict={'mapping_dict':notification_mapping_dict})
+
 
 portal.portal_notifications.activate(activity='SQLQueue').sendMessage(
-  recipient=user_name,
-  subject=subject,
-  message=message,
-  message_text_format='text/plain',
-  notifier_list=('Mail Message',),
-  store_as_event=False,
-  attachment_list=attachment_list,
-)
+    recipient=user_name,
+    subject=subject,
+    message=message,
+    message_text_format=message_text_format,
+    notifier_list=('Mail Message',),
+    store_as_event=False,
+    attachment_list=attachment_list,
+  )
