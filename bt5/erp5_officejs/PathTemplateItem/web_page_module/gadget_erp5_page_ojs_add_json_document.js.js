d6a8616689ff1d6418cb00a5af6889014bd86fd1
Boris Kocherov <bk@raskon.ru>
2018-11-21 01:39:56 +0300

    erp5_officejs: schema_editor: remove unused code

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_erp5_page_ojs_add_json_document.js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_erp5_page_ojs_add_json_document.js.js
@@ -3,17 +3,6 @@
 (function (window, rJS, RSVP) {
   "use strict";
 
-  var content_type = {
-      Spreadsheet: 'application/x-asc-spreadsheet',
-      Presentation: 'application/x-asc-presentation',
-      Text: 'application/x-asc-text'
-    },
-    file_ext = {
-      Spreadsheet: 'xlsy',
-      Presentation: 'ppty',
-      Text: 'docy'
-    };
-
   rJS(window)
     /////////////////////////////////////////////////////////////////
     // Acquired methods
@@ -31,18 +20,12 @@
       var gadget = this;
       return RSVP.Queue()
         .push(function () {
-          var portal_type = options.portal_type,
-            ext = file_ext[portal_type],
-            ret = {
+          var ret = {
               title: "Untitled Document",
               portal_type: "JSON Document",
               schema: options.schema,
-              parent_relative_url: "document_module",
-              content_type: content_type[portal_type] || undefined
+              parent_relative_url: "document_module"
             };
-          if (ext) {
-            ret.filename = "default." + ext;
-          }
           return gadget.jio_post(ret);
         })
         .push(function (id) {
