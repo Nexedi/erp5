d971de7149467b1216afdf73b1a57f8230bf5bdb
Jérome Perrin <jerome@nexedi.com>
2018-11-20 15:22:24 +0100

    oauth_facebook_login: inline getFacebookClientIdAndSecretKey
    no need to expose this, also move ERP5Site_redirectToFacebookLoginPage
    as an external method, to be able to access the shared utility function

--- bt5/erp5_oauth_facebook_login/ExtensionTemplateItem/portal_components/extension.erp5.FacebookLoginUtility.py
+++ bt5/erp5_oauth_facebook_login/ExtensionTemplateItem/portal_components/extension.erp5.FacebookLoginUtility.py
@@ -1,8 +1,38 @@
 import facebook
+from ZTUtils import make_query
+
 from Products.ERP5Security.ERP5ExternalOauth2ExtractionPlugin import getFacebookUserEntry
 
+def _getFacebookClientIdAndSecretKey(portal, reference="default"):
+  """Returns facebook client id and secret key.
+
+  Internal function.
+  """
+  result_list = portal.portal_catalog.unrestrictedSearchResults(
+    portal_type="Facebook Connector",
+    reference=reference,
+    validation_state="validated",
+    limit=2,
+  )
+  assert result_list, "Facebook Connector not found"
+  if len(result_list) == 2:
+    raise ValueError("Impossible to select one Facebook Connector")
+  facebook_connector = result_list[0]
+  return facebook_connector.getClientId(), facebook_connector.getSecretKey()
+
+def redirectToFacebookLoginPage(self, came_from=None):
+  client_id, _ = _getFacebookClientIdAndSecretKey(self.getPortalObject())
+  query = make_query({
+    # Call at he context of the appropriate web_service.
+    'client_id': client_id,
+    'redirect_uri': "{0}/ERP5Site_callbackFacebookLogin".format(came_from or self.absolute_url()),
+    'scope': 'email'
+  })
+
+  return self.REQUEST.RESPONSE.redirect("https://www.facebook.com/v2.10/dialog/oauth?{}".format(query))
+
 def getAccessTokenFromCode(self, code, redirect_uri):
-  client_id, secret_key = self.ERP5Site_getFacebookClientIdAndSecretKey()
+  client_id, secret_key = _getFacebookClientIdAndSecretKey(self.getPortalObject())
   return facebook.GraphAPI(version="2.7").get_access_token_from_code(
     code=code, redirect_uri=redirect_uri,
     app_id=client_id, app_secret=secret_key)
