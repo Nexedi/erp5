c11a37265cad2492e01a0a714b75673ab4dd1468
Jérome Perrin <jerome@nexedi.com>
2018-11-20 14:58:22 +0100

    oauth_google_login: inline getGoogleClientIdAndSecretKey
    no need to expose this

--- bt5/erp5_oauth_google_login/ExtensionTemplateItem/portal_components/extension.erp5.GoogleLoginUtility.py
+++ bt5/erp5_oauth_google_login/ExtensionTemplateItem/portal_components/extension.erp5.GoogleLoginUtility.py
@@ -5,8 +5,27 @@ from Products.ERP5Security.ERP5ExternalOauth2ExtractionPlugin import getGoogleUs
 SCOPE_LIST = ['https://www.googleapis.com/auth/userinfo.profile',
               'https://www.googleapis.com/auth/userinfo.email']
 
+
+def _getGoogleClientIdAndSecretKey(portal, reference="default"):
+  """Returns google client id and secret key.
+
+  Internal function.
+  """
+  result_list = portal.portal_catalog.unrestrictedSearchResults(
+    portal_type="Google Connector",
+    reference=reference,
+    validation_state="validated",
+    limit=2,
+  )
+  assert result_list, "Google Connector not found"
+  if len(result_list) == 2:
+    raise ValueError("Impossible to select one Google Connector")
+
+  google_connector = result_list[0].getObject()
+  return google_connector.getClientId(), google_connector.getSecretKey()
+
 def redirectToGoogleLoginPage(self):
-  client_id, secret_key = self.ERP5Site_getGoogleClientIdAndSecretKey()
+  client_id, secret_key = _getGoogleClientIdAndSecretKey(self.getPortalObject())
   flow = oauth2client.client.OAuth2WebServerFlow(
     client_id=client_id,
     client_secret=secret_key,
@@ -18,8 +37,7 @@ def redirectToGoogleLoginPage(self):
   self.REQUEST.RESPONSE.redirect(flow.step1_get_authorize_url())
 
 def getAccessTokenFromCode(self, code, redirect_uri):
-  portal = self.getPortalObject()
-  client_id, secret_key = portal.ERP5Site_getGoogleClientIdAndSecretKey()
+  client_id, secret_key = _getGoogleClientIdAndSecretKey(self.getPortalObject())
   flow = oauth2client.client.OAuth2WebServerFlow(
     client_id=client_id,
     client_secret=secret_key,
