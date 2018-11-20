4182ae93b9ea55d85fe5097c4afe3e8aef39ef11
Jérome Perrin <jerome@nexedi.com>
2018-11-20 15:24:59 +0100

    oauth_facebook_login: reuse setAuthCookie
    setAuthCookie sets authentication cookies with all the necessary
    attributes that makes sense for security.
    
    It's also how auto-logout for inactivity is implemented.

--- bt5/erp5_oauth_facebook_login/TestTemplateItem/portal_components/test.erp5.testFacebookLogin.py
+++ bt5/erp5_oauth_facebook_login/TestTemplateItem/portal_components/test.erp5.testFacebookLogin.py
@@ -111,6 +111,13 @@ class TestFacebookLogin(ERP5TypeTestCase):
     self.assertNotIn("secret_key=", location)
     self.assertIn("ERP5Site_callbackFacebookLogin", location)
 
+  def test_auth_cookie(self):
+    self.portal.ERP5Site_callbackFacebookLogin(code=CODE)
+    response = self.portal.REQUEST.RESPONSE
+    ac_cookie, = [v for (k, v) in response.listHeaders() if k.lower() == 'set-cookie' and '__ac_facebook_hash=' in v]
+    self.assertIn('; Secure', ac_cookie)
+    self.assertIn('; HTTPOnly', ac_cookie)
+
   def test_create_user_in_ERP5Site_createFacebookUserToOAuth(self):
     """
       Check if ERP5 set cookie properly after receive code from external service
