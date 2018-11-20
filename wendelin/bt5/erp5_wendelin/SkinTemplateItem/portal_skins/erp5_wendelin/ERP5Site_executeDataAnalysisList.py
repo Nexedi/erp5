18d8c2bb2b5df244c9c6c2b857126e23a80189ca
Klaus Wölfel <klaus@nexedi.com>
2018-11-20 16:24:01 +0000

    add support for refresh in analysis execution

--- wendelin/bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/ERP5Site_executeDataAnalysisList.py
+++ wendelin/bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/ERP5Site_executeDataAnalysisList.py
@@ -15,7 +15,7 @@ portal = context.getPortalObject()
 #}
 
 #portal.portal_catalog.searchAndActivate(
-#  method_id='DataAnalysis_executeDataOperation', 
+#  method_id='DataAnalysis_executeDataOperation',
 #  method_kw=method_kw,
 #  activate_kw=activate_kw,
 #  **search_kw)
@@ -23,5 +23,13 @@ portal = context.getPortalObject()
 for data_analysis in portal.portal_catalog(portal_type = "Data Analysis",
                                            simulation_state = "started"):
   if not data_analysis.hasActivity():
-    data_analysis.activate(serialization_tag=str(data_analysis.getUid()))\
-      .DataAnalysis_executeDataOperation()
+    if data_analysis.getRefreshState() == "current":
+      data_analysis.activate(serialization_tag=str(data_analysis.getUid()))\
+        .DataAnalysis_executeDataOperation()
+
+for data_analysis in portal.portal_catalog(portal_type = "Data Analysis",
+                                           refresh_state = "refresh_planned"):
+  if data_analysis.getRefreshState() == "refresh_planned":
+    if not data_analysis.hasActivity():
+      data_analysis.activate(serialization_tag=str(data_analysis.getUid()))\
+        .DataAnalysis_clearAndReprocessFromScratch()
