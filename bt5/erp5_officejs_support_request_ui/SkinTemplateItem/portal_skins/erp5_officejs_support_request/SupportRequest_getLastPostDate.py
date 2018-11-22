f665c90e3c8cb7e5183c55dc9df1ec34eb6e4d9e
Jérome Perrin <jerome@nexedi.com>
2018-11-22 09:13:19 +0100

    officejs_support_request_ui: reuse existing API script for "Comment {Date,Author}"
    Because this API script returns JSON, it cannot easily be used
    "internally" server side, so the logic as been moved to a
    SupportRequest_getCommentPostList and
    SupportRequest_getCommentPostListAsJson just does the serialization to
    JSON part.
    
    It has been found that there is no convention for "scripts returning
    JSON but that can also be called by other script", so this way is not an
    example to follow.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostDate.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequest_getLastPostDate.py
@@ -1,32 +1,4 @@
-portal = context.getPortalObject()
-
-# get the related Support Request, this should not be None
-support_request_list = portal.portal_catalog(portal_type="Support Request", id=context.getId()) # with id keyword, this function will return a sequence data type which contains one element.
-
-support_request_object = support_request_list[0].getObject()
-
-# get the all HTML Posts which related to this Support Request
-
-post_list = portal.portal_catalog(portal_type="HTML Post", strict_follow_up_uid=support_request_object.getUid()) # with id keyword, this function will return a sequence data type which contains one element.
-
-post_list = sorted(post_list, key=lambda x:x.getStartDate(), reverse=True)
-  
-preferred_date_order = portal.portal_preferences.getPreferredDateOrder()
-
-if post_list:
-  # raise NotImplementedError(post_list[0].getStartDate())
-  post_date = post_list[0].getStartDate()
-  if is_pure_date:
-    return post_date
-  # raise NotImplementedError(post_date.strftime("%H:%M:%S"))
-  if post_date.isCurrentDay():
-    return post_date.strftime("%H:%M:%S")
-  else:
-    if preferred_date_order == 'dmy':
-      return "%s/%s/%s" % (post_date.dd(), post_date.mm(), post_date.year())
-    if preferred_date_order == 'mdy':
-      return "%s/%s/%s" % (post_date.mm(), post_date.dd(), post_date.year())
-    # ymd
-    return "%s/%s/%s" % (post_date.year(), post_date.mm(), post_date.dd())
-else:
-  return None
+from DateTime import DateTime
+comment_list = context.SupportRequest_getCommentPostList()
+if comment_list:
+  return DateTime(comment_list[-1]['date'])
