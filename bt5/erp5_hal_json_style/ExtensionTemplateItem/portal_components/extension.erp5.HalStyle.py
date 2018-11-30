64a8f8e18b4490e1a65ffc636bec11d91941475a
Romain Courteaud <romain@nexedi.com>
2018-11-30 16:31:49 +0000

    [erp5_hal_json_style] Speed up listbox column calculation
    Shamelessly copy Listbox.py implementation, which limits acquisition usage

--- bt5/erp5_hal_json_style/ExtensionTemplateItem/portal_components/extension.erp5.HalStyle.py
+++ bt5/erp5_hal_json_style/ExtensionTemplateItem/portal_components/extension.erp5.HalStyle.py
@@ -1,4 +1,8 @@
 from Acquisition import aq_self, aq_base, aq_inner
+from Products.ERP5Type.Utils import UpperCase
+from ZODB.POSException import ConflictError
+from AccessControl import Unauthorized
+
 
 def Base_aqSelf(self):
   return aq_self(self)
@@ -16,6 +20,65 @@ def Portal_abortTransaction(self):
 def Field_getSubFieldKeyDict(self, field, field_id, key=None):
   """XXX"""
   return field.generate_subfield_key(field_id, key=key)
+
+def Listbox_getBrainValue(self, brain, obj, select, editable_field=None):
+  """
+  ListBox.py / getValueList
+  """
+  tales = False
+
+  # Use a widget, if any.
+  if editable_field is not None:
+    # XXX we need to take care of whether the editable field is
+    # a proxy field or not, because a proxy field may inherit a
+    # tales expression from a template field, and the API is not
+    # unified.
+    get_tales = getattr(editable_field, 'get_recursive_tales',
+                        editable_field.get_tales)
+    tales = get_tales('default')
+    if tales:
+      default_field_value = editable_field.__of__(obj).get_value('default',
+                                                                 cell=brain)
+
+  # If a tales expression is not defined, get a skin, an accessor or a property.
+  if not tales:
+    if getattr(aq_self(brain), select, None) is not None:
+      default_field_value = getattr(brain, select)
+    else:
+      try:
+        # Get the trailing part.
+        try:
+          property_id = select[select.rindex('.') + 1:]
+        except ValueError:
+          property_id = select
+        try:
+          accessor_name = 'get%s' % UpperCase(property_id)
+          # Make sure the object have the attribute, and this is not
+          # acquired, but still get the attribute on the acquisition wrapper
+          getattr(aq_base(obj), accessor_name)
+          default_field_value = getattr(obj, accessor_name)
+        except AttributeError:
+          default_field_value = getattr(obj, property_id, None)
+      except (AttributeError, KeyError, Unauthorized):
+        default_field_value = None
+
+  # If the value is callable, evaluate it.
+  if callable(default_field_value):
+    try:
+      try:
+        default_field_value = default_field_value(brain=brain)
+      except TypeError:
+        default_field_value = default_field_value()
+    except (ConflictError, RuntimeError):
+      raise
+    except:
+      default_field_value = None
+
+  if default_field_value is None:
+    default_field_value = ''
+
+  return default_field_value
+
 
 def WorkflowTool_listActionParameterList(self):
   action_list = self.listActions()
