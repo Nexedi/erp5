23a762750a17894602761f6f85d781395cba77b9
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-12-04 13:06:01 +0000

    erp5_trade: add inventory report portal type for report
    call calculate product report action to generate automatically current stock report

---
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/SalePackingListCell_getDestinationAssetPrice.py
@@ -0,0 +1,23 @@
+line = context
+delivery = line.getParentValue().getParentValue()
+if delivery.getPortalType() != "Sale Packing List":
+  return None
+section = delivery.getDestinationSectionValue()
+source_currency = delivery.getPriceCurrencyValue()
+from Products.ERP5Type.Document import newTempAccountingTransactionLine
+temp_transaction = newTempAccountingTransactionLine(
+  context.getPortalObject(),
+  "accounting_line",
+  source_section=section.getRelativeUrl(),
+  resource=source_currency.getRelativeUrl(),
+  start_date=delivery.getStartDate(),
+)
+exchange_rate = source_currency.getPrice(
+  context=temp_transaction.asContext(
+      categories=[temp_transaction.getResource(base=True),
+                  section.getPriceCurrency(base=True)],
+  )
+)
+if exchange_rate:
+  return exchange_rate * line.getPrice()
+return line.getPrice()
