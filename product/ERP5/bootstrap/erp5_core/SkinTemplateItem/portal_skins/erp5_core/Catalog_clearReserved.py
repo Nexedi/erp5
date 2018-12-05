44f5377f0d9bc63a31b914600a4f457a0916d398
Ayush Tiwari <ayush.tiwari@nexedi.com>
2018-12-05 12:40:29 +0100

    [erp5_core] Follow guidelines for Actions on portal types Catalog and Catalog Tool by adding dialog before executing

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Catalog_clearReserved.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Catalog_clearReserved.py
@@ -1,4 +1,9 @@
-# Script to clear Catalog
+# Script to clear Reserved UIDs from Catalog
 
 REQUEST = context.REQUEST
+
+if not clear_reserved:
+  return REQUEST.RESPONSE.redirect(context.absolute_url() +
+              '/view?portal_status_message=Reserved%20UIDs%20Not%20Cleared')
+
 return context.manage_catalogClearReserved(REQUEST)
