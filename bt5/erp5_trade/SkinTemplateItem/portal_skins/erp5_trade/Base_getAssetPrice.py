786ce32a95807fec7e67b5f6208ee2aeadcc765b
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-12-05 17:22:51 +0000

    xxxtest

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Base_getAssetPrice.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Base_getAssetPrice.py
@@ -1,17 +1,19 @@
 from Products.ERP5Type.Document import newTempAccountingTransactionLine
-temp_transaction = newTempAccountingTransactionLine(
-  context.getPortalObject(),
-  "accounting_line",
-  source_section=section.getRelativeUrl(),
-  resource=source_currency.getRelativeUrl(),
-  start_date=delivery.getStartDate(),
-)
-exchange_rate = source_currency.getPrice(
-  context=temp_transaction.asContext(
-      categories=[temp_transaction.getResource(base=True),
-                  section.getPriceCurrency(base=True)],
+if source_currency:
+  temp_transaction = newTempAccountingTransactionLine(
+    context.getPortalObject(),
+    "accounting_line",
+    source_section=section.getRelativeUrl(),
+    resource=source_currency.getRelativeUrl(),
+    start_date=delivery.getStartDate(),
   )
-)
-if exchange_rate:
-  return exchange_rate * context.getPrice()
+  exchange_rate = source_currency.getPrice(
+    context=temp_transaction.asContext(
+        categories=[temp_transaction.getResource(base=True),
+                    section.getPriceCurrency(base=True)],
+    )
+  )
+
+  if exchange_rate:
+    return exchange_rate * context.getPrice()
 return context.getPrice()
--- product/ERP5/tests/testInventoryModule.py
+++ product/ERP5/tests/testInventoryModule.py
@@ -47,6 +47,12 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
   view_stock_date = '2005/12/31' # The day where we are looking for stock
   size_list = ['Child/32','Child/34']
 
+  def getBusinessTemplateList(self):
+    """
+    """
+    return super(TestInventoryModule, self).getBusinessTemplateList() + ('erp5_accounting',)
+  
+
   def getTitle(self):
     return "Inventory Module"
 
@@ -621,10 +627,11 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
 
   def stepCreateThreeCurrencys(self, sequence=None,
                                  sequence_list=None, **kw):
+    self.login()
     start_date = DateTime('2018/01/01 00:00')
     stop_date = DateTime('3018/01/01 00:00')
     portal_type = 'Currency'
-    currency_module = self.portal.getDefaultModule(portal_type)
+    currency_module = self.portal.currency_module
     rmb = currency_module.newContent(
       portal_type=portal_type,
       title='Yuan',
@@ -694,7 +701,9 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
     rmb.validate()
     euro.validate()
     dollar.validate()
-  
+    self.tic()
+    #self.portal.ERP5Site_reindexAll(clear_catalog=1)
+    self.tic()
     sequence.edit(
       rmb = rmb,
       euro = euro,
@@ -977,12 +986,17 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
     pl.start()
     pl.stop()
     pl.deliver() 
+    self.tic()
+    from Testing import ZopeTestCase
+    for i in self.portal.portal_simulation.getInventoryList():
+      ZopeTestCase._print(' total_price %s quantity %s portal type %s  \n' % (i.total_price, i.total_quantity, i.portal_type))
   
   def stepTestCalculateProduct(self, sequence=None, sequence_list=None, **kw):
     inventory_report = self.getInventoryModule().newContent(portal_type='Inventory Report')
     inventory_report.edit(
       destination_value = sequence.get('organisation_rmb'),
-      destination_section_value = sequence.get('organisation_rmb')
+      destination_section_value = sequence.get('organisation_rmb'),
+      valuation_method = 'Fifo'
       )
     self.tic()
     inventory_report.InventoryReport_calculateProductStock(batch_mode=True)
@@ -1010,7 +1024,8 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
     inventory_report = self.getInventoryModule().newContent(portal_type='Inventory Report')
     inventory_report.edit(
       destination_value = sequence.get('organisation_euro'),
-      destination_section_value = sequence.get('organisation_euro')
+      destination_section_value = sequence.get('organisation_euro'),
+      valuation_method = 'Fifo'
     )
     self.tic()
     inventory_report.InventoryReport_calculateProductStock(batch_mode=True)
@@ -1030,7 +1045,8 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
     inventory_report = self.getInventoryModule().newContent(portal_type='Inventory Report')
     inventory_report.edit(
       destination_value = sequence.get('organisation_dollar'),
-      destination_section_value = sequence.get('organisation_dollar')
+      destination_section_value = sequence.get('organisation_dollar'),
+      valuation_method = 'Fifo'
     )
     self.tic()
     inventory_report.InventoryReport_calculateProductStock(batch_mode=True)
@@ -1055,22 +1071,31 @@ class TestInventoryModule(TestOrderMixin, ERP5TypeTestCase):
     sequence_string = 'CreateNotVariatedResource \
                        stepCreateThreeCurrencys \
                        stepCreateThreeOrganisations \
+                       stepTic \
                        stepCreateSalesPackingListRMBToEURO \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreateSalesPackingListEUROToUSD \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreatePurchasePackingListFromRMBToEURO \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreatePurchasePackingListFromEUROToUSD \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreateVariatedResource \
+                       stepTic \
                        stepCreateSalesPackingListEUROToRMB \
                        stepCreateVariatedSalesPackingListLine \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreateSalesPackingListRMBToUSD \
                        stepCreateVariatedSalesPackingListLine \
                        stepDeliverPackingList \
+                       stepTic \
                        stepCreateVariatedResource \
+                       stepTic \
                        stepCreatePurchasePackingListFromUSDToRMB \
                        stepCreateVariatedPurchasePackingListLine \
                        stepDeliverPackingList \
