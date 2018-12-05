efce16cbac9c91748e1f5fad2db9d989c243b883
Jérome Perrin <jerome@nexedi.com>
2018-12-05 04:27:08 +0100

    configurator: reset configurator cookies when launching config from workfow
    If user still has a cookie from a previous run this this will cause
    problems.
    
    This is a dirty workaround. Why configurator has to use cookies ?

--- bt5/erp5_configurator/SkinTemplateItem/portal_skins/erp5_configurator/Workflow_launchConfiguration.py
+++ bt5/erp5_configurator/SkinTemplateItem/portal_skins/erp5_configurator/Workflow_launchConfiguration.py
@@ -7,4 +7,12 @@ bc = context.business_configuration_module.newContent(
 
 request.set("field_your_business_configuration", bc.getRelativeUrl())
 
+# Configurator use a cookie to know what's the "current" configuration,
+# we also need to initialize this cookie (yes, this is a ugly)
+request.RESPONSE.setCookie(
+  'business_configuration_key',
+  bc.getRelativeUrl(),
+  path=portal.portal_configurator.absolute_url_path()
+)
+
 return portal.portal_configurator.login(REQUEST=request)
