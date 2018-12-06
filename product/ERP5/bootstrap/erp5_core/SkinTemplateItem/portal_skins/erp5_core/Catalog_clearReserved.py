77670f9da24557a947a59c8179f6b319ebd43778
Ayush Tiwari <ayush.tiwari@nexedi.com>
2018-12-06 11:47:20 +0100

    ERP5Catalog + [erp5_core] Cleanup useless actions and functions

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Catalog_clearReserved.py
+++
@@ -1,9 +0,0 @@
-# Script to clear Reserved UIDs from Catalog
-
-REQUEST = context.REQUEST
-
-if not clear_reserved:
-  return REQUEST.RESPONSE.redirect(context.absolute_url() +
-              '/view?portal_status_message=Reserved%20UIDs%20Not%20Cleared')
-
-return context.manage_catalogClearReserved(REQUEST)
