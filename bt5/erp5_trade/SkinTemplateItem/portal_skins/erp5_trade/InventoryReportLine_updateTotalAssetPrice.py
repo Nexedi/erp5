257d50019e18cef0aa6d43004c071e116da7447c
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-12-17 09:46:14 +0000

    erp5_trade: no need to execute twice and use getParentValue

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/InventoryReportLine_updateTotalAssetPrice.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/InventoryReportLine_updateTotalAssetPrice.py
@@ -1,7 +1,7 @@
 line_total_asset_price = context.getPortalObject().portal_simulation.getCurrentInventoryAssetPrice(**kw) or 0
 context.edit(
-  total_asset_price = context.getPortalObject().portal_simulation.getCurrentInventoryAssetPrice(**kw) or 0
+  total_asset_price = line_total_asset_price
   )
-inventory_report = context.getParent()
+inventory_report = context.getParentValue()
 inventory_report.edit(
   total_asset_price = inventory_report.getTotalAssetPrice() + line_total_asset_price)
