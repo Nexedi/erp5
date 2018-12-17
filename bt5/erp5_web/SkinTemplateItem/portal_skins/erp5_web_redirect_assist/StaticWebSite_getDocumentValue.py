7afe6dd352f99291c6cf2d832a6b3d501025d12c
Romain Courteaud <romain@nexedi.com>
2018-12-17 16:24:51 +0000

    fixup

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/StaticWebSite_getDocumentValue.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/StaticWebSite_getDocumentValue.py
@@ -7,8 +7,6 @@ Proxy of StaticWebSection_getDocumentValue to allow redirection
 # ------------------------------------------------------------------------------
 # name                           main_template
 
-# context.REQUEST.other['actual_url'] = context.REQUEST['ACTUAL_URL']
-
 # catch KeyError on source_path with urls "", "?", "/?", "...&"
 try:
   context.REQUEST.other['source_path'] = name
