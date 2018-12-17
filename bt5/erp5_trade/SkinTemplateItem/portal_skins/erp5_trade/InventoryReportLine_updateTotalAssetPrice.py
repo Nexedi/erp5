528b07bf4c725b8751c202186d1754eccb96956c
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2018-12-17 09:46:14 +0000

    erp5_trade: no need to execute one by one

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/InventoryReportLine_updateTotalAssetPrice.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/InventoryReportLine_updateTotalAssetPrice.py
@@ -1,7 +1,3 @@
-line_total_asset_price = context.getPortalObject().portal_simulation.getCurrentInventoryAssetPrice(**kw) or 0
 context.edit(
   total_asset_price = context.getPortalObject().portal_simulation.getCurrentInventoryAssetPrice(**kw) or 0
   )
-inventory_report = context.getParent()
-inventory_report.edit(
-  total_asset_price = inventory_report.getTotalAssetPrice() + line_total_asset_price)
