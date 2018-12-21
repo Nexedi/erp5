27126964cf0fae20245453c5f4a033be3596f2a2
Julien Muchembled <jm@nexedi.com>
2018-12-21 19:55:07 +0100

    Fix Base_reindexObjectSecurity when an allowed content is a solver

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_reindexObjectSecurity.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_reindexObjectSecurity.py
@@ -2,10 +2,9 @@
 #
 # After all, recursively re-indexing a module in a production system
 # with lots of content could mean hours of non-usable overloaded system.
-type_tool = context.getPortalObject().portal_types 
+type_tool = context.getPortalObject().portal_types
 for portal_type_name in context.getTypeInfo().getTypeAllowedContentTypeList():
-  portal_type = type_tool[portal_type_name]
-  if portal_type.getTypeAcquireLocalRole():
+  if getattr(type_tool, portal_type_name).getTypeAcquireLocalRole():
     reindex = context.recursiveReindexObject
     break
 else:
