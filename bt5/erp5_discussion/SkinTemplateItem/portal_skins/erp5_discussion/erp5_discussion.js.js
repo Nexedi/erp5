7aa5fb7690e0b57d532ac08fd9e50ea4e0f6cfa1
Jérome Perrin <jerome@nexedi.com>
2019-01-17 12:01:56 +0900

    discussion: remove temporary hidden input added for click
    When clicking "Reply" on a discussion post, this script adds a hidden input to mark which post we are replying to.
    This cause problem  when ctrl+clicking reply to reply in a new tab, because it leaves the page with the extra hidden input, which cause problems, like clicking reply again cause `Error Value: You are not allowed to access '['1', '1']`' in this context , or clicking change page will reply.
    
    This is a minimal effort fix, just remove the added element after click.
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/823

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/erp5_discussion.js.js
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/erp5_discussion.js.js
@@ -1,14 +1,27 @@
-function deleteDiscussionPost(id){
-  /* this will add respective input box for delete post id (so multiple delete buttons can 
-     safely coexist in one HTML page with one HTML form */
-  $("form").append('<input type="hidden" name="discussion_post_uid" value="' +id +  '">');
-  clickSaveButton("DiscussionThread_deleteDiscussionPost");
-}
-
-function redirectCreateCitedNewDiscussionPost(id){
+function clickSaveButtonWithDiscussionPostUidHidden(id, buttonName) {
   /* this will add respective input box for reply post id (so multiple reply buttons can 
-     safely coexist in one HTML page with one HTML form */
-  $("form").append('<input type="hidden" name="discussion_post_uid" value="' +id +  '">');
-  clickSaveButton("DiscussionThread_redirectCreateNewDiscussionPost");
+     safely coexist in one HTML page with one HTML form.
+     Once the button was clicked, the hidden input is removed (so that this can be used
+     multiple times without reloading).   
+  */
+  var hiddenInput = $('<input type="hidden" name="discussion_post_uid">'),
+    form = $('form');
+  hiddenInput.val(id);
+  form.append(hiddenInput);
+  clickSaveButton(buttonName);
+  hiddenInput.remove();
 }
 
+function deleteDiscussionPost(id) {
+  clickSaveButtonWithDiscussionPostUidHidden(
+    id,
+    'DiscussionThread_deleteDiscussionPost'
+  );
+}
+
+function redirectCreateCitedNewDiscussionPost(id) {
+  clickSaveButtonWithDiscussionPostUidHidden(
+    id,
+    'DiscussionThread_redirectCreateNewDiscussionPost'
+  );
+}
