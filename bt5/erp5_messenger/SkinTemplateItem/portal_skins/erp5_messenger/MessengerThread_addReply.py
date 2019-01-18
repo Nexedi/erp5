8173e6d11ab81cbb45640c3e972309527549d2da
Roque Porchetto <roque.porchetto@nexedi.com>
2019-01-18 10:44:07 +0000

    erp5_messenger: messenger now relies on post_module and web messages (events) - refactoring in scripts to create threads and replies - new query to get posts

--- bt5/erp5_messenger/SkinTemplateItem/portal_skins/erp5_messenger/MessengerThread_addReply.py
+++ bt5/erp5_messenger/SkinTemplateItem/portal_skins/erp5_messenger/MessengerThread_addReply.py
@@ -1,20 +1,8 @@
 assert context.getPortalType() == "Messenger Thread"
 
-portal = context.getPortalObject()
-
-person = context.ERP5Site_getAuthenticatedMemberPersonValue()
-person = portal.person_module.get("roqueporchetto")
-#TODO get person from user/autenticatedMember
-
 title = "Re: " + context.getTitle()
 
-messenger_post = portal.messenger_post_module.newContent(
-                    title = title,
-                    text_content = text_content,
-                    source_value = person,
-                    follow_up_value = context,
-                    portal_type = "Messenger Post"
-)
+context.MessengerThread_createPost(title, text_content)
 
 portal_status_message = "New post created"
 context.Base_redirect("view", keep_items = dict(portal_status_message = portal_status_message))
