9c79cde0d398c90def6fee13b3e60e17be90aba9
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2019-01-25 09:13:55 +0000

    erp5_corporate_identity: make sure header list has element before access

--- bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_downgradeHeaders.py
+++ bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_downgradeHeaders.py
@@ -9,11 +9,13 @@ def pushDown(level):
   return ''.join(["h", str(level), ">"])
 
 for header in re.findall("<h[1-6].*</h[1-6]>", content or ""):
-  tag = re.findall("<(h[1-6]>)", header)[0] #h2>
-  key = tag[1]
-  content = content.replace(
-    header,
-    header.replace(tag, pushDown(int(key) + (downgrade or 1)))
-  )
+  header_list = re.findall("<(h[1-6]>)", header)
+  if len(header_list):
+    tag = header_list[0] #h2>
+    key = tag[1]
+    content = content.replace(
+      header,
+      header.replace(tag, pushDown(int(key) + (downgrade or 1)))
+    )
 
 return content
