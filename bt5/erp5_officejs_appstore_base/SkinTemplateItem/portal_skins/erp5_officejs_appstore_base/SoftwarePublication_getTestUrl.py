34057cae81bd4e84d69bca09392dade28cd035f0
Romain Courteaud <romain@nexedi.com>
2019-01-30 14:04:21 +0000

    [erp5_officejs_appstore_base] Stop hardcoding the appstore URL
    Set the wildcard URL on a system preference.

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwarePublication_getTestUrl.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwarePublication_getTestUrl.py
@@ -8,8 +8,12 @@ software_product = software_publication_line.getResourceValue(portal_type="Softw
 if not software_product or not software_release or not software_product.getFollowUpId(portal_type="Web Section"):
   return
 
-# XXX Hardcoded
-return "https://%s.app.officejs.com/%s/" % (
+portal = context.getPortalObject()
+preference_tool = portal.portal_preferences
+
+return "%s://%s.%s/%s/" % (
+  preference_tool.getPreferredSystemAppstoreWildcardProtocol(),
   software_product.getFollowUpId(portal_type="Web Section"),
+  preference_tool.getPreferredSystemAppstoreWildcardDomain(),
   software_release.getReference(),
 )
