cdce67b55b7478860451805a951ef7c57ecf3ba9
Jérome Perrin <jerome@nexedi.com>
2019-02-26 10:15:46 +0100

    access_token: merge all tests in one test
    no need to use different test components

--- bt5/erp5_access_token/TestTemplateItem/portal_components/test.erp5.testERP5AccessToken.py
+++ bt5/erp5_access_token/TestTemplateItem/portal_components/test.erp5.testERP5AccessToken.py
@@ -1,14 +1,61 @@
-# Copyright (c) 2002-2013 Nexedi SA and Contributors. All Rights Reserved.
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2002-2019 Nexedi SA and Contributors. All Rights Reserved.
+#                    Tristan Cavelier <tristan.cavelier@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+##############################################################################
 
+from ZPublisher.HTTPRequest import HTTPRequest
+from ZPublisher.HTTPResponse import HTTPResponse
+from DateTime import DateTime
+import base64
+import StringIO
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
+from Products.ERP5Security.ERP5DumbHTTPExtractionPlugin import ERP5DumbHTTPExtractionPlugin
 
-class TestERP5AccessTokenSkins(ERP5TypeTestCase):
 
-  test_token_extraction_id = 'test_erp5_access_token_extraction'
-
+class AccessTokenTestCase(ERP5TypeTestCase):
   def getBusinessTemplateList(self):
     return ('erp5_base',
             'erp5_access_token')
+
+  def _createPerson(self, new_id, password=None):
+    """Creates a person in person module, and returns the object, after
+    indexing is done. """
+    person_module = self.getPersonModule()
+    person = person_module.newContent(portal_type='Person',
+      reference='TESTP-' + new_id)
+    if password:
+      person.setPassword(password)
+    person.newContent(portal_type = 'Assignment').open()
+    self.tic()
+    return person
+
+
+class TestERP5AccessTokenSkins(AccessTokenTestCase):
+  test_token_extraction_id = 'test_erp5_access_token_extraction'
 
   def generateNewId(self):
     return str(self.portal.portal_ids.generateNewId(
@@ -36,16 +83,6 @@ class TestERP5AccessTokenSkins(ERP5TypeTestCase):
     elif len(access_extraction_list) > 1:
       raise ValueError
     self.commit()
-
-  def _createPerson(self, new_id):
-    """Creates a person in person module, and returns the object, after
-    indexing is done. """
-    person_module = self.getPersonModule()
-    person = person_module.newContent(portal_type='Person',
-      user_id='TESTP-' + new_id)
-    person.newContent(portal_type = 'Assignment').open()
-    self.tic()
-    return person
 
   def _getTokenCredential(self, request):
     """Authenticate the request and return (user_id, login) or None if not authorized."""
@@ -256,3 +293,122 @@ class TestERP5AccessTokenSkins(ERP5TypeTestCase):
 
     result = access_token.OneTimeRestrictedAccessToken_getUserId()
     self.assertEqual(result, None)
+
+
+class TestERP5AccessTokenAlarm(AccessTokenTestCase):
+
+  def test_alarm_old_validated_restricted_access_token(self):
+    access_token = self.portal.access_token_module.newContent(
+      portal_type="One Time Restricted Access Token",
+    )
+    access_token.workflow_history['edit_workflow'] = [{
+        'comment':'Fake history',
+        'error_message': '',
+        'actor': 'ERP5TypeTestCase',
+        'state': 'current',
+        'time': DateTime('2012/11/15 11:11'),
+        'action': 'foo_action'
+        }]
+    self.portal.portal_workflow._jumpToStateFor(access_token, 'validated')
+    self.tic()
+
+    self.portal.portal_alarms.\
+      erp5_garbage_collect_one_time_restricted_access_token.activeSense()
+    self.tic()
+
+    self.assertEqual('invalidated', access_token.getValidationState())
+    self.assertEqual(
+        'Unused for 1 day.',
+        access_token.workflow_history['validation_workflow'][-1]['comment'])
+
+  def test_alarm_recent_validated_restricted_access_token(self):
+    access_token = self.portal.access_token_module.newContent(
+      portal_type="One Time Restricted Access Token",
+    )
+    self.portal.portal_workflow._jumpToStateFor(access_token, 'validated')
+    self.tic()
+
+    self.portal.portal_alarms.\
+      erp5_garbage_collect_one_time_restricted_access_token.activeSense()
+    self.tic()
+
+    self.assertEqual('validated', access_token.getValidationState())
+
+  def test_alarm_old_non_validated_restricted_access_token(self):
+    access_token = self.portal.access_token_module.newContent(
+      portal_type="One Time Restricted Access Token",
+    )
+    access_token.workflow_history['edit_workflow'] = [{
+        'comment':'Fake history',
+        'error_message': '',
+        'actor': 'ERP5TypeTestCase',
+        'state': 'current',
+        'time': DateTime('2012/11/15 11:11'),
+        'action': 'foo_action'
+        }]
+    self.tic()
+
+    self.portal.portal_alarms.\
+      erp5_garbage_collect_one_time_restricted_access_token.activeSense()
+    self.tic()
+
+    self.assertEqual('draft', access_token.getValidationState())
+
+
+class TestERP5DumbHTTPExtractionPlugin(AccessTokenTestCase):
+
+  test_id = 'test_erp5_dumb_http_extraction'
+
+  def generateNewId(self):
+    return str(self.portal.portal_ids.generateNewId(
+         id_group=('erp5_dumb_http_test_id')))
+
+  def afterSetUp(self):
+    """
+    This is ran before anything, used to set the environment
+    """
+    self.new_id = self.generateNewId()
+    self._setupDumbHTTPExtraction()
+    self.tic()
+
+  def do_fake_request(self, request_method, headers=None):
+    if headers is None:
+      headers = {}
+    __version__ = "0.1"
+    env={}
+    env['SERVER_NAME']='bobo.server'
+    env['SERVER_PORT']='80'
+    env['REQUEST_METHOD']=request_method
+    env['REMOTE_ADDR']='204.183.226.81 '
+    env['REMOTE_HOST']='bobo.remote.host'
+    env['HTTP_USER_AGENT']='Bobo/%s' % __version__
+    env['HTTP_HOST']='127.0.0.1'
+    env['SERVER_SOFTWARE']='Bobo/%s' % __version__
+    env['SERVER_PROTOCOL']='HTTP/1.0 '
+    env['HTTP_ACCEPT']='image/gif, image/x-xbitmap, image/jpeg, */* '
+    env['SERVER_HOSTNAME']='bobo.server.host'
+    env['GATEWAY_INTERFACE']='CGI/1.1 '
+    env['SCRIPT_NAME']='Main'
+    env.update(headers)
+    return HTTPRequest(StringIO.StringIO(), env, HTTPResponse())
+
+  def _setupDumbHTTPExtraction(self):
+    pas = self.portal.acl_users
+    access_extraction_list = [q for q in pas.objectValues() \
+        if q.meta_type == 'ERP5 Dumb HTTP Extraction Plugin']
+    if len(access_extraction_list) == 0:
+      dispacher = pas.manage_addProduct['ERP5Security']
+      dispacher.addERP5DumbHTTPExtractionPlugin(self.test_id)
+      getattr(pas, self.test_id).manage_activateInterfaces(
+        ('IExtractionPlugin',))
+    elif len(access_extraction_list) == 1:
+      self.test_id = access_extraction_list[0].getId()
+    elif len(access_extraction_list) > 1:
+      raise ValueError
+    self.commit()
+
+  def test_working_authentication(self):
+    self._createPerson(self.new_id, "test")
+    request = self.do_fake_request("GET", {"HTTP_AUTHORIZATION": "Basic " + base64.b64encode("%s:test" % self.new_id)})
+    ret = ERP5DumbHTTPExtractionPlugin("default_extraction").extractCredentials(request)
+    self.assertEqual(ret, {'login': self.new_id, 'password': 'test', 'remote_host': 'bobo.remote.host', 'remote_address': '204.183.226.81 '})
