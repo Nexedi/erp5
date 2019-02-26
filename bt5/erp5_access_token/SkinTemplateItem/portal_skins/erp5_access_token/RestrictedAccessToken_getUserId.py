7d456f9897f02a574e08ddde1b522b214740e201
Jérome Perrin <jerome@nexedi.com>
2019-02-26 07:45:26 +0100

    access_token: rename token scripts
    As script return user_ids it's more logical to name them *_getUserId

--- bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/RestrictedAccessToken_getUserId.py
+++ bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/RestrictedAccessToken_getUserId.py
@@ -19,7 +19,7 @@ if access_token_document.getValidationState() == 'validated':
     # use hmac.compare_digest and not string comparison to avoid timing attacks
     if not hmac.compare_digest(access_token_document.getReference(), reference):
       return None
-    
+
     agent_document = access_token_document.getAgentValue()
     if agent_document is not None:
       result = agent_document.Person_getUserId()
