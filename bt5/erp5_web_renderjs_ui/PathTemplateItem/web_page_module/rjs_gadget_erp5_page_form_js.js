6f3a268b5e53c24dd9d2c5e8c5410eb3fb51e649
Tristan Cavelier <tristan.cavelier@nexedi.com>
2019-03-05 16:18:40 +0100

    [erp5_web_renderjs_ui] Display module actions in panel
    - Hardcodes a panel "VIEWS: List" to hide "consistency", "history" and "metadata".
    - Only show "WORKFLOWS" on module document.
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/673

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_form_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_form_js.js
@@ -49,6 +49,7 @@ and handling data send&receive.
       'gadget_html5_textarea.html',
       'gadget_html5_select.html'
     ],
+    erp5_module_regexp = /^[^\/]+_module$/,
     erp5_module_document_regexp = /^[^\/]+_module\/.+$/;
   /*jslint regexp: false*/
 
@@ -308,16 +309,23 @@ and handling data send&receive.
         })
         .push(function () {
           var jio_key = gadget.state.options.jio_key;
-          /*jslint regexp: true*/
-          if ((erp5_module_document_regexp.test(jio_key)) || (/^portal_.*\/.+$/.test(jio_key))) {
+          if (erp5_module_regexp.test(jio_key)) {
+            if (erp5_document._links) {
+              // hardcode "VIEWS: List" to hide "consistency", "history" and "metadata"
+              erp5_document._links.action_object_view =
+                [{"name": "view", "title": "List", "href": "view", "icon": null}];
+            }
+            /*jslint regexp: true*/
+          } else if (!(erp5_module_document_regexp.test(jio_key) || (/^portal_.*\/.+$/.test(jio_key)))) {
             /*jslint regexp: false*/
-            return gadget.updatePanel({
-              erp5_document: erp5_document,
-              editable: gadget.state.options.editable,
-              jio_key: jio_key,
-              view: options.view
-            });
+            return;
           }
+          return gadget.updatePanel({
+            erp5_document: erp5_document,
+            editable: gadget.state.options.editable,
+            jio_key: jio_key,
+            view: options.view
+          });
         });
     })
     /** SubmitContent should be called by the gadget which renders submit button
