eee8b714b337ddc0c6df2d405764920892c4fd3f
Yusei Tahara <yusei@nexedi.com>
2019-03-07 16:27:07 +0900

    erp5_movement_table_catalog: Update related keys format.

--- bt5/erp5_movement_table_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_explanation_source_section.sql
+++ bt5/erp5_movement_table_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_explanation_source_section.sql
@@ -1,4 +1,7 @@
-<dtml-var table_0>.uid = catalog.uid
- AND <dtml-var table_0>.explanation_uid = <dtml-var table_1>.uid
- AND <dtml-var table_1>.base_category_uid = <dtml-var "portal_categories.source_section.getUid()">
- AND <dtml-var table_1>.category_uid = <dtml-var table_2>.uid
+<dtml-var table_0>.explanation_uid = <dtml-var table_1>.uid
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_1>.base_category_uid = <dtml-var "portal_categories.source_section.getUid()">
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_1>.category_uid = <dtml-var table_2>.uid
+AND
+<dtml-var table_0>.uid = <dtml-var query_table>.uid
