8db4765d09438dc394a99ba685a3dfca667541e7
Jérome Perrin <jerome@nexedi.com>
2019-03-20 11:24:52 +0900

    accounting: reset grouping reference in a "context free" script
    Introduce a new ERP5Site_resetAccountingTransactionLineGroupingReference
    script to reset grouping reference on accounting transaction lines.
    
    This solve a problem with the way we activated on
    AccountingTransactionLine_resetGroupingReference the context of the
    line, if line was removed from OFS before activity was executed, the
    activity was discarded and grouping references on related lines was not
    reset.
    
    Also drop the unused `keep_if_valid_group` parameter, it was not making
    sense.

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransactionLine_resetGroupingReference.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransactionLine_resetGroupingReference.py
@@ -1,48 +1,37 @@
 """Resets grouping reference on this line and all related lines.
 
-If the parameter keep_if_valid_group is true, then the grouping reference
-will be kept as is if the group is still valid, ie. the total quantity
-of all accounting lines in the group is 0.
-Returns the list of ungroupped lines.
+This runs by default asynchronously, but can be call with `async=False` to
+run synchronously and returns the list of ungrouped lines. With `async=True`,
+the returned list is always empty.
 """
 
 if not context.getGroupingReference():
-  # The line grouping reference can alredy have been removed, for example when two
+  # The line grouping reference can already have been removed, for example when two
   # lines of the same transaction have the same grouping reference.
   return []
 
 portal = context.getPortalObject()
-precision = context.getResourceValue(portal_type='Currency').getQuantityPrecision()
 
-if context.AccountingTransaction_isSourceView():
-  node_uid = context.getSourceUid()
-  section_category = None
-  section = context.getSourceSectionValue()
-  if section is not None:
-    section = section.Organisation_getMappingRelatedOrganisation()
-    section_category = section.getGroup(base=1)
-  mirror_section_uid = context.getDestinationSectionUid()
-else:
-  node_uid = context.getDestinationUid()
-  section = context.getDestinationSectionValue()
-  if section is not None:
-    section = section.Organisation_getMappingRelatedOrganisation()
-    section_category = section.getGroup(base=1)
-  mirror_section_uid = context.getSourceSectionUid()
+resetGroupingReference = portal.ERP5Site_resetAccountingTransactionLineGroupingReference
+if async:
+  resetGroupingReference = portal.portal_simulation.activate(
+      after_tag='accounting_grouping_reference'
+  ).ERP5Site_resetAccountingTransactionLineGroupingReference
 
-line_list = portal.portal_simulation.getMovementHistoryList(
-                  portal_type=portal.getPortalAccountingMovementTypeList(),
-                  grouping_reference=context.getGroupingReference(),
-                  node_uid=node_uid,
-                  section_category=section_category,
-                  mirror_section_uid=mirror_section_uid)
+ungrouped_line_list = []
+for (section_value, node_uid, mirror_section_uid) in (
+  (context.getSourceSectionValue(), context.getSourceUid(), context.getDestinationSectionUid(),),
+  (context.getDestinationSectionValue(), context.getDestinationUid(), context.getSourceSectionUid(),),
+):
+  if section_value is not None:
+    section_value = section_value.Organisation_getMappingRelatedOrganisation()
+    section_category = section_value.getGroup(base=True)
+    if section_category:
+      ungrouped_line_list.extend(resetGroupingReference(
+          section_category=section_category,
+          node_uid=node_uid,
+          mirror_section_uid=mirror_section_uid,
+          grouping_reference=context.getGroupingReference()
+      ) or [])
 
-# If the group is still valid, we may want to keep it as is.
-if keep_if_valid_group and round(sum([(l.total_price or 0) for l in line_list]), precision) == 0:
-  return
-
-for line in line_list:
-  line.setGroupingReference(None)
-  line.setGroupingDate(None)
-
-return line_list
+return ungrouped_line_list
