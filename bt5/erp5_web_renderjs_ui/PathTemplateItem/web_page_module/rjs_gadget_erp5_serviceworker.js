9874245ed0afcfebe84eff2018e3297555155925
Romain Courteaud <romain@nexedi.com>
2019-03-26 09:51:32 +0000

    WIP [erp5_web_renderjs_ui] Replace appcache by a service worker
    Fetch usage can be bypassed to do not use service worker when not needed.
    
    As appcache has been dropped on Firefox, this change will improve the speed on Firefox.
    
    No change is expected on Chrome/Safari.
    
    Replace Base_getListFileFromAppcache with Base_getTranslationSourceFileList.
    
    Collect a list of files from service worker code.
    
    Use cache.add because safari does not support cache.addAll.
    
    Don't give request object itself to cache.match. Firefox's Cache Storage does not work properly when VARY contains Accept-Language. Give URL string instead, then cache.match works on both Firefox and Chrome.
    
    Stop calling skipWaiting() and clients.claim() for the new service worker.
    
    To preserve the consistency of code and data, let the new service worker wait until all tabs and windows of the old version are closed.
    
    New client can use the latest cache without waiting for the new service worker to be activated.
    
    And once client was associated with a cache, client keeps using the same cache.
    
    Fix translation script. Get service worker filename from layout property. Don't hardcode it.
    
    Fix service worker. Don't hardcode the special cache name.
    
    The name must be different per web site, else if the same service worker code is used by multiple web sites in ERP5 web site module, service worker does not work correctly.
    
    Add more comments because service worker is unstable and hard to use safely.
    
    If service worker failed to install cache, unregister this service worker explicitly.
    
    Update service worker code. Client_id is null when it is the first request, in other words if request is navigate mode. Since major web browsers already implement client_id, if client_is is null, let's use the latest cache and don't get cache_key from CACHE_MAP and erp5js_cache.
    
    service worker document may not exist
    
    duplicate files by adding language prefix
    
    experiment base tag on launcher
    
    use relative url with .. to fetch gadgets
    
    Drop base tag, because it will probably break all erp5js forks, which hardcode url strings
    
    remove all hardcoded precache requirement
    
    fetch the wallpaper from the base url
    
    remove all clientIds handling
    
    Waiting for all clients to be closed before activating the new service worker is not usable, as it will lead to user keeping the old code.
    Drop the logic for now.
    
    Fetch the list of precache url dynamically in order to decouple the worker code from the list of files.
    
    Calculate dynamically the list of files from a python script, depending on the web site configuration.
    
    Update the service worker as soon as the web site modification date changes.
    This (I hope) will simplify ERP5 upgrade handling.
    
    allow to extend the list of precache urls
    
    do not hammer zope during installation
    
    erp5_web_renderjs_ui: browser send GMT suffix
    
    * reload the page only if there is no notification
    * delay the sw installation
    * do not reload after first worker installation
    * embed the dependency list in the worker code
    * global are deleted when the worker is stopped, which make the current fetch code less interesting...
    * automatically handle service worker update and page refresh
    * ensure that user automatically uses the latest code without manually refreshing the page.
    * catch worker registration error
    * catch worker update error
    
    erp5_web_renderjs_ui: precache: missing dependencies + json export

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_serviceworker.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_serviceworker.js
@@ -1,149 +1,86 @@
 /*jslint indent: 2*/
-/*global self, caches, fetch*/
-(function (self, caches, fetch) {
+/*global self, caches, fetch, Promise, URL, location, JSON*/
+(function (self, caches, fetch, Promise, URL, location, JSON) {
   "use strict";
 
-  var CACHE_NAME = 'Thu, 12 July 2016 12:00:00 GMT',
-    // Files required to make this app work offline
-    REQUIRED_FILES = [
-      './',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.eot?v=4.2.0',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.eot?#iefix&v=4.2.0',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.woff?v=4.2.0',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.ttf?v=4.2.0',
-      'https://netdna.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.svg?v=4.2.0#fontawesomeregular',
-      'URI.js',
-      'erp5_launcher.js',
-      'gadget_erp5.css',
-      'gadget_erp5_editor_panel.html',
-      'gadget_erp5_editor_panel.js',
-      'gadget_erp5_field_checkbox.html',
-      'gadget_erp5_field_checkbox.js',
-      'gadget_erp5_field_datetime.html',
-      'gadget_erp5_field_datetime.js',
-      'gadget_erp5_field_email.html',
-      'gadget_erp5_field_email.js',
-      'gadget_erp5_field_file.html',
-      'gadget_erp5_field_file.js',
-      'gadget_erp5_field_float.html',
-      'gadget_erp5_field_float.js',
-      'gadget_erp5_field_gadget.html',
-      'gadget_erp5_field_gadget.js',
-      'gadget_erp5_field_image.html',
-      'gadget_erp5_field_image.js',
-      'gadget_erp5_field_integer.html',
-      'gadget_erp5_field_integer.js',
-      'gadget_erp5_field_list.html',
-      'gadget_erp5_field_list.js',
-      'gadget_erp5_field_listbox.html',
-      'gadget_erp5_field_listbox.js',
-      'gadget_erp5_field_multicheckbox.html',
-      'gadget_erp5_field_multicheckbox.js',
-      'gadget_erp5_field_multilist.html',
-      'gadget_erp5_field_multilist.js',
-      'gadget_erp5_field_multirelationstring.html',
-      'gadget_erp5_field_multirelationstring.js',
-      'gadget_erp5_field_radio.html',
-      'gadget_erp5_field_radio.js',
-      'gadget_erp5_field_readonly.html',
-      'gadget_erp5_field_readonly.js',
-      'gadget_erp5_field_relationstring.html',
-      'gadget_erp5_field_relationstring.js',
-      'gadget_erp5_field_string.html',
-      'gadget_erp5_field_string.js',
-      'gadget_erp5_field_password.html',
-      'gadget_erp5_field_password.js',
-      'gadget_erp5_field_textarea.html',
-      'gadget_erp5_field_textarea.js',
-      'gadget_erp5_form.html',
-      'gadget_erp5_form.js',
-      'gadget_erp5_header.html',
-      'gadget_erp5_header.js',
-      'gadget_erp5_jio.html',
-      'gadget_erp5_jio.js',
-      'gadget_erp5_page_action.html',
-      'gadget_erp5_page_action.js',
-      'gadget_erp5_page_form.html',
-      'gadget_erp5_page_form.js',
-      'gadget_erp5_page_front.html',
-      'gadget_erp5_page_front.js',
-      'gadget_erp5_page_history.html',
-      'gadget_erp5_page_history.js',
-      'gadget_erp5_page_jump.html',
-      'gadget_erp5_page_jump.js',
-      'gadget_erp5_page_logout.html',
-      'gadget_erp5_page_logout.js',
-      'gadget_erp5_page_preference.html',
-      'gadget_erp5_page_preference.js',
-      'gadget_erp5_page_relation_search.html',
-      'gadget_erp5_page_relation_search.js',
-      'gadget_erp5_page_search.html',
-      'gadget_erp5_page_search.js',
-      'gadget_erp5_page_tab.html',
-      'gadget_erp5_page_tab.js',
-      'gadget_erp5_page_worklist.html',
-      'gadget_erp5_page_worklist.js',
-      'gadget_erp5_panel.html',
-      'gadget_erp5_panel.js',
-      'gadget_erp5_pt_form_dialog.html',
-      'gadget_erp5_pt_form_dialog.js',
-      'gadget_erp5_pt_form_list.html',
-      'gadget_erp5_pt_form_list.js',
-      'gadget_erp5_pt_form_view.html',
-      'gadget_erp5_pt_form_view.js',
-      'gadget_erp5_pt_form_view_editable.html',
-      'gadget_erp5_pt_form_view_editable.js',
-      'gadget_erp5_pt_report_view.html',
-      'gadget_erp5_pt_report_view.js',
-      'gadget_erp5_router.html',
-      'gadget_erp5_router.js',
-      'gadget_erp5_relation_input.html',
-      'gadget_erp5_relation_input.js',
-      'gadget_erp5_search_editor.html',
-      'gadget_erp5_search_editor.js',
-      'gadget_erp5_searchfield.html',
-      'gadget_erp5_searchfield.js',
-      'gadget_erp5_sort_editor.html',
-      'gadget_erp5_sort_editor.js',
-      'gadget_global.js',
-      'gadget_erp5_global.js',
-      'gadget_jio.html',
-      'gadget_jio.js',
-      'gadget_translation.html',
-      'gadget_translation.js',
-      'gadget_translation_data.js',
-      'handlebars.js',
-      'i18next.js',
-      'jiodev.js',
-      'renderjs.js',
-      'rsvp.js'
-    ];
+  var prefix = location.toString() + '_',
+    CACHE_NAME = prefix + '${modification_date}',
+    REQUIRED_FILES = JSON.parse('${required_url_list}'),
+    required_url_list = [],
+    i,
+    len = REQUIRED_FILES.length;
+
+  for (i = 0; i < len; i += 1) {
+    required_url_list.push(
+      new URL(REQUIRED_FILES[i], location.toString()).toString()
+    );
+  }
 
   self.addEventListener('install', function (event) {
     // Perform install step:  loading each required file into cache
     event.waitUntil(
       caches.open(CACHE_NAME)
         .then(function (cache) {
-          // Add all offline dependencies to the cache
-          return cache.addAll(REQUIRED_FILES);
+          var promise = Promise.resolve();
+
+          function append(url_to_cache) {
+            promise = promise
+              .then(function () {
+                // Use cache.add because safari does not support cache.addAll.
+                return cache.add(url_to_cache);
+              });
+          }
+
+          len = required_url_list.length;
+          for (i = 0; i < len; i += 1) {
+            // Add all offline dependencies to the cache
+            // One by one, to not hammer zopes
+            append(required_url_list[i]);
+          }
+          return promise;
         })
         .then(function () {
-          // At this point everything has been cached
+          // When user accesses ERP5JS web site first time, service worker is
+          // installed but it is not activated yet, service worker is activated
+          // when the page is refreshed or when a new tab opens the site again.
+          // If user does not refresh the page and continue to use the site,
+          // user can't use cache, so everything becomes slow. We must avoid this
+          // situation.
+          // So, we want to activate the new service worker immediately if it was
+          // the first one.
           return self.skipWaiting();
+        })
+        .catch(function (error) {
+          // Since we do not allow to override existing cache, if cache installation
+          // failed, we need to delete the cache completely.
+          caches.delete(CACHE_NAME);
+          throw error;
         })
     );
   });
 
   self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      caches.match(event.request)
+    var url = new URL(event.request.url);
+    url.hash = '';
+    if ((event.request.method !== 'GET') ||
+        (required_url_list.indexOf(url.toString()) === -1)) {
+      // Try not to use the untrustable fetch function
+      // It can only be skip synchronously
+      return;
+    }
+    return event.respondWith(
+      caches.open(CACHE_NAME)
+        .then(function (cache) {
+          // Don't give request object itself. Firefox's Cache Storage
+          // does not work properly when VARY contains Accept-Language.
+          // Give URL string instead, then cache.match works on both Firefox and Chrome.
+          return cache.match(event.request.url);
+        })
         .then(function (response) {
           // Cache hit - return the response from the cached version
           if (response) {
             return response;
           }
-
           // Not in cache - return the result from the live server
           // `fetch` is essentially a "fallback"
           return fetch(event.request);
@@ -168,7 +105,8 @@
               .filter(function (key) {
                 // Filter by keys that don't start with the latest version prefix.
                 // return !key.startsWith(version);
-                return key !== CACHE_NAME;
+                return ((key !== CACHE_NAME) &&
+                        key.startsWith(prefix));
               })
               .map(function (key) {
                 /* Return a promise that's fulfilled
@@ -179,11 +117,21 @@
           );
         })
         .then(function () {
-          self.clients.claim();
+          return self.clients.claim();
+        })
+        .then(function () {
+          return self.clients.matchAll();
+        })
+        .then(function (client_list) {
+          // Notify all clients that they can reload the page
+          var j,
+            client_len = client_list.length;
+          for (j = 0; j < client_len; j += 1) {
+            client_list[j].postMessage('claim');
+          }
+
         })
     );
   });
 
-
-
-}(self, caches, fetch));
+}(self, caches, fetch, Promise, URL, location, JSON));
