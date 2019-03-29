feca5a9688c37c651e84aab7b691b56867672de4
Rafael Monnerat <rafael@nexedi.com>
2019-03-29 15:21:58 +0000

    erp5_administration: Accept some ignore list when deump Installed Business Template List
    Ignore list is usefull when you have old business templates in production (used to include data for example) and test business templates on test environment (ie;: erp5_test_ui_core). This allow us control the dump to assert changes easily.

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpInstalledBusinessTemplateList.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpInstalledBusinessTemplateList.py
@@ -1,6 +1,7 @@
 for business_template in sorted(context.getPortalObject().portal_templates.contentValues(portal_type='Business Template'),
                   key=lambda x:x.getTitle()):
-  if business_template.getInstallationState() == 'installed':
+  if business_template.getInstallationState() == 'installed' and \
+    business_template.getTitle() not in ignore_business_template_list:
     print business_template.getTitle()
 
 return printed
