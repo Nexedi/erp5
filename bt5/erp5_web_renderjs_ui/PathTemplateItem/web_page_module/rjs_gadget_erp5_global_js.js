4740008a882780b2c57043b5c89e0e95bdee549f
Romain Courteaud <romain@nexedi.com>
2019-04-02 11:34:32 +0200

    [erp5_core/js] Move functions unrelated from ERP5 itself

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_global_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_global_js.js
@@ -1,6 +1,6 @@
-/*global window, RSVP, Array, isNaN */
+/*global window, RSVP */
 /*jslint indent: 2, maxerr: 3, nomen: true, unparam: true */
-(function (window, RSVP, Array, isNaN) {
+(function (window, RSVP) {
   "use strict";
 
   window.calculatePageTitle = function (gadget, erp5_document) {
@@ -15,62 +15,4 @@
       });
   };
 
-  /** Return true if the value truly represents an empty value.
-
-  Calling isEmpty(x) is more robust than expression !x.
-  */
-  function isEmpty(value) {
-    return (value === undefined ||
-            value === null ||
-            value.length === 0 ||
-            (typeof value === "number" && isNaN(value)));
-  }
-  window.isEmpty = isEmpty;
-
-  /** Make sure that returned object is an Array instance.
-  */
-  function ensureArray(obj) {
-    if (Array.isArray(obj)) {return obj; }
-    if (isEmpty(obj)) {return []; }
-    return [obj];
-  }
-  window.ensureArray = ensureArray;
-
-  /** Return first non-empty variable or the last one.
-
-  Calling getNonEmpy(a, b, "") is more robust way of writing a || b || "".
-  Variables coercing to false (e.g 0) do not get skipped anymore.
-  */
-  function getFirstNonEmpty() {
-    var i;
-    if (arguments.length === 0) {
-      return null;
-    }
-    for (i = 0; i < arguments.length; i++) {
-      if (!isEmpty(arguments[i])) {
-        return arguments[i];
-      }
-    }
-    if (arguments.length === 1) {
-      return arguments[0];
-    }
-    return arguments[arguments.length - 1];
-  }
-  window.getFirstNonEmpty = getFirstNonEmpty;
-
-  /** Convert anything to boolean value correctly (even "false" will be false)*/
-  function asBoolean(obj) {
-    if (typeof obj === "boolean") {
-      return obj;
-    }
-    if (typeof obj === "string") {
-      return obj.toLowerCase() === "true" || obj === "1";
-    }
-    if (typeof obj === "number") {
-      return obj !== 0;
-    }
-    return Boolean(obj);
-  }
-  window.asBoolean = asBoolean;
-
-}(window, RSVP, Array, isNaN));
+}(window, RSVP));
