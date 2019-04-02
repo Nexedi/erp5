dda2ab64ccb4a5e6a2d30d8d0b32d6204cdadc31
Romain Courteaud <romain@nexedi.com>
2019-04-02 07:24:42 +0000

    [erp5_core / js] Move function unrelated from ERP5 itself

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_global_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_global_js.js
@@ -1,6 +1,8 @@
-/*global window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query */
+/*global window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query,
+         ensureArray */
 /*jslint indent: 2, maxerr: 3, nomen: true, unparam: true, continue: true */
-(function (window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query) {
+(function (window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query,
+           ensureArray) {
   "use strict";
 
   window.calculatePageTitle = function (gadget, erp5_document) {
@@ -14,64 +16,6 @@
         return portal_type + ': ' + title;
       });
   };
-
-  /** Return true if the value truly represents an empty value.
-
-  Calling isEmpty(x) is more robust than expression !x.
-  */
-  function isEmpty(value) {
-    return (value === undefined ||
-            value === null ||
-            value.length === 0 ||
-            (typeof value === "number" && isNaN(value)));
-  }
-  window.isEmpty = isEmpty;
-
-  /** Make sure that returned object is an Array instance.
-  */
-  function ensureArray(obj) {
-    if (Array.isArray(obj)) {return obj; }
-    if (isEmpty(obj)) {return []; }
-    return [obj];
-  }
-  window.ensureArray = ensureArray;
-
-  /** Return first non-empty variable or the last one.
-
-  Calling getNonEmpy(a, b, "") is more robust way of writing a || b || "".
-  Variables coercing to false (e.g 0) do not get skipped anymore.
-  */
-  function getFirstNonEmpty(first_argument) {
-    var i;
-    if (arguments.length === 0) {
-      return null;
-    }
-    for (i = 0; i < arguments.length; i += 1) {
-      if (!isEmpty(arguments[i])) {
-        return arguments[i];
-      }
-    }
-    if (arguments.length === 1) {
-      return first_argument;
-    }
-    return arguments[arguments.length - 1];
-  }
-  window.getFirstNonEmpty = getFirstNonEmpty;
-
-  /** Convert anything to boolean value correctly (even "false" will be false)*/
-  function asBoolean(obj) {
-    if (typeof obj === "boolean") {
-      return obj;
-    }
-    if (typeof obj === "string") {
-      return obj.toLowerCase() === "true" || obj === "1";
-    }
-    if (typeof obj === "number") {
-      return obj !== 0;
-    }
-    return Boolean(obj);
-  }
-  window.asBoolean = asBoolean;
 
   ///////////////////////////////
   // Handle listbox action list
@@ -227,4 +171,4 @@
   window.declareGadgetClassCanHandleListboxClipboardAction =
     declareGadgetClassCanHandleListboxClipboardAction;
 
-}(window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query));
+}(window, RSVP, Array, isNaN, SimpleQuery, ComplexQuery, Query, ensureArray));
