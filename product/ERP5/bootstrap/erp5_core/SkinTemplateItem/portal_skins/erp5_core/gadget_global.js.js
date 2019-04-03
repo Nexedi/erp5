e1ab90985890d1757fdad8f348c409f8ca3bf587
Romain Courteaud <romain@nexedi.com>
2019-04-03 17:27:50 +0200

    [erp5_core/rjs] Move functions unrelated to ERP5JS

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/gadget_global.js.js
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/gadget_global.js.js
@@ -3,6 +3,65 @@
 (function (window, RSVP, FileReader) {
   "use strict";
 
+
+  /** Return true if the value truly represents an empty value.
+
+  Calling isEmpty(x) is more robust than expression !x.
+  */
+  function isEmpty(value) {
+    return (value === undefined ||
+            value === null ||
+            value.length === 0 ||
+            (typeof value === "number" && isNaN(value)));
+  }
+  window.isEmpty = isEmpty;
+
+  /** Make sure that returned object is an Array instance.
+  */
+  function ensureArray(obj) {
+    if (Array.isArray(obj)) {return obj; }
+    if (isEmpty(obj)) {return []; }
+    return [obj];
+  }
+  window.ensureArray = ensureArray;
+
+  /** Return first non-empty variable or the last one.
+
+  Calling getNonEmpy(a, b, "") is more robust way of writing a || b || "".
+  Variables coercing to false (e.g 0) do not get skipped anymore.
+  */
+  function getFirstNonEmpty(first_argument) {
+    var i;
+    if (arguments.length === 0) {
+      return null;
+    }
+    for (i = 0; i < arguments.length; i += 1) {
+      if (!isEmpty(arguments[i])) {
+        return arguments[i];
+      }
+    }
+    if (arguments.length === 1) {
+      return first_argument;
+    }
+    return arguments[arguments.length - 1];
+  }
+  window.getFirstNonEmpty = getFirstNonEmpty;
+
+  /** Convert anything to boolean value correctly (even "false" will be false)*/
+  function asBoolean(obj) {
+    if (typeof obj === "boolean") {
+      return obj;
+    }
+    if (typeof obj === "string") {
+      return obj.toLowerCase() === "true" || obj === "1";
+    }
+    if (typeof obj === "number") {
+      return obj !== 0;
+    }
+    return Boolean(obj);
+  }
+  window.asBoolean = asBoolean;
+
   window.loopEventListener = function (target, type, useCapture, callback,
                                        prevent_default) {
     //////////////////////////
