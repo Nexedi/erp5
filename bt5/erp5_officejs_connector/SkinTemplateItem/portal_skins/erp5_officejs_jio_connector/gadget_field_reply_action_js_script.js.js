b20eb74a298a55d3db9d462e77d232875d599458
Roque Porchetto <roque.porchetto@nexedi.com>
2019-04-22 18:39:38 +0000

    erp5_officejs_connector: gadget for reply post action

---
+++ bt5/erp5_officejs_connector/SkinTemplateItem/portal_skins/erp5_officejs_jio_connector/gadget_field_reply_action_js_script.js.js
@@ -0,0 +1,89 @@
+/*global window, rJS, RSVP */
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS, RSVP) {
+  "use strict";
+
+  var gadget_utils;
+
+  rJS(window)
+
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("jio_get", "jio_get")
+    .declareAcquiredMethod("jio_put", "jio_put")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod('getUrlParameter', 'getUrlParameter')
+    .declareAcquiredMethod("notifySubmitted", 'notifySubmitted')
+    .declareAcquiredMethod("notifySubmitting", "notifySubmitting")
+    .declareAcquiredMethod("redirect", "redirect")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareMethod("render", function (options) {
+      console.log("GADGET FIELD RENDER METHOD!!!");
+      return;
+    })
+
+    .declareMethod("handleRender", function (gadget, gadget_utils, options, action_reference, parent_portal_type, form_definition) {
+      console.log("GADGET FIELD handleRender METHOD!!!");
+      var child_gadget_url = 'gadget_erp5_pt_form_view_editable.html';\
+      return gadget.jio_get(options.jio_key)
+      .push(function (result) {
+        parent_document = result;
+        var title = parent_document.title;
+        if (!title.startsWith('Re: ')) { title = 'Re: ' + parent_document.title; }
+        return gadget.changeState({
+          doc: {title: title},
+          submit_code: submit_code,
+          parent_document: parent_document,
+          child_gadget_url: child_gadget_url,
+          form_definition: form_definition,
+          view: action_reference,
+          editable: true,
+          has_more_views: false,
+          has_more_actions: false,
+          is_form_list: false
+        });
+      });
+    })
+
+    .declareMethod("handleSubmit", function (gadget, gadget_utils, jio_key, content_dict) {
+      console.log("GADGET FIELD handleSubmit METHOD!!!");
+      var document = {
+        my_title: gadget.state.doc.title,
+        portal_type: gadget.state.parent_document.portal_type,
+        parent_relative_url: gadget.state.parent_document.parent_relative_url,
+        my_source_reference: gadget.state.parent_document.source_reference
+      }, property;
+      for (property in content_dict) {
+        if (content_dict.hasOwnProperty(property)) {
+          document['my_' + property] = content_dict[property];
+        }
+      }
+      return gadget_utils.createDocument(document)
+      .push(function (id) {
+        jio_key = id;
+        return gadget.notifySubmitting();
+      })
+      .push(function () {
+        return gadget.notifySubmitted({message: 'Data Updated', status: 'success'});
+      })
+      .push(function () {
+        return gadget.redirect({
+          command: 'display',
+          options: {
+            jio_key: jio_key,
+            editable: true
+          }
+        });
+      });
+    });
+
+    /*.onStateChange(function () {
+      return gadget_utils.renderGadget(this);
+    });*/
+
+}(window, rJS, RSVP));
