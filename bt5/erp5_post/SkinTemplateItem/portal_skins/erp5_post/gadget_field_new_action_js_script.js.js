d3c8d6378888c668b2c0234ae1a12ca371c1450e
Roque Porchetto <roque.porchetto@nexedi.com>
2019-04-30 16:48:32 +0000

    erp5_post: new post action uses dialog form

--- bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/gadget_field_new_action_js_script.js.js
+++ bt5/erp5_post/SkinTemplateItem/portal_skins/erp5_post/gadget_field_new_action_js_script.js.js
@@ -41,23 +41,17 @@
 
     .declareMethod("render", function (parent_options, action_reference, form_definition) {
       var gadget = this;
-      return gadget.createDocument(parent_options)
-        .push(function (jio_key) {
-          return gadget.jio_get(jio_key)
-          .push(function (new_document) {
-            return gadget.changeState({
-              jio_key: jio_key,
-              doc: new_document,
-              child_gadget_url: 'gadget_erp5_pt_form_view_editable.html',
-              form_type: 'page',
-              form_definition: form_definition,
-              view: action_reference,
-              editable: true,
-              has_more_views: false,
-              has_more_actions: true
-            });
-          });
-        });
+      return gadget.changeState({
+        doc: {title: "Untitled Document"},
+        parent_options: parent_options,
+        child_gadget_url: 'gadget_erp5_pt_form_dialog.html',
+        form_type: 'dialog',
+        form_definition: form_definition,
+        view: action_reference,
+        editable: true,
+        has_more_views: false,
+        has_more_actions: true
+      });
     })
 
     .onStateChange(function () {
@@ -79,32 +73,35 @@
       var gadget = this,
         jio_key = options[0],
         //target_url = options[1],
-        content_dict = options[2];
-      return gadget.notifySubmitting()
-        .push(function () {
-          return gadget.jio_get(jio_key);
-        })
-        .push(function (document) {
-          var property;
-          for (property in content_dict) {
-            if (content_dict.hasOwnProperty(property)) {
-              document[property] = content_dict[property];
-            }
+        content_dict = options[2],
+        document = {
+        portal_type: gadget.state.parent_options.portal_type,
+        parent_relative_url: gadget.state.parent_options.parent_relative_url,
+        my_source_reference: gadget.state.parent_options.my_source_reference
+      }, property;
+      delete content_dict.dialog_method;
+      for (property in content_dict) {
+        if (content_dict.hasOwnProperty(property)) {
+          document['my_' + property] = content_dict[property];
+        }
+      }
+      return this.createDocument(document)
+      .push(function (id) {
+        jio_key = id;
+        return gadget.notifySubmitting();
+      })
+      .push(function () {
+        return gadget.notifySubmitted({message: 'Data Updated', status: 'success'});
+      })
+      .push(function () {
+        return gadget.redirect({
+          command: 'display',
+          options: {
+            jio_key: jio_key,
+            editable: true
           }
-          return gadget.jio_put(jio_key, document);
-        })
-        .push(function () {
-          return gadget.notifySubmitted({message: 'Data Updated', status: 'success'});
-        })
-        .push(function () {
-          return gadget.redirect({
-            command: 'display',
-            options: {
-              jio_key: jio_key,
-              editable: true
-            }
-          });
         });
+      });
     });
 
 }(window, rJS, RSVP));
