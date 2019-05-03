149eb4a278df64cd2e8a5220b36b34062a5c812a
Romain Courteaud <romain@nexedi.com>
2019-05-03 09:47:36 +0000

    [erp5_hal_json_style] More tests

--- bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalRestrictedJsonStyle.py
+++ bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalRestrictedJsonStyle.py
@@ -15,3 +15,46 @@ class TestHalRestricted(ERP5HALJSONStyleSkinsMixin):
     self.assertEquals(fake_request.RESPONSE.getHeader('WWW-Authenticate'),
       'X-Delegate uri="%s/connection/login_form{?came_from}"' % self.portal.web_site_module.hateoas.absolute_url()
     )
+
+  @simulate('Base_getRequestUrl', '*args, **kwargs',
+      'return "http://example.org/bar"')
+  @simulate('Base_getRequestHeader', '*args, **kwargs',
+            'return "application/hal+json"')
+  @changeSkin('HalRestricted')
+  def test_mode_traverse(self):
+    document_relative_url = self._makeDocument().getRelativeUrl()
+    fake_request = do_fake_request("GET")
+    self.logout()
+    result = self.portal.web_site_module.hateoas.ERP5Document_getHateoas(REQUEST=fake_request, mode="traverse", relative_url=document_relative_url)
+    self.assertEquals(fake_request.RESPONSE.status, 401)
+    self.assertEquals(fake_request.RESPONSE.getHeader('WWW-Authenticate'),
+      'X-Delegate uri="%s/connection/login_form{?came_from}"' % self.portal.web_site_module.hateoas.absolute_url()
+    )
+
+  @simulate('Base_getRequestUrl', '*args, **kwargs',
+      'return "http://example.org/bar"')
+  @simulate('Base_getRequestHeader', '*args, **kwargs',
+            'return "application/hal+json"')
+  @changeSkin('HalRestricted')
+  def test_mode_search(self):
+    fake_request = do_fake_request("GET")
+    self.logout()
+    result = self.portal.web_site_module.hateoas.ERP5Document_getHateoas(REQUEST=fake_request, mode="search")
+    self.assertEquals(fake_request.RESPONSE.status, 401)
+    self.assertEquals(fake_request.RESPONSE.getHeader('WWW-Authenticate'),
+      'X-Delegate uri="%s/connection/login_form{?came_from}"' % self.portal.web_site_module.hateoas.absolute_url()
+    )
+
+  @simulate('Base_getRequestUrl', '*args, **kwargs',
+      'return "http://example.org/bar"')
+  @simulate('Base_getRequestHeader', '*args, **kwargs',
+            'return "application/hal+json"')
+  @changeSkin('HalRestricted')
+  def test_mode_worklist(self):
+    fake_request = do_fake_request("GET")
+    self.logout()
+    result = self.portal.web_site_module.hateoas.ERP5Document_getHateoas(REQUEST=fake_request, mode="worklist")
+    self.assertEquals(fake_request.RESPONSE.status, 401)
+    self.assertEquals(fake_request.RESPONSE.getHeader('WWW-Authenticate'),
+      'X-Delegate uri="%s/connection/login_form{?came_from}"' % self.portal.web_site_module.hateoas.absolute_url()
+    )
