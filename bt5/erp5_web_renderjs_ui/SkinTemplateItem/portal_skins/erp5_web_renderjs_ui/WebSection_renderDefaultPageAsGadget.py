e96e388e94846ac3679523ed6ce850865788b919
Romain Courteaud <romain@nexedi.com>
2019-05-06 13:10:25 +0000

    [erp5_web_renderjs_ui] Allow to configure a global wallpaper

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSection_renderDefaultPageAsGadget.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSection_renderDefaultPageAsGadget.py
@@ -58,11 +58,37 @@ if configuration_manifest_url is None:
   mapping_dict["manifest_attribute"] = ''
 else:
   mapping_dict["manifest_attribute"] = 'manifest="%s"' % configuration_manifest_url
-  
+
 configuration_webapp_manifest_url = web_section.getLayoutProperty("configuration_webapp_manifest_url", default=None)
 if configuration_webapp_manifest_url is None:
   mapping_dict["webapp_manifest_full_link_tag"] = ''
 else:
   mapping_dict["webapp_manifest_full_link_tag"] = '<link rel="manifest" href="' + configuration_webapp_manifest_url + '">'
+
+# Wallpaper
+# It can not be done in Javascript, due to CSP protection.
+# data- attribute can not be accessed outside the content property
+# So, it is easier to insert a data URL for this functionnality
+wallpaper_url = web_section.getLayoutProperty("configuration_wallpaper_url", default=None)
+if wallpaper_url is None:
+  mapping_dict["extra_css_full_link_tag"] = ''
+else:
+  from base64 import urlsafe_b64encode
+  mapping_dict["extra_css_full_link_tag"] = '<link rel="stylesheet" href="data:text/css;base64,%s">' % urlsafe_b64encode("""
+  html::after {
+    content: "";
+    opacity: 0.1;
+    top: 0;
+    left: 0;
+    width : 100%%;
+    height: 100%%;
+    position: fixed;
+    z-index: -1;
+    background-size: cover;
+    background-position: 0 0;
+    background-attachment: fixed;
+    background-image: url("%s");
+  }
+  """ % wallpaper_url);
 
 return view_as_web_method(mapping_dict=mapping_dict)
