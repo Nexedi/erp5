4c7556babad2b371adc4f9bc87c7168df97fb99d
Roque Porchetto <roque.porchetto@nexedi.com>
2019-05-06 17:07:18 +0000

    erp5_officejs: post list form rendered using local storage form definition

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_erp5_page_post_list_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_erp5_page_post_list_js.js
@@ -17,63 +17,20 @@
     // declared methods
     /////////////////////////////////////////////////////////////////
 
-    //HARDCODED UNTIL form_def is stored in local storage
-    .declareMethod("getFormDefinition", function (gadget) {
-      var fake_thread_uid = "thread-" + ("0000" + ((Math.random() * Math.pow(36, 4)) | 0).toString(36)).slice(-4),
+    .declareMethod("render", function (options) {
+      var gadget = this,
+        default_view = "jio_view",
+        common_utils_gadget_url = "gadget_officejs_common_utils.html",
+        child_gadget_url = 'gadget_erp5_pt_form_list.html',
+        //HARDCODED NEW ACTION INFORMATION - TODO: get like in action_offline gadget (refactor/reuse code)
+        fake_thread_uid = "thread-" + ("0000" + ((Math.random() * Math.pow(36, 4)) | 0).toString(36)).slice(-4),
         action_info = {
           page: "handle_action",
           action: "new",
           portal_type: "HTML Post",
           parent_portal_type: "Post Module",
           my_source_reference: fake_thread_uid
-        },
-        form_definition = {
-          _debug: "traverse",
-          pt: "form_view",
-          title: "Post",
-          group_list: [[
-            "bottom",
-            [["my_listbox"]]
-          ]],
-          //this fields_raw_properties is totally made up, but somewhere in the definition there must be
-          //information about the fields. So, foreach field: key->info
-          fields_raw_properties: {
-            "my_listbox": {
-              "type": "ListBox",
-              "key": "field_listbox", // or my_listbox ??
-              "values": {
-                "column_list": [['title', 'Title'], ['modification_date', 'Modification Date']],
-                "show_anchor": 0,
-                "default_params": {},
-                "editable": 1,
-                "editable_column_list": [],
-                "lines": 30,
-                "list_method": "portal_catalog",
-                // is this correct? the query should come from the form definition, right?
-                "query": "urn:jio:allDocs?query=portal_type%3A%22HTML Post%22",
-                "portal_type": [],
-                "search_column_list": [['title', 'Title'], ['modification_date', 'Modification Date']],
-                "sort_column_list": [['title', 'Title'], ['modification_date', 'Modification Date']],
-                "sort": [['modification_date', 'descending']],
-                "title": "Posts"
-              },
-              "tales": {},
-              "overrides": {},
-              "message_values": {}
-            }
-          },
-          action: "Base_edit",
-          update_action: "",
-          _links: { "type": { name: "" }, "action_object_new_content_action": action_info }
         };
-      return form_definition;
-    })
-
-    .declareMethod("render", function (options) {
-      var gadget = this,
-        default_view = "jio_view",
-        common_utils_gadget_url = "gadget_officejs_common_utils.html",
-        child_gadget_url = 'gadget_erp5_pt_form_list.html';
       return RSVP.Queue()
         .push(function () {
           return RSVP.all([
@@ -82,10 +39,11 @@
           ]);
         })
         .push(function (result) {
-            return gadget.getFormDefinition();
-            //return result[1].getFormDefinition(result[0], default_view);
+            return result[1].getFormDefinition(result[0], default_view);
           })
           .push(function (form_definition) {
+            //HARDCODED ACTION INFORMATION
+            form_definition._links.action_object_new_content_action = action_info;
             return gadget.changeState({
               jio_key: options.jio_key,
               child_gadget_url: child_gadget_url,
