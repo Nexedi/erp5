59180bd2c8b8fa31a8c150a1ae73330038addc0b
Rafael Monnerat <rafael@nexedi.com>
2019-05-10 19:50:05 +0000

    erp5_open_trade: Optimise calls of updateSimulation to not overcall the API
      This optimisation aims reduce the potential activity leak when over launching accross multiple activities/transactions the updateSimulation.
      SubscriptionItem: Add a method to call updateSimulation from sqldict activities
        updateExpandableRootSimulation allow launch activities in a safer matter and have lower calls.

--- bt5/erp5_open_trade/SkinTemplateItem/portal_skins/erp5_open_trade/OpenOrder_updateSimulation.py
+++ bt5/erp5_open_trade/SkinTemplateItem/portal_skins/erp5_open_trade/OpenOrder_updateSimulation.py
@@ -1,9 +1,27 @@
 subscription_item_set = set()
+portal = context.getPortalObject()
+
+def hasUpdateSimulationActivity(item):
+  for activity in portal.portal_activities.getMessageList(
+     method_id="_updateSimulation", path="/erp5/" + item.getRelativeUrl()):
+    if activity.kw == {"expand_root" : 1}:
+      return True
+
+  return False
 
 for open_order_line in context.objectValues():
   for ob in [open_order_line] + open_order_line.getCellValueList():
     for item in ob.getAggregateValueList():
-       if getattr(item.aq_explicit, 'updateSimulation', None) is not None and \
+      if getattr(item.aq_explicit, 'updateSimulation', None) is not None and \
           item not in subscription_item_set:
-         subscription_item_set.add(item)
-         item.updateSimulation(expand_root=1)
+        subscription_item_set.add(item)
+
+        # Optmise for not launch an activity when some activity
+        # is there already pending for the same purpose.
+        if not hasUpdateSimulationActivity(item):
+          if getattr(item.aq_explicit, 'updateExpandableRootSimulation', None):
+            # If this method is implemented use activate to only execute it
+            # once.
+            item.activate().updateExpandableRootSimulation()
+          else:
+            item.updateSimulation(expand_root=1)
