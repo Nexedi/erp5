564cca957ff2a3ba0c02dbbda07278ebd414eb78
Vincent Pelletier <vincent@nexedi.com>
2019-05-14 15:54:09 +0900

    IdTool: Add poisoning support.
    Allows for safe migration to another id generator, by preventing silent
    success of any piece of code still using the former generator, allowing
    its detection and resolution before duplicate ids are emitted.
    As a consequence, also allows on-demand and partial migration of id
    generators.

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/IdTool_zSetLastId.sql
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/IdTool_zSetLastId.sql
@@ -1,7 +1,7 @@
 BEGIN
 <dtml-var sql_delimiter>
 INSERT INTO portal_ids (`id_group`, `last_id`)
- VALUES (<dtml-sqlvar id_group type="string">, <dtml-sqlvar last_id type="int">)
- ON DUPLICATE KEY UPDATE `last_id` = <dtml-sqlvar last_id type="int">
+ VALUES (<dtml-sqlvar id_group type="string">, <dtml-sqlvar last_id type="int" optional>)
+ ON DUPLICATE KEY UPDATE `last_id` = <dtml-sqlvar last_id type="int" optional>
 <dtml-var sql_delimiter>
 COMMIT
