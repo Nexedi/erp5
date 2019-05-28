9fe2cbca5928d33fbeabb35e8c9917b6b657de20
Romain Courteaud <romain@nexedi.com>
2019-05-28 13:07:00 +0000

    [erp5_core] Make live test gadget reusable
    Remove all globals to allow multiple live tests running on the same page.
    Stop reading the test output if the browser tab doesn't have the focus.
    Stop hardcoding the URL in the javascript.
    Stop hardcoding other fields path in the javascript (+ embed the textarea in the gadget).
    
    Propagate all parameters via formulator.
    It will allow to use this gadget from another context document.

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ComponentTool_refreshLiveTestDialog.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ComponentTool_refreshLiveTestDialog.py
@@ -1,6 +1,1 @@
-request = context.REQUEST
-if request.get('form', None) is not None:
-  context.REQUEST['form']['field_your_text_output']=""
-  context.REQUEST['form']['text_output']=""
-return context.ComponentTool_viewLiveTestDialog(
-  text_output="")
+return context.Base_renderForm('ComponentTool_viewLiveTestDialog')
