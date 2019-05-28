8cace320bf92a81542237b20bc77f21237ec8b10
Romain Courteaud <romain@nexedi.com>
2019-05-28 15:25:23 +0000

    [erp5_officejs_appstore_base] Force refreshing the manifest
    Prevent CDN cache inconsistency

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/WebSection_renderOfficeJSRedirectAppCache.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/WebSection_renderOfficeJSRedirectAppCache.py
@@ -4,8 +4,9 @@ if response is None:
   response = REQUEST.RESPONSE
 
 web_section = context
+modification_date = max(web_section.Base_getWebDocumentDrivenModificationDate(), DateTime().earliestTime()  + (DateTime().hour() /24.0)).rfc822()
 
-if REQUEST.getHeader('If-Modified-Since', '') == web_section.getModificationDate().rfc822():
+if REQUEST.getHeader('If-Modified-Since', '') == modification_date:
   response.setStatus(304)
   return ""
 
@@ -13,4 +14,4 @@ response.setHeader('Content-Type', 'text/cache-manifest')
 response.setHeader('Cache-Control', 'max-age=0, public, must-revalidate')
 
 return """CACHE MANIFEST
-# %s + hash""" % context.getLayoutProperty("configuration_latest_version", default="development")
+# %s / %s""" % (context.getLayoutProperty("configuration_latest_version", default="development"), modification_date)
