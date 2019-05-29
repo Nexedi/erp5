04718353f2b5761ad4e477ea9151d2b039970409
Romain Courteaud <romain@nexedi.com>
2019-05-30 01:03:33 +0900

    Revert "[erp5_web] Desactivate the portal_skin URL parameter on Static Web Section"
    This reverts commit 7cc3d26de5718dcef8290b871b074e14a41f65b8.

--- bt5/erp5_web_ui_test/TestTemplateItem/portal_components/test.erp5.testStaticWebSiteRedirection.py
+++ bt5/erp5_web_ui_test/TestTemplateItem/portal_components/test.erp5.testStaticWebSiteRedirection.py
@@ -170,9 +170,6 @@ class TestStaticWebSectionRedirection(ERP5TypeTestCase):
   def test_queryStringIgnoreLayout(self):
     self.runTestRedirect("?ignore_layout=1")
 
-  def test_queryStringPortalSkin(self):
-    self.runTestRedirect("?portal_skin=FOOBAR")
-
   def test_plainRedirectGetId(self):
     self.runTestRedirect("getId")
 
