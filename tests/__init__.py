08f25ea9a21eca95dfe9af3672a11a22eaf7feb8
Romain Courteaud <romain@nexedi.com>
2019-06-03 13:47:37 +0200

    XXX HARDCODED run erp5_web tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -41,6 +41,7 @@ class _ERP5(SavedTestSuite):
         # ['%s/product/ERP5/tests/testXHTML.py' % path] +
         # ['%s/product/ERP5Type/tests/testFunctionalCore.py' % path] +
         # ['%s/product/ERP5Type/tests/testFunctionalAnonymousSelection.py' % path] +
+        glob('%s/bt5/erp5_web/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         glob('%s/bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         glob('%s/bt5/erp5_web_renderjs_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         # glob('%s/bt5/erp5_web_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
@@ -56,7 +57,6 @@ class _ERP5(SavedTestSuite):
         # glob('%s/bt5/erp5_officejs_afs_directory_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         # glob('%s/bt5/erp5_officejs_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path)
       ):
-        # glob('%s/bt5/erp5_web/TestTemplateItem/portal_components/test.*.test*.py' % path)):
       component_re_match = component_re.match(test_path)
       if component_re_match is not None:
         test_case = "%s:%s" % (component_re_match.group(1),
