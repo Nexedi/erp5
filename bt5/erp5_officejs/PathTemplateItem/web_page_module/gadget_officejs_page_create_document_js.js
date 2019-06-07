3317acb96a23cb302af1d739d066c606e618d470
Roque Porchetto <roque.porchetto@nexedi.com>
2019-06-07 13:15:41 +0000

    erp5_officejs: generic create document page - add element gadget removed, combined into create document

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_create_document_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_create_document_js.js
@@ -1,36 +1,9 @@
-/*global window, document, rJS, RSVP, Handlebars */
+/*global window, document, rJS, RSVP */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, document, rJS, RSVP, Handlebars) {
+(function (window, document, rJS, RSVP) {
   "use strict";
 
-  /////////////////////////////////////////////////////////////////
-  // Handlebars
-  /////////////////////////////////////////////////////////////////
-  // Precompile the templates while loading the first gadget instance
-  var gadget_klass = rJS(window),
-    table_template = Handlebars.compile(gadget_klass.__template_element
-                         .getElementById("table-template")
-                         .innerHTML);
-
-  function renderLinkList(gadget, title, icon, erp5_link_list) {
-    // prepare links for template (replace @href for RJS link)
-    return gadget.translateHtml(
-      table_template({
-        "definition_i18n": title,
-        "definition_title": title,
-        "definition_icon": icon,
-        "document_list": erp5_link_list.map(function (erp5_link, index) {
-          return {
-            "title": erp5_link.title,
-            "i18n": erp5_link.title,
-            "link": erp5_link_list[index].href
-          };
-        })
-      })
-    );
-  }
-
-  gadget_klass
+  rJS(window)
     /////////////////////////////////////////////////////////////////
     // Acquired methods
     /////////////////////////////////////////////////////////////////
@@ -42,41 +15,17 @@
     .declareAcquiredMethod("getUrlParameter", "getUrlParameter")
     .declareAcquiredMethod("updateHeader", "updateHeader")
     .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("redirect", "redirect")
+    .declareAcquiredMethod("jio_post", "jio_post")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
     /////////////////////////////////////////////////////////////////
 
-    .declareMethod("getHTMLElementList", function (portal_type_list, jio_key, parent_portal_type) {
-      var gadget = this,
-        i = 0,
-        portal_type_info_list = [],
-        portal_type,
-        url_for_parameter_list = [],
-        x;
-      for (x = 0; x < portal_type_list.length; x += 1) {
-        portal_type = portal_type_list[x];
-        url_for_parameter_list.push({ command: 'change', options: {page: "add_element", jio_key: jio_key, portal_type: portal_type, parent_portal_type: parent_portal_type} });
-        portal_type_info_list[i] = { reference: portal_type, title: portal_type};
-        i += 1;
-      }
-      return gadget.getUrlForList(url_for_parameter_list)
-        .push(function (url_list) {
-          var html_element_list = [], j, element;
-          for (j = 0; j < url_list.length; j += 1) {
-            element = { href: url_list[j],
-              icon: null,
-              name: portal_type_info_list[j].reference,
-              title: portal_type_info_list[j].title };
-            html_element_list.push(element);
-          }
-          return html_element_list;
-        });
-    })
-
     .declareMethod("render", function (options) {
       var gadget = this,
         allowed_sub_types_list = options.allowed_sub_types_list.split(","),
+        parent_portal_type = options.portal_type,
         portal_type,
         document_title;
       return gadget.jio_get(options.jio_key)
@@ -98,6 +47,7 @@
           form_result.form_definition.title = "Create Document";
           return gadget.changeState({
             doc: { title: document_title, portal_type: allowed_sub_types_list },
+            parent_portal_type: parent_portal_type,
             action_options: options,
             child_gadget_url: 'gadget_erp5_pt_form_dialog.html',
             form_type: 'dialog',
@@ -121,11 +71,24 @@
         });
     })
 
-    .declareMethod('triggerSubmit', function () {
-      return this.getDeclaredGadget('fg')
-        .push(function (gadget) {
-          return gadget.triggerSubmit();
+    .allowPublicAcquisition('submitContent', function (options) {
+      var gadget = this,
+        content_dict = options[2],
+        doc = {
+          title: "Untitled Document",
+          portal_type: content_dict.portal_type,
+          parent_relative_url: gadget.state.parent_portal_type.replace(/ /g, '_').toLowerCase()
+        };
+      return gadget.jio_post(doc)
+        .push(function (id) {
+          return gadget.redirect({
+            command: 'display',
+            options: {
+              jio_key: id,
+              editable: true
+            }
+          });
         });
     });
 
-}(window, document, rJS, RSVP, Handlebars));
+}(window, document, rJS, RSVP));
