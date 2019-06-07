4a89f9c2963454d0aad8be159de6467f5a467277
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2019-06-07 07:39:25 +0000

    erp5_officejs_support_request_ui: keep last user search in Support Request Module view

--- bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_panel_js.js
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_panel_js.js
@@ -95,7 +95,7 @@
             return RSVP.all([
               context.getUrlFor({command: 'display', options: {page: "supportrequest_preference"}}),
               context.getUrlFor({command: 'display', options: {page: "logout"}}),
-              context.getUrlFor({command: 'display', options: {jio_key: "support_request_module"}})
+              context.getUrlFor({command: 'display_stored_state', options: {jio_key: "support_request_module"}})
             ]);
           })
           .push(function (result_list) {
@@ -105,7 +105,7 @@
                 panel_template_body({
                   "preference_href": result_list[0],
                   "logout_href": result_list[1],
-                  "supportrequest_href": result_list[3]
+                  "supportrequest_href": result_list[2]
                 })
             );
           })
