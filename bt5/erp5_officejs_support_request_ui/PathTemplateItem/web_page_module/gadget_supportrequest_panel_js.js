c3403804df1b11a279ff92d591c04f05969dab10
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2019-06-07 07:30:08 +0000

    erp5_officejs_support_request_ui: remove dead code

--- bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_panel_js.js
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_panel_js.js
@@ -95,7 +95,6 @@
             return RSVP.all([
               context.getUrlFor({command: 'display', options: {page: "supportrequest_preference"}}),
               context.getUrlFor({command: 'display', options: {page: "logout"}}),
-              context.getUrlFor({command: 'display', options: {page: "search"}}),
               context.getUrlFor({command: 'display', options: {jio_key: "support_request_module"}})
             ]);
           })
@@ -106,7 +105,6 @@
                 panel_template_body({
                   "preference_href": result_list[0],
                   "logout_href": result_list[1],
-                  "search_href": result_list[2],
                   "supportrequest_href": result_list[3]
                 })
             );
