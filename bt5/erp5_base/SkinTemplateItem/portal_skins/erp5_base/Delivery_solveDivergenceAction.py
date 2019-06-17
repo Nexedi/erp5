88cb6b649ae29727ebc8d0111457303d3b4d450a
Romain Courteaud <romain@nexedi.com>
2019-06-17 14:34:20 +0000

    [erp5_simulation] Allow to resolve divergency in the RJS UI
    [erp5_base]
    Allow Manager to solve the divergency
    Pass the transition ID as the other workflow actions
    
    [erp5_simulation]
    Prevent duplicated keep_items
    
    Fix request value modifications
    
    Do not mark fields as required
    The RJS field marks it as required, and user can't submit anymore.
    Instead, use an external validator to only require the field when the user submit

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_solveDivergenceAction.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Delivery_solveDivergenceAction.py
@@ -1,6 +1,7 @@
+"""
 if context.Delivery_getSolverProcess() is None:
   message = context.Base_translateString("Workflow state may have been updated by other user. Please try again.")
   return context.Base_redirect('view',
                                 keep_items={'portal_status_message': message})
-
-return context.Delivery_viewSolveDivergenceDialog(*args, **kw)
+"""
+return context.Base_renderForm('Delivery_viewSolveDivergenceDialog')
