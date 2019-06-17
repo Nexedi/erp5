43bf241943e35de1ebf235e9bb726213bce91aef
Romain Courteaud <romain@nexedi.com>
2019-06-17 12:51:44 +0000

    [erp5_trade_renderjs_ui_test] Time to fail

--- bt5/erp5_trade_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_trade_renderjs_ui_test/TradeRJSZuite_deleteData.py
+++ bt5/erp5_trade_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_trade_renderjs_ui_test/TradeRJSZuite_deleteData.py
@@ -5,7 +5,11 @@ delivery_id = "erp5_trade_renderjs_ui_test_delivery"
 
 # Delete event
 module = portal.getDefaultModule(delivery_portal_type)
-if getattr(module, delivery_id, None) is not None:
+delivery = getattr(module, delivery_id, None)
+if delivery is not None:
+
+  for linking_document in portal.portal_catalog(strict__any__uid=delivery.getUid()):
+    linking_document.getParentValue().manage_delObjects([linking_document.getId()])
   module.manage_delObjects([delivery_id])
 
 return "Deleted Successfully."
