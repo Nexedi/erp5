e3cef87270b2d207b5da72522e7f97991016df5c
lu.xu <lu.xu@nexedi.com>
2019-06-18 08:29:13 +0000

    Final changes

--- bt5/erp5_smart_assistant/SkinTemplateItem/portal_skins/erp5_smart_assistant_text/SmartAssistantText_respond.py
+++
@@ -1,3 +0,0 @@
-context.setTextContent(context.getTextContent() + "\n" + field_your_reply)
-context.respond()
-return context.Base_redirect('view', keep_items=dict(portal_status_message="Responded"))
