0f54260cc2a8332e73089c504366452cd1881ab7
Romain Courteaud <romain@nexedi.com>
2019-06-18 08:39:24 +0000

    [erp5_simulation] Prevent duplicated keep_items

--- bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
+++ bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
@@ -5,8 +5,8 @@ listbox = request.get('listbox')
 line_list = context.Delivery_getSolverDecisionList(listbox=listbox)
 
 if len(line_list) == 0:
-  message = translateString("Workflow state may have been updated by other user. Please try again.")
-  return context.Base_redirect(form_id, keep_items={'portal_status_message': message}, **kw)
+  kw["keep_items"] = {'portal_status_message': translateString("Workflow state may have been updated by other user. Please try again.")}
+  return context.Base_redirect(form_id, **kw)
 
 for listbox_key in listbox:
   listbox_dict = listbox[listbox_key]
@@ -23,5 +23,5 @@ for listbox_key in listbox:
       line.setProperty(property, value)
 request.set('your_dialog_updated', '1')
 request.form['dialog_updated'] = '1'
-request.form['listbox'] = listbox
+request.set('listbox', listbox)
 return context.Base_renderForm('Delivery_viewSolveDivergenceDialog')
