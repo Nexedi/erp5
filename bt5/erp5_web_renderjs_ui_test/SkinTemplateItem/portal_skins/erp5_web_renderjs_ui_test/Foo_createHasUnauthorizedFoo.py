ca229c3ee9a7f80e8daec3788d8e9ffcf806db01
Romain Courteaud <romain@nexedi.com>
2019-06-21 10:45:01 +0000

    [erp5_web_renderjs_ui_test] Reindexation fails if Manager doesn't have access to the document
    Ensure permission removal is done after the indexation

--- bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/Foo_createHasUnauthorizedFoo.py
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/Foo_createHasUnauthorizedFoo.py
@@ -1,6 +1,9 @@
-foo1 = context.foo_module.newContent(portal_type='Foo')
-foo2 = context.foo_module.newContent(portal_type='Foo')
-foo1.setTitle('hasAccessUnauthorized')
-foo1.setSuccessorValue(foo2)
-foo2.activate().manage_permission( 'Access contents information', 'Manager', 0)
+activity_tag = 'Foo_createHasUnauthorizedFoo'
+with context.foo_module.defaultActivateParameterDict({'tag': activity_tag}, placeless=True):
+  foo1 = context.foo_module.newContent(portal_type='Foo')
+  foo2 = context.foo_module.newContent(portal_type='Foo')
+  foo1.setTitle('hasAccessUnauthorized')
+  foo1.setSuccessorValue(foo2)
+
+foo2.activate(after_tag=activity_tag).manage_permission('Access contents information', 'Manager', 0)
 return 'Done'
