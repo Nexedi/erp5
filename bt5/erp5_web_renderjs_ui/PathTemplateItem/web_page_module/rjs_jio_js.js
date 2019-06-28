c5071ec28f3103e374be2a308f1f9c35f795ad9b
Romain Courteaud <romain@nexedi.com>
2019-06-28 13:51:54 +0000

    [erp5_web_renderjs_ui] Update jio 3.38.0

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
@@ -6930,6 +6930,9 @@ return new Parser;
       throw new TypeError("jioquery.sortOn(): " +
                           "Argument 1 is not of type 'array'");
     }
+    if (sort_on_option.length === 0) {
+      return list;
+    }
     list.sort(generateSortFunction(
       key_schema,
       sort_on_option
@@ -14916,13 +14919,19 @@ return new Parser;
   }
 
   function waitForOpenIndexedDB(db_name, callback) {
+    var request;
+
+    function canceller() {
+      if ((request !== undefined) && (request.result !== undefined)) {
+        request.result.close();
+      }
+    }
+
     function resolver(resolve, reject) {
       // Open DB //
-      var request = indexedDB.open(db_name);
+      request = indexedDB.open(db_name);
       request.onerror = function (error) {
-        if (request.result) {
-          request.result.close();
-        }
+        canceller();
         if ((error !== undefined) &&
             (error.target instanceof IDBOpenDBRequest) &&
             (error.target.error instanceof DOMError)) {
@@ -14934,17 +14943,16 @@ return new Parser;
       };
 
       request.onabort = function () {
-        request.result.close();
+        canceller();
         reject("Aborting connection to: " + db_name);
       };
 
       request.ontimeout = function () {
-        request.result.close();
         reject("Connection to: " + db_name + " timeout");
       };
 
       request.onblocked = function () {
-        request.result.close();
+        canceller();
         reject("Connection to: " + db_name + " was blocked");
       };
 
@@ -14952,26 +14960,32 @@ return new Parser;
       request.onupgradeneeded = handleUpgradeNeeded;
 
       request.onversionchange = function () {
-        request.result.close();
+        canceller();
         reject(db_name + " was upgraded");
       };
 
       request.onsuccess = function () {
+        var result;
+        try {
+          result = callback(request.result);
+        } catch (error) {
+          reject(error);
+        }
         return new RSVP.Queue()
           .push(function () {
-            return callback(request.result);
+            return result;
           })
-          .push(function (result) {
-            request.result.close();
-            resolve(result);
+          .push(function (final_result) {
+            canceller();
+            resolve(final_result);
           }, function (error) {
-            request.result.close();
+            canceller();
             reject(error);
           });
       };
     }
 
-    return new RSVP.Promise(resolver);
+    return new RSVP.Promise(resolver, canceller);
   }
 
   function waitForTransaction(db, stores, flag, callback) {
@@ -15001,14 +15015,8 @@ return new Parser;
             reject(error);
           });
       };
-      tx.onerror = function (error) {
-        canceller();
-        reject(error);
-      };
-      tx.onabort = function (evt) {
-        reject(evt.target);
-      };
-      return tx;
+      tx.onerror = reject;
+      tx.onabort = reject;
     }
     return new RSVP.Promise(resolver, canceller);
   }
