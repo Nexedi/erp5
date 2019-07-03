4b544bf13bbb8db0bfd941542b9b1b9e962c9baa
Ivan Tyagov <ivan@nexedi.com>
2019-07-03 14:47:06 +0300

    Setup data-lake during configuration phase.

--- wendelin/bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_configureWendelinSetupDataNotebook.py
+++ wendelin/bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_configureWendelinSetupDataNotebook.py
@@ -8,3 +8,13 @@ if kw.get('setup_data_notebook'):
     update_catalog=False,
     install_dependency=True,
   )
+  
+# setup "data-lake"
+if kw.get('setup_data_lake'):
+  configuration_save.addConfigurationItem(
+    "Standard BT5 Configurator Item",
+    title='erp5_wendelin_data_lake_ingestion', 
+    bt5_id='erp5_wendelin_data_lake_ingestion',
+    update_catalog=False,
+    install_dependency=True,
+  )
