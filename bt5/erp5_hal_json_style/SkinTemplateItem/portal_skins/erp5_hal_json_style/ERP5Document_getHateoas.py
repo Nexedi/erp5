590946abf168dfb82f577327322fe84157fd23d0
Romain Courteaud <romain@nexedi.com>
2019-07-15 08:33:40 +0000

    hal: remove comments

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -1727,32 +1727,10 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
         selection_tool.setSelectionFor(selection_name, selection)
 
         if 'selection_domain' in catalog_kw:
-          # for selection_domain_key, selection_domain_value in catalog_kw['selection_domain'].items():
-          # selection_domain_dict = ensureDeserialized(
-          #   byteify(json.loads(selection_domain)))
-
-          # selection.domain_root = selection_domain_dict.keys()
-          # selection.domain_list = selection_domain_dict.values()
-
-          # selection.edit(domain_path=catalog_kw['selection_domain'].keys(), domain_list=[x.getRelativeUrl() for x in catalog_kw['selection_domain'].values()])
-          # selection.domain_path = catalog_kw['selection_domain'].keys()
-          # selection.domain_list = [x.getRelativeUrl() for x in catalog_kw['selection_domain'].values()]
-
-          """
-          raise NotImplementedError(catalog_kw['selection_domain'])
-          selection.domain_root = catalog_kw['selection_domain'].keys()
-          selection.domain_list = [x.getRelativeUrl() for x in catalog_kw['selection_domain'].values()] 
-          """
-          # selection.domain_list = selection_domain_dict.values()
-          # selection_tool.setDomainRootFromParam(REQUEST, selection_name, catalog_kw['selection_domain'].keys(),
-          #                                       domain_list=[x.getRelativeUrl() for x in catalog_kw['selection_domain'].values()])
-          # new
-
           new_selection_dict = {}
           selection_domain_dict = ensureDeserialized(
               byteify(json.loads(selection_domain)))
           for domain_root_id in selection_domain_dict:
-            # new_selection_dict[domain_root_id] = ('%s/%s' % (domain_root_id, selection_domain_dict[domain_root_id]), )
             new_selection_dict[domain_root_id] = ('portal_categories', '%s/%s' % (domain_root_id, selection_domain_dict[domain_root_id]), )
 
           selection_tool.setDomainDictFromParam(selection_name, new_selection_dict)
