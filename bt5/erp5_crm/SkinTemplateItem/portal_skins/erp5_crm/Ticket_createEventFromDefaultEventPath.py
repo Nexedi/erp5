b55ecd93016de70e38104bd17342706a4edae930
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2019-07-17 07:27:21 +0000

    erp5_crm: activate searchAndActivate
    Because the number of objects to be retrieved can be too big, or because the query can be complex (depending on the definition of the domain), and because searchAndActivate add a sort on uid (even if it's a primary key, this sort is amplified by the 2 previous reasons), searchAndActivate should be activated to avoid timeouts for the user

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_createEventFromDefaultEventPath.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_createEventFromDefaultEventPath.py
@@ -7,7 +7,8 @@ else:
   event_path = context.getDefaultEventPathValue(portal_type="Event Path")
   method_kw = {'event_path': event_path.getRelativeUrl(),
    'keep_draft': keep_draft}
-  portal.portal_catalog.searchAndActivate("Entity_createEventFromDefaultEventPath",
+  portal.portal_catalog.activate(activity='SQLQueue').searchAndActivate(
+    "Entity_createEventFromDefaultEventPath",
     selection_domain={domain.getParentId(): ('portal_domains', domain.getRelativeUrl())},
     method_kw=method_kw)
   message = 'Events are being created in background'
