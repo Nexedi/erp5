a09c1faac023037dfa9e03816aaa9a4a70de3125
Roque Porchetto <roque.porchetto@nexedi.com>
2019-07-18 14:24:26 +0200

    erp5_web_renderjs_ui: new configuration elements on site layout configuration

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSection_renderDefaultPageAsGadget.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSection_renderDefaultPageAsGadget.py
@@ -51,6 +51,11 @@ mapping_dict = {
   "website_url_set": json.dumps(website_url_set),
   "site_description": web_section.getLayoutProperty("description", default=""),
   "site_keywords": web_section.getLayoutProperty("subject", default=""),
+  "app_view_reference": web_section.getLayoutProperty("configuration_app_view_action_reference", default=""),
+  "app_allowed_sub_types": web_section.getLayoutProperty("configuration_app_allowed_sub_types", default=""),
+  "header_jump_button": web_section.getLayoutProperty("configuration_header_jump_button", default=0),
+  "hide_header_add_button": web_section.getLayoutProperty("configuration_hide_header_add_button", default=0),
+  "app_actions": web_section.getLayoutProperty("configuration_app_actions", default=""),
 }
 
 configuration_manifest_url = web_section.getLayoutProperty("configuration_manifest_url", default=None)
