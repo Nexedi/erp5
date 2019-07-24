0a6664a2bb0a0fa8e0e7bec2770609e4a0ff4c48
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2019-07-24 07:22:22 +0000

    fxup! erp5_pdm: support calculation of base_price applying to all items of a same slice

--- bt5/erp5_pdm/WorkflowTemplateItem/portal_workflow/supply_line_interaction_workflow/scripts/SupplyLine_updateBasePriceCellRange.py
+++ bt5/erp5_pdm/WorkflowTemplateItem/portal_workflow/supply_line_interaction_workflow/scripts/SupplyLine_updateBasePriceCellRange.py
@@ -1,4 +1,11 @@
 supply_line = state_change['object']
+
+# Currently state_change.kwargs['workflow_method_args'] contains
+# a list of string, so this script is called every time.
+# As it is not the intent, let's return early if nothing changes
+if [float(x) for x in sorted(state_change.kwargs['workflow_method_args'][0], key=lambda x: float(x))] == sorted(supply_line.getQuantityStepList()):
+  return
+
 if supply_line.isBasePricePerSlice():
   price_parameter = 'slice_base_price'
 else:
