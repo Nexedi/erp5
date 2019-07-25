d607ac57c16a7553788976b55b7bb3ff32508ef4
Ivan Tyagov <ivan@nexedi.com>
2019-07-25 13:07:43 +0300

    Disable logging.

--- bt5/erp5_wendelin_data_lake_ingestion/WorkflowTemplateItem/portal_workflow/validation_workflow/scripts/Action_invalidateRelatedObjects.py
+++ bt5/erp5_wendelin_data_lake_ingestion/WorkflowTemplateItem/portal_workflow/validation_workflow/scripts/Action_invalidateRelatedObjects.py
@@ -7,7 +7,7 @@ object = state_change['object']
 if object.getPortalType() == "Data Set":
   data_set = object
   data_set_prefix = data_set.getReference() + portal.getIngestionReferenceDictionary()["reference_separator"]
-  context.logEntry("Invalidating data set '%s' and dependencies." % data_set.getReference())
+  #context.logEntry("Invalidating data set '%s' and dependencies." % data_set.getReference())
   reference_query = Query(**{'reference': data_set_prefix + '%'})
   kw_dict = {"portal_type": "Data Stream",
              "query": reference_query}
