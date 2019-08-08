5c7be90e3b68debedf4e2143b7d4569b9c5d81c1
Gabriel Monnerat <gabriel@nexedi.com>
2019-08-08 20:16:18 +0200

    component: We don't need Permissions.ModifyPortalContent to check consistency. Permissions.AccessContentsInformation is enough

--- product/ERP5Type/mixin/component.py
+++ product/ERP5Type/mixin/component.py
@@ -208,7 +208,7 @@ class ComponentMixin(PropertyRecordableMixin, Base):
     except (AttributeError, KeyError, IndexError):
       return 'draft'
 
-  security.declareProtected(Permissions.ModifyPortalContent, 'checkConsistency')
+  security.declareProtected(Permissions.AccessContentsInformation, 'checkConsistency')
   def checkConsistency(self, *args, **kw):
     """
     Check the consistency of the Component upon validate or when being
