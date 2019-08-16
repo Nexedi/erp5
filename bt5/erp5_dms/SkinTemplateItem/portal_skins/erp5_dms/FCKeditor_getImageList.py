1f605cedf36984a03467fab94c5d59441c62992c
Yusei Tahara <yusei@nexedi.com>
2019-08-16 15:27:46 +0900

    [erp5_dms] Fix a bug on FCKeditor_getImageList.

--- bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/FCKeditor_getImageList.py
+++ bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/FCKeditor_getImageList.py
@@ -1,2 +1,4 @@
-return context.getPortalObject().portal_catalog(
-    context.FCKeditor_getDocumentListQuery(document_type='Image'), **kw)
+result = context.FCKeditor_getDocumentListQuery(document_type='Image')
+if result is not None:
+  kw['query'] = result
+return context.getPortalObject().portal_catalog(**kw)
