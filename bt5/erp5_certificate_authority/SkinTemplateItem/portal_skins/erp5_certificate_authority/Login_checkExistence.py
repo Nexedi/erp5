325db4a61e970fbc3c8f0543815917e32f5a0472
Rafael Monnerat <rafael@nexedi.com>
2019-08-26 17:10:00 +0000

    erp5_certificate_authority: Fix up script and set portal skins priority order
    Prefer to use Login_checkExistence from this Skins over the version on erp5_base.

--- bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/Login_checkExistence.py
+++ bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/Login_checkExistence.py
@@ -6,7 +6,7 @@ if portal_type in ("ERP5 Login", "Certificate Login"):
   portal_type = ("ERP5 Login", "Certificate Login")
 
 return context.getPortalObject().Base_checkLoginExistence(
-  portal_type=context.getPortalType(),
+  portal_type=portal_type,
   reference=context.getReference(),
   ignore_uid=context.getUid(),
   ignore_user_uid=context.getParentValue().getUid(),
