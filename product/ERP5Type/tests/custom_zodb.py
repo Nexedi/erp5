50a8b88f17d49da4c0e3c0d2da4cff9d48d38354
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2019-08-28 16:22:23 +0900

    runUnitTest: ZODB is removed on '--save', so also recreate the Catalog tables installed by bt5s.
    Assuming that the test database already exists and 'movement' table already
    contains data (from past execution for example), on '--save' before this
    commit:
      1. Create a new ERP5 Site:
         1. Install erp5_mysql_innodb_catalog.
         2. Call ERP5Site_reindexAll(clear_catalog=True) (from ERP5Generator.setupIndex()).
            => At this point, erp5_mysql_innodb_catalog tables are recreated.
      2. Install erp5_movement_table_catalog or any bt5 adding a new SQL table.
         => This does not recreate the table and leave the existing data as it is.
      3. Dump MySQL database to dump.sql.
      => dump.sql contains 'INSERT INTO' for 'movement' table *before* this '--save'.
    
    This fixes random customer Unit Tests failures on SQL queries accessing
    'movement' table directly and getting {non-existing,past executions}
    Movements.
    
    With this commit, after installing all bt5, all tables handled by the Catalog
    will be cleared and ERP5Site_reindexAll will be called.

--- product/ERP5Type/tests/custom_zodb.py
+++ product/ERP5Type/tests/custom_zodb.py
@@ -56,10 +56,17 @@ if load:
       _print("Could not find MySQL dump (%r), will recreate catalog ... " % dump_sql_path)
       os.environ['erp5_tests_recreate_catalog'] = '1'
   _print("Restoring static files ... ")
-elif save and not (neo_storage or zeo_client) and os.path.exists(data_fs_path):
-  _print("About to remove existing Data.fs %s (press Ctrl+C to abort)" % data_fs_path)
-  time.sleep(5)
-  os.remove(data_fs_path)
+elif save:
+  if not (neo_storage or zeo_client) and os.path.exists(data_fs_path):
+    _print("About to remove existing Data.fs %s (press Ctrl+C to abort)" % data_fs_path)
+    time.sleep(5)
+    os.remove(data_fs_path)
+
+# Whether passing --save or not passing both --load and --save, the catalog
+# must be cleared of data from previous execution if any
+if not load:
+  _print("Catalog will be recreated to clear data (if any) from previous execution")
+  os.environ['erp5_tests_recreate_catalog'] = '1'
 
 zeo_server_pid = None
 node_pid_list = []
