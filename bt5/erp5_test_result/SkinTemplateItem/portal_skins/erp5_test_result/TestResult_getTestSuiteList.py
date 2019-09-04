cb818a61206d7f17c61cc4d5a217bd161737b15f
Ivan Tyagov <ivan@nexedi.com>
2019-09-04 15:04:24 +0200

    testnode uses this format to report
    ['Person: 372 doc/hour; SaleOrder: 132 doc/hour;']
    Thus adjust accordingly. Do not mix http and https.
    
    Testnode report multiple test suites thus show links for graph generation of everyone of them.

---
+++ bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_getTestSuiteList.py
@@ -0,0 +1,16 @@
+"""
+  Return list of Test Suites being tested by introspecting Test Result Lines
+"""
+test_suite_list = []
+test_result_line_list = context.objectValues(portal_type = 'Test Result Line')
+
+if len(test_result_line_list) > 0:
+  test_result_line = test_result_line_list[0]
+  stdout = test_result_line.getProperty('stdout')
+  context.log(stdout)
+  for i in stdout.split(';'):
+    test_suite_list.append(i.split(':')[0].strip())
+
+# remote empty elements
+test_suite_list = [x for x in test_suite_list if x.strip()!='']
+return test_suite_list
