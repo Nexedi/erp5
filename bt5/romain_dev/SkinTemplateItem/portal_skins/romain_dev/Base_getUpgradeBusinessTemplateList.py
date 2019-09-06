c40850f091abdb119e2add7577b1ceb7e3a50dcf
Romain Courteaud <romain@nexedi.com>
2019-09-06 10:18:53 +0000

    [romain_dev] wip tools

---
+++ bt5/romain_dev/SkinTemplateItem/portal_skins/romain_dev/Base_getUpgradeBusinessTemplateList.py
@@ -0,0 +1,46 @@
+bt5_update_catalog_list = ('erp5_ingestion_mysql_innodb_catalog', 'erp5_full_text_mroonga_catalog')
+bt5_installation_list = bt5_update_catalog_list + ('erp5_xhtml_style',
+                                                   # 'erp5_officejs', 'erp5_web_jabber_client',
+                                                   # 'erp5_officejs_ooffice',
+                                                   'erp5_upgrader',
+                                                   'erp5_hal_json_style',
+                                                   'erp5_ods_style',
+                                                   'erp5_odt_style',
+                                                   'erp5_deferred_style',
+                                                   # 'erp5_officejs_ui_test',
+                                                   'erp5_crm_renderjs_ui_test',
+                                                   'erp5_trade_renderjs_ui_test',
+                                                   'erp5_web_renderjs_ui_test',
+                                                   'erp5_pdm_renderjs_ui_test',
+                                                   'erp5_accounting_renderjs_ui_test',
+                                                   'erp5_item_renderjs_ui_test',
+                                                   # 'erp5_officejs_ui_test',
+                                                   'erp5_officejs_support_request_ui_test',
+                                                   # 'erp5_calendar',
+                                                   # 'erp5_crm',
+                                                   # 'erp5_mrp',
+                                                   'erp5_project',
+
+                                                   # test dependencies
+                                                   # 'erp5_configurator_standard_trade_template',
+                                                   # 'erp5_simulation_test',
+                                                   # 'erp5_accounting',
+                                                   # 'erp5_monaco_editor_ui_test',
+
+                                                   # 'erp5_gadget_interface_validator_ui_test',
+                                                   'erp5_forge',
+                                                   # 'erp5_web_jabber_client',
+                                                   'erp5_web_ui_test',
+                                                   'erp5_officejs_afs_directory_ui_test',
+                                                   'erp5_accounting_ui_test',
+                                                   'erp5_dms_ui_test',
+                                                   # 'erp5_travel_expense', 'erp5_travel_expense_ui_test',
+                                                   # 'officejs_olapy_demo', 'erp5_officejs_connector'
+                                                   # 'erp5_discussion'
+                                                   'erp5_hr',
+                                                   'erp5_item',
+                                                   'erp5_mrp',
+                                                   'erp5_payroll',
+                                                   'erp5_authentication_policy',
+                                                   )
+return bt5_installation_list, []
