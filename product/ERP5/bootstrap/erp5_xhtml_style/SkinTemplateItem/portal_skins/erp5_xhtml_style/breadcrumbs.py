4ec1a1a23d24309ef922bdaf7573e64a71ab53da
Bryton Lacquement <bryton.lacquement@nexedi.com>
2019-09-09 16:51:45 +0200

    erp5_xhtml_style: do not use getToolByName to access tools
    Also some code refactoring.

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/breadcrumbs.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/breadcrumbs.py
@@ -1,31 +1,29 @@
 "Modified version for ERP5 to append the default action (/view) in the URL."
 
-from Products.CMFCore.utils import getToolByName
-ptool = getToolByName(script, 'portal_properties')
-utool = getToolByName(script, 'portal_url')
-portal_url = utool()
-result = []
-param = int(context.REQUEST.get('ignore_layout', 0)) and '?ignore_layout:int=1' or ''
-if include_root:
-    result.append( { 'id'      : 'root'
-                   , 'title'   : ptool.title()
-                   , 'url'     : '%s/view%s' % (portal_url, param)
-                   }
-                 )
+portal = context.getPortalObject()
 
-relative = utool.getRelativeContentPath(context)
-portal = utool.getPortalObject()
+utool = portal.portal_url
+portal_url = utool()
+param = '?ignore_layout:int=1' if int(portal.REQUEST.get('ignore_layout', 0)) else ''
+if include_root:
+    result = [{
+        'id'    : 'root',
+        'title' : portal.portal_properties.title(),
+        'url'   : '%s/view%s' % (portal_url, param),
+    }]
+else:
+    result = []
 
 obj = portal
 now = []
-for name in relative:
+for name in utool.getRelativeContentPath(context):
     obj = obj.restrictedTraverse(name)
     now.append(name)
     title = (
       getattr(obj, "getCompactTranslatedTitle", lambda: None)() or
       obj.getTitle() or obj.getId()
     )
-    if not name == 'talkback':
+    if name != 'talkback':
         result.append( { 'id'      : name
                        , 'title'   : title
                        , 'url'     : '%s/%s/view%s' % (portal_url, '/'.join(now), param)
