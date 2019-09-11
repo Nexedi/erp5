814e42cc2d711fa80e0678d5863bea3388e61379
Ivan Tyagov <ivan@nexedi.com>
2019-09-11 16:08:13 +0200

    Fixup: always use first Test Result Line from Test Result instead of random one.

--- bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_getTestSuiteList.py
+++ bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_getTestSuiteList.py
@@ -2,12 +2,13 @@
   Return list of Test Suites being tested by introspecting Test Result Lines
 """
 test_suite_list = []
-test_result_line_list = context.objectValues(portal_type = 'Test Result Line')
+test_result_line_list = context.objectValues(
+                          portal_type = 'Test Result Line', 
+                          sort_on="int_index")
 
 if len(test_result_line_list) > 0:
   test_result_line = test_result_line_list[0]
   stdout = test_result_line.getProperty('stdout')
-  context.log(stdout)
   for i in stdout.split(';'):
     test_suite_list.append(i.split(':')[0].strip())
 
