528f9363486038636c1a0d245593349ba4a28ce5
Gabriel Monnerat <gabriel@nexedi.com>
2019-09-16 01:19:01 +0000

    erp5_document_scanner: Add gadget to scanner documents using mobile
    For now, this is chrome only
    
    erp5_document_scanner_ui_test: Add tests to Document Scanner gadget

---
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_storeDocumentFromCameraInActiveProcess.py
@@ -0,0 +1,39 @@
+import json
+from base64 import decodestring
+portal = context.getPortalObject()
+
+translateString = portal.Base_translateString
+
+gadget_data = json.loads(document_scanner_gadget)
+image_str = decodestring(gadget_data.pop("input_value"))
+preferred_cropped_canvas_data = json.dumps(gadget_data["preferred_cropped_canvas_data"])
+
+active_preference = portal.portal_preferences.getActiveUserPreference()
+if not active_preference:
+  active_preference = portal.portal_preferences.getActivePreference()
+
+if active_preference and preferred_cropped_canvas_data:
+  active_preference.setPreferredCroppedCanvasData(preferred_cropped_canvas_data)
+
+if not image_str:
+  if batch_mode:
+    if active_process_url:
+      return portal.restrictedTraverse(active_process_url)
+    return None
+
+  return context.Base_renderForm('Base_viewUploadDocumentFromCameraDialog',
+                               message=translateString('Nothing to capture'))
+
+active_process = context.Base_postDataToActiveResult(
+  active_process_url,
+  image_str)
+
+context.REQUEST.form["your_active_process_url"] = active_process.getRelativeUrl()
+context.REQUEST.form.pop("field_your_document_scanner_gadget")
+context.REQUEST.form.pop('document_scanner_gadget')
+
+if batch_mode:
+  return active_process
+
+return context.Base_renderForm('Base_viewUploadDocumentFromCameraDialog',
+                               message=translateString('Captured'))
