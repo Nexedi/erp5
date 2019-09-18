528ef5bd186c6491164a20edfc9872e0eb0e0372
Romain Courteaud <romain@nexedi.com>
2019-09-18 14:07:08 +0000

    erp5_ui_test new test action

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_executePythonAction.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_executePythonAction.py
@@ -1,4 +1,4 @@
-if foo_url is None:
+if not foo_url:
   return context.Base_redirect('view', keep_items={'portal_status_message': 'Missing expected foo_url parameter', 'portal_status_level': 'error'})
 else:
   return context.Base_redirect('view', keep_items={'portal_status_message': 'foo_url parameter equals %s' % foo_url})
