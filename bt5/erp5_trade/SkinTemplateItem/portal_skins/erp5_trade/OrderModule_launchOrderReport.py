7331580f266200e1d8caf0e2d810c508cfaeef14
Romain Courteaud <romain@nexedi.com>
2019-09-20 09:56:16 +0000

    wip

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/OrderModule_launchOrderReport.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/OrderModule_launchOrderReport.py
@@ -6,17 +6,18 @@ portal = context.getPortalObject()
 N_ = portal.Base_translateString
 # Check deferred style is present
 if not 'Deferred' in portal.portal_skins.getSkinSelections():
+  portal.changeSkin(previous_skin_selection)
   return context.Base_redirect('view', keep_items=dict(
               portal_status_message=N_("Deferred style must be installed to run this report")))
   
 person_value = portal.portal_membership.getAuthenticatedMember().getUserValue()
 if person_value is None:
-  portal.changeSkin(None)
+  portal.changeSkin(previous_skin_selection)
   return context.Base_redirect('view', keep_items=dict(
               portal_status_message=N_("No person found for your user")))
 
 if person_value.getDefaultEmailText('') in ('', None):
-  portal.changeSkin(None)
+  portal.changeSkin(previous_skin_selection)
   return context.Base_redirect('view', keep_items=dict(
               portal_status_message=N_("You haven't defined your email address")))
 
@@ -36,6 +37,7 @@ active_process.postResult(ActiveResult(
       ))
 
 request = context.REQUEST
+previous_skin_selection = portal.portal_skins.getCurrentSkinName()
 context.getPortalObject().portal_skins.changeSkin("Deferred")
 request.set('portal_skin', "Deferred")
 assert deferred_portal_skin is not None, "No deferred portal skin found in parameters"
@@ -48,6 +50,7 @@ kw.update(parameter_dict)
 kw.pop('format',None)
 return context.Base_activateReport(
   form = getattr(context, report_method_id),
+  previous_skin_selection = previous_skin_selection,
   after_tag=script.id,
   **kw
   )
