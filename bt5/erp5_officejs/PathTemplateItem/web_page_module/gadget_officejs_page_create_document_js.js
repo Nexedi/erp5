595216d385ce3a37df15349f7241dd6c5c5f5711
Roque Porchetto <roque.porchetto@nexedi.com>
2019-09-20 14:26:55 +0200

    erp5_officejs: refactoring in new officejs approach - reorginize and extend app setting and customization - each portal type has its own setting dictionary - general code refactoring and cleanup - handling blob types in document render and submit - remove hardcoded app configuration manifest - allow custom view in controller - handle_action handles notify submit and redirect base on custom action return - controller default page forces storage sync if app version changes - fix 401 login redirect in storage selection

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_create_document_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_create_document_js.js
@@ -8,7 +8,6 @@
     // Acquired methods
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("jio_get", "jio_get")
-    .declareAcquiredMethod("getSetting", "getSetting")
     .declareAcquiredMethod("redirect", "redirect")
     .declareAcquiredMethod("jio_post", "jio_post")
 
@@ -16,32 +15,13 @@
     // declared methods
     /////////////////////////////////////////////////////////////////
 
-    .declareMethod("createDocument", function (portal_type,
-                                               parent_portal_type) {
-      var gadget = this,
-        doc = {
-          title: "Untitled Document",
-          portal_type: portal_type,
-          parent_relative_url: parent_portal_type
-        };
-      return gadget.jio_post(doc)
-        .push(function (id) {
-          return gadget.redirect({
-            command: 'display',
-            options: {
-              jio_key: id,
-              editable: true
-            }
-          });
-        });
-    })
-
     .declareMethod("render", function (options) {
       var gadget = this,
         allowed_sub_types_list = options.allowed_sub_types_list.split(","),
         parent_portal_type = options.portal_type,
+        dialog_form = options.new_content_dialog_form,
+        dialog_category = options.new_content_category,
         portal_type,
-        form_definition,
         document_title;
       return gadget.jio_get(options.jio_key)
         .push(function (document) {
@@ -53,32 +33,32 @@
         })
         .push(function (portal_type_result) {
           portal_type = portal_type_result;
-          return gadget.getSetting("new_content_action");
-        })
-        .push(function (new_content_action) {
-          if (!new_content_action) {
-            throw new Error("Missing site configuration 'new_content_action'");
+          if (dialog_form) {
+            return gadget.declareGadget("gadget_officejs_common_util.html")
+              .push(function (gadget_util) {
+                return gadget_util.getDialogFormDefinition(dialog_form,
+                                                           dialog_category);
+              })
+              .push(function (form_definition) {
+                return gadget.changeState({
+                  doc: { header_title: form_definition.title || document_title,
+                         portal_type: allowed_sub_types_list },
+                  //TODO this should be a portal_dict setting and not global
+                  parent_portal_type: parent_portal_type,
+                  action_options: options,
+                  child_gadget_url: form_definition.child_gadget_url,
+                  form_type: form_definition.form_type,
+                  form_definition: form_definition,
+                  view: "view",
+                  show_dialog: true
+                });
+              });
           }
-          return gadget.jio_get(new_content_action);
-        })
-        .push(function (form_result) {
-          form_definition = form_result.raw_dict._embedded._view
-            ._embedded.form_definition;
-          form_definition.fields_raw_properties = form_result.raw_dict._embedded
-            ._view.my_fields_raw_properties["default"];
-          form_definition._actions = form_result.raw_dict._embedded
-            ._view._actions;
-          form_definition.group_list = form_result.raw_dict.group_list;
-          form_definition.title = "Create Document";
           return gadget.changeState({
-            doc: { title: document_title, portal_type: allowed_sub_types_list },
+            doc: { header_title: document_title,
+                   portal_type: allowed_sub_types_list },
             parent_portal_type: parent_portal_type,
-            action_options: options,
-            child_gadget_url: 'gadget_erp5_pt_form_dialog.html',
-            form_type: 'dialog',
-            form_definition: form_definition,
-            view: "view",
-            show_dialog: allowed_sub_types_list.length > 1
+            show_dialog: false
           });
         });
     })
@@ -96,21 +76,57 @@
           .push(function (form_view_gadget) {
             return form_view_gadget.render(gadget.state);
           });
-      } else {
-        // if there is only one sub portal type
-        // skip create document dialog rendering
-        return gadget.createDocument(gadget.state.doc.portal_type[0],
-                                     gadget.state.parent_portal_type
-                                     .replace(/ /g, '_').toLowerCase());
       }
+      // if no form, skip dialog assuming there is only one portal type
+      return gadget.createDocument(gadget.state.doc.portal_type[0],
+                                   gadget.state.parent_portal_type
+                                   .replace(/ /g, '_').toLowerCase());
+    })
+
+    .declareMethod("createDocument", function (portal_type,
+                                               parent_portal_type,
+                                               content) {
+      var gadget = this,
+        doc = {};
+      if (!content) {
+        doc.portal_type = portal_type;
+        doc.parent_relative_url = parent_portal_type;
+      } else {
+        doc = content;
+      }
+      if (!doc.title) {
+        doc.title = "Untitled document";
+      }
+      return gadget.jio_post(doc)
+        .push(function (id) {
+          return gadget.redirect({
+            command: 'display',
+            options: {
+              jio_key: id,
+              editable: true
+            }
+          });
+        });
+    })
+
+    .declareMethod("triggerSubmit", function () {
+      return this.element.querySelector('button[type="submit"]').click();
     })
 
     .allowPublicAcquisition('submitContent', function (options) {
       var gadget = this,
         content_dict = options[2];
-      return gadget.createDocument(content_dict.portal_type,
+      if (!content_dict.portal_type) {
+        content_dict.portal_type = gadget.state.doc.portal_type[0];
+      }
+      if (!content_dict.parent_relative_url) {
+        content_dict.parent_relative_url = gadget.state.parent_portal_type
+          .replace(/ /g, '_').toLowerCase();
+      }
+      return gadget.createDocument(gadget.state.doc.portal_type[0],
                                    gadget.state.parent_portal_type
-                                   .replace(/ /g, '_').toLowerCase());
+                                   .replace(/ /g, '_').toLowerCase(),
+                                   content_dict);
     });
 
 }(window, document, rJS));
