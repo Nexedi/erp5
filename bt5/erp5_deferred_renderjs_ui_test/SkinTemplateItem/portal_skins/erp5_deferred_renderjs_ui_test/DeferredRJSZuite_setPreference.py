c86020f17e56ab4d54429a1023b24fcdf07e4a5f
Romain Courteaud <romain@nexedi.com>
2019-09-23 15:39:07 +0000

    deferred rjs test setup pref

--- bt5/erp5_deferred_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_deferred_renderjs_ui_test/DeferredRJSZuite_setPreference.py
+++ bt5/erp5_deferred_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_deferred_renderjs_ui_test/DeferredRJSZuite_setPreference.py
@@ -1,4 +1,8 @@
 context.Zuite_setPreference('')
 preference = context.portal_preferences.erp5_ui_test_preference
+preference.edit(
+  preferred_report_style='ODS',
+  preferred_report_format='html'
+)
 
 return 'Set Deferred Preference Successfully.'
