b9ef8cb1d3d1e19eb7d9f20f3da0554a7e7c5f1b
Roque Porchetto <roque.porchetto@nexedi.com>
2019-09-23 12:02:04 +0200

    erp5_officejs_ooffice: migrate Only Office Spreadsheet app - corresponding test update - custom view to handle cloudoo only office

--- bt5/erp5_officejs_ui_test/SkinTemplateItem/portal_skins/erp5_officejs_ui_test/gadget_officejs_create_bad_document.js.js
+++ bt5/erp5_officejs_ui_test/SkinTemplateItem/portal_skins/erp5_officejs_ui_test/gadget_officejs_create_bad_document.js.js
@@ -11,12 +11,13 @@
       }),
         gadget = this;
       return storage.put('bad_document', {
-        'filename': 'test.xlsx',
+        'reference': 'bad_document_ooffice_upload',
         'title': 'Bad Document',
+        'filename': 'test.xlsx',
         'portal_type': 'Spreadsheet',
-        'mime_type': 'xlsx',
-        'modification_date': 'Fri, 17 Aug 2018 11:21:22 +0000',
-        'content_type': "application/x-asc-spreadsheet"
+        'parent_relative_url': "document_module",
+        'content_type': "application/x-asc-spreadsheet",
+        'modification_date': 'Fri, 17 Aug 2018 11:21:22 +0000'
       })
       .push(function (id) {
         return jIO.util.ajax({
@@ -41,6 +42,10 @@
       .push(function () {
         var div = document.createElement('div');
         div.textContent = 'Document Created';
+        gadget.element.appendChild(div);
+      }, function (error) {
+        var div = document.createElement('div');
+        div.textContent = 'Error creating document: ' + error.message;
         gadget.element.appendChild(div);
       });
     });
