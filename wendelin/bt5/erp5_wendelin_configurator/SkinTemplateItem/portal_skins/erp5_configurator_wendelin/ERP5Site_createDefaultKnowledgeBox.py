49f26b36dfd356fe2573c61bb3891fd817d84b24
Ivan Tyagov <ivan@nexedi.com>
2019-09-24 14:11:14 +0300

    Use proper gadget id.

--- wendelin/bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/ERP5Site_createDefaultKnowledgeBox.py
+++ wendelin/bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/ERP5Site_createDefaultKnowledgeBox.py
@@ -1,5 +1,5 @@
 knowledge_box = knowledge_pad.newContent(
   portal_type='Knowledge Box',
-  specialise='portal_gadgets/wendelin_info_gadget',
+  specialise='portal_gadgets/WendelinInformationGadget',
   activate_kw=activate_kw)
 knowledge_box.visible()
