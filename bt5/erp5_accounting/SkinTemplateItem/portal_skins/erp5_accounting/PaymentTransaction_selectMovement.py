aa016b54f958796ffca8a0654667d09ec504873d
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2019-09-27 15:30:05 +0000

    erp5_accounting: change payment movement selection criteria

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/PaymentTransaction_selectMovement.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/PaymentTransaction_selectMovement.py
@@ -1,9 +1,14 @@
 kw['parent_specialise_reference'] = 'default_payment_rule'
-kw['grand_parent_simulation_state'] = 'started'
+kw['grand_parent_simulation_state'] = ('started', 'stopped')
 kw['delivery_uid'] = None
 kw['left_join_list'] = ['delivery_uid']
 kw['select_dict'] = dict(delivery_uid=None)
-kw['group_by'] = ('uid',)
+
+# We assume that all simulation movements without a delivery are in planned or auto planned state.
+# By passing this, catalog should use an index on portal_type + simulation state.
+# XXX actually we do not even pass auto_planned to have only 1 value, because we just do not use
+# auto_planned state in this project.
+kw['simulation_state'] = ('planned', )
 
 kw['src__'] = src__ 
 return context.portal_catalog(**kw)
