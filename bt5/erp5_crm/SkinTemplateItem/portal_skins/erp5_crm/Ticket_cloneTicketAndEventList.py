4f19f40f3b73b52f6e0a57aa1ddbe854e347d8f2
Romain Courteaud <romain@nexedi.com>
2019-09-27 13:19:35 +0000

    [erp5_accounting/crm/project/trade] Activate actions for ERP5JS
    [erp5_core] Return 400 status code when updating a dialog

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_cloneTicketAndEventList.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Ticket_cloneTicketAndEventList.py
@@ -10,4 +10,4 @@ portal.portal_catalog.activate(activity='SQLQueue').searchAndActivate(
 
 portal_status_message = portal.Base_translateString('Events are beeing cloned in the background.')
 keep_items = {'portal_status_message':portal_status_message}
-new_ticket.Base_redirect(form_id=form_id, keep_items=keep_items)
+return new_ticket.Base_redirect(form_id=form_id, keep_items=keep_items)
