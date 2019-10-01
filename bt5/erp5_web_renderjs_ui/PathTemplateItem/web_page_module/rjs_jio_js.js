419f6d08f302adf2884daa01173d51d5eff17cc4
Klaus Woelfel <klaus@nexedi.com>
2019-10-01 19:31:56 +0200

    [erp5_core/erp5_web_rjs_ui] jIO 3.41.0

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_jio_js.js
@@ -8885,7 +8885,7 @@ return new Parser;
         var ceilHeapSize = function (v) {
             // The asm.js spec says:
             // The heap object's byteLength must be either
-            // 2^n for n in [12, 24) or 2^24 * n for n ≥ 1.
+            // 2^n for n in [12, 24) or 2^24 * n for n â‰¥ 1.
             // Also, byteLengths smaller than 2^16 are deprecated.
             var p;
             // If v is smaller than 2^16, the smallest possible solution
@@ -15886,26 +15886,50 @@ return new Parser;
  */
 
 /*jslint nomen: true*/
-/*global jIO, RSVP, DOMParser, XMLSerializer*/
+/*global document, jIO, RSVP, DOMParser, XMLSerializer*/
 (function (jIO, RSVP, DOMParser, XMLSerializer) {
   "use strict";
 
   var parser = new DOMParser(),
     serializer = new XMLSerializer();
 
-  function makeXmlRpcRequest(file, from, to) {
+  function makeXmlRpcRequest(file, from, to, conversion_kw) {
     var xml = parser.parseFromString(
       '<?xml version="1.0" encoding="UTF-8"?><methodCall>' +
         '<methodName>convertFile</methodName><params>' +
         '<param><value><string></string></value></param>' +
         '<param><value><string></string></value></param>' +
-        '<param><value><string></string></value></param></params></methodCall>',
+        '<param><value><string></string></value></param>' +
+        '<param><struct></struct></param>' +
+        '</params></methodCall>',
       'text/xml'
     ),
+      elt,
+      member,
+      name,
+      value,
+      key,
+      struct = xml.getElementsByTagName('struct'),
       string_list = xml.getElementsByTagName('string');
     string_list[0].textContent = file;
     string_list[1].textContent = from;
     string_list[2].textContent = to;
+    if (conversion_kw) {
+      for (key in conversion_kw) {
+        if (conversion_kw.hasOwnProperty(key)) {
+          elt = document.createElementNS(null, conversion_kw[key][1]);
+          elt.textContent = conversion_kw[key][0];
+          value = document.createElementNS(null, "value");
+          value.appendChild(elt);
+          name = document.createElementNS(null, "name");
+          name.textContent = key;
+          member = document.createElementNS(null, "member");
+          member.appendChild(name);
+          member.appendChild(value);
+          struct[0].appendChild(member);
+        }
+      }
+    }
     return serializer.serializeToString(xml);
   }
 
@@ -15914,7 +15938,7 @@ return new Parser;
    * from a format to another
    * return converted blob.
    **/
-  function convert(url, blob, from, to) {
+  function convert(url, blob, from, to, conversion_kw) {
     return new RSVP.Queue()
       .push(function () {
         return jIO.util.readBlobAsDataURL(blob);
@@ -15926,7 +15950,8 @@ return new Parser;
           data: makeXmlRpcRequest(
             result.target.result.split('base64,')[1],
             from,
-            to
+            to,
+            conversion_kw
           )
         });
       })
@@ -15976,11 +16001,13 @@ return new Parser;
     return this._sub_storage.getAttachment.apply(this._sub_storage, arguments);
   };
 
-  CloudoooStorage.prototype.putAttachment = function (id, name, blob) {
+  CloudoooStorage.prototype.putAttachment = function (id, name, blob,
+    conversion_kw
+    ) {
     var storage = this;
     return storage.get(id)
       .push(function (doc) {
-        return convert(storage._url, blob, doc.from, doc.to);
+        return convert(storage._url, blob, doc.from, doc.to, conversion_kw);
       })
       .push(function (converted_blob) {
         return storage._sub_storage.putAttachment(id, name, converted_blob);
