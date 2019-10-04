0a66e5148d7da78c380fc3e3e2095e1c413f0060
Ivan Tyagov <ivan@nexedi.com>
2019-10-04 09:26:13 +0300

    Default ERP5 instanciation uses following schema for naming all zope nodes:   <family_name>-0..9 when number of nodes <= 10   <family_name>-01..09 (or n) when number of nodes > 10
    In scalability tests instance keeps track of old used nodes and when nodes > 10
    will pick by default sort "activity-0" which is NO longer an existing zope process.
    This is an ugly fix and real woraround is to have something like activities-000x schema by default

--- bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_setUpActivityTool.py
+++ bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_setUpActivityTool.py
@@ -20,6 +20,15 @@ if distribution_node_list:
 
 if distribution_node is None:
   distribution_node = activity_node_list[0]
+  # default ERP5 instanciation uses following schema for naming all zope nodes
+  # activities-0..9 when number of nodes <= 10
+  # activites-01..09 (or n) when number of nodes > 10
+  # in scalability tests instance keeps track of old used nodes and when nodes > 10 
+  # will pick by default sort "activity-0" which is NO longer an existing zope process
+  # this is an ugly fix and real woraround is to have something like activities-000x schema by default
+  if distribution_node == "activities-0" and "activities-00" in activity_node_list:
+    # give precedence
+    distribution_node = "activities-00"
 
 context.portal_activities.manage_addToProcessingList(activity_node_list)
 context.portal_activities.manage_setDistributingNode(distribution_node)
