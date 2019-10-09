2f1befb12204120a62cd578ae23aaf4dcd824575
Romain Courteaud <romain@nexedi.com>
2019-10-09 08:17:43 +0000

    XXX [erp5_hal_json_style] Hack on Base_redirect
    XXX hal send the expected redirect url
    
    hal fixup
    
    hal fixup Base_redirect
    
    hal fixup redirect on fake direct script action

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
@@ -55,6 +55,28 @@ result_dict = {
     }
   }
 }
+
+# XXX COPY PASTED (and buggy)
+# Move to ze big script instead
+site_root = context.getWebSectionValue()
+
+if site_root is not None:
+
+  def getRealRelativeUrl(document):
+    return '/'.join(document.getPortalObject().portal_url.getRelativeContentPath(document))
+
+  traverse_generator = "%(root_url)s/%(script_id)s?mode=traverse" + \
+                        "&relative_url=%(relative_url)s&view=%(view)s"
+
+  if (form_id is not None) and (form_id != 'Base_viewFakePythonScriptActionForm'):
+    link_url = traverse_generator % {
+            "root_url": site_root.absolute_url(),
+            "script_id": 'ERP5Document_getHateoas',
+            "relative_url": getRealRelativeUrl(context).replace("/", "%2F"),
+            "view": form_id,
+    }
+    result_dict['_links']['location'] = {'href': link_url}
+
 result = json.dumps(result_dict, indent=2)
 if abort_transaction:
   response.write(result)
