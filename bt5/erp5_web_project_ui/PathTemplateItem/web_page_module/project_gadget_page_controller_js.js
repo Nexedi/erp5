c78dc8f2101c402d15caa61e5ab40db175f800c0
Roque Porchetto <roque.porchetto@nexedi.com>
2019-10-11 12:18:09 +0000

    erp5_web_project_ui: front page redirects to custom project module - filtered, sorted, custom columns

---
+++ bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_controller_js.js
@@ -0,0 +1,35 @@
+/*global document, window, rJS */
+/*jslint nomen: true, indent: 2, maxerr: 10, maxlen: 90 */
+(function (document, window, rJS) {
+  "use strict";
+
+  rJS(window)
+
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("redirect", "redirect")
+    //.declareAcquiredMethod("getSetting", "getSetting")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      return gadget.redirect({
+        'command': 'display',
+        'options': {
+          'page': 'form',
+          'editable': 1,
+          'jio_key': 'project_module',
+          'view': 'view',
+          'field_listbox_sort_list:json': [["title", "ascending"]],
+          'field_listbox_column_list:json': ["title",
+                                             "default_destination_section_title"],
+          'extended_search': 'translated_validation_state_title:  "Started"'
+        }
+      });
+    });
+
+}(document, window, rJS));
