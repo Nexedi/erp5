86b1843bb19f6aabeda52027c7efde22106460fb
Roque Porchetto <roque.porchetto@nexedi.com>
2019-10-11 09:44:27 +0000

    erp5_web_project_ui: initial commit for project.nexedi.net - new skin folder with project quick view form, scripts and gadgets - front page redirects to custom project module

---
+++ bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_controller_js.js
@@ -0,0 +1,40 @@
+/*global document, window, rJS */
+/*jslint nomen: true, indent: 2, maxerr: 10, maxlen: 90 */
+(function (document, window, rJS) {
+  "use strict";
+
+  rJS(window)
+
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("redirect", "redirect")
+    .declareAcquiredMethod("getSetting", "getSetting")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      return gadget.getSetting("hateoas_url")
+        .push(function (hateoas_url) {
+          return gadget.redirect({
+            'command': 'display',
+            'options': {
+              'page': 'form',
+              'editable': 0,
+              'jio_key': 'project_module',
+              'view': hateoas_url +
+              '/ERP5Document_getHateoas?mode=traverse&relative_url=' +
+              'project_module&view=ProjectModule_viewProjectManagementList',
+              'field_listbox_sort_list:json': [["title", "ascending"]],
+              'field_listbox_column_list:json': ["title",
+                                                 "default_destination_section_title"],
+              'extended_search': 'selection_domain_state_project_domain:  "started"'
+            }
+          });
+        });
+    });
+
+}(document, window, rJS));
