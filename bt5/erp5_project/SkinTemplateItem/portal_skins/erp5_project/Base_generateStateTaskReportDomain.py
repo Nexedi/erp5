3fdd7a0a20fe3bbb73dedcb9690916e429a64d56
Roque Porchetto <roque.porchetto@nexedi.com>
2019-10-22 14:43:16 +0000

    erp5_project: domains for project views

---
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskReportDomain.py
@@ -0,0 +1,18 @@
+domain_list = []
+
+started_domain = parent.generateTempDomain(id='started')
+
+started_domain.edit(title="Confirmed",
+                    criterion_property_list=['simulation_state'])
+started_domain.setCriterion('simulation_state', identity='confirmed')
+domain_list.append(started_domain)
+
+not_started_domain = parent.generateTempDomain(id='not_started')
+
+not_started_domain.edit(title="Not Confirmed",
+                       criterion_property_list=['simulation_state'])
+not_started_domain.setCriterion('simulation_state', identity=['delivered', 'stopped', 'draft'])
+
+domain_list.append(not_started_domain)
+
+return domain_list
