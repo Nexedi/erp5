a4136edf38ce6ba04b782c1c9a6542be203652a1
Jérome Perrin <jerome@nexedi.com>
2019-10-29 06:41:42 +0000

    monaco_editor: increase debounce timeout for pylint checks XXX
    on very large python files (>1000 lines) sometimes they queue up and we
    have to wait for all requests that were queued by zope.
    
    XXX maybe this does not happen when accessing through haproxy/apache, I
    am observing this when hitting zope directly

--- product/ERP5Type/patches/SourceCodeEditorZMI.py
+++ product/ERP5Type/patches/SourceCodeEditorZMI.py
@@ -108,6 +108,7 @@ def manage_page_footer(self):
                               textarea_selector=textarea_selector,
                               portal_url=portal_url,
                               bound_names=bound_names,
+                              script_name=document.getId(),
                               mode=mode).encode('utf-8'))
 
   return default
