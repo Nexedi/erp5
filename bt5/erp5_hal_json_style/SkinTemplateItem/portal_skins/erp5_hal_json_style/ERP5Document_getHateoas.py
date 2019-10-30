583bdfd267b4ac71c30a1b8462291b013bdb15e0
Romain Courteaud <romain@nexedi.com>
2019-10-30 11:12:17 +0000

    hal: check that a listbox cell field is enabled

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -1934,6 +1934,10 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
       for select in select_list:
         contents_item[select] = {}
         editable_field = editable_field_dict.get(select, None)
+        if editable_field is not None:
+          # Check if the field is enabled
+          if not editable_field.get_value('enabled', REQUEST=REQUEST):
+            editable_field = None
         default_field_value = Listbox_getBrainValue(
           brain,
           brain_document,
