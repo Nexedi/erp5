0fb7f3e397cf0561ff9cdd73776dc2ec7a13d655
Bryton Lacquement <bryton.lacquement@nexedi.com>
2019-10-30 13:21:35 +0100

    erp5_forge: fix

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_toolbox/BusinessTemplate_getDiffWithZODBAsHTML.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_toolbox/BusinessTemplate_getDiffWithZODBAsHTML.py
@@ -1,8 +1,11 @@
+from transaction import doom
+from zExceptions import Success
+
 def respond(v):
   if REQUEST is None:
     return v
   REQUEST.RESPONSE.setHeader("Content-Type", "text/html")
-  REQUEST.RESPONSE.write(v)
-  raise ValueError("Abort Transaction")
+  doom()
+  raise Success(v)
 
 return respond(context.Base_formatDiffObjectListToHTML(context.BusinessTemplate_getDiffObjectListFromZODB(detailed=detailed)))
--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_toolbox/BusinessTemplate_getDiffWithZODBAsText.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_toolbox/BusinessTemplate_getDiffWithZODBAsText.py
@@ -1,7 +1,10 @@
+from transaction import doom
+from zExceptions import Success
+
 def respond(v):
   if REQUEST is None:
     return v
-  REQUEST.RESPONSE.write(v)
-  raise ValueError("Abort Transaction")
+  doom()
+  raise Success(v)
 
 return respond(context.Base_formatDiffObjectListToText(context.BusinessTemplate_getDiffObjectListFromZODB(detailed=detailed)))
