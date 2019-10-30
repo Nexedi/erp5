bf625f3a2457782b4e4ab46376d90dd75a7dfd52
Ivan Tyagov <ivan@nexedi.com>
2019-10-30 12:47:57 +0200

    erp5.util: Release erp5.util (0.4.65)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.4.64'
+version = '0.4.65'
 name = 'erp5.util'
 long_description = open("README.erp5.util.txt").read() + "\n"
 
