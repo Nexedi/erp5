9f4af817fabba15493314727b5b17f77dd1c367e
Gabriel Monnerat <gabriel@nexedi.com>
2019-11-05 17:24:09 +0000

    erp5_document_scanner: We need to create active process manager
    Normal users can not call it directly

--- erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_storeDocumentFromCameraInActiveProcess.py
+++ erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_storeDocumentFromCameraInActiveProcess.py
@@ -15,21 +15,18 @@ if not active_preference:
 if active_preference and preferred_cropped_canvas_data:
   active_preference.setPreferredCroppedCanvasData(preferred_cropped_canvas_data)
 
-if active_process_url:
-  active_process = portal.restrictedTraverse(active_process_url)
-
 if not image_str:
   if batch_mode:
-    return active_process
+    return None
 
   return context.Base_renderForm('Base_viewUploadDocumentFromCameraDialog',
                                message=translateString('Nothing to capture'))
 
-if not active_process_url:
-  active_process = portal.portal_activities.newActiveProcess()
-  context.REQUEST.form["your_active_process_url"] = active_process.getRelativeUrl()
+active_process = context.Base_postDataToActiveResult(
+  active_process_url,
+  image_str)
 
-active_process.postActiveResult(detail=image_str)
+context.REQUEST.form["your_active_process_url"] = active_process.getRelativeUrl()
 context.REQUEST.form.pop("field_your_document_scanner_gadget")
 context.REQUEST.form.pop('document_scanner_gadget')
 
