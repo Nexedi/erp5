fdf0a96fd1cbe797be5f5fef8bbeef33600c1425
Gabriel Monnerat <gabriel@nexedi.com>
2019-11-06 21:56:50 +0000

    erp5_document_scanner: Use proxy role as manager where is required

--- erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_uploadDocumentFromCamera.py
+++ erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_uploadDocumentFromCamera.py
@@ -4,23 +4,16 @@ class StringIOWithFileName(StringIO):
   filename =  "{}.pdf".format(
     kw.get("title") or DateTime().strftime('%d-%m-%Y_%Hh%M'))
 
-
 portal = context.getPortalObject()
-pdf_data_list = []
-image_module = portal.image_module
 active_process = portal.restrictedTraverse(active_process_url)
 
-for result in active_process.getResultList():
-  pdf_data_list.append(
-    image_module.newContent(data=result.detail,
-                            portal_type="Image",
-                            temp_object=True).convert(format="pdf")[1])
-
+pdf_data_list = context.Base_getTempImageList(active_process)
 pdf_data = context.ERP5Site_mergePDFList(pdf_data_list=pdf_data_list)
 file_object = StringIOWithFileName(pdf_data)
 
 doc = context.Base_contribute(file=file_object,
                               batch_mode=True,
+                              redirect_to_document=False,
                               follow_up_list=[context.getRelativeUrl(),],
                               **kw)
 
@@ -36,4 +29,4 @@ else:
 for action in action_list:
   getattr(doc, action)()
 
-portal.portal_activities.manage_delObjects(ids=[active_process.getId(),])
+context.Base_removeActiveProcessFromActivityTool(active_process)
