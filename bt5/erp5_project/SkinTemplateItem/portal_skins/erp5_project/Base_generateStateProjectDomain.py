30ce7ab86b2a8ba9cf8096ee8b5fe7231c1eaa50
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-06 17:10:53 +0000

    erp5_project: refactor in domain scripts

--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateProjectDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateProjectDomain.py
@@ -1,19 +1,4 @@
-domain_list = []
-
-started_domain = parent.generateTempDomain(id='started')
-
-started_domain.edit(title="Started",
-                    criterion_property_list=['validation_state'])
-started_domain.setCriterion('validation_state', identity='validated')
-domain_list.append(started_domain)
-
-not_started_domain = parent.generateTempDomain(id='not_started')
-
-not_started_domain.edit(title="Not Started",
-                       criterion_property_list=['validation_state'],
-                       identity_criterion=dict(validation_state=('invalidated')))
-not_started_domain.setCriterion('validation_state', identity=['draft', 'invalidated', 'suspended'])
-
-domain_list.append(not_started_domain)
-
-return domain_list
+return [
+         context.Base_generateDomain(parent, 'started', 'Started', 'validation_state', 'validated'),
+         context.Base_generateDomain(parent, 'not_started', 'Not Started', 'validation_state', ['draft', 'invalidated', 'suspended'])
+       ]
--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskDomain.py
@@ -1,26 +1,5 @@
-domain_list = []
-
-started_domain = parent.generateTempDomain(id='confirmed')
-
-started_domain.edit(title="Confirmed",
-                    criterion_property_list=['simulation_state'])
-started_domain.setCriterion('simulation_state', identity='confirmed')
-domain_list.append(started_domain)
-
-open_domain = parent.generateTempDomain(id='not_confirmed')
-
-open_domain.edit(title="Not Confirmed",
-                       criterion_property_list=['simulation_state'])
-open_domain.setCriterion('simulation_state', identity=['planned', 'ordered', 'draft'])
-
-domain_list.append(open_domain)
-
-cancelled_domain = parent.generateTempDomain(id='cancelled')
-
-cancelled_domain.edit(title="Cancelled",
-                       criterion_property_list=['simulation_state'])
-cancelled_domain.setCriterion('simulation_state', identity=['cancelled', 'deleted'])
-
-domain_list.append(cancelled_domain)
-
-return domain_list
+return [
+         context.Base_generateDomain(parent, 'confirmed', 'Confirmed', 'simulation_state', 'confirmed'),
+         context.Base_generateDomain(parent, 'not_confirmed', 'Not Confirmed', 'simulation_state', ['planned', 'ordered', 'draft']),
+         context.Base_generateDomain(parent, 'cancelled', 'Cancelled', 'simulation_state', ['cancelled', 'deleted'])
+       ]
