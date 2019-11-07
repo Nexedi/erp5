5b5728a799d81141ea4912ec8440981030f19840
Jérome Perrin <jerome@nexedi.com>
2019-11-07 10:55:32 +0100

    patches/pylint: fix regression with namedtuples
    with namedtuples and unicode literals enabled, our patch break with:
    
      Module py2stdlib, line 266, in infer_named_tuple
        ''' % {'name': name, 'fields': attributes})
      Module Products.ERP5Type.patches.pylint, line 74, in string_build
        encoding = _guess_encoding(data)
      Module astroid.builder, line 65, in _guess_encoding
        if string.startswith('\xef\xbb\xbf'):
    UnicodeDecodeError: 'ascii' codec can't decode byte 0xef in position 0: ordinal not in range(128)

--- product/ERP5Type/patches/pylint.py
+++ product/ERP5Type/patches/pylint.py
@@ -70,6 +70,10 @@ def string_build(self, data, modname='', path=None):
       module.file_bytes = data.encode('utf-8')
       return self._post_build(module, 'utf-8')
     """
+    if isinstance(data, unicode):
+        # when used internally, like when inferring namedtuples, this can be called
+        # with unicode.
+        data = data.encode('utf-8')
     encoding = _guess_encoding(data)
     if encoding is None:
         # Encoding not defined in the source file, assuming utf-8...
