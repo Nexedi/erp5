f8ff4674c225749b4ad8fe3666910c31c9379ef9
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-07 13:36:13 +0000

    erp5_web_project_ui: using new project management list form

--- bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_controller_js.js
+++ bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_controller_js.js
@@ -9,7 +9,7 @@
     // Acquired methods
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("redirect", "redirect")
-    //.declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("getSetting", "getSetting")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -17,19 +17,24 @@
 
     .declareMethod("render", function (options) {
       var gadget = this;
-      return gadget.redirect({
-        'command': 'display_with_history',
-        'options': {
-          'page': 'form',
-          'editable': 0,
-          'jio_key': 'project_module',
-          'view': 'view',
-          'field_listbox_sort_list:json': [["title", "ascending"]],
-          'field_listbox_column_list:json': ["title",
-                                             "default_destination_section_title"],
-          'extended_search': 'selection_domain_state_project_domain:  "started"'
-        }
-      });
+      return gadget.getSetting("hateoas_url")
+        .push(function (hateoas_url) {
+          return gadget.redirect({
+            'command': 'display',
+            'options': {
+              'page': 'form',
+              'editable': 0,
+              'jio_key': 'project_module',
+              'view': hateoas_url +
+              '/ERP5Document_getHateoas?mode=traverse&relative_url=' +
+              'project_module&view=ProjectManagement_viewProjectList',
+              'field_listbox_sort_list:json': [["title", "ascending"]],
+              'field_listbox_column_list:json': ["title",
+                                                 "default_destination_section_title"],
+              'extended_search': 'selection_domain_state_project_domain:  "started"'
+            }
+          });
+        });
     });
 
 }(document, window, rJS));
