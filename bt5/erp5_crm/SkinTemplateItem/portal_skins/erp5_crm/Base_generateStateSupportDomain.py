5e39704b3f49e55784e89934e0dd982dfbcf42f0
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-08 12:12:57 +0000

    erp5_crm: fix identation in domain scripts

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Base_generateStateSupportDomain.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Base_generateStateSupportDomain.py
@@ -1,5 +1,5 @@
 return [
-         context.Base_generateDomain(parent, 'validated', 'Validated', 'simulation_state', 'validated'),
-         context.Base_generateDomain(parent, 'not_validated', 'Not Validated', 'simulation_state', ['submitted', 'suspended', 'draft', 'invalidated']),
-         context.Base_generateDomain(parent, 'cancelled', 'Cancelled', 'simulation_state', ['cancelled', 'deleted'])
-       ]
+  context.Base_generateDomain(parent, 'validated', 'Validated', 'simulation_state', 'validated'),
+  context.Base_generateDomain(parent, 'not_validated', 'Not Validated', 'simulation_state', ['submitted', 'suspended', 'draft', 'invalidated']),
+  context.Base_generateDomain(parent, 'cancelled', 'Cancelled', 'simulation_state', ['cancelled', 'deleted'])
+]
