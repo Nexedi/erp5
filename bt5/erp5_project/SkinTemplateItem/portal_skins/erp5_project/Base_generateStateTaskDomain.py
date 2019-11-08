543f0f99113eb7819035e35abb6b6a415caf113f
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-08 12:12:26 +0000

    erp5_project: fix identation in domain scripts

--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskDomain.py
@@ -1,5 +1,5 @@
 return [
-         context.Base_generateDomain(parent, 'confirmed', 'Confirmed', 'simulation_state', 'confirmed'),
-         context.Base_generateDomain(parent, 'not_confirmed', 'Not Confirmed', 'simulation_state', ['planned', 'ordered', 'draft']),
-         context.Base_generateDomain(parent, 'cancelled', 'Cancelled', 'simulation_state', ['cancelled', 'deleted'])
-       ]
+  context.Base_generateDomain(parent, 'confirmed', 'Confirmed', 'simulation_state', 'confirmed'),
+  context.Base_generateDomain(parent, 'not_confirmed', 'Not Confirmed', 'simulation_state', ['planned', 'ordered', 'draft']),
+  context.Base_generateDomain(parent, 'cancelled', 'Cancelled', 'simulation_state', ['cancelled', 'deleted'])
+]
--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskReportDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskReportDomain.py
@@ -1,5 +1,5 @@
 return [
-         context.Base_generateDomain(parent, 'started', 'Confirmed', 'simulation_state', 'confirmed'),
-         context.Base_generateDomain(parent, 'not_started', 'Not Confirmed', 'simulation_state', 'draft'),
-         context.Base_generateDomain(parent, 'closed', 'Closed', 'simulation_state', ['delivered', 'stopped'])
-       ]
+  context.Base_generateDomain(parent, 'started', 'Confirmed', 'simulation_state', 'confirmed'),
+  context.Base_generateDomain(parent, 'not_started', 'Not Confirmed', 'simulation_state', 'draft'),
+  context.Base_generateDomain(parent, 'closed', 'Closed', 'simulation_state', ['delivered', 'stopped'])
+]
