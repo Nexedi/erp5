7156aff229a335500f229615ed500d3d67782e50
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-08 11:47:36 +0100

    [HARDCODED] undo change in test init

--- tests/__init__.py
+++ tests/__init__.py
@@ -38,7 +38,6 @@ class _ERP5(ERP5TypeTestSuite):
         glob('%s/product/*/tests/test*.py' % path) +
         glob('%s/bt5/*/TestTemplateItem/test*.py' % path) +
         glob('%s/bt5/*/TestTemplateItem/portal_components/test.*.test*.py' % path)):
-	#glob('%s/bt5/erp5_officejs_ui_test/TestTemplateItem/portal_components/test.*.test*Project*.py' % path)):
       component_re_match = component_re.match(test_path)
       if component_re_match is not None:
         test_case = "%s:%s" % (component_re_match.group(1),
