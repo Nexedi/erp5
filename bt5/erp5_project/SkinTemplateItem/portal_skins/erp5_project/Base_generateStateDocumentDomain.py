b394f1502f4e5eff18830a44cc8c84de48cab0c2
Roque Porchetto <roque.porchetto@nexedi.com>
2019-11-11 09:22:14 +0000

    erp5_project: new domain for documents

---
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateDocumentDomain.py
@@ -0,0 +1,5 @@
+return [
+  context.Base_generateDomain(parent, 'confirmed', 'Confirmed', 'validation_state', ['shared', 'released', 'published', 'shared_alive', 'released_alive', 'published_alive']),
+  context.Base_generateDomain(parent, 'not_confirmed', 'Not Confirmed', 'validation_state', ['submitted', 'draft', 'requested', 'assigned', 'translated', 'split']),
+  context.Base_generateDomain(parent, 'archived_discarded', 'Archived/Discarded', 'validation_state', ['archived', 'deleted', 'cancelled', 'hidden'])
+]
