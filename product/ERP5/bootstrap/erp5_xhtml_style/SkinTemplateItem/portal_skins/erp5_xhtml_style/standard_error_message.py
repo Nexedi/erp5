d0b1824bf2c1eeebf21448d9129924e9f7363693
Jérome Perrin <jerome@nexedi.com>
2019-11-12 08:32:27 +0000

    xhtml_style: set error detail as response headers
    This should make it easier for web services calls to know what went
    wrong when there's a server side error.

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/standard_error_message.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/standard_error_message.py
@@ -5,6 +5,15 @@
   root is defined then it renders the error from portal.
 """
 from zExceptions import Unauthorized
+request = container.REQUEST
+
+# set response header for ERP5JS display of errors
+request.response.setHeader('X-ERP5-Error-Type', kw.get('error_type'))
+request.response.setHeader('X-ERP5-Error-Value', kw.get('error_value'))
+error_log_url = kw.get('error_log_url')
+if error_log_url:
+  request.response.setHeader('X-ERP5-Error-Log-URL', error_log_url)
+
 try:
   return context.standard_error_message_template(*args, **kw)
 except Unauthorized:
@@ -12,7 +21,7 @@ except Unauthorized:
 try:
   # Note: - user can be unauthorized to getWebSiteValue() from this context
   #       - "web_site_value" comes from erp5.Document.WebSite.WEBSITE_KEY (unauthorized to import)
-  web_site_split_path = context.REQUEST.get("web_site_value")
+  web_site_split_path = request.get("web_site_value")
   if web_site_split_path:
     web_site_value = context.getPortalObject().restrictedTraverse(web_site_split_path, None)
     if web_site_value is not None:
