c406085db1ee9dd37a6f6faccb791435a25d72e2
Jérome Perrin <jerome@nexedi.com>
2019-11-12 05:13:04 +0000

    core: display a warning when errors found while editing component
    This should make it easier for developer to notice that the changes were
    refused when some errors were found in the code, especially when they
    are using the text editor in full-page mode.

--- product/ERP5/bootstrap/erp5_core/WorkflowTemplateItem/portal_workflow/dynamic_class_generation_interaction_workflow/scripts/Component_setModifiedStateIfValidated.py
+++ product/ERP5/bootstrap/erp5_core/WorkflowTemplateItem/portal_workflow/dynamic_class_generation_interaction_workflow/scripts/Component_setModifiedStateIfValidated.py
@@ -1,3 +1,8 @@
+from Products.ERP5Type.Message import translateString
 obj = state_change['object']
 if obj.getValidationState() == 'validated':
   obj.modify()
+  container.REQUEST.set(
+      'portal_status_message',
+      translateString("Errors found in source code"))
+  container.REQUEST.set('portal_status_level', 'warning')
