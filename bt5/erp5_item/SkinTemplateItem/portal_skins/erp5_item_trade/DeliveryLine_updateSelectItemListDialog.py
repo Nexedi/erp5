a6207d11a23e0646e3ac9a08c8d25538b96be73f
Romain Courteaud <romain@nexedi.com>
2019-11-13 17:59:02 +0100

    WIP BROKEN [erp5[_mrp]_item] new action
    Do not access form submission REQUEST from the listbox list method,
    as it is rendered asynchronously in ERP5JS

--- bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_updateSelectItemListDialog.py
+++ bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_updateSelectItemListDialog.py
@@ -1,9 +1,14 @@
 request = context.REQUEST
 portal = context.getPortalObject()
+Base_translateString = portal.Base_translateString
 
 # FIXME: this is a workaround, because if listbox is present in request.form, 
 #   editable fields will be empty when re-displaying the dialog.
 request.form.pop('listbox', None)
 request.other.pop('listbox', None)
 
-return getattr(context, dialog_id)(**kw)
+context.Base_updateDialogForm()
+return context.Base_renderForm(
+  dialog_id,
+  keep_items={'portal_status_message': Base_translateString('Updated')}
+)
