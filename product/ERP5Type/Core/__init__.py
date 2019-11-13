2260fc70fcdf0e64a951fbf0ce151e3f7725292a
Julien Muchembled <jm@nexedi.com>
2019-11-13 14:49:22 +0100

    Revert "Make sure CMFCategory product is imported before ERP5Type.Core.Folder"
    This reverts commit 0af3e8f670e72218a80120c64a6be4cf4fb26a0b.
    
    This is not required anymore since ERP5Type.PropertySheet is a dynamic module
    (commit 9ddcbfc1a30a2c750340f2ad8dde3f0a5999f38a).

--- product/ERP5Type/Core/__init__.py
+++
@@ -1,5 +0,0 @@
-# Make sure CMFCategory product is imported before ERP5Type.Core.Folder
-# because Folder needs PropertySheet.CategoryCore
-# This is important for ZEO server which may need to import conflict-resolution
-# classes from products that depend (at 'import' time) directly on Folder.
-import Products.CMFCategory
