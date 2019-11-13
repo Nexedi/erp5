a687d8338aaf97124e54a67f87bb0256123c2cd2
Jérome Perrin <jerome@nexedi.com>
2019-11-13 04:28:37 +0100

    preferences: clear cache in a more clever way
    Introduce something similar to "cache cookie", but more suitable for
    preferences that depend both on the site configuration and user
    preferences.
    
    We store for each user a value in _preference_cache and we modify this
    value each time user change his user preference. This value is then
    used part of the a cache key.
    The same mechanism happen with a global value for site and group
    preferences.
    
    This way, when one user modify their preference this does not reset
    cache for every user.
    
    Because we have proper invalidation, increase the case duration by using
    erp5_ui_short instead of erp5_ui_long

--- product/ERP5/bootstrap/erp5_core/WorkflowTemplateItem/portal_workflow/preference_interaction_workflow/scripts/ERP5Site_clearPreferrenceCache.py
+++ product/ERP5/bootstrap/erp5_core/WorkflowTemplateItem/portal_workflow/preference_interaction_workflow/scripts/ERP5Site_clearPreferrenceCache.py
@@ -1,13 +1,2 @@
-"""Clear caches used by methods of this preference
-
-# TODO: clear different caches according to the preference priority
-# TODO: (XXX) currently, if one use enables / disables a cache, caches
-        for all other users are reset. This is not good for a system
-        in which users do a lot of preference validation. A better solution
-        is needed for this. But it is not easy because the concept of
-        "per user cache" has been proven to be ambiguous or useless.
-        In theory, a solution could consist in using more keys to
-        select caches or to delete "manually" certain cache keys.
-"""
 portal = sci['object'].getPortalObject()
-portal.portal_caches.clearCache(cache_factory_list=('erp5_ui_short',))
+portal.portal_preferences.clearCache(sci['object'])
