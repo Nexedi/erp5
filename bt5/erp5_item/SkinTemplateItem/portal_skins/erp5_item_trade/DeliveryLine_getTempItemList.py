27d62e95b27640c629256a30b00ca404c36857be
Romain Courteaud <romain@nexedi.com>
2019-11-13 17:59:02 +0100

    WIP BROKEN [erp5[_mrp]_item] new action
    Do not access form submission REQUEST from the listbox list method,
    as it is rendered asynchronously in ERP5JS

--- bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_getTempItemList.py
+++ bt5/erp5_item/SkinTemplateItem/portal_skins/erp5_item_trade/DeliveryLine_getTempItemList.py
@@ -1,14 +1,9 @@
 from Products.ERP5Type.Document import newTempDeliveryLine
 portal = context.getPortalObject()
 
-try:
-  count = int(context.REQUEST.get('field_your_item_count', ''))
-except ValueError:
-  count = 10
-
 result = []
 
-for i in range(count):
+for i in range(item_count):
   obj = newTempDeliveryLine(portal, id="tmp_item_%s" % i, uid="new_item_%s" % i)
   result.append(obj)
 
