b022f1016ce35331042c796ee22f90fdca8f5950
Gabriel Monnerat <gabriel@nexedi.com>
2019-11-14 17:28:01 +0000

    erp5_web_renderjs_ui: Extend code to support submit dialog with custom method

---
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/gadget_submit_button.js.js
@@ -0,0 +1,35 @@
+/*global window, rJS, RSVP, jIO, console, document */
+/*jslint nomen: true, maxlen:80, indent:2*/
+(function (rJS, jIO, RSVP, window, document) {
+  "use strict";
+
+  rJS(window)
+    .declareMethod('render', function (options) {
+      return this.changeState(options);
+    })
+
+    .declareMethod('getContent', function () {
+      var result = {};
+      result[this.state.key] = 'forbarcontent';
+      return result;
+    })
+
+    .onStateChange(function () {
+      var button = document.createElement('button');
+      button.type = 'button';
+      button.textContent = this.state.title;
+      button.name = 'action_custom';
+      this.element.innerHTML = '';
+      this.element.appendChild(button);
+    })
+
+    .declareAcquiredMethod(
+      "submitDialogWithCustomDialogMethod", "submitDialogWithCustomDialogMethod")
+    .onEvent('click', function click(evt) {
+      if (evt.target.type === "button") {
+        evt.preventDefault();
+        return this.submitDialogWithCustomDialogMethod(this.state.python_script_id);
+      }
+    }, false, false);
+
+}(rJS, jIO, RSVP, window, document));
