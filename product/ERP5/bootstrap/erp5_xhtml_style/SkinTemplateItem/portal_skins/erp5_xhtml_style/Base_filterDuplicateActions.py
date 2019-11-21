b2c946644a298d8bb78b9da2bf5adc6ae1b56612
Romain Courteaud <romain@nexedi.com>
2019-11-21 15:22:07 +0100

    [erp5_core/xhtml_style] Handle xhtml only view action

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_filterDuplicateActions.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_filterDuplicateActions.py
@@ -8,16 +8,21 @@ The script must be called on the context of the document.
 from Products.ERP5Type.Cache import CachingMethod
 def filterDuplicateActions(actions):
   new_actions = {}
-
   for action_category, action_list in actions.items():
-    existing_actions = set()
-    new_actions[action_category] = []
+    if action_category == 'object_onlyxhtml_view':
+      action_category = 'object_view'
+
+    new_actions.setdefault(action_category, [])
+    existing_actions = set([x['id'] for x in new_actions[action_category]])
     keep_action = new_actions[action_category].append
 
     for action in action_list:
       if action['id'] not in existing_actions:
         existing_actions.add(action['id'])
         keep_action(action)
+    if (action_category == 'object_view'):
+      new_actions[action_category].sort(key=lambda x: x.get('priority', 1.0))
+
   return new_actions
 
 
@@ -36,7 +41,6 @@ hasDuplicateActions = CachingMethod(
                           hasDuplicateActions,
                           id='Base_filterDuplicateActions.hasDuplicateActions',
                           cache_factory='erp5_ui_long')
-
 user_name = getattr(container.REQUEST, 'AUTHENTICATED_USER', '')
 
 if getattr(context, 'getPortalType', None) is not None:
