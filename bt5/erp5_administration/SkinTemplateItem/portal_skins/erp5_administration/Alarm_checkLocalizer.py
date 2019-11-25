efff95fd1a06c0919250d7d0eb7c3ff4fad3d468
Jérome Perrin <jerome@nexedi.com>
2019-11-25 06:47:28 +0100

    administration: fixes for pylint

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/Alarm_checkLocalizer.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/Alarm_checkLocalizer.py
@@ -6,13 +6,13 @@ alarm_warn_ratio = 0.25
 
 localizer = context.Localizer
 for message_catalog in localizer.objectValues('MessageCatalog'):
-  all = len(message_catalog.MessageCatalog_getMessageDict().keys())
+  all_messages = len(message_catalog.MessageCatalog_getMessageDict().keys())
   not_translated = len(message_catalog.MessageCatalog_getNotTranslatedMessageDict().keys())
-  enable_warning = not_translated > all*alarm_warn_ratio
+  enable_warning = not_translated > all_messages * alarm_warn_ratio
   if enable_warning:
     # we have more than allowed number of untranslated messages,
     # fire alarm
-    context.log("Too many untranslated Localizer messages for %s %s/%s" %(message_catalog, all, not_translated))
+    context.log("Too many untranslated Localizer messages for %s %s/%s" %(message_catalog, all_messages, not_translated))
     return True
 
 return False
