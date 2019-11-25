f5c6e5bc049fbc34a29c4b1e095eb08d5f87de2c
Jérome Perrin <jerome@nexedi.com>
2019-11-25 06:47:28 +0100

    administration: fixes for pylint

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/Base_viewSecurityMappingAsUser.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/Base_viewSecurityMappingAsUser.py
@@ -4,17 +4,18 @@ security_category_dict = {}
 # XXX This is a duplicate of logic present deep inside ERP5GroupManager.getGroupsForPrincipal()
 # Please refactor into an accessible method so this code can be removed
 def getDefaultSecurityCategoryMapping():
-    return ((
-              'ERP5Type_getSecurityCategoryFromAssignment',
-              context.getPortalObject().getPortalAssignmentBaseCategoryList()
-            ),)
+  return ((
+    'ERP5Type_getSecurityCategoryFromAssignment',
+    context.getPortalObject().getPortalAssignmentBaseCategoryList()
+  ),)
+
 getSecurityCategoryMapping = getattr(context, 'ERP5Type_getSecurityCategoryMapping', getDefaultSecurityCategoryMapping)
 # XXX end of code duplication
 for method_id, base_category_list in getSecurityCategoryMapping():
   try:
     security_category_dict.setdefault(tuple(base_category_list), []).extend(
       getattr(context, method_id)(base_category_list, login, context, ''))
-  except: # XXX: it is not possible to log message with traceback from python script
+  except Exception: # XXX: it is not possible to log message with traceback from python script
     print 'It was not possible to invoke method %s with base_category_list %s'%(method_id, base_category_list)
 
 for base_category_list, category_value_list in security_category_dict.items():
