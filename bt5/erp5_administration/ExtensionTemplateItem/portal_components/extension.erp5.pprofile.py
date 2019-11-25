c6f9150ded478b8f01b27cd9ff95363de964dd83
Jérome Perrin <jerome@nexedi.com>
2019-11-25 06:47:28 +0100

    administration: fixes for pylint

--- bt5/erp5_administration/ExtensionTemplateItem/portal_components/extension.erp5.pprofile.py
+++ bt5/erp5_administration/ExtensionTemplateItem/portal_components/extension.erp5.pprofile.py
@@ -1,1 +1,2 @@
-from zpprofile import getProfiler, getStatisticalProfilerAndThread
+# these names are used in external methods
+from zpprofile import getProfiler, getStatisticalProfilerAndThread #  pylint: disable=unused-import
