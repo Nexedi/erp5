94af2283e8287dd24f14e0443173e5fb2989dab7
Jérome Perrin <jerome@nexedi.com>
2019-11-25 11:41:27 +0100

    ProxyField: fix 'cannot find a template field' error message
    XXX wrong ! this is a regression, somewhere in base class __repr__
    changed recently. We use to have:
    
       ipdb> repr(self)
       '<ProxyField at /erp5/portal_skins/erp5_ingestion_test/erp5_web_content_layout_test/applicable_layout>'
       ipdb> str(self)
       '<ProxyField at applicable_layout>'

--- product/ERP5Form/ProxyField.py
+++ product/ERP5Form/ProxyField.py
@@ -546,7 +546,7 @@ class ProxyField(ZMIField):
     else:
       proxied_field = self.getTemplateField()
       if proxied_field is None:
-        raise AttributeError('The proxy field %r cannot find a template field' % self)
+        raise AttributeError('The proxy field %s cannot find a template field' % self)
       if proxied_field.__class__ == ProxyField:
         return proxied_field.get_recursive_tales(id)
       else:
