9e75aa25d9306567ffba48161322376a8176b5ad
Roque Porchetto <roque.porchetto@nexedi.com>
2019-12-09 09:04:43 +0000

    Project management improvements
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/1000

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_forge/Base_generateStateBugDomain.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_forge/Base_generateStateBugDomain.py
@@ -1,5 +1,5 @@
 return [
   context.Base_generateDomain(parent, 'started', 'Open', 'simulation_state', ['confirmed', 'ready']),
   context.Base_generateDomain(parent, 'closed', 'Solved/Closed', 'simulation_state', ['delivered', 'stopped']),
-  context.Base_generateDomain(parent, 'not_started', 'Not Confirmed', 'simulation_state', ['draft', 'cancelled'])
+  context.Base_generateDomain(parent, 'not_started', 'Cancelled', 'simulation_state', ['cancelled'])
 ]
