0e2140d82acda7a660391264495b4e29995bdec2
Roque Porchetto <roque.porchetto@nexedi.com>
2019-12-13 13:30:10 +0000

    erp5_project: refactor milestone domain script

--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateDateMilestoneDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateDateMilestoneDomain.py
@@ -1,17 +1,32 @@
 from DateTime import DateTime
-criterion_property = "stop_date"
 criterion_property = "delivery.stop_date"
-future_domain = parent.generateTempDomain(id="future")
-future_domain.edit(title='Future',
-                criterion_property_list=[criterion_property])
-future_domain.setCriterion(criterion_property, min=DateTime(), max=DateTime('9999/01/01 00:00'))
+now = DateTime()
+domain_list = []
+parents_criterion_dict = {}
 
-past_domain = parent.generateTempDomain(id="past")
-past_domain.edit(title='Past',
-                criterion_property_list=[criterion_property])
-past_domain.setCriterion(criterion_property, min=DateTime('2000/01/01 00:00'), max=DateTime())
+def appendNewTempDomain(id, criterion_dict=None, **kw):
+  if criterion_dict is None:
+    criterion_dict = parents_criterion_dict
+  else:
+    criterion_dict.update(parents_criterion_dict)
+  domain = parent.generateTempDomain(id=id)
+  domain.edit(
+    criterion_property_list=criterion_dict.keys(),
+    **kw
+  )
+  for property_id, criterion_kw in criterion_dict.items():
+    domain.setCriterion(property_id, **criterion_kw)
+  domain_list.append(domain)
 
-return [
-  future_domain,
-  past_domain
-]
+appendNewTempDomain(
+  id="future",
+  title="Future",
+  criterion_dict={criterion_property: {"min": now, "max": DateTime('9999/01/01 00:00')}},
+)
+appendNewTempDomain(
+  id="past",
+  title="Past",
+  criterion_dict={criterion_property: {"min": DateTime('2000/01/01 00:00'), "max": now}},
+)
+
+return domain_list
