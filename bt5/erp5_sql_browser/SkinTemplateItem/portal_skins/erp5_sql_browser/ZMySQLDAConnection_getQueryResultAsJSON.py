cf3a5b534e22b9a2153f39797e232e30d1eb19a7
Jérome Perrin <jerome@nexedi.com>
2019-12-16 00:42:24 +0000

    sql_browser: use monaco editor
    Use monaco editor with https://github.com/joe-re/sql-language-server
    completion provider bundled in
    https://lab.nexedi.com/jerome/monaco-editor-sql-completion-provider
    This completion provider is not perfect, but codemirror's one was not so
    good eiher. At least we can use monaco editor.
    
    Also add a quick "copy to cliboard" feature, which copies the table as
    html or in markdown format for text.
    
    And also fix Server-Timing header that was an obsolete syntax no longer
    supported by chrome.

--- bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_getQueryResultAsJSON.py
+++ bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_getQueryResultAsJSON.py
@@ -32,6 +32,6 @@ for line in data[1:]:
     new_line.append(v)
   new_data.append(new_line)
 
-response.setHeader("Server-Timing", "db=%s;" % (time.time() - start))
+response.setHeader("Server-Timing", 'db;dur=%s;desc="SQL query"' % (time.time() - start))
 response.setHeader('Content-Type', 'application/json')
 return json.dumps(new_data, indent=2)
