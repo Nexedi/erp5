3c0a81a744c61648fa103d0a91acbdb0327d45a1
Romain Courteaud <romain@nexedi.com>
2019-12-20 14:11:37 +0100

    MORE TEST

--- tests/__init__.py
+++ tests/__init__.py
@@ -209,7 +209,7 @@ class ERP5BusinessTemplateCodingStyleTestSuite(_ERP5):
   def getTestList(self):
     test_list = []
     for business_template_path in (
-        glob('%s/product/ERP5/bootstrap/erp5_*' % HERE) +
+        glob('%s/../product/ERP5/bootstrap/erp5_*' % HERE) +
         glob('%s/../bt5/erp5_adm*' % HERE) +
         glob('%s/../bt5/erp5_*hal*' % HERE) +
         glob('%s/../bt5/erp5_*renderjs*' % HERE) +
