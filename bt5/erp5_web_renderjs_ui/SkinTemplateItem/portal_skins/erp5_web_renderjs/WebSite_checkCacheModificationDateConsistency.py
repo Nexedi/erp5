3eafdf3f6a384bb437599a474b665c60583c2348
Jérome Perrin <jerome@nexedi.com>
2019-12-20 14:23:37 +0900

    renderjs_ui: "Touch" cache manifest in Post-Upgrade
    With the current architecture of cache manifest, it was required that
    developpers change the cache manifests referencing a web page every time
    they change a web page. In practice, developers were never doing this,
    so we sometimes had issues where client keep using old version of
    website even though a new version has been deployed, when after
    deployment we did not change the cache manifest.
    
    To automate the scenario of modifying the cache manifest, introduce a
    post-upgrade constraint that will check that the manifest is newer than
    all of the referenced pages. If that's the case, the constraint can fix
    by modifying the manifest content, which also changes the modification
    date of the manifest, which might be used in "if-modified-since"
    negociations.
    
    According to spec [1], cache manifest is updated if the HTTP responses
    is not "304 not modified" and if the manifest content is not
    byte-for-byte identical to the previously cached version.
    
    [1] https://www.w3.org/TR/2008/WD-html5-20080122/#updating1
    
    /reviewed-on https://lab.nexedi.com/nexedi/erp5/merge_requests/1009

---
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSite_checkCacheModificationDateConsistency.py
@@ -0,0 +1,27 @@
+from DateTime import DateTime
+appcache_reference = context.getLayoutProperty("configuration_manifest_url")
+
+getDocumentValue = context.getDocumentValue
+
+error_list = []
+if appcache_reference:
+  url_list = context.Base_getListFileFromAppcache()
+  # Check that the manifest is newer than all cached resources.
+  appcache_manifest = getDocumentValue(appcache_reference).getObject()
+  appcache_manifest_modification_date = appcache_manifest.getModificationDate()
+  for url in url_list:
+    if url:
+      referenced_document = getDocumentValue(url)
+      if referenced_document is not None and (
+          referenced_document.getModificationDate() >
+          appcache_manifest_modification_date):
+        error_list.append(
+            "Document {} is newer than cache manifest".format(url))
+
+  if error_list and fixit:
+    appcache_manifest.edit(
+        text_content='''# Last modified by {} on {}
+{}
+'''.format(script.getId(), DateTime(), appcache_manifest.getTextContent()))
+
+return error_list
---
+++ bt5/erp5_web_renderjs_ui_test/TestTemplateItem/portal_components/test.erp5.testRJSUpgrader.py
@@ -0,0 +1,108 @@
+##############################################################################
+#
+# Copyright (c) 2018 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+import textwrap
+import time
+
+from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
+
+
+class TestRenderJSUpgrade(ERP5TypeTestCase):
+  """Test Upgrader scripts for renderjs UI.
+  """
+  def afterSetUp(self):
+    self.login()
+    self.web_site = self.portal.web_site_module.newContent(
+        portal_type='Web Site',
+        skin_selection_name='RJS',
+    )
+    self.web_site.publish()
+    self.manifest = self.portal.web_page_module.newContent(
+        portal_type='Web Manifest',
+        text_content="# empty",
+        reference='{}.appcache'.format(self.id()),
+    )
+    self.manifest.publish()
+    self.html_page = self.portal.web_page_module.newContent(
+        portal_type='Web Page',
+        text_content="<b>content</b>",
+        reference='{}.html'.format(self.id()))
+    self.html_page.publish()
+    self.javascript = self.portal.web_page_module.newContent(
+        portal_type='Web Script',
+        text_content="alert('hello !')",
+        reference='{}.js'.format(self.id()))
+    self.javascript.publish()
+
+  def test_upgrade_empty_site(self):
+    self.assertEqual([], self.web_site.checkConsistency())
+    self.assertEqual([], self.web_site.fixConsistency())
+
+  def test_upgrade_fix_pages_modification_date(self):
+    # ERP5JS Web Sites define the list of pages to be cached using an
+    # application cache manifest. We have a post-upgrade constraint which
+    # checks that the manifest is more recent that the referenced files.
+    manifest_content = textwrap.dedent(
+        '''\
+        CACHE MANIFEST
+        # v1 - 2011-08-13
+        # This is a comment.
+        http://www.example.com/index.html
+
+        {}
+        {}
+        NETWORK:
+        *
+        ''').format(
+            self.html_page.getReference(), self.javascript.getReference())
+    self.manifest.edit(text_content=manifest_content)
+    self.web_site.setProperty(
+        'configuration_manifest_url', self.manifest.getReference())
+    self.tic()
+    time.sleep(1)
+    self.javascript.edit(text_content="alert('hello again !')")
+    self.tic()
+
+    self.assertLess(
+        self.manifest.getModificationDate(),
+        self.javascript.getModificationDate())
+    self.assertEqual(
+        [
+            'Document {} is newer than cache manifest'.format(
+                self.javascript.getReference()),
+        ], [str(m.getMessage()) for m in self.web_site.checkConsistency()])
+    self.web_site.fixConsistency()
+    self.tic()
+
+    self.assertEqual(
+        [], [str(m.getMessage()) for m in self.web_site.checkConsistency()])
+    self.assertGreater(
+        self.manifest.getModificationDate(),
+        self.javascript.getModificationDate())
+    self.assertIn(
+        'Last modified by WebSite_checkCacheModificationDateConsistency on',
+        self.manifest.getTextContent())
+    self.assertIn(manifest_content, self.manifest.getTextContent())
