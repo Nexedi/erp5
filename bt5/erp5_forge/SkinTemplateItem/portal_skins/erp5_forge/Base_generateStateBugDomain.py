0c007a1d59c813309420d4933709eff74d356906
Roque Porchetto <roque.porchetto@nexedi.com>
2019-12-20 16:40:21 +0000

    project_management: fix names and ids in domains

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_forge/Base_generateStateBugDomain.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_forge/Base_generateStateBugDomain.py
@@ -1,5 +1,5 @@
 return [
-  context.Base_generateDomain(parent, 'started', 'Open', 'simulation_state', ['confirmed', 'ready']),
+  context.Base_generateDomain(parent, 'open', 'Open', 'simulation_state', ['confirmed', 'ready']),
   context.Base_generateDomain(parent, 'closed', 'Solved/Closed', 'simulation_state', ['delivered', 'stopped']),
   context.Base_generateDomain(parent, 'not_started', 'Cancelled', 'simulation_state', ['cancelled'])
 ]
--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskReportDomain.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Base_generateStateTaskReportDomain.py
@@ -1,4 +1,4 @@
 return [
-  context.Base_generateDomain(parent, 'started', 'Confirmed', 'simulation_state', 'confirmed'),
+  context.Base_generateDomain(parent, 'confirmed', 'Confirmed', 'simulation_state', 'confirmed'),
   context.Base_generateDomain(parent, 'closed', 'Closed', 'simulation_state', ['delivered', 'stopped'])
 ]
