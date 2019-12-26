da0990b94dd3c8815170b423cc547422195caa0d
Vincent Pelletier <vincent@nexedi.com>
2019-12-26 06:59:38 +0000

    erp5_pdm: Fix Supply Cell update when {en,dis}abling sliced pricing.
    Deleting matrix cells with manage_delObjects is not a good solution, as
    this breaks matrix index consistency.
    Also, call isBasePricePerSlice rather than introspecting wrapped method
    arguments - the only wrapped method is _setBasePricePerSlice anyway !
    Also, explicitly trigger SupplyCell_updateSliceBasePrice as it depends on
    isBasePricePerSlice value at line level, and it just changed, and cell-level
    interaction workflow does not trigger on line changes.
    Also, get rid of a trivial local variable.
    Also, fix the exception expected from list.index .
    Also, raise if multiple quantity criterion are somehow found.

--- bt5/erp5_pdm/WorkflowTemplateItem/portal_workflow/supply_line_interaction_workflow/scripts/SupplyLine_updateBasePricePerSlice.py
+++ bt5/erp5_pdm/WorkflowTemplateItem/portal_workflow/supply_line_interaction_workflow/scripts/SupplyLine_updateBasePricePerSlice.py
@@ -1,12 +1,9 @@
 supply_line = state_change['object']
-to_delete_cell_list = [
-  cell for cell in supply_line.getCellIdList(base_id='path')
-  if hasattr(supply_line, cell)
-]
-supply_line.manage_delObjects(ids=to_delete_cell_list)
-if state_change.kwargs['workflow_method_args'][0]:
-  price_parameter = 'slice_base_price'
-else:
-  price_parameter = 'base_price'
-supply_line.updateQuantityPredicate(price_parameter)
+supply_line.updateQuantityPredicate(
+  'slice_base_price'
+  if supply_line.isBasePricePerSlice() else
+  'base_price'
+)
 supply_line.updateCellRange(base_id='path')
+for cell_value in supply_line.getCellValueList(base_id='path'):
+  cell_value.SupplyCell_updateSliceBasePrice()
