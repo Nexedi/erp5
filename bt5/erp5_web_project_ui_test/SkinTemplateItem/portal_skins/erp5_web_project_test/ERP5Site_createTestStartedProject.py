7a75d3425672f37cecd792933a9f44416ceb1aa9
Roque Porchetto <roque.porchetto@nexedi.com>
2019-12-27 13:27:16 +0100

    erp5_web_project_ui_test: test project home page

--- bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
+++ bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
@@ -1,5 +1,11 @@
 portal = context.getPortalObject()
 
+if home_page:
+  module = portal.getDefaultModule('Web Page')
+  home_page = module.newContent(portal_type = 'Web Page',
+                              reference = 'test-project-Home.Page')
+  home_page.publishAlive()
+
 module = portal.getDefaultModule('Project')
 project = module.newContent(portal_type = 'Project',
                             reference = 'test-project')
