e19ac9d07f60c95bfdf4464b54ddaeb525047411
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2019-12-30 02:24:21 +0000

    erp5_ui_test: fr language will be used in some tests

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Base_createUITestLanguages.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Base_createUITestLanguages.py
@@ -1,7 +1,7 @@
 portal = context.getPortalObject()
 Localizer = portal.Localizer
 
-for language in ('wo', 'xh'):
+for language in ('fr', 'wo', 'xh'):
   Localizer.manage_addLanguage(language)
   for catalog in Localizer.objectValues():
     catalog.manage_addLanguage(language)
