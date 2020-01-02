281653f33efabf163a131d97596b69cd82511ee2
Roque Porchetto <roque.porchetto@nexedi.com>
2020-01-02 19:36:55 +0000

    erp5_web_project_ui: get project home page by publication section preference - use global preference to get publication section value - test updated

--- bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
+++ bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
@@ -1,14 +1,28 @@
 portal = context.getPortalObject()
-
-if home_page:
-  module = portal.getDefaultModule('Web Page')
-  home_page = module.newContent(portal_type = 'Web Page',
-                              reference = 'test-project-Home.Page')
-  home_page.publishAlive()
+from datetime import datetime
+now = datetime.now()
+date_time = now.strftime("%m-%d-%Y-%H-%M-%S")
+test_id = "test-project-" + date_time
+project_reference = 'test-project-home' if home_page else 'test-project'
+page_reference = 'test-home-page-' + date_time
 
 module = portal.getDefaultModule('Project')
-project = module.newContent(portal_type = 'Project',
-                            reference = 'test-project')
+project = module.newContent(id = test_id,
+                            portal_type = 'Project',
+                            reference = project_reference)
 project.validate()
+
+if home_page:
+  system_preference = portal.portal_preferences.getActiveSystemPreference()
+  system_preference.setPreferredProjectHomePagePublicationSectionCategory("project_home_page_for_test")
+  publication_section = context.restrictedTraverse('portal_categories/publication_section/project_home_page_for_test')
+  module = portal.getDefaultModule('Web Page')
+  home_page = module.newContent(id = test_id,
+                                portal_type = 'Web Page',
+                                reference = page_reference,
+                                publication_section_value = publication_section,
+                                follow_up_value = project)
+  home_page.publishAlive()
+
 print "Project Created"
 return printed
