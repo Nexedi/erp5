79a7e1ded5b615078a8509a8c59aa14fe0305ec4
Roque Porchetto <roque.porchetto@nexedi.com>
2020-01-02 19:36:55 +0000

    erp5_web_project_ui: get project home page by publication section preference - use global preference to get publication section value - test updated

--- bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
+++ bt5/erp5_web_project_ui_test/SkinTemplateItem/portal_skins/erp5_web_project_test/ERP5Site_createTestStartedProject.py
@@ -1,14 +1,19 @@
 portal = context.getPortalObject()
+project_reference = 'test-project'
+publication_section = context.restrictedTraverse('portal_categories/publication_section/project_home_page_for_test')
+
+module = portal.getDefaultModule('Project')
+project = module.newContent(portal_type = 'Project',
+                            reference = project_reference)
+project.validate()
 
 if home_page:
   module = portal.getDefaultModule('Web Page')
   home_page = module.newContent(portal_type = 'Web Page',
-                              reference = 'test-project-Home.Page')
+                                reference = project_reference + '-home.page',
+                                publication_section_value = publication_section,
+                                follow_up_value = project)
   home_page.publishAlive()
 
-module = portal.getDefaultModule('Project')
-project = module.newContent(portal_type = 'Project',
-                            reference = 'test-project')
-project.validate()
 print "Project Created"
 return printed
