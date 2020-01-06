a181ff84c2bc524dc92b8c37faa3da1aaae01449
Romain Courteaud <romain@nexedi.com>
2020-01-06 15:12:28 +0000

    erp5_web_renderjs_ui: change header/panel

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_jump_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_jump_js.js
@@ -1,10 +1,10 @@
-/*global window, rJS */
+/*global window, rJS, renderFormViewHeader */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS) {
+(function (window, rJS, renderFormViewHeader) {
   "use strict";
 
   var gadget_klass = rJS(window),
-    method_list = ['triggerSubmit', 'checkValidity', 'getContent', 'render'],
+    method_list = ['triggerSubmit', 'checkValidity', 'getContent'],
     i;
 
   function propagateMethod(method_name) {
@@ -21,4 +21,48 @@
     gadget_klass.declareMethod(method_list[i], propagateMethod(method_list[i]));
   }
 
-}(window, rJS));
+  gadget_klass
+    .declareMethod('render', function (options) {
+      var argument_list = arguments;
+      this.state.jio_key = options.jio_key;
+      this.state.view = options.view;
+      return this.getDeclaredGadget('page_form')
+        .push(function (g) {
+          return g.render.apply(g, argument_list);
+        });
+    })
+
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod('isDesktopMedia', 'isDesktopMedia')
+    .declareAcquiredMethod('getUrlParameter', 'getUrlParameter')
+    .declareAcquiredMethod("getUrlForList", "getUrlForList")
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+    .declareAcquiredMethod("updatePanel", "updatePanel")
+
+    .allowPublicAcquisition("updateHeader", function updateHeader() {
+      var gadget = this;
+      return gadget.jio_getAttachment(gadget.state.jio_key,
+                                            'links')
+        .push(function (erp5_document) {
+          return renderFormViewHeader(gadget, gadget.state.jio_key,
+                                      gadget.state.view,
+                                      erp5_document);
+        });
+    })
+
+    .allowPublicAcquisition("updatePanel", function updatePanel(param_list) {
+      var gadget = this;
+      return gadget.jio_getAttachment(gadget.state.jio_key,
+                                            'links')
+        .push(function (erp5_document) {
+          return gadget.updatePanel({
+            display_workflow_list: true,
+            erp5_document: erp5_document,
+            editable: true,
+            jio_key: gadget.state.jio_key,
+            view: gadget.state.view
+          });
+        });
+    });
+
+}(window, rJS, renderFormViewHeader));
