5080e33cb2a59c37b27e70eba1ff3633160d5847
Romain Courteaud <romain@nexedi.com>
2020-01-08 17:26:46 +0100

    erp5_hal_json_style: put the web site in the acquisition context + keep_items

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
@@ -60,10 +60,17 @@ result_dict = {
   }
 }
 
+# Put the web site in the acquisition context
+# this occurs when doing a .getObject from the catalog for example
+if (not context.isWebMode()) and (context.REQUEST.get('web_section_value', None) is not None):
+  portal = context.getPortalObject()
+  web_section = portal.restrictedTraverse("/".join(context.REQUEST['web_section_value']))
+  context = web_section.restrictedTraverse(context.getRelativeUrl())
+
 # form_id = 'view' means use default document view. Let the JS handle it
 # In case of dialog submit, if redirecting to the original form, let the JS handle the navigation history
-if (form_id is not None) and \
-   (form_id not in ['view', 'Base_viewFakePythonScriptActionForm']) and \
+if (form_id not in [None, 'Base_viewFakePythonScriptActionForm', 'Base_viewFakeJumpForm']) and \
+   ((form_id != 'view') or (keep_items)) and \
    (form_id != previous_form_id) and \
    (not form_id.startswith('http')) and \
    (context.isWebMode()):
