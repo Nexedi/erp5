c74e8a6830f6a76c546b379aab8547ee56b3dc99
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-01-13 09:39:13 +0000

    erp5_accounting: round debit credit for payment transaction

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/PaymentTransaction_postGeneration.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/PaymentTransaction_postGeneration.py
@@ -5,3 +5,5 @@ payment_transaction = context
 # initialize accounting_workflow to planned state
 if payment_transaction.getSimulationState() == "draft":
   payment_transaction.plan(comment=translateString("Initialised by Delivery Builder."))
+
+payment_transaction.AccountingTransaction_roundDebitCredit()
