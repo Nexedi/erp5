3afd3bcec1bed28664a94330ecdb2975eb50d87e
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-01-15 11:10:56 +0000

    erp5_invoicing: add return sale order in query

--- bt5/erp5_invoicing/SkinTemplateItem/portal_skins/erp5_invoicing/SaleInvoiceTransaction_selectPackingListMovement.py
+++ bt5/erp5_invoicing/SkinTemplateItem/portal_skins/erp5_invoicing/SaleInvoiceTransaction_selectPackingListMovement.py
@@ -1,11 +1,16 @@
 kw['parent_specialise_portal_type'] = 'Invoice Simulation Rule'
-kw['explanation_portal_type'] = 'Sale Order', 'Sale Packing List', 'Returned Sale Packing List'
+kw['explanation_portal_type'] = 'Sale Order', 'Sale Packing List', 'Returned Sale Packing List', 'Returned Sale Order'
 kw['portal_type'] = 'Simulation Movement'
 kw['grand_parent_simulation_state'] = 'started', 'stopped', 'delivered'
 
 kw['delivery_uid'] = None
 kw['left_join_list'] = ['delivery_uid']
 kw['select_dict'] = dict(delivery_uid=None)
-kw['group_by'] = ('uid',)
+
+# We assume that all simulation movements without a delivery are in planned or auto planned state.
+# By passing this, catalog should use an index on portal_type + simulation state.
+# XXX actually we do not even pass auto_planned to have only 1 value, because we just do not use
+# auto_planned state in this project.
+kw['simulation_state'] = ('planned', )
 
 return context.portal_catalog(**kw)
