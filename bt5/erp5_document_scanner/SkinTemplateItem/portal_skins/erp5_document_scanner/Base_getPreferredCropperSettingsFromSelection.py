770483cd6a2873c0cf64d6d81d9f0e64b654d744
Romain Courteaud <romain@nexedi.com>
2020-01-16 15:40:15 +0000

    erp5_document_scanner: fully erase the gadget DOM when changing its state
    Stop relying of CSS hide/show attributes.
    Instead, only put needed elements in the DOM.

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getPreferredCropperSettingsFromSelection.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getPreferredCropperSettingsFromSelection.py
@@ -7,6 +7,5 @@ selection_mapping = portal.portal_selections.getSelectionParamsFor(
   REQUEST=context.REQUEST) or {}
 
 canvas_data = selection_mapping.get(context.REQUEST["HTTP_USER_AGENT"]) or {}
-canvas_data["dialog_method"] = context.Base_storeDocumentFromCameraInActiveProcess.getId()
 
 return json.dumps(canvas_data)
