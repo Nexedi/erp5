813144371e08e366681068a9c3fb0033d5633e8a
Łukasz Nowak <luke@nexedi.com>
2020-01-17 14:09:07 +0100

    erp5_web_monitoring_ui_test: Simulate backend in ERP5
    Thanks to not starting backend separately, the test became much simpler
    and shall be easier to run and check.

---
+++ bt5/erp5_web_monitoring_ui_test/PathTemplateItem/portal_tests/web_monitoring_ui_zuite/MonitorBackend_simulator.py
@@ -0,0 +1,10 @@
+value = container.MonitorBackend_simulatorMapping(path)
+if value is not None:
+  return value
+else:
+  return 'OK'
+"""
+    self.send_header("Access-Control-Allow-Origin", "*")
+    self.send_header("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST")
+    self.send_header("Access-Control-Allow-Headers", "Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Authorization")
+"""
