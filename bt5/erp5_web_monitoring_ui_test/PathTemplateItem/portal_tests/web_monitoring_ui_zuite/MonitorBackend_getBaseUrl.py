35c4c555b0eb04d657458de4b97f58e430ea98e0
Łukasz Nowak <luke@nexedi.com>
2020-01-17 14:09:07 +0100

    erp5_web_monitoring_ui_test: Simulate backend in ERP5
    Thanks to not starting backend separately, the test became much simpler
    and shall be easier to run and check.
    
    Use Zope's access rule in the folder in order to set headers for OPTIONS
    method too.

---
+++ bt5/erp5_web_monitoring_ui_test/PathTemplateItem/portal_tests/web_monitoring_ui_zuite/MonitorBackend_getBaseUrl.py
@@ -0,0 +1,1 @@
+return context.absolute_url() + '/MonitorBackend_simulator?path='
---
+++ bt5/erp5_web_monitoring_ui_test/PathTemplateItem/portal_tests/web_monitoring_ui_zuite/MonitorBackend_simulator.py
@@ -0,0 +1,5 @@
+value = container.MonitorBackend_simulatorMapping(path)
+if value is not None:
+  return value
+else:
+  return 'OK'
