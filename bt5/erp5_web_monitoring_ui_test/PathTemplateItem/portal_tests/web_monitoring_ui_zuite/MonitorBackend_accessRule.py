a969326fb3af357671e37060d48ce690fc6f4cad
Łukasz Nowak <luke@nexedi.com>
2020-01-17 14:09:07 +0100

    erp5_web_monitoring_ui_test: Simulate backend in ERP5
    Thanks to not starting backend separately, the test became much simpler
    and shall be easier to run and check.
    
    Use Zope's access rule in the folder in order to set headers for OPTIONS
    method too and also to avoid having ? in the backend URL.

---
+++ bt5/erp5_web_monitoring_ui_test/PathTemplateItem/portal_tests/web_monitoring_ui_zuite/MonitorBackend_accessRule.py
@@ -0,0 +1,12 @@
+setHeader = context.REQUEST.response.setHeader
+setHeader("Access-Control-Allow-Origin", "*")
+setHeader("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST")
+setHeader("Access-Control-Allow-Headers", "Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Authorization")
+PATH = context.REQUEST['PATH_INFO']
+if '/simulator' in PATH:
+  script.log('Hacking path')
+  real, simulator = PATH.split('simulator')
+  container.REQUEST.set('PATH_INFO', real)
+  container.REQUEST.set('SIMULATOR_PATH', simulator)
+  container.REQUEST.set('SCRIPT_NAME', 'MonitorBackend_simulator')
+  container.REQUEST.set('TraversalRequestNameStack', ['MonitorBackend_simulator'])
