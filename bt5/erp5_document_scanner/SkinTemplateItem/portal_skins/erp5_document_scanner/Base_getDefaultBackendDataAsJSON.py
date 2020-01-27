7bcb051115448ba4c33954d48feb8a3c73965751
Gabriel Monnerat <gabriel@nexedi.com>
2020-01-27 20:40:21 +0000

    erp5_document_scanner: Add a tales expression as default field value
    The default value is a JSON containing 2 keys:
    - the relative_url of an active process
    - the list of active process image content to aggregate
    By default, when the field is rendered for the first time, this tales expression is evaluated and a new active process is created at that point.
    So, default json is like: {active_process: 'portal_activities/1234', image_list: []}
    * store the active_process relative_url in the gadget state in the render method
    * store the image_list in the gadget state in the render_method
    * change getContent method to send a json build from the active_process and image_list state

---
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getDefaultBackendDataAsJSON.py
@@ -0,0 +1,8 @@
+import json
+context.log("Called")
+active_process = context.getPortalObject().portal_activities.newActiveProcess()
+
+return json.dumps({
+  "active_process": active_process.getRelativeUrl(),
+  "image_list": []
+})
