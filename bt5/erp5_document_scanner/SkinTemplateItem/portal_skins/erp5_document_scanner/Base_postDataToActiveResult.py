011972b092756c8b785798553ad9652f6da8fe82
Gabriel Monnerat <gabriel@nexedi.com>
2020-01-28 15:28:54 +0000

    erp5_document_scanner: Response body may contain a local reference of the uploaded image
    The http response should return a 200 response in case of success. The response body may contain a local reference of the uploaded image

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_postDataToActiveResult.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_postDataToActiveResult.py
@@ -2,6 +2,17 @@
   We need proy role as manager to create a new active process
   and post active result
 """
+import string
+import random
+
+reference =  str(
+  DateTime().millis()) + '-' + ''.join(
+    random.sample(
+      string.letters+string.digits, random.randint(
+        6, 10)
+    )
+)
+
 portal = context.getPortalObject()
 
 if REQUEST:
@@ -12,6 +23,5 @@ if active_process_url:
 else:
   active_process = portal.portal_activities.newActiveProcess()
 
-active_process.postActiveResult(detail=detail)
-
-return active_process
+active_process.postActiveResult(detail=detail, reference=reference)
+return active_process, reference
