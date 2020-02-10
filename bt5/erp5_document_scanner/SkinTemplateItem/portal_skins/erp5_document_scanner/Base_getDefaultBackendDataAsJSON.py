67284a676bd83e38d17edaee677684f5e25b9af4
Gabriel Monnerat <gabriel@nexedi.com>
2020-02-10 22:27:43 +0000

    erp5_document_scanner: Avoid reference hardcoded in many places

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getDefaultBackendDataAsJSON.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getDefaultBackendDataAsJSON.py
@@ -1,6 +1,7 @@
 import json
 
-active_process = context.getPortalObject().portal_activities.newActiveProcess()
+active_process = context.getPortalObject().portal_activities.newActiveProcess(
+  reference=context.Base_getDocumentScannerDefaultReference())
 
 return json.dumps({
   "active_process": active_process.getRelativeUrl(),
