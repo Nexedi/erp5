c1c1505a0e898473a5e1dbdcb283897702fe0545
Jérome Perrin <jerome@nexedi.com>
2020-02-12 19:30:47 +0900

    bank_reconciliation: typo in message

--- bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/BankReconciliation_notifySelectNonReconciledFinished.py
+++ bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/BankReconciliation_notifySelectNonReconciledFinished.py
@@ -1,3 +1,3 @@
 from Products.ERP5Type.Message import translateString
 context.getPortalObject().portal_workflow.doActionFor(
-  context, 'edit_action', comment=translateString('Select non reconciled transcations finished'))
+  context, 'edit_action', comment=translateString('Select non reconciled transactions finished'))
