8f823f2c12be71f35e09989b404d07d212c7a416
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-02-13 11:06:06 +0000

    erp5_payroll_l10n_fr: add fr simplified payslip report

---
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_printPayslipReport.py
@@ -0,0 +1,28 @@
+"""
+================================================================================
+Wire PaySheetTransaction through to erp5_corporate_identity Report
+================================================================================
+"""
+# ERP5 web uses format= argument, which is also a python builtin
+# pylint: disable=redefined-builtin
+
+# parameters   (* default)
+# ------------------------------------------------------------------------------
+# format:                   output in html*, pdf
+# international_form        translate terms
+# language                  target_language
+return context.Base_printAsReport(
+  format=format,
+  report_header = "PaySheetTransaction_generatePayslipReportHeader",
+  report_name = "PaySheetTransaction_generatePayslipReport",
+  report_footer = "PaySheetTransaction_generatePayslipReportFooter",
+  conversion_dict = dict(
+    margin_top=70,
+    header_spacing=5
+  ),
+  css_path="payslip_css/payslip",
+  document_language=target_language,
+  start_date=context.getStartDate() or None,
+  stop_date=context.getStopDate() or None,
+  **kw
+)
