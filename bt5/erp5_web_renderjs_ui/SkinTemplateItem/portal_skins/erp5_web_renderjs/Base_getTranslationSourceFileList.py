371f8f21eae7f8a94458ad5ae34903bcbb60b99c
Romain Courteaud <romain@nexedi.com>
2020-02-17 15:34:39 +0000

    erp5_web_renderjs_ui: extract translatable file list

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Base_getTranslationSourceFileList.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Base_getTranslationSourceFileList.py
@@ -1,34 +1,4 @@
-import re
-
-service_worker_reference = context.getLayoutProperty("configuration_service_worker_url", default="gadget_erp5_serviceworker.js")
-
-service_worker = context.getPortalObject().portal_catalog.getResultValue(
-  portal_type='Web Script',
-  reference=service_worker_reference)
-
-if service_worker is None:
-  text_content = ''
-else:
-  text_content = service_worker.getTextContent()
-
-filename_pattern = re.compile("'(?P<filename>[a-zA-Z0-9-_\.\?=]*)'")
-filename_list = []
-
-start = False
-for line in text_content.split('\n'):
-  if start is False and 'REQUIRED_FILES' in line:
-    start = True
-    continue
-  if not line:
-    continue
-  if start:
-    if ']' in line:
-      break
-    matched = filename_pattern.search(line)
-    if matched is not None:
-      filename = matched.groupdict().get('filename')
-      if filename:
-        filename_list.append(filename)
+filename_list = context.WebSection_getPrecacheManifest()
 
 file_list = []
 translation_data_file = []
