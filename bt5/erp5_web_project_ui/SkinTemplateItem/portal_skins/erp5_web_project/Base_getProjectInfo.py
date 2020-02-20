a92fab10c46609aa51a46c1d12aef20b041b8827
Roque Porchetto <roque.porchetto@nexedi.com>
2020-02-20 10:30:23 +0000

    erp5_web_project_ui: refactor SQL query

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_getProjectInfo.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_getProjectInfo.py
@@ -1,23 +1,5 @@
 import json
-# TODO remove hardcoded fields and values, use script parameters
-query = """SELECT DISTINCT COUNT(*) AS  `#number`  ,`related_source_project__relative_url_1_catalog`.`relative_url` AS `source_project__relative_url`,
-                          `catalog`.`portal_type` AS `portal_type`, `catalog`.`modification_date` AS `modification_date`
-            FROM (catalog AS `catalog`
-              LEFT JOIN ( category AS `related_source_project__relative_url_category`
-              INNER JOIN catalog AS `related_source_project__relative_url_1_catalog`
-              ON related_source_project__relative_url_category.base_category_uid = 282 AND related_source_project__relative_url_1_catalog.uid = related_source_project__relative_url_category.category_uid)
-              ON related_source_project__relative_url_category.uid = catalog.uid)
-            WHERE
-              1 = 1
-              AND (`catalog`.`modification_date` >= "2019-02-19 10:02:00")
-              AND `catalog`.`simulation_state` IN ('planned', 'ordered', 'confirmed', 'delivered', 'ready')
-              AND ( `catalog`.`portal_type` IN ('Task', 'Bug', 'Task Report'))
-              AND ( `related_source_project__relative_url_1_catalog`.`relative_url` IS NOT NULL)
-            GROUP BY
-              `related_source_project__relative_url_1_catalog`.`relative_url`, `catalog`.`portal_type`"""
-
-project_dict = {'someproperty': 0, 'anotherproperty': 'value'}
-project_dict = {}
+from datetime import datetime
 
 def getProjectId(id):
   segments = id.split("/");
@@ -25,14 +7,51 @@ def getProjectId(id):
     return id
   return "/".join(segments[0:-1])
 
-for row in context.cmf_activity_sql_connection.manage_test(query):
+project_dict = {}
+now_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#TODO these should be parameters
+limit_date = "2019-12-19 00:00:00"
+valid_states = ['planned', 'ordered', 'confirmed', 'delivered', 'ready']
+valid_states = "('" + "', '".join(valid_states) + "')"
+valid_types = ['Task', 'Bug', 'Task Report']
+valid_types = "('" + "', '".join(valid_types) + "')"
+# TODO: GET THE CATEGORY_UID
+category = 282
+
+query = """SELECT DISTINCT
+            COUNT(*) AS  `#number`,
+            `related_source_project__relative_url_1_catalog`.`relative_url` AS `source_project__relative_url`,
+            `catalog`.`portal_type` AS `portal_type`
+          FROM (catalog AS `catalog`
+            LEFT JOIN ( category AS `related_source_project__relative_url_category`
+            INNER JOIN catalog AS `related_source_project__relative_url_1_catalog`
+            ON related_source_project__relative_url_category.base_category_uid = %i
+              AND related_source_project__relative_url_1_catalog.uid = related_source_project__relative_url_category.category_uid)
+            ON related_source_project__relative_url_category.uid = catalog.uid)
+          WHERE
+            1 = 1
+            AND (`catalog`.`modification_date` <= "%s")
+            AND `catalog`.`simulation_state` IN %s
+            AND ( `catalog`.`portal_type` IN %s)
+            AND ( `related_source_project__relative_url_1_catalog`.`relative_url` IS NOT NULL)
+          GROUP BY
+            `related_source_project__relative_url_1_catalog`.`relative_url`,
+            `catalog`.`portal_type`"""
+
+total_query = query % (category, now_date, valid_states, valid_types)
+
+for row in context.cmf_activity_sql_connection.manage_test(total_query):
   key = getProjectId(row['source_project__relative_url'])
   if key in project_dict:
-    project_dict[key][row['portal_type']] = row["#number"]
+    project_dict[key][row['portal_type']] = { 'total' : row["#number"], 'outdated' : 0 }
   else:
-    project_dict[key] = {row['portal_type'] : row["#number"]}
+    project_dict[key] = {row['portal_type'] : { 'total' : row["#number"], 'outdated' : 0 }}
 
-# TODO set number of outdated docs!!
-# one query by "opened" state (total) and the same but filtering by modification date (outdated)
+outdated_query = query % (category, limit_date, valid_states, valid_types)
+
+for row in context.cmf_activity_sql_connection.manage_test(outdated_query):
+  key = getProjectId(row['source_project__relative_url'])
+  project_dict[key][row['portal_type']]['outdated'] = row["#number"]
 
 return json.dumps(project_dict, indent=2)
