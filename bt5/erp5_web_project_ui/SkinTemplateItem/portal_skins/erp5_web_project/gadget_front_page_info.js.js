6607a8255940655933f5f0e4dd2df39e05ee7019
Roque Porchetto <roque.porchetto@nexedi.com>
2020-02-24 11:07:22 +0000

    erp5_web_project_ui: drop SQL script

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/gadget_front_page_info.js.js
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/gadget_front_page_info.js.js
@@ -1,10 +1,12 @@
 /*jslint nomen: true, indent: 2 */
-/*global window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query*/
-(function (window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query) {
+/*global window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query, parseInt*/
+(function (window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query, parseInt) {
   "use strict";
 
+  /*jshint esnext: true */
   const STATUS_OK = "green";
   const STATUS_NOT_OK = "red";
+  const RADIX = 10;
 
   function getProjectId(id) {
     var segments = id.split("/");
@@ -47,20 +49,7 @@
       project_query = Query.objectToSearchText(
         getComplexQuery({"portal_type" : "Project",
                          "validation_state" : "validated"},
-                        "AND"))/*,
-      milestone_query = Query.objectToSearchText(
-        getComplexQuery({"portal_type" : "Project Milestone",
-                         "parent__validation_state" : "validated",
-                         "parent__portal_type" : "Project"},
-                        "AND")),
-      test_result_query = Query.objectToSearchText(
-        getComplexQuery({"portal_type" : "Test Result",
-                         "source_project__validation_state" : "validated"},
-                        "AND")),
-      test_state_list = ["failed", "stopped"];
-
-    console.log("milestone_query:", milestone_query);
-    test_result_query += ' AND simulation_state: ("' + test_state_list.join('", "') + '")'*/;
+                        "AND"));
 
     //TODO filter too old objects?
     /*var date_query, one_year_old_date = new Date();
@@ -74,67 +63,17 @@
       value: one_year_old_date
     });*/
 
-    var document_query,
-      portal_type_list = ["Task", "Bug", "Task Report"],
-      valid_state_list = ["planned", "ordered", "confirmed", "delivered", "ready"];
-
-    document_query = Query.objectToSearchText(new SimpleQuery({
-      key: "source_project__validation_state",
-      operator: "=",
-      type: "simple",
-      value: "validated"
-    }));
-    // Only way to build these queries using Query objects is by iterating the lists and
-    // creating a Simple OR query for each one (then AND)
-    // JIO Queries doesn't cover "IN" operator to do a one SimpleQuery like "portal_type IN ('a','b' ... 'etc')"
-    document_query += ' AND simulation_state: ("' + valid_state_list.join('", "') + '")';
-    document_query += ' AND portal_type: ("' + portal_type_list.join('", "') + '")';
-
-    return new RSVP.Queue()
-      .push(function () {
-        var promise_list = [],
-          limit = [0, 100000];
-        //this project query is needed to get each project info like title, etc
-        //that can't be get via milestones (there could be a project without milestones)
-        //or via documents (documents could be related to a project line and no the project itself)
-        promise_list.push(gadget.jio_allDocs({
-          query: project_query,
-          limit: limit,
-          select_list: ['title'],
-          sort_on: [["modification_date", "ascending"]]
-        }));
-        /*promise_list.push(gadget.jio_allDocs({
-          query: milestone_query,
-          limit: limit,
-          select_list: ["title", 'portal_type', "parent__title", "parent__relative_url"],
-          //select_list: ["title", 'portal_type', "parent__title", "parent__relative_url", 'count(*)'],
-          //select_list: ["parent__title", 'portal_type', 'count(*)'],
-          sort_on: [["modification_date", "descending"]]
-          //group_by: ['portal_type', 'parent__relative_url']
-        }));
-        promise_list.push(gadget.jio_allDocs({
-          query: test_result_query,
-          limit: limit,
-          select_list: ['source_project__relative_url', 'portal_type'],
-          group_by: ['source_project__relative_url'],
-          sort_on: [["modification_date", "descending"]]
-        }));
-        promise_list.push(gadget.jio_allDocs({
-          query: document_query,
-          limit: limit,
-          select_list: ['source_project__relative_url', 'portal_type', 'count(*)'],
-          group_by: ['portal_type', 'source_project__relative_url'],
-          sort_on: [["modification_date", "descending"]]
-        }));*/
-        return RSVP.all(promise_list);
-      })
-      .push(function (result_list) {
-        return result_list[0].data.rows;
-        //return [result_list[0].data.rows, result_list[1].data.rows, result_list[2].data.rows, result_list[2].data.rows];
-      });
+    return gadget.jio_allDocs({
+      query: project_query,
+      limit: 10000,
+      select_list: ['title'],
+      sort_on: [["modification_date", "ascending"]]
+    })
+    .push(function (result) {
+      return result.data.rows;
+    });
   }
 
-  //function renderProjectList(project_info_dict, project_list, milestone_list, test_result_list) {
   function renderProjectList(project_list) {
     var i,
       item,
@@ -222,46 +161,6 @@
       }
       ul_list.appendChild(project_html);
     }
-
-    /*for (i = 0; i < milestone_list.length; i += 1) {
-      project_id = getProjectId(milestone_list[i].id);
-      if (!project_info_dict.hasOwnProperty(project_id)) {
-        project_info_dict[project_id] = {};
-      }
-      outdated = (new Date(milestone_list[i].value.modification_date) < milestone_limit_date) ? 1 : 0;
-      if (project_info_dict[project_id].hasOwnProperty(milestone_list[i].value.portal_type)) {
-        project_info_dict[project_id][milestone_list[i].value.portal_type].total += 1;
-        project_info_dict[project_id][milestone_list[i].value.portal_type].total += outdated;
-      } else {
-        project_info_dict[project_id][milestone_list[i].value.portal_type] = { 'total' : 1, 'outdated' : outdated };
-      }
-    }
-
-    for (i = 0; i < test_result_list.length; i += 1) {
-      project_id = test_result_list[i].value.source_project__relative_url;
-      if (!project_info_dict.hasOwnProperty(project_id)) {
-        project_info_dict[project_id] = {};
-      }
-      //TODO for test result, total and outdated are values the test itself should retrieve (all_tests and failed) but those are _local_properties
-      project_info_dict[project_id][test_result_list[i].value.portal_type] = { 'total' : 1, 'outdated' : 0 };
-    }
-
-    for (i = 0; i < project_list.length; i += 1) {
-      project_html_element_list = createProjectHtmlElement(project_list[i].id, project_list[i].value.title);
-      project_html = project_html_element_list[0];
-      left_div_html = project_html_element_list[1];
-
-      project_dict = project_info_dict[project_list[i].id];
-      for (type in project_dict) {
-        if (project_dict.hasOwnProperty(type)) {
-          left_line_html = createProjectLineHtmlElement(((line_title_list.hasOwnProperty(type)) ? line_title_list[type] : type),
-                                                        project_dict[type].total, project_dict[type].outdated,
-                                                        ((project_dict[type].outdated > 0) ? STATUS_NOT_OK : STATUS_OK));
-          left_div_html.appendChild(left_line_html);
-        }
-      }
-      ul_list.appendChild(project_html);
-    }*/
   }
 
   rJS(window)
@@ -273,72 +172,14 @@
     .declareAcquiredMethod("getSetting", "getSetting")
 
     .declareMethod('render', function (options) {
-      if (options.project_info_dict) {
-        options.project_info_dict = JSON.parse(options.project_info_dict);
-      }
       return this.changeState(options);
     })
 
-    .onStateChange(function (modification_dict) {
+    .onStateChange(function () {
       var gadget = this;
       return getProjectElementList(gadget)
         .push(function (project_list) {
           return renderProjectList(project_list);
-          //renderProjectList(element_list[3], element_list[0], element_list[1], element_list[2]);
-          //renderProjectList(modification_dict.project_info_dict, element_list[0], element_list[1], element_list[2]);
-        })
-        .push(function (project_list) {
-          gadget.myJob();
-        });
-    })
-
-    .declareJob("myJob", function () {
-      console.log("myJob!");
-      var gadget = this,
-        i, project_id,
-        outdated,
-        milestone_list,
-        status_span,
-        status_total,
-        status_outdated,
-        milestone_query = Query.objectToSearchText(
-        getComplexQuery({"portal_type" : "Project Milestone",
-                         //"parent__validation_state" : "validated",
-                         "validation_state" : "validated",
-                         "parent__portal_type" : "Project"},
-                        "AND")),
-        milestone_limit_date = new Date();
-      milestone_limit_date.setFullYear(milestone_limit_date.getFullYear() - 1);
-      return new RSVP.Queue()
-        .push(function () {
-          return gadget.jio_allDocs({
-            query: milestone_query,
-            limit: 10000,
-            select_list: ["title", 'portal_type', "parent__title", "parent__relative_url"],
-            sort_on: [["modification_date", "descending"]]
-          })
-          .push(function (result) {
-            milestone_list = result.data.rows;
-            console.log("milestone_list:", milestone_list);
-            for (i = 0; i < milestone_list.length; i += 1) {
-              console.log("milestone ", milestone_list[i].id);
-              project_id = getProjectId(milestone_list[i].id);
-              console.log("project_id ", project_id);
-              outdated = (new Date(milestone_list[i].value.modification_date) < milestone_limit_date) ? 1 : 0;
-              console.log("outdated ", outdated);
-              status_span = document.getElementById(getProjectSpanId(project_id, milestone_list[i].value.portal_type, "status"));
-              status_total = document.getElementById(getProjectSpanId(project_id, milestone_list[i].value.portal_type, "total"));
-              status_outdated = document.getElementById(getProjectSpanId(project_id, milestone_list[i].value.portal_type, "outdated"));
-              console.log("status_span ", status_span);
-              console.log("status_total ", status_total);
-              console.log("status_outdated ", status_outdated);
-              console.log("status_total.value ", parseInt(status_total.textContent));
-              status_total.textContent = parseInt(status_total.textContent) + 1;
-              status_outdated.textContent = parseInt(status_outdated.textContent) + outdated;
-              status_span.classList.add((parseInt(status_outdated.textContent) > 0) ? STATUS_NOT_OK : STATUS_OK);
-              console.log("");
-            }
-          });
         });
     })
 
@@ -350,4 +191,4 @@
       return true;
     });
 
-}(window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query));
+}(window, rJS, RSVP, document, SimpleQuery, ComplexQuery, Query, parseInt));
