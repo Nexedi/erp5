62199a2022b776c7d5c8038ab2a9a69237ff8462
Ivan Tyagov <ivan@nexedi.com>
2020-03-03 13:45:43 +0200

    Call alarm in an activity and set starting date. Call script ONLY if not already installed.

--- bt5/erp5_wendelin_auto_configuration/SkinTemplateItem/portal_skins/erp5_wendelin_auto_configuration/Alarm_setupAutoConfiguration.py
+++ bt5/erp5_wendelin_auto_configuration/SkinTemplateItem/portal_skins/erp5_wendelin_auto_configuration/Alarm_setupAutoConfiguration.py
@@ -2,4 +2,17 @@
 This script will setup the default Wendelin's configuration as saved in erp5_wendelin_scalability_test business template.
 As this modifies your site care must be taken!
 """
-context.ERP5Site_bootstrapScalabilityTest(user_quantity=0, setup_activity_tool=False, create_test_data=False, set_id_generator=False)
+
+portal = context.getPortalObject()
+configurator = getattr(portal.business_configuration_module, "default_wendelin_configuration", None)
+
+if configurator == None:
+  context.log("Could not find the scalability business configuration object. Be sure to have erp5_scalability_test business template installed.")
+  return
+
+# install configurator if not intalled
+if configurator.getCurrentStateTitle() != "End":
+  # nothing installed, thus do it
+  context.ERP5Site_bootstrapScalabilityTest(user_quantity=0, setup_activity_tool=False, create_test_data=False, set_id_generator=False)
+else:
+  context.log("All configured. Nothing to do.")
