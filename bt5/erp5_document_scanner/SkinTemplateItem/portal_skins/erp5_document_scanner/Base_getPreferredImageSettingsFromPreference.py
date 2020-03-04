ca00d59912b1e67062fecfd34b32a41ef6c15d78
Gabriel Monnerat <gabriel@nexedi.com>
2020-03-04 16:02:32 +0000

    erp5_document_scanner: Use preference API properly

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getPreferredImageSettingsFromPreference.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getPreferredImageSettingsFromPreference.py
@@ -1,15 +1,12 @@
 import json
 
-portal = context.getPortalObject()
-active_preference = portal.portal_preferences.getActiveUserPreference()
-if not active_preference:
-  active_preference = portal.portal_preferences.getActivePreference()
+preference_tool = context.getPortalObject().portal_preferences
 
 setting_dict = {
-  "compression": active_preference.getPreferredImageScannerConversionCompression(),
-  "enable_greyscale": active_preference.getPreferredImageScannerConversionEnableGreyscale(),
-  "brightness": active_preference.getPreferredImageScannerConversionBrightness(),
-  "contrast": active_preference.getPreferredImageScannerConversionContrast(),
+  "compression": preference_tool.getPreferredImageScannerConversionCompression(),
+  "enable_greyscale": preference_tool.getPreferredImageScannerConversionEnableGreyscale(),
+  "brightness": preference_tool.getPreferredImageScannerConversionBrightness(),
+  "contrast": preference_tool.getPreferredImageScannerConversionContrast(),
 }
 
 return json.dumps(setting_dict)
