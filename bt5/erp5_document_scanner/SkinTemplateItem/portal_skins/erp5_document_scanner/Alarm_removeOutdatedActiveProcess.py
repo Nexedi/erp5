8048d29f836095a52c954d563f6fceb8a8025961
Gabriel Monnerat <gabriel@nexedi.com>
2020-03-04 13:14:25 +0000

    erp5_document_scanner: Improve alarm's id and title

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Alarm_removeOutdatedActiveProcess.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Alarm_removeOutdatedActiveProcess.py
@@ -1,3 +1,5 @@
+from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
+
 portal = context.getPortalObject()
 
 portal.portal_catalog.searchAndActivate(
@@ -5,6 +7,7 @@ portal.portal_catalog.searchAndActivate(
   method_id='ActiveProcess_removeItselfFromActivityTool',
   reference=context.Base_getDocumentScannerDefaultReference(),
   # Active Process don't have creation date set
-  modification_date=(DateTime()-4).earliestTime(),
-  comparison_operator="<"
+  modification_date=SimpleQuery(
+    modification_date=(DateTime()-4).earliestTime(),
+    comparison_operator="<")
 )
