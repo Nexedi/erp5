6f6c80396ec73e862bd9ecf047280a6a5c917f19
Ivan Tyagov <ivan@nexedi.com>
2020-03-05 13:14:07 +0200

    No need to check if no activities left as any left innocently failing activity can mark site as not installed. Fix skin layer and remove Guards so ERP5Site_isReady script can be use from anonymous user (this is fine as long as we use this bt5 ONLY for automated testing).

--- wendelin/bt5/erp5_wendelin_auto_configuration/SkinTemplateItem/portal_skins/erp5_wendelin_auto_configuration/ERP5Site_isReady.py
+++ wendelin/bt5/erp5_wendelin_auto_configuration/SkinTemplateItem/portal_skins/erp5_wendelin_auto_configuration/ERP5Site_isReady.py
@@ -2,10 +2,8 @@
   The purpose of the script is to tell if configuration of a site was finished or not.
   It is used from Deployment Tests.
 """
-number_of_activities = len(context.portal_activities.getMessageList())
-
 installed_bt5_list = [x.getTitle() for x in context.portal_templates.getInstalledBusinessTemplateList()]
-if "erp5_wendelin" in installed_bt5_list and number_of_activities == 0:
+if "erp5_wendelin" in installed_bt5_list:
   return 1
 else:
   return 0
