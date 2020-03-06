7401827ba09e53d3c5221cb15481f8ea6a9d22f0
Romain Courteaud <romain@nexedi.com>
2020-03-06 17:36:37 +0100

    MORE TESTS

--- tests/__init__.py
+++ tests/__init__.py
@@ -39,12 +39,12 @@ class _ERP5(SavedTestSuite):
         # glob('%s/product/Formulator/tests/test*.py' % path) +
         # glob('%s/product/ERP5Form/tests/test*.py' % path) +
         # ['%s/product/ERP5OOo/tests/testDeferredStyle.py' % path] +
-        # ['%s/product/ERP5/tests/testXHTML.py' % path] +
+        ['%s/product/ERP5/tests/testXHTML.py' % path] +
         # ['%s/product/ERP5/tests/testERP5Core.py' % path] +
         # ['%s/product/ERP5/tests/testQueryModule.py' % path] +
         # ['%s/product/ERP5/tests/testBankReconciliation.py' % path] +
         # ['%s/product/ERP5Security/tests/testERP5Security.py' % path] +
-        # ['%s/product/ERP5Type/tests/testFunctionalCore.py' % path] +
+        ['%s/product/ERP5Type/tests/testFunctionalCore.py' % path] +
         # ['%s/product/ERP5Type/tests/testFunctionalAnonymousSelection.py' % path] +
         # glob('%s/bt5/erp5_web/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         # glob('%s/bt5/erp5_web_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
@@ -70,6 +70,8 @@ class _ERP5(SavedTestSuite):
         # glob('%s/bt5/erp5_test_result/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         glob('%s/bt5/erp5_officejs_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         glob('%s/bt5/erp5_web_manifest_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
+        glob('%s/bt5/erp5_km_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
+        glob('%s/bt5/erp5_monaco_editor_ui_test/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         # glob('%s/bt5/erp5_oauth*/TestTemplateItem/portal_components/test.*.test*.py' % path) +
         []
       ):
