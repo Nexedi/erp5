54b53665cb7338b30da39bafd02294df70ae2c00
Ivan Tyagov <ivan@nexedi.com>
2020-03-09 16:08:27 +0200

    This script can be used for deployment tests which do not require these changes thus make it configurable but still keeping the default behaviour as it was.

--- bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_bootstrapScalabilityTest.py
+++ bt5/erp5_scalability_test/SkinTemplateItem/portal_skins/erp5_scalability_test/ERP5Site_bootstrapScalabilityTest.py
@@ -11,7 +11,8 @@ portal_catalog = portal.portal_catalog
 status_code = 0
 error_message = "No error."
 
-context.ERP5Site_setUpActivityTool()
+if setup_activity_tool:
+  context.ERP5Site_setUpActivityTool()
 
 if user_quantity is None: 
   return json.dumps({"status_code" : 1, 
@@ -42,12 +43,14 @@ if configurator.getSimulationState() == "draft":
 
 # create users if installation is done
 try:
-  context.portal_categories.activate(after_method_id = ('ERP5Site_afterConfigurationSetup',
-                                     'immediateReindexObject')
-                                     ).ERP5Site_createTestData(user_quantity, password)
-  context.portal_categories.activate(after_method_id = ('ERP5Site_afterConfigurationSetup',
-                                     'immediateReindexObject')
-                                     ).ERP5Site_setIdGenerator()
+  if create_test_data:
+    context.portal_categories.activate(after_method_id = ('ERP5Site_afterConfigurationSetup',
+                                       'immediateReindexObject')
+                                       ).ERP5Site_createTestData(user_quantity, password)
+  if set_id_generator:                                       
+    context.portal_categories.activate(after_method_id = ('ERP5Site_afterConfigurationSetup',
+                                       'immediateReindexObject')
+                                       ).ERP5Site_setIdGenerator()
 except Exception as e:
   status_code = 1
   error_message = "Error calling ERP5Site_createTestData script: " + str(e)
