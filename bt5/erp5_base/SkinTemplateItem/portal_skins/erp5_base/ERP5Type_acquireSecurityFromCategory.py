3043e7aa4cdedb5e2f9d302be5463b9ef171042e
Jérome Perrin <jerome@nexedi.com>
2020-03-10 05:32:49 +0100

    base: fix pylint messages and enable coding style test

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/ERP5Type_acquireSecurityFromCategory.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/ERP5Type_acquireSecurityFromCategory.py
@@ -1,21 +1,21 @@
 """
 WARNING: this script requires proxy manager
 
-This script tries to acquire category values from other objects
+This script tries to acquire category values from other documents
 
 base_category_list - list of category values we need to retrieve
-object             - object which we want to assign roles to.
+obj                - document which we want to assign roles to.
 """
 category_dict = {}
 
-if object is None:
+if obj is None:
   return []
 
 for base_category in base_category_list:
   if base_category == 'parent':
-    o_list = [object.getParentValue()]
+    o_list = [obj.getParentValue()]
   else:
-    o_list = object.getValueList(base_category)
+    o_list = obj.getValueList(base_category)
   for o in o_list:
     for user, role_list in o.get_local_roles():
       for role in role_list:
