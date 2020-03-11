a7b5c18e935d0b396bc95529deeb9b63e94b3f48
Jérome Perrin <jerome@nexedi.com>
2020-03-11 06:51:45 +0100

    credential: fix pylint messages and enable coding style test

--- bt5/erp5_credential/DocumentTemplateItem/portal_components/document.erp5.CredentialRequest.py
+++ bt5/erp5_credential/DocumentTemplateItem/portal_components/document.erp5.CredentialRequest.py
@@ -31,58 +31,59 @@ from Products.ERP5Type import Permissions, PropertySheet
 from Products.ERP5.Document.Ticket import Ticket
 from erp5.component.mixin.EncryptedPasswordMixin import EncryptedPasswordMixin
 
+
 class CredentialRequest(Ticket, EncryptedPasswordMixin):
+  """
+  """
+
+  meta_type = 'ERP5 Credential Request'
+  portal_type = 'Credential Request'
+  add_permission = Permissions.AddPortalContent
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  # Declarative properties
+  property_sheets = ( PropertySheet.CredentialQuestion
+                    , PropertySheet.DefaultCredentialQuestion
+                    , PropertySheet.Login
+                    , PropertySheet.Codification
+                    , PropertySheet.Person
+                    , PropertySheet.Reference
+                    , PropertySheet.Url
+                    )
+
+  def checkUserCanChangePassword(self):
+    # every body can change a password of a credential request as annonymous
+    # should be able to do it
+    pass
+
+  def checkPasswordValueAcceptable(self, value):
+    # all passwords are acceptable on Credential Request
+    pass
+
+  security.declareProtected(Permissions.AccessContentsInformation,
+                            'getTitle')
+  def getTitle(self, **kw):
     """
+      Returns the title if it exists or a combination of
+      first name and last name
     """
+    if self.title == '':
+      name_list = []
+      if self.getFirstName() not in (None, ''):
+        name_list.append(self.getFirstName())
+      if self.getLastName() not in (None, ''):
+        name_list.append(self.getLastName())
+      if name_list:
+        return ' '.join(name_list)
+      return self.getReference() or self.getId()
+    else:
+      return self.title
 
-    meta_type = 'ERP5 Credential Request'
-    portal_type = 'Credential Request'
-    add_permission = Permissions.AddPortalContent
-
-    # Declarative security
-    security = ClassSecurityInfo()
-    security.declareObjectProtected(Permissions.AccessContentsInformation)
-
-    # Declarative properties
-    property_sheets = ( PropertySheet.CredentialQuestion
-                      , PropertySheet.DefaultCredentialQuestion
-                      , PropertySheet.Login
-                      , PropertySheet.Codification
-                      , PropertySheet.Person
-                      , PropertySheet.Reference
-                      , PropertySheet.Url
-                      )
-
-    def checkUserCanChangePassword(self):
-      # every body can change a password of a credential request as annonymous
-      # should be able to do it
-      pass
-
-    def checkPasswordValueAcceptable(self, value):
-      # all passwords are acceptable on Credential Request
-      pass
-
-    security.declareProtected(Permissions.AccessContentsInformation,
-                              'getTitle')
-    def getTitle(self, **kw):
-      """
-        Returns the title if it exists or a combination of
-        first name and last name
-      """
-      if self.title == '':
-        name_list = []
-        if self.getFirstName() not in (None, ''):
-          name_list.append(self.getFirstName())
-        if self.getLastName() not in (None, ''):
-          name_list.append(self.getLastName())
-        if name_list:
-          return ' '.join(name_list)
-        return self.getReference() or self.getId()
-      else:
-        return self.title
-
-    security.declareProtected(Permissions.AccessContentsInformation,
-                              'hasTitle')
-    def hasTitle(self):
-      return self.title or self.hasFirstName() or self.hasLastName()
+  security.declareProtected(Permissions.AccessContentsInformation,
+                            'hasTitle')
+  def hasTitle(self):
+    return self.title or self.hasFirstName() or self.hasLastName()
 
