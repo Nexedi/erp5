8543590bc0ed113b0b62e709249de1b11b44668b
Ivan Tyagov <ivan@nexedi.com>
2020-03-16 08:47:31 +0200

    erp5_accounting is installed before auto-configurtion and is unreliable thus use erp5_trade which certainly is installed by configurator.

--- bt5/erp5_scalability_test_auto_configuration/SkinTemplateItem/portal_skins/erp5_scalability_test_auto_configuration/ERP5Site_isReady.py
+++ bt5/erp5_scalability_test_auto_configuration/SkinTemplateItem/portal_skins/erp5_scalability_test_auto_configuration/ERP5Site_isReady.py
@@ -2,4 +2,4 @@
   The purpose of the script is to tell if configuration of a site was finished or not.
   It is used from Deployment Tests.
 """
-return context.portal_templates.getInstalledBusinessTemplate("erp5_accounting", strict=True) is not None
+return context.portal_templates.getInstalledBusinessTemplate("erp5_trade", strict=True) is not None
