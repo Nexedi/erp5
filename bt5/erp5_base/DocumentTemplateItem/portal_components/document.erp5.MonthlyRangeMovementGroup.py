00c4a1f39569b97916b4eab1a92f3f21762e4675
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2020-03-19 08:51:30 +0100

    MonthlyRangeMovementGroup: set an appropriate time offset at the end of month.

--- bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.MonthlyRangeMovementGroup.py
+++ bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.MonthlyRangeMovementGroup.py
@@ -27,6 +27,7 @@
 ##############################################################################
 
 from erp5.component.document.MovementGroup import MovementGroup
+from DateTime import DateTime
 from Products.ERP5Type.DateUtils import atTheEndOfPeriod
 
 
@@ -48,7 +49,9 @@ class MonthlyRangeMovementGroup(MovementGroup):
     for property_name in self.getTestedPropertyList() or ('start_date', 'stop_date'):
       date = movement.getProperty(property_name, None)
       if date is not None:
-        end_of_month = atTheEndOfPeriod(date, 'month')-1
+        # wrap atTheEndOfPeriod value with DateTime(value..strftime('%Y/%m/%d')) so that
+        # it returns a value with an appropriate time offset at that date.
+        end_of_month = DateTime((atTheEndOfPeriod(date, 'month')-0.5).strftime('%Y/%m/%d'))
         property_dict[property_name] = end_of_month
     return property_dict
 
