a8b2c5c46be19dbe88a6b8d767b7afd5a61681f7
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-03-19 09:29:56 +0000

    erp5_commerce_loyalty_program: remove useless file

--- bt5/erp5_commerce_loyalty_program/SkinTemplateItem/portal_skins/erp5_commerce_loyalty_program/Alarm_deliverLoyaltyTransaction.py
+++
@@ -1,8 +0,0 @@
-portal = context.getPortalObject()
-search_kw = {'portal_type': 'Loyalty Transaction',
-             'simulation_state': 'confirmed',
-            }
-portal.portal_catalog.searchAndActivate(
-           method_id='deliver',
-           activate_kw={'tag':"deliver_loyalty_transaction", 'priority': 8},
-           **search_kw)
