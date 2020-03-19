98e7a734ba8baca5d463f4bb0b1a8b23749e0525
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-03-19 16:45:29 +0000

    erp5_commerce_loyalty_program_test: update tests

--- bt5/erp5_commerce_loyalty_program_test/TestTemplateItem/portal_components/test.erp5.testCommerceLoyaltyProgram.py
+++ bt5/erp5_commerce_loyalty_program_test/TestTemplateItem/portal_components/test.erp5.testCommerceLoyaltyProgram.py
@@ -48,54 +48,20 @@ class testCommerceLoyaltyProgram(ERP5TypeTestCase):
   def afterSetUp(self):
     portal = self.getPortalObject()
     self.portal = portal
-    system_preference = portal.portal_preferences.default_system_preference
-    system_preference.edit(
-      preferred_loyalty_record_currency='currency_module/test_loyalty_record_currency',
-      preferred_loyalty_reward_threshold=180,
-      preferred_loyalty_reward_business_process='business_process_module/erp5_default_business_process',
-      preferred_loyalty_reward_currency='currency_module/test_loyalty_reward_currency',
-      preferred_loyalty_reward_price_='0.3',
-      preferred_loyalty_reward_notification_message_reference='Loyalty_Notification',
-      preferred_loyalty_reward_one_time_trade_condition_reference='STC-ONE-TIME',
-      preferred_loyalty_reward_used_trade_condition_reference='STC-USED-ONE-TIME',
-      preferred_loyalty_reward_trade_model_line_trade_phase = 'default/invoicing'
-    )
-    delete_id_list = []
-    for loyalty_account in self.portal.person_module.test_sale_order_receiver.objectValues(portal_type='Loyalty Account'):
-      delete_id_list.append(loyalty_account.getId())
-    self.portal.person_module.test_sale_order_receiver.manage_delObjects(ids=delete_id_list)
     delivery_builder_list = self.portal.business_process_module.erp5_default_business_process.invoice.getDeliveryBuilderList()
     if 'portal_deliveries/loyalty_transaction_builder' not in delivery_builder_list:
       delivery_builder_list.append('portal_deliveries/loyalty_transaction_builder')
       self.portal.business_process_module.erp5_default_business_process.invoice.edit(
         delivery_builder_list=delivery_builder_list)
-    if portal.portal_workflow.isTransitionPossible(system_preference, 'enable'):
-      system_preference.enable()
     for loyalty_transaction_line in self.portal.portal_catalog(portal_type='Loyalty Transaction Line'):
       loyalty_transaction_line.edit(quantity=0)
-    sale_trade_condition = self.portal.person_module.test_sale_order_receiver.Person_getLoyaltyRewardTradeCondition()
-    if sale_trade_condition and portal.portal_workflow.isTransitionPossible(sale_trade_condition, 'invalidate'):
-      sale_trade_condition.invalidate()
     for rule in self.portal.portal_rules.objectValues():
       if rule.getValidationState() != 'validated':
         rule.validate()
-
     self.tic()
 
-  def failUnlessMailSentWithText(self, text, last_messages_amount=2,to=None):
-    success = False
-    last_message = self.portal.MailHost._last_message
-    if text in last_message[-1]:
-      if to and 0:
-        for recipient in last_message[1]:
-          if '"%s"' % to in recipient.replace('\\', ''):
-            success = True
-            break
-      else:
-        success = True
-    self.assertTrue(success)
 
-  def createSaleOrder(self, specialise='business_process_module/erp5_default_business_process'):
+  def createSaleOrder(self, specialise='sale_trade_condition_module/test_loyalty_program'):
     sale_order = self.portal.sale_order_module.newContent(
       portal_type='Sale Order',
       destination_section = 'person_module/test_sale_order_receiver',
@@ -126,7 +92,7 @@ class testCommerceLoyaltyProgram(ERP5TypeTestCase):
     sale_packing_list.deliver()
     return sale_packing_list
 
-  def generateConfirmedLoyaltyTransaction(self):
+  def generateLoyaltyTransaction(self):
     sale_order=self.createSaleOrder()
     self.deliverSaleOrder(sale_order)
     self.tic()
@@ -134,77 +100,57 @@ class testCommerceLoyaltyProgram(ERP5TypeTestCase):
     self.tic()
     return sale_order.getCausalityRelatedValue(portal_type='Loyalty Transaction')
 
+  def createLoyaltyAccountIfNotExisted(self, user):
+    if not user.objectValues(portal_type='Loyalty Account'):
+      return user.newContent(portal_type='Loyalty Account')
 
-  def test_loyalty_transaction_creation(self):
-    loyalty_transaction = self.generateConfirmedLoyaltyTransaction()
+  def test_no_loyalty_point_if_no_loyalty_account(self):
+    delete_id_list = []
+    for loyalty_account in self.portal.person_module.test_sale_order_receiver.objectValues(portal_type='Loyalty Account'):
+      delete_id_list.append(loyalty_account.getId())
+    self.portal.person_module.test_sale_order_receiver.manage_delObjects(ids=delete_id_list)
+    self.tic()
+    sale_order=self.createSaleOrder()
+    self.deliverSaleOrder(sale_order)
+    self.tic()
+    self.portal.portal_alarms.loyalty_transaction_builder_alarm.activeSense()
+    self.tic()
+    loyalty_transaction = sale_order.getCausalityRelatedValue(portal_type='Loyalty Transaction')
+    self.assertEqual(loyalty_transaction, None)
+    self.assertEqual(self.portal.person_module.test_sale_order_receiver.Person_getTotalLoyaltyPoint(), 0)
+
+  def test_collect_loyalty_point_fixed_quantity(self):
+    self.createLoyaltyAccountIfNotExisted(self.portal.person_module.test_sale_order_receiver)
+    # if specify quantity is defined, only get such quantity
+    self.portal.sale_trade_condition_module.default_loyalty_program.collect_point.setLoyaltyPointQuantity(10)
+    self.tic()
+    loyalty_transaction = self.generateLoyaltyTransaction()
     self.assertTrue(loyalty_transaction is not None)
-    self.assertEqual(loyalty_transaction.getSimulationState(), 'confirmed')
-    self.assertEqual(loyalty_transaction.getTotalQuantity(), 10*10)
-    self.assertEqual(loyalty_transaction.getDestinationSection(), 'person_module/test_sale_order_receiver')
-    self.assertEqual(loyalty_transaction.getDestination(), 'person_module/test_sale_order_receiver')
-    self.assertEqual(loyalty_transaction.getSourceSection(), 'organisation_module/test_sale_order_supplier')
-    self.assertEqual(loyalty_transaction.getSource(), 'organisation_module/test_sale_order_supplier')
+    self.assertEqual(self.portal.person_module.test_sale_order_receiver.Person_getTotalLoyaltyPoint(), 10)
 
-  def test_loyalty_trade_condition_creation(self):
-    loyalty_transaction = self.generateConfirmedLoyaltyTransaction()
-    self.portal.portal_alarms.loyalty_deliver.activeSense()
+  def test_collect_loyalty_point_percentage(self):
+    self.createLoyaltyAccountIfNotExisted(self.portal.person_module.test_sale_order_receiver)
+    # if specify quantity is defined, only get such quantity
+    self.portal.sale_trade_condition_module.default_loyalty_program.collect_point.setLoyaltyPointQuantity(0)
+    self.portal.sale_trade_condition_module.default_loyalty_program.collect_point.setLoyaltyPointPrice(0.5)
     self.tic()
-    self.assertEqual(loyalty_transaction.getSimulationState(), 'delivered')
-    self.portal.portal_alarms.loyalty_reward_creation.activeSense()
-    self.tic()
-    sale_trade_condition = self.portal.person_module.test_sale_order_receiver.Person_getLoyaltyRewardTradeCondition()
-    #loyalty is 100 < 150
-    self.assertTrue(not sale_trade_condition)
-    loyalty_transaction = self.generateConfirmedLoyaltyTransaction()
-    self.portal.portal_alarms.loyalty_deliver.activeSense()
-    self.tic()
-    self.portal.portal_alarms.loyalty_reward_creation.activeSense()
-    sale_trade_condition = self.portal.person_module.test_sale_order_receiver.Person_getLoyaltyRewardTradeCondition()
-    # no loyalty account
-    self.assertTrue(not sale_trade_condition)
-    self.portal.person_module.test_sale_order_receiver.newContent(portal_type='Loyalty Account')
-    self.tic()
-    self.portal.portal_alarms.loyalty_reward_creation.activeSense()
-    sale_trade_condition = self.portal.person_module.test_sale_order_receiver.Person_getLoyaltyRewardTradeCondition()
-    self.assertEqual(sale_trade_condition.getSpecialise(), self.portal.portal_preferences.getPreferredLoyaltyRewardBusinessProcess())
-    self.assertEqual(sale_trade_condition.getPriceCurrency(), self.portal.portal_preferences.getPreferredLoyaltyRewardCurrency())
-    trade_model_line = sale_trade_condition.objectValues(portal_type='Trade Model Line')
-    self.assertEqual(1, len(trade_model_line))
-    trade_model_line = trade_model_line[0]
-    self.assertEqual(trade_model_line.getPrice(), self.portal.portal_preferences.getPreferredLoyaltyRewardPrice())
-    self.assertEqual(trade_model_line.getUse(), 'trade/discount_service')
-    self.assertEqual(trade_model_line.getBaseApplication(), 'base_amount/discounted')
-    loyalty_transaction = sale_trade_condition.getCausalityRelatedValue(portal_type='Loyalty Transaction')
-    self.assertEqual(loyalty_transaction.getSimulationState(), 'delivered')
-    self.assertEqual(loyalty_transaction.getTotalQuantity(), -self.portal.portal_preferences.getPreferredLoyaltyRewardThreshold())
+    loyalty_transaction = self.generateLoyaltyTransaction()
+    self.assertTrue(loyalty_transaction is not None)
+    self.assertEqual(self.portal.person_module.test_sale_order_receiver.Person_getTotalLoyaltyPoint(), 100*0.5)
 
-  def test_loyalty_trade_condition_invalidation(self):
-    self.generateConfirmedLoyaltyTransaction()
-    self.generateConfirmedLoyaltyTransaction()
-    self.portal.person_module.test_sale_order_receiver.newContent(portal_type='Loyalty Account')
+  def test_use_loyalty_point(self):
+    # initialise loyalty point to 50
+    self.createLoyaltyAccountIfNotExisted(self.portal.person_module.test_sale_order_receiver)
+    self.portal.sale_trade_condition_module.default_loyalty_program.collect_point.setLoyaltyPointQuantity(0)
+    self.portal.sale_trade_condition_module.default_loyalty_program.collect_point.setLoyaltyPointPrice(0.5)
     self.tic()
-    self.portal.portal_alarms.loyalty_deliver.activeSense()
+    self.generateLoyaltyTransaction()
     self.tic()
-    self.portal.portal_alarms.loyalty_reward_creation.activeSense()
+    #spend 10 point for coupon, and %10 of sale order price point for discount
+    self.portal.sale_trade_condition_module.default_loyalty_program.coupon.setLoyaltyPointQuantity(-10)
+    self.portal.sale_trade_condition_module.default_loyalty_program.discount.setLoyaltyPointQuantity(0)
+    self.portal.sale_trade_condition_module.default_loyalty_program.discount.setLoyaltyPointPrice(-0.1)
     self.tic()
-    sale_trade_condition = self.portal.person_module.test_sale_order_receiver.Person_getLoyaltyRewardTradeCondition()
-    sale_order = self.createSaleOrder(specialise=sale_trade_condition.getRelativeUrl())
-    sale_order.confirm()
+    self.generateLoyaltyTransaction()
     self.tic()
-    self.portal.portal_alarms.loyalty_reward_invalidation.activeSense()
-    self.tic()
-    self.assertTrue(sale_trade_condition.getReference().startswith(self.portal.portal_preferences.getPreferredLoyaltyRewardUsedTradeConditionReference()))
-
-  def test_notify_loyalty_reward(self):
-    self.portal.notification_message_module.test_notification_message.edit(
-      reference = self.portal.portal_preferences.getPreferredLoyaltyRewardNotificationMessageReference())
-    self.generateConfirmedLoyaltyTransaction()
-    self.generateConfirmedLoyaltyTransaction()
-    self.portal.person_module.test_sale_order_receiver.newContent(portal_type='Loyalty Account')
-    self.tic()
-    self.portal.portal_alarms.loyalty_deliver.activeSense()
-    self.tic()
-    self.portal.portal_alarms.loyalty_reward_creation.activeSense()
-    self.tic()
-    self.failUnlessMailSentWithText('Congratulations',to=self.portal.person_module.test_sale_order_receiver.getTitle())
-
+    self.assertEqual(self.portal.person_module.test_sale_order_receiver.Person_getTotalLoyaltyPoint(), 100*0.5 + 100*0.5 - 10 - 100*0.1)
