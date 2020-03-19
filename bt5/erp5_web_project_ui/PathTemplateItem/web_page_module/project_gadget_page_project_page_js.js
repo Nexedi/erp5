6e5138d4e212ccfd35670c752439bf99bf2f75e8
Roque Porchetto <roque.porchetto@nexedi.com>
2020-03-19 17:34:48 +0000

    erp5_web_project_ui: styles for project home pages domsugar

--- bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_project_page_js.js
+++ bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_project_page_js.js
@@ -1,8 +1,8 @@
 /*jslint nomen: true, indent: 2 */
 /*global window, rJS, RSVP, document, ensureArray, DOMParser, XMLSerializer,
-SimpleQuery, ComplexQuery, Query*/
+SimpleQuery, ComplexQuery, Query, domsugar*/
 (function (window, rJS, RSVP, document, ensureArray, DOMParser, XMLSerializer,
-            SimpleQuery, ComplexQuery, Query) {
+            SimpleQuery, ComplexQuery, Query, domsugar) {
   "use strict";
 
   var VALID_STATE_LIST = ["shared", "released", "published",
@@ -21,13 +21,12 @@ SimpleQuery, ComplexQuery, Query*/
     var parser = new DOMParser(), i,
       oSerializer = new XMLSerializer(),
       doc = parser.parseFromString(html, "text/html"),
-      link_list = doc.querySelectorAll("a"),
-      styles_link = document.createElement('link'),
-      header = doc.querySelector("head");
-    styles_link.href = "gadget_erp5_page_project.css";
-    styles_link.type = "text/css";
-    styles_link.rel = "stylesheet";
-    header.appendChild(styles_link);
+      link_list = doc.querySelectorAll("a");
+    doc.querySelector("head").appendChild(domsugar('link', {
+      href: "gadget_erp5_page_project.css",
+      type: "text/css",
+      rel: "stylesheet"
+    }));
     for (i = 0; i < link_list.length; i += 1) {
       link_list[i].setAttribute('href',
                                 addRedirectionToReference(link_list[i]
@@ -354,4 +353,4 @@ SimpleQuery, ComplexQuery, Query*/
       return true;
     });
 
-}(window, rJS, RSVP, document, ensureArray, DOMParser, XMLSerializer, SimpleQuery, ComplexQuery, Query));
+}(window, rJS, RSVP, document, ensureArray, DOMParser, XMLSerializer, SimpleQuery, ComplexQuery, Query, domsugar));
