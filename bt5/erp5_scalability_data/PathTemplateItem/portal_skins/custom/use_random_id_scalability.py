0128b31e56e37443a295659c708fdb9ccdc5b6a5
Jérome Perrin <jerome@nexedi.com>
2020-03-26 08:18:45 +0100

    scalability_data: remove
    This business template is not used and contain a preference referencing
    non existing paths, which causes errors in coding style test with
    test_moduleListMethod that cannot display accounting module, because
    preference is set to group/my_group which does not exist.
    
    Instead of trying to fix, let's remove the business template if it's not
    used.

--- bt5/erp5_scalability_data/PathTemplateItem/portal_skins/custom/use_random_id_scalability.py
+++
@@ -1,9 +0,0 @@
-portal = context.getPortalObject()
-
-# Set id generator
-portal.portal_simulation.setIdGenerator("_generatePerDayId")
-portal.web_page_module.setIdGenerator("_generatePerDayId")
-portal.sale_order_module.setIdGenerator("_generatePerDayId")
-portal.person_module.setIdGenerator("_generatePerDayId")
-
-return 1
