4568cc17b3a4e247d21b53238644793d5f4af515
Jérome Perrin <jerome@nexedi.com>
2020-03-27 04:31:10 +0100

    budget: fix pylint messages and enable coding style
    Not that the old `reversed` function was different from the builtin, we
    replace it with sorted(reverse=True) to keep the same logic.

--- bt5/erp5_budget/SkinTemplateItem/portal_skins/erp5_budget/BudgetCell_asPredicate.py
+++ bt5/erp5_budget/SkinTemplateItem/portal_skins/erp5_budget/BudgetCell_asPredicate.py
@@ -1,13 +1,3 @@
 # We don't have good implementation of budget predicate yet, so don't fill predicate table for now.
 return None
-
-# XXX strict membership is missing ?
-# ideally strict membership should be an option for each category, but this API
-# does not exist in predicates.
-
 # TODO: support virtual "All other nodes"
-
-
-return context.generatePredicate(
-      multimembership_criterion_base_category_list = ('resource',),
-      criterion_property_list = ('start_date', 'stop_date'))
