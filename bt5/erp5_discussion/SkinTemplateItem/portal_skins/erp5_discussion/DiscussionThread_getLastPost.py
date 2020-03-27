230a826624d05ee57d74599eff9f5762e5d68388
Romain Courteaud <romain@nexedi.com>
2020-03-27 10:47:41 +0000

    erp5_discussion: only the first element is needed

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionThread_getLastPost.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionThread_getLastPost.py
@@ -1,7 +1,8 @@
 """
   Get last Disccussion Post for a Discussion Thread.
 """
-post_list = context.searchFolder(portal_type='Discussion Post', \
+post_list = context.searchFolder(portal_type='Discussion Post',
+                                 limit=1,
                                  sort_on=[('creation_date','descending')])
 if len(post_list) == 0:
   return None
