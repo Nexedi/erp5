3e4f3f3615ed375a1f4fcf6834339eb776c0dbd0
Romain Courteaud <romain@nexedi.com>
2020-03-30 08:16:41 +0000

    erp5_document_scanner: add precache script

---
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/WebSection_getDocumentScannerPrecacheManifestList.py
@@ -0,0 +1,12 @@
+url_list = [
+  "renderjs.js",
+  "rsvp.js",
+  "gadget_document_scanner.html",
+  "gadget_document_scanner.js",
+  "gadget_document_scanner.css",
+  "cropper.min.css",
+  "cropper.min.js",
+  "caman.full.min.js"
+]
+
+return url_list
