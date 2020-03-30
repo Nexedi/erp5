9ba630f45f5319289c1b3fa9cffdc0679db5d7b9
Jérome Perrin <jerome@nexedi.com>
2020-03-30 08:45:37 +0200

    core/administration: move scripts used in coding style to core
    These scripts are used in coding style tests, but because it was in
    erp5_administration, all coding style tests had to install
    erp5_administration and all its dependencies. Which is against one idea
    of the coding style test, which is supposed to check that business
    templates declare their dependencies correctly, but if we install all
    business templates that erp5_administration depends on, the test might
    have false positives of missing dependencies.

---
+++ product/ERP5/bootstrap/erp5_core/ExtensionTemplateItem/portal_components/extension.erp5.PythonCodeUtils.py
@@ -0,0 +1,46 @@
+import json
+from Products.ERP5Type.Utils import checkPythonSourceCode
+
+def checkPythonSourceCodeAsJSON(self, data, REQUEST=None):
+  """
+  Check Python source suitable for Source Code Editor and return a JSON object
+  """
+
+  # XXX data is encoded as json, because jQuery serialize lists as []
+  if isinstance(data, basestring):
+    data = json.loads(data)
+
+  # data contains the code, the bound names and the script params. From this
+  # we reconstruct a function that can be checked
+  def indent(text):
+    return ''.join(("  " + line) for line in text.splitlines(True))
+
+  is_python_script = 'bound_names' in data
+  if is_python_script:
+    signature_parts = data['bound_names']
+    if data['params']:
+      signature_parts += [data['params']]
+    signature = ", ".join(signature_parts)
+
+    function_name = "function_name"
+    body = "def %s(%s):\n%s" % (function_name,
+                                signature,
+                                indent(data['code']) or "  pass")
+  else:
+    body = data['code']
+
+  message_list = checkPythonSourceCode(body.encode('utf8'), data.get('portal_type'))
+  for message_dict in message_list:
+    if is_python_script:
+      message_dict['row'] = message_dict['row'] - 2
+    else:
+      message_dict['row'] = message_dict['row'] - 1
+
+    if message_dict['type'] in ('E', 'F'):
+      message_dict['type'] = 'error'
+    else:
+      message_dict['type'] = 'warning'
+
+  if REQUEST is not None:
+    REQUEST.RESPONSE.setHeader('content-type', 'application/json')
+  return json.dumps(dict(annotations=message_list))
