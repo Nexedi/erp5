8e4ac7d5b35e84c4e2e4d4bfebf0807664aa12b7
Jérome Perrin <jerome@nexedi.com>
2020-03-31 06:50:32 +0200

    ods_style: fix pylint messages and enable coding style test

--- bt5/erp5_ods_style/SkinTemplateItem/portal_skins/erp5_ods_style/Base_getDynamicODSStyleSheet.py
+++ bt5/erp5_ods_style/SkinTemplateItem/portal_skins/erp5_ods_style/Base_getDynamicODSStyleSheet.py
@@ -1,26 +1,7 @@
-from Products.ERP5Type.Message import Message
-
 # XXX for now, we always use the default Base_getODSStyleSheet
 # we use to have Base_getODSListStyleSheet with a line at the bottom of
-# the page, for better print display. Now we rather agreed that
-# ods_style is a style for export, not report and the rendering appearance
-# was not so important.
+# the page, for better print display and also Base_getODSStyleSheetLandscape
+# when landscape mode is used.
+# Now we rather agreed that ods_style is a style for export, not report and
+# the rendering appearance was not so important.
 return context.Base_getODSStyleSheet
-
-translate = lambda msg: Message('ui', msg)
-request = context.REQUEST
-landscape = int(request.get('landscape', 0))
-if context.pt != 'form_list':
-  if landscape == 1:
-    #normal style sheet with preview of landscape
-    return context.Base_getODSStyleSheetLandscape
-  else:
-    #preview portrait(Default) 
-    return context.Base_getODSStyleSheet
-else:
-  if landscape == 1:
-    #style sheet for list, there is under line in preview
-    return context.Base_getODSListStyleSheetLandscape
-  else:
-    #preview portrait(Default)
-    return context.Base_getODSListStyleSheet
