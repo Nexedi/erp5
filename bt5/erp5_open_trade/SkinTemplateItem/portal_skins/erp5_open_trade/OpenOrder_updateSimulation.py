b8d5be3c1005c12f95c6cd85ba4009691930e819
Jérome Perrin <jerome@nexedi.com>
2020-03-31 06:47:37 +0200

    open_trade: fix pylint messages and enable coding style test

--- bt5/erp5_open_trade/SkinTemplateItem/portal_skins/erp5_open_trade/OpenOrder_updateSimulation.py
+++ bt5/erp5_open_trade/SkinTemplateItem/portal_skins/erp5_open_trade/OpenOrder_updateSimulation.py
@@ -3,7 +3,7 @@ subscription_item_set = set()
 for open_order_line in context.objectValues():
   for ob in [open_order_line] + open_order_line.getCellValueList():
     for item in ob.getAggregateValueList():
-       if getattr(item.aq_explicit, 'updateSimulation', None) is not None and \
+      if getattr(item.aq_explicit, 'updateSimulation', None) is not None and \
           item not in subscription_item_set:
-         subscription_item_set.add(item)
-         item.updateSimulation(expand_root=1)
+        subscription_item_set.add(item)
+        item.updateSimulation(expand_root=1)
