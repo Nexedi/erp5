49faf84525eb83f75ffe344969071743456ffb10
Roque Porchetto <roque.porchetto@nexedi.com>
2020-03-31 08:41:37 +0000

    erp5_officejs: drop appcache in apps and migrate to service worker - text editor - svg editor - codemirror editor - pdf viewer

---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_codemirror/WebSection_getCodemirrorEditorPrecacheManifestList.py
@@ -0,0 +1,21 @@
+url_list = [
+  #code mirror specific
+  "gadget_officejs_codemirror.configuration",
+  "officejs_javascript_editor_icon.png",
+  "gadget_officejs_json_manifest_javascript_editor.json",
+  "gadget_officejs_codemirror_router.html",
+  "gadget_officejs_jio_web_script_view.html",
+  "gadget_officejs_jio_web_script_view.js",
+
+  #app_configuration_resources
+  #CONFIGURATION ELEMENTS generated on Wed Apr 01 14:45:53 2020. Same as in configuration manifest
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBTY3JpcHQ=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBTY3JpcHQvY29kZW1pcnJvcl92aWV3",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb2ZmaWNlanNfY29kZW1pcnJvci9XZWJQYWdlX3ZpZXdBc0ppb0ZvckNvZGVtaXJyb3I=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZS9jb2RlbWlycm9yX3ZpZXc=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb2ZmaWNlanNfY29kZW1pcnJvci9XZWJQYWdlTW9kdWxlX3ZpZXdXZWJQYWdlTGlzdEFzSmlvRm9yQ29kZW1pcnJvcg==",
+  #/app_configuration_resources
+]
+
+return url_list
