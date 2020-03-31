0db6e4e58b691568f30922778fe7e51ddcfea27e
Roque Porchetto <roque.porchetto@nexedi.com>
2020-03-31 08:41:37 +0000

    erp5_officejs: drop appcache and migrate to service worker

---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_text_editor/WebSection_getTextEditorPrecacheManifestList.py
@@ -0,0 +1,25 @@
+url_list = [
+  #text editor specific
+  "gadget_officejs_text_editor.configuration",
+  "gadget_officejs_text_editor.json",
+  "officejs_logo_text_editor.png",
+  "gadget_officejs_text_editor_router.html",
+
+  #app custom actions
+  "action_texteditor_clone.html",
+  "action_texteditor_clone.js",
+
+  #app_configuration_resources
+  #CONFIGURATION ELEMENTS generated on Fri Dec 13 14:45:53 2019. Same as in configuration manifest
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZS90ZXh0X2VkaXRvcl92aWV3",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfdGV4dF9lZGl0b3IvV2ViUGFnZU1vZHVsZV92aWV3V2ViUGFnZUxpc3RBc0ppb0ZvclRleHRFZGl0b3I=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdl",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlL3RleHRfZWRpdG9yX3ZpZXc=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfdGV4dF9lZGl0b3IvV2ViUGFnZV92aWV3QXNUZXh0RG9jdW1lbnRGb3JUZXh0RWRpdG9y",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlL3RleHRfZWRpdG9yX2Nsb25l",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfdGV4dF9lZGl0b3IvQmFzZV9jbG9uZURvY3VtZW50Rm9yVGV4dEVkaXRvcg==",
+  #/app_configuration_resources
+]
+
+return url_list
