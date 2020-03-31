1748c2f3399b33d3d1c1c2b43193400097affbb8
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-03-31 14:59:20 +0900

    ZODB Components: Ignore low-level TypeProvider (TypesTool and SolverTool) classes when migrating code from filesystem.

--- product/ERP5/Document/BusinessTemplate.py
+++ product/ERP5/Document/BusinessTemplate.py
@@ -6617,7 +6617,9 @@ Business Template is a set of definitions, such as skins, portal types and categ
                 continue
 
               from Products.ERP5Type.ERP5Type import ERP5TypeInformation
-              if issubclass(document_class, ERP5TypeInformation):
+              from Products.ERP5Type.Tool.TypesTool import TypeProvider
+              if issubclass(document_class, (ERP5TypeInformation,
+                                             TypeProvider)):
                 # Skip for now as all portal_types/* must be able to be loaded
                 # to generate Workflow methods...
                 continue
