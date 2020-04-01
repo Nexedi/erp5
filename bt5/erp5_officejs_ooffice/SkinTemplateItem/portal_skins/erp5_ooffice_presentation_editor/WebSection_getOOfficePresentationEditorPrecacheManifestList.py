dd95e193814f2195a5ea639d29ecaa84d62f2eb1
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-01 13:00:10 +0000

    erp5_officejs_ooffice: drop appcache in apps and migrate to service worker - text editor - spreadsheet editor - presentation editor

---
+++ bt5/erp5_officejs_ooffice/SkinTemplateItem/portal_skins/erp5_ooffice_presentation_editor/WebSection_getOOfficePresentationEditorPrecacheManifestList.py
@@ -0,0 +1,25 @@
+url_list = [
+  #presentation specific
+  "gadget_ooffice_presentation.configuration",
+  "gadget_ooffice_presentation_router.html",
+  "gadget_officejs_jio_presentation_view.html",
+  "gadget_officejs_jio_presentation_view.js",
+  "gadget_ooffice_presentation.json",
+  "500px-Gnome-x-office-presentation.svg.png",
+
+  #app_configuration_resources
+  #CONFIGURATION ELEMENTS generated on Wed Apr 01 14:45:53 2020. Same as in configuration manifest
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZS9vb2ZmaWNlX3ByZXNlbnRhdGlvbl92aWV3",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV9wcmVzZW50YXRpb25fZWRpdG9yL0RvY3VtZW50TW9kdWxlX3ZpZXdEb2N1bWVudExpc3RBc0ppb0Zvck9vZmZpY2VQcmVzZW50YXRpb25FZGl0b3I=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZS9vb2ZmaWNlX3VwbG9hZF9kb2N1bWVudA==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV9wcmVzZW50YXRpb25fZWRpdG9yL0Jhc2VfdXBsb2FkRGlhbG9nRm9yT29mZmljZUVkaXRvcg==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1ByZXNlbnRhdGlvbg==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1ByZXNlbnRhdGlvbi9vb2ZmaWNlX3ByZXNlbnRhdGlvbl92aWV3",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV9wcmVzZW50YXRpb25fZWRpdG9yL1ByZXNlbnRhdGlvbkRvY3VtZW50X3ZpZXdBc0ppb0Zvck9vZmZpY2VQcmVzZW50YXRpb25FZGl0b3I=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1ByZXNlbnRhdGlvbi9vb2ZmaWNlX2Rvd25sb2FkX2RvY3VtZW50",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV9wcmVzZW50YXRpb25fZWRpdG9yL0Jhc2VfZG93bmxvYWREaWFsb2dGb3JPb2ZmaWNlRWRpdG9y",
+  #/app_configuration_resources
+]
+
+return url_list
