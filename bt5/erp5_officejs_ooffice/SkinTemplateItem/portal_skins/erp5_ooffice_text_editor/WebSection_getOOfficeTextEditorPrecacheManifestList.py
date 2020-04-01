67d63764af757982c06b3e1c18c8682dd62f9035
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-01 13:00:10 +0000

    erp5_officejs_ooffice: drop appcache in apps and migrate to service worker - text editor - spreadsheet editor - presentation editor

---
+++ bt5/erp5_officejs_ooffice/SkinTemplateItem/portal_skins/erp5_ooffice_text_editor/WebSection_getOOfficeTextEditorPrecacheManifestList.py
@@ -0,0 +1,25 @@
+url_list = [
+  #text specific
+  "gadget_ooffice_text.configuration",
+  "gadget_ooffice_text_router.html",
+  "gadget_officejs_jio_text_view.html",
+  "gadget_officejs_jio_text_view.js",
+  "gadget_ooffice_text.json",
+  "500px-Gnome-x-office-document.svg.png",
+
+  #app_configuration_resources
+  #CONFIGURATION ELEMENTS generated on Wed Apr 01 14:45:53 2020. Same as in configuration manifest
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1RleHQ=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1RleHQvb29mZmljZV90ZXh0X3ZpZXc=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV90ZXh0X2VkaXRvci9UZXh0RG9jdW1lbnRfdmlld0FzSmlvRm9yT29mZmljZVRleHRFZGl0b3I=",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1RleHQvb29mZmljZV9kb3dubG9hZF9kb2N1bWVudA==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV90ZXh0X2VkaXRvci9CYXNlX2Rvd25sb2FkRGlhbG9nRm9yT29mZmljZUVkaXRvcg==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZS9vb2ZmaWNlX3RleHRfdmlldw==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV90ZXh0X2VkaXRvci9Eb2N1bWVudE1vZHVsZV92aWV3RG9jdW1lbnRMaXN0QXNKaW9Gb3JPb2ZmaWNlVGV4dEVkaXRvcg==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL0RvY3VtZW50IE1vZHVsZS9vb2ZmaWNlX3VwbG9hZF9kb2N1bWVudA==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfb29mZmljZV90ZXh0X2VkaXRvci9CYXNlX3VwbG9hZERpYWxvZ0Zvck9vZmZpY2VFZGl0b3I=",
+  #/app_configuration_resources
+]
+
+return url_list
