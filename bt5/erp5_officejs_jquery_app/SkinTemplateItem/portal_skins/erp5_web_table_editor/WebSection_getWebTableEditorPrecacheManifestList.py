8252302cc1fe3556e0a0b0e6715d60c8e409319a
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-01 15:24:20 +0000

    erp5_officejs_jquery_app: drop appcache in apps and migrate to service worker - image editor - web table editor

---
+++ bt5/erp5_officejs_jquery_app/SkinTemplateItem/portal_skins/erp5_web_table_editor/WebSection_getWebTableEditorPrecacheManifestList.py
@@ -0,0 +1,57 @@
+url_list = [
+  #web table specific
+  "gadget_officejs_web_table.configuration",
+  "gadget_officejs_web_table_router.html",
+  "gadget_officejs_jio_web_table_view.html",
+  "gadget_officejs_jio_web_table_view.js",
+  "gadget_officejs_web_table.json",
+  "web_table_app_logo.svg",
+
+  #jquery-sheets
+  "jquery.js",
+  "jquery-sheets.gadget.html",
+  "jquery-sheets.gadget.js",
+  "jquery-sheets/wickedgrid.css",
+  "jquery-sheets/menu.html",
+  "jquery-sheets/font-awesome.css",
+  "jquery-sheets/js/thaw.js",
+  "jquery-sheets/js/raphael.js",
+  "jquery-sheets/js/copy3_of_undomanager.js",
+  "jquery-sheets/js/jquery-ui.js",
+  "jquery-sheets/js/globalize.js",
+  "jquery-sheets/js/jquery.nearest.js",
+  "jquery-sheets/js/megatable.js",
+  "jquery-sheets/js/wickedgrid.js",
+  "jquery-sheets/js/graphael/g.bar.js",
+  "jquery-sheets/js/graphael/g.line.js",
+  "jquery-sheets/js/graphael/g.raphael.js",
+  "jquery-sheets/js/graphael/g.pie.js",
+  "jquery-sheets/js/graphael/g.dot.js",
+  "jquery-sheets/js/ZeroClipboard.js",
+  "jquery-sheets/js/jquery.elastic.source.js",
+  "jquery-sheets/js/infiniscroll.js",
+  "jquery-sheets/js/undomanager.js",
+  "jquery-sheets/js/MouseWheel.js",
+  "jquery-sheets/bootstrap-theme.css",
+  "jquery-sheets/images/underline1.svg",
+  "jquery-sheets/images/strikethrough.svg",
+  "jquery-sheets/images/right183.svg",
+  "jquery-sheets/images/left181.svg",
+  "jquery-sheets/images/centered5.svg",
+  "jquery-sheets/images/italic3.svg",
+  "jquery-sheets/images/bold13.svg",
+  "jquery-sheets/bootstrap.css",
+
+  #app_configuration_resources
+  #CONFIGURATION ELEMENTS generated on Thu Apr 02 20:31:13 2019. Same as in configuration manifest
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBQYWdlIE1vZHVsZS93ZWJfdGFibGVfdmlldw==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfd2ViX3RhYmxlX2VkaXRvci9XZWJQYWdlTW9kdWxlX3ZpZXdXZWJUYWJsZUxpc3RBc0ppb0ZvcldlYlRhYmxlRWRpdG9y",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBUYWJsZQ==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3R5cGVzL1dlYiBUYWJsZS93ZWJfdGFibGVfdmlldw==",
+  "hateoas_appcache/definition_view/cG9ydGFsX3NraW5zL2VycDVfd2ViX3RhYmxlX2VkaXRvci9XZWJUYWJsZV92aWV3QXNKaW9Gb3JXZWJUYWJsZUVkaXRvcg==",
+  #/app_configuration_resources
+
+]
+
+return url_list
