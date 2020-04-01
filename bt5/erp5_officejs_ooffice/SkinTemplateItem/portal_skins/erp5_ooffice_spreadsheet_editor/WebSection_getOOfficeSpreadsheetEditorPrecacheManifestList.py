500f1c6d6ea56de27da91c9f4c0b7950d1d5e479
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-01 13:00:10 +0000

    erp5_officejs_ooffice: drop appcache in apps and migrate to service worker - text editor - spreadsheet editor - presentation editor

---
+++ bt5/erp5_officejs_ooffice/SkinTemplateItem/portal_skins/erp5_ooffice_spreadsheet_editor/WebSection_getOOfficeSpreadsheetEditorPrecacheManifestList.py
@@ -0,0 +1,11 @@
+url_list = [
+  #spreadsheet specific
+  "gadget_ooffice_spreadsheet.configuration",
+  "gadget_ooffice_spreadsheet_router.html",
+  "gadget_officejs_jio_spreadsheet_view.html",
+  "gadget_officejs_jio_spreadsheet_view.js",
+  "gadget_ooffice_spreadsheet.json",
+  "500px-Gnome-x-office-spreadsheet.svg.png",
+]
+
+return url_list
