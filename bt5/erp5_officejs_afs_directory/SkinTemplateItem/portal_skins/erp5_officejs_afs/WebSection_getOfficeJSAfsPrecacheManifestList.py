7a0d379ee03ab138be21ab7781302bc7cfea93fb
Sven Franck <sven.franck@nexedi.com>
2020-04-02 13:41:34 +0000

    erp5_officejs_afs_directory: move from appcache to serviceworker

---
+++ bt5/erp5_officejs_afs_directory/SkinTemplateItem/portal_skins/erp5_officejs_afs/WebSection_getOfficeJSAfsPrecacheManifestList.py
@@ -0,0 +1,50 @@
+url_list = [
+  # afs page files
+  'gadget_erp5_page_afs_success_case_list.html',
+  'gadget_erp5_page_afs_success_case_list.js',
+  'gadget_erp5_page_afs_success_case.html',
+  'gadget_erp5_page_afs_success_case.js',
+  'gadget_erp5_page_afs_publisher_list.html',
+  'gadget_erp5_page_afs_publisher_list.js',
+  'gadget_erp5_page_afs_publisher.html',
+  'gadget_erp5_page_afs_publisher.js',
+  'gadget_erp5_page_afs_software_list.html',
+  'gadget_erp5_page_afs_software_list.js',
+  'gadget_erp5_page_afs_software.html',
+  'gadget_erp5_page_afs_software.js',
+  'gadget_erp5_page_afs_export.html',
+  'gadget_erp5_page_afs_export.js',
+  'gadget_erp5_page_afs_publisher_statistic.html',
+  'gadget_erp5_page_afs_publisher_statistic.js',
+  'gadget_erp5_page_afs_financial_list.html',
+  'gadget_erp5_page_afs_financial_list.js',
+  #gadget_erp5_page_afs_unsplash.html',
+  #gadget_erp5_page_afs_unsplash.js',
+  'gadget_erp5_page_afs_directory.html',
+  'gadget_erp5_page_afs_directory.js',
+
+  # afs jio
+  'gadget_erp5_afs_publisher_jio.html',
+  'gadget_erp5_afs_publisher_jio.js',
+  'gadget_erp5_afs_publisher_storage.js',
+  'gadget_erp5_afs_export_jio.js',
+  'gadget_erp5_afs_export_jio.html',
+  'gadget_erp5_afs_export_storage.js',
+  # afs custom erp5
+  'gadget_erp5_afs_panel.html',
+  'gadget_erp5_afs_panel.js',
+  'gadget_erp5_afs_router.html',
+  'gadget_erp5_afs_router.js',
+  'gadget_erp5_afs.css',
+  # afs dygraph
+  'gadget_erp5_afs_dygraph.html',
+  'gadget_erp5_afs_dygraph.js',
+  'dygraph.js',
+  'dygraph.css',
+  # afs images
+  'gadget_erp5_afs_camera.png',
+  'gadget_erp5_afs_logo.png',
+  'gadget_erp5_page_afs_banner_unsplash?format='
+]
+
+return url_list
