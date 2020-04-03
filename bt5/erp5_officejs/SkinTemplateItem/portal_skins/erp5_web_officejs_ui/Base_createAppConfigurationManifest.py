2511b70f8b8efb70ea190adbce782a127c3897e5
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-03 10:22:29 +0000

    erp5_officejs: refactor Base_createAppConfigurationManifest

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_officejs_ui/Base_createAppConfigurationManifest.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_officejs_ui/Base_createAppConfigurationManifest.py
@@ -15,10 +15,12 @@ def getElementFromContent(key, content):
 
 portal = context.getPortalObject()
 portal_catalog = portal.portal_catalog
+date = datetime.now().strftime("%c")
 
 try:
 
   hateoas_appcache = context.getLayoutProperty("hateoas_appcache", default="hateoas_appcache")
+
   router_file_reference = context.getLayoutProperty("configuration_router_gadget_url", default="")
   if router_file_reference is "":
     raise ValueError("Router Gadget Layout Property is missing")
@@ -28,7 +30,6 @@ try:
       reference = router_file_reference)
   if router_file is None:
     raise ValueError("Router web page '%s' not found" % router_file_reference)
-
   router_content = router_file.getTextContent()
 
   configuration_file_reference = getElementFromContent("configuration_manifest", router_content)
@@ -43,73 +44,16 @@ try:
                                                reference=configuration_file_reference)
     configuration_manifest.publish()
 
-  portal_skin = getElementFromContent("portal_skin_folder", router_content)
-  if portal_skin is None:
-    raise KeyError("portal_skin_folder setting not found in router")
-  app_action_string = getElementFromContent("app_actions", router_content)
-  if app_action_string is None:
-    raise KeyError("app_actions setting not found in router")
-  date = datetime.now().strftime("%c")
-  app_action_list = []
-
-  app_action_string = app_action_string.replace('(', '[').replace(')', ']').replace(',]', ']').replace("'", '"')
-  app_action_raw_list = json.loads(app_action_string)
-  for app_action in app_action_raw_list:
-    pair = app_action.split(" | ")
-    if len(pair) != 2:
-      raise SyntaxError("Syntax error in app_action router setting")
-    app_action_list.append(pair)
-
-  portal_actions_dict = {}
-  for app_action in app_action_list:
-    portal_type = str(app_action[0])
-    action = str(app_action[1])
-    if portal_type in portal_actions_dict:
-      portal_actions_dict[portal_type] = portal_actions_dict[portal_type] + [action]
-    else:
-      portal_actions_dict[portal_type] = [action]
-
-  new_dialog_form_list = []
-  for portal_type in portal_actions_dict:
-    portal_type_dict_setting = portal_type.replace(" ", '_').lower() + "_dict"
-    portal_type_dict = getElementFromContent(portal_type_dict_setting, router_content)
-    if portal_type_dict is not None:
-      portal_type_dict = json.loads(portal_type_dict)
-      if "new_content_dialog_form" in portal_type_dict:
-        new_dialog_form_list.append(str(portal_type_dict["new_content_dialog_form"]))
-
-  configuration_path_list = []
-
-  for key in portal_actions_dict:
-    path = "portal_types/%s" % key
-    configuration_path_list.append(base64.b64encode(path))
-    for action in portal_actions_dict[key]:
-      path = "portal_types/%s/%s" % (key, action)
-      configuration_path_list.append(base64.b64encode(path))
-      try:
-        action_object = context.restrictedTraverse(path)
-        form = action_object.getActionText().split('/')[-1]
-        path = "portal_skins/%s/%s" % (portal_skin, form)
-        configuration_path_list.append(base64.b64encode(path))
-      except KeyError as e:
-        raise KeyError("Error getting portal action info: " + str(e))
-
-  if new_dialog_form_list:
-    for form in new_dialog_form_list:
-      path = "portal_skins/%s/%s" % (portal_skin, form)
-      configuration_path_list.append(base64.b64encode(path))
-
-  configuration_element_lines = ""
-  for path in configuration_path_list:
-    configuration_element_lines += hateoas_appcache + "/definition_view/" + path + "\n"
-
+  url_list = context.WebSection_getBase64ConfigurationUrlList(batch_mode=0)
+  configuration_element_lines_string = ""
+  for path in url_list:
+    configuration_element_lines_string += hateoas_appcache + "/definition_view/" + path + "\n"
   content = "CONFIGURATION MANIFEST\n# generated on %s\nCACHE:\n\n" % date
-  content += configuration_element_lines
+  content += configuration_element_lines_string
   content += "\nNETWORK:\n*"
   configuration_manifest.setTextContent(content)
 
-
-except (ValueError, KeyError, SyntaxError) as e:
+except (ValueError, KeyError, SyntaxError, AttributeError) as e:
   if batch_mode:
     return 'ERROR: ' + str(e)
   return context.Base_redirect('view', keep_items=dict(portal_status_message='ERROR creating configuration manifest: ' + str(e)))
---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_web_officejs_ui/WebSection_getBase64ConfigurationUrlList.py
@@ -0,0 +1,98 @@
+import json
+import base64
+from Products.ERP5Type.Log import log
+
+def getElementFromContent(key, content):
+  before_template = '"%s" type="text/x-renderjs-configuration">'
+  before = before_template % key
+  after  = '</script>'
+  start = content.find(before) + len(before)
+  stop  = content.find(after, start)
+  result = content[start:stop]
+  if (not "<" in result) and (not ">" in result) and (result != ""):
+    return result
+  return None
+
+portal = context.getPortalObject()
+portal_catalog = portal.portal_catalog
+
+try:
+
+  hateoas_appcache = context.getLayoutProperty("hateoas_appcache", default="hateoas_appcache")
+
+  router_file_reference = context.getLayoutProperty("configuration_router_gadget_url", default="")
+  if router_file_reference is "":
+    raise ValueError("Router Gadget Layout Property is missing")
+  router_file = portal_catalog.getResultValue(
+      portal_type = 'Web Page',
+      reference = router_file_reference)
+  if router_file is None:
+    raise ValueError("Router web page '%s' not found" % router_file_reference)
+  router_content = router_file.getTextContent()
+
+  portal_skin = getElementFromContent("portal_skin_folder", router_content)
+  if portal_skin is None:
+    raise KeyError("portal_skin_folder setting not found in router")
+
+  app_action_string = getElementFromContent("app_actions", router_content)
+  if app_action_string is None:
+    raise KeyError("app_actions setting not found in router")
+
+  app_action_list = []
+  app_action_string = app_action_string.replace('(', '[').replace(')', ']').replace(',]', ']').replace("'", '"')
+  app_action_raw_list = json.loads(app_action_string)
+  for app_action in app_action_raw_list:
+    pair = app_action.split(" | ")
+    if len(pair) != 2:
+      raise SyntaxError("Syntax error in app_action router setting")
+    app_action_list.append(pair)
+
+  portal_actions_dict = {}
+  for app_action in app_action_list:
+    portal_type = str(app_action[0])
+    action = str(app_action[1])
+    if portal_type in portal_actions_dict:
+      portal_actions_dict[portal_type] = portal_actions_dict[portal_type] + [action]
+    else:
+      portal_actions_dict[portal_type] = [action]
+
+  new_dialog_form_list = []
+  for portal_type in portal_actions_dict:
+    portal_type_dict_setting = portal_type.replace(" ", '_').lower() + "_dict"
+    portal_type_dict = getElementFromContent(portal_type_dict_setting, router_content)
+    if portal_type_dict is not None:
+      portal_type_dict = json.loads(portal_type_dict)
+      if "new_content_dialog_form" in portal_type_dict:
+        new_dialog_form_list.append(str(portal_type_dict["new_content_dialog_form"]))
+
+  configuration_path_list = []
+  for key in portal_actions_dict:
+    path = "portal_types/%s" % key
+    configuration_path_list.append(base64.b64encode(path))
+    for action in portal_actions_dict[key]:
+      path = "portal_types/%s/%s" % (key, action)
+      configuration_path_list.append(base64.b64encode(path))
+      try:
+        action_object = context.restrictedTraverse(path)
+        form = action_object.getActionText().split('/')[-1]
+        path = "portal_skins/%s/%s" % (portal_skin, form)
+        configuration_path_list.append(base64.b64encode(path))
+      except KeyError as e:
+        raise KeyError("Error getting portal action info: " + str(e))
+
+  if new_dialog_form_list:
+    for form in new_dialog_form_list:
+      path = "portal_skins/%s/%s" % (portal_skin, form)
+      configuration_path_list.append(base64.b64encode(path))
+
+  url_list = []
+  for path in configuration_path_list:
+    url_list.append(hateoas_appcache + "/definition_view/" + path)
+
+  return url_list
+
+except (ValueError, KeyError, SyntaxError, AttributeError) as e:
+  if batch_mode == "0":
+    raise e
+  log('ERROR generating Base64 configuration url list: ' + str(e))
+  return []
