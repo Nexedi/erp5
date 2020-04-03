5cda2c1bcc82b6e6080dfb72a49f3c548801ead8
Ivan Tyagov <ivan@nexedi.com>
2020-04-03 14:26:15 +0300

    Add two new bt5s and re-create the default configuration used for wendelin in both Scalability and Deployment tests.

--- wendelin/bt5/erp5_wendelin_scalability_test/SkinTemplateItem/portal_skins/erp5_wendelin_scalability_test/BusinessConfiguration_setupWendelinConfigurationSave.py
+++ wendelin/bt5/erp5_wendelin_scalability_test/SkinTemplateItem/portal_skins/erp5_wendelin_scalability_test/BusinessConfiguration_setupWendelinConfigurationSave.py
@@ -33,7 +33,10 @@ bt5_installation_list = ('erp5_full_text_mroonga_catalog',
                          'erp5_wendelin',
                          'erp5_wendelin_examples',
                          'erp5_wendelin_data',
-                         'erp5_wendelin_development'
+                         'erp5_wendelin_development',
+                         # data lake
+                         'erp5_wendelin_data_lake_ingestion',
+                         'erp5_wendelin_data_lake_ui'
                         )
 
 bt5_update_catalog = ('erp5_ingestion_mysql_innodb_catalog', 'erp5_wendelin',)
