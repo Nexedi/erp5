409865eeb2c91b42295e67d4752a1ad24a9a5714
Gabriel Monnerat <gabriel@nexedi.com>
2020-04-09 20:57:35 +0000

    erp5_officejs_support_request_ui: Avoid to hardcode sort_on in the python script
    Because this breaks the sort and filter buttons in the new ERP5 UI

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/Folder_searchFolderSortByReferenceDescending.py
+++
@@ -1,9 +0,0 @@
-if 'sort_on' in kw:
-  del kw['sort_on']
-
-return context.searchFolder(sort_on=[
-    ('modification_date', 'descending'),
-    # XXX to get stable test result, we also sort by start date (because modification date
-    # has a one second precision)
-    ('delivery.start_date', 'descending'),
-    ], **kw)
