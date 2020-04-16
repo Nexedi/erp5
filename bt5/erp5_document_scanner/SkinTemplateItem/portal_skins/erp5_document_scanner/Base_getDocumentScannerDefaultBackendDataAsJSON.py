c5f05bf6f0c0f073eb3592d6c3e15b41c8cfda40
Georgios Dagkakis <georgios.dagkakis@nexedi.com>
2020-04-16 14:24:16 +0000

    erp5_document_scanner: add Manager proxy role to script
    for Base_getDocumentScannerDefaultBackendDataAsJSON
    since we need to be able to create Active Process

--- bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getDocumentScannerDefaultBackendDataAsJSON.py
+++ bt5/erp5_document_scanner/SkinTemplateItem/portal_skins/erp5_document_scanner/Base_getDocumentScannerDefaultBackendDataAsJSON.py
@@ -1,5 +1,9 @@
+# Manager Role, to be able to create Active Process
+from zExceptions import Unauthorized
 import json
 
+if REQUEST is not None:
+  raise Unauthorized
 active_process = context.getPortalObject().portal_activities.newActiveProcess(
   reference=context.Base_getDocumentScannerDefaultReference())
 
