19e947d50bb2c8591588573acf5fbccbd596de50
Romain Courteaud <romain@nexedi.com>
2020-04-16 12:05:53 +0000

    erp5_configurator_officejs_development: setup bt5

--- bt5/erp5_configurator_officejs_development/SkinTemplateItem/portal_skins/erp5_configurator_officejs_development/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
+++ bt5/erp5_configurator_officejs_development/SkinTemplateItem/portal_skins/erp5_configurator_officejs_development/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
@@ -1,14 +1,10 @@
 """ Simple place for keep the list of business template to install on this project
 """
 
-keep_bt5_id_list = ['erp5_ui_test',
-                    'erp5_ui_test_core',
-                    'erp5_forge',
-                    'erp5_l10n_fa',
-                    'officejs_test']
+keep_bt5_id_list = []
 
 bt5_update_catalog_list = ('erp5_ingestion_mysql_innodb_catalog', 'erp5_full_text_mroonga_catalog')
 
-bt5_installation_list = bt5_update_catalog_list + ('officejs_appstore_configurator', 'officejs_meta')
+bt5_installation_list = bt5_update_catalog_list + ('officejs_development_configurator', 'erp5_admin', 'erp5_forge', 'erp5_officejs_ui_test', 'officejs_todomvc')
 
 return bt5_installation_list, bt5_update_catalog_list, keep_bt5_id_list
