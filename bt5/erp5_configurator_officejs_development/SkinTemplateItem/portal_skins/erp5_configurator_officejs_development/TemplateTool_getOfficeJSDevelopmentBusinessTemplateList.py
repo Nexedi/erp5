886358f857e1987a1b555cd31d9dbd55ed7ff869
Romain Courteaud <romain@nexedi.com>
2020-04-16 12:33:32 +0000

    erp5_configurator_officejs_development: more bt5

--- bt5/erp5_configurator_officejs_development/SkinTemplateItem/portal_skins/erp5_configurator_officejs_development/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
+++ bt5/erp5_configurator_officejs_development/SkinTemplateItem/portal_skins/erp5_configurator_officejs_development/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
@@ -5,6 +5,14 @@ keep_bt5_id_list = []
 
 bt5_update_catalog_list = ('erp5_ingestion_mysql_innodb_catalog', 'erp5_full_text_mroonga_catalog')
 
-bt5_installation_list = bt5_update_catalog_list + ('officejs_development_configurator', 'erp5_admin', 'erp5_forge', 'erp5_officejs_ui_test', 'officejs_todomvc')
+bt5_installation_list = bt5_update_catalog_list + (
+  'officejs_development_configurator',
+  'erp5_admin',
+  'erp5_forge',
+  'erp5_monaco_editor',
+  'erp5_code_mirror',
+  'erp5_officejs_ui_test',
+  'officejs_todomvc'
+)
 
 return bt5_installation_list, bt5_update_catalog_list, keep_bt5_id_list
