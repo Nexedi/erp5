f56e918a8da7c3bbee55483205a99f7e0a81fc80
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-04-20 11:59:03 +0000

    erp5_jexcel_editor: initial commit, bt created
    added jexcel source
    
    erp5_jexcel_editor: added jsuites source
    
    erp5_jexcel_editor: init jexcel.gadget.html and jexcel.gadget.js
    
    erp5_jexcel_editor: init jexcel.gadget.html and jexcel.gadget.js
    
    erp5_jexcel_editor: xml files generated by zope
    
    erp5_jexcel_editor: reimport jexcel sources after it disappeared
    
    erp5_jexcel_editor: display of a simple 10x10 spreadsheet
    
    erp5_jexcel_editor: css source for toolbar icons
    
    erp5_jexcel_editor: added simple toolbar
    
    erp5_jexcel_editor:
    init deferNotifySubmit deferNotifyChange getContent and render method
    render a 26x200 sheet
    added toolbar
    can turn on fullscreen mode
    
    erp5_jexcel_editor: can add new tabs but erase the others and the toolbar
    
    erp5_jexcel_editor: test button
    
    erp5_jexcel_editor: test button
    
    erp5_jexcel_editor: moved sheet init in onchangestate and removed ready method
    
    erp5_jexcel_editor: can open a sheet which config is in the options_dict of the render method
    
    erp5_jexcel_editor: can nom open a sheet which config is in parameter of render
    problem with the toolbar undo redo
    
    erp5_jexcel_editor: toolbar when a sheet is loaded should work

---
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -0,0 +1,202 @@
+/*jslint nomen: true, indent: 2 */
+/*global window, rJS, RSVP, jexcel*/
+(function (window, rJS, RSVP, jexcel) {
+  "use strict";
+
+  rJS(window)
+
+    .declareAcquiredMethod("notifySubmit", "notifySubmit")
+    .declareJob("deferNotifySubmit", function () {
+      // Ensure error will be correctly handled
+      return this.notifySubmit();
+    })
+
+    .declareAcquiredMethod("notifyChange", "notifyChange")
+    .declareJob("deferNotifyChange", function () {
+      // Ensure error will be correctly handled
+      return this.notifyChange();
+    })
+
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      var state_dict = {
+          key: options.key,
+          editable: options.editable === undefined ? true : options.editable,
+          value: options.value === undefined ? "" : options.value
+        };
+      return this.changeState(state_dict);
+    })
+
+    .declareMethod("updateValue", function () {
+      this.state.value = this.table.getConfig();
+    })
+
+    .onStateChange(function (modification_dict) {
+      var gadget = this;
+      var table;
+      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
+      if (modification_dict.hasOwnProperty('value') && modification_dict.value !== "") {
+        table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(this.state.value, {
+          editable: gadget.state.editable,
+          onchange: gadget.deferNotifyChangeBinded, 
+          onafterchanges: gadget.updateValue, 
+          toolbar: [
+            {
+              type: 'i',
+              content: 'undo',
+              id: "undo",
+              onclick: function () {
+                console.log("aaa");
+                table.undo();
+              }
+            },
+            {
+              type: 'i',
+              content: 'redo',
+              onclick: function () {
+                  table.redo();
+                }
+            },
+            {
+              type: 'select',
+              k: 'font-family',
+              v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
+            },
+            {
+              type: 'select',
+              k: 'font-size',
+              v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
+            },
+            {
+              type: 'i',
+              content: 'format_align_left',
+              k: 'text-align',
+              v: 'left'
+            },
+            {
+              type: 'i',
+              content: 'format_align_center',
+              k: 'text-align',
+              v: 'center'
+            },
+            {
+              type: 'i',
+              content: 'format_align_right', 
+              k: 'text-align',
+              v: 'right'
+            },
+            {
+              type: 'i',
+              content: 'format_bold',
+              k: 'font-weight',
+              v: 'bold'
+            },
+            {
+              type: 'color',
+              content: 'format_color_text',
+              k: 'color'
+            },
+            {
+              type: 'color',
+              content: 'format_color_fill',
+              k: 'background-color'
+            }
+          ]
+        }));
+        jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
+        this.table = table;
+      }
+      else {
+        table = jexcel(this.element.querySelector(".spreadsheet"), {
+          editable: this.state.editable,
+          minDimensions: [26, 200],
+          defaultColWidth: 100,
+          fullscreen: true,
+          allowComments: true,
+          search: true,
+          rowResize: true,
+          tableOverflow: true,
+          lazyLoading: true,
+          loadingSpin: true,
+          parseFormulas: false,
+          onchange: gadget.deferNotifyChangeBinded,
+          onafterchanges: gadget.updateValue,
+          toolbar: [
+            {
+              type: 'i',
+              content: 'undo',
+              id: "undo",
+              onclick: function () {
+                console.log("aaa");
+                table.undo();
+              }
+            },
+            {
+              type: 'i',
+              content: 'redo',
+              onclick: function () {
+                  table.redo();
+                }
+            },
+            {
+              type: 'select',
+              k: 'font-family',
+              v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
+            },
+            {
+              type: 'select',
+              k: 'font-size',
+              v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
+            },
+            {
+              type: 'i',
+              content: 'format_align_left',
+              k: 'text-align',
+              v: 'left'
+            },
+            {
+              type: 'i',
+              content: 'format_align_center',
+              k: 'text-align',
+              v: 'center'
+            },
+            {
+              type: 'i',
+              content: 'format_align_right', 
+              k: 'text-align',
+              v: 'right'
+            },
+            {
+              type: 'i',
+              content: 'format_bold',
+              k: 'font-weight',
+              v: 'bold'
+            },
+            {
+              type: 'color',
+              content: 'format_color_text',
+              k: 'color'
+            },
+            {
+              type: 'color',
+              content: 'format_color_fill',
+              k: 'background-color'
+            }
+          ]
+        });
+        this.table = table;
+      }
+    })
+
+    .declareMethod('getContent', function () {
+      var form_data = {};
+      if (this.state.editable) {
+        form_data[this.state.key] = this.table.getConfig();
+        this.state.value = form_data[this.state.key];
+      }
+      return form_data;
+    })
+  
+    ;
+
+}(window, rJS, RSVP, jexcel));
