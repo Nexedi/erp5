8a3b90bca0bdb33a9acbe47574923a341a33b830
Ivan Tyagov <ivan@nexedi.com>
2020-04-20 14:14:09 +0300

    Make it possible to configure if default (open) security model is to be installed for Wendelin's Data Lake. Default is not install.

--- bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_configureWendelinSetupDataNotebook.py
+++ bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_configureWendelinSetupDataNotebook.py
@@ -25,13 +25,15 @@ if kw.get('setup_data_lake'):
     update_catalog=False,
     install_dependency=True,
   )
-  configuration_save.addConfigurationItem(
-    "Standard BT5 Configurator Item",
-    title='erp5_wendelin_data_lake_ingestion_default_security_model',
-    bt5_id='erp5_wendelin_data_lake_ingestion_default_security_model',
-    update_catalog=False,
-    install_dependency=True,
-  )
+  # add default security model - "open" one
+  if kw.get('setup_data_lake_default_security_model'):
+    configuration_save.addConfigurationItem(
+      "Standard BT5 Configurator Item",
+      title='erp5_wendelin_data_lake_ingestion_default_security_model',
+      bt5_id='erp5_wendelin_data_lake_ingestion_default_security_model',
+      update_catalog=False,
+      install_dependency=True,
+    )
 
 # setup 'tutorial'
 if kw.get('setup_wendelin_tutorial'):
