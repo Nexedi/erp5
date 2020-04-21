39103910da9b4d6b87800cbe5352e4492cd74a38
Romain Courteaud <romain@nexedi.com>
2020-04-21 16:31:26 +0000

    erp5_configurator_standard: depedency

--- bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_officejs_sdk/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
+++ bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_officejs_sdk/TemplateTool_getOfficeJSDevelopmentBusinessTemplateList.py
@@ -6,8 +6,8 @@ keep_bt5_id_list = []
 bt5_update_catalog_list = ('erp5_ingestion_mysql_innodb_catalog', 'erp5_full_text_mroonga_catalog')
 
 bt5_installation_list = bt5_update_catalog_list + (
-  'erp5_configurator_officejs_development',
-  'erp5_upgrader_officejs_development',
+  'erp5_configurator_standard',
+  'erp5_upgrader_officejs_sdk',
   'erp5_administration',
   'erp5_forge',
   'erp5_monaco_editor',
