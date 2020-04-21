eb038a82c631da74ec6d8c214be3a121ec4df33d
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-04-21 12:09:57 +0000

    erp5_jexcel_editor: added simple toolbar

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -17,10 +17,70 @@
     .ready(function () {
       var context = this;
       context.deferNotifyChangeBinded = context.deferNotifyChange.bind(context);
-      var editor = jexcel(this.element.querySelector(".spreadsheet"), {
-          minDimensions: [10, 10],
-          columns: {width: 200}
+      var table = jexcel(this.element.querySelector(".spreadsheet"), {
+          minDimensions: [20, 20],
+          toolbar: [
+            {
+              type: 'i',
+              content: 'undo',
+              onclick: function () {
+                table.undo();
+              }
+            },
+            {
+              type: 'i',
+              content: 'redo',
+              onclick: function () {
+                table.redo();
+              }
+            },
+            {
+              type: 'select',
+              k: 'font-family',
+              v: ['Arial', 'Verdana']
+            },
+            {
+              type: 'select',
+              k: 'font-size',
+              v: ['9px', '10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px']
+            },
+            {
+              type: 'i',
+              content: 'format_align_left',
+              k: 'text-align',
+              v: 'left'
+            },
+            {
+              type: 'i',
+              content: 'format_align_center',
+              k: 'text-align',
+              v: 'center'
+            },
+            {
+              type: 'i',
+              content: 'format_align_right', 
+              k: 'text-align',
+              v: 'right'
+            },
+            {
+              type: 'i',
+              content: 'format_bold',
+              k: 'font-weight',
+              v: 'bold'
+            },
+            {
+              type: 'color',
+              content: 'format_color_text',
+              k: 'color'
+            },
+            {
+              type: 'color',
+              content: 'format_color_fill',
+              k: 'background-color'
+            }
+          ]
         });
+      this.table = table;
     })
     .declareMethod("render", function (option_dict) {
       var gadget = this;
