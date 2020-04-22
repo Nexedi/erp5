bb9b3383e74f01dc7afd4881cf9f3ccb4b3419a0
Romain Courteaud <romain@nexedi.com>
2020-04-22 10:05:56 +0000

    erp5_configurator_standard: coding style

--- bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_setupEmployeesNumber.py
+++ bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_setupEmployeesNumber.py
@@ -1,4 +1,3 @@
-configuration_save_url = kw.get('configuration_save_url', None)
 company_employees_number = kw.get('company_employees_number', 1)
 next_transition = context.getNextTransition().getRelativeUrl()
 
@@ -6,9 +5,9 @@ if company_employees_number>1:
   # mark next transition  as multiple
   context.setMultiEntryTransition(next_transition, company_employees_number)
 else:
-  # explicitly reset next transition as not multiple because 
+  # explicitly reset next transition as not multiple because
   # we may have already set it as multiple
   context.setMultiEntryTransition(next_transition, 0)
 
-# store globally 
+# store globally
 context.setGlobalConfigurationAttr(company_employees_number=company_employees_number)
