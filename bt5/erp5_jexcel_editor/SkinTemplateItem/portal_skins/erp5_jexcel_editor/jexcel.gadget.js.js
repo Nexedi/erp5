0b724cbdc39cc6f58881bd40583dbf7389c3238c
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-04-22 15:21:55 +0000

    erp5_jexcel_editor: can nom open a sheet which config is in parameter of render problem with the toolbar undo redo

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -2,22 +2,74 @@
 /*global window, rJS, RSVP, jexcel*/
 (function (window, rJS, RSVP, jexcel) {
   "use strict";
-  
-  var buildToolbar = function (table) {
-    var toolbar = [
+
+  rJS(window)
+
+    .declareAcquiredMethod("notifySubmit", "notifySubmit")
+    .declareJob("deferNotifySubmit", function () {
+      // Ensure error will be correctly handled
+      return this.notifySubmit();
+    })
+
+    .declareAcquiredMethod("notifyChange", "notifyChange")
+    .declareJob("deferNotifyChange", function () {
+      // Ensure error will be correctly handled
+      return this.notifyChange();
+    })
+
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      var state_dict = {
+          key: options.key,
+          editable: options.editable === undefined ? true : options.editable,
+          value: options.value === undefined ? "" : options.value
+        };
+      return this.changeState(state_dict);
+    })
+
+    .declareMethod("updateValue", function () {
+      this.state.value = this.table.getConfig();
+    })
+
+    .onStateChange(function (modification_dict) {
+      var gadget = this;
+      var table;
+      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
+      if (modification_dict.hasOwnProperty('value') && modification_dict.value !== "") {
+        modification_dict.value.editable = this.state.editable;
+        this.state.value.onchange = gadget.deferNotifyChangeBinded;
+        this.state.value.onafterchanges = gadget.updateValue;
+        this.state.value.toolbar = null;
+        table = jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
+        jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
+        this.table = table;
+      }
+      else {
+        table = jexcel(this.element.querySelector(".spreadsheet"), {
+          editable: this.state.editable,
+          minDimensions: [26, 200],
+          defaultColWidth: 100,
+          fullscreen: true,
+          allowComments: true,
+          search: true,
+          rowResize: true,
+          tableOverflow: true,
+          lazyLoading: true,
+          loadingSpin: true,
+          parseFormulas: false,
+          onchange: gadget.deferNotifyChangeBinded,
+          onafterchanges: gadget.updateValue,
+          toolbar: [
             {
               type: 'i',
               content: 'undo',
-              onclick: function () {
-                table.undo();
-              }
+              id: "undo"
+              //onclick: table.undo
             },
             {
               type: 'i',
               content: 'redo',
-              onclick: function () {
-                table.redo();
-              }
+              //onclick: table.redo
             },
             {
               type: 'select',
@@ -64,63 +116,19 @@
               k: 'background-color'
             }
           ]
-    return toolbar;
-  };
-
-  rJS(window)
-
-    .declareAcquiredMethod("notifySubmit", "notifySubmit")
-    .declareJob("deferNotifySubmit", function () {
-      // Ensure error will be correctly handled
-      return this.notifySubmit();
-    })
-
-    .declareAcquiredMethod("notifyChange", "notifyChange")
-    .declareJob("deferNotifyChange", function () {
-      // Ensure error will be correctly handled
-      console.log(this.table);
-      return this.notifyChange();
-    })
-
-    .declareMethod("render", function (options) {
-      var gadget = this;
-      var state_dict = {
-          key: options.key,
-          editable: options.editable === undefined ? true : options.editable,
-          value: options.value === undefined ? "" : options.value
-        };
-      return this.changeState(state_dict);
-    })
-  
-    .onStateChange(function (modification_dict) {
-      var gadget = this;
-      var table;
-      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
-      if (modification_dict.hasOwnProperty('value') && modification_dict.value !== "") {
-        modification_dict.value.editable = this.state.editable;
-        this.state.value.onchange = gadget.deferNotifyChangeBinded;
-        this.state.value.toolbar = buildToolbar(table);
-        table = jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
-        jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
-        this.table = table;
-      }
-      else {
-        table = jexcel(this.element.querySelector(".spreadsheet"), {
-          editable: this.state.editable,
-          minDimensions: [26, 200],
-          defaultColWidth: 100,
-          fullscreen: true,
-          allowComments: true,
-          search: true,
-          rowResize: true,
-          tableOverflow: true,
-          lazyLoading: true,
-          loadingSpin: true,
-          parseFormulas: false,
-          onchange: gadget.deferNotifyChangeBinded,
-          toolbar: buildToolbar(table)
         });
         this.table = table;
+      }
+    })
+  
+    .onEvent("click", function (event) {
+      var gadget = this;
+      var element = event.target;
+      if (element == gadget.element.querySelector(".jexcel_toolbar").childNodes[0]) {
+        this.table.undo();
+      }
+      else if (element == gadget.element.querySelector(".jexcel_toolbar").childNodes[1]) {
+        this.table.redo();
       }
     })
 
