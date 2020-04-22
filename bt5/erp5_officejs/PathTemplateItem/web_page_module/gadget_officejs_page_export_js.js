0ab4663671fec40e2383030001481ab827128dc9
Roque Porchetto <roque.porchetto@nexedi.com>
2020-04-22 10:21:03 +0000

    erp5_officejs: update exporter app to use new precache storage to generate the app zip files

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_export_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_page_export_js.js
@@ -6,10 +6,13 @@
 
   var origin_url = (window.location.origin + window.location.pathname)
     .replace("officejs_export/", ""),
+    precache_manifest = "WebSection_getPrecacheManifest",
+    //TODO get rid of this old HARDCODED app dict and get everything from config
     application_dict = {
       "Text Editor": {
         "url": "officejs_text_editor/",
-        "cache": "gadget_officejs_text_editor.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Smart Assistant": {
         "url": "officejs_smart_assistant/",
@@ -25,11 +28,13 @@
       },
       "Illustration Editor": {
         "url": "officejs_svg_editor/",
-        "cache": "gadget_officejs_illustration.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "PDF Viewer": {
         "url": "officejs_pdf_viewer/",
-        "cache": "gadget_officejs_pdf_viewer.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Cribjs": {
         "url": "officejs_cribjs/",
@@ -41,27 +46,33 @@
       },
       "Onlyoffice Text": {
         "url": "ooffice_text/",
-        "cache": "gadget_ooffice_text.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Onlyoffice Spreadsheet": {
         "url": "ooffice_spreadsheet/",
-        "cache": "gadget_ooffice_spreadsheet.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Onlyoffice Presentation": {
         "url": "ooffice_presentation/",
-        "cache": "gadget_ooffice_presentation.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Web Table Editor": {
         "url": "officejs_web_table_editor/",
-        "cache": "gadget_officejs_web_table.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Image Editor": {
         "url": "officejs_image_editor/",
-        "cache": "gadget_officejs_image_editor.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       },
       "Awesome Free Software Publisher List": {
         "url": "afs/",
-        "cache": "gadget_erp5_afs.appcache",
+        "storage_type": "precache",
+        "cache": precache_manifest,
         "no_installer": true
       },
       "Jabber Client": {
@@ -108,7 +119,8 @@
       },
       "Javascript editor (CodeMirror)": {
         "url": "officejs_codemirror/",
-        "cache": "gadget_officejs_codemirror.appcache"
+        "storage_type": "precache",
+        "cache": precache_manifest
       }
     };
 
@@ -125,7 +137,8 @@
     app = application_dict[form_result.web_site];
     zip_name = form_result.filename;
 
-    return gadget.fillZip(app.cache, origin_url + app.url, app.no_installer)
+    return gadget.fillZip(app.storage_type, app.cache, origin_url + app.url,
+                          app.no_installer)
       .push(function (zip_file) {
         var element = gadget.element,
           a = document.createElement("a"),
@@ -146,7 +159,8 @@
     .ready(function (g) {
       g.props = {};
     })
-    .declareMethod("fillZip", function (cache_file, site_url, no_installer) {
+    .declareMethod("fillZip", function (storage_type, cache_file, site_url,
+                                        no_installer) {
       var file_storage = jIO.createJIO({
           type: "replicate",
           conflict_handling: 2,
@@ -160,7 +174,8 @@
             type: "filesystem",
             document: site_url,
             sub_storage: {
-              type: "appcache",
+              //keep appcache as default for backward compatibility
+              type: storage_type || "appcache",
               take_installer: no_installer === undefined,
               manifest: cache_file,
               origin_url: site_url,
