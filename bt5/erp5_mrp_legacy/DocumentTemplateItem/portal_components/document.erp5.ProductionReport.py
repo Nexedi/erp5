1a126ca820b987725d4ddd9430a8cfb190a94a7e
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-04-23 13:24:00 +0900

    WIP: ZODB Components: Migrate Delivery Document to erp5_core (not erp5_trade as it is used everywhere) (MR !1107).
    * Project Report: Removed (deprecated 14 years ago over Task Report).
      TODO: ExpenseModule_viewExpenseList/listbox.xml?

--- bt5/erp5_mrp_legacy/DocumentTemplateItem/portal_components/document.erp5.ProductionReport.py
+++ bt5/erp5_mrp_legacy/DocumentTemplateItem/portal_components/document.erp5.ProductionReport.py
@@ -29,7 +29,7 @@
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type import Permissions, PropertySheet
 
-from Products.ERP5.Document.Delivery import Delivery
+from erp5.component.document.Delivery import Delivery
 
 class ProductionReport(Delivery):
     """
