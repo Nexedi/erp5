f37b0acf231cbf18f2e0768e4a669707eca62c68
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-04-23 13:24:00 +0900

    ZODB Components: Migrate Delivery Document to erp5_core (not erp5_trade as it is used everywhere) (MR !1107).
    * Project Report: Removed (deprecated 14 years ago over Task Report).
      + Remove Expense{Module,Line}_view* as these use Project Report and Project Report Line.

--- bt5/erp5_accounting/DocumentTemplateItem/portal_components/document.erp5.Invoice.py
+++ bt5/erp5_accounting/DocumentTemplateItem/portal_components/document.erp5.Invoice.py
@@ -29,7 +29,7 @@
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type import Permissions, PropertySheet
 from erp5.component.document.AccountingTransaction import AccountingTransaction
-from Products.ERP5.Document.Delivery import Delivery
+from erp5.component.document.Delivery import Delivery
 
 class Invoice(AccountingTransaction):
   # CMF Type Definition
