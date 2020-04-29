11872b9e397c6e621bf0e735b6a145cfd5c55c3a
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-04-29 09:42:04 +0000

    erp5_jexcel_editor: can now store the entire sheet config in indexeddb and load one if existing (code not clean)

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/gadget_model.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/gadget_model.js.js
@@ -8,17 +8,50 @@
     .ready(function () {
         var gadget = this;
         return this.changeState({storage: jIO.createJIO({
-          type: "indexeddb",
-          database: "sheets"
+              type: "uuid",
+              sub_storage: {
+                  type: "indexeddb",
+                  database: "sheet"
+              }
         })
        });
-      })
-  
-    .declareMethod("getSheet", function () {
-      return this.storage.alldocs()
-      .push(function (result) {
-        console.log(result);
+    })
+
+    .declareMethod("getSheets", function () {
+        var gadget = this;
+        var sheet = {};
+        var id_save;
+        return gadget.state.storage.allDocs({include_docs: true})
+        .push(function (result) {
+            if(result.data.total_rows == 1){
+                sheet.id = result.data.rows[0].id;
+                sheet.config = result.data.rows[0].doc.config;
+                return sheet;
+            }
+            else {
+              return gadget.state.storage.post({config: ""})
+              .push(function (id) {
+                id_save = id;
+                return gadget.state.storage.get(id);
+              })
+              .push(function (result) {
+                sheet.id = id_save;
+                sheet.config = result.config;
+                return sheet;
+              })
+            };
       });
+    })
+  
+    .declareMethod("putSheet", function (id, sheet) {
+      return this.state.storage.put(id, sheet);
+    })
+
+    .declareMethod("postSheet", function (config) {
+        var gadget = this;
+        return gadget.state.storage.post({
+            config: config
+        })
     });
 
   }(window, RSVP, rJS, jIO));
--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -1,206 +1,287 @@
 /*jslint nomen: true, indent: 2 */
 /*global window, rJS, RSVP, jexcel*/
 (function (window, rJS, RSVP, jexcel) {
-  "use strict";
-
-  rJS(window)
+    "use strict";
   
-    //////////// WIP
-    .declareService(function () {
-      var gadget = this;
-      console.log("aaa");
-      return gadget.getDeclaredGadget("model")
-      .push(function (model) {
-        model.getSheet();
-      });
-    })
-    /////////////////
-
-    .declareAcquiredMethod("notifySubmit", "notifySubmit")
-    .declareJob("deferNotifySubmit", function () {
-      // Ensure error will be correctly handled
-      return this.notifySubmit();
-    })
-
-    .declareAcquiredMethod("notifyChange", "notifyChange")
-    .declareJob("deferNotifyChange", function () {
-      // Ensure error will be correctly handled
-      return this.notifyChange();
-    })
-
-    .declareMethod("render", function (options) {
-      var gadget = this;
-      return this.changeState(options);
-    })
-
-    .declareMethod("updateValue", function () {
-      this.state.value = this.table.getConfig();
-    })
-
-    .onStateChange(function (modification_dict) {
-      var gadget = this;
-      var table;
-      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
-      if (modification_dict.hasOwnProperty('value') && modification_dict.value !== "") {
-        table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(this.state.value, {
-          editable: gadget.state.editable,
-          onchange: gadget.deferNotifyChangeBinded, 
-          onafterchanges: gadget.updateValue, 
-          toolbar: [
-            {
-              type: 'i',
-              content: 'undo',
-              id: "undo",
-              onclick: function () {
-                table.undo();
-              }
-            },
-            {
-              type: 'i',
-              content: 'redo',
-              onclick: function () {
-                  table.redo();
-                }
-            },
-            {
-              type: 'select',
-              k: 'font-family',
-              v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
-            },
-            {
-              type: 'select',
-              k: 'font-size',
-              v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
-            },
-            {
-              type: 'i',
-              content: 'format_align_left',
-              k: 'text-align',
-              v: 'left'
-            },
-            {
-              type: 'i',
-              content: 'format_align_center',
-              k: 'text-align',
-              v: 'center'
-            },
-            {
-              type: 'i',
-              content: 'format_align_right', 
-              k: 'text-align',
-              v: 'right'
-            },
-            {
-              type: 'i',
-              content: 'format_bold',
-              k: 'font-weight',
-              v: 'bold'
-            },
-            {
-              type: 'color',
-              content: 'format_color_text',
-              k: 'color'
-            },
-            {
-              type: 'color',
-              content: 'format_color_fill',
-              k: 'background-color'
-            }
-          ]
-        }));
-        jexcel(gadget.element.querySelector(".spreadsheet"), this.state.value);
-        this.table = table;
-      }
-      else {
-        table = jexcel(this.element.querySelector(".spreadsheet"), {
-          editable: this.state.editable,
-          minDimensions: [26, 200],
-          defaultColWidth: 100,
-          fullscreen: true,
-          allowComments: true,
-          search: true,
-          rowResize: true,
-          tableOverflow: true,
-          lazyLoading: true,
-          loadingSpin: true,
-          parseFormulas: false,
-          onchange: gadget.deferNotifyChangeBinded,
-          onafterchanges: gadget.updateValue,
-          toolbar: [
-            {
-              type: 'i',
-              content: 'undo',
-              id: "undo",
-              onclick: function () {
-                table.undo();
-              }
-            },
-            {
-              type: 'i',
-              content: 'redo',
-              onclick: function () {
-                  table.redo();
-                }
-            },
-            {
-              type: 'select',
-              k: 'font-family',
-              v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
-            },
-            {
-              type: 'select',
-              k: 'font-size',
-              v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
-            },
-            {
-              type: 'i',
-              content: 'format_align_left',
-              k: 'text-align',
-              v: 'left'
-            },
-            {
-              type: 'i',
-              content: 'format_align_center',
-              k: 'text-align',
-              v: 'center'
-            },
-            {
-              type: 'i',
-              content: 'format_align_right', 
-              k: 'text-align',
-              v: 'right'
-            },
-            {
-              type: 'i',
-              content: 'format_bold',
-              k: 'font-weight',
-              v: 'bold'
-            },
-            {
-              type: 'color',
-              content: 'format_color_text',
-              k: 'color'
-            },
-            {
-              type: 'color',
-              content: 'format_color_fill',
-              k: 'background-color'
-            }
-          ]
-        });
-        this.table = table;
-      }
-    })
-
-    .declareMethod('getContent', function () {
-      var form_data = {};
-      if (this.state.editable) {
-        form_data[this.state.key] = this.table.getConfig();
-        this.state.value = form_data[this.state.key];
-      }
-      return form_data;
-    })
+    rJS(window)
+    
+      //////////// WIP
+      .ready(function () {
+        var gadget = this;
+        console.log("ready12");
+        return gadget.getDeclaredGadget("model")
+        .push(function (model) {
+            gadget.storage = model;
+          });
+      })
+ 
+      /////////////////
   
-    ;
-
-}(window, rJS, RSVP, jexcel));
+      .declareAcquiredMethod("notifySubmit", "notifySubmit")
+      .declareJob("deferNotifySubmit", function () {
+        // Ensure error will be correctly handled
+        return this.notifySubmit();
+      })
+  
+      .declareAcquiredMethod("notifyChange", "notifyChange")
+      .declareJob("deferNotifyChange", function () {
+        // Ensure error will be correctly handled
+        var gadget = this;
+            gadget.state.sheet.config = JSON.parse(JSON.stringify(this.table.getConfig()));
+            return this.storage.putSheet(gadget.state.sheet.id, {config: gadget.state.sheet.config});
+        //return this.notifyChange();
+      })
+  
+      .declareMethod("render", function (options) {
+        var gadget = this;
+        return gadget.storage.getSheets()
+        .push(function (sheet) {
+            gadget.state.sheet = sheet;
+        })
+        .push(function () {
+            return gadget.changeState(options);
+        })
+      })
+  
+      .declareJob("updateSheet", function () {
+            var gadget = this;
+            gadget.state.sheet.config = this.table.getConfig();
+            console.log("update");
+            return this.storage.putSheet(gadget.state.sheet.id, {config: this.state.sheet.config});
+        })
+  
+      .onStateChange(function (modification_dict) {
+        var gadget = this;
+        var table;
+        gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
+        if (modification_dict.hasOwnProperty('value') && modification_dict.value !== "") {
+          table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(this.state.value, {
+            editable: gadget.state.editable,
+            onchange: gadget.deferNotifyChangeBinded, 
+            onafterchanges: gadget.updateValue, 
+            toolbar: [
+              {
+                type: 'i',
+                content: 'undo',
+                id: "undo",
+                onclick: function () {
+                  table.undo();
+                }
+              },
+              {
+                type: 'i',
+                content: 'redo',
+                onclick: function () {
+                    table.redo();
+                  }
+              },
+              {
+                type: 'select',
+                k: 'font-family',
+                v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
+              },
+              {
+                type: 'select',
+                k: 'font-size',
+                v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
+              },
+              {
+                type: 'i',
+                content: 'format_align_left',
+                k: 'text-align',
+                v: 'left'
+              },
+              {
+                type: 'i',
+                content: 'format_align_center',
+                k: 'text-align',
+                v: 'center'
+              },
+              {
+                type: 'i',
+                content: 'format_align_right', 
+                k: 'text-align',
+                v: 'right'
+              },
+              {
+                type: 'i',
+                content: 'format_bold',
+                k: 'font-weight',
+                v: 'bold'
+              },
+              {
+                type: 'color',
+                content: 'format_color_text',
+                k: 'color'
+              },
+              {
+                type: 'color',
+                content: 'format_color_fill',
+                k: 'background-color'
+              }
+            ]
+          }));
+          this.table = table;
+        }
+        else if (gadget.state.sheet.config !== "") {
+            table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(this.state.sheet.config, {
+                editable: gadget.state.editable,
+                onchange: gadget.deferNotifyChangeBinded, 
+                onafterchanges: gadget.updateValue, 
+                toolbar: [
+                  {
+                    type: 'i',
+                    content: 'undo',
+                    id: "undo",
+                    onclick: function () {
+                      table.undo();
+                    }
+                  },
+                  {
+                    type: 'i',
+                    content: 'redo',
+                    onclick: function () {
+                        table.redo();
+                      }
+                  },
+                  {
+                    type: 'select',
+                    k: 'font-family',
+                    v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
+                  },
+                  {
+                    type: 'select',
+                    k: 'font-size',
+                    v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
+                  },
+                  {
+                    type: 'i',
+                    content: 'format_align_left',
+                    k: 'text-align',
+                    v: 'left'
+                  },
+                  {
+                    type: 'i',
+                    content: 'format_align_center',
+                    k: 'text-align',
+                    v: 'center'
+                  },
+                  {
+                    type: 'i',
+                    content: 'format_align_right', 
+                    k: 'text-align',
+                    v: 'right'
+                  },
+                  {
+                    type: 'i',
+                    content: 'format_bold',
+                    k: 'font-weight',
+                    v: 'bold'
+                  },
+                  {
+                    type: 'color',
+                    content: 'format_color_text',
+                    k: 'color'
+                  },
+                  {
+                    type: 'color',
+                    content: 'format_color_fill',
+                    k: 'background-color'
+                  }
+                ]
+              }));
+              this.table = table;
+        }
+        else {
+          table = jexcel(this.element.querySelector(".spreadsheet"), {
+            editable: this.state.editable,
+            minDimensions: [26, 200],
+            defaultColWidth: 100,
+            fullscreen: true,
+            allowComments: true,
+            search: true,
+            rowResize: true,
+            tableOverflow: true,
+            lazyLoading: true,
+            loadingSpin: true,
+            parseFormulas: false,
+            onchange: gadget.deferNotifyChangeBinded,
+            //onchange: gadget.updateSheet,
+            toolbar: [
+              {
+                type: 'i',
+                content: 'undo',
+                id: "undo",
+                onclick: function () {
+                  table.undo();
+                }
+              },
+              {
+                type: 'i',
+                content: 'redo',
+                onclick: function () {
+                    table.redo();
+                  }
+              },
+              {
+                type: 'select',
+                k: 'font-family',
+                v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
+              },
+              {
+                type: 'select',
+                k: 'font-size',
+                v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
+              },
+              {
+                type: 'i',
+                content: 'format_align_left',
+                k: 'text-align',
+                v: 'left'
+              },
+              {
+                type: 'i',
+                content: 'format_align_center',
+                k: 'text-align',
+                v: 'center'
+              },
+              {
+                type: 'i',
+                content: 'format_align_right', 
+                k: 'text-align',
+                v: 'right'
+              },
+              {
+                type: 'i',
+                content: 'format_bold',
+                k: 'font-weight',
+                v: 'bold'
+              },
+              {
+                type: 'color',
+                content: 'format_color_text',
+                k: 'color'
+              },
+              {
+                type: 'color',
+                content: 'format_color_fill',
+                k: 'background-color'
+              }
+            ]
+          });
+          this.table = table;
+        }
+      })
+  
+      .declareMethod('getContent', function () {
+        var form_data = {};
+        if (this.state.editable) {
+          form_data[this.state.key] = this.table.getConfig();
+          this.state.value = form_data[this.state.key];
+        }
+        return form_data;
+      })
+    
+      ;
+  
+  }(window, rJS, RSVP, jexcel));
