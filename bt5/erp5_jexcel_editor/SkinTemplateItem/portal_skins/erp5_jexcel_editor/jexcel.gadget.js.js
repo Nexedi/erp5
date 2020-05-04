3046d1ac0e216c5d56eda6d96eb2c81d2b58250a
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-05-04 08:55:01 +0000

    erp5_jexcel_editor: new template

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -4,23 +4,11 @@
   "use strict";
 
   var template = {
-    minDimensions: [26, 200],
-    defaultColWidth: 100,
-    fullscreen: true,
-    allowComments: true,
-    //search: true,
-    loadindSPin: true,
-    rowResize: true,
-    columnDrag: true,
-    //tableOverflow: true,
-    //lazyLoading: true,
-    loadingSpin: true,
-    parseFormulas: false
+    minDimensions: [2, 2],
+    toolbar: true
   };
 
   rJS(window)
-
-    //// Acquired methods /////////////////////////////////////
 
     .declareAcquiredMethod("notifySubmit", "notifySubmit")
     .declareJob("deferNotifySubmit", function () {
@@ -34,85 +22,22 @@
       return this.notifyChange();
     })
 
-    // call changeState to render the gadget
     .declareMethod("render", function (options) {
       var gadget = this;
-      return gadget.changeState(options);
+      console.log("ready111");
+      //return gadget.changeState(options);
     })
 
-    // Initialize the jexcel sheet
     .onStateChange(function (modification_dict) {
       var gadget = this;
       var table;
       gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
       if (modification_dict.hasOwnProperty('value')) {
         var tmp = Object.assign({}, template);
+        Object.assign(tmp, template);
         Object.assign(tmp, gadget.state.value);
-        Object.assign(template, this.state.value);
         table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(tmp, {
-          editable: gadget.state.editable,
-          onchange: gadget.deferNotifyChangeBinded,
-          toolbar: [
-            {
-              type: 'i',
-              content: 'undo',
-              id: "undo",
-              onclick: function () {
-                table.undo();
-              }
-            },
-            {
-              type: 'i',
-              content: 'redo',
-              onclick: function () {
-                  table.redo();
-                }
-            },
-            {
-              type: 'select',
-              k: 'font-family',
-              v: ['Arial', 'Arial Black', 'Verdana', 'Impact', 'Comic Sans MS', 'Tahoma', 'Trebuchet MS']
-            },
-            {
-              type: 'select',
-              k: 'font-size',
-              v: ['20px', '21px', '22px', '23px', '24px', '25px', '26px', '27px', '28px', '29px', '30px', '32px', '34px', '36px', '38px', '40px']
-            },
-            {
-              type: 'i',
-              content: 'format_align_left',
-              k: 'text-align',
-              v: 'left'
-            },
-            {
-              type: 'i',
-              content: 'format_align_center',
-              k: 'text-align',
-              v: 'center'
-            },
-            {
-              type: 'i',
-              content: 'format_align_right', 
-              k: 'text-align',
-              v: 'right'
-            },
-            {
-              type: 'i',
-              content: 'format_bold',
-              k: 'font-weight',
-              v: 'bold'
-            },
-            {
-              type: 'color',
-              content: 'format_color_text',
-              k: 'color'
-            },
-            {
-              type: 'color',
-              content: 'format_color_fill',
-              k: 'background-color'
-            }
-          ]
+          onchange: gadget.deferNotifyChangeBinded
         }));
         this.table = table;
       }
