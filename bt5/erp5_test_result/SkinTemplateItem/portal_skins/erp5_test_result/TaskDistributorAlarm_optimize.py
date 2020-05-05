25a70167adb433386fa04c89669c40cafc8b21fc
Sebastien Robin <seb@nexedi.com>
2020-05-05 16:19:51 +0200

    erp5_test_result: take care of timeouts when we invalidate test nodes

--- bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TaskDistributorAlarm_optimize.py
+++ bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TaskDistributorAlarm_optimize.py
@@ -6,16 +6,25 @@ now = DateTime()
 list_node = context.portal_catalog(
        portal_type="Test Node",
        )
-old_date = now-1.0/24*11
+default_old_date = now-1.0/24*11
 for test_node in list_node:
+  old_date = default_old_date
   test_node = test_node.getObject()
   ping_date = test_node.getPingDate()
   validation_state = test_node.getValidationState()
+  distributor = test_node.getSpecialiseValue()
+  if distributor is not None:
+    timeout_method = getattr(distributor, 'getProcessTimeout', None)
+    if timeout_method is not None:
+      timeout = timeout_method()
+      if timeout:
+        old_date = now - float(timeout)/(24*3600)
   if validation_state == 'validated':
     if ping_date is not None:
       if ping_date <= old_date:
         test_node.invalidate()
   elif validation_state == 'invalidated':
+    __traceback_info__ = test_node
     if test_node.getSpecialise():
       test_node.getSpecialiseValue().cleanupInvalidatedTestNode(test_node)
 
