5b41ead2edc3cd7742ffcdc0f62d66b5fa3e9a09
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-05-05 13:37:42 +0000

    erp5_jexcel_editor: fixed all bugs

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -4,8 +4,29 @@
   "use strict";
 
   var template = {
-    minDimensions: [2, 2],
-    toolbar: true
+    minDimensions: [26, 200],
+    defaultColWidth: 100,
+    allowExport: true,
+    columnSorting: true,
+    columnDrag: true,
+    columnResize: true,
+    rowResize: true,
+    rowDrag: true,
+    editable: true,
+    allowInsertRow: true,
+    allowManualInsertRow: true,
+    allowInsertColumn: true,
+    allowManualInsertColumn: true,
+    allowDeleteRow: true,
+    allowRenameColumn: true,
+    allowComments: true,
+    selectionCopy: true,
+    search: true,
+    fullscreen: true,
+    lazyLoading: true,
+    loadingSpin: true,
+    tableOverflow: true,
+    autoIncrement: true
   };
 
   rJS(window)
@@ -24,8 +45,7 @@
 
     .declareMethod("render", function (options) {
       var gadget = this;
-      console.log("ready111");
-      //return gadget.changeState(options);
+      return gadget.changeState(options);
     })
 
     .onStateChange(function (modification_dict) {
@@ -37,16 +57,77 @@
         Object.assign(tmp, template);
         Object.assign(tmp, gadget.state.value);
         table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(tmp, {
-          onchange: gadget.deferNotifyChangeBinded
+          onchange: gadget.deferNotifyChangeBinded,
+          toolbar: [
+            {
+              type: 'i',
+              content: 'undo',
+              onclick: function () {
+                  table.undo();
+                }
+            },
+            {
+              type: 'i',
+              content: 'redo',
+              onclick: function () {
+                  table.redo();
+                }
+            },
+            {
+              type: 'select',
+              k: 'font-family',
+              v: ['Arial', 'Verdana']
+            },
+            {
+              type: 'select',
+              k: 'font-size',
+              v: ['9px', '10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px']
+            },
+            {
+              type: 'i',
+              content: 'format_align_left',
+              k: 'text-align',
+              v: 'left'
+            },
+            {
+              type: 'i',
+              content: 'format_align_center',
+              k: 'text-align',
+              v: 'center'
+            },
+            {
+              type: 'i',
+              content: 'format_align_right', 
+              k: 'text-align',
+              v: 'right'
+            },
+            {
+              type: 'i',
+              content: 'format_bold',
+              k: 'font-weight',
+              v: 'bold'
+            },
+            {
+              type: 'color',
+              content: 'format_color_text',
+              k: 'color'
+            },
+            {
+              type: 'color',
+              content: 'format_color_fill',
+              k: 'background-color'
+            }
+          ]
         }));
         this.table = table;
       }
     })
 
     .declareMethod('getContent', function () {
+      var gadget = this;
       var form_data = {};
       if (this.state.editable) {
-        form_data[this.state.key] = JSON.stringify(this.table.getConfig());
+        form_data[this.state.key] = JSON.stringify(gadget.table.getConfig());
         this.state.value = form_data[this.state.key];
       }
       return form_data;
