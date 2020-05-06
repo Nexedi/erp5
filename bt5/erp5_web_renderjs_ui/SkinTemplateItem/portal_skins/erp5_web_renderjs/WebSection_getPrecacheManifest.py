2234b3379394448ccbea13cd4bc10e9443f5d858
Roque Porchetto <roque.porchetto@nexedi.com>
2020-05-06 11:50:39 +0000

    erp5_web_renderjs_ui: use weak etag header on precache manifest script response - web site modification date is used as etag value for caching purpouses

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
@@ -8,7 +8,7 @@ url_list = [
   'font-awesome/font-awesome-webfont.woff2',
   'font-awesome/font-awesome-webfont.ttf',
   'font-awesome/font-awesome-webfont.svg',
-  'gadget_erp5_worklist_empty.svg?format=svg',
+  'gadget_erp5_worklist_empty.svg',
   'erp5_launcher_nojqm.js',
   'gadget_erp5_nojqm.css',
   'gadget_erp5_configure_editor.html',
@@ -99,7 +99,7 @@ url_list = [
   'gadget_erp5_page_worklist.js',
   'gadget_erp5_panel.html',
   'gadget_erp5_panel.js',
-  'gadget_erp5_panel.png?format=png',
+  'gadget_erp5_panel.png',
   'gadget_erp5_pt_embedded_form_render.html',
   'gadget_erp5_pt_embedded_form_render.js',
   'gadget_erp5_pt_form_dialog.html',
@@ -180,6 +180,8 @@ for precache_manifest_script_id in precache_manifest_url_list:
 if REQUEST is not None:
   import json
   REQUEST.RESPONSE.setHeader('Content-Type', 'application/json')
+  modification_date = web_section.getModificationDate().strftime("%m/%d/%Y-%H:%M:%S.%f")
+  REQUEST.RESPONSE.setHeader('ETag', 'W/"%s"' % modification_date)
   return json.dumps(dict.fromkeys(url_list), indent=2)
 
 return list(set(url_list))
