551f012747c38d17b0fb91d150521f2566eaae54
Ivan Tyagov <ivan@nexedi.com>
2020-05-06 11:25:31 +0300

    Use proper script to get all Data Streams for a Data Set rather than rely on reference. Test it.

--- wendelin/bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/DataSet_getDataStreamList.py
+++ wendelin/bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/DataSet_getDataStreamList.py
@@ -4,7 +4,7 @@
 
   Note: This code is quite computationally costly (for Data Streams having thousands of iles) as it needs to:
   1. Query MariaDB to find ingestion lines
-  2. Read from ZODB both Data Ingestion Lines and Data Streams (whoch itself can be big too)
+  2. Read from ZODB both Data Ingestion Lines and Data Streams (which itself can be big too)
 """
 data_ingestion_line_list = context.portal_catalog(
                              portal_type = "Data Ingestion Line",
