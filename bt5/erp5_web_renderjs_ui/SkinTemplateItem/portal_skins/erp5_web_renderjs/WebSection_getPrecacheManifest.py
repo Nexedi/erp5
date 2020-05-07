4cf6848c9a852493855a25bb102adaf2e015aedc
Roque Porchetto <roque.porchetto@nexedi.com>
2020-05-07 12:10:30 +0000

    erp5_web_renderjs_ui: optimize 304 response on precache script

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
@@ -1,5 +1,14 @@
 web_section = context
 
+if REQUEST is not None:
+  modification_date_string = web_section.getModificationDate().rfc822()
+  weak_etag_header = 'W/"%s"' % modification_date_string
+  REQUEST.RESPONSE.setHeader('ETag', weak_etag_header)
+  if_none_match = REQUEST.getHeader('If-None-Match', '').replace("-gzip", "")
+  if if_none_match == weak_etag_header:
+    REQUEST.RESPONSE.setStatus(304)
+    return ""
+
 # Add all ERP5JS gadget
 url_list = [
   'favicon.ico',
@@ -180,12 +189,6 @@ for precache_manifest_script_id in precache_manifest_url_list:
 if REQUEST is not None:
   import json
   REQUEST.RESPONSE.setHeader('Content-Type', 'application/json')
-  modification_date_string = web_section.getModificationDate().rfc822()
-  weak_etag_header = 'W/"%s"' % modification_date_string
-  REQUEST.RESPONSE.setHeader('ETag', weak_etag_header)
-  if_none_match = REQUEST.getHeader('If-None-Match', '').replace("-gzip", "")
-  if if_none_match == weak_etag_header:
-    REQUEST.RESPONSE.setStatus(304)
   return json.dumps(dict.fromkeys(url_list), indent=2)
 
 return list(set(url_list))
