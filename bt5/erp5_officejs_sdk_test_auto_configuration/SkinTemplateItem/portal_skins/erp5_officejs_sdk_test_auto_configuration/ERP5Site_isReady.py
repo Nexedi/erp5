665aed84092f16625b96487d586fd3799176e43d
Romain Courteaud <romain@nexedi.com>
2020-05-11 11:16:57 +0200

    Drop erp5_officejs_sdk_test_auto_configuration

--- bt5/erp5_officejs_sdk_test_auto_configuration/SkinTemplateItem/portal_skins/erp5_officejs_sdk_test_auto_configuration/ERP5Site_isReady.py
+++
@@ -1,5 +0,0 @@
-"""
-  The purpose of the script is to tell if configuration of a site was finished or not.
-  It is used from Deployment Tests.
-"""
-return context.portal_templates.getInstalledBusinessTemplate("erp5_officejs_ui_test", strict=True) is not None
