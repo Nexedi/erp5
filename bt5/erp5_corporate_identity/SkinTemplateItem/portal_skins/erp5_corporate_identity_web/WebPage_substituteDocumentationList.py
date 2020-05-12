32da4e13d629ccb5510de837392442a1d49b44ef
Romain Courteaud <romain@nexedi.com>
2020-05-12 13:18:07 +0000

    erp5_corporate_identity: stop hardcoding categories

--- bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity_web/WebPage_substituteDocumentationList.py
+++ bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity_web/WebPage_substituteDocumentationList.py
@@ -6,17 +6,12 @@ def escapeInnerHTML(string_to_escape):
 def escapeAttributeProperty(string_to_escape):
   return cgi.escape("%s" % string_to_escape, quote=True)
 
-# XXX HARDCODED
-category_relative_url_list = [
-  'group/template_test_group',
-  'function/company/executive'
-]
-
 web_site_value = context.getWebSiteValue()
 
 result = {}
 
 if (web_site_value is not None):
+  category_relative_url_list = web_site_value.getLayoutProperty('layout_substitution_category_document_list', default="").split('\n')
   for category_relative_url in category_relative_url_list:
     base_category, _ = category_relative_url.split('/', 1)
 
