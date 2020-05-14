d67f30f7ae5586366101fbf74eef022cbecb3027
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-05-14 13:43:15 +0000

    erp5_jexcel_editor: clean bt from old branch

---
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/WebSection_getJexcelAppPrecacheManifestList.py
@@ -0,0 +1,9 @@
+url_list = [
+  "gadget_officejs_jexcel_editor.configuration",
+  "gadget_officejs_jexcel_editor_router.html",
+  #app custom actions
+  "action_jexcel_editor_clone.html",
+  "action_jexcel_editor_clone.js"
+]
+
+return url_list
---
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/WebSection_getJexcelEditorPrecacheManifestList.py
@@ -0,0 +1,16 @@
+url_list = [
+  "renderjs.js",
+  "rsvp.js",
+  "jexcel.gadget.html",
+  "jexcel.gadget.js",
+  "fonts.ttf",
+  "icons.css",
+  "jexcel/jexcel.js",
+  "jexcel/jexcel.css",
+  "jexcel/jexcel.theme.css",
+  "jexcel/jexcel.webcomponent.js",
+  "jsuites/jsuites.js",
+  "jsuites/jsuites.css",
+]
+
+return url_list
