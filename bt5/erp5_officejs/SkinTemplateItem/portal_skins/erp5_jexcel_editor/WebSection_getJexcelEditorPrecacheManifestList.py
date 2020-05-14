dff733f9f0f881792f1da4dfdbc305a31551fc7b
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-05-14 10:11:01 +0000

    erp5_officejs: jexcel app changes

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_jexcel_editor/WebSection_getJexcelEditorPrecacheManifestList.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_jexcel_editor/WebSection_getJexcelEditorPrecacheManifestList.py
@@ -3,6 +3,7 @@ url_list = [
   "rsvp.js",
   "jexcel.gadget.html",
   "jexcel.gadget.js",
+  "fonts.ttf",
   "icons.css",
   "jexcel/jexcel.js",
   "jexcel/jexcel.css",
@@ -10,12 +11,6 @@ url_list = [
   "jexcel/jexcel.webcomponent.js",
   "jsuites/jsuites.js",
   "jsuites/jsuites.css",
-
-  "gadget_officejs_jexcel_editor.configuration",
-  "gadget_officejs_jexcel_editor_router.html",
-  #app custom actions
-  "action_jexcel_editor_clone.html",
-  "action_jexcel_editor_clone.js"
 ]
 
 return url_list
--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -1,6 +1,6 @@
 /*jslint nomen: true, indent: 2 */
 /*global window, rJS, RSVP, jexcel*/
-(function (window, rJS, RSVP, jexcel) {
+(function (window, rJS, jexcel) {
   "use strict";
 
   var template = {
@@ -32,6 +32,8 @@
 
   rJS(window)
 
+    .setState({saveStyle: false})
+
     .declareAcquiredMethod("notifySubmit", "notifySubmit")
     .declareJob("deferNotifySubmit", function () {
       // Ensure error will be correctly handled
@@ -50,40 +52,47 @@
     })
 
     .onStateChange(function (modification_dict) {
-      var gadget = this;
-      var table;
+      var gadget = this, tmp = Object.assign({}, template), table;
       gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
-      gadget.deferNotifySubmitBinded = gadget.deferNotifySubmit.bind(gadget);
       if (modification_dict.hasOwnProperty('value')) {
-        var tmp = Object.assign({}, template);
+        gadget.state.value = gadget.state.value === "" ? gadget.state.value : JSON.parse(gadget.state.value);
         Object.assign(tmp, template);
         Object.assign(tmp, gadget.state.value);
         table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(tmp, {
-          onchange: gadget.deferNotifyChangeBinded,
+          onevent: function (ev) {
+            var exluded_events = ["onload", "onfocus", "onblur", "onselection"];
+            if (!exluded_events.includes(ev)) {
+              if ((ev === "onchangestyle" && gadget.state.saveStyle) || ev !== "onchangestyle") {
+                gadget.deferNotifyChangeBinded();
+              } else {
+                gadget.state.saveStyle = true;
+              }
+            }
+          },
           toolbar: [
             {
               type: 'i',
               content: 'undo',
               onclick: function () {
-                  table.undo();
-                }
+                table.undo();
+              }
             },
             {
               type: 'i',
               content: 'redo',
               onclick: function () {
-                  table.redo();
-                }
+                table.redo();
+              }
             },
             {
               type: 'select',
               k: 'font-family',
-              v: ['Arial', 'Verdana']
+              v: ['Arial', 'Comic Sans MS', 'Verdana', 'Calibri', 'Tahoma', 'Helvetica', 'DejaVu Sans', 'Times New Roman', 'Georgia', 'Antiqua']
             },
             {
               type: 'select',
               k: 'font-size',
-              v: ['9px', '10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px']
+              v: ['9px', '10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px', '22px', '24px', '26px', '28px', '30px']
             },
             {
               type: 'i',
@@ -99,7 +108,7 @@
             },
             {
               type: 'i',
-              content: 'format_align_right', 
+              content: 'format_align_right',
               k: 'text-align',
               v: 'right'
             },
@@ -108,6 +117,12 @@
               content: 'format_bold',
               k: 'font-weight',
               v: 'bold'
+            },
+            {
+              type: 'i',
+              content: 'format_italic',
+              k: 'font-style',
+              v: 'italic'
             },
             {
               type: 'color',
@@ -126,8 +141,7 @@
     })
 
     .declareMethod('getContent', function () {
-      var gadget = this;
-      var form_data = {};
+      var gadget = this, form_data = {};
       if (this.state.editable || true) {
         form_data[this.state.key] = JSON.stringify(gadget.table.getConfig());
         this.state.value = form_data[this.state.key];
@@ -135,4 +149,4 @@
       return form_data;
     });
 
-}(window, rJS, RSVP, jexcel));
+}(window, rJS, jexcel));
