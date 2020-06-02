69d8f742853c7bf4c30cb32c89c23312b5682957
Romain Courteaud <romain@nexedi.com>
2020-06-02 14:04:27 +0000

    erp5_hal_json_style: para

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -475,6 +475,7 @@ def renderField(traversed_document, field, form, value=MARKER, meta_type=None, k
     if meta_type == "ParallelListField":
       hash_script_id = field.get_value('hash_script_id')
       if hash_script_id:
+        result['title'] = ''
         result.update({"couscous": getattr(field, hash_script_id)(
                 [x for x in result['items'] if (x[1] and x[0])],
                 # Drop empty values
@@ -491,6 +492,10 @@ def renderField(traversed_document, field, form, value=MARKER, meta_type=None, k
                 },
                 is_right_display=0
         )})
+        for subdict in result['couscous']:
+          subdict['key'] = field.generate_subfield_key(
+                                     subdict['key'], key=result['key'])
+
       # else:
       #   result['']
 
