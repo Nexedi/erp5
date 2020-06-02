e25b622ebe5b8b8b15bcb53c6f482d38f1beba22
Romain Courteaud <romain@nexedi.com>
2020-06-02 15:38:35 +0000

    erp5_ui_test: para
    erp5_web_renderjs_ui_test: para
    
    erp5_web_renderjs_ui_test: para
    
    erp5_ui_test: para
    
    erp5_web_renderjs_ui: para
    
    erp5_web_renderjs_ui: para
    
    erp5_web_renderjs_ui: para

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_parallellistfield_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_parallellistfield_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, document, RSVP, isEmpty, ensureArray, console */
-/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 200, unparam: true */
-(function (window, rJS, document, RSVP, isEmpty, ensureArray, getFirstNonEmpty) {
+/*global window, rJS, document, RSVP, ensureArray */
+/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
+(function (window, rJS, document, RSVP, ensureArray) {
   'use strict';
 
   var SCOPE_PREFIX = 'PARALLEL_SUB_FIELD_';
@@ -17,10 +17,9 @@
       field_list: []
     })
     .declareMethod('render', function (options) {
-      console.log('start render');
       var field_json = options.field_json || {},
         state_dict = {
-          field_list: field_json.couscous,
+          field_list: field_json.subfield_list,
           previous_field_length: this.state.field_list.length,
           // Force calling subfield render
           // as user may have modified the input value
@@ -28,13 +27,11 @@
         };
       return this.changeState(state_dict)
         .push(function (error) {
-          console.log('stop render');
           return error;
         });
     })
 
     .onStateChange(function () {
-      console.log('change state', this.state);
       var gadget = this,
         promise_list = [],
         element = gadget.element,
@@ -44,11 +41,9 @@
       // First update/create needed gadgets
       for (i = 0; i < gadget.state.field_list.length; i += 1) {
         if (i < gadget.state.previous_field_length) {
-          console.log('get', i);
           // Only need to get the existing gadget
           promise_list.push(gadget.getDeclaredGadget(SCOPE_PREFIX + i));
         } else {
-          console.log('declare', i);
           div = document.createElement('div');
           gadget.element.appendChild(div);
           promise_list.push(gadget.declareGadget('gadget_erp5_label_field.html',
@@ -57,34 +52,24 @@
       }
 
       // And remove all subgadgets not requested anymore
-      for (i = gadget.state.previous_field_length - 1; i >= gadget.state.field_list.length; i -= 1) {
-        console.log('drop', i);
+      for (i = gadget.state.previous_field_length - 1;
+           i >= gadget.state.field_list.length; i -= 1) {
         promise_list.push(gadget.dropGadget(SCOPE_PREFIX + i));
         element.removeChild(element.lastChild);
       }
-      console.log(promise_list);
       return new RSVP.Queue(RSVP.all(promise_list))
         .push(function (gadget_list) {
-          console.log(gadget_list, gadget.state.field_list);
           var sub_state;
           promise_list = [];
           for (i = 0; i < gadget.state.field_list.length; i += 1) {
-            console.log('render', i);
             sub_state = gadget.state.field_list[i];
 
-            // Compatibility
-            if (!sub_state.hasOwnProperty('items')) {
-              sub_state.items = sub_state.item_list;
-            }
             promise_list.push(gadget_list[i].render({
               field_json: sub_state,
               field_type: sub_state.field_type
             }));
           }
           return RSVP.all(promise_list);
-        })
-        .push(function () {
-          console.log('stop change state');
         });
     })
 
@@ -106,7 +91,6 @@
           return RSVP.all(promise_list);
         })
         .push(function (content_list) {
-          console.log('contnent', content_list);
           var result = {},
             key,
             j,
@@ -127,51 +111,13 @@
               }
             }
           }
-          console.log('result', result);
           return result;
         });
 
     }, {mutex: 'changestate'})
 
-    .allowPublicAcquisition('notifyValid', function () {
-      return;
-    })
-
-    .declareAcquiredMethod("notifyValid", "notifyValid")
-    .declareAcquiredMethod("notifyInvalid", "notifyInvalid")
-
     .declareMethod('checkValidity', function () {
       return true;
+    });
 
-      var gadget = this,
-        empty = true;
-      if (this.state.editable && this.state.required) {
-        return new RSVP.Queue()
-          .push(function () {
-            return RSVP.all([
-              gadget.getContent(),
-              gadget.translate("Input is required but no input given.")
-            ]);
-          })
-          .push(function (all_result) {
-            var value_list = all_result[0][gadget.state.sub_select_key],
-              error_message = all_result[1],
-              i;
-            for (i = 0; i < value_list.length; i += 1) {
-              if (value_list[i]) {
-                empty = false;
-              }
-            }
-            if (empty) {
-              return gadget.notifyInvalid(error_message);
-            }
-            return gadget.notifyValid();
-          })
-          .push(function () {
-            return !empty;
-          });
-      }
-      return true;
-    }, {mutex: 'changestate'});
-
-}(window, rJS, document, RSVP, isEmpty, ensureArray, getFirstNonEmpty));
+}(window, rJS, document, RSVP, ensureArray));
