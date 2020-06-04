b6385d9c1878909edcbfea95706c85863925f475
Jérome Perrin <jerome@nexedi.com>
2020-06-04 15:20:19 +0900

    simulation: don't look in request in action scripts
    formulator passes arguments, no need to look in request by hand. Also
    this makes scripts easier to test if passed arguments are used.

--- bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
+++ bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
@@ -1,16 +1,14 @@
 request = context.REQUEST
 from Products.ERP5Type.Message import translateString
 
-listbox = request.get('listbox')
 line_list = context.Delivery_getSolverDecisionList(listbox=listbox)
 
 if len(line_list) == 0:
   kw["keep_items"] = {'portal_status_message': translateString("Workflow state may have been updated by other user. Please try again.")}
   return context.Base_redirect(form_id, **kw)
 
-for listbox_key in listbox:
-  listbox_dict = listbox[listbox_key]
-  line = [x for x in line_list if x.getPath() == listbox_key][0]
+for listbox_dict in listbox:
+  line = [x for x in line_list if x.getPath() == listbox_dict['listbox_key']][0]
   uid = line.getUid()
   for prop in ('solver', 'solver_configuration', 'delivery_solver', 'comment',):
     value = listbox_dict.get(prop, None)
