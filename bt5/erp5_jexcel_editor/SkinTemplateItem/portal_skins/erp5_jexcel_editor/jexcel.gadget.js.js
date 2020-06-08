d83d8005c3a74a64425c24390f999a540bf8836a
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-06-08 08:56:47 +0000

    erp5_jexcel_editor: add multiple tabs, no formula bar

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -5,76 +5,88 @@
 
   rJS(window)
 
-    .setState({saveConfig: false})
+    .setState({
+      saveConfig: false,
+      newSheet: false
+    })
 
     .declareAcquiredMethod("notifyChange", "notifyChange")
 
     .declareJob("deferNotifyChange", function () {
-      // Ensure error will be correctly handled
       return this.notifyChange();
     })
 
     .declareMethod("render", function (options) {
       var gadget = this;
-      return gadget.getDeclaredGadget("toolbar_gadget")
-      .push(function (toolbar_gadget) {
-        gadget.toolbar_gadget = toolbar_gadget;
+      return gadget.getDeclaredGadget("template_gadget")
+      .push(function (template_gadget) {
+        gadget.template_gadget = template_gadget;
       })
       .push(function () {
+        options.newSheet = true;
         return gadget.changeState(options);
       });
     })
 
     .declareMethod('getContent', function () {
       var gadget = this, form_data = {};
-      if (this.state.editable || true) {
-        form_data[this.state.key] = JSON.stringify(gadget.table.getConfig());
+      if (this.state.editable) {
+        var sheets = [];
+        gadget.element.querySelector('.spreadsheet').jexcel.forEach(function (sheet) {
+          sheets.push(sheet.getConfig());
+        });
+        form_data[this.state.key] = JSON.stringify(sheets);
         this.state.value = form_data[this.state.key];
       }
       return form_data;
     })
 
+    .declareMethod("addSheet", function () {
+      var gadget = this;
+      return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), {
+        undo_redo: true,
+        add: true,
+        merge: true,
+        text_font: true,
+        text_position: true,
+        color_picker: true
+      })
+      .push(function (dict) {
+        dict.sheetName = "Sheet " + (gadget.element.querySelector('.spreadsheet').jexcel.length + 1);
+        jexcel.tabs(gadget.element.querySelector(".spreadsheet"), [dict]);
+        return gadget.changeState({newSheet: true});
+      });
+    })
+
     .onStateChange(function (modification_dict) {
-      var template = {
-        minDimensions: [52, 300],
-        defaultColWidth: 100,
-        allowExport: true,
-        columnSorting: true,
-        columnDrag: true,
-        columnResize: true,
-        rowResize: true,
-        rowDrag: true,
-        editable: true,
-        allowInsertRow: true,
-        allowManualInsertRow: true,
-        allowInsertColumn: true,
-        allowManualInsertColumn: true,
-        allowDeleteRow: true,
-        allowRenameColumn: true,
-        allowComments: true,
-        selectionCopy: true,
-        search: true,
-        fullscreen: true,
-        autoIncrement: true,
-        parseFormulas: true
-      },
-      gadget = this, tmp = Object.assign({}, template), table, toolbar_list;
+      console.log(modification_dict.hasOwnProperty("newSheet"));
+      var gadget = this, tmp;
       gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
       if (modification_dict.hasOwnProperty('value')) {
-        gadget.state.value = gadget.state.value === "" ? gadget.state.value : JSON.parse(gadget.state.value);
-        Object.assign(tmp, template);
-        Object.assign(tmp, gadget.state.value);
-        return gadget.toolbar_gadget.getToolbarList({
+        return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), {
+            undo_redo: true,
+            add: true,
+            merge: true,
             text_font: true,
             text_position: true,
             color_picker: true
-          })
-        .push(function (list) {
-            toolbar_list = list;
-          })
-        .push(function () {
-            table = jexcel(gadget.element.querySelector(".spreadsheet"), Object.assign(tmp, {
-              onevent: function (ev) {
+        })
+        .push(function (config) {
+            tmp = Object.assign({}, config);
+            if (gadget.state.value === "") {
+              gadget.state.value = [tmp];
+            }
+            else {
+              gadget.state.value = JSON.parse(gadget.state.value);
+              gadget.state.value.map(sheet => {
+                var res = Object.assign(sheet, tmp);
+                return res;
+              });
+            }
+            gadget.state.value.map((sheet, i) => {
+              sheet.sheetName = "Sheet " + (i + 1);
+              sheet.onEvent = function (ev) {
+                console.log("event");
                 var exluded_events = ["onload", "onfocus", "onblur", "onselection"];
                 if (!exluded_events.includes(ev)) {
                   if ((ev === "onchangestyle" && gadget.state.saveConfig) || ev !== "onchangestyle") {
@@ -83,116 +95,57 @@
                     gadget.state.saveConfig = true;
                   }
                 }
-              },
-              onselection: function (ev) {
-                var cell = gadget.element.querySelector("td.highlight-selected");
-                var formula = gadget.element.querySelector("input.jexcel_formula");
-                formula.value = cell.textContent;
-              },
-              toolbar: [
-                //undo
-                {
-                  type: 'i',
-                  content: 'undo',
-                  onclick: function () {
-                    table.undo();
-                  }
-                },
-                //redo
-                {
-                  type: 'i',
-                  content: 'redo',
-                  onclick: function () {
-                    table.redo();
-                  }
-                },
-                //merge cells
-                {
-                  type: 'i',
-                  content: 'table_chart',
-                  onclick: function () {
-                    var cell = gadget.element.querySelector("td.highlight-selected");
-                    var x = Number(cell.dataset.x);
-                    var selected = table.getJson(true);
-                    var colspan = Object.keys(selected[0]).length;
-                    var rowspan = selected.length;
-                    var letter = "";
-                    if (x <= 25) {
-                      letter += String.fromCharCode(97 + x).toUpperCase();
-                    }
-                    else {
-                      letter += String.fromCharCode(97 + Math.trunc(x / 25) - 1).toUpperCase();
-                      letter += String.fromCharCode(97 + (x % 26)).toUpperCase();
-                    }
-                    var coor = letter + (Number(cell.dataset.y) + 1).toString();
-                    table.setMerge(coor, colspan, rowspan);
-                  }
-                },
-                //unmerge cell
-                {
-                  type: 'i',
-                  content: 'close',
-                  onclick: function () {
-                    var cell = gadget.element.querySelector("td.highlight-selected");
-                    var x = Number(cell.dataset.x);
-                    var letter = "";
-                    if (x <= 25) {
-                      letter += String.fromCharCode(97 + x).toUpperCase();
-                    }
-                    else {
-                      letter += String.fromCharCode(97 + Math.trunc(x / 25) - 1).toUpperCase();
-                      letter += String.fromCharCode(97 + (x % 26)).toUpperCase();
-                    }
-                    var coor = letter + (Number(cell.dataset.y) + 1).toString();
-                    table.removeMerge(coor);
-                  }
-                },
-                //destroy all merged cells
-                {
-                  type: 'i',
-                  content: 'cancel',
-                  onclick: function () {
-                    table.destroyMerged();
-                  }
-                }
-              ].concat(toolbar_list)
-            }));
-            gadget.table = table;
-            var filter = gadget.element.querySelector(".jexcel_filter");
-            gadget.element.querySelector(".jexcel_toolbar").appendChild(filter);
-            var formula_div = document.createElement("div");
-            formula_div.classList.add("jexcel_formula");
-            var img = document.createElement("img");
-            img.src = "fx.png";
-            var formula_input = document.createElement("input");
-            formula_input.classList.add("jexcel_formula");
-            formula_div.appendChild(img);
-            formula_div.appendChild(formula_input);
-            gadget.element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, gadget.element.querySelector("div.jexcel_toolbar").nextSibling);
-            console.log(gadget.element.querySelectorAll("i"));
-            var icon_title = {
-              "undo": "Undo",
-              "redo": "Redo",
-              "table_chart": "Merge cells",
-              "close": "Destroy merge",
-              "cancel": "Destroy all merges",
-              "format_bold": "Bold",
-              "format_italic": "Italic",
-              "format_underlined": "Underline",
-              "format_align_left": "Align left",
-              "format_align_center": "Align center",
-              "format_align_right": "Align right",
-              "format_align_justify": "Align justify",
-              "vertical_align_top": "Align top",
-              "vertical_align_center": "Align middle",
-              "vertical_align_bottom": "Align bottom",
-            }
-            gadget.element.querySelectorAll("i").forEach(i => {
-              if (i.dataset.k === "color") {i.title = "Color"}
-              else if (i.dataset.k === "background-color") {i.title = "Background color"}
-              else {i.title = icon_title[i.textContent]}
-            });
+                return sheet;
+              };
+              //sheet.onselection = function (ev) {
+                //var cell = gadget.element.querySelector("td.highlight-selected");
+                //var formula = gadget.element.querySelector("input.jexcel_formula");
+                //formula.value = cell.textContent;
+              //};
           });
+          return gadget.state.value;
+        })
+        .push(function (sheets) {
+            jexcel.tabs(gadget.element.querySelector(".spreadsheet"), sheets)
+        });
+      }
+      console.log("aaaaaa");
+      if (modification_dict.hasOwnProperty("newSheet") || true) {
+        var filter = gadget.element.querySelector(".jexcel_filter");
+        gadget.element.querySelector(".jexcel_toolbar").appendChild(filter);
+        var formula_div = document.createElement("div");
+        formula_div.classList.add("jexcel_formula");
+        var img = document.createElement("img");
+        img.src = "fx.png";
+        var formula_input = document.createElement("input");
+        formula_input.classList.add("jexcel_formula");
+        formula_div.appendChild(img);
+        formula_div.appendChild(formula_input);
+        gadget.element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, gadget.element.querySelector("div.jexcel_toolbar").nextSibling);
+        var icon_title = {
+          "undo": "Undo",
+          "redo": "Redo",
+          "add": "Add",
+          "table_chart": "Merge cells",
+          "close": "Destroy merge",
+          "cancel": "Destroy all merges",
+          "format_bold": "Bold",
+          "format_italic": "Italic",
+          "format_underlined": "Underline",
+          "format_align_left": "Align left",
+          "format_align_center": "Align center",
+          "format_align_right": "Align right",
+          "format_align_justify": "Align justify",
+          "vertical_align_top": "Align top",
+          "vertical_align_center": "Align middle",
+          "vertical_align_bottom": "Align bottom",
+        }
+        gadget.element.querySelectorAll("i").forEach(i => {
+          if (i.dataset.k === "color") {i.title = "Color"}
+          else if (i.dataset.k === "background-color") {i.title = "Background color"}
+          else {i.title = icon_title[i.textContent]}
+        });
+        gadget.state.newSheet = false;
       }
     })
 
@@ -200,7 +153,7 @@
         var gadget = this;
         var formula = gadget.element.querySelector("input.jexcel_formula");
         if (ev.target == gadget.element.querySelector("td.highlight-selected input")) {
-          formula.value = ev.target.value;
+          //formula.value = ev.target.value;
         }
       }, false, false)
 
