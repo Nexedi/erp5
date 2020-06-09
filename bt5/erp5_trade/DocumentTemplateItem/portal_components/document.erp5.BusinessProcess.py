5c1479e8cc35111bbcff36e5fff2ea9f118a534c
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2020-06-09 08:27:22 +0200

    erp5_trade: move the composition cache in BusinessProcess.py

--- bt5/erp5_trade/DocumentTemplateItem/portal_components/document.erp5.BusinessProcess.py
+++ bt5/erp5_trade/DocumentTemplateItem/portal_components/document.erp5.BusinessProcess.py
@@ -886,3 +886,15 @@ class BusinessProcess(Path, XMLObject):
           phase_set.discard(phase)
           phase_set |= next_set
     return result
+
+  # Cache created classes to avoid memory leaks.
+  __class_cache = {}
+
+  @classmethod
+  def getComposedType(cls, class_name, base_class_list):
+    try:
+      return cls.__class_cache[class_name]
+    except KeyError:
+      composed_class = type(class_name, base_class_list + (cls,), {})
+      cls.__class_cache[class_name] = composed_class
+      return composed_class
