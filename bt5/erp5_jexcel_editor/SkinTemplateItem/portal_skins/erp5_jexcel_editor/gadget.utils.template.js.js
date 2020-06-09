9ebb320711d74339a9be5de7e94fa8af4c7d2ea3
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-06-09 08:01:22 +0000

    erp5_jexcel_editor: handle multiple tabs (formula bar not working)
    erp5_jexcel_editor: fix tabs and formula bar
    
    erp5_jexcel_editor: remove and rename tabs
    
    erp5_jexcel_editor: fixed delete and rename tabs
    
    erp5_jexcel_editor: add formula from toolbar and real cell value in formula bar
    
    erp5_jexcel_editor: more formulas
    
    erp5_jexcel_editor: fixed merge issue, added warning before merge, fixed formula dropdown, parse formulas and hide js code i the cell, added cell selection input
    
    erp5_jexcel_editor: new  columns types : images, html, color, text, calendar, checkbox
    
    erp5_jexcel_editor: fixed select width
    
    erp5_jexcel_editor: stable version
    
    erp5_jexcel_editor: fixed merge save conflict
    
    erp5_jexcel_editor: fixed formula dropdown, different cursors

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/gadget.utils.template.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/gadget.utils.template.js.js
@@ -3,8 +3,24 @@
 (function (window, rJS, jexcel) {
   "use strict";
 
+  var numberToLetter = function (i) {
+    return (i >= 26 ? numberToLetter((i / 26 >> 0) - 1) : '') + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[i % 26 >> 0];
+  };
+
+  var getCoordsFromCell = function (cell) {
+    var x = Number(cell.dataset.x);
+    var y = Number(cell.dataset.y) + 1;
+    return numberToLetter(x) + y.toString();
+  };
+
+  var fireDblClick = function (el) {
+    var clickEvent  = document.createEvent('MouseEvents');
+    clickEvent.initEvent('dblclick', true, true);
+    el.dispatchEvent(clickEvent);
+  };
+
   var template = {
-    minDimensions: [26, 100],
+    minDimensions: [26, 200],
     defaultColWidth: 100,
     allowExport: true,
     columnSorting: true,
@@ -24,7 +40,8 @@
     search: true,
     fullscreen: true,
     autoIncrement: true,
-    parseFormulas: true
+    parseFormulas: true,
+    wordWrap: true
   };
 
   var undo = {
@@ -47,21 +64,12 @@
     type: 'i',
     content: 'table_chart',
     onclick: function (a, b, c) {
-      var cell = document.querySelector("td.highlight");
-      var x = Number(cell.dataset.x);
+      var cell = a.querySelector("td.highlight");
       var selected = b.getJson(true);
       var colspan = Object.keys(selected[0]).length;
       var rowspan = selected.length;
-      var letter = "";
-      if (x <= 25) {
-        letter += String.fromCharCode(97 + x).toUpperCase();
-      }
-      else {
-        letter += String.fromCharCode(97 + Math.trunc(x / 25) - 1).toUpperCase();
-        letter += String.fromCharCode(97 + (x % 26)).toUpperCase();
-      }
-      var coor = letter + (Number(cell.dataset.y) + 1).toString();
-      b.setMerge(coor, colspan, rowspan);
+      var coor = getCoordsFromCell(cell);
+      confirm("Top left selected cell's content will be kept, other will be erased.") ? b.setMerge(coor, colspan, rowspan) : null;
     }
   };
 
@@ -71,15 +79,7 @@
     onclick: function (a, b, c) {
       var cell = document.querySelector("td.highlight-selected");
       var x = Number(cell.dataset.x);
-      var letter = "";
-      if (x <= 25) {
-        letter += String.fromCharCode(97 + x).toUpperCase();
-      }
-      else {
-        letter += String.fromCharCode(97 + Math.trunc(x / 25) - 1).toUpperCase();
-        letter += String.fromCharCode(97 + (x % 26)).toUpperCase();
-      }
-      var coor = letter + (Number(cell.dataset.y) + 1).toString();
+      var coor = getCoordsFromCell(cell);
       b.removeMerge(coor);
     }
   };
@@ -97,10 +97,11 @@
     k: 'font-family',
     v: ['Arial', 'Comic Sans MS', 'Verdana', 'Calibri', 'Tahoma', 'Helvetica', 'DejaVu Sans', 'Times New Roman', 'Georgia', 'Antiqua']
   };
+
   var font_size = {
     type: 'select',
     k: 'font-size',
-    v: ['9px', '10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px', '22px', '24px', '26px', '28px', '30px']
+    v: ['8px', '10px', '12px', '14px', '16px', '18px', '20px', '22px', '24px', '26px', '28px', '30px', '34px', '38px', '42px', '46px', '50px']
   };
 
   var text_align_left = {
@@ -183,9 +184,147 @@
     k: 'background-color'
   };
 
+  var image = {
+    type: "i",
+    content: "image",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        instance.options.columns[Number(cell.dataset.x)].type = "image";
+        fireDblClick(cell);
+      }
+    }
+  };
+
+  var checkbox = {
+    type: "i",
+    content: "checkbox",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        array.shift();
+        array.forEach(function (cell) {
+          instance.setValue(getCoordsFromCell(cell), "");
+          cell.innerHTML = "<input type='checkbox' name='c" + cell.dataset.x + "'>";
+        });
+        instance.options.columns[Number(cell.dataset.x)].type = "checkbox";
+      }
+    }
+  };
+
+  var radio = {
+    type: "i",
+    content: "radio",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        array.shift();
+        array.forEach(function (cell) {
+          instance.setValue(getCoordsFromCell(cell), "");
+          cell.innerHTML = "<input type='radio'>";
+        });
+        instance.options.columns[Number(cell.dataset.x)].type = "radio";
+      }
+    }
+  };
+
+  var text = {
+    type: "i",
+    content: "title",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        instance.options.columns[Number(cell.dataset.x)].type = "text";
+        array.shift();
+        array.forEach(function (cell) {
+          cell.innerHTML = "";
+          instance.setValue(getCoordsFromCell(cell), "");
+        });
+        fireDblClick(cell);
+      }
+    }
+  };
+
+  var html = {
+    type: "i",
+    content: "list",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        instance.options.columns[Number(cell.dataset.x)].type = "html";
+        array.shift();
+        array.forEach(function (cell) {
+          cell.innerHTML = "";
+          instance.setValue(getCoordsFromCell(cell), "");
+        });
+        fireDblClick(cell);
+      }
+    }
+  };
+
+  var calendar = {
+    type: "i",
+    content: "calendar_today",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        instance.options.columns[Number(cell.dataset.x)].type = "calendar";
+        array.shift();
+        array.forEach(function (cell) {
+          cell.innerHTML = "";
+          instance.setValue(getCoordsFromCell(cell), "");
+        });
+        fireDblClick(cell);
+      }
+    }
+  };
+
+  var color = {
+    type: "i",
+    content: "color_lens",
+    onclick: function (a, b, c) {
+      var cell = a.querySelector("td.highlight-selected");
+      if (cell && confirm("Data in this column will be erased.")) {
+        var worksheet = document.querySelector('.selected').getAttribute('data-spreadsheet');
+        var instance = document.querySelector('.spreadsheet').jexcel[worksheet];
+        var column = instance.el.querySelectorAll("td[data-x='" + cell.dataset.x + "']");
+        var array = [...column];
+        array.shift();
+        array.forEach(function (cell) {
+          instance.setValue(getCoordsFromCell(cell), "");
+          cell.innerHTML = "<div class='color' style='background-color: rgb(0,0,0);'></div>";
+        });
+        instance.options.columns[Number(cell.dataset.x)].type = "color";
+        fireDblClick(cell);
+      }
+    }
+  };
+
   rJS(window)
 
-    .declareMethod("getToolbarList", function (add_function, dict) {
+    .declareMethod("getToolbarList", function (add_function, remove_function, dict) {
       var list = [];
       if (dict.hasOwnProperty("undo_redo") && dict.undo_redo) {
         list.push(undo, redo);
@@ -196,7 +335,12 @@
           content: 'add',
           onclick : add_function
         };
-        list.push(add);
+        var remove = {
+          type: 'i',
+          content: 'delete',
+          onclick: remove_function
+        };
+        list.push(add, remove);
       }
       if (dict.hasOwnProperty("merge") && dict.merge) {
         list.push(merge, unmerge, destroy_merge);
@@ -210,9 +354,26 @@
       if (dict.hasOwnProperty("color_picker") && dict.color_picker) {
         list.push(text_color, background_color);
       }
+      if (dict.hasOwnProperty("type") && dict.type) {
+        list.push(text, image, checkbox, html, calendar, color);
+      }
       var res = Object.assign({}, template);
       res.toolbar = list;
       return res;
+    })
+
+    .declareMethod("buildOptions", function () {
+      var str = "";
+      var formulas = ["SUM", "MIN", "MAX", "COUNT", "AVERAGE", "FLOOR", "ABS", "SQRT", "ISEVEN", "ISODD", "TODAY", "UPPER", "LOWER", "TRUNC", "TYPE", "TRIM",
+                     "SIN", "COS", "TAN", "ARCSIN", "ARCCOS", "ARCTAN", "ROUND", "RAND", "RANDBETWEEN", "RADIANS", "POWER", "PI", "PHI", "MOD", "LEN", "LN",
+                      "LOG", "LOG10", "FACT", "TRUE", "FALSE", "AND", "OR", "XOR", "EVEN", "ODD", "EXP", "CONCATENATE", "BITAND", "BITOR", "BIN2DEC", "BIN2HEX",
+                     "BIN2OCT", "DEC2BIN", "DEC2HEX", "DEC2OCT", "HEX2BIN", "HEX2DEC", "HEX2OCT", "NOT", "OCT2BIN", "OCT2DEC", "OCT2HEX", "PRODUCT", "QUOTIENT",
+                     "COLUMN", "ROW", "CELL"].sort();
+      formulas.forEach(function (value) {
+        str += "<option class='formula_option' value=" + value + ">" + value + "()" + "</option>";
+      });
+      str = "<option class='formula_option'>FORMULA</option>" + str;
+      return str;
     });
 
 }(window, rJS, jexcel));
