9e762de4ff350d21a059d85eea1b9bc7560cb9e0
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-06-09 08:01:22 +0000

    erp5_jexcel_editor: handle multiple tabs (formula bar not working)

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -23,7 +23,6 @@
         gadget.template_gadget = template_gadget;
       })
       .push(function () {
-        options.newSheet = true;
         return gadget.changeState(options);
       });
     })
@@ -54,14 +53,57 @@
       .push(function (dict) {
         dict.sheetName = "Sheet " + (gadget.element.querySelector('.spreadsheet').jexcel.length + 1);
         jexcel.tabs(gadget.element.querySelector(".spreadsheet"), [dict]);
+      })
+      .push(function () {
         return gadget.changeState({newSheet: true});
       });
     })
 
+    .declareMethod("setupTable", function (element) {
+      var filter = element.querySelector(".jexcel_filter");
+      element.querySelector(".jexcel_toolbar").appendChild(filter);
+      var formula_div = document.createElement("div");
+      formula_div.classList.add("jexcel_formula");
+      var img = document.createElement("img");
+      img.src = "fx.png";
+      var formula_input = document.createElement("input");
+      formula_input.classList.add("jexcel_formula");
+      formula_div.appendChild(img);
+      formula_div.appendChild(formula_input);
+      element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, element.querySelector("div.jexcel_toolbar").nextSibling);
+      var icon_title = {
+        "undo": "Undo",
+        "redo": "Redo",
+        "add": "Add",
+        "table_chart": "Merge cells",
+        "close": "Destroy merge",
+        "cancel": "Destroy all merges",
+        "format_bold": "Bold",
+        "format_italic": "Italic",
+        "format_underlined": "Underline",
+        "format_align_left": "Align left",
+        "format_align_center": "Align center",
+        "format_align_right": "Align right",
+        "format_align_justify": "Align justify",
+        "vertical_align_top": "Align top",
+        "vertical_align_center": "Align middle",
+        "vertical_align_bottom": "Align bottom",
+      }
+      element.querySelectorAll("i").forEach(i => {
+        if (i.dataset.k === "color") {i.title = "Color"}
+        else if (i.dataset.k === "background-color") {i.title = "Background color"}
+        else {i.title = icon_title[i.textContent]}
+      });
+      gadget.state.newSheet = false;
+    })
+
     .onStateChange(function (modification_dict) {
-      console.log(modification_dict.hasOwnProperty("newSheet"));
       var gadget = this, tmp;
       gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
+      if (modification_dict.hasOwnProperty('newSheet') && modification_dict.newSheet) {
+        var tabs = (gadget.element.querySelectorAll(".jexcel_container"));
+        return gadget.setupTable(tabs[tabs.length - 1]);
+      }
       if (modification_dict.hasOwnProperty('value')) {
         return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), {
             undo_redo: true,
@@ -106,46 +148,11 @@
           return gadget.state.value;
         })
         .push(function (sheets) {
-            jexcel.tabs(gadget.element.querySelector(".spreadsheet"), sheets)
+            jexcel.tabs(gadget.element.querySelector(".spreadsheet"), sheets);
+            gadget.element.querySelectorAll(".jexcel_container").forEach(tab => {
+              return gadget.setupTable(tab);
+            })
         });
-      }
-      console.log("aaaaaa");
-      if (modification_dict.hasOwnProperty("newSheet") || true) {
-        var filter = gadget.element.querySelector(".jexcel_filter");
-        gadget.element.querySelector(".jexcel_toolbar").appendChild(filter);
-        var formula_div = document.createElement("div");
-        formula_div.classList.add("jexcel_formula");
-        var img = document.createElement("img");
-        img.src = "fx.png";
-        var formula_input = document.createElement("input");
-        formula_input.classList.add("jexcel_formula");
-        formula_div.appendChild(img);
-        formula_div.appendChild(formula_input);
-        gadget.element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, gadget.element.querySelector("div.jexcel_toolbar").nextSibling);
-        var icon_title = {
-          "undo": "Undo",
-          "redo": "Redo",
-          "add": "Add",
-          "table_chart": "Merge cells",
-          "close": "Destroy merge",
-          "cancel": "Destroy all merges",
-          "format_bold": "Bold",
-          "format_italic": "Italic",
-          "format_underlined": "Underline",
-          "format_align_left": "Align left",
-          "format_align_center": "Align center",
-          "format_align_right": "Align right",
-          "format_align_justify": "Align justify",
-          "vertical_align_top": "Align top",
-          "vertical_align_center": "Align middle",
-          "vertical_align_bottom": "Align bottom",
-        }
-        gadget.element.querySelectorAll("i").forEach(i => {
-          if (i.dataset.k === "color") {i.title = "Color"}
-          else if (i.dataset.k === "background-color") {i.title = "Background color"}
-          else {i.title = icon_title[i.textContent]}
-        });
-        gadget.state.newSheet = false;
       }
     })
 
