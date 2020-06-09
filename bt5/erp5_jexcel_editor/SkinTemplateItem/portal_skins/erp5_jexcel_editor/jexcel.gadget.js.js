0cf412ec1b2a397bb8a103e66c5de1a20c2a449e
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-06-09 08:01:22 +0000

    erp5_jexcel_editor: handle multiple tabs (formula bar not working)
    erp5_jexcel_editor: fix tabs and formula bar
    
    erp5_jexcel_editor: remove and rename tabs
    
    erp5_jexcel_editor: fixed delete and rename tabs
    
    erp5_jexcel_editor: add formula from toolbar and real cell value in formula bar
    
    erp5_jexcel_editor: more formulas
    
    erp5_jexcel_editor: fixed merge issue, added warning before merge, fixed formula dropdown, parse formulas and hide js code i the cell, added cell selection input
    
    erp5_jexcel_editor: new  columns types : images, html, color, text, calendar, checkbox
    
    erp5_jexcel_editor: fixed select width

--- bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/jexcel.gadget.js.js
@@ -3,11 +3,28 @@
 (function (window, rJS, jexcel) {
   "use strict";
 
+  var toolbar_dict = {
+    undo_redo: true,
+    add: true,
+    merge: true,
+    text_font: true,
+    text_position: true,
+    color_picker: true,
+    type: true
+  };
+
+  var getCoordsFromCell = function (cell) {
+    var x = Number(cell.dataset.x);
+    var y = Number(cell.dataset.y) + 1;
+    return (x >= 26 ? numberToLetter((x / 26 >> 0) - 1) : '') + 'ABCDEFGHIJKLMNOPQRSTWXYZ'[x % 26 >> 0] + y.toString();
+  };
+
   rJS(window)
 
     .setState({
       saveConfig: false,
-      newSheet: false
+      newSheet: false,
+      updateSelection: true
     })
 
     .declareAcquiredMethod("notifyChange", "notifyChange")
@@ -18,12 +35,12 @@
 
     .declareMethod("render", function (options) {
       var gadget = this;
+      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
       return gadget.getDeclaredGadget("template_gadget")
       .push(function (template_gadget) {
         gadget.template_gadget = template_gadget;
       })
       .push(function () {
-        options.newSheet = true;
         return gadget.changeState(options);
       });
     })
@@ -36,96 +53,81 @@
           sheets.push(sheet.getConfig());
         });
         form_data[this.state.key] = JSON.stringify(sheets);
-        this.state.value = form_data[this.state.key];
+        this.state.value = sheets;
       }
       return form_data;
     })
 
+    .declareMethod("getCurrentSheet", function () {
+      var gadget = this;
+      var worksheet = gadget.element.querySelector('.selected').getAttribute('data-spreadsheet');
+      return gadget.element.querySelector('.spreadsheet').jexcel[worksheet];
+    })
+
     .declareMethod("addSheet", function () {
       var gadget = this;
-      return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), {
-        undo_redo: true,
-        add: true,
-        merge: true,
-        text_font: true,
-        text_position: true,
-        color_picker: true
-      })
+      return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), toolbar_dict)
       .push(function (dict) {
         dict.sheetName = "Sheet " + (gadget.element.querySelector('.spreadsheet').jexcel.length + 1);
+        return gadget.bindEvents(dict);
+      })
+      .push(function (dict) {
         jexcel.tabs(gadget.element.querySelector(".spreadsheet"), [dict]);
+        gadget.deferNotifyChangeBinded()
         return gadget.changeState({newSheet: true});
       });
     })
 
-    .onStateChange(function (modification_dict) {
-      console.log(modification_dict.hasOwnProperty("newSheet"));
-      var gadget = this, tmp;
-      gadget.deferNotifyChangeBinded = gadget.deferNotifyChange.bind(gadget);
-      if (modification_dict.hasOwnProperty('value')) {
-        return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), {
-            undo_redo: true,
-            add: true,
-            merge: true,
-            text_font: true,
-            text_position: true,
-            color_picker: true
-        })
-        .push(function (config) {
-            tmp = Object.assign({}, config);
-            if (gadget.state.value === "") {
-              gadget.state.value = [tmp];
+    .declareMethod("setupTable", function (element) {
+      var gadget = this;
+      var filter = element.querySelector(".jexcel_filter");
+      element.querySelector(".jexcel_toolbar").appendChild(filter);
+      element.querySelector("select.jexcel_toolbar_item").classList.add("minimize");
+      var formula_div = document.createElement("div");
+      formula_div.classList.add("jexcel_formula");
+      var img = document.createElement("img");
+      img.src = "fx.png";
+      var formula_input = document.createElement("input");
+      formula_input.classList.add("jexcel_formula");
+      formula_div.appendChild(img);
+      formula_div.appendChild(formula_input);
+      element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, element.querySelector("div.jexcel_toolbar").nextSibling);
+      var cell_input = document.createElement("input");
+      cell_input.classList.add("cell_input");
+      formula_div.insertBefore(cell_input, img);
+      return gadget.template_gadget.buildOptions()
+      .push(function (options) {
+        var select = document.createElement("select");
+        select.innerHTML = options;
+        select.classList.add("minimize");
+        select.onclick = function () {
+          var select = this;
+          var cell = gadget.element.querySelector("td.highlight-selected");
+          return gadget.getCurrentSheet()
+          .push(function (sheet) {
+            var cell = sheet.el.querySelector("td.highlight-selected");
+            if (cell) {
+              var x = Number(cell.dataset.x);
+              var y = Number(cell.dataset.y);
+              var currentValue = sheet.getValueFromCoords(x, y);
+              var value;
+              if (currentValue === "" || currentValue[0] !== "="){
+                value = "=" + select.options[select.selectedIndex].value + "(" + currentValue + ")";
+              }
+              else {
+                value = "=" + select.options[select.selectedIndex].value + "(" + currentValue.substring(1, currentValue.length) + ")";
+              }
+              sheet.setValueFromCoords(x, y, value);
+              formula_input.value = value;
             }
-            else {
-              gadget.state.value = JSON.parse(gadget.state.value);
-              gadget.state.value.map(sheet => {
-                var res = Object.assign(sheet, tmp);
-                return res;
-              });
-            }
-            gadget.state.value.map((sheet, i) => {
-              sheet.sheetName = "Sheet " + (i + 1);
-              sheet.onEvent = function (ev) {
-                console.log("event");
-                var exluded_events = ["onload", "onfocus", "onblur", "onselection"];
-                if (!exluded_events.includes(ev)) {
-                  if ((ev === "onchangestyle" && gadget.state.saveConfig) || ev !== "onchangestyle") {
-                    gadget.deferNotifyChangeBinded();
-                  } else {
-                    gadget.state.saveConfig = true;
-                  }
-                }
-                return sheet;
-              };
-              //sheet.onselection = function (ev) {
-                //var cell = gadget.element.querySelector("td.highlight-selected");
-                //var formula = gadget.element.querySelector("input.jexcel_formula");
-                //formula.value = cell.textContent;
-              //};
-          });
-          return gadget.state.value;
-        })
-        .push(function (sheets) {
-            jexcel.tabs(gadget.element.querySelector(".spreadsheet"), sheets)
-        });
-      }
-      console.log("aaaaaa");
-      if (modification_dict.hasOwnProperty("newSheet") || true) {
-        var filter = gadget.element.querySelector(".jexcel_filter");
-        gadget.element.querySelector(".jexcel_toolbar").appendChild(filter);
-        var formula_div = document.createElement("div");
-        formula_div.classList.add("jexcel_formula");
-        var img = document.createElement("img");
-        img.src = "fx.png";
-        var formula_input = document.createElement("input");
-        formula_input.classList.add("jexcel_formula");
-        formula_div.appendChild(img);
-        formula_div.appendChild(formula_input);
-        gadget.element.querySelector("div.jexcel_toolbar").parentNode.insertBefore(formula_div, gadget.element.querySelector("div.jexcel_toolbar").nextSibling);
+          })
+        }
+        element.querySelector(".jexcel_toolbar").insertBefore(select, filter);
         var icon_title = {
           "undo": "Undo",
           "redo": "Redo",
-          "add": "Add",
+          "add": "Add sheet",
+          "delete": "Delete sheet",
           "table_chart": "Merge cells",
           "close": "Destroy merge",
           "cancel": "Destroy all merges",
@@ -139,22 +141,132 @@
           "vertical_align_top": "Align top",
           "vertical_align_center": "Align middle",
           "vertical_align_bottom": "Align bottom",
+          "image": "Set column type : Image",
+          "radio": "Set column type: Radio button",
+          "checkbox": "Set column type: Checkbox",
+          "title": "Set column type: Text",
+          "list": "Set column type: HTML",
+          "calendar_today": "Set column type: Calendar",
+          "color_lens": "Set column type: Color"
+      }
+      element.querySelectorAll("i").forEach(i => {
+        if (i.dataset.k === "color") {i.title = "Color"}
+        else if (i.dataset.k === "background-color") {i.title = "Background color"}
+        else {i.title = icon_title[i.textContent]}
+      });
+      gadget.element.querySelectorAll(".jexcel_tab_link").forEach(tab => tab.title = "Right click to rename");
+      gadget.state.newSheet = false;
+      });
+    })
+
+    .declareMethod("bindEvents", function (sheet) {
+      var gadget = this;
+      sheet.onevent = function (ev) {
+        var exluded_events = ["onload", "onfocus", "onblur", "onselection"];
+        if (!exluded_events.includes(ev)) {
+          if ((ev === "onchangestyle" && gadget.state.saveConfig) || ev !== "onchangestyle") {
+            gadget.deferNotifyChangeBinded();
+          }
         }
-        gadget.element.querySelectorAll("i").forEach(i => {
-          if (i.dataset.k === "color") {i.title = "Color"}
-          else if (i.dataset.k === "background-color") {i.title = "Background color"}
-          else {i.title = icon_title[i.textContent]}
+      };
+      sheet.onselection = function (ev) {
+        return gadget.getCurrentSheet()
+        .push(function (instance) {
+          var tab = gadget.element.querySelectorAll(".jexcel_container")[gadget.element.querySelector("div.jexcel_tab_link.selected").getAttribute("data-spreadsheet")];
+          var cell = tab.querySelector("td.highlight-selected");
+          var cell_input = tab.querySelector("input.cell_input");
+          var formula = tab.querySelector("input.jexcel_formula");
+          cell_input.value = getCoordsFromCell(cell);
+          var x = Number(cell.dataset.x);
+          var y = Number(cell.dataset.y);
+          formula.value = ["text", "calendar", "checkbox", "color"].includes(instance.options.columns[x].type) ? instance.getValueFromCoords(x, y) : "";
+        })
+      };
+      sheet.oneditionend = function (a, b, c, d, e) {
+        if (e[0] === "=" && e[e.length - 1] !== ")") {
+          var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+          if (numbers.includes(e[e.length - 1])){
+            var worksheet = gadget.element.querySelector('.selected').getAttribute('data-spreadsheet');
+            var tab = gadget.element.querySelector('.spreadsheet').jexcel[worksheet];
+            tab.setValueFromCoords(c, d, e);
+          }
+          else {
+            b.textContent = e;
+          }
+        }
+      };
+      return sheet;
+    })
+
+    .onStateChange(function (modification_dict) {
+      var gadget = this, tmp;
+      if (modification_dict.hasOwnProperty('newSheet') && modification_dict.newSheet) {
+        var tabs = (gadget.element.querySelectorAll(".jexcel_container"));
+        return gadget.setupTable(tabs[tabs.length - 1]);
+      }
+      if (modification_dict.hasOwnProperty('value')) {
+        return gadget.template_gadget.getToolbarList(() => gadget.addSheet(), toolbar_dict)
+        .push(function (config) {
+            tmp = Object.assign({}, config);
+            if (gadget.state.value === "") {
+              gadget.state.value = [tmp];
+            }
+            else {
+              gadget.state.value = JSON.parse(gadget.state.value);
+              gadget.state.value.map(sheet => {
+                var res = Object.assign(sheet, tmp);
+                return res;
+              });
+            }
+            gadget.state.value.map((sheet, i) => {
+              if (!sheet.hasOwnProperty("sheetName")) {sheet.sheetName = "Sheet " + (i + 1)};
+              return gadget.bindEvents(sheet);
+          });
+          return gadget.state.value;
+        })
+        .push(function (sheets) {
+            jexcel.tabs(gadget.element.querySelector(".spreadsheet"), sheets);
+            gadget.element.querySelectorAll(".jexcel_container").forEach(tab => {
+              return gadget.setupTable(tab);
+            })
         });
-        gadget.state.newSheet = false;
       }
     })
 
     .onEvent("input", function (ev) {
         var gadget = this;
-        var formula = gadget.element.querySelector("input.jexcel_formula");
-        if (ev.target == gadget.element.querySelector("td.highlight-selected input")) {
-          //formula.value = ev.target.value;
+        return gadget.getCurrentSheet()
+        .push(function (sheet) {
+          var formula = sheet.el.querySelector("input.jexcel_formula");
+          var td = sheet.el.querySelector("td.highlight-selected");
+          if (td && ev.target == td.childNodes[0]) {
+            formula.value = ev.target.value;
+          }
+        })
+      }, false, false)
+
+     .onEvent("contextmenu", function (ev) {
+        var gadget = this;
+        if (ev.target.classList[0] === "jexcel_tab_link") {
+          ev.preventDefault();
+          var name = prompt("Sheet name :", ev.target.textContent);
+          ev.target.textContent = name !== null ? name : ev.target.textContent;
+          return gadget.getContent()
+          .push(function () {
+            var tabs = gadget.element.querySelectorAll(".jexcel_tab_link");
+            gadget.state.value.forEach((sheet, i) => {
+              sheet.sheetName = tabs[i].textContent;
+            });
+            gadget.deferNotifyChangeBinded();
+          })
         }
       }, false, false)
+
+    .onEvent("click", function (ev) {
+      var gadget = this;
+      if (ev.target.title === "Delete sheet") {
+        gadget.deferNotifyChangeBinded();
+      }
+    }, false, false);
 
 }(window, rJS, jexcel));
