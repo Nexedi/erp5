db0d2b18b04e7d06493007ce5cb5fd1528ab1b84
Jérome Perrin <jerome@nexedi.com>
2020-06-10 13:24:14 +0900

    pdm: repair converted inventory report listbox
    getConvertedInventoryList is deprectated and use mergeResults which
    does not support brain classes, so the brain.node_title was no longer
    correct.
    Use a simpler approach, same as Resource_getInventoryList ("stock
    browser"), ie: getFutureInventoryList to get all future stocks +
    getCurrentInventory and getAvailableInventory for each brain. It's a bit
    less efficient because we now do 2 getInventory for each line instead of
    3 getInventoryList merged, but that should be enough.
    
    Also:
     - use float fields to format numbers properly
     - activate domain tree for site (instead of showing all nodes)

--- bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/ResourceModule_getConvertedInventoryStat.py
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/ResourceModule_getConvertedInventoryStat.py
@@ -1,1 +1,25 @@
-return context.ResourceModule_getConvertedInventoryList(inventory_list=0, **kw)
+from Products.PythonScripts.standard import Object
+portal = context.getPortalObject()
+
+resource_uid = context.ResourceModule_getSelection().getUidList()
+
+obj = Object(uid="new_")
+obj["node_title"] = ""
+obj["section_title"] = ""
+obj["variation_text"] = ""
+obj["getCurrentInventory"] = portal.portal_simulation.getCurrentInventory(
+    quantity_unit=kw['quantity_unit'],
+    metric_type=kw['metric_type'],
+    resource_uid=resource_uid,
+    selection_domain=kw.get('selection_domain', None))
+obj["getAvailableInventory"] = portal.portal_simulation.getAvailableInventory(
+    quantity_unit=kw['quantity_unit'],
+    resource_uid=resource_uid,
+    metric_type=kw['metric_type'],
+    selection_domain=kw.get('selection_domain', None))
+obj["converted_quantity"] = portal.portal_simulation.getFutureInventory(
+    quantity_unit=kw['quantity_unit'],
+    resource_uid=resource_uid,
+    metric_type=kw['metric_type'],
+    selection_domain=kw.get('selection_domain', None))
+return [obj,]
