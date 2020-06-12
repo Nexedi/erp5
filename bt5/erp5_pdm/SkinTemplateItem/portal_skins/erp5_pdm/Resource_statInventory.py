90160a47bbb710d7987301dfa75cab67c6822206
Jérome Perrin <jerome@nexedi.com>
2020-06-12 20:00:51 +0900

    pdm: allow to select node category in stock browser dialog
    This allows to filter the node by site in new and old interface in
    a consistent way.
    
    The domain tree is also removed for consistency (so that we don't have
    different features on new and old interface)
    
    Old UI tests have also been updated to use more meaningful macro names.

--- bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_statInventory.py
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_statInventory.py
@@ -1,10 +1,16 @@
 from Products.PythonScripts.standard import Object
+inventory_kw = {
+  'selection_domain': selection_domain
+}
+if node_category:
+  inventory_kw['node_category'] = node_category
+
 obj = Object(uid="new_")
 obj["node_title"] = ""
 obj["section_title"] = ""
 obj["variation_text"] = ""
-obj["getCurrentInventory"] = context.getCurrentInventory(selection_domain=kw.get('selection_domain', None))
-obj["getAvailableInventory"] = context.getAvailableInventory(selection_domain=kw.get('selection_domain', None))
-obj["inventory"] = context.getFutureInventory(selection_domain=kw.get('selection_domain', None))
+obj["getCurrentInventory"] = context.getCurrentInventory(**inventory_kw)
+obj["getAvailableInventory"] = context.getAvailableInventory(**inventory_kw)
+obj["inventory"] = context.getFutureInventory(**inventory_kw)
 
 return [obj,]
