8e0a3068ab46f0299349588cc11c3e410fabe611
Jérome Perrin <jerome@nexedi.com>
2020-06-12 20:00:51 +0900

    pdm: allow to select node category in stock browser dialog
    This allows to filter the node by site in new and old interface in
    a consistent way.
    
    The domain tree is also removed for consistency (so that we don't have
    different features on new and old interface)
    
    Old UI tests have also been updated to use more meaningful macro names.

---
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_getFutureInventoryList.py
@@ -0,0 +1,15 @@
+from Products.PythonScripts.standard import Object
+portal = context.getPortalObject()
+
+inventory_kw = {
+    'selection_domain': selection_domain,
+    'group_by_section': False,
+    'group_by_node': True,
+    'group_by_variation': True,
+    'resource_uid': context.getUid(),
+}
+
+if node_category:
+  inventory_kw['node_category'] = node_category
+
+return portal.portal_simulation.getFutureInventoryList(**inventory_kw)
