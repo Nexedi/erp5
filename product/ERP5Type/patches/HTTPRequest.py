554cd154756478f45f2cbbed3eb2a53c2df6d222
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2020-06-16 15:08:30 +0200

    ERP5Type/patches: use the first entry of HTTP_X_FORWARDED_FOR as the source IP address.

--- product/ERP5Type/patches/HTTPRequest.py
+++ product/ERP5Type/patches/HTTPRequest.py
@@ -12,4 +12,11 @@ def __init__(self, *args, **kw):
   import erp5.component
   erp5.component.ref_manager.add_request(self)
 
+  # Frontend-facing proxy is responsible for sanitising
+  # HTTP_X_FORWARDED_FOR, and only trusted accesses should bypass
+  # that proxy. So trust first entry.
+  forwarded_for = self.environ.get('HTTP_X_FORWARDED_FOR', '').split(',', 1)[0].strip()
+  if forwarded_for:
+    self._client_addr = forwarded_for
+
 HTTPRequest.__init__ = __init__
