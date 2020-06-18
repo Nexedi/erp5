bd40e06ae75a890e0214f22314f652e9d0039c0f
Jérome Perrin <jerome@nexedi.com>
2020-06-18 16:15:50 +0900

    web_renderjs_ui: Use same terms as old UI in login pages

--- bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/Base_createBaseLanguage.py
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/Base_createBaseLanguage.py
@@ -30,6 +30,7 @@ param_dict = [
   { 'message': 'Logout', 'translation': 'tuichu', 'language': 'wo'},
   { 'message': 'User', 'translation': 'yonghu', 'language': 'wo'},
   { 'message': 'Username', 'translation': u'turu jëfandikookat', 'language': 'wo'},
+  { 'message': 'Recover your Account', 'translation': u'tëjju barabu këllu jëfandikookat', 'language': 'wo'},
   { 'message': 'Sort Editor', 'translation': 'paixubianyiqi', 'language': 'wo'},
   { 'message': 'Home', 'translation': 'zhuye', 'language': 'wo'},
   { 'message': 'State', 'translation': 'zhuangtai', 'language': 'wo'},
