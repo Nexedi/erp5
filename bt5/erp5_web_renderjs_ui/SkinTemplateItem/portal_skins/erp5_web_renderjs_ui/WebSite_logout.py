c12029762d80ef097d0843b9bfd8d2da060b7597
Jérome Perrin <jerome@nexedi.com>
2020-06-19 12:07:15 +0900

    web_renderjs_ui: support logout with google/facebook logins
    These authentications are using a different cookie.

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSite_logout.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/WebSite_logout.py
@@ -1,10 +1,15 @@
 """
   Default logout handler, overwritten to give website specific portal status message.
 """
+portal = context.getPortalObject()
 REQUEST = context.REQUEST
 if REQUEST.has_key('portal_skin'):
-  context.portal_skins.clearSkinCookie()
+  portal.portal_skins.clearSkinCookie()
 REQUEST.RESPONSE.expireCookie('__ac', path='/')
+if getattr(portal.portal_skins, "erp5_oauth_google_login", None):
+  REQUEST.RESPONSE.expireCookie('__ac_google_hash', path='/')
+if getattr(portal.portal_skins, "erp5_oauth_facebook_login", None):
+  REQUEST.RESPONSE.expireCookie('__ac_facebook_hash', path='/')
 REQUEST.RESPONSE.setHeader('Location', came_from or context.getPermanentURL(context))
 REQUEST.RESPONSE.setStatus(303)
 # REQUEST.RESPONSE.redirect(came_from or context.getPermanentURL(context));
