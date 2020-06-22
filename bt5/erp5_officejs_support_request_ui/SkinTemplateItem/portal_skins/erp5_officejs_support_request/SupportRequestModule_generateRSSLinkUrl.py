3d67e3180b49b66bb713f4778d8d876f10a0936d
Gabriel Monnerat <gabriel@nexedi.com>
2020-06-22 22:39:36 +0000

    erp5_officejs_support_request_ui: New gadget to generate RSS Link and remove it from dashboard
    Having a big "Generate RSS" button on the front page is strange, because the front page button are for common use cases that users are supposed to execute a lot.
    
    For example, makes sense to have a "Submit new Support Request" button, because users are using this app to submit support requests, but generating RSS is very exceptional action, so we wanted to remove it from the front page.
    
    Also, we changed:
    - change the generate_rss_link action in Support Request Module, to directly call SupportRequestModule_generateRSSLinkUrl.py instead of the form.
    - change SupportRequestModule_generateRSSLinkUrl to display the form at the end.
    - Use Base_renderForm like for erp5_crm
    - Put the RSS url in the REQUEST
    - change the gadget field configuration to use the value from the REQUEST (if not found, put None)
    - change the gadget implementation to display the URL in an html5 string field  + a button

---
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/SupportRequestModule_generateRSSLinkUrl.py
@@ -0,0 +1,36 @@
+absolute_url = context.absolute_url()
+request_url = "%s/SupportRequestModule_viewLastSupportRequestListAsRss" % (absolute_url,)
+portal = context.getPortalObject()
+person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+
+if person is None:
+  url = request_url
+else:
+  access_token = None
+  for token_item in portal.portal_catalog(
+    portal_type="Restricted Access Token",
+    default_agent_uid=person.getUid(),
+    validation_state='validated'
+    ):
+    if token_item.getUrlString() == request_url:
+      access_token = token_item
+      reference = access_token.getReference()
+      break
+
+  if access_token is None:
+    access_token = portal.access_token_module.newContent(
+      portal_type="Restricted Access Token",
+      url_string=request_url,
+      url_method="GET",
+    )
+    access_token.setAgentValue(person)
+    reference = access_token.getReference()
+    access_token.validate()
+
+  url = "%s?access_token=%s&access_token_secret=%s" % (
+          request_url,
+          access_token.getId(),
+          reference)
+
+context.REQUEST.form["your_rss_url"] = url
+return context.Base_renderForm('SupportRequestModule_viewGenerateRSSLinkDialog', message='URL generated')
---
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/gadget_generate_rss_link.js.js
@@ -0,0 +1,47 @@
+/*global rJS, window, navigator*/
+/*jslint nomen: true, maxlen:80, indent:2*/
+(function (rJS, window, navigator) {
+  'use strict';
+
+  rJS(window)
+    .declareAcquiredMethod("getTranslationList", "getTranslationList")
+    .declareAcquiredMethod("notifyChange", "notifyChange")
+    .declareMethod('render', function (options) {
+      return this.changeState({
+        rss_url: options.rss_url
+      });
+    })
+    .onStateChange(function () {
+      var gadget = this;
+      return gadget.getDeclaredGadget('text_field')
+        .push(function (text_gadget) {
+          return text_gadget.render({
+            value: gadget.state.rss_url
+          });
+        });
+    })
+    .onEvent('click', function (evt) {
+      var tag_name = evt.target.tagName,
+        gadget = this,
+        button_text;
+
+      if (tag_name !== 'BUTTON') {
+        return;
+      }
+
+      // Disable any button. It must be managed by this gadget
+      evt.preventDefault();
+
+      return gadget.getTranslationList(["Copied"])
+        .push(function (result) {
+          button_text = result[0];
+          return navigator.clipboard.writeText(gadget.state.rss_url);
+        })
+        .push(function () {
+          return gadget.notifyChange({
+            "message": button_text,
+            "status": "success"
+          });
+        });
+    }, false, false);
+}(rJS, window, navigator));
