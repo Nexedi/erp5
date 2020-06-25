41a6146f3eb4afc054c4da652e565e4feb7b94a1
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 14:29:20 +0900

    ERP5Wizard: Archived to erp5-archive repository (ack'ed by @jp): not used anymore and not going to be migrated to ZODB Components.

--- bt5/tiolive_base/SkinTemplateItem/portal_skins/tiolive_base/ERP5Site_redirectToTioLiveChangePasswordDialog.py
+++
@@ -1,5 +0,0 @@
-portal = context.getPortalObject()
-cancel_url = portal.absolute_url()
-return container.REQUEST.RESPONSE.redirect(
-         '%s/ERP5Site_viewChangeAuthenticatedMemberPasswordDialog?cancel_url=%s' 
-           %(portal.ERP5Site_getTioLiveSiteRootUrl(), cancel_url))
