2774db50eabeb4118f05aa20797942fbf9097d86
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 13:41:33 +0900

    TioSafe: Archived unused parts to erp5-archive repository (acked by @jp).
    erp5_tiosafe_core and ERP5TioSafe/{Conduit,Tool,Document} are used by SlapOS.
    Everything else has not be used at all for many years (and Unit Tests have been
    disabled for 7 years now).

--- product/ERP5TioSafe/tests/dump/prestashop/dump_person_sync_11.sql
+++
@@ -1,4 +0,0 @@
-/* ********** Update person ********** */
-UPDATE `ps_customer` SET birthday = '1959-08-03' WHERE id_customer = 1;
-DELETE FROM `ps_address`;
-INSERT INTO `ps_address` (id_address, id_country, id_customer, address1, postcode, city, deleted) VALUES (1, 1, 1, '123 boulevard des Capucines', '72160', 'Stuttgart', 0), (2, 1, 1, '789 avenue des Fleurs', '44001', 'Dortmund', 0);
