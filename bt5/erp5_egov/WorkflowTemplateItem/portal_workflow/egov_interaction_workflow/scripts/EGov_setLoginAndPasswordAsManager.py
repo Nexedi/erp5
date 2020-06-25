e880f83bfff6b968147b033aa9be7655cf2a5395
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 12:37:57 +0900

    eGov: Archived to erp5-archive repository (ack'ed by @jp): not used anymore and not going to be migrated to ZODB Components.

--- bt5/erp5_egov/WorkflowTemplateItem/portal_workflow/egov_interaction_workflow/scripts/EGov_setLoginAndPasswordAsManager.py
+++
@@ -1,3 +0,0 @@
-organisation.setRole('entreprise/siege') #XXX this should not be hardcoded
-organisation.setReference(login)
-organisation.setPassword(password)
--- bt5/erp5_registry_ohada/SkinTemplateItem/portal_skins/erp5_psj_crm/Ticket_updateRelatedEventLocalRole.py
+++
@@ -1,6 +0,0 @@
-portal = context.getPortalObject()
-event_portal_type_list = portal.getPortalEventTypeList()
-
-for i in portal.portal_catalog(portal_type=event_portal_type_list,
-                               follow_up_uid=context.getUid()):
-  i.activate().updateLocalRolesOnSecurityGroups()
