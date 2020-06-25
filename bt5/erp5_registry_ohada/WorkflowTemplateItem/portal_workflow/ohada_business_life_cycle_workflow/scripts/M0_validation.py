fe45f1d5b5810403773a1bbe18ce40a2ae0f3477
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 12:37:57 +0900

    eGov: Archived to erp5-archive repository (acked by @jp): not used anymore and not going to be migrated to ZODB Components.

--- bt5/erp5_registry_ohada/WorkflowTemplateItem/portal_workflow/ohada_business_life_cycle_workflow/scripts/M0_validation.py
+++
@@ -1,5 +0,0 @@
-request_efrom = state_change['object']
-request_efrom.portal_workflow.doActionFor(M0_Scribus,
-                                          'validate_action',
-                                          wf_id='form_validation_workflow',
-                                          comment=comment or '')
