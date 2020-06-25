ed60c2d5978471fda2801c8fadb7f19a2104d6c9
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 13:41:33 +0900

    TioSafe: Archived unused parts to erp5-archive repository (ack'ed by @jp).
    erp5_tiosafe_core and ERP5TioSafe/{Conduit,Tool,Document} are used by SlapOS.
    Everything else has not be used at all for many years (and Unit Tests have been
    disabled for 7 years now).

--- bt5/erp5_tiosafe_document/SkinTemplateItem/portal_skins/erp5_tiosafe_document/DocumentConnector_getPaymentTransactionList.py
+++
@@ -1,1 +0,0 @@
-return context.DocumentConnector_readDocument(reference=reference, table="Payment Transaction", line_id=payment_transaction_id)
--- bt5/erp5_tiosafe_document_cyberplus/SkinTemplateItem/portal_skins/erp5_tiosafe_document_cyberplus/WebServiceRequest_buildSpreadsheetPaymentTransactionBrain.py
+++
@@ -1,9 +0,0 @@
-# country property is used in gid computation of organisation
-# transform it to category as soon as possible
-
-
-brain_list = [brain(context=context,
-                    object_type=context.getDestinationObjectType(),
-                    **parameter_dict),]
-
-return brain_list
