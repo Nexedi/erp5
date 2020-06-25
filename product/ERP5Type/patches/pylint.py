27ab8bd5eb7f0dde1fcf463e08db958673d1f2aa
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-06-25 18:57:39 +0900

    pylint: Fix false positive error `No name 'ElementMaker' in module 'lxml.builder' (no-name-in-module)`.

--- product/ERP5Type/patches/pylint.py
+++ product/ERP5Type/patches/pylint.py
@@ -288,15 +288,22 @@ _what_not_even_god_should_do = []
 register_module_extender(MANAGER, 'AccessControl.PermissionRole',
                          AccessControl_PermissionRole_transform)
 
-## No name 'OOBTree' in module 'BTrees.OOBTree' (no-name-in-module)
-##
-## When the corresponding C Extension (BTrees._Foo) is available, update
-## BTrees.Foo namespace from the C extension, otherwise use Python definitions
-## by dropping the `Py` suffix in BTrees.Foo symbols.
-import BTrees
-import inspect
+## Package dynamically extending the namespace of their modules with C
+## extension symbols
 # astroid/brain/ added dynamically to sys.path by astroid __init__
 from py2gi import _gi_build_stub as build_stub
+def _register_module_extender_from_live_module(module_name, module):
+    def transform():
+        return AstroidBuilder(MANAGER).string_build(build_stub(module))
+    register_module_extender(MANAGER, module_name, transform)
+
+# No name 'OOBTree' in module 'BTrees.OOBTree' (no-name-in-module)
+#
+# When the corresponding C Extension (BTrees._Foo) is available, update
+# BTrees.Foo namespace from the C extension, otherwise use Python definitions
+# by dropping the `Py` suffix in BTrees.Foo symbols.
+import BTrees
+import inspect
 for module_name, module in inspect.getmembers(BTrees, inspect.ismodule):
     if module_name[0] != '_':
         continue
@@ -305,12 +312,20 @@ for module_name, module in inspect.getmembers(BTrees, inspect.ismodule):
     except KeyError:
         continue
     else:
-        def _create_transform_function(module):
-            def transform():
-                return AstroidBuilder(MANAGER).string_build(build_stub(module))
-            return transform
-        register_module_extender(MANAGER, extended_module.__name__,
-                                 _create_transform_function(module))
+        _register_module_extender_from_live_module(extended_module.__name__,
+                                                   module)
+
+# No name 'ElementMaker' in module 'lxml.builder' (no-name-in-module)
+#
+# imp.load_dynamic() on .so file
+import lxml
+import os
+for filename in os.listdir(os.path.dirname(lxml.__file__)):
+    if filename.endswith('.so'):
+        module_name = 'lxml.' + filename[:-3]
+        _register_module_extender_from_live_module(
+            module_name,
+            __import__(module_name, fromlist=[module_name], level=0))
 
 # Properly search for namespace packages: original astroid (as of 1.3.8) only
 # checks at top-level and it doesn't work for Shared.DC.ZRDB (defined in
