37f11810c78bd125768b7e35a6c1cdcb4f40ba16
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-26 17:06:30 +0000

    erp5_wendelin_data_lake_ingestion: new alarm to check data lake datastreams state consistency

---
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/Alarm_checkDataStreamStateConsistency.py
@@ -0,0 +1,12 @@
+# Consistency state alarm that checks and fixes (changes state) that
+# all published Data Sets have their linked Data Stream(s) matching its state.
+
+for data_set in context.portal_catalog(portal_type = "Data Set",
+                               validation_state = "published"):
+  data_ingestion_line_list = context.portal_catalog(
+                             portal_type = "Data Ingestion Line",
+                             aggregate_uid = data_set.getUid())
+  for data_ingestion_line in data_ingestion_line_list:
+    data_stream = data_ingestion_line.getAggregateValue(portal_type = "Data Stream")
+    if data_stream.getValidationState() == "validated":
+      data_stream.publish()
