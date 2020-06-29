62c437306199a7b5b6f2c6bc93c7a710c8ca1c22
Jérome Perrin <jerome@nexedi.com>
2020-06-29 10:42:00 +0900

    configuration_standard: use erp5_configurator_standard_categories
    Instead of using a spreadsheet for categories, use
    erp5_configurator_standard_categories business template.
    
    This business template also configures quantity unit definition in the
    new way and no longer use obsolete way of setting quantity property on
    quantity unit categories.
    
    The included spreadsheet is still used, but all categories except group and
    site have been removed, it now only contain group/my_group and site/main.

--- bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_setupStandardCategory.py
+++ bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_setupStandardCategory.py
@@ -5,3 +5,11 @@ context.setGlobalConfigurationAttr(
 
 configuration_save.addConfigurationItem("Categories Spreadsheet Configurator Item",
                                  configuration_spreadsheet_data = getattr(context, "standard_category.ods").data)
+
+configuration_save.addConfigurationItem(
+    "Standard BT5 Configurator Item",
+    title='erp5_configurator_standard_categories',
+    bt5_id='erp5_configurator_standard_categories',
+    update_catalog=False,
+    install_dependency=True,
+)
