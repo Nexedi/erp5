4a79ba909842b6bd3f8044ac6e6cdab84d80f493
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-29 18:53:27 +0000

    erp5_officejs: common clone action gadget (text editor updated)

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_common/action_clone.js.js
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_common/action_clone.js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, RSVP */
+/*global window, rJS, RSVP, jIO */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS, RSVP) {
+(function (window, rJS, RSVP, jIO) {
   "use strict";
 
   rJS(window)
@@ -10,6 +10,9 @@
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("jio_get", "jio_get")
     .declareAcquiredMethod("jio_post", "jio_post")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod("jio_putAttachment", "jio_putAttachment")
+    .declareAcquiredMethod("jio_allAttachments", "jio_allAttachments")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -38,16 +41,36 @@
         }
       }, gadget = this,
         document = parent_options.doc,
-        property;
-      delete content_dict.dialog_method;
-      for (property in content_dict) {
-        if (content_dict.hasOwnProperty(property)) {
-          document[property] = content_dict[property];
-        }
-      }
-      return gadget.jio_post(document)
-        .push(function (jio_key) {
-          return_submit_dict.notify.message = "Data Updated";
+        all_attachments,
+        promise_list = [],
+        parent_jio_key = parent_options.action_options.jio_key,
+        jio_key;
+      return gadget.jio_allAttachments(parent_jio_key)
+        .push(function (result) {
+          var property;
+          all_attachments = result;
+          return gadget.jio_post(document);
+        })
+        .push(function (result_jio_key) {
+          var attachment_id;
+          jio_key = result_jio_key;
+          for (attachment_id in all_attachments) {
+            if (all_attachments.hasOwnProperty(attachment_id)) {
+              promise_list.push(gadget.jio_getAttachment(parent_jio_key, attachment_id));
+            }
+          }
+          return RSVP.all(promise_list);
+        })
+        .push(function (result_list) {
+          var i;
+          promise_list = [];
+          for (i = 0; i < result_list.length; i += 1) {
+            promise_list.push(gadget.jio_putAttachment(jio_key, Object.keys(all_attachments)[i], result_list[i]));
+          }
+          return RSVP.all(promise_list);
+        })
+        .push(function () {
+          return_submit_dict.notify.message = "Clone Document Created";
           return_submit_dict.notify.status = "success";
           return_submit_dict.redirect.options = {
             jio_key: jio_key,
@@ -64,4 +87,4 @@
         });
     });
 
-}(window, rJS, RSVP));
+}(window, rJS, RSVP, jIO));
