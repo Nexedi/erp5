7133c34fa9bb2dd799c2d3cca79e06c28b606990
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-29 18:53:27 +0000

    erp5_officejs: common clone action gadget (text editor updated)

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_text_editor/WebSection_getTextEditorPrecacheManifestList.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_text_editor/WebSection_getTextEditorPrecacheManifestList.py
@@ -4,10 +4,6 @@ url_list = [
   "gadget_officejs_text_editor.json",
   "officejs_logo_text_editor.png",
   "gadget_officejs_text_editor_router.html",
-
-  #app custom actions
-  "action_texteditor_clone.html",
-  "action_texteditor_clone.js"
 ]
 
 return url_list
