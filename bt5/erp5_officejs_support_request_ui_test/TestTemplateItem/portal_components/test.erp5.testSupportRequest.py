b556f413bff8e4e2c3967c9d4cb2c54826934154
Gabriel Monnerat <gabriel@nexedi.com>
2020-07-03 00:48:23 +0000

    erp5_officejs_support_request_ui_test: Add test to make sure we have all expected actions to access support request web site
    * Check if each portal_type has the expected view action
    * add test to cover all possible attachments in a Support Request

--- bt5/erp5_officejs_support_request_ui_test/TestTemplateItem/portal_components/test.erp5.testSupportRequest.py
+++ bt5/erp5_officejs_support_request_ui_test/TestTemplateItem/portal_components/test.erp5.testSupportRequest.py
@@ -94,6 +94,39 @@ class SupportRequestTestCase(ERP5TypeTestCase, object):
 
 
 class TestSupportRequestCreateNewSupportRequest(SupportRequestTestCase):
+
+  def test_existing_portal_type_action_to_support_request(self):
+    view_categorie_list = ["object_view", "object_jio_view",
+                           "object_web_view", "object_jio_search"]
+    portal_type_list = [
+      p for p in self.portal.getPortalDocumentTypeList() \
+      if p not in ("Sound", "Video", "Web Page", 'Video', 'Web Illustration',
+                   'Web Manifest', 'Web Page', 'Web Script', 'Web Style',
+                   'Web Table')]
+    # Should not happens but we never know
+    assert portal_type_list, portal_type_list
+
+    for portal_type_str in portal_type_list:
+      portal_type = self.portal.portal_types[portal_type_str]
+      action_list = portal_type.objectValues(portal_type="Action Information")
+      filtered_action_list = [action.getReference() \
+        for action in sorted(action_list, key=lambda x: x.getFloatIndex()) \
+        if action.getActionType() in view_categorie_list
+      ]
+      self.assertTrue(any(a in ("preview", "web_view") for a in filtered_action_list),
+                     "missing preview or web_view in {} {}".format(portal_type_str, filtered_action_list))
+
+    for portal_type_str in ["Support Request", "Support Request Module"]:
+      portal_type = self.portal.portal_types[portal_type_str]
+      action_list = portal_type.objectValues(portal_type="Action Information")
+      filtered_action_list = [action.getReference() \
+        for action in sorted(action_list, key=lambda x: x.getFloatIndex()) \
+        if action.getActionType() in view_categorie_list
+      ]
+      self.assertIn("officejs_support_request_view",
+                    filtered_action_list,
+                    "missing officejs_support_request_view in {}".format(portal_type_str))
+
   def test_submit_support_request(self):
     self.getWebSite().SupportRequestModule_createSupportRequest(
         description='<b>Help !!!</b>',
@@ -463,7 +496,7 @@ class TestSupportRequestRSSSOneEvent(SupportRequestRSSTestCase, DefaultTestRSSMi
   def test_RSS_with_token(self):
     self.login(self.user.getUserId())
     # get rss link url
-    self.portal.support_request_module.SupportRequestModule_generateRSSLinkUrl()
+    self.getWebSite().support_request_module.SupportRequestModule_generateRSSLinkUrl()
     restricted_access_url = self.portal.REQUEST.form["your_rss_url"]
     parsed_url = urlparse.urlparse(restricted_access_url)
     restricted_access_url = restricted_access_url.replace(
