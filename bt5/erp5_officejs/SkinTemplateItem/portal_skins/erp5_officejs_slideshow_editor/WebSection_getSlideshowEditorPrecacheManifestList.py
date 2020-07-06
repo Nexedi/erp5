4fa32b030c4de64bda3960cf2c1905bad6dfba1c
Ludovic Kiefer <ludovic.kiefer@nexedi.com>
2020-07-06 09:56:24 +0000

    erp5_officejs: OfficeJ Slideshow Editor application

---
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
@@ -0,0 +1,11 @@
+url_list = [
+  #"renderjs.js",
+  #"rsvp.js",
+  "gadget_officejs_slideshow_editor.configuration",
+  "gadget_officejs_slideshow_editor_router.html",
+  "slideeditor.gadget.html",
+  "slideeditor.gadget.js",
+  "slideeditor.gadget.css"
+]
+
+return url_list
