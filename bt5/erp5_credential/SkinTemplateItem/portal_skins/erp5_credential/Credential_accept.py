f67a0be320a656c4239a06b439b49e99cd10179c
Jérome Perrin <jerome@nexedi.com>
2020-07-07 12:16:44 +0900

    credential: prevent multiple failures
    If a Credential Request/Recovery can not be accepted, the alarm should not
    retry again and again.
    
    We use an hasActivity test before trying to accept to prevent causing multiple
    failing activities (if it already failed once, no need to retry) and maybe some
    race conditions if alarm is too slow to run.

--- bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/Credential_accept.py
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/Credential_accept.py
@@ -1,6 +1,10 @@
 """
 Intent is to ignore accept/accept race conditions but complain about
 any other race condition (ex: accept/reject).
+
+This is intendend to run periodically from an alarm.
 """
+if context.hasActivity():
+  return
 if context.getValidationState() != 'accepted':
   context.accept()
