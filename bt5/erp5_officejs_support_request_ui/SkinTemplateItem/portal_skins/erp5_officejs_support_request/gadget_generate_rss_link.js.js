fc452e2f7a76d4e47bd30cd2994bfacd1c24f2ec
Gabriel Monnerat <gabriel@nexedi.com>
2020-07-07 23:23:57 +0000

    erp5_officejs_support_request_ui: Improve rss gadget
    - change the generate_rss_link action in Support Request Module, to directly call SupportRequestModule_generateRSSLinkUrl.py instead of the form.
    - change SupportRequestModule_generateRSSLinkUrl to display the form at the end.
    - Use Base_renderForm like for erp5_crm
    - Put the RSS url in the REQUEST
    - change the gadget field configuration to use the value from the REQUEST (if not found, put None)
    - change the gadget implementation to display the URL in an html5 string field  + a button

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/gadget_generate_rss_link.js.js
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/gadget_generate_rss_link.js.js
@@ -1,36 +1,47 @@
-/*global rJS, window, navigator, RSVP*/
+/*global rJS, window, navigator*/
 /*jslint nomen: true, maxlen:80, indent:2*/
-(function (rJS, window, navigator, RSVP) {
+(function (rJS, window, navigator) {
   'use strict';
 
   rJS(window)
     .declareAcquiredMethod("getTranslationList", "getTranslationList")
+    .declareAcquiredMethod("notifyChange", "notifyChange")
     .declareMethod('render', function (options) {
       return this.changeState({
         rss_url: options.rss_url
       });
     })
-    .onEvent(
-      'click',
-      function (evt) {
-        var gadget = this,
-          root = this.element,
-          button = evt.target,
-          button_text;
-        evt.preventDefault();
-        return gadget.getTranslationList(["Copied"])
-          .push(function (result) {
-            button_text = result[0];
-            button.classList.remove("ui-icon-copy");
-            return navigator.clipboard.writeText(gadget.state.rss_url);
-          })
-          .push(function () {
-            button.classList.add("ui-icon-check");
-            button.textContent = " " + button_text;
+    .onStateChange(function () {
+      var gadget = this;
+      return gadget.getDeclaredGadget('text_field')
+        .push(function (text_gadget) {
+          return text_gadget.render({
+            value: gadget.state.rss_url
           });
-      },
-      false,
-      false
-    );
+        });
+    })
+    .onEvent('click', function (evt) {
+      var tag_name = evt.target.tagName,
+        gadget = this,
+        button_text;
 
-}(rJS, window, navigator, RSVP));
+      if (tag_name !== 'BUTTON') {
+        return;
+      }
+
+      // Disable any button. It must be managed by this gadget
+      evt.preventDefault();
+
+      return gadget.getTranslationList(["Copied"])
+        .push(function (result) {
+          button_text = result[0];
+          return navigator.clipboard.writeText(gadget.state.rss_url);
+        })
+        .push(function () {
+          return gadget.notifyChange({
+            "message": button_text,
+            "status": "success"
+          });
+        });
+    }, false, false);
+}(rJS, window, navigator));
