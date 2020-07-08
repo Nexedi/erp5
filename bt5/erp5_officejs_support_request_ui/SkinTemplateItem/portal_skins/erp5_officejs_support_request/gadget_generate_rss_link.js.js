3455357ae326079a26514c292f82f5c01f13eaaf
Gabriel Monnerat <gabriel@nexedi.com>
2020-07-08 01:43:09 +0000

    fixup! erp5_officejs_support_request_ui: New gadget to generate RSS Link

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/gadget_generate_rss_link.js.js
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/gadget_generate_rss_link.js.js
@@ -1,37 +1,26 @@
-/*global rJS, window, navigator, RSVP*/
+/*global rJS, window, navigator*/
 /*jslint nomen: true, maxlen:80, indent:2*/
-(function (rJS, window, navigator, RSVP) {
+(function (rJS, window, navigator) {
   'use strict';
 
   rJS(window)
     .declareAcquiredMethod("getTranslationList", "getTranslationList")
-    .declareAcquiredMethod("nofitySubmited", "nofitySubmited")
+    .declareAcquiredMethod("notifySubmit", "notifySubmit")
     .declareMethod('render', function (options) {
       return this.changeState({
         rss_url: options.rss_url
       });
     })
     .declareMethod('getContent', function () {
-      return {"your_rss_url": this.state.rss_url};
-    })
-    .onEvent(
-      'click',
-      function (evt) {
-        var gadget = this,
-          root = this.element,
-          button_text;
-        evt.preventDefault();
-        return gadget.getTranslationList(["Copied"])
-          .push(function (result) {
-            button_text = result[0];
-            return navigator.clipboard.writeText(gadget.state.rss_url);
-          })
-          .push(function () {
-            return gadget.nofitySubmited(button_text);
-          });
-      },
-      false,
-      false
-    );
-
-}(rJS, window, navigator, RSVP));
+      var gadget = this,
+        button_text;
+      return gadget.getTranslationList(["Copied"])
+        .push(function (result) {
+          button_text = result[0];
+          return navigator.clipboard.writeText(gadget.state.rss_url);
+        })
+        .push(function () {
+          return gadget.notifySubmit(button_text);
+        });
+    });
+}(rJS, window, navigator));
