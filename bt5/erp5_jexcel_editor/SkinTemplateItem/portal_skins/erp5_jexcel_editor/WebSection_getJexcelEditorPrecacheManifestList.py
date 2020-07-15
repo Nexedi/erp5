540b0526e4da19819c43cbde282f620883da9ffc
Mikolaï Krol <mikolai.krol@nexedi.com>
2020-07-15 13:29:22 +0000

    erp5_jexcel_editor: jexcel editor source
    erp5_jexcel_editor: all formulas saved
    
    erp5_jexcel_editor: code review correction
    
    erp5_jexcel_editor: changed cursor for color table
    
    erp5_jexcel_editor: reunited the two gadgets
    
    erp5_jexcel_editor: compatibility with sheet names, domsugar for dom generation, change column types in undo/redo history, replace callback functions by jobs, drop setTimeout, changed context menu event
    
    erp5_jexcel_editor: less config attributes in table, moved changing type to context menu
    
    erp5_jexcel_editor: add/delete columns/rows in toolbar

---
+++ bt5/erp5_jexcel_editor/SkinTemplateItem/portal_skins/erp5_jexcel_editor/WebSection_getJexcelEditorPrecacheManifestList.py
@@ -0,0 +1,18 @@
+url_list = [
+  "renderjs.js",
+  "rsvp.js",
+  "jio.js",
+  "domsugar.js",
+  "jsuites/jsuites.js",
+  "jsuites/jsuites.css",
+  "jexcel/jexcel.js",
+  "jexcel/jexcel.css",
+  "jexcel.gadget.html",
+  "jexcel.gadget.js",
+  "complements.css",
+  "fx.png",
+  "fonts.ttf",
+  "icons.css",
+]
+
+return url_list
