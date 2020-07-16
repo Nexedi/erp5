69e1be3c27f259f25ebbb035d6f0ecf9b872949a
Jérome Perrin <jerome@nexedi.com>
2020-07-16 11:46:00 +0900

    test_result: display retried tests

--- bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_statTestResultLineList.py
+++ bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_statTestResultLineList.py
@@ -4,4 +4,6 @@ return [Object(duration=int(context.getProperty('duration', 0)),
                all_tests=context.getProperty('all_tests'),
                errors=context.getProperty('errors'),
                failures=context.getProperty('failures'),
-               skips=context.getProperty('skips'))]
+               skips=context.getProperty('skips'),
+               test_result_retry_count=context.getProperty('test_result_retry_count', 0),
+               )]
