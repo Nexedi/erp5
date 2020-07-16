04c3d16df2268d6ed5c35f62d55345b9190a81c7
Rafael Monnerat <rafael@nexedi.com>
2020-07-16 23:30:59 +0000

    erp5_administration: make dump scripts more flexible
        This allow including more items to be ignored, preventing diffs from pure development.

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpSkinProperty.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpSkinProperty.py
@@ -10,5 +10,8 @@
 skin_tool = context.getPortalObject().portal_skins
 for name, layers in skin_tool.getSkinPaths():
   for layer in layers.split(','):
+    if ignore_skin_folder_list is not None and\
+        layer in ignore_skin_folder_list:
+      continue
     print '%s,%s' % (name, layer)
 return printed
