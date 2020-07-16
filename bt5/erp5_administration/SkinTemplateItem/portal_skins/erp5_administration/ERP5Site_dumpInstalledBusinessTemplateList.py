3d31a30545d85e0a1ba4dcbd6f58f7f898fcfd7c
Rafael Monnerat <rafael@nexedi.com>
2020-07-16 23:30:59 +0000

    erp5_administration: make dump scripts more flexible
        This allow including more items to be ignored, preventing diffs from pure development.

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpInstalledBusinessTemplateList.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpInstalledBusinessTemplateList.py
@@ -1,3 +1,5 @@
+if ignore_business_template_list is None:
+  ignore_business_template_list = []
 for business_template in sorted(context.getPortalObject().portal_templates.contentValues(portal_type='Business Template'),
                   key=lambda x:x.getTitle()):
   if business_template.getInstallationState() == 'installed' and \
