b4d60d051aaf91a45bdd747c92e08a650181f12d
Ivan Tyagov <ivan@nexedi.com>
2020-07-27 14:14:38 +0300

    Add comments.

--- wendelin/bt5/erp5_wendelin/PathTemplateItem/portal_callables/IngestionPolicy_parseSimpleIngestionTag.py
+++ wendelin/bt5/erp5_wendelin/PathTemplateItem/portal_callables/IngestionPolicy_parseSimpleIngestionTag.py
@@ -1,3 +1,11 @@
+"""
+  This script is used in Ingestion Policy objects to parse provided by fluentd / ebulk reference.
+  In this case it expects a reference in this format for example:
+
+  'sensor1.product1' 
+
+"""
+
 sensor_reference = reference.split('.')[0]
 data_product_reference = reference.split('.')[1]
 return {'resource_reference' : data_product_reference,
