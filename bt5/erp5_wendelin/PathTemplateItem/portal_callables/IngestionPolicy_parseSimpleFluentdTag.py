bdc6c780cf4e9cbcf32e10ef84db491ff1b29c90
Ivan Tyagov <ivan@nexedi.com>
2020-07-27 14:14:38 +0300

    Add comments.

--- bt5/erp5_wendelin/PathTemplateItem/portal_callables/IngestionPolicy_parseSimpleFluentdTag.py
+++ bt5/erp5_wendelin/PathTemplateItem/portal_callables/IngestionPolicy_parseSimpleFluentdTag.py
@@ -1,3 +1,19 @@
+"""
+  This script is used in Ingestion Policy objects to parse provided by fluentd / ebulk reference.
+  In this case it expects a reference in this format for example:
+
+  'sensor1' 
+
+  i.e. no special characters and one name only.
+
+  If you need to use more complex hierachical reference notations like:
+
+  'sensor1.product1' 
+
+  you can use script: 'IngestionPolicy_parseSimpleIngestionTag'
+ 
+"""
+
 project_reference = reference.split('.')[0]
 return {'resource_reference' : reference,
         'specialise_reference': project_reference,
