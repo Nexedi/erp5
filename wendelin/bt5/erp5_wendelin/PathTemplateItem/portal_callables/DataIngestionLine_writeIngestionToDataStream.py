4261d8bc74aa5144c33eb0a84a3183c51e238771
Ivan Tyagov <ivan@nexedi.com>
2020-07-28 13:05:38 +0300

    Update description.

--- wendelin/bt5/erp5_wendelin/PathTemplateItem/portal_callables/DataIngestionLine_writeIngestionToDataStream.py
+++ wendelin/bt5/erp5_wendelin/PathTemplateItem/portal_callables/DataIngestionLine_writeIngestionToDataStream.py
@@ -1,7 +1,8 @@
 """
-  This script is used during fluentd ingestion.
-  It will append data sent from fluentd to Wendelin 'as it is' to respective "Data Stream".
-  By default data will be encoded in MsgPack format.
+  This script is a general ingestion script which can be used with fluentd or with other http based ingestion tools.
+  It will append data sent to Wendelin 'as it is' to respective "Data Stream".
+
+  Note that by default fluentd data is encoded in msgpack format, this script will not unpack it.
 """
 
 out_stream["Data Stream"].appendData(data_chunk)
