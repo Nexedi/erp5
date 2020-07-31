9747beb6049eebd6851426734147c2fd60e4f2e6
Romain Courteaud <romain@nexedi.com>
2020-07-31 08:40:21 +0000

    erp5_core: do one catalog call only
    Catalog seems to correctly handle the OR parameters.
    
    Do not lose the possible external `query` parameter.

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getRelatedDocumentList.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_getRelatedDocumentList.py
@@ -1,16 +1,28 @@
 from Products.ZSQLCatalog.SQLCatalog import Query, ComplexQuery
 kw.pop('relative_url', None)
 kw.pop('follow_up_uid', None)
-portal_catalog=context.getPortalObject().portal_catalog
-follow_up_related_document_list = portal_catalog(
-           portal_type=portal_type,
-           follow_up_uid=context.getUid(), **kw)
 
-kw['query'] = Query(relative_url='%s/%%' % context.getRelativeUrl().replace('_', r'\_'))
-if follow_up_related_document_list:
-  kw['query'] = ComplexQuery(
-    kw['query'],
-    Query(uid=[x.getUid() for x in follow_up_related_document_list]),
-    logical_operator='or')
+portal = context.getPortalObject()
+portal_catalog = portal.portal_catalog
 
-return portal_catalog(portal_type=portal_type, limit=limit, **kw)
+if portal_type is None:
+  portal_type = portal.getPortalDocumentTypeList() + portal.getPortalEmbeddedDocumentTypeList()
+
+document_query = ComplexQuery(
+  Query(follow_up_uid=context.getUid()),
+  Query(relative_url='%s/%%' % context.getRelativeUrl().replace('_', r'\_')),
+  logical_operator='or'
+)
+if query is None:
+  query = document_query
+else:
+  query = ComplexQuery(
+    query,
+    document_query,
+    logical_operator='and'
+  )
+
+return portal_catalog(
+  portal_type=portal_type,
+  query=query,
+  **kw)
