ccb9a0f90d67599f15ef12ece7012a657ae55b27
Jérome Perrin <jerome@nexedi.com>
2020-08-07 10:10:43 +0200

    HACK officejs_support_request_ui: include fr locales of momentjs
    We don't want to include a full build of locales, so for now since we
    needs it only for french, we include the localization inline here.
    
    HACK: This needs more thinking about what is the right way. This way
    works for now.

--- bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_pt_form_view_discussable_js.js
+++ bt5/erp5_officejs_support_request_ui/PathTemplateItem/web_page_module/gadget_supportrequest_pt_form_view_discussable_js.js
@@ -5,6 +5,71 @@
   var gadget_klass = rJS(window),
     comment_list_template_source = gadget_klass.__template_element.getElementById("template-document-list").innerHTML
 
+  /**
+   * french locale for momentjs, copied from https://momentjs.com/docs/
+   */
+  moment.locale('fr', {
+      months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+      monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+      monthsParseExact : true,
+      weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+      weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
+      weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+      weekdaysParseExact : true,
+      longDateFormat : {
+          LT : 'HH:mm',
+          LTS : 'HH:mm:ss',
+          L : 'DD/MM/YYYY',
+          LL : 'D MMMM YYYY',
+          LLL : 'D MMMM YYYY HH:mm',
+          LLLL : 'dddd D MMMM YYYY HH:mm'
+      },
+      calendar : {
+          sameDay : '[Aujourd’hui à] LT',
+          nextDay : '[Demain à] LT',
+          nextWeek : 'dddd [à] LT',
+          lastDay : '[Hier à] LT',
+          lastWeek : 'dddd [dernier à] LT',
+          sameElse : 'L'
+      },
+      relativeTime : {
+          future : 'dans %s',
+          past : 'il y a %s',
+          s : 'quelques secondes',
+          m : 'une minute',
+          mm : '%d minutes',
+          h : 'une heure',
+          hh : '%d heures',
+          d : 'un jour',
+          dd : '%d jours',
+          M : 'un mois',
+          MM : '%d mois',
+          y : 'un an',
+          yy : '%d ans'
+      },
+      dayOfMonthOrdinalParse : /\d{1,2}(er|e)/,
+      ordinal : function (number) {
+          return number + (number === 1 ? 'er' : 'e');
+      },
+      meridiemParse : /PD|MD/,
+      isPM : function (input) {
+          return input.charAt(0) === 'M';
+      },
+      // In case the meridiem units are not separated around 12, then implement
+      // this function (look at locale/id.js for an example).
+      // meridiemHour : function (hour, meridiem) {
+      //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+      // },
+      meridiem : function (hours, minutes, isLower) {
+          return hours < 12 ? 'PD' : 'MD';
+      },
+      week : {
+          dow : 1, // Monday is the first day of the week.
+          doy : 4  // Used to determine first week of the year.
+      }
+  });
+
+
   gadget_klass
     /////////////////////////////////////////////////////////////////
     // Acquired methods
