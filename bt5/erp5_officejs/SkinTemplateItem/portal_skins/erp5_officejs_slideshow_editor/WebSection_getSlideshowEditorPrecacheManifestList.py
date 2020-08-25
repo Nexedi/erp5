db6f1a2a10dc11ac4e617daa65192c31097ad08f
Ludovic Kiefer <ludovic.kiefer@nexedi.com>
2020-08-25 10:36:13 +0000

    erp5_officejs: add a new view to edit the slideshow source code

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
@@ -3,7 +3,9 @@ url_list = [
   "gadget_officejs_slideshow_editor_router.html",
   "slideeditor.gadget.html",
   "slideeditor.gadget.js",
-  "slideeditor.gadget.css"
+  "slideeditor.gadget.css",
+  "gadget_erp5_page_tab_officejs.js",
+  "gadget_erp5_page_tab_officejs.html",
 ]
 
 return url_list
