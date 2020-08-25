866de7a63b274cd84a50f1a1cccb13e253ebe886
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-08-25 15:11:53 +0000

    erp5_corporate_identity: code improvement
    1. set 0 as default value, otherwise leaflet_display_side will always be true
    2. show message instead of crash
    3. correct parameters
    4. clean code

--- bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_validateImage.py
+++ bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_validateImage.py
@@ -12,7 +12,6 @@ Upgrade image for the specific type of display
 # img_svg_format                display image as svg (default png/None)
 
 import re
-from zExceptions import NotFound
 
 if img_string is None or img_string == "":
   return img_string
@@ -25,32 +24,26 @@ img_src = img_src[0]
 if img_src.startswith('data:image/'):
   return img_string
 
-img_type = None
-
 # START user ignoring guidelines:
-validation_freebie = None
 new_src = None
 
 # Guideline: no relative links. we cover
 if img_src.startswith("./"):
   new_src = img_src.replace("./", "")
   img_string = img_string.replace(img_src, new_src)
+  img_src = new_src
 
 # Guideline: include format. we cover
-img_src = new_src or img_src
 if img_src.find("format=") == -1:
   if img_src.find("?") == -1:
     new_src = img_src + "?format="
   else:
     new_src = img_src + "&amp;format="
   img_string = img_string.replace(img_src, new_src)
+  img_src = new_src
 
 # Guideline: images must be stored locally, don't complain if your pdf breaks
-img_src = new_src or img_src
-if img_src.startswith("http"):
-  validation_freebie = True
-
-if validation_freebie is None:
+if not img_src.lower().startswith("http"):
 #END user ignoring guidelines
   if img_src[0] == '/':
     img_src = img_src[1:]
@@ -58,21 +51,20 @@ if validation_freebie is None:
 
   # flag broken link until further notice
   if img_obj is None:
-    raise NotFound('The following image could not be found in erp5 OR is not following guidelines for links (eg no ./ prefix): %s' % (img_src.split("?")[0]))
-
-  img_type = img_obj.getContentType()
-
+    return '<p style="color:red">The following image could not be found in erp5 OR is not following guidelines for links (eg no ./ prefix): <span style="font-weight:bold">%s</span></p>' % (img_src.split("?")[0])
   # ensure alt attributes are set
   if img_string.find('alt=') == -1:
     img_string.replace ("src=", 'alt="%s" src=' % img_caption or img_obj.getTitle())
 
+  img_type = img_obj.getContentType()
+
   # force svg display as svg or png
   if img_type == "image/svg+xml":
-    if img_svg_format == "png" or img_svg_format is None:
+    if img_svg_format == "png":
       img_string = img_string.replace('type="image/svg+xml"', '')
       img_string = img_string.replace("type='image/svg+xml'", '')
       img_string = img_string.replace('format=svg', 'format=png')
-    if img_svg_format == "svg":
+    elif img_svg_format == "svg":
       img_string = img_string.replace('src=', 'type="image/svg+xml" src=')
       img_string = img_string.replace('src=', "type='image/svg+xml' src=")
       img_string = img_string.replace('format=png', 'format=svg')
