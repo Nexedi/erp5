02eee71b532c82d6aa288fb2dbabbe801a357ad6
Jérome Perrin <jerome@nexedi.com>
2020-08-28 08:56:35 +0200

    HACK: use translated title in support request graphs
    The gadget will build a string query from these, so depending on the
    translation, this may not work (for example if the translation of a state was
    "O\" OR 1=1", but that should be good enough for English and french

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
@@ -11,10 +11,7 @@ workflow = portal.portal_workflow.ticket_workflow
 
 for state in workflow.getStateValueList():
   state_title = state.title_or_id()
-  if 0:
-    # We don't translate yet, it needs several other fixes
-    # see https://lab.nexedi.com/nexedi/erp5/merge_requests/778
-    state_title = unicode(translateString(
+  state_title = unicode(translateString(
       '%s [state in %s]' % (state_title, workflow.getId()),
       default=unicode(translateString(state_title))))
   info[state.getReference()] = state_title
