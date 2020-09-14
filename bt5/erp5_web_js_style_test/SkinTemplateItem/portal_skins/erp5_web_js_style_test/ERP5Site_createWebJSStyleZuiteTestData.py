664439bd9e737116296b90990acf6e7861735459
Romain Courteaud <romain@nexedi.com>
2020-09-14 09:08:08 +0000

    erp5_web_js_style_test: up

--- bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/ERP5Site_createWebJSStyleZuiteTestData.py
+++ bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/ERP5Site_createWebJSStyleZuiteTestData.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 from DateTime import DateTime
 
 portal = context.getPortalObject()
@@ -6,9 +7,15 @@ web_page_portal_type = "Web Page"
 web_site_portal_type = "Web Site"
 
 web_site_id = "erp5_web_js_style_test_site"
+web_page_frontend_reference = "erp5_web_js_style_test_frontpage"
 web_page_frontend_en_id = "erp5_web_js_style_test_frontpage_en"
 web_page_frontend_fr_id = "erp5_web_js_style_test_frontpage_fr"
 web_page_frontend_zh_id = "erp5_web_js_style_test_frontpage_zh"
+
+web_page_content_reference = "erp5_web_js_style_test_contentpage"
+web_page_content_en_id = "erp5_web_js_style_test_contentpage_en"
+web_page_content_fr_id = "erp5_web_js_style_test_contentpage_fr"
+web_page_content_zh_id = "erp5_web_js_style_test_contentpage_zh"
 
 ### English web page
 module = portal.getDefaultModule(web_page_portal_type)
@@ -17,14 +24,28 @@ if getattr(module, web_page_frontend_en_id, None) is not None:
 web_page = module.newContent(
   portal_type=web_page_portal_type,
   id=web_page_frontend_en_id,
-  reference="erp5_web_js_style_test_frontpage",
+  reference=web_page_frontend_reference,
   language="en",
   version="001",
   text_content="""
-    <p>Frontpage content</p>
+<p>Frontpage content</p>
 
-    <p><a href='TEST-Js.Style.Demo.Content'>TEST-Js.Style.Demo.Content</a></p>
-  """
+<p><a href='%s'>%s</a></p>
+""" % (web_page_content_reference, web_page_content_reference)
+)
+portal.portal_workflow.doActionFor(web_page, 'publish_action')
+
+if getattr(module, web_page_content_en_id, None) is not None:
+  module.manage_delObjects([web_page_content_en_id])
+web_page = module.newContent(
+  portal_type=web_page_portal_type,
+  id=web_page_content_en_id,
+  reference=web_page_content_reference,
+  language="en",
+  version="001",
+  text_content="""
+<p>Subpage content</p>
+"""
 )
 portal.portal_workflow.doActionFor(web_page, 'publish_action')
 
@@ -34,14 +55,28 @@ if getattr(module, web_page_frontend_fr_id, None) is not None:
 web_page = module.newContent(
   portal_type=web_page_portal_type,
   id=web_page_frontend_fr_id,
-  reference="erp5_web_js_style_test_frontpage",
+  reference=web_page_frontend_reference,
   language="fr",
   version="001",
   text_content="""
 <p>Contenu de la page d'accueil</p>
 
-<p><a href='TEST-Js.Style.Demo.Content'>TEST-Js.Style.Demo.Content</a></p>
-  """
+<p><a href='%s'>%s</a></p>
+""" % (web_page_content_reference, web_page_content_reference)
+)
+portal.portal_workflow.doActionFor(web_page, 'publish_action')
+
+if getattr(module, web_page_content_fr_id, None) is not None:
+  module.manage_delObjects([web_page_content_fr_id])
+web_page = module.newContent(
+  portal_type=web_page_portal_type,
+  id=web_page_content_fr_id,
+  reference=web_page_content_reference,
+  language="fr",
+  version="001",
+  text_content="""
+<p>Contenu de la sous page</p>
+"""
 )
 portal.portal_workflow.doActionFor(web_page, 'publish_action')
 
@@ -51,14 +86,28 @@ if getattr(module, web_page_frontend_zh_id, None) is not None:
 web_page = module.newContent(
   portal_type=web_page_portal_type,
   id=web_page_frontend_zh_id,
-  reference="erp5_web_js_style_test_frontpage",
+  reference=web_page_frontend_reference,
   language="zh",
   version="001",
   text_content="""
 <p>主页内容</p>
 
-<p><a href='TEST-Js.Style.Demo.Content'>TEST-Js.Style.Demo.Content</a></p>
-  """
+<p><a href='%s'>%s</a></p>
+""" % (web_page_content_reference, web_page_content_reference)
+)
+portal.portal_workflow.doActionFor(web_page, 'publish_action')
+
+if getattr(module, web_page_content_zh_id, None) is not None:
+  module.manage_delObjects([web_page_content_zh_id])
+web_page = module.newContent(
+  portal_type=web_page_portal_type,
+  id=web_page_content_zh_id,
+  reference=web_page_content_reference,
+  language="zh",
+  version="001",
+  text_content="""
+<p>子页面内容</p>
+"""
 )
 portal.portal_workflow.doActionFor(web_page, 'publish_action')
 
@@ -75,7 +124,7 @@ configuration_dict = {
     'available_language_list': ['en', 'fr', 'zh'],
     'static_language_selection': True,
     'language': "en",
-    'aggregate_value': web_page,
+    'aggregate_value': module.restrictedTraverse(web_page_frontend_en_id),
     'title': "Demo Style With Language",
   }
 }
