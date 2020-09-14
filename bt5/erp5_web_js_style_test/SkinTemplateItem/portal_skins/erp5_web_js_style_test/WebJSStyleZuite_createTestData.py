b1a05c1d81100ef373d8b4f3e5c5d2ddfb4a36fd
Romain Courteaud <romain@nexedi.com>
2020-09-14 08:03:07 +0000

    erp5_web_js_style_test: check that can go from the second to the third language

--- bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/WebJSStyleZuite_createTestData.py
+++ bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/WebJSStyleZuite_createTestData.py
@@ -8,7 +8,9 @@ web_site_portal_type = "Web Site"
 web_site_id = "erp5_web_js_style_test_site"
 web_page_frontend_en_id = "erp5_web_js_style_test_frontpage_en"
 web_page_frontend_fr_id = "erp5_web_js_style_test_frontpage_fr"
+web_page_frontend_zh_id = "erp5_web_js_style_test_frontpage_zh"
 
+### English web page
 module = portal.getDefaultModule(web_page_portal_type)
 if getattr(module, web_page_frontend_en_id, None) is not None:
   module.manage_delObjects([web_page_frontend_en_id])
@@ -26,6 +28,7 @@ web_page = module.newContent(
 )
 portal.portal_workflow.doActionFor(web_page, 'publish_action')
 
+### French web page
 if getattr(module, web_page_frontend_fr_id, None) is not None:
   module.manage_delObjects([web_page_frontend_fr_id])
 web_page = module.newContent(
@@ -42,6 +45,23 @@ web_page = module.newContent(
 )
 portal.portal_workflow.doActionFor(web_page, 'publish_action')
 
+### Chinese web page
+if getattr(module, web_page_frontend_zh_id, None) is not None:
+  module.manage_delObjects([web_page_frontend_zh_id])
+web_page = module.newContent(
+  portal_type=web_page_portal_type,
+  id=web_page_frontend_zh_id,
+  reference="erp5_web_js_style_test_frontpage",
+  language="zh",
+  version="001",
+  text_content="""
+<p>主页内容</p>
+
+<p><a href='TEST-Js.Style.Demo.Content'>TEST-Js.Style.Demo.Content</a></p>
+  """
+)
+portal.portal_workflow.doActionFor(web_page, 'publish_action')
+
 configuration_dict = {
   'nostyle': {
     'title': 'No Style'
@@ -52,7 +72,7 @@ configuration_dict = {
   },
   'language': {
     'configuration_style_gadget_url': "jsstyle_demo.html",
-    'available_language_list': ['en', 'fr'],
+    'available_language_list': ['en', 'fr', 'zh'],
     'static_language_selection': True,
     'language': "en",
     'aggregate_value': web_page,
@@ -60,16 +80,16 @@ configuration_dict = {
   }
 }
 
+### Web site
 module = portal.getDefaultModule(web_site_portal_type)
 if getattr(module, web_site_id, None) is not None:
   module.manage_delObjects([web_site_id])
-web_site = module.newContent(
+module.newContent(
   portal_type=web_site_portal_type,
   id=web_site_id,
   skin_selection_name="Jsstyle",
   layout_configuration_form_id="WebSection_viewJsstylePreference",
   **configuration_dict[configuration]
 )
-# portal.portal_workflow.doActionFor(web_site, 'publish_action')
 
 return "Web Site created."
