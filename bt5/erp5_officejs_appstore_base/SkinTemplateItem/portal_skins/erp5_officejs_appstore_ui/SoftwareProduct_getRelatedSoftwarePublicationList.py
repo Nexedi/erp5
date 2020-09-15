e284a969b990fa09ac8d6d2940daac409a10012a
Romain Courteaud <romain@nexedi.com>
2020-09-15 12:37:45 +0000

    erp5_officejs_appstore_base: handle Software Publication without lines

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_ui/SoftwareProduct_getRelatedSoftwarePublicationList.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_ui/SoftwareProduct_getRelatedSoftwarePublicationList.py
@@ -5,9 +5,11 @@ software_publication_list = portal.portal_catalog(
   **kw
 )
 for software_publication in software_publication_list:
-  software_publication_line = software_publication.objectValues(
+  software_publication_line_list = software_publication.objectValues(
     portal_type="Software Publication Line"
-  )[0]
-  if context.getRelativeUrl() == software_publication_line.getResource():
-    result.append(software_publication)
+  )
+  if software_publication_line_list:
+    software_publication_line = software_publication_line_list[0]
+    if context.getRelativeUrl() == software_publication_line.getResource():
+      result.append(software_publication)
 return result
