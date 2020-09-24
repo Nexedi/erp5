09a1d4baef4e1951e93df0ddb49df7b19c2929fb
Romain Courteaud <romain@nexedi.com>
2020-09-24 13:03:17 +0000

    erp5_web_renderjs_ui: Query and display tools by catalog if developer mode is activated

---
+++ bt5/erp5_web_renderjs_ui/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_makeModuleListQuery.py
@@ -0,0 +1,32 @@
+from Products.ZSQLCatalog.SQLCatalog import ComplexQuery, SimpleQuery
+
+is_developer_mode = (context.getPortalObject().portal_preferences
+                     .getPreferredHtmlStyleDevelopperMode())
+
+simple_query = SimpleQuery(parent_uid=0)
+module_query = ComplexQuery(
+  SimpleQuery(id="%\\_module", comparison_operator="like"),
+  SimpleQuery(meta_type="ERP5 Folder"),
+  logical_operator='AND'
+)
+
+if is_developer_mode:
+  query = ComplexQuery(
+    simple_query,
+    ComplexQuery(
+      module_query,
+      SimpleQuery(id="portal\\_%", comparison_operator="like"),
+      logical_operator='OR'
+    ),
+    logical_operator='AND')
+
+else:
+  query = ComplexQuery(
+    simple_query,
+    module_query,
+    logical_operator='AND')
+
+return ComplexQuery(
+  query,
+  SimpleQuery(id=value, comparison_operator="like"),
+  logical_operator='AND')
