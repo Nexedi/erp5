4f25762ac23589ffd45bd15890f6fa92fdd594b4
Romain Courteaud <romain@nexedi.com>
2020-10-06 14:36:21 +0200

    erp5*: use plural as tool's title

--- product/ERP5/bootstrap/erp5_core/ToolComponentTemplateItem/portal_components/tool.erp5.ContributionTool.py
+++ product/ERP5/bootstrap/erp5_core/ToolComponentTemplateItem/portal_components/tool.erp5.ContributionTool.py
@@ -74,7 +74,7 @@ class ContributionTool(BaseTool):
         some text rewriting is necessary.
 
   """
-  title = 'Contribution Tool'
+  title = 'Contributions'
   id = 'portal_contributions'
   meta_type = 'ERP5 Contribution Tool'
   portal_type = 'Contribution Tool'
