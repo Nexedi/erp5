e70f6041849eb7217f5caa1acc19ebda396c641c
Jérome Perrin <jerome@nexedi.com>
2020-10-13 10:16:03 +0200

    content_translation: Also index translations from message catalogs
    Now that we are able to search using content translation, we also want
    to extend this so that columns like "group__title" can be searchable in listboxs,
    but categories are usually translated with `erp5_content`, which was not indexed
    in content_translation table.
    
    Extend content translation indexing to also support translations coming from
    Localizer catalog make this possible.
    
    The only limitation I could think of is that categories will need to be reindexed
    when the messages are modified in catalog.

--- bt5/erp5_content_translation/TestTemplateItem/portal_components/test.erp5.testTranslatedRelatedKeys.py
+++ bt5/erp5_content_translation/TestTemplateItem/portal_components/test.erp5.testTranslatedRelatedKeys.py
@@ -45,16 +45,36 @@ class TestTranslatedRelatedKeys(ERP5TypeTestCase):
     #  * title as "content translated properties" on Organisation
     #  * short_title as "content translated properties" on Organisation (because it contain a _,
     #    to make sure we are supporting properties with _)
+    #  * some "group" categories and their translations in erp5_content
     #  * two organisations
     self.portal.portal_types.Organisation.setTranslationDomain(
         prop_name='title', domain='content_translation')
     self.portal.portal_types.Organisation.setTranslationDomain(
         prop_name='short_title', domain='content_translation')
 
+    # translations for categories
+    message_catalog = self.portal.Localizer.erp5_content
+    message_catalog.add_language('fr')
+    message_catalog.add_language('ja')
+    message_catalog.gettext('Nexedi', add=True)
+    message_catalog.gettext('Another', add=True)
+    message_catalog.message_edit('Nexedi', 'fr', u'Catégorie Nexedi', '')
+    message_catalog.message_edit('Another', 'fr', u'Autre Catégorie', '')
+    message_catalog.message_edit('Nexedi', 'ja', u'ネクセディカテゴリー', '')
+    message_catalog.message_edit('Another', 'ja', u'別カテゴリー', '')
+
+    # categories (which will be translated using erp5_content)
+    group_base_category = self.portal.portal_categories.group
+    if 'nexedi' not in group_base_category.objectIds():
+      group_base_category.newContent(id='nexedi', title='Nexedi')
+    if 'another' not in group_base_category.objectIds():
+      group_base_category.newContent(id='another', title='Another')
+
     self.organisation_nexedi = self.portal.organisation_module.newContent(
         portal_type='Organisation',
         title='Nexedi',
         short_title='Nexedi SA',
+        group_value=group_base_category.nexedi,
     )
     self.organisation_nexedi.setFrTranslatedTitle("Nexedi")
     self.organisation_nexedi.setFrTranslatedShortTitle("Nexedi Société Anonyme")
@@ -63,6 +83,7 @@ class TestTranslatedRelatedKeys(ERP5TypeTestCase):
     self.organisation_another = self.portal.organisation_module.newContent(
         portal_type='Organisation',
         title='Another not translated Organisation',
+        group_value=group_base_category.another,
     )
     self.tic()
 
@@ -204,3 +225,35 @@ class TestTranslatedRelatedKeys(ERP5TypeTestCase):
               'Nexedi',
               'ネクセディ',
           ])
+
+  def test_erp5_content_search_folder(self):
+    localizer = self.portal.Localizer
+    with mock.patch.object(
+        localizer,
+        'get_selected_language',
+        return_value='ja',
+    ):
+      self.assertEqual(
+          [
+              x.getObject()
+              for x in self.portal.organisation_module.searchFolder(
+                  uid=(
+                      self.organisation_nexedi.getUid(),
+                      self.organisation_another.getUid()),
+                  group__translated__title='ネクセディカテゴリー')
+          ], [self.organisation_nexedi])
+
+    with mock.patch.object(
+        localizer,
+        'get_selected_language',
+        return_value='fr',
+    ):
+      self.assertEqual(
+          [
+              x.getObject()
+              for x in self.portal.organisation_module.searchFolder(
+                  uid=(
+                      self.organisation_nexedi.getUid(),
+                      self.organisation_another.getUid()),
+                  group__translated__title='Catégorie Nexedi')
+          ], [self.organisation_nexedi])
