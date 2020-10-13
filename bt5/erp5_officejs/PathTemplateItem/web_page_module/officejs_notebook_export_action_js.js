a429c77ea3a4bdfc1745eb6d9a4ce9c6237b64af
Roque Porchetto <roque.porchetto@nexedi.com>
2020-10-13 15:31:46 +0000

    erp5_officejs: notebook pdf export - drop html2pdf for now - directly print the editor contento with paged.js

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/officejs_notebook_export_action_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/officejs_notebook_export_action_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, RSVP, jIO, Blob */
+/*global window, document, rJS, RSVP */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
-(function (window, rJS, RSVP, jIO, Blob) {
+(function (window, document, rJS, RSVP) {
   "use strict";
 
   rJS(window)
@@ -34,10 +34,22 @@
           return erp5_form_gadget.getDeclaredGadget("text_content");
         })
         .push(function (result) {
-          var html_data = result.element
+          var print_preview_window,
+              pagedjs_style = document.createElement('link'),
+              pagedjs_script = document.createElement('script'),
+              html_data = result.element
             .querySelector('[data-gadget-scope="editor"]').firstChild
             .contentDocument.body.firstChild.contentDocument.firstChild;
-          return html2pdf(html_data);
+          pagedjs_style.setAttribute('type', 'text/css');
+          pagedjs_style.setAttribute('href', 'interface.css');
+          pagedjs_script.setAttribute('src', 'paged.polyfill.js');
+          pagedjs_script.setAttribute('id', 'paged-js-source');
+          print_preview_window = window.open('', '', 'height=400,width=800');
+          print_preview_window.document.write(html_data.innerHTML);
+          print_preview_window.document.head.appendChild(pagedjs_style);
+          print_preview_window.document.head.appendChild(pagedjs_script);
+          print_preview_window.document.close();
+          print_preview_window.print();
         })
         .push(function () {
           return_submit_dict.redirect = {
@@ -65,4 +77,4 @@
         });
     });
 
-}(window, rJS, RSVP, jIO, Blob));
+}(window, document, rJS, RSVP));
