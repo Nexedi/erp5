1fe57553fdcc8aac01819d0061150b816e9af633
Roque Porchetto <roque.porchetto@nexedi.com>
2020-11-02 19:06:15 +0000

    erp5_officejs: notebook print action uses Paged.js and html2pdf

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/officejs_notebook_export_action_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/officejs_notebook_export_action_js.js
@@ -22,8 +22,7 @@
     .declareMethod('handleSubmit', function (content_dict, parent_options) {
       var gadget = this, html_data,
         parent_gadget = parent_options.gadget,
-        return_submit_dict = {},
-        pdfReactor = new PDFreactor("https://cloud.pdfreactor.com/service/rest");
+        return_submit_dict = {};
       return parent_gadget.getDeclaredGadget("fg")
         .push(function (subgadget) {
           return subgadget.getDeclaredGadget("erp5_pt_gadget");
@@ -35,10 +34,7 @@
           return erp5_form_gadget.getDeclaredGadget("text_content");
         })
         .push(function (result) {
-          var print_preview_window,
-            pagedjs_style = document.createElement('link'),
-            pagedjs_script = document.createElement('script'),
-            html_data = result.element
+          var html_data = result.element
               .querySelector('[data-gadget-scope="editor"]').firstChild
               .contentDocument.body.firstChild.contentDocument.firstChild,
             notebook_execution_done = html_data
@@ -51,40 +47,12 @@
             return return_submit_dict;
           }
           return new RSVP.Queue()
-            //PAGED.JS
-            /*.push(function () {
-              pagedjs_style.setAttribute('type', 'text/css');
-              pagedjs_style.setAttribute('href', 'interface.css');
-              pagedjs_script.setAttribute('src', 'paged.polyfill.js');
-              pagedjs_script.setAttribute('id', 'paged-js-source');
-              print_preview_window = window.open('', '', 'height=400,width=800');
-              print_preview_window.document.write(html_data.innerHTML);
-              print_preview_window.document.head.appendChild(pagedjs_style);
-              print_preview_window.document.head.appendChild(pagedjs_script);
-              print_preview_window.document.close();
-              print_preview_window.print();
-            })*/
-            //HTML2PDF
             .push(function () {
-              //var element = document.getElementById('printable_id');
-              console.log("html_data", html_data);
-              console.log("html_data.head", html_data.head);
+              //converts the html content to a pdf file
+              //and opens a file download window
               return html2pdf(html_data.innerHTML);
             })
             .push(function () {
-              return {};
-            })
-            //PDFREACTOR
-            /*.push(function () {
-              return pdfReactor.convert({
-                document: "<html>" + html_data.innerHTML + "</html>"
-              });
-            })
-            .push(function (result) {
-              var download_pdf = document.createElement('a');
-              download_pdf.href = "data:application/pdf;base64," + result.document;
-              download_pdf.download = "my_pdf.pdf";
-              download_pdf.click();
               return_submit_dict.redirect = {
                 command: 'display',
                 options: {
@@ -93,7 +61,7 @@
                 }
               };
               return return_submit_dict;
-            })*/
+            })
             .push(undefined, function (error) {
               console.log("ERROR:", error);
               return_submit_dict.notify = {
@@ -110,21 +78,6 @@
               return return_submit_dict;
             });
         });
-        /*.push(undefined, function (error) {
-          console.log("ERROR:", error);
-          return_submit_dict.notify = {
-            message: "Failure exporting document",
-            status: "error"
-          };
-          return_submit_dict.redirect = {
-            command: 'display',
-            options: {
-              jio_key: parent_options.jio_key,
-              editable: true
-            }
-          };
-          return return_submit_dict;
-        });*/
     });
 
 }(window, document, rJS, RSVP, console));
