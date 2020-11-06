e862fbab1cc93bd59f481dc9d5f23d95fcd3aaf6
Roque Porchetto <roque.porchetto@nexedi.com>
2020-11-06 14:41:27 +0000

    erp5_officejs: Notebook Download HTML action

---
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/officejs_notebook_download_html_action_js.js
@@ -0,0 +1,110 @@
+/*global window, document, rJS, RSVP, Blob, console */
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, document, rJS, RSVP, Blob, console) {
+  "use strict";
+
+  function downloadHTML(gadget, html_content, title) {
+    var element = gadget.element,
+      a = window.document.createElement("a"),
+      url = window.URL.createObjectURL(new Blob([html_content], {type: 'text/plain'})),
+      name_list = [title, "html"];
+    element.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = name_list.join('.');
+    a.click();
+    element.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  function removeScripts(head_inner_html) {
+    var div = document.createElement('div'), script_list, i;
+    div.innerHTML = head_inner_html;
+    script_list = div.getElementsByTagName('script');
+    i = script_list.length;
+    while (i--) {
+      script_list[i].parentNode.removeChild(script_list[i]);
+    }
+    return div.innerHTML;
+  }
+
+  rJS(window)
+
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareAcquiredMethod("jio_get", "jio_get")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+
+    .declareMethod('preRenderDocument', function (parent_options) {
+      return this.jio_get(parent_options.jio_key);
+    })
+
+    .declareMethod('handleSubmit', function (content_dict, parent_options) {
+      var gadget = this, notebook_html, notebook_editor_iframe,
+        parent_gadget = parent_options.gadget,
+        return_submit_dict = {};
+      return_submit_dict.redirect = {
+        command: 'display',
+        options: {
+          jio_key: parent_options.action_options.jio_key,
+          editable: true
+        }
+      };
+      return parent_gadget.getDeclaredGadget("fg")
+        .push(function (subgadget) {
+          return subgadget.getDeclaredGadget("erp5_pt_gadget");
+        })
+        .push(function (subgadget) {
+          return subgadget.getDeclaredGadget("erp5_form");
+        })
+        .push(function (erp5_form_gadget) {
+          return erp5_form_gadget.getDeclaredGadget("text_content");
+        })
+        .push(function (result) {
+          notebook_editor_iframe = result.element
+            .querySelector('[data-gadget-scope="editor"]').firstChild
+            .contentDocument.body.firstChild;
+          if (notebook_editor_iframe.tagName !== "IFRAME") {
+            return_submit_dict.notify = {
+              message: "Wait until the notebook is fully executed",
+              status: "error"
+            };
+            delete return_submit_dict.redirect;
+            return return_submit_dict;
+          }
+          notebook_html = notebook_editor_iframe.contentDocument.firstChild;
+          //remove all notebook scripts from header as they may cause issues
+          notebook_html.firstChild.innerHTML =
+            removeScripts(notebook_html.firstChild.innerHTML);
+          return new RSVP.Queue()
+            .push(function () {
+              return downloadHTML(gadget, notebook_html.innerHTML,
+                                  parent_options.doc.title);
+            })
+            .push(function () {
+              return return_submit_dict;
+            })
+            .push(undefined, function (error) {
+              console.log("ERROR:", error);
+              return_submit_dict.notify = {
+                message: "Failure exporting document",
+                status: "error"
+              };
+              return_submit_dict.redirect = {
+                command: 'display',
+                options: {
+                  jio_key: parent_options.action_options.jio_key,
+                  editable: true
+                }
+              };
+              return return_submit_dict;
+            });
+        });
+    });
+
+}(window, document, rJS, RSVP, Blob, console));
