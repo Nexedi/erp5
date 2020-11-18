1eb79c5adba5ea167f2136cb733e56fd35d1415f
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-11-18 11:35:37 +0900

    WIP: ERP5Workflow: DC Workflows are now ERP5 object and migrated to ERP5Workflow.
    Work done by Wenjie Zheng, Isabelle Vallet, Sebastien Robin and myself.

---
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/WorkflowTool_convertWorkflow.py
@@ -0,0 +1,38 @@
+from Products.PythonScripts.standard import url_quote
+
+workflow_list = kw.get('workflow_list')
+selected_workflow_list = []
+selected_workflow_id_list = []
+
+if batch_mode == False and workflow_id_list is None:
+  if workflow_list:
+    for workflow in workflow_list:
+      if workflow.get('listbox_selected'):
+        selected_workflow_list.append(context.restrictedTraverse(workflow.get('listbox_key')))
+
+  if len(selected_workflow_list) == 0:
+    return context.Base_redirect(
+        'WorkflowTool_viewWorkflowConversion',
+        keep_items=dict(portal_status_message='No Workflow Selected.'))
+else:
+  for workflow_id in workflow_id_list:
+    selected_workflow_list.append(getattr(context, workflow_id))
+
+for workflow in selected_workflow_list:
+  if workflow is not None and not workflow.isTempObject() and workflow.getPortalType() in ('Workflow', 'Interaction Workflow'):
+    return context.Base_redirect(
+        'WorkflowTool_viewWorkflowConversion',
+        keep_items=dict(portal_status_message='Workflow(s) already exist.'))
+
+  # conversion and reassignment
+  new_workflow = workflow.convertToERP5Workflow(temp_object=False)
+  context.reassignWorkflow(new_workflow.getId())
+  selected_workflow_id_list.append(new_workflow.getId())
+
+if batch_mode:
+  return
+
+return context.Base_redirect(
+    'view',
+     keep_items=dict(portal_status_message="Workflows converted: %s" %
+                     ' '.join(selected_workflow_id_list)))
