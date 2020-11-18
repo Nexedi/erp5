159b081722265cfd07199dbd2a841ee5c1db04eb
Vincent Pelletier <vincent@nexedi.com>
2020-11-18 09:52:41 +0100

    erp5_web_renderjs_ui: Add minimal oauth2 token support.
    This should evolve to become a browser-side OAuth2 client.

---
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui/login_form.py
@@ -0,0 +1,23 @@
+# Short-circuit old (pre-oauth2) web-mode "login_form"s
+import urllib
+web_section_value = context.getWebSectionValue()
+client_id = context.getPortalObject().ERP5Site_getOAuth2ClientConnectorClientId(
+  connector_id=(
+    None
+    if web_section_value is None else
+    web_section_value.getLayoutProperty('configuration_oauth2_client_connector_id', default=None)
+  ),
+)
+if client_id is None:
+  # BBB: OAuth2 is not enabled
+  return context.login_once_form(has_oauth2=False)
+if came_from:
+  # Make the user go through WebSite_login after authentication, so it does its url de-templatification magic
+  came_from = context.absolute_url() + '/WebSite_login?' + urllib.urlencode((('came_from', came_from), ))
+return context.skinSuper('erp5_web_renderjs_ui', script.id)(
+  REQUEST=REQUEST,
+  RESPONSE=RESPONSE,
+  client_id=client_id,
+  came_from=came_from,
+  portal_status_message=portal_status_message,
+)
