a3b0cd2ec6aae7c43c8f83a9a4a4c6f490f19738
Romain Courteaud <romain@nexedi.com>
2020-11-23 14:41:39 +0000

    erp5_officejs: WIP try to show the slide

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
@@ -3,7 +3,13 @@ url_list = [
   "gadget_officejs_slideshow_editor_router.html",
   "slideeditor.gadget.html",
   "slideeditor.gadget.js",
-  "slideeditor.gadget.css"
+  "slideeditor.gadget.css",
+  "slideviewer.gadget.html",
+  "slideviewer.gadget.js",
+  "reveal.js",
+  "zoom.js",
+  "reveal.css",
+  "reveal_theme_white.css"
 ]
 
 return url_list
