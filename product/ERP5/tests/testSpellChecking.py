4abe8598ad531dcfd9cd2cea01e3db2c1fdd5738
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2020-11-30 15:19:56 +0900

    RFC: Merge erp5_workflow into erp5_core.
    Ignore for now the differences between ERP5Workflow implemented for Configurator
    a long time ago and "new" ERP5 Workflows.
    
    This commit is going to be squashed before merging to master.

--- product/ERP5/tests/testSpellChecking.py
+++ product/ERP5/tests/testSpellChecking.py
@@ -74,7 +74,6 @@ class TestSpellChecking(ERP5TypeTestCase):
             'erp5_accounting',
             'erp5_pdm',
             'erp5_trade',
-            'erp5_workflow',
             'erp5_jquery',
             'erp5_jquery_ui',
             'erp5_web',
