0ea80f129d7ff783ced1db0508513d52d40bafa7
Aurel <aurel@nexedi.com>
2020-12-03 10:41:11 +0100

    ZServer (imported by TimerServer) does not support py3, skip it for now

--- product/ERP5Type/Timeout.py
+++ product/ERP5Type/Timeout.py
@@ -30,7 +30,7 @@ import threading
 import time
 from AccessControl.SecurityInfo import ModuleSecurityInfo
 from ZPublisher.HTTPResponse import status_codes
-from Products.TimerService.timerserver.TimerServer import TimerRequest
+#from Products.TimerService.timerserver.TimerServer import TimerRequest
 
 __all__ = (
   'TimeoutReachedError', 'Deadline', 'getDeadline', 'getTimeLeft',
