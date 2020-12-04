6edb077a5ef8fe81105d24fd0f3ecf4b18fa4f93
Romain Courteaud <romain@nexedi.com>
2020-12-04 11:12:36 +0100

    erp5_forge: start to break vcs for erp5js
    erp5_force: break by dropping the custom page template
    
    erp5_forge: drop vcs_dialog
    
    erp5_forge: use Base_redirect
    
    erp5_forge: use formulator to propagate the values
    
    erp5_forge drop some fields from changelog form
    
    erp5_forge:
    
    erp5_forge: stop accessing the request to pass arguments
    
    erp5_forge: commit from xhtml_style and from erp5js
    
    erp5_forge: remove comment
    
    erp5_forge: drop useless comment
    
    erp5_forge: useless import
    
    erp5_forge: revert & update action
    
    erp5_forge: handle SVN exception
    
    erp5_forge: drop log

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doCreateWorkingCopy.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doCreateWorkingCopy.py
@@ -1,8 +1,9 @@
-from ZTUtils import make_query
+from Products.ERP5Type.Message import translateString
+
 form_results = context.BusinessTemplate_viewCreateWorkingCopy.validate_all(REQUEST)
 working_copy = form_results['your_repository']
 context.getVcsTool(path=working_copy).createBusinessTemplateWorkingCopy()
 
-query_string = make_query(portal_status_message='Business Template Working Copy created')
-REQUEST.response.redirect('%s/BusinessTemplate_viewVcsStatus?%s' % 
-                          (context.absolute_url_path(), query_string))
+return context.Base_redirect('BusinessTemplate_viewVcsStatus', keep_items=dict(
+  portal_status_message=translateString('Business Template Working Copy created')
+))
