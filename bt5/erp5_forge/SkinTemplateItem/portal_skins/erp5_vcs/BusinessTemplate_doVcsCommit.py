55a88306b0928a8af1a42c4042d4056ab6162b54
Romain Courteaud <romain@nexedi.com>
2020-12-07 15:06:55 +0000

    erp5_forge:

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsCommit.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsCommit.py
@@ -11,11 +11,12 @@ changelog = request.get('changelog', '')
 if not changelog.strip():
   from Products.ERP5Type.Message import translateString
   error_msg = "Please set a ChangeLog message."
-  request.set('portal_status_message', translateString(error_msg))
-  request.set('cancel_url', context.absolute_url() +
-    '/BusinessTemplate_viewVcsStatus?do_extract:int=0'
-    '&portal_status_message=Commit%20cancelled.')
-  return context.asContext(**kw).Base_renderForm('BusinessTemplate_viewVcsChangelog')
+  return context.asContext(**kw).Base_renderForm('BusinessTemplate_viewVcsChangelog', keep_items={
+    'portal_status_message': translateString(error_msg),
+    'cancel_url': context.absolute_url() +
+      '/BusinessTemplate_viewVcsStatus?do_extract:int=0'
+      '&portal_status_message=Commit%20cancelled.'
+  })
 
 try:
   return context.getVcsTool().commit(changelog, **kw)
