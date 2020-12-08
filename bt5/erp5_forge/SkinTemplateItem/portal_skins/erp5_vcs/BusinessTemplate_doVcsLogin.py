b2d6c14633e7a1b069b23aec6c5f08a834ddef09
Romain Courteaud <romain@nexedi.com>
2020-12-08 15:58:48 +0000

    erp5_forge: commit from xhtml_style and from erp5js

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsLogin.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsLogin.py
@@ -1,5 +1,6 @@
-if caller_kw is None:
-  caller_kw = {}
 context.getVcsTool().setLogin(auth, user, password)
 
-return context.restrictedTraverse(caller)(**caller_kw)
+import json
+commit_dict = json.loads(commit_json)
+context.log('BusinessTemplate_doVcsLogin\ndict %s' % str(commit_dict))
+return context.restrictedTraverse(commit_dict['caller'].encode())(commit_json=commit_json)# XXX**commit_dict['caller_kw'])
