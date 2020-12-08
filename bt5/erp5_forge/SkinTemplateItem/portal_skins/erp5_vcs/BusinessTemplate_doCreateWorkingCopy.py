32935b2673f585cfebe2b98906d9995b1bff2d13
Romain Courteaud <romain@nexedi.com>
2020-12-08 16:59:49 +0000

    erp5_forge: create working copy from erp5js

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doCreateWorkingCopy.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doCreateWorkingCopy.py
@@ -1,9 +1,8 @@
 from Products.ERP5Type.Message import translateString
 
-form_results = context.BusinessTemplate_viewCreateWorkingCopy.validate_all(REQUEST)
-working_copy = form_results['your_repository']
+working_copy = repository
 context.getVcsTool(path=working_copy).createBusinessTemplateWorkingCopy()
 
-return context.Base_redirect('BusinessTemplate_viewVcsStatus', keep_items=dict(
+return context.Base_redirect('view', keep_items=dict(
   portal_status_message=translateString('Business Template Working Copy created')
 ))
