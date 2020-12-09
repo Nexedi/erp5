38f7b3c03a135a9ad9e230d18661af3846dba156
Romain Courteaud <romain@nexedi.com>
2020-12-09 09:27:35 +0000

    erp5_forge: drop useless comment

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsLogin.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsLogin.py
@@ -2,5 +2,4 @@ context.getVcsTool().setLogin(auth, user, password)
 
 import json
 commit_dict = json.loads(commit_json)
-context.log('BusinessTemplate_doVcsLogin\ndict %s' % str(commit_dict))
-return context.restrictedTraverse(commit_dict['caller'].encode())(commit_json=commit_json)# XXX**commit_dict['caller_kw'])
+return context.restrictedTraverse(commit_dict['caller'].encode())(commit_json=commit_json)
