9e872ac3777c786c1c756b46a6ffaf9bd58251b9
Jérome Perrin <jerome@nexedi.com>
2020-12-09 01:23:41 +0100

    crm: remove obsolete and misnamed Entity_addEventDialog
    This was no longer used and replaced by Entity_addEventDialog since
    fbd062eb4e (Allow creation of events from any document, 2011-02-25)

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Entity_addEvent.py
+++
@@ -1,3 +0,0 @@
-from erp5.component.module.Log import log
-log('Obsoleted, please use Base_addEvent (with Base_viewAddEventDialog) instead')
-return context.Base_addEvent(title, direction, portal_type, resource, **kw)
