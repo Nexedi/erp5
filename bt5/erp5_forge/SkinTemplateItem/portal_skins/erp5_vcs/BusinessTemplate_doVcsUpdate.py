3b7784e42bc8d5e8e3f7782b654d4f320b067508
Romain Courteaud <romain@nexedi.com>
2020-12-10 11:00:37 +0000

    erp5_forge: handle SVN exception

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsUpdate.py
+++ bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doVcsUpdate.py
@@ -1,11 +1,19 @@
+import json
+commit_dict = json.loads(commit_json) if commit_json is not None else {
+  'keep': keep
+}
+
+if keep:
+  commit_dict['keep'] = keep
+
 try:
-  new_bt = context.getVcsTool().update(keep)
+  new_bt = context.getVcsTool().update(commit_dict['keep'])
 except Exception, error:
   return context.BusinessTemplate_handleException(
-    error, script.id, form_id=form_id, keep=keep)
+    error, script.id, commit_dict)
 
 return new_bt.Base_redirect('BusinessTemplate_viewInstallationDialog', keep_items={
   'portal_status_message': 'Working copy updated successfully.',
   'workflow_action': 'install_action',
-  'form_id': form_id
+  'form_id': 'view'
 })
