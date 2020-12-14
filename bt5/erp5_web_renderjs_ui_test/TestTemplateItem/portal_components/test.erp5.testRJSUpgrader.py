d80b1d9de02a7587fa9b141e1206b4a5e80839e4
Jérome Perrin <jerome@nexedi.com>
2020-12-14 06:15:55 +0100

    web_renderjs_ui: use lxml to extract data-i18n messages
    The previous regular expression based approach sometimes could not extract
    message properly. Using xml parser simplify code and fix several messages
    that were not extracted properly, like messages containing ", [] or {}
    
    This also fix some problems when looking for messages sources:
      - archived web pages were sometimes used instead of published ones
      - messages from gadgets implemented as page templates/OFS files were not
        extracted.
    
    A few more unit tests for the scripts involved in this process are added.

--- bt5/erp5_web_renderjs_ui_test/TestTemplateItem/portal_components/test.erp5.testRJSUpgrader.py
+++ bt5/erp5_web_renderjs_ui_test/TestTemplateItem/portal_components/test.erp5.testRJSUpgrader.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 ##############################################################################
 #
 # Copyright (c) 2018 Nexedi SA and Contributors. All Rights Reserved.
@@ -27,12 +28,11 @@
 import textwrap
 import time
 
+from Products.ERP5Type.tests.utils import createZODBPythonScript
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
 
 
-class TestRenderJSUpgrade(ERP5TypeTestCase):
-  """Test Upgrader scripts for renderjs UI.
-  """
+class RenderJSUpgradeTestCase(ERP5TypeTestCase):
   def afterSetUp(self):
     self.login()
     self.web_site = self.portal.web_site_module.newContent(
@@ -57,6 +57,10 @@ class TestRenderJSUpgrade(ERP5TypeTestCase):
         reference='{}.js'.format(self.id()))
     self.javascript.publish()
 
+
+class TestRenderJSUpgrade(RenderJSUpgradeTestCase):
+  """Test Upgrader scripts for renderjs UI.
+  """
   def test_upgrade_empty_site(self):
     self.assertEqual([], self.web_site.checkConsistency())
     self.assertEqual([], self.web_site.fixConsistency())
@@ -179,3 +183,178 @@ class TestRenderJSUpgrade(ERP5TypeTestCase):
     self.assertGreater(
         test_upgrade_site_translation_data_js.getModificationDate(),
         test_upgrade_site_translation_data_js_modification_date)
+
+
+class TestRenderUpdateTranslationData(RenderJSUpgradeTestCase):
+  """Tests for "Update Translation Data" utilities on RJS web sites.
+  """
+  def afterSetUp(self):
+    super(TestRenderUpdateTranslationData, self).afterSetUp()
+    # create a translation script for this web site
+    self.web_script_translation_data_js = self.portal.web_page_module.newContent(
+        portal_type='Web Script',
+        # the convention is that this script has suffix translation_data.js
+        reference='{}_translation_data.js'.format(self.id()),
+        text_content='// will be filled',
+    )
+    self.web_script_translation_data_js.publish()
+    self.web_page_translation_gadget = self.portal.web_page_module.newContent(
+        portal_type='Web Page',
+        reference='{}.html'.format(self.id()),
+        content_type='text/html',
+        text_content=textwrap.dedent('''
+            <!DOCTYPE html>
+              <html>
+                <head>
+                  <meta charset="utf-8" />
+                  <meta name="viewport" content="width=device-width" />
+                  <title>Translation Gadget</title>
+                  <link rel="http://www.renderjs.org/rel/interface" href="interface_translation.html">
+
+                  <!-- renderjs -->
+                  <script src="rsvp.js" type="text/javascript"></script>
+                  <script src="renderjs.js" type="text/javascript"></script>
+
+                  <!-- custom script -->
+                  <script src="{translation_data_js_reference}" type="text/javascript"></script>
+                  <script src="gadget_translation.js" type="text/javascript"></script>
+
+                  </head>
+                <body>
+                </body>
+              </html>
+              ''').format(translation_data_js_reference=self.web_script_translation_data_js.getReference()),
+      )
+    self.web_page_translation_gadget.publish()
+    self.web_site.setProperty(
+        'configuration_translation_gadget_url',
+        self.web_page_translation_gadget.getReference()
+    )
+
+    self.web_site.setAvailableLanguageList(['en', 'fa'])
+
+    # add a manifest to list web pages to extract messages from
+    createZODBPythonScript(
+        self.portal.portal_skins.custom,
+        'WebSection_getTestPrecacheManifestList',
+        '',
+        textwrap.dedent('''
+        return [
+          'test_gadget_with_translation.html',
+          'test_gadget_with_translation.js',
+          'test_portal_skins_gadget.html',
+        ]
+        '''))
+    self.web_site.setProperty(
+        'configuration_precache_manifest_script_list',
+        'WebSection_getTestPrecacheManifestList')
+    self.tic()
+
+  def beforeTearDown(self):
+    super(TestRenderUpdateTranslationData, self).beforeTearDown()
+    if 'test_gadget_with_translation_html' in self.portal.web_page_module.objectIds():
+      self.portal.web_page_module.manage_delObjects(ids=['test_gadget_with_translation_html'])
+      self.tic()
+
+  def test_WebSite_getTranslationDataWebScriptValue(self):
+    self.assertEqual(
+        self.web_site.WebSite_getTranslationDataWebScriptValue(),
+        self.web_script_translation_data_js)
+
+  def test_Base_getTranslationSourceFileList(self):
+    self.assertIn(
+        'test_gadget_with_translation.html',
+        self.web_site.Base_getTranslationSourceFileList())
+    self.assertIn(
+        'test_gadget_with_translation.js',
+        self.web_site.Base_getTranslationSourceFileList())
+    self.assertIn(
+        'test_gadget_with_translation.html',
+        self.web_site.Base_getTranslationSourceFileList(only_html=True))
+    self.assertNotIn(
+        'test_gadget_with_translation.js',
+        self.web_site.Base_getTranslationSourceFileList(only_html=True))
+
+  def test_WebSite_getTranslationDataTextContent_extract_from_web_page(self):
+    self.portal.web_page_module.newContent(
+        portal_type='Web Page',
+        id='test_gadget_with_translation_html',
+        reference='test_gadget_with_translation.html',
+        text_content=textwrap.dedent('''
+        <html>
+          <!--
+            data-i18n=Message in comments
+            data-i18n="Quoted message in comments"
+            data-i18n=Message with "some parts" 'quoted' in comments
+            data-i18n=
+          -->
+          <h1 data-i18n="Message in attributes">Message in attributes</h1>
+          <input type="submit" data-i18n="[value]Message for attribute" value="Message for attribute"></input>
+          <h1 data-i18n="Message with {substitution}">Message with {substitution}</h1>
+          <h1 data-i18n="Message with [square brackets]">Message with [square brackets]</h1>
+          <div data-i18n="[html]Message in <a href='link'>HTML</a>">
+          </div>
+          <div data-i18n="">Empty data-i18n</div>
+          <script>
+            <span data-i18n="Message in script attributes">Message in script attributes</span>
+          </script>
+          <div data-i18n="メッサージュ"></div>
+        ''')
+    ).publish()
+    self.tic()
+
+    translation_data_text_content = self.web_site.WebSite_getTranslationDataTextContent()
+    self.assertIn('"Message in comments":', translation_data_text_content)
+    self.assertIn('"Quoted message in comments":', translation_data_text_content)
+    self.assertIn('"Message with \\"some parts\\" \'quoted\' in comments":', translation_data_text_content)
+    self.assertIn('"Message in attributes":', translation_data_text_content)
+    self.assertIn('"Message for attribute":', translation_data_text_content)
+    self.assertIn('"Message with {substitution}":', translation_data_text_content)
+    self.assertIn('"Message with [square brackets]":', translation_data_text_content)
+    self.assertIn('"Message in <a href=\'link\'>HTML</a>":', translation_data_text_content)
+    self.assertIn('"Message in script attributes":', translation_data_text_content)
+    self.assertIn('"メッサージュ":', translation_data_text_content)
+
+  def test_WebSite_getTranslationDataTextContent_extract_from_page_template(self):
+    self.portal.portal_skins.custom.manage_addProduct['PageTemplates'].manage_addPageTemplate(
+        'test_portal_skins_gadget.html',
+        text=textwrap.dedent('''
+          <html>
+          <!--
+           data-i18n=Message from page template
+           -->
+          </html>'''))
+    self.portal.changeSkin(None) # refresh skin cache
+    translation_data_text_content = self.web_site.WebSite_getTranslationDataTextContent()
+    self.assertIn('"Message from page template":', translation_data_text_content)
+
+  def test_WebSite_getTranslationDataTextContent_ignore_draft_web_page(self):
+    self.portal.web_page_module.newContent(
+        portal_type='Web Page',
+        id='test_gadget_with_translation_html',
+        reference='test_gadget_with_translation.html',
+        text_content=textwrap.dedent('''
+          <html>
+          <!--
+            data-i18n=Message in draft web page
+          -->
+          </html>'''))
+    translation_data_text_content = self.web_site.WebSite_getTranslationDataTextContent()
+    self.assertNotIn('"Message in draft web page":', translation_data_text_content)
+
+  def test_WebSite_getTranslationDataTextContent_ignore_archived_web_page(self):
+    web_page = self.portal.web_page_module.newContent(
+        portal_type='Web Page',
+        id='test_gadget_with_translation_html',
+        reference='test_gadget_with_translation.html',
+        text_content=textwrap.dedent('''
+          <html>
+          <!--
+            data-i18n=Message in archived web page
+          -->
+          </html>'''))
+    web_page.publish()
+    web_page.archive()
+    self.tic()
+    translation_data_text_content = self.web_site.WebSite_getTranslationDataTextContent()
+    self.assertNotIn('"Message in archived web page":', translation_data_text_content)
