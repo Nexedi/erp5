7cffcf61a6f6f776fb40e7d680960f08c8cd7d06
Romain Courteaud <romain@nexedi.com>
2020-12-15 17:06:54 +0000

    erp5_web_renderjs_ui: add an alarm to foce disabling xhtml style

---
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Alarm_disableXHtmlStyle.py
@@ -0,0 +1,18 @@
+portal = context.getPortalObject()
+portal_preferences = portal.portal_preferences
+
+if not portal_preferences.isPreferredHtmlStyleDisabled():
+
+  system_preference = portal_preferences.getActiveSystemPreference()
+  if system_preference is None:
+    system_preference = portal_preferences.newContent(
+                   portal_type="System Preference",
+                   title="Created by Alarm",
+                   activate_kw={'tag': tag})
+    system_preference.enable()
+
+  system_preference.edit(
+    preferred_html_style_disabled=True
+  )
+
+context.activate(after_tag=tag).getId()
