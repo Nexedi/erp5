1ad47aaecb2bc16e1e1d51d1689198d0f11a5a66
Romain Courteaud <romain@nexedi.com>
2020-12-15 15:43:46 +0000

    erp5_web_renderjs_ui: disable xhtml on demand

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -1,1 +1,7 @@
-raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
+from zExceptions import Forbidden
+
+preference_tool = context.getPortalObject().portal_preferences
+if preference_tool.isPreferredHtmlStyleDisabled():
+  raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
+
+pass
