a93e90d808e696d5095ad95d12d5872f6baaf819
Romain Courteaud <romain@nexedi.com>
2020-12-16 09:55:48 +0000

    erp5_web_renderjs_ui: allow some user to continue using xhtml style

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -1,7 +1,10 @@
 from zExceptions import Forbidden
+from AccessControl import getSecurityManager
 
 preference_tool = context.getPortalObject().portal_preferences
 if preference_tool.isPreferredHtmlStyleDisabled():
-  raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
+  u = getSecurityManager().getUser()
+  user_id = u.getId()
 
-pass
+  if user_id not in preference_tool.getPreferredHtmlStyleAllowedUserIdList():
+    raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
