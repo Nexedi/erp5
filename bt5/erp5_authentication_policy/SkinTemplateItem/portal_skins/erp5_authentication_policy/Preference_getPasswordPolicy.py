48c1584149fc537bfa6176cc94a2c2ba436faefa
Gabriel Monnerat <gabriel@nexedi.com>
2020-12-23 14:45:50 +0000

    erp5_authentication_policy: Use getDocumentValueList API

--- bt5/erp5_authentication_policy/SkinTemplateItem/portal_skins/erp5_authentication_policy/Preference_getPasswordPolicy.py
+++ bt5/erp5_authentication_policy/SkinTemplateItem/portal_skins/erp5_authentication_policy/Preference_getPasswordPolicy.py
@@ -1,7 +1,11 @@
 portal = context.getPortalObject()
 reference = portal.portal_preferences.getPreferredPasswordPolicyReference()
 if reference:
-  document = portal.portal_catalog(reference=reference, limit=1)
+  document = portal.portal_catalog.getDocumentValueList(
+    portal_type="Web Page",
+    validation_state=["released", "published_alive", "published"],
+    reference=reference,
+    limit=1)
   if document:
     return document[0].getTextContent()
 return ''
