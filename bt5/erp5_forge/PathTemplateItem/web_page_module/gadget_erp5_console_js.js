36e082c0910e502ce18621d6b08835da2f122a19
Gabriel Monnerat <gabriel@nexedi.com>
2020-12-28 12:52:45 +0000

    erp5_hal_json_style: Add console to run command line

---
+++ bt5/erp5_forge/PathTemplateItem/web_page_module/gadget_erp5_console_js.js
@@ -0,0 +1,48 @@
+/*jslint indent: 2, maxerr: 3, nomen: true */
+/*global window, rJS, jIO, loopEventListener, console */
+(function (window, rJS, jIO, loopEventListener) {
+  "use strict";
+  rJS(window)
+    .declareAcquiredMethod("notifySubmitting", "notifySubmitting")
+    .declareAcquiredMethod("notifySubmitted", "notifySubmitted")
+    .declareAcquiredMethod("displayErrorContent", "displayErrorContent")
+    .declareMethod('render', function render(options) {
+      return this.changeState({
+        context_url: options.context_url.trim()
+      });
+    })
+    .declareMethod('getContent', function getContent() {
+      return {
+        "your_command_line": this.element.querySelector("input").value
+      };
+    })
+    .onEvent("submit", function (evt) {
+      var gadget = this,
+        input_value = evt.target.querySelector("input").value,
+        output = gadget.element.querySelector("output");
+      return gadget.notifySubmitting()
+        .push(function () {
+          return jIO.util.ajax({
+            url: gadget.state.context_url + "/" + input_value,
+            xhrFields: {
+              withCredentials: true
+            }
+          });
+        })
+        .push(function (response) {
+          var content_type = response.target.getResponseHeader("Content-Type");
+          if (content_type.indexOf("text/html") !== -1) {
+            output.innerHTML = response.target.responseText;
+          } else {
+            output.value = response.target.responseText;
+          }
+          return gadget.notifySubmitted();
+        }, function (error) {
+          if (error.target.status === 404) {
+            output.textContent = "404 Not Found";
+          } else {
+            output.textContent = "Unexpected error. Please debug";
+          }
+        });
+    }, false, true);
+}(window, rJS, jIO, loopEventListener));
