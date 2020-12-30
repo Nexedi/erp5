bc4caed568c22c36c126495665004f0fea780181
Gabriel Monnerat <gabriel@nexedi.com>
2020-12-30 01:36:08 +0000

    erp5_web_renderjs_ui: use .onEvent instead of manually using .declareService

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_console_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_console_js.js
@@ -1,6 +1,6 @@
 /*jslint indent: 2, maxerr: 3, nomen: true */
-/*global window, rJS, RSVP, jIO, loopEventListener, JSON, console */
-(function (window, rJS, RSVP, jIO, loopEventListener, JSON) {
+/*global window, rJS, jIO, loopEventListener, JSON, console */
+(function (window, rJS, jIO, loopEventListener, JSON) {
   "use strict";
   rJS(window)
     .declareAcquiredMethod("notifySubmitting", "notifySubmitting")
@@ -15,43 +15,33 @@
         "your_command_line": this.element.querySelector("input").value
       };
     })
-    .declareService(function () {
+    .onEvent("submit", function (evt) {
       var gadget = this,
+        input_value = evt.target.querySelector("input").value,
         output = gadget.element.querySelector("output");
-      function formSubmit(evt) {
-        var input_value = evt.target.querySelector("input").value;
-        return gadget.notifySubmitting()
-          .push(function () {
-            // A hack during the christmas is allowed :)
-            return jIO.util.ajax({
-              url: gadget.state.context_url + "/" + input_value,
-              xhrFields: {
-                withCredentials: true
-              }
-            });
-          })
-          .push(function (response) {
-            var content_type = response.target.getResponseHeader("Content-Type");
-            if (content_type.indexOf("text/html") !== -1) {
-              output.innerHTML = response.target.responseText;
-            } else {
-              output.value = response.target.responseText;
+      return gadget.notifySubmitting()
+        .push(function () {
+          return jIO.util.ajax({
+            url: gadget.state.context_url + "/" + input_value,
+            xhrFields: {
+              withCredentials: true
             }
-            return gadget.notifySubmitted();
-          }, function (error) {
-            if (error.target.status === 404) {
-              output.innerText = "404 Not Found";
-            }
-            output.innerText = "Unexpected error. Please debug";
           });
-      }
-      return loopEventListener(
-        this.element.querySelector("form"),
-        "submit",
-        false,
-        formSubmit,
-        true
-      );
-    });
-
-}(window, rJS, RSVP, jIO, loopEventListener, JSON));
+        })
+        .push(function (response) {
+          var content_type = response.target.getResponseHeader("Content-Type");
+          if (content_type.indexOf("text/html") !== -1) {
+            output.innerHTML = response.target.responseText;
+          } else {
+            output.value = response.target.responseText;
+          }
+          return gadget.notifySubmitted();
+        }, function (error) {
+          if (error.target.status === 404) {
+            output.innerText = "404 Not Found";
+          } else {
+            output.innerText = "Unexpected error. Please debug";
+          }
+        });
+    }, false, true);
+}(window, rJS, jIO, loopEventListener, JSON));
