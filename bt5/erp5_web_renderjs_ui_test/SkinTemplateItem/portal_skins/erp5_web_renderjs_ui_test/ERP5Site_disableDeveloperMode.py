5064a0c038d0e671e4a5819e8ea4387216c5c7d2
Gabriel Monnerat <gabriel@nexedi.com>
2021-01-11 02:04:38 +0000

    erp5_web_renderjs_ui_test: Add sample of action that allow uses access slideshow from ERP5JS
    erp5_web_renderjs_ui_test: use macro as much as possible
    
    fixup! erp5_web_renderjs_ui_test: use macro as much as possible
    
    fixup! erp5_web_renderjs_ui_test: use macro as much as possible
    
    erp5_web_renderjs_ui_test: clean up
    
    erp5_web_renderjs_ui_test: remove useless script
    
    erp5_web_renderjs_ui_test: remove useless portal type action

--- bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/ERP5Site_disableDeveloperMode.py
+++
@@ -1,8 +0,0 @@
-portal = context.getPortalObject()
-active_preference = portal.portal_preferences.getActivePreference()
-if active_preference.getPreferredHtmlStyleDevelopperMode():
-  active_preference.setPreferredHtmlStyleDevelopperMode(0)
-  active_preference.setPreferredHtmlStyleContextualHelp(0)
-  active_preference.setPreferredHtmlStyleTranslatorMode(0)
-
-return "OK"
