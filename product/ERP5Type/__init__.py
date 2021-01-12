2e978de162d2b9ec7b26fbe12835a5e338f5a065
Aurel <aurel@nexedi.com>
2021-01-12 16:13:58 +0100

    ERP5Type: allow to load fomer MembershipTool

--- product/ERP5Type/__init__.py
+++ product/ERP5Type/__init__.py
@@ -189,3 +189,16 @@ import zExceptions
 ModuleSecurityInfo('zExceptions').declarePublic(*filter(
   lambda x: Exception in getattr(getattr(zExceptions, x), '__mro__', ()),
   dir(zExceptions)))
+
+# BBB : allow load of fomer Products.CMFDefault.MembershipTool
+# that has been replaced by Products.CMFCore.MembershipTool
+import sys, imp
+m = 'Products.CMFDefault'
+if m not in sys.modules:
+  sys.modules[m] = imp.new_module(m)
+  m += ".MembershipTool"
+  sys.modules[m] = m = imp.new_module(m)
+  from Products.CMFCore.MembershipTool import MembershipTool
+  m.MembershipTool = MembershipTool
+  del m
+
