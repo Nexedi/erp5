ab7d6923d4fe5c900339e5f2b5015b3fbb3c8ca9
Gabriel Monnerat <gabriel@nexedi.com>
2021-01-13 14:56:19 +0000

    erp5_web_renderjs_ui: add generic function to merge action with raw actions by group

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_tabpage_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_tabpage_js.js
@@ -1,10 +1,12 @@
-/*global window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray */
+/*global window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray,
+         mergeGlobalActionWithRawActionList */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
 
 /** Page for displaying Views, Jump and BreadCrumb navigation for a document.
 */
 
-(function (window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray) {
+(function (window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray,
+           mergeGlobalActionWithRawActionList) {
   "use strict";
 
   /** Go recursively up the parent-chain and insert breadcrumbs in the last argument.
@@ -101,31 +103,23 @@
         jump_list;
 
       return gadget.jio_getAttachment(gadget.state.jio_key, "links")
-
         .push(function (result) {
-          var i,
-            url_for_kw_list = [];
           erp5_document = result;
-          view_list = ensureArray(erp5_document._links.view);
-          jump_list = ensureArray(erp5_document._links.action_object_jio_jump);
+          return mergeGlobalActionWithRawActionList(gadget,
+            erp5_document._links, [
+              "view",
+              "action_object_jio_jump"
+            ]);
+          })
+        .push(function (group_mapping) {
+          var url_for_kw_list = [];
+          view_list = group_mapping.view;
+          jump_list = group_mapping.action_object_jio_jump;
 
-          for (i = 0; i < view_list.length; i += 1) {
-            url_for_kw_list.push({command: 'display_with_history', options: {
-              jio_key: gadget.state.jio_key,
-              view: view_list[i].href,
-              page: undefined  // Views in ERP5 must be forms but because of
-                               // OfficeJS we keep it empty for different default
-            }});
-          }
-          for (i = 0; i < jump_list.length; i += 1) {
-            url_for_kw_list.push({command: 'display_dialog_with_history', options: {
-              jio_key: gadget.state.jio_key,
-              view: jump_list[i].href
-            }});
-          }
+          url_for_kw_list = url_for_kw_list.concat(
+            view_list).concat(jump_list);
 
           url_for_kw_list.push({command: 'cancel_dialog_with_history'});
-
           return RSVP.hash({
             url_list: gadget.getUrlForList(url_for_kw_list),
             _: modifyBreadcrumbList(gadget,
@@ -135,21 +129,20 @@
             page_title: calculatePageTitle(gadget, erp5_document)
           });
         })
-
         .push(function (result_dict) {
           var i,
             j = 0;
 
           for (i = 0; i < view_list.length; i += 1) {
             tab_list.push({
-              title: view_list[i].title,
+              title: view_list[i].options.title,
               link: result_dict.url_list[j]
             });
             j += 1;
           }
           for (i = 0; i < jump_list.length; i += 1) {
             jump_action_list.push({
-              title: jump_list[i].title,
+              title: jump_list[i].options.title,
               link: result_dict.url_list[j]
             });
             j += 1;
@@ -171,4 +164,4 @@
       return;
     });
 
-}(window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray));
+}(window, rJS, RSVP, domsugar, URI, calculatePageTitle, ensureArray, mergeGlobalActionWithRawActionList));
