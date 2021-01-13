d9ebc903e44614e106c78942e416a41de112f392
Romain Courteaud <romain@nexedi.com>
2021-01-13 14:08:32 +0000

    romain_dev: forum
    romain_dev: forum use html viewer
    
    romain_dev: forum improve post rendering
    
    romain_dev: thread reader use page language inqtead of local
    
    romain_dev: forum set expected link on the listbox cells
    
    romain_dev: thread fixup
    
    romain_dev: thread use line separator instead of background color
    
    romain_dev: thread paginate
    
    romain_dev: forum link to last page
    
    romain_dev: thread simplify conf from erp5form
    
    romain_dev: forum attachment
    
    romain_dev: thread force white background

--- bt5/romain_dev/PathTemplateItem/web_page_module/romain_forum_js.js
+++ bt5/romain_dev/PathTemplateItem/web_page_module/romain_forum_js.js
@@ -1,14 +1,18 @@
 /*global window, rJS, RSVP, domsugar, SimpleQuery, ComplexQuery, Query,
          console */
 /*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80, continue:true */
-(function (window, rJS, RSVP, domsugar, SimpleQuery, ComplexQuery, Query) {
+(function (window, rJS, domsugar, SimpleQuery, ComplexQuery, Query) {
   "use strict";
   // XXX history_previous: prevent getting to erp5 ui by checking the
   // historing or forcing the page value
   // XXX create topic by followup (allDocs group by follow up)
 
   var DISPLAY_READER = 'display_reader',
-    MAIN_SCOPE = 'child_scope';
+    DISPLAY_THREAD = 'display_thread',
+    // DISPLAY_POST = 'display_post',
+    MAIN_SCOPE = 'child_scope',
+    DISPLAYED_POST_COUNT = 15,
+    THREAD_READER_FIELD_KEY = 'field_listbox';
 
   function loadChildGadget(gadget, gadget_url, must_declare, callback) {
     var queue,
@@ -43,7 +47,7 @@
           column_list = [
             ['title', 'Title'],
             ['DiscussionThread_getDiscussionPostCount', 'Responses'],
-            ['modification_date', 'Modification Date']
+            ['modification_date', 'Date']
           ];
 
         field_dict.listbox = {
@@ -117,7 +121,7 @@
   }
 
   function renderDiscussionThread(gadget, must_declare, jio_key) {
-    return loadChildGadget(gadget, "gadget_erp5_pt_form_view.html",
+    return loadChildGadget(gadget, "gadget_erp5_pt_form_view_editable.html",
                            must_declare, function (form_gadget) {
 
         var thread_info_dict;
@@ -137,54 +141,28 @@
             // XXX implement pseudo getResultValue
             thread_info_dict = result_list.data.rows[0].value;
             var group_list = [],
-              field_dict = {},
-              column_list = [
-                ['asStrippedHTML', 'Content'],
-                ['modification_date', 'Modification Date']
-              ];
+              field_dict = {};
 
-            field_dict.listbox = {
-              "column_list": column_list,
-              "show_anchor": 0,
-              "default_params": {},
+            field_dict.nutnut = {
               "editable": 1,
-              "editable_column_list": [],
-              "key": "field_listbox",
-              "lines": 15,
-              "list_method": "portal_catalog",
-              "query": "urn:jio:allDocs?query=" + Query.objectToSearchText(
-                new ComplexQuery({
-                  operator: "AND",
-                  query_list: [
-                    new SimpleQuery({
-                      key: "portal_type",
-                      operator: "=",
-                      type: "simple",
-                      value: "Discussion Post"
-                    }),
-                    new SimpleQuery({
-                      key: "parent_uid",
-                      operator: "=",
-                      type: "simple",
-                      // XXX Check usual states
-                      value: thread_info_dict.uid
-                    })
-                  ],
-                  type: "complex"
-                })
-              ),
-              "portal_type": ["Discussion Post"],
-              "search_column_list": [],
-              "sort_column_list": [],
-              "sort": [['modification_date', 'ASC']],
+              "key": THREAD_READER_FIELD_KEY,
               "title": "Discussion Posts",
-              "type": "ListBox"
+              "type": "GadgetField",
+              "url": "gadget_thread_reader.html",
+              "sandbox": "",
+              "renderjs_extra": JSON.stringify({
+                query_dict: {
+                  portal_type: 'Discussion Post',
+                  parent_uid: thread_info_dict.uid
+                },
+                sort: [['modification_date', 'ASC'], ['uid', 'ASC']],
+                lines: DISPLAYED_POST_COUNT
+              }),
+              "hidden": 0
             };
             group_list.push([
               "bottom",
-              [["listbox"]]
-            ], [
-              "hidden", ["listbox_modification_date"]
+              [["nutnut"]]
             ]);
 
             return form_gadget.render({
@@ -218,72 +196,7 @@
 
   }
 
-  function renderDiscussionPost(gadget, must_declare, jio_key) {
-    return loadChildGadget(gadget, "gadget_erp5_pt_form_view.html",
-                           must_declare,
-                           function (form_gadget) {
-
-        var thread_info_dict;
-        return gadget.jio_allDocs({
-          select_list: ['asStrippedHTML', 'title'],
-          query: Query.objectToSearchText(
-            new SimpleQuery({
-              key: "relative_url",
-              operator: "=",
-              type: "simple",
-              value: jio_key
-            })
-          ),
-          limit: [0, 1]
-        })
-          .push(function (result_list) {
-            // XXX implement pseudo getResultValue
-            thread_info_dict = result_list.data.rows[0].value;
-            var group_list = [],
-              field_dict = {};
-
-            field_dict.content = {
-              "editable": 0,
-              "key": "content",
-              "default": thread_info_dict.asStrippedHTML,
-              "type": "EditorField"
-            };
-            group_list.push([
-              "bottom",
-              [["content"]]
-            ]);
-
-            return form_gadget.render({
-              erp5_document: {
-                "_embedded": {
-                  "_view": field_dict
-                },
-                "_links": {
-                  "type": {
-                    // form_list display portal_type in header
-                    name: ""
-                  }
-                }
-              },
-              form_definition: {
-                group_list: group_list
-              }
-            });
-          })
-          .push(function () {
-            return gadget.getUrlFor({command: 'history_previous'});
-          })
-          .push(function (url) {
-            return gadget.updateHeader({
-              page_title: 'Post: ' + thread_info_dict.title,
-              page_icon: 'comment',
-              front_url: url
-            });
-          });
-      });
-
-  }
-
+/*
   function createMultipleSimpleOrQuery(key, value_list) {
     var i,
       query_list = [];
@@ -301,6 +214,7 @@
       type: "complex"
     });
   }
+*/
 
   rJS(window)
     /////////////////////////////////////////////////////////////////
@@ -317,147 +231,6 @@
       return;
     })
 
-    .declareMethod('render2', function renderHeader() {
-      var gadget = this;
-      return gadget.jio_allDocs({
-        select_list: ['uid', 'follow_up_title', 'title',
-                      'modification_date', 'countFolder'],
-        sort_on: [['modification_date', 'DESC']],
-        query: Query.objectToSearchText(
-          new ComplexQuery({
-            operator: "AND",
-            query_list: [
-              new SimpleQuery({
-                key: "portal_type",
-                operator: "=",
-                type: "simple",
-                value: "Discussion Thread"
-              }),
-              new SimpleQuery({
-                key: "validation_state",
-                operator: "=",
-                type: "simple",
-                // XXX Check usual states
-                value: "shared"
-              })
-            ],
-            type: "complex"
-          })
-        ),
-        limit: 15
-      })
-        .push(function (result) {
-          var uid_list = [];
-          if (result.data.total_rows.length === 0) {
-            uid_list.push(-1);
-          } else {
-            uid_list = result.data.rows.map(function (x) {
-              return x.value.uid;
-            });
-          }
-          // Search amount of documentation web pages related to those products
-          return gadget.jio_allDocs({
-            select_list: ['parent_uid', 'count(*)'],
-            query: Query.objectToSearchText(
-              new ComplexQuery({
-                operator: "AND",
-                query_list: [
-                  new SimpleQuery({
-                    key: "portal_type",
-                    operator: "=",
-                    type: "simple",
-                    value: "Discussion Post"
-                  }),
-                  new ComplexQuery({
-                    operator: "OR",
-                    type: "complex",
-                    query_list: uid_list.map(function (parent_uid) {
-                      return new SimpleQuery({
-                        key: "parent_uid",
-                        operator: "=",
-                        type: "simple",
-                        value: parent_uid
-                      });
-                    })
-                  })
-                ],
-                type: "complex"
-              })
-            ),
-            group_by: ['parent_uid'],
-            limit: 10000
-          })
-            .push(function () {
-              return result;
-            });
-
-        })
-        .push(function (result) {
-          console.log(result);
-          var element_list = [],
-            i;
-          console.log(result);
-          for (i = 0; i < result.data.total_rows; i += 1) {
-            element_list.push(
-              domsugar('br'),
-              domsugar('div', [
-                domsugar('p', {text: result.data.rows[i].value.title}),
-                domsugar('p', {text: result.data.rows[i].value.modification_date}),
-                domsugar('p', {text: result.data.rows[i].value.follow_up_title}),
-                domsugar('p', {text: result.data.rows[i].value.countFolder}),
-              ])
-            );
-          }
-          // XXX group by discussion post by parent_uid
-          console.log(element_list);
-          return domsugar(gadget.element, element_list);
-        });
-
-      var gadget = this,
-        product_uid_dict = {},
-        meta_product_uid_list = [],
-        product_list = [];
-      // First, get the list of products
-      return searchAllProject(gadget, product_uid_dict,
-                              meta_product_uid_list, product_list)
-        .push(function () {
-          return RSVP.hash({
-            status_dom: buildSoftwareStatusDom(gadget, product_list,
-                                               product_uid_dict),
-            documentation_dom: buildDocumentationDom(gadget,
-                                                     meta_product_uid_list)
-          });
-        })
-        .push(function (result_dict) {
-
-          domsugar(gadget.element, [
-            domsugar('img', {src: 'NXD-Official.Logo.svg?format=',
-                             alt: 'Nexedi Logo'}),
-            domsugar('section', {class: 'ui-content-header-plain'}, [
-              domsugar('h3', [
-                domsugar('span', {class: 'ui-icon ui-icon-exchange',
-                                  text: ' '}),
-                'Documentation'
-              ])
-            ]),
-            result_dict.documentation_dom,
-            domsugar('section', {class: 'ui-content-header-plain'}, [
-              domsugar('h3', [
-                domsugar('span', {class: 'ui-icon ui-icon-exchange',
-                                  text: ' '}),
-                'Software Status'
-              ])
-            ]),
-            result_dict.status_dom
-          ]);
-
-          return gadget.updateHeader({
-            page_title: 'Nexedi Project Quality',
-            page_icon: 'puzzle-piece'
-          });
-        });
-    })
-
     ////////////////////////////////////////////////////////////////////
     // Go
     ////////////////////////////////////////////////////////////////////
@@ -465,26 +238,61 @@
       return;
     })
 
-    .allowPublicAcquisition("jio_allDocs", function (param_list) {
+    .allowPublicAcquisition("jio_allDocs", function (param_list, scope) {
       // XXX Convert iso date to a DateTime field
       // XXX Paginate message to last message on modification date column
+      if (this.state.display_step !== DISPLAY_READER) {
+        throw new rJS.AcquisitionError();
+      }
+
       var gadget = this,
         options = param_list[0];
+      console.log(scope, param_list);
       return gadget.jio_allDocs(options)
         .push(function (result) {
-          var i, date, len = result.data.total_rows;
+          var i, date,
+            len = result.data.total_rows,
+            key,
+            url_value,
+            last_url_value,
+            count;
           for (i = 0; i < len; i += 1) {
+            url_value = {
+              command: 'index',
+              options: {
+                jio_key: result.data.rows[i].id,
+                page: gadget.state.page
+              }
+            };
+            last_url_value = {
+              command: 'index',
+              options: {
+                jio_key: result.data.rows[i].id,
+                page: gadget.state.page
+              }
+            };
+            count = result.data.rows[i].value
+                          .DiscussionThread_getDiscussionPostCount;
+            last_url_value.options[THREAD_READER_FIELD_KEY + '_begin_from'] =
+              count - (count % DISPLAYED_POST_COUNT);
+
+            for (key in result.data.rows[i].value) {
+              if (result.data.rows[i].value.hasOwnProperty(key)) {
+                result.data.rows[i].value[key] = {
+                  url_value: url_value,
+                  default: result.data.rows[i].value[key]
+                };
+              }
+            }
+
             if (result.data.rows[i].value.hasOwnProperty("modification_date")) {
-              date = new Date(result.data.rows[i].value.modification_date);
-              result.data.rows[i].value.modification_date = {
-                url_value: {
-                  command: 'index',
-                  options: {
-                    jio_key: result.data.rows[i].id,
-                    page: gadget.state.page
-                  }
-                },
-                field_gadget_param: {
+              date = new Date(
+                result.data.rows[i].value.modification_date.default
+              );
+              result.data.rows[i].value.modification_date.url_value =
+                last_url_value;
+              result.data.rows[i].value.modification_date
+                    .field_gadget_param = {
                   allow_empty_time: 0,
                   ampm_time_style: 0,
                   css_class: "date_field",
@@ -499,9 +307,28 @@
                   timezone_style: 0,
                   title: "Modification Date",
                   type: "DateTimeField"
-                }
-              };
+                };
             }
+
+            if (result.data.rows[i].value.hasOwnProperty(
+                "DiscussionThread_getDiscussionPostCount"
+              )) {
+              result.data.rows[i].value
+                    .DiscussionThread_getDiscussionPostCount
+                    .field_gadget_param = {
+                  description: "Count",
+                  editable: 0,
+                  hidden: 0,
+                  "default": result.data.rows[i].value
+                                   .DiscussionThread_getDiscussionPostCount
+                                   .default,
+                  key: "count",
+                  required: 0,
+                  title: "Responses",
+                  type: "IntegerField"
+                };
+            }
+            /*
             if (result.data.rows[i].value.hasOwnProperty("asStrippedHTML")) {
               result.data.rows[i].value.asStrippedHTML = {
                 url_value: {
@@ -523,22 +350,33 @@
                 }
               };
             }
+            */
 
           }
           return result;
         });
     })
 
-    .setState({
-      display_step: DISPLAY_READER
-    })
     .declareMethod('render', function (options) {
       console.log(options);
+
+      var display_step,
+        jio_key = options.jio_key;
+      if (jio_key === undefined) {
+        display_step = DISPLAY_READER;
+      } else if ((jio_key.match(/\//g) || []).length === 1) {
+        // XXX HACK
+        display_step = DISPLAY_THREAD;
+      }
+
       return this.changeState({
-        first_render: true,
+        // first_render: true,
         page: options.page,
-        jio_key: options.jio_key,
-        options: options
+        jio_key: jio_key,
+        display_step: display_step,
+        options: options,
+        // Force display in any case
+        render_timestamp: new Date().getTime()
         /*
         display_step: DISPLAY_TREE,
         // Only build the bt5 during the first query
@@ -565,51 +403,29 @@
       console.log('changestate', modification_dict);
       var gadget = this;
 
-      // Always refresh the reader
       if (gadget.state.display_step === DISPLAY_READER) {
-        if (gadget.state.jio_key === undefined) {
-          return renderDiscussionThreadList(
-            gadget,
-            modification_dict.hasOwnProperty('display_step') || modification_dict.first_render
-          );
-        }
-        // XXX HACK
-        if ((gadget.state.jio_key.match(/\//g) || []).length === 1) {
-          return renderDiscussionThread(
-            gadget,
-            modification_dict.hasOwnProperty('display_step') || modification_dict.first_render,
-            gadget.state.jio_key
-          );
-        }
-        return renderDiscussionPost(
+        return renderDiscussionThreadList(
           gadget,
-          modification_dict.hasOwnProperty('display_step') || modification_dict.first_render,
+          modification_dict.hasOwnProperty('display_step')
+        );
+      }
+      if (gadget.state.display_step === DISPLAY_THREAD) {
+        return renderDiscussionThread(
+          gadget,
+          modification_dict.hasOwnProperty('display_step'),
           gadget.state.jio_key
         );
       }
-/*
-      if (gadget.state.display_step === DISPLAY_TREE) {
-        console.log(modification_dict);
-        if (modification_dict.hasOwnProperty('display_step')) {
-          return renderTreeView(gadget,
-                                modification_dict.hasOwnProperty('extract'));
-        }
-        if (modification_dict.hasOwnProperty('expand_tree')) {
-          return expandTreeView(gadget);
-        }
-      }
-
-      if (modification_dict.display_step === DISPLAY_DIFF) {
-        return renderDiffView(gadget);
-      }
-
-      if (modification_dict.display_step === DISPLAY_CHANGELOG) {
-        return renderChangelogView(gadget);
+    /*
+        return renderDiscussionPost(
+          gadget,
+          modification_dict.hasOwnProperty('display_step') ||
+          modification_dict.first_render,
+          gadget.state.jio_key
+        );
       }
 */
-      if (modification_dict.hasOwnProperty('display_step')) {
-        throw new Error('Unhandled display step: ' + gadget.state.display_step);
-      }
+      throw new Error('Unhandled display step: ' + gadget.state.display_step);
     });
 
-}(window, rJS, RSVP, domsugar, SimpleQuery, ComplexQuery, Query));
+}(window, rJS, domsugar, SimpleQuery, ComplexQuery, Query));
