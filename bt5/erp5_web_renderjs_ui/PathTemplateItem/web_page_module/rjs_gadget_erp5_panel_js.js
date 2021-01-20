5c5ea1dc2ae2e171adc1a31abcf0a4ca863b18cc
Gabriel Monnerat <gabriel@nexedi.com>
2021-01-20 15:05:17 +0000

    erp5_web_renderjs_ui: Set expected command to action_object_jio_action

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_panel_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_panel_js.js
@@ -103,6 +103,7 @@
                 "action_object_jio_jump"
               ], {
                 "action_object_jio_jump": "display_dialog_with_history",
+                "action_object_jio_action": "display_dialog_with_history",
                 "action_object_view": "display_with_history",
                 "action_workflow": "display_dialog_with_history"
               }, {
