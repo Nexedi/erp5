8ec5eb601a102ee8897799e2c54a28369d0edd93
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2021-01-20 10:29:54 +0000

    erp5_administration: Add Scripts for Alarm to update validation state according to consistency state

---
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/Alarm_activateUpdateValidationStateFromConsistencyResult.py
@@ -0,0 +1,20 @@
+kw = {}
+
+if context.getProperty('incremental_check'):
+  last_active_process = context.getLastActiveProcess()
+  if last_active_process is not None:
+    kw['indexation_timestamp'] = '>= %s' % last_active_process.getStartDate().ISO()
+
+active_process = context.newActiveProcess().getRelativeUrl()
+query_string = context.getProperty('catalog_query_string', '')
+# the query sould be something like "validation_state:!=deleted validation_state:!=draft portal_type:Organisation" etc
+portal = context.getPortalObject()
+
+if query_string is not None:
+  kw.update(SearchableText=query_string)
+
+kw.update(parent_uid=[portal.restrictedTraverse(module).getUid() for module in context.getProperty('module_list') or []])
+
+portal.portal_catalog.searchAndActivate(method_id='Base_updateValidationStateFromConsistencyResult', method_kw={'fixit': fixit, 'active_process': active_process}, activate_kw={'tag':tag, 'priority': 8}, **kw)
+
+context.activate(after_tag=tag).getId()
