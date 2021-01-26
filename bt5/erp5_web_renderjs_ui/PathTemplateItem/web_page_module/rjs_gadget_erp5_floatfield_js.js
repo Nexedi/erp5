143623a30795e5f034541c6d663dd4695ddb07cb
Romain Courteaud <romain@nexedi.com>
2021-01-26 10:22:27 +0000

    erp5_web_renderjs_ui: float crash everything

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_floatfield_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_floatfield_js.js
@@ -4,7 +4,13 @@
   "use strict";
 
   var separator_re = /\d([\., \-_])?\d\d\d/,
-    input_format_re = /(-?)(\d+)(\.\d+)?/;
+    input_format_re = /(-?)(\d+)(\.\d+)?/,
+    HTML5_INPUT_STYLE = "-1234.5",
+    SPACE_INPUT_STYLE = "-1 234.5",
+    SPACE_COMMA_INPUT_STYLE = "-1 234,5",
+    DOT_COMMA_INPUT_STYLE = "-1.234,5",
+    COMMA_DOT_INPUT_STYLE = "-1,234.5",
+    PERCENT_INPUT_STYLE = "-12.3%";
 
   /** Slice any slice-able parameter into triplets **/
   function toTriplets(sliceable) {
@@ -17,16 +23,48 @@
     return parts;
   }
 
+  function convertFloatToHTML5Input(precision, input_style, float) {
+    // ERP5 always devides the value by 100 if it is set to percentages
+    // thus we have to mitigate that in javascript here
+    if (input_style === PERCENT_INPUT_STYLE) {
+      float *= 100.0;
+    }
+    return float.toString();
+    // XXX precision
+    // Always convert
+  }
+
+  function convertERP5InputToHTML5Input(precision, input_style, text) {
+    // Convert ERP5 input style to html5 float text
+    if (input_style === HTML5_INPUT_STYLE) {
+      return text;
+    } else {
+      throw new Error('No supported input style: ' + input_style);
+    }
+  }
+
+  function convertHTML5InputToERP5Input(precision, input_style, text) {
+    if (input_style === HTML5_INPUT_STYLE) {
+      return text;
+    } else {
+      throw new Error('No supported input style: ' + input_style);
+    }
+  }
+
   rJS(window)
     .setState({
       tag: 'p',  // important for CSS styles - noneditable element must be <p>
       type: "number"
     })
     .declareMethod('render', function (options) {
+      console.log('floatfield.render', options.field_json);
       var field_json = options.field_json || {},
-        input_style = (field_json.input_style || ""),
-        percentage = input_style.endsWith("%"),
-        thousand_sep = separator_re.test(input_style) ? (separator_re.exec(input_style)[1] || "") : "",
+        input_style = (field_json.input_style || DEFAULT_INPUT_STYLE),
+        value = field_json.default,
+        text_content,
+        precision = field_json.precision,
+        // percentage = input_style.endsWith("%"),
+        // thousand_sep = separator_re.test(input_style) ? (separator_re.exec(input_style)[1] || "") : "",
         state_dict = {
           editable: field_json.editable,
           required: field_json.required,
@@ -34,11 +72,11 @@
           id: field_json.key,
           name: field_json.key,
           title: field_json.description,
-          precision: window.parseFloat(field_json.precision),
+          // precision: window.parseFloat(field_json.precision),
           error_text: field_json.error_text,
           // erp5 always put value into "default" (never "value")
-          value: window.parseFloat(field_json.default),
-          text_content: '',
+          // value: window.parseFloat(field_json.default),
+          // text_content: '',
           // `step` is used for browser-level validation thus a mandatory value
           // if unspecified we can use "any" value
           step: "any",
@@ -50,6 +88,19 @@
         },
         tmp;
 
+      if (typeof(value) === 'number') {
+        value = convertFloatToHTML5Input(precision, input_style, value);
+        text_content = convertHTML5InputToERP5Input(precision, input_style, value);
+      } else {
+        text_content = value;
+        value = convertERP5InputToHTML5Input(precision, input_style, value);
+      }
+
+      state_dict.value = value;
+      state_dict.text_content = text_content;
+      state_dict.input_style = input_style;
+
+/*
       if (percentage) {
         // ERP5 always devides the value by 100 if it is set to percentages
         // thus we have to mitigate that in javascript here
@@ -72,10 +123,14 @@
           tmp = undefined;
         }
       }
+      console.log('floatfield.render2', state_dict.value);
+*/
+
       return this.changeState(state_dict);
     })
 
     .onStateChange(function (modification_dict) {
+      console.log('floatfield.onstatechange', modification_dict.value);
       var element = this.element,
         gadget = this,
         url,
@@ -106,10 +161,22 @@
     })
 
     .declareMethod('getContent', function () {
+      var gadget = this;
       if (this.state.editable) {
         return this.getDeclaredGadget('sub')
-          .push(function (gadget) {
-            return gadget.getContent();
+          .push(function (sub_gadget) {
+            return sub_gadget.getContent();
+          })
+          .push(function (result) {
+            result[gadget.state.key] =
+              convertHTML5InputToERP5Input(
+                gadget.state.precision,
+                gadget.state.input_style,
+                result[gadget.state.key]
+              );
+
+            console.log('floatfield.getContent', result);
+            return result;
           });
       }
       return {};
