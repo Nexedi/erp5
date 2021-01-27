fe941384a0032c964404209c1bd4f50582ebbfec
Jérome Perrin <jerome@nexedi.com>
2021-01-27 10:04:12 +0100

    trade: support "Asset Price" valuation method in stock report
    This shows the asset price from `stock.total_price` which can be just the
    movement prices or the evaluated prices if getDestinationAssetPrice and/or
    getSourceAssetPrice type based methods are defined.

--- bt5/erp5_trade/TestTemplateItem/portal_components/test.erp5.testTradeReports.py
+++ bt5/erp5_trade/TestTemplateItem/portal_components/test.erp5.testTradeReports.py
@@ -1651,6 +1651,35 @@ class TestTradeReports(ERP5ReportTestCase):
         total_price=7,
     )
 
+  def testStockReport_valuation_method_asset_price(self):
+    self._createConfirmedSalePackingListForStockReportTest()
+    request = self.portal.REQUEST
+    request.form['at_date'] = DateTime(2007, 3, 3)
+    request.form['node_category'] = 'site/demo_site_A'
+    request.form['simulation_period'] = 'future'
+    request.form['inventory_valuation_method'] = 'asset_price'
+
+    line_list = self.portal.inventory_module.Base_viewStockReportBySite.listbox.\
+        get_value('default',
+                  render_format='list', REQUEST=self.portal.REQUEST)
+
+    data_line_list = [l for l in line_list if l.isDataLine()]
+    self.assertEqual(1, len(data_line_list))
+    data_line = data_line_list[0]
+    self.assertEqual(
+        data_line.column_id_list,
+        ['resource_title', 'resource_reference', 'variation_category_item_list', 'inventory', 'quantity_unit', 'total_price'])
+
+    self.checkLineProperties(
+        data_line_list[0],
+        resource_title='product_A',
+        resource_reference='ref 2',
+        variation_category_item_list=[],
+        inventory=1,
+        quantity_unit='G',
+        total_price=10,
+    )
+
   def testStockReport_valuation_and_quantity_unit_conversion(self):
     self._createConfirmedSalePackingListForStockReportTest(
         quantity=0.5,
