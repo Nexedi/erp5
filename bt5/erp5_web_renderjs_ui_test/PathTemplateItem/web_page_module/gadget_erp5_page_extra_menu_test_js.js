b2ccbe47c835e1eff7cea1db31aa08d01d485247
Gabriel Monnerat <gabriel@nexedi.com>
2021-02-08 21:46:43 +0000

    erp5_web_renderjs_ui_test: move to extra_menu_list

--- bt5/erp5_web_renderjs_ui_test/PathTemplateItem/web_page_module/gadget_erp5_page_extra_menu_test_js.js
+++ bt5/erp5_web_renderjs_ui_test/PathTemplateItem/web_page_module/gadget_erp5_page_extra_menu_test_js.js
@@ -1,10 +1,10 @@
-/*global window, rJS, RSVP */
+/*global window, rJS, RSVP, ensureArray*/
 /*jslint nomen: true, indent: 2, maxerr: 3 */
 /**
 Page Form is a top-level gadget (a "Page") taking care of rendering form
 and handling data send&receive.
 */
-(function (window, rJS, RSVP) {
+(function (window, rJS, RSVP, ensureArray) {
   "use strict";
 
   rJS(window)
@@ -15,6 +15,7 @@ and handling data send&receive.
     .declareAcquiredMethod("updatePanel", "updatePanel")
     .declareAcquiredMethod("updateHeader", "updateHeader")
     .declareAcquiredMethod("getUrlForList", "getUrlForList")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
 
     .declareMethod("render", function render(options) {
       var gadget = this,
@@ -31,26 +32,33 @@ and handling data send&receive.
         }];
       return gadget.updateHeader({page_title: "Extra Menu"})
         .push(function () {
+          if (options.jio_key) {
+            return gadget.jio_getAttachment(options.jio_key,
+                                            options.view);
+          }
+        })
+        .push(function (result) {
+          new_state.erp5_document = result;
           return gadget.getUrlForList(parameter_list);
         })
         .push(function (href_list) {
-          new_state.extra_menu_dict = {
-            'Link' : {
-              'icon': 'link',
-              'action_list': [{
-                'title' : 'Contribute',
-                'class_name' : 'ui-btn-icon-left ui-icon-file'
-              }],
-              'href_list': href_list
-            }
-          };
+          new_state.extra_menu_list = [{
+            'title' : 'Contribute',
+            'href': href_list[0],
+            // if class_name is passed here, it is propagated to link
+            'class_name': "active custom-class"
+          }];
           return gadget.changeState(new_state);
         });
     })
 
     .onStateChange(function onStateChange() {
-      return this.updatePanel({
-        extra_menu_dict: this.state.extra_menu_dict
+      var gadget = this;
+      return gadget.updatePanel({
+        extra_menu_list: gadget.state.extra_menu_list,
+        jio_key: gadget.state.options.jio_key,
+        erp5_document: gadget.state.erp5_document,
+        view_list: []
       });
     });
-}(window, rJS, RSVP));
+}(window, rJS, RSVP, ensureArray));
