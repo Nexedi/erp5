f48010062cbe09b453ffa6a4047299674fb5b5d4
Romain Courteaud <romain@nexedi.com>
2021-02-08 13:40:48 +0000

    romain_dev: access

--- bt5/romain_dev/PathTemplateItem/web_page_module/romain_access_js.js
+++ bt5/romain_dev/PathTemplateItem/web_page_module/romain_access_js.js
@@ -1,14 +1,26 @@
 /*jslint indent: 2, maxerr: 3, maxlen: 80 */
-/*global window, rJS */
-(function (window, rJS) {
+/*global window, rJS, RSVP */
+(function (window, rJS, RSVP) {
   "use strict";
 
   rJS(window)
+    .declareAcquiredMethod("getTranslationDict", "getTranslationDict")
     .declareMethod('render', function (options) {
-      return this.getDeclaredGadget('access')
-        .push(function (access_gadget) {
-          return access_gadget.render(options);
+      return new RSVP.Queue(RSVP.hash({
+        access_gadget: this.getDeclaredGadget('access'),
+        translation_dict: this.getTranslationDict(['Contribute', 'New'])
+      }))
+        .push(function (result_dict) {
+          return result_dict.access_gadget.render(options, [{
+            title: result_dict.translation_dict.New,
+            jio_key: 'portal_contributions',
+            erp5_action: 'create_a_document'
+          }, {
+            title: result_dict.translation_dict.Contribute,
+            jio_key: 'document_module',
+            erp5_action: 'contribute_file'
+          }]);
         });
     });
 
-}(window, rJS));
+}(window, rJS, RSVP));
