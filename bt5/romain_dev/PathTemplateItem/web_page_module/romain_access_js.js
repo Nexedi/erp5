e22429e6bbc921b3cef7f847d691702f3d8de05c
Romain Courteaud <romain@nexedi.com>
2021-02-08 14:34:13 +0000

    romain_dev: access

--- bt5/romain_dev/PathTemplateItem/web_page_module/romain_access_js.js
+++ bt5/romain_dev/PathTemplateItem/web_page_module/romain_access_js.js
@@ -8,7 +8,7 @@
     .declareMethod('render', function (options) {
       return new RSVP.Queue(RSVP.hash({
         access_gadget: this.getDeclaredGadget('access'),
-        translation_dict: this.getTranslationDict(['Contribute', 'New'])
+        translation_dict: this.getTranslationDict(['Contribute File', 'New'])
       }))
         .push(function (result_dict) {
           return result_dict.access_gadget.render(options, [{
@@ -16,7 +16,7 @@
             jio_key: 'portal_contributions',
             erp5_action: 'create_a_document'
           }, {
-            title: result_dict.translation_dict.Contribute,
+            title: result_dict.translation_dict['Contribute File'],
             jio_key: 'document_module',
             erp5_action: 'contribute_file'
           }]);
