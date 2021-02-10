a220ffde3a71a408d0fcfa3fa34157ceac615d39
Romain Courteaud <romain@nexedi.com>
2021-02-10 10:27:03 +0000

    romain_dev: create new doc

--- bt5/romain_dev/PathTemplateItem/web_page_module/romain_site_document_creator_js.js
+++ bt5/romain_dev/PathTemplateItem/web_page_module/romain_site_document_creator_js.js
@@ -1,88 +1,74 @@
 /*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
-/*global domsugar, window, rJS, RSVP */
-(function (domsugar, window, rJS, RSVP) {
+/*global domsugar, window, rJS */
+(function (domsugar, window, rJS) {
   "use strict";
 
   rJS(window)
-    /////////////////////////////////////////////////////////////////
-    // Acquired methods
-    /////////////////////////////////////////////////////////////////
-    .declareAcquiredMethod("updateHeader", "updateHeader")
-    .declareAcquiredMethod("getTranslationDict", "getTranslationDict")
-    .declareAcquiredMethod("getUrlForDict", "getUrlForDict")
-    .declareAcquiredMethod("redirect", "redirect")
-    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
-
-    .declareMethod('triggerSubmit', function () {
-      return;
-    })
-
-    .allowPublicAcquisition('updateHeader', function () {
-      return;
-    })
-    .allowPublicAcquisition('updatePanel', function () {
-      return;
-    })
-
-    ////////////////////////////////////////////////////////////////////
-    // Go
-    ////////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("notifySubmit", "notifySubmit")
     .declareMethod('render', function (options) {
+      return this.changeState({
+        key: options.key,
+        erp5_add_list: JSON.stringify(options.erp5_add_list)
+      });
+    })
+    .onStateChange(function () {
       var gadget = this,
-        erp5_add_list = options.erp5_add_list,
+        erp5_add_list = JSON.parse(gadget.state.erp5_add_list),
         i,
         j,
         k,
         business_application_element_list = [],
-        module_element_list,
-        url_for_dict = {};
+        module_element_list;
+
 
       for (i = 0; i < erp5_add_list.length; i += 1) {
+        module_element_list = [];
+
         for (j = 0; j < erp5_add_list[i][1].length; j += 1) {
           for (k = 0; k < erp5_add_list[i][1][j][2].length; k += 1) {
-            url_for_dict[erp5_add_list[i][1][j][1] +
-                         erp5_add_list[i][1][j][2][k][1]] = {
-              command: 'display_erp5_action_with_history',
-              options: {
-                jio_key: erp5_add_list[i][1][j][1],
-                page: erp5_add_list[i][1][j][2][k][1]
-              }
-            };
+            module_element_list.push(
+              domsugar('li', [domsugar('button', {
+                type: 'submit',
+                'data-value': erp5_add_list[i][1][j][1] + ' ' +
+                              erp5_add_list[i][1][j][2][k][1],
+                text: erp5_add_list[i][1][j][2][k][0]
+              })])
+            );
           }
         }
+
+        business_application_element_list.push(domsugar('li', [
+          domsugar('h2', {
+            text: erp5_add_list[i][0]
+          }),
+          domsugar('ul', module_element_list)
+        ]));
       }
 
-      return new RSVP.Queue(gadget.getUrlForDict(url_for_dict))
-        .push(function (url_dict) {
+      domsugar(gadget.element, [
+        domsugar('ul', {'class': 'gridlist'},
+                 business_application_element_list)
+      ]);
 
-          for (i = 0; i < erp5_add_list.length; i += 1) {
-            module_element_list = [];
+    })
 
-            for (j = 0; j < erp5_add_list[i][1].length; j += 1) {
-              for (k = 0; k < erp5_add_list[i][1][j][2].length; k += 1) {
-                module_element_list.push(
-                  domsugar('li', [domsugar('button', {
-                    type: 'button',
-                    text: erp5_add_list[i][1][j][2][k][0]
-                  })])
-                );
-              }
-            }
+    .declareMethod('getContent', function () {
+      var result = {};
+      if (this.state.value !== undefined) {
+        result[this.state.key] = this.state.value;
+      }
+      return result;
+    }, {mutex: 'changestate'})
 
-            business_application_element_list.push(domsugar('li', [
-              domsugar('h2', {
-                text: erp5_add_list[i][0]
-              }),
-              domsugar('ul', module_element_list)
-            ]));
-          }
+    .declareMethod('checkValidity', function () {
+      return (this.state.value !== undefined);
+    }, {mutex: 'changestate'})
 
-          domsugar(gadget.element, [
-            domsugar('ul', {'class': 'gridlist'},
-                     business_application_element_list)
-          ]);
-        });
+    .onEvent('click', function (evt) {
+      if (evt.target.tagName === 'BUTTON') {
+        this.state.value = evt.target.getAttribute('data-value');
+      }
+      return this.notifySubmit();
+    }, false);
 
-    });
-
-}(domsugar, window, rJS, RSVP));
+}(domsugar, window, rJS));
