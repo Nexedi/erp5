9b02854d0dd41df5b7a090d617fa869b03f635ea
Romain Courteaud <romain@nexedi.com>
2021-02-12 16:04:39 +0000

    officejs_todomvc: use renderJS.loopEventListener

--- bt5/officejs_todomvc/PathTemplateItem/web_page_module/officejs_todomvc_gadget_router_js.js
+++ bt5/officejs_todomvc/PathTemplateItem/web_page_module/officejs_todomvc_gadget_router_js.js
@@ -1,6 +1,6 @@
 /*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80*/
 /*global window, RSVP, rJS*/
-(function (window, RSVP, rJS) {
+(function (window, RSVP, rJS, loopEventListener) {
   "use strict";
 
 
@@ -16,67 +16,6 @@
       return "";
     }
   }
-
-  function loopEventListener(target, type, useCapture, callback,
-    prevent_default) {
-    //////////////////////////
-    // Infinite event listener (promise is never resolved)
-    // eventListener is removed when promise is cancelled/rejected
-    //////////////////////////
-    var handle_event_callback,
-      callback_promise;
-
-    if (prevent_default === undefined) {
-      prevent_default = true;
-    }
-
-    function cancelResolver() {
-      if ((callback_promise !== undefined) &&
-          (typeof callback_promise.cancel === "function")) {
-        callback_promise.cancel();
-      }
-    }
-
-    function canceller() {
-      if (handle_event_callback !== undefined) {
-        target.removeEventListener(type, handle_event_callback, useCapture);
-      }
-      cancelResolver();
-    }
-    function itsANonResolvableTrap(resolve, reject) {
-      var result;
-      handle_event_callback = function (evt) {
-        if (prevent_default) {
-          evt.stopPropagation();
-          evt.preventDefault();
-        }
-
-        cancelResolver();
-
-        try {
-          result = callback(evt);
-        } catch (e) {
-          result = RSVP.reject(e);
-        }
-
-        callback_promise = result;
-        new RSVP.Queue()
-          .push(function () {
-            return result;
-          })
-          .push(undefined, function (error) {
-            if (!(error instanceof RSVP.CancellationError)) {
-              canceller();
-              reject(error);
-            }
-          });
-      };
-
-      target.addEventListener(type, handle_event_callback, useCapture);
-    }
-    return new RSVP.Promise(itsANonResolvableTrap, canceller);
-  }
-
 
   /* Initialization */
 
@@ -100,4 +39,4 @@
 
     .declareAcquiredMethod("setQuery", "setQuery");
 
-}(window, RSVP, rJS));
+}(window, RSVP, rJS, rJS.loopEventListener));
