a765532263e894c79e018e0e19c32ddc999f0aa7
Romain Courteaud <romain@nexedi.com>
2021-03-02 13:55:22 +0000

    erp5_web_renderjs_ui: follow the html5_input latest changes

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_html5_textarea_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_html5_textarea_js.js
@@ -3,14 +3,6 @@
 (function (window, rJS, RSVP, navigator) {
   "use strict";
 
-  function checkChange() {
-    var gadget = this;
-    return RSVP.all([
-      gadget.checkValidity(),
-      gadget.notifyChange()
-    ]);
-  }
-
   rJS(window)
     .setState({
       editable: false,
@@ -18,49 +10,71 @@
     })
 
     .declareMethod('render', function render(options) {
-      return this.changeState(options);
+      return this.changeState({
+        value: options.value,
+        editable: options.editable,
+        required: options.required,
+        id: options.id,
+        name: options.name,
+        title: options.title,
+        error_text: options.error_text || "",
+        hidden: options.hidden
+      });
     })
 
     .onStateChange(function onStateChange(modification_dict) {
       var textarea = this.element.querySelector('textarea');
 
-      if (this.state.error_text &&
-          !textarea.classList.contains("is-invalid")) {
-        textarea.classList.add("is-invalid");
-      } else if (!this.state.error_text &&
-                 textarea.classList.contains("is-invalid")) {
-        textarea.classList.remove("is-invalid");
+      if (modification_dict.hasOwnProperty('value') ||
+          modification_dict.hasOwnProperty('editable') ||
+          modification_dict.hasOwnProperty('required') ||
+          modification_dict.hasOwnProperty('id') ||
+          modification_dict.hasOwnProperty('name') ||
+          modification_dict.hasOwnProperty('title') ||
+          modification_dict.hasOwnProperty('hidden')
+          ) {
+
+        if (modification_dict.hasOwnProperty("value")) {
+          textarea.value = modification_dict.value;
+        }
+
+        if (modification_dict.hasOwnProperty("name")) {
+          textarea.setAttribute('name', modification_dict.name);
+          textarea.setAttribute('id', modification_dict.name);
+        }
+
+        if (modification_dict.hasOwnProperty("title")) {
+          textarea.setAttribute('title', modification_dict.title);
+        }
+
+        if (this.state.required) {
+          textarea.required = true;
+        } else {
+          textarea.required = false;
+        }
+
+        if (this.state.editable) {
+          textarea.removeAttribute('readonly');
+        } else {
+          textarea.setAttribute('readonly', 'readonly');
+        }
+
+        if (this.state.hidden) {
+          textarea.hidden = true;
+        } else {
+          textarea.hidden = false;
+        }
+
       }
 
-      if (modification_dict.hasOwnProperty("value")) {
-        textarea.value = modification_dict.value;
-      }
-
-      if (modification_dict.hasOwnProperty("name")) {
-        textarea.setAttribute('name', modification_dict.name);
-        textarea.setAttribute('id', modification_dict.name);
-      }
-
-      if (modification_dict.hasOwnProperty("title")) {
-        textarea.setAttribute('title', modification_dict.title);
-      }
-
-      if (this.state.required) {
-        textarea.setAttribute('required', 'required');
-      } else {
-        textarea.removeAttribute('required');
-      }
-
-      if (this.state.editable) {
-        textarea.removeAttribute('readonly');
-      } else {
-        textarea.setAttribute('readonly', 'readonly');
-      }
-
-      if (this.state.hidden) {
-        textarea.hidden = true;
-      } else {
-        textarea.hidden = false;
+      if (modification_dict.hasOwnProperty('error_text')) {
+        if (this.state.error_text &&
+            !textarea.classList.contains("is-invalid")) {
+          textarea.classList.add("is-invalid");
+        } else if (!this.state.error_text &&
+                   textarea.classList.contains("is-invalid")) {
+          textarea.classList.remove("is-invalid");
+        }
       }
 
     })
@@ -90,18 +104,35 @@
             return result;
           });
       }
+      if (this.state.error_text) {
+        return this.notifyInvalid(this.state.error_text)
+          .push(function () {
+            return result;
+          });
+      }
       return result;
     })
 
-    .declareAcquiredMethod("notifyChange", "notifyChange")
-    .onEvent('change', checkChange, false, true)
-    .onEvent('input', checkChange, false, true)
+    .declareJob('deferErrorText', function deferErrorText(error_text) {
+      return this.changeState({
+        error_text: error_text
+      });
+    }, {mutex: 'changestate'})
 
-    .declareAcquiredMethod("notifyInvalid", "notifyInvalid")
-    .onEvent('invalid', function invalid(evt) {
-      // invalid event does not bubble
-      return this.notifyInvalid(evt.target.validationMessage);
-    }, true, false)
+    .declareAcquiredMethod("notifyChange", "notifyChange")
+    .onEvent('change', function change() {
+      return RSVP.all([
+        this.checkValidity(),
+        this.notifyChange("change")
+      ]);
+    }, false, false)
+
+    .onEvent('input', function input() {
+      return RSVP.all([
+        this.checkValidity(),
+        this.notifyChange("input")
+      ]);
+    }, false, false)
 
     .declareAcquiredMethod("notifyFocus", "notifyFocus")
     .onEvent('focus', function focus() {
@@ -112,6 +143,16 @@
     .onEvent('blur', function blur() {
       return this.notifyBlur();
     }, true, false)
+
+    .declareAcquiredMethod("notifyInvalid", "notifyInvalid")
+    .onEvent('invalid', function invalid(evt) {
+      // invalid event does not bubble
+      return RSVP.all([
+        this.deferErrorText(evt.target.validationMessage),
+        this.notifyInvalid(evt.target.validationMessage)
+      ]);
+    }, true, false)
+
 
     .declareAcquiredMethod("notifySubmit", "notifySubmit")
     .onEvent('keydown', function keydown(evt) {
