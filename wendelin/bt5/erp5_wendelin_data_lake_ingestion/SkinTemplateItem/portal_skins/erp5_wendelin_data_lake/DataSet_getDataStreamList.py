464ab91d881eba1908f71e9e51dab209474a792a
Roque Porchetto <roque.porchetto@nexedi.com>
2021-03-03 14:58:06 +0100

    erp5_wendelin: fix script name conflict

---
+++ wendelin/bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/DataSet_getDataStreamList.py
@@ -0,0 +1,13 @@
+"""
+  Return list of Data Streams belonging to a Date Set.
+  Data Ingestion line aggregates both Data Set and Data Stream.
+
+  Note: This code is quite computationally costly (for Data Streams having thousands of iles) as it needs to:
+  1. Query MariaDB to find ingestion lines
+  2. Read from ZODB both Data Ingestion Lines and Data Streams (which itself can be big too)
+"""
+data_ingestion_line_list = context.portal_catalog(
+                             portal_type = "Data Ingestion Line",
+                             aggregate_uid = context.getUid())
+return [x.getAggregateValue(portal_type = "Data Stream") \
+          for x in data_ingestion_line_list]
