70bd2dd91fe4de6c3775c3e19bbbfe22958fc4a6
Romain Courteaud <romain@nexedi.com>
2021-03-03 10:30:28 +0100

    MORE TESTS

--- tests/__init__.py
+++ tests/__init__.py
@@ -40,7 +40,7 @@ class _ERP5(ERP5TypeTestSuite):
         # glob('%s/product/ERP5Form/tests/test*.py' % path) +
         # ['%s/product/ERP5OOo/tests/testDeferredStyle.py' % path] +
         # glob('%s/product/ERP5/tests/test.*.py' % path) +
-        # ['%s/product/ERP5/tests/testXHTML.py' % path] +
+        ['%s/product/ERP5/tests/testXHTML.py' % path] +
         # ['%s/product/ERP5/tests/testERP5Core.py' % path] +
         # ['%s/product/ERP5/tests/testQueryModule.py' % path] +
         # ['%s/product/ERP5/tests/testBankReconciliation.py' % path] +
