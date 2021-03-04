de71cc3005e5934e1b15cd307e96f45ef911533b
Romain Courteaud <romain@nexedi.com>
2021-03-04 08:33:23 +0000

    erp5_xhtml_style: remove probably not needed cache

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_filterDuplicateActions.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_filterDuplicateActions.py
@@ -5,7 +5,6 @@ In case of duplicate, only the first action will be kept.
 `actions` is the mapping returned by ActionsTool.listFilteredActionsFor
 The script must be called on the context of the document.
 """
-from Products.ERP5Type.Cache import CachingMethod
 def filterDuplicateActions(actions):
   new_actions = {}
 
@@ -26,29 +25,6 @@ def filterDuplicateActions(actions):
 
   return new_actions
 
-
-def hasDuplicateActions(portal_type, user_name):
-  # Compare the count of action categories and actions
-  # Give every category a amount of 1000, as
-  # 'object_onlyxhtml_view' is transformed into 'object_view'
-  len_actions = 0
-  len_filtered_actions = 0
-  for cat in actions.values():
-    len_actions += 1000 + len(cat)
-  filtered_actions = filterDuplicateActions(actions)
-  for cat in filtered_actions.values():
-    len_filtered_actions += 1000 + len(cat)
-  return len_actions != len_filtered_actions
-
-
-hasDuplicateActions = CachingMethod(
-                          hasDuplicateActions,
-                          id='Base_filterDuplicateActions.hasDuplicateActions',
-                          cache_factory='erp5_ui_long')
-
-user_name = getattr(container.REQUEST, 'AUTHENTICATED_USER', '')
-
 if getattr(context, 'getPortalType', None) is not None:
-  if hasDuplicateActions(context.getPortalType(), user_name):
-    return filterDuplicateActions(actions)
+  return filterDuplicateActions(actions)
 return actions
