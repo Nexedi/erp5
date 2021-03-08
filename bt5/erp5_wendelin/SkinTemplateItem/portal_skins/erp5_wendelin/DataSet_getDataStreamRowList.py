63d406c773bd4e73bdc6b2900d6118c61234188e
Roque Porchetto <roque.porchetto@nexedi.com>
2021-03-08 12:45:35 +0000

    erp5_wendelin: rename to avoid duplicated script name

--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamRowList.py
+++ bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamRowList.py
@@ -2,4 +2,4 @@
 Get list of Data Streams for context Data set.
 """
 data_set_uid = context.getUid()
-return context.ERP5Site_getDataStreamList(data_set_uid, limit)
+return context.DataSet_getDataStreamList(data_set_uid, limit)
