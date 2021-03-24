5aa832c387581e1f15775c2ab6f4fe729e66d8ed
Gabriel Monnerat <gabriel@nexedi.com>
2021-03-24 14:14:01 +0000

    erp5_hal_json_style: Use a better status message instead of Action succeeded

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirectToPortalTypeDocument.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirectToPortalTypeDocument.py
@@ -1,4 +1,10 @@
 portal = context.getPortalObject()
 type_info = portal.portal_types.getTypeInfo(context)
 if type_info is not None and type_info.Base_getSourceVisibility():
-  return type_info.Base_redirect()
+  translate = context.Base_translateString
+  return type_info.Base_redirect(
+    keep_items={
+      "portal_status_message": "%s: %s" % (
+        translate("Portal Type"),
+        translate(type_info.getTitle()))
+    })
