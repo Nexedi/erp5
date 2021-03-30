550be7cb7e9104f2d3b480040e7f31384127e46c
Jérome Perrin <jerome@nexedi.com>
2021-03-30 06:34:09 +0200

    test: use PNG files for reference data
    BMP are not compressed and use more disk space than PNG.
    The trick is that ERP5 uses by default a quality of 75% on images ([this is
    default on propertysheet]) so we need to dump the image with maximum (100%)
    quality.
    
    Update also assertion message to not include the full base64 image in case of
    difference, use a short assertion message and save images in log folder.
    
    [this is default on propertysheet]: https://lab.nexedi.com/nexedi/erp5/blob/2f9b67fae3ac59955231d4191a34134ff1b6079a/product/ERP5/bootstrap/erp5_property_sheets/PropertySheetTemplateItem/portal_property_sheets/HtmlStylePreference/preferred_image_quality_property.xml#L56
    
    erp5_payroll_l10n_fr_test:  wip

--- bt5/erp5_payroll_l10n_fr/TestTemplateItem/portal_components/test.erp5.testSimplifiedPayslipReport.py
+++ bt5/erp5_payroll_l10n_fr/TestTemplateItem/portal_components/test.erp5.testSimplifiedPayslipReport.py
@@ -30,10 +30,8 @@ from DateTime import DateTime
 from PIL import Image
 import cStringIO
 import math
-import io
-import base64
+import os.path
 from Products.Localizer.itools.i18n.accept import AcceptLanguage
-
 
 
 class TestSimplifiedPayslipReport(ERP5TypeTestCase):
@@ -74,21 +72,18 @@ class TestSimplifiedPayslipReport(ERP5TypeTestCase):
     # - rms is ~1.0 if date is 2017-06-07 vs 2017-06-06 with bmp images
     return rms
 
-  def convertToPng(self, img_data):
-    bmp_file = Image.open(io.BytesIO(img_data))
-    img_buff = cStringIO.StringIO()
-    bmp_file.save(img_buff, format='PNG', optimize=True, quality=75)
-    img_data = img_buff.getvalue()
-    return ''.join(['data:image/png;base64,', base64.encodestring(img_data)])
-
   def assertImageRenderingEquals(self, test_image_data, expected_image_data, message="Images rendering differs", max_rms=10.0):
     rms = self.computeImageRenderingRootMeanSquare(test_image_data, expected_image_data)
     if rms <= max_rms:
       return
-    raise AssertionError("%(message)s\nComparing rendered image:\n%(base64_1)s\nWith expected image:\n%(base64_2)s\nRMS: %(rms)s > %(max_rms)s\nAssertionError: %(message)s" % {
+    from Products.ERP5Type.tests.runUnitTest import log_directory
+    if log_directory:
+      with open(os.path.join(log_directory, '%s-expected.png' % self.id()), 'wb') as f:
+        f.write(expected_image_data)
+      with open(os.path.join(log_directory, '%s-actual.png' % self.id()), 'wb') as f:
+        f.write(test_image_data)
+    raise AssertionError("%(message)s\nRMS: %(rms)s > %(max_rms)s\nAssertionError: %(message)s" % {
       "message": message,
-      "base64_1": self.convertToPng(test_image_data),
-      "base64_2": self.convertToPng(expected_image_data),
       "rms": rms,
       "max_rms": max_rms,
     })
@@ -184,5 +179,5 @@ class TestSimplifiedPayslipReport(ERP5TypeTestCase):
     pdf_data = test_pay_sheet_transaction.PaySheetTransaction_printPayslipReport(format="pdf")
     image_source_pdf_doc.setData(pdf_data)
     self.tic()
-    _, bmp = image_source_pdf_doc.convert("bmp", frame=0)
-    self.assertImageRenderingEquals(str(bmp), str(expected_image.getData()))
+    _, png = image_source_pdf_doc.convert("png", frame=0, quality=100)
+    self.assertImageRenderingEquals(str(png), str(expected_image.getData()))
