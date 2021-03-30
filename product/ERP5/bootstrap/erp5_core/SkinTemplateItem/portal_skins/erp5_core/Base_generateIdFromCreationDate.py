57331d8f5b68c973b972b244c926d794c6e50178
Roque Porchetto <roque.porchetto@nexedi.com>
2021-03-30 12:18:22 +0000

    erp5_core: fix unnecessary control on generate id from creation date script

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_generateIdFromCreationDate.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_generateIdFromCreationDate.py
@@ -1,12 +1,9 @@
 # retrieve a date from object and return the new id
 if obj is None:
   obj = context
-date = None
 # first try to get the creation date directly
-if getattr(obj, 'getCreationDate', None) is not None:
-  date = obj.getCreationDate()
+date = obj.getCreationDate()
 if date is None:
-  # if none, try to get date from workflow
   workflow_item_list = context.Base_getWorkflowHistoryItemList('edit_workflow', display=0)
   workflow_item_list.reverse()
   for workflow_item in workflow_item_list:
