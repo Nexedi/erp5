705f594302d660fe19248612d7187bd962760caf
Gabriel Monnerat <gabriel@nexedi.com>
2021-03-31 21:21:14 +0000

    erp5_web_renderjs_ui: Initial implementation of Graphic Gadget
    * Render graphics in RJS without code
    * Supporting the generation of bar graphics using domains

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSection_getPrecacheManifest.py
@@ -2,6 +2,7 @@ web_section = context
 
 # Add all ERP5JS gadget
 url_list = [
+  'echarts-all.js',
   'favicon.ico',
   'font-awesome/font-awesome-webfont.eot',
   'font-awesome/font-awesome-webfont.woff',
@@ -137,6 +138,9 @@ url_list = [
   'gadget_erp5_sort_editor.html',
   'gadget_erp5_sort_editor.js',
   'gadget_global.js',
+  'gadget_graphic.html',
+  'gadget_graphic.js',
+  'gadget_graphic.css',
   'gadget_html5_element.html',
   'gadget_html5_element.js',
   'gadget_html5_input.html',
@@ -163,6 +167,8 @@ url_list = [
   'jiodev.js',
   'renderjs.js',
   'rsvp.js',
+  'unsafe/gadget_field_graph_echarts.html',
+  'unsafe/gadget_field_graph_echarts.js'
 ]
 
 # Add all root gadgets
