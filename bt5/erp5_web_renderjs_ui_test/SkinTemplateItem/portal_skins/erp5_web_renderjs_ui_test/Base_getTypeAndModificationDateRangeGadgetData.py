76880b2e0eea188dc853bd173315810c2c06cc3b
Gabriel Monnerat <gabriel@nexedi.com>
2021-03-31 21:21:14 +0000

    erp5_web_renderjs_ui: Add gadget to render graphics without code

---
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/Base_getTypeAndModificationDateRangeGadgetData.py
@@ -0,0 +1,22 @@
+from urllib import quote
+
+def extract_date(date):
+  return quote(date.strftime("%Y-%m-%d"))
+
+translateString = context.Base_translateString
+
+return [
+  ("group_by", "portal_type"),
+  ("date_range_catalog_key", "modification_date"),
+  ("date_range_list", [
+    # label, start, end
+    ["< 2", extract_date(DateTime()-2), extract_date(DateTime()+1)],
+    ["2 - 7", extract_date(DateTime()-7), extract_date(DateTime()-2)],
+    ["7 - 30", extract_date(DateTime()-30), extract_date(DateTime()-7)],
+    ["> 30", extract_date(DateTime(1900, 1, 1)), extract_date(DateTime()-30)],
+  ]),
+  ("query_by", "getTranslatedPortalType"),
+  ("base_query", "parent_uid:=%s" % context.getUid()),
+  ("graph_title", translateString("Number of events from a range of modification_date")),
+  ("title", translateString("Days"))
+]
