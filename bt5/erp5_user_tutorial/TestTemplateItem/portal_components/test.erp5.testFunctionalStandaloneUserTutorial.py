572c0f3ec1259f6a006163cf5e3cfb2593c30f60
Boxiang Sun <boxiang.sun@nexedi.com>
2021-04-01 13:55:54 +0000

    erp5_user_tutorial: Remove BTs 3rd round

--- bt5/erp5_user_tutorial/TestTemplateItem/portal_components/test.erp5.testFunctionalStandaloneUserTutorial.py
+++ bt5/erp5_user_tutorial/TestTemplateItem/portal_components/test.erp5.testFunctionalStandaloneUserTutorial.py
@@ -147,10 +147,6 @@ class TestZeleniumStandaloneUserTutorial(ERP5TypeFunctionalTestCase):
       Return the list of business templates.
     """
     return ('erp5_core_proxy_field_legacy', 'erp5_full_text_mroonga_catalog',
-            'erp5_base', 'erp5_ui_test_core',
-            'erp5_dhtml_style',
-            'erp5_jquery', 'erp5_jquery_ui',
-            'erp5_simulation',
             'erp5_configurator',
             'erp5_configurator_standard',
             'erp5_configurator_standard_solver',
