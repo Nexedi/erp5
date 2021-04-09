4aae6ab553f266470d36774d737d55b2f0b4a23b
Boxiang Sun <boxiang.sun@nexedi.com>
2021-04-09 15:46:05 +0000

    erp5_configurator_standard: Activate all available language after configuration
    Then the user can select the prefered language in the ERP5JS UI

--- bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_afterConfiguration.py
+++ bt5/erp5_configurator_standard/SkinTemplateItem/portal_skins/erp5_configurator_standard/BusinessConfiguration_afterConfiguration.py
@@ -90,6 +90,10 @@ for business_application_category_id, module_ids in module_business_application_
     if module is not None:
       module.edit(business_application = business_application_category_id)
 
+# activate all available languages to allow user can select them in the ERP5JS UI
+available_language_list = list(portal.Localizer.get_supported_languages())
+portal.web_site_module.osoe_runner.edit(available_language_set = available_language_list)
+
 print "Indexing translations"
 portal.ERP5Site_updateTranslationTable()
 
