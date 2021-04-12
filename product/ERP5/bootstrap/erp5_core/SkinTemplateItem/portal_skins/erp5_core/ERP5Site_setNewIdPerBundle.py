ba949a3405debd657da9bf41a15dc7a9e122c8b0
Roque Porchetto <roque.porchetto@nexedi.com>
2021-04-12 19:00:41 +0000

    erp5_core: fix setid script

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_setNewIdPerBundle.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_setNewIdPerBundle.py
@@ -4,6 +4,6 @@ method = getattr(context, method)
 for id_ in id_list:
   ob = folder.get(id_)
   new_id = method(ob)
-  ob.setDefaultActivateParameterDict(activate_kw)
-  if new_id != ob.getId():
+  if new_id != id_:
+    ob.setDefaultActivateParameterDict(activate_kw)
     ob.setId(new_id)
