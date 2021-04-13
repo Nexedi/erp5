30386e720ed2739d911a7aac3f474482f9ffb604
Gabriel Monnerat <gabriel@nexedi.com>
2021-04-13 00:49:09 +0000

    erp5_web_renderjs_ui: Display dev action to gadget field and avoid duplicated actions to parallel field

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_label_field_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_label_field_js.js
@@ -77,15 +77,19 @@
   }
 
   function addDeveloperAction(class_name, title_href, title, root_element) {
-    var field_href = domsugar("a", {
-      "class": class_name,
-      href: title_href,
-      title: title
-    });
+    var div,
+      field_href = domsugar("a", {
+        "class": class_name,
+        href: title_href,
+        title: title
+      });
     if (root_element.constructor === HTMLLabelElement) {
       root_element.appendChild(field_href);
-    } else {
-      root_element.insertBefore(field_href, root_element.querySelector("div"));
+      return;
+    }
+    div = root_element.querySelector("div");
+    if (div) {
+      root_element.insertBefore(field_href, div);
     }
   }
 
@@ -230,7 +234,8 @@
               field_gadget = declared_gadget;
             });
 
-          if (field_json && gadget.state.options.development_link !== false) {
+          if (field_json && gadget.state.options.development_link !== false &&
+              (gadget.state.label === true || gadget.state.label_text)) {
             queue
               .push(function () {
                 return gadget.getTranslationList([
@@ -245,9 +250,10 @@
 
                 if (gadget.state.label === true) {
                   root_element = gadget.props.label_element;
-                } else {
-                  root_element = field_gadget.element;
+                } else if (gadget.state.label_text) {
+                  root_element = gadget.element;
                 }
+
                 if (field_json.hasOwnProperty('edit_field_href') &&
                     !root_element.querySelector(".edit-field")) {
                   addDeveloperAction(
@@ -292,7 +298,6 @@
                     root_element.removeChild(field);
                   }
                 }
-                return;
               });
           }
           return queue
