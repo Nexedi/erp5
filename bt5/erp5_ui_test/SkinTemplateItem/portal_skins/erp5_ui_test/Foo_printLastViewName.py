e776280d123cd69cbf9a65738c84bc2e744e5d1b
Romain Courteaud <romain@nexedi.com>
2021-04-15 12:48:25 +0000

    erp5_*: use Base_renderForm API to change the REQUEST

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_printLastViewName.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_printLastViewName.py
@@ -2,5 +2,4 @@
 #
 # Print action expects the previous view to be accessible in `form_id`
 # and it prints it out in both UI compatible way - as a redirect message.
-context.REQUEST.form['last_form_id'] = form_id
-return context.Base_renderForm('Foo_viewPrintoutForm')
+return context.Base_renderForm('Foo_viewPrintoutForm', keep_items={'last_form_id': form_id})
