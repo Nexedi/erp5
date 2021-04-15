6a405559f677404992946c5e4a553e6b9c113819
Romain Courteaud <romain@nexedi.com>
2021-04-15 12:59:38 +0000

    erp5_crm: keep_items Base_renderForm

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Event_updateCreateResponse.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Event_updateCreateResponse.py
@@ -1,5 +1,6 @@
 """Preview the response from notification message for event create response dialog.
 """
+keep_items = None
 if response_event_notification_message:
   temp_event = context.getPortalObject().event_module.newContent(
     temp_object=True,
@@ -16,13 +17,12 @@ if response_event_notification_message:
      reference=response_event_notification_message,
      substitution_method_parameter_dict=dict(reply_body=context.getReplyBody(),
                                              reply_subject=context.getReplySubject()))
-                                             
-  # XXX this relies on formulator internals, we force the variables in request and
-  # re-render the form.
-  request = container.REQUEST
-  request.form['your_response_event_notification_message'] = ''
-  request.form['your_response_event_title'] = temp_event.getTitle()
-  request.form['your_response_event_text_content'] = temp_event.getTextContent()
-  request.form['your_response_event_resource'] = temp_event.getResource()
 
-return context.Base_renderForm('Event_viewCreateResponseDialog')
+  keep_items = {
+    'your_response_event_notification_message': '',
+    'your_response_event_title': temp_event.getTitle(),
+    'your_response_event_text_content': temp_event.getTextContent(),
+    'your_response_event_resource': temp_event.getResource()
+  }
+
+return context.Base_renderForm('Event_viewCreateResponseDialog', keep_items=keep_items)
