57fcbcec848ce204eeb06960797de24d6e027ea0
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2021-04-20 21:18:08 +0900

    ERP5Workflow: Remove unused variable_default_value and use variable_default_expression instead (!1378).

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/WorkflowVariable_getNonEvaluatedVariableDefault.py
+++
@@ -1,6 +0,0 @@
-default_value = context.getVariableDefaultExpression(evaluate=0)
-if default_value is None:
-  # variable_default_expression takes precedence over "static" variable_default_value property
-  default_value = repr(context.getVariableDefaultValue())
-
-return default_value
