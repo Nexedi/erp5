c6d044a86803e066261bae622e8459a832793c33
aurel <aurel@nexedi.com>
2021-04-21 11:04:08 +0200

    apply WSGIPublisher patch to zope2

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -24,7 +24,11 @@ import six
 from Products.ERP5Type import WITH_LEGACY_WORKFLOW
 
 # Load all monkey patches
-#from Products.ERP5Type.patches import WSGIPublisher
+try:
+  from ZPublisher.WSGIPublisher import _MODULES
+except ImportError:
+  # BBB Zope2
+  from Products.ERP5Type.patches import WSGIPublisher
 from Products.ERP5Type.patches import HTTPRequest
 from Products.ERP5Type.patches import AccessControl_patch
 from Products.ERP5Type.patches import Restricted
