8496bcaee9ccc1a4de01b53887b4261345b7c143
Jérome Perrin <jerome@nexedi.com>
2021-04-22 02:13:26 +0000

    payment_mean_ui_test: new business template
    Functional test for erp5_payment_mean

---
+++ bt5/erp5_payment_mean_ui_test/SkinTemplateItem/portal_skins/erp5_payment_mean_ui_test/ERP5Site_deletePaymentMeanUiTestData.py
@@ -0,0 +1,20 @@
+portal = context.getPortalObject()
+
+for portal_type, document_id_list in (
+  ('Organisation', ('erp5_payment_mean_ui_test_organisation', )),
+  ('Payment Transaction Group', ('erp5_payment_mean_ui_test_payment_transaction_group',),),
+  ('Payment Transaction', (
+      'erp5_payment_mean_ui_test_incoming_payment',
+      'erp5_payment_mean_ui_test_outgoing_payment',
+      'erp5_payment_mean_ui_test_second_outgoing_payment',
+      'erp5_payment_mean_ui_test_wrong_payment_mode_outgoing_payment',
+      'erp5_payment_mean_ui_test_wrong_currency_outgoing_payment',
+  ),),
+):
+  module = portal.getDefaultModule(portal_type)
+  for document_id in document_id_list:
+    if getattr(module, document_id, None) is not None:
+      module.manage_delObjects([document_id])
+
+
+return "Deleted Successfully."
