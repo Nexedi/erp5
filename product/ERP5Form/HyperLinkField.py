5356bdc5ad9d683e4876b932268e2a44e8d44d85
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2021-04-23 09:13:17 +0000

    ERP5Form: support relative_url in HyperLinkField.

--- product/ERP5Form/HyperLinkField.py
+++ product/ERP5Form/HyperLinkField.py
@@ -6,7 +6,7 @@ from Products.Formulator.DummyField import fields
 from Products.Formulator import Validator
 
 class HyperLinkWidget(LabelWidget):
-    property_names = LabelWidget.property_names + ['href']
+    property_names = LabelWidget.property_names + ['href'] + ['is_relative']
 
     href = fields.LinkField('href',
                            title='Href',
@@ -14,12 +14,21 @@ class HyperLinkWidget(LabelWidget):
                            default="",
                            required=1)
 
+    is_relative = fields.CheckBoxField('is_relative',
+                                       title='Relative ?',
+                                       description='Check if Href value is the relative URL of an ERP5 object',
+                                       default="",
+                                       required=0)
+
     def render(self, field, key, value, REQUEST, render_prefix=None):
         return self.render_view(field, value, REQUEST, render_prefix)
 
     def render_view(self, field, value, REQUEST=None, render_prefix=None):
+        href = field.get_value('href')
+        if field.get_value('is_relative'):
+            href = field.getPortalObject().unrestrictedTraverse(href).absolute_url()
         return render_element("a",
-                              href=field.get_value('href'),
+                              href=href,
                               css_class=field.get_value('css_class'),
                               contents=field.get_value('default'),
                               extra=field.get_value('extra'))
