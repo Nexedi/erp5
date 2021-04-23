105c8e751e11c23fd7771b8984d167e719eafe14
Jérome Perrin <jerome@nexedi.com>
2021-04-23 04:32:14 +0200

    payment_mean: New mode to select payments
    Instead of selecting already stopped payments, introduce a new "mode" field in
    the dialog, where user can choose the previous behavior of selecting stopped
    payments, or a new behavior where user would select planned or confirmed payments,
    in that case the payments will be automatically stopped before being added to
    the group.

--- bt5/erp5_payment_mean_ui_test/SkinTemplateItem/portal_skins/erp5_payment_mean_ui_test/ERP5Site_createPaymentMeanUiTestData.py
+++ bt5/erp5_payment_mean_ui_test/SkinTemplateItem/portal_skins/erp5_payment_mean_ui_test/ERP5Site_createPaymentMeanUiTestData.py
@@ -7,6 +7,14 @@ for rule in portal.portal_rules.contentValues():
   if rule.getValidationState() != 'validated':
     rule.validate()
 
+# validate other documents used in this test
+for document in (
+    portal.account_module.equity,
+    portal.account_module.bank,
+    portal.currency_module.euro, ):
+  if document.getValidationState() != 'validated':
+    document.validate()
+
 organisation_id = "erp5_payment_mean_ui_test_organisation"
 organisation = portal.organisation_module.newContent(
     portal_type='Organisation',
@@ -35,94 +43,40 @@ payment_mean = portal.payment_transaction_group_module.newContent(
 )
 payment_mean.open()
 
-incoming_payment = portal.accounting_module.newContent(
-    portal_type='Payment Transaction',
-    id="erp5_payment_mean_ui_test_incoming_payment",
-    title="Incoming payment",
-    source_section_value=organisation,
-    destination_section_value=organisation,
-    source_payment_value=bank_account,
-    start_date=DateTime('2019/10/20'),
-    resource_value=portal.currency_module.euro,
-    payment_mode_value=portal.portal_categories.payment_mode.cash,
-    specialise_value=portal.business_process_module.erp5_default_business_process,
-)
-incoming_payment.bank.edit(
-    source_value=portal.account_module.bank,
-    source_debit=100,
-)
-incoming_payment.stop()
-
-outgoing_payment = portal.accounting_module.newContent(
-    portal_type='Payment Transaction',
-    id="erp5_payment_mean_ui_test_outgoing_payment",
-    title="Outgoing payment",
-    source_section_value=organisation,
-    destination_section_value=organisation,
-    source_payment_value=bank_account,
-    start_date=DateTime('2019/10/20'),
-    resource_value=portal.currency_module.euro,
-    payment_mode_value=portal.portal_categories.payment_mode.cash,
-    specialise_value=portal.business_process_module.erp5_default_business_process,
-)
-outgoing_payment.bank.edit(
-    source_value=portal.account_module.bank,
-    source_credit=100,
-)
-outgoing_payment.stop()
-
-second_outgoing_payment = portal.accounting_module.newContent(
-    portal_type='Payment Transaction',
-    id="erp5_payment_mean_ui_test_second_outgoing_payment",
-    title="Second outgoing payment",
-    source_section_value=organisation,
-    destination_section_value=organisation,
-    source_payment_value=bank_account,
-    start_date=DateTime('2019/10/21'),
-    resource_value=portal.currency_module.euro,
-    payment_mode_value=portal.portal_categories.payment_mode.cash,
-    specialise_value=portal.business_process_module.erp5_default_business_process,
-)
-second_outgoing_payment.bank.edit(
-    source_value=portal.account_module.bank,
-    source_credit=100,
-)
-second_outgoing_payment.stop()
-
-wrong_payment_mode_outgoing_payment = portal.accounting_module.newContent(
-    portal_type='Payment Transaction',
-    id="erp5_payment_mean_ui_test_wrong_payment_mode_outgoing_payment",
-    title="Wrong Payment Mode outgoing payment",
-    source_section_value=organisation,
-    destination_section_value=organisation,
-    source_payment_value=bank_account,
-    start_date=DateTime('2019/10/22'),
-    resource_value=portal.currency_module.euro,
-    payment_mode_value=portal.portal_categories.payment_mode.check,
-    specialise_value=portal.business_process_module.erp5_default_business_process,
-)
-wrong_payment_mode_outgoing_payment.bank.edit(
-    source_value=portal.account_module.bank,
-    source_credit=100,
-)
-wrong_payment_mode_outgoing_payment.stop()
-
-wrong_currency_outgoing_payment = portal.accounting_module.newContent(
-    portal_type='Payment Transaction',
-    id="erp5_payment_mean_ui_test_wrong_currency_outgoing_payment",
-    title="Wrong Currency outgoing payment",
-    source_section_value=organisation,
-    destination_section_value=organisation,
-    source_payment_value=bank_account,
-    start_date=DateTime('2019/10/23'),
-    resource_value=portal.currency_module.yen,
-    payment_mode_value=portal.portal_categories.payment_mode.cash,
-    specialise_value=portal.business_process_module.erp5_default_business_process,
-)
-wrong_currency_outgoing_payment.bank.edit(
-    source_value=portal.account_module.bank,
-    source_credit=100,
-)
-wrong_currency_outgoing_payment.stop()
-
+for id_, date, resource_value, payment_mode_value, quantity, state, consistent in (
+    ('erp5_payment_mean_ui_test_incoming_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, -100, 'stopped', True),
+    ('erp5_payment_mean_ui_test_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, 100, 'stopped', True),
+    ('erp5_payment_mean_ui_test_second_outgoing_payment', DateTime('2019/10/21'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, 100, 'stopped', True),
+    ('erp5_payment_mean_ui_test_planned_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, 100, 'planned', True),
+    ('erp5_payment_mean_ui_test_confirmed_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, 100, 'confirmed', True),
+    ('erp5_payment_mean_ui_test_confirmed_not_consistent_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.cash, 100, 'confirmed', False),
+    ('erp5_payment_mean_ui_test_wrong_payment_mode_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.euro, portal.portal_categories.payment_mode.check, 100, 'stopped', True),
+    ('erp5_payment_mean_ui_test_wrong_currency_outgoing_payment', DateTime('2019/10/20'), portal.currency_module.yen, portal.portal_categories.payment_mode.cash, 100, 'stopped', True),
+):
+  payment = portal.accounting_module.newContent(
+      portal_type='Payment Transaction',
+      id=id_,
+      source_section_value=organisation,
+      source_payment_value=bank_account,
+      start_date=date,
+      resource_value=resource_value,
+      payment_mode_value=payment_mode_value,
+      specialise_value=portal.business_process_module.erp5_default_business_process,
+  )
+  payment.bank.edit(
+      source_value=portal.account_module.bank,
+      quantity=quantity
+  )
+  payment.receivable.edit(
+      source_value=portal.account_module.equity if consistent else None,
+      quantity=-quantity
+  )
+  if state == 'planned':
+    payment.plan()
+  elif state == 'confirmed':
+    payment.confirm()
+  elif state == 'stopped':
+    payment.stop()
+  else:
+    raise ValueError("unknown state", state)
 return "Test Data Created."
