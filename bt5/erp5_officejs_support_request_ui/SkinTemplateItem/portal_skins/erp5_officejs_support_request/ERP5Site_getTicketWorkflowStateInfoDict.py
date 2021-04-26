fc0fa324a06c62b5e57f961047e370737933d970
Jérome Perrin <jerome@nexedi.com>
2021-04-26 08:48:41 +0200

    officejs_support_request_ui: adjust script for new workflow API
    state.getId() now have a prefix, we want to use state.getReference() here.

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
@@ -17,6 +17,6 @@ for state in workflow.getStateValueList():
     state_title = unicode(translateString(
       '%s [state in %s]' % (state_title, workflow.getId()),
       default=unicode(translateString(state_title))))
-  info[state.getId()] = state_title
+  info[state.getReference()] = state_title
 
 return info
