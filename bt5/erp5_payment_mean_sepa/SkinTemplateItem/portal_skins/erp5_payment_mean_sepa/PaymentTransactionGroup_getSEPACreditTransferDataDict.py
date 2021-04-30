104f9a94737f147df89646f5af38957a7854cb33
Jérome Perrin <jerome@nexedi.com>
2021-04-30 09:52:32 +0200

    payment_mean_sepa: new business template
    Support generating pain.001.001.02 credit transfer from payment transaction
    groups.

---
+++ bt5/erp5_payment_mean_sepa/SkinTemplateItem/portal_skins/erp5_payment_mean_sepa/PaymentTransactionGroup_getSEPACreditTransferDataDict.py
@@ -0,0 +1,105 @@
+section = context.getSourceSectionValue()
+section_bank_account = context.getSourcePaymentValue()
+assert section_bank_account.getValidationState() == 'validated'
+
+currency = context.getPriceCurrencyReference() or context.getPriceCurrencyTitle()
+assert currency.upper() == 'EUR', 'Wrong currency %s' % currency
+
+
+data_dict = {
+  'GrpHdr': {
+    'MsgId': context.getSourceReference(),
+    'CreDtTm': context.getStopDate().toZone('UTC').asdatetime().isoformat(),
+    'InitgPty': {
+      'Nm': section.getCorporateName() or section.getTitle(),
+      'Id': {
+        'OrgId': {
+          'PrtryId': {
+            'Id': None, # TODO is this really required ?
+          }
+        }
+      }
+    }
+  },
+  'PmtInf': {
+    'PmtInfId': context.getReference() or context.getSourceReference(),
+    'ReqdExctnDt':context.getStopDate().toZone('UTC').asdatetime().strftime('%Y-%m-%d'),
+    'Dbtr': {
+      'Nm':  section.getCorporateName() or section.getTitle(),
+    },
+    'DbtrAcct': {
+      'Id': {
+        'IBAN': section_bank_account.getIban().replace(' ', '').replace('\t', ''),
+      }
+    },
+    'DbtrAgt': {
+      'FinInstnId': {
+        'BIC': section_bank_account.getBicCode().strip(),
+      }
+    }
+  },
+}
+
+
+# This does not support payment transactions with multiple lines.
+# To prevent generating a file where the same end to end id exists twice,
+# we just check that EndToEndId are unique in this file.
+end_to_end_id_set = set([])
+
+transaction_list = []
+total = 0
+for brain in context.PaymentTransactionGroup_getAccountingTransactionLineList():
+  amount = round(-brain.total_quantity, 2)
+  assert amount > 0
+  total += amount
+
+  transaction_line = brain.getObject()
+  transaction = transaction_line.getExplanationValue()
+
+  if brain.payment_uid == transaction_line.getSourcePaymentUid():
+    creditor_entity = transaction_line.getDestinationSectionValue()
+    creditor_bank_account = transaction_line.getDestinationPaymentValue()
+  else:
+    assert brain.payment_uid == transaction_line.getDestinationPaymentUid()
+    creditor_entity = transaction_line.getSourceSectionValue()
+    creditor_bank_account = transaction_line.getSourcePaymentValue() 
+  assert creditor_bank_account.getValidationState() == 'validated', \
+    '%s is not validated' % creditor_bank_account.getRelativeUrl() 
+
+  end_to_end_id = transaction.PaymentTransaction_getOrGenerateReference()
+  assert end_to_end_id not in end_to_end_id_set
+  end_to_end_id_set.add(end_to_end_id)
+
+  transaction_list.append(
+    {
+      'PmtId': {
+        'EndToEndId': end_to_end_id,
+      },
+      'RmtInf': {
+        'Ustrd': transaction_line.AccountingTransactionLine_getSEPACreditTransferRemittanceInformation(),
+      },
+      'Cdtr': {
+        'Nm': creditor_entity.getCorporateName() or creditor_entity.getTitle(),
+        'PstlAdr': (creditor_entity.getDefaultAddressText() or '').splitlines(),
+        'Ctry': creditor_entity.getDefaultAddressRegion() and creditor_entity.getDefaultAddressValue().getRegionReference(),
+      },
+      'Amt': {
+        'InstdAmt': '%0.2f' % amount
+      },
+      'CdtrAcct': {
+         'Id': {
+           'IBAN': creditor_bank_account.getIban().replace(' ', '').replace('\t', ''),
+         }
+      },
+      'CdtrAgt': {
+        'FinInstnId': {
+          'BIC': section_bank_account.getBicCode().strip(),
+        }
+      }
+    }
+  )
+
+data_dict['GrpHdr']['NbOfTxs'] = len(transaction_list)
+data_dict['GrpHdr']['CtrlSum'] = '%0.2f' % total
+data_dict['transaction_list'] = transaction_list
+return data_dict
