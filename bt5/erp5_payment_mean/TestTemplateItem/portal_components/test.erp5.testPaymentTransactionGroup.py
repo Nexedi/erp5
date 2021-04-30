d41d8f948f382f9fc08dd0b378db29da05e9ac20
Jérome Perrin <jerome@nexedi.com>
2021-04-30 09:51:28 +0200

    payment_mean: constraint for PaymentTransactionGroup
    This constraint will have to be enabled by configurator/upgrader.

--- bt5/erp5_payment_mean/TestTemplateItem/portal_components/test.erp5.testPaymentTransactionGroup.py
+++ bt5/erp5_payment_mean/TestTemplateItem/portal_components/test.erp5.testPaymentTransactionGroup.py
@@ -25,7 +25,10 @@
 #
 ##############################################################################
 
+from DateTime import DateTime
+
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
+
 
 class TestPaymentTransactionGroup(ERP5TypeTestCase):
   def test_source_reference_generated(self):
@@ -42,3 +45,99 @@ class TestPaymentTransactionGroup(ERP5TypeTestCase):
     # a new source reference
     self.assertNotEqual(
         ptg.getSourceReference(), cloned_ptg.getSourceReference())
+
+
+class TestPaymentTransactionGroupConstraint(ERP5TypeTestCase):
+  def afterSetUp(self):
+    ti = self.portal.portal_types['Payment Transaction Group']
+    ti.setTypePropertySheetList(
+        ti.getTypePropertySheetList() + ['PaymentTransactionGroupConstraint'])
+    self.commit()
+
+  def beforeTearDown(self):
+    self.abort()
+    ti = self.portal.portal_types['Payment Transaction Group']
+    ti.setTypePropertySheetList(
+        [ps for ps in ti.getTypePropertySheetList() if ps != 'PaymentTransactionGroupConstraint'])
+    self.commit()
+  
+  def test_constraints(self):
+    ptg = self.portal.payment_transaction_group_module.newContent(
+        portal_type='Payment Transaction Group',
+        stop_date=None,
+    )
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Currency must be defined',
+          'Date must be defined',
+          'Payment Mode must be defined',
+          'Payment Transaction Group Type must be defined',
+          'Section bank account must be defined',
+          'Section must be defined',
+        ],
+    )
+
+    ptg.setPriceCurrencyValue(
+        self.portal.currency_module.newContent(portal_type='Currency'))
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Date must be defined',
+          'Payment Mode must be defined',
+          'Payment Transaction Group Type must be defined',
+          'Section bank account must be defined',
+          'Section must be defined',
+        ],
+    )
+
+    ptg.setStopDate(DateTime(2021, 1, 1))
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Payment Mode must be defined',
+          'Payment Transaction Group Type must be defined',
+          'Section bank account must be defined',
+          'Section must be defined',
+        ],
+    )
+
+    ptg.setPaymentModeValue(
+        self.portal.portal_categories.payment_mode.newContent(portal_type='Category'))
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Payment Transaction Group Type must be defined',
+          'Section bank account must be defined',
+          'Section must be defined',
+        ],
+    )
+
+    ptg.setPaymentTransactionGroupTypeValue(
+        self.portal.portal_categories.payment_transaction_group_type.incoming)
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Section bank account must be defined',
+          'Section must be defined',
+        ],
+    )
+
+    section = self.portal.organisation_module.newContent(
+        portal_type='Organisation'
+    )
+    ptg.setSourceSectionValue(section)
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [
+          'Section bank account must be defined',
+        ],
+    )
+
+    bank_account = section.newContent(
+        portal_type='Bank Account'
+    )
+    ptg.setSourcePaymentValue(bank_account)
+    self.assertEqual(
+        sorted([str(m.getMessage()) for m in ptg.checkConsistency()]),
+        [])
