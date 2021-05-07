0362171d87a11609e9e3e214678bc862b3634fb6
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-07 14:48:23 +0000

    erp5_officejs_appstore_base: improve action

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationUrl.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationUrl.py
@@ -1,1 +1,8 @@
-return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url changed!"))
+software_product = context
+web_site = software_product.getFollowUpValue(portal_type="Web Section")
+
+software_product.setReference(new_url_domain)
+web_site.setId(new_url_domain)
+software_product.setFollowUpValue(web_site)
+
+return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url updated"))
