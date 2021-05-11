5f8ac014d2f6da804f0dde031b27d63b58e8db26
Jérome Perrin <jerome@nexedi.com>
2021-05-11 08:08:49 +0200

    payment_mean(_sepa): generate reference when grouping
    For SEPA files "End to End ID" we need a reference on payment transactions.
    In the first prototype, the reference was set at the time of generating the
    file, but it's a non common behaviour to generate reference when producing
    reports, so we changed to ensuring payment has reference when adding to the
    group.

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/AccountingTransactionLine_addPaymentTransactionGroup.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/AccountingTransactionLine_addPaymentTransactionGroup.py
@@ -1,6 +1,8 @@
 """Adds the payment transaction group to the already aggregated items on this line.
 
-This script has a proxy role to be able to modify delivered lines
+Also ensures that the payment transaction has a reference.
+
+This script has a proxy role to be able to modify delivered lines and to call generateReference.
 """
 portal = context.getPortalObject()
 
@@ -8,3 +10,4 @@ assert context.getPortalType() in portal.getPortalAccountingMovementTypeList()
 
 context.setDefaultActivateParameterDict({"activate_kw": activate_kw})
 context.setAggregate(aggregate, portal_type='Payment Transaction Group')
+context.getParentValue().getTypeBasedMethod('generateReference')()
--- bt5/erp5_payment_mean_sepa/SkinTemplateItem/portal_skins/erp5_payment_mean_sepa/PaymentTransaction_getOrGenerateReference.py
+++
@@ -1,4 +0,0 @@
-"""Returns an unique reference for this payment transaction. If it does not already have a reference, a new one is generated.
-"""
-context.getTypeBasedMethod('generateReference')()
-return context.getReference()
