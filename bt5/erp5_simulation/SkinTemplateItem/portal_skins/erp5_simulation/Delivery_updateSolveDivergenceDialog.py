0a043f4e91e546d4c42e8e47124f34732bba9710
Romain Courteaud <romain@nexedi.com>
2021-05-11 14:38:24 +0000

    erp5_simulation: use Base_renderForm api

--- bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
+++ bt5/erp5_simulation/SkinTemplateItem/portal_skins/erp5_simulation/Delivery_updateSolveDivergenceDialog.py
@@ -7,20 +7,21 @@ if len(line_list) == 0:
   kw["keep_items"] = {'portal_status_message': translateString("Workflow state may have been updated by other user. Please try again.")}
   return context.Base_redirect(form_id, **kw)
 
+keep_items = {
+  'your_dialog_updated': '1',
+}
 for listbox_dict in listbox:
   line = [x for x in line_list if x.getPath() == listbox_dict['listbox_key']][0]
   uid = line.getUid()
   for prop in ('solver', 'solver_configuration', 'delivery_solver', 'comment',):
     value = listbox_dict.get(prop, None)
     key = 'field_listbox_%s_%s' % (prop, uid)
-    request.form[key] = request.other[key] = value
+    keep_items[key] = value
     if prop == 'solver_configuration':
       if value is not None:
         line.updateConfiguration(**value.as_dict())
     else:
       line.setProperty(prop, value)
 
-return context.Base_renderForm('Delivery_viewSolveDivergenceDialog', keep_items={
-  'your_dialog_updated': '1',
-  'listbox': listbox
-})
+keep_items['listbox'] = listbox
+return context.Base_renderForm('Delivery_viewSolveDivergenceDialog', keep_items=keep_items)
