6903c3eacc2049a3228b058c465369e0cfdd5fb1
Romain Courteaud <romain@nexedi.com>
2021-05-12 14:44:25 +0000

    erp5_web_renderjs_ui: show user on the logout page

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_logoutform_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_erp5_pt_logoutform_js.js
@@ -1,6 +1,6 @@
-/*global window, rJS, UriTemplate */
+/*global window, rJS, UriTemplate, domsugar, RSVP */
 /*jslint indent: 2, maxerr: 3, nomen: true */
-(function (window, rJS, UriTemplate) {
+(function (window, rJS, UriTemplate, domsugar, RSVP) {
   "use strict";
 
   rJS(window)
@@ -9,10 +9,12 @@
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("redirect", "redirect")
     .declareAcquiredMethod("getUrlFor", "getUrlFor")
-    .declareAcquiredMethod("getUrlForList", "getUrlForList")
+    .declareAcquiredMethod("getUrlForDict", "getUrlForDict")
     .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
-    .declareAcquiredMethod("translate", "translate")
+    .declareAcquiredMethod("getTranslationDict", "getTranslationDict")
     .declareAcquiredMethod("updateHeader", "updateHeader")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -20,25 +22,79 @@
     .declareMethod("render", function () {
       var gadget = this;
 
-      return gadget.getUrlForList([
-        // Back url
-        {command: 'display'},
-        // Change language
-        {command: 'display', options: {page: 'language'}}
-      ])
-        .push(function (url_list) {
-          return gadget.updateHeader({
-            page_title: 'Logout',
-            page_icon: 'power-off',
-            front_url: url_list[0],
-            language_url: url_list[1]
-          });
+      return new RSVP.Queue(RSVP.hash({
+        translation: gadget.getTranslationDict([
+          'Confirm',
+          'User'
+        ]),
+        me: gadget.getSetting('me')
+          .push(function (me) {
+            if (me !== undefined) {
+              return gadget.jio_allDocs({
+                query: 'relative_url:"' + me + '"',
+                select_list: ['title']
+              });
+            }
+          })
+          .push(function (result) {
+            var user;
+            // Calculate user name
+            if (result === undefined) {
+              user = "Who are you?";
+            } else {
+              user = result.data.rows[0].value.title;
+            }
+            return user;
+          }),
+        erp5_form: gadget.getDeclaredGadget("erp5_form"),
+        url_dict: gadget.getUrlForDict({
+          // Back url
+          back: {command: 'display'},
+          // Change language
+          change_language: {command: 'display', options: {page: 'language'}}
         })
-        .push(function () {
-          return gadget.translate('Confirm');
-        })
-        .push(function (translated_text) {
-          gadget.element.querySelector('input').value = translated_text;
+      }))
+        .push(function (result_dict) {
+          domsugar(gadget.element.querySelector('.dialog_button_container'), [
+            domsugar('input', {name: 'action_update',
+                               type: 'submit',
+                               value: result_dict.translation.Confirm})
+          ]);
+          console.log(result_dict);
+          return RSVP.all([
+            gadget.updateHeader({
+              page_title: 'Logout',
+              page_icon: 'power-off',
+              front_url: result_dict.url_dict.back,
+              language_url: result_dict.url_dict.change_language
+            }),
+
+            result_dict.erp5_form.render({
+              erp5_document: {"_embedded": {"_view": {
+                'User': {
+                  "default": result_dict.me,
+                  "editable": 0,
+                  "key": "field_user",
+                  "title": result_dict.translation.User,
+                  "type": "StringField"
+                }
+              }},
+                "_links": {
+                  "type": {
+                    // form_list display portal_type in header
+                    name: ""
+                  }
+                }
+                },
+              form_definition: {
+                group_list: [[
+                  "left",
+                  [["User"]]
+                ]]
+              }
+            })
+
+          ]);
         });
     })
 
@@ -67,4 +123,4 @@
     .declareMethod("triggerSubmit", function () {
       return;
     });
-}(window, rJS, UriTemplate));
+}(window, rJS, UriTemplate, domsugar, RSVP));
