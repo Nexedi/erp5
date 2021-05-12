e707eedb751c54607b530fa69b5f59cf9446ffd5
Romain Courteaud <romain@nexedi.com>
2021-05-12 09:56:25 +0000

    erp5_ui_test: request test

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_updateManuallyRequest.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_updateManuallyRequest.py
@@ -4,17 +4,10 @@ custom_integer += 1
 custom_listfield += '$'
 custom_radio += '@'
 
-request = container.REQUEST
-request.form['your_custom_string'] = custom_string
-request.form['your_custom_textarea'] = custom_textarea
-request.form['your_custom_integer'] = custom_integer
-request.form['your_custom_listfield'] = custom_listfield
-request.form['your_custom_radio'] = custom_radio
-
-# request.set('your_custom_variable', your_custom_variable)
-# request.set('previous_custom_variable', custom_variable)
-
 return context.Base_renderForm("Foo_viewManuallyUpdatedRequestDialog", keep_items={
-  # 'your_custom_variable': your_custom_variable,
-  # 'previous_custom_variable': custom_variable
+  'your_custom_string': custom_string,
+  'your_custom_textarea': custom_textarea,
+  'your_custom_integer': custom_integer,
+  'your_custom_listfield': custom_listfield,
+  'your_custom_radio': custom_radio
 })
