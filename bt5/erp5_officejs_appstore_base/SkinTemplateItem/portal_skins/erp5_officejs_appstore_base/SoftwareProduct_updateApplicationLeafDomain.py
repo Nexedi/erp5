7a2ab4e736bcc920053f8a628a55951b2af2bb14
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-17 20:09:28 +0000

    erp5_officejs_appstore_base: use edit instead of set

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
@@ -1,8 +1,8 @@
 software_product = context
 web_site = software_product.getFollowUpValue(portal_type="Web Section")
 
-software_product.setReference(new_leaf_domain)
-web_site.setId(new_leaf_domain)
+software_product.edit(reference=new_leaf_domain)
+web_site.edit(id=new_leaf_domain)
 software_product.setFollowUpValue(web_site)
 
 return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url updated"))
