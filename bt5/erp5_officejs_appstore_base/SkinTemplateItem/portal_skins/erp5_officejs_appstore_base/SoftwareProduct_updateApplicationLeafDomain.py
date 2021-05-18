35157608a364bf663f99e22d1463d22a34194f70
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-18 14:21:24 +0000

    erp5_officejs_appstore_base: fix test

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
@@ -2,6 +2,6 @@ software_product = context
 web_site = software_product.getFollowUpValue(portal_type="Web Section")
 
 software_product.edit(reference=new_leaf_domain)
-web_site.edit(id=new_leaf_domain)
+web_site.setId(new_leaf_domain)
 
 return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url updated"))
