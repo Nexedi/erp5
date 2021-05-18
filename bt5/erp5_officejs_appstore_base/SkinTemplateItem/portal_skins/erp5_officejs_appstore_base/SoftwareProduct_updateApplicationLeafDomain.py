fbfd03f46e1be612e874b6a1884f78ae3529ba51
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-18 13:07:37 +0000

    erp5_officejs_appstore_base: remove followUp fix

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
@@ -3,6 +3,5 @@ web_site = software_product.getFollowUpValue(portal_type="Web Section")
 
 software_product.edit(reference=new_leaf_domain)
 web_site.edit(id=new_leaf_domain)
-software_product.setFollowUpValue(web_site)
 
 return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url updated"))
