4094cc873ebee8976b6eaed6f8e9a71194054e17
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-19 18:52:24 +0000

    erp5_officejs_appstore_base: fix workflow script

--- bt5/erp5_officejs_appstore_base/WorkflowTemplateItem/portal_workflow/software_product_interaction_workflow/scripts/SoftwareProduct_setReference.py
+++ bt5/erp5_officejs_appstore_base/WorkflowTemplateItem/portal_workflow/software_product_interaction_workflow/scripts/SoftwareProduct_setReference.py
@@ -1,13 +1,2 @@
 software_product = sci['object']
-request = container.REQUEST
-
-args = str(sci['kwargs']['workflow_method_args'])
-software_product.setReference(args)
-
-
-#if sci['kwargs']['workflow_method_args'] == ('before commit',):
-#  foo.setShortTitle("set by interaction workflow")
-
-#if sci['kwargs']['workflow_method_args'] == ('Custom Message',):
-#  request.set('portal_status_message', 'Custom Message.')
-#  request.set('portal_status_level', 'error')
+software_product.SoftwareProduct_fixRelatedWebSite()
