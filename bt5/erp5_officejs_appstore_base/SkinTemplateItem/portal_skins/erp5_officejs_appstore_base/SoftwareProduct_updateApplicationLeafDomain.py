a52e59e04bc739f0ba83bc53b6569f26f08afe81
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-20 21:35:16 +0200

    [temp] Revert "erp5_officejs_appstore_base: software product action to change app url"
    This reverts commit 93ceacad0dad577d05631bf861787037a68c1eb8.

--- bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/SoftwareProduct_updateApplicationLeafDomain.py
+++
@@ -1,5 +0,0 @@
-software_product = context
-software_product.edit(reference=new_leaf_domain)
-#setReference interaction workflow will update the corresponding web site
-
-return context.Base_redirect('view', keep_items=dict(portal_status_message="Application domain url updated"))
