a498dbc384707e4367d6c5ba27fdb954c5b1ee8a
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2021-05-20 15:58:20 +0000

    erp5_api_style: Introduce ERP5 APi Style
    erp5_api_style: Use default module for object creation
    
    erp5_api_style: jIOStyle fix error handling for post
    
    erp5_api_style: Start improving error feedback
    
    erp5_api_style: Add error record module and portal type
    
    erp5_api_style: Fix error output as jIO Style
    
    erp5_api_style: Introduce jIO Web Section
    
    erp5_api_style: catch value Error when error text is not a json
    
    erp5_api_style: Add generic way to handle errors
    
    erp5_api_style: Fix Web Section Portal Type for api style
    
    erp5_api_style: Fix jIO Api Style
    
    * use self.REQUEST instead of parameter in jIO Web Section
    * Use List error in allDocs
    
    erp5_api_style: API error return http error code 400
    
    erp5_api_style: Display actions title in jio web section configuration listbox
    
    erp5_api_style: allDocs return indented result
    
    erp5_api_style: Add response schema to jIO Style. Object creation return 201 http code
    
    erp5_api_style: Post set status to 201 only if status is not yet defined
    
    erp5_api_style: action list is now sorted by float index
    
    erp5_api_style: all docs return error dict like post
    
    erp5_api_style: error can return error name and error link
    
    erp5_api_style: improve error handling in jIO Style
    
    * Give name to errors
    * Error Name is stored in error record title
    
    erp5_api_style: Add views for error records
    
    erp5_api_style: add script to provide JSON API Hyperschema

---
+++ bt5/erp5_api_style/SkinTemplateItem/portal_skins/erp5_api_style/ERP5Site_getAllActionListForAPIAllDocs.py
@@ -0,0 +1,4 @@
+return context.ERP5Site_getAllActionListForActionType(
+  context.getLayoutProperty("configuration_alldocs_action_type"),
+  parent_portal_type="jIO Web Section",
+)
---
+++ bt5/erp5_api_style/SkinTemplateItem/portal_skins/erp5_api_style/ERP5Site_logApiErrorAndReturn.py
@@ -0,0 +1,23 @@
+import json
+context.log(error_message)
+if not text_content:
+  text_content = context.REQUEST.get("BODY")
+error = context.getPortalObject().error_record_module.newContent(
+  portal_type="Error Record",
+  description=str(error_message),
+  text_content=str(text_content)
+)
+container.REQUEST.RESPONSE.setStatus(400)
+# We follow here Paypal api guideline
+# https://github.com/paypal/api-standards/blob/master/api-style-guide.md#error-schema
+error_dict = {
+  "debug_id": error.getId(),
+  "status": error_code,
+  "message": error_message
+}
+if error_name:
+  error_dict["name"] = error_name
+  error.setTitle(error_name)
+if error_link:
+  error_dict["link"] = error_link
+return json.dumps(error_dict, indent=2)
