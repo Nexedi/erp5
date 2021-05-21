67a80e571fe51d15003584aa3cf96cab7d85a0d1
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-21 14:48:04 +0000

    erp5_officejs_appstore_base: fix workflow script

--- bt5/erp5_officejs_appstore_base/WorkflowTemplateItem/portal_workflow/software_product_interaction_workflow/scripts/SoftwareProduct_setReference.py
+++ bt5/erp5_officejs_appstore_base/WorkflowTemplateItem/portal_workflow/software_product_interaction_workflow/scripts/SoftwareProduct_setReference.py
@@ -1,4 +1,5 @@
 software_product = sci['object']
 web_site = software_product.getFollowUpValue(portal_type="Web Section")
 if web_site:
-  software_product.SoftwareProduct_fixRelatedWebSite()
+  software_product.activate(tag="SoftwareProduct_setReference_%s" % software_product.getUid()).reindexObject()
+  software_product.activate(priority=2, after_tag="SoftwareProduct_setReference_%s" % software_product.getUid()).SoftwareProduct_fixRelatedWebSite()
