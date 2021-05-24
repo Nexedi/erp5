33acf80792b5520442c705107e520c7fdff38b4f
Jérome Perrin <jerome@nexedi.com>
2021-05-24 10:59:36 +0900

    accounting_ui_test: fix pylint warnings
    This ERP5 Python script was not checked until now, and add wrong indentation

--- bt5/erp5_accounting_ui_test/SkinTemplateItem/portal_skins/erp5_accounting_ui_test/AccountingZuite_createReportOtherPartiesDataset.py
+++ bt5/erp5_accounting_ui_test/SkinTemplateItem/portal_skins/erp5_accounting_ui_test/AccountingZuite_createReportOtherPartiesDataset.py
@@ -21,30 +21,32 @@ now = DateTime()
 today = DateTime(now.year(), now.month(), now.day()) + 8 * one_hour
 
 if with_ledger:
-    extra_kwargs_general = {'ledger': 'accounting/general'}
-    extra_kwargs_detailed = {'ledger': 'accounting/detailed'}
+  extra_kwargs_general = {'ledger': 'accounting/general'}
+  extra_kwargs_detailed = {'ledger': 'accounting/detailed'}
 
-    accounting_ledger = ledger.get('accounting', None)
-    if accounting_ledger is None:
-        accounting_ledger = ledger.newContent(portal_type='Category', id='accounting')
+  accounting_ledger = ledger.get('accounting', None)
+  if accounting_ledger is None:
+    accounting_ledger = ledger.newContent(portal_type='Category',
+                                          id='accounting')
 
-    for sub_category_name in ('general', 'detailed'):
-        sub_category = accounting_ledger.get(sub_category_name, None)
-        if sub_category is None:
-            sub_category = accounting_ledger.newContent(portal_type='Category', id=sub_category_name)
+  for sub_category_name in ('general', 'detailed'):
+    sub_category = accounting_ledger.get(sub_category_name, None)
+    if sub_category is None:
+      sub_category = accounting_ledger.newContent(portal_type='Category',
+                                                  id=sub_category_name)
 
-    # necessary for the "ledger" form field to appear and have correct options
-    accounting_transaction = portal.portal_types.get('Accounting Transaction')
-    ledger_list = accounting_transaction.getLedgerList()
-    if 'accounting/general' not in ledger_list:
-        ledger_list.append('accounting/general')
-    if 'accounting/detailed' not in ledger_list:
-        ledger_list.append('accounting/detailed')
-    accounting_transaction.setLedgerList(ledger_list)
+  # necessary for the "ledger" form field to appear and have correct options
+  accounting_transaction = portal.portal_types.get('Accounting Transaction')
+  ledger_list = accounting_transaction.getLedgerList()
+  if 'accounting/general' not in ledger_list:
+    ledger_list.append('accounting/general')
+  if 'accounting/detailed' not in ledger_list:
+    ledger_list.append('accounting/detailed')
+  accounting_transaction.setLedgerList(ledger_list)
 
 else:
-    extra_kwargs_general = {}
-    extra_kwargs_detailed = {}
+  extra_kwargs_general = {}
+  extra_kwargs_detailed = {}
 
 context.AccountingTransactionModule_createAccountingTestDocument(
     portal_type='Accounting Transaction',
@@ -54,13 +56,12 @@ context.AccountingTransactionModule_createAccountingTestDocument(
     destination_section_value=organisation_module.client_1,
     start_date=today + 1 * one_hour,
     embedded=(dict(portal_type='Accounting Transaction Line',
-              source_value=account_module.receivable,
-              source_debit=100.0),
+                   source_value=account_module.receivable,
+                   source_debit=100.0),
               dict(portal_type='Accounting Transaction Line',
-              source_value=account_module.goods_sales,
-              source_credit=100.0)),
-    **extra_kwargs_general
-    )
+                   source_value=account_module.goods_sales,
+                   source_credit=100.0)),
+    **extra_kwargs_general)
 
 context.AccountingTransactionModule_createAccountingTestDocument(
     portal_type='Accounting Transaction',
@@ -70,25 +71,25 @@ context.AccountingTransactionModule_createAccountingTestDocument(
     destination_section_value=organisation_module.client_1,
     start_date=today + 2 * one_hour,
     embedded=(dict(portal_type='Accounting Transaction Line',
-              source_value=account_module.payable, source_debit=200.0),
+                   source_value=account_module.payable,
+                   source_debit=200.0),
               dict(portal_type='Accounting Transaction Line',
-              source_value=account_module.goods_sales,
-              source_credit=200.0)),
-    **extra_kwargs_general
-    )
+                   source_value=account_module.goods_sales,
+                   source_credit=200.0)),
+    **extra_kwargs_general)
 
 if with_ledger:
-    context.AccountingTransactionModule_createAccountingTestDocument(
-        portal_type='Accounting Transaction',
-        title='Transaction 3',
-        source_reference='3',
-        simulation_state='delivered',
-        destination_section_value=organisation_module.client_1,
-        start_date=today + 3 * one_hour,
-        embedded=(dict(portal_type='Accounting Transaction Line',
-                  source_value=account_module.payable, source_debit=400.0),
-                  dict(portal_type='Accounting Transaction Line',
-                  source_value=account_module.goods_sales,
-                  source_credit=400.0)),
-        **extra_kwargs_detailed
-        )
+  context.AccountingTransactionModule_createAccountingTestDocument(
+      portal_type='Accounting Transaction',
+      title='Transaction 3',
+      source_reference='3',
+      simulation_state='delivered',
+      destination_section_value=organisation_module.client_1,
+      start_date=today + 3 * one_hour,
+      embedded=(dict(portal_type='Accounting Transaction Line',
+                     source_value=account_module.payable,
+                     source_debit=400.0),
+                dict(portal_type='Accounting Transaction Line',
+                     source_value=account_module.goods_sales,
+                     source_credit=400.0)),
+      **extra_kwargs_detailed)
