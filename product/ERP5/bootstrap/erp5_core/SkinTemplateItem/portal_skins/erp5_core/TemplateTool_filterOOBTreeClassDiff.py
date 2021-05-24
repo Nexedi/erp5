b1f36ffde1a03c679b9d3af83bc8226f36c5efe1
Jérome Perrin <jerome@nexedi.com>
2021-05-24 11:02:23 +0900

    core: fix pylint warnings
    these ERP5 Python Scripts were not covered by coding style tests

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/TemplateTool_filterOOBTreeClassDiff.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/TemplateTool_filterOOBTreeClassDiff.py
@@ -1,5 +1,5 @@
 if len(old_line_list) == 1 and len(new_line_list) == 1 and \
    new_line_list[0] == old_line_list[0].replace("BTrees._OOBTree","BTrees.OOBTree"):
-    return True
+  return True
 
 return False
