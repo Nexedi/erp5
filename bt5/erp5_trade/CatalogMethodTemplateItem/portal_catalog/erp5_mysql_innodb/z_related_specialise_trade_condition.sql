0659ef32f34eb809e3a7a6e8560111057639b5cf
Jérome Perrin <jerome@nexedi.com>
2021-06-03 17:07:29 +0900

    trade/accounting: fix "Trade Condition" column on accounting module
    Rename this column from "Sale Trade Condition" to "Trade Condition", so
    that it can be used also for purchase and internal trade conditions.

--- bt5/erp5_trade/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_specialise_trade_condition.sql
+++ bt5/erp5_trade/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_specialise_trade_condition.sql
@@ -1,5 +1,5 @@
 <dtml-var table_1>.uid = <dtml-var table_0>.category_uid
 AND <dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.specialise.getUid()">
-AND <dtml-var table_1>.portal_type = 'Sale Trade Condition'
+AND <dtml-var table_1>.portal_type in ('Sale Trade Condition', 'Purchase Trade Condition', 'Internal Trade Condition')
 <dtml-var RELATED_QUERY_SEPARATOR>
 <dtml-var table_0>.uid = <dtml-var query_table>.uid
