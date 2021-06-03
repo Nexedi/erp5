7eb27fb25495f3a47999789c51bb7d79178cea71
Jérome Perrin <jerome@nexedi.com>
2021-06-03 15:44:24 +0900

    trade_ui_test: test Organisation's "jump to trade condition"

--- bt5/erp5_trade_ui_test/SkinTemplateItem/portal_skins/erp5_trade_ui_test/Zuite_deleteDataForTradeZuite.py
+++ bt5/erp5_trade_ui_test/SkinTemplateItem/portal_skins/erp5_trade_ui_test/Zuite_deleteDataForTradeZuite.py
@@ -1,10 +1,13 @@
 portal = context.getPortalObject()
 
 object_value_list = [
-  getattr(context.sale_order_module, 'erp5_trade_ui_test_sale_order_1', None)
+  getattr(portal.sale_order_module, 'erp5_trade_ui_test_sale_order_1', None),
+  getattr(portal.organisation_module, 'erp5_trade_ui_test_organisation_1', None),
+  getattr(portal.organisation_module, 'erp5_trade_ui_test_organisation_2', None),
 ] + [
-  getattr(portal.getDefaultModule(portal_type), "erp5_trade_ui_test_trade_condition", None)
+  getattr(portal.getDefaultModule(portal_type), "erp5_trade_ui_test_trade_condition" + suffix, None)
   for portal_type in ("Internal Trade Condition", "Purchase Trade Condition", "Sale Trade Condition")
+  for suffix in ('', '_1', '_2', '_3')
 ]
 
 for object_value in object_value_list:
