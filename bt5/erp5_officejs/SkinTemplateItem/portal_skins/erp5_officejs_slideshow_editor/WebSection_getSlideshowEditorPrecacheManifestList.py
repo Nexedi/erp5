03ded7c1d29eab77269f936b4aa8e2f7528bf84d
Roque Porchetto <roque.porchetto@nexedi.com>
2021-06-16 19:48:46 +0000

    erp5_officejs: fix slide app manifest

--- bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
+++ bt5/erp5_officejs/SkinTemplateItem/portal_skins/erp5_officejs_slideshow_editor/WebSection_getSlideshowEditorPrecacheManifestList.py
@@ -2,6 +2,7 @@ url_list = [
   "gadget_officejs_slideshow_editor.configuration",
   "gadget_officejs_slideshow_editor_router.html",
   "gadget_officejs_slideshow.json",
+  "slideshow_editor_app_logo.svg",
   "slideeditor.gadget.html",
   "slideeditor.gadget.js",
   "slideeditor.gadget.css"
