23f882879f797959ac6458ed41b2f55699c5148b
Boxiang Sun <boxiang.sun@nexedi.com>
2021-06-22 16:19:19 +0000

    erp5_ui_test_core:  Zuite_createAndLaunchSeleniumTest.py fixup

--- bt5/erp5_ui_test_core/SkinTemplateItem/portal_skins/erp5_ui_test_core/Zuite_createAndLaunchSeleniumTest.py
+++ bt5/erp5_ui_test_core/SkinTemplateItem/portal_skins/erp5_ui_test_core/Zuite_createAndLaunchSeleniumTest.py
@@ -8,6 +8,4 @@ zuite = context.getPortalObject().portal_tests.Zuite_addZuite(zuite_id)
 for text, title in test_list:
   zuite.Zuite_addTest(None, title, text)
 
-ZELENIUM_BASE_URL = "portal_tests/%s/core/TestRunner.html?auto=true&test=../test_suite_html&resultsUrl=../postResults"
-
-return zuite.Base_redirect(ZELENIUM_BASE_URL % zuite_id)
+return zuite.Base_redirect("portal_tests/%s/core/TestRunner.html?auto=true&test=..%%2Ftest_suite_html&resultsUrl=..%%2FpostResults" % zuite_id)
