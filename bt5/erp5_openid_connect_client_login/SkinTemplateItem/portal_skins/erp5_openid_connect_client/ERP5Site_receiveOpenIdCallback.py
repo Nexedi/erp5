9ba5664bbde14bfc6dac773ad87e6f59b6665c82
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2021-06-25 13:38:57 +0000

    Introduce OpenId Connect
    erp5_openid_connect_client_login: Add dedicated cache factory to work with open id connect
    
    erp5_openid_connect_client_login: Add Scope, Client Metadata and state
    
    * Add configurable scope to connector
    * Client Metadata is a JSON defined on connector
    * Add state parameter to make redirect non repeatable
    
    erp5_openid_connect_client_login: Update OpenId Connect to be be fully functionnal
    
    erp5_openid_connect_client_login: Add tests and some minor fixup
    
    erp5_openid_connect_client_login: Don't call create user on each call to callback
    
    erp5_openid_connect_client_login: Test create connector in portal web services
    
    erp5_openid_connect_client_login: Fix test on open Id connector creation in afterSetUp
    
    OpenIDConnect: Add Extraction Plugin
    
    openidconnect: Have functionnal Extractor
    
    erp5_core: Add Case of OpenId Connect Client
    
    erp5_xhtml_style: Add OpenId Connect to Login Form
    
    erp5_web: Add OpenId Connect Logout
    
    erp5_web_renderjs_ui: Add OpenId Connect to Login Form and Logout
    
    erp5_credential: Add OpenId Connect to login form

---
+++ bt5/erp5_openid_connect_client_login/SkinTemplateItem/portal_skins/erp5_openid_connect_client/ERP5Site_receiveOpenIdCallback.py
@@ -0,0 +1,80 @@
+import time
+
+request = container.REQUEST
+response = request.RESPONSE
+
+cache_factory = "openid_connect_server_auth_token_cache_factory"
+
+def handleError(error, error_description="", state=None):
+  if state:
+    context.Base_setBearerToken(state, None, cache_factory)
+  return context.getWebSiteValue().Base_redirect(
+    'login_form',
+    keep_items={"portal_status_message":
+                context.Base_translateString(
+                  "There was problem with your login: ${error} ${error_description}. Please contact your administrator.",
+                  mapping={"error": error, "error_description": error_description})
+               })
+
+
+state = state or request.form["state"]
+if not state:
+  raise ValueError("Missing state value")
+
+session = context.Base_getBearerToken(state, cache_factory)
+if not session:
+  raise ValueError("Unknown state Value")
+error = error or request.form.get("error", None)
+error_description = error_description or request.form.get("error_description", None)
+code = code or request.form.get("code", None)
+
+if error is not None:
+  return handleError(error, error_description, state)
+
+elif code is not None:
+  response_dict = context.ERP5Site_getOpenIdConnectAccessTokenFromCode(
+    context.REQUEST.environ['QUERY_STRING'],
+    "{0}/hateoas/connection/oid_auth".format(context.absolute_url())
+  )
+  if response_dict is not None:
+    """
+    {
+      'access_token': u'XXXX0koYI5hASXZaExeYsGlqz1bSIcGyEg',
+      'token_type': u'Bearer',
+      'expires_in': 3599,
+      'refresh_token': u'ysbCp8e4TlPbNSXQocR4zlxQUdrit5sZ1FutZcece9'
+    }
+    """
+    if "error" in response_dict:
+      return handleError(response_dict.get('error'), response_dict.get('error_description'), state)
+    access_token = response_dict['access_token'].encode('utf-8')
+    hash_str = context.Base_getHMAC(access_token, access_token)
+    context.setAuthCookie(response, '__ac_openidconnect_hash', hash_str)
+    # store timestamp in second since the epoch in UTC is enough
+    response_dict["response_timestamp"] = time.time()
+    context.Base_setBearerToken(hash_str,
+                                response_dict,
+                                "openid_connect_server_auth_token_cache_factory")
+    user_dict = context.ERP5Site_getOpenIdUserEntry(token=access_token)
+    user_reference = user_dict["sub"].encode('utf-8')
+    context.Base_setBearerToken(access_token,
+                                {"reference": user_reference},
+                                "openid_connect_server_auth_token_cache_factory")
+    # XXX for ERP5JS web sites without a rewrite rule, we make sure there's a trailing /
+    web_site_value = context.getWebSiteValue() or context
+    person_relative_url = context.ERP5Site_getPersonFromOpenIdLogin(user_reference)
+    if not person_relative_url:
+      method = getattr(context, "ERP5Site_createOpenIdConnectUserToOAuth", None)
+      if method is not None:
+        method(user_reference, user_dict)
+        # XXX CLN Hackish redirect
+        return web_site_value.Base_redirect('hateoas/connection/login_form', keep_items={
+          "portal_status_message": "Your user is being created, please retry clicking on 'Login with OpenId Connect' in 1 minute."
+        })
+
+    came_from = web_site_value.absolute_url() + "/#!login?n.me=%s" % person_relative_url
+
+    response.setHeader('Location', came_from)
+    response.setStatus(303)
+else:
+  return handleError('')
