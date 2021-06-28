cbfa7048eb7060b595ffd5c12f0deb50e202e1c5
Romain Courteaud <romain@nexedi.com>
2021-06-28 10:38:08 +0000

    erp5_web_js_style: wip relative url

--- bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSection_generateLayoutPropertyUrl.py
+++ bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSection_generateLayoutPropertyUrl.py
@@ -1,3 +1,14 @@
 web_section = context
 
-return web_section.getLayoutProperty(layout_property, default='')
+layout_property_value = web_section.getLayoutProperty(layout_property, default='')
+
+# If nothing defined, nothing to change
+if layout_property_value == '':
+  return layout_property_value
+
+relative_url_prefix = web_section.WebSection_generateRelativeUrlPrefix()
+
+if relative_url_prefix and ('/' in layout_property_value):
+  raise NotImplementedError('Add correct URL calculation if gadget is not a reference')
+
+return relative_url_prefix + layout_property_value
