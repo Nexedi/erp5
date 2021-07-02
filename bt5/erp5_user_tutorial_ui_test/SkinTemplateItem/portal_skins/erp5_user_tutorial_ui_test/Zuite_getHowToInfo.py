8d8fe56ba6a0f1f1b32e26f968a1291d7375f0b0
Boxiang Sun <boxiang.sun@nexedi.com>
2021-07-02 15:41:56 +0200

    erp5_user_tutorial_ui_test: Use a real password to login as manager
    Not use an empty password

--- bt5/erp5_user_tutorial_ui_test/SkinTemplateItem/portal_skins/erp5_user_tutorial_ui_test/Zuite_getHowToInfo.py
+++ bt5/erp5_user_tutorial_ui_test/SkinTemplateItem/portal_skins/erp5_user_tutorial_ui_test/Zuite_getHowToInfo.py
@@ -130,5 +130,5 @@ return {'now': DateTime(),
         'functional_test_user_email': 'lucas@nexedi.com',
 
         'manager_username': 'ERP5TypeTestCase',
-        'manager_password': '',
+        'manager_password': 'secret',
         }
