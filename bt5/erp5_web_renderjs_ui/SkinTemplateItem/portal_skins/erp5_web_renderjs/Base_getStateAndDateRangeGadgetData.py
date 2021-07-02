003a5f5d2a0c4f1739e56a9ece8c6c81120bb8f7
Gabriel Monnerat <gabriel@nexedi.com>
2021-07-02 22:34:53 +0000

    erp5_web_renderjs_ui: update parameters

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Base_getStateAndDateRangeGadgetData.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Base_getStateAndDateRangeGadgetData.py
@@ -1,27 +1,27 @@
-from urllib import quote
-
-def extract_date(date):
-  return quote(date.strftime("%Y-%m-%d"))
-
 translateString = context.Base_translateString
 
 return [
   ("group_by", "simulation_state"),
-  ("date_range_catalog_key", "delivery.start_date"),
-  ("date_range_list", [
-    # label, start, end
-    ["< 2", extract_date(DateTime()-2), extract_date(DateTime()+1)],
-    ["2 - 7", extract_date(DateTime()-7), extract_date(DateTime()-2)],
-    ["7 - 30", extract_date(DateTime()-30), extract_date(DateTime()-7)],
-    ["> 30", extract_date(DateTime(1900, 1, 1)), extract_date(DateTime()-30)],
-  ]),
-  ("query_by", "getTranslatedSimulationStateTitle"),
-  ("base_query", {"parent_uid": context.getUid()}),
+  ("query_by", {"parent_uid": context.getUid()}),
   ("title", translateString(
     "%s Pipes" % context.getPortalType().replace(" Module", "")
   )),
   ("layout", {
-    "x": translateString("Days"),
-    "y": translateString("Quantity")
+    "x": {
+      "title": translateString("Days"),
+      "key": "getTranslatedSimulationStateTitle",
+      "label_list": ["< 2", "2 - 7", "7 - 30", "> 30"],
+      "domain_list": [(
+        "graphic_gadget_domain", (
+          'delivery_start_date_lt2',
+          'delivery_start_date_2to7',
+          'delivery_start_date_7to30',
+          'delivery_start_date_gt30'
+        )
+      )]
+    },
+    "y": {
+      "title": translateString("Quantity")
+    }
   })
 ]
