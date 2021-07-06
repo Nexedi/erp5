8342a08f2ecc742745dcabd72f031901c94a2242
Rafael Monnerat <rafael@nexedi.com>
2021-07-06 15:18:02 +0200

    Remove most disguised interactions related to simulation.
    Notes:
    
    Do nothing on 'calculate' instead of disabling *_causality_interaction_workflow as
    these interaction workflows do not generate activities directly, and do work
    that can't be postponed.
    
    By contrast, 'calculate' transition of delivery_causality_workflow is modified
    so that no 'updateCausalityState' activity is created by default.
    
    This commit also update list of method_id which should not call calculate_causality

--- bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/accounting_workflow/script_Delivery_localBuild.py
+++ bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/accounting_workflow/script_Delivery_localBuild.py
@@ -1,5 +1,1 @@
-state_change['object'].localBuild()
-alarm = getattr(state_change.getPortal().portal_alarms, "invoice_builder_alarm",
-  None)
-if alarm is not None:
-  alarm.activeSense()
+# INTERACTION DROPPED
--- bt5/erp5_project/WorkflowTemplateItem/portal_workflow/task_workflow/script_Task_buildTaskReport.py
+++ bt5/erp5_project/WorkflowTemplateItem/portal_workflow/task_workflow/script_Task_buildTaskReport.py
@@ -1,1 +1,1 @@
-state_change['object'].localBuild()
+# INTERACTION DROPPED
