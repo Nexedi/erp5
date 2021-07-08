0c51b3dd08d98e6cc7f885bff0d8ada9232ea197
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-07-08 15:50:02 +0200

    erp5_web_project_ui: update cache manifest list

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/WebSection_getWebProjectPrecacheManifestList.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/WebSection_getWebProjectPrecacheManifestList.py
@@ -2,13 +2,15 @@ url_list = [
   'handlebars.js',
   'gadget_erp5_page_project_controller.html',
   'gadget_erp5_page_project_controller.js',
-  'gadget_project_info.html',
-  'gadget_project_info.css',
-  'gadget_project_info.js',
   'gadget_erp5_page_project_redirector.html',
   'gadget_erp5_page_project_redirector.js',
   'gadget_erp5_project_panel.html',
   'gadget_erp5_project_panel.js',
+  'gadget_erp5_page_project.css',
+  'gadget_erp5_page_project_front_page.html',
+  'gadget_erp5_page_project_front_page.js',
+  'gadget_erp5_page_project_page.html',
+  'gadget_erp5_page_project_page.js'
 ]
 
 return url_list
