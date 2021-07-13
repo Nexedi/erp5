7bee21687f9e793fed6ad4ab8ea9b2bd72a39d38
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-07-13 13:59:12 +0000

    erp5_web_renderjs_ui: allow anonymous access if has allowed list
    so user can login

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -5,6 +5,7 @@ preference_tool = context.getPortalObject().portal_preferences
 if preference_tool.isPreferredHtmlStyleDisabled():
   u = getSecurityManager().getUser()
   user_id = u.getId()
-
-  if user_id not in preference_tool.getPreferredHtmlStyleAllowedUserIdList():
+  allowed_user_id_list = preference_tool.getPreferredHtmlStyleAllowedUserIdList()
+  #user is not in allowed list or anonymous access when allowed list is empty
+  if (user_id and user_id not in allowed_user_id_list) or (not user_id and not allowed_user_id_list):
     raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
