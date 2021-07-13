c395dd70b3a2933cf64ab1d67527a0d23e8cb999
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-07-13 13:59:12 +0000

    erp5_web_renderjs_ui&test: user should at least be able to login

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -6,5 +6,10 @@ if preference_tool.isPreferredHtmlStyleDisabled():
   u = getSecurityManager().getUser()
   user_id = u.getId()
 
-  if user_id not in preference_tool.getPreferredHtmlStyleAllowedUserIdList():
-    raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
+  if user_id:
+    if user_id not in preference_tool.getPreferredHtmlStyleAllowedUserIdList():
+      raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
+  else:
+    # user should be able to login
+    if not context.REQUEST['PATH_INFO'].endswith('login_form'):
+      raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
