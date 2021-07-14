c61a9d8cc80f0e591f70ce58eefb0978e7de2844
Gabriel Monnerat <gabriel@nexedi.com>
2021-07-14 22:16:46 +0000

    erp5_web_renderjs_ui: initial prototype to extract gadget data from listbox

---
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/Base_extractGadgetDataDictFromListbox.py
@@ -0,0 +1,22 @@
+domain_id = listbox.get_value("domain_root_list")[0][0]
+column_list, domain_list = context.Base_getSubdomainTitleAndIdList(domain_id)
+
+default_param_dict = {}
+
+# Use dict() is simpler but is slower
+for (key, value) in listbox.get_value('default_params') or []:
+  if isinstance(value, list):
+    default_param_dict.setdefault(key, []).extend(value)
+  else:
+    default_param_dict.setdefault(key, []).append(value)
+
+group_by = default_param_dict.pop("group_by", [])
+
+return {
+  "title": listbox.get_value("title"),
+  "query_by": default_param_dict,
+  "group_by": group_by,
+  "domain_id": domain_id,
+  "column_list": column_list,
+  "domain_list": domain_list
+}
