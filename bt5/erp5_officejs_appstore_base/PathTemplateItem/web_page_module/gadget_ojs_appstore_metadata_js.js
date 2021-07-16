6a711f65839b5058569a3d0072bd7f085c440cfc
Roque Porchetto <roque.porchetto@nexedi.com>
2021-07-16 20:15:18 +0000

    erp5_officejs_appstore_base: metadata info field in app view

---
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_metadata_js.js
@@ -0,0 +1,33 @@
+/*global window, rJS, URL, jIO, RSVP*/
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+(function (window, rJS, URL, jIO) {
+  "use strict";
+  rJS(window)
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("redirect", "redirect")
+    .declareMethod('render', function (options) {
+      var value = "Not found",
+        css_class = "ui-btn orange";
+      if (options.metadata) {
+        var metadata = JSON.parse(options.metadata);
+        value = metadata.message;
+        if (metadata.status === 0) {
+          css_class = "ui-btn-disabled green";
+        }
+      }
+      return this.changeState({
+        value: value,
+        css_class: css_class
+      });
+    })
+    .onStateChange(function (modification_dict) {
+      var a = this.element.querySelector("a");
+      if (modification_dict.hasOwnProperty('value')) {
+        a.textContent = modification_dict.value;
+      }
+      if (modification_dict.hasOwnProperty('css_class')) {
+        a.setAttribute('class', this.state.css_class);
+      }
+    });
+
+}(window, rJS, URL, jIO, RSVP));
