4706cb3b216e60d8b763bd068ea741f87179750f
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-07-20 15:25:58 +0000

    erp5_web_js_style&test: add meta tags for web site

---
+++ bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSite_getOgLocaleDict.py
@@ -0,0 +1,12 @@
+return {
+  'en': 'en_US',
+  'de': 'de_DE',
+  'fa': 'fa_IR',
+  'fr': 'fr_FR',
+  'ja': 'ja_JP',
+  'ko': 'ko_KR',
+  'pl': 'pl_PL',
+  'pt': 'pt_BR',
+  'ru': 'ru_RU',
+  'zh': 'zh_CN'
+}
