1fdadb53ab7bf858476a2c83f057d7d139ba4970
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-09-10 14:44:18 +0000

    erp5_travel_expense: limit number to check

--- bt5/erp5_travel_expense/SkinTemplateItem/portal_skins/erp5_hr_record/Alarm_createRepresentativeRecordForLeaveRequest.py
+++ bt5/erp5_travel_expense/SkinTemplateItem/portal_skins/erp5_hr_record/Alarm_createRepresentativeRecordForLeaveRequest.py
@@ -1,7 +1,12 @@
+from Products.ZSQLCatalog.SQLCatalog import Query
+from erp5.component.module.DateUtils import addToDate
+from DateTime import DateTime
+
 portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
   portal_type="Leave Request",
   method_id='LeaveRequest_createRepresentativeRecord',
+  creation_date = Query(creation_date=addToDate(DateTime(), to_add={'day': -90}), range="min"),
   activate_kw={'tag': tag},
 )
 context.activate(after_tag=tag).getId()
