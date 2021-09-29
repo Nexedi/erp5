b3991b89dccca2cac1692ff9d60ebcecf319650a
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-09-29 15:31:30 +0000

    erp5_payroll_l10n_fr: improvement
    1. display employee number
    2. add preview
    3. add parameter to send to maileva

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_printPayslipReport.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_printPayslipReport.py
@@ -11,18 +11,27 @@ Wire PaySheetTransaction through to erp5_corporate_identity Report
 # format:                   output in html*, pdf
 # international_form        translate terms
 # language                  target_language
-return context.Base_printAsReport(
-  format=format,
-  report_header = "PaySheetTransaction_generatePayslipReportHeader",
-  report_name = "PaySheetTransaction_generatePayslipReport",
-  report_footer = "PaySheetTransaction_generatePayslipReportFooter",
-  conversion_dict = dict(
-    margin_top=60,
-    header_spacing=5
-  ),
-  css_path="payslip_css/payslip",
-  document_language=target_language,
-  start_date=context.getStartDate() or None,
-  stop_date=context.getStopDate() or None,
-  **kw
+
+kw['format'] =format
+kw['report_header'] = "PaySheetTransaction_generatePayslipReportHeader"
+kw['report_name'] = "PaySheetTransaction_generatePayslipReport"
+kw['report_footer'] = "PaySheetTransaction_generatePayslipReportFooter"
+kw['conversion_dict'] = dict(
+  margin_top=60,
+  header_spacing=5
 )
+kw['css_path']="payslip_css/payslip"
+kw['document_language']=target_language
+kw['start_date']=context.getStartDate() or None
+kw['stop_date']=context.getStopDate() or None
+kw['get_doc_after_save'] = send_to_maileva
+
+if not send_to_maileva:
+  return context.Base_printAsReport(**kw)
+
+document = context.Base_printAsReport(**kw)
+document.edit(title='BULLETIN DE PAIE: %s' % document.getTitle())
+
+document.PDF_sendToMaileva(recipient=context.getSourceSectionValue(), sender=context.getDestinationTradeValue())
+document.setFollowUpValue(context)
+return document.Base_redirect('PDF_viewPDFJSPreview', keep_items={'portal_status_message': 'This document is sending to maileva'})
