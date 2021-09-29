09d16d2d95b18e3082a133c89d5d14f6a4386313
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-09-29 15:31:30 +0000

    erp5_payroll_l10n_fr: improvement
    1. display registration number
    2. add preview
    3. add parameter to send to maileva

---
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_updatePayslipReportDialog.py
@@ -0,0 +1,11 @@
+from Products.ERP5Type.Message import translateString
+if dialog_id is not None:
+  request = container.REQUEST
+  request.form['portal_status_message'] = translateString('Preview updated.')
+  request.form['cancel_url'] = cancel_url
+  request.form['format'] = format
+  request.form['send_to_maileva'] = send_to_maileva
+  request.form['evoluation_remuneration'] = evoluation_remuneration
+  request.form['taken_holiday'] = taken_holiday
+  request.form['total_holiday'] = total_holiday
+  return context.Base_renderForm(dialog_id)
