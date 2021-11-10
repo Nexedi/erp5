3a06f0087bc547be7adfa8af40cbf393832b4c82
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-10 16:04:30 +0000

    erp5_credential: refactor credential to give unrestricted access via external methods

--- bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/ERP5Site_activeLogin.py
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/ERP5Site_activeLogin.py
@@ -1,15 +1,26 @@
 from Products.ERP5Type.Message import translateString
+from ZTUtils import make_query
+
 portal = context.getPortalObject()
 
 assert key
-mail_message = portal.portal_catalog.getResultValue(portal_type="Mail Message", reference=key)
+mail_message = portal.ERP5Site_unrestrictedSearchMessage(key=key)
 
-credential_request = mail_message.getFollowUpValue()
+came_from = context.getWebSiteValue().absolute_url()
+credential_request = portal.ERP5Site_unrestrictedSearchCredential(mail_message)
+
 if credential_request.getValidationState() in ('submitted', 'accepted'):
   message = translateString("Your account is already active.")
 else:
   credential_request.submit(comment=translateString('Created by subscription form'))
-  mail_message.deliver()
+  portal.ERP5Site_unrestrictedDeliverMessage(mail_message)
   message = translateString("Your account is being activated. You will receive an e-mail when activation is complete.")
 
-return portal.Base_redirect("login_form", keep_items=dict(portal_status_message=message))
+url = "%s/login_form?portal_status_message=%s&%s" % (
+  context.getWebSectionValue().absolute_url(),
+  message,
+  make_query({"came_from": came_from})
+)
+
+context.REQUEST.RESPONSE.setHeader('Location', url)
+context.REQUEST.RESPONSE.setStatus(303)
