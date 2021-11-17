9beb88e58b9c125f61244d14e69fb71c1de660f5
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-17 18:02:19 +0000

    erp5_officejs_appstore_base: redirect page JS

---
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
@@ -0,0 +1,26 @@
+/*globals window, document, RSVP, rJS, jIO, navigator, URL, console*/
+/*jslint indent: 2, maxlen: 80, nomen: true*/
+
+(function (window, document, RSVP, rJS, jIO, navigator, URL) {
+  "use strict";
+  rJS(window)
+    .ready(function (gadget) {
+      function redirect_version() {
+        //document.location.replace("${latest_version}/" + document.location.hash);
+        //XXX HARDOCDED - how to get ${latest_version} from here?
+        document.location.replace("23bf8c1dc2/" + document.location.hash);
+      };
+      var queue = new RSVP.Queue();
+      return queue
+        .push(function () {
+          return navigator.serviceWorker.register(
+            "gadget_officejs_root_serviceworker.js"
+          );
+        })
+        .push(function (registration) {
+          console.log('(OJS Redirect) SW REGISTERED with scope: ', registration.scope);
+          window.setTimeout(redirect_version, 1);
+        })
+    });
+
+}(window, document, RSVP, rJS, jIO, navigator, URL));
