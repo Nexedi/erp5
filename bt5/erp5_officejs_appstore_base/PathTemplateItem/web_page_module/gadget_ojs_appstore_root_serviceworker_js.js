a8a8020a5de8fb9545e05c2904df8e42e9764d64
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-17 19:26:07 +0000

    erp5_officejs_appstore_base: (wip) appstore service worker

--- bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_root_serviceworker_js.js
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_root_serviceworker_js.js
@@ -23,33 +23,51 @@ var global = self, window = self;
 
   self.cache_list = [];
 
-  function createStorage(database) {
-    return self.jIO.createJIO({
-      type: "indexeddb",
-      database: database
-    });
-  }
-
-  function getFromLocal(relative_url) {
-    if (self.storage.get === undefined) {
-      self.storage = createStorage("ojs_source_code");
-    }
-    return self.storage.getAttachment(self.registration.scope, relative_url)
-      .push(function (blob) {
-        return new Response(blob, {
-          'headers': {
-            'content-type': blob.type
-          }
-        });
-      });
-  }
-
   self.addEventListener('install', function (event) {
-    console.log("(ROOT SW) Bootloader Service Worker INSTALL");
-    event.waitUntil(self.skipWaiting());
+    console.log("(ROOT SW) Bootloader Service Worker INSTALL. event:", event);
+    // Perform install step:  loading each required file into cache
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function (cache) {
+          // Add all offline dependencies to the cache, one by one, to not
+          // hammer zopes.
+          var promise = Promise.resolve();
+
+          function append(url_to_cache) {
+            promise = promise
+              .then(function () {
+                return cache.add(url_to_cache);
+              });
+          }
+
+          var i, len = required_url_list.length;
+          for (i = 0; i < len; i += 1) {
+            append(required_url_list[i]);
+          }
+          return promise;
+        })
+        .then(function () {
+          // When user accesses ERP5JS web site first time, service worker is
+          // installed but it is not activated yet, service worker is activated
+          // when the page is refreshed or when a new tab opens the site again.
+          // If user does not refresh the page and continue to use the site,
+          // user can't use cache, so everything becomes slow. We must avoid this
+          // situation.
+          // So, we want to activate the new service worker immediately if it was
+          // the first one.
+          return self.skipWaiting();
+        })
+        .catch(function (error) {
+          // Since we do not allow to override existing cache, if cache installation
+          // failed, we need to delete the cache completely.
+          caches.delete(CACHE_NAME);
+          throw error;
+        })
+    );
   });
+
   self.addEventListener('activate', function (event) {
-    console.log("(ROOT SW) Bootloader Service Worker ACTIVATE");
+    console.log("(ROOT SW) Bootloader Service Worker ACTIVATE. event:", event);
     event.waitUntil(self.clients.claim());
   });
 
@@ -57,16 +75,21 @@ var global = self, window = self;
     var url = new URL(event.request.url);
     url.hash = '';
     console.log("");
-    console.log("(ROOT SW) Bootloader Service Worker FETCH url:", url);
+    console.log("(ROOT SW) FETCH url:", url.href);
     /*if ((event.request.method !== 'GET') ||
         (required_url_list.indexOf(url.toString()) === -1)) {
       // Try not to use the untrustable fetch function
       // It can only be skip synchronously
       return;
     }*/
+    var relative_url = './' + event.request.url.split("#")[0]
+      .replace(self.registration.scope, "")
+      .replace(self.version_url, "");
+    console.log("relative_url:", relative_url);
     return event.respondWith(
       caches.open(CACHE_NAME)
         .then(function (cache) {
+          console.log("checking if in cache...");
           // Don't give request object itself. Firefox's Cache Storage
           // does not work properly when VARY contains Accept-Language.
           // Give URL string instead, then cache.match works on both Firefox and Chrome.
@@ -75,12 +98,13 @@ var global = self, window = self;
         .then(function (response) {
           // Cache hit - return the response from the cached version
           if (response) {
-            console.log("GOT response from CACHE");
+            console.log("response from cache !!");
             return response;
           }
           // Not in cache - return the result from the live server
           // `fetch` is essentially a "fallback"
-          console.log("no response from cache, FETCH from live server");
+          console.log("no response from cache. fetching url from live server");
+          console.log("TODO: save response in cache");
           return fetch(event.request);
         })
     );
