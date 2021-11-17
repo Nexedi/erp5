54dfcee39aec392e51a19f42e1c4997a099948df
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-17 17:59:56 +0000

    erp5_officejs_appstore_base: appstore service worker

---
+++ bt5/erp5_officejs_appstore_base/SkinTemplateItem/portal_skins/erp5_officejs_appstore_base/WebSection_getAppstorePrecacheManifest.py
@@ -0,0 +1,11 @@
+# minimum list of required files for appstore service worker
+
+url_list = [
+  './',
+  'rsvp.js',
+  'renderjs.js',
+  'officejs-redirect.js',
+  'favicon.ico'
+]
+
+return list(set(url_list))
