d5835ab00399395061ebdafff6d7412565bd9b47
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-18 15:34:32 +0000

    erp5_officejs_appstore_base: fix redirect page app latest version value

--- bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
@@ -1,16 +1,19 @@
-/*globals window, document, RSVP, rJS, jIO, navigator, URL, console*/
+/*globals window, document, RSVP, rJS, navigator*/
 /*jslint indent: 2, maxlen: 80, nomen: true*/
 
-(function (window, document, RSVP, rJS, jIO, navigator, URL) {
+(function (window, document, RSVP, rJS, navigator) {
   "use strict";
+
   rJS(window)
     .ready(function (gadget) {
+      var latest_version = document.head.querySelector(
+        'script[app-configuration="latest_version"]'
+      ).textContent,
+        queue = new RSVP.Queue();
       function redirect_version() {
-        //document.location.replace("${latest_version}/" + document.location.hash);
-        //XXX HARDOCDED - how to get ${latest_version} from here?
-        document.location.replace("23bf8c1dc2/" + document.location.hash);
-      };
-      var queue = new RSVP.Queue();
+        document.location.replace(latest_version + "/" +
+                                  document.location.hash);
+      }
       return queue
         .push(function () {
           return navigator.serviceWorker.register(
@@ -18,9 +21,8 @@
           );
         })
         .push(function (registration) {
-          console.log('(OJS Redirect) ROOT SW REGISTERED with scope: ', registration.scope);
           window.setTimeout(redirect_version, 1);
-        })
+        });
     });
 
-}(window, document, RSVP, rJS, jIO, navigator, URL));
+}(window, document, RSVP, rJS, navigator));
