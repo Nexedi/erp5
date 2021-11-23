6c81a445a21ed37d715579189461865c6c5cb86b
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-11-23 15:16:51 +0000

    erp5_base: change message

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Career_checkEmployeeNumber.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Career_checkEmployeeNumber.py
@@ -2,5 +2,5 @@ if context.portal_catalog(portal_type='Career',
                           parent_uid=context.getParentUid(),
                           reference=context.getReference(),
                           validation_state='open'):
-  return ['There has already started career with same employee number']
+  return ['There already is a started career with the same employee number']
 return []
