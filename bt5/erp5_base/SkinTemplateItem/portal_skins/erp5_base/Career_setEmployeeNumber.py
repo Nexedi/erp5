63524479f3ae0204653e44684efed1d8d3c7d293
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2021-11-26 10:12:26 +0100

    erp5_base: add constraint and action to make sure Employee Number is unique instead of manually setting

---
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Career_setEmployeeNumber.py
@@ -0,0 +1,15 @@
+Base_translateString = context.Base_translateString
+
+if not context.getReference() or force:
+  if not employee_number:
+    employee_number = str(context.portal_ids.generateNewId(id_group='employee_number', id_generator='uid')).zfill(8)
+  context.edit(reference=employee_number)
+  if not batch:
+    return context.Base_redirect('view', keep_items={
+      'portal_status_message': Base_translateString('Employee Number: ${employee_number} is set',
+                                                    mapping=dict(employee_number=employee_number))})
+else:
+  return context.Base_redirect('view', keep_items={
+    'portal_status_message': Base_translateString('User has already Employee Number'),
+    'portal_status_level': 'error'
+  })
