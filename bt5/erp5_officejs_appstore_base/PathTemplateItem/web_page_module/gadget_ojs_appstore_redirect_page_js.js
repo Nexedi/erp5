6df7a74357d5781583a6f41d29a886f50680717b
Roque Porchetto <roque.porchetto@nexedi.com>
2021-11-29 14:59:53 +0000

    erp5_officejs_appstore_base: restore appcache browser compatibility

--- bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
@@ -10,17 +10,25 @@
         'script[app-configuration="latest_version"]'
       ).textContent,
         queue = new RSVP.Queue();
-      function redirect_version() {
-        document.location.replace(latest_version + "/" +
-                                  document.location.hash);
-      }
       return queue
         .push(function () {
           return navigator.serviceWorker.register(
             "gadget_officejs_root_serviceworker.js");
         })
         .push(function (registration) {
-          //registration.update();
+          function redirect_version() {
+            document.location.replace(latest_version + "/" +
+                                      document.location.hash);
+          }
+          var timeout_duration = 10000;
+          try {
+            window.applicationCache.addEventListener('cached', redirect_version);
+            window.applicationCache.addEventListener('noupdate', redirect_version);
+            window.applicationCache.addEventListener('error', redirect_version);
+            window.applicationCache.addEventListener('updateready', function () {window.location.reload(); });
+          } catch (e) {
+            timeout_duration = 1;
+          }
           window.setTimeout(redirect_version, 1);
         });
     });
