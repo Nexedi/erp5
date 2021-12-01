4ce03cb25900a1f1524e129dc9601ddaa609352d
Roque Porchetto <roque.porchetto@nexedi.com>
2021-12-01 16:01:16 +0100

    Revert "erp5_credential: refactor credential to give unrestricted access via external methods"
    This reverts commit b3479b64c6fe3796af6ffc8c5a5da9d520e8036d.

--- bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/ERP5Site_activeLogin.py
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/ERP5Site_activeLogin.py
@@ -1,29 +1,15 @@
 from Products.ERP5Type.Message import translateString
-from ZTUtils import make_query
-
 portal = context.getPortalObject()
 
 assert key
-mail_message = portal.ERP5Site_unrestrictedSearchMessage(key=key)
+mail_message = portal.portal_catalog.getResultValue(portal_type="Mail Message", reference=key)
 
-credential_request = portal.ERP5Site_unrestrictedSearchCredential(mail_message)
-
+credential_request = mail_message.getFollowUpValue()
 if credential_request.getValidationState() in ('submitted', 'accepted'):
   message = translateString("Your account is already active.")
 else:
   credential_request.submit(comment=translateString('Created by subscription form'))
-  portal.ERP5Site_unrestrictedDeliverMessage(mail_message)
+  mail_message.deliver()
   message = translateString("Your account is being activated. You will receive an e-mail when activation is complete.")
 
-if context.getWebSiteValue():
-  came_from = context.getWebSiteValue().absolute_url()
-  url = "%s/login_form?portal_status_message=%s&%s" % (
-    context.getWebSectionValue().absolute_url(),
-    message,
-    make_query({"came_from": came_from})
-  )
-
-  context.REQUEST.RESPONSE.setHeader('Location', url)
-  context.REQUEST.RESPONSE.setStatus(303)
-else:
-  return portal.Base_redirect("login_form", keep_items=dict(portal_status_message=message))
+return portal.Base_redirect("login_form", keep_items=dict(portal_status_message=message))
