12b49ac8ea3dc1db2f22b89e3713b12a7f2cf237
Romain Courteaud <romain@nexedi.com>
2021-12-09 15:02:46 +0000

    erp5_search_rank_catalog: do not hide errors

--- bt5/erp5_search_rank_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_search_rank_list.sql
+++ bt5/erp5_search_rank_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_search_rank_list.sql
@@ -1,7 +1,8 @@
-INSERT IGNORE INTO search_rank VALUES
+INSERT INTO search_rank (uid) VALUES
     <dtml-in prefix="loop" expr="uid">
       <dtml-if sequence-start><dtml-else>,</dtml-if>
 (
   <dtml-sqlvar expr="uid[loop_item]" type="int">
 )
     </dtml-in>
+  ON DUPLICATE KEY UPDATE uid=VALUES(uid);
