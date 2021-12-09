4a0c43db5b01515a315f93448ff56e257ca678cc
Romain Courteaud <romain@nexedi.com>
2021-12-09 14:42:36 +0000

    erp5_search_rank_catalog: plop

---
+++ bt5/erp5_search_rank_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_search_rank_list.sql
@@ -0,0 +1,14 @@
+DELETE FROM
+  search_rank
+WHERE
+<dtml-sqltest uid type="int" multiple>
+
+<dtml-var sql_delimiter>
+
+INSERT INTO search_rank VALUES
+    <dtml-in prefix="loop" expr="uid">
+      <dtml-if sequence-start><dtml-else>,</dtml-if>
+(
+  <dtml-sqlvar expr="uid[loop_item]" type="int">
+)
+    </dtml-in>
