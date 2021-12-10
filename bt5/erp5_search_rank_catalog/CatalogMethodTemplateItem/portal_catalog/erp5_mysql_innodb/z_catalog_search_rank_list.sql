59e9ca799874efa6d1403c9816b5f87dd717d1a1
Romain Courteaud <romain@nexedi.com>
2021-12-10 15:00:49 +0000

    erp5_search_rank_catalog: fixup

--- bt5/erp5_search_rank_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_search_rank_list.sql
+++ bt5/erp5_search_rank_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_search_rank_list.sql
@@ -1,5 +1,5 @@
 INSERT INTO search_rank (uid) VALUES
-    <dtml-in prefix="loop" expr="uid">
+    <dtml-in prefix="loop" expr="_.range(_.len(uid))">
       <dtml-if sequence-start><dtml-else>,</dtml-if>
 (
   <dtml-sqlvar expr="uid[loop_item]" type="int">
