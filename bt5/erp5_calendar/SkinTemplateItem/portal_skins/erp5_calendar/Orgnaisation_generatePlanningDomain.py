257974be47006581892b077e589b63d6d95bf23d
Jérome Perrin <jerome@nexedi.com>
2022-01-13 12:11:51 +0900

    calendar: address pylint warnings
    also rewrite a sorted to use key instead of cmp

--- bt5/erp5_calendar/SkinTemplateItem/portal_skins/erp5_calendar/Orgnaisation_generatePlanningDomain.py
+++ bt5/erp5_calendar/SkinTemplateItem/portal_skins/erp5_calendar/Orgnaisation_generatePlanningDomain.py
@@ -7,19 +7,15 @@ if object_path is None:
   # Sometimes the object_path not comes with the request, when you edit for example.
   object_path = request.get('URL1').split('/')[-1]
 
-def sorted(seq, comparator):
-  new_seq = seq[::]
-  new_seq.sort(comparator)
-  return new_seq
 
 def display_method(doc):
   return doc.getTitle()
 
 domain_list = []
 organisation =  context.organisation_module.restrictedTraverse(object_path)
-for person in sorted(organisation.getSubordinationRelatedValueList(
-                       portal_type='Person', checked_permission='View'),
-                     lambda a, b: cmp(display_method(a), display_method(b))):
+for person in sorted(
+      organisation.getSubordinationRelatedValueList(portal_type='Person', checked_permission='View'),
+      key=display_method):
   domain = parent.generateTempDomain(id=person.getId())
   domain.edit(title = display_method(person),
               membership_criterion_base_category = ('source', 'destination'), 
