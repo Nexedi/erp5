e8aef5b3bdb68f072d10cd7508f83c0a76ce7ca9
Roque Porchetto <roque.porchetto@nexedi.com>
2022-02-02 16:04:37 +0000

    erp5_officejs_appstore_base: fix jslint errors

--- bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
+++ bt5/erp5_officejs_appstore_base/PathTemplateItem/web_page_module/gadget_ojs_appstore_redirect_page_js.js
@@ -1,4 +1,4 @@
-/*globals window, document, RSVP, rJS, navigator*/
+/*globals window, document, RSVP, rJS, navigator, console*/
 /*jslint indent: 2, maxlen: 80, nomen: true*/
 
 (function (window, document, RSVP, rJS, navigator, console) {
@@ -13,14 +13,14 @@
       // Make this redirection compatible with no-service-worker browsers
       if ('serviceWorker' in navigator) {
         queue.push(function () {
-          return navigator.serviceWorker.register(
-            "gadget_erp5_serviceworker.js");
+          return navigator.serviceWorker
+            .register("gadget_erp5_serviceworker.js");
         })
-        .push(undefined, function (error) {
-          // If service worker installation is rejected,
-          // do not prevent the site to be usable, even if slower
-          console.warn("Service worker registration failed", error);
-        });
+          .push(undefined, function (error) {
+            // If service worker installation is rejected,
+            // do not prevent the site to be usable, even if slower
+            console.warn("Service worker registration failed", error);
+          });
       }
       return queue
         .push(function () {
