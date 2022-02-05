3b9dd829e01ee45f8d42b996c395a96fe523be85
Jérome Perrin <jerome@nexedi.com>
2022-02-05 13:46:45 +0900

    web: use setEmptyCriterionValid accessor directly
    The accessor was added in 99cd5823a9 (erp5_property_sheets: Add
    Predicate/empty_criterion_valid_property ., 2013-11-20) it's been
    long enough to assume that every site have this property now.

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web/WebSection_init.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web/WebSection_init.py
@@ -1,3 +1,2 @@
 # List no document until at least one Criterion is created.
-# BBB: property does not exist on old property sheets
-getattr(context, 'setEmptyCriterionValid', lambda _: None)(False)
+context.setEmptyCriterionValid(False)
