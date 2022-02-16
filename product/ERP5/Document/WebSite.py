de0b1774c1145c605fa9cfd2e450a34995c1ddc7
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2022-02-16 09:15:16 +0000

    Document/WebSite&erp5_web: introduce web site language priority convention to fix report language issue on renderjs ui
    1. [Browser is supposed to send something between 0 and 1]( https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language )
    
    2. [Localizer will set 2 or 3 for cases where user selected language]( https://lab.nexedi.com/nexedi/erp5/blob/08be2f8cc936fbdc3d5bafb4355c3a475198abc1/product/Localizer/Localizer.py#L149-172 )
    
    3. all print scripts seems set selected language priority to 10
    
    so here we introduce a convention that site language priority should be 8(>3 and < 10) to fix report language issue

--- product/ERP5/Document/WebSite.py
+++ product/ERP5/Document/WebSite.py
@@ -114,13 +114,13 @@ class WebSiteTraversalHook(WebSectionTraversalHook):
     default_language = container.getDefaultAvailableLanguage()
     if default_language and container.isStaticLanguageSelection():
       if request.get('AcceptLanguage') is not None:
-        request['AcceptLanguage'].set(default_language, 80)
+        request['AcceptLanguage'].set(default_language, 8)
     else:
       accept_language = request.get('AcceptLanguage')
       if accept_language is not None:
         selected_language = accept_language.select_language(
             container.getAvailableLanguageList())
         if selected_language:
-          request['AcceptLanguage'].set(selected_language, 80)
+          request['AcceptLanguage'].set(selected_language, 8)
         elif default_language:
-          request['AcceptLanguage'].set(default_language, 80)
+          request['AcceptLanguage'].set(default_language, 8)
