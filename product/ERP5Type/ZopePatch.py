808131847dfe4c9f7d181f363a87153bb5462e07
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2022-02-26 09:50:02 +0100

    zope4: OFS.File monkey patch adding _setData() method (7c72a354) was removed.
    See Revert "zope patches: display OFSFile content using full available height in zmi".
    
    This fixes testBigFile failures because of `size` not being set to 0 when `data`
    is set to None.

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -89,6 +89,7 @@ from Products.ERP5Type.patches import AcceleratedHTTPCacheManager
 from Products.ERP5Type.patches import ExceptionFormatter
 from Products.ERP5Type.patches import WebDAV
 from Products.ERP5Type.patches import CMFCoreUtils
+from Products.ERP5Type.patches import OFSFile
 from Products.ERP5Type.patches import ZSQLMethod
 from Products.ERP5Type.patches import MimetypesRegistry
 from Products.ERP5Type.patches import users
