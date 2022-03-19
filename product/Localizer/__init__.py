bf8bbb6ece98d08cd8fc16cac8043a51f3e0f305
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2022-03-19 14:33:41 +0100

    zope4: registerHelp*() removed in favor of Sphinx documentation.

--- product/Localizer/__init__.py
+++ product/Localizer/__init__.py
@@ -66,6 +66,3 @@ def initialize(context):
         constructors = (MessageCatalog.manage_addMessageCatalogForm,
                         MessageCatalog.manage_addMessageCatalog),
         icon='img/message_catalog.gif')
-
-    context.registerHelp()
-
