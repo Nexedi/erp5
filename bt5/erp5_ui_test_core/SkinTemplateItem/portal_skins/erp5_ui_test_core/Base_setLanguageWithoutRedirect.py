d4179c329915f7b2649bb94f99681e5cca190b01
Jérome Perrin <jerome@nexedi.com>
2022-04-14 10:02:36 +0900

    ui_test_core: address pylint warnings and enable coding style test

--- bt5/erp5_ui_test_core/SkinTemplateItem/portal_skins/erp5_ui_test_core/Base_setLanguageWithoutRedirect.py
+++ bt5/erp5_ui_test_core/SkinTemplateItem/portal_skins/erp5_ui_test_core/Base_setLanguageWithoutRedirect.py
@@ -1,3 +1,6 @@
+# pylint:disable=redefined-builtin
+# select_language is a builtin because of a Localizer.itools.i18n.accept patch
+
 context.Base_doLanguage(select_language)
 # Don't redirect. Base_doLanguage tries to redirect to the same page
 # and then selenium main page is loaded again in the bottom frame of
