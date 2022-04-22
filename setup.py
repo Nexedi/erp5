18701c5ac1d3eddba4876b4b0ace2a1a7ad1f0ce
Thomas Gambier <thomas.gambier@gmail.com>
2022-04-22 10:08:01 +0200

    erp5.util: Release erp5.util (0.4.73)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.4.72'
+version = '0.4.73'
 name = 'erp5.util'
 long_description = open("README.erp5.util.txt").read() + "\n"
 
