d12a23fed431befdddb909c0aca97b22612fd018
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2022-04-23 11:47:49 +0200

    py3: Make metaclass work on both python2 and python3.

--- product/ERP5Type/Utils.py
+++ product/ERP5Type/Utils.py
@@ -572,6 +572,41 @@ if six.PY3:
 else:
   ensure_list = lambda x: x
 
+def with_metaclass(meta, *bases):
+  """
+  Function from jinja2/_compat.py. License: BSD (copy/paste here for
+  ExtensionClass)
+
+  Use it like this::
+      class BaseForm(object):
+          pass
+
+      class FormType(type):
+          pass
+
+      class Form(with_metaclass(FormType, BaseForm)):
+          pass
+
+  This requires a bit of explanation: the basic idea is to make a
+  dummy metaclass for one level of class instantiation that replaces
+  itself with the actual metaclass.  Because of internal type checks
+  we also need to make sure that we downgrade the custom metaclass
+  for one level to something closer to type (that's why __call__ and
+  __init__ comes back from type etc.).
+
+  This has the advantage over six.with_metaclass of not introducing
+  dummy classes into the final MRO.
+  """
+  class metaclass(meta):
+    __call__ = type.__call__
+    __init__ = type.__init__
+    def __new__(cls, name, this_bases, d):
+      if this_bases is None:
+        import ExtensionClass
+        return ExtensionClass.ExtensionClass.__new__(cls, name, (), d)
+      return meta(name, bases, d)
+  return metaclass('temporary_class', None, {})
+
 #####################################################
 # Globals initialization
 #####################################################
