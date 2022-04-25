ef99db3a3b6a1dd237d3105de74c67ed00e86a84
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2022-04-25 22:27:11 +0200

    TODO

--- product/ERP5Type/Accessor/TypeDefinition.py
+++ product/ERP5Type/Accessor/TypeDefinition.py
@@ -98,8 +98,10 @@ def asString(value):
     if value is None:
       result = ''
     else:
-      if isinstance(value, unicode):
+      if six.PY2 and isinstance(value, unicode):
         result = value.encode('utf-8')
+      elif six.PY3 and isinstance(value, bytes):
+        result = value.decode('utf-8')
       else:
         result = str(value)
   except TypeError:
@@ -109,6 +111,8 @@ def asString(value):
 def asList(value):
   """
     Return the value as a list or a type-specific default value if it fails.
+
+    XXX-zope4py3: bytes()?
   """
   if isinstance(value, (list, tuple)):
     result = list(value)
