d76ae3c49960e384c972a646f611f8e220a12505
Levin Zimmermann <levin.zimmermann@nexedi.com>
2022-05-11 13:01:18 +0200

    fixup! erp5_core_test: Simplify code

--- bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testRestrictedPythonSecurity.py
+++ bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testRestrictedPythonSecurity.py
@@ -740,7 +740,7 @@ class TestRestrictedPythonSecurity(ERP5TypeTestCase):
     test_file_path = ".testPandasReadJson.json"
     json_test_data = [1, 2, 3]
     with open(test_file_path, 'w') as json_file:
-      json.dump(json_test_data)
+      json.dump(json_test_data, json_file)
     self.addCleanup(os.remove, test_file_path)
 
     # Ensure json creation was successful
