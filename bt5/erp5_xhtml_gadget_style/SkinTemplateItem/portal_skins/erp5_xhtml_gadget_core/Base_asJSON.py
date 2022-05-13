fa144eeedfd97b1ee43b70a9854579d75aaa07a5
Romain Courteaud <romain@nexedi.com>
2022-05-13 12:21:19 +0200

    Drop erp5_xhtml_gadget_style
    It is now replaced by erp5_web_renderjs_ui

--- bt5/erp5_xhtml_gadget_style/SkinTemplateItem/portal_skins/erp5_xhtml_gadget_core/Base_asJSON.py
+++
@@ -1,2 +0,0 @@
-from json import dumps
-return dumps(d)
--- bt5/erp5_xhtml_gadget_style/SkinTemplateItem/portal_skins/erp5_xhtml_gadget_core/ERP5Site_processAdvancedSearch.py
+++
@@ -1,4 +0,0 @@
-return context.ERP5Site_viewQuickSearchResultList(
-    field_your_search_text = field_your_search_text,
-    field_your_search_portal_type = field_your_search_portal_type,
-    all_languages=all_languages)
