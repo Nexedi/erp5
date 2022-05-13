23e7213eab20237ae7fd11eca839f29df907b549
Romain Courteaud <romain@nexedi.com>
2022-05-13 13:36:23 +0000

    erp5_web: force redirection on website using a service worker

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/StaticWebSite_getRedirectSourceUrl.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/StaticWebSite_getRedirectSourceUrl.py
@@ -20,7 +20,43 @@ try:
   source_path = REQUEST.other["source_path"]
   redirect_url = "/".join([redirect_url, source_path])
 except(KeyError):
+  source_path = None
   redirect_url = redirect_url + "/"
+
+service_worker_to_unregister = context.getLayoutProperty("configuration_service_worker_url")
+if (service_worker_to_unregister) and (source_path == service_worker_to_unregister) and (not query_string):
+  # Do not redirect the service worker URL
+  # instead, unregister it and force all clients to reload themself
+  response = REQUEST.RESPONSE
+  response.setHeader('Content-Type', 'application/javascript')
+  return """/*jslint indent: 2*/
+/*global self, Promise*/
+(function (self, Promise) {
+  "use strict";
+
+  self.addEventListener('install', function (event) {
+    event.waitUntil(self.skipWaiting());
+  });
+
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(
+      self.registration.unregister()
+        .then(function () {
+          return self.clients.matchAll({type: 'window'});
+        })
+        .then(function (client_list) {
+          var i,
+            promise_list = [];
+          for (i = 0; i < client_list.length; i += 1) {
+            promise_list.push(client_list[i].navigate(client_list[i].url));
+          }
+          return Promise.all(promise_list);
+        })
+    );
+  });
+
+}(self, Promise));
+"""
 
 if query_string:
   redirect_url = '?'.join([redirect_url, query_string])
