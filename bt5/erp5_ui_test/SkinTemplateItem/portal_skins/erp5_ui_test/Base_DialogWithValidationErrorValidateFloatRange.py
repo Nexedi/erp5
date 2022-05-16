9cde5cd312d275334334358bbdcba38ccbf49c43
Valentin Benozillo <valentin.benozillo@nexedi.com>
2022-05-16 14:34:06 +0000

    erp5_ui_test: dd

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Base_DialogWithValidationErrorValidateFloatRange.py
+++
@@ -1,5 +0,0 @@
-from Products.Formulator.Errors import ValidationError
-
-if value < 1234 or value >= 1235:
-  raise ValidationError('external_validator_failed', context, context.Base_translateString("The integer you entered was out of range."))
-return True
