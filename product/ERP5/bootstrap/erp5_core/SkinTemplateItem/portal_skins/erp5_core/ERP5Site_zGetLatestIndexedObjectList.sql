2dbd6163357cc11d343abcb95955ee27fcec033d
Jérome Perrin <jerome@nexedi.com>
2022-06-03 14:30:09 +0900

    core: repair ERP5Site_reindexLatestIndexedObjects
    This script stopped working since Catalog changed to be ERP5 document,
    because it was calling the unindex method of the catalog (which
    itself is indexable like any other ERP5 document). Update to use the
    uncatalogObject which is the method to unindex a document by uid.
    
    Even though it was somehow working before, it was not really correct
    in selecting objects, because it was using - operator on TIMESTAMP
    column, which is not computing a difference in seconds as this script
    was expecting. See for example https://stackoverflow.com/a/24504132/7607763
    for an explanation. Update to use TIMESTAMPDIFF function to compute
    a difference in seconds.
    
    This query was also sorting by indexation_timestamp, which does not
    use an index. Remove the sort because it's not really needed.
    
    --
    Another reproduction of the timestamp arithmetic problem
    
    select
       TIMESTAMP('2021-01-02 00:00:00') - TIMESTAMP('2021-01-01 00:00:00') a,
       20210102000000 - 20210101000000 aa,
       TIMESTAMP('2021-06-01 00:00:00') - TIMESTAMP('2021-05-31 00:00:00') b,
       20210601000000 - 20210531000000 bb,
       TIMESTAMPDIFF(second, TIMESTAMP('2021-05-31 00:00:00'), TIMESTAMP('2021-06-01 00:00:00')) c
    
    | a | aa | b | bb | c |
    | ------ | ------ | ------ | ------ | ------ |
    | 1000000 | 1000000 | 70000000 | 70000000 | 86400 |

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_zGetLatestIndexedObjectList.sql
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_zGetLatestIndexedObjectList.sql
@@ -4,6 +4,4 @@ FROM
   catalog
 WHERE
   path != 'reserved'
-  AND CURRENT_TIMESTAMP - indexation_timestamp <= <dtml-sqlvar delta type="int">
-ORDER BY
-  indexation_timestamp DESC
+  AND TIMESTAMPDIFF(SECOND, indexation_timestamp, CURRENT_TIMESTAMP) <= <dtml-sqlvar delta type="int">
