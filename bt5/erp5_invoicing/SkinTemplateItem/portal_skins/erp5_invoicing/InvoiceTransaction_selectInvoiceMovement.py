a29afa63cf9eacf7bf2bb7a5aea8b2ca7e06c5ca
Julien Muchembled <jm@nexedi.com>
2022-06-06 19:43:14 +0200

    invoicing: speed up InvoiceTransaction_selectInvoiceMovement

--- bt5/erp5_invoicing/SkinTemplateItem/portal_skins/erp5_invoicing/InvoiceTransaction_selectInvoiceMovement.py
+++ bt5/erp5_invoicing/SkinTemplateItem/portal_skins/erp5_invoicing/InvoiceTransaction_selectInvoiceMovement.py
@@ -3,11 +3,16 @@
 Simulation Movements can come from normal Invoice Transaction Rule
 and same rule which is children of Trade Model Rule """
 
+portal = context.getPortalObject()
+searchRules = portal.portal_rules.searchFolder
+getRuleUidList = lambda *args: [x.getUid() for x in searchRules(reference=args)]
+
 # search for normal movements
-kw['parent_specialise_reference'] = ['default_invoice_transaction_rule']
-kw['grand_grand_parent_specialise_reference'] = [
-             'default_invoicing_rule', 'default_invoice_rule', 'default_tax_rule']
-if context.Invoice_isAdvanced():
+kw['parent_specialise_uid'] = getRuleUidList('default_invoice_transaction_rule')
+
+kw['grand_grand_parent_specialise_uid'] = getRuleUidList(
+  'default_invoicing_rule', 'default_invoice_rule', 'default_tax_rule')
+if portal.Invoice_isAdvanced():
   kw['explanation_portal_type']       = ['%s Order' % trade_type,
                                          '%s Invoice' % trade_type,
                                          '%s Packing List' % trade_type,
@@ -27,13 +32,14 @@ kw['group_by'] = ('uid',)
 search_kw = kw.copy()
 search_kw['grand_parent_simulation_state'] = ['started']
 
-movement_list = list(context.portal_catalog(**search_kw))
+movement_list = list(portal.portal_catalog(**search_kw))
 
 # update query to search for movements which are children of Trade Model Rule
-kw['grand_grand_parent_specialise_reference'] = 'default_trade_model_rule'
+kw['grand_grand_parent_specialise_uid'] = getRuleUidList(
+  'default_trade_model_rule')
 kw['grand_grand_grand_parent_simulation_state'] = ['started']
 
-movement_list += list(context.portal_catalog(**kw))
+movement_list += portal.portal_catalog(**kw)
 
 # Simulation movement's state maybe not yet updated, make sure it's the right one
 return [x for x in movement_list if x.getParentValue().getParentValue().getSimulationState() == 'started']
