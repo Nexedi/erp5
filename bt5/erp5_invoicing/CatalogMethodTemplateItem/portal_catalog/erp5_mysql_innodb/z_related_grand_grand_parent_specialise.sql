c90802a5d33f7a79ce94ee6e20fc7beeddd7c82b
Julien Muchembled <jm@nexedi.com>
2022-06-06 19:43:14 +0200

    invoicing: fix performance issue with z_related_grand_grand_parent_specialise
    It was written in weird way and apparently,
    it prevented MariaDB to optimize the query plan.

--- bt5/erp5_invoicing/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_grand_grand_parent_specialise.sql
+++ bt5/erp5_invoicing/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_grand_grand_parent_specialise.sql
@@ -1,11 +1,11 @@
-<dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.specialise.getUid()">
-<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_1>.uid = <dtml-var table_0>.parent_uid
 
-<dtml-var table_0>.uid = <dtml-var table_2>.parent_uid
 <dtml-var RELATED_QUERY_SEPARATOR>
-
 <dtml-var table_2>.uid = <dtml-var table_1>.parent_uid
-<dtml-var RELATED_QUERY_SEPARATOR>
+AND <dtml-var table_2>.base_category_uid = <dtml-var "portal_categories.specialise.getUid()">
 
-<dtml-var table_3>.uid = <dtml-var table_0>.category_uid
-AND <dtml-var table_1>.uid = <dtml-var query_table>.parent_uid
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_3>.uid = <dtml-var table_2>.category_uid
+
+<dtml-var RELATED_QUERY_SEPARATOR>
+<dtml-var table_0>.uid = <dtml-var query_table>.parent_uid
