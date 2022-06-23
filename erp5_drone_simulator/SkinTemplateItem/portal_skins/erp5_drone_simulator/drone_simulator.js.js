03a8956896f5b06354afb1ddda63ad2857ffdcad
Roque Porchetto <roque.porchetto@nexedi.com>
2022-06-23 17:41:35 +0000

    erp5_drone_simulator: drone simulator gadget

---
+++ erp5_drone_simulator/SkinTemplateItem/portal_skins/erp5_drone_simulator/drone_simulator.js.js
@@ -0,0 +1,109 @@
+/*jslint nomen: true, indent: 2 */
+/*global window, rJS, RSVP, document*/
+(function (window, rJS, RSVP, document) {
+  "use strict";
+
+  rJS(window)
+    .declareAcquiredMethod('triggerMaximize', 'triggerMaximize')
+    .allowPublicAcquisition('triggerMaximize', function (param_list) {
+      var gadget = this;
+      if (!this.element.classList.contains('editor-maximize')) {
+        this.element.classList.toggle('editor-maximize');
+      }
+      return this.triggerMaximize.apply(this, param_list)
+        .push(undefined, function () {
+          if (gadget.element.classList.contains('editor-maximize')) {
+            gadget.element.classList.remove('editor-maximize');
+          }
+        });
+    })
+
+    .declareMethod('render', function (options) {
+      return this.renderAsynchronously(options);
+    })
+
+    .declareJob('renderAsynchronously', function (options) {
+      var state_dict = {
+          value: options.value || "",
+          maximize: options.maximize,
+          key: options.key, //id
+          // Force calling subfield render
+          // as user may have modified the input value
+          render_timestamp: new Date().getTime()
+        };
+      return this.changeState(state_dict);
+    })
+
+    .onStateChange(function (modification_dict) {
+      var element = this.element,
+        gadget = this,
+        div = document.createElement('div'),
+        div_max = document.createElement('div'),
+        code,
+        queue = new RSVP.Queue();
+
+      while (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
+      element.appendChild(div_max);
+      queue
+        .push(function () {
+          return gadget.triggerMaximize(false);
+        })
+        .push(function () {
+          return gadget.declareGadget("gadget_button_maximize.html", {
+            scope: 'maximize',
+            element: div_max,
+            sandbox: 'public'
+          });
+        }, function (error) {
+        // Check Acquisition, old erp5 ui don't have triggerMaximize
+          if (error.name !== "AcquisitionError") {
+            throw error;
+          }
+        });
+      element.appendChild(div);
+
+      queue
+        .push(function () {
+          return gadget.declareGadget(
+            //TODO CHANGE instead of declare this, directly do a runGame job here
+            "rescue_swarm_game.html",
+            {
+              scope: 'editor',
+              sandbox: 'iframe',
+              element: div
+            }
+          );
+        });
+      queue
+        .push(function () {
+          return gadget.getDeclaredGadget('editor');
+        })
+        .push(function (editor_gadget) {
+          return editor_gadget.render(gadget.state);
+        });
+
+      if (modification_dict.maximize === "auto") {
+        queue
+          .push(function () {
+            return gadget.getDeclaredGadget("maximize");
+          })
+          .push(function (gadget_maximize) {
+            return gadget_maximize.callMaximize(true);
+          });
+      }
+      return queue;
+    })
+
+    .declareMethod('getContent', function () {
+      //TODO return simulator result (rename to getResult?)
+      return {};
+    }, {mutex: 'changestate'})
+
+    .declareMethod('checkValidity', function () {
+      // XXX How to implement this for editors?
+      return true;
+    });
+
+}(window, rJS, RSVP, document));
