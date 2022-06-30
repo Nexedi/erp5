14af09e0c4118ac99a913a4b3c21fe8645bc2a11
Jérome Perrin <jerome@nexedi.com>
2022-06-30 14:43:30 +0900

    accounting: fix grouping dialog loosing section category on next page
    In xhtml_style, just after clicking next or previous page in the listbox
    buttons, the values selected in 'Section Category' and 'Section Category
    Strict' were not used, because they were used from request and not from
    as normal script parameters.
    
    This fixes only the xhtml_style version, ERP5JS has another problem that
    dialog fields values are reset when going to next page, so it would also
    need this problem to be fixed.

--- bt5/erp5_accounting_ui_test/SkinTemplateItem/portal_skins/erp5_accounting_ui_test/AccountingTransactionModule_createAccountingTransactionList.py
+++ bt5/erp5_accounting_ui_test/SkinTemplateItem/portal_skins/erp5_accounting_ui_test/AccountingTransactionModule_createAccountingTransactionList.py
@@ -2,7 +2,6 @@ from DateTime import DateTime
 from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
 
 # params
-section_title = 'My Organisation'
 portal = context.getPortalObject()
 accounting_module = portal.accounting_module
 year = 2005
@@ -14,9 +13,9 @@ business_process = portal.portal_catalog.getResultValue(
   portal_type='Business Process').getRelativeUrl()
 
 # if the previous test didn't change input data, no need to recreate content
-current_script_data_id = '%s_month_count_%s_draft_%s_state_%s_payment_%s_leger_%s' % (
+current_script_data_id = '%s_month_count_%s_draft_%s_state_%s_payment_%s_leger_%s_both_sections_%s' % (
      month_count, add_draft_transactions, transaction_state,
-     add_related_payments, set_ledger, script.getId())
+     add_related_payments, set_ledger, both_sections, script.getId())
 
 if accounting_module.getProperty('current_content_script',
                                 '') == current_script_data_id:
@@ -60,7 +59,10 @@ def getOrganisationByTitle(title):
   assert len(document_list) == 1, \
         '%d organisation with title "%s"' % (len(document_list), title)
   return document_list[0]
-section = getOrganisationByTitle(section_title)
+
+section_list = [getOrganisationByTitle('My Organisation')]
+if both_sections:
+  section_list.append(getOrganisationByTitle('My Master Organisation'))
 
 euro_resource = 'currency_module/euro'
 
@@ -75,93 +77,50 @@ else:
                               title='Dummy Product for testing')
 
 for _ in range(random.randint(5, 10)):
-  pl = portal.sale_packing_list_module.newContent(
-        portal_type='Sale Packing List',
-        title='Dummy Packing List for testing',
-        source_section=section,
-        source=section,
-        destination_section=getOrganisationByTitle('Client 1'),
-        destination=getOrganisationByTitle('Client 1'),
-        specialise=business_process,
-        start_date=default_date, )
-  line = pl.newContent(portal_type='Sale Packing List Line',
-                resource_value=product,
-                quantity=random.randint(300, 500),
-                price=random.randint(300, 500))
+  for section in section_list:
+    pl = portal.sale_packing_list_module.newContent(
+          portal_type='Sale Packing List',
+          title='Dummy Packing List for testing',
+          source_section=section,
+          source=section,
+          destination_section=getOrganisationByTitle('Client 1'),
+          destination=getOrganisationByTitle('Client 1'),
+          specialise=business_process,
+          start_date=default_date, )
+    line = pl.newContent(portal_type='Sale Packing List Line',
+                  resource_value=product,
+                  quantity=random.randint(300, 500),
+                  price=random.randint(300, 500))
 
   # TODO: add an external method to modify workflow state of an object
   #context.portal_tests.setSimulationStateFor(pl, 'stopped')
   #assert pl.getSimulationState() == 'stopped'
   #pl.recursiveReindexObject()
 
-for month in range(1, month_count + 1):
-  default_date = DateTime(year, month, 1)
-  tr = accounting_module.newContent(
-        title='Accounts opening',
-        portal_type='Accounting Transaction',
-        source_section=section,
-        created_by_builder=1,
-        ledger_value=test_ledger_1,
-        start_date=default_date,
-        stop_date=default_date,
-        resource=euro_resource,
-    )
-
-  tr.newContent(portal_type='Accounting Transaction Line',
-                source=getAccountByTitle('Equity'),
-                quantity=20000)
-  tr.newContent(portal_type='Accounting Transaction Line',
-                source=getAccountByTitle('Fixed Assets'),
-                quantity=-15000)
-  tr.newContent(portal_type='Accounting Transaction Line',
-                source=getAccountByTitle('Stocks'),
-                quantity=-5000)
-  # TODO: "validated" should be renamed to "stopped"
-  if transaction_state == 'validated':
-    tr.stop()
-    assert tr.getSimulationState() == 'stopped'
-  elif transaction_state == 'delivered':
-    tr.stop()
-    tr.deliver()
-    assert tr.getSimulationState() == 'delivered'
-  else:
-    # other cases not supported for now
-    assert transaction_state == 'draft'
-
-  vat_rate = .1
-
-  for client_title, amount  in (('Client 1', 2000), ('Client 2', 3000)):
-    default_date += 1
+for section in section_list:
+  for month in range(1, month_count + 1):
+    default_date = DateTime(year, month, 1)
     tr = accounting_module.newContent(
-          portal_type='Sale Invoice Transaction',
-          title='%s Sale Invoice' % client_title,
+          title='Accounts opening',
+          portal_type='Accounting Transaction',
           source_section=section,
-          destination_section=getOrganisationByTitle(client_title),
-          source=section,
-          destination=getOrganisationByTitle(client_title),
           created_by_builder=1,
           ledger_value=test_ledger_1,
           start_date=default_date,
           stop_date=default_date,
-          specialise=business_process,
           resource=euro_resource,
       )
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Receivable'),
-                  quantity=-(amount * (1 + vat_rate)))
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Collected VAT 10%'),
-                  quantity=amount * vat_rate)
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Goods Sales'),
-                  quantity=amount)
-    # add a random invoice line, which should not impact our tests
-    tr.newContent(portal_type='Invoice Line',
-                  source=section,
-                  destination=getOrganisationByTitle(client_title),
-                  resource_value=product,
-                  quantity=random.randint(300, 400),
-                  price=random.randint(300, 400), )
+
+    tr.newContent(portal_type='Accounting Transaction Line',
+                  source=getAccountByTitle('Equity'),
+                  quantity=20000)
+    tr.newContent(portal_type='Accounting Transaction Line',
+                  source=getAccountByTitle('Fixed Assets'),
+                  quantity=-15000)
+    tr.newContent(portal_type='Accounting Transaction Line',
+                  source=getAccountByTitle('Stocks'),
+                  quantity=-5000)
+    # TODO: "validated" should be renamed to "stopped"
     if transaction_state == 'validated':
       tr.stop()
       assert tr.getSimulationState() == 'stopped'
@@ -172,103 +131,148 @@ for month in range(1, month_count + 1):
     else:
       # other cases not supported for now
       assert transaction_state == 'draft'
-    if add_related_payments:
-      payment = accounting_module.newContent(
-            causality_value=tr,
-            portal_type='Payment Transaction',
-            title='%s Payment' % client_title,
+
+    vat_rate = .1
+
+    for client_title, amount  in (('Client 1', 2000), ('Client 2', 3000)):
+      default_date += 1
+      tr = accounting_module.newContent(
+            portal_type='Sale Invoice Transaction',
+            title='%s Sale Invoice' % client_title,
+            source_section=section,
+            destination_section=getOrganisationByTitle(client_title),
+            source=section,
+            destination=getOrganisationByTitle(client_title),
+            created_by_builder=1,
+            ledger_value=test_ledger_1,
+            start_date=default_date,
+            stop_date=default_date,
+            specialise=business_process,
+            resource=euro_resource,
+        )
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
+                    source=getAccountByTitle('Receivable'),
+                    quantity=-(amount * (1 + vat_rate)))
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
+                    source=getAccountByTitle('Collected VAT 10%'),
+                    quantity=amount * vat_rate)
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
+                    source=getAccountByTitle('Goods Sales'),
+                    quantity=amount)
+      # add a random invoice line, which should not impact our tests
+      tr.newContent(portal_type='Invoice Line',
+                    source=section,
+                    destination=getOrganisationByTitle(client_title),
+                    resource_value=product,
+                    quantity=random.randint(300, 400),
+                    price=random.randint(300, 400), )
+      if transaction_state == 'validated':
+        tr.stop()
+        assert tr.getSimulationState() == 'stopped'
+      elif transaction_state == 'delivered':
+        tr.stop()
+        tr.deliver()
+        assert tr.getSimulationState() == 'delivered'
+      else:
+        # other cases not supported for now
+        assert transaction_state == 'draft'
+      if add_related_payments:
+        payment = accounting_module.newContent(
+              causality_value=tr,
+              portal_type='Payment Transaction',
+              title='%s Payment' % client_title,
+              source_section=section,
+              destination_section=getOrganisationByTitle(client_title),
+              created_by_builder=1,
+              ledger_value=test_ledger_1,
+              start_date=default_date + .1, # make sure this will be after the invoice
+              stop_date=default_date + .1,
+              resource=euro_resource,
+          )
+        payment.newContent(portal_type='Accounting Transaction Line',
+                      source=getAccountByTitle('Bank'),
+                      quantity=-(amount * (1 + vat_rate)))
+        payment.newContent(portal_type='Accounting Transaction Line',
+                      source=getAccountByTitle('Receivable'),
+                      quantity=(amount * (1 + vat_rate)))
+        if transaction_state in ('validated', 'delivered'):
+          payment.stop()
+          assert payment.getSimulationState() == 'stopped'
+          if transaction_state == 'delivered':
+            tr.deliver()
+            assert tr.getSimulationState() == 'delivered'
+          if not keep_grouping_reference:
+            tag = script.id + '_payment_indexation_' + payment.getPath()
+            payment.recursiveReindexObject(activate_kw={'tag': tag})
+            for line in payment.getMovementList(
+                            portal_type=payment.getPortalAccountingMovementTypeList()):
+              if line.getGroupingReference():
+                line.activate(after_tag=tag).AccountingTransactionLine_resetGroupingReference()
+
+        else:
+          # other cases not supported for now
+          assert transaction_state == 'draft'
+
+
+    amount=7000
+    default_date += 1
+    tr = accounting_module.newContent(
+          portal_type='Purchase Invoice Transaction',
+          title='First Purchase Invoice',
+          destination_section=section,
+          source_section=getOrganisationByTitle('Supplier'),
+          created_by_builder=1,
+          ledger_value=test_ledger_1,
+          start_date=default_date-5, # In purchase invoice transaction, stop_date is accounting operation date.
+          stop_date=default_date,
+          specialise=business_process,
+          resource=euro_resource,
+      )
+    tr.newContent(portal_type='Purchase Invoice Transaction Line',
+                  destination=getAccountByTitle('Payable'),
+                  quantity=-(amount * (1 + vat_rate)))
+    tr.newContent(portal_type='Purchase Invoice Transaction Line',
+                  destination=getAccountByTitle('Refundable VAT 10%'),
+                  quantity=amount * vat_rate)
+    tr.newContent(portal_type='Purchase Invoice Transaction Line',
+                  destination=getAccountByTitle('Goods Purchase'),
+                  quantity=amount)
+    if transaction_state == 'validated':
+      tr.stop()
+      assert tr.getSimulationState() == 'stopped'
+    elif transaction_state == 'delivered':
+      tr.stop()
+      tr.deliver()
+      assert tr.getSimulationState() == 'delivered'
+    else:
+      # other cases not supported for now
+      assert transaction_state == 'draft'
+
+  if add_draft_transactions:
+    # finally, add random accounting transcactions in draft state, which have no
+    # impact on the test
+    for client_title, amount  in (('Client 1', 2000), ('Client 2', 3000)):
+      tr = accounting_module.newContent(
+            portal_type='Sale Invoice Transaction',
+            title='%s Sale Invoice' % client_title,
             source_section=section,
             destination_section=getOrganisationByTitle(client_title),
             created_by_builder=1,
             ledger_value=test_ledger_1,
-            start_date=default_date + .1, # make sure this will be after the invoice
-            stop_date=default_date + .1,
+            start_date=default_date,
+            stop_date=default_date,
             resource=euro_resource,
+            specialise=business_process,
         )
-      payment.newContent(portal_type='Accounting Transaction Line',
-                    source=getAccountByTitle('Bank'),
-                    quantity=-(amount * (1 + vat_rate)))
-      payment.newContent(portal_type='Accounting Transaction Line',
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
                     source=getAccountByTitle('Receivable'),
-                    quantity=(amount * (1 + vat_rate)))
-      if transaction_state in ('validated', 'delivered'):
-        payment.stop()
-        assert payment.getSimulationState() == 'stopped'
-        if transaction_state == 'delivered':
-          tr.deliver()
-          assert tr.getSimulationState() == 'delivered'
-        if not keep_grouping_reference:
-          tag = script.id + '_payment_indexation_' + payment.getPath()
-          payment.recursiveReindexObject(activate_kw={'tag': tag})
-          for line in payment.getMovementList(
-                          portal_type=payment.getPortalAccountingMovementTypeList()):
-            if line.getGroupingReference():
-              line.activate(after_tag=tag).AccountingTransactionLine_resetGroupingReference()
-
-      else:
-        # other cases not supported for now
-        assert transaction_state == 'draft'
-      
-
-  amount=7000
-  default_date += 1
-  tr = accounting_module.newContent(
-        portal_type='Purchase Invoice Transaction',
-        title='First Purchase Invoice',
-        destination_section=section,
-        source_section=getOrganisationByTitle('Supplier'),
-        created_by_builder=1,
-        ledger_value=test_ledger_1,
-        start_date=default_date-5, # In purchase invoice transaction, stop_date is accounting operation date.
-        stop_date=default_date,
-        specialise=business_process,
-        resource=euro_resource,
-    )
-  tr.newContent(portal_type='Purchase Invoice Transaction Line',
-                destination=getAccountByTitle('Payable'),
-                quantity=-(amount * (1 + vat_rate)))
-  tr.newContent(portal_type='Purchase Invoice Transaction Line',
-                destination=getAccountByTitle('Refundable VAT 10%'),
-                quantity=amount * vat_rate)
-  tr.newContent(portal_type='Purchase Invoice Transaction Line',
-                destination=getAccountByTitle('Goods Purchase'),
-                quantity=amount)
-  if transaction_state == 'validated':
-    tr.stop()
-    assert tr.getSimulationState() == 'stopped'
-  elif transaction_state == 'delivered':
-    tr.stop()
-    tr.deliver()
-    assert tr.getSimulationState() == 'delivered'
-  else:
-    # other cases not supported for now
-    assert transaction_state == 'draft'
-  
-if add_draft_transactions:
-  # finally, add random accounting transcactions in draft state, which have no
-  # impact on the test
-  for client_title, amount  in (('Client 1', 2000), ('Client 2', 3000)):
-    tr = accounting_module.newContent(
-          portal_type='Sale Invoice Transaction',
-          title='%s Sale Invoice' % client_title,
-          source_section=section,
-          destination_section=getOrganisationByTitle(client_title),
-          created_by_builder=1,
-          ledger_value=test_ledger_1,
-          start_date=default_date,
-          stop_date=default_date,
-          resource=euro_resource,
-          specialise=business_process,
-      )
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Receivable'),
-                  quantity=random.randint(300, 400),)
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Collected VAT 10%'),
-                  quantity=random.randint(300, 400),)
-    tr.newContent(portal_type='Sale Invoice Transaction Line',
-                  source=getAccountByTitle('Goods Sales'),
-                  quantity=random.randint(300, 400),)
+                    quantity=random.randint(300, 400),)
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
+                    source=getAccountByTitle('Collected VAT 10%'),
+                    quantity=random.randint(300, 400),)
+      tr.newContent(portal_type='Sale Invoice Transaction Line',
+                    source=getAccountByTitle('Goods Sales'),
+                    quantity=random.randint(300, 400),)
 
 accounting_module.setProperty('current_content_script',
                               current_script_data_id)
