9a6062d7e7f00fb3e8b71c98d2a94fa92b065aed
Roque Porchetto <roque.porchetto@nexedi.com>
2022-07-06 15:31:39 +0000

    erp5_drone_simulator: print drone path

--- erp5_drone_simulator/SkinTemplateItem/portal_skins/erp5_drone_simulator/js/GameManager.js.js
+++ erp5_drone_simulator/SkinTemplateItem/portal_skins/erp5_drone_simulator/js/GameManager.js.js
@@ -47,6 +47,7 @@ var GameManager = /** @class */ (function (console) {
         this.finish_deferred = null;
         if (!simulation_speed) { simulation_speed = 5; }
         this._max_step_animation_frame = simulation_speed;
+        this._last_position_print = null;
         // ----------------------------------- CODE ZONES AND PARAMS
         // JIO : AI
         // XXX
@@ -267,6 +268,27 @@ var GameManager = /** @class */ (function (console) {
         }
         if (update_dom) {
           this._timeDisplay.textContent = this._formatTimeToMinutesAndSeconds(this._game_duration);
+        }
+        var drone_position_x = this._teamLeft[0]._controlMesh.position.x,
+          drone_position_y = this._teamLeft[0]._controlMesh.position.z;
+        //log drone info
+        console.log("LOG drone info:", this._game_duration, drone_position_x, drone_position_y);
+        //print drone position every 2 seconds
+        if (seconds % 2 === 0) {
+          if (this._last_position_print !== seconds) {
+            this._last_position_print = seconds;
+            var position_obj = BABYLON.MeshBuilder.CreateSphere("obs_" + seconds, {
+                'diameterX': 3.5,
+                'diameterY': 3.5,
+                'diameterZ': 3.5
+            }, this._scene);
+            position_obj.position = new BABYLON.Vector3(drone_position_x, 0.1, drone_position_y);
+            position_obj.scaling = new BABYLON.Vector3(3.5, 3.5, 3.5);
+            var material = new BABYLON.StandardMaterial(this._scene);
+            material.alpha = 1;
+            material.diffuseColor = new BABYLON.Color3(255, 165, 0);
+            position_obj.material = material;
+          }
         }
     };
     /**
