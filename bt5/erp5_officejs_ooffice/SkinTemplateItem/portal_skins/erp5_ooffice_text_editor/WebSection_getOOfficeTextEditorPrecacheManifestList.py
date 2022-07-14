bc68a556e8387450a676fb22b8f4f99afb4180cd
Roque Porchetto <roque.porchetto@nexedi.com>
2022-07-14 17:42:29 +0000

    erp5_officejs_ooffice: fix (undo)

--- bt5/erp5_officejs_ooffice/SkinTemplateItem/portal_skins/erp5_ooffice_text_editor/WebSection_getOOfficeTextEditorPrecacheManifestList.py
+++ bt5/erp5_officejs_ooffice/SkinTemplateItem/portal_skins/erp5_ooffice_text_editor/WebSection_getOOfficeTextEditorPrecacheManifestList.py
@@ -6,9 +6,6 @@ url_list = [
   "gadget_officejs_jio_text_view.js",
   "gadget_ooffice_text.json",
   "500px-Gnome-x-office-document.svg.png",
-  "gadget_erp5_page_ojs_oo_about.html",
-  "gadget_erp5_page_ojs_oo_about.js",
-  "gadget_erp5_page_ojs_oo_about.css",
 ]
 
 return url_list
