d0b448a2b5614626614d948cdbcd4f70fd955f54
Gabriel Monnerat <gabriel@nexedi.com>
2022-08-01 15:41:38 +0000

    erp5_stripe: introduce new module and features to handle Stripe checkout and payments in ERP5
    This business template contains a framework to integrate stripe checkout payments in ERP5.
    
    To configure the connector:
    
     - Create an account on stripe.com
     - In stripe.com's dashboard: Developers / API keys use "Create restricted key" to create a key with write access to "All Checkout resources".
     - Create a stripe connector in portal_web_services. Set URL to `https://api.stripe.com/v1/` and the restricted key created in the previous step
     - In stripe.com's dashboard: Developers / Webhooks use "Add endpoint", with "Endpoint URL" set to https://your.erp5.public.hostname/ERP5Site_receiveStripeWebHook and "Events to send" including all events from "checkout.session" category.
    
    To integrate in an ERP5 project:
    
     - Implement a custom script to start a checkout session. The script should call `StripePaymentSessionModule_createStripeSession` to initiate the session and redirect the end user to stripe checkout page.
     - Implement an interaction workfow on `Stripe Payment Session.complete` which inspects the value of `state_change['kwargs']['payment_status']` and implement the logic (such as create a payment transaction) depending on the status `"paid"` or `"unpaid"`.
    
    Example script to create session
    
    ```py
    web_site = context.getWebSiteValue()
    data = {
      # custom web sections for success and cancel URLs
      "success_url": web_site.stripe_payment_session_success.absolute_url(),
      "cancel_url": web_site.stripe_payment_session_cancel.absolute_url(),
      "line_items": {
          "price_data": {
            "currency": "EUR",
            "unit_amount": 2000, # for 20.00 EUR
            "product_data": {
              "name": "Product Name",
            }
          },
          "quantity": 1
        }
    }
    
     # this will redirect the user to stripe checkout page
    return module.StripePaymentSessionModule_createStripeSession(
      connector,
      data,
      context.getRelativeUrl(),
      resource="service_module/...",
    )
    ```
    
    Example interaction workflow script
    
    ```py
    if state_change['kwargs']['payment_status'] == 'paid':
      portal.accounting_module.newContent(
        portal_type='Payment Transaction',
        causality_value=state_change['object'],
        description="Stripe checkout ...",
        ...
      )
    
    ```
    
    Implementation notes:
    
    * Add new stripe connector in ERP5 to access retrieve Stripe session
    * New ERP5 functional module to handle Stripe Payment Sessions
    * Stripe Payment Session reflects a payment transaction/session initiated using the Stripe Checkout solution.
    * Workflow associated with Stripe Payment Sessions provides for the "draft", "open", "completed" and "expired" states in order to reflect the existing states on Stripe.
    * Each Stripe Payment Session offers a history and complete traceability of the HTTP exchanges carried out between ERP5 and Stripe from the start and the end of the payment transaction. All Stripe API calls (createSession, retrieveSession) and Stripe webhook POSTs are logged as system events (HTTP Exchange ERP5) related to a Stripe Payment Session.
    * Add alarm to handle Stripe Payment Sessions open whose date are expired
    * Set web service as source in Stripe Payment Session
    
    Reviewed-by: Jérome Perrin <jerome@nexedi.com>
    Reviewed-on: https://lab.nexedi.com/nexedi/erp5/merge_requests/1656

---
+++ bt5/erp5_stripe/SkinTemplateItem/portal_skins/erp5_stripe/ERP5Site_receiveStripeWebHook.py
@@ -0,0 +1,30 @@
+# proxy role: Author to create HTTP Exchange
+import json
+
+response = container.REQUEST.RESPONSE
+response.setHeader("Content-type", "application/json; charset=utf-8")
+
+# Stripe will only POST here
+if context.REQUEST["REQUEST_METHOD"] == "POST":
+  portal = context.getPortalObject()
+  data = json.loads(container.REQUEST.get("BODY") or "{}")
+  assert data, "BODY should not be empty"
+  assert "id" in data, "ID is missing"
+  assert data["object"] == "event", "Unexpected object"
+  store_webhook_tag = "store_webhook"
+  with portal.portal_activities.defaultActivateParameterDict({"tag": store_webhook_tag}, placeless=True):
+    system_event = portal.system_event_module.newContent(
+      title="WebHook Response",
+      portal_type="HTTP Exchange",
+      response=json.dumps(data, indent=2),
+      resource_value=portal.portal_categories.http_exchange_resource.stripe.webhook,
+    )
+    system_event.confirm()
+  alarm = portal.portal_alarms.handle_confirmed_http_exchanges
+  alarm.activate(
+    after_tag=store_webhook_tag,
+    activity='SQLQueue',
+  ).activeSense()
+  response.setStatus(200)
+else:
+  response.setStatus(400)
