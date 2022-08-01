0873d99f5433f48d73fd4d8eeb38167e6c73488d
Gabriel Monnerat <gabriel@nexedi.com>
2022-08-01 15:41:38 +0000

    erp5_stripe: introduce new module and features to handle Stripe checkout and payments in ERP5
    * Add new stripe connector in ERP5 to access retrieve Stripe session
    * New ERP5 functional module to handle Stripe Payment Sessions
    * Stripe Payment Session reflects a payment transaction/session initiated using the Stripe Checkout solution.
    * Workflow associated with Stripe Payment Sessions provides for the "draft", "open", "completed" and "expired" states in order to comply
    with the existing states on Stripe.
    * Each Stripe Payment Session offers a history and complete traceability of the HTTP exchanges carried out between ERP5 and Stripe from the start and the end of the payment transaction. All Stripe API calls (createSession, retrieveSession) and Stripe webhook POSTs are logged as system events (HTTP Exchange ERP5) related to a Stripe Payment Session.
    * Add alarm to handke Stripe Payment Sessions open

---
+++ bt5/erp5_stripe/SkinTemplateItem/portal_skins/erp5_stripe/Alarm_checkStripePaymentSession.py
@@ -0,0 +1,37 @@
+portal = context.getPortalObject()
+
+active_process = context.newActiveProcess().getRelativeUrl()
+
+connector_url, = [o.getRelativeUrl() for o in portal.portal_catalog(
+  portal_type="Stripe Connector",
+  reference=context.getTypeBasedMethod('getStripeConnectorReference')(),
+  validation_state="validated",
+)]
+
+method_id = "StripePaymentSession_checkStripeSessionOpen"
+
+kw = {
+  "portal_type": "Stripe Payment Session",
+  "validation_state": "open",
+}
+
+uid_list = [r.uid
+  for r in portal.portal_catalog(**kw)
+  if portal.portal_activities.countMessage(
+    method_id=method_id,
+    path=r.path) == 0
+]
+
+if not uid_list:
+  return
+
+kw["uid"] = uid_list
+
+portal.portal_catalog.searchAndActivate(
+  method_id=method_id,
+  method_kw={
+    "active_process": active_process,
+    "connector_url": connector_url
+  },
+  **kw
+)
