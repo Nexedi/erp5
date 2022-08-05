3095622c1575b8c3d9cd1abd0e35df733729268e
Romain Courteaud <romain@nexedi.com>
2022-08-05 12:43:14 +0000

    game_loop: init game loop with usage of WebWorker

---
+++ bt5/game_loop/PathTemplateItem/web_page_module/gadget_erp5_page_game_logic_js.js
@@ -0,0 +1,11 @@
+/*global window, rJS, jIO, RSVP, domsugar, console,
+         requestAnimationFrame, cancelAnimationFrame,
+         Worker,
+         DroneGameManager*/
+/*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
+
+// game.js
+(function () {
+  "use strict";
+  console.log('game logic');
+}(this));
