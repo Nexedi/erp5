2938aa5ce3a0fe8819417bd8827ecbdf290aec87
Gabriel Monnerat <gabriel@nexedi.com>
2022-08-30 17:39:26 +0000

    erp5_stripe: allow custom projects to create workflow interactions that waits catalog to index http exchange

--- bt5/erp5_stripe/SkinTemplateItem/portal_skins/erp5_stripe/StripePaymentSession_checkStripeSessionOpen.py
+++ bt5/erp5_stripe/SkinTemplateItem/portal_skins/erp5_stripe/StripePaymentSession_checkStripeSessionOpen.py
@@ -16,24 +16,29 @@ web_service = portal.restrictedTraverse(
 assert web_service.getPortalType() == "Stripe Connector", \
   "Unexpected connector %s" % connector_url
 
-http_exchange = context.StripePaymentSession_retrieveSession(
-  web_service,
-  batch_mode=True
-)
 
-response = json.loads(http_exchange.getResponse())
-
-if "error" in response and response["error"]["type"] == "invalid_request_error":
-  raise ValueError("Error to check %s" % context.getRelativeUrl())
-
-assert response["object"] == "checkout.session", "Unexpected Stripe Object"
-assert response["id"] == context.getReference(), "Unexpected Stripe ID"
-
-if response["status"] == "expired":
-  context.expire()
-elif response["status"] == "complete":
-  context.complete()
-else:
-  context.setExpirationDate(
-    context.getTypeBasedMethod('getStripePaymentSessionExpirationDate')()
+activity_tag = context.StripePaymentSession_generateActivityTag()
+with context.defaultActivateParameterDict({'tag': activity_tag}, placeless=True):
+  http_exchange = context.StripePaymentSession_retrieveSession(
+    web_service,
+    batch_mode=True
   )
+
+  response = json.loads(http_exchange.getResponse())
+
+  if "error" in response and response["error"]["type"] == "invalid_request_error":
+    raise ValueError("Error to check %s" % context.getRelativeUrl())
+
+  assert response["object"] == "checkout.session", "Unexpected Stripe Object"
+  assert response["id"] == context.getReference(), "Unexpected Stripe ID"
+
+# allow custom projects create workflow interactions that wait catalog to index http exchange
+with context.defaultActivateParameterDict({'after_tag': activity_tag}, placeless=True):
+  if response["status"] == "expired":
+    context.expire()
+  elif response["status"] == "complete":
+    context.complete()
+  else:
+    context.setExpirationDate(
+      context.getTypeBasedMethod('getStripePaymentSessionExpirationDate')()
+    )
