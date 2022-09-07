ca37e6d7ad13c6e29b6e631d316e2d259549c38a
Roque Porchetto <roque.porchetto@nexedi.com>
2022-09-07 14:50:13 +0000

    [WEB-WORKER] [WIP] Drone simulator

--- erp5_drone_simulator/PathTemplateItem/web_page_module/drone_web_worker_web_worker_js.js
+++ erp5_drone_simulator/PathTemplateItem/web_page_module/drone_web_worker_web_worker_js.js
@@ -4,33 +4,172 @@
          DroneGameManager*/
 /*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
 
-// game.js
-(function (worker) {
-  "use strict";
-  var window = {};
-  console.log('worker loading');
-  worker.onmessage = function (evt) {
-    //console.log('Worker: Message received from main script', evt.data);
-    var type = evt.data.type;
-    if (type === 'start') {
-      console.log('Worker: Message received from main script', evt.data);
+var document = {
+  addEventListener: function () {},
+  createElement: function () {}
+};
 
-      importScripts('babylon.js', evt.data.logic_url);
-      runGame(evt.data.canvas);
-      return worker.postMessage({'type': 'started'});
-    }
-    if (type === 'update') {
-      var i;
-      for (i = 0; i < 100000000; i += 1) {
-        1+1;
+(function (worker) {
+  importScripts('babylon.js', 'babylon.gui.js');
+}(this));
+
+var window = {
+  addEventListener: function (event, fn, opt) {
+    bindHandler('window', event, fn, opt);
+  },
+  PointerEvent: true
+};
+
+document = {
+  addEventListener: function (event, fn, opt) {
+    bindHandler('document', event, fn, opt);
+  },
+  createElement: function () {
+    return {onwheel: true};
+  },
+  defaultView: window
+};
+
+function mainToWorker(evt) {
+  switch (evt.data.type) {
+    case 'start':
+      var offscreen_canvas = prepareCanvas(evt.data);
+      //override createElement as it is needed by babylon to create a canvas
+      document.createElement = function (type) {
+        if (type === 'canvas') { return offscreen_canvas; }
+        return { onwheel: true };
       }
-      return worker.postMessage({'type': 'updated'});
-    }
-    throw new Error('Unsupported message ' + JSON.stringify(evt.data));
-    //self.postMessage('nutnut', evt);
-  };
+      //TODO evt.data.logic_url should contain the list of scripts
+      importScripts(
+                    'rsvp.js',
+                    'GameManager.js',
+                    'DroneManager.js',
+                    'MapManager.js',
+                    'ObstacleManager.js',
+                    'DroneAaileFixeAPI.js',
+                    'DroneLogAPI.js',
+                    'DroneAPI.js',
+                    evt.data.logic_url);
+      RSVP = window.RSVP;
+      window = undefined;
+      return new RSVP.Queue()
+        .push(function () {
+          return runGame(offscreen_canvas, evt.data.script,
+                         evt.data.game_parameters_json, evt.data.log);
+        })
+        .push(function () {
+          return postMessage({'type': 'started'});
+        });
+      break;
+    case 'update':
+      return new RSVP.Queue()
+        .push(function () {
+          return updateGame();
+        })
+        .push(function () {
+          return postMessage({'type': 'updated'});
+        });
+      break;
+    case 'event':
+      handleEvent(evt.data);
+      break;
+    //case 'mousewheel':
+    //  eventGame(evt.data.eventClone);
+    //  break;
+    default:
+      throw new Error('Unsupported message ' + JSON.stringify(evt.data));
+  }
+};
+
+// Doesn't work without it
+class HTMLElement {}
+
+self.handlers = new Map();
+self.canvas = null;
+
+// getBoundingInfo()
+const rect = {
+	top: 0,
+	left: 0,
+	right: 0,
+	bottom: 0,
+	x: 0,
+	y: 0,
+	height: 0,
+	width: 0,
+};
+
+function bindHandler(targetName, eventName, fn, opt) {
+	const handlerId = targetName + eventName;
+  console.log("[WEBWORKER] bindHandler. handlerId:", handlerId);
+	handlers.set(handlerId, fn);
+	postMessage({
+		type: 'event',
+		targetName: targetName,
+		eventName: eventName,
+		opt: opt,
+	})
+}
+
+function handleEvent(event) {
+	const handlerId = event.targetName + event.eventName;
+  console.log("[WEBWORKER] handlerId:", handlerId);
+	event.eventClone.preventDefault = noop;
+	event.eventClone.target = self.canvas;
+	if (!handlers.has(handlerId)) {
+		throw new Error('Unknown handlerId: ' + handlerId);
+	}
+	handlers.get(handlerId)(event.eventClone);
+}
+
+function prepareCanvas(data) {
+	const canvas = data.canvas;
+  self.canvas = canvas;
+	canvas.clientWidth = data.width;
+	canvas.clientHeight = data.height;
+	canvas.width = data.width;
+	canvas.height = data.height;
+	rect.right = rect.width = data.width;
+	rect.bottom = rect.height = data.height;
+	canvas.setAttribute = function (name, value) {
+		postMessage({
+			type: 'canvasMethod',
+			method: 'setAttribute',
+			args: [name, value],
+		})
+	};
+	canvas.addEventListener = function (event, fn, opt) {
+		bindHandler('canvas', event, fn, opt);
+	};
+  canvas.getBoundingClientRect = function () {
+		return rect;
+	};
+	canvas.focus = function () {
+		postMessage({
+			type: 'canvasMethod',
+			method: 'focus',
+			args: [],
+		})
+	};
+	// noinspection JSUnusedGlobalSymbols
+	const style = {
+		set touchAction(value) {
+			postMessage({
+				type: 'canvasStyle',
+				name: 'touchAction',
+				value: value,
+			})
+		}
+	};
+	Object.defineProperty(canvas, 'style', {get() {return style}});
+	return canvas;
+}
+
+function noop() { console.log("noop!"); }
+
+(function (worker) {
+  worker.onmessage = mainToWorker;
   worker.postMessage({
     'type': 'loaded'
   });
-  //throw new Error('argh');
 }(this));
