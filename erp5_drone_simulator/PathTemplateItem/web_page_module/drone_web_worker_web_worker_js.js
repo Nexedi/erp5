0712c40d1d2ad3de5e7abfcfb14ecdf0a792f196
Roque Porchetto <roque.porchetto@nexedi.com>
2022-09-07 14:50:13 +0000

    [WEB-WORKER] [WIP] Drone simulator

--- erp5_drone_simulator/PathTemplateItem/web_page_module/drone_web_worker_web_worker_js.js
+++ erp5_drone_simulator/PathTemplateItem/web_page_module/drone_web_worker_web_worker_js.js
@@ -3,28 +3,69 @@
          Worker, importScripts,
          DroneGameManager*/
 /*jslint nomen: true, indent: 2, maxerr: 3, maxlen: 80 */
+// game.js
+
+var document = {
+  addEventListener: function () {},
+  createElement: function () {}
+};
+
+(function (worker) {
+  importScripts('babylon.js', 'babylon.gui.js');
+}(this));
+
+var window = {
+  addEventListener: function () {}
+};
 
 // game.js
 (function (worker) {
-  "use strict";
-  var window = {};
   console.log('worker loading');
   worker.onmessage = function (evt) {
     //console.log('Worker: Message received from main script', evt.data);
     var type = evt.data.type;
     if (type === 'start') {
       console.log('Worker: Message received from main script', evt.data);
-
-      importScripts('babylon.js', evt.data.logic_url);
-      runGame(evt.data.canvas);
-      return worker.postMessage({'type': 'started'});
+      //override createElement as it is needed by babylon to create a canvas
+      document.createElement = function (type) {
+        if (type === 'canvas') {
+          return evt.data.canvas;
+        }
+      }
+      //TODO evt.data.logic_url should contain the list of scripts
+      importScripts(
+                    'rsvp.js',
+                    'GameManager.js',
+                    'DroneManager.js',
+                    'MapManager.js',
+                    'ObstacleManager.js',
+                    'DroneAaileFixeAPI.js',
+                    'DroneLogAPI.js',
+                    'DroneAPI.js',
+                    evt.data.logic_url);
+      RSVP = window.RSVP;
+      window = undefined;
+      return new RSVP.Queue()
+        .push(function () {
+          return runGame(evt.data.canvas, evt.data.script, evt.data.map, evt.data.log);
+        })
+        .push(function () {
+          return worker.postMessage({'type': 'started'});
+        });
     }
     if (type === 'update') {
-      var i;
+      return new RSVP.Queue()
+        .push(function () {
+          return updateGame();
+        })
+        .push(function () {
+          return worker.postMessage({'type': 'updated'});
+        });
+      /*var i;
       for (i = 0; i < 100000000; i += 1) {
         1+1;
       }
-      return worker.postMessage({'type': 'updated'});
+      return worker.postMessage({'type': 'updated'});*/
     }
     throw new Error('Unsupported message ' + JSON.stringify(evt.data));
     //self.postMessage('nutnut', evt);
