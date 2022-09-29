5943aae08a5182708522f1fc29c995382fdf458c
Roque Porchetto <roque.porchetto@nexedi.com>
2022-09-29 19:07:49 +0000

    erp5_standard_drone_simulator: drone simulator page with babylon js basic example running on a web worker

---
+++ bt5/erp5_standard_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_gadget_js.js
@@ -0,0 +1,48 @@
+(function (window, rJS, domsugar, DroneGameManager) {
+  "use strict";
+
+  rJS(window)
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+
+    .declareMethod('render', function renderHeader() {
+      var gadget = this,
+        // XXX hardcoded
+        parameter_gamelogic = 'gadget_erp5_page_drone_simulator_logic.js',
+        canvas = domsugar('canvas'),
+        offscreen;
+      domsugar(gadget.element, [canvas]);
+
+      //TODO fix hardcoded
+      canvas.width = 680;//canvas.clientWidth; <-- this is 0
+      canvas.height = 340;//canvas.clientHeight; <-- this is 0
+
+      // https://doc.babylonjs.com/divingDeeper/scene/offscreenCanvas
+      offscreen = canvas.transferControlToOffscreen();
+
+      return new RSVP.Queue()
+        .push(function () {
+          gadget.runGame({
+            logic_url: parameter_gamelogic,
+            canvas: offscreen,
+            canvas_original: canvas,
+            width: canvas.width,
+            height: canvas.height
+          });
+
+          return gadget.updateHeader({
+            page_title: 'BabylonJS Canvas In Web Worker',
+            page_icon: 'puzzle-piece'
+          });
+        });
+    })
+
+    .declareJob('runGame', function runGame(options) {
+      var gadget = this,
+        game_manager = new DroneGameManager();
+      return game_manager.play(options);
+    });
+
+}(window, rJS, domsugar, DroneGameManager));
