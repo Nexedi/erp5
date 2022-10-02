81393f87acda9c235b876ee179f206a5de7bdb93
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-02 16:17:44 +0200

    import six.string_types as basestring for Python 3.

--- bt5/erp5_syncml/ToolComponentTemplateItem/portal_components/tool.erp5.SynchronizationTool.py
+++ bt5/erp5_syncml/ToolComponentTemplateItem/portal_components/tool.erp5.SynchronizationTool.py
@@ -26,6 +26,7 @@
 ##############################################################################
 
 from logging import getLogger
+from six import string_types as basestring
 
 from AccessControl import ClassSecurityInfo
 
