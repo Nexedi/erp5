ae7e4aeb1d362f05116c9f91ad7894a7d8eeb980
Romain Courteaud <romain@nexedi.com>
2022-10-04 12:12:21 +0000

    erp5_web_js_style_test: code test

--- bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/WebSite_downloadFakeImage.py
+++ bt5/erp5_web_js_style_test/SkinTemplateItem/portal_skins/erp5_web_js_style_test/WebSite_downloadFakeImage.py
@@ -2,9 +2,9 @@ response = REQUEST.RESPONSE
 
 response.setHeader('Content-Type', 'image/svg+xml')
 
-if (format == 'jpg') and (display == 'small') and (quality == '90'):
-  emoji = '👍'
+if (display == 'small') and (quality == '90'):
+  emoji = 'Y'
 else:
-  emoji = '👎'
+  emoji = 'N'
 
 return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90%">' + emoji + '</text></svg>'
