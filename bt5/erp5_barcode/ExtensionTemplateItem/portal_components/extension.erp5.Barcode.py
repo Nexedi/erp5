c7078c077375cf2b2391ab64cd831317e3ea1f7c
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-04 10:19:21 +0200

    fixup! py2/py3: stop using deprecated string functions, that are removed in python 3.

--- bt5/erp5_barcode/ExtensionTemplateItem/portal_components/extension.erp5.Barcode.py
+++ bt5/erp5_barcode/ExtensionTemplateItem/portal_components/extension.erp5.Barcode.py
@@ -26,7 +26,6 @@
 ##############################################################################
 
 import os
-from string import zfill
 from urllib import urlencode
 import tempfile
 from DateTime import DateTime
@@ -82,14 +81,14 @@ def printBarcodeSheet(self, sheet_number=1, input_list=[], test=False):
   tempfile.tempdir = tempdir
   if test:
     #Fake list
-    input_list = os.linesep.join(['TEST%s' % zfill(b, 8) for b in range(1111111,  1111111 + ( row_number * column_number * sheet_number ))])
+    input_list = os.linesep.join(['TEST%s' % str(b).zfill(8) for b in range(1111111,  1111111 + ( row_number * column_number * sheet_number ))])
   elif input_list not in ('', None):
    if not isinstance(input_list, list):
      input_list = os.linesep.join(map(escapeString, input_list.split(os.linesep)))
    else:
      input_list = os.linesep.join(input_list)
   else:
-    input_list = os.linesep.join(['%s' % zfill(self.portal_ids.generateNewId(id_group='barcode'), 12) for b in range( row_number * column_number * sheet_number )])
+    input_list = os.linesep.join(['%s' % str(self.portal_ids.generateNewId(id_group='barcode')).zfill(12) for b in range( row_number * column_number * sheet_number )])
   text_command = 'echo "%s" > %s' % (input_list, new_txt_file_path)
   ret = os.system(text_command)
   if ret != 0:
