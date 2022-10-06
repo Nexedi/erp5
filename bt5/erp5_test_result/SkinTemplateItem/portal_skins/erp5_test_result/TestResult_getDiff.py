c02f3c1958a4aaae757c8763a1a1eb1fb92a7574
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-07 00:01:48 +0200

    erp5_test_result: Make code compatible with both python2 and python3.

--- bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_getDiff.py
+++ bt5/erp5_test_result/SkinTemplateItem/portal_skins/erp5_test_result/TestResult_getDiff.py
@@ -9,6 +9,7 @@ if len(value_list) != 2:
 a, b = value_list
 
 from Products.ERP5Type.Document import newTempBase
+import six
 
 # make sure that a is the oldest result
 if a.getIntIndex() > b.getIntIndex():
@@ -50,7 +51,7 @@ for a_line in a.searchFolder(portal_type='Test Result Line'):
                                    before_url=a_line.absolute_url(),
                                    **d))
 
-for title, not_in_a in b_title_dict.iteritems():
+for title, not_in_a in six.iteritems(b_title_dict):
   b_line = b[not_in_a]
   d = dict((prop, b_line.getProperty(prop)) for prop in compared_prop_list)
   object_list.append(newTempBase(context, title,
