42fac530ee15bd44768df4aa5623533ae8fb66be
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-07 17:17:15 +0200

    py2/py3: modernize -f xrange_six.

--- bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testFolder.py
+++ bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testFolder.py
@@ -37,6 +37,7 @@ from Products.ERP5Type.Cache import clearCache
 from Products.ERP5Type.Core.Folder import FragmentedLength, FRAGMENTED_LENGTH_THRESHOLD
 from AccessControl.ZopeGuards import guarded_getattr
 from zExceptions import Unauthorized
+from six.moves import range
 
 class TestFolder(ERP5TypeTestCase, LogInterceptor):
 
@@ -108,7 +109,7 @@ class TestFolder(ERP5TypeTestCase, LogInterceptor):
               'return %s[len(context)]' % (repr(id_generator_id_list), ))
     self.folder.setIdGenerator(id_generator_script_name)
     self.assertEqual(self.folder.getIdGenerator(), id_generator_script_name)
-    for expected_length in xrange(len(id_generator_id_list)):
+    for expected_length in range(len(id_generator_id_list)):
       self.assertEqual(len(self.folder), expected_length)
       obj = self.newContent()
       self.assertEqual(obj.getId(), id_generator_id_list[expected_length])
@@ -278,7 +279,7 @@ class TestFolder(ERP5TypeTestCase, LogInterceptor):
     self.assertEqual(len(folder), 1)
     self.assertIsInstance(folder_dict['_count'], Length)
     original_length_oid = folder_dict['_count']._p_oid
-    for _ in xrange(FRAGMENTED_LENGTH_THRESHOLD - len(folder) - 1):
+    for _ in range(FRAGMENTED_LENGTH_THRESHOLD - len(folder) - 1):
       folder.newContent(portal_type='Folder')
     self.assertEqual(len(folder), FRAGMENTED_LENGTH_THRESHOLD - 1)
     self.assertIsInstance(folder_dict['_count'], Length)
--- bt5/erp5_stock_cache/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_trimInventoryCacheOnCatalog.py
+++ bt5/erp5_stock_cache/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_trimInventoryCacheOnCatalog.py
@@ -1,11 +1,12 @@
 from Products.ERP5Type.Errors import ProgrammingError
+from six.moves import range
 
 zTrimInventoryCacheFromDateOnCatalog = getattr(context, 'SimulationTool_zTrimInventoryCacheFromDateOnCatalog', None)
 if zTrimInventoryCacheFromDateOnCatalog is None:
   return
 
 min_date = None
-for loop_item in xrange(len(uid)):
+for loop_item in range(len(uid)):
     if not isInventoryMovement[loop_item] and isMovement[loop_item] and getResourceUid[loop_item]:
         if getDestinationUid[loop_item] and getStopDate[loop_item]:
             if min_date:
