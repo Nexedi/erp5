bda034129772d0169143d1930e0046dbe423a1d3
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-07 17:17:15 +0200

    py2/py3: modernize -f xrange_six.

--- bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_getDummyReturningOnlyWhatShownListBoxLineList.py
+++ bt5/erp5_ui_test/SkinTemplateItem/portal_skins/erp5_ui_test/Foo_getDummyReturningOnlyWhatShownListBoxLineList.py
@@ -1,5 +1,6 @@
 """Returns a list of temp base objects"""
 from Products.ERP5Type.Document import newTempBase
+from six.moves import range
 portal_object = context.getPortalObject()
 
 limit = kw.get('limit')
@@ -9,7 +10,7 @@ size = list_lines or limit
 
 result_list = []
 result_list_append = result_list.append
-for i in xrange(7):
+for i in range(7):
   if list_start < i + 1<= list_start + size:
     caption = str(i)
     result_list_append(newTempBase(portal_object, caption, a='A' + caption, b='B' + caption))
