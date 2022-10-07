45acd1b595409b610df2714ac478b942767a6b59
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-07 17:17:15 +0200

    py2/py3: modernize -f xrange_six.

--- bt5/erp5_dms/TestTemplateItem/portal_components/test.erp5.testERP5WebWithDms.py
+++ bt5/erp5_dms/TestTemplateItem/portal_components/test.erp5.testERP5WebWithDms.py
@@ -46,6 +46,7 @@ from Products.ERP5Type.tests.utils import FileUpload, createZODBPythonScript
 from erp5.component.document.Document import ConversionError
 
 from PIL import Image
+from six.moves import range
 
 LANGUAGE_LIST = ('en', 'fr', 'de', 'bg',)
 IMAGE_COMPARE_TOLERANCE = 850
@@ -494,7 +495,7 @@ class TestERP5WebWithDms(ERP5TypeTestCase, ZopeTestCase.Functional):
     policy_list = self.portal.caching_policy_manager.listPolicies()
     policy = [policy[1] for policy in policy_list\
                 if policy[0] == 'unauthenticated no language'][0]
-    for i in xrange(3):
+    for i in range(3):
       path = '/'.join((website_url,
                        reference,
                        'img%s.png' % i))
@@ -671,7 +672,7 @@ class TestERP5WebWithDms(ERP5TypeTestCase, ZopeTestCase.Functional):
       self.assertEqual(policy.getMaxAgeSecs(), 1200)
       self.assertEqual(policy.getStaleWhileRevalidateSecs(), 30)
       self.assertEqual(policy.getStaleIfErrorSecs(), 600)
-      for i in xrange(3):
+      for i in range(3):
         path = '/'.join((website_url,
                          reference,
                          'img%s.png' % i))
