88e5f9323870ed0e362f354dd89906a2b6766fee
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-07 16:38:32 +0200

    py2/py3: import cStringIO from six.moves.

--- bt5/erp5_big_file/TestTemplateItem/portal_components/test.erp5.testBigFile.py
+++ bt5/erp5_big_file/TestTemplateItem/portal_components/test.erp5.testBigFile.py
@@ -24,7 +24,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from cStringIO import StringIO
+from six.moves import cStringIO as StringIO
 
 from ZPublisher.HTTPRequest import HTTPRequest
 from ZPublisher.HTTPResponse import HTTPResponse
