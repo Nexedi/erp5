c85dd3b14d6efe39f6b43345ae76261b9e469577
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-10 09:59:38 +0200

    py2/py3: use hexdigest instead of digest in inventory cache key to avoid non-valid UTF-8 bytes.
    One problem with hexdigest is that it's longer and the column was
    created as binary(16). We don't have a mechanism to run migrations on
    this table, so we use UNHEX in SQL to have the equivalent as digest()
    from hexdigest()
    
    Co-authored-by: Jérome Perrin <jerome@nexedi.com>

--- bt5/erp5_stock_cache/SkinTemplateItem/portal_skins/erp5_stock_cache/Resource_zGetInventoryCacheResult.sql
+++ bt5/erp5_stock_cache/SkinTemplateItem/portal_skins/erp5_stock_cache/Resource_zGetInventoryCacheResult.sql
@@ -4,7 +4,7 @@ SELECT
 FROM 
   inventory_cache 
 WHERE 
-  inventory_cache.query=<dtml-sqlvar query type="string"> 
+  inventory_cache.query = UNHEX(<dtml-sqlvar query type="string">)
 <dtml-if date>
   AND 
   inventory_cache.date <= <dtml-sqlvar date type="datetime">
