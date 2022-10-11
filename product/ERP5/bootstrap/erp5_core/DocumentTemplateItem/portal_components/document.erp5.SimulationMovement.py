0b2df3e6deb5260d4137c23dc6a673726fbdc120
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-11 16:16:34 +0200

    py2/py3: explicitly cast dict keys to list.

--- product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.SimulationMovement.py
+++ product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.SimulationMovement.py
@@ -32,6 +32,7 @@ from AccessControl import ClassSecurityInfo
 
 from Products.ERP5Type import Permissions, PropertySheet, interfaces
 from Products.ERP5Type.TransactionalVariable import getTransactionalVariable
+from Products.ERP5Type.Utils import ensure_list
 from erp5.component.document.Movement import Movement
 from erp5.component.module.ExpandPolicy import policy_dict, TREE_DELIVERED_CACHE_KEY
 
@@ -287,7 +288,7 @@ class SimulationMovement(PropertyRecordableMixin, Movement, ExplainableMixin):
         # applicable rule per reference. It indicates a configuration error.
         applicable_rule_dict.setdefault(reference, rule)
 
-    applicable_rule_list = applicable_rule_dict.values()
+    applicable_rule_list = ensure_list(applicable_rule_dict.values())
     for applied_rule in list(self.objectValues()):
       rule = applied_rule.getSpecialiseValue()
       try:
