cba1d82286c946c6ecd7956cb27553a22bb5cf12
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-17 22:47:39 +0200

    workflow: now another workflow script should be accessed by container.script_<SCRIPT_ID>, instead of container.<SCRIPT_ID>.

--- bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/internal_invoice_transaction_simulation_workflow/script_validateTransactionLines.py
+++ bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/internal_invoice_transaction_simulation_workflow/script_validateTransactionLines.py
@@ -7,4 +7,4 @@ if old_state.getId() == 'draft':
   if internal_invoice.InternalInvoiceTransaction_getAuthenticatedUserSection() == internal_invoice.getDestinationSection():
     raise ValidationFailed(translateString("Your entity should not be destination."))
 
-return state_change.getPortal().portal_workflow.accounting_workflow.scripts[script.getId()](state_change)
+return state_change.getPortal().portal_workflow.accounting_workflow[script.getId()](state_change)
