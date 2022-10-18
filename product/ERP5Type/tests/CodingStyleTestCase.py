c57e620d1564c78893817abbc1b91bb65fa82978
Vincent Pelletier <vincent@nexedi.com>
2022-10-18 14:14:24 +0900

    ERP5Type.tests.CodingStyleTestCase: Trigger workflow migration.
    This should detect any business template with non-migrated workflows.

--- product/ERP5Type/tests/CodingStyleTestCase.py
+++ product/ERP5Type/tests/CodingStyleTestCase.py
@@ -138,6 +138,8 @@ class CodingStyleTestCase(ERP5TypeTestCase):
     template_tool = self.portal.portal_templates
     for bt_title in self.getTestedBusinessTemplateList():
       bt = template_tool.getInstalledBusinessTemplate(bt_title, strict=True)
+      # run migrations on the business template
+      bt.BusinessTemplate_convertAllDCWorkflowToERP5Workflow()
       # make sure we can rebuild
       bt.build()
 
