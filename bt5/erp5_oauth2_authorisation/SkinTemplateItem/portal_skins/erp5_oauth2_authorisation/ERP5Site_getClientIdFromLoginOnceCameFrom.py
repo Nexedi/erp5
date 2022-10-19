c64c00182b238fea57e5b116cbf5c12ad20a6894
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-19 15:43:26 +0200

    py2/py3: import from six.moves.

--- bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/ERP5Site_getClientIdFromLoginOnceCameFrom.py
+++ bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/ERP5Site_getClientIdFromLoginOnceCameFrom.py
@@ -6,14 +6,14 @@ Once the user is authenticated, the same value can be accessed with:
   from AccessControl import getSecurityManager
   getSecurityManager().getUser().getClientId()
 """
-import urlparse
+from six.moves.urllib.parse import parse_qsl, urlsplit
 # The came_from for login_once_form is special: it has no scheme, no netloc, a path and a query.
 # Verify this so caller knows if they are providing the wrong value.
 if not context.ERP5Site_isOAuth2CameFrom(came_from=came_from):
   raise ValueError
 result, = [
   value
-  for name, value in urlparse.parse_qsl(urlparse.urlsplit(came_from).query)
+  for name, value in parse_qsl(urlsplit(came_from).query)
   if name == 'client_id'
 ]
 return result
