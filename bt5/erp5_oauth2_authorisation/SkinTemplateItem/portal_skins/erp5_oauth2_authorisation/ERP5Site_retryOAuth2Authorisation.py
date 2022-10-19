3d077903d191db598c0bd5952cd306123a687739
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-10-19 15:43:26 +0200

    py2/py3: import from six.moves.

--- bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/ERP5Site_retryOAuth2Authorisation.py
+++ bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/ERP5Site_retryOAuth2Authorisation.py
@@ -3,16 +3,16 @@
 Retry calling /authorize using the values in came_from
 (which a previous call to /authorize generated, and is not a traditional came_from).
 """
-import urlparse
+from six.moves.urllib.parse import parse_qsl, urlsplit
 from erp5.component.document.OAuth2AuthorisationServerConnector import substituteRequest
 if not context.ERP5Site_isOAuth2CameFrom(came_from):
   # came_from is broken, there is no way to call /authorize , so escape to wherever.
   context.Base_redirect()
   return
-parsed_came_from = urlparse.urlsplit(came_from)
+parsed_came_from = urlsplit(came_from)
 query_list = [
   (key, value)
-  for key, value in urlparse.parse_qsl(parsed_came_from.query)
+  for key, value in parse_qsl(parsed_came_from.query)
   if key != 'portal_status_message'
 ]
 if portal_status_message is not None:
