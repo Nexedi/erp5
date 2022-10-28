db30442323dabf3c5440a859232e7890b0c18fd2
Jérome Perrin <jerome@nexedi.com>
2022-10-28 15:17:29 +0900

    stripe: remove not used "extra_header" parameter

--- bt5/erp5_stripe/DocumentTemplateItem/portal_components/document.erp5.StripeConnector.py
+++ bt5/erp5_stripe/DocumentTemplateItem/portal_components/document.erp5.StripeConnector.py
@@ -91,7 +91,6 @@ class StripeConnector(XMLObject):
     header_dict = {
       'Content-Type': 'application/x-www-form-urlencoded',
     }
-    header_dict.update(kw.get("extra_header") or {})
     url_string = self.getUrlString()
     if not url_string.endswith("/"):
       url_string += "/"
