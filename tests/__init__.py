8eff9aa7ccf44a82c5648e9c172a4aa09d62ce4f
Roque Porchetto <roque.porchetto@nexedi.com>
2022-10-31 20:30:07 +0100

    hardcoded

--- tests/__init__.py
+++ tests/__init__.py
@@ -40,7 +40,7 @@ class _ERP5(ERP5TypeTestSuite):
         glob(path + '/product/*/tests/test*.py'),
         #glob(path + '/bt5/*/TestTemplateItem/test*.py'),
         #glob(path + '/bt5/*/TestTemplateItem/portal_components/test.*.test*.py')):
-        glob(path + '/bt5/erp5_drone_flight_comparison_ui_test/TestTemplateItem/portal_components/test.*.test*.py')):
+        glob(path + '/bt5/erp5_officejs_drone_simulator_test/TestTemplateItem/portal_components/test.*.test*.py')):
       component_re_match = component_re.match(test_path)
       if component_re_match is not None:
         test_case = "%s:%s" % (component_re_match.group(1),
