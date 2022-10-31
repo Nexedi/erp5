be060152699a13257e8e1c375163005cc253110c
Roque Porchetto <roque.porchetto@nexedi.com>
2022-10-31 15:32:45 +0000

    erp5_drone_simulator: gadget and game logic

---
+++ bt5/erp5_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_gadget_js.js
@@ -0,0 +1,50 @@
+(function (window, rJS, domsugar, DroneGameManager, document, Blob) {
+  "use strict";
+
+  var canvas, offscreen;
+  var LOGIC_FILE_LIST = [
+    'gadget_erp5_page_drone_simulator_logic.js',
+    'gadget_erp5_page_drone_simulator_droneaaailefixe.js',
+    'gadget_erp5_page_drone_simulator_dronelogfollower.js'
+  ];
+
+  rJS(window)
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    // for standaloneDemo
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+
+    .declareMethod('render', function render() {
+      var gadget = this,
+        loading = domsugar('span', ["Loading..."]),
+        container = domsugar('div');
+      canvas = domsugar('canvas');
+      loading.id = "loading";
+      container.className = 'container';
+      container.appendChild(canvas);
+      domsugar(gadget.element, [loading, container]);
+      //TODO fix hardcoded
+      canvas.width = 680;//canvas.clientWidth; <-- this is 0
+      canvas.height = 340;//canvas.clientHeight; <-- this is 0
+      // https://doc.babylonjs.com/divingDeeper/scene/offscreenCanvas
+      offscreen = canvas.transferControlToOffscreen();
+    })
+
+    // To be called outside
+    .declareMethod('runGame', function runGame(options) {
+      options.canvas = offscreen;
+      options.canvas_original = canvas;
+      options.width = canvas.width;
+      options.height = canvas.height;
+      options.logic_url_list = LOGIC_FILE_LIST;
+      var gadget = this,
+        game_manager = new DroneGameManager(gadget);
+      return game_manager.play(options)
+      .push(function () {
+        return game_manager.result();
+      });
+    });
+
+}(window, rJS, domsugar, DroneGameManager, document, Blob));
