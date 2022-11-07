0f8d7f272e3f1541d79e668c9c3038ee204141f8
Jérome Perrin <jerome@nexedi.com>
2022-11-07 17:32:39 +0900

    hal_json_style: fix double translation of worklist message
    There was a regression with 45c03413ac (hal_json_style: prevent a
    translation of worklist with document count, 2021-05-10), the title
    of the worklist was translated twice. This was supposed to be
    catched by the test, by checking that the mocked gettext had only
    one call for "Draft to Validate", assuming that the translation of
    "Draft to Validate" was "Draft to Validate" and that a double
    translation would cause this to be translated twice, but because of
    a side effect of another test inserting a translation for
    "Draft to Validate", this problem was not noticed.
    
    This fixes the double translation and adjust the test to not depend
    on the actual content of message catalog.
    
    Co-authored-by: Georgios Dagkakis <georgios.dagkakis@nexedi.com>

--- bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalJsonStyle.py
+++ bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalJsonStyle.py
@@ -65,6 +65,7 @@ def simulate(script_id, params_string, code_string):
       try:
         result = f(self, *args, **kw)
       finally:
+        transaction.abort()
         if script_id in self.portal.portal_skins.custom.objectIds():
           self.portal.portal_skins.custom.manage_delObjects(script_id)
         transaction.commit()
@@ -159,17 +160,11 @@ def replace_request(new_request, context):
 
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
 
-#####################################################
-# Base_getRequestHeader
-#####################################################
+
 class ERP5HALJSONStyleSkinsMixin(ERP5TypeTestCase):
   def afterSetUp(self):
     self.login()
     wipeFolder(self.portal.foo_module, commit=False)
-
-
-  def beforeTearDown(self):
-    transaction.abort()
 
   def generateNewId(self):
     return "%sö" % self.portal.portal_ids.generateNewId(
@@ -184,6 +179,13 @@ class ERP5HALJSONStyleSkinsMixin(ERP5TypeTestCase):
     )
     return foo
 
+<<<<<<< HEAD
+=======
+
+#####################################################
+# Base_getRequestHeader
+#####################################################
+>>>>>>> 4ba1c39f16 (hal_json_style: adjust transaction interactions in the test)
 class TestBase_getRequestHeader(ERP5HALJSONStyleSkinsMixin):
   @changeSkin('Hal')
   def test_getRequestHeader_REQUEST_disallowed(self):
@@ -2437,8 +2439,9 @@ class TestERP5Person_getHateoas_mode_search(ERP5HALJSONStyleSkinsMixin):
     self.tic()
 
   def beforeTearDown(self):
+    super(TestERP5Person_getHateoas_mode_search, self).beforeTearDown()
     self.portal.person_module.deleteContent(self.person.getId())
-
+    self.tic()
 
   @simulate('Base_getRequestUrl', '*args, **kwargs', 'return "http://example.org/bar"')
   @simulate('Base_getRequestHeader', '*args, **kwargs', 'return "application/hal+json"')
@@ -2942,9 +2945,8 @@ return msg"
     # worklists with the actual count of document
     fake_request = do_fake_request("GET")
 
-    default_gettext = self.portal.Localizer.erp5_ui.gettext
     def gettext(message, **kw):
-      return default_gettext(message, **kw)
+      return kw.get('default', message)
 
     with mock.patch.object(self.portal.Localizer.erp5_ui.__class__, 'gettext', side_effect=gettext) as gettext_mock:
       self.portal.web_site_module.hateoas.ERP5Document_getHateoas(

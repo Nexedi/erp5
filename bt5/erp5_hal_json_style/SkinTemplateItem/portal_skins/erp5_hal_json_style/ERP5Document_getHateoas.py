Nexedi/erp5

commit 45c03413ac6c7762027a4b7908ef907c870968a7

hal_json_style: prevent a translation of worklist with document count
When viewing the worklist page, a request is made to traverse portal_workflow,
which translate global worklist actions including the document count.

The non regression test was failing with:

    [u'Draft To Validate (1)'] != ['Draft To Validate']

and in real usage, several messages were added to Localizer.

bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -343,6 +343,14 @@ portal_absolute_url = portal.absolute_url()
 preference_tool = portal.portal_preferences
 Base_translateString = portal.Base_translateString
 
+def translateWorklistActionName(worklist_action_name):
+  """Translate a worklist action name, that may contain the document count.
+
+  Instead of translating "Draft to Validate (123)", we translate "Draft to Validate".
+  """
+  return Base_translateString(re.sub(r' \(\d+\)$', '', worklist_action_name))
+
+
 preferred_html_style_developer_mode = preference_tool.getPreferredHtmlStyleDevelopperMode()
 preferred_html_style_translator_mode = preference_tool.getPreferredHtmlStyleTranslatorMode()
 
@@ -1519,7 +1527,10 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
           'href': '%s' % view_action['url'],
           'name': view_action['id'],
           'icon': view_action['icon'],
-          'title': Base_translateString(view_action['title']),
+          'title': Base_translateString(
+              translateWorklistActionName(view_action['title'])
+              if 'worklist_id' in view_action
+              else view_action['title']),
         })
 
         global_action_type = ("view", "workflow", "object_new_content_action",
@@ -2321,7 +2332,7 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
         'href': url_template_dict["jio_search_template"] % {
           "query": make_query({"query": query})
         },
-        'name': Base_translateString(re.sub(r' \(\d+\)$', '', action['name'])),
+        'name': translateWorklistActionName(action['name']),
         'count': action['count']
       }
 

bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalJsonStyle.py
@@ -16,6 +16,7 @@ import json
 import re
 import urllib
 
+import mock
 from zope.globalrequest import setRequest #  pylint: disable=no-name-in-module, import-error
 from Acquisition import aq_base
 from Products.ERP5Form.Selection import Selection, DomainSelection
@@ -2871,6 +2872,33 @@ return msg"
     self.assertEqual(work_list[0]['href'], 'urn:jio:allDocs?query=portal_type%3A%28%22Bar%22%20OR%20%22Foo%22%29%20AND%20simulation_state%3A%22draft%22')
 
     self.assertEqual(result_dict['_debug'], "worklist")
+
+  @simulate('Base_getRequestUrl', '*args, **kwargs',
+      'return "http://example.org/bar"')
+  @simulate('Base_getRequestHeader', '*args, **kwargs',
+            'return "application/hal+json"')
+  @simulate('Base_translateString', 'msg, catalog="ui", encoding="utf8", lang="wo", **kw',
+  code_string)
+  @createIndexedDocument()
+  @changeSkin('Hal')
+  def test_getHateoasWorklist_portal_workflow_worklist_translation(self, document):
+    # Non regression test that traversing portal_workflow does not translate
+    # worklists with the actual count of document
+    fake_request = do_fake_request("GET")
+
+    default_gettext = self.portal.Localizer.erp5_ui.gettext
+    def gettext(message, **kw):
+      return default_gettext(message, **kw)
+
+    with mock.patch.object(self.portal.Localizer.erp5_ui.__class__, 'gettext', side_effect=gettext) as gettext_mock:
+      self.portal.web_site_module.hateoas.ERP5Document_getHateoas(
+        REQUEST=fake_request,
+        mode='traverse',
+        relative_url='portal_workflow'
+      )
+    self.assertEqual(
+        [x[1][0] for x in gettext_mock.mock_calls if x[1][0].startswith('Draft To Validate')],
+        ['Draft To Validate'])
 
   @simulate('Base_getRequestUrl', '*args, **kwargs',
       'return "http://example.org/bar"')

commit 0f8d7f272e3f1541d79e668c9c3038ee204141f8

hal_json_style: fix double translation of worklist message
There was a regression with 45c03413ac (hal_json_style: prevent a
translation of worklist with document count, 2021-05-10), the title
of the worklist was translated twice. This was supposed to be
catched by the test, by checking that the mocked gettext had only
one call for "Draft to Validate", assuming that the translation of
"Draft to Validate" was "Draft to Validate" and that a double
translation would cause this to be translated twice, but because of
a side effect of another test inserting a translation for
"Draft to Validate", this problem was not noticed.

This fixes the double translation and adjust the test to not depend
on the actual content of message catalog.

Co-authored-by: Georgios Dagkakis <georgios.dagkakis@nexedi.com>

bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/ERP5Document_getHateoas.py
@@ -1546,10 +1546,11 @@ def calculateHateoas(is_portal=None, is_site_root=None, traversed_document=None,
           'href': '%s' % view_action['url'],
           'name': view_action['id'],
           'icon': view_action['icon'],
-          'title': Base_translateString(
-              translateWorklistActionName(view_action['title'])
-              if 'worklist_id' in view_action
-              else view_action['title']),
+          'title': translateWorklistActionName(
+            view_action['title']
+            ) if 'worklist_id' in view_action else Base_translateString(
+              view_action['title']
+            ),
         })
 
         global_action_type = ("view", "workflow", "object_new_content_action",

bt5/erp5_hal_json_style/TestTemplateItem/portal_components/test.erp5.testHalJsonStyle.py
@@ -65,6 +65,7 @@ def simulate(script_id, params_string, code_string):
       try:
         result = f(self, *args, **kw)
       finally:
+        transaction.abort()
         if script_id in self.portal.portal_skins.custom.objectIds():
           self.portal.portal_skins.custom.manage_delObjects(script_id)
         transaction.commit()
@@ -159,17 +160,11 @@ def replace_request(new_request, context):
 
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
 
-#####################################################
-# Base_getRequestHeader
-#####################################################
+
 class ERP5HALJSONStyleSkinsMixin(ERP5TypeTestCase):
   def afterSetUp(self):
     self.login()
     wipeFolder(self.portal.foo_module, commit=False)
-
-
-  def beforeTearDown(self):
-    transaction.abort()
 
   def generateNewId(self):
     return "%sö" % self.portal.portal_ids.generateNewId(
@@ -184,6 +179,13 @@ class ERP5HALJSONStyleSkinsMixin(ERP5TypeTestCase):
     )
     return foo
 
+<<<<<<< HEAD
+=======
+
+#####################################################
+# Base_getRequestHeader
+#####################################################
+>>>>>>> 4ba1c39f16 (hal_json_style: adjust transaction interactions in the test)
 class TestBase_getRequestHeader(ERP5HALJSONStyleSkinsMixin):
   @changeSkin('Hal')
   def test_getRequestHeader_REQUEST_disallowed(self):
@@ -2437,8 +2439,9 @@ class TestERP5Person_getHateoas_mode_search(ERP5HALJSONStyleSkinsMixin):
     self.tic()
 
   def beforeTearDown(self):
+    super(TestERP5Person_getHateoas_mode_search, self).beforeTearDown()
     self.portal.person_module.deleteContent(self.person.getId())
-
+    self.tic()
 
   @simulate('Base_getRequestUrl', '*args, **kwargs', 'return "http://example.org/bar"')
   @simulate('Base_getRequestHeader', '*args, **kwargs', 'return "application/hal+json"')
@@ -2942,9 +2945,8 @@ return msg"
     # worklists with the actual count of document
     fake_request = do_fake_request("GET")
 
-    default_gettext = self.portal.Localizer.erp5_ui.gettext
     def gettext(message, **kw):
-      return default_gettext(message, **kw)
+      return kw.get('default', message)
 
     with mock.patch.object(self.portal.Localizer.erp5_ui.__class__, 'gettext', side_effect=gettext) as gettext_mock:
       self.portal.web_site_module.hateoas.ERP5Document_getHateoas(