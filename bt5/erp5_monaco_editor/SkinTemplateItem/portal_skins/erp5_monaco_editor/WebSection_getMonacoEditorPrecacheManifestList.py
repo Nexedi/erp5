d7165c6748a6b19fcad827b6ed92abaf1be85bd8
Rafael Monnerat <rafael@nexedi.com>
2022-11-09 17:51:19 +0000

    erp5_monaco_editor: Add Precache Manifest script
       Probably there are missing files but it is a start.

---
+++ bt5/erp5_monaco_editor/SkinTemplateItem/portal_skins/erp5_monaco_editor/WebSection_getMonacoEditorPrecacheManifestList.py
@@ -0,0 +1,12 @@
+url_list = [
+  "monaco-editor.gadget.html",
+  "monaco-editor.gadget.js",
+  "jslint.js",
+  "monaco-editor/prettier@2.0.5/parser-babel.js",
+  "monaco-editor/prettier@2.0.5/standalone.js",
+  "monaco-editor/app.bundle.min.js",
+  "monaco-editor/editor.worker.bundle.min.js",
+  "monaco-editor/7287.bundle.min.js"
+]
+
+return url_list
