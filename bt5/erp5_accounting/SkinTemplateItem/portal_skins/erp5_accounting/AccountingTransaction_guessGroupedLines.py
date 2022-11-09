b6e11409aba2f4e6064e152f5ec0f6516965a5d0
Jérome Perrin <jerome@nexedi.com>
2022-11-09 21:47:20 +0900

    accounting: support more cases of grouping internal invoices
    The logic to guess groupable lines was considering source of
    destination side of an accounting transaction depending on
    preferences. This was not good, for two reasons:
     - with internal transactions we want to consider both the source
      and the destination
     - if user preferences are mis-configured, grouping would not work.
    
    switch to a logic where we consider both source and destination
    sides to prevent these problems.

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_guessGroupedLines.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_guessGroupedLines.py
@@ -14,8 +14,7 @@ lines_per_node = {}
 portal = context.getPortalObject()
 
 allow_grouping_with_different_quantity = portal.portal_preferences.getPreference(
-                                         'preferred_grouping_with_different_quantities', 0)
-
+  'preferred_grouping_with_different_quantities', 0)
 
 accounting_transaction_line_value_list = []
 if accounting_transaction_line_uid_list is None:
@@ -25,56 +24,71 @@ if accounting_transaction_line_uid_list is None:
                           accounting_transaction.getUid() != context.getUid():
       continue
     for line in accounting_transaction.getMovementList(
-                            portal.getPortalAccountingMovementTypeList()):
+        portal.getPortalAccountingMovementTypeList()):
       if line.getGroupingReference():
         continue
       accounting_transaction_line_value_list.append(line)
 else:
   if accounting_transaction_line_uid_list:
     accounting_transaction_line_value_list = [
-      brain.getObject() for brain in portal.portal_catalog(uid=accounting_transaction_line_uid_list)]
+      brain.getObject() for brain in portal.portal_catalog(
+        uid=accounting_transaction_line_uid_list)
+    ]
+
 
 for line in accounting_transaction_line_value_list:
-  accounting_transaction = line.getParentValue()
-  if accounting_transaction.AccountingTransaction_isSourceView():
-    section_relative_url = None
-    source_section = line.getSourceSectionValue(portal_type='Organisation')
-    if source_section is not None:
-      source_section = \
-        source_section.Organisation_getMappingRelatedOrganisation()
-      section_relative_url = source_section.getRelativeUrl()
+  # source
+  section_relative_url = None
+  source_section = line.getSourceSectionValue(portal_type='Organisation')
+  if source_section is not None:
+    source_section = \
+      source_section.Organisation_getMappingRelatedOrganisation()
+    section_relative_url = source_section.getRelativeUrl()
+  lines_per_node.setdefault(
+      (
+        line.getSource(portal_type='Account'),
+        section_relative_url,
+        line.getDestinationSection(),
+        line.AccountingTransactionLine_getGroupingExtraParameterList(
+          source=True),
+      ), []).append(
+        dict(
+          total_price=line.getSourceInventoriatedTotalAssetPrice() or 0,
+          date=line.getStartDate(),
+          path=line.getRelativeUrl()))
 
-    lines_per_node.setdefault(
-                  (line.getSource(portal_type='Account'),
-                   section_relative_url,
-                   line.getDestinationSection(),
-                   line.AccountingTransactionLine_getGroupingExtraParameterList(source=True),
-                   ), []).append(
-      dict(total_price=line.getSourceInventoriatedTotalAssetPrice() or 0,
-           date=line.getStartDate(),
-           path=line.getRelativeUrl()))
-  else:
-    section_relative_url = None
-    destination_section = line.getDestinationSectionValue(
-                                    portal_type='Organisation')
-    if destination_section is not None:
-      destination_section = \
-        destination_section.Organisation_getMappingRelatedOrganisation()
-      section_relative_url = destination_section.getRelativeUrl()
-    lines_per_node.setdefault(
-              (line.getDestination(portal_type='Account'),
-               section_relative_url,
-               line.getSourceSection(),
-               line.AccountingTransactionLine_getGroupingExtraParameterList(source=False),
-               ), []).append(
-    dict(total_price=line.getDestinationInventoriatedTotalAssetPrice() or 0,
-         date=line.getStopDate(),
-         path=line.getRelativeUrl()))
+  # destination
+  section_relative_url = None
+  destination_section = line.getDestinationSectionValue(
+    portal_type='Organisation')
+  if destination_section is not None:
+    destination_section = \
+      destination_section.Organisation_getMappingRelatedOrganisation()
+    section_relative_url = destination_section.getRelativeUrl()
+  lines_per_node.setdefault(
+    (
+      line.getDestination(portal_type='Account'),
+      section_relative_url,
+      line.getSourceSection(),
+      line.AccountingTransactionLine_getGroupingExtraParameterList(
+        source=False),
+    ), []).append(
+      dict(
+        total_price=line.getDestinationInventoriatedTotalAssetPrice() or 0,
+        date=line.getStopDate(),
+        path=line.getRelativeUrl()))
 
-changed_line_list = []
-for (node, section, mirror_section, _), line_info_list in lines_per_node.items():
+
+changed_line_set = set()
+for (
+    node,
+    section,
+    mirror_section,
+    _,
+), line_info_list in lines_per_node.items():
   if node is None:
     continue
+
   # get the currency rounding for this section, with a fallback that something that would
   # allow grouping in case precision is not defined.
   currency_precision = 5
@@ -87,10 +101,10 @@ for (node, section, mirror_section, _), line_info_list in lines_per_node.items()
     # we should include mirror node in the id_group, but this would reset
     # id generators and generate grouping references that were already used.
     id_group = ('grouping_reference', node, section, mirror_section)
-    previous_default = context.portal_ids.getLastGeneratedId(id_group=id_group, default=0)
-    grouping_reference = portal.portal_ids.generateNewId(id_generator='uid',
-                                                  id_group=id_group,
-                                                  default=previous_default + 1)
+    previous_default = context.portal_ids.getLastGeneratedId(
+      id_group=id_group, default=0)
+    grouping_reference = portal.portal_ids.generateNewId(
+      id_generator='uid', id_group=id_group, default=previous_default + 1)
 
     # convert from int to letters
     string_reference = int2letter(grouping_reference)
@@ -99,11 +113,14 @@ for (node, section, mirror_section, _), line_info_list in lines_per_node.items()
     date = max([line['date'] for line in line_info_list])
 
     for line in line_info_list:
+      if line['path'] in changed_line_set:
+        continue
       line_obj = portal.restrictedTraverse(line['path'])
       assert not line_obj.getGroupingReference(), line
       line_obj.setGroupingReference(string_reference)
       line_obj.setGroupingDate(date)
-      line_obj.reindexObject(activate_kw=dict(tag='accounting_grouping_reference'))
-      changed_line_list.append(line['path'])
+      line_obj.reindexObject(
+        activate_kw=dict(tag='accounting_grouping_reference'))
+      changed_line_set.add(line['path'])
 
-return changed_line_list
+return changed_line_set
