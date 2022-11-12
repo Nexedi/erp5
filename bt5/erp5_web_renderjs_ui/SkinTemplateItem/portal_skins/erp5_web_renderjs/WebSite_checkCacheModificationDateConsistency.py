cf82769631b5aea6cb046bd456f29a1e2604d0f1
Jérome Perrin <jerome@nexedi.com>
2022-11-12 22:34:18 +0900

    web_renderjs: optimize post upgrade constraints
    by using getDocumentValueList to lookup all documents at once instead
    of using getDocumentValue for each document

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSite_checkCacheModificationDateConsistency.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_web_renderjs/WebSite_checkCacheModificationDateConsistency.py
@@ -1,24 +1,20 @@
 from DateTime import DateTime
 appcache_reference = context.getLayoutProperty("configuration_manifest_url")
 
-getDocumentValue = context.getDocumentValue
-
 error_list = []
 if appcache_reference:
-  appcache_manifest = getDocumentValue(appcache_reference)
+  appcache_manifest = context.getDocumentValue(appcache_reference)
   if not appcache_manifest:
     return ['Error: Web Site %s references a non existant appcache %s' % (context.getRelativeUrl(),appcache_reference)]
-  url_list = context.Base_getListFileFromAppcache()
+  url_list = [url for url in context.Base_getListFileFromAppcache() if url]
   # Check that the manifest is newer than all cached resources.
   appcache_manifest_modification_date = appcache_manifest.getObject().getModificationDate()
-  for url in url_list:
-    if url:
-      referenced_document = getDocumentValue(url)
-      if referenced_document is not None and (
-          referenced_document.getModificationDate() >
-          appcache_manifest_modification_date):
+
+  if url_list:
+    for referenced_document in context.getDocumentValueList(reference=url_list):
+      if referenced_document.getModificationDate() > appcache_manifest_modification_date:
         error_list.append(
-            "Document {} is newer than cache manifest".format(url))
+          "Document {} is newer than cache manifest".format(referenced_document.getReference()))
 
   if error_list and fixit:
     text_list = appcache_manifest.getTextContent().split('\n')
