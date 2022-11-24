616d6cf6fdfd75898a4038c67f8681774cfabf21
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2022-11-24 09:49:11 +0000

    erp5_accounting, erp5_payment_mean: display currently selected Bank Account even if it is invalidated.

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getSourcePaymentItemList.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getSourcePaymentItemList.py
@@ -1,4 +1,5 @@
 return context.AccountModule_getBankAccountItemList(
         organisation=context.getSourceSection(),
+        base_category='source_payment',
         skip_invalidated_bank_accounts=
                     (context.getSimulationState() != 'delivered'))
