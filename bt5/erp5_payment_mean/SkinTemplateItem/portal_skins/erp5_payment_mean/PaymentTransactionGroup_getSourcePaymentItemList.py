570b5cacc3ebd1fe6212d1e9342d8013d133d497
Jérome Perrin <jerome@nexedi.com>
2022-12-18 18:03:44 +0900

    accounting, bank_reconciliation, payment_mean: only show validated bank accounts
    drop skip_invalidated_bank_accounts option, we are now using
    base_category instead.

--- bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getSourcePaymentItemList.py
+++ bt5/erp5_payment_mean/SkinTemplateItem/portal_skins/erp5_payment_mean/PaymentTransactionGroup_getSourcePaymentItemList.py
@@ -1,5 +1,3 @@
 return context.AccountModule_getBankAccountItemList(
         organisation=context.getSourceSection(),
-        base_category='source_payment',
-        skip_invalidated_bank_accounts=
-                    (context.getValidationState() != 'delivered'))
+        base_category='source_payment')
