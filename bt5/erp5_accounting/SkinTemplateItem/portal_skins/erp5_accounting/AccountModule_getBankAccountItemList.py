ed62428161901eb5bf977bdfbbf0a431dab08455
Jérome Perrin <jerome@nexedi.com>
2022-12-21 17:30:17 +0900

    accounting: support passing a person in AccountModule_getBankAccountItemList
    The parameter is still named `organisation` for "API stability", but
    this parameter can be a person.
    
    This was already used by the fields on accounting transaction for
    source/destination_payment and more or less working already, except
    for the case where the person had a subordination to an organisation
    member of the group, in that case instead of showing the bank accounts
    from the person this was showing the bank accounts from the
    organisation

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountModule_getBankAccountItemList.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountModule_getBankAccountItemList.py
@@ -2,11 +2,14 @@
 If `organisation` is passed, then we only show bank accounts available for that
 organisation, using the following policy:
  - if organisation is independent accounting entity (ie. have accounting periods),
-   only bank accounts from this organisation can be selected
+   only bank accounts from this organisation can be selected.
  - otherwise, bank accounts from this organisation and all organisation directly
-   members of the parent groups can be us
+   members of the parent groups can be used
  - if organisation higher in the group hierarchy contains bank accounts, bank
    accounts from parent organisations can be selected
+
+`organisation` can actually be a person or other portal types, in this case they
+are assumed to be independent accounting entities.
 
 If organisation is not passed, this script will return all bank accounts
 applicable for section_category and section_category_strict_membership.
@@ -16,21 +19,22 @@ base_category is anyway included.
 """
 portal = context.getPortalObject()
 
+
 search_kw = dict(
   portal_type=portal.getPortalPaymentNodeTypeList(),
   validation_state='validated',
 )
 
 if organisation:
-  organisation_value = portal.restrictedTraverse(organisation)
-
-  # if organisation is an independent accounting section and contains bank accounts,
+  entity_value = portal.restrictedTraverse(organisation)
+  # if entity is an independent accounting section and contains bank accounts,
   # only take into account those.
-  if organisation_value == organisation_value.Organisation_getMappingRelatedOrganisation():
-    bank_account_list = organisation_value.searchFolder(**search_kw)
+  if entity_value.getPortalType() != 'Organisation' \
+      or entity_value == entity_value.Organisation_getMappingRelatedOrganisation():
+    bank_account_list = entity_value.searchFolder(**search_kw)
   # else we lookup in organisations from parent groups.
   else:
-    group_value = organisation_value.getGroupValue(None)
+    group_value = entity_value.getGroupValue(None)
     if group_value is not None:
       uid_list = []
       while group_value.getPortalType() != 'Base Category':
