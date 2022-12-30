e1f37b78611e08ae984de8b49a3841c08398b48e
Ivan Tyagov <ivan@nexedi.com>
2022-12-30 12:00:03 +0200

    Update documentation for ebulk.

--- bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_download_js.js
+++ bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_download_js.js
@@ -6,12 +6,12 @@
     .declareAcquiredMethod("jio_get", "jio_get")
     .declareService(function () {
       var gadget = this,
-        download_deb_element = document.querySelector("#deb_link"),
-        jio_key = 'document_module/ebulk-1.1.9.deb';
+        //download_deb_element = document.querySelector("#deb_link"),
+        //jio_key = 'document_module/ebulk-1.1.9.deb';
       return gadget.jio_get(jio_key)
         .push(function (jio_document) {
-          download_deb_element.download = jio_document.short_title;
-          download_deb_element.href = window.location.origin + "/" + jio_key + "/getData";
+          //download_deb_element.download = jio_document.short_title;
+          //download_deb_element.href = window.location.origin + "/" + jio_key + "/getData";
         }, function (error) {
           console.log("Latest .deb ebulk package document not available:", error);
         });
