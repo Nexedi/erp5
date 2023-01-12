1d760fb57fd543cad4febb58773dedf721637bf6
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2023-01-12 15:10:12 +0000

    erp5_pdm: fix data inconsistency after change preference

--- bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_getVariationBaseCategoryItemList.py
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_getVariationBaseCategoryItemList.py
@@ -8,19 +8,20 @@ portal_type = context.getPortalType()
 
 from Products.ERP5Type.Cache import CachingMethod
 
+#xxx default preference value [] for fix a bug
+method_name = 'getPreferred%sVariationBaseCategoryList' % portal_type.replace(' ', '')
+method = getattr(context.portal_preferences, method_name)
+url_list = method([])
+
 def getVariationsBaseCategoryList(portal_type):
   result = []
-  #xxx default preference value [] for fix a bug
-  method_name = 'getPreferred%sVariationBaseCategoryList' % portal_type.replace(' ', '')
-  method = getattr(context.portal_preferences, method_name)
-  url_list = method([])
   for url in url_list:
     base_category = context.portal_categories[url]
     result.append((base_category.getTranslatedTitle(), base_category.getRelativeUrl()))
   return result
 
 getVariationsBaseCategoryList = CachingMethod(getVariationsBaseCategoryList,
-    id=(script.id, context.Localizer.get_selected_language()),
+    id=(script.id, context.Localizer.get_selected_language(), url_list),
     cache_factory='erp5_ui_long')
 
 return getVariationsBaseCategoryList(portal_type)
