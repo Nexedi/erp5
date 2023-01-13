0b4c3533f419311a4e9616498cda3c9e0d2d07c0
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2023-01-13 08:26:25 +0000

    erp5_web: fix data inconsistency after change preference

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_crm/WebSection_getEventResourceItemList.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_crm/WebSection_getEventResourceItemList.py
@@ -5,8 +5,9 @@ Use Auditor proxy role to let anonymous users accessing resources.
 from Products.ERP5Type.Cache import CachingMethod
 portal = context.getPortalObject()
 
+use_uid = portal.portal_categories.getCategoryUid(portal.portal_preferences.getPreferredEventUse(), base_category='use')
 sql_kw = {'portal_type': portal.getPortalResourceTypeList(),
-          'use_uid': portal.portal_categories.getCategoryUid(portal.portal_preferences.getPreferredEventUse(), base_category='use'),
+          'use_uid': use_uid,
           'validation_state': 'validated',
           'sort_on': 'title'}
 
@@ -14,7 +15,7 @@ def getResourceItemList():
   return [('', '')] + [(result.getTitle(), result.getRelativeUrl()) for result in portal.portal_catalog(**sql_kw)]
 
 getResourceItemList = CachingMethod(getResourceItemList,
-      id=(script.id, context.Localizer.get_selected_language()),
+      id=(script.id, context.Localizer.get_selected_language(), use_uid),
       cache_factory='erp5_ui_long')
 
 return getResourceItemList()
