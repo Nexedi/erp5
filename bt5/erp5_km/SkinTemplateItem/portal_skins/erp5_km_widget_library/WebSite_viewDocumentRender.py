c6b927af0c73f93f86b5123387dacac696b70fa1
Jérome Perrin <jerome@nexedi.com>
2023-01-19 13:59:22 +0100

    km: use context free document lookup in "document renderer"
    This gadget looks up a document by reference, by using
    getDocumentValueList on the context of the web section.
    getDocumentValueList also applies the criterions from the context, but
    because web page references are already unique, adding more criterions
    can just cause the document to be not found and the gadget to display
    the "No document" error.

--- bt5/erp5_km/SkinTemplateItem/portal_skins/erp5_km_widget_library/WebSite_viewDocumentRender.py
+++ bt5/erp5_km/SkinTemplateItem/portal_skins/erp5_km_widget_library/WebSite_viewDocumentRender.py
@@ -8,7 +8,7 @@ if box_relative_url:
 preferences = box.KnowledgeBox_getDefaultPreferencesDict()
 reference=preferences.get('preferred_document_reference', None)
 if reference is not None:
-  web_page_list = context.getDocumentValueList(reference=reference, all_languages=True,
+  web_page_list = portal.portal_catalog.getDocumentValueList(reference=reference, all_languages=True,
                                          portal_type='Web Page')
   if len(web_page_list):
     return '<div class="web-page-renderer">%s</div>' %web_page_list[0].getObject().asStrippedHTML()
