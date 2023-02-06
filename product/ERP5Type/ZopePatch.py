81edae9f7296409ccadc1a0b37f63d54c413dc66
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2023-02-06 21:26:33 +0000

    fixup! Zope2: revive patches for Zope2.

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -102,6 +102,8 @@ from Products.ERP5Type.patches import OFSFile
 from Products.ERP5Type.patches import ZSQLMethod
 from Products.ERP5Type.patches import MimetypesRegistry
 from Products.ERP5Type.patches import users
+if not IS_ZOPE4:
+  from Products.ERP5Type.patches import Publish
 from Products.ERP5Type.patches import WSGITask
 if six.PY2:
   # XXX-zope4py3: urllib2 removed (see future/backports/urllib/request.py)
