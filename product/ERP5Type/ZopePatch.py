01e43cb5aed862b27333810674f1d52c39f66422
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2023-02-08 13:48:57 +0000

    Zope2: apply SharedDCScriptsBindings and ZPublisherBeforeTraverse in Zope2 only.

--- product/ERP5Type/ZopePatch.py
+++ product/ERP5Type/ZopePatch.py
@@ -95,12 +95,13 @@ from Products.ERP5Type.patches import OFSFile
 from Products.ERP5Type.patches import ZSQLMethod
 from Products.ERP5Type.patches import MimetypesRegistry
 from Products.ERP5Type.patches import users
+if IS_ZOPE2: # BBB Zope2
+  from Products.ERP5Type.patches import SharedDCScriptsBindings
+  from Products.ERP5Type.patches import ZPublisherBeforeTraverse
 from Products.ERP5Type.patches import WSGITask
 if six.PY2:
   # XXX-zope4py3: urllib2 removed (see future/backports/urllib/request.py)
   from Products.ERP5Type.patches import urllib_opener
-from Products.ERP5Type.patches import SharedDCScriptsBindings
-from Products.ERP5Type.patches import ZPublisherBeforeTraverse
 
 # These symbols are required for backward compatibility
 from Products.ERP5Type.patches.PropertyManager import ERP5PropertyManager
