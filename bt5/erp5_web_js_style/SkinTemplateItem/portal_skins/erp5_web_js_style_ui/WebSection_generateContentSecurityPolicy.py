b0013fa3e9c3311db30bc0c7582cfba139cfe17d
Romain Courteaud <romain@nexedi.com>
2023-02-16 09:44:39 +0000

    erp5_web_js_style: configure the accepted iframe list on the web site

--- bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSection_generateContentSecurityPolicy.py
+++ bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSection_generateContentSecurityPolicy.py
@@ -1,7 +1,10 @@
 content_security_policy = "default-src 'self'; img-src 'self' data:"
 
 if no_style_gadget_url:
-  content_security_policy += "; frame-src 'self' https://www.youtube-nocookie.com/embed/"
+  web_section = context
+  iframe_url_list = [x.strip() for x in web_section.getLayoutProperty('configuration_iframe_url_text', default='').split('\n') if x.strip()]
+  if iframe_url_list:
+    content_security_policy = "%s; frame-src 'self' %s" % (content_security_policy, ' '.join(iframe_url_list))
 else:
   # If not rendering gadget, fully disable javascript
   # as nothing is expected
