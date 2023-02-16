0c3b04358f8772965b18d150fb59346efb72bcdb
Romain Courteaud <romain@nexedi.com>
2023-02-16 09:10:22 +0000

    erp5_web_js_style: factorise content security policy value

---
+++ bt5/erp5_web_js_style/SkinTemplateItem/portal_skins/erp5_web_js_style_ui/WebSection_generateContentSecurityPolicy.py
@@ -0,0 +1,10 @@
+content_security_policy = "default-src 'self'"
+
+if no_style_gadget_url:
+  content_security_policy += "; frame-src 'self' https://www.youtube-nocookie.com/embed/"
+else:
+  # If not rendering gadget, fully disable javascript
+  # as nothing is expected
+  content_security_policy += "; script-src 'none'"
+
+return content_security_policy
