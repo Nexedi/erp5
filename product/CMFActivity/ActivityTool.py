Nexedi/erp5

commit fcd26be026a77f48e18a3c4a61593fa910204170

CMFActivity,syncml: stop using a dedicated log file
$INSTANCE_HOME/log only exists in runUnitTest instances, this code had
no effect in slapos instances.

It is a future problem because it depends on the Signals module (part
of ZServer package) and we are removing this dependency in the python 3
port.

For the SynchronizationTool, it was even more problematic because it
can cause the component not to loed, with an error like:

    ImportError: erp5.component.tool.SynchronizationTool: cannot load Component SynchronizationTool (signal only works in main thread)

product/CMFActivity/ActivityTool.py
@@ -98,24 +98,6 @@ activity_logger = logging.getLogger('CMFActivity')
 activity_tracking_logger = logging.getLogger('Tracking')
 activity_timing_logger = logging.getLogger('CMFActivity.TimingLog')
 
-# Direct logging to "[instancehome]/log/CMFActivity.log", if this directory exists.
-# Otherwise, it will end up in root logging facility (ie, event.log).
-from App.config import getConfiguration
-import os
-instancehome = getConfiguration().instancehome
-if instancehome is not None:
-  log_directory = os.path.join(instancehome, 'log')
-  if os.path.isdir(log_directory):
-    from Signals import Signals
-    from ZConfig.components.logger.loghandler import FileHandler
-    log_file_handler = FileHandler(os.path.join(log_directory, 'CMFActivity.log'))
-    # Default zope log format string borrowed from
-    # ZConfig/components/logger/factory.xml, but without the extra "------"
-    # line separating entries.
-    log_file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s", "%Y-%m-%dT%H:%M:%S"))
-    Signals.registerZopeSignals([log_file_handler])
-    activity_logger.addHandler(log_file_handler)
-    activity_logger.propagate = 0
 
 def activity_timing_method(method, args, kw):
   begin = time()

bt5/erp5_syncml/ToolComponentTemplateItem/portal_components/tool.erp5.SynchronizationTool.py
@@ -25,8 +25,7 @@
 #
 ##############################################################################
 
-from os import path
-from logging import getLogger, Formatter
+from logging import getLogger
 
 from AccessControl import ClassSecurityInfo
 
@@ -43,30 +42,7 @@ from Products.ERP5.ERP5Site import getSite
 synchronous_engine = SyncMLSynchronousEngine()
 asynchronous_engine = SyncMLAsynchronousEngine()
 
-# Logging channel definitions
-# Main logging channel
 syncml_logger = getLogger('ERP5SyncML')
-# Direct logging to "[instancehome]/log/ERP5SyncML.log", if this
-# directory exists. Otherwise, it will end up in root logging
-# facility (ie, event.log).
-from App.config import getConfiguration
-instancehome = getConfiguration().instancehome
-if instancehome is not None:
-  log_directory = path.join(instancehome, 'log')
-  if path.isdir(log_directory):
-    from Signals import Signals
-    from ZConfig.components.logger.loghandler import FileHandler
-    log_file_handler = FileHandler(path.join(log_directory,
-                                                'ERP5SyncML.log'))
-    # Default zope log format string borrowed from
-    # ZConfig/components/logger/factory.xml, but without the extra "------"
-    # line separating entries.
-    log_file_handler.setFormatter(Formatter(
-      "%(asctime)s %(levelname)s %(name)s %(message)s",
-      "%Y-%m-%dT%H:%M:%S"))
-    Signals.registerZopeSignals([log_file_handler])
-    syncml_logger.addHandler(log_file_handler)
-    syncml_logger.propagate = 0
 
 
 def checkAlertCommand(syncml_request):

commit f39ad5dfbfc66fb02e31bd38fc64e439cb5463bb

CMFActivity: fix test after removing CMFActivity.log
fcd26be026 (CMFActivity,syncml: stop using a dedicated log file,
2023-02-17) introduced a regression in test and left an unused variable.

product/CMFActivity/ActivityTool.py
@@ -92,9 +92,6 @@ ROLE_PROCESSING = 1
 
 # Logging channel definitions
 import logging
-# Main logging channel
-activity_logger = logging.getLogger('CMFActivity')
-# Some logging subchannels
 activity_tracking_logger = logging.getLogger('Tracking')
 activity_timing_logger = logging.getLogger('CMFActivity.TimingLog')
 

product/CMFActivity/tests/testCMFActivity.py
@@ -2472,7 +2472,7 @@ class TestCMFActivity(ERP5TypeTestCase, LogInterceptor):
     activity_tool = self.portal.portal_activities
 
     kw = {}
-    self._catch_log_errors(subsystem='CMFActivity')
+    self._catch_log_errors()
     try:
       activity_tool.activity_failure_mail_notification = True
       for kw['activity'] in ActivityTool.activity_dict:
@@ -2496,11 +2496,12 @@ class TestCMFActivity(ERP5TypeTestCase, LogInterceptor):
           self.commit()
           self.assertEqual(1, activity_tool.countMessage())
           activity_tool.tic()
-          self.assertIn('no object found', self.logged.pop().getMessage())
+          cmf_activty_log, = [log for log in self.logged if 'CMFActivity' in log.name]
+          self.logged = []
+          self.assertIn('no object found', cmf_activty_log.getMessage())
     finally:
       self._ignore_log_errors()
-    self.assertFalse(self.logged)
-    self.assertFalse(message_list, message_list)
+    self.assertFalse(message_list)
 
   def test_activateByPath(self):
     organisation = self.getOrganisation()