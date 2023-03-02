1e6b125158a8f3b91be55509a8ac57370a511de5
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2023-03-02 15:10:02 +0900

    zope4: ZServer removal: Remove Publisher monkey patches.

--- product/Localizer/__init__.py
+++ product/Localizer/__init__.py
@@ -25,7 +25,7 @@ from DocumentTemplate.DT_String import String
 # Import from Localizer
 import six
 if six.PY2:
-  from .patches import get_request
+  from . import patches as _
 from . import Localizer, MessageCatalog
 from .LocalFiles import LocalDTMLFile
 
