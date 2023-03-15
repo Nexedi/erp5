fc185adabdfa2d70c77fe0fc23399a657401d3c3
Jérome Perrin <jerome@nexedi.com>
2023-03-15 11:45:14 +0900

    py2/py3: more six.moves.UserDict + SQLJoblib ( broken commit )

--- bt5/erp5_accounting/DocumentTemplateItem/portal_components/document.erp5.BalanceTransaction.py
+++ bt5/erp5_accounting/DocumentTemplateItem/portal_components/document.erp5.BalanceTransaction.py
@@ -26,7 +26,7 @@
 #
 ##############################################################################
 
-from UserDict import UserDict
+from six.moves import UserDict
 
 from AccessControl import ClassSecurityInfo
 from Products.ERP5Type import Permissions, PropertySheet
