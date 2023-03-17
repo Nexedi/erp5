12f0ecf0c123392cd26aaada5cd9d072166660c9
Jérome Perrin <jerome@nexedi.com>
2023-03-17 15:03:30 +0900

    CMFActivity py3

--- product/CMFActivity/Activity/SQLDict.py
+++ product/CMFActivity/Activity/SQLDict.py
@@ -142,10 +142,10 @@ class SQLDict(SQLBase):
           if reserve_uid_list:
             self.assignMessageList(db, processing_node, reserve_uid_list)
           else:
-            db.query("COMMIT") # XXX: useful ?
+            db.query(b"COMMIT") # XXX: useful ?
         except:
           self._log(WARNING, 'Failed to reserve duplicates')
-          db.query("ROLLBACK")
+          db.query(b"ROLLBACK")
           raise
         if uid_list:
           self._log(TRACE, 'Reserved duplicate messages: %r' % uid_list)
