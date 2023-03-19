e5a81bb64f6ba0c1abbb01e748bacd9fdaccc28d
Jérome Perrin <jerome@nexedi.com>
2023-03-19 13:52:24 +0900

    *: use base64 encodebytes for python3.9

--- product/ERP5Type/CachePlugins/DistributedRamCache.py
+++ product/ERP5Type/CachePlugins/DistributedRamCache.py
@@ -37,7 +37,7 @@ from .BaseCache import BaseCache
 from .BaseCache import CacheEntry
 from Products.ERP5Type import interfaces
 import zope.interface
-from base64 import encodestring
+from base64 import encodebytes
 
 try:
   from Products.ERP5Type.Tool.MemcachedTool import MemcachedDict, SharedDict
