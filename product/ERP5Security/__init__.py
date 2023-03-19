f221d5d8a703ffa40d76786dc9e271e45b0927bf
Jérome Perrin <jerome@nexedi.com>
2023-03-19 13:55:41 +0900

    *: fix wrong base64 usage

--- product/ERP5Security/__init__.py
+++ product/ERP5Security/__init__.py
@@ -62,31 +62,30 @@ if IS_ZOPE2: # BBB
   def _setUserNameForAccessLog(username, REQUEST):
     """Make the current user look as `username` in Zope's Z2.log
 
-    Taken from Products.CMFCore.CookieCrumbler._setAuthHeader
-    """
-    # Set the authorization header in the medusa http request
-    # so that the username can be logged to the Z2.log
-    # Put the full-arm latex glove on now...
+  Taken from Products.CMFCore.CookieCrumbler._setAuthHeader
+  """
+  # Set the authorization header in the medusa http request
+  # so that the username can be logged to the Z2.log
+  # Put the full-arm latex glove on now...
+  try:
+    # Is this WSGI ?
+    REQUEST._orig_env['wsgi.input']
+  except KeyError:
+    # Not WSGI, maybe Medusa
     try:
-      # Is this WSGI ?
-      REQUEST._orig_env['wsgi.input']
-    except KeyError:
-      # Not WSGI, maybe Medusa
-      try:
-        medusa_headers = REQUEST.RESPONSE.stdout._request._header_cache
-      except AttributeError:
-        pass
-      else:
-        medusa_headers['authorization'] = 'Basic %s' % encodebytes('%s:' % username).rstrip()
+      medusa_headers = REQUEST.RESPONSE.stdout._request._header_cache
+    except AttributeError:
+      pass
     else:
-      REQUEST._orig_env['REMOTE_USER'] = username
+      medusa_headers['authorization'] = 'Basic %s' % encodebytes(('%s:' % username).encode()).decode().rstrip()
+  else:
+    REQUEST._orig_env['REMOTE_USER'] = username
 else: # zope4
   def _setUserNameForAccessLog(username, REQUEST):
     """
     Nothing to do for this on Zope4.
     """
     pass
-
 
 def initialize(context):
   from . import (
