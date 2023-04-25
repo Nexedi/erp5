72b3bf3664f81310c50bfa4ce0e3dda8f038cf31
Roque Porchetto <roque.porchetto@nexedi.com>
2023-04-25 14:33:25 +0000

    erp5_officejs_drone_capture_flag: app web site and pages, skin folder and assets

---
+++ bt5/erp5_officejs_drone_capture_flag/SkinTemplateItem/portal_skins/erp5_officejs_drone_capture_flag/WebSection_getDroneCaptureFlagPrecacheManifestList.py
@@ -0,0 +1,27 @@
+url_list = [
+  "gadget_officejs_drone_capture_flag.json",
+  "drone_flag.png",
+  "gadget_erp5_page_drone_capture_flag_logic.js",
+  "gadget_erp5_page_drone_capture_flag_script_page.html",
+  "gadget_erp5_page_drone_capture_flag_script_page.js",
+  "gadget_erp5_panel_drone_capture_flag.html",
+  "gadget_erp5_panel_drone_capture_flag.js",
+  "gadget_erp5_page_drone_capture_flag_fixedwingdrone.js",
+  "gadget_erp5_page_drone_capture_flag_enemydrone.js",
+  "assets/map/terrain.jpg",
+  "assets/map/map.babylon",
+  "assets/drone/drone.babylon",
+  "assets/drone/drone_bleu.jpg",
+  "assets/drone/drone_bleu_old.jpg",
+  "assets/drone/drone_rouge.jpg",
+  "assets/drone/drone_rouge_old.jpg",
+  "assets/skybox/sky_nx.jpg",
+  "assets/skybox/sky_ny.jpg",
+  "assets/skybox/sky_nz.jpg",
+  "assets/skybox/sky_px.jpg",
+  "assets/skybox/sky_py.jpg",
+  "assets/skybox/sky_pz.jpg",
+  "libraries/seedrandom.min.js"
+]
+
+return url_list
