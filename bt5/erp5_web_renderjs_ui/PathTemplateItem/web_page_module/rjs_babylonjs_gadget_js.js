97332910fd5f5cbcfa9b481d5b3010982f8431e3
Roque Porchetto <roque.porchetto@nexedi.com>
2023-04-26 15:58:11 +0000

    erp5_web_renderjs_ui: update babylonjs libraries (version 6) and more - babylon gadget msg htm - babylonjs gadget fullscreen - bug fix in offscreen canvas

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_babylonjs_gadget_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_babylonjs_gadget_js.js
@@ -14,7 +14,7 @@
     'offsetY', 'pageY', 'relatedTarget', 'returnValue', 'screenX', 'screenY',
     'shiftKey', 'timeStamp', 'type', 'which', 'x', 'wheelDelta', 'wheelDeltaX',
     'wheelDeltaY', 'y', 'deltaX', 'deltaY', 'deltaZ', 'deltaMode'
-    ]), game_result, canvas, offscreen;
+    ]), game_result, canvas, offscreen, game_manager, fullscreen = false;
 
   //////////////////////////////////////////
   // Webworker
@@ -117,6 +117,9 @@
     result: function resultGameManager() {
       return game_result;
     },
+    fullscreen: function fullScreenGameManager() {
+      fullscreen = !fullscreen;
+    },
     play: function startGameManager(options) {
       if (this.hasOwnProperty('loop_promise')) {
         throw new Error('Can not start the game if already started');
@@ -139,7 +142,8 @@
               context.loop_promise
                 .push(function () {
                   worker.postMessage({
-                    type: 'update'
+                    type: 'update',
+                    fullscreen: fullscreen
                   });
                   update_defer = RSVP.defer();
                   return RSVP.all([
@@ -208,6 +212,8 @@
                   var loading =
                       context._gadget.element.querySelector('#loading');
                   if (loading) { loading.innerHTML = ""; }
+                  context._gadget.element.querySelector('#fullscreen')
+                    .style.visibility = 'visible';
                 }
                 context.unpause();
                 return step();
@@ -263,12 +269,22 @@
     .declareMethod('render', function render(options) {
       var gadget = this,
         loading = domsugar('span', ["Loading..."]),
-        container = domsugar('div');
+        container = domsugar('div'),
+        messages = domsugar('div'),
+        button = domsugar('div');
+      button.className = 'ui-icon-expand ui-btn-icon-notext';
+      button.addEventListener('click', function (event) {
+        game_manager.fullscreen();
+      });
+      button.style.visibility = 'hidden';
+      button.id = "fullscreen";
       canvas = domsugar('canvas');
       loading.id = "loading";
       container.className = 'container';
+      messages.id = 'messages';
+      messages.className = 'messages';
       container.appendChild(canvas);
-      domsugar(gadget.element, [loading, container]);
+      domsugar(gadget.element, [loading, button, container, messages]);
       canvas.width = options.width;
       canvas.height = options.height;
       // https://doc.babylonjs.com/divingDeeper/scene/offscreenCanvas
@@ -291,8 +307,8 @@
       options.width = canvas.width;
       options.height = canvas.height;
       options.logic_url_list = options.logic_file_list;
-      var gadget = this,
-        game_manager = new DroneGameManager(gadget);
+      var gadget = this;
+      game_manager = new DroneGameManager(gadget);
       return game_manager.play(options)
       .push(function () {
         return game_manager.result();
