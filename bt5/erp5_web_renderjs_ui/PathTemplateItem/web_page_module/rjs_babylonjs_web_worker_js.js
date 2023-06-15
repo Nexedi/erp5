081e58ba80700367664b21961e2fced92d2ba61e
Roque Porchetto <roque.porchetto@nexedi.com>
2023-06-15 15:55:09 +0000

    erp5_web_renderjs_ui: fix bug that prevents canvas/engine resize to work

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_babylonjs_web_worker_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_babylonjs_web_worker_js.js
@@ -111,8 +111,6 @@ function handleEvent(event) {
 function prepareCanvas(data) {
   var canvas = data.canvas, style;
   self.canvas = canvas;
-  canvas.clientWidth = data.width;
-  canvas.clientHeight = data.height;
   canvas.width = data.width;
   canvas.height = data.height;
   rect.right = rect.width = data.width;
