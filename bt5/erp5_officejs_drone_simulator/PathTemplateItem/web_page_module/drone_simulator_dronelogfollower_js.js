Nexedi/erp5

commit 41f48cbe466a20f6575030ae1f61b300bc5f6829

erp5_officejs_drone_simulator: comply with Jslint

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_dronelogfollower_js.js
@@ -43,9 +43,8 @@ var DroneLogAPI = /** @class */ (function () {
       return log_entry_list;
     }
     var log = this._drone_info.log_content, entry_1, entry_2, interval,
-      map_dict = this._mapManager.getMapInfo(),
       min_height = 15, converted_log_point_list = [],
-      i, splitted_log_entry, x, y, position, lat, lon, height, timestamp,
+      i, splitted_log_entry, position, lat, lon, height, timestamp,
       time_offset = 1, log_entry_list = getLogEntries(log);
     //XXX: Patch to determine log time format (if this is standarized, drop it)
     if (log_entry_list[0] && log_entry_list[1]) {

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_dronelogfollower_js.xml
@@ -246,11 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-<<<<<<< HEAD
-                      <value> <string>1006.43901.65103.9898</string> </value>
-=======
-                      <value> <string>1009.27112.31386.24968</string> </value>
->>>>>>> 928ec4f23a (erp5_officejs_drone_simulator: update API)
+                      <value> <string>1009.38419.20212.62293</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -270,11 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-<<<<<<< HEAD
-                              <float>1677599947.8</float>
-=======
-                              <float>1688043479.22</float>
->>>>>>> 928ec4f23a (erp5_officejs_drone_simulator: update API)
+                              <float>1688635412.87</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_fixedwingdrone_js.js
@@ -310,7 +310,7 @@ var FixedWingDroneAPI = /** @class */ (function () {
       } else {
         this._loiter_mode = false;
       }
-    }
+    };
 
   FixedWingDroneAPI.prototype.sendMsg = function (msg, to) {
     var _this = this,

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_fixedwingdrone_js.xml
@@ -240,7 +240,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1009.36979.17507.53811</string> </value>
+                      <value> <string>1009.38408.26347.58350</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -260,7 +260,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1688549051.54</float>
+                              <float>1688634921.8</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.js
@@ -581,13 +581,13 @@ var GameManager = /** @class */ (function () {
           (0 < _this.waiting_update_count)) {
         _this.ongoing_update_promise = _this._update(TIME_DELTA, fullscreen)
           .push(function () {
-          _this.waiting_update_count -= 1;
-          _this.ongoing_update_promise = null;
-          triggerUpdateIfPossible();
-        }).push(undefined, function (error) {
-          console.log("ERROR on Game Manager update:", error);
-          _this.finish_deferred.reject.bind(_this.finish_deferred);
-        });
+            _this.waiting_update_count -= 1;
+            _this.ongoing_update_promise = null;
+            triggerUpdateIfPossible();
+          }).push(undefined, function (error) {
+            console.log("ERROR on Game Manager update:", error);
+            _this.finish_deferred.reject.bind(_this.finish_deferred);
+          });
       }
     }
     try {

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.xml
@@ -260,7 +260,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1688549052.81</float>
+                              <float>1688635184.98</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

commit f7f9553872d23b2959fc20f7088c0ca262ca8e60

erp5_officejs_drone_simulator: revert bug introduced by 41f48cbe466a20f6575030ae1f61b300bc5f6829

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_dronelogfollower_js.js
@@ -43,8 +43,9 @@ var DroneLogAPI = /** @class */ (function () {
       return log_entry_list;
     }
     var log = this._drone_info.log_content, entry_1, entry_2, interval,
+      map_dict = this._mapManager.getMapInfo(),
       min_height = 15, converted_log_point_list = [],
-      i, splitted_log_entry, position, lat, lon, height, timestamp,
+      i, splitted_log_entry, position, x, y, lat, lon, height, timestamp,
       time_offset = 1, log_entry_list = getLogEntries(log);
     //XXX: Patch to determine log time format (if this is standarized, drop it)
     if (log_entry_list[0] && log_entry_list[1]) {

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_dronelogfollower_js.xml
@@ -232,7 +232,7 @@
                   </item>
                   <item>
                       <key> <string>actor</string> </key>
-                      <value> <string>zope</string> </value>
+                      <value> <unicode>zope</unicode> </value>
                   </item>
                   <item>
                       <key> <string>comment</string> </key>
@@ -246,7 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1009.38419.20212.62293</string> </value>
+                      <value> <string>1010.10611.27339.33706</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -266,7 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1688635412.87</float>
+                              <float>1690899110.17</float>
                               <string>UTC</string>
                             </tuple>
                           </state>