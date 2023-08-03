ac66cf204929e7361d36c744c6d5d255147d6a89
Rafael Monnerat <rafael@nexedi.com>
2023-08-03 04:24:21 +0000

    erp5_certificate_authority: get/revoke action are moved to Certficate Login

--- bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/CertificateLogin_getCertificate.py
+++ bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/CertificateLogin_getCertificate.py
@@ -2,4 +2,6 @@ certificate = context.getCertificate()
 request = context.REQUEST
 request.set('your_certificate', certificate['certificate'])
 request.set('your_key', certificate['key'])
-return context.Person_viewCertificateDialog()
+return context.CertificateLogin_viewCertificateDialog(
+  keep_items = {'portal_status_message' : 'Certificate generated.'}
+)
