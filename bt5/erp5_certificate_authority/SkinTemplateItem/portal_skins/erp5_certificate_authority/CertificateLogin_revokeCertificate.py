6bcdeceaa5a2da99e01fb2886c0c0b5f114e5a17
Rafael Monnerat <rafael@nexedi.com>
2023-08-03 04:37:21 +0000

    erp5_certificate_authority: Ensure user can call the action before get/revoke the certificates

--- bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/CertificateLogin_revokeCertificate.py
+++ bt5/erp5_certificate_authority/SkinTemplateItem/portal_skins/erp5_certificate_authority/CertificateLogin_revokeCertificate.py
@@ -1,2 +1,7 @@
+parent = context.getParentValue()
+
+if parent.getPortalType() == "Person":
+  parent.checkCertificateRequest()
+
 context.revokeCertificate()
 return context.Base_redirect(form_id, keep_items = {'portal_status_message' : 'Certificate revoked.'},  **kw)
