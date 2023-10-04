fc1998364b04576c47fee7b5b8154c54f1a3f916
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2023-10-04 08:43:19 +0200

    fixup! accounting: fix workflow script after ERP5 workflow migration

--- bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/internal_invoice_transaction_simulation_workflow/script_validateTransactionLines.py
+++ bt5/erp5_accounting/WorkflowTemplateItem/portal_workflow/internal_invoice_transaction_simulation_workflow/script_validateTransactionLines.py
@@ -3,7 +3,7 @@ from Products.ERP5Type.Message import translateString
 
 internal_invoice = state_change['object']
 old_state = state_change['old_state']
-if old_state.getId() == 'draft':
+if old_state.getReference() == 'draft':
   if internal_invoice.InternalInvoiceTransaction_getAuthenticatedUserSection() == internal_invoice.getDestinationSection():
     raise ValidationFailed(translateString("Your entity should not be destination."))
 
