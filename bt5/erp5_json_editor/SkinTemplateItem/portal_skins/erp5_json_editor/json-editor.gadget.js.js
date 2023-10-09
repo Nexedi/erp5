29f2aa56bf8c5f88986c65661c579cd46b1ff110
Rafael Monnerat <rafael@nexedi.com>
2023-10-09 20:06:45 +0000

    erp5_json_editor: Introduce JSON Editor
      This gadget aims to edit json based on json schema.

---
+++ bt5/erp5_json_editor/SkinTemplateItem/portal_skins/erp5_json_editor/json-editor.gadget.js.js
@@ -0,0 +1,210 @@
+/*jslint nomen: true, indent: 2 */
+/*global window, rJS, RSVP, JSONEditor, domsugar */
+(function (window, rJS, RSVP, JSONEditor, domsugar) {
+  'use strict';
+
+  rJS(window)
+    .declareMethod('render', function (options) {
+      return this.changeState({
+        schema_url: options.schema_url,
+        json_field: options.json_field,
+        default_dict: options.default_dict,
+        editable: options.editable,
+        // Force refresh in any case
+        render_timestamp: new Date().getTime()
+      });
+    })
+
+    .onStateChange(function () {
+      var gadget = this,
+       jsonEditorContainer = gadget.element.querySelector('.json-editor-container');
+
+      /* Overwrite some default implementation to follow up Nexedi features */
+      JSONEditor.AbstractEditor.prototype.getDefault = function () {
+        if (typeof this.schema.enum !== 'undefined') {
+          this.schema.enum.unshift("");
+          return "";
+        }
+        return undefined;
+      }
+
+      JSONEditor.defaults.editors.object.prototype.getValue = function () {
+        if (!this.dependenciesFulfilled) {
+          return undefined
+        }
+        /* original code uses super.getValue() but we cannot use super here */
+        const result = this.value
+
+        /* Also check if constructor is an array */
+        const isEmpty = obj => typeof obj === 'undefined' || obj === '' ||
+        (
+          obj === Object(obj) &&
+          Object.keys(obj).length === 0 &&
+          (obj.constructor === Object || obj.constructor === Array)
+        )
+        if (result && (this.jsoneditor.options.remove_empty_properties || this.options.remove_empty_properties)) {
+          Object.keys(result).forEach(key => {
+            if (isEmpty(result[key])) {
+              delete result[key]
+            }
+          })
+        }
+        return result
+      }
+
+      JSONEditor.AbstractEditor.prototype.preBuild = function () {
+        if (this.jsoneditor.options.readonly) {
+          this.schema.readOnly = this.jsoneditor.options.readonly;
+        }
+      }
+
+      if (JSONEditor.defaults.editors.select.prototype.original_preBuild === undefined) {
+        JSONEditor.defaults.editors.select.prototype.original_preBuild = JSONEditor.defaults.editors.select.prototype.preBuild
+      }
+      JSONEditor.defaults.editors.select.prototype.preBuild = function () {
+        if (this.jsoneditor.options.readonly) {
+          this.schema.readOnly = this.jsoneditor.options.readonly;
+        }
+        if (typeof this.schema.enum !== 'undefined') {
+          this.schema.enum.unshift("");
+        }
+        this.original_preBuild()
+        if (this.schema.type === 'boolean') {
+          /* the original code on preBuild include an empty first value if the value
+           is not required, but we always want the empty value */
+          if (this.isRequired()) {
+            this.enum_display.unshift(' ')
+            this.enum_options.unshift('undefined')
+            this.enum_values.unshift(undefined)
+          }
+        }
+      }
+
+      JSONEditor.defaults.editors.select.prototype.getValue = function () {
+        if (this.value === "") {
+          return undefined
+        }
+        if (this.value === undefined) {
+          return undefined
+        }
+        if (!this.dependenciesFulfilled) {
+          return undefined
+        }
+        return this.typecast(this.value)
+      }
+      JSONEditor.defaults.editors.select.prototype.typecast = function(value) {
+        if (this.schema.type === 'boolean') return value === 'undefined' || value === undefined ? undefined : !!value
+        else if (this.schema.type === 'number' && value === "") return undefined
+        else if (this.schema.type === 'integer' && value === "") return undefined
+        else if (this.schema.type === 'number') return 1 * value || 0
+        else if (this.schema.type === 'integer') return Math.floor(value * 1 || 0)
+        else if (this.schema.enum && value === undefined) return undefined
+        return `${value}`
+      }
+
+      /* The original code would remove the field if value is undefined */
+      JSONEditor.defaults.editors.object.prototype.setValue = function(value, initial) {
+        value = value || {}
+    
+        if (typeof value !== 'object' || Array.isArray(value)) value = {}                                                               
+    
+        /* First, set the values for all of the defined properties */
+        Object.entries(this.cached_editors).forEach(([i, editor]) => {
+          /* Value explicitly set */
+          if (typeof value[i] !== 'undefined') {
+            this.addObjectProperty(i)
+            editor.setValue(value[i], initial)
+            editor.activate()
+            /* Otherwise if it is read only remove the field */
+          } else if (editor.schema.readOnly) {
+            this.removeObjectProperty(i);
+            /* Otherwise, set the value to the default */
+          } else {
+            editor.setValue(editor.getDefault(), initial)
+          }
+        })
+    
+        Object.entries(value).forEach(([i, val]) => {
+          if (!this.cached_editors[i]) {
+            this.addObjectProperty(i)
+            if (this.editors[i]) this.editors[i].setValue(val, initial, !!this.editors[i].template)                                     
+          }
+        })
+    
+        this.refreshValue()
+        this.layoutEditors()
+        this.onChange()
+      }
+
+      JSONEditor.defaults.editors.string.prototype.setValueToInputField = function (value) {
+        this.input.value = value === undefined ? '' : value
+        // ERP5: Once you set the value to the input, you also 
+        // updates the field value, otherwise the getValue behave badly
+        this.value = this.input.value 
+      }
+
+      /* Backward compatibility with the usage of textarea property 
+        if converts into json-editor proper property */
+      JSONEditor.defaults.editors.string.prototype.preBuild = function () {
+        if ((this.schema.textarea === true) || (this.schema.textarea === 1)) {
+          this.schema.format = 'textarea';
+        }
+        if (this.jsoneditor.options.readonly) {
+          this.schema.readOnly = this.jsoneditor.options.readonly;
+        }        
+      }
+
+      /* End of patches related to ERP5 features */
+
+      if (Object.keys(gadget.state.json_field).length === 0) {
+        return domsugar(jsonEditorContainer);
+      }
+
+      return new RSVP.Queue()
+        .push(function () {
+          return new JSONEditor(domsugar(jsonEditorContainer), {
+            schema: gadget.state.json_field,
+            ajax: true,
+            theme: 'bootstrap5',
+            show_errors: 'always',
+            iconlib: 'fontawesome5',
+            object_layout: 'normal',
+            disable_collapse: false,
+            disable_edit_json: true,
+            disable_properties: false,
+            keep_only_existing_values: false,
+            use_default_values: false,		// important
+            disable_array_reorder: true,
+            disable_array_delete_all_rows: true,
+            disable_array_delete_last_row: true,
+            no_additional_properties: false,     // important
+            remove_empty_properties: true,
+            keep_oneof_values: false,		// important
+            startval: gadget.state.default_dict,
+            readonly: gadget.state.editable ? false: true
+          });
+        })
+        .push(function (editor) {
+          gadget.editor = editor;
+          //editor.setValue(gadget.state.default_dict);
+          return editor
+        })
+    })
+    .declareMethod(
+      'getContent',
+      function () {
+        if (this.editor === undefined) {
+          return {};
+        } 
+        return this.editor.getValue();
+      },
+      { mutex: 'changestate' }
+    )
+
+    .declareMethod('checkValidity', function () {
+      if (this.state.errors !== undefined) {
+        return this.state.errors.length === 0;
+      }
+      return true;
+    });
+})(window, rJS, RSVP, JSONEditor, domsugar);
