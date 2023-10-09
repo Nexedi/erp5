f180ffe56834fda3003beeb10f8863910448bb18
Rafael Monnerat <rafael@nexedi.com>
2023-10-09 20:06:45 +0000

    erp5_json_editor: Introduce JSON Editor
      This gadget aims to edit json based on json schema.

---
+++ bt5/erp5_json_editor/SkinTemplateItem/portal_skins/erp5_json_editor/WebSection_getJsonEditorPrecacheManifestList.py
@@ -0,0 +1,8 @@
+url_list = [
+  "font-awesome.5.1/font-awesome.5.1.css",
+  "json-editor.gadget.css",
+  "json-editor.gadget.html",
+  "json-editor.gadget.js",
+  "jsoneditor.js"
+]
+return url_list
