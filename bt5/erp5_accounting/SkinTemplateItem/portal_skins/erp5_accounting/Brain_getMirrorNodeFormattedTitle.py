21025d4db49c26e5192cfe75c92c6bc185328621
Jérome Perrin <jerome@nexedi.com>
2023-10-10 15:24:12 +0200

    accounting: show mirror account in grouping dialog
    A typical problem which prevents grouping is that the line is used in an
    internal invoice and while all the conditions for grouping seems OK for
    one side, they are not OK for the other side, because the accounts are
    different from the mirror side. By showing the mirror account it should
    be a bit easier for users to figure out what is wrong.

---
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/Brain_getMirrorNodeFormattedTitle.py
@@ -0,0 +1,8 @@
+mirror_node_formatted_title = ''
+if context.mirror_node_uid:
+  brain_list = context.getPortalObject().portal_catalog(uid=context.mirror_node_uid, limit=2)
+  if brain_list:
+    brain, = brain_list
+    mirror_node_formatted_title = brain.getObject().Account_getFormattedTitle()
+
+return mirror_node_formatted_title
