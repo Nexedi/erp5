c906f22710e70ce09b3815c2e4bf960cce33d4c9
Vincent Pelletier <vincent@nexedi.com>
2023-10-19 08:41:46 +0200

    z_related_translated_causality_portal_type: Add support for left-join use

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_translated_causality_portal_type.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_translated_causality_portal_type.sql
@@ -1,6 +1,12 @@
+<dtml-var table_1>.uid = <dtml-var table_0>.category_uid
+
+<dtml-var RELATED_QUERY_SEPARATOR>
+
 <dtml-var table_2>.original_message = <dtml-var table_1>.portal_type
 AND <dtml-var table_2>.message_context = "portal_type"
 AND <dtml-var table_2>.language = <dtml-sqlvar "Localizer.get_selected_language()" type="string">
-AND <dtml-var table_1>.uid = <dtml-var table_0>.category_uid
+
+<dtml-var RELATED_QUERY_SEPARATOR>
+
+<dtml-var table_0>.uid = catalog.uid
 AND <dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.causality.getUid()">
-AND <dtml-var table_0>.uid = catalog.uid
