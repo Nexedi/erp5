91adac4b2d17c6c2a52bc25854da72d75f281075
Jérome Perrin <jerome@nexedi.com>
2023-10-22 07:42:57 +0200

    Zelenium: output selenium logs to javascript console
    so that we can see them on testnode.

--- product/Zelenium/selenium/core/scripts/selenium-logging.js
+++ product/Zelenium/selenium/core/scripts/selenium-logging.js
@@ -117,18 +117,24 @@ Logger.prototype = {
     },
 
     debug: function(message) {
+       // debug is too verbose
+       // console.debug(new Date().toISOString(), message);
        this.log("debug", message);
     },
 
     info: function(message) {
+       // selenium instructions are logged with level info, we try not to include passwords in logs.
+       console.info(new Date().toISOString(), message.toString().replace(/password.*/, '***password redacted***'));
        this.log("info", message);
     },
 
     warn: function(message) {
+       console.warn(new Date().toISOString(), message);
        this.log("warn", message);
     },
 
     error: function(message) {
+       console.error(new Date().toISOString(), message);
        this.log("error", message);
     },
 
