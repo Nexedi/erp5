0679efe7e9e0422e565ee0a4332592376f6e7414
Roque Porchetto <roque.porchetto@nexedi.com>
2023-10-24 15:05:05 +0000

    erp5_web_monitoring: move sync lock

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
@@ -62,55 +62,6 @@
       return storage.removeAttachment.apply(storage, arguments);
     })
     .declareMethod('repair', function () {
-      
-      function promiseLock(name, options, callback) {
-        var callback_promise = null,
-          controller = new AbortController();
-
-        function canceller(msg) {
-          controller.abort();
-          if (callback_promise !== null) {
-            callback_promise.cancel(msg);
-          }
-        }
-
-        function resolver(resolve, reject) {
-          if (callback === undefined) {
-            callback = options;
-            options = {};
-          }
-          options.signal = controller.signal;
-
-          function handleCallback(lock) {
-            if (!lock) {
-              // The lock was not granted - get out fast.
-              return reject('Lock not granted');
-            }
-            try {
-              callback_promise = callback();
-            } catch (e) {
-              return reject(e);
-            }
-
-            callback_promise = new RSVP.Queue(callback_promise)
-              .push(resolve, function handleCallbackError(error) {
-                // Prevent rejecting the lock, if the result cancelled itself
-                if (!(error instanceof RSVP.CancellationError)) {
-                  canceller(error.toString());
-                  reject(error);
-                }
-              });
-            return callback_promise;
-          }
-
-          return navigator.locks.request(name, options, handleCallback)
-            .then(undefined, reject);
-        }
-
-        return new RSVP.Promise(resolver, canceller);
-      }
-      //TO DO
-      //promiseLock(name, options, storage.repair.apply, args) //storage, arguments
       var storage = this.props.jio_storage;
       return storage.repair.apply(storage, arguments)
         .push(undefined, function (error) {
