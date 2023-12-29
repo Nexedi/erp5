2b591c21d10bdd9c8f81e93a9bdc18b93dea4be5
Roque Porchetto <roque.porchetto@nexedi.com>
2023-12-29 18:21:39 +0000

    erp5_web_monitoring: refactor monitoring app to use new ojs framework - site web section changes - update app router - update app panel - adapt monitoring jio gadget and jio storage - add form views configuration - add new portal types, views and actions - migration_version check - update precache manifest - sync checks and redirection - refactor dispatch page according to new monitor urls - add opml redirects to front page - slapos portal types - drop obsolete files - drop auto-sync on add OPML page - parameters gadget field for instance tree view - custom views for software instance resources and processes - options header gadget (custom header options) - custom submit gadgets - graph gadget field

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_synchronize_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_synchronize_js.js
@@ -25,7 +25,7 @@
             });
         })
         .push(function () {
-          var redirect_options = {"page": "ojsm_status_list"};
+          var redirect_options = {"page": "ojsm_dispatch"};
           if (options.reset === "1") {
             // reset redirections
             return gadget.setSetting("sync_redirect_options", undefined)
