5736f050c2cba66b875e44389c6246271d18e69b
Romain Courteaud <romain@nexedi.com>
2024-01-15 09:17:12 +0000

    erp5_credential: add compatibility with Notification Message usage in erp5_crm

--- bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/NotificationMessage_getSubstitutionMappingDictFromArgument.py
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/NotificationMessage_getSubstitutionMappingDictFromArgument.py
@@ -2,4 +2,9 @@
 bug #1998 ('TextDocument should not required a substitution method')
 is fixed'''
 
-return mapping_dict
+result = kw
+
+if mapping_dict is not None:
+  result.update(mapping_dict)
+
+return result
