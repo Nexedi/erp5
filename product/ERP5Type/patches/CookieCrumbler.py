fc613af6fa72a61d19fab54f10cdff10419602b0
Jérome Perrin <jerome@nexedi.com>
2024-01-23 00:23:10 +0900

    base64 WIP

--- product/ERP5Type/patches/CookieCrumbler.py
+++ product/ERP5Type/patches/CookieCrumbler.py
@@ -193,7 +193,7 @@ def credentialsChanged(self, user, name, pw, request=None):
   # <patch>
   # We don't want new lines, so use base64.standard_b64encode instead of
   # base64.encodebytes
-  ac = standard_b64encode('%s:%s' % (name, pw)).rstrip()
+  ac = standard_b64encode(('%s:%s' % (name, pw)).encode()).rstrip().decode()
   # </patch>
   method = self.getCookieMethod('setAuthCookie',
                                  self.defaultSetAuthCookie)
