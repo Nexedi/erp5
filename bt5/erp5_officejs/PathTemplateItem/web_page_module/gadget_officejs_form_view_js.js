8c5f5ea19a4504bd1bdf5e1cdc28d770b86a9dbb
Roque Porchetto <roque.porchetto@nexedi.com>
2024-01-23 16:57:39 +0000

    erp5_officejs: more dev framework flexibility and fixes - allow custom app configurator - render listbox form field extra configuration - fix date format rendering - allow configuration form fields to have extra js code - allow to customize listbox column styles - allow to customize listbox jio_alldocs - fix custom view handling - fix gadget field rendering - allow dev user to set custom header options - truncate long string fields - add custom submit gadget option - improve renderjs_extra - update graph gadget

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_form_view_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_form_view_js.js
@@ -1,11 +1,12 @@
-/*global document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console */
+/*global document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console, escape */
 /*jslint nomen: true, indent: 2, maxerr: 10, maxlen: 80 */
-(function (document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console) {
+(function (document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console, escape) {
   "use strict";
 
-  function renderField(field_id, field_definition,
-                       context_document, data, blob_type, content_editable) {
-    var key, raw_value, override, final_value, item_list, result = {};
+  function renderField(field_id, field_definition, context_document,
+                       data, blob_type, content_editable, gadget) {
+    var key, raw_value, override, final_value, item_list, result = {}, i,
+        extra_query, param_name, doc_key;
     for (key in field_definition.values) {
       if (field_definition.values.hasOwnProperty(key)) {
         // order to get the final value (based on Field.py get_value)
@@ -50,11 +51,42 @@
         result.type = "EditorField";
       }
     }
+    if (field_definition.type == "ListBox") {
+      if (field_definition.values.default_params) {
+        for (i = 0; i < field_definition.values.default_params.length; i += 1) {
+          param_name = field_definition.values.default_params[i][0];
+          doc_key = field_definition.values.default_params[i][1];
+          if (context_document.hasOwnProperty(doc_key) &&
+              context_document[doc_key]) {
+            extra_query = ` AND ${param_name}:"${context_document[doc_key]}"`;
+            result.query += escape(extra_query);
+          }
+        }
+      }
+    }
+    if (field_definition.type == "GadgetField") {
+      var context_dict = {}, renderjs_extra, rjs_extra_parsed;
+      Object.assign(context_dict, gadget.state.doc);
+      context_dict.jio_key = gadget.state.options.jio_key;
+      if (field_definition.values.renderjs_extra &&
+          !Array.isArray(field_definition.values.renderjs_extra)) {
+        rjs_extra_parsed = JSON.parse(field_definition.values.renderjs_extra);
+      }
+      renderjs_extra = Object.assign({}, context_dict, rjs_extra_parsed);
+      field_definition.values.renderjs_extra = JSON.stringify(renderjs_extra);
+      result.renderjs_extra = JSON.stringify(renderjs_extra);
+    }
+    if (field_definition.values.extra) {
+      eval(field_definition.values.extra);
+    }
+    if (field_definition.values.style_columns) {
+      gadget.state.style_columns = field_definition.values.style_columns;
+    }
     return result;
   }
 
   function renderForm(form_definition, context_document, data, blob_type,
-                      content_editable) {
+                      content_editable, gadget) {
     var i, j, field_list, field_info, my_element, element_id, rendered_field,
       raw_properties = form_definition.fields_raw_properties,
       form_json = {
@@ -79,7 +111,7 @@
           field_info = raw_properties[my_element];
           rendered_field = renderField(element_id, field_info,
                                        context_document, data, blob_type,
-                                       content_editable);
+                                       content_editable, gadget);
           form_json.erp5_document._embedded._view[my_element] =
             rendered_field;
         }
@@ -129,42 +161,77 @@
     .declareAcquiredMethod("updateHeader", "updateHeader")
     .declareAcquiredMethod("getUrlForList", "getUrlForList")
     .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+    .declareAcquiredMethod("jio_get", "jio_get")
     .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
     .declareAcquiredMethod("jio_putAttachment", "jio_putAttachment")
     .declareAcquiredMethod("notifySubmitting", "notifySubmitting")
     .declareAcquiredMethod("notifySubmitted", 'notifySubmitted')
 
-    // XXX Hardcoded for modification_date rendering
+    // Format date rendering
     .allowPublicAcquisition("jio_allDocs", function (param_list) {
       var gadget = this;
+      if (gadget.state.style_columns &&
+          gadget.state.style_columns[0][0] === 'jio_allDocs') {
+        return new RSVP.Queue()
+          .push(function () {
+            return gadget.declareGadget(gadget.state.style_columns[0][1]);
+          })
+          .push(function (jio_alldocs) {
+            return jio_alldocs.jio_allDocs(param_list, gadget);
+          })
+          .push(function (result) {
+            if (result) {
+              return result;
+            } else {
+              return gadget.jio_allDocs(param_list[0]);
+            }
+          });
+      }
       return gadget.jio_allDocs(param_list[0])
         .push(function (result) {
-          var i, date, len = result.data.total_rows;
+          function truncate(str, n) {
+            return (str.length > n) ? str.slice(0, n - 1) + '...' : str;
+          }
+          var i, date, len = result.data.total_rows, date_key_array;
           for (i = 0; i < len; i += 1) {
-            if (result.data.rows[i].value.hasOwnProperty("modification_date")) {
-              date = new Date(result.data.rows[i].value.modification_date);
-              result.data.rows[i].value.modification_date = {
-                field_gadget_param: {
-                  allow_empty_time: 0,
-                  ampm_time_style: 0,
-                  css_class: "date_field",
-                  date_only: 0,
-                  description: "The Date",
-                  editable: 0,
-                  hidden: 0,
-                  hidden_day_is_last_day: 0,
-                  "default": date.toUTCString(),
-                  key: "modification_date",
-                  required: 0,
-                  timezone_style: 0,
-                  title: "Modification Date",
-                  type: "DateTimeField"
-                }
-              };
-              result.data.rows[i].value["listbox_uid:list"] = {
-                key: "listbox_uid:list",
-                value: 2713
-              };
+            // truncate long strings
+            for (var key in result.data.rows[i].value) {
+              result.data.rows[i].value[key] =
+                truncate(result.data.rows[i].value[key], 100);
+            }
+            // render dates with proper format
+            date_key_array = Object.keys(
+              result.data.rows[i].value).filter((k) => k.includes("date") ||
+                                                k.includes("Date"));
+            date_key_array.forEach((date_key) => {
+              if (result.data.rows[i].value.hasOwnProperty(date_key)) {
+                date = new Date(result.data.rows[i].value[date_key]);
+                result.data.rows[i].value[date_key] = {
+                  field_gadget_param: {
+                    allow_empty_time: 0,
+                    ampm_time_style: 0,
+                    css_class: "date_field",
+                    date_only: 0,
+                    description: "The Date",
+                    editable: 0,
+                    hidden: 0,
+                    hidden_day_is_last_day: 0,
+                    "default": date.toUTCString(),
+                    key: "date",
+                    required: 0,
+                    timezone_style: 0,
+                    title: "Date",
+                    type: "DateTimeField"
+                  }
+                };
+                result.data.rows[i].value["listbox_uid:list"] = {
+                  key: "listbox_uid:list",
+                  value: 2713
+                };
+              }
+            });
+            if (gadget.state.style_columns) {
+              eval(gadget.state.style_columns[0][0]);
             }
           }
           return result;
@@ -184,6 +251,13 @@
 
     .declareMethod("triggerSubmit", function (argument_list) {
       var gadget = this, child_gadget, content_dict;
+      if (gadget.state.form_definition.portal_type_dict.custom_submit) {
+        return gadget.declareGadget(gadget.state.form_definition
+                                    .portal_type_dict.custom_submit)
+          .push(function (submit_gadget) {
+            return submit_gadget.handle_submit(argument_list, gadget.state);
+          });
+      }
       return gadget.getDeclaredGadget('erp5_pt_gadget')
         .push(function (result) {
           child_gadget = result;
@@ -250,7 +324,7 @@
       }
       form_json = renderForm(gadget.state.form_definition, gadget.state.doc,
                                gadget.state.data, gadget.state.blob_type,
-                               content_editable);
+                               content_editable, gadget);
       while (gadget.element.firstChild) {
         gadget.element.removeChild(gadget.element.firstChild);
       }
@@ -267,13 +341,14 @@
     .declareMethod("renderSubGadget", function (options, subgadget, form_json) {
       var gadget = this, erp5_document = form_json.erp5_document,
         portal_type_dict = form_json.form_definition.portal_type_dict,
-        page_title;
+        page_title = '', header_dict;
       if (options.doc && options.doc.title) {
         page_title = options.doc.title;
       } else if (options.doc && options.doc.header_title) {
         page_title = options.doc.header_title;
-      } else {
-        page_title = portal_type_dict.title;
+      }
+      if (portal_type_dict.title) {
+        page_title = portal_type_dict.title + page_title;
       }
       return subgadget.render({
         jio_key: options.jio_key,
@@ -288,7 +363,9 @@
       // render the header
         .push(function () {
           var url_for_parameter_list = [
-            {command: 'change', options: {page: "tab"}},
+            {command: 'change', options: {page: "action_officejs",
+                                          jio_key: options.jio_key,
+                                          portal_type: options.portal_type}},
             {command: 'change', options: {page: "action_officejs",
                                           jio_key: options.jio_key,
                                           portal_type: options.portal_type}},
@@ -316,7 +393,7 @@
           return gadget.getUrlForList(url_for_parameter_list);
         })
         .push(function (url_list) {
-          var header_dict = { "page_title": page_title };
+          header_dict = { "page_title": page_title };
           if (options.form_type === 'dialog') {
             //TODO: find correct url
             header_dict.cancel_url = url_list[6];
@@ -335,7 +412,9 @@
             if (portal_type_dict.has_more_views) {
               header_dict.tab_url = url_list[0];
             }
-            header_dict.save_action = portal_type_dict.editable === 1;
+            if (portal_type_dict.editable === 1) {
+              header_dict.save_action = true;
+            }
             if (portal_type_dict.has_more_actions ||
                 portal_type_dict.has_more_views) {
               header_dict.actions_url = url_list[1];
@@ -353,8 +432,20 @@
               }
             }
           }
-          return gadget.updateHeader(header_dict);
+          return gadget.declareGadget(portal_type_dict.custom_header);
+        })
+        .push(function (header_gadget) {
+          return header_gadget.getOptions(portal_type_dict, options, header_dict);
+        }, function (error) {
+          if (!portal_type_dict.custom_header) {
+            return header_dict;
+          } else {
+            throw error;
+          }
+        })
+        .push(function (header_options) {
+          return gadget.updateHeader(header_options);
         });
     });
 
-}(document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console));
+}(document, window, rJS, RSVP, Blob, URL, jIO, ensureArray, console, escape));
