890729c6867f4420c967d4d58f2abba6978d35a5
Vincent Pelletier <vincent@nexedi.com>
2024-01-24 11:38:12 +0900

    erp5_hal_json_style Base_redirect: Remove dead code
    Nothing uses redirect_url, so do not bother setting it.

--- bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
+++ bt5/erp5_hal_json_style/SkinTemplateItem/portal_skins/erp5_hal_json_style/Base_redirect.py
@@ -16,19 +16,6 @@ request_form.update(kw)
 request_form = context.ERP5Site_filterParameterList(request_form)
 request_form.update(keep_items)
 
-if form_id == 'view':
-  redirect_url = "%s/ERP5Document_getHateoas" % context.absolute_url()
-else:
-  redirect_url = '%s/%s' % (context.absolute_url(), form_id)
-
-parameters = make_query(dict([(k, v) for k, v in request_form.items() if k and v is not None]))
-if len(parameters):
-  if '?' in redirect_url:
-    separator = '&'
-  else:
-    separator = '?'
-  redirect_url = '%s%s%s' % (redirect_url, separator, parameters)
-
 
 # XXX Allow CORS
 response = context.REQUEST.RESPONSE
