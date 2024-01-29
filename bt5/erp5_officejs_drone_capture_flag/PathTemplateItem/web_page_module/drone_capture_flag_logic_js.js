688e8b32126d75db9e7dbf714393d9255bbc9728
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2024-01-29 13:49:20 +0000

    erp5_officejs_drone_simulator: rename getGroundSpeed into getSpeed
    As it is conventional to return the speed on latitude and longitude axis
    (either ground or air speed) when asking for the drone speed,
    groundspeed can be here used as default (there is no meaning for
    airspeed as wind is neglected in the simulation).

--- bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.js
+++ bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.js
@@ -350,9 +350,9 @@ var DroneManager = /** @class */ (function () {
   DroneManager.prototype.get3DSpeed = function () {
     return this._speed;
   };
-  DroneManager.prototype.getGroundSpeed = function () {
-    if (typeof this._API.getGroundSpeed !== "undefined") {
-      return this._API.getGroundSpeed(this);
+  DroneManager.prototype.getSpeed = function () {
+    if (typeof this._API.getSpeed !== "undefined") {
+      return this._API.getSpeed(this);
     }
     return;
   };
@@ -369,7 +369,7 @@ var DroneManager = /** @class */ (function () {
     return;
   };
   DroneManager.prototype.land = function () {
-    return this._API.triggerParachute(this);
+    return this._API.land(this);
   };
   DroneManager.prototype.exit = function () {
     this._internal_crash();
@@ -986,7 +986,7 @@ var GameManager = /** @class */ (function () {
                   game_manager._game_duration, geo_coordinates.latitude,
                   geo_coordinates.longitude,
                   map_info.start_AMSL + drone_position.z,
-                  drone_position.z, drone.getYaw(), drone.getGroundSpeed(),
+                  drone_position.z, drone.getYaw(), drone.getSpeed(),
                   drone.getClimbRate()
                 ]);
               }
