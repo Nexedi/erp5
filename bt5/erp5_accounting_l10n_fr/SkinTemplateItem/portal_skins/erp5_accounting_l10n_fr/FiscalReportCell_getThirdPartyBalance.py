b3de3db900a25a53305a6b98cc135e46cfb18436
Jérome Perrin <jerome@nexedi.com>
2024-01-29 10:53:16 +0900

    accounting_l10n_fr py3

--- bt5/erp5_accounting_l10n_fr/SkinTemplateItem/portal_skins/erp5_accounting_l10n_fr/FiscalReportCell_getThirdPartyBalance.py
+++ bt5/erp5_accounting_l10n_fr/SkinTemplateItem/portal_skins/erp5_accounting_l10n_fr/FiscalReportCell_getThirdPartyBalance.py
@@ -30,7 +30,7 @@ if ledger is not None:
   if isinstance(ledger, list) or isinstance(ledger, tuple):
     kw['ledger_uid'] = [portal_categories.ledger.restrictedTraverse(item).getUid() for item in ledger]
   else:
-    kw['ledger_uid'] = portal_categories.ledger.restrictedTraverse(item).getUid()
+    kw['ledger_uid'] = portal_categories.ledger.restrictedTraverse(ledger).getUid()
 
 
 # Find accounts that can be expanded according category membership
@@ -47,7 +47,7 @@ accounts_to_expand_by_tp = rec_cat.getAccountTypeRelatedValueList(**params) + \
 
 total_balance = 0.0
 for account_gap_number in accounts:
-  # We get all acounts strict member of this GAP category
+  # We get all accounts strict member of this GAP category
   gap = context.restrictedTraverse('portal_categories/' + getURL(account_gap_number))
 
   for account in gap.getGapRelatedValueList(portal_type='Account'):
