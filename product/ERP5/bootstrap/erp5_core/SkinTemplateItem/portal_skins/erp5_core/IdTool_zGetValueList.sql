c89b099ce88d611750c0d37b9c92e13a4bb506a7
Jérome Perrin <jerome@nexedi.com>
2024-02-01 00:25:48 +0900

    IdTool: handle bytes for group_id on python3
    group_id is used as key of OOBtree and as documented, it's not
    possible to mix keys that can not be compared, so we can not have a mix
    of string and bytes. Since years

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/IdTool_zGetValueList.sql
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/IdTool_zGetValueList.sql
@@ -1,3 +1,3 @@
-select id_group, last_id from portal_ids 
-  <dtml-if id_group>where id_group > "<dtml-var id_group>"</dtml-if>
+select cast(id_group as CHAR) id_group, last_id from portal_ids
+  <dtml-if id_group>where id_group > <dtml-sqlvar id_group type="string"></dtml-if>
   order by id_group
