41e1b6c286ab33509b5a6d132d41a8d5360680bc
Jérome Perrin <jerome@nexedi.com>
2024-01-31 14:33:12 +0900

    CRM py3

--- product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.File.py
+++ product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.File.py
@@ -199,9 +199,9 @@ class File(Document, OFS_File):
 
   security.declareProtected(Permissions.AccessContentsInformation, 'getMimeTypeAndContent')
   def getMimeTypeAndContent(self):
+    # type: () -> tuple[str, bytes]
     """This method returns a tuple which contains mimetype and content."""
     from erp5.component.document.EmailDocument import MimeTypeException
-    # return a tuple (mime_type, data)
     content = None
     mime_type = self.getContentType()
 
@@ -223,6 +223,8 @@ class File(Document, OFS_File):
       elif getattr(self, 'getBaseData', None) is not None:
         content = self.getBaseData()
 
+    if isinstance(content, six.text_type):
+      content = content.encode('utf-8')
     if content and not isinstance(content, bytes):
       content = bytes(content)
 
