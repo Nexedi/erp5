8afd0b8f61137e2772fd75d636386d90af0249e3
Jérome Perrin <jerome@nexedi.com>
2024-01-31 14:21:57 +0900

    *: use key instead of cmp to sort

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getAccountingTransactionLineList.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getAccountingTransactionLineList.py
@@ -1,12 +1,15 @@
 if not portal_type:
   portal_type = context.getPortalObject().getPortalAccountingMovementTypeList()
 
-sort_dict = { 'income': 0,
-              'expense': -2,
-              'receivable': -2,
-              'payable': 0,
-              'collected_vat': -1,
-              'refundable_vat': -1 }
+sort_dict = {
+  'bank': -3,
+  'income': 0,
+  'expense': -2,
+  'receivable': -2,
+  'payable': 0,
+  'collected_vat': -1,
+  'refundable_vat': -1,
+}
 
 def getAccountingTransactionLineSortKey(line):
   return sort_dict.get(line.getId(), (line.getIntIndex() or line.getIntId() or 0))
