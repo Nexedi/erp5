afad31effb1696ca2f7675302bf25783dbe6e770
Jérome Perrin <jerome@nexedi.com>
2024-02-01 13:53:36 +0900

    bearer_token: py3

--- bt5/erp5_bearer_token/ExtensionTemplateItem/portal_components/extension.erp5.BearerToken.py
+++ bt5/erp5_bearer_token/ExtensionTemplateItem/portal_components/extension.erp5.BearerToken.py
@@ -1,10 +1,12 @@
 import hashlib
 import hmac
 from Products.ERP5Type.Cache import DEFAULT_CACHE_SCOPE
+import six
 
 CACHE_FACTORY_NAME = 'bearer_token_cache_factory'
 
 def getHMAC(self, key, body):
+  # type: (erp5.portal_type.Base, bytes, bytes) -> str
   digest = hmac.new(key, body, digestmod=hashlib.md5)
   return digest.hexdigest()
 
@@ -22,6 +24,10 @@ def _getCacheFactory(self, cache_factory_name):
   return cache_tool.getRamCacheRoot().get(cache_factory_name)
 
 def setBearerToken(self, key, body, cache_factory_name=CACHE_FACTORY_NAME):
+  # type: (erp5.portal_type.Base, str, Any, str) -> None
+  if not isinstance(key, six.string_types):
+    __traceback_info__ = key
+    raise TypeError("Wrong key type %s" % (type(key)))
   cache_factory = _getCacheFactory(self, cache_factory_name)
   cache_duration = cache_factory.cache_duration
   for cache_plugin in cache_factory.getCachePluginList():
@@ -29,6 +35,8 @@ def setBearerToken(self, key, body, cache_factory_name=CACHE_FACTORY_NAME):
                      body, cache_duration=cache_duration)
 
 def getBearerToken(self, key, cache_factory_name=CACHE_FACTORY_NAME):
+  # type: (erp5.portal_type.Base, str, str) -> Any
+  assert isinstance(key, six.string_types)
   cache_factory = _getCacheFactory(self, cache_factory_name)
   for cache_plugin in cache_factory.getCachePluginList():
     cache_entry = cache_plugin.get(key, DEFAULT_CACHE_SCOPE)
