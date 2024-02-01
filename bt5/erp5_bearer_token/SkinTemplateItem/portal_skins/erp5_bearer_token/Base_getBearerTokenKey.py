33b8c8c057e6091f86ff2f12e44cd247dee5a61c
Jérome Perrin <jerome@nexedi.com>
2024-02-01 13:53:36 +0900

    bearer_token: py3

--- bt5/erp5_bearer_token/SkinTemplateItem/portal_skins/erp5_bearer_token/Base_getBearerTokenKey.py
+++ bt5/erp5_bearer_token/SkinTemplateItem/portal_skins/erp5_bearer_token/Base_getBearerTokenKey.py
@@ -1,4 +1,4 @@
 if REQUEST is not None:
   # mini security
   return None
-return context.getPortalObject().portal_preferences.getPreferredBearerTokenKey()
+return (context.getPortalObject().portal_preferences.getPreferredBearerTokenKey() or '').encode('utf-8')
