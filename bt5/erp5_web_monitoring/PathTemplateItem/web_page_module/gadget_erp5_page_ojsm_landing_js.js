b6676edffdac9ba2fe47a26e859b2c04a1ce734e
Roque Porchetto <roque.porchetto@nexedi.com>
2024-02-01 14:30:31 +0000

    erp5_web_monitoring: refactor landing page query handling

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_landing_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_landing_js.js
@@ -14,7 +14,7 @@
     // declared methods
     /////////////////////////////////////////////////////////////////
     .declareMethod("render", function (options) {
-      var gadget = this,
+      var gadget = this, portal_type, extended_search, query_parts_list,
         redirect_options = {
           "page": "ojsm_dispatch",
           "url": options.url,
@@ -22,21 +22,42 @@
           "password": options.password,
           "query": options.query
         };
+      //specific instance tree/software
       if (options.query) {
+        // URL queries from slapos master / panel rapid space:
+        // query = portal_type: Instance Tree AND title:"my-title"
+        // query = portal_type: Software Instance AND title:"my-title"
+        //           AND specialise_title:"my-parent-title"
+        if (options.query.includes("portal_type")) {
+          query_parts_list = options.query.split('AND');
+          portal_type = query_parts_list[0].replace('portal_type:', '').trim();
+          extended_search = options.query.replace(query_parts_list[0] + 'AND ', '').trim();
+          redirect_options = {
+            "page": "ojs_local_controller",
+            "portal_type": portal_type + " Module",
+            "url": options.url,
+            "username": options.username,
+            "password": options.password,
+            "extended_search": extended_search
+          };
+        }
         return gadget.redirect({"command": "display",
                                 "options": redirect_options
                                });
       }
+      // add opml (old setting configurator)
       if (options.url && options.username && options.password) {
         redirect_options.page = "ojsm_opml_add";
         return gadget.redirect({"command": "display",
                                 "options": redirect_options
                                });
       }
+      // default front page (list of promises)
       return gadget.redirect({command: 'display',
                               options: {page: "ojs_local_controller",
                                         portal_type: "Promise Module"}
                              });
+      //TODO check here the last sync and redirect to import-export page?
     });
 
 }(window, rJS));
