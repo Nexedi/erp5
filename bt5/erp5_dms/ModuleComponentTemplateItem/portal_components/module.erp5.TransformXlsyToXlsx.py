88ace73bd6e47be544d66d49681341e66c44a411
Jérome Perrin <jerome@nexedi.com>
2024-02-07 08:44:12 +0900

    dms: py3

--- bt5/erp5_dms/ModuleComponentTemplateItem/portal_components/module.erp5.TransformXlsyToXlsx.py
+++ bt5/erp5_dms/ModuleComponentTemplateItem/portal_components/module.erp5.TransformXlsyToXlsx.py
@@ -23,7 +23,7 @@ class XlsyToXlsx:
     raise AttributeError(attr)
 
   def convert(self, orig, data, cache=None, filename=None, context=None, **kwargs):
-    data = str(orig)
+    data = bytes(orig)
     xlsy = OOOdCommandTransform(context, filename, data, self.inputs[0])
     xlsx = xlsy.convertTo('xlsx')
     if cache is not None:
