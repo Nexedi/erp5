abd1bb99140f6e2131f391715bcee5cf4ebe5bbb
Jérome Perrin <jerome@nexedi.com>
2024-02-07 08:44:12 +0900

    dms: py3

--- bt5/erp5_dms/ModuleComponentTemplateItem/portal_components/module.erp5.TransformOdsToHtml.py
+++ bt5/erp5_dms/ModuleComponentTemplateItem/portal_components/module.erp5.TransformOdsToHtml.py
@@ -24,7 +24,7 @@ class OdsToHtml:
     raise AttributeError(attr)
 
   def convert(self, orig, data, cache=None, filename=None, context=None, **kwargs):
-    data = str(orig)
+    data = bytes(orig)
     ods = OOOdCommandTransform(context, filename, data, self.inputs[0])
     html = ods.convertTo('html')
     if cache is not None:
