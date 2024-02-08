517a4201b725268c69abe307f4a817a1689d7b6c
Jérome Perrin <jerome@nexedi.com>
2024-02-09 00:15:07 +0900

    payroll_l10n_fr: py3

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_generatePayslipReport.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/PaySheetTransaction_generatePayslipReport.py
@@ -1,10 +1,11 @@
+import six
 kw["report_data"] = context.PaySheetTransaction_getPayslipData()
 kw["report_data"]["amount_of_remuneration_evolution"] = context.REQUEST.get('field_your_evoluation_remuneration', 0)
 kw["report_data"]["total_holiday"] = context.REQUEST.get('field_your_total_holiday', 0)
 kw["report_data"]["taken_holiday"] = context.REQUEST.get('field_your_taken_holiday', 0)
 rep_content = context.PaySheetTransaction_generatePayslipReportContent(**kw)
 
-if isinstance(rep_content, unicode):
+if six.py2 and isinstance(rep_content, six.text_type):
   rep_content = rep_content.encode("utf8")
 
 return rep_content,"",""
