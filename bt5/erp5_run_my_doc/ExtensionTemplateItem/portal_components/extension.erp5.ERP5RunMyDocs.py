becab1ff1cc562c142bd45bd192523ebb7d22626
Jérome Perrin <jerome@nexedi.com>
2024-02-09 00:14:10 +0900

    run_my_doc: py3

--- bt5/erp5_run_my_doc/ExtensionTemplateItem/portal_components/extension.erp5.ERP5RunMyDocs.py
+++ bt5/erp5_run_my_doc/ExtensionTemplateItem/portal_components/extension.erp5.ERP5RunMyDocs.py
@@ -76,7 +76,7 @@ def extractTest(text):
           # Include Macros as it is defined by the user.
           testcode += row[0].text
         else:
-          testcode += lxml.html.tostring(row)
+          testcode += lxml.html.tostring(row, encoding='unicode')
   return testcode.strip()
 
 """
