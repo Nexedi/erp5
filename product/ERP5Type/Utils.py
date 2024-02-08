c503f31821dfd3b0ae1eecca61d339f0ff5c1030
Jérome Perrin <jerome@nexedi.com>
2024-02-08 09:38:14 +0100

    ERP5Type.Utils: new non_publishable decorator

--- product/ERP5Type/Utils.py
+++ product/ERP5Type/Utils.py
@@ -418,6 +418,19 @@ def fill_args_from_request(*optional_args):
     return decorator(function)
   return decorator
 
+
+if six.PY3:
+  from ZPublisher import zpublish
+  non_publishable = zpublish(publish=False)
+else:
+  def non_publishable(f):
+    """Decorator to prevent method f from being publishable by Zope.
+    """
+    del getattr(f, '__func__', f).__doc__
+    assert not f.__doc__
+    return f
+
+
 _pylint_message_re = re.compile(
   r'^(?P<type>[CRWEF]):\s*(?P<row>\d+),\s*(?P<column>\d+):\s*(?P<message>.*)$')
 
