3041de1f2ce89ff6ded0dec3b794d3bbb6e7fd9c
Jérome Perrin <jerome@nexedi.com>
2024-02-09 00:51:56 +0900

    officejs_support_request_ui: py3

--- bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
+++ bt5/erp5_officejs_support_request_ui/SkinTemplateItem/portal_skins/erp5_officejs_support_request/ERP5Site_getTicketWorkflowStateInfoDict.py
@@ -2,6 +2,7 @@
 
 This script has proxy role, as only manager can access workflow configuration.
 """
+import six
 from Products.ERP5Type.Message import translateString
 portal = context.getPortalObject()
 
@@ -11,9 +12,9 @@ workflow = portal.portal_workflow.ticket_workflow
 
 for state in workflow.getStateValueList():
   state_title = state.title_or_id()
-  state_title = unicode(translateString(
+  state_title = six.text_type(translateString(
       '%s [state in %s]' % (state_title, workflow.getId()),
-      default=unicode(translateString(state_title))))
+      default=six.text_type(translateString(state_title))))
   info[state.getReference()] = state_title
 
 return info
