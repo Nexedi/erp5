8282ce71a2da5de6b1b7e4e72bf8017a4721061e
Jérome Perrin <jerome@nexedi.com>
2024-02-09 06:35:16 +0100

    core/gadget_global.js: make isEmpty supports empty object
    Co-authored-by: Romain Courteaud <romain@nexedi.com>

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/gadget_global.js.js
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/gadget_global.js.js
@@ -9,10 +9,14 @@
   Calling isEmpty(x) is more robust than expression !x.
   */
   function isEmpty(value) {
-    return (value === undefined ||
-            value === null ||
-            value.length === 0 ||
-            (typeof value === "number" && isNaN(value)));
+    return (
+      (value === undefined) ||
+      (value === null) ||
+      ((typeof value === "number") ?
+        isNaN(value) :
+        (Object.keys(value).length === 0)
+      )
+    );
   }
   window.isEmpty = isEmpty;
 
