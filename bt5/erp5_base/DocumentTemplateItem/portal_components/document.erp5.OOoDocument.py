3e01add7c5478de61a9c404b3760afb98d4e99b7
Jérome Perrin <jerome@nexedi.com>
2024-02-11 00:34:41 +0900

    ERP5OOo: py3

--- bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.OOoDocument.py
+++ bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.OOoDocument.py
@@ -38,8 +38,8 @@ from Products.ERP5Type import Permissions, PropertySheet
 from Products.ERP5Type.Cache import CachingMethod
 from erp5.component.document.File import File
 from erp5.component.document.Document import Document, \
-       VALID_IMAGE_FORMAT_LIST, ConversionError, NotConvertedError
-from Products.ERP5Type.Utils import bytes2str, fill_args_from_request, str2bytes
+       VALID_IMAGE_FORMAT_LIST, VALID_TEXT_FORMAT_LIST, ConversionError, NotConvertedError
+from Products.ERP5Type.Utils import guessEncodingFromText, bytes2str, fill_args_from_request, str2bytes
 
 # Mixin Import
 from erp5.component.mixin.BaseConvertableFileMixin import BaseConvertableFileMixin
@@ -334,7 +334,12 @@ class OOoDocument(OOoDocumentExtensibleTraversableMixin, BaseConvertableFileMixi
         # store conversion
         self.setConversion(data, mime, format=original_format, **kw)
 
-    return self.getConversion(format=original_format, **kw)
+    mime, data = self.getConversion(format=original_format, **kw)
+    if format in VALID_TEXT_FORMAT_LIST:
+      # Libreoffice conversions on cloudooo usually have a BOM, we are using guessEncodingFromText
+      # here mostly as a convenient way to decode with the encoding from BOM
+      data = data.decode(guessEncodingFromText(data) or 'ascii')
+    return mime, data
 
   security.declareProtected(Permissions.ModifyPortalContent,
                             '_populateConversionCacheWithHTML')
