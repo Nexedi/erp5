f99e730032bd7b0942ad1f62800644349d9b9644
Jérome Perrin <jerome@nexedi.com>
2024-02-12 22:11:03 +0900

    testOOoStyle: py3

--- product/ERP5OOo/tests/testOOoStyle.py
+++ product/ERP5OOo/tests/testOOoStyle.py
@@ -700,7 +700,7 @@ class TestOOoStyle(ERP5TypeTestCase, ZopeTestCase.Functional):
     self.assertEqual('text/html;charset=utf-8', content_type.lower())
     self.assertFalse(response.getHeader('content-disposition'))
     # Simplistic assertion that we are viewing the ODF XML source
-    self.assertIn('office:document-content', response.getBody())
+    self.assertIn(b'office:document-content', response.getBody())
 
   def test_form_list_ZMI(self):
     """We can edit form_list in the ZMI."""
@@ -710,7 +710,7 @@ class TestOOoStyle(ERP5TypeTestCase, ZopeTestCase.Functional):
     content_type = response.getHeader('content-type')
     self.assertEqual('text/html;charset=utf-8', content_type.lower())
     self.assertFalse(response.getHeader('content-disposition'))
-    self.assertIn('office:document-content', response.getBody())
+    self.assertIn(b'office:document-content', response.getBody())
 
   def test_report_view_ZMI(self):
     """We can edit report_view in the ZMI."""
@@ -720,7 +720,8 @@ class TestOOoStyle(ERP5TypeTestCase, ZopeTestCase.Functional):
     content_type = response.getHeader('content-type')
     self.assertEqual('text/html;charset=utf-8', content_type.lower())
     self.assertFalse(response.getHeader('content-disposition'))
-    self.assertIn('office:document-content', response.getBody())
+    self.assertIn(b'office:document-content', response.getBody())
+
 
 class TestODTStyle(TestOOoStyle):
   skin = 'ODT'
