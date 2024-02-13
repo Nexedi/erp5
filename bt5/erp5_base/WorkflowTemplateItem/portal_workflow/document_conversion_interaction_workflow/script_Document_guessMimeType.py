f28f3917dcdea2b7ef3cc9056928ecf6381fc595
Jérome Perrin <jerome@nexedi.com>
2024-02-14 01:05:06 +0900

    base/conversion: setContentType before converting to base format
    some conversion (TextDocument) depend on the content type, so it's
    necessary to first update the content type before trying to convert to
    base format

--- bt5/erp5_base/WorkflowTemplateItem/portal_workflow/document_conversion_interaction_workflow/script_Document_guessMimeType.py
+++ bt5/erp5_base/WorkflowTemplateItem/portal_workflow/document_conversion_interaction_workflow/script_Document_guessMimeType.py
@@ -2,7 +2,7 @@ document = state_change['object']
 portal = document.getPortalObject()
 
 filename = document.getFilename()
-content_type = portal.mimetypes_registry.lookupExtension(filename)
-
-if content_type is not None:
-  document.setContentType(str(content_type))
+if filename:
+  content_type = portal.mimetypes_registry.lookupExtension(filename)
+  if content_type is not None:
+    document.setContentType(str(content_type))
