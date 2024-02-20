2bc8c2a1a089a8d7abbb08ab466a464d33c6e1b5
Roque Porchetto <roque.porchetto@nexedi.com>
2024-02-20 17:10:50 +0000

    erp5_web_monitoring: reactor dispatch page according to new monitor urls

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
@@ -10,25 +10,50 @@
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("redirect", "redirect")
     .declareAcquiredMethod("getSetting", "getSetting")
-    .declareAcquiredMethod("setSetting", "setSetting")
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
     /////////////////////////////////////////////////////////////////
     .declareMethod("render", function (options) {
-      var gadget = this, portal_type, extended_search,
-        query_parts_list, redirect_options;
-      // add opml (old setting configurator)
-      if (!options.query && options.url && options.username && options.password) {
-        redirect_options = {
-          "url": options.url,
-          "username": options.username,
-          "password": options.password,
-          "page": "ojsm_opml_add"
-        };
-        return gadget.redirect({"command": "display",
-                                "options": redirect_options
-                               });
+      var gadget = this, portal_type, extended_search, query_parts_list;
+      if (options.query && options.url && options.username && options.password) {
+        return new RSVP.Queue()
+          .push(function () {
+            return gadget.jio_allDocs({
+              query: options.query
+            });
+          })
+        .push(function (result) {
+          if (result === undefined || result.data.total_rows === 0) {
+            return gadget.redirect(
+              {"command": "display",
+               "options": {
+              "url": options.url,
+              "username": options.username,
+              "password": options.password,
+              "page": "ojsm_opml_add"
+            }
+            });
+          } else {
+            // URL queries from slapos master / panel rapid space:
+            // 'portal_type: "Instance Tree" AND title:"my-title"'
+            // or
+            // 'portal_type: "Software Instance" AND title:"my-title"
+            //           AND specialise_title:"my-parent-title"'
+            query_parts_list = options.query.split('AND');
+            portal_type = query_parts_list[0].replace('portal_type:', '').replaceAll('"', '').trim();
+            extended_search = options.query.replace(query_parts_list[0] + 'AND ', '').trim();
+            return gadget.redirect(
+              {"command": "display",
+               "options": {
+                "page": "ojs_local_controller",
+                "portal_type": portal_type + " Module",
+                "extended_search": extended_search
+              }
+              });
+          }
+        });
       }
       return new RSVP.Queue()
         .push(function () {
@@ -38,40 +63,14 @@
               // 1209600000 = 1000*60*60*24*14
               if (import_date === undefined ||
                   (import_date + 1209600000) < new Date().getTime()) {
-                return gadget.setSetting('sync_redirect_options', {
-                  query: options.query,
-                  page: 'ojsm_dispatch'
-                })
-                  .push(function () {
-                    return gadget.redirect({command: 'change', options: {
-                      page: "ojsm_erp5_configurator",
-                      type: "erp5"
-                    }});
-                  });
+                return gadget.redirect({command: 'change', options: {
+                  page: "ojsm_erp5_configurator",
+                  type: "erp5"
+                }});
               }
             });
         })
         .push(function () {
-          //specific instance tree/software
-          if (options.query) {
-            // URL queries from slapos master / panel rapid space:
-            // 'portal_type: "Instance Tree" AND title:"my-title"'
-            // 'portal_type: "Software Instance" AND title:"my-title"
-            //           AND specialise_title:"my-parent-title"'
-            if (options.query.includes("portal_type")) {
-              query_parts_list = options.query.split('AND');
-              portal_type = query_parts_list[0].replace('portal_type:', '').replaceAll('"', '').trim();
-              extended_search = options.query.replace(query_parts_list[0] + 'AND ', '').trim();
-              redirect_options = {
-                "page": "ojs_local_controller",
-                "portal_type": portal_type + " Module",
-                "extended_search": extended_search
-              };
-            }
-            return gadget.redirect({"command": "display",
-                                    "options": redirect_options
-                                   });
-          }
           // default front page (list of promises)
           return gadget.redirect({command: 'display',
                                   options: {page: "ojs_local_controller",
