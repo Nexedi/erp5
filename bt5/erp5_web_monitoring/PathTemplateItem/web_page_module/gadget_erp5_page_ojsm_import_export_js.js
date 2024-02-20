1e8e6f3b7ece4750fa59c1d135cdcf36a747d410
Roque Porchetto <roque.porchetto@nexedi.com>
2024-02-20 17:11:22 +0000

    erp5_web_monitoring: add a control on sync data processing

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_import_export_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_import_export_js.js
@@ -379,22 +379,24 @@
             if (tmp_parameter === undefined) {
               tmp_parameter = {username: "", password: "", opml_url: undefined};
             }
-            opml_list.push({
-              portal_type: "opml",
-              title: instance_tree_list[uid_dict[tmp_uid]]
-                .title,
-              relative_url: instance_tree_list[uid_dict[tmp_uid]]
-                .relative_url,
-              url: tmp_parameter.opml_url || String(tmp_uid) + " NO MONITOR",
-              has_monitor: tmp_parameter.opml_url !== undefined,
-              username: tmp_parameter.username,
-              password: tmp_parameter.password,
-              basic_login: btoa(tmp_parameter.username + ':' +
-                                tmp_parameter.password),
-              active: tmp_parameter.opml_url !== undefined &&
-                instance_tree_list[uid_dict[tmp_uid]].active,
-              state: instance_tree_list[uid_dict[tmp_uid]].state
-            });
+            if (instance_tree_list[uid_dict[tmp_uid]]) {
+              opml_list.push({
+                portal_type: "opml",
+                title: instance_tree_list[uid_dict[tmp_uid]]
+                  .title,
+                relative_url: instance_tree_list[uid_dict[tmp_uid]]
+                  .relative_url,
+                url: tmp_parameter.opml_url || String(tmp_uid) + " NO MONITOR",
+                has_monitor: tmp_parameter.opml_url !== undefined,
+                username: tmp_parameter.username,
+                password: tmp_parameter.password,
+                basic_login: btoa(tmp_parameter.username + ':' +
+                                  tmp_parameter.password),
+                active: tmp_parameter.opml_url !== undefined &&
+                  instance_tree_list[uid_dict[tmp_uid]].active,
+                state: instance_tree_list[uid_dict[tmp_uid]].state
+              });
+            }
           }
         }
         return opml_list;
