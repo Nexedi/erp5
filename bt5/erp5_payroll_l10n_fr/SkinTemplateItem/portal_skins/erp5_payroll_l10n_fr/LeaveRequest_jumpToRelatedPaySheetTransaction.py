acdb1447b1bce605e65cddd1bdc86573f048dfb8
Rafael Monnerat <rafael@nexedi.com>
2024-02-21 20:15:41 +0000

    erp5_payroll_l10n_fr: Improve jump if no document related.
       Also include a minimal message.

--- bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/LeaveRequest_jumpToRelatedPaySheetTransaction.py
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/LeaveRequest_jumpToRelatedPaySheetTransaction.py
@@ -1,1 +1,11 @@
-return context.LeaveRequest_getRelatedPaySheetTransaction().Base_redirect()
+transaction = context.LeaveRequest_getRelatedPaySheetTransaction()
+translateString = context.Base_translateString
+
+if transaction is not None:
+  return transaction.Base_redirect("view",
+    keep_items={
+      'portal_status_message': translateString('Related Pay Sheet Transaction.')})
+
+return context.Base_redirect("view",
+    keep_items={
+      'portal_status_message': translateString('No Related Pay Sheet Transaction.')})
