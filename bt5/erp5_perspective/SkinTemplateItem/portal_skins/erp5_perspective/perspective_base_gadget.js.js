4de87f6a7d18b136b390e11fbd65e7c3c1329290
Nikola <nikola.balog@rwth-aachen.de>
2024-02-21 15:43:46 +0100

    Perspective gadget works as a root gadget.
    The perspective gadget creates child gadgets which need to implement the getData method.
    The child gadget can, but is not required to, provide the postprocessing method to manipulate the perspective table.

--- bt5/erp5_perspective/SkinTemplateItem/portal_skins/erp5_perspective/perspective_base_gadget.js.js
+++ bt5/erp5_perspective/SkinTemplateItem/portal_skins/erp5_perspective/perspective_base_gadget.js.js
@@ -1,12 +1,38 @@
-/*global window, rJS */
+/*global window, rJS, document */
 /*jslint nomen: true, indent:2*/
-// Provides perspective worker
 (function (window, rJS) {
   "use strict";
   rJS(window)
+    .declareMethod("render", function (options) {
+      var gadget = this,
+        child_html = options.child_html,
+        el = document.getElementsByTagName("perspective-viewer")[0];
+
+      gadget.getChildData(options.child_html)
+        .push(function (data) {
+          return gadget.getTable(data);
+        })
+        .push(function (table) {
+          return el.load(table);
+        })
+        .push(function () {
+          gadget.getDeclaredGadget(child_html)
+            .push(function (child) {
+              return child.postprocessing(el);
+            });
+        });
+
+    })
     .declareMethod("getTable", function (data) {
       return window.Worker.table(data);
+    })
+    .declareMethod("getChildData", function (data) {
+      return this.declareGadget(data, {
+        scope: data
+      })
+        .push(function (child) {
+          return child.getData();
+        });
     });
 
 }(window, rJS));
-
