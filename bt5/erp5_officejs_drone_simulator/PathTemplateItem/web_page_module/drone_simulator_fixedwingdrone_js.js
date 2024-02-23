2f7689d1f20b348f446b2c1b357769788f44dd81
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2024-02-23 16:24:38 +0000

    erp5_officejs_drone_simulator: add max_command_frequency
    See merge request !1894
    
    * set _game_duration as an unix epoch
    * provide timestamp to onStart function
    * add max_command_frequency, if direction commands (setTargetCoordinates
      and loiter) are send more often than max frenquency the drone crashes

--- bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_fixedwingdrone_js.js
+++ bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_fixedwingdrone_js.js
@@ -92,6 +92,10 @@ var FixedWingDroneAPI = /** @class */ (function () {
     if (drone._maxClimbRate > drone._maxSpeed) {
       throw new Error('max climb rate cannot be superior to max speed');
     }
+    drone._maxCommandFrequency = this.getMaxCommandFrequency();
+    if (drone._maxCommandFrequency <= 0) {
+      throw new Error('max command frequence must be superior to 0');
+    }
     return;
   };
   /*
@@ -407,6 +411,9 @@ var FixedWingDroneAPI = /** @class */ (function () {
   };
   FixedWingDroneAPI.prototype.getMaxClimbRate = function () {
     return this._flight_parameters.drone.maxClimbRate;
+  };
+  FixedWingDroneAPI.prototype.getMaxCommandFrequency = function () {
+    return this._flight_parameters.drone.maxCommandFrequency;
   };
   FixedWingDroneAPI.prototype.getYawVelocity = function (drone) {
     return 360 * EARTH_GRAVITY
