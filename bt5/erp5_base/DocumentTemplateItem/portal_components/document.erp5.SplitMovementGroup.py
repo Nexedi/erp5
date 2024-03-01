0ef0333ee2ea9bc728bdd2f774ae54d2b606cf75
Rafael Monnerat <rafael@nexedi.com>
2024-03-01 01:22:19 +0100

    erp5_base: Update comment
      Since the test() method was fixed the comment became irrelevant

--- bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.SplitMovementGroup.py
+++ bt5/erp5_base/DocumentTemplateItem/portal_components/document.erp5.SplitMovementGroup.py
@@ -33,10 +33,6 @@ class SplitMovementGroup(MovementGroup):
   This movement group is used to split all the movements that are aggregated
   by the Simulation Select Method.
 
-  XXX-Tatuya: However this test() method returns True, so the aggregated
-  movements can be inserted into existing Delivery/Line/Cell that are aggregated
-  by the Delivery Select Method. What use case this is applied for?
-
   * Reference:
   http://www.erp5.org/HowToConfigureMovementGroup
 
