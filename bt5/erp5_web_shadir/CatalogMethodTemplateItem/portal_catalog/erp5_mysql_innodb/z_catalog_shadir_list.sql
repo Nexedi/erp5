8b7e104dcf6f35478e4115d3bca0ed723a530157
Julien Muchembled <jm@nexedi.com>
2024-03-05 20:36:43 +0100

    fixup! erp5_web_shadir: index shadir entries in catalog
    Unit test written by Rafael Monnerat.
    
    See commit 3d7d6d5e40dc0fbf20e67876c89de682b415632d.

--- bt5/erp5_web_shadir/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_shadir_list.sql
+++ bt5/erp5_web_shadir/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_shadir_list.sql
@@ -6,13 +6,13 @@ WHERE
 <dtml-var sql_delimiter>
 
 <dtml-let
-    a2b_hex="__import__('binascii').a2b_hex"
     loads="__import__('json').loads"
+    tostr="lambda x: x.encode('utf-8') if type(x) is unicode else x"
     row_list="[]">
   <dtml-in prefix="loop" expr="_.range(_.len(uid))">
-    <dtml-if "getFollowUp[loop_item]">
+    <dtml-if "getFollowUp[loop_item] and getContentType[loop_item] == 'application/json'">
       <dtml-let data="loads(loads(getData[loop_item])[0])">
-        <dtml-call expr="row_list.append((uid[loop_item], a2b_hex(data['sha512']), data.get('url')))">
+        <dtml-call expr="row_list.append((uid[loop_item], data['sha512'], getFilename[loop_item] or None, ';'.join(['%s=%r' % (x, tostr(data[x])) for x in ('file', 'requirement', 'revision', 'software_url', 'os', 'url') if data.get(x)]) or None))">
       </dtml-let>
     </dtml-if>
   </dtml-in>
@@ -23,8 +23,9 @@ VALUES
     <dtml-in row_list prefix="row">
 (
   <dtml-sqlvar expr="row_item[0]" type="int">,
-  <dtml-sqlvar expr="row_item[1]" type="string">,
-  <dtml-sqlvar expr="row_item[2]" type="string">
+  x'<dtml-var expr="row_item[1]">',
+  <dtml-sqlvar expr="row_item[2]" type="string" optional>,
+  <dtml-sqlvar expr="row_item[3]" type="string" optional>
 )
 <dtml-if sequence-end><dtml-else>,</dtml-if>
     </dtml-in>
--- bt5/erp5_web_shadir/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_shadir.sql
+++ bt5/erp5_web_shadir/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_shadir.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `shadir` (
   `uid` BIGINT UNSIGNED PRIMARY KEY,
   `sha512` BINARY(64) NOT NULL,
-  `url` TEXT,
+  `filename` TINYTEXT,
+  `summary` TEXT,
   KEY(`sha512`)
 ) ENGINE=InnoDB;
