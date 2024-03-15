Nexedi/erp5

commit 2f7689d1f20b348f446b2c1b357769788f44dd81

erp5_officejs_drone_simulator: add max_command_frequency
See merge request !1894

* set _game_duration as an unix epoch
* provide timestamp to onStart function
* add max_command_frequency, if direction commands (setTargetCoordinates
  and loiter) are send more often than max frenquency the drone crashes

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.js
@@ -25,6 +25,8 @@ var DroneManager = /** @class */ (function () {
     this._maxRollAngle = 0;
     this._maxSinkRate = 0;
     this._maxClimbRate = 0;
+    this._maxCommandFrequency = 0;
+    this._last_command_timestamp = 0;
     this._speed = 0;
     this._acceleration = 0;
     this._direction = new BABYLON.Vector3(0, 0, 1); // North
@@ -138,7 +140,7 @@ var DroneManager = /** @class */ (function () {
     this._canPlay = true;
     this._canCommunicate = true;
     try {
-      return this.onStart();
+      return this.onStart(this._API._gameManager._game_duration);
     } catch (error) {
       console.warn('Drone crashed on start due to error:', error);
       this._internal_crash(error);
@@ -156,16 +158,22 @@ var DroneManager = /** @class */ (function () {
       if (!this._canPlay || !this.isReadyToFly()) {
         return;
       }
+      if (this._API._gameManager._game_duration - this._last_command_timestamp
+            < 1000 / this._API.getMaxCommandFrequency()) {
+        this._internal_crash(new Error('Minimum interval between commands is ' +
+            1000 / this._API.getMaxCommandFrequency() + ' milliseconds'));
+      }
       //each drone API process coordinates on its needs
       //e.g. fixedwing drone converts real geo-coordinates to virtual x-y
       this._targetCoordinates =
         this._API.processCoordinates(latitude, longitude, altitude);
-      return this._API.internal_setTargetCoordinates(
+      this._API.internal_setTargetCoordinates(
         this,
         this._targetCoordinates,
         speed,
         radius
       );
+      this._last_command_timestamp = this._API._gameManager._game_duration;
     };
   /**
    * Returns the list of things a drone "sees"
@@ -373,7 +381,7 @@ var DroneManager = /** @class */ (function () {
   /**
    * Function called on game start
    */
-  DroneManager.prototype.onStart = function () { return; };
+  DroneManager.prototype.onStart = function (timestamp) { return; };
   /**
    * Function called on game update
    * @param timestamp The tic value
@@ -1219,8 +1227,8 @@ var GameManager = /** @class */ (function () {
     _this.ongoing_update_promise = null;
     _this.finish_deferred = RSVP.defer();
     console.log("Simulation started.");
-    this._game_duration = 0;
-    this._totalTime = GAMEPARAMETERS.gameTime;
+    this._game_duration = Date.now();
+    this._totalTime = GAMEPARAMETERS.gameTime + this._game_duration;
 
     return new RSVP.Queue()
       .push(function () {

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.xml
@@ -246,7 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.56745.33504.26197</string> </value>
+                      <value> <string>1014.60733.7318.44953</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -266,7 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1709223446.72</float>
+                              <float>1709546292.39</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.js
@@ -25,6 +25,8 @@ var DroneManager = /** @class */ (function () {
     this._maxRollAngle = 0;
     this._maxSinkRate = 0;
     this._maxClimbRate = 0;
+    this._maxCommandFrequency = 0;
+    this._last_command_timestamp = 0;
     this._speed = 0;
     this._acceleration = 0;
     this._direction = new BABYLON.Vector3(0, 0, 1); // North
@@ -124,7 +126,7 @@ var DroneManager = /** @class */ (function () {
     this._canCommunicate = true;
     this._targetCoordinates = initial_position;
     try {
-      return this.onStart();
+      return this.onStart(this._API._gameManager._game_duration);
     } catch (error) {
       console.warn('Drone crashed on start due to error:', error);
       this._internal_crash(error);
@@ -142,15 +144,21 @@ var DroneManager = /** @class */ (function () {
       if (!this._canPlay || !this.isReadyToFly()) {
         return;
       }
+      if (this._API._gameManager._game_duration - this._last_command_timestamp
+            < 1000 / this._API.getMaxCommandFrequency()) {
+        this._internal_crash(new Error('Minimum interval between commands is ' +
+            1000 / this._API.getMaxCommandFrequency() + ' milliseconds'));
+      }
       //convert real geo-coordinates to virtual x-y coordinates
       this._targetCoordinates =
         this._API.processCoordinates(latitude, longitude, altitude);
-      return this._API.internal_setTargetCoordinates(
+      this._API.internal_setTargetCoordinates(
         this,
         this._targetCoordinates,
         speed,
         radius
       );
+      this._last_command_timestamp = this._API._gameManager._game_duration;
     };
   DroneManager.prototype.internal_update = function (delta_time) {
     var context = this;
@@ -328,7 +336,7 @@ var DroneManager = /** @class */ (function () {
   /**
    * Function called on game start
    */
-  DroneManager.prototype.onStart = function () { return; };
+  DroneManager.prototype.onStart = function (timestamp) { return; };
   /**
    * Function called on game update
    * @param timestamp The tic value
@@ -895,8 +903,8 @@ var GameManager = /** @class */ (function () {
     _this.ongoing_update_promise = null;
     _this.finish_deferred = RSVP.defer();
     console.log("Simulation started.");
-    this._game_duration = 0;
-    this._totalTime = GAMEPARAMETERS.gameTime;
+    this._game_duration = Date.now();
+    this._totalTime = GAMEPARAMETERS.gameTime + this._game_duration;
 
     return new RSVP.Queue()
       .push(function () {

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.xml
@@ -240,7 +240,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.12072.47950.5358</string> </value>
+                      <value> <string>1014.60716.53618.32563</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -260,7 +260,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1706542965.82</float>
+                              <float>1709288704.17</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_enemydrone_js.js
@@ -241,7 +241,7 @@ var EnemyDroneAPI = /** @class */ (function () {
       '  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2);\n' +
       '}\n' +
       '\n' +
-      'me.onStart = function () {\n' +
+      'me.onStart = function (timestamp) {\n' +
       '  me.setDirection(0,0,0);\n' +
       '  return;\n' +
       '\n' +
@@ -279,6 +279,9 @@ var EnemyDroneAPI = /** @class */ (function () {
   };
   EnemyDroneAPI.prototype.getMaxAcceleration = function () {
     return this._flight_parameters.drone.maxAcceleration;
+  };
+  EnemyDroneAPI.prototype.getMaxCommandFrequency = function () {
+    return Infinity;
   };
   EnemyDroneAPI.prototype.land = function (drone) {
     var drone_pos = drone.getCurrentPosition();

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_enemydrone_js.xml
@@ -246,7 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.56714.1017.16076</string> </value>
+                      <value> <string>1014.60631.26636.59528</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -266,7 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1709221104.19</float>
+                              <float>1709288499.16</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_fixedwingdrone_js.js
@@ -88,6 +88,10 @@ var FixedWingDroneAPI = /** @class */ (function () {
     if (drone._maxClimbRate > drone._maxSpeed) {
       throw new Error('max climb rate cannot be superior to max speed');
     }
+    drone._maxCommandFrequency = this.getMaxCommandFrequency();
+    if (drone._maxCommandFrequency <= 0) {
+      throw new Error('max command frequence must be superior to 0');
+    }
     return;
   };
   /*
@@ -449,6 +453,9 @@ var FixedWingDroneAPI = /** @class */ (function () {
   };
   FixedWingDroneAPI.prototype.getMaxClimbRate = function () {
     return this._flight_parameters.drone.maxClimbRate;
+  };
+  FixedWingDroneAPI.prototype.getMaxCommandFrequency = function () {
+    return this._flight_parameters.drone.maxCommandFrequency;
   };
   FixedWingDroneAPI.prototype.getYawVelocity = function (drone) {
     return 360 * EARTH_GRAVITY *

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_fixedwingdrone_js.xml
@@ -246,7 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.56746.48697.2713</string> </value>
+                      <value> <string>1014.60714.57448.36454</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -266,7 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1709223091.4</float>
+                              <float>1709288420.35</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

commit 59a85a737798aaaf931ab999fbb60a8e7940c5bc

erp5_officejs_drone_simulator: fix timeout
Timeout feature was broken by 2f7689d1f20b348f446b2c1b357769788f44dd81 .

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.js
@@ -1043,8 +1043,7 @@ var GameManager = /** @class */ (function () {
     };
 
   GameManager.prototype._timeOut = function () {
-    var seconds = Math.floor(this._game_duration / 1000);
-    return this._totalTime - seconds <= 0;
+    return this._totalTime - this._game_duration <= 0;
   };
 
   GameManager.prototype._allDronesFinished = function () {
@@ -1227,7 +1226,7 @@ var GameManager = /** @class */ (function () {
     _this.finish_deferred = RSVP.defer();
     console.log("Simulation started.");
     this._game_duration = Date.now();
-    this._totalTime = GAMEPARAMETERS.gameTime + this._game_duration;
+    this._totalTime = GAMEPARAMETERS.gameTime * 1000 + this._game_duration;
 
     return new RSVP.Queue()
       .push(function () {

bt5/erp5_officejs_drone_capture_flag/PathTemplateItem/web_page_module/drone_capture_flag_logic_js.xml
@@ -246,7 +246,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.65026.25145.27272</string> </value>
+                      <value> <string>1015.15557.49327.13158</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -266,7 +266,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1709560433.33</float>
+                              <float>1710511088.86</float>
                               <string>UTC</string>
                             </tuple>
                           </state>

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.js
@@ -763,8 +763,7 @@ var GameManager = /** @class */ (function () {
     };
 
   GameManager.prototype._timeOut = function () {
-    var seconds = Math.floor(this._game_duration / 1000);
-    return this._totalTime - seconds <= 0;
+    return this._totalTime - this._game_duration <= 0;
   };
 
   GameManager.prototype._allDronesFinished = function () {
@@ -904,7 +903,7 @@ var GameManager = /** @class */ (function () {
     _this.finish_deferred = RSVP.defer();
     console.log("Simulation started.");
     this._game_duration = Date.now();
-    this._totalTime = GAMEPARAMETERS.gameTime + this._game_duration;
+    this._totalTime = GAMEPARAMETERS.gameTime * 1000 + this._game_duration;
 
     return new RSVP.Queue()
       .push(function () {

bt5/erp5_officejs_drone_simulator/PathTemplateItem/web_page_module/drone_simulator_logic_js.xml
@@ -240,7 +240,7 @@
                   </item>
                   <item>
                       <key> <string>serial</string> </key>
-                      <value> <string>1014.65027.38618.31573</string> </value>
+                      <value> <string>1015.15533.18317.53657</string> </value>
                   </item>
                   <item>
                       <key> <string>state</string> </key>
@@ -260,7 +260,7 @@
                           </tuple>
                           <state>
                             <tuple>
-                              <float>1709560536.57</float>
+                              <float>1710509638.96</float>
                               <string>UTC</string>
                             </tuple>
                           </state>