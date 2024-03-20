cff9daa31dc4cdb56cb8c1671612934d805b15b0
Rafael Monnerat <rafael@nexedi.com>
2024-03-20 19:40:26 +0000

    erp5_maileva_connector: Use script to not hardcore dependence
       Otherwise, erp5_payroll_l10n_fr would be a direct dependent of this service, which is not what we want.
    
       It also allows implement other kind of document sending w/o hardcode the core or overwriting too much.

---
+++ bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/Base_isMailevaEnabled.py
@@ -0,0 +1,6 @@
+"""
+  Consider always enabled, and let the projects define the logic 
+  to enable/disable this feature by overwriting this script.
+"""
+
+return True
---
+++ bt5/erp5_payroll_l10n_fr/SkinTemplateItem/portal_skins/erp5_payroll_l10n_fr/Base_isMailevaEnabled.py
@@ -0,0 +1,4 @@
+""" This script should be overwrited by erp5_maileva_connector
+   business template. 
+"""
+return False
