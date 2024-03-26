52cf97b953dfac69b7154e6e69ccf3d72e9085a3
Roque Porchetto <roque.porchetto@nexedi.com>
2024-03-26 23:11:08 +0000

    [WIP][DEBUG] erp5_web_monitoring: fix jio storage

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/monitoring_jio_storage_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/monitoring_jio_storage_js.js
@@ -154,11 +154,16 @@
     var storage = this._local_sub_storage;
     return storage.get(id)
       .push(function (doc) {
+        console.log("monitoring jio storage REMOVE. id:", id);
+        console.log("with portal_type:", doc.portal_type);
         if (doc.portal_type !== 'opml') {
-          throw new TypeError("Object with portal_type" + doc.portal_type +
-                              "is frozen, cannot remove it.");
+          console.log("portal_type !== 'opml'! just jio remove");
+          return storage.remove(id);
+          /*throw new TypeError("Object with portal_type" + doc.portal_type +
+                              "is frozen, cannot remove it.");*/
         }
         function removeOPMLTree(url) {
+          console.log("portal_type 'opml'! complex opml remove");
           var remove_id_list = [],
             remove_signature_id_list = [];
 
