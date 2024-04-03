baadac86ea030546dfc277f2173c310c0ef9bf0a
Jérome Perrin <jerome@nexedi.com>
2024-04-03 19:56:11 +0900

    fixup! fixup! web_renderjs_ui: py3

--- bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/MailHost_redirectPageToURLFromLastMessage.py
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/MailHost_redirectPageToURLFromLastMessage.py
@@ -3,4 +3,4 @@ from Products.ERP5Type.Utils import bytes2str
 last_message_text = bytes2str(context.getMessageList()[-1][2])
 __traceback_info__ = last_message_text
 
-container.REQUEST.RESPONSE.redirect(re.findall(r"http:.*", bytes2str(last_message_text))[0])
+return container.REQUEST.RESPONSE.redirect(re.findall(r"http:.*", last_message_text)[0])
