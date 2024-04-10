5ea407ba3dbf42225df3894d74b7dc5533cec2e8
Jérome Perrin <jerome@nexedi.com>
2024-04-10 10:10:27 +0200

    oauth_google_login: reimplement with oauthlib/requests
    Because the librairies used here were never ported to python3.
    
    Notable changes:
    
      - ERP5Site_createGoogleUserToOAuth is dropped
      - internal API changed radically, so customizations made by overriding
        scripts are broken.
      - the core logic is now implemented in a connector class (still in
        portal_oauth for simplicity, but it would be simpler to move it to
        portal_web_services)
    
    No changes required in the google console, the redirect uri is still
    ERP5Site_receiveGoogleCallback

--- bt5/erp5_oauth_google_login/SkinTemplateItem/portal_skins/erp5_oauth_google_login/ERP5Site_receiveGoogleCallback.py
+++ bt5/erp5_oauth_google_login/SkinTemplateItem/portal_skins/erp5_oauth_google_login/ERP5Site_receiveGoogleCallback.py
@@ -1,4 +1,5 @@
-import time
+from Products.ERP5Type.Utils import str2bytes
+portal = context.getPortalObject()
 
 request = container.REQUEST
 response = request.RESPONSE
@@ -16,27 +17,22 @@ if error is not None:
   return handleError(error)
 
 elif code is not None:
-  response_dict = context.ERP5Site_getAccessTokenFromCode(
-    code,
-    "{0}/ERP5Site_receiveGoogleCallback".format(context.absolute_url()))
-  if response_dict is not None:
-    access_token = response_dict['access_token'].encode('utf-8')
-    hash_str = context.Base_getHMAC(access_token, access_token)
-    context.setAuthCookie(response, '__ac_google_hash', hash_str)
-    # store timestamp in second since the epoch in UTC is enough
-    response_dict["response_timestamp"] = time.time()
-    context.Base_setBearerToken(hash_str,
-                                response_dict,
-                                "google_server_auth_token_cache_factory")
-    user_dict = context.ERP5Site_getGoogleUserEntry(access_token)
-    user_reference = user_dict["email"]
-    context.Base_setBearerToken(access_token,
-                                {"reference": user_reference},
-                                "google_server_auth_token_cache_factory")
-    method = getattr(context, "ERP5Site_createGoogleUserToOAuth", None)
-    if method is not None:
-      method(user_reference, user_dict)
-    # XXX for ERP5JS web sites without a rewrite rule, we make sure there's a trailing /
-    return response.redirect(request.get("came_from") or context.absolute_url() + '/')
+  google_connector = portal.ERP5Site_getDefaultGoogleConnector()
+  response_dict = google_connector.getTokenFromCode(
+    state=state,
+    code=code,
+    redirect_uri="{}/ERP5Site_receiveGoogleCallback".format(portal.absolute_url()),
+  )
+
+  access_token = str2bytes(response_dict['access_token'])
+  hash_str = portal.Base_getHMAC(access_token, access_token)
+  portal.setAuthCookie(response, '__ac_google_hash', hash_str)
+  portal.Base_setBearerToken(
+    hash_str,
+    response_dict,
+    "google_server_auth_token_cache_factory")
+
+  # XXX for ERP5JS web sites without a rewrite rule, we make sure there's a trailing /
+  return response.redirect(request.get("came_from") or context.absolute_url() + '/')
 
 return handleError('')
