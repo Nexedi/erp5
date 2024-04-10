ee43a0f4ac5db903d35459df3e9eb5d6d82e58f0
Jérome Perrin <jerome@nexedi.com>
2024-04-10 23:33:16 +0900

    fixup! *: fix python2 compatibility with base64.encodebytes

--- bt5/erp5_smart_assistant/SkinTemplateItem/portal_skins/erp5_smart_assistant/Query_createExpenseRecord.py
+++ bt5/erp5_smart_assistant/SkinTemplateItem/portal_skins/erp5_smart_assistant/Query_createExpenseRecord.py
@@ -1,16 +1,18 @@
 
 import six
+# pylint:disable=no-name-in-module
 if six.PY2:
   from base64 import encodestring as encodebytes
 else:
   from base64 import encodebytes
+# pylint:enable=no-name-in-module
 
 portal = context.getPortalObject()
 expense_record_module = portal.getDefaultModule('Expense Record')
 sender = portal.portal_membership.getAuthenticatedMember().getUserValue()
 data = bytes(context.getData())
-data64 = u''.join(encodebytes(data).decode().splitlines())
-photo_data = u'data:%s;base64,%s' % ("image/*", data64)
+data64 = ''.join(encodebytes(data).decode().splitlines())
+photo_data = 'data:%s;base64,%s' % ("image/*", data64)
 expense_record_module.newContent(
   comment=comment,
   resource_title=currency,
