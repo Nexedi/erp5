0d083d8662b07edefa15e092653494b2f91db519
Roque Porchetto <roque.porchetto@nexedi.com>
2024-04-18 15:39:24 +0000

    erp5_web_monitoring: update options header (instance tree view)

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_options_header_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_options_header_js.js
@@ -7,16 +7,44 @@
     /////////////////////////////////////////////////////////////////
     // Acquired methods
     /////////////////////////////////////////////////////////////////
-    .declareAcquiredMethod("updateHeader", "updateHeader")
     .declareAcquiredMethod("jio_get", "jio_get")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
     /////////////////////////////////////////////////////////////////
 
-    .declareMethod("getOptions", function (argument_list) {
-      console.log("header getOptions...", argument_list);
-      return {'a': 1};
+    .declareMethod("getOptions", function (portal_type_dict, page_options, header_options) {
+      var gadget = this;
+      switch (portal_type_dict.view) {
+      case "instance_tree":
+        return new RSVP.Queue()
+          .push(function () {
+            return gadget.jio_get(page_options.jio_key);
+          })
+          .push(function (instance_tree) {
+            //TODO migrate
+            gadget.getUrlFor({command: 'store_and_change', options: {
+              page: "ojsm_jump",
+              jio_key: instance_tree.opml_url,
+              title: instance_tree.title,
+              view_title: "Related OPML",
+              search_page: "ojsm_status_list"
+            }});
+          })
+          .push(function (url) {
+            //TODO set title here
+            header_options.jump_url = url;
+            header_options.save_action = true;
+            return header_options;
+          });
+      case "software_instance":
+        return {};
+      case "promise":
+        return {};
+      default:
+        return {};
+      }
     });
 
 }(document, window, rJS, RSVP));
