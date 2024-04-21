d4f5f7a0914168a07cd3ed6b97c34146f1ef51f5
Jérome Perrin <jerome@nexedi.com>
2024-04-21 22:26:57 +0900

    test_result: pylint py3

--- bt5/erp5_test_result/ExtensionTemplateItem/portal_components/extension.erp5.TestResults.py
+++ bt5/erp5_test_result/ExtensionTemplateItem/portal_components/extension.erp5.TestResults.py
@@ -301,7 +301,7 @@ def TestResult_sendEmailNotification(self, mail_to=None, mail_from=None,
                     " %s lines" % max_line_count)
       traceback_attachment[max_line_count:] = '', '', '(truncated) ...'
     content = '\n'.join(traceback_attachment)
-    if isinstance(content, unicode):
+    if not isinstance(content, bytes):
       content = content.encode('utf8')
     attachment_list.append(dict(name='traceback.txt',
                                 mime_type='text/plain',
