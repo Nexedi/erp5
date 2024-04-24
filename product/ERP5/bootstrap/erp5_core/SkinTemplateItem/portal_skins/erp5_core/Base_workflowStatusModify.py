a0d9114d0d1d828930b21f0cf09fb0d232358ee0
Jérome Perrin <jerome@nexedi.com>
2024-04-24 23:42:26 +0900

    core: pylint for py3

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_workflowStatusModify.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Base_workflowStatusModify.py
@@ -3,16 +3,15 @@ from Products.CMFCore.WorkflowCore import WorkflowException
 
 o = context.getObject()
 
-if 1: # keep indentation
-  try :
-    context.portal_workflow.doActionFor( o,
-     workflow_action,
-     comment=comment,
-     **kw)
-  except WorkflowException:
-    pass
-  except ValidationFailed as message:
-    if getattr(message, 'msg', None) and same_type(message.msg, []):
-      message = '. '.join('%s' % x for x in message.msg)
-    if not batch :
-      return context.Base_redirect(keep_items={'portal_status_message': str(message)})
+try :
+  context.portal_workflow.doActionFor( o,
+    workflow_action,
+    comment=comment,
+    **kw)
+except WorkflowException:
+  pass
+except ValidationFailed as message:
+  if getattr(message, 'msg', None) and same_type(message.msg, []):
+    message = '. '.join('%s' % x for x in message.msg)
+  if not batch :
+    return context.Base_redirect(keep_items={'portal_status_message': str(message)})
