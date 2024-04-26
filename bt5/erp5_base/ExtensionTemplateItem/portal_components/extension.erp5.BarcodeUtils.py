a6330464d2549d043d72c7c0ae2cf52fd6e3ab10
Jérome Perrin <jerome@nexedi.com>
2024-04-26 07:22:20 +0200

    base: python3 support for Base_generateBarcodeImage
    also add the missing tests

--- bt5/erp5_base/ExtensionTemplateItem/portal_components/extension.erp5.BarcodeUtils.py
+++ bt5/erp5_base/ExtensionTemplateItem/portal_components/extension.erp5.BarcodeUtils.py
@@ -1,7 +1,10 @@
+import io
+import six
+from Products.ERP5Type.Utils import str2bytes
+
+# pylint:disable=import-error
 def generateBarcodeImage(self, barcode_type, data, REQUEST=None):
-  # huBarcode's DataMatrix support has limitation for data size.
-  # huBarcode's QRCode support is broken.
-  # more 1-D barcode types can be added by pyBarcode library.
+  # type: (str, str, HTTPRequest) -> bytes
   barcode_type = barcode_type.lower()
   if barcode_type == 'datamatrix':
     from subprocess import Popen, PIPE
@@ -10,26 +13,51 @@ def generateBarcodeImage(self, barcode_type, data, REQUEST=None):
                      stdout=PIPE,
                      stderr=PIPE,
                      close_fds=True)
-    output, _ = process.communicate(input=data)
+    output, _ = process.communicate(input=str2bytes(data))
   elif barcode_type == 'ean13':
-    from hubarcode.ean13 import EAN13Encoder
-    encoder = EAN13Encoder(data)
-    output = encoder.get_imagedata()
+    if six.PY3:
+      import barcode.ean
+      import barcode.writer
+      fp = io.BytesIO()
+      barcode.ean.EuropeanArticleNumber13(
+        data, writer=barcode.writer.ImageWriter()
+      ).render().save(fp, format='png')
+      output = fp.getvalue()
+    else:
+      from hubarcode.ean13 import EAN13Encoder
+      encoder = EAN13Encoder(data)
+      output = encoder.get_imagedata()
   elif barcode_type == 'code128':
-    from hubarcode.code128 import Code128Encoder
-    encoder = Code128Encoder(data)
-    encoder.text = '' # get barcode image only
-    output = encoder.get_imagedata()
+    if six.PY3:
+      import barcode.codex
+      import barcode.writer
+      class NoTextImageWriter(barcode.writer.ImageWriter):
+        def _paint_text(self, *args):
+          pass
+      fp = io.BytesIO()
+      barcode.codex.Code128(
+        data, writer=NoTextImageWriter()
+      ).render(
+        # we also set a font_size of 0, so that the bounding box of the
+        # image does not include white space for the text.
+        writer_options={'font_size': 0}
+      ).save(fp, format='png')
+      output = fp.getvalue()
+    else:
+      from hubarcode.code128 import Code128Encoder
+      encoder = Code128Encoder(data)
+      encoder.text = '' # get barcode image only
+      output = encoder.get_imagedata()
   elif barcode_type == 'qrcode':
     import qrcode
-    from cStringIO import StringIO
-    fp = StringIO()
-    img = qrcode.make(data)
+    fp = io.BytesIO()
+    img = qrcode.make(str2bytes(data))
     img.save(fp, 'png')
     fp.seek(0)
-    output = fp.read()
+    output = fp.getvalue()
   else:
     raise NotImplementedError('barcode_type=%s is not supported' % barcode_type)
   if REQUEST is not None:
     REQUEST.RESPONSE.setHeader('Content-Type', 'image/png')
   return output
+# pylint:enable=import-error
