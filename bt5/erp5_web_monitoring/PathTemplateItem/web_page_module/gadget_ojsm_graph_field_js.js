2183a5f5590618c32e78ac1b19fc11922daa823d
Roque Porchetto <roque.porchetto@nexedi.com>
2024-04-29 14:52:38 +0000

    erp5_web_monitoring: update app router configuration - custom submit gadget - update precache manifest - update software instance graph - graph gadget field to software instance view form

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_ojsm_graph_field_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_ojsm_graph_field_js.js
@@ -12,16 +12,102 @@
 
   rJS(window)
     .declareAcquiredMethod("notifySubmitted", 'notifySubmitted')
+    .declareAcquiredMethod("jio_get", 'jio_get')
+
     .declareMethod('render', function (options) {
-      var field_json = options || {},
-        state_dict = {
-          data_dict: field_json.value.data_dict,
-          data_url: field_json.value.data_url,
-          data_filename: field_json.value.data_filename,
-          extract_method: field_json.value.extract_method,
-          basic_login: field_json.value.basic_login
-        };
-      return this.changeState(state_dict);
+      var gadget = this, graph_data, instance = options, opml;
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.jio_get(instance.parent_id);
+        })
+        .push(function (opml_outline) {
+          return gadget.jio_get(opml_outline.parent_url);
+        })
+        .push(function (opml_doc) {
+          opml = opml_doc;
+          var graph_options = {
+            data_url: "",
+            data_filename: "monitor_state.data",
+            basic_login: ""
+          };
+
+          if (instance._links !== undefined) {
+            graph_options  = {
+              data_url: instance._links.private_url.href +
+                'documents/',
+              data_filename: instance.data.state,
+              basic_login: opml.basic_login
+            };
+          }
+
+          graph_options.extract_method = function (element_dict) {
+            var promise_data = [
+                "Date, Success, Error",
+                new Date() + ",0,0"
+              ],
+              data = element_dict.data || promise_data,
+              data_list = [],
+              line_list,
+              i;
+
+            data_list.push({
+              value_dict: {"0": [], "1": []},
+              type: "scatter",
+              axis_mapping_id_dict: {"1": "1_1"},
+              title: "promises success"
+            });
+            data_list.push({
+              value_dict: {"0": [], "1": []},
+              type: "scatter",
+              axis_mapping_id_dict: {"1": "1_2"},
+              title: "promises error"
+            });
+            for (i = 1; i < data.length; i += 1) {
+              line_list = data[i].split(',');
+              data_list[0].value_dict["0"].push(line_list[0]);
+              data_list[0].value_dict["1"].push(line_list[1]);
+
+              // XXX repeating date entry
+              data_list[1].value_dict["0"].push(line_list[0]);
+              data_list[1].value_dict["1"].push(line_list[2]);
+            }
+            return data_list;
+          };
+          graph_options.data_dict = {
+            data: {},
+            layout: {
+              axis_dict : {
+                "0": {
+                  "title": "Promises Failure Progression",
+                  "scale_type": "linear",
+                  "value_type": "date"
+                },
+                "1_1": {
+                  "title": "Promises success",
+                  "position": "right"
+                },
+                "1_2": {
+                  "title": "Promises error",
+                  "position": "right"
+                }
+              },
+              title: "Promises Failure Progression"
+            }
+          };
+          return graph_options;
+        })
+        .push(function (g) {
+          graph_data = { value: g };
+          var field_json = graph_data,
+            state_dict = {
+              data_dict: field_json.value.data_dict,
+              data_url: field_json.value.data_url,
+              data_filename: field_json.value.data_filename,
+              extract_method: field_json.value.extract_method,
+              basic_login: field_json.value.basic_login
+            };
+          return gadget.changeState(state_dict);
+        });
     })
 
     .onStateChange(function () {
