ecb10c279a581f2f104febf122f2d95fedca6549
Romain Courteaud <romain@nexedi.com>
2024-05-07 07:54:33 +0000

    erp5_credential: add assignment request
    This is a new module usefull to manage the assignments, without having write permission on a person document.

---
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/Assignment_createMissingAssignmentRequest.py
@@ -0,0 +1,40 @@
+portal = context.getPortalObject()
+assignment = context
+
+if assignment.getValidationState() != 'open':
+  # No need to create anything in such case
+  # Skip
+  return
+
+assert assignment.getDestination(None) is None, assignment.getDestination(None)
+
+
+search_kw = {}
+for category in assignment.getCategoryList():
+  base_category, _ = category.split('/', 1)
+  search_kw['strict__%s__uid' % base_category] = assignment.getValueUidList(base_category)
+search_kw['strict__destination__uid'] = assignment.getParentValue().getUid()
+
+assignment_request = portal.portal_catalog.getResultValue(
+  portal_type='Assignment Request',
+  simulation_state=['submitted', 'validated', 'suspended'],
+  **search_kw
+)
+if assignment_request is not None:
+  return
+
+assignment_request = portal.assignment_request_module.newContent(
+  portal_type='Assignment Request',
+  title=assignment.getTitle(),
+  category_list=assignment.getCategoryList(),
+  activate_kw=activate_kw
+)
+assert assignment_request.getDestination(None) is None, assignment_request.getDestination(None)
+assignment_request.edit(
+  destination_value=assignment.getParentValue(),
+  activate_kw=activate_kw
+)
+
+assignment_request.submit()
+assignment_request.validate()
+return assignment_request
