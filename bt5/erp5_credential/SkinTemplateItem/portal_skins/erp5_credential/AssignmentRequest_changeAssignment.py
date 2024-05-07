3b77b933df3efab699a8450b40d3c2fd10a9fadc
Romain Courteaud <romain@nexedi.com>
2024-05-07 07:54:33 +0000

    erp5_credential: add assignment request
    This is a new module usefull to manage the assignments, without having write permission on a person document.

---
+++ bt5/erp5_credential/SkinTemplateItem/portal_skins/erp5_credential/AssignmentRequest_changeAssignment.py
@@ -0,0 +1,45 @@
+assignment_request = context
+portal = context.getPortalObject()
+assignment = None
+person = assignment_request.getDestinationValue(portal_type='Person')
+
+if person is None:
+  raise ValueError('No person document found')
+
+search_kw = {}
+for category in assignment_request.getCategoryList():
+  base_category, _ = category.split('/', 1)
+  search_kw['strict__%s__uid' % base_category] = assignment_request.getValueUidList(base_category)
+search_kw.pop('strict__destination__uid')
+
+assignment_list = portal.portal_catalog(
+  portal_type='Assignment',
+  parent_uid=person.getUid(),
+  validation_state='open',
+  **search_kw
+)
+
+assignment_request_state = assignment_request.getSimulationState()
+if assignment_request_state == 'submitted':
+  if 0 < len(assignment_list):
+    assignment_request.validate(comment='Assignment already exists (%s).' % assignment_list[0].getRelativeUrl())
+    assignment_request.invalidate(comment='Assignment already exists (%s).' % assignment_list[0].getRelativeUrl())
+  else:
+    category_list = [x for x in assignment_request.getCategoryList() if not x.startswith('destination/')]
+    assignment = person.newContent(
+      portal_type='Assignment',
+      title=assignment_request.getTitle(),
+      category_list=category_list,
+      activate_kw=activate_kw
+    )
+    assignment.open(comment='Assignment created by %s.' % assignment_request.getRelativeUrl())
+    # create a new assignment for the user
+    assignment_request.validate(comment='Assignment created (%s).' % assignment.getRelativeUrl())
+
+elif assignment_request_state == 'suspended':
+  for assignment in assignment_list:
+    assignment.close('Closed by the assignment request (%s)' % assignment_request.getRelativeUrl())
+  assignment_request.invalidate(comment='Assignment closed.')
+
+assignment_request.reindexObject(activate_kw=activate_kw)
+return assignment
