729c454c4560b5380b57158f503dd8161c1a8ddd
Roque Porchetto <roque.porchetto@nexedi.com>
2024-05-29 14:13:50 +0000

    erp5_web_monitoring: extend slapos master url storages from 1 to many - update queries, views, forms to include slapos-master-url - update opml portal type - minor graph refactoring - refactor multiple erp5 storages - drop obsolete gadgets - fix and extend sync redirect options

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_erp5_page_ojsm_dispatch_js.js
@@ -10,13 +10,14 @@
     /////////////////////////////////////////////////////////////////
     .declareAcquiredMethod("redirect", "redirect")
     .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("setSetting", "setSetting")
     .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
 
     /////////////////////////////////////////////////////////////////
     // declared methods
     /////////////////////////////////////////////////////////////////
     .declareMethod("render", function (options) {
-      var gadget = this, portal_type, extended_search, query_parts_list;
+      var gadget = this, portal_type, extended_search, query_parts_list, query_redirect;
       if (options.query && options.url && options.username && options.password) {
         return new RSVP.Queue()
           .push(function () {
@@ -25,33 +26,40 @@
             });
           })
         .push(function (result) {
+          // URL queries from slapos master / panel rapid space:
+          // 'portal_type: "Instance Tree" AND title:"my-title"
+          //           AND slapos_master_url:"url"'
+          // or
+          // 'portal_type: "Software Instance" AND title:"my-title"
+          //           AND specialise_title:"my-parent-title"
+          //           AND slapos_master_url:"url"'
+          query_parts_list = options.query.split('AND');
+          portal_type = query_parts_list[0].replace('portal_type:', '').replaceAll('"', '').trim();
+          extended_search = options.query.replace(query_parts_list[0] + 'AND ', '').trim();
+          query_redirect = {
+            "page": "ojs_local_controller",
+            "portal_type": portal_type + " Module",
+            "extended_search": extended_search
+          };
           if (result === undefined || result.data.total_rows === 0) {
-            return gadget.redirect(
-              {"command": "display",
-               "options": {
-                 "url": options.url,
-                 "username": options.username,
-                 "password": options.password,
-                 "page": "ojsm_opml_add"
-               }
+            return gadget.setSetting('sync_redirect_options', query_redirect)
+              .push(function (result) {
+                return gadget.redirect(
+                  {"command": "display",
+                   "options": {
+                     "url": options.url,
+                     "username": options.username,
+                     "password": options.password,
+                     "slapos_master_url": options.slapos_master_url || "not-provided",
+                     "page": "ojsm_opml_add"
+                   }
+                  });
               });
           } else {
-            // URL queries from slapos master / panel rapid space:
-            // 'portal_type: "Instance Tree" AND title:"my-title"'
-            // or
-            // 'portal_type: "Software Instance" AND title:"my-title"
-            //           AND specialise_title:"my-parent-title"'
-            query_parts_list = options.query.split('AND');
-            portal_type = query_parts_list[0].replace('portal_type:', '').replaceAll('"', '').trim();
-            extended_search = options.query.replace(query_parts_list[0] + 'AND ', '').trim();
-            return gadget.redirect(
-              {"command": "display",
-               "options": {
-                "page": "ojs_local_controller",
-                "portal_type": portal_type + " Module",
-                "extended_search": extended_search
-              }
-              });
+            return gadget.redirect({
+              "command": "display",
+              "options": query_redirect
+            });
           }
         });
       } else if (options.url && options.username && options.password) {
@@ -61,6 +69,7 @@
              "url": options.url,
              "username": options.username,
              "password": options.password,
+             "slapos_master_url": options.slapos_master_url || "not-provided",
              "page": "ojsm_opml_add"
            }
           });
