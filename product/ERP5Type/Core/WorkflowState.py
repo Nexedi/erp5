16ab007c5e9c1d6edc9aa58356b96c02868afc58
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2024-06-01 13:25:37 +0200

    fixup! ERP5Workflow: DC Workflows are now ERP5 objects (!1378).
    if acquire_permission is not yet set in a Workflow State, we should consider it as 'acquire all permissions', like DCWorkflow implementation.

--- product/ERP5Type/Core/WorkflowState.py
+++ product/ERP5Type/Core/WorkflowState.py
@@ -95,6 +95,17 @@ class WorkflowState(IdAsReferenceMixin("state_"),
     return [parent._getOb(destination_id) for destination_id in
             self.getDestinationIdList()]
 
+  security.declareProtected(Permissions.AccessContentsInformation,
+                            'getAcquirePermissionList')
+  def getAcquirePermissionList(self):
+    """
+    acquire all permissions if not yet configured, like DCWorkflow
+    """
+    if not self.state_permission_role_list_dict:
+      return self.getWorkflowManagedPermissionList()
+    else:
+      return self._baseGetAcquirePermissionList()
+
   security.declareProtected(Permissions.ModifyPortalContent,
                             'setStatePermissionRoleListDict')
   def setStatePermissionRoleListDict(self, permission_roles):
