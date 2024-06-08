c4f04b64a4bb5fa4d3dc47c9dc3c62da0f737c9b
Jérome Perrin <jerome@nexedi.com>
2024-06-08 23:32:31 +0900

    py3: fix return type of DummyMailHostMixin.getMessageList
    mail body is str

--- bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/MailHost_redirectPageToURLFromLastMessage.py
+++ bt5/erp5_web_renderjs_ui_test/SkinTemplateItem/portal_skins/erp5_web_renderjs_ui_test/MailHost_redirectPageToURLFromLastMessage.py
@@ -1,6 +1,3 @@
 import re
-from Products.ERP5Type.Utils import bytes2str
-last_message_text = bytes2str(context.getMessageList()[-1][2])
-__traceback_info__ = last_message_text
-
+last_message_text = context.getMessageList()[-1][2]
 return container.REQUEST.RESPONSE.redirect(re.findall(r"http.*", last_message_text)[0])
