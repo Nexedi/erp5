1416eaa3beddebedcd29b5ff1650d6a8e90f1a30
Klaus Wölfel <klaus@nexedi.com>
2024-07-02 15:11:13 +0200

    erp5_pdm: do not show hidden (deleted) lines in movement history list

--- bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_getReversedMovementHistoryList.py
+++ bt5/erp5_pdm/SkinTemplateItem/portal_skins/erp5_pdm/Resource_getReversedMovementHistoryList.py
@@ -1,5 +1,10 @@
 history_list = context.getMovementHistoryList(**kw)
 reverse_list = []
 for x in history_list:
+  try:
+    if x.getVisibilityState() == "hidden":
+      continue
+  except AttributeError:
+    pass
   reverse_list.insert(0, x)
 return reverse_list
