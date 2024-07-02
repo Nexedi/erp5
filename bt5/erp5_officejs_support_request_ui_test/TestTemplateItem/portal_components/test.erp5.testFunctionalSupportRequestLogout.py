8b88c0865effec3a375328c7d7fa7b85f10c5252
Romain Courteaud <romain@nexedi.com>
2024-07-02 09:02:46 +0000

    erp5_officejs_support_request_ui_test: move logout zuite to a separate test

---
+++ bt5/erp5_officejs_support_request_ui_test/TestTemplateItem/portal_components/test.erp5.testFunctionalSupportRequestLogout.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+# Copyright (c) 2002-2017 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+##############################################################################
+
+from Products.ERP5Type.tests.ERP5TypeFunctionalTestCase import ERP5TypeFunctionalTestCase
+
+class TestSupportRequestUILogout(ERP5TypeFunctionalTestCase):
+  run_only = "officejs_support_request_ui_logout_zuite"
+
+  def afterSetUp(self):
+    super(TestSupportRequestUILogout, self).afterSetUp()
+    # fix consistency to update translation
+    self.portal.web_site_module.erp5_officejs_support_request_ui.fixConsistency()
+    self.tic()
+
+del ERP5TypeFunctionalTestCase
