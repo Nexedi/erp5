9afcf24cb46bc2ca49d7713d41f0f82f9db7381f
Roque Porchetto <roque.porchetto@nexedi.com>
2024-07-12 19:23:20 +0000

    erp5_web_monitoring: refactor how app handles data - app does not manipulate data structure anymore (jio does) - use union storage for multiple masters - remote slapos masters fetch is done in replicateopml storage repair method - sync repair method recreates the storage on new master urls, and updates the stored objects accordingly

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
@@ -52,6 +52,69 @@
     return new RSVP.Promise(resolver, canceller);
   }
 
+  function getUpdatedMonitoringStorageSpec(gadget, check_update) {
+    var storage_definition_list = [], i, master_url_list,
+      update_settings, monitoring_jio;
+    return gadget.getSettingList(['latest_master_url_list',
+                                  'master_url_list',
+                                  'default_view_reference'])
+      .push(function (result_list) {
+        // TODO sets should be used to compare elements are different
+        if (result_list[0] || result_list[1]) {
+          // if no previous master list or latest list is different (new configuration set), update
+          if (!result_list[1] ||
+              result_list[0].toString() !== result_list[1].toString()) {
+            master_url_list = result_list[0];
+            update_settings = true;
+          }
+          else {
+            // if no new configuration, do not re create storage on repair
+            if (check_update) {
+              return;
+            }
+            master_url_list = result_list[1];
+          }
+          for (i = 0; i < master_url_list.length; i += 1) {
+            storage_definition_list.push({
+              type: "erp5",
+              url: master_url_list[i],
+              default_view_reference: result_list[2]
+            });
+          }
+        }
+        monitoring_jio =
+        {
+          type: "replicatedopml",
+          remote_storage_unreachable_status: "WARNING",
+          remote_opml_check_time_interval: 86400000,
+          request_timeout: 25000, // timeout is to 25 second
+          local_sub_storage: {
+            type: "query",
+            sub_storage: {
+              type: "uuid",
+              sub_storage: {
+                type: "indexeddb",
+                database: "monitoring_local.db"
+              }
+            }
+          },
+          remote_sub_storage: {
+            type: "union",
+            storage_list: storage_definition_list
+          }
+        };
+      })
+      .push(function () {
+        if (update_settings) {
+          return gadget.setSettingList({'master_url_list': master_url_list,
+                                        'latest_master_url_list': master_url_list});
+        }
+      })
+      .push(function () {
+        return monitoring_jio;
+      });
+  }
+
   rJS(window)
 
     .ready(function (gadget) {
@@ -60,6 +123,7 @@
     })
 
     .declareAcquiredMethod("getSettingList", "getSettingList")
+    .declareAcquiredMethod("setSettingList", "setSettingList")
     .declareAcquiredMethod("setSetting", "setSetting")
 
     .declareMethod('updateConfiguration', function (appcache_storage, migration_version, current_version, jio_storage) {
@@ -73,7 +137,9 @@
           return jio_storage.repair();
         })
         .push(function () {
-          return gadget.setSetting("migration_version", current_version);
+          return gadget.setSettingList({'migration_version': current_version,
+                                        'master_url_list': undefined,
+                                        'latest_master_url_list': undefined});
         });
     })
 
@@ -88,7 +154,7 @@
 
     .declareMethod('createJio', function (options) {
       var gadget = this, current_version, index, appcache_storage,
-        monitoring_jio, appcache_jio, migration_version, manifest,
+        appcache_jio, migration_version, manifest, monitoring_jio,
         origin_url = window.location.href;
       return gadget.getSettingList(['configuration_manifest',
                                     'migration_version'])
@@ -99,22 +165,16 @@
           index = current_version.indexOf(window.location.host) + window.location.host.length;
           current_version = current_version.substr(index);
           manifest = "gadget_officejs_monitoring.configuration";
-          monitoring_jio = {
-            type: "replicatedopml",
-            remote_storage_unreachable_status: "WARNING",
-            remote_opml_check_time_interval: 86400000,
-            request_timeout: 25000, // timeout is to 25 second
-            local_sub_storage: {
-              type: "query",
-              sub_storage: {
-                type: "uuid",
-                sub_storage: {
-                  type: "indexeddb",
-                  database: "monitoring_local.db"
-                }
-              }
-            }
-          };
+          if (migration_version !== current_version) {
+            return gadget.setSettingList({'master_url_list': undefined,
+                                          'latest_master_url_list': undefined});
+          }
+        })
+        .push(function () {
+          return getUpdatedMonitoringStorageSpec(gadget);
+        })
+        .push(function (spec) {
+          monitoring_jio = spec;
           appcache_jio = {
             type: "replicate",
             parallel_operation_attachment_amount: 10,
@@ -228,10 +288,19 @@
       });
     })
     .declareMethod('repair', function () {
-      var storage = this.props.jio_storage,
+      var gadget = this, storage_definition_list = [],
         argument_list = arguments;
       return promiseLock(LOCK_NAME, {}, function () {
-        return storage.repair.apply(storage, argument_list);
+        return getUpdatedMonitoringStorageSpec(gadget, true)
+          .push(function (monitoring_jio) {
+            if (monitoring_jio) {
+              return gadget.createStorage(undefined, monitoring_jio);
+            }
+          })
+          .push(function () {
+            var storage = gadget.props.jio_storage;
+            return storage.repair.apply(storage, argument_list);
+          });
       });
     });
 
