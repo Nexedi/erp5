9492e1d2575d5f818a908bf60dfe924429eab15b
Roque Porchetto <roque.porchetto@nexedi.com>
2024-07-16 15:16:38 +0000

    erp5_web_monitoring: control on master_url_list

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
@@ -89,8 +89,8 @@
     .declareMethod('createJio', function (options) {
       var gadget = this, current_version, index, appcache_storage,
         monitoring_jio, appcache_jio, migration_version, manifest,
-        origin_url = window.location.href, i, master_url_list = [],
-        storage_definition_list;
+        origin_url = window.location.href, i, master_url_list,
+        storage_definition_list = [];
       return gadget.getSettingList(['configuration_manifest',
                                     'migration_version',
                                     'default_view_reference',
@@ -98,13 +98,13 @@
         .push(function (result_list) {
           if (result_list[3]) {
             master_url_list = result_list[3];
-          }
-          for (i = 0; i < master_url_list.length; i += 1) {
-            storage_definition_list.push({
-              type: "erp5",
-              url: master_url_list[i],
-              default_view_reference: result_list[2]
-            });
+            for (i = 0; i < master_url_list.length; i += 1) {
+              storage_definition_list.push({
+                type: "erp5",
+                url: master_url_list[i],
+                default_view_reference: result_list[2]
+              });
+            }
           }
           //TODO fix missing router setting (it's set but get returns undefined)
           migration_version = result_list[1];
@@ -139,7 +139,7 @@
                 type: "uuid",
                 sub_storage: {
                   type: "indexeddb",
-                  database: "monitoring_local_roque.db"
+                  database: "monitoring_local.db"
                 }
               }
             },
