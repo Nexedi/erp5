d22a8ff332d36fda52b6870994624b7325abc7ae
Jérome Perrin <jerome@nexedi.com>
2024-07-16 17:02:10 +0200

    WIP: py2/py3: Make Products code compatible with both python2 and python3.
    * Zope API changes:
      + publish() stdin is now BytesIO rather than StringIO.
      + BaseResponse `body` property is now bytes().
    * Python3 API changes:
      + builtin reduce() was removed.
      + Use BytesIO rather than StringIO to follow py3 API.
      + hmac.new() requires digestmod argument from Python 3.8.
      + Use six.moves library to handle moved objects from py2 to py3.
      + `modernize -f xrange_six` then slightly adjusted manually to just use range
         where it does not make a significant difference (for example in test).
      + base64.b64encode() now expects bytes().
      + UserDict() interface changed:
        - New parameter in update() and pop().
        - `failobj` setdefault parameter renamed to `default`.
      + ensure_list() on dict.{values,items}() and list(dict) for dict.keys() when
        we really need a list and not an iterable (Python3).
        - Add ensure_list() to RestrictedPython safe_builtins as it's very common usage.
      + Make dict iteration works on both version of Python.
        - Use six.iter{items,values,keys}().
        - has_key() has been removed.
        - Make sure that dict.{items,values,keys}() returns a real list when modified (ensure_list()).
      + Exceptions:
        - No longer behave as sequences. Using `args` attribute instead.
        - When an exception has been assigned using `as target`, it is cleared at
          the end of the except clause.
    
      + file: py2 was returning `str` upon reading, now it returns text strings.
        Also, opening mode is text strings by default.
      + Data strings are bytes().
    
    Co-Authored-by:
      Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
      Arnaud Fontaine <arnaud.fontaine@nexedi.com>
      Carlos Ramos Carreño <carlos.ramos@nexedi.com>
      Emmeline Vouriot <emmeline.vouriot@nexedi.com>

--- bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testIdToolUpgrade.py
+++ bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testIdToolUpgrade.py
@@ -34,6 +34,8 @@ from Products.ERP5Type.Globals import PersistentMapping
 from Products.ERP5Type.Utils import ScalarMaxConflictResolver
 from BTrees.Length import Length
 from BTrees.OOBTree import OOBTree
+from six.moves import range
+import six
 
 
 class TestIdToolUpgrade(ERP5TypeTestCase):
@@ -137,7 +139,7 @@ class TestIdToolUpgrade(ERP5TypeTestCase):
     del id_tool.__class__.getTypeInfo
     bt = self.portal.portal_templates.getInstalledBusinessTemplate('erp5_core',
                                                                   strict=True)
-    for path, obj in bt._path_item._objects.iteritems():
+    for path, obj in six.iteritems(bt._path_item._objects):
       path, obj_id = path.rsplit('/', 1)
       if path == 'portal_ids':
         id_tool._setObject(obj_id, obj._getCopy(bt))
@@ -173,14 +175,14 @@ class TestIdToolUpgrade(ERP5TypeTestCase):
 
   def _setUpLastMaxIdDict(self, id_generator_reference):
     def countup(id_generator, id_group, until):
-      for _ in xrange(until + 1):
+      for _ in range(until + 1):
         self.id_tool.generateNewId(id_generator=id_generator_reference,
                                    id_group=id_group)
 
     countup(id_generator_reference, 'A-01', 2)
     countup(id_generator_reference, 'B-01', 1)
     var_id = 'C-%04d'
-    for x in xrange(self.a_lot_of_key):
+    for x in range(self.a_lot_of_key):
       countup(id_generator_reference, var_id % x, 0)
 
   def _getLastIdDictName(self, id_generator):
@@ -214,7 +216,7 @@ class TestIdToolUpgrade(ERP5TypeTestCase):
     last_id_dict = self._getLastIdDict(id_generator)
     self.assertEqual(2, self._getValueFromLastIdDict(last_id_dict, 'A-01'))
     self.assertEqual(1, self._getValueFromLastIdDict(last_id_dict, 'B-01'))
-    for x in xrange(self.a_lot_of_key):
+    for x in range(self.a_lot_of_key):
       key = 'C-%04d' % x
       self.assertEqual(0, self._getValueFromLastIdDict(last_id_dict, key))
 
