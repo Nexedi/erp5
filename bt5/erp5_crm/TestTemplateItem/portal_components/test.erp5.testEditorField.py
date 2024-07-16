c85304423566246f26cc090935245ea9f42f4e5a
Jérome Perrin <jerome@nexedi.com>
2024-07-16 17:02:10 +0200

    py2/py3: Make Products code compatible with both python2 and python3.
    * Zope API changes:
      + publish() stdin is now BytesIO rather than StringIO.
      + HTTPResponse `body` property is now bytes().
      + OFS.Image.File file parameter is bytes().
    * Python3 API changes:
      + builtin reduce() was removed.
      + Use BytesIO rather than StringIO to follow py3 API.
      + hmac.new() requires digestmod argument from Python 3.8.
      + Use six.moves library to handle moved objects from py2 to py3.
      + `modernize -f xrange_six` then slightly adjusted manually to just use range
         where it does not make a significant difference (for example in test).
      + base64.b64encode() now expects bytes().
      + UserDict() interface changed:
        - New parameter in update() and pop().
        - `failobj` setdefault parameter renamed to `default`.
      + ensure_list() on dict.{values,items}() and list(dict) for dict.keys() when
        we really need a list and not an iterable (Python3).
        - Add ensure_list() to RestrictedPython safe_builtins as it's very common usage.
      + Make dict iteration works on both version of Python.
        - Use six.iter{items,values,keys}().
        - has_key() has been removed.
        - Make sure that dict.{items,values,keys}() returns a real list when
        modified (ensure_list()).
      + Comparisons between int and NoneType raises TypeError.
      + No more unbound methods in python3 so use six.get_unbound_function().
      + Exceptions:
        - No longer behave as sequences. Using `args` attribute instead.
        - When an exception has been assigned using `as target`, it is cleared at
          the end of the except clause.
      + file: py2 was returning `str` upon reading, now it returns text strings.
        Also, opening mode is text strings by default.
      + Data strings are bytes().
        - Replace str() by bytes().
      + iterators no longer have next() method, instead there is next() builtin.
      + New ConnectionError exception so rename existing one to not clash.
      + Integer division is now with //.
    
    Co-Authored-by:
      Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
      Arnaud Fontaine <arnaud.fontaine@nexedi.com>
      Carlos Ramos Carreño <carlos.ramos@nexedi.com>
      Emmeline Vouriot <emmeline.vouriot@nexedi.com>

--- bt5/erp5_crm/TestTemplateItem/portal_components/test.erp5.testEditorField.py
+++ bt5/erp5_crm/TestTemplateItem/portal_components/test.erp5.testEditorField.py
@@ -168,7 +168,8 @@ class TestEditorField(ERP5TypeTestCase, ZopeTestCase.Functional):
       document -- the document which content is displayed in
                   read only mode
     """
-    html_text = html_text.encode('utf-8')
+    if six.PY2:
+      html_text = html_text.encode('utf-8')
     match_string1 = "data-gadget-editable="
     match_string2 = 'data-gadget-value="%s"' % html_quote(text_content)
     if html_text.find(match_string1) != -1:
@@ -204,7 +205,7 @@ class TestEditorField(ERP5TypeTestCase, ZopeTestCase.Functional):
 
     # Set a fake file on Event and make sure no more editor is displayed
     # and that instead a div with page CSS style appears with stripped HTML
-    event.setData('fake')
+    event.setData(b'fake')
     self.assertFalse(event.Event_view.my_text_content.get_value('editable'))
     html_text = event.view()
     self.assertTrue(self._isReadOnlyEditor(html_text, event, 'fake'))
@@ -233,7 +234,7 @@ class TestEditorField(ERP5TypeTestCase, ZopeTestCase.Functional):
 
     # Set a fake file on Event and make sure no more editor is displayed
     # and that instead a div with page CSS style appears with stripped HTML
-    event.setData('fake')
+    event.setData(b'fake')
     self.assertFalse(event.Event_view.my_text_content.get_value('editable'))
     html_text = event.view()
     self.assertTrue(self._isReadOnlyEditor(html_text, event, 'fake'))
@@ -261,7 +262,7 @@ class TestEditorField(ERP5TypeTestCase, ZopeTestCase.Functional):
 
     # Set a fake file on Event and make sure no more editor is displayed
     # and that instead a div with page CSS style appears with stripped HTML
-    event.setData('fake')
+    event.setData(b'fake')
     self.assertFalse(event.Event_view.my_text_content.get_value('editable'))
     html_text = event.view()
     self.assertTrue(self._isReadOnlyEditor(html_text, event, 'fake'))
@@ -289,7 +290,7 @@ class TestEditorField(ERP5TypeTestCase, ZopeTestCase.Functional):
 
     # Set a fake file on Event and make sure no more editor is displayed
     # and that instead a div with page CSS style appears with stripped HTML
-    event.setData('fake')
+    event.setData(b'fake')
     self.assertFalse(event.Event_view.my_text_content.get_value('editable'))
     html_text = event.view()
     self.assertTrue(self._isReadOnlyEditor(html_text, event, 'fake'))
--- bt5/erp5_simplified_invoicing/TestTemplateItem/portal_components/test.erp5.testSimulationPerformance.py
+++ bt5/erp5_simplified_invoicing/TestTemplateItem/portal_components/test.erp5.testSimulationPerformance.py
@@ -60,7 +60,7 @@ class TestSimulationPerformance(TestTradeModelLineSale):
   def perf_01_invoiceSimpleOrder(self, order_count=1):
     start = time()
     order = self.portal.unrestrictedTraverse(self._order)
-    order_list = [self.clone(order) for _ in xrange(order_count)]
+    order_list = [self.clone(order) for _ in range(order_count)]
     for order in order_list:
       for line in list(order.getMovementList()):
         self.clone(line)
