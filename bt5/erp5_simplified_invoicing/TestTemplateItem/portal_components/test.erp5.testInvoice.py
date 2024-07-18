dc1e347b89fa916edb30446406e7be82cf489c10
Jérome Perrin <jerome@nexedi.com>
2024-07-18 17:45:54 +0200

    OOoUtils: Followup of 48e2e4fd: Parsed OOo file content is bytes().

--- bt5/erp5_simplified_invoicing/TestTemplateItem/portal_components/test.erp5.testInvoice.py
+++ bt5/erp5_simplified_invoicing/TestTemplateItem/portal_components/test.erp5.testInvoice.py
@@ -640,7 +640,7 @@ class TestInvoice(TestInvoiceMixin):
     parser = OOoParser()
     parser.openFromBytes(odt)
     style_xml = parser.oo_files['styles.xml']
-    self.assertNotIn('<draw:image', style_xml)
+    self.assertNotIn(b'<draw:image', style_xml)
 
   def test_Invoice_viewAsODT_invalid_image(self):
     resource = self.portal.getDefaultModule(
