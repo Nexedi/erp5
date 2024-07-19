8742f30a6f41f0451ca38731837a0dcabeb83627
Roque Porchetto <roque.porchetto@nexedi.com>
2024-07-19 15:44:10 +0000

    erp5_web_monitoring: - use latest master url list setting - recreate storage if new master url list - recreate storage on repair if new master urls - fix default master url

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/gadget_officejs_monitoring_jio_js.js
@@ -60,6 +60,7 @@
     })
 
     .declareAcquiredMethod("getSettingList", "getSettingList")
+    .declareAcquiredMethod("setSettingList", "setSettingList")
     .declareAcquiredMethod("setSetting", "setSetting")
 
     .declareMethod('updateConfiguration', function (appcache_storage, migration_version, current_version, jio_storage) {
@@ -91,13 +92,23 @@
         monitoring_jio, appcache_jio, migration_version, manifest,
         origin_url = window.location.href, i, master_url_list,
         storage_definition_list = [];
+    
       return gadget.getSettingList(['configuration_manifest',
                                     'migration_version',
                                     'default_view_reference',
+                                    'latest_master_url_list',
                                     'master_url_list'])
         .push(function (result_list) {
-          if (result_list[3]) {
-            master_url_list = result_list[3];
+          if (result_list[3] || result_list[4]) {
+            if (!result_list[4] ||
+                result_list[3].toString() !== result_list[4].toString()) {
+              master_url_list = result_list[3];
+              gadget.setSetting('master_url_list', master_url_list);
+              gadget.setSetting('latest_master_url_list', master_url_list);
+            }
+            else {
+              master_url_list = result_list[4];
+            }
             for (i = 0; i < master_url_list.length; i += 1) {
               storage_definition_list.push({
                 type: "erp5",
@@ -106,12 +117,15 @@
               });
             }
           }
+        
+          // move this up ------------------------------------------------------------------
           //TODO fix missing router setting (it's set but get returns undefined)
           migration_version = result_list[1];
           current_version = window.location.href.replace(window.location.hash, "");
           index = current_version.indexOf(window.location.host) + window.location.host.length;
           current_version = current_version.substr(index);
           manifest = "gadget_officejs_monitoring.configuration";
+          // move this up ------------------------------------------------------------------
           monitoring_jio =
           {
             type: "replicatedopml",
@@ -133,6 +147,7 @@
               storage_list: storage_definition_list
             }
           };
+        
           appcache_jio = {
             type: "replicate",
             parallel_operation_attachment_amount: 10,
@@ -246,10 +261,62 @@
       });
     })
     .declareMethod('repair', function () {
-      var storage = this.props.jio_storage,
-        argument_list = arguments;
+      var gadget = this, storage_definition_list = [],
+        argument_list = arguments, master_url_list, i, update_settings = false;
       return promiseLock(LOCK_NAME, {}, function () {
-        return storage.repair.apply(storage, argument_list);
+        return gadget.getSettingList(['latest_master_url_list',
+                                      'master_url_list'])
+          .push(function (result_list) {
+            if (result_list[0] || result_list[1]) {
+              // if there is a new list of master url, re-create storage
+              // TODO sets should be used to compare elements are different
+              if (!result_list[1] ||
+                  result_list[0].toString() !== result_list[1].toString()) {
+                master_url_list = result_list[0];
+                update_settings = true;
+                for (i = 0; i < master_url_list.length; i += 1) {
+                  storage_definition_list.push({
+                    type: "erp5",
+                    url: master_url_list[i],
+                    default_view_reference: result_list[2]
+                  });
+                }
+                var monitoring_jio =
+                {
+                  type: "replicatedopml",
+                  remote_storage_unreachable_status: "WARNING",
+                  remote_opml_check_time_interval: 86400000,
+                  request_timeout: 25000, // timeout is to 25 second
+                  local_sub_storage: {
+                    type: "query",
+                    sub_storage: {
+                      type: "uuid",
+                      sub_storage: {
+                        type: "indexeddb",
+                        database: "monitoring_local.db"
+                      }
+                    }
+                  },
+                  remote_sub_storage: {
+                    type: "union",
+                    storage_list: storage_definition_list
+                  }
+                };
+                return gadget.createStorage(undefined, monitoring_jio);
+                //TODO remove objects of previous masters?
+              }
+            }
+          })
+          .push(function () {
+            if (update_settings) {
+              return gadget.setSettingList({'master_url_list': master_url_list,
+                                            'latest_master_url_list': master_url_list});
+            }
+          })
+          .push(function () {
+            var storage = gadget.props.jio_storage;
+            return storage.repair.apply(storage, argument_list);
+          });
       });
     });
 
