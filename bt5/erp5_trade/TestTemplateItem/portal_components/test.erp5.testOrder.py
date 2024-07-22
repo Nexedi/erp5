d76fce323137341e9ae029f50ebd92cfc526c1b8
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2024-07-22 13:53:56 +0200

    Cosmetic changes and remove unused imports.

--- bt5/erp5_trade/TestTemplateItem/portal_components/test.erp5.testOrder.py
+++ bt5/erp5_trade/TestTemplateItem/portal_components/test.erp5.testOrder.py
@@ -2838,11 +2838,11 @@ class TestOrder(TestOrderMixin, ERP5TypeTestCase):
                               portal_type='Organisation', title='Client',
                               default_image_file=image)
     from OFS.Image import Pdata
-    self.assertTrue(isinstance(client.getDefaultImageValue().data, Pdata))
+    self.assertIsInstance(client.getDefaultImageValue().data, Pdata)
     vendor = self.portal.organisation_module.newContent(
                               portal_type='Organisation', title='Vendor',
                               default_image_file=image)
-    self.assertTrue(isinstance(vendor.getDefaultImageValue().data, Pdata))
+    self.assertIsInstance(vendor.getDefaultImageValue().data, Pdata)
     order = self.portal.getDefaultModule(self.order_portal_type).newContent(
                               portal_type=self.order_portal_type,
                               specialise=self.business_process,
