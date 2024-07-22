ff9e166c0df9bf982f2abb71ac989699319eb3a2
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2024-07-22 13:53:56 +0200

    Cosmetic changes and remove unused imports.

--- bt5/erp5_safeimage/ExtensionTemplateItem/portal_components/extension.erp5.ERP5ZoomifyImage.py
+++ bt5/erp5_safeimage/ExtensionTemplateItem/portal_components/extension.erp5.ERP5ZoomifyImage.py
@@ -16,7 +16,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##############################################################################
 
-from __future__ import print_function
 import os, sys, shutil, tempfile
 from cStringIO import StringIO
 from zLOG import LOG,ERROR,INFO,WARNING
@@ -28,7 +27,6 @@ try:
 except ImportError: # BBB Zope2
   from Globals import package_home
 import PIL.Image as PIL_Image
-import thread
 import random
 import base64
 from OFS.Folder import Folder
@@ -176,15 +174,12 @@ class ZoomifyBase:
         lr_y = ul_y + self.tileSize
       else:
         lr_y = self.originalHeight
-      print("Going to open image")
       imageRow = image.crop([0, ul_y, self.originalWidth, lr_y])
       saveFilename = root + str(tier) + '-' + str(row) +  ext
       if imageRow.mode != 'RGB':
         imageRow = imageRow.convert('RGB')
       imageRow.save(os.path.join(tempfile.gettempdir(), saveFilename),
                                                         'JPEG', quality=100)
-      print("os path exist : %r" % os.path.exists(os.path.join(
-                                        tempfile.gettempdir(), saveFilename)))
       if os.path.exists(os.path.join(tempfile.gettempdir(), saveFilename)):
         self.processRowImage(tier=tier, row=row)
       row += 1
@@ -192,7 +187,6 @@ class ZoomifyBase:
   def processRowImage(self, tier=0, row=0):
     """ for an image, create and save tiles """
 
-    print('*** processing tier: ' + str(tier) + ' row: ' + str(row))
     tierWidth, tierHeight = self._v_scaleInfo[tier]
     rowsForTier = tierHeight/self.tileSize
     if tierHeight % self.tileSize > 0:
@@ -335,7 +329,7 @@ class ZoomifyZopeProcessor(ZoomifyBase):
   def openImage(self):
     """ load the image data """
 
-    return PIL_Image.open(self._v_imageObject.name)
+    return PIL_Image.open(self._v_imageObject)
 
   def createDefaultViewer(self):
     """ add the default Zoomify viewer to the Zoomify metadata """
