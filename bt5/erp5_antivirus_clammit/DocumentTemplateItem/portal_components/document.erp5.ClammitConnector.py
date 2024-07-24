08987c2d78633f78b65d33ad71009d36c2e8f1f4
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2024-07-24 07:58:01 +0000

    erp5_antivirus_clammit: clammit/clamd implementation to be used with erp5_antivirus

---
+++ bt5/erp5_antivirus_clammit/DocumentTemplateItem/portal_components/document.erp5.ClammitConnector.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2013 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+##############################################################################
+
+from AccessControl import ClassSecurityInfo
+from Products.ERP5Type import Permissions
+from Products.ERP5Type.XMLObject import XMLObject
+
+import requests
+
+class ClammitConnector(XMLObject):
+  # CMF Type Definition
+  meta_type = "Clammit Connector"
+  portal_type = "Clammit Connector"
+
+  # Declarative security
+  security = ClassSecurityInfo()
+  security.declareObjectProtected(Permissions.AccessContentsInformation)
+
+  _SANE_HTTP_STATUS_CODE = 200
+  _INFECTED_HTTP_STATUS_CODE = 418
+  _DEFAULT_TIMEOUT = 30 # In seconds
+
+  def isSafe(self, data):
+    response = requests.post(
+      self.getUrlString() + '/scan',
+      files={'file': data},
+      timeout=self.getTimeout(self._DEFAULT_TIMEOUT),
+      verify=False, # TODO: how to do self-certs correctly ?
+    )
+    if response.status_code == self._SANE_HTTP_STATUS_CODE:
+      return True
+    elif response.status_code == self._INFECTED_HTTP_STATUS_CODE:
+      return False
+    else:
+      raise ValueError("Unknown status code")
+
+  def isReady(self):
+    response = requests.get(
+      self.getUrlString() + '/readyz',
+      timeout=self.getTimeout(self._DEFAULT_TIMEOUT),
+      verify=False, # TODO: how to do self-certs correctly ?
+    )
+    if response.status_code == 200:
+      return True
+    return False
---
+++ bt5/erp5_antivirus_clammit/WorkflowTemplateItem/portal_workflow/clammit_antivirus_scan_interaction_workflow/script_runClammitScan.py
@@ -0,0 +1,1 @@
+state_change['object'].Document_activateRunClammitScan()
