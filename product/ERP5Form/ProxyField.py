77f6c0744e94692f04294c9c542b5d1158d9e62b
Arnaud Fontaine <arnaud.fontaine@nexedi.com>
2024-07-31 18:11:35 +0200

    Fix typo (a17bb910: py2/py3: Make Products code compatible with both python2 and python3).

--- product/ERP5Form/ProxyField.py
+++ product/ERP5Form/ProxyField.py
@@ -248,7 +248,7 @@ class ProxyField(ZMIField):
       for key, value in result.items():
         if isinstance(value, six.binary_type):
           # in unicode mode, Formulator UI always uses UTF-8
-          value = six.text_type(message, 'utf-8')
+          value = six.text_type(value, 'utf-8')
         new_result[key] = value
       result = new_result
 
