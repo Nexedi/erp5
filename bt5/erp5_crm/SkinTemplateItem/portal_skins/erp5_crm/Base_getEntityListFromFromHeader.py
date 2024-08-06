da6274f6369cf6d2303bd7b2636adb250561f06b
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2024-08-06 10:10:49 +0200

    erp5_crm: keep order in Base_getEntityListFromFromHeader.

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Base_getEntityListFromFromHeader.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Base_getEntityListFromFromHeader.py
@@ -1,13 +1,14 @@
+from collections import OrderedDict
 getResultValue = context.portal_catalog.getResultValue
 
 from Products.ERP5Type.Utils import Email_parseAddressHeader
 
-result = set()
-for _, recipient in set(Email_parseAddressHeader(text)):
+result = OrderedDict()
+for recipient in OrderedDict((x[1], True) for x in Email_parseAddressHeader(text)):
   if recipient:
     email = getResultValue(url_string={'query':recipient, 'key':'ExactMatch'}, portal_type='Email', parent_portal_type='Person')
     if email is None:
       email = getResultValue(url_string={'query':recipient, 'key':'ExactMatch'}, portal_type='Email', parent_portal_type='Organisation')
     if email is not None:
-      result.add(email.getParentValue())
+      result[email.getParentValue()] = True
 return list(result)
