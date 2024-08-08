ec4ba50e05f254fc4cae6419e8e8d67f1daba623
Titouan Soulard <titouan.soulard@rapid.space>
2024-08-08 12:22:26 +0200

    erp5_trade: fix method `Movement_zGetPackedQuantity`
    The ZSQLMethod was using a wrong table `movement` instead of the `stock` table,
    hence throwing errors.

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_zGetPackedQuantity.sql
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_zGetPackedQuantity.sql
@@ -1,17 +1,17 @@
 SELECT
-	SUM(movement.quantity) AS quantity
+	SUM(stock.quantity) AS quantity
 FROM
-	catalog AS container_cell, movement
+	catalog AS container_cell, stock
 WHERE
-	movement.explanation_uid = <dtml-sqlvar explanation_uid type="int">
+	stock.explanation_uid = <dtml-sqlvar explanation_uid type="int">
   AND container_cell.has_cell_content = 0
-  AND container_cell.uid = movement.uid
+  AND container_cell.uid = stock.uid
   AND	(container_cell.portal_type = "Container Cell" 
       OR container_cell.portal_type = "Container Line")
 
 <dtml-if resource_uid>
-  AND movement.resource_uid = <dtml-sqlvar resource_uid type="int">
+  AND stock.resource_uid = <dtml-sqlvar resource_uid type="int">
 </dtml-if>
 <dtml-if variation_text>
-  AND movement.variation_text = <dtml-sqlvar variation_text type="string">
+  AND stock.variation_text = <dtml-sqlvar variation_text type="string">
 </dtml-if>
