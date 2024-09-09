fb1e246e510403d7dc2c5ae850469df6171722bb
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2024-09-09 13:43:25 +0200

    fixup! erp5_mysql_innodb_catalog: record deleted object paths.

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_delete_uid.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_delete_uid.sql
@@ -3,6 +3,7 @@ REPLACE INTO
 VALUES
   (<dtml-sqlvar uid type="int">, 'deleted','',NULL,'',NULL)
 <dtml-var sql_delimiter>
+<dtml-if expr="'deleted_catalog' in portal_catalog.getSQLCatalog().getTableIds()">
 <dtml-comment>
   Here we use REPLACE instead of INSERT to avoid duplicate entries.
 </dtml-comment>
@@ -11,6 +12,7 @@ REPLACE INTO
 VALUES
   (<dtml-sqlvar uid type="int">, <dtml-sqlvar path type="string">)
 <dtml-var sql_delimiter>
+</dtml-if>
 <dtml-comment>
   Note on "UPDATE stock" query: this query preserve transactionality of movement
   deletion: all movements deleted in a single transaction will be either
