0b9b83afbef24f4349185a606600a12908c31a6a
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2024-09-13 06:53:22 +0000

    erp5_web_renderjs_ui: Translate the content of the Maximize button
    Notably showned as a companion of the editor gadget

--- bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_button_maximize_js.js
+++ bt5/erp5_web_renderjs_ui/PathTemplateItem/web_page_module/rjs_gadget_button_maximize_js.js
@@ -5,6 +5,18 @@
 
   rJS(window)
     .declareAcquiredMethod('triggerMaximize', 'triggerMaximize')
+    .declareAcquiredMethod("translate", "translate")
+
+    .declareMethod('render', function () {
+      var gadget = this,
+          button_element = gadget.element.querySelector("button");
+
+      return gadget.translate(
+        button_element.getAttribute("data-i18n")
+      ).then(function (translation) {
+        button_element.textContent = translation;
+      });
+    })
 
     .onEvent('click', function (event) {
       if (event.target.tagName === "BUTTON") {
