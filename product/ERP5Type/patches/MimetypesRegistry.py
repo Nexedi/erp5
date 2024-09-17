482d1631fe48416daa327f3bf2e0fc845b416209
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2024-09-17 13:17:16 +0200

    patches/MimetypesRegistry: add OnlyOffice MIME types.

--- product/ERP5Type/patches/MimetypesRegistry.py
+++ product/ERP5Type/patches/MimetypesRegistry.py
@@ -1,4 +1,5 @@
 from Products.MimetypesRegistry import MimeTypesRegistry, mime_types
+from Products.MimetypesRegistry.MimeTypeItem import MimeTypeItem
 import six
 
 preferred_extension_dict = {
@@ -9,6 +10,30 @@ preferred_extension_dict = {
   "tar": "application/x-tar",
 }
 
+additional_mimetype_item_list = (
+  {
+    "name": "OnlyOffice Text",
+    "mimetypes": ("application/x-asc-text",),
+    "extensions": ("docy",),
+    "binary": True,
+    "icon_path": "application.png",
+  },
+  {
+    "name": "OnlyOffice Spreadsheet",
+    "mimetypes": ("application/x-asc-spreadsheet",),
+    "extensions": ("xlsy",),
+    "binary": True,
+    "icon_path": "application.png",
+  },
+  {
+    "name": "OnlyOffice Presentation",
+    "mimetypes": ("application/x-asc-presentation",),
+    "extensions": ("ppty",),
+    "binary": True,
+    "icon_path": "application.png",
+  },
+)
+
 def initialize(registry):
     mime_types.initialize(registry)
     for ext, mime in six.iteritems(preferred_extension_dict):
@@ -18,5 +43,13 @@ def initialize(registry):
         x.remove(ext)
         x.insert(0, ext)
         mime.extensions = tuple(x)
+    for item in additional_mimetype_item_list:
+      registry.manage_addMimeType(
+        id=item["name"],
+        mimetypes=item["mimetypes"],
+        extensions=item["extensions"],
+        binary=item["binary"],
+        icon_path=item["icon_path"],
+      )
 
 MimeTypesRegistry.initialize = initialize
