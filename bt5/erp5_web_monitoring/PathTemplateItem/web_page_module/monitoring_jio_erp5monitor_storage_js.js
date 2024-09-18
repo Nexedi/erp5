f9fee35b189cf9c6ab484a28893bda161c070f5b
Roque Porchetto <roque.porchetto@nexedi.com>
2024-09-18 18:13:40 +0000

    erp5_web_monitoring: fix ERP5Monitor storage name

--- bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/monitoring_jio_erp5monitor_storage_js.js
+++ bt5/erp5_web_monitoring/PathTemplateItem/web_page_module/monitoring_jio_erp5monitor_storage_js.js
@@ -13,12 +13,12 @@
   /**
    * Monitor erp5 layer to wrap erp5 storages for monitor app
    *
-   * @class ERP55Monitor
+   * @class ERP5Monitor
    * @constructor
    */
-  function ERP55Monitor(spec) {
+  function ERP5Monitor(spec) {
     if (!spec.sub_storage || spec.sub_storage.type !== 'erp5') {
-      throw new TypeError("ERP55Monitor subtorage must be erp5 type");
+      throw new TypeError("ERP5Monitor subtorage must be erp5 type");
     }
     if (spec.limit) {
       LIMIT = spec.limit;
@@ -27,27 +27,27 @@
     this._sub_storage = jIO.createJIO(spec.sub_storage);
   }
 
-  ERP55Monitor.prototype.get = function (id) {
+  ERP5Monitor.prototype.get = function (id) {
     return this._sub_storage.get.apply(this._sub_storage, arguments);
   };
 
-  ERP55Monitor.prototype.post = function () {
+  ERP5Monitor.prototype.post = function () {
     return this._sub_storage.post.apply(this._sub_storage, arguments);
   };
 
-  ERP55Monitor.prototype.put = function () {
+  ERP5Monitor.prototype.put = function () {
     return this._sub_storage.put.apply(this._sub_storage, arguments);
   };
 
-  ERP55Monitor.prototype.remove = function () {
+  ERP5Monitor.prototype.remove = function () {
     return this._sub_storage.remove.apply(this._sub_storage, arguments);
   };
 
-  ERP55Monitor.prototype.hasCapacity = function (capacity) {
+  ERP5Monitor.prototype.hasCapacity = function (capacity) {
     return (capacity === "list") || (capacity === "limit") || (capacity === "include") || (capacity === "query") || (capacity === "select");
   };
 
-  ERP55Monitor.prototype.buildQuery = function () {
+  ERP5Monitor.prototype.buildQuery = function () {
     var sub_storage = this._sub_storage, args = arguments, master_url = this._storage_definition.url, i;
     if (!arguments[0].limit) {
       arguments[0].limit = [0, LIMIT];
@@ -64,6 +64,6 @@
       });
   };
 
-  jIO.addStorage('erp5monitor', ERP55Monitor);
+  jIO.addStorage('erp5monitor', ERP5Monitor);
 
 }(jIO, RSVP));
