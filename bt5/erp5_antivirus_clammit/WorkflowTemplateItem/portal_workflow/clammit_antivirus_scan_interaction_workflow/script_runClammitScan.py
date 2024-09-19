01d12e4d9d3b8ad5c792fe351ddcb528f4a493dc
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2024-09-19 09:31:44 +0000

    fixup! erp5_antivirus_clammit: clammit/clamd implementation to be used with erp5_antivirus

--- bt5/erp5_antivirus_clammit/WorkflowTemplateItem/portal_workflow/clammit_antivirus_scan_interaction_workflow/script_runClammitScan.py
+++ bt5/erp5_antivirus_clammit/WorkflowTemplateItem/portal_workflow/clammit_antivirus_scan_interaction_workflow/script_runClammitScan.py
@@ -1,14 +1,11 @@
 from erp5.component.module.Log import log
 
 object_value = state_change['object']
-clammit_connector = context.getPortalObject().portal_web_services.searchFolder(
+clammit_connector, = context.getPortalObject().portal_web_services.searchFolder(
   portal_type="Clammit Connector",
   limit=1,
 )
-if not clammit_connector:
-  raise ValueError("A Clammit Connector must be configured in order to run antivirus scans")
-else:
-  clammit_connector_value = clammit_connector[0].getObject()
+clammit_connector_value = clammit_connector.getObject()
 comment = "Checked by ClamAV Antivirus"
 if clammit_connector_value.isSafe(object_value.getData()):
   object_value.setSafe(comment=comment)
