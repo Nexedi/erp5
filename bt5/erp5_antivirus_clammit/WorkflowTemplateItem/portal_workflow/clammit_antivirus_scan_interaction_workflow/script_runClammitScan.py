e20ed734db7b8b1864b3b62b41cf6448ff91ca3e
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2024-09-20 08:23:39 +0000

    erp5_antivirus_clammit: move workflow's code into portal skins
    Which will make it easier for projects to customize

--- bt5/erp5_antivirus_clammit/WorkflowTemplateItem/portal_workflow/clammit_antivirus_scan_interaction_workflow/script_runClammitScan.py
+++ bt5/erp5_antivirus_clammit/WorkflowTemplateItem/portal_workflow/clammit_antivirus_scan_interaction_workflow/script_runClammitScan.py
@@ -1,14 +1,1 @@
-from erp5.component.module.Log import log
-
-object_value = state_change['object']
-clammit_connector, = context.getPortalObject().portal_web_services.searchFolder(
-  portal_type="Clammit Connector",
-  limit=1,
-)
-clammit_connector_value = clammit_connector.getObject()
-comment = "Checked by ClamAV Antivirus"
-if clammit_connector_value.isSafe(object_value.getData()):
-  object_value.setSafe(comment=comment)
-else:
-  log("ClamAV result: the file %s has been found infected" % object_value.getRelativeUrl())
-  object_value.setInfected(comment=comment)
+state_change['object'].Document_activateRunClammitScan()
