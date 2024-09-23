a8670f406174b38330bc92c3828bb06c8a6c15b0
Jérome Perrin <jerome@nexedi.com>
2024-09-24 00:21:30 +0900

    full_text_mroonga_catalog: don't use VARBINARY columns in index
    Mroonga 14.07 seems to have a problem that such columns can not be
    selected, but VARCHAR seems OK.
    
    See discussions on https://lab.nexedi.com/nexedi/slapos/-/merge_requests/1644

--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_content_translation.sql
+++ bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_content_translation.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `content_translation` (
   `uid` BIGINT UNSIGNED NOT NULL,
-  `property_name` VARBINARY(100),
-  `content_language` VARBINARY(100),
+  `property_name` VARCHAR(100) BINARY,
+  `content_language` VARCHAR(100) BINARY,
   `translated_text` TEXT,
   PRIMARY KEY (`uid`, `property_name`, `content_language`),
   FULLTEXT KEY (`translated_text`) COMMENT 'parser "TokenBigramSplitSymbolAlphaDigit"'
