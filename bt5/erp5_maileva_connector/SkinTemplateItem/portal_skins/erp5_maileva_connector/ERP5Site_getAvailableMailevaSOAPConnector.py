455c3e1da8991657550f667e06ad4d6d59e2ec51
Rafael Monnerat <rafael@nexedi.com>
2024-09-25 17:32:57 +0000

    erp5_maileva_connector: Rework security to not require roles
       The way to use the connector should NOT require us to set Roles on portal_web_services objects.
    
       Documents on portal_web_services contains passwords to third-party software so, that should be not accessible by the user (even with proper roles).
    
       I set a minimal set of proxy roles and re-work to use path rather them object to not require roles everywhere.

--- bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/ERP5Site_getAvailableMailevaSOAPConnector.py
+++ bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/ERP5Site_getAvailableMailevaSOAPConnector.py
@@ -1,7 +1,10 @@
+from zExceptions import Unauthorized
+if REQUEST is not None:
+  raise Unauthorized
 maileva_connector = context.portal_catalog.getResultValue(
   portal_type='Maileva SOAP Connector',
   reference=reference,
   validation_state='validated')
 if not maileva_connector:
   raise ValueError('Maileav soap connector is not defined')
-return maileva_connector
+return maileva_connector.getRelativeUrl()
