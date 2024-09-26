4baff4fd936eae1d0aa66f4096dc33a20936119f
Rafael Monnerat <rafael@nexedi.com>
2024-09-26 20:28:35 +0000

    erp5_maileva_connector: Responses should be translated before return

--- bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/PDF_reSendToMaileva.py
+++ bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/PDF_reSendToMaileva.py
@@ -1,9 +1,13 @@
+translateString = context.Base_translateString
+
 if context.getSendState() != 'failed':
-  return context.Base_redirect('Document_viewMailevaConnectionStatus', keep_items={'portal_status_message': 'This document is not in failed state'})
+  return context.Base_redirect('Document_viewMailevaConnectionStatus',
+    keep_items={'portal_status_message': translateString('This document is not in failed state')})
 
 maileva_exchange = context.getFollowUpRelatedValue(portal_type='Maileva Exchange')
 context.PDF_sendToMaileva(
   recipient = maileva_exchange.getDestinationValue(),
   sender = maileva_exchange.getSourceValue()
 )
-return context.Base_redirect('PDF_viewPDFJSPreview', keep_items={'portal_status_message': 'This document is resending to maileva'})
+return context.Base_redirect('PDF_viewPDFJSPreview',
+  keep_items={'portal_status_message': translateString('This document is resending to maileva')})
