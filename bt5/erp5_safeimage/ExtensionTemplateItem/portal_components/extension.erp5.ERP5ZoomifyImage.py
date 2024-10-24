f353d12e301171c0b1fba72ecade8ae543c1cc9b
Jérome Perrin <jerome@nexedi.com>
2024-10-24 10:33:44 +0200

    WIP: patches/python: override py3 builtin round to keep py2 behavior.
    And workaround safeimage impacted by this round2 patch.

--- bt5/erp5_safeimage/ExtensionTemplateItem/portal_components/extension.erp5.ERP5ZoomifyImage.py
+++ bt5/erp5_safeimage/ExtensionTemplateItem/portal_components/extension.erp5.ERP5ZoomifyImage.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##############################################################################
 
+import contextlib
 import os, sys, shutil, tempfile
 import io
 from zLOG import LOG,ERROR,INFO,WARNING
@@ -31,6 +32,29 @@ import random
 import base64
 from OFS.Folder import Folder
 from Products.ERP5Type.Utils import str2bytes
+
+
+import six
+# XXX zope4py3, we patch builtin round to keep python2 behavior
+# but this interfers with PIL, because python2 round sometimes
+# returns an int where python3 round is supposed to return a float.
+# This context manager swaps the implementation with the native
+# float on python3, which is NOT THREAD SAFE. Don't use it in
+# production, for now this makes the test pass 🤐.
+import Products.ERP5Type.patches.python
+@contextlib.contextmanager
+def native_round():
+  if six.PY3:
+    round_original = __builtins__['round']
+    try:
+      __builtins__['round'] = Products.ERP5Type.patches.python.round_native
+      yield
+    finally:
+      __builtins__['round'] = round_original
+  else:
+    # on python2 do nothing
+    yield
+
 
 class ZoomifyBase:
 
@@ -179,9 +203,10 @@ class ZoomifyBase:
       saveFilename = root + str(tier) + '-' + str(row) +  ext
       if imageRow.mode != 'RGB':
         imageRow = imageRow.convert('RGB')
-      imageRow.save(os.path.join(tempfile.gettempdir(), saveFilename),
-      # see https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#jpeg-saving
-      # for quality, Values above 95 should be avoided;
+      with native_round():
+        imageRow.save(os.path.join(tempfile.gettempdir(), saveFilename),
+        # see https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#jpeg-saving
+        # for quality, Values above 95 should be avoided;
                                                         'JPEG', quality=95)
       if os.path.exists(os.path.join(tempfile.gettempdir(), saveFilename)):
         self.processRowImage(tier=tier, row=row)
@@ -262,7 +287,8 @@ class ZoomifyBase:
             (imageWidth//2, imageHeight//2),
             PIL_Image.LANCZOS if six.PY3 else PIL_Image.ANTIALIAS,
           )
-          tempImage.save(os.path.join(tempfile.gettempdir(), root + str(tier)
+          with native_round():
+            tempImage.save(os.path.join(tempfile.gettempdir(), root + str(tier)
                                        + '-' + str(row) + ext))
           tempImage = None
       rowImage = None
@@ -541,7 +567,8 @@ class ERP5ZoomifyZopeProcessor(ZoomifyZopeProcessor):
       tile_group_id = self.getAssignedTileContainerName()
       tile_group=self.document[tile_group_id]
       tileImageData= io.BytesIO()
-      image.save(tileImageData, 'JPEG', quality=self.qualitySetting)
+      with native_round():
+        image.save(tileImageData, 'JPEG', quality=self.qualitySetting)
       tileImageData.seek(0)
       if tile_group is None:
         raise AttributeError('unable to fine tile group %r' % tile_group_id)
