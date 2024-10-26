a1e9e48661d4c75c5d1ba4dd8c3092787a233972
Jérome Perrin <jerome@nexedi.com>
2024-10-26 11:43:15 +0900

    wip select methods

--- bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Task_selectMovement.py
+++ bt5/erp5_project/SkinTemplateItem/portal_skins/erp5_project/Task_selectMovement.py
@@ -1,7 +1,4 @@
-kw['explanation_portal_type'] = 'Task'
-kw['parent_specialise_portal_type'] = 'Delivery Simulation Rule'
-kw['grand_parent_simulation_state'] = 'confirmed'
-
+kw['simulation_state'] = 'planned'
 kw['delivery_uid'] = None
 kw['left_join_list'] = ['delivery_uid']
 kw['select_dict'] = dict(delivery_uid=None)
