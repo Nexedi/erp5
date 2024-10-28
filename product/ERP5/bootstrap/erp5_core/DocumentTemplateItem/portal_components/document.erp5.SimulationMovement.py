065d52b6475813b396dee9b1819f77dec708996c
Jérome Perrin <jerome@nexedi.com>
2024-10-28 19:37:26 +0900

    Revert "simulation: introduce Rule.getSimulationMovementSimulationState"
    This reverts commit 5e21f77f9b1bfe9ba07f7eceeec5bc4672844eed.
    
    This was done too quickly based on a wrong assumption that simulation
    movements to build would always be in planned state and that we could
    have an efficient way of selecting them by capago with index on
    portal_type and simulation state, but it does not work this way.
    
    Maybe the change is useful for something else, but since we don't have
    any use case for now, let's just revert.

--- product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.SimulationMovement.py
+++ product/ERP5/bootstrap/erp5_core/DocumentTemplateItem/portal_components/document.erp5.SimulationMovement.py
@@ -36,6 +36,8 @@ from Products.ERP5Type.Utils import ensure_list
 from erp5.component.document.Movement import Movement
 from erp5.component.module.ExpandPolicy import policy_dict, TREE_DELIVERED_CACHE_KEY
 
+from zLOG import LOG, WARNING
+
 from Products.ERP5.mixin.property_recordable import PropertyRecordableMixin
 from erp5.component.mixin.ExplainableMixin import ExplainableMixin
 from erp5.component.interface.IExpandable import IExpandable
@@ -126,8 +128,13 @@ class SimulationMovement(PropertyRecordableMixin, Movement, ExplainableMixin):
   def getSimulationState(self, id_only=1):
     """Returns the current state in simulation
 
-      Inherit from delivery when built, otherwise, let the rule decide.
+      Inherit from delivery or parent (using a conversion table to make orders
+      planned when parent is confirmed).
 
+      In the case of simulation coming from an item, the simulation state is
+      delegated to the item.
+
+      XXX: movements in zero stock rule can not acquire simulation state
     """
     delivery = self.getDeliveryValue()
     if delivery is not None:
@@ -138,9 +145,18 @@ class SimulationMovement(PropertyRecordableMixin, Movement, ExplainableMixin):
       return order.getSimulationState()
 
     applied_rule = self.getParentValue()
-    rule = applied_rule.getSpecialiseValue()
-    if rule is not None:
-      return rule.getSimulationMovementSimulationState(self)
+    parent = applied_rule.getParentValue()
+    try:
+      if isinstance(parent, SimulationMovement):
+        return parent_to_movement_simulation_state[parent.getSimulationState()]
+      getState = applied_rule.getCausalityValue() \
+        .aq_explicit.getSimulationMovementSimulationState
+    except (AttributeError, KeyError):
+      LOG('SimulationMovement.getSimulationState', WARNING,
+          'Could not acquire simulation state from %s'
+          % self.getRelativeUrl(), error=True)
+    else:
+      return getState(self)
 
   security.declareProtected( Permissions.AccessContentsInformation,
                              'getTranslatedSimulationStateTitle')
