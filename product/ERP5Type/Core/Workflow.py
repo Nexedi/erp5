Nexedi/erp5

commit ff624fd2ad85d56aab8ec1691e5c63932547e5b7

ERP5Workflow: newly added permission should be acquired for all existing states.
before, all permissions became acquired in all states once we update permission list.

product/ERP5Type/Core/Workflow.py
@@ -1440,11 +1440,15 @@ class Workflow(XMLObject):
     return res
 
   def _setWorkflowManagedPermissionList(self, permission_list):
+    current_permission_list = self.getWorkflowManagedPermissionList()
     self._baseSetWorkflowManagedPermission(permission_list)
 
     # Add/remove the added/removed Workflow permissions to each state
     for state in self.getStateValueList():
-      state.setAcquirePermissionList(permission_list)
+      state.setAcquirePermissionList(
+        [e for e in state.getAcquirePermissionList() if e in permission_list] + \
+          list(set(permission_list) - set(current_permission_list))
+      )
 
       permission_role_list_dict = state.getStatePermissionRoleListDict()
       state.setStatePermissionRoleListDict({

bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testERP5Workflow.py
@@ -173,11 +173,16 @@ class TestERP5Workflow(ERP5TypeTestCase):
       state1.getStatePermissionRoleListDict(),
       {k: () for k in permission_list},
     )
+    state1.setAcquirePermissionList(['View'])
     state1.setStatePermissionRoleListDict(
       {k: ['Assignor', 'Manager'] for k in permission_list},
     )
     workflow.setWorkflowManagedPermissionList(
       permission_list + ['Delete objects']
+    )
+    self.assertEqual(
+      state1.getAcquirePermissionList(),
+      ['View', 'Delete objects'],
     )
     self.assertEqual(
       state1.getStatePermissionRoleListDict(),

commit cbef6282162ab1b015ebb3536776f66c379931dd

ERP5Workflow: make sure not create duplicate permissions
Fix a problem introduced in ff624fd2ad (ERP5Workflow: newly added
permission should be acquired for all existing states., 2024-11-04),
visible in a test failure

product/ERP5Type/Core/Workflow.py
@@ -1446,9 +1446,9 @@ class Workflow(XMLObject):
     # Add/remove the added/removed Workflow permissions to each state
     for state in self.getStateValueList():
       state.setAcquirePermissionList(
-        [e for e in state.getAcquirePermissionList() if e in permission_list] + \
-          list(set(permission_list) - set(current_permission_list))
-      )
+        list(
+          {e for e in state.getAcquirePermissionList() if e in permission_list}
+          | (set(permission_list) - set(current_permission_list))))
 
       permission_role_list_dict = state.getStatePermissionRoleListDict()
       state.setStatePermissionRoleListDict({