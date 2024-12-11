2ccca1608aa6fad061fcef9707484bccf5258991
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:38:01 +0000

    erp5_discussion: new portal types, forms, scripts

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
@@ -3,10 +3,15 @@
   Due to asynchronous nature of catalog it can use
   passed from REQUEST not index yet threads.
 """
-thread_relative_url = context.REQUEST.get('thread_relative_url')
-discussion_thread_list = [x.getObject() for x  in context.getDocumentValueList(**kw)]
-if thread_relative_url is not None:
-  thread = context.restrictedTraverse(thread_relative_url)
-  if thread is not None and thread not in discussion_thread_list:
-    discussion_thread_list = [thread] + discussion_thread_list
-return discussion_thread_list
+
+forum = context.getDestinationValue()
+if forum is not None:
+  discussion_thread_list = [x.getObject() for x  in forum.searchResults(**kw)] #searches in all erp5 docs linked to this predicate config
+  thread_relative_url = context.REQUEST.get('thread_relative_url')
+  if thread_relative_url is not None:
+    thread = forum.restrictedTraverse(thread_relative_url)
+    if thread is not None and thread not in discussion_thread_list:
+      discussion_thread_list = [thread] + discussion_thread_list
+  return discussion_thread_list
+else:
+  return []
