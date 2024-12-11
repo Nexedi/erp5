d8418fe8c9b30a6f4b409d1c933e1b2068fa5e94
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:38:01 +0000

    erp5_discussion: discussion forum portal type - new portal types, forms, scripts (+refactoring) - permission and security configuration (type roles, wf, proxy roles, etc) - update tests

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionThread_getLastPost.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionThread_getLastPost.py
@@ -1,9 +1,10 @@
 """
   Get last Disccussion Post for a Discussion Thread.
 """
+order = 'ascending' if first else 'descending'
 post_list = context.searchFolder(portal_type='Discussion Post',
                                  limit=1,
-                                 sort_on=[('creation_date','descending')])
+                                 sort_on=[('creation_date',order)])
 if len(post_list) == 0:
   return None
 else:
