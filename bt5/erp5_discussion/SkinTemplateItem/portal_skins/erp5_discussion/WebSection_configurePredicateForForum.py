93421c56b497a381c00e27c67d966f515002715f
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:38:01 +0000

    erp5_discussion: new portal types, forms, scripts

---
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_configurePredicateForForum.py
@@ -0,0 +1,15 @@
+# default predicate configuration
+multimembership_criterion_base_category=["publication_section"]
+membership_criterion_category=["publication_section/forum"]
+# forum predicate configuration
+forum_relative_url = context.REQUEST.get('forum_relative_url')
+if forum_relative_url:
+  forum = context.restrictedTraverse(forum_relative_url)
+  multimembership_criterion_base_category=forum.getMultimembershipCriterionBaseCategoryList()
+  membership_criterion_category=forum.getMembershipCriterionCategoryList()
+
+context.edit(criterion_property=("portal_type",),
+             membership_criterion_base_category=[],
+             multimembership_criterion_base_category=multimembership_criterion_base_category,
+             membership_criterion_category=membership_criterion_category)
+context.setCriterion("portal_type", "Discussion Forum")
