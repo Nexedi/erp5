cb6f1b6ea4b682566f91d122f20ea7d4ac902f47
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:38:01 +0000

    erp5_discussion: new portal types, forms, scripts - permission and security configuration

---
+++ bt5/erp5_discussion/PathTemplateItem/web_page_module/discussion_gadget_latest_post_js.js
@@ -0,0 +1,48 @@
+/*jslint indent: 2, maxerr: 3, nomen: true */
+/*global window, rJS, RSVP, domsugar*/
+(function (window, rJS, RSVP, domsugar) {
+  "use strict";
+
+  rJS(window)
+    //////////////////////////////////////////////
+    // acquired method
+    //////////////////////////////////////////////
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    //////////////////////////////////////////////
+    // initialize the gadget content
+    //////////////////////////////////////////////
+    .declareMethod('render', function render(options) {
+      var gadget = this, jio_key = options.jio_key,
+        view_posts = options.view || "view_posts",
+        last_post = options.last_post || false,
+        author = options.author_dict.author_title,
+        url_options = {
+          command: 'push_history',
+          options: {
+            'jio_key': jio_key,
+            'page': 'form'
+          }
+        };
+      return gadget.getSetting("hateoas_url")
+        .push(function (hateoas_url) {
+          var posts_view = hateoas_url +
+            '/ERP5Document_getHateoas?mode=traverse&relative_url=' +
+            jio_key +
+            '&view=' + view_posts;
+          url_options.options.view = posts_view;
+          if (last_post) {
+            url_options.options.last_page = last_post;
+          }
+          return gadget.getUrlFor(url_options);
+        })
+        .push(function (url) {
+          gadget.element.appendChild(domsugar('a', {
+            href: url,
+            text: author
+          }));
+        });
+    });
+
+}(window, rJS, RSVP, domsugar));
--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
@@ -1,12 +1,14 @@
 """
-  This script gets list of Discussion Thread for a Forum using catalog.
-  Due to asynchronous nature of catalog it can use
-  passed from REQUEST not index yet threads.
+ Old forum backward compatibility script, wraps new DiscussionForum_getDiscussionThreadList
 """
-thread_relative_url = context.REQUEST.get('thread_relative_url')
-discussion_thread_list = [x.getObject() for x  in context.getDocumentValueList(**kw)]
-if thread_relative_url is not None:
-  thread = context.restrictedTraverse(thread_relative_url)
-  if thread is not None and thread not in discussion_thread_list:
-    discussion_thread_list = [thread] + discussion_thread_list
-return discussion_thread_list
+
+# get the related forum using follow_up
+result = context.getFollowUpRelatedValueList(portal_type = "Discussion Forum")
+valid_states = ('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive')
+result = [forum for forum in result if forum.getValidationState() in valid_states]
+if result:
+  forum = result[0]
+else:
+  raise ValueError, 'Unable to found a valid Discussion Forum for the current web site/section'
+
+return forum.DiscussionForum_getValidDiscussionThreadList(**kw)
