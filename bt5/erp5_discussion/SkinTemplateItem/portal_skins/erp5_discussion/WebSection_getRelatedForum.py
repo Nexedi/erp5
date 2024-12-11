09705cef3049e71985035cbd664a33434d399e3c
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:38:01 +0000

    erp5_discussion: discussion forum portal type - new portal types, forms, scripts (+refactoring) - permission and security configuration (type roles, wf, proxy roles, etc) - update tests

---
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getRelatedForum.py
@@ -0,0 +1,10 @@
+"""
+ Old forum backward compatibility script, returns the related forum object created during the migration, using predicate search
+"""
+
+valid_states = ('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive')
+result = [x.getObject() for x  in context.searchResults(portal_type='Discussion Forum', validation_state=valid_states)]
+if result:
+  return result[0]
+else:
+  raise ValueError('Unable to found a valid Discussion Forum for the current web site/section')
