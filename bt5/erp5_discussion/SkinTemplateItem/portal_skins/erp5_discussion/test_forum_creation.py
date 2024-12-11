0e7ac65c6d85d902fe6bd89d8d6fef66bae07357
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-11 20:43:34 +0000

    erp5_discussion: update script

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/test_forum_creation.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/test_forum_creation.py
@@ -18,40 +18,43 @@ def _createObject(from_, mixKW, searchKW={}, newContentKW={}):
 # // utils functions ---------------------------------------------------------
 
 # setup site -----------------------------------------------------------------
-# - web_site_module/site_$id
-test_web_site = _createObject(
-  portal.web_site_module,
-  dict(
-    portal_type = "Web Site",
-    id = test_id + "_web_site"
+try:
+  # - web_site_module/site_$id
+  test_web_site = _createObject(
+    portal.web_site_module,
+    dict(
+      portal_type = "Web Site",
+      id = test_id + "_web_site"
+    )
   )
-)
-# - web_site_module/site_$id/public_websection
-test_public_web_section = _createObject(
-  test_web_site,
-  dict(
-    portal_type = "Web Section",
-    id = "public_section"
+  # - web_site_module/site_$id/public_websection
+  test_public_web_section = _createObject(
+    test_web_site,
+    dict(
+      portal_type = "Web Section",
+      id = "public_section"
+    )
   )
-)
-# - portal_categories/publication_section/blog
-publication_section_blog = _createObject(
-  portal.portal_categories.publication_section, dict(
-    portal_type = "Category",
-    id = "blog"
-  ), newContentKW = dict(title = "Blog")
-)
-publication_section_blog_path = "publication_section/blog"
-# - portal_categories/publication_section/forum
-publication_section_forum = _createObject(
-  portal.portal_categories.publication_section,
-  dict(
-    portal_type = "Category",
-    id = "forum"
-  ),
-  newContentKW = dict(title = "Forum")
-)
-publication_section_forum_path = "publication_section/forum"
+  # - portal_categories/publication_section/blog
+  publication_section_blog = _createObject(
+    portal.portal_categories.publication_section, dict(
+      portal_type = "Category",
+      id = "blog"
+    ), newContentKW = dict(title = "Blog")
+  )
+  publication_section_blog_path = "publication_section/blog"
+  # - portal_categories/publication_section/forum
+  publication_section_forum = _createObject(
+    portal.portal_categories.publication_section,
+    dict(
+      portal_type = "Category",
+      id = "forum"
+    ),
+    newContentKW = dict(title = "Forum")
+  )
+  publication_section_forum_path = "publication_section/forum"
+except:
+  pass
 # // setup site ---------------------------------------------------------------
 
 # create public blog and forum
@@ -64,25 +67,29 @@ try:
   )
 except:
   pass
-# check what was created
-blog = test_public_web_section.searchFolder(
-  portal_type="Web Section",
-  id="blog_%s" % test_id
-)[0].getObject()
-forum = test_public_web_section.searchFolder(
-  portal_type="Web Section",
-  id="forum_%s" % test_id
-)[0].getObject()
 
-print("blog.getMembershipCriterionCategoryList (should be %s):" % publication_section_blog_path)
-print(blog.getMembershipCriterionCategoryList())
-print('blog criterion list (should be ("portal_type", "Web Page")):')
-print([(x.property, x.identity) for x in blog.getCriterionList()])
-print("forum.getMembershipCriterionCategoryList (should be %s):" % publication_section_forum_path)
-print(forum.getMembershipCriterionCategoryList())
-print('forum criterion list (should be ("portal_type", "Discussion Thread")):')
-print([(x.property, x.identity) for x in forum.getCriterionList()])
+try:
+  # check what was created
+  blog = test_public_web_section.searchFolder(
+    portal_type="Web Section",
+    id="blog_%s" % test_id
+  )[0].getObject()
+  forum = test_public_web_section.searchFolder(
+    portal_type="Web Section",
+    id="forum_%s" % test_id
+  )[0].getObject()
 
-print(blog.getRelativeUrl())
-print(forum.getRelativeUrl())
+  print("blog.getMembershipCriterionCategoryList (should be %s):" % publication_section_blog_path)
+  print(blog.getMembershipCriterionCategoryList())
+  print('blog criterion list (should be ("portal_type", "Web Page")):')
+  print([(x.property, x.identity) for x in blog.getCriterionList()])
+  print("forum.getMembershipCriterionCategoryList (should be %s):" % publication_section_forum_path)
+  print(forum.getMembershipCriterionCategoryList())
+  print('forum criterion list (should be ("portal_type", "Discussion Thread")):')
+  print([(x.property, x.identity) for x in forum.getCriterionList()])
+
+  print(blog.getRelativeUrl())
+  print(forum.getRelativeUrl())
+except IndexError:
+  print("re-run for tic()")
 return printed
