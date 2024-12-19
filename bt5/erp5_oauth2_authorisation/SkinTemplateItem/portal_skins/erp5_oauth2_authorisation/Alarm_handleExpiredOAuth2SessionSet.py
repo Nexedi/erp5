62cb58ee0d059a4c703d9d7a4a441d168cb009b7
Vincent Pelletier <vincent@nexedi.com>
2024-12-19 16:24:56 +0100

    Alarm_handleExpiredOAuth2SessionSet: Prevent next iteration from running until all spawned activities are executed
    So reindexations and unindexations are all gone, so the new round of queries
    do not match documents already being processed.

--- bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/Alarm_handleExpiredOAuth2SessionSet.py
+++ bt5/erp5_oauth2_authorisation/SkinTemplateItem/portal_skins/erp5_oauth2_authorisation/Alarm_handleExpiredOAuth2SessionSet.py
@@ -10,6 +10,7 @@ container_value = context.getPortalObject().session_module
 searchFolder = container_value.searchFolder
 deletion_id_list = []
 client_lifespan_accuracy_dict = {}
+action_tag = script.id + '_action'
 def getSessionAccuracyCompensatedExpirationMaxDate(session_value):
   key = session_value.getSource()
   try:
@@ -24,73 +25,68 @@ if deletion_delay_days is None:
   deletion_cutoff = None
 else:
   deletion_cutoff = now - deletion_delay_days
-load_limit = 1000
-for (state_list, catalog_date_condition, expiration_max_date, getSessionExpirationMaxDate, action) in (
-  ( # Draft sessions' expiration date is the time the Authorisation Code expires.
-    # These should be quite rare, as they mean authentication succeeded but was not transformed into a token.
-    # The session did not actually become something, so mark it deleted.
-    ('draft', ),
-    now_catalog_condition,
-    now,
-    getSessionRawExpirationMaxDate,
-    lambda x: x.delete(),
-  ),
-  ( # Validated sessions' expiration date, plus the associated client's accuracy, is the time the Refresh Token expires.
-    # Invalidate the session, as it is now unusable and should not be presented to the user when listing active sessions.
-    ('validated', ),
-    now,
-    now_catalog_condition,
-    getSessionAccuracyCompensatedExpirationMaxDate,
-    lambda x: x.invalidate(),
-  ),
-  (
-    # Invalidated and deleted (state) sessions get deleted after enough time has passed.
-    ('invalidated', 'deleted'),
-    (
-      None
-      if deletion_cutoff is None else
-      '<%f' % deletion_cutoff.timeTime()
+load_limit = 10000
+with context.defaultActivateParameterDict({'tag': action_tag}):
+  for (state_list, catalog_date_condition, expiration_max_date, getSessionExpirationMaxDate, action) in (
+    ( # Draft sessions' expiration date is the time the Authorisation Code expires.
+      # These should be quite rare, as they mean authentication succeeded but was not transformed into a token.
+      # The session did not actually become something, so mark it deleted.
+      ('draft', ),
+      now_catalog_condition,
+      now,
+      getSessionRawExpirationMaxDate,
+      lambda x: x.delete(),
     ),
-    deletion_cutoff,
-    getSessionRawExpirationMaxDate,
-    lambda x: deletion_id_list.append(x.getId()),
-  ),
-):
-  if expiration_max_date is None:
-    continue
-  for state in state_list: # Query with a single state at a time for better SQL index efficiency
-    result_list = searchFolder(
-      portal_type='OAuth2 Session',
-      validation_state=state,
-      float_index=catalog_date_condition,
-      limit=load_limit,
-    )
-    load_limit -= len(result_list)
-    for session_value in result_list:
-      session_value = session_value.getObject()
-      # Recheck document data from ZODB
-      if (
-        session_value.getValidationState() == state and
-        getSessionExpirationMaxDate(session_value) <= expiration_max_date
-      ):
-        action(session_value)
-    if load_limit <= 0:
-      break
-if deletion_id_list:
-  DELETE_CHUNK = 1000
-  active_delObjects = container_value.activate(
-    activity='SQLQueue',
-    priority=10,
-  ).manage_delObjects
-  while deletion_id_list:
-    active_delObjects(ids=deletion_id_list[:DELETE_CHUNK])
-    deletion_id_list = deletion_id_list[DELETE_CHUNK:]
+    ( # Validated sessions' expiration date, plus the associated client's accuracy, is the time the Refresh Token expires.
+      # Invalidate the session, as it is now unusable and should not be presented to the user when listing active sessions.
+      ('validated', ),
+      now,
+      now_catalog_condition,
+      getSessionAccuracyCompensatedExpirationMaxDate,
+      lambda x: x.invalidate(),
+    ),
+    (
+      # Invalidated and deleted (state) sessions get deleted after enough time has passed.
+      ('invalidated', 'deleted'),
+      (
+        None
+        if deletion_cutoff is None else
+        '<%f' % deletion_cutoff.timeTime()
+      ),
+      deletion_cutoff,
+      getSessionRawExpirationMaxDate,
+      lambda x: deletion_id_list.append(x.getId()),
+    ),
+  ):
+    if expiration_max_date is None:
+      continue
+    for state in state_list: # Query with a single state at a time for better SQL index efficiency
+      result_list = searchFolder(
+        portal_type='OAuth2 Session',
+        validation_state=state,
+        float_index=catalog_date_condition,
+        limit=load_limit,
+      )
+      load_limit -= len(result_list)
+      for session_value in result_list:
+        session_value = session_value.getObject()
+        # Recheck document data from ZODB
+        if (
+          session_value.getValidationState() == state and
+          getSessionExpirationMaxDate(session_value) <= expiration_max_date
+        ):
+          action(session_value)
+      if load_limit <= 0:
+        break
+  if deletion_id_list:
+    container_value.manage_delObjects(ids=deletion_id_list)
 if load_limit <= 0:
   # The load quota was exhausted, there may be more to cleanup
   getattr(
     context.activate(
       activity='SQLQueue',
       priority=11,
+      after_tag=action_tag,
     ),
     script.id,
   )()
