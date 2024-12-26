00b27d9b4b3098476c28c9b3bf4cecaa28b21e18
Roque Porchetto <roque.porchetto@nexedi.com>
2024-12-26 15:00:53 +0000

    erp5_discussion: forum_init

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_init.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_init.py
@@ -6,14 +6,19 @@ multimembership_criterion_base_category_set.update(membership_criterion_base_cat
 multimembership_criterion_base_category_set.add("publication_section")
 membership_criterion_base_category_list = []
 multimembership_criterion_base_category_list = list(multimembership_criterion_base_category_set)
-membership_criterion_category_list = context.getMembershipCriterionCategoryList()
+membership_criterion_category_list = [] #context.getMembershipCriterionCategoryList()
 
 context.setTitle("Forum")
-context.setDefaultPageDisplayed(True)
-context.setCustomRenderMethodId("WebSection_viewDiscussionThreadForm")
-context.setCriterionProperty("portal_type")
-context.setEmptyCriterionValid(True)
-context.setMembershipCriterionBaseCategory(membership_criterion_base_category_list)
-context.setMultimembershipCriterionBaseCategory(multimembership_criterion_base_category_list)
-context.setMembershipCriterionCategory(membership_criterion_category_list + ["publication_section/forum"])
-context.setCriterion("portal_type", "Discussion Thread")
+context.edit(custom_render_method_id="WebSection_viewDiscussionThreadForm",
+             default_page_displayed=True,
+             criterion_property="portal_type",
+             empty_criterion_valid=True,
+             membership_criterion_base_category=membership_criterion_base_category_list,
+             multimembership_criterion_base_category=multimembership_criterion_base_category_list,
+             membership_criterion_category=membership_criterion_category_list + ["publication_section/forum"]) #???
+#context.setMembershipCriterionBaseCategory(membership_criterion_base_category_list)
+#context.setMultimembershipCriterionBaseCategory(multimembership_criterion_base_category_list)
+#context.setMembershipCriterionCategory(membership_criterion_category_list + ["publication_section/forum"])
+
+# TODO FIX
+#context.setCriterion("portal_type", "Discussion Thread")
