fe44b56f48d7f341f7f0cdbe6e46f3d8d7ef12e8
Rafael Monnerat <rafael@nexedi.com>
2025-01-07 02:42:49 +0100

    fixup! erp5_computer_immobilisation: Missing init script

--- bt5/erp5_computer_immobilisation/SkinTemplateItem/portal_skins/erp5_computer_immobilisation/ComputeNode_init.py
+++ bt5/erp5_computer_immobilisation/SkinTemplateItem/portal_skins/erp5_computer_immobilisation/ComputeNode_init.py
@@ -1,9 +1,1 @@
-if not context.hasUserId():
-  context.setUserId(
-    'C%i' % (
-      context.getPortalObject().portal_ids.generateNewId(
-        id_group='user_id',
-        id_generator='non_continuous_integer_increasing',
-      ),
-    ),
-  )
+return
