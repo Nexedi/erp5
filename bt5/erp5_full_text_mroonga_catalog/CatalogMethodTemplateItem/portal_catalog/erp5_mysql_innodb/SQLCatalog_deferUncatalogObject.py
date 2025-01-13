deecd8b1d921871dbbe25cd57969b374c2ee4c10
Kazuhiko SHIOZAKI <kazuhiko@nexedi.com>
2025-01-13 14:35:52 +0100

    erp5_full_text_mroonga_catalog: defer fulltext index and unindex using dedicated catalog keys.

--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObject.py
+++ bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObject.py
@@ -1,7 +1,6 @@
-# This script is called to defer fulltext indexing.
+# This script is called to defer fulltext unindexing.
 METHOD_ID = script.id + 'Activity'
 GROUP_METHOD_ID = context.getRelativeUrl() + '/' + METHOD_ID
-activateObject = context.getPortalObject().portal_activities.activateObject
 try:
   priority = context.getActivityRuntimeEnvironment().getPriority()
 except KeyError:
@@ -13,17 +12,9 @@ except KeyError:
 # Optimise cache usage by reducing the likelyhood of a processing node
 # including activities spawned by others into its activity group.
 # But prevent the group from going below 10 activities, for better throughput.
-group_method_cost = min(.1, 1. / len(getPath))
-for document in getPath:
-  getattr(
-    activateObject(
-      document,
-      activity='SQLDict',
-      priority=priority,
-      node='same',
-      group_method_id=GROUP_METHOD_ID,
-      group_method_cost=group_method_cost,
-      serialization_tag='full_text_' + document,
-    ),
-    METHOD_ID,
-  )()
+context.activate(
+  activity='SQLQueue',
+  priority=priority,
+  node='same',
+  group_method_id=GROUP_METHOD_ID,
+).deferUnindexObject(uid=uid)
