63088700ccbee8bd90409de6725b788f6bcf2f10
Roque Porchetto <roque.porchetto@nexedi.com>
2025-01-14 21:10:32 +0000

    erp5_discussion: backward compatibility

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_getDiscussionThreadList.py
@@ -4,12 +4,18 @@
   passed from REQUEST not index yet threads.
 """
 thread_relative_url = context.REQUEST.get('thread_relative_url')
-related_forum_url = context.follow_up
-if related_forum_url is not None:
-  forum = context.restrictedTraverse(related_forum_url)
+# backward compatibility for web section (this should be dropped)
+if context.getPortalType() == 'Web Section':
+  related_forum_url = context.follow_up
+  if related_forum_url is not None:
+    forum = context.restrictedTraverse(related_forum_url)
+    discussion_thread_list = [x.getObject() for x  in forum.getDocumentValueList(**kw)] #searches in all erp5 docs linked to this predicate config
+  else:
+    discussion_thread_list = []
+if context.getPortalType() == 'Discussion Forum':
+  forum = context
   discussion_thread_list = [x.getObject() for x  in forum.getDocumentValueList(**kw)] #searches in all erp5 docs linked to this predicate config
-else:
-  discussion_thread_list = []
+
 if thread_relative_url is not None:
   thread = forum.restrictedTraverse(thread_relative_url)
   if thread is not None and thread not in discussion_thread_list:
