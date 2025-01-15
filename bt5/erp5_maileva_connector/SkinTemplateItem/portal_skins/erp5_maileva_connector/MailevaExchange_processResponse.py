e4ce15f2f345bf8a02d66aee40704ed86d331839
Rafael Monnerat <rafael@nexedi.com>
2025-01-15 11:58:58 -0300

    erp5_maileva_connector: Fixup coding style test
      'Just' a typo

--- bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/MailevaExchange_processResponse.py
+++ bt5/erp5_maileva_connector/SkinTemplateItem/portal_skins/erp5_maileva_connector/MailevaExchange_processResponse.py
@@ -1,6 +1,6 @@
 from zExceptions import Unauthorized
 if REQUEST is not None:
-    raise Unauthorized
+  raise Unauthorized
 
 connector = context.getResourceValue()
 connector.processResponse(response, context.getObject(), failed)
