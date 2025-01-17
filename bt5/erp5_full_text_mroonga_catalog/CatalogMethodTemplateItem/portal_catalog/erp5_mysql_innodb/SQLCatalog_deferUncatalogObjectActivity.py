77f7a03bc4dd2e8a459c826dd4f630719a9cfe16
Jérome Perrin <jerome@nexedi.com>
2025-01-17 18:20:30 +0900

    full_text_mroonga_catalog: fix a PY3 regression
    from 031408c61d (erp5_full_text_mroonga_catalog: defer fulltext index
    and unindex using dedicated catalog keys., 2025-01-13)

--- bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.py
+++ bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.py
@@ -1,7 +1,9 @@
+contains_more_than_uid = set(['uid']).isdisjoint
+
 uid_list = []
 for grouped_message in grouped_message_list:
   assert not grouped_message.args
-  assert grouped_message.kw.keys() == ['uid']
+  assert not contains_more_than_uid(grouped_message.kw)
   uid_list.append(grouped_message.kw['uid'])
 for method_id in context.getSqlDeferredUncatalogObjectList():
   method = getattr(context, method_id)
