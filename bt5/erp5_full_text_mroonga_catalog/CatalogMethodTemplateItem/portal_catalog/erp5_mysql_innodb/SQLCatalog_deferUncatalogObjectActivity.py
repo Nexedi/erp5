Nexedi/erp5

commit 031408c61d5351e9f6087e61608eefc1fd42710e

erp5_full_text_mroonga_catalog: defer fulltext index and unindex using dedicated catalog keys.

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.py
@@ -1,0 +1,10 @@
+uid_list = []
+for grouped_message in grouped_message_list:
+  assert not grouped_message.args
+  assert grouped_message.kw.keys() == ['uid']
+  uid_list.append(grouped_message.kw['uid'])
+for method_id in context.getSqlDeferredUncatalogObjectList():
+  method = getattr(context, method_id)
+  method(uid=uid_list)
+for grouped_message in grouped_message_list:
+  grouped_message.result = None

bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testCopySupport.py
@@ -134,8 +134,11 @@ class TestCopySupport(ERP5TypeTestCase):
       self.tic()
     finally:
       ActivityTool.invokeGroup = ActivityTool_invokeGroup
-    self.assertEqual(invokeGroup_list,
-      ['portal_catalog/uncatalogObjectList', uid_list, 'SQLQueue', False])
+    catalog_relative_url = self.portal.portal_catalog.getSQLCatalog().getRelativeUrl()
+    self.assertEqual(invokeGroup_list, [
+      'portal_catalog/uncatalogObjectList', uid_list, 'SQLQueue', False,
+      catalog_relative_url + '/SQLCatalog_deferUncatalogObjectActivity', uid_list, 'SQLQueue', False,
+    ])
     self.assertEqual(len(search_catalog(uid=uid_list)), 0)
 
 

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferFullTextIndexActivity.py
@@ -3,31 +3,31 @@ from ZODB.POSException import ConflictError
 from zExceptions import Unauthorized
 import six
 
-method = context.z_catalog_fulltext_list
-property_list = method.arguments_src.split()
-parameter_dict = {x: [] for x in property_list}
-for group_object in object_list:
-  tmp_dict = {}
-  try:
-    obj = group_object.object
-    for property in property_list:
-      getter = getattr(obj, property, None)
-      if callable(getter):
-        value = getter()
-      else:
-        value = getattr(obj, 'get%s' % UpperCase(property))()
-      tmp_dict[property] = value
-  except ConflictError:
-    raise
-  except Unauthorized: # should happen in tricky testERP5Catalog tests only
-    # Fake activity success: if indexation cannot View document, ignore it.
-    group_object.result = None
-  except Exception:
-    group_object.raised()
-  else:
-    for property, value in six.iteritems(tmp_dict):
-      parameter_dict[property].append(value)
-    group_object.result = None
-
-if parameter_dict:
-  return method(**parameter_dict)
+for method_id in context.getSqlDeferredCatalogObjectListList():
+  method = getattr(context, method_id)
+  property_list = method.arguments_src.split()
+  parameter_dict = {x: [] for x in property_list}
+  for group_object in object_list:
+    tmp_dict = {}
+    try:
+      obj = group_object.object
+      for property in property_list:
+        getter = getattr(obj, property, None)
+        if callable(getter):
+          value = getter()
+        else:
+          value = getattr(obj, 'get%s' % UpperCase(property))()
+        tmp_dict[property] = value
+    except ConflictError:
+      raise
+    except Unauthorized: # should happen in tricky testERP5Catalog tests only
+      # Fake activity success: if indexation cannot View document, ignore it.
+      group_object.result = None
+    except Exception:
+      group_object.raised()
+    else:
+      for property, value in six.iteritems(tmp_dict):
+        parameter_dict[property].append(value)
+      group_object.result = None
+  if parameter_dict:
+    method(**parameter_dict)

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObject.catalog_keys.xml
@@ -1,5 +1,5 @@
 <catalog_method>
- <item key="sql_deferred_catalog_object_list" type="int">
+ <item key="sql_uncatalog_object" type="int">
   <value>1</value>
  </item>
 </catalog_method>

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObject.py
@@ -1,7 +1,6 @@
-# This script is called to defer fulltext indexing.
+# This script is called to defer fulltext unindexing.
 METHOD_ID = script.id + 'Activity'
 GROUP_METHOD_ID = context.getRelativeUrl() + '/' + METHOD_ID
-activateObject = context.getPortalObject().portal_activities.activateObject
 try:
   priority = context.getActivityRuntimeEnvironment().getPriority()
 except KeyError:
@@ -12,18 +11,11 @@ except KeyError:
   priority = 1
 # Optimise cache usage by reducing the likelyhood of a processing node
 # including activities spawned by others into its activity group.
-# But prevent the group from going below 10 activities, for better throughput.
-group_method_cost = min(.1, 1. / len(getPath))
-for document in getPath:
-  getattr(
-    activateObject(
-      document,
-      activity='SQLDict',
-      priority=priority,
-      node='same',
-      group_method_id=GROUP_METHOD_ID,
-      group_method_cost=group_method_cost,
-      serialization_tag='full_text_' + document,
-    ),
-    METHOD_ID,
-  )()
+# Use unindexObject as dummy method id for compatibility with
+#   after_method_id=('unindexObject', ...)
+context.activate(
+  activity='SQLQueue',
+  priority=priority,
+  node='same',
+  group_method_id=GROUP_METHOD_ID,
+).unindexObject(uid=uid)

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObject.xml
@@ -1,0 +1,72 @@
+<?xml version="1.0"?>
+<ZopeData>
+  <record id="1" aka="AAAAAAAAAAE=">
+    <pickle>
+      <global name="Python Script" module="erp5.portal_type"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>_bind_names</string> </key>
+            <value>
+              <object>
+                <klass>
+                  <global name="_reconstructor" module="copy_reg"/>
+                </klass>
+                <tuple>
+                  <global name="NameAssignments" module="Shared.DC.Scripts.Bindings"/>
+                  <global name="object" module="__builtin__"/>
+                  <none/>
+                </tuple>
+                <state>
+                  <dictionary>
+                    <item>
+                        <key> <string>_asgns</string> </key>
+                        <value>
+                          <dictionary>
+                            <item>
+                                <key> <string>name_container</string> </key>
+                                <value> <string>container</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_context</string> </key>
+                                <value> <string>context</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_m_self</string> </key>
+                                <value> <string>script</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_subpath</string> </key>
+                                <value> <string>traverse_subpath</string> </value>
+                            </item>
+                          </dictionary>
+                        </value>
+                    </item>
+                  </dictionary>
+                </state>
+              </object>
+            </value>
+        </item>
+        <item>
+            <key> <string>_params</string> </key>
+            <value> <string>uid</string> </value>
+        </item>
+        <item>
+            <key> <string>description</string> </key>
+            <value>
+              <none/>
+            </value>
+        </item>
+        <item>
+            <key> <string>id</string> </key>
+            <value> <string>SQLCatalog_deferUncatalogObject</string> </value>
+        </item>
+        <item>
+            <key> <string>portal_type</string> </key>
+            <value> <string>Python Script</string> </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+</ZopeData>

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.catalog_keys.xml
@@ -1,0 +1,2 @@
+<catalog_method>
+</catalog_method>

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.xml
@@ -1,0 +1,80 @@
+<?xml version="1.0"?>
+<ZopeData>
+  <record id="1" aka="AAAAAAAAAAE=">
+    <pickle>
+      <global name="Python Script" module="erp5.portal_type"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>_bind_names</string> </key>
+            <value>
+              <object>
+                <klass>
+                  <global name="_reconstructor" module="copy_reg"/>
+                </klass>
+                <tuple>
+                  <global name="NameAssignments" module="Shared.DC.Scripts.Bindings"/>
+                  <global name="object" module="__builtin__"/>
+                  <none/>
+                </tuple>
+                <state>
+                  <dictionary>
+                    <item>
+                        <key> <string>_asgns</string> </key>
+                        <value>
+                          <dictionary>
+                            <item>
+                                <key> <string>name_container</string> </key>
+                                <value> <string>container</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_context</string> </key>
+                                <value> <string>context</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_m_self</string> </key>
+                                <value> <string>script</string> </value>
+                            </item>
+                            <item>
+                                <key> <string>name_subpath</string> </key>
+                                <value> <string>traverse_subpath</string> </value>
+                            </item>
+                          </dictionary>
+                        </value>
+                    </item>
+                  </dictionary>
+                </state>
+              </object>
+            </value>
+        </item>
+        <item>
+            <key> <string>_params</string> </key>
+            <value> <string>grouped_message_list</string> </value>
+        </item>
+        <item>
+            <key> <string>_proxy_roles</string> </key>
+            <value>
+              <tuple>
+                <string>Manager</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>description</string> </key>
+            <value>
+              <none/>
+            </value>
+        </item>
+        <item>
+            <key> <string>id</string> </key>
+            <value> <string>SQLCatalog_deferUncatalogObjectActivity</string> </value>
+        </item>
+        <item>
+            <key> <string>portal_type</string> </key>
+            <value> <string>Python Script</string> </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+</ZopeData>

commit 8631cc5dead32a22d6f3da1a4866527450983554

full_text_mroonga_catalog: fix a PY3 regression
from 031408c61d (erp5_full_text_mroonga_catalog: defer fulltext index
and unindex using dedicated catalog keys., 2025-01-13)

bt5/erp5_full_text_mroonga_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/SQLCatalog_deferUncatalogObjectActivity.py
@@ -1,7 +1,8 @@
+from Products.ERP5Type.Utils import ensure_list
 uid_list = []
 for grouped_message in grouped_message_list:
   assert not grouped_message.args
-  assert grouped_message.kw.keys() == ['uid']
+  assert ensure_list(grouped_message.kw.keys()) == ['uid']
   uid_list.append(grouped_message.kw['uid'])
 for method_id in context.getSqlDeferredUncatalogObjectList():
   method = getattr(context, method_id)