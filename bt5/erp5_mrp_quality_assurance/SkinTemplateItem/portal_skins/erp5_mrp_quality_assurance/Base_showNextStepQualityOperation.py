51e6a55d835d9a5d949b9fb50a08301506f742aa
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2025-01-20 14:37:24 +0000

    erp5_mrp_quality_assurance: do some improvement
    1. add missing categories
    2. remove no need code

--- bt5/erp5_mrp_quality_assurance/SkinTemplateItem/portal_skins/erp5_mrp_quality_assurance/Base_showNextStepQualityOperation.py
+++ bt5/erp5_mrp_quality_assurance/SkinTemplateItem/portal_skins/erp5_mrp_quality_assurance/Base_showNextStepQualityOperation.py
@@ -1,8 +1,11 @@
 po = me.getCausalityValue(portal_type='Production Order')
 if po:
-  ME_dict = po.ProductionOrder_getRelatedManufacturingExecutionDict()
-  quality_me = ME_dict['quality_execution']
-
+  portal = context.getPortalObject()
+  quality_me = portal.portal_catalog.getResultValue(
+    portal_type='Manufacturing Execution',
+    strict_causality_uid = po.getUid(),
+    strict_ledger_uid=portal.portal_categories.ledger.manufacturing.quality_insurance.getUid()
+  )
   line_list = quality_me.objectValues(sort_on=(('int_index', 'ascending'), ))
   for line in line_list:
     target = line.getAggregateValue(portal_type=("Quality Control", "Traceability", "Gate", "SMON", "ACOM"), checked_permission='View')
