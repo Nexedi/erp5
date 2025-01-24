d2a64e7e7434dee3a609e8542bb9b0b6d587f4e4
Titouan Soulard <titouan.soulard@rapid.space>
2025-01-24 13:50:41 +0900

    erp5_trade: merge `isQuantityEditable` and `isQuantityEnabled` scripts

--- bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_isQuantityEnabled.py
+++ bt5/erp5_trade/SkinTemplateItem/portal_skins/erp5_trade/Movement_isQuantityEnabled.py
@@ -1,1 +1,14 @@
-return context.isMovement() and not context.getVariationCategoryList()
+"""This script is used to know if quantity can be seen and edited by user.
+
+* If this is not a movement (line containing lines or cell), user
+cannot edit this line which is just a container, but no actual movement.
+
+* If this line has variation category list, then it means it's a line that
+will contain cell, so it's already not possible to set quantity, user have
+to create cells and set quantities on cells.
+"""
+
+if not context.isMovement():
+  return False
+
+return (not context.getVariationCategoryList(omit_optional_variation=1)) or ('Cell' in context.getPortalType())
