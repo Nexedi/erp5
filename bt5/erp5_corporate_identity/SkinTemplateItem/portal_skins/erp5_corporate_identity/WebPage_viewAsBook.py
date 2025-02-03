Nexedi/erp5

commit 1b555dbf6827d420ffd594a2308c4b273035c01d

py2/py3: Make Products code compatible with both python2 and python3 (nexedi/erp5!1751).
* Zope API changes:
  + publish():
    - stdin is now BytesIO rather than StringIO.
    - Returned value of a script is passed to str() in python2, not in python3 anymore.
  + HTTPResponse `body` property is now bytes().
  + OFS.Image.File file parameter is bytes().
  + zope.interface implements() is now @implementer decorator.
  + Python standard logging module recommended instead of zLOG.
* Python3 API changes:
  + builtin reduce() was removed.
  + urlnorm is now available.
  + Use BytesIO rather than StringIO to follow py3 API.
  + hmac.new() requires digestmod argument from Python 3.8.
  + Use six.moves library to handle moved objects from py2 to py3.
  + `modernize -f xrange_six` then slightly adjusted manually to just use range
     where it does not make a significant difference (for example in test).
  + base64.b64encode() now expects bytes().
  + UserDict() interface changed:
    - New parameter in update() and pop().
    - `failobj` setdefault parameter renamed to `default`.
  + ensure_list() on dict.{values,items}() and list(dict) for dict.keys() when
    we really need a list and not an iterable (Python3).
  + Make dict iteration works on both version of Python.
    - Use six.iter{items,values,keys}().
    - has_key() has been removed.
    - Make sure that dict.{items,values,keys}() returns a real list when
    modified (ensure_list()).
  + Comparisons between int and NoneType raises TypeError.
    + BTrees key must be str() not int() (_getOb()).
  + No more unbound methods in python3 so use six.get_unbound_function().
  + Exceptions:
    - No longer behave as sequences. Using `args` attribute instead.
    - When an exception has been assigned using `as target`, it is cleared at
      the end of the except clause.
  + file: py2 was returning `str` upon reading, now it returns text strings.
    Also, opening mode is text strings by default.

bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_viewAsBook.py
@@ -33,8 +33,10 @@ MAIN FILE: generate book in different output formats
 # display_svg                           format for svg images (svg, png*)
 
 import re
+import six
 
 from Products.PythonScripts.standard import html_quote
+from Products.ERP5Type.Utils import str2bytes, bytes2str, unicode2str
 from base64 import b64encode
 
 blank = ''
@@ -83,9 +85,8 @@ book_version = html_quote(override_document_version) if override_document_versio
 book_description = html_quote(override_document_description) if override_document_description else book.getDescription()
 book_title = html_quote(override_document_title) if override_document_title else book.getTitle()
 
-# unicode
-if isinstance(book_content, unicode):
-  book_content = book_content.encode("UTF-8")
+if six.PY2 and isinstance(book_content, six.text_type):
+  book_content = unicode2str(book_content)
 
 # backcompat
 book_history_section_list = re.findall('<section.+?>.+?</section>', book_content, re.S)
@@ -188,16 +189,18 @@ if book_include_reference_table:
     book_table_list=book_table_list
   )
   #if book_format == 'html' or book_format == 'mhtml':
-  #  book_references = book_references.encode('UTF-8').strip()
+  #  book_references = unicode2str(book_references).strip()
 
   # backcompat for manual history tables at the beginning of documents
   # NOTE: assumes <section>s are not used elsewhere!
   #if len(book_history_section_list) > 0:
-  #  book_content = book_content.replace(book_history_section_list[-1], (book_history_section_list[-1] + book_references.encode('UTF-8').strip()))
+  #  book_content = book_content.replace(book_history_section_list[-1], (book_history_section_list[-1] + unicode2str(book_references).strip()))
   #else:
-  #  book_content = book_content.replace("${WebPage_insertTableOfReferences}", book_references.encode('UTF-8').strip())
+  #  book_content = book_content.replace("${WebPage_insertTableOfReferences}", unicode2str(book_references).strip())
   book_references = book.Base_unescape(book_references)
-  book_content = book_content.replace("${WebPage_insertTableOfReferences}", book_references.encode('UTF-8').strip())
+  if six.PY2:
+    book_references = unicode2str(book_references)
+  book_content = book_content.replace("${WebPage_insertTableOfReferences}", book_references.strip())
 else:
   book_content = book_content.replace("${WebPage_insertTableOfReferences}", blank)
 
@@ -209,7 +212,7 @@ if book_include_content_table:
     book_table_of_content = book.WebPage_createBookXslTableOfContent(
       book_toc_title=book_translated_toc_title,
       margin_15mm = margin_15mm
-    ).encode('UTF-8').strip()
+    ).strip()
   elif book_format == "html":
     book_content, book_table_of_content = book.WebPage_createTableOfContent(
       doc_content=book_content,
@@ -357,22 +360,22 @@ elif book_format == "pdf":
   )
 
   # ================ encode and build cloudoo elements =========================
-  header_embedded_html_data = book.Base_convertHtmlToSingleFile(book_head, allow_script=True)
+  header_embedded_html_data = str2bytes(book.Base_convertHtmlToSingleFile(book_head, allow_script=True))
   before_toc_data_list = [
-    b64encode(book.Base_convertHtmlToSingleFile(book_cover, allow_script=True)),
+    bytes2str(b64encode(str2bytes(book.Base_convertHtmlToSingleFile(book_cover, allow_script=True)))),
   ]
   after_toc_data_list = []
   if book_include_history_table:
     before_toc_data_list.append(
-      b64encode(book.Base_convertHtmlToSingleFile(book_history, allow_script=True))
+      bytes2str(b64encode(str2bytes(book.Base_convertHtmlToSingleFile(book_history, allow_script=True))))
     )
   #if book_include_reference_table:
   #  after_toc_data_list.append(
-  #    b64encode(book.Base_convertHtmlToSingleFile(book_references, allow_script=True))
+  #    bytes2str(b64encode(str2bytes(book.Base_convertHtmlToSingleFile(book_references, allow_script=True))))
   #  )
-  xsl_style_sheet_data = book_table_of_content
-  embedded_html_data = book.Base_convertHtmlToSingleFile(book_content, allow_script=True)
-  footer_embedded_html_data = book.Base_convertHtmlToSingleFile(book_foot, allow_script=True)
+  xsl_style_sheet_data = str2bytes(book_table_of_content)
+  embedded_html_data = str2bytes(book.Base_convertHtmlToSingleFile(book_content, allow_script=True))
+  footer_embedded_html_data = str2bytes(book.Base_convertHtmlToSingleFile(book_foot, allow_script=True))
   if margin_15mm:
     margin_top = 50
     margin_bottom = 25
@@ -385,11 +388,11 @@ elif book_format == "pdf":
     margin_bottom=margin_bottom,
     toc=True if book_include_content_table else False,
     before_toc_data_list=before_toc_data_list,
-    xsl_style_sheet_data=b64encode(xsl_style_sheet_data),
+    xsl_style_sheet_data=bytes2str(b64encode(xsl_style_sheet_data)),
     after_toc_data_list=after_toc_data_list,
-    header_html_data=b64encode(header_embedded_html_data),
+    header_html_data=bytes2str(b64encode(header_embedded_html_data)),
     header_spacing=10,
-    footer_html_data=b64encode(footer_embedded_html_data),
+    footer_html_data=bytes2str(b64encode(footer_embedded_html_data)),
     footer_spacing=3,
     )
   )

commit a1fcf9dcbd51f50cad689e3ad9bf98b90b434d04

erp5_corporate_identity&test: fix UnicodeEncodeError when export page in other language
it's a regression introduced by https://lab.nexedi.com/nexedi/erp5/-/commit/1b555dbf6827d420ffd594a2308c4b273035c01d

bt5/erp5_corporate_identity/SkinTemplateItem/portal_skins/erp5_corporate_identity/WebPage_viewAsBook.py
@@ -211,7 +211,7 @@ if book_include_content_table:
     book_table_of_content = book.WebPage_createBookXslTableOfContent(
       book_toc_title=book_translated_toc_title,
       margin_15mm = margin_15mm
-    ).strip()
+    ).encode('utf-8').strip()
   elif book_format == "html":
     book_content, book_table_of_content = book.WebPage_createTableOfContent(
       doc_content=book_content,

bt5/erp5_corporate_identity_test/PathTemplateItem/image_module/template_test_image_test_pdfBookInFrench.xml
@@ -1,0 +1,283 @@
+<?xml version="1.0"?>
+<ZopeData>
+  <record id="1" aka="AAAAAAAAAAE=">
+    <pickle>
+      <global name="Image" module="erp5.portal_type"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>_Access_contents_information_Permission</string> </key>
+            <value>
+              <tuple>
+                <string>Assignee</string>
+                <string>Assignor</string>
+                <string>Manager</string>
+                <string>Owner</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>_Add_portal_content_Permission</string> </key>
+            <value>
+              <tuple>
+                <string>Assignee</string>
+                <string>Assignor</string>
+                <string>Manager</string>
+                <string>Owner</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>_Change_local_roles_Permission</string> </key>
+            <value>
+              <tuple>
+                <string>Assignor</string>
+                <string>Manager</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>_Modify_portal_content_Permission</string> </key>
+            <value>
+              <tuple>
+                <string>Assignee</string>
+                <string>Assignor</string>
+                <string>Manager</string>
+                <string>Owner</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>_View_Permission</string> </key>
+            <value>
+              <tuple>
+                <string>Assignee</string>
+                <string>Assignor</string>
+                <string>Manager</string>
+                <string>Owner</string>
+              </tuple>
+            </value>
+        </item>
+        <item>
+            <key> <string>_count</string> </key>
+            <value>
+              <persistent> <string encoding="base64">AAAAAAAAAAI=</string> </persistent>
+            </value>
+        </item>
+        <item>
+            <key> <string>_mt_index</string> </key>
+            <value>
+              <persistent> <string encoding="base64">AAAAAAAAAAM=</string> </persistent>
+            </value>
+        </item>
+        <item>
+            <key> <string>_tree</string> </key>
+            <value>
+              <persistent> <string encoding="base64">AAAAAAAAAAQ=</string> </persistent>
+            </value>
+        </item>
+        <item>
+            <key> <string>content_md5</string> </key>
+            <value> <string>ef1e1722c92bbcd159b1b1c1697253f3</string> </value>
+        </item>
+        <item>
+            <key> <string>content_type</string> </key>
+            <value> <string>image/png</string> </value>
+        </item>
+        <item>
+            <key> <string>height</string> </key>
+            <value> <int>842</int> </value>
+        </item>
+        <item>
+            <key> <string>id</string> </key>
+            <value> <string>template_test_image_test_pdfBookInFrench</string> </value>
+        </item>
+        <item>
+            <key> <string>width</string> </key>
+            <value> <int>595</int> </value>
+        </item>
+        <item>
+            <key> <string>workflow_history</string> </key>
+            <value>
+              <persistent> <string encoding="base64">AAAAAAAAAAU=</string> </persistent>
+            </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+  <record id="2" aka="AAAAAAAAAAI=">
+    <pickle>
+      <global name="Length" module="BTrees.Length"/>
+    </pickle>
+    <pickle> <int>0</int> </pickle>
+  </record>
+  <record id="3" aka="AAAAAAAAAAM=">
+    <pickle>
+      <global name="OOBTree" module="BTrees.OOBTree"/>
+    </pickle>
+    <pickle>
+      <none/>
+    </pickle>
+  </record>
+  <record id="4" aka="AAAAAAAAAAQ=">
+    <pickle>
+      <global name="OOBTree" module="BTrees.OOBTree"/>
+    </pickle>
+    <pickle>
+      <none/>
+    </pickle>
+  </record>
+  <record id="5" aka="AAAAAAAAAAU=">
+    <pickle>
+      <global name="PersistentMapping" module="Persistence.mapping"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>data</string> </key>
+            <value>
+              <dictionary>
+                <item>
+                    <key> <string>document_publication_workflow</string> </key>
+                    <value>
+                      <persistent> <string encoding="base64">AAAAAAAAAAY=</string> </persistent>
+                    </value>
+                </item>
+                <item>
+                    <key> <string>edit_workflow</string> </key>
+                    <value>
+                      <persistent> <string encoding="base64">AAAAAAAAAAc=</string> </persistent>
+                    </value>
+                </item>
+              </dictionary>
+            </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+  <record id="6" aka="AAAAAAAAAAY=">
+    <pickle>
+      <global name="WorkflowHistoryList" module="Products.ERP5Type.Workflow"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>_log</string> </key>
+            <value>
+              <list>
+                <dictionary>
+                  <item>
+                      <key> <string>action</string> </key>
+                      <value>
+                        <none/>
+                      </value>
+                  </item>
+                  <item>
+                      <key> <string>actor</string> </key>
+                      <value> <unicode>zope</unicode> </value>
+                  </item>
+                  <item>
+                      <key> <string>comment</string> </key>
+                      <value> <string></string> </value>
+                  </item>
+                  <item>
+                      <key> <string>error_message</string> </key>
+                      <value> <string></string> </value>
+                  </item>
+                  <item>
+                      <key> <string>time</string> </key>
+                      <value>
+                        <object>
+                          <klass>
+                            <global name="_reconstructor" module="copy_reg"/>
+                          </klass>
+                          <tuple>
+                            <global name="DateTime" module="DateTime.DateTime"/>
+                            <global name="object" module="__builtin__"/>
+                            <none/>
+                          </tuple>
+                          <state>
+                            <tuple>
+                              <float>1738594190.45</float>
+                              <string>UTC</string>
+                            </tuple>
+                          </state>
+                        </object>
+                      </value>
+                  </item>
+                  <item>
+                      <key> <string>validation_state</string> </key>
+                      <value> <string>draft</string> </value>
+                  </item>
+                </dictionary>
+              </list>
+            </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+  <record id="7" aka="AAAAAAAAAAc=">
+    <pickle>
+      <global name="WorkflowHistoryList" module="Products.ERP5Type.Workflow"/>
+    </pickle>
+    <pickle>
+      <dictionary>
+        <item>
+            <key> <string>_log</string> </key>
+            <value>
+              <list>
+                <dictionary>
+                  <item>
+                      <key> <string>action</string> </key>
+                      <value> <string>edit</string> </value>
+                  </item>
+                  <item>
+                      <key> <string>actor</string> </key>
+                      <value> <unicode>zope</unicode> </value>
+                  </item>
+                  <item>
+                      <key> <string>comment</string> </key>
+                      <value> <string></string> </value>
+                  </item>
+                  <item>
+                      <key> <string>error_message</string> </key>
+                      <value> <string></string> </value>
+                  </item>
+                  <item>
+                      <key> <string>serial</string> </key>
+                      <value> <string>0.0.0.0</string> </value>
+                  </item>
+                  <item>
+                      <key> <string>state</string> </key>
+                      <value> <string>current</string> </value>
+                  </item>
+                  <item>
+                      <key> <string>time</string> </key>
+                      <value>
+                        <object>
+                          <klass>
+                            <global name="_reconstructor" module="copy_reg"/>
+                          </klass>
+                          <tuple>
+                            <global name="DateTime" module="DateTime.DateTime"/>
+                            <global name="object" module="__builtin__"/>
+                            <none/>
+                          </tuple>
+                          <state>
+                            <tuple>
+                              <float>1738594190.45</float>
+                              <string>UTC</string>
+                            </tuple>
+                          </state>
+                        </object>
+                      </value>
+                  </item>
+                </dictionary>
+              </list>
+            </value>
+        </item>
+      </dictionary>
+    </pickle>
+  </record>
+</ZopeData>

bt5/erp5_corporate_identity_test/TestTemplateItem/portal_components/test.erp5.testCorporateIdentityTemplateList.py
@@ -1420,6 +1420,26 @@ class TestCorporateIdentityTemplateList(ERP5TypeTestCase):
       )
     )
 
+  @changeSkin('Book')
+  def test_pdfBookInFrench(self):
+    test_page = self.portal.web_page_module.template_test_book_input_001_en_html
+    clone_page = test_page.Base_createCloneDocument(batch_mode=1)
+    clone_page.setLanguage('fr')
+    self.runPdfTestPattern(
+      clone_page.getId(),
+      "template_test_image_source_pdf",
+      **dict(
+        page_number=1,
+        use_skin="Book",
+        test_method="WebPage_exportAsBook",
+        format="pdf",
+        override_revision=1,
+        include_content_table=1
+      )
+    )
+
+
+
   # XXX change to a single pdf from which pics are generated!
   @changeSkin('Book')
   def test_pdfBookAllOptions(self):

bt5/erp5_corporate_identity_test/bt/dependency_list
@@ -2,3 +2,4 @@ erp5_corporate_identity
 erp5_pdm
 erp5_ui_test_core
 erp5_ui_test
+erp5_l10n_fr