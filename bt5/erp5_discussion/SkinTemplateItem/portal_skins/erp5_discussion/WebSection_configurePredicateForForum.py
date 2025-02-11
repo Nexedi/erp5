3a73932e34a03a20c8b83dd18e5a05fc41c5c844
Roque Porchetto <roque.porchetto@nexedi.com>
2025-02-11 18:25:22 +0000

    erp5_discussion: fix script parametrization

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_configurePredicateForForum.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_configurePredicateForForum.py
@@ -2,7 +2,6 @@
 multimembership_criterion_base_category=["publication_section"]
 membership_criterion_category=["publication_section/forum"]
 # forum predicate configuration
-forum_relative_url = context.REQUEST.get('forum_relative_url')
 if forum_relative_url:
   forum = context.restrictedTraverse(forum_relative_url)
   multimembership_criterion_base_category=forum.getMultimembershipCriterionBaseCategoryList()
