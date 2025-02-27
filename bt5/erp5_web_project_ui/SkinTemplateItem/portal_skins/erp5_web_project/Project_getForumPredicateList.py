95ac0acfcdabf8e322b1bd90638d1998dafa4921
Roque Porchetto <roque.porchetto@nexedi.com>
2025-02-27 18:59:44 +0000

    erp5_web_project_ui: add forum treatment

---
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Project_getForumPredicateList.py
@@ -0,0 +1,28 @@
+from Products.CMFCore.utils import getToolByName
+
+project = context
+portal = project.getPortalObject()
+domain_tool = getToolByName(portal, 'portal_domains')
+
+if project.getPortalType() != "Project":
+  return []
+
+tmp_context = portal.discussion_thread_module.newContent(
+  id='fake',
+  portal_type='Discussion Thread',
+  temp_object=True,
+  publication_section='forum',
+  follow_up=project.getRelativeUrl()
+)
+
+def filter_method(element_list):
+  return [x for x in element_list if 'follow_up' in x.getMultimembershipCriterionBaseCategoryList()]
+
+predicate_list = [x.getRelativeUrl() for x in domain_tool.searchPredicateList(
+  tmp_context,
+  portal_type='Discussion Forum',
+  filter_method=filter_method,
+  tested_base_category_list=['follow_up']
+)]
+
+return predicate_list
