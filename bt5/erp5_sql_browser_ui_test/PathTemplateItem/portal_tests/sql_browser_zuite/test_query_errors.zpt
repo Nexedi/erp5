Nexedi/erp5

commit 41646898cc8e9d3f1046cd671456413879b399f4

sql_browser: add a checkbox to run "ANALYZE FORMAT=json" on the current query
and some python3 fixes

bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_getQueryResultAsJSON.py
@@ -18,10 +18,18 @@ except Exception as e:
   from Products.ERP5Type.Utils import str2bytes
   response.setStatus(500)
   try:
-    response.write(str2bytes(str(e[1])))
-  except Exception:
-    response.write(str2bytes(str(e)))
+    error_text = str(e[1])
+  except IndexError:
+    error_text = str(e)
+  response.write(str2bytes(error_text))
   return
+
+analyze_output = [[""]]
+if analyze:
+  try:
+    analyze_output = context.manage_test("ANALYZE FORMAT=JSON " + query)
+  except Exception:
+    pass
 
 # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger
 MAX_SAFE_INTEGER = 2**53 - 1
@@ -44,10 +52,13 @@ for line in results.tuples():
       # as string, this will still not work for pivot table, but at least the spreadsheet
       # will not display truncated values.
       v = str(v)
+    elif isinstance(v, bytes) and six.PY3:
+      v = v.decode('utf-8', errors='repr')
 
     new_line.append(v)
   data.append(new_line)
 
+
 response.setHeader("Server-Timing", 'db;dur=%s;desc="SQL query"' % (1000 * (time.time() - start)))
 response.setHeader('Content-Type', 'application/json')
-return json.dumps(data, indent=2)
+return json.dumps({"results": data, "analyze": analyze_output[0][0]}, indent=2)

bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_viewQueryBrowser.html
@@ -1,47 +1,65 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>SQL Browser</title>
 
-    <script type="text/javascript" src="jquery/core/jquery-1.9.min.js"></script>
-    <script type="text/javascript" src="jquery/ui/js/jquery-ui.min.js"></script>
-    <link rel="stylesheet" media="screen" href="jquery/ui/css/erp5-theme/jquery-ui.min.css">
+<head>
+  <title>SQL Browser</title>
 
-    <!-- "local" handsontable TODO make a bt ? -->
-    <script src="sql_browser/handsontable.full.js"></script>
-    <link rel="stylesheet" media="screen" href="sql_browser/handsontable.full.css">
+  <script type="text/javascript" src="jquery/core/jquery-1.9.min.js"></script>
+  <script type="text/javascript" src="jquery/ui/js/jquery-ui.min.js"></script>
+  <link rel="stylesheet" media="screen" href="jquery/ui/css/erp5-theme/jquery-ui.min.css">
 
-    <!-- "local" jquery noty -->
-    <script type="text/javascript" src="sql_browser/jquery.noty.packaged.js"></script>
+  <!-- "local" handsontable TODO make a bt ? -->
+  <script src="sql_browser/handsontable.full.js"></script>
+  <link rel="stylesheet" media="screen" href="sql_browser/handsontable.full.css">
 
-    <!-- build from  https://lab.nexedi.com/jerome/monaco-editor-sql-completion-provider/ -->
-    <script type="text/javascript" src="sql_browser/monaco-editor-sql-completion-provider.bundle.js"></script>
+  <!-- "local" jquery noty -->
+  <script type="text/javascript" src="sql_browser/jquery.noty.packaged.js"></script>
+
+  <!-- build from  https://lab.nexedi.com/jerome/monaco-editor-sql-completion-provider/ -->
+  <script type="text/javascript" src="sql_browser/monaco-editor-sql-completion-provider.bundle.js"></script>
 
 
-    <!-- "local" c3.js (0.4.10) and d3.js (3.5.5) -->
-    <link rel="stylesheet" type="text/css" href="sql_browser/c3.min.css">
-    <script type="text/javascript" src="sql_browser/d3.min.js"></script>
-    <script type="text/javascript" src="sql_browser/c3.min.js"></script>
+  <!-- "local" c3.js (0.4.10) and d3.js (3.5.5) -->
+  <link rel="stylesheet" type="text/css" href="sql_browser/c3.min.css">
+  <script type="text/javascript" src="sql_browser/d3.min.js"></script>
+  <script type="text/javascript" src="sql_browser/c3.min.js"></script>
 
-    <!-- PivotTable.js libs from erp5_pivot_table -->
-    <link rel="stylesheet" type="text/css" href="pivottable/pivot.css">
-    <script type="text/javascript" src="pivottable/pivot.js"></script>
-    <script type="text/javascript" src="pivottable/gchart_renderers.js"></script>
-    <script type="text/javascript" src="pivottable/c3_renderers.js"></script>
+  <!-- PivotTable.js libs from erp5_pivot_table -->
+  <link rel="stylesheet" type="text/css" href="pivottable/pivot.css">
+  <script type="text/javascript" src="pivottable/pivot.js"></script>
+  <script type="text/javascript" src="pivottable/gchart_renderers.js"></script>
+  <script type="text/javascript" src="pivottable/c3_renderers.js"></script>
 
-    <style>
-      body {font-family: Verdana;}
-      .c3-line {stroke-width: 3px;}
-      .c3 circle {stroke: white;}
-      .c3 text {font-size: 12px;}
-      .c3 text {font-size: 12px;}
-      .tick line {stroke: white;}
-    </style>
-  </head>
+  <style>
+    body {
+      font-family: Verdana;
+    }
 
-  <body>
-    <script type="text/javascript">
-    var monaco_editor;
+    .c3-line {
+      stroke-width: 3px;
+    }
+
+    .c3 circle {
+      stroke: white;
+    }
+
+    .c3 text {
+      font-size: 12px;
+    }
+
+    .c3 text {
+      font-size: 12px;
+    }
+
+    .tick line {
+      stroke: white;
+    }
+  </style>
+</head>
+
+<body>
+  <script type="text/javascript">
+    var monaco_editor, analyze_output_editor;
     $(function() {
         var editor,
             ht = new Handsontable(document.getElementById('table_container'), { data: [[0]], rowHeaders: true, colHeaders: true}),
@@ -58,19 +76,41 @@
               var intFormat = numberFormat({digitsAfterDecimal: 0});
               var notification = noty({type: "info", text: "Refreshing data", layout: "bottom"});
 
-              $.post("ZMySQLDAConnection_getQueryResultAsJSON", {query: editor.getValue()}, function(mps) {
+              fetch("ZMySQLDAConnection_getQueryResultAsJSON", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({ 
+                  query: editor.getValue(),
+                  "analyze:int": document.getElementById('Analyze').checked ? 1 : 0,
+                })
+              })
+              .then(response => {
+                  if (!response.ok) {
+                    return response.text().then(e => { throw e; })
+                  }
+                  const serverTiming = response.headers.get("Server-Timing");
+                  return response.json().then(data => ({ data, serverTiming }));
+              })
+              .then(({ data, serverTiming }) => {
+                document.getElementById("server_timing").textContent = serverTiming;
                 ht.clear();
-                if (mps.length > 1) {
+                const results = data['results'];
+                const analyze = data['analyze']
+                if (results.length > 1) {
                   ht.updateSettings({
-                    data: mps.slice(1),
-                    colHeaders: mps[0]
+                    data: results.slice(1),
+                    colHeaders: results[0]
                   });
                 } else {
                   ht.updateSettings({ data: [[0]], colHeaders: true})
                 }
 
-
-                $("#output").pivotUI(mps, {
+                if (analyze) {
+                  analyze_output_editor.setValue(analyze);
+                }
+                $("#pivot_table").pivotUI(results, {
                   renderers: renderers,
 
                   hiddenAttributes: [],
@@ -90,9 +130,9 @@
                 });
                 notification.close();
               }
-             , 'json').fail(function(jqXHR, textStatus, errorThrown) {
+            ).catch((error) => {
                 notification.close();
-                noty({type: "error", text: jqXHR.responseText, timeout: 5000, layout: "bottom" });
+                noty({type: "error", text: error, timeout: 5000, layout: "bottom" });
               });
             };
 
@@ -130,8 +170,21 @@
               return null;
             }
           });
+          analyze_output_editor =  monaco.editor.create(
+            document.querySelector('#analyze_output'),
+            {
+              automaticLayout: window.ResizeObserver ? true : false,
+              fontSize: 10,
+              lineNumbers: false,
+            }
+          );
+          monaco.editor.setModelLanguage(
+            analyze_output_editor.getModel(),
+            "json"
+          );
 
           $("#query").resizable();
+          $("#analyze_output").resizable();
 
           $.getJSON("ZMySQLDAConnection_getSchemaAsJSON").then(
             function(schema) {
@@ -139,6 +192,7 @@
               document.querySelector('#query').classList.add('initialized');
             });
 
+          $('input[name="Analyze"]').click(redraw);
           $('button[name="Query"]').click(redraw);
           $('button[name="Copy"]').click(e => {
             const tableAsMarkdown = (([
@@ -182,39 +236,43 @@
           redraw();
         });
       });
-    </script>
-<!--
+  </script>
+  <!--
     Presets: <select id="presets"><option selected="selected" value=""></option></select>
     <button name="Save">Save Preset</button>
     <br/>
 -->
 
-    <div style="width: 100%; height: 300px" id="query"> </div>
-    <button name="Query">Run Query</button>
-    <button name="Copy">Copy Results</button>
+  <div style="width: 100%; height: 30vh" id="query"> </div>
+  <button name="Query">Run Query</button>
+  <button name="Copy">Copy Results</button>
+  <input type="checkbox" name="Analyze" id="Analyze"><label for="Analyze">Analyze Query</label>
 
+  <div id="table_container" style="margin: 10px; height: 30vh; overflow: hidden; z-index: 1;"></div>
+  <small><pre id="server_timing"></pre></small>
+  <div style="display: grid;  grid-template-columns: 1fr 1fr;">
+    <div id="pivot_table" style="margin: 10px; overflow: scroll; width: 100%"></div>
+    <div id="analyze_output" style="width: 100%; height: 40vh;"> </div>
+  </div>
 
-    <div id="table_container" style="margin: 10px; height: 300px; overflow: hidden; z-index: 1;"></div>
-    <div id="output" style="margin: 10px; overflow: scroll"></div>
+  <!-- TODO: debug saved configuration -->
+  <div style="display: none">
+    <pre id="config_json"></pre>
+  </div>
+  <div style="display: none">
+    <textarea name="editor_backup"></textarea>
+  </div>
 
-    <!-- TODO: debug saved configuration -->
-    <div style="display: none">
-      <pre id="config_json"></pre>
-    </div>
-    <div style="display: none">
-      <textarea name="editor_backup"></textarea>
-    </div>
+</body>
 
-  </body>
-
-  <!-- monaco editor from erp5_monaco_editor bt -->
-  <script>
-    /*
+<!-- monaco editor from erp5_monaco_editor bt -->
+<script>
+  /*
       This is understood by our build of monaco-editor
       https://lab.nexedi.com/jerome/monaco-editor-erp5/blob/master/public-path.js
     */
     window.monacoEditorWebPackResourceBaseUrl = "monaco-editor/";
-  </script>
-  <script src="monaco-editor/app.bundle.min.js"></script>
+</script>
+<script src="monaco-editor/app.bundle.min.js"></script>
 
 </html>

bt5/erp5_sql_browser_ui_test/PathTemplateItem/portal_tests/sql_browser_zuite/test_query_analyze.xml
@@ -2,7 +2,7 @@
 <ZopeData>
   <record id="1" aka="AAAAAAAAAAE=">
     <pickle>
-      <global name="PythonScript" module="Products.PythonScripts.PythonScript"/>
+      <global name="ZopePageTemplate" module="Products.PageTemplates.ZopePageTemplate"/>
     </pickle>
     <pickle>
       <dictionary>
@@ -25,18 +25,6 @@
                         <value>
                           <dictionary>
                             <item>
-                                <key> <string>name_container</string> </key>
-                                <value> <string>container</string> </value>
-                            </item>
-                            <item>
-                                <key> <string>name_context</string> </key>
-                                <value> <string>context</string> </value>
-                            </item>
-                            <item>
-                                <key> <string>name_m_self</string> </key>
-                                <value> <string>script</string> </value>
-                            </item>
-                            <item>
                                 <key> <string>name_subpath</string> </key>
                                 <value> <string>traverse_subpath</string> </value>
                             </item>
@@ -49,12 +37,24 @@
             </value>
         </item>
         <item>
-            <key> <string>_params</string> </key>
-            <value> <string>query="", analyze=False</string> </value>
+            <key> <string>content_type</string> </key>
+            <value> <string>text/html</string> </value>
+        </item>
+        <item>
+            <key> <string>expand</string> </key>
+            <value> <int>0</int> </value>
         </item>
         <item>
             <key> <string>id</string> </key>
-            <value> <string>ZMySQLDAConnection_getQueryResultAsJSON</string> </value>
+            <value> <string>test_query_analyze</string> </value>
+        </item>
+        <item>
+            <key> <string>output_encoding</string> </key>
+            <value> <string>utf-8</string> </value>
+        </item>
+        <item>
+            <key> <string>title</string> </key>
+            <value> <unicode></unicode> </value>
         </item>
       </dictionary>
     </pickle>

bt5/erp5_sql_browser_ui_test/PathTemplateItem/portal_tests/sql_browser_zuite/test_query_analyze.zpt
@@ -1,0 +1,70 @@
+<html xmlns:tal="http://xml.zope.org/namespaces/tal" xmlns:metal="http://xml.zope.org/namespaces/metal">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title tal:content="template/title_and_id"></title>
+</head>
+
+<body>
+  <table cellpadding="1" cellspacing="1" border="1">
+    <thead>
+      <tr>
+        <td rowspan="1" colspan="3" tal:content="template/title_and_id"></td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>open</td>
+        <td tal:content="string: ${here/portal_url}/erp5_sql_connection/ZMySQLDAConnection_viewQueryBrowser"></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <td>waitForElementPresent</td>
+        <td>css=div.monaco-editor.vs</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>assertEval</td>
+        <td>
+          selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'type', {'text': 'select * from catalog where owner = "System Processes"'})
+        </td>
+        <td>null</td>
+      </tr>
+      <tr>
+        <td>click</td>
+        <td>//button[@name="Query"]</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>waitForText</td>
+        <td>xpath=(//div[contains(@class, "handsontable")]//th//span[@class="colHeader"])[3]</td>
+        <td>owner</td>
+      </tr>
+      <tr>
+        <td>assertText</td>
+        <td>//div[contains(@class, "handsontable")]//tbody/tr/td[3]</td>
+        <td>System Processes</td>
+      </tr>
+      <tr>
+        <td>click</td>
+        <td>//input[@name="Analyze"]</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>waitForEval</td>
+        <td>
+          JSON.parse(selenium.browserbot.getCurrentWindow().analyze_output_editor.getValue()).query_block.table.table_name
+        </td>
+        <td>catalog</td>
+      </tr>
+      <tr>
+        <td>assertText</td>
+        <td>//div[contains(@class, "handsontable")]//tbody/tr/td[3]</td>
+        <td>System Processes</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>

commit 518a98c63d3577332c73aeecb1b7492ef72c475f

sql_browser: fix recent regressions
introduced in 41646898cc (sql_browser: add a checkbox to run
"ANALYZE FORMAT=json" on the current query, 2025-03-16)  and a missing
html escape problem that were also here before

bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_getQueryResultAsJSON.py
@@ -19,7 +19,7 @@ except Exception as e:
   response.setStatus(500)
   try:
     error_text = str(e[1])
-  except IndexError:
+  except Exception:
     error_text = str(e)
   response.write(str2bytes(error_text))
   return

bt5/erp5_sql_browser/SkinTemplateItem/portal_skins/erp5_sql_browser/ZMySQLDAConnection_viewQueryBrowser.html
@@ -60,6 +60,12 @@
 <body>
   <script type="text/javascript">
     var monaco_editor, analyze_output_editor;
+    function escapeHTML(text){
+      let sanitizer = document.createElement('div');
+      sanitizer.textContent = text;
+      return sanitizer.innerHTML;
+    }
+
     $(function() {
         var editor,
             ht = new Handsontable(document.getElementById('table_container'), { data: [[0]], rowHeaders: true, colHeaders: true}),
@@ -132,7 +138,7 @@
               }
             ).catch((error) => {
                 notification.close();
-                noty({type: "error", text: error, timeout: 5000, layout: "bottom" });
+                noty({type: "error", text: escapeHTML(error), timeout: 5000, layout: "bottom" });
               });
             };
 
@@ -185,12 +191,15 @@
 
           $("#query").resizable();
           $("#analyze_output").resizable();
+          $("#pivot_table").resizable();
 
-          $.getJSON("ZMySQLDAConnection_getSchemaAsJSON").then(
-            function(schema) {
+          fetch("ZMySQLDAConnection_getSchemaAsJSON")
+            .then(response => response.json())
+            .then(schema => {
               monaco.languages.registerCompletionItemProvider('sql', new SQLCompletionProvider(schema));
               document.querySelector('#query').classList.add('initialized');
-            });
+            })
+            .catch(error => console.error("Error fetching schema:", error));
 
           $('input[name="Analyze"]').click(redraw);
           $('button[name="Query"]').click(redraw);

bt5/erp5_sql_browser_ui_test/PathTemplateItem/portal_tests/sql_browser_zuite/test_query_errors.xml
@@ -46,7 +46,7 @@
         </item>
         <item>
             <key> <string>id</string> </key>
-            <value> <string>test_query_commands_out_of_sync</string> </value>
+            <value> <string>test_query_errors</string> </value>
         </item>
         <item>
             <key> <string>output_encoding</string> </key>

bt5/erp5_sql_browser_ui_test/PathTemplateItem/portal_tests/sql_browser_zuite/test_query_errors.zpt
@@ -27,7 +27,9 @@
       <tr>
         <td>assertEval</td>
         <td>
+          <pre>
           selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'type', {'text': 'select 1+1; select "boom"'})
+          </pre>
         </td>
         <td>null</td>
       </tr>
@@ -42,11 +44,48 @@
         <td></td>
       </tr>
       <tr>
+        <td>waitForElementNotPresent</td>
+        <td>//div[contains(@class, "noty_message")]/span[@class="noty_text"]</td>
+        <td></td>
+      </tr>
+      <tal:block metal:define-macro="resetAndCheckQueryOK">
+        <tr>
+          <td>assertEval</td>
+          <td>
+            <pre>
+            selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'editor.action.smartSelect.expand');
+            selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'editor.action.deleteLines');
+            selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'type', {'text': 'select 1+1'})
+            </pre>
+          </td>
+          <td>null</td>
+        </tr>
+        <tr>
+          <td>click</td>
+          <td>//button[@name="Query"]</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>waitForText</td>
+          <td>xpath=(//div[contains(@class, "handsontable")]//th//span[@class="colHeader"])[2]</td>
+          <td>1+1</td>
+        </tr>
+        <tr>
+          <td>assertText</td>
+          <td>//div[contains(@class, "handsontable")]//tbody/tr/td</td>
+          <td>2</td>
+        </tr>
+      </tal:block>
+
+      <tr>
         <td>assertEval</td>
         <td>
+          <pre>
           selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'editor.action.smartSelect.expand');
           selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'editor.action.deleteLines');
-          selenium.browserbot.getCurrentWindow().monaco_editor.trigger('', 'type', {'text': 'select 1+1'})
+          selenium.browserbot.getCurrentWindow().monaco_editor.trigger(
+            '', 'type', {'text': 'select * from catalog group by 1 with rollup order by 1'});
+          </pre>
         </td>
         <td>null</td>
       </tr>
@@ -56,15 +95,11 @@
         <td></td>
       </tr>
       <tr>
-        <td>waitForText</td>
-        <td>xpath=(//div[contains(@class, "handsontable")]//th//span[@class="colHeader"])[2]</td>
-        <td>1+1</td>
+        <td>waitForElementPresent</td>
+        <td>//div[contains(@class, "noty_message")]/span[@class="noty_text" and contains(text(), "Incorrect usage of CUBE/ROLLUP")]</td>
+        <td></td>
       </tr>
-      <tr>
-        <td>assertText</td>
-        <td>//div[contains(@class, "handsontable")]//tbody/tr/td</td>
-        <td>2</td>
-      </tr>
+      <tal:block metal:use-macro="template/macros/resetAndCheckQueryOK" />
 
     </tbody>
   </table>