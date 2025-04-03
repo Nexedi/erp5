788dfffefd3201f5991c3a1c2c969322e1eb7041
Jérome Perrin <jerome@nexedi.com>
2025-04-04 00:02:00 +0900

    system_event: make Alarm_handleConfirmedHTTPExchanges use the tag from alarm
    This also make it possible to explicitly activate this alarm with a tag
    to schedule more actions after the alarm processed.
    
    Also:
    
     * Activate with node='' so that the HTTPExchange_setFollowUpAndInquiry
    executions can happen in parrallel on different nodes.
    
     * Activate a `getId` activity to prevent the alarm itself to be
    executed twice in a concurrent way.

--- bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/Alarm_handleConfirmedHTTPExchanges.py
+++ bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/Alarm_handleConfirmedHTTPExchanges.py
@@ -1,8 +1,9 @@
-tag = script.id
-for exchange in context.getPortalObject().system_event_module.searchFolder(
+for brain in context.getPortalObject().system_event_module.searchFolder(
       portal_type='HTTP Exchange',
       validation_state='confirmed',
     ):
+  exchange = brain.getObject()
   if exchange.getValidationState() == 'confirmed':
-    exchange.activate(tag=tag).HTTPExchange_setFollowUpAndInquiry()
+    exchange.activate(node='', tag=tag).HTTPExchange_setFollowUpAndInquiry()
     exchange.acknowledge()
+context.activate(after_tag=tag).getId()
