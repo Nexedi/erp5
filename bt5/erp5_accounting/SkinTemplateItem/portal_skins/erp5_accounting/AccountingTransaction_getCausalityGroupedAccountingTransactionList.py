762402b8d16d6f42c37b092142460ac223573fde
Romain Courteaud <romain@nexedi.com>
2025-04-04 09:09:27 +0000

    erp5_accounting: check the full list of the accounting transaction linked by causality while grouping

--- bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getCausalityGroupedAccountingTransactionList.py
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountingTransaction_getCausalityGroupedAccountingTransactionList.py
@@ -4,15 +4,18 @@ portal = context.getPortalObject()
 accounting_transaction_type_list = \
               portal.getPortalAccountingTransactionTypeList()
 accounting_transaction_set = {}
-accounting_transaction_set[context] = 1
+accounting_transaction_to_check_list = [context]
 
 # Add all causality transactions
-for accounting_transaction in [context] + context.getCausalityValueList(
-                  portal_type=accounting_transaction_type_list):
-  accounting_transaction_set[accounting_transaction] = 1
-  for related_causality in accounting_transaction.getCausalityRelatedValueList(
-              portal_type=accounting_transaction_type_list):
-    if related_causality.getSimulationState() not in ('cancelled', 'deleted'):
-      accounting_transaction_set[related_causality] = 1
+while accounting_transaction_to_check_list:
+  accounting_transaction = accounting_transaction_to_check_list.pop()
+  if accounting_transaction not in accounting_transaction_set:
+    accounting_transaction_set[accounting_transaction] = 1
+
+    for related_causality in accounting_transaction.getCausalityRelatedValueList(
+                portal_type=accounting_transaction_type_list) + accounting_transaction.getCausalityValueList(
+                portal_type=accounting_transaction_type_list):
+      if related_causality.getSimulationState() not in ('cancelled', 'deleted'):
+        accounting_transaction_to_check_list.append(related_causality)
 
 return accounting_transaction_set.keys()
