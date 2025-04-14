3afab393468e7b06279417ec2fcbe943f200c8e1
Roque Porchetto <roque.porchetto@nexedi.com>
2025-04-14 15:48:24 +0000

    erp5_web_project_ui: fix url script

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Listbox_getDiscussionThreadReaderUrl.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Listbox_getDiscussionThreadReaderUrl.py
@@ -1,13 +1,16 @@
-context.log("brain:")
-context.log(kw['brain'].getObject())
-context.log(kw['brain'].getObject().getRelativeUrl())
-discussion_thread = kw['brain'].getObject()
+if url_dict:
+  jio_key = context.getRelativeUrl()
+  return {
+    'command': 'index',
+    'options': {
+      'jio_key': jio_key,
+      'editable': True
+    },
+    'view_kw': {
+      'view': 'DiscussionThread_viewReaderForm',
+      'jio_key': jio_key,
+    }
+  }
 
-key = discussion_thread.getRelativeUrl()
-portal_absolute_url = context.getPortalObject().absolute_url()
-return '%s/%s' % (portal_absolute_url, key)
-
-
-form_id = 'My_viewForm'
-redirect_url = '%s/%s' % (discussion_thread.absolute_url(), form_id)
-return redirect_url
+else:
+  return {}
