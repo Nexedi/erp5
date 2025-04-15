143b42b51e90ced9527e3010d025552415a60010
Paul Graydon <paul.graydon@nexedi.com>
2025-04-15 08:43:02 +0000

    erp5_wendelin_telecom_*: Add Wendelin Telecom business templates
    Merge all Wendelin Telecom business templates, making the project generic.
    Wendelin Telecom is a Wendelin configuration dedicated to the automatic ingestion and processing of ORS enb.xlog log contents into KPIs for viewing by ORS-owning clients.

---
+++ wendelin/bt5/erp5_wendelin_telecom_web/SkinTemplateItem/portal_skins/erp5_wendelin_telecom_web_renderjs_ui/WebSection_getWendelinTelecomClientFrontPrecacheManifestList.py
@@ -0,0 +1,11 @@
+url_list = [
+  'wendelin_telecom_home_message.html',
+  'gadget_erp5_page_wendelin_telecom_client_front.html',
+  'gadget_erp5_page_wendelin_telecom_client_front.js',
+  'gadget_wendelin_telecom_client_panel.html',
+  'gadget_wendelin_telecom_client_panel.js',
+  'gadget_wendelin_telecom_panel.css',
+  'gadget_wendelin_telecom_panel.png'
+]
+
+return url_list
