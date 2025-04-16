2992df8e9370bf82c9ec23fef97eb78030b491b4
Romain Courteaud <romain@nexedi.com>
2025-04-16 09:32:58 +0000

    erp5_system_event: parallelize the execution on all activity nodes

--- bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/Alarm_handleConfirmedHTTPExchanges.py
+++ bt5/erp5_system_event/SkinTemplateItem/portal_skins/erp5_system_event/Alarm_handleConfirmedHTTPExchanges.py
@@ -4,7 +4,8 @@ for exchange in context.getPortalObject().system_event_module.searchFolder(
   validation_state='confirmed',
 ):
   if exchange.getValidationState() == 'confirmed':
-    exchange.activate(failure_state='non_blocking', **activate_kw).HTTPExchange_setFollowUpAndInquiry()
+    # node='' to parallelize the execution on all activity nodes
+    exchange.activate(failure_state='non_blocking', node='', **activate_kw).HTTPExchange_setFollowUpAndInquiry()
     exchange.acknowledge()
     exchange.reindexObject(activate_kw=activate_kw)
 
