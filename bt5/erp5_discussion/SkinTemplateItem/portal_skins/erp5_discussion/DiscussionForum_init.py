48b227e421f3a7aa27fc8c7e59d57a8f37be6e28
Roque Porchetto <roque.porchetto@nexedi.com>
2025-04-17 13:28:19 +0000

    erp5_discussion: forum thread view listbox urls

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_init.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_init.py
@@ -5,5 +5,5 @@ context.edit(criterion_property=("portal_type",),
              membership_criterion_base_category=[],
              multimembership_criterion_base_category=["publication_section"],
              membership_criterion_category= ["publication_section/forum"])
-context.setCriterion("portal_type", "Discussion Thread")
+context.setCriterion("portal_type", ["Project", "Discussion Thread"])
 context.setCategoryList(["publication_section/forum"]) # can be used by other predicates to reach the forum
