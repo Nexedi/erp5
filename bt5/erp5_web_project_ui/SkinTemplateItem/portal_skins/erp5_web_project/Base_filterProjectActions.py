549da66cf17401a303a298952628994903640e60
Roque Porchetto <roque.porchetto@nexedi.com>
2025-04-23 23:16:28 +0000

    erp5_web_project_ui: use project_view in filter views script

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
@@ -1,15 +1,9 @@
 filtered_actions = {}
 for action_category_name, action_list in actions.items():
   if (action_category_name == 'object_view'):
-    filtered_actions[action_category_name] = [action for action in action_list if action['id'] in ['view_threads', 'view_posts']]
-    # TODO: use the default app view project_view:
-    #filtered_actions[action_category_name] = [action for action in action_list if action['id'] in ['project_view']]
+    filtered_actions[action_category_name] = [action for action in action_list if action['id'] in ['project_view']]
   elif (action_category_name == 'object_jio_action'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['post_query']]
   else:
     filtered_actions[action_category_name] = action_list
 return filtered_actions
-
-
-# object_view - view, view_threads, etc
-# object_jio_action - post_query
