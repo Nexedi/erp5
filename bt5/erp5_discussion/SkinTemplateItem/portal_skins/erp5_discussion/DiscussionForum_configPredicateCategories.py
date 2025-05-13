70340a91471804d6b414803f244dc62d4e56d7c4
Roque Porchetto <roque.porchetto@nexedi.com>
2025-05-13 17:16:38 +0000

    erp5_discussion: udpate categories script

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_configPredicateCategories.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_configPredicateCategories.py
@@ -1,2 +1,3 @@
 category_list = context.getMembershipCriterionCategoryList()
 context.setCategoryList(category_list)
+context.Base_redirect('view',keep_items={'portal_status_message': 'Discussion Forum categories updated.'})
