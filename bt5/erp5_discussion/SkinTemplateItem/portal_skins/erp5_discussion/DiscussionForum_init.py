c6a29e1811c23c272537cbcd062e0c4d56181f80
Roque Porchetto <roque.porchetto@nexedi.com>
2025-05-13 14:59:34 +0000

    erp5_discussion: drop forum init script

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_init.py
+++
@@ -1,8 +0,0 @@
-context.setTitle("New Forum")
-# Default predicate configuration, could be updated by the user in the Predicate view
-context.edit(criterion_property=("portal_type",),
-             empty_criterion_valid=True,
-             membership_criterion_base_category=[],
-             multimembership_criterion_base_category=["publication_section"],
-             membership_criterion_category= ["publication_section/forum"])
-context.setCategoryList(["publication_section/forum"])
