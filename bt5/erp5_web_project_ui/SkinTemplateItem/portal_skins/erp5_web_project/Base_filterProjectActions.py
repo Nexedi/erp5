056edf201912dfadcab8c92bc25777eae6ed4e3e
Roque Porchetto <roque.porchetto@nexedi.com>
2025-05-15 17:42:08 +0000

    erp5_web_project_ui: filter jump to Edit portal type action

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
@@ -5,8 +5,9 @@ for action_category_name, action_list in actions.items():
   elif (action_category_name == 'object_jio_action'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['post_query']]
   elif (action_category_name == 'project_view'):
-    filtered_actions['object_view'].extend(action_list)
+    filtered_actions['object_view'] = action_list
+  elif (action_category_name == 'object_jio_jump'):
+    filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['jump_to_portal_type']]
   else:
-    #TODO filter more
     filtered_actions[action_category_name] = action_list
 return filtered_actions
