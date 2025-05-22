6385f0ecdbbc55059ca239abe20b5eac13b0b7e5
Roque Porchetto <roque.porchetto@nexedi.com>
2025-05-22 15:10:33 +0000

    erp5_discussion: drop DiscussionForum_configPredicateCategories

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_configPredicateCategories.py
+++
@@ -1,3 +0,0 @@
-category_list = context.getMembershipCriterionCategoryList()
-context.setCategoryList(category_list)
-context.Base_redirect('view',keep_items={'portal_status_message': 'Discussion Forum categories updated.'})
