8367741ea7cf67d431f363b0f596cae4da5c0cf5
Jérome Perrin <jerome@nexedi.com>
2025-05-27 13:07:21 +0900

    testERP5TestNode: update test now that testnode expects ipv6_range in config

--- erp5/tests/testERP5TestNode.py
+++ erp5/tests/testERP5TestNode.py
@@ -98,7 +98,8 @@ class ERP5TestNode(TestCase):
     config["software_list"] = ["foo", "bar"]
     config["partition_reference"] = "part"
     config["ipv4_address"] = "1.2.3.4"
-    config["ipv6_address"] = "::1"
+    config["ipv6_address"] = "2001:db8::1"
+    config["ipv6_range"] = "2001:db8:8001::/48"
     config["slapos_binary"] = "/opt/slapgrid/HASH/bin/slapos"
     config["srv_directory"] = "srv_directory"
     config["shared_part_list"] = "/not/exists\n /not/exists_either"
