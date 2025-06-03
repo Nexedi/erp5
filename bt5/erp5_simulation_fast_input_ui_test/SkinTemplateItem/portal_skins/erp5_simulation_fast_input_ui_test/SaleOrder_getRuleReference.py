7ecd535cffa740bbd1f925be86e81e82371d68cb
Yusei Tahara <yusei@nexedi.com>
2025-06-03 19:56:24 +0900

    Reduce the impact on other tests.

--- bt5/erp5_simulation_fast_input_ui_test/SkinTemplateItem/portal_skins/erp5_simulation_fast_input_ui_test/SaleOrder_getRuleReference.py
+++ bt5/erp5_simulation_fast_input_ui_test/SkinTemplateItem/portal_skins/erp5_simulation_fast_input_ui_test/SaleOrder_getRuleReference.py
@@ -1,1 +1,2 @@
-return 'order_simulation_rule_for_simulation_fast_input_test'
+if context.getId() == 'sale_order_for_simulation_fast_input':
+  return 'order_simulation_rule_for_simulation_fast_input_test'
