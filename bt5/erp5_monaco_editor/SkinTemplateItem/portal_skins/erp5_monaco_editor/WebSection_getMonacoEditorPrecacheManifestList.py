c5acac585f516928417e9a42efc94ed13b333b58
Jérome Perrin <jerome@nexedi.com>
2025-06-12 11:05:54 +0900

    monaco_editor: drop embedded prettier
    current monaco versions have a built in javascript formatting, that
    happens to format the code in a way more compatible with the jslint's
    expectations.

--- bt5/erp5_monaco_editor/SkinTemplateItem/portal_skins/erp5_monaco_editor/WebSection_getMonacoEditorPrecacheManifestList.py
+++ bt5/erp5_monaco_editor/SkinTemplateItem/portal_skins/erp5_monaco_editor/WebSection_getMonacoEditorPrecacheManifestList.py
@@ -2,8 +2,6 @@ url_list = [
   "monaco-editor.gadget.html",
   "monaco-editor.gadget.js",
   "jslint.js",
-  "monaco-editor/prettier@2.0.5/parser-babel.js",
-  "monaco-editor/prettier@2.0.5/standalone.js",
   "monaco-editor/app.bundle.min.js",
   "monaco-editor/editor.worker.bundle.min.js",
   "monaco-editor/5196.bundle.min.js"
