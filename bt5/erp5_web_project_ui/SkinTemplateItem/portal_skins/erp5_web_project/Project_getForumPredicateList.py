fbea4a3c89db4a8833e653e75d49adc4a5886f70
Roque Porchetto <roque.porchetto@nexedi.com>
2025-06-16 19:26:53 +0000

    [WIP] erp5_web_project_ui: get forum from project

---
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Project_getForumPredicateList.py
@@ -0,0 +1,28 @@
+from Products.CMFCore.utils import getToolByName
+
+project = context
+portal = project.getPortalObject()
+domain_tool = getToolByName(portal, 'portal_domains')
+
+tested_base_category_list = ['follow_up']
+# not used, as thread-project is a follow_up relation
+destination_value = portal.portal_membership.getAuthenticatedMember().getUserValue()
+predicate_portal_type = 'Project'
+
+tmp_context = portal.portal_trash.newContent(
+  portal_type='Discussion Thread',
+  temp_object=1,
+  follow_up=project.getRelativeUrl()
+)
+print("tmp_context", tmp_context)
+# e.g. discussion_thread_module/278/getFollowUpList : project_module/20120914-11FAD6
+
+predicate_list = [x.getObject() for x in domain_tool.searchPredicateList(
+  tmp_context,
+  portal_type=predicate_portal_type,
+  acquired=0,
+  tested_base_category_list=tested_base_category_list
+)]
+print("predicate_list", predicate_list)
+return printed
+return predicate_list
