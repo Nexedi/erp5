e51b3b65047c85c22e9c3a39dd8198f13a5e5222
Roque Porchetto <roque.porchetto@nexedi.com>
2025-06-25 18:47:48 +0000

    erp5_web_project_ui: Project_getForumPredicateList fixed

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Project_getForumPredicateList.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Project_getForumPredicateList.py
@@ -11,12 +11,17 @@ tmp_context = portal.discussion_thread_module.newContent(
   id='fake',
   portal_type='Discussion Thread',
   temp_object=True,
+  publication_section='forum',
   follow_up=project.getRelativeUrl()
 )
+
+def filter_method(element_list):
+  return [x for x in element_list if 'follow_up' in x.getMultimembershipCriterionBaseCategoryList()]
 
 predicate_list = [x.getObject() for x in domain_tool.searchPredicateList(
   tmp_context,
   portal_type='Discussion Forum',
+  filter_method=filter_method,
   tested_base_category_list=['follow_up']
 )]
 
