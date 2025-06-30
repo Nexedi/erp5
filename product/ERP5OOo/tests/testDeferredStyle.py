c7499d5a855ce23475daa07a66abaaa952e01c79
Titouan Soulard <titouan.soulard@rapid.space>
2025-06-30 14:36:43 +0200

    ERP5OOo: remove call to base data in tests

--- product/ERP5OOo/tests/testDeferredStyle.py
+++ product/ERP5OOo/tests/testDeferredStyle.py
@@ -471,7 +471,6 @@ class TestDeferredStyleBase(DeferredStyleTestCase):
       data=part.get_payload(decode=True),
       temp_object=True,
     )
-    doc.convertToBaseFormat()
     report_as_txt = doc.asText()
 
     self.assertIn(
