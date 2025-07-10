29989ab268ca99775125102dd1506625903bd8f6
Jérome Perrin <jerome@nexedi.com>
2025-07-10 22:20:49 +0900

    accounting: use ParallelListField to show GAP filter in report dialogs
    Having one big multi list field with all gaps is too hard to use.
    
    Refactor Account_hashGapItemList to be able to reuse the logic and drop
    the cache since the underlying values are already cached.

---
+++ bt5/erp5_accounting/SkinTemplateItem/portal_skins/erp5_accounting/AccountModule_hashGapItemList.py
@@ -0,0 +1,8 @@
+return context.Account_hashGapItemList(
+  item_list=item_list,
+  value_list=value_list,
+  is_right_display=is_right_display,
+  size=5,
+  field_type='MultiListField',
+  include_empty=False,
+)
