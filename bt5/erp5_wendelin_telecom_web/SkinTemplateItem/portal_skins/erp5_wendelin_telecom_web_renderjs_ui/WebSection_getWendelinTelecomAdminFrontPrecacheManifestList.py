728658ab235cdce5bd6e8898e9911507f9b7c4eb
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2025-07-15 09:00:59 +0000

    erp5_wendelin_telecom_web: fix file names
    previous files don't exist, seems already renamed

--- bt5/erp5_wendelin_telecom_web/SkinTemplateItem/portal_skins/erp5_wendelin_telecom_web_renderjs_ui/WebSection_getWendelinTelecomAdminFrontPrecacheManifestList.py
+++ bt5/erp5_wendelin_telecom_web/SkinTemplateItem/portal_skins/erp5_wendelin_telecom_web_renderjs_ui/WebSection_getWendelinTelecomAdminFrontPrecacheManifestList.py
@@ -1,10 +1,10 @@
 url_list = [
   'gadget_erp5_page_wendelin_telecom_admin_front.html',
   'gadget_erp5_page_wendelin_telecom_admin_front.js',
-  'gadget_wendelin_telecom_admin_panel.html',
-  'gadget_wendelin_telecom_admin_panel.js',
-  'gadget_wendelin_telecom_panel.css',
-  'gadget_wendelin_telecom_panel.png'
+  'wendelin_telecom_gadget_admin_panel.html',
+  'wendelin_telecom_gadget_admin_panel.js',
+  'wendelin_telecom_gadget_panel.css',
+  'wendelin_telecom_gadget_panel.png'
 ]
 
 return url_list
