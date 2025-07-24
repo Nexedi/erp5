20ea2a8d0028d8ede5d2f264ed97be567a808e54
Roque Porchetto <roque.porchetto@nexedi.com>
2025-07-24 15:29:52 +0000

    erp5_discussion: link to last post

---
+++ bt5/erp5_discussion/PathTemplateItem/web_page_module/discussion_gadget_latest_post_js.js
@@ -0,0 +1,43 @@
+/*jslint indent: 2, maxerr: 3, nomen: true */
+/*global window, rJS, RSVP, domsugar*/
+(function (window, rJS, RSVP, domsugar) {
+  "use strict";
+
+  rJS(window)
+    //////////////////////////////////////////////
+    // acquired method
+    //////////////////////////////////////////////
+    .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    //////////////////////////////////////////////
+    // initialize the gadget content
+    //////////////////////////////////////////////
+    .declareMethod('render', function render(options) {
+      var gadget = this, jio_key = options.jio_key,
+        author = options.last_post_author_dict.author_title;
+      return gadget.getSetting("hateoas_url")
+        .push(function (hateoas_url) {
+          var posts_view = hateoas_url +
+            '/ERP5Document_getHateoas?mode=traverse&relative_url=' +
+            jio_key +
+            '&view=view_posts';
+          return gadget.getUrlFor({
+            command: 'push_history',
+            options: {
+              'jio_key': jio_key,
+              'page': 'form',
+              'view': posts_view,
+              'last_page': true
+            }
+          });
+        })
+        .push(function (url) {
+          gadget.element.appendChild(domsugar('a', {
+            href: url,
+            text: author
+          }));
+        });
+    });
+
+}(window, rJS, RSVP, domsugar));
