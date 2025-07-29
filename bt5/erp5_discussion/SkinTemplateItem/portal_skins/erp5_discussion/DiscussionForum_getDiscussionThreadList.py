7db168d60b7cc4cf7b9b17e895ce05f33e3509bc
Roque Porchetto <roque.porchetto@nexedi.com>
2025-07-30 03:28:23 +0900

    erp5_discussion: - change redirect to drop thread_relative_url parameter - update discussion forum module access permission - refactor old WebSection_ script - refactor thread list script to drop getObject - fix xhtml forum threads view - state column in thread listbox - handle only valid threads

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_getDiscussionThreadList.py
@@ -1,14 +1,5 @@
 """
-  This script gets list of Discussion Thread for a Forum using catalog.
-  Due to asynchronous nature of catalog it can use
-  passed from REQUEST not index yet threads.
+  This script gets list of Discussion Thread for a Forum using predicate search.
 """
-
-thread_relative_url = context.REQUEST.get('thread_relative_url')
-discussion_thread_list = [x.getObject() for x  in context.searchResults(portal_type="Discussion Thread", sort_on=[('modification_date', 'descending')])] #searches in all erp5 docs linked to this predicate config
-
-if thread_relative_url is not None:
-  thread = context.restrictedTraverse(thread_relative_url)
-  if thread is not None and thread not in discussion_thread_list:
-    discussion_thread_list = [thread] + discussion_thread_list
-return discussion_thread_list
+return context.searchResults(portal_type='Discussion Thread', sort_on=[('modification_date', 'descending')])
+# couldn't make this work on listbox tales config directly
