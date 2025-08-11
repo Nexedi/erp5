47eb1718296f5a03ad5edcd3cd04424fddd30c1e
Roque Porchetto <roque.porchetto@nexedi.com>
2025-08-11 18:17:00 +0000

    erp5_discussion: fix redirect in old forum scripts

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
@@ -1,5 +1,8 @@
 """
- Old forum backward compatibility script, wraps new DiscussionForum_getDiscussionThreadList
+ Old forum backward compatibility script
+  This script gets list of Discussion Thread for a Forum using predicate search
+  Due to asynchronous nature of catalog it can use
+  passed from REQUEST not index yet threads.
 """
 
 # get the related forum using follow_up
@@ -11,4 +14,10 @@ if result:
 else:
   raise ValueError, 'Unable to found a valid Discussion Forum for the current web site/section'
 
-return forum.DiscussionForum_getValidDiscussionThreadList(**kw)
+thread_relative_url = context.REQUEST.get('thread_relative_url')
+discussion_thread_list = [x.getObject() for x  in context.searchResults(portal_type='Discussion Thread', sort_on=[('modification_date', 'descending')], validation_state=('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive'))]
+if thread_relative_url is not None:
+  thread = context.restrictedTraverse(thread_relative_url)
+  if thread is not None and thread not in discussion_thread_list:
+    discussion_thread_list = [thread] + discussion_thread_list
+return discussion_thread_list
