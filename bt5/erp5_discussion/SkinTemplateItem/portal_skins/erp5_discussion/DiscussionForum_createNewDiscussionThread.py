46f463c070bab72a18b774252bd970539e74c277
Roque Porchetto <roque.porchetto@nexedi.com>
2025-08-15 18:59:49 +0000

    erp5_discussion: fix redirect

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_createNewDiscussionThread.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_createNewDiscussionThread.py
@@ -134,5 +134,4 @@ if redirect_url:
                                keep_items = dict(portal_status_message=context.Base_translateString(portal_status_message),
                                                  thread_relative_url=discussion_thread.getRelativeUrl()))
 else:
-  return context.Base_redirect(redirect_url=discussion_thread.getRelativeUrl(),
-                               keep_items = dict(portal_status_message=context.Base_translateString(portal_status_message)))
+  return discussion_thread.Base_redirect(keep_items = dict(portal_status_message=context.Base_translateString(portal_status_message)))
