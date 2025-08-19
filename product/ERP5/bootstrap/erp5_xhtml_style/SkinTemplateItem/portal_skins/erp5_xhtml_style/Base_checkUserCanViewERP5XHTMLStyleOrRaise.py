a9c97b3a313c63d3980513c84988b753e13a07a5
Romain Courteaud <romain@nexedi.com>
2025-08-19 09:13:01 +0000

    erp5_xhtml_style: forbid using the ignore_layout parameter by anonymous user
    Prevent anonymous web bots to crawl xhtml style from a public web site.

--- product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ product/ERP5/bootstrap/erp5_xhtml_style/SkinTemplateItem/portal_skins/erp5_xhtml_style/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -1,2 +1,13 @@
 # Override this script to control user access to ERP5 XHTML-style rendering.
-pass
+from zExceptions import Redirect
+
+portal = context.getPortalObject()
+if portal.portal_membership.isAnonymousUser():
+  # Forbid the usage of the ignore_layout parameter for anonymous user
+  # This prevents web bots to crawl xhtml style, as it leads to a lot of urls
+  web_site_value = context.getWebSiteValue()
+  if web_site_value is not None:
+    if portal.REQUEST.form.get('ignore_layout', None):
+      # Use the 303 status code, to ensure changing the HTTP method to a GET
+      portal.REQUEST.RESPONSE.setStatus(303, lock=True)
+      raise Redirect(web_site_value.absolute_url())
