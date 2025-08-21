78aff6308c6db8bd44518da1304c7c3f0639028f
Romain Courteaud <romain@nexedi.com>
2025-08-21 14:50:07 +0000

    erp5_web_renderjs_ui: completely block xhtml style for anonymous user

--- bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
+++ bt5/erp5_web_renderjs_ui/SkinTemplateItem/portal_skins/erp5_xhtml_disabled/Base_checkUserCanViewERP5XHTMLStyleOrRaise.py
@@ -12,10 +12,16 @@ if preference_tool.isPreferredHtmlStyleDisabled():
     raise Forbidden('xhtml_style is disabled. Please use ERP5JS')
 
 elif portal.portal_membership.isAnonymousUser():
-  # Forbid the usage of the ignore_layout parameter for anonymous user
-  # This prevents web bots to crawl xhtml style, as it leads to a lot of urls
   web_site_value = context.getWebSiteValue()
-  if web_site_value is not None:
+  if web_site_value is None:
+    if context.getRelativeUrl() != portal.getRelativeUrl():
+      # Forbid rendering a document in xhtml style
+      # and force users to be authenticated
+      portal.REQUEST.RESPONSE.setStatus(303, lock=True)
+      raise Redirect(portal.absolute_url())
+  else:
+    # Forbid the usage of the ignore_layout parameter for anonymous user
+    # This prevents web bots to crawl xhtml style, as it leads to a lot of urls
     if portal.REQUEST.form.get('ignore_layout', None):
       # Use the 303 status code, to ensure changing the HTTP method to a GET
       portal.REQUEST.RESPONSE.setStatus(303, lock=True)
