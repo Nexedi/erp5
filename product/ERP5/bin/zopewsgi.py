Nexedi/erp5

commit b181f50c059ecde42653b16dad192aaca3c562df

zopewsgi,CMFActivity: implement shutdown in bin/zopewsgi
 - register handler for SIGTERM on python3: with supervisor, process
   receive SIGTERM after `slapos node stop`, until this commit on
   python3 shutdown never had a change to clean up.
 - give more time to waitress to finish current requests
 - trigger CMFActivity's process_shutdown on shutdown
 - explicitly stop TimerService, ActivityTool is safe thanks to
   process_shutdown, but something is needed for other subscribers such
   as AlarmTool. This also prevents waitress to log warning about the
   tasks that it would have to cancel if we leave timerserver queueing
   new tasks during shutdown.

product/ERP5/bin/zopewsgi.py
@@ -1,4 +1,3 @@
-import six
 import argparse
 import atexit
 from io import BytesIO
@@ -187,6 +186,7 @@ def runwsgi():
     parser.add_argument('--timerserver-interval', help='Interval for timerserver', type=float)
     parser.add_argument('--threads', help='Number of threads', default=4, type=int)
     parser.add_argument('--pidfile', help='Write process id in file')
+    parser.add_argument('--shutdown-timeout', help='Timeout in seconds for server shutdown', default=45, type=float)
     parser.add_argument(
       '--large-file-threshold',
       help='Requests bigger than this size in bytes get saved into a temporary file '
@@ -267,18 +267,14 @@ def runwsgi():
 
     make_wsgi_app({}, zope_conf=args.zope_conf)
 
-    if six.PY2:
-      from Signals.SignalHandler import SignalHandler
-      SignalHandler.registerHandler(signal.SIGTERM, sys.exit)
-    else:
-      warnings.warn("zope4py3: SignalHandling not implemented!")
-
     if args.timerserver_interval:
       import Products.TimerService
-      Products.TimerService.timerserver.TimerServer.TimerServer(
+      timerserver = Products.TimerService.timerserver.TimerServer.TimerServer(
           module='Zope2',
           interval=args.timerserver_interval,
       )
+    else:
+      timerserver = None
 
     if args.with_max_rlimit_nofile:
       cur_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
@@ -286,7 +282,7 @@ def runwsgi():
       resource.setrlimit(resource.RLIMIT_NOFILE, new_limit)
 
     port = urlsplit('//' + args.address).port
-    createServer(
+    server = createServer(
         app_wrapper(
           large_file_threshold=args.large_file_threshold,
           webdav_ports=[port] if args.webdav else ()),
@@ -296,4 +292,28 @@ def runwsgi():
         asyncore_use_poll=True,
         # Prevent waitress from adding its own Via and Server response headers.
         ident=None,
-    ).run()
+    )
+    logger = logging.getLogger(__name__)
+
+    def shutdown(signum, _frame):
+      logger.info("Initiating shutdown after signal %s", signal.Signals(signum).name)
+      deadline = time.time() + args.shutdown_timeout
+
+      if timerserver:
+        # shutdown timerserver, to stop queueing new waitress tasks
+        timerserver.shutdown()
+
+      # wait for current activities
+      import Products.CMFActivity.ActivityTool
+      clean = Products.CMFActivity.ActivityTool.shutdown(timeout=max(deadline - time.time(), 1))
+      if clean:
+        # shutdown waitress to stop processing http requests
+        clean = server.task_dispatcher.shutdown(timeout=max(deadline - time.time(), 1))
+      if not clean:
+        logger.critical("Could not finish clean shutdown")
+      server.close()
+      sys.exit(0 if clean else 1)
+
+    signal.signal(signal.SIGTERM, shutdown)
+    signal.signal(signal.SIGINT, shutdown)
+    server.run()

product/CMFActivity/ActivityTool.py
@@ -68,7 +68,7 @@ from Products.MailHost.MailHost import MailHostError
 
 from zLOG import LOG, INFO, WARNING, ERROR
 import warnings
-from time import time
+from time import sleep, time
 from pprint import pformat
 
 try:
@@ -622,14 +622,39 @@ class ActiveWrapper(object):
 # True when activities cannot be executing any more.
 has_processed_shutdown = False
 
-def cancelProcessShutdown():
+def shutdown(timeout):
+  """Signal ActivityTool that the application is shutting down.
+
+  Prevent shutdown from happening while an activity queue is
+  processing a batch.
+
+  Returns true if shutdown could be processed before the timeout.
   """
-    This method reverts the effect of calling "process_shutdown" on activity
-    tool.
+  global has_processed_shutdown
+  clean = True
+  if not has_processed_shutdown:
+    has_processed_shutdown = True
+    LOG('CMFActivity', INFO, "Shutdown: Waiting for activities to finish.")
+    if six.PY2:
+      end = time() + timeout
+      while time() < end:
+        sleep(0.1)
+        clean = is_running_lock.acquire(False)
+        if clean:
+          break
+    else:
+      clean = is_running_lock.acquire(timeout=timeout)
+    LOG('CMFActivity', INFO, "Shutdown: Activities finished." )
+  return clean
+
+
+def cancelProcessShutdown():
+  """This function reverts the effect of calling "shutdown".
   """
   global has_processed_shutdown
   is_running_lock.release()
   has_processed_shutdown = False
+
 
 # Due to a circular import dependency between this module and
 # Products.ERP5Type.Core.Folder, both modules must import after the definitions
@@ -1274,19 +1299,6 @@ class ActivityTool (BaseTool):
           '/manageLoadBalancing?manage_tabs_message=' +
           urllib.parse.quote(message))
 
-    security.declarePrivate('process_shutdown')
-    def process_shutdown(self, phase, time_in_phase):
-        """
-          Prevent shutdown from happening while an activity queue is
-          processing a batch.
-        """
-        global has_processed_shutdown
-        if phase == 3 and not has_processed_shutdown:
-          has_processed_shutdown = True
-          LOG('CMFActivity', INFO, "Shutdown: Waiting for activities to finish.")
-          is_running_lock.acquire()
-          LOG('CMFActivity', INFO, "Shutdown: Activities finished.")
-
     security.declareProtected(CMFCorePermissions.ManagePortal, 'process_timer')
     def process_timer(self, tick, interval, prev="", next=""):
       """
@@ -1361,6 +1373,8 @@ class ActivityTool (BaseTool):
             break
         finally:
           is_running_lock.release()
+          if has_processed_shutdown:
+            break
 
     security.declarePublic('tic')
     def tic(self, processing_node=1, force=0):
@@ -1406,6 +1420,9 @@ class ActivityTool (BaseTool):
               break
           finally:
             is_running_lock.release()
+            if has_processed_shutdown:
+              break
+
       finally:
         # decrease the number of active_threads
         with tic_lock:

product/CMFActivity/tests/testCMFActivity.py
@@ -53,7 +53,7 @@ from zLOG import LOG
 from ZODB.POSException import ConflictError
 from DateTime import DateTime
 from Products.CMFActivity.ActivityTool import (
-  cancelProcessShutdown, Message, getCurrentNode, getServerAddress)
+  cancelProcessShutdown, shutdown, Message, getCurrentNode, getServerAddress)
 from MySQLdb import OperationalError
 from Products.ZMySQLDA.db import DB
 import gc
@@ -1765,7 +1765,7 @@ class TestCMFActivity(ERP5TypeTestCase, LogInterceptor):
       # activity won't be executed.
       class ProcessShutdownThread(threading.Thread):
         def run(self):
-          activity_tool.process_shutdown(3, 0)
+          shutdown(5)
       process_shutdown_thread = ProcessShutdownThread()
       # Do not try to outlive main thread.
       process_shutdown_thread.daemon = True

product/TimerService/timerserver/TimerServer.py
@@ -21,9 +21,14 @@ class TimerServer(threading.Thread):
         self.daemon = True
         self.module = module
         self.interval = interval
+        self._shutdown = False
         self.start()
         logger.info('Service initialized with interval of %s second(s).',
                     interval)
+
+    def shutdown(self):
+        logger.debug('Shutdown request received')
+        self._shutdown = True
 
     def run(self):
         try:
@@ -93,7 +98,7 @@ class TimerServer(threading.Thread):
 
         logger.info('Service ready.')
 
-        while 1:
+        while not self._shutdown:
             time.sleep(interval)
             # send message to zope
             try:
@@ -102,7 +107,8 @@ class TimerServer(threading.Thread):
                 response = TimerResponse(out, err)
                 handle(module, TimerRequest(response, interval), response)
             except Exception:
-                logger.warn("Ignoring exception in run loop", exc_info=True)
+                logger.warning("Ignoring exception in run loop", exc_info=True)
+        logger.info('Finished')
 
 
 TIMER_SERVICE_PATH = '/Control_Panel/timer_service'

commit 9e66b11cbb3c07b64316595e02a5524605b190a0

zopewsgi: fix shutdown on py2
A regression was introduced in b181f50c05 (zopewsgi,CMFActivity:
implement shutdown in bin/zopewsgi, 2025-07-12), the graceful shutdown
handler was no longer executed on python2, process was terminated
without doing any clean up.

product/ERP5/bin/zopewsgi.py
@@ -305,7 +305,11 @@ def runwsgi():
     logger = logging.getLogger(__name__)
 
     def shutdown(signum, _frame):
-      logger.info("Initiating shutdown after signal %s", signal.Signals(signum).name)
+      if sys.version_info >= (3, ):
+        signal_name = signal.Signals(signum).name
+      else:
+        signal_name = signum
+      logger.info("Initiating shutdown after signal %s", signal_name)
       deadline = time.time() + args.shutdown_timeout
 
       if timerserver: