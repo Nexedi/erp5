e174ccd5160c328442fe9930773e4c0064797590
Roque Porchetto <roque.porchetto@nexedi.com>
2025-08-27 18:51:16 +0000

    erp5_discussion: refactor web_section-forum get into a script

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
@@ -5,17 +5,11 @@
   passed from REQUEST not index yet threads.
 """
 
-# get the related forum using follow_up
-result = context.getFollowUpRelatedValueList(portal_type = "Discussion Forum")
-valid_states = ('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive')
-result = [forum for forum in result if forum.getValidationState() in valid_states]
-if result:
-  forum = result[0]
-else:
-  raise ValueError, 'Unable to found a valid Discussion Forum for the current web site/section'
-
+forum = context.WebSection_getRelatedForum()
 thread_relative_url = context.REQUEST.get('thread_relative_url')
-discussion_thread_list = [x.getObject() for x  in context.searchResults(portal_type='Discussion Thread', sort_on=[('modification_date', 'descending')], validation_state=('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive'))]
+discussion_thread_list = [x.getObject() for x  in context.searchResults(portal_type='Discussion Thread',
+                                                                        sort_on=[('modification_date', 'descending')],
+                                                                        validation_state=('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive'))]
 if thread_relative_url is not None:
   thread = context.restrictedTraverse(thread_relative_url)
   if thread is not None and thread not in discussion_thread_list:
