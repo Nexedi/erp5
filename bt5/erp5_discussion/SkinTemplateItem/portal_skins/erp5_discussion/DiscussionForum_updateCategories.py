79474f43b45af73e2ab556c99c06ca07df96d311
Roque Porchetto <roque.porchetto@nexedi.com>
2025-08-29 15:50:15 +0000

    erp5_discussion: do not explicitly set follow up

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_updateCategories.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/DiscussionForum_updateCategories.py
@@ -7,8 +7,4 @@ membership_criterion_category = forum.getMembershipCriterionCategoryList()
 for base_category in multi_membership_criterion_category:
   category_list.extend([x for x in membership_criterion_category if x.startswith(base_category)])
 
-# keep follow up values before set categories (e.g. old web_section forum for bakcward compatibility)
-previous_follow_up_list = forum.getFollowUpList()
 forum.setCategoryList(category_list)
-new_follow_up_list = list(set(forum.getFollowUpList() + previous_follow_up_list))
-forum.setFollowUpList(new_follow_up_list)
