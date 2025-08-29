e3063d67cd049f61cd26c9775462a8f15851b0b8
Roque Porchetto <roque.porchetto@nexedi.com>
2025-08-29 15:06:32 +0000

    erp5_discussion: WebSection_getRelatedForum uses predicate search

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getRelatedForum.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getRelatedForum.py
@@ -1,11 +1,9 @@
 """
- Old forum backward compatibility script, returns the related forum object created during the migration
+ Old forum backward compatibility script, returns the related forum object created during the migration, using predicate search
 """
 
-# get the related forum using follow_up
-result = context.getFollowUpRelatedValueList(portal_type = "Discussion Forum")
 valid_states = ('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive')
-result = [forum for forum in result if forum.getValidationState() in valid_states]
+result = [x.getObject() for x  in context.searchResults(portal_type='Discussion Forum', validation_state=valid_states)]
 if result:
   return result[0]
 else:
