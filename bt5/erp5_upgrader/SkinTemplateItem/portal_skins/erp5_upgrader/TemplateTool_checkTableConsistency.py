dc20090255a5db6c514b41f7d3b50f7a07ecab3b
Jérome Perrin <jerome@nexedi.com>
2025-09-26 22:03:45 +0900

    Inventory API: Do not use regex for measure
    `concat(stock.variation_text,'\n') REGEXP measure.variation` is slow.
    This introduces an alternative approach of indexing the first 4
    variations directly in columns and use a traditional join instead or
    a regexp match.
    In a real production case of a 2 seconds query, this was one order of
    magnitude faster.
    
    This has some limitations:
      - it does not support more than 4 variations
      - it does not support optional variations
    
    The previous regexp based implementation is kept in this patch, so that
    it's easy to rollback this change if side effects are found.

--- bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/TemplateTool_checkTableConsistency.py
+++ bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/TemplateTool_checkTableConsistency.py
@@ -4,4 +4,10 @@ If `fixit` is True, then it will create/drop table or alter existing tables
 """
 portal = context.getPortalObject()
 show_source = not(fixit)
-return portal.portal_catalog.upgradeSchema(src__=show_source)
+output = portal.portal_catalog.upgradeSchema(src__=show_source)
+if fixit:
+  for line in output:
+    if "ALTER TABLE `measure`" in line and "ADD COLUMN `variation_text_line_1`" in line:
+      output += ["reindexing measures for new variation_text_line_* structure"]
+      portal.portal_catalog.searchAndActivate(portal_type='Measure', method_id='recursiveImmediateReindexObject')
+return output
