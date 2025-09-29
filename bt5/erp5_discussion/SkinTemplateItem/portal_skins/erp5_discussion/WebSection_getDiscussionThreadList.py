d3f6b74e66d3cb261d5053ffac093ed1967bb2b5
Roque Porchetto <roque.porchetto@nexedi.com>
2025-09-29 15:19:16 +0000

    erp5_discussion: control result

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getDiscussionThreadList.py
@@ -4,12 +4,16 @@
   Due to asynchronous nature of catalog it can use
   passed from REQUEST not index yet threads.
 """
+from zExceptions import Unauthorized
 
 forum = context.WebSection_getRelatedForum()
 thread_relative_url = context.REQUEST.get('thread_relative_url')
-discussion_thread_list = [x.getObject() for x  in forum.searchResults(portal_type='Discussion Thread',
+try:
+  discussion_thread_list = [x.getObject() for x  in forum.searchResults(portal_type='Discussion Thread',
                                                                         sort_on=[('modification_date', 'descending')],
                                                                         validation_state=('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive'))]
+except Unauthorized:
+  discussion_thread_list = []
 if thread_relative_url is not None:
   thread = context.restrictedTraverse(thread_relative_url)
   if thread is not None and thread not in discussion_thread_list:
