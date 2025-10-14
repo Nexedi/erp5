b10a0ecb584cc04c1295e3a186d4572ab888779d
Jérome Perrin <jerome@nexedi.com>
2025-10-14 09:23:49 +0900

    testXHTML: fix error reporting on py3

--- product/ERP5/tests/testXHTML.py
+++ product/ERP5/tests/testXHTML.py
@@ -39,7 +39,7 @@ from subprocess import Popen, PIPE
 from AccessControl import getSecurityManager
 from Testing import ZopeTestCase
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
-from Products.ERP5Type.Utils import parse_http_header
+from Products.ERP5Type.Utils import parse_http_header, unicode2str
 from Products.ERP5Type.tests.utils import addUserToDeveloperRole, findContentChain
 from Products.ERP5Form.ProxyField import BrokenProxyField
 from Products.CMFCore.utils import getToolByName
@@ -500,9 +500,9 @@ class NuValidator(object):
         severity_list = warning_list
       else:
         severity_list = error_list
-      txt = message['message'].encode('UTF-8')
+      txt = unicode3str(message['message'])
       if 'extract' in message:
-        txt += ': %s' % message['extract'].encode('UTF-8')
+        txt += ': %s' % unicode2str(message['extract'])
       severity_list.append([message['lastLine'], message['lastColumn'], txt])
     return [error_list, warning_list]
 
