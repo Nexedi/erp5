c424bdeb03148c4665efaaff316630557cff400e
Rafael Monnerat <rafael@nexedi.com>
2025-11-26 23:00:29 -0300

    erp5_development_wizard: Fixup CodingStyle tests (pylint)

--- bt5/erp5_development_wizard/SkinTemplateItem/portal_skins/erp5_development/Base_createSkinFolder.py
+++ bt5/erp5_development_wizard/SkinTemplateItem/portal_skins/erp5_development/Base_createSkinFolder.py
@@ -1,5 +1,5 @@
 portal_skins = context.getPortalObject().portal_skins
-skin_folder = portal_skins.manage_addProduct['OFSP'].manage_addFolder(skin_folder_id)
+portal_skins.manage_addProduct['OFSP'].manage_addFolder(skin_folder_id)
 
 # Add the new skin folder at the top of portal_skins.
 for skin_name, selection in portal_skins.getSkinPaths():
