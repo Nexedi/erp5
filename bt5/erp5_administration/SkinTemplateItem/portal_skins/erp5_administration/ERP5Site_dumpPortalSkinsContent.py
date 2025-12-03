8a2ecef823040a89d61aa4d3e430cd4fd3810723
Rafael Monnerat <rafael@nexedi.com>
2025-12-03 18:47:12 +0000

    erp5_administration: Handle Pdata for Skin.data
       use str is not enough to determinate the content if skin.data is a Pdata object.

--- bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpPortalSkinsContent.py
+++ bt5/erp5_administration/SkinTemplateItem/portal_skins/erp5_administration/ERP5Site_dumpPortalSkinsContent.py
@@ -16,7 +16,11 @@ def getSkinHash(skin, skin_container):
   if skin.meta_type in ('Script (Python)', 'Z SQL Method', ):
     content = skin.document_src()
   elif skin.meta_type in ('File', ):
-    content = str(skin.data)
+    content = skin.data
+    # In case skin.data is PData, convert direct to bytes
+    if not isinstance(content, bytes) and \
+               not isinstance(content, str):
+      content = bytes(content)
   elif skin.meta_type in ('ERP5 Form', ):
     try:
       content = skin.formXML()
@@ -25,7 +29,9 @@ def getSkinHash(skin, skin_container):
       content = "broken form %s" % e
     content = 'ignore'
   m = hashlib.md5()
-  m.update(str2bytes(content))
+  if not isinstance(content, bytes):
+    content = str2bytes(content)
+  m.update(content)
   content_hash = m.hexdigest()
   return ";".join((skin_container.getId(), skin.getId(), skin.meta_type, content_hash))
 
