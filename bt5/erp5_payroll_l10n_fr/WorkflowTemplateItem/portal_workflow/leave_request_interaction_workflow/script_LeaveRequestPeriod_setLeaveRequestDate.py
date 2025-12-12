707bd6f94d84f7c6a7abf361ce72ca4f73467010
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2025-12-12 14:52:55 +0000

    erp5_payroll_l10n_fr: fix

--- bt5/erp5_payroll_l10n_fr/WorkflowTemplateItem/portal_workflow/leave_request_interaction_workflow/script_LeaveRequestPeriod_setLeaveRequestDate.py
+++ bt5/erp5_payroll_l10n_fr/WorkflowTemplateItem/portal_workflow/leave_request_interaction_workflow/script_LeaveRequestPeriod_setLeaveRequestDate.py
@@ -2,9 +2,27 @@ leave_request = state_change['object'].getParentValue()
 leave_request_period_list = leave_request.objectValues(
   portal_type='Leave Request Period',
 )
-leave_request.setStartDate(
-  min([x.getStartDate() for x in leave_request_period_list])
-)
-leave_request.setStopDate(
-  max([x.getStopDate() for x in leave_request_period_list])
+
+
+min_start_date = None
+max_stop_date = None
+
+for x in leave_request_period_list:
+  start_date = x.getStartDate()
+  if start_date:
+    if not min_start_date:
+      min_start_date = start_date
+    elif min_start_date > start_date:
+        min_start_date = start_date
+
+  stop_date = x.getStopDate()
+  if stop_date:
+    if not max_stop_date:
+      max_stop_date = stop_date
+    elif max_stop_date < stop_date:
+      max_stop_date = stop_date
+
+leave_request.edit(
+  start_date = min_start_date,
+  stop_date = max_stop_date
 )
