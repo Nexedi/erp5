1006e748b956596d649fbf16da33e56b95c02c23
Titouan Soulard <titouan.soulard@rapid.space>
2026-01-20 10:57:28 +0100

    erp5_bank_reconciliation: define Bank Reconciliation Line type
    Introduces the type and some basic views, as well as required changes to the
    parent Bank Reconciliation type, since the new type will be used for importing
    spreadsheet into a Bank Reconciliation object.

---
+++ bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/BankReconciliation_getModulePortalTypeSortedPropertyList.py
@@ -0,0 +1,18 @@
+translateString = context.Base_translateString
+# XXX: assumes we only allow one Portal Type for children
+line_portal_type_reference = context.allowedContentTypes()[0].id
+
+base_property_list = map(lambda x: (
+  translateString(x[0]),
+  "%s.%s" % (line_portal_type_reference, x[1]),
+), [
+  ("Transaction Date", "start_date"),
+  ("Accounting Date", "stop_date"),
+  ("Title", "title"),
+  ("Reference", "reference"),
+  ("Debit", "source_debit"),
+  ("Credit", "source_credit"),
+])
+base_property_list.sort()
+
+return base_property_list
