0a159b33bd42e7450fb320ff6444fb39cdcf9efe
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-01-22 15:28:44 +0100

    erp5_mysql_innodb: sql

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/SkinTemplateItem/portal_skins/erp5_mysql_innodb/IdTool_zGenerateId.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/SkinTemplateItem/portal_skins/erp5_mysql_innodb/IdTool_zGenerateId.sql
@@ -1,13 +1,10 @@
-# Warning : This method is obsolete, the new method is IdTool_zGenerateId
-#           it's kept for backward compatiblity
-
 # DO NOT FORGET TO COMMIT AFTER !!
-# commit ZSQL method should be z_portal_ids_commit
+# commit ZSQL method should be IdTool_zCommit
 
 BEGIN
 <dtml-var sql_delimiter>
 INSERT INTO portal_ids (`id_group`, `last_id`)
- VALUES (<dtml-sqlvar id_group type="string">, <dtml-sqlvar expr="id_count + default" type="int">)
+ VALUES (<dtml-sqlvar id_group type="string">, <dtml-sqlvar expr="id_count - 1 + default" type="int">)
  ON DUPLICATE KEY UPDATE `last_id` = `last_id` + <dtml-sqlvar id_count type="int">
 <dtml-var sql_delimiter>
 SELECT `last_id` AS `LAST_INSERT_ID` FROM portal_ids
