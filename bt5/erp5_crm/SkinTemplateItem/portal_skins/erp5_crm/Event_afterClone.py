636c88ba3ac1702da1ef1bf1cd4bc7b6c9f65aea
Titouan Soulard <titouan.soulard@rapid.space>
2026-01-23 10:40:04 +0100

    erp5_crm: do not reset data on clone
    Text content is now data, so it makes no sense to edit both.

--- bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Event_afterClone.py
+++ bt5/erp5_crm/SkinTemplateItem/portal_skins/erp5_crm/Event_afterClone.py
@@ -4,7 +4,6 @@ content_dict = context.getPropertyDictFromContent()
 content_dict['title'] = context.getTitle()
 content_dict['text_content'] = context.getTextContent()
 content_dict['content_type'] = context.getContentType()
-content_dict['data'] = None
 # we may have specified (i.e. user input) source and destination
 content_dict['destination_list'] = content_dict.get('destination_list', []) + \
                                      [x.getRelativeUrl() for x in context.getDestinationValueList() \
