a6e988849ef1807ed1fc231889b536b80ec00c4e
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-01-28 11:31:13 +0100

    tests/__init__.py: only core tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -38,9 +38,9 @@ class _Base(ERP5TypeTestSuite):
     component_re = re.compile(".*/([^/]+)/TestTemplateItem/portal_components"
                               "/test\.[^.]+\.([^.]+).py$")
     for test_path in chain(
-        glob(path + '/product/*/tests/test*.py'),
-        glob(path + '/bt5/*/TestTemplateItem/test*.py'),
-        glob(path + '/bt5/*/TestTemplateItem/portal_components/test.*.test*.py')):
+        #glob(path + '/product/*/tests/test*.py'),
+        glob(path + '/bt5/erp5_core_test/TestTemplateItem/test*.py'),
+        glob(path + '/bt5/erp5_core_test/TestTemplateItem/portal_components/test.*.test*.py')):
       component_re_match = component_re.match(test_path)
       if component_re_match is not None:
         test_case = "%s:%s" % (component_re_match.group(1),
