9e53e03e5ee98862e5269123f2a907e092d2ed10
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-02-04 14:04:57 +0100

    ZMySQLDA: xxxx

--- product/ZMySQLDA/db.py
+++ product/ZMySQLDA/db.py
@@ -6,7 +6,10 @@ if erp5_catalog_storage == 'erp5_sqlite_catalog':
   from .sqlite import OperationalError
   from .sqlite import DeferredSqliteDB as DeferredDB
   from .sqlite import SqliteDB as DB
+  from .sqlite import hosed_connection
 else:
   from .mysql import OperationalError
   from .mysql import DeferredMysqlDB as DeferredDB
   from .mysql import MysqlDB as DB
+  from .mysql import hosed_connection
+
