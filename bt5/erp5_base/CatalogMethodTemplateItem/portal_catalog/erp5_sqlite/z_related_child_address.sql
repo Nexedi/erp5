59bc264a31c71947b14c83ab6005230ab93f82ae
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-02-19 10:03:45 +0000

    erp5_base:

---
+++ bt5/erp5_base/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/z_related_child_address.sql
@@ -0,0 +1,4 @@
+<dtml-var table_0>.uid = <dtml-var table_1>.uid
+<dtml-var RELATED_QUERY_SEPARATOR>
+ <dtml-var table_0>.portal_type = 'Address'
+AND <dtml-var query_table>.uid = <dtml-var table_0>.parent_uid
