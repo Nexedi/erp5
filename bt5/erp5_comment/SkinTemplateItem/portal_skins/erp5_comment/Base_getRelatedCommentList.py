0a0754650af1ffac3458091efe0f48ee0e9ce97a
Klaus Wölfel <klaus@nexedi.com>
2026-02-27 17:18:40 +0000

    erp5_comment: fixup!

--- bt5/erp5_comment/SkinTemplateItem/portal_skins/erp5_comment/Base_getRelatedCommentList.py
+++ bt5/erp5_comment/SkinTemplateItem/portal_skins/erp5_comment/Base_getRelatedCommentList.py
@@ -2,7 +2,7 @@ portal = context.getPortalObject()
 objectValues = portal.comment_module.objectValues
 
 date = DateTime().strftime('%Y%m%d')
-today_set = {x for x in objectValues(base_id=date) if x.getFollowUpValue() == context}
+today_set = {x for x in objectValues(base_id=date, checked_permission="View") if x.getFollowUp() == context.getRelativeUrl()}
 related_set= {x.getObject() for x in context.getFollowUpRelatedValueList(portal_type="Comment")}
 result_list = list(today_set.union(related_set))
 result_list.sort(key=lambda x: x.getCreationDate())
