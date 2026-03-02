00adb81b95a858a4c098f2a2dca59b0104f81d56
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-02 15:08:58 +0100

    erp5_core: move out to support multi catalog

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/ERP5Site_zGetLatestIndexedObjectList.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  uid, path
-FROM
-  catalog
-WHERE
-  indexation_timestamp >= TIMESTAMPADD(SECOND, - <dtml-sqlvar delta type="int">, CURRENT_TIMESTAMP)
--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/IdTool_zCreateEmptyTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `portal_ids` (
-  `id_group` VARCHAR(255) BINARY,
-  `last_id` BIGINT UNSIGNED,
-  PRIMARY KEY  (`id_group`)
-) ENGINE=InnoDB;
