2d5e3fc551d28590e4b500304ab8c0841e194512
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-02 14:10:31 +0100

    SqliteFullTextKey&erp5_full_text_sqlite_catalog

--- bt5/erp5_full_text_sqlite_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/z_create_catalog_fulltext.sql
+++ bt5/erp5_full_text_sqlite_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/z_create_catalog_fulltext.sql
@@ -9,7 +9,52 @@ CREATE TABLE catalog_full_text (
 
 CREATE VIRTUAL TABLE catalog_full_text_fts
 USING fts5(
-  uid UNINDEXED,
   title,
-  description
+  description,
+  content='catalog_full_text',
+  content_rowid='uid',
+  tokenize='unicode61'
 );
+
+<dtml-var sql_delimiter>
+
+
+CREATE TRIGGER catalog_full_text_ai
+AFTER INSERT ON catalog_full_text
+BEGIN
+  INSERT INTO catalog_full_text_fts(rowid, title, description)
+  VALUES (new.uid, new.title, new.description);
+END;
+
+<dtml-var sql_delimiter>
+
+
+CREATE TRIGGER catalog_full_text_ad
+AFTER DELETE ON catalog_full_text
+BEGIN
+  INSERT INTO catalog_full_text_fts(
+    catalog_full_text_fts,
+    rowid,
+    title,
+    description
+  )
+  VALUES('delete', old.uid, old.title, old.description);
+END;
+
+<dtml-var sql_delimiter>
+
+
+CREATE TRIGGER catalog_full_text_au
+AFTER UPDATE ON catalog_full_text
+BEGIN
+  INSERT INTO catalog_full_text_fts(
+    catalog_full_text_fts,
+    rowid,
+    title,
+    description
+  )
+  VALUES('delete', old.uid, old.title, old.description);
+
+  INSERT INTO catalog_full_text_fts(rowid, title, description)
+  VALUES (new.uid, new.title, new.description);
+END;
