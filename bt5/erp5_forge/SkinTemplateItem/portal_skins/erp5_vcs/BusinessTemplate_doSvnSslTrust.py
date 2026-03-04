dd1cb120e590491f3bc10994f5ff4e517fd2a518
Jérome Perrin <jerome@nexedi.com>
2026-03-04 17:39:33 +0900

    forge: Remove Subversion support
    We are no longer using Subversion as our version control system and it's
    painful to maintain this code, especially updating dependencies to newer
    python3 versions.

--- bt5/erp5_forge/SkinTemplateItem/portal_skins/erp5_vcs/BusinessTemplate_doSvnSslTrust.py
+++
@@ -1,9 +0,0 @@
-request = context.REQUEST
-if caller_kw is None:
-  caller_kw = {}
-trust_dict=dict((x, request[x]) for x in (
-  'valid_until', 'hostname', 'realm', 'finger_print', 'valid_from', 'issuer_dname', 'failures'))
-
-context.getVcsTool().acceptSSLServer(trust_dict, True)
-
-return context.restrictedTraverse(caller)(**caller_kw)
