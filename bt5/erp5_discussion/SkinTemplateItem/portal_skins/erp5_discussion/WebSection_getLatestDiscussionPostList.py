303c60cdc9483f0a5b8ef019e0d97caa2b8a1b4c
Roque Porchetto <roque.porchetto@nexedi.com>
2026-03-06 17:22:50 +0000

    erp5_discussion: old backward-compat script wraps new script to fetch latest posts - force displaying gadget field content

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getLatestDiscussionPostList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getLatestDiscussionPostList.py
@@ -1,32 +1,6 @@
 """
-  Get list of latest Posts which belong to a forum (i.e. websection + predicate)
+ Old forum backward compatibility script, wraps new DiscussionForum_getLatestPostList
 """
 
-limit = kw.get("limit", 100)
-size = kw.pop("size", None)
-if size is not None:
-  limit = min(size, limit)
-
-parent_uid_list = None
-
 forum = context.WebSection_getRelatedForum()
-if forum:
-  # get list of all forum Threads (details should be set on predicate)
-  parent_uid_list = [
-    x.uid for x in forum.searchResults(portal_type='Discussion Thread',
-                                       sort_on=[('modification_date', 'descending')],
-                                       validation_state=('published', 'published_alive', 'released', 'released_alive', 'shared', 'shared_alive'))
-  ]
-
-if not parent_uid_list:
-  # no related forum or
-  # no parent discussion threads therefore no posts
-  return []
-
-# get sorted list of all "contained" in them Posts
-kw['sort_on'] = (('modification_date', 'DESC'),)
-kw['portal_type'] = 'Discussion Post'
-kw['parent_uid'] = parent_uid_list
-kw['limit'] = limit
-
-return context.portal_catalog(**kw)
+return forum.DiscussionForum_getLatestPostList(**kw)
