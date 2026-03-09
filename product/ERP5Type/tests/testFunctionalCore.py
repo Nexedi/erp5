9913acc0eff41dce462733ec83ed5226997a6006
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-09 11:21:34 +0100

    erp5_ingestion_mysql_innodb_catalog --> erp5_base

--- product/ERP5Type/tests/testFunctionalCore.py
+++ product/ERP5Type/tests/testFunctionalCore.py
@@ -71,7 +71,7 @@ class TestZeleniumCore(ERP5TypeFunctionalTestCase):
                 'erp5_simulation_test',
                 'erp5_accounting_ui_test',
                 'erp5_project_ui_test',
-                'erp5_ingestion_mysql_innodb_catalog', 'erp5_ingestion',
+                'erp5_ingestion',
                 'erp5_web', 'erp5_dms', 'erp5_dms_ui_test',
                 'erp5_ui_test_data',
                 'erp5_knowledge_pad_ui_test',
