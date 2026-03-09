9c0eb0615b3c97f5893e4f9fa4741f1cdc44d9dd
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-09 11:21:34 +0100

    erp5_ingestion_mysql_innodb_catalog --> erp5_base

--- bt5/erp5_wendelin_configurator/TestTemplateItem/portal_components/test.erp5.WendelinConfiguratorMixin.py
+++ bt5/erp5_wendelin_configurator/TestTemplateItem/portal_components/test.erp5.WendelinConfiguratorMixin.py
@@ -160,7 +160,6 @@ class WendelinConfiguratorMixin(SecurityTestCase):
       'erp5_web_renderjs_ui',
       'erp5_dms',
       'erp5_jquery',
-      'erp5_ingestion_mysql_innodb_catalog',
       'erp5_ingestion',
       'erp5_forge',
       'erp5_jquery_plugin_elastic',
