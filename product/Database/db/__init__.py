b15fd12586570ad9cf5bb642e1213e9746d482c2
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-09 11:11:44 +0100

    product/Database: add Database product to load dynamically database adaptor

---
+++ product/Database/db/__init__.py
@@ -0,0 +1,13 @@
+_backend = None
+
+def configure(erp5_catalog_storage):
+    global _backend
+    if erp5_catalog_storage == 'erp5_mysql_innodb_catalog':
+        from Products.ZMySQLDA import db as _backend
+    elif erp5_catalog_storage == 'erp5_sqlite_catalog':
+        from Products.ZSQLiteDA import db as _backend
+    else:
+        raise ImportError("Unsupported DB type %s" % erp5_catalog_storage)
+
+def __getattr__(name):
+    return getattr(_backend, name)
