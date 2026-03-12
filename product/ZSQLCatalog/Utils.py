b173dc4982169b4f6594abec3661cf81bb407453
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-12 15:40:30 +0100

    ZSQLCatalog/Utils: let's see

--- product/ZSQLCatalog/Utils.py
+++ product/ZSQLCatalog/Utils.py
@@ -27,6 +27,7 @@
 import six
 from Products.ERP5Type.Utils import bytes2str
 import os
+from Products.ERP5.ERP5Site import getSite
 
 def sqlquote(value):
   # See MySQL documentation of string literals.
@@ -50,6 +51,7 @@ def sqlquote(value):
       #.replace('\x22', r'\"')
       .replace('\x27', r"''")
     ) + "'"
+  """
   return "'" + (value
     .replace('\x5c', r'\\')
     .replace('\x00', r'\0')
@@ -59,5 +61,8 @@ def sqlquote(value):
     .replace('\x0d', r'\r')
     .replace('\x1a', r'\Z')
     .replace('\x22', r'\"')
-    .replace('\x27', r"\'")
+    .replace('\x27', r"''")
   ) + "'"
+  """
+  connection = getSite()['erp5_sql_connection']
+  return connection.sql_quote__(value).decode()
