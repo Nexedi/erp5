299e9e93fdede8fdc1fdacd602b130f190853bec
Titouan Soulard <titouan.soulard@rapid.space>
2026-03-12 08:45:49 +0100

    erp5_bank_reconciliation: fix coding style test

--- bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/BankReconciliation_getModulePortalTypeSortedPropertyList.py
+++ bt5/erp5_bank_reconciliation/SkinTemplateItem/portal_skins/erp5_bank_reconciliation/BankReconciliation_getModulePortalTypeSortedPropertyList.py
@@ -2,17 +2,17 @@ translateString = context.Base_translateString
 # XXX: assumes we only allow one Portal Type for children
 line_portal_type_reference = context.allowedContentTypes()[0].id
 
-base_property_list = map(lambda x: (
+base_property_list = [(
   translateString(x[0]),
   "%s.%s" % (line_portal_type_reference, x[1]),
-), [
+) for x in [
   ("Transaction Date", "start_date"),
   ("Accounting Date", "stop_date"),
   ("Title", "title"),
   ("Reference", "reference"),
   ("Debit", "source_debit"),
   ("Credit", "source_credit"),
-])
+]]
 base_property_list.sort()
 
 return base_property_list
