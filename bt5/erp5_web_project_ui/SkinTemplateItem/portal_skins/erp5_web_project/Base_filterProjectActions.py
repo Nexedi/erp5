1bf00c58d2c541af572ceafda723e55958686ef3
Roque Porchetto <roque.porchetto@nexedi.com>
2026-03-30 14:06:38 -0300

    erp5_web_project_ui: fix project views filter script

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
@@ -5,10 +5,14 @@ for action_category_name, action_list in actions.items():
   elif (action_category_name == 'object_jio_action'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['post_query']]
   elif (action_category_name == 'project_view'):
-    filtered_actions['object_view'] = action_list
     filtered_actions[action_category_name] = action_list
   elif (action_category_name == 'object_jio_jump'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['jump_to_portal_type']]
   else:
     filtered_actions[action_category_name] = action_list
+
+# Ensure project_view actions are handled after dict iteration
+if 'project_view' in filtered_actions:
+  filtered_actions['object_view'] = filtered_actions['project_view']
+
 return filtered_actions
