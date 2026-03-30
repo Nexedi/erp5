406da039b7b5f49125493fc215514d4c0e013128
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-03-30 15:15:32 +0200

    *: use dtml-sqlvar

--- product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_search_security.sql
+++ product/ERP5/bootstrap/erp5_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_search_security.sql
@@ -4,4 +4,4 @@ FROM
   roles_and_users
 WHERE 
   allowedRolesAndUsers 
-  IN (<dtml-in security_roles_list><dtml-var sequence-item><dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
+  IN (<dtml-in security_roles_list><dtml-sqlvar sequence-item type="string"><dtml-if sequence-end><dtml-else>,</dtml-if></dtml-in>)
