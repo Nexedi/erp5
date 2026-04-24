be714a8e5b0050e047819ccc0c4b2b197a567682
Jérome Perrin <jerome@nexedi.com>
2026-04-24 13:36:05 +0900

    dms: introduce "Document Preview Max Allowed Size" preference
    This will control the maximum size of to documents in the "Preview" tab,
    in order to prevent long requests on too large documents.

--- bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/Document_getPreviewAsHTML.py
+++ bt5/erp5_dms/SkinTemplateItem/portal_skins/erp5_dms/Document_getPreviewAsHTML.py
@@ -1,16 +1,20 @@
 from Products.PythonScripts.standard import html_quote
+portal = context.getPortalObject()
+translateString = portal.Base_translateString
 
 result = ''
 error_message = None
-try:
-  result = context.asStrippedHTML()
-  if result:
-    return result
-except Exception as e:
-  from erp5.component.module.Log import log
-  log("asStrippedHTML", str(e))
-  error_message = "%s %s" % (context.Base_translateString("Preview Error:"),
-                              str(e))
+if context.getSize() > (portal.portal_preferences.getPreferredDocumentPreviewMaxAllowedSize() or 5 * 1024 * 1024):
+  error_message = translateString("Document is too large to be previewed")
+else:
+  try:
+    result = context.asStrippedHTML()
+    if result:
+      return result
+  except Exception as e:
+    from erp5.component.module.Log import log
+    log("asStrippedHTML", str(e))
+    error_message = "%s %s" % (translateString("Preview Error:"), str(e))
 
 if error_message is not None:
   return '<div class="error">%s</div>' % html_quote(error_message)
