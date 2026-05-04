e5f11278fb34a97eba07f07c3c3f9d7ae8ffe774
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-05-04 15:17:53 +0200

    CMFActivity: load according to catalog storage

---
+++ product/CMFActivity/Activity/__init__.py
@@ -0,0 +1,10 @@
+def configure(erp5_catalog_storage):
+    from . import SQLBase
+    SQLBase.configure(erp5_catalog_storage)
+    from . import SQLDict, SQLJoblib, SQLQueue
+    SQLDict.configure(erp5_catalog_storage)
+    SQLJoblib.configure(erp5_catalog_storage)
+    import Products.CMFActivity.ActivityTool as ActivityTool
+    ActivityTool.activity_dict.update(
+        {k: getattr(v, k)()
+        for k, v in {'SQLDict': SQLDict, 'SQLQueue': SQLQueue, 'SQLJoblib': SQLJoblib}.items()})
