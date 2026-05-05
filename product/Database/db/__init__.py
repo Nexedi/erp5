378e4e62784236f015d5f965437effa0994ff229
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-05-05 15:29:56 +0200

    *: load CMFActivity and DA according to selected catalog storage

--- product/Database/db/__init__.py
+++ product/Database/db/__init__.py
@@ -3,11 +3,11 @@ _backend = None
 def configure(erp5_catalog_storage):
     global _backend
     if erp5_catalog_storage == 'erp5_mysql_innodb_catalog':
-        from Products.CMFActivity.Activity import MySQLJoblib as _backend
+        from Products.ZMySQLDA import db as _backend
     elif erp5_catalog_storage == 'erp5_sqlite_catalog':
-        from Products.CMFActivity.Activity import SQLiteJoblib as _backend
+        from Products.ZSQLiteDA import db as _backend
     else:
-        raise ImportError("Unsupported storage %s" % erp5_catalog_storage)
+        raise ImportError("Unsupported DB type %s" % erp5_catalog_storage)
 
 def __getattr__(name):
     return getattr(_backend, name)
