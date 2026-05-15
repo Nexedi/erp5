9da099b1539177fca375ebd5af9ed2681e18b31f
Roque Porchetto <roque.porchetto@nexedi.com>
2026-05-15 13:33:30 -0300

    Revert "erp5_officejs: exp"
    This reverts commit 6eab6ee1f3d753fcbaa11865e38c29ea5576e258.

--- bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_bootloader_serviceworker_js.js
+++ bt5/erp5_officejs/PathTemplateItem/web_page_module/gadget_officejs_bootloader_serviceworker_js.js
@@ -1,8 +1,5 @@
 /*jslint indent: 2*/
 /*global self, fetch, Request, Response, URL, Blob */
-// Version stamp injected by server-side text substitution.
-// Changes when the BT is updated, triggering browser SW update check.
-var APP_VERSION = '${modification_date}';
 var global = self, window = self;
 
 (function (self, fetch, Response, Blob) {
@@ -24,21 +21,6 @@ var global = self, window = self;
 
   self.cache_list = [];
 
-  function getDatabasePrefix() {
-    // Derive per-app IndexedDB prefix from the SW scope URL path.
-    // On officejs.com subdomains, scope path is "/" -> empty prefix
-    // (backward compatible). On ERP5 embedded, scope path is e.g.
-    // "/erp5/web_site_module/officejs_text_editor/" -> prefix is
-    // "officejs_text_editor_".
-    var scope_path = new URL(self.registration.scope).pathname;
-    var parts = scope_path.replace(/\/+$/, '').split('/');
-    var site_id = parts[parts.length - 1];
-    if (!site_id) {
-      return "";
-    }
-    return site_id + "_";
-  }
-
   function createStorage(database) {
     return self.jIO.createJIO({
       type: "indexeddb",
@@ -48,7 +30,7 @@ var global = self, window = self;
 
   function getFromLocal(relative_url) {
     if (self.storage.get === undefined) {
-      self.storage = createStorage(getDatabasePrefix() + "ojs_source_code");
+      self.storage = createStorage("ojs_source_code");
     }
     return self.storage.getAttachment(self.registration.scope, relative_url)
       .push(function (blob) {
@@ -64,18 +46,7 @@ var global = self, window = self;
     event.waitUntil(self.skipWaiting());
   });
   self.addEventListener('activate', function (event) {
-    event.waitUntil(
-      self.clients.claim()
-        .then(function () {
-          return self.clients.matchAll();
-        })
-        .then(function (client_list) {
-          var i;
-          for (i = 0; i < client_list.length; i += 1) {
-            client_list[i].postMessage('version_changed');
-          }
-        })
-    );
+    event.waitUntil(self.clients.claim());
   });
 
   self.addEventListener("fetch", function (event) {
