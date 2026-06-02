e5bc0383be1ff81f3970658fca07238e12b8f4ca
Roque Porchetto <roque.porchetto@nexedi.com>
2026-06-02 11:37:12 -0300

    erp5_web: redirct assist websection_getdocument update

--- bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/WebSection_getDocumentValue.py
+++ bt5/erp5_web/SkinTemplateItem/portal_skins/erp5_web_redirect_assist/WebSection_getDocumentValue.py
@@ -1,39 +1,16 @@
-"""
-Override of WebSection_getDocumentValue for the RedirectAssist skin selection.
-
-When the sub-path matches a Discussion Thread reference, issue a 301 redirect to the project app thread URL
-"""
+"""Forward unresolved sub-paths to the project app, which resolves the thread."""
 if not name:
   return None
 
+redirect_domain = context.getLayoutProperty('redirect_domain', '')
+if redirect_domain:
+  redirect_url = '%s?old_thread=%s' % (redirect_domain, name)
+  context.REQUEST.RESPONSE.redirect(redirect_url, status=301, lock=1)
+  return None
+
+# Not migrated: fall back to the default document lookup.
 if portal is None:
   portal = context.getPortalObject()
-
-thread_brain_list = portal.portal_catalog(
-  portal_type='Discussion Thread',
-  reference=name,
-  limit=1,
-)
-if thread_brain_list:
-  thread = thread_brain_list[0].getObject()
-  forum_list = list(portal.portal_domains.searchPredicateList(
-    thread,
-    portal_type='Discussion Forum',
-    validation_state=('published', 'published_alive', 'released',
-                      'released_alive', 'shared', 'shared_alive'),
-  ))
-  if forum_list:
-    forum = forum_list[0]
-    redirect_domain = context.getLayoutProperty('redirect_domain', '')
-    base_url = redirect_domain.split('/#/')[0]
-    redirect_url = (
-      '%s/#!push_history_stored_state'
-      '?p.jio_key=%s&p.page=form&p.view=view'
-      '&n.jio_key=%s&n.page=form&n.view=view'
-    ) % (base_url, forum.getRelativeUrl(), thread.getRelativeUrl())
-    context.REQUEST.RESPONSE.redirect(redirect_url, status=301, lock=1)
-    return None
-
 kw['limit'] = 1
 document_list = portal.portal_catalog.getDocumentValueList(
   reference={'query': name, 'key': 'ExactMatch'},
