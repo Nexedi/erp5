Nexedi/erp5

commit 673e7dd385f58465bf6041f8bd5f7645857cb292

py3.12: imp module has been removed.
* imp.new_module() -> types.ModuleType()
* Implement imp.load_source() for py3.
* Global import lock imp module {acquire,release}_lock() and lock_held() functions are now in _imp module.

cpython.git:326997829d02458246dfd5b6d03297e2418bde52

product/ERP5Type/dynamic/component_package.py
@@ -34,7 +34,6 @@ import errno
 import os
 import six
 import sys
-import imp
 import collections
 from six import reraise
 import traceback
@@ -43,7 +42,7 @@ import coverage
 from Products.ERP5Type.Utils import ensure_list
 from Products.ERP5.ERP5Site import getSite
 from Products.ERP5Type import product_path as ERP5Type_product_path
-from . import aq_method_lock
+from . import aq_method_lock, global_import_lock
 from types import ModuleType
 from zLOG import LOG, BLATHER, WARNING
 from Acquisition import aq_base
@@ -155,11 +154,9 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
         if not fullname.startswith(self._namespace_prefix):
           return None
 
-    import_lock_held = True
-    try:
-      imp.release_lock()
-    except RuntimeError:
-      import_lock_held = False
+    import_lock_held = global_import_lock.held()
+    if import_lock_held:
+      global_import_lock.release()
 
     try:
       site = getSite()
@@ -214,7 +211,7 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
       # Internal release of import lock at the end of import machinery will
       # fail if the hook is not acquired
       if import_lock_held:
-        imp.acquire_lock()
+        global_import_lock.acquire()
 
   def find_spec(self, name, path=None, target=None):
     """PEP-0451
@@ -349,8 +346,7 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
 
     # All the required objects have been loaded, acquire import lock to modify
     # sys.modules and execute PEP302 requisites
-    imp.acquire_lock()
-    try:
+    with global_import_lock:
       # The module *must* be in sys.modules before executing the code in case
       # the module code imports (directly or indirectly) itself (see PEP 302)
       sys.modules[module_fullname] = module
@@ -407,9 +403,6 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
       import erp5.component
       erp5.component.ref_manager.add_module(module)
 
-    finally:
-      imp.release_lock()
-
     component._hookAfterLoad(module)
     return module
 
@@ -430,11 +423,9 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
     #
     # Also, handle the case where find_module() may be called without import
     # statement as it does not change anything in sys.modules
-    import_lock_held = True
-    try:
-      imp.release_lock()
-    except RuntimeError:
-      import_lock_held = False
+    import_lock_held = global_import_lock.held()
+    if import_lock_held:
+      global_import_lock.release()
 
     aq_method_lock.acquire()
     try:
@@ -445,7 +436,7 @@ class ComponentDynamicPackage(ModuleType, MetaPathFinder):
       # Internal release of import lock at the end of import machinery will
       # fail if the hook is not acquired
       if import_lock_held:
-        imp.acquire_lock()
+        global_import_lock.acquire()
 
   def find_load_module(self, name):
     """

bt5/erp5_core_test/TestTemplateItem/portal_components/test.erp5.testBusinessTemplate.py
@@ -8832,7 +8832,7 @@ class TestConstraintTemplateItem(_LocalTemplateItemMixin,
   document_title = 'UnitTest'
   document_data = ' \nclass UnitTest: \n  """ \n  Fake constraint for unit test \n \
     """ \n  _properties = ( \n  ) \n  _categories = ( \n  ) \n\n'
-  document_data_updated = ' \nclass UnitTest2: \n  """ \n  Second Fake constraint for unit test \n \
+  document_data_updated = ' \nclass UnitTest: \n  """ \n  Second Fake constraint for unit test \n \
     """ \n  _properties = ( \n  ) \n  _categories = ( \n  ) \n\n'
   document_base_path = os.path.join(getConfiguration().instancehome, 'Constraint')
   template_property = 'template_constraint_id_list'

product/ERP5/Document/BusinessTemplate.py
@@ -30,7 +30,7 @@
 import six
 from six import string_types as basestring
 from Products.ERP5Type.Utils import ensure_list, bytes2str, str2bytes
-import fnmatch, gc, glob, imp, os, re, shutil, sys, time, tarfile
+import fnmatch, gc, glob, types, os, re, shutil, sys, time, tarfile
 from collections import defaultdict
 from Shared.DC.ZRDB import Aqueduct
 from Shared.DC.ZRDB.Connection import Connection as RDBConnection
@@ -67,7 +67,7 @@ from Products.ERP5Type.Utils import readLocalExtension, \
 from Products.ERP5Type.Utils import readLocalTest, \
                                     writeLocalTest, \
                                     removeLocalTest
-from Products.ERP5Type.Utils import convertToUpperCase
+from Products.ERP5Type.Utils import convertToUpperCase, loadModuleFromPathname
 from Products.ERP5Type import Permissions, PropertySheet, interfaces
 from Products.ERP5Type.XMLObject import XMLObject
 from Products.ERP5Type.dynamic.lazy_class import ERP5BaseBroken
@@ -4122,7 +4122,7 @@ class FilesystemToZodbTemplateItem(FilesystemDocumentTemplateItem,
     raise NotImplementedError
 
   @staticmethod
-  def _migrateFromFilesystem(tool, filesystem_path, filesystem_file, class_id):
+  def _migrateFromFilesystem(tool, filesystem_path, class_id):
     raise NotImplementedError
 
   def _migrateAllFromFilesystem(self,
@@ -4163,20 +4163,13 @@ class FilesystemToZodbTemplateItem(FilesystemDocumentTemplateItem,
       # A filesystem Property Sheet may already exist in the instance
       # home if the Business Template has been previously installed,
       # otherwise it is created
-      if os.path.exists(filesystem_path):
-        filesystem_file = open(filesystem_path)
-      else:
-        filesystem_file = open(filesystem_path, 'w+')
-        filesystem_file.write(migrate_object_dict[class_id])
-        filesystem_file.seek(0)
+      if not os.path.exists(filesystem_path):
+        with open(filesystem_path, 'w+') as f:
+          f.write(migrate_object_dict[class_id])
 
-      try:
-        migrated_object = self._migrateFromFilesystem(tool,
-                                                      filesystem_path,
-                                                      filesystem_file,
-                                                      class_id).aq_base
-      finally:
-        filesystem_file.close()
+      migrated_object = self._migrateFromFilesystem(tool,
+                                                    filesystem_path,
+                                                    class_id).aq_base
 
       # Delete the file only if there was no error encountered during
       # migration
@@ -4256,7 +4249,6 @@ class PropertySheetTemplateItem(FilesystemToZodbTemplateItem):
   @staticmethod
   def _migrateFromFilesystem(tool,
                              filesystem_path,
-                             filesystem_file,
                              class_id):
     """
     Migration of a filesystem Property Sheet involves loading the
@@ -4268,10 +4260,8 @@ class PropertySheetTemplateItem(FilesystemToZodbTemplateItem):
     # the class will be stored, thus don't only use the class name as
     # it may clash with already loaded module, such as
     # BusinessTemplate.
-    module = imp.load_source('Migrate%sFilesystemPropertySheet' % class_id,
-                             filesystem_path,
-                             filesystem_file)
-
+    module = loadModuleFromPathname('Migrate%sFilesystemPropertySheet' % class_id,
+                                    filesystem_path)
     try:
       klass = getattr(module, class_id)
     except AttributeError:
@@ -5919,7 +5909,7 @@ Business Template is a set of definitions, such as skins, portal types and categ
         module_id = 'Products.ERP5Type.Document.' + template_id
         if module_id not in sys.modules:
           module_id_list.append(module_id)
-          sys.modules[module_id] = module = imp.new_module(module_id)
+          sys.modules[module_id] = module = types.ModuleType(module_id)
           setattr(module, template_id, type(template_id,
             (SimpleItem.SimpleItem,), {'__module__': module_id}))
 

product/ERP5/Tool/TrashTool.py
@@ -26,7 +26,7 @@
 #
 ##############################################################################
 
-import imp, re, sys
+import re, sys
 from AccessControl import ClassSecurityInfo
 from ZODB.broken import Broken
 from Products.ERP5Type.Accessor.Constant import PropertyGetter as ConstantGetter

product/ERP5Type/Tool/TypesTool.py
@@ -16,7 +16,7 @@
 ##############################################################################
 
 from six import string_types as basestring
-import imp, sys, warnings
+import types, sys, warnings
 import inspect
 from itertools import chain
 import zope.interface
@@ -354,7 +354,7 @@ class TypesTool(TypeProvider):
       trashbin._setOb(old_types_tool.id, old_types_tool)
 
 # Compatibility code to access old "ERP5 Role Information" objects.
-OldRoleInformation = imp.new_module('Products.ERP5Type.RoleInformation')
+OldRoleInformation = types.ModuleType('Products.ERP5Type.RoleInformation')
 sys.modules[OldRoleInformation.__name__] = OldRoleInformation
 from OFS.SimpleItem import SimpleItem
 OldRoleInformation.RoleInformation = SimpleItem

product/ERP5Type/Utils.py
@@ -789,7 +789,23 @@ def updateGlobals(this_module, global_hook,
 # Modules Import
 #####################################################
 
-import imp
+if six.PY3:
+  import importlib.util
+  import importlib.machinery
+  def loadModuleFromPathname(name, pathname):
+    """
+    Python2 imp.load_source() removed in Python 3.12 implemented with ModuleSpec
+    PEP-0451 (this is a copy/paste from cpython/Doc/whatsnew/3.12.rst)
+    """
+    loader = importlib.machinery.SourceFileLoader(name, pathname)
+    spec = importlib.util.spec_from_file_location(name, pathname, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module.__name__] = module
+    loader.exec_module(module)
+    return module
+else:
+  import imp
+  loadModuleFromPathname = imp.load_source
 
 from App.config import getConfiguration
 
@@ -842,7 +858,7 @@ def writeLocalPropertySheet(class_id, text, create=1, instance_home=None):
   with open(path, 'w') as f:
     f.write(text)
   # load the file, so that an error is raised if file is invalid
-  module = imp.load_source(class_id, path)
+  module = loadModuleFromPathname(class_id, path)
   getattr(module, class_id)
 
 def importLocalPropertySheet(class_id, path = None):
@@ -851,18 +867,16 @@ def importLocalPropertySheet(class_id, path = None):
     # We should save a copy in ZODB here XXX
     instance_home = getConfiguration().instancehome
     path = os.path.join(instance_home, "PropertySheet")
-  path = os.path.join(path, "%s.py" % class_id)
-  with open(path) as f:
-    module = imp.load_source(class_id, path, f)
-    klass = None
-    try:
-      klass = getattr(module, class_id)
-    except AttributeError:
-      raise AttributeError("Property Sheet '%s' should contain a class " \
-          "with the same name" % class_id)
-    setattr(PropertySheet, class_id, klass)
-    # Register base categories
-    registerBaseCategories(klass)
+  module = loadModuleFromPathname(class_id, os.path.join(path, "%s.py" % class_id))
+  klass = None
+  try:
+    klass = getattr(module, class_id)
+  except AttributeError:
+    raise AttributeError("Property Sheet '%s' should contain a class " \
+        "with the same name" % class_id)
+  setattr(PropertySheet, class_id, klass)
+  # Register base categories
+  registerBaseCategories(klass)
 
 base_category_dict = {}
 def registerBaseCategories(property_sheet):
@@ -886,9 +900,7 @@ def importLocalInterface(module_id, path = None, is_erp5_type=False):
     if path is None:
       instance_home = getConfiguration().instancehome
       path = os.path.join(instance_home, "interfaces")
-    path = os.path.join(path, "%s.py" % module_id)
-    with open(path) as f:
-      module = imp.load_source(class_id, path, f)
+    module = loadModuleFromPathname(class_id, os.path.join(path, "%s.py" % module_id))
     from zope.interface import Interface
     from Products.ERP5Type import interfaces
     InterfaceClass = type(Interface)
@@ -901,21 +913,17 @@ def importLocalConstraint(class_id, path = None):
   if path is None:
     instance_home = getConfiguration().instancehome
     path = os.path.join(instance_home, "Constraint")
-  path = os.path.join(path, "%s.py" % class_id)
-  with open(path) as f:
-    module = imp.load_source(class_id, path, f)
-    setattr(Products.ERP5Type.Constraint, class_id, getattr(module, class_id))
+  module = loadModuleFromPathname(class_id, os.path.join(path, "%s.py" % class_id))
+  setattr(Products.ERP5Type.Constraint, class_id, getattr(module, class_id))
 
 def importLocalInteractor(class_id, path=None):
   import Products.ERP5Type.Interactor
   if path is None:
     instance_home = getConfiguration().instancehome
     path = os.path.join(instance_home, "Interactor")
-  path = os.path.join(path, "%s.py" % class_id)
-  with open(path) as f:
-    module = imp.load_source(class_id, path, f)
-    setattr(Products.ERP5Type.Interactor, class_id, getattr(module, class_id))
-    registerInteractorClass(class_id, getattr(Products.ERP5Type.Interactor, class_id))
+  module = loadModuleFromPathname(class_id, os.path.join(path, "%s.py" % class_id))
+  setattr(Products.ERP5Type.Interactor, class_id, getattr(module, class_id))
+  registerInteractorClass(class_id, getattr(Products.ERP5Type.Interactor, class_id))
 
 def getLocalExtensionList():
   if not getConfiguration:
@@ -1035,7 +1043,7 @@ def writeLocalConstraint(class_id, text, create=1, instance_home=None):
   with open(path, 'w') as f:
     f.write(text)
   # load the file, so that an error is raised if file is invalid
-  module = imp.load_source(class_id, path)
+  module = loadModuleFromPathname(class_id, path)
   getattr(module, class_id)
 
 def removeLocalConstraint(class_id):
@@ -1160,7 +1168,7 @@ def importLocalDocument(class_id, path=None, class_path=None):
     path = os.path.join(path, "%s.py" % class_id)
     module_path = "erp5.document"
     class_path = "%s.%s" % (module_path, class_id)
-    module = imp.load_source(class_path, path)
+    module = loadModuleFromPathname(class_path, path)
     klass = getattr(module, class_id, None)
     # Tolerate that Document doesn't define any class, which can be useful
     # if we only want to monkey patch.

product/ERP5Type/__init__.py
@@ -214,11 +214,11 @@ ModuleSecurityInfo('zExceptions').declarePublic(*[
 try:
   from Products.CMFDefault.MembershipTool import MembershipTool
 except ImportError:
-  import sys, imp
+  import sys, types
   m = 'Products.CMFDefault'
-  sys.modules[m] = imp.new_module(m)
+  sys.modules[m] = types.ModuleType(m)
   m += ".MembershipTool"
-  sys.modules[m] = m = imp.new_module(m)
+  sys.modules[m] = m = types.ModuleType(m)
   from Products.CMFCore.MembershipTool import MembershipTool
   m.MembershipTool = MembershipTool
   del m

product/ERP5Type/dynamic/__init__.py
@@ -1,2 +1,24 @@
 import threading
 aq_method_lock = threading.RLock()
+
+import six
+if six.PY3:
+  import _imp
+else:
+  import imp as _imp
+class global_import_lock:
+  @staticmethod
+  def acquire():
+    return _imp.acquire_lock()
+  @staticmethod
+  def release():
+    return _imp.release_lock()
+  @staticmethod
+  def held():
+    return _imp.lock_held()
+  def __enter__(self):
+    self.acquire()
+  def __exit__(self, exc_type, exc, traceback):
+    self.release()
+    return False
+global_import_lock = global_import_lock()

commit 0da94b8919d4c74c3fb00e0b9a71362cf881ba2f

component_package: release all import lock levels to avoid ABBA deadlock
On CPython 2, the import lock is re-entrant: each nested __import__
increments import_lock_level, and imp.release_lock() only decrements
it by one (see unlock_import in
https://github.com/python/cpython/blob/v2.7.18/Python/import.c#L321):

    static int
    unlock_import(void)
    {
        import_lock_level--;
        if (import_lock_level == 0) {
            import_lock_thread = -1;
            PyThread_release_lock(import_lock);
        }
        return 1;
    }

When find_module or load_module is called during a nested import
(lock level >= 2), a single release() decrements the level (e.g.
2 -> 1) but does NOT actually release the underlying pthread lock.
Any other thread that needs the import lock remains blocked.

This causes an ABBA deadlock in the following real-world scenario:

  Thread A  holds aq_method_lock (dynamic_module.__getattr__ ->
            generateLazyPortalTypeClass -> InitializeClass), then
            does 'from AccessControl.Permission import ...' which
            needs the import lock.

  Thread B  holds import lock at level >= 2 (guarded_import ->
            secureModule -> __import__('Products.X.Y') [level=1] ->
            module body does another import [level=2] -> find_module
            called with Products path), releases once (level 2->1,
            pthread lock NOT freed), then blocks on aq_method_lock.

Fix: loop global_import_lock.release() until held() returns False
(draining all levels), then re-acquire the same count in the finally
block.  This ensures the actual lock is freed regardless of nesting
depth, allowing the other thread to proceed.

Regression introduced in 673e7dd385 ("py3.12: imp module has been
removed", 2025-10-21) which changed the old try/except RuntimeError
pattern to a lock_held() check + single release.

product/ERP5Type/dynamic/component_package.py
@@ -349,9 +349,10 @@ except ImportError: # < v3.4
         # the only way to let importer continues searching for filesystem
         # module...
         import erp5.component
-        import_lock_held = global_import_lock.held()
-        if import_lock_held:
+        import_lock_level = 0
+        while global_import_lock.held():
           global_import_lock.release()
+          import_lock_level += 1
         aq_method_lock.acquire()
         try:
           if erp5.component.filesystem_import_dict is None:
@@ -361,9 +362,7 @@ except ImportError: # < v3.4
             return None
         finally:
           aq_method_lock.release()
-          # Internal release of import lock at the end of import machinery will
-          # fail if the hook is not acquired
-          if import_lock_held:
+          for _ in range(import_lock_level):
             global_import_lock.acquire()
 
       return spec.loader
@@ -755,9 +754,10 @@ else: # not USE_COMPONENT_PEP_451_LOADER
       Make sure that loading module is thread-safe using aq_method_lock to make
       sure that modules do not disappear because of an ongoing reset
       """
-      import_lock_held = global_import_lock.held()
-      if import_lock_held:
+      import_lock_level = 0
+      while global_import_lock.held():
         global_import_lock.release()
+        import_lock_level += 1
 
       aq_method_lock.acquire()
       try:
@@ -765,9 +765,7 @@ else: # not USE_COMPONENT_PEP_451_LOADER
       finally:
         aq_method_lock.release()
 
-        # Internal release of import lock at the end of import machinery will
-        # fail if the hook is not acquired
-        if import_lock_held:
+        for _ in range(import_lock_level):
           global_import_lock.acquire()
 
     # Hack for SourceFileLoader()

product/ERP5Type/tests/testDynamicClassGeneration.py
@@ -416,6 +416,112 @@ class TestPortalTypeClass(ERP5TypeTestCase):
     finally:
       PortalTypeMetaClass.generatePortalTypeAccessors = PortalTypeMetaClass_generatePortalTypeAccessors
 
+  def testConcurrentImportAndClassGeneration(self):
+    """Reproduce ABBA deadlock between aq_method_lock and import lock.
+
+    Real-world scenario:
+      Thread A: dynamic_module.__getattr__ holds aq_method_lock, calls
+        InitializeClass which does 'from AccessControl.Permission import ...'
+        needing the import lock.
+      Thread B: guarded_import triggers secureModule -> __import__ (level=1),
+        module executes and does another import (level=2), during which
+        find_module's if-path branch releases import lock only once (level
+        drops to 1, actual lock NOT freed), then blocks on aq_method_lock.
+
+    The fix must release ALL import lock levels before acquiring
+    aq_method_lock, then restore the same number of levels afterward.
+    """
+    import threading
+    import time
+    from Products.ERP5Type.dynamic import aq_method_lock, global_import_lock
+    from Products.ERP5Type.dynamic.component_package import (
+        COMPONENT_META_PATH_FINDER)
+
+    if six.PY3:
+      raise unittest.SkipTest(
+        "Python 3 import lock is per-module and reentrant")
+
+    import Products.ERP5Type
+    products_path = Products.ERP5Type.__path__
+
+    t1_ready = threading.Event()
+    t2_ready = threading.Event()
+    import_lock_held_after_release = [None]
+    errors = []
+
+    def thread_a():
+      """Hold aq_method_lock, then check if import lock is fully released.
+
+      After find_module calls release(), the import lock must be fully freed
+      (level=0) regardless of how many nested levels were held. If it's still
+      held, another thread needing the import lock while aq_method_lock is
+      held would deadlock (the real-world InitializeClass scenario).
+      """
+      try:
+        with aq_method_lock:
+          t1_ready.set()
+          t2_ready.wait(timeout=5)
+          # Wait for thread_b to enter find_module and call release()
+          time.sleep(0.5)
+          # With the bug: thread_b released once (level 2->1), lock still held
+          # With the fix: thread_b released all levels (2->0), lock freed
+          import_lock_held_after_release[0] = global_import_lock.held()
+        # aq_method_lock released here, thread_b unblocks in find_module
+      except Exception as e:
+        errors.append(e)
+
+    def thread_b():
+      """Call find_module with import lock at nested level (>= 2).
+
+      Reproduces the real call chain where secureModule's __import__ holds
+      the lock at level 1, module body does another import (level 2), and
+      during that nested import find_module is called with a Products path.
+      """
+      try:
+        t1_ready.wait(timeout=5)
+        # Simulate nested import context: lock at level 2
+        global_import_lock.acquire()  # level 1 (outer __import__)
+        global_import_lock.acquire()  # level 2 (nested import)
+        t2_ready.set()
+        # find_module enters if-path branch: releases lock, tries
+        # aq_method_lock (blocks until thread_a releases it), checks
+        # filesystem_import_dict, then re-acquires lock levels.
+        COMPONENT_META_PATH_FINDER.find_module(
+            'Products.ERP5Type._test_nonexistent_module',
+            products_path)
+        # Clean up: release the levels thread_b acquired
+        global_import_lock.release()
+        global_import_lock.release()
+      except Exception as e:
+        errors.append(e)
+        # Best-effort cleanup to avoid poisoning other tests
+        for _ in range(4):
+          try:
+            global_import_lock.release()
+          except RuntimeError:
+            break
+
+    t1 = threading.Thread(target=thread_a)
+    t2 = threading.Thread(target=thread_b)
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+    t1.join(timeout=10)
+    t2.join(timeout=10)
+
+    self.assertFalse(t1.is_alive(),
+        "Thread A still blocked (should not happen in this test design)")
+    self.assertFalse(t2.is_alive(),
+        "Thread B deadlocked on aq_method_lock inside find_module "
+        "(thread A could not release aq_method_lock because it needed "
+        "the import lock which thread B still held)")
+    self.assertFalse(errors, errors)
+    self.assertFalse(import_lock_held_after_release[0],
+        "Import lock not fully released by find_module when held at nested "
+        "level. Single release() only decrements level (2->1) without "
+        "freeing the actual lock, causing ABBA deadlock with aq_method_lock.")
+
 class TestZodbPropertySheet(ERP5TypeTestCase):
   """
   XXX: WORK IN PROGRESS