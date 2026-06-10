cdf29c5feffc72d7e55d3eea267aea339076a823
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-06-10 09:26:08 +0200

    product&bt5: support sqlite as catalog

---
+++ product/ERP5/bootstrap/erp5_sqlite_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/z_create_delivery.sql
@@ -0,0 +1,29 @@
+-- Table: delivery
+
+CREATE TABLE delivery (
+  uid INTEGER NOT NULL,
+  source_uid INTEGER DEFAULT 0,
+  destination_uid INTEGER DEFAULT 0,
+  source_section_uid INTEGER DEFAULT 0,
+  destination_section_uid INTEGER DEFAULT 0,
+  resource_uid INTEGER DEFAULT 0,
+  start_date TEXT DEFAULT NULL,
+  start_date_range_min TEXT DEFAULT NULL,
+  start_date_range_max TEXT DEFAULT NULL,
+  stop_date TEXT DEFAULT NULL,
+  stop_date_range_min TEXT DEFAULT NULL,
+  stop_date_range_max TEXT DEFAULT NULL,
+  PRIMARY KEY (uid)
+);
+<dtml-var sql_delimiter>
+CREATE INDEX source_uid ON delivery (source_uid);
+<dtml-var sql_delimiter>
+CREATE INDEX destination_uid ON delivery (destination_uid);
+<dtml-var sql_delimiter>
+CREATE INDEX source_section_uid ON delivery (source_section_uid);
+<dtml-var sql_delimiter>
+CREATE INDEX destination_section_uid ON delivery (destination_section_uid);
+<dtml-var sql_delimiter>
+CREATE INDEX resource_uid ON delivery (resource_uid);
+<dtml-var sql_delimiter>
+CREATE INDEX start_date ON delivery (start_date);
