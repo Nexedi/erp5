f9a0e47b9995d25a5b774142a882741b957efba7
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-06-10 09:26:08 +0200

    product&bt5: support sqlite as catalog

---
+++ product/ERP5/bootstrap/erp5_sqlite_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/SQLCatalog_zGetDocumentValueList.sql
@@ -0,0 +1,82 @@
+<dtml-let
+  asQuery="getattr(search_context, 'asQuery', None)"
+  dummy="None if asQuery is None else kw.update({'websection_query': asQuery()})"
+  security_query="getSecurityQuery(sql_catalog_id=getId(), local_roles=kw.pop('local_roles', None))"
+  dummy2="None if security_query is None else kw.update({'security_query': security_query})"
+  query="buildSQLQuery(**kw)"
+>
+
+SELECT
+  *
+FROM (
+  SELECT
+    catalog.uid,
+    catalog.path,
+    catalog.int_index,
+    catalog.modification_date,
+    catalog.reference,
+    catalog.creation_date,
+    catalog.title,
+
+    (
+      CASE my_versioning.language
+           WHEN <dtml-sqlvar language type="string"> THEN '4'
+           WHEN '' THEN '3'
+           WHEN <dtml-sqlvar expr="Localizer.get_default_language() or 'en'" type="string"> THEN '2'
+           ELSE '1'
+      END
+      || my_versioning.version
+    ) AS priority
+
+    <dtml-if "query['select_expression']">
+      , <dtml-var "query['select_expression']">
+    </dtml-if>,
+
+    ROW_NUMBER() OVER (
+      <dtml-if "query['group_by_expression']">
+        PARTITION BY <dtml-var "query['group_by_expression']">
+      </dtml-if>
+      ORDER BY
+        (
+          CASE my_versioning.language
+               WHEN <dtml-sqlvar language type="string"> THEN '4'
+               WHEN '' THEN '3'
+               WHEN <dtml-sqlvar expr="Localizer.get_default_language() or 'en'" type="string"> THEN '2'
+               ELSE '1'
+          END
+          || my_versioning.version
+        ) DESC
+    ) AS rn
+
+  FROM
+    <dtml-in prefix="table" expr="query['from_table_list']">
+      <dtml-var table_item> AS <dtml-var table_key>,
+    </dtml-in>
+    versioning AS my_versioning
+
+  WHERE
+    my_versioning.uid = catalog.uid
+
+    <dtml-if "query['where_expression']">
+      AND <dtml-var "query['where_expression']">
+    </dtml-if>
+
+    <dtml-if strict_language>
+      AND my_versioning.language IN (
+        <dtml-sqlvar language type="string">,
+        ''
+      )
+    </dtml-if>
+
+) subquery
+
+WHERE rn = 1
+
+ORDER BY
+  <dtml-var "query['order_by_expression'] or 'priority DESC'">
+
+<dtml-if "query['limit_expression']">
+  LIMIT <dtml-var "query['limit_expression']">
+</dtml-if>
+
+</dtml-let>
