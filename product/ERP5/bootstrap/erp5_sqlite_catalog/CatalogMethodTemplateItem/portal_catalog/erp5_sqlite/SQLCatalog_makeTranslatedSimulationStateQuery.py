6fe88e49b8f0e84ff04965ca54efdacb6bcb81ea
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-06-10 15:47:56 +0200

    bt/catalog: factorize

--- product/ERP5/bootstrap/erp5_sqlite_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_sqlite/SQLCatalog_makeTranslatedSimulationStateQuery.py
+++
@@ -1,9 +0,0 @@
-return context.SQLCatalog_makeTranslationQuery(
-  search_value=search_value,
-  column_id='simulation_state',
-  message_context='simulation_state',
-  search_key=search_key,
-  group=group,
-  logical_operator=logical_operator,
-  comparison_operator=comparison_operator,
-)
--- product/ERP5/bootstrap/erp5_sqlite_catalog/SkinTemplateItem/portal_skins/erp5_sqlite/z_get_deleted_path_list.sql
+++
@@ -1,1 +0,0 @@
-SELECT uid, path FROM deleted_catalog WHERE <dtml-sqltest column="deletion_timestamp" expr="timestamp" op="ge" type="datetime">
