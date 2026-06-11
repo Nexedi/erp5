78cd7b3fa492821fedbe5ca497c41bfbb69eead8
Rafael Monnerat <rafael@nexedi.com>
2026-06-11 18:29:22 +0000

    erp5_workgroup: Implement Default User Security script
       Include an updated version of ERP5User_getUserSecurityCategoryValueList to
       extend erp5_core behaviour, to enable workgroup usage by default.

--- bt5/erp5_workgroup/TestTemplateItem/portal_components/test.erp5.testWorkgroup.py
+++ bt5/erp5_workgroup/TestTemplateItem/portal_components/test.erp5.testWorkgroup.py
@@ -29,33 +29,6 @@
 
 from Products.ERP5Type.tests.ERP5TypeTestCase import ERP5TypeTestCase
 from AccessControl import getSecurityManager
-from Products.ERP5Type.tests.utils import TemporaryPythonScript
-
-
-WORKGROUP_SCRIPT = """
-rule_dict = {
-  tuple(context.getPortalObject().getPortalAssignmentBaseCategoryList()): ((), )
-}
-category_list = context.ERP5User_getSecurityCategoryValueFromAssignment(
-                                                           rule_dict=rule_dict)
-
-# Extend default behaviour with workgroup support, this is not performance
-# optimal.
-from DateTime import DateTime
-now = DateTime()
-for assignment in context.objectValues(portal_type='Assignment'):
-  workgroup = assignment.getDestinationValue(portal_type="Workgroup")
-  if assignment.getValidationState() == 'open' and workgroup is not None and \
-    (
-      not assignment.hasStartDate() or assignment.getStartDate() <= now
-    ) and (
-      not assignment.hasStopDate() or assignment.getStopDate() >= now
-    ):
-    category_list.extend(
-      workgroup.ERP5User_getSecurityCategoryValueFromAssignment(
-        rule_dict=rule_dict))
-return category_list
-"""
 
 class TestWorkgroup(ERP5TypeTestCase):
   """
@@ -88,74 +61,71 @@ class TestWorkgroup(ERP5TypeTestCase):
     self.tic()
 
   def test(self):
-    with TemporaryPythonScript(self.portal,
-        "ERP5User_getUserSecurityCategoryValueList", "", WORKGROUP_SCRIPT):
+    self.login(self.person.getUserId())
+    user = getSecurityManager().getUser()
+    self.assertIn('Authenticated', user.getRoles())
+    self.assertSameSet([], user.getGroups())
 
-      self.login(self.person.getUserId())
-      user = getSecurityManager().getUser()
-      self.assertIn('Authenticated', user.getRoles())
-      self.assertSameSet([], user.getGroups())
+    # add to group category
+    self.login()
+    # Ensure fake group exist
+    category_tool = self.portal.portal_categories
+    group_category = getattr(
+      category_tool.group, "test_workgroup_company", None)
+    if group_category is None:
+      group_category = category_tool.group.newContent(portal_type="Category",
+                                     id="test_workgroup_company")
+    group_category.edit(codification="G-TESTWGCOMPANY")
 
-      # add to group category
-      self.login()
-      # Ensure fake group exist
-      category_tool = self.portal.portal_categories
-      group_category = getattr(
-        category_tool.group, "test_workgroup_company", None)
-      if group_category is None:
-        group_category = category_tool.group.newContent(portal_type="Category",
-                                       id="test_workgroup_company")
-      group_category.edit(codification="G-TESTWGCOMPANY")
+    # Add assignment and check if the Codification is added as Roles
+    self.person.newContent(portal_type='Assignment',
+                      group='test_workgroup_company').open()
+    self.tic()
+    self.portal.portal_caches.clearAllCache()
+    self.login(self.person.getUserId())
+    user = getSecurityManager().getUser()
+    self.assertIn('Authenticated', user.getRoles())
+    self.assertSameSet([group_category.getCodification()], user.getGroups())
 
-      # Add assignment and check if the Codification is added as Roles
-      self.person.newContent(portal_type='Assignment',
-                        group='test_workgroup_company').open()
-      self.tic()
-      self.portal.portal_caches.clearAllCache()
-      self.login(self.person.getUserId())
-      user = getSecurityManager().getUser()
-      self.assertIn('Authenticated', user.getRoles())
-      self.assertSameSet([group_category.getCodification()], user.getGroups())
+    # add workgroup
+    self.login()
+    workgroup = self.portal.workgroup_module.newContent(
+      portal_type='Workgroup',
+      title='workgroup-%s' % self.person.getUid()
+    )
+    workgroup.validate()
 
-      # add workgroup
-      self.login()
-      workgroup = self.portal.workgroup_module.newContent(
-        portal_type='Workgroup',
-        title='workgroup-%s' % self.person.getUid()
-      )
-      workgroup.validate()
+    self.person.newContent(portal_type='Assignment',
+      destination_value=workgroup).open()
+    self.tic()
 
-      self.person.newContent(portal_type='Assignment',
-        destination_value=workgroup).open()
-      self.tic()
+    self.portal.portal_caches.clearAllCache()
+    self.login(self.person.getUserId())
+    user = getSecurityManager().getUser()
+    self.assertIn('Authenticated', user.getRoles())
+    self.assertSameSet([group_category.getCodification()], user.getGroups())
 
-      self.portal.portal_caches.clearAllCache()
-      self.login(self.person.getUserId())
-      user = getSecurityManager().getUser()
-      self.assertIn('Authenticated', user.getRoles())
-      self.assertSameSet([group_category.getCodification()], user.getGroups())
+    self.login()
 
-      self.login()
+    # Ensure fake function exist
+    category_tool = self.portal.portal_categories
+    function_category = getattr(
+      category_tool.function, "test_workgroup_function", None)
+    if function_category is None:
+      function_category = category_tool.function.newContent(
+          portal_type="Category", id="test_workgroup_function")
+    function_category.edit(codification="F-TESTWGFUNC")
 
-      # Ensure fake role exist
-      category_tool = self.portal.portal_categories
-      function_category = getattr(
-        category_tool.function, "test_workgroup_function", None)
-      if function_category is None:
-        function_category = category_tool.function.newContent(
-            portal_type="Category", id="test_workgroup_function")
-      function_category.edit(codification="F-TESTWGFUNC")
+    # Add assignment to the workgroup and check if the Codification is
+    # added as user Roles
+    workgroup.newContent(portal_type='Assignment',
+                      function='test_workgroup_function').open()
+    self.tic()
 
-      # Add assignment to the workgroup and check if the Codification is
-      # added as user Roles
-      workgroup.newContent(portal_type='Assignment',
-                        function='test_workgroup_function').open()
-      self.tic()
-
-      self.portal.portal_caches.clearAllCache()
-      self.login(self.person.getUserId())
-      user = getSecurityManager().getUser()
-      self.assertIn('Authenticated', user.getRoles())
-      self.assertSameSet([function_category.getCodification(),
-        group_category.getCodification()],
-        user.getGroups())
+    self.portal.portal_caches.clearAllCache()
+    self.login(self.person.getUserId())
+    user = getSecurityManager().getUser()
+    self.assertIn('Authenticated', user.getRoles())
+    self.assertSameSet([function_category.getCodification(),
+      group_category.getCodification()],
+      user.getGroups())
