eb26ecc4e229dd8d67ec6259da85488f65028344
Rafael Monnerat <rafael@nexedi.com>
2026-06-15 21:40:40 +0200

    ERP5Security: Allow import getValidAssignmentList directly on Restricted Python
      This makes easy to don't repeat this implementation everywhere.

--- bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Person_getAvailableAssignmentValueList.py
+++ bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Person_getAvailableAssignmentValueList.py
@@ -1,17 +1,2 @@
-current_date=DateTime()
-valid_assignment_list = []
-
-# Keep consistency with Product.ERP5Security.ERP5UserManager.getValidAssignmentList
-# except: checked_permission usage and no transaction
-for assignment in context.contentValues(
-    portal_type='Assignment',
-    checked_permission="Access contents information"):
-  if assignment.getValidationState() == 'open':
-    if assignment.getStartDate() is not None and \
-           assignment.getStartDate() >= current_date:
-      continue
-    if assignment.getStopDate() is not None and \
-           assignment.getStopDate() <= current_date:
-      continue
-    valid_assignment_list.append(assignment)
-return valid_assignment_list
+from Products.ERP5Security.Utils import getValidAssignmentList
+return getValidAssignmentList(context, checked_permission="Access contents information")
