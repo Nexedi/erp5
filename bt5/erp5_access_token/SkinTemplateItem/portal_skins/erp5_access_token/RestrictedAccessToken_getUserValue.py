Nexedi/erp5

commit 634e4bcb3a77d6b47bd5534e9009e327747c0521

ERP5Security: Unify getValidAssignmentList implementation
  Move all different implementation of 'getValidAssignmentList' into
  Utils.py

    - getValidAssignmentList: Used everywhere, no cache, accepts
    start_date only setting.

    - getCachedValidAssignmentList: Same as above, however cached over
      the transaction per user (parameter)

    - hasValidAssignment: Return True as soon it finds the first
      valid one, with no cache.

  Allow import *ValidAssignmentList directly on Restricted Python so we
  don't need to write External Methods to re-use the implementation.

  Related code was updated accross the business templates, the solo
  difference in implementation is that StandardSecurity use
  transactional cache version rather then no cache. This prevents code
  re-calculate the assignment list more then once accross the
  transaction. Due this cache was updated acordinally.

  Other locations were kept using uncached version for compatibility
  since it has low impact or it is not widely used code.

x

bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/RestrictedAccessToken_getUserValue.py
@@ -3,6 +3,8 @@ import hmac
 if REQUEST is not None:
   raise Unauthorized
 
+from Products.ERP5Security.Utils import hasValidAssignment
+
 result = None
 access_token_document = context
 request = context.REQUEST
@@ -29,14 +31,7 @@ if access_token_document.getValidationState() == 'validated':
         if agent_document.getValidationState() == 'deleted':
           return None
         now = DateTime()
-        for assignment in agent_document.contentValues(portal_type='Assignment'):
-          if assignment.getValidationState() == "open" and (
-              not assignment.hasStartDate() or assignment.getStartDate() <= now
-            ) and (
-              not assignment.hasStopDate() or assignment.getStopDate() >= now
-            ):
-            break
-        else:
+        if not hasValidAssignment(agent_document):
           return None
 
         user, = context.getPortalObject().acl_users.searchUsers(

bt5/erp5_base/SkinTemplateItem/portal_skins/erp5_base/Person_getAvailableAssignmentValueList.py
@@ -1,15 +1,2 @@
-current_date=DateTime()
-assignment_list = []
-for assignment in context.contentValues(portal_type='Assignment', checked_permission="Access contents information"):
-  if assignment.getValidationState() == 'open':
-    start_date = assignment.getStartDate()
-    stop_date = assignment.getStopDate() if assignment.hasStopDate() else None
-    if start_date is not None and stop_date is not None and start_date <= current_date < stop_date:
-      assignment_list.append(assignment)
-    elif start_date is not None and stop_date is None and start_date <= current_date:
-      assignment_list.append(assignment)
-    elif stop_date is not None and start_date is None and current_date < stop_date:
-      assignment_list.append(assignment)
-    elif start_date is None and stop_date is None:
-      assignment_list.append(assignment)
-return assignment_list
+from Products.ERP5Security.Utils import getValidAssignmentList
+return getValidAssignmentList(context, checked_permission="Access contents information")

product/ERP5/bootstrap/erp5_core/ExtensionTemplateItem/portal_components/extension.erp5.StandardSecurity.py
@@ -27,7 +27,7 @@
 from collections import defaultdict
 from itertools import product
 import six
-from DateTime import DateTime
+from Products.ERP5Security.Utils import getCachedValidAssignmentList
 
 _STOP_RECURSION_PORTAL_TYPE_SET = ('Base Category', 'ERP5 Site')
 
@@ -77,32 +77,26 @@ def getSecurityCategoryValueFromAssignment(self, rule_dict):
   category_value_set_dict = defaultdict(set)
   parent_category_value_dict = {}
   assignment_membership_dict_list = []
-  now = DateTime()
-  for assignment_value in self.objectValues(portal_type='Assignment'):
-    if assignment_value.getValidationState() == 'open' and (
-      not assignment_value.hasStartDate() or assignment_value.getStartDate() <= now
-    ) and (
-      not assignment_value.hasStopDate() or assignment_value.getStopDate() >= now
-    ):
-      assignment_membership_dict = {}
-      for base_category in base_category_set:
-        category_value_list = assignment_value.getAcquiredValueList(base_category)
-        if category_value_list:
-          assignment_membership_dict[base_category] = tuple(set(category_value_list))
-          category_value_set_dict[base_category].update(category_value_list)
-          if base_category in recursive_base_category_set:
-            for category_value in category_value_list:
-              while True:
-                parent_category_value = category_value.getParentValue()
-                if (
-                  category_value in parent_category_value_dict or
-                  parent_category_value.getPortalType() in _STOP_RECURSION_PORTAL_TYPE_SET
-                ):
-                  break
-                parent_category_value_dict[category_value] = parent_category_value
-                category_value = parent_category_value
-      if assignment_membership_dict:
-        assignment_membership_dict_list.append(assignment_membership_dict)
+  for assignment_value in getCachedValidAssignmentList(self):
+    assignment_membership_dict = {}
+    for base_category in base_category_set:
+      category_value_list = assignment_value.getAcquiredValueList(base_category)
+      if category_value_list:
+        assignment_membership_dict[base_category] = tuple(set(category_value_list))
+        category_value_set_dict[base_category].update(category_value_list)
+        if base_category in recursive_base_category_set:
+          for category_value in category_value_list:
+            while True:
+              parent_category_value = category_value.getParentValue()
+              if (
+                category_value in parent_category_value_dict or
+                parent_category_value.getPortalType() in _STOP_RECURSION_PORTAL_TYPE_SET
+              ):
+                break
+              parent_category_value_dict[category_value] = parent_category_value
+              category_value = parent_category_value
+    if assignment_membership_dict:
+      assignment_membership_dict_list.append(assignment_membership_dict)
   result = []
   for base_category_list, recursion_list in six.iteritems(rule_dict):
     result_entry = set()
@@ -245,30 +239,24 @@ def getSecurityCategoryFromAssignment(
     return []
   user_path, = user_path_set
   person_object = self.getPortalObject().unrestrictedTraverse(user_path)
-  now = DateTime()
 
   # We look for every valid assignments of this user
-  for assignment in person_object.contentValues(filter={'portal_type': 'Assignment'}):
-    if assignment.getValidationState() == "open" and (
-      not assignment.hasStartDate() or assignment.getStartDate() <= now
-    ) and (
-      not assignment.hasStopDate() or assignment.getStopDate() >= now
-    ):
-      category_dict = {}
-      for base_category in base_category_list:
-        category_value_list = assignment.getAcquiredValueList(base_category)
-        if category_value_list:
-          for category_value in category_value_list:
-            if base_category in child_category_list:
-              if category_value.getPortalType() not in ('Base Category', 'ERP5 Site'):
-                while category_value.getPortalType() not in ('Base Category', 'ERP5 Site'):
-                  category_dict.setdefault(base_category, []).append('%s*' % category_value.getRelativeUrl())
-                  category_value = category_value.getParentValue()
-              else:
-                category_dict.setdefault(base_category, []).append(category_value.getRelativeUrl())
+  for assignment in getCachedValidAssignmentList(person_object):
+    category_dict = {}
+    for base_category in base_category_list:
+      category_value_list = assignment.getAcquiredValueList(base_category)
+      if category_value_list:
+        for category_value in category_value_list:
+          if base_category in child_category_list:
+            if category_value.getPortalType() not in ('Base Category', 'ERP5 Site'):
+              while category_value.getPortalType() not in ('Base Category', 'ERP5 Site'):
+                category_dict.setdefault(base_category, []).append('%s*' % category_value.getRelativeUrl())
+                category_value = category_value.getParentValue()
             else:
               category_dict.setdefault(base_category, []).append(category_value.getRelativeUrl())
-      category_list.append(category_dict)
+          else:
+            category_dict.setdefault(base_category, []).append(category_value.getRelativeUrl())
+    category_list.append(category_dict)
 
   return category_list
 

product/ERP5Security/ERP5KeyAuthPlugin.py
@@ -58,6 +58,7 @@ from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 from Products.ERP5Type.Utils import bytes2str, str2bytes
 from Products.ERP5Security.ERP5UserManager import ERP5UserManager, \
                                                   _AuthenticationFailure
+from Products.ERP5Security.Utils import hasValidAssignment
 from Products import ERP5Security
 
 
@@ -362,30 +363,11 @@ class ERP5KeyAuthPlugin(ERP5UserManager, CookieAuthHelper):
           raise _AuthenticationFailure()
         user_value = user.getUserValue()
 
-        if True:
-          try:
-            # get assignment list
-            assignment_list = [x for x in user_value.contentValues(portal_type="Assignment") \
-                                 if x.getValidationState() == "open"]
-            valid_assignment_list = []
-            # check dates if exist
-            login_date = DateTime()
-            for assignment in assignment_list:
-              if assignment.getStartDate() is not None and \
-                        assignment.getStartDate() > login_date:
-                continue
-              if assignment.hasStopDate() and \
-                  assignment.getStopDate() < login_date:
-                continue
-              valid_assignment_list.append(assignment)
+        # get assignment list
+        if hasValidAssignment(user_value):
+          return (user.getId(), user.getUserName())
 
-            # validate
-            if len(valid_assignment_list) > 0:
-              return (user.getId(), user.getUserName())
-          finally:
-            pass
-
-          raise _AuthenticationFailure()
+        raise _AuthenticationFailure()
 
       #Cache Method for best performance
       _authenticateCredentials = CachingMethod(_authenticateCredentials,

product/ERP5Security/ERP5LoginUserManager.py
@@ -35,8 +35,8 @@ from Products.PluggableAuthService.utils import classImplements
 from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
 from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin
 from Products.ERP5Type.TransactionalVariable import getTransactionalVariable
-from DateTime import DateTime
 from Products import ERP5Security
+from Products.ERP5Security.Utils import hasValidAssignment
 from AccessControl import SpecialUsers
 from Shared.DC.ZRDB.DA import DatabaseError
 from zLOG import LOG, ERROR
@@ -141,17 +141,7 @@ class ERP5LoginUserManager(BasePlugin):
     if user_value.getValidationState() == 'deleted':
       return
     if user_value.getPortalType() in ('Person', ):
-      now = DateTime()
-      for assignment in user_value.contentValues(portal_type="Assignment"):
-        if assignment.getValidationState() == "open" and (
-          not assignment.hasStartDate() or assignment.getStartDate() <= now
-        ) and (
-          not assignment.hasStopDate() or assignment.getStopDate() >= now
-        ):
-          return True
-      else:
-        return
-    
+      return hasValidAssignment(user=user_value)
     return True
 
 

product/ERP5Security/ERP5UserManager.py
@@ -33,6 +33,7 @@ from ZODB.POSException import ConflictError
 from DateTime import DateTime
 from zLOG import LOG, PROBLEM
 from Products import ERP5Security
+from Products.ERP5Security.Utils import getCachedValidAssignmentList
 
 manage_addERP5UserManagerForm = PageTemplateFile(
   'www/ERP5Security_addERP5UserManager', globals(),
@@ -90,23 +91,6 @@ def getUserByLogin(portal, login, exact_match=True):
   #  by default (feature).
   return [x.getObject() for x in result if not exact_match
                                            or x['reference'] in login]
-@transactional_cached(lambda *args: args)
-def getValidAssignmentList(user):
-  """Returns list of valid assignments."""
-  assignment_list = [x for x in user.contentValues(portal_type="Assignment") if x.getValidationState() == "open"]
-  valid_assignment_list = []
-  # check dates if exist
-  login_date = DateTime()
-  for assignment in assignment_list:
-    if assignment.getStartDate() is not None and \
-           assignment.getStartDate() >= login_date:
-      continue
-    if assignment.getStopDate() is not None and \
-           assignment.getStopDate() <= login_date:
-      continue
-    valid_assignment_list.append(assignment)
-  return valid_assignment_list
-
 class ERP5UserManager(BasePlugin):
   """ PAS plugin for managing users in ERP5
   """
@@ -157,7 +141,7 @@ class ERP5UserManager(BasePlugin):
       try:
 
         if (ignore_password or pw_validate(user.getPassword(), password)) and \
-            len(getValidAssignmentList(user)) and user  \
+            len(getCachedValidAssignmentList(user)) and user  \
             .getValidationState() != 'deleted': #user.getCareerRole() == 'internal':
           return login, login # use same for user_id and login
       finally:

product/ERP5Security/Utils.py
@@ -1,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2026 Nexedi SA and Contributors. All Rights Reserved.
+#                    Rafael Monnerat <rafael@nexedi.com>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+##############################################################################
+
+from DateTime import DateTime
+from Products.ERP5Type.Cache import CachingMethod, transactional_cached
+
+def _getValidAssignmentList(user, return_bool=False, checked_permission=None):
+  """Returns list of valid assignments."""
+  valid_assignment_list = []
+  # check dates if exist
+  kw = {
+    "portal_type": "Assignment"
+  }
+  if checked_permission:
+    kw["checked_permission"] = checked_permission
+
+  now = DateTime()
+  for assignment in user.contentValues(**kw):
+    if assignment.getValidationState() == "open" and (
+      not assignment.hasStartDate() or assignment.getStartDate() <= now
+    ) and (
+      not assignment.hasStopDate() or assignment.getStopDate() >= now
+    ):
+      valid_assignment_list.append(assignment)
+      if return_bool:
+        return True
+
+  if return_bool:
+    return False
+  return valid_assignment_list
+
+def getValidAssignmentList(user, checked_permission=None):
+  """ Return a list of valid assignment. """
+  return _getValidAssignmentList(user=user, checked_permission=checked_permission)
+
+def hasValidAssignment(user):
+  """ Proceed with faster calculation and return a bool"""
+  return _getValidAssignmentList(user=user, return_bool=True)
+
+@transactional_cached(lambda *args: args)
+def getCachedValidAssignmentList(user):
+  """ Returns list of valid assignments for a given user
+    Only calculated once per transaction """
+  return _getValidAssignmentList(user=user)
+

product/ERP5Security/__init__.py
@@ -30,6 +30,13 @@ from Products.PluggableAuthService.PluggableAuthService import registerMultiPlug
 from Products.PluggableAuthService.permissions import ManageGroups
 from Products.ERP5Type import IS_ZOPE2
 
+from AccessControl.SecurityInfo import allow_module
+from AccessControl.SecurityInfo import ModuleSecurityInfo
+
+allow_module('Products.ERP5Security.Utils')
+ModuleSecurityInfo('Products.ERP5Security.Utils').declarePublic(
+  'getValidAssignmentList', 'hasValidAssignment', 'getCachedAssignmentList')
+
 # This user is used to bypass all security checks.
 SUPER_USER = '__erp5security-=__'
 

commit b100514cf39b57a54c792a083b71a1301ba2fcfe

erp5_access_token: fix coding style test
RestrictedAccessToken_getUserValue:33:8:Unused variable 'now' (unused-variable)

Started failing after https://lab.nexedi.com/nexedi/erp5/-/commit/634e4bcb3a77d6b47bd5534e9009e327747c0521

/cc @rafael

bt5/erp5_access_token/SkinTemplateItem/portal_skins/erp5_access_token/RestrictedAccessToken_getUserValue.py
@@ -30,7 +30,6 @@ if access_token_document.getValidationState() == 'validated':
         # authentication)
         if agent_document.getValidationState() == 'deleted':
           return None
-        now = DateTime()
         if not hasValidAssignment(agent_document):
           return None
 