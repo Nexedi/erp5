df873b6f8f9b211c9872f0924d17587bfb3aeaff
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2026-06-29 15:53:08 +0200

    testIngestion: XXXXX

--- product/ERP5OOo/tests/testIngestion.py
+++ product/ERP5OOo/tests/testIngestion.py
@@ -65,7 +65,7 @@ class IngestionTestCase(ERP5TypeTestCase):
       Return the list of required business templates.
     """
     return ('erp5_core_proxy_field_legacy', 'erp5_base',
-            'erp5_ingestion', 'erp5_ingestion_mysql_innodb_catalog',
+            'erp5_ingestion',
             'erp5_web', 'erp5_crm', 'erp5_dms')
 
   def afterSetUp(self):
