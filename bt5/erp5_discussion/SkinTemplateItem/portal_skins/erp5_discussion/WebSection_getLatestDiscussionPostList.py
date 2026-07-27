cfd872b732fc8aa0363f190f49742fb9b3930e9e
Roque Porchetto <roque.porchetto@nexedi.com>
2026-07-27 11:14:21 -0300

    erp5_discussion: rename script

--- bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getLatestDiscussionPostList.py
+++ bt5/erp5_discussion/SkinTemplateItem/portal_skins/erp5_discussion/WebSection_getLatestDiscussionPostList.py
@@ -1,6 +1,6 @@
 """
- Old forum backward compatibility script, wraps new DiscussionForum_getLatestPostList
+ Old forum backward compatibility script, wraps new DiscussionForum_getLatestDiscussionPostList
 """
 
 forum = context.WebSection_getRelatedForum()
-return forum.DiscussionForum_getLatestPostList(**kw)
+return forum.DiscussionForum_getLatestDiscussionPostList(**kw)
