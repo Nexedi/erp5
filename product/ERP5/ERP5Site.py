e506729195fcd6f1bd41fecc6d031dda9c43f7d0
Rafael Monnerat <rafael@nexedi.com>
2026-07-31 18:55:01 +0000

    Update description

--- product/ERP5/ERP5Site.py
+++ product/ERP5/ERP5Site.py
@@ -1855,8 +1855,12 @@ class ERP5Site(ResponseHeaderGenerator, FolderMixIn, PortalObjectBase, CacheCook
   security.declareProtected(Permissions.AccessContentsInformation,
                             'getPortalActorTypeList')
   def getPortalActorTypeList(self):
-    """
-    Returns Actor types.
+    """ The Actor type list encapsulate Person and Workgroup under
+      a common abstraction representing participants capable of
+      performing actions, receiving assignments, and owning 
+      responsibilities. 
+
+      It separates operational roles from legal or business entities.
     """
     return self._getPortalGroupedTypeList('actor') or\
            self._getPortalConfiguration('portal_actor_type_list')
