7b020fbaed690025c72249df3f0b61f0b3ffaadb
Roque Porchetto <roque.porchetto@nexedi.com>
2026-08-13 11:38:22 -0300

    erp5_web_project_ui: improve front page links

--- bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
+++ bt5/erp5_web_project_ui/SkinTemplateItem/portal_skins/erp5_web_project/Base_filterProjectActions.py
@@ -11,10 +11,10 @@ for action_category_name, action_list in actions.items():
   elif (action_category_name == 'object_jio_action'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['post_query']]
   elif (action_category_name == 'project_view'):
+    # Merged into object_view only: nothing consumes a project_view category client side
     if 'object_view' not in filtered_actions:
       filtered_actions['object_view'] = []
     filtered_actions['object_view'].extend(action_list)
-    filtered_actions[action_category_name] = action_list
   elif (action_category_name == 'object_jio_jump'):
     filtered_actions[action_category_name] = [action for action in action_list if action['id'] not in ['jump_to_portal_type']]
   else:
