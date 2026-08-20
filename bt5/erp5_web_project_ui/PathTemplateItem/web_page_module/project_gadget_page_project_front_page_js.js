58b766748658119afa03dc3ee4514ab76416b6c1
Roque Porchetto <roque.porchetto@nexedi.com>
2026-08-20 15:46:59 -0300

    erp5_web_project_ui: front page translation and limit dates constants

--- bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_project_front_page_js.js
+++ bt5/erp5_web_project_ui/PathTemplateItem/web_page_module/project_gadget_page_project_front_page_js.js
@@ -14,27 +14,35 @@
     NAME_SPAN = "name",
     FORUM_LINK_ID_SUFFIX = "forum",
     FORUM_LINK_TYPE = "link",
-    OUTDATED_LABEL = " out of date",
-    FAILED_LABEL = " failed",
+    OUTDATED_LABEL = "out of date",
+    FAILED_LABEL = "failed",
     TEST_RESULT_PORTAL_TYPE = "Test Result",
     QUERY_LIMIT = 100000,
     SUPERVISOR_FIELD_TITLE = "Supervisor",
-    //XXX hardcoded limit dates (3 months for milestones, 3 weeks for documents)
-    //define dates in System Preference Project tab?
-    //date ISO string format: "yyyy-mm-ddThh:mm:ss.mmmm"
-    //JIO query date format:  "yyyy-mm-dd hh:mm:ss"
-    MILESTONE_LIMIT_DATE = new Date(new Date().setDate(new Date().getDate() - 90))
-      .toISOString().substring(0, new Date().toISOString().length - 5).replace("T", " "),
-    DOCUMENT_LIMIT_DATE = new Date(new Date().setDate(new Date().getDate() - 21))
-      .toISOString().substring(0, new Date().toISOString().length - 5).replace("T", " "),
+    MILESTONE_OUTDATED_DAY_COUNT = 90,
+    DOCUMENT_OUTDATED_DAY_COUNT = 21,
+    PORTAL_TYPE_LIST = ["Task", "Bug", "Task Report"],
+    VALID_STATE_LIST = ["planned", "auto_planned", "ordered", "confirmed",
+                        "ready", "stopped", "started", "submitted", "validated"],
+    PROJECT_PAGE_LABEL = "Project Page",
+    EMPTY_LIST_LABEL = "No projects yet.",
+    PAGE_TITLE = "Project Management",
     PORTAL_TITLE_DICT = {"Task": "Tasks",
                          "Test Result" : "Test Results",
                          "Bug" : "Bugs",
                          "Project Milestone" : "Milestones",
                          "Task Report": "Task Reports"},
-    PORTAL_TYPE_LIST = ["Task", "Bug", "Task Report"],
-    VALID_STATE_LIST = ["planned", "auto_planned", "ordered", "confirmed",
-                        "ready", "stopped", "started", "submitted", "validated"];
+    TRANSLATABLE_STRING_LIST = ["Tasks", "Test Results", "Bugs", "Milestones",
+                                "Task Reports", SUPERVISOR_FIELD_TITLE,
+                                PROJECT_PAGE_LABEL, EMPTY_LIST_LABEL,
+                                OUTDATED_LABEL, FAILED_LABEL, PAGE_TITLE];
+
+  function getLimitDate(day_count) {
+    //JIO query date format: "yyyy-mm-dd hh:mm:ss"
+    var date = new Date();
+    date.setDate(date.getDate() - day_count);
+    return date.toISOString().substring(0, 19).replace("T", " ");
+  }
 
   function createMultipleSimpleOrQuery(key, value_list) {
     var i,
@@ -212,6 +220,21 @@
       });
   }
 
+  function getTranslationDict(gadget) {
+    return new RSVP.Queue()
+      .push(function () {
+        return gadget.getTranslationList(TRANSLATABLE_STRING_LIST);
+      })
+      .push(function (translation_list) {
+        var i,
+          translation_dict = {};
+        for (i = 0; i < TRANSLATABLE_STRING_LIST.length; i += 1) {
+          translation_dict[TRANSLATABLE_STRING_LIST[i]] = translation_list[i];
+        }
+        return translation_dict;
+      });
+  }
+
   function getProjectList(gadget) {
     var project_query = Query.objectToSearchText(
       getComplexQuery({"portal_type" : "Project",
@@ -281,7 +304,7 @@
     }));
   }
 
-  function renderProjectList(gadget, project_list) {
+  function renderProjectList(gadget, project_list, translation_dict) {
     var i,
       project_html,
       left_div_html,
@@ -306,11 +329,12 @@
         project_link = domsugar('a', {
           href: project_url,
           id: project_title + "-project_page_link"
-        }, ["(Project Page)"]),
+        }, ["(" + translation_dict[PROJECT_PAGE_LABEL] + ")"]),
         title_div = domsugar('div', { class: "project-title" },
                              [project_title_span, project_link]),
         left_info_div = domsugar('div', { class: "project-left" }),
-        supervisor_field_label = domsugar('label', {}, [SUPERVISOR_FIELD_TITLE]),
+        supervisor_field_label = domsugar('label', {},
+                                          [translation_dict[SUPERVISOR_FIELD_TITLE]]),
         supervisor_value_link = domsugar('a', {
           href: supervisor_url
         }, [supervisor]),
@@ -358,7 +382,8 @@
         open_bracket_span = domsugar('span', {}, ["("]),
         close_bracket_span = domsugar('span', {}, [")"]),
         outdated_label_span = domsugar('span', {}, [
-          (portal_type === TEST_RESULT_PORTAL_TYPE) ? FAILED_LABEL : OUTDATED_LABEL
+          " " + translation_dict[(portal_type === TEST_RESULT_PORTAL_TYPE) ?
+                                 FAILED_LABEL : OUTDATED_LABEL]
         ]),
         number_span = domsugar('span', {
           id: getProjectHtlmElementId(project_id, portal_type, NUMBER_SPAN),
@@ -443,7 +468,7 @@
         if (project_list.length === 0) {
           ul_list.appendChild(domsugar('li', {
             class: "empty-state-message",
-            text: "No projects yet."
+            text: translation_dict[EMPTY_LIST_LABEL]
           }));
           return;
         }
@@ -472,13 +497,15 @@
                   return result_dict.test_result_url_list[i];
                 }
               })(type);
-              left_line_html = createProjectLineHtmlElement(project_list[i].id, type,
-                                                            line_url,
-                                                            ((PORTAL_TITLE_DICT
-                                                              .hasOwnProperty(type)) ?
-                                                             PORTAL_TITLE_DICT[type] :
-                                                             type),
-                                                            0, 0, NONE_STATUS);
+              left_line_html = createProjectLineHtmlElement(
+                project_list[i].id,
+                type,
+                line_url,
+                translation_dict[PORTAL_TITLE_DICT[type]],
+                0,
+                0,
+                NONE_STATUS
+              );
               left_div_html.appendChild(left_line_html);
             }
           }
@@ -496,12 +523,17 @@
     .declareAcquiredMethod("getUrlForList", "getUrlForList")
     .declareAcquiredMethod("jio_allDocs", "jio_allDocs")
     .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("getTranslationList", "getTranslationList")
 
     .declareMethod('render', function (options) {
       var gadget = this;
-      return getProjectList(gadget)
-        .push(function (project_list) {
-          options.project_list = project_list;
+      return new RSVP.Queue()
+        .push(function () {
+          return RSVP.all([getProjectList(gadget), getTranslationDict(gadget)]);
+        })
+        .push(function (result_list) {
+          options.project_list = result_list[0];
+          options.translation_dict = result_list[1];
           return gadget.changeState(options);
         });
     })
@@ -509,13 +541,14 @@
     .onStateChange(function () {
       var gadget = this;
       return gadget.updateHeader({
-        page_title: 'Project Management'
+        page_title: gadget.state.translation_dict[PAGE_TITLE]
       });
     })
 
     .declareService(function () {
       var gadget = this;
-      return renderProjectList(gadget, gadget.state.project_list)
+      return renderProjectList(gadget, gadget.state.project_list,
+                               gadget.state.translation_dict)
         .push(function () {
           //run the rest of queries and render async
           gadget.detachRenderMilestoneInfo();
@@ -531,7 +564,7 @@
     })
 
     .declareJob("detachRenderOutdatedMilestoneInfo", function () {
-      return renderMilestoneLineList(this, MILESTONE_LIMIT_DATE);
+      return renderMilestoneLineList(this, getLimitDate(MILESTONE_OUTDATED_DAY_COUNT));
     })
 
     .declareJob("detachRenderProjectDocumentInfo", function () {
@@ -539,7 +572,7 @@
     })
 
     .declareJob("detachRenderOutdatedDocumentInfo", function () {
-      return renderProjectDocumentLines(this, DOCUMENT_LIMIT_DATE);
+      return renderProjectDocumentLines(this, getLimitDate(DOCUMENT_OUTDATED_DAY_COUNT));
     })
 
     .declareJob("detachRenderTestResultInfo", function () {
